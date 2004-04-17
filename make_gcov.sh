d9154d11760bc650ca396d19bd31240616d4f80a
greg@kroah.com <greg@kroah.com>
2004-04-17 00:09:17 -0700

    [PATCH] add scripts to run gcov for udev from Leann Ogasawara <ogasawara@osdl.org>

---
+++ make_gcov.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+#
+# gcov capability for udev
+#
+# Provides code coverage analysis for udev.
+#
+# make_gcov.sh assumes the same same default parameters as make, but also
+# accepts the same parameters as make (see README file in udev/ for
+# parameter info).  There is one exception, klibc can not be used with
+# gcov as it will not compile cleanly.
+#
+# make_gcov.sh then overrides CFLAGS to strip out optimization in order
+# for gcov to get correct code coverage analysis.
+#
+# Leann Ogasawara <ogasawara@osdl.org>, April 2004
+
+# clean up udev dir
+clean_udev () {
+	find -name "*.da" -exec rm -f "{}" \;
+	find -name "*.bb" -exec rm -f "{}" \;
+	find -name "*.bbg" -exec rm -f "{}" \;
+	find -name "*.gcov" -exec rm -f "{}" \;
+	make clean
+}
+
+PWD=`pwd`
+GCCINCDIR=`gcc -print-search-dirs | sed -ne "s/install: \(.*\)/\1include/gp"`
+LIBSYSFS="-I$PWD/libsysfs"
+WARNINGS="-Wall -Wshadow -Wstrict-prototypes -Wmissing-prototypes -Wmissing-declarations"
+GCC="-I$GCCINCDIR"
+USE_LOG="-DLOG"
+DEBUG="-D_GNU_SOURCE"
+GCOV_FLAGS="-pipe -fprofile-arcs -ftest-coverage"
+
+for i in $*; do
+	pre=`echo $i | sed 's/=.*//g'`
+	post=`echo $i | sed 's/.*=//g'`
+	if [ $pre = "USE_KLIBC" ] && [ $post = "true" ]; then
+		echo "cannot use gcov with klibc, will not compile"
+		exit
+	elif [ $pre = "USE_LOG" ] && [ $post = "false" ]; then
+		USE_LOG=""
+	elif [ $pre = "DEBUG" ] && [ $post = "true" ]; then
+		DEBUG="-g -DDEBUG -D_GNU_SOURCE"
+	elif [ $pre = "clean" ]; then
+		clean_udev
+		exit
+	fi
+done
+
+clean_udev
+
+make $* CFLAGS="$WARNINGS $GCOV_FLAGS $USE_LOG $DEBUG $GCC $LIBSYSFS"
---
+++ run_gcov.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+#
+# run gcov on udev
+#
+# Generate code coverage analysis for udev files
+#
+# This requires that you compiled udev with gcov flags i.e.
+# you should have compiled udev with the make_gcov.sh script.
+#
+# Leann Ogasawara <ogasawara@osdl.org>, April 2004
+
+PWD=`pwd`
+
+# check if root else may not have access to *.da files
+# and gcov analysis will fail.
+if [ $(id -u) -ne 0 ]; then
+	echo "please become root before executing run_gcov.sh"
+	exit 1
+fi
+
+echo > udev_gcov.txt
+echo "CODE COVERAGE ANALYSIS FOR UDEV" >> udev_gcov.txt
+echo  >> udev_gcov.txt
+
+for file in `find -maxdepth 1 -name "*.bb"`; do
+	name=`basename $file .bb`
+	echo "################" >> udev_gcov.txt
+	echo "$name.c" >> udev_gcov.txt
+	echo "################" >> udev_gcov.txt
+	if [ -e "$name.da" ]; then
+		gcov -l "$name.c" >> udev_gcov.txt 2>&1
+	else
+		echo "code for $name.c was never executed" >> udev_gcov.txt 2>&1
+		echo "no code coverage analysis to be done" >> udev_gcov.txt 2>&1
+	fi
+	echo >> udev_gcov.txt
+done
+
+echo "udev gcov analysis done.  View udev_gcov.txt for results."
