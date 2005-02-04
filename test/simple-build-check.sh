61599bdd06b059e7a5792b0417828d0a87541038
kay.sievers@vrfy.org <kay.sievers@vrfy.org>
2005-02-04 21:57:39 +0100

    [PATCH] simple_build_check: make it possible to pass KERNEL_DIR

--- test/simple-build-check.sh
+++ test/simple-build-check.sh
@@ -1,26 +1,29 @@
 #/bin/sh
 
-EXTRA="extras/chassis_id extras/scsi_id extras/volume_id"
+EXTRAS="extras/chassis_id extras/scsi_id extras/volume_id"
 
-make spotless EXTRAS="$EXTRA" >/dev/null
-make all EXTRAS="$EXTRA" || exit
+[ -z "$KERNEL_DIR" ] && KERNEL_DIR=/lib/modules/`uname -r`/build
+echo KERNEL_DIR: "$KERNEL_DIR"
+
+make spotless EXTRAS="$EXTRAS" >/dev/null
+make all EXTRAS="$EXTRAS" || exit
 echo -e "\n\n"
 
-make spotless EXTRAS="$EXTRA" >/dev/null
-make all DEBUG=true EXTRAS="$EXTRA"  || exit
+make spotless EXTRAS="$EXTRAS" >/dev/null
+make all DEBUG=true EXTRAS="$EXTRAS"  || exit
 echo -e "\n\n"
 
-make spotless EXTRAS="$EXTRA" >/dev/null
-make all USE_LOG=false EXTRAS="$EXTRA"  || exit
+make spotless EXTRAS="$EXTRAS" >/dev/null
+make all USE_LOG=false EXTRAS="$EXTRAS"  || exit
 echo -e "\n\n"
 
-make spotless EXTRAS="$EXTRA" >/dev/null
-make all USE_KLIBC=true DEBUG=true EXTRAS="$EXTRA" || exit
+make spotless EXTRAS="$EXTRAS" >/dev/null
+make all USE_KLIBC=true DEBUG=true EXTRAS="$EXTRAS" KERNEL_DIR="$KERNEL_DIR" || exit
 echo -e "\n\n"
 
-make spotless EXTRAS="$EXTRA" >/dev/null
-make all USE_KLIBC=true USE_LOG=false EXTRAS="$EXTRA" || exit
+make spotless EXTRAS="$EXTRAS" >/dev/null
+make all USE_KLIBC=true USE_LOG=false EXTRAS="$EXTRAS" KERNEL_DIR="$KERNEL_DIR" || exit
 echo -e "\n\n"
 
-make spotless EXTRAS="$EXTRA" >/dev/null
+make spotless EXTRAS="$EXTRAS" >/dev/null
 
