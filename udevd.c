a105fe495bdc95a24eaf2db557104534f6e1db34
Marco d'Itri <md@Linux.IT>
2005-06-23 00:28:54 +0200

    [PATCH] #define NETLINK_KOBJECT_UEVENT
    On many systems the system kernel headers are not recent enough to
    compile udev.

--- udevd.c
+++ udevd.c
@@ -48,6 +48,10 @@
 #include "udevd.h"
 #include "logging.h"
 
+#ifndef NETLINK_KOBJECT_UEVENT
+#define NETLINK_KOBJECT_UEVENT 15
+#endif
+
 /* global variables*/
 static int udevd_sock;
 static int uevent_netlink_sock;
