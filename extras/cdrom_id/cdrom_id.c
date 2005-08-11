8f6919e6390d79a2765e1dc9b0057412e987dce2
Greg KH <greg@press.(none)>
2005-08-11 13:59:21 -0700

    Created cdrom_id program to make it easier to determine cdrom types
    Based on the framework from ata_id by Kay.
    Now we can drop the cdsymlinks.sh and .c files
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

---
+++ extras/cdrom_id/cdrom_id.c
@@ -0,0 +1,129 @@
+/*
+ * cdrom_id - determines the capabilities of cdrom drives
+ *
+ * Copyright (C) 2005 Greg Kroah-Hartman <gregkh@suse.de>
+ *
+ *	This program is free software; you can redistribute it and/or modify it
+ *	under the terms of the GNU General Public License as published by the
+ *	Free Software Foundation version 2 of the License.
+ *
+ * Framework based on ata_id which is:
+ *	Copyright (C) 2005 Kay Sievers <kay.sievers@vrfy.org>
+ *
+ */
+
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE 1
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <ctype.h>
+#include <errno.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <linux/types.h>
+#include <linux/cdrom.h>
+
+#include "../../logging.h"
+#include "../../udev_utils.h"
+
+#ifdef USE_LOG
+void log_message(int priority, const char *format, ...)
+{
+	va_list args;
+	static int udev_log = -1;
+
+	if (udev_log == -1) {
+		const char *value;
+
+		value = getenv("UDEV_LOG");
+		if (value)
+			udev_log = log_priority(value);
+		else
+			udev_log = LOG_ERR;
+	}
+
+	if (priority > udev_log)
+		return;
+
+	va_start(args, format);
+	vsyslog(priority, format, args);
+	va_end(args);
+}
+#endif
+
+int main(int argc, char *argv[])
+{
+	const char *node = NULL;
+	int i;
+	int export = 0;
+	int fd;
+	int rc = 0;
+	int result;
+
+	logging_init("cdrom_id");
+
+	for (i = 1 ; i < argc; i++) {
+		char *arg = argv[i];
+
+		if (strcmp(arg, "--export") == 0) {
+			export = 1;
+		} else
+			node = arg;
+	}
+	if (!node) {
+		err("no node specified");
+		rc = 1;
+		goto exit;
+	}
+
+	fd = open(node, O_RDONLY);
+	if (fd < 0)
+		if (errno == ENOMEDIUM)
+			fd = open(node, O_RDONLY|O_NONBLOCK);
+	if (fd < 0) {
+		err("unable to open '%s'", node);
+		rc = 1;
+		goto exit;
+	}
+
+	result = ioctl(fd, CDROM_GET_CAPABILITY);
+	if (result < 0) {
+		err("CDROM_GET_CABILITY failed for '%s'", node);
+		rc = 3;
+		goto close;
+	}
+
+	printf("ID_CDC=true\n");
+
+	if (result & CDC_CD_R)
+		printf("ID_CDC_CD_R=true\n");
+	if (result & CDC_CD_RW)
+		printf("ID_CDC_CD_RW=true\n");
+
+	if (result & CDC_DVD)
+		printf("ID_CDC_DVD=true\n");
+	if (result & CDC_DVD_R)
+		printf("ID_CDC_DVD_R=true\n");
+	if (result & CDC_DVD_RAM)
+		printf("ID_CDC_DVD_RAM=true\n");
+
+	if (result & CDC_MRW)
+		printf("ID_CDC_MRW=true\n");
+	if (result & CDC_MRW_W)
+		printf("ID_CDC_MRW_W=true\n");
+	
+	if (result & CDC_RAM)
+		printf("ID_CDC_RAM=true\n");
+	goto close;
+
+close:
+	close(fd);
+exit:
+	logging_close();
+	return rc;
+}
