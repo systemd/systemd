aab571f3621de595dfb2b014040520804c71796c
Kay Sievers <kay.sievers@suse.de>
2005-09-08 12:24:18 +0200

    fix useless but funny name_cdrom.pl script to work again
    Signed-off-by: Kay Sievers <kay.sievers@suse.de>

--- extras/name_cdrom.pl
+++ extras/name_cdrom.pl
@@ -2,45 +2,31 @@
 
 # a horribly funny script that shows how flexible udev can really be
 # This is to be executed by udev with the following rules:
-# KERNEL="[hs]d[a-z]", PROGRAM="name_cdrom.pl %M %m", NAME="%c{1}", SYMLINK="cdrom"
+#
+# KERNEL="hd*[!0-9]|sr*", PROGRAM="name_cdrom.pl $tempnode", SYMLINK+="%c"
 
 use strict;
 use warnings;
 
-use CDDB_get qw( get_cddb );
-
-my $dev_node = "/tmp/cd_foo";
+use CDDB_get qw(get_cddb);
 
 # following variables just need to be declared if different from defaults
 my %config;
-$config{CDDB_HOST}="freedb.freedb.org";		# set cddb host
-$config{CDDB_PORT}=8880;			# set cddb port
-$config{CDDB_MODE}="cddb";			# set cddb mode: cddb or http
-$config{CD_DEVICE}="$dev_node";			# set cd device
+$config{'CDDB_HOST'} = "freedb.freedb.org";	# set cddb host
+$config{'CDDB_PORT'} = 8880;			# set cddb port
+$config{'CDDB_MODE'} = "cddb";			# set cddb mode: cddb or http
+$config{'CD_DEVICE'} = $ARGV[0];		# set cd device
+$config{'input'} = 0; 				# no user interaction
 
-# No user interaction, this is a automated script!
-$config{input}=0;
+my %cd = get_cddb(\%config);
 
-my $major = $ARGV[0];
-my $minor = $ARGV[1];
-
-# create our temp device node to read the cd info from
-unlink($dev_node);
-if (system("mknod $dev_node b $major $minor")) {
-       die "bad mknod failed";
+if (!defined $cd{title}) {
+	exit 1;
 }
 
-# get it on
-my %cd=get_cddb(\%config);
+# print out our cd name
+$cd{artist} =~ s/ /_/g;
+$cd{title} =~ s/ /_/g;
+print "$cd{artist}-$cd{title}\n";
 
-# remove the dev node we just created
-unlink($dev_node);
-
-# print out our cd name if we have found it or skip rule by nonzero exit
-if (defined $cd{title}) {
-	$cd{artist} =~ s/ /_/g;
-	$cd{title} =~ s/ /_/g;
-	print "$cd{artist}-$cd{title}\n";
-} else {
-	exit -1;
-}
+exit 0;
