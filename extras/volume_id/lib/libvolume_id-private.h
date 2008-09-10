ef5c2d048c875b6307fdc0e10f6b703fcb57772e
Kay Sievers <kay.sievers@vrfy.org>
2008-09-10 02:31:11 +0200

    volume_id: merge util.h in libvolume_id-private.h

--- extras/volume_id/lib/libvolume_id-private.h
+++ extras/volume_id/lib/libvolume_id-private.h
@@ -13,14 +13,71 @@
 
 #include <stdint.h>
 #include <stddef.h>
+#include <endian.h>
+#include <byteswap.h>
+#include <syslog.h>
 
 #include "libvolume_id.h"
 
-#define VOLUME_ID_LABEL_SIZE		64
-#define VOLUME_ID_UUID_SIZE		36
-#define VOLUME_ID_FORMAT_SIZE		32
-#define VOLUME_ID_PATH_MAX		256
-#define VOLUME_ID_PARTITIONS_MAX	256
+#define ALLOWED_CHARS			"#+-.:=@_"
+
+#ifndef PACKED
+#define PACKED				__attribute__((packed))
+#endif
+
+#define err(format, arg...)	volume_id_log_fn(LOG_ERR, __FILE__, __LINE__, format, ##arg)
+#define info(format, arg...)	volume_id_log_fn(LOG_INFO, __FILE__, __LINE__, format, ##arg)
+#ifdef DEBUG
+#define dbg(format, arg...)	volume_id_log_fn(LOG_DEBUG, __FILE__, __LINE__, format, ##arg)
+#else
+#define dbg(format, arg...)	do { } while (0)
+#endif
+
+#if (__BYTE_ORDER == __LITTLE_ENDIAN)
+#define le16_to_cpu(x) (x)
+#define le32_to_cpu(x) (x)
+#define le64_to_cpu(x) (x)
+#define be16_to_cpu(x) bswap_16(x)
+#define be32_to_cpu(x) bswap_32(x)
+#define cpu_to_le16(x) (x)
+#define cpu_to_le32(x) (x)
+#define cpu_to_be32(x) bswap_32(x)
+#elif (__BYTE_ORDER == __BIG_ENDIAN)
+#define le16_to_cpu(x) bswap_16(x)
+#define le32_to_cpu(x) bswap_32(x)
+#define le64_to_cpu(x) bswap_64(x)
+#define be16_to_cpu(x) (x)
+#define be32_to_cpu(x) (x)
+#define cpu_to_le16(x) bswap_16(x)
+#define cpu_to_le32(x) bswap_32(x)
+#define cpu_to_be32(x) (x)
+#endif /* __BYTE_ORDER */
+
+enum uuid_format {
+	UUID_STRING,
+	UUID_HEX_STRING,
+	UUID_DCE,
+	UUID_DOS,
+	UUID_64BIT_LE,
+	UUID_MD,
+	UUID_LVM,
+};
+
+enum endian {
+	LE = 0,
+	BE = 1
+};
+
+#define VOLUME_ID_LABEL_SIZE			64
+#define VOLUME_ID_UUID_SIZE			36
+#define VOLUME_ID_FORMAT_SIZE			32
+#define VOLUME_ID_PATH_MAX			256
+#define VOLUME_ID_PARTITIONS_MAX		256
+
+/* size of superblock buffer, reiserfs block is at 64k */
+#define SB_BUFFER_SIZE				0x11000
+/* size of seek buffer, FAT cluster is 32k max */
+#define SEEK_BUFFER_SIZE			0x10000
 
 enum volume_id_usage {
 	VOLUME_ID_UNUSED,
@@ -51,6 +108,17 @@ struct volume_id {
 	uint64_t	seekbuf_off;
 	size_t		seekbuf_len;
 };
+
+/* utils */
+extern int volume_id_utf8_encoded_valid_unichar(const char *str);
+extern size_t volume_id_set_unicode16(uint8_t *str, size_t len, const uint8_t *buf, enum endian endianess, size_t count);
+extern void volume_id_set_usage(struct volume_id *id, enum volume_id_usage usage_id);
+extern void volume_id_set_label_raw(struct volume_id *id, const uint8_t *buf, size_t count);
+extern void volume_id_set_label_string(struct volume_id *id, const uint8_t *buf, size_t count);
+extern void volume_id_set_label_unicode16(struct volume_id *id, const uint8_t *buf, enum endian endianess, size_t count);
+extern void volume_id_set_uuid(struct volume_id *id, const uint8_t *buf, size_t len, enum uuid_format format);
+extern uint8_t *volume_id_get_buffer(struct volume_id *id, uint64_t off, size_t len);
+extern void volume_id_free_buffer(struct volume_id *id);
 
 /* filesystems */
 extern int volume_id_probe_cramfs(struct volume_id *id, uint64_t off, uint64_t size);
