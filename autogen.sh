d4e7bda70c9ae96df91660e62b160e7ae959e43c
Kay Sievers <kay.sievers@vrfy.org>
2008-12-19 01:30:41 +0100

    build: just use autoreconf -i
    Thanks to Scott for mentioning!

--- autogen.sh
+++ autogen.sh
@@ -1,52 +1,12 @@
 #!/bin/sh -e
 
-(autoconf --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have autoconf installed to generate the build system."
-	echo
-	exit 1
-}
-(libtoolize --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have libtool installed to generate the build system."
-	echo
-	exit 1
-}
-(autoheader --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have autoheader installed to generate the build system."
-	echo
-	exit 1
-}
-(automake --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have automake installed to generate the build system."
-	echo
-	exit 1
-}
-
-test -f udev/udevd.c || {
-	echo "You must run this script in the top-level source directory"
-	exit 1
-}
-
-echo "   aclocal:    $(aclocal --version | head -1)"
-aclocal
-echo "   autoconf:   $(autoconf --version | head -1)"
-autoconf
-echo "   libtool:   $(automake --version | head -1)"
-libtoolize --force
-echo "   autoheader: $(autoheader --version | head -1)"
-autoheader
-echo "   automake:   $(automake --version | head -1)"
-automake --add-missing
+autoreconf -i
 
 CFLAGS="-g -Wall \
 -Wmissing-declarations -Wmissing-prototypes \
 -Wnested-externs -Wpointer-arith \
 -Wpointer-arith -Wsign-compare -Wchar-subscripts \
 -Wstrict-prototypes -Wshadow"
-
 args="--prefix=/usr --exec-prefix= --sysconfdir=/etc --with-selinux"
 libdir=$(basename $(cd /lib/$(gcc -print-multi-os-directory); pwd))
 
@@ -68,9 +28,7 @@ case "$1" in
 	*clean)
 		./configure
 		make maintainer-clean
-		find . -name Makefile.in | xargs -r rm
-		rm -f depcomp aclocal.m4 config.h.in configure install-sh
-		rm -f missing config.guess config.sub ltmain.sh
+		git clean -f -X
 		exit 0
 		;;
 	*)
