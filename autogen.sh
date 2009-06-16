bb0059f36e14cca41bdc242b13f39d57e8b7884d
Kay Sievers <kay.sievers@vrfy.org>
2009-06-16 21:29:13 +0200

    autogen.sh: make "CFLAGS=-O0 ./autogen.sh" working

--- autogen.sh
+++ autogen.sh
@@ -3,17 +3,25 @@
 gtkdocize
 autoreconf --install --symlink
 
-CFLAGS="-g -Wall \
+MYCFLAGS="-g -Wall \
 -Wmissing-declarations -Wmissing-prototypes \
 -Wnested-externs -Wpointer-arith \
 -Wpointer-arith -Wsign-compare -Wchar-subscripts \
 -Wstrict-prototypes -Wshadow \
 -Wformat=2 -Wtype-limits"
 
+case "$CFLAGS" in
+	*-O[0-9]*)
+		;;
+	*)
+		MYCFLAGS="$MYCFLAGS -O2"
+		;;
+esac
+
 libdirname=$(basename $(cd /lib/$(gcc -print-multi-os-directory); pwd))
 args="--prefix=/usr --exec-prefix= --sysconfdir=/etc \
 --libdir=/usr/$libdirname --with-libdir-name=$libdirname \
 --with-selinux --enable-gtk-doc"
 
-export CFLAGS="$CFLAGS -O2"
+export CFLAGS="$CFLAGS $MYCFLAGS"
 ./configure $args $@
