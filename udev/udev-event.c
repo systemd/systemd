d5b5a611aeb6c78a9290ea31ad62dc039401fb44
Kay Sievers <kay.sievers@vrfy.org>
2009-07-23 20:28:16 +0200

    format names are not case insensitive

--- udev/udev-event.c
+++ udev/udev-event.c
@@ -126,7 +126,7 @@ size_t udev_event_apply_format(struct udev_event *event, const char *src, char *
 				}
 
 				for (i = 0; i < ARRAY_SIZE(map); i++) {
-					if (strncasecmp(&from[1], map[i].name, strlen(map[i].name)) == 0) {
+					if (strncmp(&from[1], map[i].name, strlen(map[i].name)) == 0) {
 						type = map[i].type;
 						from += strlen(map[i].name)+1;
 						dbg(event->udev, "will substitute format name '%s'\n", map[i].name);
