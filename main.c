8f5847c48e53882b3f94ee2693e65a23edbf8295
Lennart Poettering <lennart@poettering.net>
2010-01-27 00:20:21 +0100

    enable startup job again

--- main.c
+++ main.c
@@ -26,10 +26,10 @@ int main(int argc, char *argv[]) {
                 goto finish;
         }
 
-        /* if ((r = manager_add_job(m, JOB_START, target, JOB_REPLACE, false, &job)) < 0) { */
-        /*         log_error("Failed to start default target: %s", strerror(-r)); */
-        /*         goto finish; */
-        /* } */
+        if ((r = manager_add_job(m, JOB_START, target, JOB_REPLACE, false, &job)) < 0) {
+                log_error("Failed to start default target: %s", strerror(-r));
+                goto finish;
+        }
 
         printf("→ By units:\n");
         manager_dump_units(m, stdout, "\t");
