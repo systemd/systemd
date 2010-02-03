09a3bb9302e3c2562648e9a1ce7b2c800249852a
Lennart Poettering <lennart@poettering.net>
2010-02-03 17:23:34 +0100

    execute: drop now unnecessary GCC pragma again

--- execute.c
+++ execute.c
@@ -19,8 +19,6 @@
   along with systemd; If not, see <http://www.gnu.org/licenses/>.
 ***/
 
-#pragma GCC diagnostic ignored "-Wattributes"
-
 #include <assert.h>
 #include <dirent.h>
 #include <errno.h>
