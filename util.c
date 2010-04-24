57cd2192d0085737bbc745756b97a40a20711b80
Lennart Poettering <lennart@poettering.net>
2010-04-24 02:32:07 +0200

    util: when releasing terminal, temporarily disable SIGHUP

--- util.c
+++ util.c
@@ -1677,12 +1677,23 @@ fail:
 
 int release_terminal(void) {
         int r = 0, fd;
+        struct sigaction sa_old, sa_new;
 
-        if ((fd = open("/dev/tty", O_RDWR)) < 0)
+        if ((fd = open("/dev/tty", O_RDWR|O_NOCTTY|O_NDELAY)) < 0)
                 return -errno;
+
+        /* Temporarily ignore SIGHUP, so that we don't get SIGHUP'ed
+         * by our own TIOCNOTTY */
+
+        zero(sa_new);
+        sa_new.sa_handler = SIG_IGN;
+        sa_new.sa_flags = SA_RESTART;
+        assert_se(sigaction(SIGHUP, &sa_new, &sa_old) == 0);
 
         if (ioctl(fd, TIOCNOTTY) < 0)
                 r = -errno;
+
+        assert_se(sigaction(SIGHUP, &sa_old, NULL) == 0);
 
         close_nointr_nofail(fd);
         return r;
