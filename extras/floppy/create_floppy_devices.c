8b5651bb009bf892b3a5537c5f36b8891f238aa4
Kay Sievers <kay.sievers@vrfy.org>
2010-06-25 11:39:21 +0200

    switch a few left-over from GPLv2 to GPLv2 or later

--- extras/floppy/create_floppy_devices.c
+++ extras/floppy/create_floppy_devices.c
@@ -1,17 +1,23 @@
 /*
- * create_floppy_devices
- *
  * Create all possible floppy device based on the CMOS type.
  * Based upon code from drivers/block/floppy.c
  *
  * Copyright(C) 2005, SUSE Linux Products GmbH
  *
- * Author:
- *	Hannes Reinecke <hare@suse.de>
+ * Author: Hannes Reinecke <hare@suse.de>
  *
- *	This program is free software; you can redistribute it and/or modify it
- *	under the terms of the GNU General Public License as published by the
- *	Free Software Foundation version 2 of the License.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <stdio.h>
