99f08d14a7e4210bdb97b8c9d0752ec318ecca30
Lennart Poettering <lennart@poettering.net>
2010-11-15 04:24:04 +0100

    load-dropin: add support for .requires directories

--- src/load-dropin.c
+++ src/load-dropin.c
@@ -28,10 +28,13 @@
 #include "strv.h"
 #include "unit-name.h"
 
-static int iterate_dir(Unit *u, const char *path) {
+static int iterate_dir(Unit *u, const char *path, UnitDependency dependency) {
         DIR *d;
         struct dirent *de;
         int r;
+
+        assert(u);
+        assert(path);
 
         if (!(d = opendir(path))) {
 
@@ -52,7 +55,7 @@ static int iterate_dir(Unit *u, const char *path) {
                         goto finish;
                 }
 
-                r = unit_add_dependency_by_name(u, UNIT_WANTS, de->d_name, f, true);
+                r = unit_add_dependency_by_name(u, dependency, de->d_name, f, true);
                 free(f);
 
                 if (r < 0)
@@ -66,9 +69,57 @@ finish:
         return r;
 }
 
+static int process_dir(Unit *u, const char *unit_path, const char *name, const char *suffix, UnitDependency dependency) {
+        int r;
+        char *path;
+
+        assert(u);
+        assert(unit_path);
+        assert(name);
+        assert(suffix);
+
+        if (asprintf(&path, "%s/%s%s", unit_path, name, suffix) < 0)
+                return -ENOMEM;
+
+        if (u->meta.manager->unit_path_cache &&
+            !set_get(u->meta.manager->unit_path_cache, path))
+                r = 0;
+        else
+                r = iterate_dir(u, path, dependency);
+        free(path);
+
+        if (r < 0)
+                return r;
+
+        if (u->meta.instance) {
+                char *template;
+                /* Also try the template dir */
+
+                if (!(template = unit_name_template(name)))
+                        return -ENOMEM;
+
+                r = asprintf(&path, "%s/%s%s", unit_path, template, suffix);
+                free(template);
+
+                if (r < 0)
+                        return -ENOMEM;
+
+                if (u->meta.manager->unit_path_cache &&
+                    !set_get(u->meta.manager->unit_path_cache, path))
+                        r = 0;
+                else
+                        r = iterate_dir(u, path, dependency);
+                free(path);
+
+                if (r < 0)
+                        return r;
+        }
+
+        return 0;
+}
+
 int unit_load_dropin(Unit *u) {
         Iterator i;
-        int r;
         char *t;
 
         assert(u);
@@ -76,48 +127,16 @@ int unit_load_dropin(Unit *u) {
         /* Load dependencies from supplementary drop-in directories */
 
         SET_FOREACH(t, u->meta.names, i) {
-                char *path;
                 char **p;
 
                 STRV_FOREACH(p, u->meta.manager->lookup_paths.unit_path) {
+                        int r;
 
-                        if (asprintf(&path, "%s/%s.wants", *p, t) < 0)
-                                return -ENOMEM;
-
-                        if (u->meta.manager->unit_path_cache &&
-                            !set_get(u->meta.manager->unit_path_cache, path))
-                                r = 0;
-                        else
-                                r = iterate_dir(u, path);
-                        free(path);
-
-                        if (r < 0)
+                        if ((r = process_dir(u, *p, t, ".wants", UNIT_WANTS)) < 0)
                                 return r;
 
-                        if (u->meta.instance) {
-                                char *template;
-                                /* Also try the template dir */
-
-                                if (!(template = unit_name_template(t)))
-                                        return -ENOMEM;
-
-                                r = asprintf(&path, "%s/%s.wants", *p, template);
-                                free(template);
-
-                                if (r < 0)
-                                        return -ENOMEM;
-
-                                if (u->meta.manager->unit_path_cache &&
-                                    !set_get(u->meta.manager->unit_path_cache, path))
-                                        r = 0;
-                                else
-                                        r = iterate_dir(u, path);
-                                free(path);
-
-                                if (r < 0)
-                                        return r;
-                        }
-
+                        if ((r = process_dir(u, *p, t, ".requires", UNIT_REQUIRES)) < 0)
+                                return r;
                 }
         }
 
