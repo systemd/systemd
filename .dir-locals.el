85f0c93a50c14cebf0be2740de64c14f27ac61b3
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2011-03-07 19:23:04 +0100

    Keep emacs configuration in one configuration file.
    On 07.03.2011 19:04, Lennart Poettering wrote:
    >
    > Oh, I had assumed that .vimrc trick would allow project-wide vim
    > modelines without having to edit each and every single file.
    >
    > Currently, every file does contain an emacs modeline at the top. It
    > would be fair I guess to add a vim modeline to all those files too, even
    > though it's not necessarily pretty.
    Hi,
    maybe it makes sense to go in the opposite direction: add .dir-locals.el
    in the top directory. The settings are actually identical in all .[ch]
    files.

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+; Sets emacs variables based on mode.
+; A list of (major-mode . ((var1 . value1) (var2 . value2)))
+; Mode can be nil, which gives default values.
+
+((nil . ((indent-tabs-mode . nil)
+         (tab-width . 8)))
+)
