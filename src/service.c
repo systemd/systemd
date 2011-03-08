e527618da5f4521e884a9b0c8c7d0a7a8e859749
Lennart Poettering <lennart@poettering.net>
2011-03-08 18:51:20 +0100

    service: use uppercase SYSV prefix to make it easier to recognize

--- src/service.c
+++ src/service.c
@@ -835,7 +835,7 @@ static int service_load_sysv_path(Service *s, const char *path) {
         if (description) {
                 char *d;
 
-                if (!(d = strappend(s->sysv_has_lsb ? "LSB: " : "SysV: ", description))) {
+                if (!(d = strappend(s->sysv_has_lsb ? "LSB: " : "SYSV: ", description))) {
                         r = -ENOMEM;
                         goto finish;
                 }
