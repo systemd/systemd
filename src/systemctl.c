b74949bcdc65982a5eaca64c836f4d2a3b8a8d0c
Lennart Poettering <lennart@poettering.net>
2011-03-17 03:42:25 +0100

    chkconfig: check against runlevel 5 instead of 3, since it is a superset of the latter

--- src/systemctl.c
+++ src/systemctl.c
@@ -4098,7 +4098,7 @@ static int install_info_apply(const char *verb, LookupPaths *paths, InstallInfo 
                                 argv[1] = file_name_from_path(sysv);
                                 argv[2] =
                                         streq(verb, "enable") ? "on" :
-                                        streq(verb, "disable") ? "off" : "--level=3";
+                                        streq(verb, "disable") ? "off" : "--level=5";
 
                                 log_info("Executing %s %s %s", argv[0], argv[1], strempty(argv[2]));
 
