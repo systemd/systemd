1365b53ff9a53c94c88bbf4ec5e5782edb75e663
Lennart Poettering <lennart@poettering.net>
2011-05-23 21:37:45 +0200

    execute: fix function prototype

--- src/execute.h
+++ src/execute.h
@@ -211,10 +211,10 @@ void exec_status_exit(ExecStatus *s, ExecContext *context, pid_t pid, int code, 
 void exec_status_dump(ExecStatus *s, FILE *f, const char *prefix);
 
 const char* exec_output_to_string(ExecOutput i);
-int exec_output_from_string(const char *s);
+ExecOutput exec_output_from_string(const char *s);
 
 const char* exec_input_to_string(ExecInput i);
-int exec_input_from_string(const char *s);
+ExecInput exec_input_from_string(const char *s);
 
 const char *kill_mode_to_string(KillMode k);
 KillMode kill_mode_from_string(const char *s);
