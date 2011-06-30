d971228752cb6f2993f6609c9a07d49037d36806
Harald Hoyer <harald@redhat.com>
2011-06-30 15:19:38 +0200

    udev/udevadm-monitor.c: fixed misplaced brace

--- udev/udevadm-monitor.c
+++ udev/udevadm-monitor.c
@@ -137,10 +137,10 @@ int udevadm_monitor(struct udev *udev, int argc, char *argv[])
 			       "  --tag-match=<tag>                       filter events by tag\n"
 			       "  --help\n\n");
 			goto out;
-		}
 		default:
 			rc = 1;
 			goto out;
+		}
 	}
 
 	if (!print_kernel && !print_udev) {
