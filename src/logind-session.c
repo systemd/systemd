0254daedb1b67a16c6d54b964b31b5e8eaa4ac4c
Lennart Poettering <lennart@poettering.net>
2011-07-01 22:34:58 +0200

    logind: create private subdirectory for X11 socket

--- src/logind-session.c
+++ src/logind-session.c
@@ -380,12 +380,14 @@ static int session_link_x11_socket(Session *s) {
                 return -ENOENT;
         }
 
-        t = strappend(s->user->runtime_path, "/display");
+        t = strappend(s->user->runtime_path, "/X11/display");
         if (!t) {
                 log_error("Out of memory");
                 free(f);
                 return -ENOMEM;
         }
+
+        mkdir_parents(t, 0755);
 
         if (link(f, t) < 0) {
                 if (errno == EEXIST) {
@@ -638,7 +640,7 @@ static int session_unlink_x11_socket(Session *s) {
 
         s->user->display = NULL;
 
-        t = strappend(s->user->runtime_path, "/display");
+        t = strappend(s->user->runtime_path, "/X11/display");
         if (!t) {
                 log_error("Out of memory");
                 return -ENOMEM;
