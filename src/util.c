7862f62de3d5e71a63391e6ce7ecaee080bc6a2d
Lennart Poettering <lennart@poettering.net>
2011-07-03 23:20:56 +0200

    util: explicitly turn of O_CLOEXEC for stdin/stdout/stderr

--- src/util.c
+++ src/util.c
@@ -2947,6 +2947,10 @@ int make_stdio(int fd) {
         if (r < 0 || s < 0 || t < 0)
                 return -errno;
 
+        fd_cloexec(STDIN_FILENO, false);
+        fd_cloexec(STDOUT_FILENO, false);
+        fd_cloexec(STDERR_FILENO, false);
+
         return 0;
 }
 
