1c7dde3e475978c569a982d65fd86d4b4e3caad8
Bastien Nocera <hadess@hadess.net>
2011-07-11 17:01:28 +0100

    build-sys: Add i18n support through intltool
    And put the PolicyKit policy files up for translation.

--- autogen.sh
+++ autogen.sh
@@ -60,6 +60,7 @@ else
     rm -f config.cache
 
     libtoolize -c --force
+    intltoolize -c -f
     run_versioned aclocal "$AM_VERSION" -I m4
     run_versioned autoconf "$AC_VERSION" -Wall
     run_versioned autoheader "$AC_VERSION"
