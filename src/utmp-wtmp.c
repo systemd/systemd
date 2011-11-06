b8e47420b32b52619c6c49c98a663bee7929ccbe
Michal Schmidt <mschmidt@redhat.com>
2011-11-06 23:07:54 +0100

    utmp: no need to zero a struct before overwriting it with memcpy

--- src/utmp-wtmp.c
+++ src/utmp-wtmp.c
@@ -242,8 +242,6 @@ int utmp_put_dead_process(const char *id, pid_t pid, int code, int status) {
         if (found->ut_pid != pid)
                 return 0;
 
-        zero(store);
-
         memcpy(&store, &lookup, sizeof(store));
         store.ut_type = DEAD_PROCESS;
         store.ut_exit.e_termination = code;
