8ab49c12dcab02d9d83e63a93676d4fc8f709516
Lennart Poettering <lennart@poettering.net>
2012-01-05 18:21:26 +0100

    headers: fix git URLs for source files

--- src/systemd/sd-daemon.h
+++ src/systemd/sd-daemon.h
@@ -58,7 +58,7 @@ extern "C" {
 
   You may find an up-to-date version of these source files online:
 
-  http://cgit.freedesktop.org/systemd/plain/src/sd-daemon.h
+  http://cgit.freedesktop.org/systemd/plain/src/systemd/sd-daemon.h
   http://cgit.freedesktop.org/systemd/plain/src/sd-daemon.c
 
   This should compile on non-Linux systems, too, but with the
