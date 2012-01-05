9fbe27d9d6dc6e4530ce904d35c74326e415e34e
Kay Sievers <kay.sievers@vrfy.org>
2012-01-05 19:22:03 +0100

    autogen.sh: enable git pre-commit

--- autogen.sh
+++ autogen.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+if [ -f .git/hooks/pre-commit.sample -a ! -f .git/hooks/pre-commit ] ; then
+    cp -p .git/hooks/pre-commit.sample .git/hooks/pre-commit && \
+    chmod +x .git/hooks/pre-commit && \
+    echo "Activated pre-commit hook."
+fi
+
 gtkdocize
 autoreconf --install --symlink
 
