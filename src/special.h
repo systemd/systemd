64f5002a3a570da266cf155402318e7a52457d5f
Lennart Poettering <lennart@poettering.net>
2012-01-05 23:55:01 +0100

    special: fix name of journal socket unit

--- src/special.h
+++ src/special.h
@@ -70,7 +70,7 @@
 /* Services systemd relies on */
 #define SPECIAL_DBUS_SERVICE "dbus.service"
 #define SPECIAL_DBUS_SOCKET "dbus.socket"
-#define SPECIAL_JOURNALD_SOCKET "journald.socket"
+#define SPECIAL_JOURNALD_SOCKET "systemd-journald.socket"
 #define SPECIAL_SYSLOG_SOCKET "syslog.socket"
 
 /* Magic init signals */
