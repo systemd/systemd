6bd9fe5a734893262f2932a8e1fcfe05796f0105
Kay Sievers <kay.sievers@vrfy.org>
2012-01-09 23:14:03 +0100

    autogen.sh: diet and sync with udev and kmod

--- autogen.sh
+++ autogen.sh
@@ -2,8 +2,6 @@
 
 #  This file is part of systemd.
 #
-#  Copyright 2010 Lennart Poettering
-#
 #  systemd is free software; you can redistribute it and/or modify it
 #  under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2 of the License, or
@@ -17,64 +15,37 @@
 #  You should have received a copy of the GNU General Public License
 #  along with systemd; If not, see <http://www.gnu.org/licenses/>.
 
-AM_VERSION=1.11
-AC_VERSION=2.63
-
-run_versioned() {
-    local P
-    local V
-
-    V=$(echo "$2" | sed -e 's,\.,,g')
-
-    if [ -e "`which $1$V 2> /dev/null`" ] ; then
-        P="$1$V"
-    else
-        if [ -e "`which $1-$2 2> /dev/null`" ] ; then
-            P="$1-$2"
-        else
-            P="$1"
-        fi
-    fi
-
-    shift 2
-    "$P" "$@"
-}
-
-set -ex
-
 if [ -f .git/hooks/pre-commit.sample -a ! -f .git/hooks/pre-commit ] ; then
     cp -p .git/hooks/pre-commit.sample .git/hooks/pre-commit && \
     chmod +x .git/hooks/pre-commit && \
     echo "Activated pre-commit hook."
 fi
 
+intltoolize --force --automake
+autoreconf --force --install --symlink
+
 libdir() {
     echo $(cd $1/$(gcc -print-multi-os-directory); pwd)
 }
 
-if [ "x$1" = "xam" ] ; then
-    run_versioned automake "$AM_VERSION" -a -c --foreign
-    ./config.status
+args="\
+--with-rootprefix= \
+--sysconfdir=/etc \
+--localstatedir=/var \
+--libdir=$(libdir /usr/lib) \
+--with-rootlibdir=$(libdir /lib) \
+--libexecdir=/usr/lib"
+
+if [ "x$1" != "xc" ]; then
+    echo
+    echo "----------------------------------------------------------------"
+    echo "Initialized build system. For a common configuration please run:"
+    echo "----------------------------------------------------------------"
+    echo
+    echo "./configure CFLAGS='-g -O0' $args"
+    echo
 else
-    rm -rf autom4te.cache
-    rm -f config.cache
-
-    libtoolize -c --force
-    intltoolize -c -f
-    run_versioned aclocal "$AM_VERSION" -I m4
-    run_versioned autoconf "$AC_VERSION" -Wall
-    run_versioned autoheader "$AC_VERSION"
-    run_versioned automake "$AM_VERSION" --copy --foreign --add-missing
-
-    if [ "x$1" != "xac" ]; then
-        CFLAGS="$CFLAGS -g -O0" ./configure \
-          --with-rootprefix= \
-          --sysconfdir=/etc \
-          --localstatedir=/var \
-          --libdir=$(libdir /usr/lib) \
-          --with-rootlibdir=$(libdir /lib) \
-          --libexecdir=/usr/lib \
-          "$@"
-        make clean
-    fi
+    echo ./configure CFLAGS='-g -O0' $args
+    ./configure CFLAGS='-g -O0' $args
+    make clean
 fi
