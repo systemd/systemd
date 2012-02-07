3b2e525a5621784287601573dbf8b55d76cbe407
Kay Sievers <kay.sievers@vrfy.org>
2012-02-07 01:58:59 +0100

    release 181

--- autogen.sh
+++ autogen.sh
@@ -13,17 +13,27 @@ libdir() {
         echo $(cd $1/$(gcc -print-multi-os-directory); pwd)
 }
 
-args="\
+args="$args \
 --prefix=/usr \
---with-rootprefix= \
 --sysconfdir=/etc \
---bindir=/sbin \
 --libdir=$(libdir /usr/lib) \
---with-rootlibdir=$(libdir /lib) \
---libexecdir=/lib \
---with-systemdsystemunitdir=/lib/systemd/system \
 --with-selinux \
 --enable-gtk-doc"
+
+if [ -L /bin ]; then
+args="$args \
+--libexecdir=/usr/lib \
+--with-systemdsystemunitdir=/usr/lib/systemd/system \
+"
+else
+args="$args \
+--with-rootprefix= \
+---with-rootlibdir=$(libdir /lib) \
+--bindir=/sbin \
+--libexecdir=/lib \
+--with-systemdsystemunitdir=/lib/systemd/system \
+"
+fi
 
 echo
 echo "----------------------------------------------------------------"
