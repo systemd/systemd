5430f7f2bc7330f3088b894166bf3524a067e3d8
Lennart Poettering <lennart@poettering.net>
2012-04-12 00:20:58 +0200

    relicense to LGPLv2.1 (with exceptions)
    We finally got the OK from all contributors with non-trivial commits to
    relicense systemd from GPL2+ to LGPL2.1+.
    
    Some udev bits continue to be GPL2+ for now, but we are looking into
    relicensing them too, to allow free copy/paste of all code within
    systemd.
    
    The bits that used to be MIT continue to be MIT.
    
    The big benefit of the relicensing is that closed source code may now
    link against libsystemd-login.so and friends.

--- src/systemd-bash-completion.sh
+++ src/systemd-bash-completion.sh
@@ -3,8 +3,8 @@
 # Copyright 2010 Ran Benita
 #
 # systemd is free software; you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
+# under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; either version 2.1 of the License, or
 # (at your option) any later version.
 #
 # systemd is distributed in the hope that it will be useful, but
@@ -12,7 +12,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 # General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
+# You should have received a copy of the GNU Lesser General Public License
 # along with systemd; If not, see <http://www.gnu.org/licenses/>.
 
 __systemctl() {
