a882169a168bef88eb5cc3a9a5437e6e3518260f
Lennart Poettering <lennart@poettering.net>
2012-06-25 17:34:50 +0200

    man: document systemd-user-sessions.service

--- src/login/user-sessions.c
+++ src/login/user-sessions.c
@@ -69,17 +69,18 @@ int main(int argc, char*argv[]) {
                 int r, q;
                 char *cgroup_user_tree = NULL;
 
-                if ((r = write_one_line_file_atomic("/run/nologin", "System is going down.")) < 0)
+                r = write_one_line_file_atomic("/run/nologin", "System is going down.");
+                if (r < 0)
                         log_error("Failed to create /run/nologin: %s", strerror(-r));
 
-                if ((q = cg_get_user_path(&cgroup_user_tree)) < 0) {
+                q = cg_get_user_path(&cgroup_user_tree);
+                if (q < 0) {
                         log_error("Failed to determine use path: %s", strerror(-q));
                         goto finish;
                 }
 
                 q = cg_kill_recursive_and_wait(SYSTEMD_CGROUP_CONTROLLER, cgroup_user_tree, true);
                 free(cgroup_user_tree);
-
                 if (q < 0) {
                         log_error("Failed to kill sessions: %s", strerror(-q));
                         goto finish;
