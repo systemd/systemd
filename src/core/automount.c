e872b43c7ee51efb6bd6ca31d79e02af8cc3cb82
Lennart Poettering <lennart@poettering.net>
2012-09-18 18:59:01 +0200

    automount: also whine if an automount directory is not empty

--- src/core/automount.c
+++ src/core/automount.c
@@ -501,6 +501,9 @@ static void automount_enter_waiting(Automount *a) {
         /* We knowingly ignore the results of this call */
         mkdir_p_label(a->where, 0555);
 
+        if (dir_is_empty(a->where) <= 0)
+                log_notice("%s: Directory %s to mount over is not empty, ignoring. (To see the over-mounted files, please manually mount the underlying file system to a secondary location.)", a->meta.id, a->where);
+
         if (pipe2(p, O_NONBLOCK|O_CLOEXEC) < 0) {
                 r = -errno;
                 goto fail;
