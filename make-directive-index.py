e7098b699a025ea852a5e21b0e9122116f3f26bd
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2012-11-13 18:39:18 +0100

    lawyerese: add header to make-directive-index

--- make-directive-index.py
+++ make-directive-index.py
@@ -1,4 +1,22 @@
-# -*- coding: utf-8 -*-
+#  -*- Mode: python; coding: utf-8; indent-tabs-mode: nil -*- */
+#
+#  This file is part of systemd.
+#
+#  Copyright 2012 Zbigniew Jędrzejewski-Szmek
+#
+#  systemd is free software; you can redistribute it and/or modify it
+#  under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation; either version 2.1 of the License, or
+#  (at your option) any later version.
+#
+#  systemd is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with systemd; If not, see <http://www.gnu.org/licenses/>.
+
 import sys
 import collections
 import xml.etree.ElementTree as tree
