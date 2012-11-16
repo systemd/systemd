95ec8647c70b2c1fe816301aca2143d587a60c82
Lennart Poettering <lennart@poettering.net>
2012-11-16 03:03:54 +0100

    journal: recommend journalctl -x more often

--- src/systemctl/systemctl.c
+++ src/systemctl/systemctl.c
@@ -1342,9 +1342,9 @@ static int wait_for_jobs(DBusConnection *bus, Set *s) {
                         else if (streq(d.result, "canceled"))
                                 log_error("Job for %s canceled.", strna(d.name));
                         else if (streq(d.result, "dependency"))
-                                log_error("A dependency job for %s failed. See 'journalctl -n' for details.", strna(d.name));
+                                log_error("A dependency job for %s failed. See 'journalctl -xn' for details.", strna(d.name));
                         else if (!streq(d.result, "done") && !streq(d.result, "skipped"))
-                                log_error("Job for %s failed. See 'systemctl status %s' and 'journalctl -n' for details.", strna(d.name), strna(d.name));
+                                log_error("Job for %s failed. See 'systemctl status %s' and 'journalctl -xn' for details.", strna(d.name), strna(d.name));
                 }
 
                 if (streq_ptr(d.result, "timeout"))
