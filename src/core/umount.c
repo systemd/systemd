e1af7092a8e124eff5f7a668f0f1562c4df4b02f
Kay Sievers <kay@vrfy.org>
2013-01-20 16:18:00 +0100

    shutdown: loop - convert to enumerate match

--- src/core/umount.c
+++ src/core/umount.c
@@ -219,7 +219,8 @@ static int loopback_list_get(MountPoint **head) {
         }
 
         if (udev_enumerate_add_match_subsystem(e, "block") < 0 ||
-            udev_enumerate_add_match_sysname(e, "loop*") < 0) {
+            udev_enumerate_add_match_sysname(e, "loop*") < 0 ||
+            udev_enumerate_add_match_sysattr(e, "loop/backing_file", NULL) < 0) {
                 r = -EIO;
                 goto finish;
         }
@@ -233,19 +234,12 @@ static int loopback_list_get(MountPoint **head) {
         udev_list_entry_foreach(item, first) {
                 MountPoint *lb;
                 struct udev_device *d;
-                const char *backing;
                 char *loop;
                 const char *dn;
 
                 if (!(d = udev_device_new_from_syspath(udev, udev_list_entry_get_name(item)))) {
                         r = -ENOMEM;
                         goto finish;
-                }
-
-                backing = udev_device_get_sysattr_value(d, "loop/backing_file");
-                if (!backing) {
-                        udev_device_unref(d);
-                        continue;
                 }
 
                 if (!(dn = udev_device_get_devnode(d))) {
