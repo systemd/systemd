b0ee8068da5bc9ed949656700274bb6ff60d2073
Colin Walters <walters@verbum.org>
2013-01-25 11:21:20 -0500

    util: *DO NOT* loop for EINTR handling with close_nointr()
    See the linked references for why we should not do this.

--- src/shared/util.c
+++ src/shared/util.c
@@ -183,18 +183,25 @@ bool first_word(const char *s, const char *word) {
 }
 
 int close_nointr(int fd) {
+        int r;
+
         assert(fd >= 0);
+        r = close(fd);
 
-        for (;;) {
-                int r;
-
-                r = close(fd);
-                if (r >= 0)
-                        return r;
-
-                if (errno != EINTR)
-                        return -errno;
-        }
+        /* Just ignore EINTR; a retry loop is the wrong
+         * thing to do on Linux.
+         *
+         * http://lkml.indiana.edu/hypermail/linux/kernel/0509.1/0877.html
+         * https://bugzilla.gnome.org/show_bug.cgi?id=682819
+         * http://utcc.utoronto.ca/~cks/space/blog/unix/CloseEINTR
+         * https://sites.google.com/site/michaelsafyan/software-engineering/checkforeintrwheninvokingclosethinkagain
+         */
+        if (_unlikely_(r < 0 && errno == EINTR))
+                return 0;
+        else if (r >= 0)
+                return r;
+        else
+                return -errno;
 }
 
 void close_nointr_nofail(int fd) {
