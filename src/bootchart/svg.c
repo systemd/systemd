cd3bccaaf7b3c0e000cfd1bac638a52cb4f510c3
Auke Kok <auke-jan.h.kok@intel.com>
2013-02-12 15:14:15 -0800

    systemd-bootchart: relicense to LGPLv2.1+
    When we merged systemd-bootchart we omitted relicensing this
    to LGPL. Now that code is being used for other purposes we
    need to make sure the code is shareable within systemd
    without further problems.
    
    The original committers and contributors of bootchart before it
    was merged all have agreed with the relicensing.

--- src/bootchart/svg.c
+++ src/bootchart/svg.c
@@ -1,16 +1,24 @@
-/*
- * svg.c
- *
- * Copyright (C) 2009-2012 Intel Coproration
- *
- * Authors:
- *   Auke Kok <auke-jan.h.kok@intel.com>
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; version 2
- * of the License.
- */
+/***
+  bootchart.c - This file is part of systemd-bootchart
+
+  Copyright (C) 2009-2013 Intel Coproration
+
+  Authors:
+    Auke Kok <auke-jan.h.kok@intel.com>
+
+  systemd is free software; you can redistribute it and/or modify it
+  under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation; either version 2.1 of the License, or
+  (at your option) any later version.
+
+  systemd is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with systemd; If not, see <http://www.gnu.org/licenses/>.
+ ***/
 
 #include <stdio.h>
 #include <stdarg.h>
