d2dd0265b5140ce849cfc3821ad595df2d4c9eb7
Steven Hiscocks <steven@hiscocks.me.uk>
2013-02-16 17:32:05 +0000

    systemd-python: updated _reader header to standard license

--- src/python-systemd/_reader.c
+++ src/python-systemd/_reader.c
@@ -1,21 +1,23 @@
-/*
-_reader - Python module that reads systemd journal similar to journalctl
-Copyright (C) 2012  Steven Hiscocks
+/*-*- Mode: C; c-basic-offset: 8; indent-tabs-mode: nil -*-*/
 
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
+/***
+  This file is part of systemd.
 
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
+  Copyright 2013 Steven Hiscocks, Zbigniew Jędrzejewski-Szmek
 
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+  systemd is free software; you can redistribute it and/or modify it
+  under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation; either version 2.1 of the License, or
+  (at your option) any later version.
+
+  systemd is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with systemd; If not, see <http://www.gnu.org/licenses/>.
+***/
 #include <systemd/sd-journal.h>
 
 #include <Python.h>
