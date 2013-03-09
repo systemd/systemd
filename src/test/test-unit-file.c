ad88e758d1b08a21d25971b074e119c167757109
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2013-03-09 18:55:31 -0500

    Fix typos of /etc/systemd/systemd
    <Lekensteyn> The 198 announcement mentions
                 "/etc/systemd/systemd/foobar.service.d/*.conf", is that a
                 typo? I only have a /etc/systemd/system/. Is there a
                 manpage describing this new feature?

--- src/test/test-unit-file.c
+++ src/test/test-unit-file.c
@@ -256,12 +256,12 @@ static void test_load_env_file_3(void) {
 
 static void test_install_printf(void) {
         char    name[] = "name.service",
-                path[] = "/run/systemd/systemd/name.service",
+                path[] = "/run/systemd/system/name.service",
                 user[] = "xxxx-no-such-user";
         InstallInfo i = {name, path, user};
         InstallInfo i2 = {name, path, NULL};
         char    name3[] = "name@inst.service",
-                path3[] = "/run/systemd/systemd/name.service";
+                path3[] = "/run/systemd/system/name.service";
         InstallInfo i3 = {name3, path3, user};
         InstallInfo i4 = {name3, path3, NULL};
 
