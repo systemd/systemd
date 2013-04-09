05d0c3e1fde1794b26afc572ad1acfe52048ecba
Lennart Poettering <lennart@poettering.net>
2013-04-09 21:31:16 +0200

    update TODO

--- src/systemd/sd-bus.h
+++ src/systemd/sd-bus.h
@@ -33,8 +33,6 @@ extern "C" {
 #endif
 
 /* TODO:
- * - rewrite message_read_ap() and message_append_ap() to be non-recursive.
- *
  * - add page donation logic
  * - api for appending/reading fixed arrays
  * - merge busctl into systemctl or so?
