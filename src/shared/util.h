a432cb691ca3c1e18b012cb8e9f6e5654b3652a2
Colin Walters <walters@verbum.org>
2013-04-30 13:11:49 -0400

    util: Add _sentinel_ to strextend()
    Since it must be NULL terminated.

--- src/shared/util.h
+++ src/shared/util.h
@@ -633,7 +633,7 @@ static inline void *mempset(void *s, int c, size_t n) {
 char *hexmem(const void *p, size_t l);
 void *unhexmem(const char *p, size_t l);
 
-char *strextend(char **x, ...);
+char *strextend(char **x, ...) _sentinel_;
 char *strrep(const char *s, unsigned n);
 
 void* greedy_realloc(void **p, size_t *allocated, size_t need);
