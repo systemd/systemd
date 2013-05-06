d956ac29a1b8120e83922fa8e75cdd8f6e562529
Lennart Poettering <lennart@poettering.net>
2013-05-06 22:28:39 +0200

    core: unit_inactive_or_pending() should actually do as it claims

--- src/core/unit.c
+++ src/core/unit.c
@@ -2676,6 +2676,9 @@ bool unit_inactive_or_pending(Unit *u) {
 
         /* Returns true if the unit is inactive or going down */
 
+        if (UNIT_IS_INACTIVE_OR_DEACTIVATING(unit_active_state(u)))
+                return true;
+
         if (unit_stop_pending(u))
                 return true;
 
