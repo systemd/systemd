1acbb95c2b58373909d1e7a09a2eed0f6595cf6e
Ondrej Balaz <blami@blami.net>
2013-08-09 20:37:52 +0200

    systemd-cryptsetup: makes “discard” a synonym for “allow-discards”
    systemd-cryptsetup recognizes option 'allow-discards' in /etc/crypttab
    to enable TRIM passthrough to underlying encrypted device. In Debian
    this option was changed to 'discard' to avoid hyphen in option name.
    (see: #648868 and `man crypttab`).
    
    [zj: update crypttab(5) too, making "discard" the default.]

--- src/cryptsetup/cryptsetup.c
+++ src/cryptsetup/cryptsetup.c
@@ -129,7 +129,7 @@ static int parse_one_option(const char *option) {
                 opt_readonly = true;
         else if (streq(option, "verify"))
                 opt_verify = true;
-        else if (streq(option, "allow-discards"))
+        else if (streq(option, "allow-discards") || streq(option, "discard"))
                 opt_discards = true;
         else if (streq(option, "luks"))
                 opt_type = CRYPT_LUKS1;
