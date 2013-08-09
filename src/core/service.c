b0693d30863a36da63d2a2732aa298fff6d34d0d
Maciej Wereski <m.wereski@partner.samsung.com>
2013-08-09 12:54:10 +0200

    service: prohibit Restart= set when Type=oneshot

--- src/core/service.c
+++ src/core/service.c
@@ -1113,6 +1113,12 @@ static int service_verify(Service *s) {
                 return -EINVAL;
         }
 
+        if (s->type == SERVICE_ONESHOT && s->restart != SERVICE_RESTART_NO) {
+                log_error_unit(UNIT(s)->id,
+                                "%s has Restart setting other than no, which isn't allowed for Type=oneshot services. Refusing.", UNIT(s)->id);
+                return -EINVAL;
+        }
+
         if (s->type == SERVICE_DBUS && !s->bus_name) {
                 log_error_unit(UNIT(s)->id,
                                "%s is of type D-Bus but no D-Bus service name has been specified. Refusing.", UNIT(s)->id);
