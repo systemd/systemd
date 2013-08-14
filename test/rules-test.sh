0228a7e56a618aed45940f6ec61b24d34fff7dc5
Kay Sievers <kay@vrfy.org>
2013-08-14 22:55:40 +0200

    test: add licence header and switch to LGPL
    Checked with and got OK from Martin.

--- test/rules-test.sh
+++ test/rules-test.sh
@@ -3,6 +3,19 @@
 #
 # (C) 2010 Canonical Ltd.
 # Author: Martin Pitt <martin.pitt@ubuntu.com>
+#
+# systemd is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; either version 2.1 of the License, or
+# (at your option) any later version.
+
+# systemd is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with systemd; If not, see <http://www.gnu.org/licenses/>.
 
 [ -n "$srcdir" ] || srcdir=`dirname $0`/..
 
