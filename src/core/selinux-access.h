03e22642617f360a6b55cb853bcf59604754ea5d
Kay Sievers <kay@vrfy.org>
2013-08-14 22:34:41 +0200

    selinux-access: move GPL to LGPL licence
    On Wed, Aug 14, 2013 at 10:31 PM, Daniel J Walsh <dwalsh@redhat.com> wrote:
    > On 08/14/2013 04:17 PM, Kay Sievers wrote:
    > >
    > > this patch added GPL code to systemd, which otherwise is all LGPL. We need
    > > to make sure we can always split out any code to a separate shared library
    > > ...
    > >
    > > Mind if I switch your src/core/selinux-access.[ch] files to LGPL?
    
    > I have no problem with it. Should be LGPL anyways.

--- src/core/selinux-access.h
+++ src/core/selinux-access.h
@@ -8,16 +8,16 @@
   Copyright 2012 Dan Walsh
 
   systemd is free software; you can redistribute it and/or modify it
-  under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
+  under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation; either version 2.1 of the License, or
   (at your option) any later version.
 
   systemd is distributed in the hope that it will be useful, but
   WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-  General Public License for more details.
+  Lesser General Public License for more details.
 
-  You should have received a copy of the GNU General Public License
+  You should have received a copy of the GNU Lesser General Public License
   along with systemd; If not, see <http://www.gnu.org/licenses/>.
 ***/
 
