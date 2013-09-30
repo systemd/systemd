6c081276dc11722656906361ac78e415757865e3
Lennart Poettering <lennart@poettering.net>
2013-10-01 00:06:48 +0200

    main: don't free fds array twice

--- src/core/main.c
+++ src/core/main.c
@@ -1567,6 +1567,7 @@ int main(int argc, char *argv[]) {
         /* This will close all file descriptors that were opened, but
          * not claimed by any unit. */
         fdset_free(fds);
+        fds = NULL;
 
         if (serialization) {
                 fclose(serialization);
