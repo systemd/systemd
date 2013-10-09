8552b17660033812080a11533bd0edce74401039
Auke Kok <auke-jan.h.kok@intel.com>
2013-10-09 10:52:15 -0700

    Smack: Test if smack is enabled before mounting
    Since on most systems with xattr systemd will compile with Smack
    support enabled, we still attempt to mount various fs's with
    Smack-only options.
    
    Before mounting any of these Smack-related filesystems with
    Smack specific mount options, check if Smack is functionally
    active on the running kernel.
    
    If Smack is really enabled in the kernel, all these Smack mounts
    are now *fatal*, as they should be.
    
    We no longer mount smackfs if systemd was compiled without
    Smack support. This makes it easier to make smackfs mount
    failures a critical error when Smack is enabled.
    
    We no longer mount these filesystems with their Smack specific
    options inside containers. There these filesystems will be
    mounted with there non-mount smack options for now.

---
+++ src/shared/smack-util.c
@@ -0,0 +1,36 @@
+/*-*- Mode: C; c-basic-offset: 8; indent-tabs-mode: nil -*-*/
+
+/***
+  This file is part of systemd.
+
+  Copyright 2013 Intel Corporation
+
+  Author: Auke Kok <auke-jan.h.kok@intel.com>
+
+  systemd is free software; you can redistribute it and/or modify it
+  under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation; either version 2.1 of the License, or
+  (at your option) any later version.
+
+  systemd is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with systemd; If not, see <http://www.gnu.org/licenses/>.
+***/
+
+#include "smack-util.h"
+
+#include <unistd.h>
+
+static int use_smack_cached = -1;
+
+bool use_smack(void) {
+
+        if (use_smack_cached < 0)
+                use_smack_cached = (access("/sys/fs/smackfs", F_OK) >= 0);
+
+        return use_smack_cached;
+}
