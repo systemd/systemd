d8921c6d5c7f1047762c2929908fa6c9b506d59d
Tom Gundersen <teg@jklm.no>
2013-10-29 21:52:22 +0100

    rtnl-util: add missing files

---
+++ src/libsystemd-rtnl/rtnl-util.c
@@ -0,0 +1,71 @@
+/*-*- Mode: C; c-basic-offset: 8; indent-tabs-mode: nil -*-*/
+
+/***
+ This file is part of systemd.
+
+ Copyright (C) 2013 Tom Gundersen <teg@jklm.no>
+
+  systemd is free software; you can redistribute it and/or modify it
+  under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation; either version 2.1 of the License, or
+  (at your option) any later version.
+
+  systemd is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with systemd; If not, see <http://www.gnu.org/licenses/>.
+***/
+
+#include <netinet/ether.h>
+
+#include "sd-rtnl.h"
+
+#include "rtnl-util.h"
+
+int rtnl_set_link_properties(sd_rtnl *rtnl, int ifindex, const char *name, const struct ether_addr *mac, unsigned mtu) {
+        _cleanup_sd_rtnl_message_unref_ sd_rtnl_message *message;
+        bool need_update = false;
+        int r;
+
+        assert(rtnl);
+        assert(ifindex > 0);
+
+        r = sd_rtnl_message_link_new(RTM_NEWLINK, ifindex, 0, 0, &message);
+        if (r < 0)
+                return r;
+
+        if (name) {
+                r = sd_rtnl_message_append(message, IFLA_IFNAME, name);
+                if (r < 0)
+                        return r;
+
+                need_update = true;
+        }
+
+        if (mac) {
+                r = sd_rtnl_message_append(message, IFLA_ADDRESS, mac);
+                if (r < 0)
+                        return r;
+
+                need_update = true;
+        }
+
+        if (mtu > 0) {
+                r = sd_rtnl_message_append(message, IFLA_MTU, &mtu);
+                if (r < 0)
+                        return r;
+
+                need_update = true;
+        }
+
+        if  (need_update) {
+                r = sd_rtnl_send_with_reply_and_block(rtnl, message, 0, NULL);
+                if (r < 0)
+                        return r;
+        }
+
+        return 0;
+}
