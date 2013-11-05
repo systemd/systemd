95b015de3849d8d8d03d9441ae1682a04c38833b
Thomas Hindoe Paaboel Andersen <phomes@gmail.com>
2013-11-06 00:30:29 +0100

    hostnamed: prevent it from crashing if the chassis is unknown
    When fallback_chassis would return null it led to a crash and an
    empty result in hostnamectl. Only seen after the sd-port.

--- src/hostname/hostnamed.c
+++ src/hostname/hostnamed.c
@@ -360,6 +360,9 @@ static int property_get_chassis(
         else
                 name = c->data[PROP_CHASSIS];
 
+        if(!name)
+                name = "";
+
         r = sd_bus_message_append(reply, "s", name);
         if (r < 0)
                 return r;
