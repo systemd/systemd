b226deba789ba1a0d2cbc742ddc8160b7feb09c4
Thomas Hindoe Paaboel Andersen <phomes@gmail.com>
2013-11-09 14:11:56 +0100

    bootctl: remove unused options from getopt_long

--- src/boot/bootctl.c
+++ src/boot/bootctl.c
@@ -61,7 +61,7 @@ static int parse_argv(int argc, char *argv[]) {
         assert(argc >= 0);
         assert(argv);
 
-        while ((c = getopt_long(argc, argv, "+hH:P", options, NULL)) >= 0) {
+        while ((c = getopt_long(argc, argv, "+h", options, NULL)) >= 0) {
 
                 switch (c) {
 
