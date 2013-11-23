4b6141c42497dc199618f95fa3f359615eecfaa0
Tom Gundersen <teg@jklm.no>
2013-11-23 02:07:08 +0100

    networkd: use Type=notify
    Also start earlier during boot.

--- src/network/networkd.c
+++ src/network/networkd.c
@@ -35,28 +35,37 @@ int main(int argc, char *argv[]) {
 
         if (argc != 1) {
                 log_error("This program takes no arguments.");
-                return EXIT_FAILURE;
+                r = -EINVAL;
+                goto out;
         }
 
         r = manager_new(&m);
         if (r < 0)
-                return EXIT_FAILURE;
+                goto out;
 
         r = manager_udev_listen(m);
         if (r < 0)
-                return EXIT_FAILURE;
+                goto out;
 
         r = manager_udev_enumerate_links(m);
         if (r < 0)
-                return EXIT_FAILURE;
+                goto out;
 
         r = manager_rtnl_listen(m);
         if (r < 0)
-                return EXIT_FAILURE;
+                goto out;
+
+        sd_notify(false,
+                  "READY=1\n"
+                  "STATUS=Processing requests...");
 
         r = sd_event_loop(m->event);
         if (r < 0)
-                return EXIT_FAILURE;
+                goto out;
 
-        return  EXIT_SUCCESS;
+out:
+        sd_notify(false,
+                  "STATUS=Shutting down...");
+
+        return r < 0 ? EXIT_FAILURE : EXIT_SUCCESS;
 }
