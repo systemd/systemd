systemd/systemd

commit b7fc42e03a7f52efb8707fabbab841d8b2ffff8c

bus: break reference cycle between bus and messages
Previously (6ee4f99 bus: break reference cycle between bus and
messages) I committed the test code, but not the actual fix :)

src/libsystemd/sd-bus.c
@@ -1259,12 +1259,25 @@ _public_ sd_bus *sd_bus_ref(sd_bus *bus) {
 }
 
 _public_ sd_bus *sd_bus_unref(sd_bus *bus) {
+        unsigned i;
 
         if (!bus)
                 return NULL;
 
-        if (REFCNT_DEC(bus->n_ref) <= 0)
-                bus_free(bus);
+        i = REFCNT_DEC(bus->n_ref);
+        if (i != bus->rqueue_size + bus->wqueue_size)
+                return NULL;
+
+        for (i = 0; i < bus->rqueue_size; i++)
+                if (bus->rqueue[i]->n_ref > 1)
+                        return NULL;
+
+        for (i = 0; i < bus->wqueue_size; i++)
+                if (bus->wqueue[i]->n_ref > 1)
+                        return NULL;
+
+        /* we are the only holders on the messages */
+        bus_free(bus);
 
         return NULL;
 }

Makefile.am
@@ -2096,6 +2096,7 @@ tests += \
 	test-bus-marshal \
 	test-bus-signature \
 	test-bus-chat \
+	test-bus-cleanup \
 	test-bus-server \
 	test-bus-match \
 	test-bus-kernel \
@@ -2152,6 +2153,15 @@ test_bus_chat_CFLAGS = \
 	-pthread
 
 test_bus_chat_LDADD = \
+	libsystemd-internal.la \
+	libsystemd-id128-internal.la \
+	libsystemd-daemon-internal.la \
+	libsystemd-shared.la
+
+test_bus_cleanup_SOURCES = \
+	src/libsystemd/test-bus-cleanup.c
+
+test_bus_cleanup_LDADD = \
 	libsystemd-internal.la \
 	libsystemd-id128-internal.la \
 	libsystemd-daemon-internal.la \

commit c4e6e24255de376965c7f8ae53e16594c2581200

bus: fix issue with reference counting
b7fc42e03 introduced a regression.

src/libsystemd/sd-bus.c
@@ -1425,8 +1425,8 @@ static int dispatch_wqueue(sd_bus *bus) {
                          * it got full, then all bets are off
                          * anyway. */
 
-                        sd_bus_message_unref(bus->wqueue[0]);
                         bus->wqueue_size --;
+                        sd_bus_message_unref(bus->wqueue[0]);
                         memmove(bus->wqueue, bus->wqueue + 1, sizeof(sd_bus_message*) * bus->wqueue_size);
                         bus->windex = 0;
 