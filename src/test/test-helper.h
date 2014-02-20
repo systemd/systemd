90e633a7901060063e62bf53948c4c239a9f55d1
Jason St. John <jstjohn@purdue.edu>
2014-02-20 18:09:27 -0500

    replace tabs with spaces in some files
    Files:
    * hwdb/60-keyboard.hwdb
    * shell-completion/zsh/_systemd-coredumpctl
    * src/test/test-helper.h

--- src/test/test-helper.h
+++ src/test/test-helper.h
@@ -24,8 +24,8 @@
 #include "sd-daemon.h"
 
 #define TEST_REQ_RUNNING_SYSTEMD(x)                                 \
-	if (sd_booted() > 0) {                                      \
-		x;                                                  \
+        if (sd_booted() > 0) {                                      \
+                x;                                                  \
         } else {                                                    \
                 printf("systemd not booted skipping '%s'\n", #x);   \
         }
