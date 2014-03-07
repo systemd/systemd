36d239dbdaf94ba2d96bb60ac45ecfc58624b1eb
Daniel Mack <zonque@gmail.com>
2014-03-07 11:41:18 +0100

    core/busname: add lookup string for BUSNAME_FAILURE_SERVICE_FAILED_PERMANENT
    When a busname unit enters BUSNAME_FAILURE_SERVICE_FAILED_PERMANENT, the
    serialization will not be able to look up the result as string via
    busname_result_to_string(). This leads to an assertion trap during
    daemon-reexec.

--- src/core/busname.c
+++ src/core/busname.c
@@ -548,6 +548,7 @@ DEFINE_STRING_TABLE_LOOKUP(busname_state, BusNameState);
 static const char* const busname_result_table[_BUSNAME_RESULT_MAX] = {
         [BUSNAME_SUCCESS] = "success",
         [BUSNAME_FAILURE_RESOURCES] = "resources",
+        [BUSNAME_FAILURE_SERVICE_FAILED_PERMANENT] = "failed-permanent",
 };
 
 DEFINE_STRING_TABLE_LOOKUP(busname_result, BusNameResult);
