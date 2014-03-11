4ef0809c22a7d15bae3865d44596ff6e3cdaf1cb
Kay Sievers <kay@vrfy.org>
2014-03-11 21:46:36 +0100

    build-sys: add libsystemd-label to network/dhcp users

--- src/libsystemd-network/test-dhcp-client.c
+++ src/libsystemd-network/test-dhcp-client.c
@@ -150,7 +150,7 @@ int dhcp_network_send_raw_socket(int s, const union sockaddr_union *link,
                                  const void *packet, size_t len)
 {
         size_t size;
-        _cleanup_free_ DHCPPacket *discover;
+        _cleanup_free_ DHCPPacket *discover = NULL;
         uint16_t ip_check, udp_check;
 
         assert_se(s >= 0);
