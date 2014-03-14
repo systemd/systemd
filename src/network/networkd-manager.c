eed0eee85ac34abd81cd9e81fdb6a19f47b6c8a3
Michael Olbrich <m.olbrich@pengutronix.de>
2014-03-14 18:19:29 +0100

    networkd: fix typo
    It's HAVE_SPLIT_USR not HAVE_SPLIT_USER

--- src/network/networkd-manager.c
+++ src/network/networkd-manager.c
@@ -33,7 +33,7 @@ const char* const network_dirs[] = {
         "/etc/systemd/network",
         "/run/systemd/network",
         "/usr/lib/systemd/network",
-#ifdef HAVE_SPLIT_USER
+#ifdef HAVE_SPLIT_USR
         "/lib/systemd/network",
 #endif
         NULL};
