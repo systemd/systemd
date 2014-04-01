a08ac7e0f72a502564aae6b85a8821f6bd03493c
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2014-04-01 09:07:32 -0400

    logind: remove one cast

--- src/login/logind-seat.c
+++ src/login/logind-seat.c
@@ -120,9 +120,9 @@ int seat_save(Seat *s) {
 
                 fprintf(f,
                         "ACTIVE=%s\n"
-                        "ACTIVE_UID=%lu\n",
+                        "ACTIVE_UID="UID_FMT"\n",
                         s->active->id,
-                        (unsigned long) s->active->user->uid);
+                        s->active->user->uid);
         }
 
         if (s->sessions) {
