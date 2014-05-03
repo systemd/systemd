7cb9c51ce81818c200f27de4db4a4076cbe4265b
Tanu Kaskinen <tanu.kaskinen@linux.intel.com>
2014-05-03 11:52:12 +0300

    path-util: add path_make_relative()
    In user_dirs() in path-lookup.c, I want to replace this:
            symlink("../../../.config/systemd/user", data_home);
    with
            symlink(config_home, data_home);
    to avoid hardcoding .config when XDG_CONFIG_HOME is set.
    
    The problem is that config_home is an absolute path, and it's better
    to make the symlink relative. path_make_relative() is an utility
    function that converts an absolute path into a relative one.

--- src/shared/path-util.c
+++ src/shared/path-util.c
@@ -132,6 +132,96 @@ char *path_make_absolute_cwd(const char *p) {
         return path_make_absolute(p, cwd);
 }
 
+int path_make_relative(const char *from_dir, const char *to_path, char **_r) {
+        char *r, *p;
+        unsigned n_parents;
+        size_t to_path_len;
+
+        assert(from_dir);
+        assert(to_path);
+        assert(_r);
+
+        /* Strips the common part, and adds ".." elements as necessary. */
+
+        if (!path_is_absolute(from_dir))
+                return -EINVAL;
+
+        if (!path_is_absolute(to_path))
+                return -EINVAL;
+
+        /* Skip the common part. */
+        for (;;) {
+                size_t a;
+                size_t b;
+
+                from_dir += strspn(from_dir, "/");
+                to_path += strspn(to_path, "/");
+
+                if (!*from_dir) {
+                        if (!*to_path)
+                                /* from_dir equals to_path. */
+                                r = strdup(".");
+                        else
+                                /* from_dir is a parent directory of to_path. */
+                                r = strdup(to_path);
+
+                        if (!r)
+                                return -ENOMEM;
+
+                        *_r = r;
+                        return 0;
+                }
+
+                if (!*to_path)
+                        break;
+
+                a = strcspn(from_dir, "/");
+                b = strcspn(to_path, "/");
+
+                if (a != b)
+                        break;
+
+                if (memcmp(from_dir, to_path, a) != 0)
+                        break;
+
+                from_dir += a;
+                to_path += b;
+        }
+
+        /* If we're here, then "from_dir" has one or more elements that need to
+         * be replaced with "..". */
+
+        /* Count the number of necessary ".." elements. */
+        for (n_parents = 0;;) {
+                from_dir += strspn(from_dir, "/");
+
+                if (!*from_dir)
+                        break;
+
+                from_dir += strcspn(from_dir, "/");
+                n_parents++;
+        }
+
+        to_path_len = strlen(to_path);
+
+        r = malloc(n_parents * 3 + to_path_len);
+        if (!r)
+                return -ENOMEM;
+
+        for (p = r; n_parents > 0; n_parents--, p += 3)
+                memcpy(p, "../", 3);
+
+        if (to_path_len > 0)
+                memcpy(p, to_path, to_path_len);
+        else
+                /* "to_path" is a parent directory of "from_dir". Let's remove
+                 * the redundant slash from the end of the result. */
+                *(p - 1) = 0;
+
+        *_r = r;
+        return 0;
+}
+
 char **path_strv_make_absolute_cwd(char **l) {
         char **s;
 
