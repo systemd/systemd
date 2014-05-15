9e7e9a6524a36be94d4c570eed324f46e5270240
Lennart Poettering <lennart@poettering.net>
2014-05-15 17:26:02 +0200

    sd-bus: don't hit assert when installing match

--- src/libsystemd/sd-bus/bus-control.c
+++ src/libsystemd/sd-bus/bus-control.c
@@ -1140,7 +1140,6 @@ int bus_add_match_internal(
                 uint64_t cookie) {
 
         assert(bus);
-        assert(match);
 
         if (bus->is_kernel)
                 return bus_add_match_internal_kernel(bus, components, n_components, cookie);
