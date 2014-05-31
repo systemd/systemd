93f1a06374e335e8508d89e1bdaadf45be6ab777
Thomas Hindoe Paaboel Andersen <phomes@gmail.com>
2014-05-31 21:36:23 +0200

    util: ignore_file should not allow files ending with '~'
    ignore_file currently allows any file ending with '~' while it
    seems that the opposite was intended:
    a228a22fda4faa9ecb7c5a5e499980c8ae5d2a08

--- src/shared/util.c
+++ src/shared/util.c
@@ -1371,7 +1371,7 @@ bool ignore_file(const char *filename) {
         assert(filename);
 
         if (endswith(filename, "~"))
-                return false;
+                return true;
 
         return ignore_file_allow_backup(filename);
 }
