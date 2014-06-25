d6239dc4b0cf55a953d6c40890859b85d504ef19
Michał Bartoszkiewicz <mbartoszkiewicz@gmail.com>
2014-06-25 14:54:48 +0200

    core: use correct format string for UIDs

--- src/core/main.c
+++ src/core/main.c
@@ -1548,7 +1548,7 @@ int main(int argc, char *argv[]) {
                         log_info("Running with unpopulated /etc.");
         } else {
                 _cleanup_free_ char *t = uid_to_name(getuid());
-                log_debug(PACKAGE_STRING " running in user mode for user "PID_FMT"/%s. (" SYSTEMD_FEATURES ")",
+                log_debug(PACKAGE_STRING " running in user mode for user "UID_FMT"/%s. (" SYSTEMD_FEATURES ")",
                           getuid(), t);
         }
 
