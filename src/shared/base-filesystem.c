3fd165e53a90dd30af00ec0e963d8e132e3506f9
Kay Sievers <kay@vrfy.org>
2014-07-01 12:12:40 +0200

    base-filesystem: explicitely check existence of the platform's ABI dynamic loader

--- src/shared/base-filesystem.c
+++ src/shared/base-filesystem.c
@@ -38,16 +38,17 @@ typedef struct BaseFilesystem {
         const char *dir;
         mode_t mode;
         const char *target;
+        const char *exists;
 } BaseFilesystem;
 
 static const BaseFilesystem table[] = {
-        { "bin",      0, "usr/bin" },
-        { "lib",      0, "usr/lib" },
+        { "bin",      0, "usr/bin",                             NULL },
+        { "lib",      0, "usr/lib",                             NULL },
+        { "root",  0755, NULL,                                  NULL },
+        { "sbin",     0, "usr/sbin",                            NULL },
 #if defined(__i386__) || defined(__x86_64__)
-        { "lib64",    0, "usr/lib/x86_64-linux-gnu\0usr/lib64" },
+        { "lib64",    0, "usr/lib/x86_64-linux-gnu\0usr/lib64", "ld-linux-x86-64.so.2" },
 #endif
-        { "root",  0755, NULL },
-        { "sbin",     0, "usr/sbin" },
 };
 
 int base_filesystem_create(const char *root) {
@@ -68,6 +69,18 @@ int base_filesystem_create(const char *root) {
                         NULSTR_FOREACH(s, table[i].target) {
                                 if (faccessat(fd, s, F_OK, AT_SYMLINK_NOFOLLOW) < 0)
                                         continue;
+
+                                /* check if a specific file exists at the target path */
+                                if (table[i].exists) {
+                                        _cleanup_free_ char *p = NULL;
+
+                                        p = strjoin(s, "/", table[i].exists, NULL);
+                                        if (!p)
+                                                return log_oom();
+
+                                        if (faccessat(fd, p, F_OK, AT_SYMLINK_NOFOLLOW) < 0)
+                                                continue;
+                                }
 
                                 target = s;
                                 break;
