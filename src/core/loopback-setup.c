f3fc48150bb443f5b4623a819ebc2b39971a4f66
Tom Gundersen <teg@jklm.no>
2014-08-08 12:12:31 +0200

    core: loopback-setup - only up the loopback device
    The kernel will set up the IP addresses for us, so don't duplicate that
    in userspace.

--- src/core/loopback-setup.c
+++ src/core/loopback-setup.c
@@ -19,7 +19,6 @@
   along with systemd; If not, see <http://www.gnu.org/licenses/>.
 ***/
 
-#include <errno.h>
 #include <sys/socket.h>
 #include <net/if.h>
 #include <asm/types.h>
@@ -35,84 +34,14 @@
 #include "sd-rtnl.h"
 #include "rtnl-util.h"
 
-static int pipe_handler(sd_rtnl *rtnl, sd_rtnl_message *m, void *userdata) {
-        int *counter = userdata;
-        int r;
+/* this is hardcoded in the kernel, so don't look it up */
+#define LOOPBACK_IFINDEX 1
 
-        (*counter) --;
-
-        r = sd_rtnl_message_get_errno(m);
-
-        return r == -EEXIST ? 0 : r;
-}
-
-static int add_addresses(sd_rtnl *rtnl, int if_loopback, struct in_addr *ipv4_address, int *counter) {
-        _cleanup_rtnl_message_unref_ sd_rtnl_message *ipv4 = NULL, *ipv6 = NULL;
-        int r;
-
-        r = sd_rtnl_message_new_addr(rtnl, &ipv4, RTM_NEWADDR, if_loopback, AF_INET);
-        if (r < 0)
-                return r;
-
-        r = sd_rtnl_message_addr_set_prefixlen(ipv4, 8);
-        if (r < 0)
-                return r;
-
-        r = sd_rtnl_message_addr_set_flags(ipv4, IFA_F_PERMANENT);
-        if (r < 0)
-                return r;
-
-        r = sd_rtnl_message_addr_set_scope(ipv4, RT_SCOPE_HOST);
-        if (r < 0)
-                return r;
-
-        r = sd_rtnl_message_append_in_addr(ipv4, IFA_LOCAL, ipv4_address);
-        if (r < 0)
-                return r;
-
-        r = sd_rtnl_call_async(rtnl, ipv4, &pipe_handler, counter, 0, NULL);
-        if (r < 0)
-                return r;
-
-        (*counter) ++;
-
-        if (!socket_ipv6_is_supported())
-                return 0;
-
-        r = sd_rtnl_message_new_addr(rtnl, &ipv6, RTM_NEWADDR, if_loopback, AF_INET6);
-        if (r < 0)
-                return r;
-
-        r = sd_rtnl_message_addr_set_prefixlen(ipv6, 128);
-        if (r < 0)
-                return r;
-
-        r = sd_rtnl_message_addr_set_flags(ipv6, IFA_F_PERMANENT);
-        if (r < 0)
-                return r;
-
-        r = sd_rtnl_message_addr_set_scope(ipv6, RT_SCOPE_HOST);
-        if (r < 0)
-                return r;
-
-        r = sd_rtnl_message_append_in6_addr(ipv6, IFA_LOCAL, &in6addr_loopback);
-        if (r < 0)
-                return r;
-
-        r = sd_rtnl_call_async(rtnl, ipv6, &pipe_handler, counter, 0, NULL);
-        if (r < 0)
-                return r;
-
-        (*counter) ++;
-
-        return 0;
-}
-
-static int start_interface(sd_rtnl *rtnl, int if_loopback, struct in_addr *ipv4_address, int *counter) {
+static int start_loopback(sd_rtnl *rtnl) {
         _cleanup_rtnl_message_unref_ sd_rtnl_message *req = NULL;
         int r;
 
-        r = sd_rtnl_message_new_link(rtnl, &req, RTM_SETLINK, if_loopback);
+        r = sd_rtnl_message_new_link(rtnl, &req, RTM_SETLINK, LOOPBACK_IFINDEX);
         if (r < 0)
                 return r;
 
@@ -120,11 +49,9 @@ static int start_interface(sd_rtnl *rtnl, int if_loopback, struct in_addr *ipv4_
         if (r < 0)
                 return r;
 
-        r = sd_rtnl_call_async(rtnl, req, &pipe_handler, counter, 0, NULL);
+        r = sd_rtnl_call(rtnl, req, 0, NULL);
         if (r < 0)
                 return r;
-
-        (*counter) ++;
 
         return 0;
 }
@@ -157,49 +84,25 @@ static int check_loopback(void) {
 
 int loopback_setup(void) {
         _cleanup_rtnl_unref_ sd_rtnl *rtnl = NULL;
-        int r, if_loopback, counter = 0;
-        bool eperm = false;
-        struct in_addr ipv4_address;
-
-        errno = 0;
-        if_loopback = (int) if_nametoindex("lo");
-        if (if_loopback <= 0)
-                return errno ? -errno : -ENODEV;
-
-        ipv4_address.s_addr = htonl(INADDR_LOOPBACK);
+        int r;
 
         r = sd_rtnl_open(&rtnl, 0);
         if (r < 0)
                 return r;
 
-        r = add_addresses(rtnl, if_loopback, &ipv4_address, &counter);
-        if (r < 0)
-                return r;
-
-        r = start_interface(rtnl, if_loopback, &ipv4_address, &counter);
-        if (r < 0)
-                return r;
-
-        while (counter > 0) {
-                r = sd_rtnl_wait(rtnl, 0);
-                if (r < 0)
-                        return r;
-
-                r = sd_rtnl_process(rtnl, 0);
-                if (r < 0) {
-                        if (r == -EPERM)
-                                eperm = true;
-                        else {
-                                log_warning("Failed to configure loopback device: %s", strerror(-r));
-                                return r;
-                        }
+        r = start_loopback(rtnl);
+        if (r == -EPERM) {
+                if (check_loopback() < 0) {
+                        log_warning("Failed to configure loopback device: %s",
+                                    strerror(EPERM));
+                        return -EPERM;
                 }
+        } else if (r < 0) {
+                log_warning("Failed to configure loopback device: %s",
+                            strerror(-r));
+                return r;
         }
 
-        if (eperm && check_loopback() < 0) {
-                log_warning("Failed to configure loopback device: %s", strerror(EPERM));
-                return -EPERM;
-        }
 
         return 0;
 }
