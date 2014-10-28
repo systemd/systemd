1b3a797f6f3c32dc10d8af928d936d87b1edea43
Lennart Poettering <lennart@poettering.net>
2014-10-28 14:59:08 +0100

    sd-bus: add support for new InteractiveAuthorizationRequired error from dbus spec

--- src/systemd/sd-bus-protocol.h
+++ src/systemd/sd-bus-protocol.h
@@ -96,6 +96,8 @@ enum {
 #define SD_BUS_ERROR_INCONSISTENT_MESSAGE       "org.freedesktop.DBus.Error.InconsistentMessage"
 #define SD_BUS_ERROR_MATCH_RULE_NOT_FOUND       "org.freedesktop.DBus.Error.MatchRuleNotFound"
 #define SD_BUS_ERROR_MATCH_RULE_INVALID         "org.freedesktop.DBus.Error.MatchRuleInvalid"
+#define SD_BUS_ERROR_INTERACTIVE_AUTHORIZATION_REQUIRED \
+                                                "org.freedesktop.DBus.Error.InteractiveAuthorizationRequired"
 
 _SD_END_DECLARATIONS;
 
