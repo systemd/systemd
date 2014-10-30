74a550c5d8228e58b936dd70e183f633bf373632
Dave Reisner <dreisner@archlinux.org>
2014-10-29 20:30:25 -0400

    missing.h: fix wrong __NR_getrandom syscall def
    278 is vmsplice on x86_64. 318 is what we want:
    
    http://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git/tree/arch/x86/syscalls/syscall_64.tbl

--- src/shared/missing.h
+++ src/shared/missing.h
@@ -136,7 +136,7 @@ static inline int memfd_create(const char *name, unsigned int flags) {
 
 #ifndef __NR_getrandom
 #  if defined __x86_64__
-#    define __NR_getrandom 278
+#    define __NR_getrandom 318
 #  else
 #    warning "__NR_getrandom unknown for your architecture"
 #    define __NR_getrandom 0xffffffff
