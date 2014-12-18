systemd/systemd

commit 6feeeab0bc40b5a7f208adf79909fb85e2208e8e

path: make the check for unsupported name_to_handle_at symmetric
If child supports, but the parent does not, or when the child does
not support, but the parent does, assume the child is a mount point.

Only if neither supports use the fallback.

src/shared/path-util.c
@@ -462,6 +462,7 @@ int path_is_mount_point(const char *t, bool allow_symlink) {
         _cleanup_free_ char *parent = NULL;
         struct stat a, b;
         int r;
+        bool nosupp = false;
 
         /* We are not actually interested in the file handles, but
          * name_to_handle_at() also passes us the mount ID, hence use
@@ -476,12 +477,11 @@ int path_is_mount_point(const char *t, bool allow_symlink) {
                         /* This kernel or file system does not support
                          * name_to_handle_at(), hence fallback to the
                          * traditional stat() logic */
-                        goto fallback;
-
-                if (errno == ENOENT)
+                        nosupp = true;
+                else if (errno == ENOENT)
                         return 0;
-
-                return -errno;
+                else
+                        return -errno;
         }
 
         r = path_get_parent(t, &parent);
@@ -490,17 +490,22 @@ int path_is_mount_point(const char *t, bool allow_symlink) {
 
         h.handle.handle_bytes = MAX_HANDLE_SZ;
         r = name_to_handle_at(AT_FDCWD, parent, &h.handle, &mount_id_parent, AT_SYMLINK_FOLLOW);
-        if (r < 0) {
-                /* The parent can't do name_to_handle_at() but the
-                 * directory we are interested in can? If so, it must
-                 * be a mount point */
+        if (r < 0)
                 if (errno == EOPNOTSUPP)
-                        return 1;
-
-                return -errno;
-        }
-
-        return mount_id != mount_id_parent;
+                        if (nosupp)
+                                /* Neither parent nor child do name_to_handle_at()?
+                                   We have no choice but to fall back. */
+                                goto fallback;
+                        else
+                                /* The parent can't do name_to_handle_at() but
+                                 * the directory we are interested in can?
+                                 * Or the other way around?
+                                 * If so, it must be a mount point. */
+                                return 1;
+                else
+                        return -errno;
+        else
+                return mount_id != mount_id_parent;
 
 fallback:
         if (allow_symlink)

commit e40872fc538f3216fd4d00aa969785b999a357bf

path-util: fix breakage in path_is_mount_point
This fixes 2 problems introduced by 6feeeab0bc:

1) If name_to_handle_at returns ENOSYS for the child, we'll wrongly
return -ENOSYS when it returns the same for the parent. Immediately
jump to the fallback logic when we get ENOSYS.

2) If name_to_handle_at returns EOPNOTSUPP for the child but suceeds
for the parent, we'll be comparing an uninitialized value (mount_id) to
an initialized value (mount_id_parent). Initialize the mount_id
variables to invalid mount_ids to avoid this.

src/shared/path-util.c
@@ -458,7 +458,7 @@ int path_is_mount_point(const char *t, bool allow_symlink) {
                 .handle.handle_bytes = MAX_HANDLE_SZ
         };
 
-        int mount_id, mount_id_parent;
+        int mount_id = -1, mount_id_parent = -1;
         _cleanup_free_ char *parent = NULL;
         struct stat a, b;
         int r;
@@ -473,7 +473,11 @@ int path_is_mount_point(const char *t, bool allow_symlink) {
 
         r = name_to_handle_at(AT_FDCWD, t, &h.handle, &mount_id, allow_symlink ? AT_SYMLINK_FOLLOW : 0);
         if (r < 0) {
-                if (IN_SET(errno, ENOSYS, EOPNOTSUPP))
+                if (errno == ENOSYS)
+                        /* This kernel does not support name_to_handle_at()
+                         * fall back to the traditional stat() logic. */
+                        goto fallback;
+                else if (errno == EOPNOTSUPP)
                         /* This kernel or file system does not support
                          * name_to_handle_at(), hence fallback to the
                          * traditional stat() logic */