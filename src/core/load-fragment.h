32ee7d3309816994a02b3ff000e9734120d71214
Daniel Mack <daniel@zonque.org>
2015-01-15 20:08:42 +0100

    cgroup: add support for net_cls controllers
    Add a new config directive called NetClass= to CGroup enabled units.
    Allowed values are positive numbers for fix assignments and "auto" for
    picking a free value automatically, for which we need to keep track of
    dynamically assigned net class IDs of units. Introduce a hash table for
    this, and also record the last ID that was given out, so the allocator
    can start its search for the next 'hole' from there. This could
    eventually be optimized with something like an irb.
    
    The class IDs up to 65536 are considered reserved and won't be
    assigned automatically by systemd. This barrier can be made a config
    directive in the future.
    
    Values set in unit files are stored in the CGroupContext of the
    unit and considered read-only. The actually assigned number (which
    may have been chosen dynamically) is stored in the unit itself and
    is guaranteed to remain stable as long as the unit is active.
    
    In the CGroup controller, set the configured CGroup net class to
    net_cls.classid. Multiple unit may share the same net class ID,
    and those which do are linked together.

--- src/core/load-fragment.h
+++ src/core/load-fragment.h
@@ -90,6 +90,7 @@ int config_parse_device_allow(const char *unit, const char *filename, unsigned l
 int config_parse_blockio_weight(const char *unit, const char *filename, unsigned line, const char *section, unsigned section_line, const char *lvalue, int ltype, const char *rvalue, void *data, void *userdata);
 int config_parse_blockio_device_weight(const char *unit, const char *filename, unsigned line, const char *section, unsigned section_line, const char *lvalue, int ltype, const char *rvalue, void *data, void *userdata);
 int config_parse_blockio_bandwidth(const char *unit, const char *filename, unsigned line, const char *section, unsigned section_line, const char *lvalue, int ltype, const char *rvalue, void *data, void *userdata);
+int config_parse_netclass(const char *unit, const char *filename, unsigned line, const char *section, unsigned section_line, const char *lvalue, int ltype, const char *rvalue, void *data, void *userdata);
 int config_parse_job_mode(const char *unit, const char *filename, unsigned line, const char *section, unsigned section_line, const char *lvalue, int ltype, const char *rvalue, void *data, void *userdata);
 int config_parse_job_mode_isolate(const char *unit, const char *filename, unsigned line, const char *section, unsigned section_line, const char *lvalue, int ltype, const char *rvalue, void *data, void *userdata);
 int config_parse_exec_selinux_context(const char *unit, const char *filename, unsigned line, const char *section, unsigned section_line, const char *lvalue, int ltype, const char *rvalue, void *data, void *userdata);
