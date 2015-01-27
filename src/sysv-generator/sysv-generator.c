9993ef2e9817b35b1d467707bef12b2a140b62dc
Lennart Poettering <lennart@poettering.net>
2015-01-27 23:13:28 +0100

    sysv-generator: use is_symlink() utility call where appropriate

--- src/sysv-generator/sysv-generator.c
+++ src/sysv-generator/sysv-generator.c
@@ -147,7 +147,6 @@ static int generate_unit_file(SysvStub *s) {
         _cleanup_free_ char *wants = NULL;
         _cleanup_free_ char *conflicts = NULL;
         int r;
-        struct stat st;
 
         before = strv_join(s->before, " ");
         if (!before)
@@ -172,9 +171,9 @@ static int generate_unit_file(SysvStub *s) {
         /* We might already have a symlink with the same name from a Provides:,
          * or from backup files like /etc/init.d/foo.bak. Real scripts always win,
          * so remove an existing link */
-        if (lstat(unit, &st) == 0 && S_ISLNK(st.st_mode)) {
+        if (is_symlink(unit)) {
                 log_warning("Overwriting existing symlink %s with real service", unit);
-                unlink(unit);
+                (void) unlink(unit);
         }
 
         f = fopen(unit, "wxe");
