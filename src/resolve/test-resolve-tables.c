3a9360d60f262e6ea39802ae470b63a66c3cb6ce
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2015-02-01 20:12:46 -0500

    test-resolve-tables: new "test", useful to print mappings

---
+++ src/resolve/test-resolve-tables.c
@@ -0,0 +1,27 @@
+/***
+  This file is part of systemd
+
+  Copyright 2013 Zbigniew Jędrzejewski-Szmek
+
+  systemd is free software; you can redistribute it and/or modify it
+  under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation; either version 2.1 of the License, or
+  (at your option) any later version.
+
+  systemd is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with systemd; If not, see <http://www.gnu.org/licenses/>.
+***/
+
+#include "dns-type.h"
+#include "test-tables.h"
+
+int main(int argc, char **argv) {
+        test_table_sparse(dns_type, DNS_TYPE);
+
+        return EXIT_SUCCESS;
+}
