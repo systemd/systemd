systemd/systemd

commit 7d328b544621d4b1bec936dec612947ad8bfb65a

shared/capability: go frugal on space for caps

src/shared/capability.c
@@ -269,11 +269,12 @@ int drop_privileges(uid_t uid, gid_t gid, uint64_t keep_capabilities) {
                 return log_oom();
 
         if (keep_capabilities) {
-                cap_value_t bits[sizeof(keep_capabilities)*8];
+                cap_value_t bits[log2u64(keep_capabilities)];
 
-                for (i = 0; i < sizeof(keep_capabilities)*8; i++)
+                for (i = 0; i < ELEMENTSOF(bits); i++)
                         if (keep_capabilities & (1ULL << i))
                                 bits[j++] = i;
+                assert((keep_capabilities & (~1ULL << i)) == 0);
 
                 if (cap_set_flag(d, CAP_EFFECTIVE, j, bits, CAP_SET) < 0 ||
                     cap_set_flag(d, CAP_PERMITTED, j, bits, CAP_SET) < 0) {

src/shared/util.h
@@ -860,7 +860,7 @@ static inline unsigned u32ctz(uint32_t n) {
 #endif
 }
 
-static inline int log2i(int x) {
+static inline unsigned log2i(int x) {
         assert(x > 0);
 
         return __SIZEOF_INT__ * 8 - __builtin_clz(x) - 1;
@@ -870,6 +870,16 @@ static inline unsigned log2u(unsigned x) {
         assert(x > 0);
 
         return sizeof(unsigned) * 8 - __builtin_clz(x) - 1;
+}
+
+static inline unsigned log2u64(uint64_t x) {
+        assert(x > 0);
+
+#if __SIZEOF_LONG_LONG__ == 8
+        return 64 - __builtin_clzll(x) - 1;
+#else
+#error "Wut?"
+#endif
 }
 
 static inline unsigned log2u_round_up(unsigned x) {

commit 057255fbbf2ecb1c46e025b04087fa9340d9880d

shared/capability: don't be too frugal on space for caps
We were dropping the most significant bit. Add an assert to make sure it does not happen again.

Fixes a bug introduced in 7d328b544621d4b1bec936dec612947ad8bfb65a.

src/shared/capability.c
@@ -269,12 +269,16 @@ int drop_privileges(uid_t uid, gid_t gid, uint64_t keep_capabilities) {
                 return log_oom();
 
         if (keep_capabilities) {
-                cap_value_t bits[log2u64(keep_capabilities)];
+                cap_value_t bits[u64log2(keep_capabilities) + 1];
 
                 for (i = 0; i < ELEMENTSOF(bits); i++)
                         if (keep_capabilities & (1ULL << i))
                                 bits[j++] = i;
+
+                /* don't keep too many bits */
                 assert((keep_capabilities & (~1ULL << i)) == 0);
+                /* don't throw away too many bits */
+                assert(((keep_capabilities >> i) & (~1ULL >> i)) == 0);
 
                 if (cap_set_flag(d, CAP_EFFECTIVE, j, bits, CAP_SET) < 0 ||
                     cap_set_flag(d, CAP_PERMITTED, j, bits, CAP_SET) < 0) {