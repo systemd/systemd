da53c5516e3807145a321df08b853d6f9f21c6fc
Tom Gundersen <teg@jklm.no>
2015-02-06 11:54:30 +0100

    sd-rtnl: don't fall over when receiving 0 bytes from the socket
    causes EOF. Seems like a kernel bug. Ignoring it seems to work be the best we
    can do for now...
    
    See https://bugs.freedesktop.org/show_bug.cgi?id=88397

--- src/libsystemd/sd-rtnl/rtnl-message.c
+++ src/libsystemd/sd-rtnl/rtnl-message.c
@@ -1350,9 +1350,11 @@ static int socket_recv_message(int fd, struct iovec *iov, uint32_t *_group, bool
                         log_debug("rtnl: no data in socket");
 
                 return (errno == EAGAIN || errno == EINTR) ? 0 : -errno;
-        } else if (r == 0)
-                /* connection was closed by the kernel */
-                return -ECONNRESET;
+        } else if (r == 0) {
+                /* connection was closed by the kernel? */
+                log_warning("rtnl: ignoring empty message");
+                return 0;
+        }
 
         for (cmsg = CMSG_FIRSTHDR(&msg); cmsg; cmsg = CMSG_NXTHDR(&msg, cmsg)) {
                 if (cmsg->cmsg_level == SOL_SOCKET &&
