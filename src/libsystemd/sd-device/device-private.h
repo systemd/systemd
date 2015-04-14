813a71a206f9f8dc7f824299e94404f8bfdafd91
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2015-04-14 12:39:40 -0400

    sd-device: fix typo

--- src/libsystemd/sd-device/device-private.h
+++ src/libsystemd/sd-device/device-private.h
@@ -37,7 +37,7 @@ void device_set_is_initialized(sd_device *device);
 void device_set_watch_handle(sd_device *device, int fd);
 void device_set_db_persist(sd_device *device);
 void device_set_devlink_priority(sd_device *device, int priority);
-int device_ensure_usec_initialized(sd_device *devcie, sd_device *device_old);
+int device_ensure_usec_initialized(sd_device *device, sd_device *device_old);
 int device_add_devlink(sd_device *device, const char *devlink);
 int device_add_property(sd_device *device, const char *property, const char *value);
 int device_add_tag(sd_device *device, const char *tag);
