e03c7cc26ed731245827ada3aa24f6c937ff80a1
Tom Gundersen <teg@jklm.no>
2015-04-27 11:11:58 +0200

    udevd: worker - only allocate the worker struct in the main process
    This is not used in the worker, so avoid having to free it there.

--- src/udev/udevd.c
+++ src/udev/udevd.c
@@ -168,7 +168,6 @@ static void worker_list_cleanup(struct udev *udev) {
 
 static void worker_spawn(struct event *event) {
         struct udev *udev = event->udev;
-        struct worker *worker;
         struct udev_monitor *worker_monitor;
         pid_t pid;
 
@@ -179,15 +178,6 @@ static void worker_spawn(struct event *event) {
         /* allow the main daemon netlink address to send devices to the worker */
         udev_monitor_allow_unicast_sender(worker_monitor, monitor);
         udev_monitor_enable_receiving(worker_monitor);
-
-        worker = new0(struct worker, 1);
-        if (worker == NULL) {
-                udev_monitor_unref(worker_monitor);
-                return;
-        }
-        /* worker + event reference */
-        worker->refcount = 2;
-        worker->udev = udev;
 
         pid = fork();
         switch (pid) {
@@ -203,7 +193,6 @@ static void worker_spawn(struct event *event) {
                 dev = event->dev;
                 event->dev = NULL;
 
-                free(worker);
                 worker_list_cleanup(udev);
                 event_queue_cleanup(udev, EVENT_UNDEF);
                 udev_monitor_unref(monitor);
@@ -392,10 +381,21 @@ out:
         case -1:
                 udev_monitor_unref(worker_monitor);
                 event->state = EVENT_QUEUED;
-                free(worker);
                 log_error_errno(errno, "fork of child failed: %m");
                 break;
         default:
+        {
+                struct worker *worker;
+
+                worker = new0(struct worker, 1);
+                if (!worker) {
+                        udev_monitor_unref(worker_monitor);
+                        return;
+                }
+
+                /* worker + event reference */
+                worker->refcount = 2;
+                worker->udev = udev;
                 /* close monitor, but keep address around */
                 udev_monitor_disconnect(worker_monitor);
                 worker->monitor = worker_monitor;
@@ -409,6 +409,7 @@ out:
                 children++;
                 log_debug("seq %llu forked new worker ["PID_FMT"]", udev_device_get_seqnum(event->dev), pid);
                 break;
+        }
         }
 }
 
