450d80f6865c18f9513bab6cd8e1db4957985ef5
Daniel Mack <daniel@zonque.org>
2015-06-23 19:43:17 +0200

    networkd: adopt to new firewall-util API
    Move networkd code to the new internal firewall API. Each entry is assigned
    a numeric handle that must be used in order to remove the entry again.

--- src/network/networkd.h
+++ src/network/networkd.h
@@ -199,7 +199,7 @@ struct Address {
         union in_addr_union in_addr;
         union in_addr_union in_addr_peer;
 
-        bool ip_masquerade_done;
+        uint64_t ip_masquerade_handle;
 
         LIST_FIELDS(Address, addresses);
 };
