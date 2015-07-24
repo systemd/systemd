29ddae7b7842c75ec930726015a696839e7538aa
daurnimator <quae@daurnimator.com>
2015-07-24 22:59:19 +1000

    sd-bus: remove _VTABLE from new method vtable initialiser

--- src/systemd/sd-bus-vtable.h
+++ src/systemd/sd-bus-vtable.h
@@ -90,7 +90,7 @@ struct sd_bus_vtable {
                 .x.start.element_size = sizeof(sd_bus_vtable),          \
         }
 
-#define SD_BUS_VTABLE_METHOD_WITH_OFFSET(_member, _signature, _result, _handler, _offset, _flags)   \
+#define SD_BUS_METHOD_WITH_OFFSET(_member, _signature, _result, _handler, _offset, _flags)   \
         {                                                               \
                 .type = _SD_BUS_VTABLE_METHOD,                          \
                 .flags = _flags,                                        \
@@ -101,7 +101,7 @@ struct sd_bus_vtable {
                 .x.method.offset = _offset,                             \
         }
 #define SD_BUS_METHOD(_member, _signature, _result, _handler, _flags)   \
-        SD_BUS_VTABLE_METHOD_WITH_OFFSET(_member, _signature, _result, _handler, 0, _flags)
+        SD_BUS_METHOD_WITH_OFFSET(_member, _signature, _result, _handler, 0, _flags)
 
 #define SD_BUS_SIGNAL(_member, _signature, _flags)                      \
         {                                                               \
