2ad613addbabc3ffec6d12a1b5c62ff0d07b4a29
Tom Gundersen <teg@jklm.no>
2015-07-28 23:10:18 +0200

    resolved: packet - fix read_type_window()
    We were tracking the bit offset inside each byte, rather than inside the whole bitmap.

--- src/resolve/resolved-dns-packet.c
+++ src/resolve/resolved-dns-packet.c
@@ -1156,6 +1156,7 @@ static int dns_packet_read_type_window(DnsPacket *p, Bitmap **types, size_t *sta
         uint8_t window;
         uint8_t length;
         const uint8_t *bitmap;
+        uint8_t bit = 0;
         unsigned i;
         bool found = false;
         size_t saved_rindex;
@@ -1187,10 +1188,10 @@ static int dns_packet_read_type_window(DnsPacket *p, Bitmap **types, size_t *sta
 
         for (i = 0; i < length; i++) {
                 uint8_t bitmask = 1 << 7;
-                uint8_t bit = 0;
 
                 if (!bitmap[i]) {
                         found = false;
+                        bit += 8;
                         continue;
                 }
 
