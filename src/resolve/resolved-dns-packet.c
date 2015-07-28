1792f2231fb246396e3f50e465194dd93565b1c7
Tom Gundersen <teg@jklm.no>
2015-07-28 23:09:23 +0200

    resolve: packet - fix append_types()
    We were counting the number of bits set rather than the number of bytes they occupied.

--- src/resolve/resolved-dns-packet.c
+++ src/resolve/resolved-dns-packet.c
@@ -508,23 +508,21 @@ static int dns_packet_append_type_window(DnsPacket *p, uint8_t window, uint8_t l
 
         assert(p);
         assert(types);
+        assert(length > 0);
 
         saved_size = p->size;
 
-        if (length != 0) {
+        r = dns_packet_append_uint8(p, window, NULL);
+        if (r < 0)
+                goto fail;
 
-                r = dns_packet_append_uint8(p, window, NULL);
-                if (r < 0)
-                        goto fail;
+        r = dns_packet_append_uint8(p, length, NULL);
+        if (r < 0)
+                goto fail;
 
-                r = dns_packet_append_uint8(p, length, NULL);
-                if (r < 0)
-                        goto fail;
-
-                r = dns_packet_append_blob(p, types, length, NULL);
-                if (r < 0)
-                        goto fail;
-        }
+        r = dns_packet_append_blob(p, types, length, NULL);
+        if (r < 0)
+                goto fail;
 
         if (start)
                 *start = saved_size;
@@ -538,7 +536,7 @@ fail:
 static int dns_packet_append_types(DnsPacket *p, Bitmap *types, size_t *start) {
         Iterator i;
         uint8_t window = 0;
-        uint8_t len = 0;
+        uint8_t entry = 0;
         uint8_t bitmaps[32] = {};
         unsigned n;
         size_t saved_size;
@@ -550,30 +548,24 @@ static int dns_packet_append_types(DnsPacket *p, Bitmap *types, size_t *start) {
         saved_size = p->size;
 
         BITMAP_FOREACH(n, types, i) {
-                uint8_t entry;
-
                 assert(n <= 0xffff);
 
-                if ((n << 8) != window) {
-                        r = dns_packet_append_type_window(p, window, len, bitmaps, NULL);
+                if ((n >> 8) != window && bitmaps[entry / 8] != 0) {
+                        r = dns_packet_append_type_window(p, window, entry / 8 + 1, bitmaps, NULL);
                         if (r < 0)
                                 goto fail;
 
-                        if (len > 0) {
-                                len = 0;
-                                zero(bitmaps);
-                        }
+                        zero(bitmaps);
                 }
 
-                window = n << 8;
-                len ++;
+                window = n >> 8;
 
                 entry = n & 255;
 
                 bitmaps[entry / 8] |= 1 << (7 - (entry % 8));
         }
 
-        r = dns_packet_append_type_window(p, window, len, bitmaps, NULL);
+        r = dns_packet_append_type_window(p, window, entry / 8 + 1, bitmaps, NULL);
         if (r < 0)
                 goto fail;
 
