9436e8cae4709b50ed57f2f5858a3ffad03d5d32
Lennart Poettering <lennart@poettering.net>
2015-07-29 12:22:55 +0200

    resolved: never attempt to resolve loopback addresses via DNS/LLMNR/mDNS
    We already refuse to resolve "localhost", hence we should also refuse
    resolving "127.0.0.1" and friends.

--- src/test/test-dns-domain.c
+++ src/test/test-dns-domain.c
@@ -247,6 +247,8 @@ static void test_dns_name_reverse_one(const char *address, const char *name) {
 static void test_dns_name_reverse(void) {
         test_dns_name_reverse_one("47.11.8.15", "15.8.11.47.in-addr.arpa");
         test_dns_name_reverse_one("fe80::47", "7.4.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.8.e.f.ip6.arpa");
+        test_dns_name_reverse_one("127.0.0.1", "1.0.0.127.in-addr.arpa");
+        test_dns_name_reverse_one("::1", "1.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.ip6.arpa");
 }
 
 int main(int argc, char *argv[]) {
