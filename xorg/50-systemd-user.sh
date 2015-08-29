a611ce30e25e5ef3429b9753cf4f5349ebdbdba8
Jan Alexander Steffens (heftig) <jan.steffens@gmail.com>
2015-08-29 23:05:53 +0200

    xorg/50-systemd-user.sh: Update dbus-daemon, too
    Caveat: This will update systemd twice if we're running
    with a kernel bus.

--- xorg/50-systemd-user.sh
+++ xorg/50-systemd-user.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+if which dbus-update-activation-environment >/dev/null 2>&1; then
+        dbus-update-activation-environment DISPLAY XAUTHORITY
+fi
+
 systemctl --user import-environment DISPLAY XAUTHORITY
