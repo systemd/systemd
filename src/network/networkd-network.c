kinvolk-archives/systemd-legacy

networkd: default to "kernel" IPForwarding setting

src/network/networkd-network.c
@@ -110,6 +110,8 @@ static int network_load_one(Manager *manager, const char *filename) {
 
         network->link_local = ADDRESS_FAMILY_IPV6;
 
+        network->ip_forward = ADDRESS_FAMILY_KERNEL;
+
         r = config_parse(NULL, filename, file,
                          "Match\0"
                          "Link\0"

systemd/systemd

src/network/networkd-network.c
@@ -115,6 +115,8 @@ static int network_load_one(Manager *manager, const char *filename) {
 
         network->link_local = ADDRESS_FAMILY_IPV6;
 
+        network->ip_forward = _ADDRESS_FAMILY_BOOLEAN_INVALID;
+
         network->ipv6_privacy_extensions = IPV6_PRIVACY_EXTENSIONS_NO;
 
         r = config_parse(NULL, filename, file,