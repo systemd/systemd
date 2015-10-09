de53c4174886898410fdce7eb7551a6a1ed19655
Evgeny Vereshchagin <evvers@ya.ru>
2015-10-09 12:37:50 +0000

    systemd-run can now launch units with SyslogIdentifier

--- src/shared/bus-util.c
+++ src/shared/bus-util.c
@@ -1494,7 +1494,7 @@ int bus_append_unit_property_assignment(sd_bus_message *m, const char *assignmen
                               "UtmpIdentifier", "UtmpMode", "PAMName", "TTYPath",
                               "StandardInput", "StandardOutput", "StandardError",
                               "Description", "Slice", "Type", "WorkingDirectory",
-                              "RootDirectory"))
+                              "RootDirectory", "SyslogIdentifier"))
                 r = sd_bus_message_append(m, "v", "s", eq);
 
         else if (streq(field, "DeviceAllow")) {
