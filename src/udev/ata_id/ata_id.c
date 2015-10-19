aa6a52b0e5be05756465d49134cad235181b08b1
tomty89 <tom.ty89@gmail.com>
2015-10-20 07:01:00 +0800

    udev: make ata_id CK_COND sense check a function

--- src/udev/ata_id/ata_id.c
+++ src/udev/ata_id/ata_id.c
@@ -113,6 +113,18 @@ static int disk_scsi_inquiry_command(int      fd,
         return 0;
 }
 
+static int ck_cond_sense_check(uint8_t *sense) {
+        /*
+         * Check that there is no deferred error, sense key is RECOVERED ERROR, additional sense
+         * code is ATA PASS-THROUGH INFORMATION AVAILABLE. Allow fixed format sense data. Check
+         * that VALID bit is one and SDAT_OVFL bit is zero.
+         */
+        if (!(sense[0] == 0x72 && (sense[1] & 0x0f) == 1 && sense[2] == 0x00 && sense[3] == 0x1d) &&
+            !(sense[0] == 0xf0 && (sense[2] & 0x1f) == 1 && sense[12] == 0x00 && sense[13] == 0x1d))
+                return -1;
+        return 0;
+}
+
 static int disk_identify_command(int          fd,
                                  void         *buf,
                                  size_t          buf_len)
@@ -174,14 +186,10 @@ static int disk_identify_command(int          fd,
                         return ret;
         }
 
-        /* Check that there is no deferred error, sense key is RECOVERED ERROR, additional sense
-           code is ATA PASS-THROUGH INFORMATION AVAILABLE. Allow fixed format sense data. Check
-           that VALID bit is one and SDAT_OVFL bit is zero. */
-
-        if (!(sense[0] == 0x72 && (sense[1] & 0x0f) == 1 && sense[2] == 0x00 && sense[3] == 0x1d) &&
-            !(sense[0] == 0xf0 && (sense[2] & 0x1f) == 1 && sense[12] == 0x00 && sense[13] == 0x1d)) {
+        ret = ck_cond_sense_check(sense);
+        if (ret != 0) {
                 errno = EIO;
-                return -1;
+                return ret;
         }
 
         return 0;
@@ -254,14 +262,10 @@ static int disk_identify_packet_device_command(int          fd,
                         return ret;
         }
 
-        /* Check that there is no deferred error, sense key is RECOVERED ERROR, additional sense
-           code is ATA PASS-THROUGH INFORMATION AVAILABLE. Allow fixed format sense data. Check
-           that VALID bit is one and SDAT_OVFL bit is zero. */
-
-        if (!(sense[0] == 0x72 && (sense[1] & 0x0f) == 1 && sense[2] == 0x00 && sense[3] == 0x1d) &&
-            !(sense[0] == 0xf0 && (sense[2] & 0x1f) == 1 && sense[12] == 0x00 && sense[13] == 0x1d)) {
+        ret = ck_cond_sense_check(sense);
+        if (ret != 0) {
                 errno = EIO;
-                return -1;
+                return ret;
         }
 
         return 0;
@@ -607,7 +611,7 @@ int main(int argc, char *argv[])
                         /*
                          * If bit 3 of word 76 is set to one, then the device supports the Gen3
                          * signaling rate of 6.0 Gb/s (see SATA 3.0).
-                        /*
+                         *
                          * If bit 2 of word 76 is set to one, then the device supports the Gen2
                          * signaling rate of 3.0 Gb/s (see SATA 3.0).
                          *
