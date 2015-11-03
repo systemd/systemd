caffe412c77a662f7b856109e588b03817acc36e
Lennart Poettering <lennart@poettering.net>
2015-11-03 12:27:12 +0100

    sd-daemon: explicitly filter out -1 when parsing watchdog timeout
    We already filter out 0, and as -1 is usually special (meaning infinity,
    as in USEC_INFINITY) we should better not accept it either. Better safe
    than sorry...

--- src/libsystemd/sd-daemon/sd-daemon.c
+++ src/libsystemd/sd-daemon/sd-daemon.c
@@ -586,7 +586,7 @@ _public_ int sd_watchdog_enabled(int unset_environment, uint64_t *usec) {
         r = safe_atou64(s, &u);
         if (r < 0)
                 goto finish;
-        if (u <= 0) {
+        if (u <= 0 || u >= USEC_INFINITY) {
                 r = -EINVAL;
                 goto finish;
         }
