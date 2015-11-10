dc14f77c25360dda7db511ed73054a16c4023ae7
Susant Sahani <ssahani@gmail.com>
2015-11-10 09:07:11 +0530

    networkd: add support to configure IPv6 router_solicitation
    router_solicitation_delay:
     Number of seconds to wait after interface is brought up before sending
     Router Solicitations.
    
    router_solicitation_interval:
     Number of seconds to wait between Router Solicitations.
    
    router_solicitations:
       Number of Router Solicitations to send until assuming no routers are
    present.

--- src/network/networkd-network.c
+++ src/network/networkd-network.c
@@ -127,6 +127,9 @@ static int network_load_one(Manager *manager, const char *filename) {
         network->ipv6_privacy_extensions = IPV6_PRIVACY_EXTENSIONS_NO;
         network->ipv6_accept_ra = -1;
         network->ipv6_dad_transmits = -1;
+        network->router_solicitation_delay = 0;
+        network->router_solicitation_interval = 0;
+        network->router_solicitations = -1;
 
         r = config_parse(NULL, filename, file,
                          "Match\0"
