c292d9e953c071c445c53ae0842848783ec242cc
Lennart Poettering <lennart@poettering.net>
2015-11-17 00:02:27 +0100

    coredump: modernize error logging a bit

--- src/journal/coredump-vacuum.c
+++ src/journal/coredump-vacuum.c
@@ -157,8 +157,7 @@ int coredump_vacuum(int exclude_fd, uint64_t keep_free, uint64_t max_use) {
                 if (errno == ENOENT)
                         return 0;
 
-                log_error_errno(errno, "Can't open coredump directory: %m");
-                return -errno;
+                return log_error_errno(errno, "Can't open coredump directory: %m");
         }
 
         for (;;) {
@@ -183,7 +182,7 @@ int coredump_vacuum(int exclude_fd, uint64_t keep_free, uint64_t max_use) {
                                 if (errno == ENOENT)
                                         continue;
 
-                                log_warning("Failed to stat /var/lib/systemd/coredump/%s", de->d_name);
+                                log_warning_errno(errno, "Failed to stat /var/lib/systemd/coredump/%s: %m", de->d_name);
                                 continue;
                         }
 
@@ -259,8 +258,7 @@ int coredump_vacuum(int exclude_fd, uint64_t keep_free, uint64_t max_use) {
                         if (errno == ENOENT)
                                 continue;
 
-                        log_error_errno(errno, "Failed to remove file %s: %m", worst->oldest_file);
-                        return -errno;
+                        return log_error_errno(errno, "Failed to remove file %s: %m", worst->oldest_file);
                 } else
                         log_info("Removed old coredump %s.", worst->oldest_file);
         }
@@ -268,6 +266,5 @@ int coredump_vacuum(int exclude_fd, uint64_t keep_free, uint64_t max_use) {
         return 0;
 
 fail:
-        log_error_errno(errno, "Failed to read directory: %m");
-        return -errno;
+        return log_error_errno(errno, "Failed to read directory: %m");
 }
