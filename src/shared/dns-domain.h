54adabf727fbcf1a4f49b59362e92b79da1082f1
Beniamino Galvani <bgalvani@redhat.com>
2015-11-16 17:35:50 +0100

    dns-domain: add dns_name_to_wire_format()
    The function converts a domain name string to the wire format
    described in RFC 1035 Section 3.1.

--- src/shared/dns-domain.h
+++ src/shared/dns-domain.h
@@ -67,3 +67,5 @@ int dns_name_address(const char *p, int *family, union in_addr_union *a);
 
 int dns_name_root(const char *name);
 int dns_name_single_label(const char *name);
+
+int dns_name_to_wire_format(const char *domain, uint8_t *buffer, size_t len);
