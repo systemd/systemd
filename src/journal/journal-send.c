9a7800af088cc013573310504ae76e325d44d4b4
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2015-12-03 00:27:22 -0500

    journal: addition and multiplication do not commute

--- src/journal/journal-send.c
+++ src/journal/journal-send.c
@@ -225,8 +225,8 @@ _public_ int sd_journal_sendv(const struct iovec *iov, int n) {
         assert_return(iov, -EINVAL);
         assert_return(n > 0, -EINVAL);
 
-        w = alloca(sizeof(struct iovec) * n * 5 + 3);
-        l = alloca(sizeof(uint64_t) * n);
+        w = newa(struct iovec, n * 5 + 3);
+        l = newa(uint64_t, n);
 
         for (i = 0; i < n; i++) {
                 char *c, *nl;
