7de65273c71889d22aa1287cb877709b996aa3d6
Henrik Kaare Poulsen <henrik@kaarposoft.dk>
2015-12-15 17:02:05 +0100

    utf8: add utf8_escape_non_printable_newline
    The current utf8_escape_non_printable function
    does not have a 'newline' arg.
    
    Refactor utf8_escape_non_printable
    in a way similar to utf8_is_printable:
    
    Add new utf8_escape_non_printable_newline function with a 'newline' arg.
    Define utf8_escape_non_printable in terms of
    utf8_escape_non_printable_newline.
    
    This "should" not introduce any regressions for current
    uses of utf8_escape_non_printable,
    but adds the possibility to use utf8_escape_non_printable_newline.

--- src/basic/utf8.c
+++ src/basic/utf8.c
@@ -213,7 +213,7 @@ char *utf8_escape_invalid(const char *str) {
         return p;
 }
 
-char *utf8_escape_non_printable(const char *str) {
+char *utf8_escape_non_printable_newline(const char *str, bool newline) {
         char *p, *s;
 
         assert(str);
@@ -227,7 +227,7 @@ char *utf8_escape_non_printable(const char *str) {
 
                 len = utf8_encoded_valid_unichar(str);
                 if (len > 0) {
-                        if (utf8_is_printable(str, len)) {
+                        if (utf8_is_printable_newline(str, len, newline)) {
                                 s = mempcpy(s, str, len);
                                 str += len;
                         } else {
