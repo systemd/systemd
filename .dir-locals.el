e84750c905111675bdae8437257c764e13d776b5
Lennart Poettering <lennart@poettering.net>
2016-01-08 00:24:06 +0100

    editors: specify fill column
    Let's be a bit more precise with the editor configuration and specify a higher fill column of 119. This isn't as emacs'
    default of 70, but also not particularly high on today's screens.
    
    While we are at it, also set a couple of other emacs C coding style variables.

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,5 +3,11 @@
 ; Mode can be nil, which gives default values.
 
 ((nil . ((indent-tabs-mode . nil)
-         (tab-width . 8)))
-)
+         (tab-width . 8)
+         (fill-column . 119)))
+ (c-mode . ((c-basic-offset . 8)
+            (eval . (c-set-offset 'substatement-open 0))
+            (eval . (c-set-offset 'statement-case-open 0))
+            (eval . (c-set-offset 'case-label 0))
+            (eval . (c-set-offset 'arglist-intro '++))
+            (eval . (c-set-offset 'arglist-close 0)))))
