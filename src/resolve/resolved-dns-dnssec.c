f009fda92c67ee78672f519a62ce675170cdae4c
Lennart Poettering <lennart@poettering.net>
2016-01-18 22:45:18 +0100

    update DNSSEC TODO

--- src/resolve/resolved-dns-dnssec.c
+++ src/resolve/resolved-dns-dnssec.c
@@ -41,9 +41,6 @@
  *   - enable by default
  *   - Allow clients to request DNSSEC even if DNSSEC is off
  *   - make sure when getting an NXDOMAIN response through CNAME, we still process the first CNAMEs in the packet
- *   - update test-complex to also do ResolveAddress lookups
- *   - extend complex test to check "xn--kprw13d." DNAME domain
- *   - rework IDNA stuff: only to IDNA for ResolveAddress and ResolveService, and prepare a pair of lookup keys then, never do IDNA comparisons after that
  * */
 
 #define VERIFY_RRS_MAX 256
