5c7290b1956453024fc14abba2385ea9e9bccf8c
Evgeny Vereshchagin <evvers@ya.ru>
2016-01-31 09:01:43 +0000

    tests: add test-selinux-checks

---
+++ test/TEST-06-SELINUX/test-selinux-checks.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -x
+set -e
+set -o pipefail
+
+echo 1 >/sys/fs/selinux/enforce
+runcon -t systemd_test_start_t systemctl start hola
+runcon -t systemd_test_reload_t systemctl reload hola
+runcon -t systemd_test_stop_t systemctl stop hola
+
+touch /testok
+exit 0
