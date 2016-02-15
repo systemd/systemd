8e1afa0a5dcb95da633acd5004a56bbfc9bf9423
Alexander Kuleshov <kuleshovmail@gmail.com>
2016-02-16 00:04:09 +0600

    machine: use deserialize_timestamp_value()
    which is introduced in the ebf30a086dfa commit.

--- src/machine/machine.c
+++ src/machine/machine.c
@@ -299,17 +299,8 @@ int machine_load(Machine *m) {
                         m->class = c;
         }
 
-        if (realtime) {
-                unsigned long long l;
-                if (sscanf(realtime, "%llu", &l) > 0)
-                        m->timestamp.realtime = l;
-        }
-
-        if (monotonic) {
-                unsigned long long l;
-                if (sscanf(monotonic, "%llu", &l) > 0)
-                        m->timestamp.monotonic = l;
-        }
+        deserialize_timestamp_value(realtime, &m->timestamp.realtime);
+        deserialize_timestamp_value(monotonic, &m->timestamp.monotonic);
 
         if (netif) {
                 size_t allocated = 0, nr = 0;
