b722348d050aa2754cd9f903e8c3ce810c616b06
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2016-02-18 21:54:31 -0500

    activate: fix -E option parsing
    Fixes #2658.

--- src/activate/activate.c
+++ src/activate/activate.c
@@ -385,7 +385,7 @@ static int parse_argv(int argc, char *argv[]) {
         assert(argc >= 0);
         assert(argv);
 
-        while ((c = getopt_long(argc, argv, "+hl:aEd", options, NULL)) >= 0)
+        while ((c = getopt_long(argc, argv, "+hl:aE:d", options, NULL)) >= 0)
                 switch(c) {
                 case 'h':
                         help();
