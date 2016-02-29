0f5f63c3282e90b33a61dc7f9bb7110e49685627
Alexander Kuleshov <kuleshovmail@gmail.com>
2016-02-29 18:56:57 +0600

    core: use DUAL_TIMESTAMP_NULL to reset kernel_timestamp
    instead of direct reset of kernel_timestamp fields.

--- src/core/main.c
+++ src/core/main.c
@@ -1435,9 +1435,7 @@ int main(int argc, char *argv[]) {
 
                 /* clear the kernel timestamp,
                  * because we are in a container */
-                kernel_timestamp.monotonic = 0ULL;
-                kernel_timestamp.realtime = 0ULL;
-
+                kernel_timestamp = DUAL_TIMESTAMP_NULL;
         } else {
                 /* Running as user instance */
                 arg_running_as = MANAGER_USER;
