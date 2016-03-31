e6c8f7bfab21bf0bc7858c9288a76a44cb7740a3
Bjørnar Ness <bjornar.ness@gmail.com>
2016-03-31 22:34:29 +0000

    overlay with lower inside container

--- src/nspawn/nspawn-mount.c
+++ src/nspawn/nspawn-mount.c
@@ -526,7 +526,11 @@ static int mount_overlay(const char *dest, CustomMount *m) {
 
         (void) mkdir_p_label(m->source, 0755);
 
-        lower = joined_and_escaped_lower_dirs(m->lower);
+        if (strv_length(m->lower) == 0)
+                lower = strdup(where);
+        else
+                lower = joined_and_escaped_lower_dirs(m->lower);
+
         if (!lower)
                 return log_oom();
 
