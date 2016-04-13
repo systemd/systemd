8557fbb66f68e6f890737f3643efa00c60484a35
Susant Sahani <susant@redhat.com>
2016-04-13 09:17:23 +0530

    networkd: Add support to configure proxy arp support to interfaces
    This patch addresses 2889

--- src/network/networkd-network.h
+++ src/network/networkd-network.h
@@ -139,6 +139,7 @@ struct Network {
         int ipv6_accept_ra;
         int ipv6_dad_transmits;
         int ipv6_hop_limit;
+        int proxy_arp;
 
         union in_addr_union ipv6_token;
         IPv6PrivacyExtensions ipv6_privacy_extensions;
