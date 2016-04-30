3b6a4e97eab023f3792efde5bcb82a6e2688b15c
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2016-04-29 21:18:02 -0400

    dhcp-identifier: un-inline dhcp_validate_duid_len
    After all it is used in more than one place and is not that short.
    Also tweak the test a bit:
    - do not check that duid_len > 0, because we want to allow unknown
      duid types, and there might be some which are fine with 0 length data,
      (also assert should not be called from library code),
    - always check that duid_len <= MAX_DUID_LEN, because we could overwrite
      available buffer space otherwise.

--- src/libsystemd-network/dhcp-identifier.h
+++ src/libsystemd-network/dhcp-identifier.h
@@ -40,27 +40,28 @@ typedef enum DUIDType {
  */
 #define MAX_DUID_LEN 128
 
+/* https://tools.ietf.org/html/rfc3315#section-9.1 */
 struct duid {
         be16_t type;
         union {
                 struct {
-                        /* DHCP6_DUID_LLT */
+                        /* DUID_TYPE_LLT */
                         uint16_t htype;
                         uint32_t time;
                         uint8_t haddr[0];
                 } _packed_ llt;
                 struct {
-                        /* DHCP6_DUID_EN */
+                        /* DUID_TYPE_EN */
                         uint32_t pen;
                         uint8_t id[8];
                 } _packed_ en;
                 struct {
-                        /* DHCP6_DUID_LL */
+                        /* DUID_TYPE_LL */
                         int16_t htype;
                         uint8_t haddr[0];
                 } _packed_ ll;
                 struct {
-                        /* DHCP6_DUID_UUID */
+                        /* DUID_TYPE_UUID */
                         sd_id128_t uuid;
                 } _packed_ uuid;
                 struct {
@@ -69,36 +70,6 @@ struct duid {
         };
 } _packed_;
 
+int dhcp_validate_duid_len(uint16_t duid_type, size_t duid_len);
 int dhcp_identifier_set_duid_en(struct duid *duid, size_t *len);
 int dhcp_identifier_set_iaid(int ifindex, uint8_t *mac, size_t mac_len, void *_id);
-
-static inline int dhcp_validate_duid_len(uint16_t duid_type, size_t duid_len) {
-        struct duid d;
-
-        assert(duid_len > 0);
-
-        switch (duid_type) {
-        case DUID_TYPE_LLT:
-                if (duid_len <= sizeof(d.llt))
-                        return -EINVAL;
-                break;
-        case DUID_TYPE_EN:
-                if (duid_len != sizeof(d.en))
-                        return -EINVAL;
-                break;
-        case DUID_TYPE_LL:
-                if (duid_len <= sizeof(d.ll))
-                        return -EINVAL;
-                break;
-        case DUID_TYPE_UUID:
-                if (duid_len != sizeof(d.uuid))
-                        return -EINVAL;
-                break;
-        default:
-                if (duid_len > sizeof(d.raw))
-                        return -EINVAL;
-                /* accept unknown type in order to be forward compatible */
-                break;
-        }
-        return 0;
-}
