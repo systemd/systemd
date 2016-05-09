35262e69b7bae9478e48c28c398551de2c2d9386
Susant Sahani <ssahani@redhat.com>
2016-05-09 10:41:07 +0530

    networkd: bridge add support to configure multicast snooping
    This patch implements support for the IFLA_BR_MCAST_SNOOPING attribute
    it can change the multicast snooping value.
    
    IGMP snooping monitors the Internet Group Management Protocol (IGMP)
    traffic between hosts and multicast routers.

--- src/network/networkd-netdev-bridge.c
+++ src/network/networkd-netdev-bridge.c
@@ -96,6 +96,12 @@ static int netdev_bridge_post_create(NetDev *netdev, Link *link, sd_netlink_mess
                         return log_netdev_error_errno(netdev, r, "Could not append IFLA_BR_MCAST_QUERIER attribute: %m");
         }
 
+        if (b->mcast_snooping >= 0) {
+                r = sd_netlink_message_append_u8(req, IFLA_BR_MCAST_SNOOPING, b->mcast_snooping);
+                if (r < 0)
+                        return log_netdev_error_errno(netdev, r, "Could not append IFLA_BR_MCAST_SNOOPING attribute: %m");
+        }
+
         r = sd_netlink_message_close_container(req);
         if (r < 0)
                 return log_netdev_error_errno(netdev, r, "Could not append IFLA_LINKINFO attribute: %m");
@@ -121,6 +127,7 @@ static void bridge_init(NetDev *n) {
         assert(b);
 
         b->mcast_querier = -1;
+        b->mcast_snooping = -1;
 }
 
 const NetDevVTable bridge_vtable = {
