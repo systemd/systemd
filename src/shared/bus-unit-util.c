186ad4b1a05b5c5d231a3b4b7faba8a5c40e3c9d
Jonathan Boulle <jonathanboulle@gmail.com>
2016-05-20 15:09:14 +0200

    core/dbus: expose SELinuxContext property (#3284)
    Adds support to core for systemd D-Bus clients to send the
    `SELinuxContext` property . This means `systemd-run -p
    SELinuxContext=foo` should now work.

--- src/shared/bus-unit-util.c
+++ src/shared/bus-unit-util.c
@@ -235,7 +235,7 @@ int bus_append_unit_property_assignment(sd_bus_message *m, const char *assignmen
                               "StandardInput", "StandardOutput", "StandardError",
                               "Description", "Slice", "Type", "WorkingDirectory",
                               "RootDirectory", "SyslogIdentifier", "ProtectSystem",
-                              "ProtectHome"))
+                              "ProtectHome", "SELinuxContext"))
                 r = sd_bus_message_append(m, "v", "s", eq);
 
         else if (streq(field, "SyslogLevel")) {
