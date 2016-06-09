52e045f9b8c3228db8ef2d8b53e099c654056fcc
Susant Sahani <ssahani@users.noreply.github.com>
2016-06-09 21:11:35 +0530

    bus_util: add support to map double (#3479)
    Now we don't support parsing double at map_basic.
    when trying to  use bus_message_map_all_properties with a double
    this fails. Let's add it.

--- src/shared/bus-util.c
+++ src/shared/bus-util.c
@@ -1085,6 +1085,19 @@ static int map_basic(sd_bus *bus, const char *member, sd_bus_message *m, sd_bus_
                 break;
         }
 
+        case SD_BUS_TYPE_DOUBLE: {
+                double d;
+                double *p = userdata;
+
+                r = sd_bus_message_read_basic(m, type, &d);
+                if (r < 0)
+                        break;
+
+                *p = d;
+
+                break;
+        }
+
         default:
                 break;
         }
