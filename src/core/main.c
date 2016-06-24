systemd/systemd

commit 3a18b60489504056f9b0b1a139439cbfa60a87e1

pid1: initialize status color mode after setting up TERM
Also we had to connect PID's stdio to null later since colors_enabled()
assume that stdout is connected to the console.

src/core/main.c
@@ -1382,7 +1382,6 @@ int main(int argc, char *argv[]) {
         saved_argv = argv;
         saved_argc = argc;
 
-        log_show_color(colors_enabled());
         log_set_upgrade_syslog_to_journal(true);
 
         /* Disable the umask logic */
@@ -1393,7 +1392,6 @@ int main(int argc, char *argv[]) {
 
                 /* Running outside of a container as PID 1 */
                 arg_system = true;
-                make_null_stdio();
                 log_set_target(LOG_TARGET_KMSG);
                 log_open();
 
@@ -1511,11 +1509,18 @@ int main(int argc, char *argv[]) {
 
         /* We expect the environment to be set correctly if run inside a
          * container. */
-        if (arg_system && detect_container() <= 0)
+        if (arg_system && detect_container() <= 0) {
                 if (fixup_environment() < 0) {
                         error_message = "Failed to fix up PID1 environment";
                         goto finish;
                 }
+
+                /* Try to figure out if we can use colors with the console. No
+                 * need to do that for user instances since they never log
+                 * into the console. */
+                log_show_color(colors_enabled());
+                make_null_stdio();
+        }
 
         /* Initialize default unit */
         r = free_and_strdup(&arg_default_unit, SPECIAL_DEFAULT_TARGET);

commit 37d8755f9ec872d7f1e5ea6f7e69029a5664bb51

pid1: restore console color support for containers
Commit 3a18b60489504056f9b0b1a139439cbfa60a87e1 introduced a regression that
disabled the color mode for container.

This patch fixes this.

src/core/main.c
@@ -1507,13 +1507,14 @@ int main(int argc, char *argv[]) {
                         (void) write_string_file("/proc/sys/kernel/core_pattern", "|/bin/false", 0);
         }
 
-        /* We expect the environment to be set correctly if run inside a
-         * container. */
-        if (arg_system && detect_container() <= 0) {
-                if (fixup_environment() < 0) {
-                        error_message = "Failed to fix up PID1 environment";
-                        goto finish;
-                }
+        if (arg_system) {
+                /* We expect the environment to be set correctly
+                 * if run inside a container. */
+                if (detect_container() <= 0)
+                        if (fixup_environment() < 0) {
+                                error_message = "Failed to fix up PID1 environment";
+                                goto finish;
+                        }
 
                 /* Try to figure out if we can use colors with the console. No
                  * need to do that for user instances since they never log