a17402291b627e330c44d1461c105de3277f4d88
Michał Bartoszkiewicz <mbartoszkiewicz@gmail.com>
2016-07-05 19:23:23 +0200

    systemd-run: really make -E an alias for --setenv (#3654)
    systemd-run --help says:
      -E --setenv=NAME=VALUE          Set environment

--- src/run/run.c
+++ src/run/run.c
@@ -187,7 +187,7 @@ static int parse_argv(int argc, char *argv[]) {
         assert(argc >= 0);
         assert(argv);
 
-        while ((c = getopt_long(argc, argv, "+hrH:M:p:tq", options, NULL)) >= 0)
+        while ((c = getopt_long(argc, argv, "+hrH:M:E:p:tq", options, NULL)) >= 0)
 
                 switch (c) {
 
