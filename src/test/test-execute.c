faf86e0a9777bc50e79e263454784f08f3100dfa
Jan Synacek <jsynacek@redhat.com>
2016-07-28 10:35:07 +0200

    test: fix test-execute personality tests on ppc64 and aarch64

--- src/test/test-execute.c
+++ src/test/test-execute.c
@@ -91,6 +91,16 @@ static void test_exec_personality(Manager *m) {
 #elif defined(__s390__)
         test(m, "exec-personality-s390.service", 0, CLD_EXITED);
 
+#elif defined(__powerpc64__)
+#  if __BYTE_ORDER == __BIG_ENDIAN
+        test(m, "exec-personality-ppc64.service", 0, CLD_EXITED);
+#  else
+        test(m, "exec-personality-ppc64le.service", 0, CLD_EXITED);
+#  endif
+
+#elif defined(__aarch64__)
+        test(m, "exec-personality-aarch64.service", 0, CLD_EXITED);
+
 #elif defined(__i386__)
         test(m, "exec-personality-x86.service", 0, CLD_EXITED);
 #endif
