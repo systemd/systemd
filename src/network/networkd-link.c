systemd/systemd

commit 3104883ddc2452ae210de3c70576c03023250a51

networkd: remove route if carrier is lost (#3831)
Fixes #3669.

src/network/networkd-link.c
@@ -2314,6 +2314,35 @@ static int link_drop_foreign_config(Link *link) {
         return 0;
 }
 
+static int link_drop_config(Link *link) {
+        Address *address;
+        Route *route;
+        Iterator i;
+        int r;
+
+        SET_FOREACH(address, link->addresses, i) {
+                /* we consider IPv6LL addresses to be managed by the kernel */
+                if (address->family == AF_INET6 && in_addr_is_link_local(AF_INET6, &address->in_addr) == 1)
+                        continue;
+
+                r = address_remove(address, link, link_address_remove_handler);
+                if (r < 0)
+                        return r;
+        }
+
+        SET_FOREACH(route, link->routes, i) {
+                /* do not touch routes managed by the kernel */
+                if (route->protocol == RTPROT_KERNEL)
+                        continue;
+
+                r = route_remove(route, link, link_route_remove_handler);
+                if (r < 0)
+                        return r;
+        }
+
+        return 0;
+}
+
 static int link_update_lldp(Link *link) {
         int r;
 
@@ -2859,6 +2888,14 @@ static int link_carrier_lost(Link *link) {
                 link_enter_failed(link);
                 return r;
         }
+
+        r = link_drop_config(link);
+        if (r < 0)
+                return r;
+
+        r = link_drop_foreign_config(link);
+        if (r < 0)
+                return r;
 
         r = link_handle_bound_by_list(link);
         if (r < 0)

commit c436d553973b43ec5e8cf2eab84dd3e9e334295f

networkd: do not drop config for unmanaged interfaces
Flushing foreign configuration for unmanaged interfaces is outright
evil, especially when it's a regular occurence with Wi-Fi.

Fixes: 3104883ddc24 "networkd: remove route if carrier is lost"
Ref: #3831

src/network/networkd-link.c
@@ -2957,9 +2957,11 @@ static int link_carrier_lost(Link *link) {
         if (r < 0)
                 return r;
 
-        r = link_drop_foreign_config(link);
-        if (r < 0)
-                return r;
+        if (link->state != LINK_STATE_UNMANAGED) {
+                r = link_drop_foreign_config(link);
+                if (r < 0)
+                        return r;
+        }
 
         r = link_handle_bound_by_list(link);
         if (r < 0)