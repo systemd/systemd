57a6fb46dfe71b1a8c8d58996ffd63fae022835b
Keith Busch <keith.busch@intel.com>
2016-09-16 12:12:54 -0400

    udev/path_id: introduce support for NVMe devices
    This appends the nvme name and namespace identifier attribute the the
    PCI path for by-path links. Symlinks like the following are now present:
    
    lrwxrwxrwx. 1 root root 13 Sep 16 12:12 pci-0000:01:00.0-nvme-1 -> ../../nvme0n1
    lrwxrwxrwx. 1 root root 15 Sep 16 12:12 pci-0000:01:00.0-nvme-1-part1 -> ../../nvme0n1p1
    
    Cc: Michal Sekletar <sekletar.m@gmail.com>
    Signed-off-by: Keith Busch <keith.busch@intel.com>

--- src/udev/udev-builtin-path_id.c
+++ src/udev/udev-builtin-path_id.c
@@ -693,6 +693,15 @@ static int builtin_path_id(struct udev_device *dev, int argc, char *argv[], bool
                         parent = skip_subsystem(parent, "iucv");
                         supported_transport = true;
                         supported_parent = true;
+                } else if (streq(subsys, "nvme")) {
+                        const char *nsid = udev_device_get_sysattr_value(dev, "nsid");
+
+                        if (nsid) {
+                                path_prepend(&path, "nvme-%s", nsid);
+                                parent = skip_subsystem(parent, "nvme");
+                                supported_parent = true;
+                                supported_transport = true;
+                        }
                 }
 
                 if (parent)
