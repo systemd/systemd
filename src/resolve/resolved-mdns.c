3755027c2cada70345c96787a9b5569994dd23ed
Dmitry Rozhkov <dmitry.rozhkov@linux.intel.com>
2016-12-02 11:31:45 +0200

    resolved: adhere to RFC when handling mDNS goodbye packets
    Signed-off-by: Dmitry Rozhkov <dmitry.rozhkov@linux.intel.com>

--- src/resolve/resolved-mdns.c
+++ src/resolve/resolved-mdns.c
@@ -118,6 +118,12 @@ static int on_mdns_packet(sd_event_source *s, int fd, uint32_t revents, void *us
                               dns_name_endswith(name, "local") > 0))
                                 return 0;
 
+                        if (rr->ttl == 0) {
+                                log_debug("Got a goodbye packet");
+                                /* See the section 10.1 of RFC6762 */
+                                rr->ttl = 1;
+                        }
+
                         t = dns_scope_find_transaction(scope, rr->key, false);
                         if (t)
                                 dns_transaction_process_reply(t, p);
