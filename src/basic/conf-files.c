4e281f68eaec5592c4de9b0974f42ece71cd6f80
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2017-01-22 14:16:19 -0500

    basic/conf-files: extend conf_files_list() to list unsuffixed files
    5dd11ab5f36ce71138005 did a similar change for conf_files_list_strv().
    Here we do the same for conf_files_list() and conf_files_list_nulstr().
    
    No change for existing users. Tests are added.

--- src/basic/conf-files.c
+++ src/basic/conf-files.c
@@ -137,7 +137,6 @@ int conf_files_list(char ***strv, const char *suffix, const char *root, const ch
         va_list ap;
 
         assert(strv);
-        assert(suffix);
 
         va_start(ap, dir);
         dirs = strv_new_ap(dir, ap);
@@ -153,7 +152,6 @@ int conf_files_list_nulstr(char ***strv, const char *suffix, const char *root, c
         _cleanup_strv_free_ char **dirs = NULL;
 
         assert(strv);
-        assert(suffix);
 
         dirs = strv_split_nulstr(d);
         if (!dirs)
