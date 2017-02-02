315782db1423021c57d0dacecc48e5d15359c1cf
Lennart Poettering <lennart@poettering.net>
2017-02-02 18:35:00 +0100

    time-util: add overflow checking to monotonic timestamp specifications

--- src/basic/time-util.c
+++ src/basic/time-util.c
@@ -844,6 +844,8 @@ from_tm:
                 return -EINVAL;
 
 finish:
+        if (ret + plus < ret) /* overflow? */
+                return -EINVAL;
         ret += plus;
         if (ret > USEC_TIMESTAMP_FORMATTABLE_MAX)
                 return -EINVAL;
