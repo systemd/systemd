8367fea557cffaa6e870ccf1b94a063f560a922f
Lennart Poettering <lennart@poettering.net>
2017-02-10 03:54:48 +0100

    core: make sure to destroy all name watching bus slots when we are kicked off the bus (#5294)
    Fixes: #4528

--- src/core/unit.c
+++ src/core/unit.c
@@ -2650,7 +2650,7 @@ void unit_unwatch_bus_name(Unit *u, const char *name) {
         assert(u);
         assert(name);
 
-        hashmap_remove_value(u->manager->watch_bus, name, u);
+        (void) hashmap_remove_value(u->manager->watch_bus, name, u);
         u->match_bus_slot = sd_bus_slot_unref(u->match_bus_slot);
 }
 
