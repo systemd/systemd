b0ac3401c8a67f3a8962b65f7978cda9aa3fca6a
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2017-02-14 01:03:40 -0500

    Revert "test: make unit tests relocatable and add an "install-tests" make target"

--- src/test/test-cgroup-mask.c
+++ src/test/test-cgroup-mask.c
@@ -34,7 +34,7 @@ static int test_cgroup_mask(void) {
         int r;
 
         /* Prepare the manager. */
-        assert_se(set_unit_path(TEST_DATA_DIR("")) >= 0);
+        assert_se(set_unit_path(TEST_DIR) >= 0);
         r = manager_new(UNIT_FILE_USER, true, &m);
         if (r == -EPERM || r == -EACCES) {
                 puts("manager_new: Permission denied. Skipping test.");
