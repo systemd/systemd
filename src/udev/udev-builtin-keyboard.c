474a122d8b05bcb3b7f7a29c4aa5634e03bd34e8
Patrik Flykt <patrik.flykt@linux.intel.com>
2017-04-10 16:22:05 +0300

    udev: Use struct key instead of struct key_name
    Gperf creates functions returning a struct key, fix udev builtin
    keyboard to return the same. The compilation error leading up to
    this is:
    
      CC       src/udev/libudev_core_la-udev-builtin-keyboard.lo
    In file included from src/udev/udev-builtin-keyboard.c:33:0:
    src/udev/keyboard-keys-from-name.h:123:1: error: conflicting types for ‘keyboard_lookup_key’
     keyboard_lookup_key (register const char *str, register unsigned int len)
     ^~~~~~~~~~~~~~~~~~~
    src/udev/udev-builtin-keyboard.c:32:31: note: previous declaration of ‘keyboard_lookup_key’ was here
     static const struct key_name *keyboard_lookup_key(const char *str, GPERF_LEN_TYPE len);
                                   ^~~~~~~~~~~~~~~~~~~
    src/udev/udev-builtin-keyboard.c: In function ‘map_keycode’:
    src/udev/udev-builtin-keyboard.c:83:11: error: assignment from incompatible pointer type [-Werror=incompatible-pointer-types]
             k = keyboard_lookup_key(keycode, strlen(keycode));
               ^
    src/udev/udev-builtin-keyboard.c:85:32: error: dereferencing pointer to incomplete type ‘const struct key_name’
                     keycode_num = k->id;
                                    ^~
    cc1: some warnings being treated as errors
    Makefile:19349: recipe for target 'src/udev/libudev_core_la-udev-builtin-keyboard.lo' failed

--- src/udev/udev-builtin-keyboard.c
+++ src/udev/udev-builtin-keyboard.c
@@ -29,7 +29,7 @@
 #include "string-util.h"
 #include "udev.h"
 
-static const struct key_name *keyboard_lookup_key(const char *str, GPERF_LEN_TYPE len);
+static const struct key *keyboard_lookup_key(const char *str, GPERF_LEN_TYPE len);
 #include "keyboard-keys-from-name.h"
 
 static int install_force_release(struct udev_device *dev, const unsigned *release, unsigned release_count) {
@@ -76,7 +76,7 @@ static void map_keycode(int fd, const char *devnode, int scancode, const char *k
                 unsigned key;
         } map;
         char *endptr;
-        const struct key_name *k;
+        const struct key *k;
         unsigned keycode_num;
 
         /* translate identifier to key code */
