734005fe75777b5784033ee9ccbed6acd414b30f
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2017-04-18 17:28:14 -0400

    hwdb: use curl instead of wget, and regenerate patch after successful update
    libcurl is already our build dependency, so using curl reduces the deps
    a bit, and curl also has a more modern codebase.
    
    Regenerating the patch makes it more likely that the patch will apply
    in the future.
    
    Also, update URLs which return 302 to the new location.
    
    (Patch suggested by Igor Gnatenko.)

--- tools/meson-hwdb-update.sh
+++ tools/meson-hwdb-update.sh
@@ -1,13 +1,15 @@
 #!/bin/sh -ex
 
 cd "$1"
-wget -O usb.ids 'http://www.linux-usb.org/usb.ids'
-wget -O pci.ids 'http://pci-ids.ucw.cz/v2.2/pci.ids'
-wget -O ma-large.txt 'http://standards.ieee.org/develop/regauth/oui/oui.txt'
-wget -O ma-medium.txt 'http://standards.ieee.org/develop/regauth/oui28/mam.txt'
-wget -O ma-small.txt 'http://standards.ieee.org/develop/regauth/oui36/oui36.txt'
-wget -O pnp_id_registry.html 'http://www.uefi.org/uefi-pnp-export'
-wget -O acpi_id_registry.html 'http://www.uefi.org/uefi-acpi-export'
+
+curl -L -o usb.ids 'http://www.linux-usb.org/usb.ids'
+curl -L -o pci.ids 'http://pci-ids.ucw.cz/v2.2/pci.ids'
+curl -L -o ma-large.txt 'http://standards-oui.ieee.org/oui/oui.txt'
+curl -L -o ma-medium.txt 'http://standards-oui.ieee.org/oui28/mam.txt'
+curl -L -o ma-small.txt 'http://standards-oui.ieee.org/oui36/oui36.txt'
+curl -L -o pnp_id_registry.html 'http://www.uefi.org/uefi-pnp-export'
+curl -L -o acpi_id_registry.html 'http://www.uefi.org/uefi-acpi-export'
 ./ids-update.pl
 ./acpi-update.py > 20-acpi-vendor.hwdb.base
 patch -p0 -o- 20-acpi-vendor.hwdb.base <20-acpi-vendor.hwdb.patch >20-acpi-vendor.hwdb
+diff -u 20-acpi-vendor.hwdb.base 20-acpi-vendor.hwdb >20-acpi-vendor.hwdb.patch
