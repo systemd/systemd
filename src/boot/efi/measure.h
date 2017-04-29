1f8476b197e787a0fe237a702902adae53658659
Max Resch <mxre@users.noreply.github.com>
2017-04-29 12:24:25 +0200

    sd-boot: remove unnecessary defines (#5848)
    As discussed in #5829 `#define` is not necessary, meson and autotools
    set the correct compiler flags and the default value is provided by them.

--- src/boot/efi/measure.h
+++ src/boot/efi/measure.h
@@ -13,9 +13,6 @@
 #ifndef __SDBOOT_MEASURE_H
 #define __SDBOOT_MEASURE_H
 
-#ifndef SD_TPM_PCR
-#define SD_TPM_PCR 8
-#endif
-
 EFI_STATUS tpm_log_event(UINT32 pcrindex, const EFI_PHYSICAL_ADDRESS buffer, UINTN buffer_size, const CHAR16 *description);
+
 #endif
