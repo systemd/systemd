e9a1c62f84d824a3d757eb310cd714a4ffc75776
Susant Sahani <susant@redhat.com>
2017-05-09 19:06:59 +0530

    networkd: add support to configure route protocol.
    Closes: #5889

--- src/network/networkd-route.h
+++ src/network/networkd-route.h
@@ -77,3 +77,4 @@ int config_parse_route_scope(const char *unit, const char *filename, unsigned li
 int config_parse_route_table(const char *unit, const char *filename, unsigned line, const char *section, unsigned section_line, const char *lvalue, int ltype, const char *rvalue, void *data, void *userdata);
 int config_parse_gateway_onlink(const char *unit, const char *filename, unsigned line, const char *section, unsigned section_line, const char *lvalue, int ltype, const char *rvalue, void *data, void *userdata);
 int config_parse_ipv6_route_preference(const char *unit, const char *filename, unsigned line, const char *section, unsigned section_line, const char *lvalue, int ltype, const char *rvalue, void *data, void *userdata);
+int config_parse_route_protocol(const char *unit, const char *filename, unsigned line, const char *section, unsigned section_line, const char *lvalue, int ltype, const char *rvalue, void *data, void *userdata);
