af02b15a9ddfbfc88f9927e38436bc93d7f888aa
Evgeny Vereshchagin <evvers@ya.ru>
2017-05-10 08:54:52 +0000

    test-sigbus: skip the test under valgrind

--- src/test/test-sigbus.c
+++ src/test/test-sigbus.c
@@ -22,12 +22,20 @@
 #include "fd-util.h"
 #include "sigbus.h"
 #include "util.h"
+#ifdef HAVE_VALGRIND_VALGRIND_H
+#include <valgrind/valgrind.h>
+#endif
 
 int main(int argc, char *argv[]) {
         _cleanup_close_ int fd = -1;
         char template[] = "/tmp/sigbus-test-XXXXXX";
         void *addr = NULL;
         uint8_t *p;
+
+#ifdef HAVE_VALGRIND_VALGRIND_H
+        if (RUNNING_ON_VALGRIND)
+                return EXIT_TEST_SKIP;
+#endif
 
 #ifdef __SANITIZE_ADDRESS__
         return EXIT_TEST_SKIP;
