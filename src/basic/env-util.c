417eefb17029bdabb2aafe0a6fe39c178aa497b2
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2017-06-23 20:27:12 -0400

    basic/env-util: when serializing, actually use escaping
    Fixes #6152.

--- src/basic/env-util.c
+++ src/basic/env-util.c
@@ -779,7 +779,7 @@ int serialize_environment(FILE *f, char **environment) {
                 if (!ce)
                         return -ENOMEM;
 
-                fprintf(f, "env=%s\n", *e);
+                fprintf(f, "env=%s\n", ce);
         }
 
         /* caller should call ferror() */
