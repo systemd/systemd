1b833237198ae8bcba38c4824dc849b402eab163
Michal Suchanek <msuchanek@suse.de>
2017-06-27 11:48:45 +0200

    Use same script to build key list in Makefile and meson

---
+++ src/udev/generate-keyboard-keys-gperf.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -eu
+awk '   BEGIN {
+                print "struct key_name { const char* name; unsigned short id; };"
+                print "%null-strings"
+                print "%%"
+        }
+
+        /^KEY_/ { print tolower(substr($1 ,5)) ", " $1 }
+' < "$1"
