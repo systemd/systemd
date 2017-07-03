systemd/systemd

commit b876bc09b1de7805e3b71b682f883896bf82b42c

Include dpad buttons in joystick detection (#6240)
Since f472d466ec26 ("Remove BTN_DPAD_* keys from ID_INPUT_KEY test
    (#5701)") dpad buttons are excluded from keyboard keys for keyboard
detection.

Include them in joystick buttons for joystick detection.

src/udev/udev-builtin-input_id.c
@@ -211,6 +211,8 @@ static bool test_pointers(struct udev_device *dev,
                 has_joystick_axes_or_buttons = test_bit(button, bitmask_key);
         for (button = BTN_TRIGGER_HAPPY1; button <= BTN_TRIGGER_HAPPY40 && !has_joystick_axes_or_buttons; button++)
                 has_joystick_axes_or_buttons = test_bit(button, bitmask_key);
+        for (button = BTN_DPAD_UP; button <= BTN_DPAD_RIGHT && !has_joystick_axes_or_buttons; button++)
+                has_joystick_axes_or_buttons = test_bit(button, bitmask_key);
         for (axis = ABS_RX; axis < ABS_PRESSURE && !has_joystick_axes_or_buttons; axis++)
                 has_joystick_axes_or_buttons = test_bit(axis, bitmask_abs);
 

commit f4e5b87df7ea68d74414bc41a3393162e6568949

udev: define BTN_DPAD_RIGHT if not present
Regression introduced in commit b876bc0 when building on systemds with a pre
3.11 headers (RHEL7 and derivatives).

All the DPAD defines were introduced in the same kernel commit
d09bbfd2a8408a9954, we don't need a separate ifdef check for right.

Fixes #6240

src/udev/udev-builtin-input_id.c
@@ -47,6 +47,7 @@
 /* available as of kernel 3.11 */
 #ifndef BTN_DPAD_UP
 #define BTN_DPAD_UP 0x220
+#define BTN_DPAD_RIGHT 0x223
 #endif /* BTN_DPAD_UP */
 
 /* available as of kernel 3.13 */