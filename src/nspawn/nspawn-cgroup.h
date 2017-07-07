0402948206203ccbd6b81b10d4bf8973b87b2c60
Luke Shumaker <lukeshu@parabola.nu>
2017-07-07 18:57:08 -0400

    nspawn: Move cgroup mount stuff from nspawn-mount.c to nspawn-cgroup.c

--- src/nspawn/nspawn-cgroup.h
+++ src/nspawn/nspawn-cgroup.h
@@ -9,3 +9,6 @@
 int chown_cgroup(pid_t pid, CGroupUnified unified_requested, uid_t uid_shift);
 int sync_cgroup(pid_t pid, CGroupUnified unified_requested, uid_t uid_shift);
 int create_subcgroup(pid_t pid, bool keep_unit, CGroupUnified unified_requested);
+
+int mount_cgroups(const char *dest, CGroupUnified unified_requested, bool userns, uid_t uid_shift, uid_t uid_range, const char *selinux_apifs_context, bool use_cgns);
+int mount_systemd_cgroup_writable(const char *dest, CGroupUnified unified_requested);
