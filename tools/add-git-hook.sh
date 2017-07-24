003c8879679e21ea4b9f76770cc9a23d24759cb0
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2017-07-24 04:41:45 -0400

    meson: install the git hook (#6425)
    This was done autogen.sh previously and was dropped in
    72cdb3e783174dcf9223a49f03e3b0e2ca95ddb8. Let's add it back.
    The meson configuration step is the only reasonable place.
    
    Note that this only works for the most standard git dirs, e.g.
    the hook will not be installed if git worktree is used or if
    $GIT_DIR is specified, etc. I think that's OK because most of
    the time meson will be run at least once in the original cloned
    dir.

---
+++ tools/add-git-hook.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -eu
+
+cd "$MESON_SOURCE_ROOT"
+
+if [ ! -f .git/hooks/pre-commit.sample -o -f .git/hooks/pre-commit ]; then
+        exit 2 # not needed
+fi
+
+cp -p .git/hooks/pre-commit.sample .git/hooks/pre-commit
+chmod +x .git/hooks/pre-commit
+echo 'Activated pre-commit hook'
