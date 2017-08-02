b16bd5350fe686a0e8e1ef39cc35c383fb1560a1
Yu Watanabe <watanabe.yu+github@gmail.com>
2017-08-02 13:46:45 +0900

    seccomp-util: add parse_syscall_archs()

--- src/shared/seccomp-util.c
+++ src/shared/seccomp-util.c
@@ -30,7 +30,9 @@
 #include "macro.h"
 #include "nsflags.h"
 #include "seccomp-util.h"
+#include "set.h"
 #include "string-util.h"
+#include "strv.h"
 #include "util.h"
 #include "errno-list.h"
 
@@ -1312,4 +1314,34 @@ int seccomp_restrict_archs(Set *archs) {
                 return r;
 
         return seccomp_load(seccomp);
+}
+
+int parse_syscall_archs(char **l, Set **archs) {
+        _cleanup_set_free_ Set *_archs;
+        char **s;
+        int r;
+
+        assert(l);
+        assert(archs);
+
+        r = set_ensure_allocated(&_archs, NULL);
+        if (r < 0)
+                return r;
+
+        STRV_FOREACH(s, l) {
+                uint32_t a;
+
+                r = seccomp_arch_from_string(*s, &a);
+                if (r < 0)
+                        return -EINVAL;
+
+                r = set_put(_archs, UINT32_TO_PTR(a + 1));
+                if (r < 0)
+                        return -ENOMEM;
+        }
+
+        *archs = _archs;
+        _archs = NULL;
+
+        return 0;
 }
