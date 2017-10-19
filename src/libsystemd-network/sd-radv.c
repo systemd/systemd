55280290647d14245932908c172a13cabf8fe17b
Patrik Flykt <patrik.flykt@linux.intel.com>
2017-10-19 19:56:37 +0300

    sd-radv: Allocate space also for DNSSL iov option
    The iov array needs to hold five entries in addition to the number
    of prefixes.

--- src/libsystemd-network/sd-radv.c
+++ src/libsystemd-network/sd-radv.c
@@ -160,8 +160,9 @@ static int radv_send(sd_radv *ra, const struct in6_addr *dst,
                 .nd_opt_mtu_type = ND_OPT_MTU,
                 .nd_opt_mtu_len = 1,
         };
-        /* Reserve iov space for RA header, linkaddr, MTU, N prefixes, RDNSS */
-        struct iovec iov[4 + ra->n_prefixes];
+        /* Reserve iov space for RA header, linkaddr, MTU, N prefixes, RDNSS
+           and DNSSL */
+        struct iovec iov[5 + ra->n_prefixes];
         struct msghdr msg = {
                 .msg_name = &dst_addr,
                 .msg_namelen = sizeof(dst_addr),
