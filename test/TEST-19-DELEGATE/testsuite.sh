00e2501561e55676b04c618e4f27778aa226cf61
Lennart Poettering <lennart@poettering.net>
2017-11-23 21:34:24 +0100

    test: skip DELEGATE test if the kernel can't do cgroupv2
    Fixes: #7440

--- test/TEST-19-DELEGATE/testsuite.sh
+++ test/TEST-19-DELEGATE/testsuite.sh
@@ -4,16 +4,20 @@
 set -ex
 set -o pipefail
 
-systemd-run --wait --unit=test0.service -p "DynamicUser=1" -p "Delegate=" \
-            test -w /sys/fs/cgroup/system.slice/test0.service/ -a \
-                 -w /sys/fs/cgroup/system.slice/test0.service/cgroup.procs -a \
-                 -w /sys/fs/cgroup/system.slice/test0.service/cgroup.subtree_control
+if grep -q cgroup2 /proc/filesystems ; then
+        systemd-run --wait --unit=test0.service -p "DynamicUser=1" -p "Delegate=" \
+                    test -w /sys/fs/cgroup/system.slice/test0.service/ -a \
+                    -w /sys/fs/cgroup/system.slice/test0.service/cgroup.procs -a \
+                    -w /sys/fs/cgroup/system.slice/test0.service/cgroup.subtree_control
 
-systemd-run --wait --unit=test1.service -p "DynamicUser=1" -p "Delegate=memory pids" \
-            grep memory /sys/fs/cgroup/system.slice/test1.service/cgroup.controllers
+        systemd-run --wait --unit=test1.service -p "DynamicUser=1" -p "Delegate=memory pids" \
+                    grep memory /sys/fs/cgroup/system.slice/test1.service/cgroup.controllers
 
-systemd-run --wait --unit=test2.service -p "DynamicUser=1" -p "Delegate=memory pids" \
-            grep pids /sys/fs/cgroup/system.slice/test2.service/cgroup.controllers
+        systemd-run --wait --unit=test2.service -p "DynamicUser=1" -p "Delegate=memory pids" \
+                    grep pids /sys/fs/cgroup/system.slice/test2.service/cgroup.controllers
+else
+        echo "Skipping TEST-19-DELEGATE, as the kernel doesn't actually support cgroupsv2" >&2
+fi
 
 echo OK > /testok
 
