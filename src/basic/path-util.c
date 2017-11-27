28faeda47f75dd9f34e36c48237cb376a1dd889c
Lennart Poettering <lennart@poettering.net>
2017-11-27 20:48:46 +0100

    path-util: when checking systemd versions, check both lib and lib64
    We need to check both to be compatible with multilib images.

--- src/basic/path-util.c
+++ src/basic/path-util.c
@@ -882,7 +882,9 @@ int systemd_installation_has_version(const char *root, unsigned minimal_version)
                         * for Gentoo which does a merge without making /lib a symlink.
                         */
                        "lib/systemd/libsystemd-shared-*.so\0"
-                       "usr/lib/systemd/libsystemd-shared-*.so\0") {
+                       "lib64/systemd/libsystemd-shared-*.so\0"
+                       "usr/lib/systemd/libsystemd-shared-*.so\0"
+                       "usr/lib64/systemd/libsystemd-shared-*.so\0") {
 
                 _cleanup_strv_free_ char **names = NULL;
                 _cleanup_free_ char *path = NULL;
