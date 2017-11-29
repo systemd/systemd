2d0bc6845005e222b8b48c31fdeb8b56a6590f18
Lennart Poettering <lennart@poettering.net>
2017-11-29 19:49:05 +0100

    coccinelle: add a run-coccinelle.sh script that runs all scripts
    One day we should start running something like this as part of CI so
    that non-well-formed commits are not even accepted...

---
+++ coccinelle/run-coccinelle.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+for SCRIPT in *.cocci ; do
+        [ "$SCRIPT" = "empty-if.cocci" ] && continue
+        echo "--x-- Processing $SCRIPT --x--"
+        TMPFILE=`mktemp`
+        spatch --sp-file $SCRIPT --dir $(pwd)/.. 2> "$TMPFILE" || cat "$TMPFILE"
+        rm "$TMPFILE"
+        echo "--x-- Processed $SCRIPT --x--"
+        echo ""
+done
