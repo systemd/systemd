784b9a1a327c23652c945785dab6f2979c0af06c
Yu Watanabe <watanabe.yu+github@gmail.com>
2017-11-30 16:29:48 +0900

    run: Allows the user to reset CPUAffinity= back to an empty list
    Before this CPUAffinity= requires a valid cpu set, and the setting
    cannot be reset. Moreover, if CPUAffinity= with empty string is passed,
    then message container is closed without no values appended, thus
    we get error.
    This makes CPUAffinity= accepts empty string to reset the setting
    and avoid error.

--- src/shared/bus-unit-util.c
+++ src/shared/bus-unit-util.c
@@ -705,8 +705,7 @@ int bus_append_unit_property_assignment(sd_bus_message *m, const char *assignmen
                 if (r < 0)
                         return bus_log_create_error(r);
 
-                if (cpuset)
-                        sd_bus_message_append_array(m, 'y', cpuset, CPU_ALLOC_SIZE(ncpus));
+                sd_bus_message_append_array(m, 'y', cpuset, CPU_ALLOC_SIZE(ncpus));
 
                 r = sd_bus_message_close_container(m);
 
