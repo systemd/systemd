d937ef74edc405c3d0a014724a94ddd9d3d25960
Lennart Poettering <lennart@poettering.net>
2017-12-08 17:19:27 +0100

    resolved: synchronize introduction blurbs in all three resolv.conf files we provide
    Let's use the same wording and same contents in all three versions.

--- src/resolve/resolved-resolv-conf.c
+++ src/resolve/resolved-resolv-conf.c
@@ -239,14 +239,18 @@ static void write_resolv_conf_search(
 static int write_uplink_resolv_conf_contents(FILE *f, OrderedSet *dns, OrderedSet *domains) {
         Iterator i;
 
-        fputs_unlocked("# This file is managed by man:systemd-resolved(8). Do not edit.\n#\n"
+        fputs_unlocked("# This file is managed by man:systemd-resolved(8). Do not edit.\n"
+                       "#\n"
                        "# This is a dynamic resolv.conf file for connecting local clients directly to\n"
-                       "# all known DNS servers.\n#\n"
+                       "# all known uplink DNS servers. This file lists all configured search domains.\n"
+                       "#\n"
                        "# Third party programs must not access this file directly, but only through the\n"
                        "# symlink at /etc/resolv.conf. To manage man:resolv.conf(5) in a different way,\n"
-                       "# replace this symlink by a static file or a different symlink.\n#\n"
+                       "# replace this symlink by a static file or a different symlink.\n"
+                       "#\n"
                        "# See man:systemd-resolved.service(8) for details about the supported modes of\n"
-                       "# operation for /etc/resolv.conf.\n\n", f);
+                       "# operation for /etc/resolv.conf.\n"
+                       "\n", f);
 
         if (ordered_set_isempty(dns))
                 fputs_unlocked("# No DNS servers known.\n", f);
@@ -265,10 +269,23 @@ static int write_uplink_resolv_conf_contents(FILE *f, OrderedSet *dns, OrderedSe
 }
 
 static int write_stub_resolv_conf_contents(FILE *f, OrderedSet *dns, OrderedSet *domains) {
-        fputs("# This file is managed by man:systemd-resolved(8). Do not edit.\n#\n"
-              "# 127.0.0.53 is the systemd-resolved stub resolver.\n"
-              "# run \"systemd-resolve --status\" to see details about the actual nameservers.\n\n"
-              "nameserver 127.0.0.53\n", f);
+        fputs_unlocked("# This file is managed by man:systemd-resolved(8). Do not edit.\n"
+                       "#\n"
+                       "# This is a dynamic resolv.conf file for connecting local clients to the\n"
+                       "# internal DNS stub resolver of systemd-resolved. This file lists all\n"
+                       "# configured search domains.\n"
+                       "#\n"
+                       "# Run \"systemd-resolve --status\" to see details about the uplink DNS servers\n"
+                       "# currently in use.\n"
+                       "#\n"
+                       "# Third party programs must not access this file directly, but only through the\n"
+                       "# symlink at /etc/resolv.conf. To manage man:resolv.conf(5) in a different way,\n"
+                       "# replace this symlink by a static file or a different symlink.\n"
+                       "#\n"
+                       "# See man:systemd-resolved.service(8) for details about the supported modes of\n"
+                       "# operation for /etc/resolv.conf.\n"
+                       "\n"
+                       "nameserver 127.0.0.53\n", f);
 
         if (!ordered_set_isempty(domains))
                 write_resolv_conf_search(domains, f);
