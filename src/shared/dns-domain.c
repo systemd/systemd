59f2725cc816e1c8d3bbea30a499990ade9e8653
Lennart Poettering <lennart@poettering.net>
2017-12-14 06:08:21 +0100

    resolved: fix "in-between" logic when boundaries are equal (#7590)
    This changes dns_name_between() to deal properly with checking whether B
    is between A and C if A and C are equal. Previously we simply returned
    -EINVAL in this case, refusing checking. With this change we correct
    behaviour: if A and C are equal, then B is "between" both if it is
    different from them. That's logical, since we do < and > comparisons, not
    <= and >=, and that means that anything "right of A" and "left of C"
    lies in between with wrap-around at the ends. And if A and C are equal
    that means everything lies between, except for A itself.
    
    This fixes handling of domains using NSEC3 "white lies", for example the
    .it TLD.
    
    Fixes: #7421

--- src/shared/dns-domain.c
+++ src/shared/dns-domain.c
@@ -693,23 +693,26 @@ int dns_name_change_suffix(const char *name, const char *old_suffix, const char 
 }
 
 int dns_name_between(const char *a, const char *b, const char *c) {
-        int n;
-
         /* Determine if b is strictly greater than a and strictly smaller than c.
            We consider the order of names to be circular, so that if a is
            strictly greater than c, we consider b to be between them if it is
            either greater than a or smaller than c. This is how the canonical
            DNS name order used in NSEC records work. */
 
-        n = dns_name_compare_func(a, c);
-        if (n == 0)
-                return -EINVAL;
-        else if (n < 0)
-                /*       a<---b--->c       */
+        if (dns_name_compare_func(a, c) < 0)
+                /*
+                   a and c are properly ordered:
+                   a<---b--->c
+                */
                 return dns_name_compare_func(a, b) < 0 &&
                        dns_name_compare_func(b, c) < 0;
         else
-                /* <--b--c         a--b--> */
+                /*
+                   a and c are equal or 'reversed':
+                   <--b--c         a----->
+                   or:
+                   <-----c         a--b-->
+                */
                 return dns_name_compare_func(b, c) < 0 ||
                        dns_name_compare_func(a, b) < 0;
 }
