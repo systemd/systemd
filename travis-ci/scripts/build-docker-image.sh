bd56e9938bef181eb43638b78f5cd90bbf774677
Marek Cermak <macermak@redhat.com>
2018-01-10 23:58:52 +0100

    Structural and implementation changes based on PR review
    - remove travis-ci/patches dir
    - coverity.sh: separated build and upload
    - move travis-ci/scripts/coverity.sh into scripts/ upstream folder
    - minor changes
    
    new file:   scripts/coverity.sh
    modified:   .travis.yml
    modified:   travis-ci/Dockerfile
    modified:   travis-ci/scripts/build-docker-image.sh
    deleted:    travis-ci/patches/cov-templates-gnu-float128.patch
    deleted:    travis-ci/patches/systemd-macro-void.patch
    deleted:    travis-ci/scripts/autopatch-coverity.sh
    deleted:    travis-ci/scripts/autopatch-systemd.sh
    deleted:    travis-ci/scripts/coverity.sh
    
    Signed-off-by: Marek Cermak <macermak@redhat.com>

--- travis-ci/scripts/build-docker-image.sh
+++ travis-ci/scripts/build-docker-image.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# Build docker image
-
 # Check environment
 [ -z "$DOCKER_REPOSITORY" ] && echo "ERROR: DOCKER_REPOSITORY must be set" && exit 1
 [ -z "$TRAVIS_COMMIT" ] && echo "ERROR: TRAVIS_COMMIT must be set" && exit 1
+
+# Build docker image
+echo -e "\n\033[33;1mBuilding docker image: $DOCKER_REPOSITORY:$TRAVIS_COMMIT.\033[0m"
 
 docker build \
 --build-arg DOCKER_USER=$USER \
