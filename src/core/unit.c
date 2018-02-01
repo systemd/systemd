5607f3aacfbfb4644ca051a586ad4cd6cd2d5e72
Andrei Gherzan <andrei@resin.io>
2018-02-01 15:54:41 +0000

    core: Avoid empty directory warning when we are bind-mounting a file

--- src/core/unit.c
+++ src/core/unit.c
@@ -4726,7 +4726,7 @@ void unit_warn_if_dir_nonempty(Unit *u, const char* where) {
         assert(where);
 
         r = dir_is_empty(where);
-        if (r > 0)
+        if (r > 0 || r == -ENOTDIR)
                 return;
         if (r < 0) {
                 log_unit_warning_errno(u, r, "Failed to check directory %s: %m", where);
