438428465535f5c86ceef850310a5f6a3b0e15f6
Lennart Poettering <lennart@poettering.net>
2018-02-26 11:50:12 +0100

    coccinelle: drop empty-if.cocci script
    It doesn't work, spits out only rubbish and was already excluded of
    run-coccinelle.sh. It's a pitty it doesn't work, but let's drop this
    dead piece of code for now.

--- coccinelle/run-coccinelle.sh
+++ coccinelle/run-coccinelle.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -e
 
 for SCRIPT in ${@-*.cocci} ; do
-        [ "$SCRIPT" = "empty-if.cocci" ] && continue
         echo "--x-- Processing $SCRIPT --x--"
         TMPFILE=`mktemp`
         spatch --sp-file $SCRIPT --dir $(pwd)/.. 2> "$TMPFILE" || cat "$TMPFILE"
