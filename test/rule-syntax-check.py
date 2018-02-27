c9715ffce347310a5196d1375810d41fbdd01fd6
Filipe Brandenburger <filbranden@google.com>
2018-02-27 13:11:07 -0800

    rule-syntax-check: allow commas inside quoted strings
    Using a regex to match the groups is smarter than the split(',') that
    would break in those cases.
    
    Tested:
    
      SUBSYSTEM=="usb", ENV{ID_USB_INTERFACES}=="*:060101:*,*:070202:*", TAG+="uaccess"
    
    Rule checker doesn't break there after this commit.

--- test/rule-syntax-check.py
+++ test/rule-syntax-check.py
@@ -33,6 +33,8 @@ no_args_tests = re.compile(r'(ACTION|DEVPATH|KERNELS?|NAME|SYMLINK|SUBSYSTEMS?|D
 args_tests = re.compile(r'(ATTRS?|ENV|TEST){([a-zA-Z0-9/_.*%-]+)}\s*(?:=|!)=\s*' + quoted_string_re + '$')
 no_args_assign = re.compile(r'(NAME|SYMLINK|OWNER|GROUP|MODE|TAG|RUN|LABEL|GOTO|OPTIONS|IMPORT)\s*(?:\+=|:=|=)\s*' + quoted_string_re + '$')
 args_assign = re.compile(r'(ATTR|ENV|IMPORT|RUN){([a-zA-Z0-9/_.*%-]+)}\s*(=|\+=)\s*' + quoted_string_re + '$')
+# Find comma-separated groups, but allow commas that are inside quoted strings.
+comma_separated_group_re = re.compile(r'(?:[^,"]|' + quoted_string_re + ')+')
 
 result = 0
 buffer = ''
@@ -57,8 +59,8 @@ for path in rules_files:
 
         # Separator ',' is normally optional but we make it mandatory here as
         # it generally improves the readability of the rules.
-        for clause in line.split(','):
-            clause = clause.strip()
+        for clause_match in comma_separated_group_re.finditer(line):
+            clause = clause_match.group().strip()
             if not (no_args_tests.match(clause) or args_tests.match(clause) or
                     no_args_assign.match(clause) or args_assign.match(clause)):
 
