a3ddf73c0e4abf8e3c1b8fd91eac469220a5b44b
Lennart Poettering <lennart@poettering.net>
2018-03-02 11:55:16 +0100

    logind: voidify a function we never check the return value of

--- src/login/logind-session-device.c
+++ src/login/logind-session-device.c
@@ -106,7 +106,7 @@ static int session_device_notify(SessionDevice *sd, enum SessionDeviceNotificati
         return sd_bus_send(sd->session->manager->bus, m, NULL);
 }
 
-static int sd_eviocrevoke(int fd) {
+static void sd_eviocrevoke(int fd) {
         static bool warned = false;
 
         assert(fd >= 0);
@@ -118,8 +118,6 @@ static int sd_eviocrevoke(int fd) {
                         warned = true;
                 }
         }
-
-        return 0;
 }
 
 static int sd_drmsetmaster(int fd) {
