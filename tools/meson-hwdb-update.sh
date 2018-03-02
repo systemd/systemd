4af1aea4116f2d1936017b75ecd2aaa11dfebbae
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2018-03-02 11:50:43 +0100

    tools/hwdb-update: print what is being executed

--- tools/meson-hwdb-update.sh
+++ tools/meson-hwdb-update.sh
@@ -3,7 +3,8 @@ set -eu
 
 cd "$1"
 
-if [ "${2:-}" != "-n" ]; then
+if [ "${2:-}" != "-n" ]; then (
+        set -x
         curl -L -o usb.ids 'http://www.linux-usb.org/usb.ids'
         curl -L -o pci.ids 'http://pci-ids.ucw.cz/v2.2/pci.ids'
         curl -L -o ma-large.txt 'http://standards-oui.ieee.org/oui/oui.txt'
@@ -11,8 +12,9 @@ if [ "${2:-}" != "-n" ]; then
         curl -L -o ma-small.txt 'http://standards-oui.ieee.org/oui36/oui36.txt'
         curl -L -o pnp_id_registry.html 'http://www.uefi.org/uefi-pnp-export'
         curl -L -o acpi_id_registry.html 'http://www.uefi.org/uefi-acpi-export'
-fi
+) fi
 
+set -x
 ./acpi-update.py >20-acpi-vendor.hwdb.base
 patch -p0 -o- 20-acpi-vendor.hwdb.base <20-acpi-vendor.hwdb.patch >20-acpi-vendor.hwdb
 ! diff -u 20-acpi-vendor.hwdb.base 20-acpi-vendor.hwdb >20-acpi-vendor.hwdb.patch
