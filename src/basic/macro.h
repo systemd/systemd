989290dbf192b24609fa36379241f2f1edf19d8a
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2018-03-25 22:26:13 +0200

    fuzz-unit-file: add __has_feature(memory_sanitizer) when skipping ListenNetlink=
    https://clang.llvm.org/docs/MemorySanitizer.html#id5 documents this
    check as the way to detect MemorySanitizer at compilation time. We
    only need to skip the test if MemorySanitizer is used.
    
    Also, use this condition in cg_slice_to_path(). There, the code that is
    conditionalized is not harmful in any way (it's just unnecessary), so remove
    the FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION condition.
    
    Fixes #8482.

--- src/basic/macro.h
+++ src/basic/macro.h
@@ -63,6 +63,17 @@
 #endif
 #endif
 
+#if !defined(HAS_FEATURE_MEMORY_SANITIZER)
+#  if defined(__has_feature)
+#    if __has_feature(memory_sanitizer)
+#      define HAS_FEATURE_MEMORY_SANITIZER 1
+#    endif
+#  endif
+#  if !defined(HAS_FEATURE_MEMORY_SANITIZER)
+#    define HAS_FEATURE_MEMORY_SANITIZER 0
+#  endif
+#endif
+
 /* Temporarily disable some warnings */
 #define DISABLE_WARNING_DECLARATION_AFTER_STATEMENT                     \
         _Pragma("GCC diagnostic push");                                 \
