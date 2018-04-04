63554ed9073fd7e2cb95204c39e14ff7d9141d9b
Filipe Brandenburger <filbranden@google.com>
2018-04-04 00:32:59 -0700

    oss-fuzz: Fallback to `ninja-build` when available (#8641)
    The ninja binary is deployed as `ninja-build` in older distros such as
    RHEL 7/CentOS 7.  Detect that and use `ninja-build` instead of `ninja`
    when it's available.

--- tools/meson-build.sh
+++ tools/meson-build.sh
@@ -7,4 +7,12 @@ target="$3"
 options="$4"
 
 [ -d "$dst" ] || meson "$src" "$dst" $options
-ninja -C "$dst" "$target"
+
+# Locate ninja binary, on CentOS 7 it is called ninja-build, so
+# use that name if available.
+ninja=ninja
+if which ninja-build >/dev/null 2>&1 ; then
+    ninja=ninja-build
+fi
+
+"$ninja" -C "$dst" "$target"
