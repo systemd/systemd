11a15892230011c331d4e31cdbbd58e8b3dfe4af
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2018-04-06 18:58:55 +0200

    tree-wide: drop license boilerplate
    Files which are installed as-is (any .service and other unit files, .conf
    files, .policy files, etc), are left as is. My assumption is that SPDX
    identifiers are not yet that well known, so it's better to retain the
    extended header to avoid any doubt.
    
    I also kept any copyright lines. We can probably remove them, but it'd nice to
    obtain explicit acks from all involved authors before doing that.

--- src/libsystemd-network/dhcp6-lease-internal.h
+++ src/libsystemd-network/dhcp6-lease-internal.h
@@ -6,19 +6,6 @@
 
   Copyright (C) 2014 Tom Gundersen
   Copyright (C) 2014-2015 Intel Corporation. All rights reserved.
-
-  systemd is free software; you can redistribute it and/or modify it
-  under the terms of the GNU Lesser General Public License as published by
-  the Free Software Foundation; either version 2.1 of the License, or
-  (at your option) any later version.
-
-  systemd is distributed in the hope that it will be useful, but
-  WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public License
-  along with systemd; If not, see <http://www.gnu.org/licenses/>.
 ***/
 
 #include <stdint.h>
