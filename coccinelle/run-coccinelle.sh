00bfe67f6b178056bffcfa9fbfb04ca3fa809989
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2018-04-20 15:21:06 +0200

    coccinelle: add option to make changes in place

--- coccinelle/run-coccinelle.sh
+++ coccinelle/run-coccinelle.sh
@@ -1,12 +1,20 @@
 #!/bin/bash -e
 
 files="$(git ls-files ':/*.[ch]')"
+args=
+
+case "$1" in
+        -i)
+                args="$args --in-place"
+                shift
+                ;;
+esac
 
 for SCRIPT in ${@-coccinelle/*.cocci} ; do
         echo "--x-- Processing $SCRIPT --x--"
         TMPFILE=`mktemp`
-        echo "+ spatch --sp-file $SCRIPT ..."
-        spatch --sp-file $SCRIPT $files 2>"$TMPFILE" || cat "$TMPFILE"
+        echo "+ spatch --sp-file $SCRIPT $args ..."
+        spatch --sp-file $SCRIPT $args $files 2>"$TMPFILE" || cat "$TMPFILE"
         rm "$TMPFILE"
         echo -e "--x-- Processed $SCRIPT --x--\n"
 done
