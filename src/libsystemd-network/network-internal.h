ce088d6fb4a6363106e4b94ad749d3bce61aefc7
Yu Watanabe <watanabe.yu+github@gmail.com>
2018-05-11 14:44:03 +0900

    sd-dhcp: drop deserialize_dhcp_option() as it is equivalent to unhexmem()

--- src/libsystemd-network/network-internal.h
+++ src/libsystemd-network/network-internal.h
@@ -78,5 +78,5 @@ struct sd_dhcp_route;
 void serialize_dhcp_routes(FILE *f, const char *key, sd_dhcp_route **routes, size_t size);
 int deserialize_dhcp_routes(struct sd_dhcp_route **ret, size_t *ret_size, size_t *ret_allocated, const char *string);
 
+/* It is not necessary to add deserialize_dhcp_option(). Use unhexmem() instead. */
 int serialize_dhcp_option(FILE *f, const char *key, const void *data, size_t size);
-int deserialize_dhcp_option(void **data, size_t *data_len, const char *string);
