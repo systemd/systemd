d18cb3937bdc2efede153abce1cd8ddae941d762
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2018-05-13 22:28:24 +0200

    Turn VALGRIND variable into a meson configuration switch
    Configuration through environment variable is inconvenient with meson, because
    they cannot be convieniently changed and/or are not preserved during
    reconfiguration (https://github.com/mesonbuild/meson/issues/1503).
    This adds -Dvalgrind=true/false, which has the advantage that it can be set
    at any time with meson configure -Dvalgrind=... and ninja will rebuild targets
    as necessary. Additional minor advantages are better consistency with the
    options for hashmap debugging, and typo avoidance with '#if' instead of '#ifdef'.

--- src/basic/mempool.c
+++ src/basic/mempool.c
@@ -76,7 +76,7 @@ void mempool_free_tile(struct mempool *mp, void *p) {
         mp->freelist = p;
 }
 
-#ifdef VALGRIND
+#if VALGRIND
 
 void mempool_drop(struct mempool *mp) {
         struct pool *p = mp->first_pool;
