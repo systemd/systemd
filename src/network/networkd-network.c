e6ebebbe6aef5830793af515c2e3a998bde920b9
Susant Sahani <susant@redhat.com>
2018-05-29 20:28:11 +0530

    networkd: Add ability to set MULTICAST flag on interface
    Closes #9113
    
    fix ARP toggling flag

--- src/network/networkd-network.c
+++ src/network/networkd-network.c
@@ -247,6 +247,7 @@ static int network_load_one(Manager *manager, const char *filename) {
         network->duid.type = _DUID_TYPE_INVALID;
         network->proxy_arp = -1;
         network->arp = -1;
+        network->multicast = -1;
         network->ipv6_accept_ra_use_dns = true;
         network->ipv6_accept_ra_route_table = RT_TABLE_MAIN;
         network->ipv6_mtu = 0;
