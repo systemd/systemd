958acea18b8316c1ba9ec752b83f831c1a95c85f
Marc Kleine-Budde <mkl@pengutronix.de>
2018-06-05 15:15:33 +0200

    parse-util: add permille parser + tests

--- src/basic/parse-util.h
+++ src/basic/parse-util.h
@@ -115,6 +115,9 @@ int parse_fractional_part_u(const char **s, size_t digits, unsigned *res);
 int parse_percent_unbounded(const char *p);
 int parse_percent(const char *p);
 
+int parse_permille_unbounded(const char *p);
+int parse_permille(const char *p);
+
 int parse_nice(const char *p, int *ret);
 
 int parse_ip_port(const char *s, uint16_t *ret);
