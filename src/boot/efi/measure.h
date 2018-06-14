50d1760d2690db8636d8800b5777b03334f6afba
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2018-06-14 12:57:09 +0200

    Drop more license boilerplate
    $ git grep -e 'This program is free software' -l |grep -v LICENSE | \
      xargs perl -i -0pe 's/ \* This program.*?for more details.\s*\*\n( \* You should have.*licenses.>.\n)?//gms'
    
    For some reason they were missed previously. All those files seem to
    have proper SDPX tags.

--- src/boot/efi/measure.h
+++ src/boot/efi/measure.h
@@ -1,15 +1,5 @@
 /* SPDX-License-Identifier: LGPL-2.1+ */
 /*
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 2.1 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
  */
 #ifndef __SDBOOT_MEASURE_H
 #define __SDBOOT_MEASURE_H
