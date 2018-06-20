9e0621b0f3d899d79ff788e758155a0827bf7db8
Lennart Poettering <lennart@poettering.net>
2018-06-20 10:59:44 +0200

    make dir-locals work again with emacs 26.1
    After upgrading to emacs-26.1-1.fc28.x86_64 I noticed that our
    .dir-locals.el files weren't honoured anymore (specifically the fill
    column variable is not correctly set for c-mode files). I finally
    tracked this down to the order in which items are listed in
    .dir-locals.el: if the "nil" one is listed last everything works,
    otherwise, it's the one that is applied instead of the c-mode one.
    
    This patch simply swaps the entries, and puts the "nil" one last. My
    emacs lisp fu is a bit too limited to understand the full impact for
    this, and why emacs 26.1 changed behaviour in this regard, but from an
    outsider's view the order shouldn't negatively affect things otherwise,
    hence this patch.

--- .dir-locals.el
+++ .dir-locals.el
@@ -9,10 +9,7 @@
 ; NOTE: If you update this file make sure to update .vimrc and .editorconfig,
 ; too.
 
-((nil . ((indent-tabs-mode . nil)
-         (tab-width . 8)
-         (fill-column . 79)))
- (c-mode . ((fill-column . 119)
+((c-mode . ((fill-column . 119)
             (c-basic-offset . 8)
             (eval . (c-set-offset 'substatement-open 0))
             (eval . (c-set-offset 'statement-case-open 0))
@@ -24,4 +21,7 @@
  (meson-mode . ((meson-indent-basic . 8)))
  (sh-mode . ((sh-basic-offset . 8)
              (sh-indentation . 8)))
- (awk-mode . ((c-basic-offset . 8))))
+ (awk-mode . ((c-basic-offset . 8)))
+ (nil . ((indent-tabs-mode . nil)
+         (tab-width . 8)
+         (fill-column . 79))) )
