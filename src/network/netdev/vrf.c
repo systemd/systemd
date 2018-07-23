b215c2e7e289b52a9f9d6672ddd0997b3c204d9c
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2018-07-23 14:40:53 +0200

    Drop some more copyright headers
    Acks in https://github.com/systemd/systemd/issues/9320.

--- src/network/netdev/vrf.c
+++ src/network/netdev/vrf.c
@@ -1,7 +1,4 @@
 /* SPDX-License-Identifier: LGPL-2.1+ */
-/***
-  Copyright © 2016 Andreas Rammhold <andreas@rammhold.de>
-***/
 
 #include <net/if.h>
 
--- src/network/netdev/vrf.h
+++ src/network/netdev/vrf.h
@@ -1,10 +1,6 @@
 /* SPDX-License-Identifier: LGPL-2.1+ */
 #pragma once
 
-/***
-  Copyright © 2016 Andreas Rammhold <andreas@rammhold.de>
-***/
-
 typedef struct Vrf Vrf;
 
 #include "netdev/netdev.h"
