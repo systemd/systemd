systemd/systemd

commit db69869f264af2d1afcdd3e573e0e9fdd5bef065

sleep-config: fix useless check for swapfile type
Since 0c6f1f4ea49 the check was useless, because the kernel will
ever only write "partition" or "file" there.

OTOH, it is possible that "\\040(deleted)" (escaped " (deleted)")
will be added for removed files. This should not happen, so add
a warning to detect those cases.

src/shared/sleep-config.c
@@ -183,7 +183,7 @@ static int hibernation_partition_size(size_t *size, size_t *used) {
         (void) fscanf(f, "%*s %*s %*s %*s %*s\n");
 
         for (i = 1;; i++) {
-                _cleanup_free_ char *dev = NULL, *d = NULL, *type = NULL;
+                _cleanup_free_ char *dev = NULL, *type = NULL;
                 size_t size_field, used_field;
                 int k;
 
@@ -202,12 +202,8 @@ static int hibernation_partition_size(size_t *size, size_t *used) {
                         continue;
                 }
 
-                d = cunescape(dev);
-                if (!d)
-                        return -ENOMEM;
-
-                if (!streq(type, "partition") && !streq(type, "file")) {
-                        log_debug("Partition %s has type %s, ignoring.", d, type);
+                if (streq(type, "partition") && endswith(dev, "\\040(deleted)")) {
+                        log_warning("Ignoring deleted swapfile '%s'.", dev);
                         continue;
                 }
 

commit 3dea6886c20a32d832971e589cbc1807c549b66f

sleep-config: partitions can't be deleted, only files can
This fixes something I think was basically a typo introduced in
db69869f264af2d1afcdd3e573e0e9fdd5bef065.

src/shared/sleep-config.c
@@ -194,27 +194,30 @@ int find_hibernate_location(char **device, char **type, size_t *size, size_t *us
                            "%zu "   /* used */
                            "%*i\n", /* priority */
                            &dev_field, &type_field, &size_field, &used_field);
+                if (k == EOF)
+                        break;
                 if (k != 4) {
-                        if (k == EOF)
-                                break;
-
                         log_warning("Failed to parse /proc/swaps:%u", i);
                         continue;
                 }
 
-                if (streq(type_field, "partition")) {
+                if (streq(type_field, "file")) {
+
                         if (endswith(dev_field, "\\040(deleted)")) {
-                                log_warning("Ignoring deleted swapfile '%s'.", dev_field);
+                                log_warning("Ignoring deleted swap file '%s'.", dev_field);
                                 continue;
                         }
 
+                } else if (streq(type_field, "partition")) {
                         const char *fn;
+
                         fn = path_startswith(dev_field, "/dev/");
                         if (fn && startswith(fn, "zram")) {
-                                log_debug("Ignoring compressed ram swap device '%s'.", dev_field);
+                                log_debug("Ignoring compressed RAM swap device '%s'.", dev_field);
                                 continue;
                         }
                 }
+
                 if (device)
                         *device = TAKE_PTR(dev_field);
                 if (type)