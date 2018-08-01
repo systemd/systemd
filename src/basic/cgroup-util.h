b924c79720cc2bf2edf75fa3ff43bb4954fccf1f
rpm-build <rpm-build>
2018-08-01 13:19:39 +0200

    pid1: bump DefaultTasksMax to 80% of the kernel pid.max value
    This should be hopefully high enough even for the very big deployments.
    
    Resolves: #1523236

--- src/basic/cgroup-util.h
+++ src/basic/cgroup-util.h
@@ -100,7 +100,7 @@ static inline bool CGROUP_BLKIO_WEIGHT_IS_OK(uint64_t x) {
 }
 
 /* Default resource limits */
-#define DEFAULT_TASKS_MAX_PERCENTAGE            15U /* 15% of PIDs, 4915 on default settings */
+#define DEFAULT_TASKS_MAX_PERCENTAGE            80U /* 80% of PIDs, 26214 on default settings */
 #define DEFAULT_USER_TASKS_MAX_PERCENTAGE       33U /* 33% of PIDs, 10813 on default settings */
 
 typedef enum CGroupUnified {
