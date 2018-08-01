c7f77dfd2bfa593bfbbdf82eea8b600ca1b46f4c
rpm-build <rpm-build>
2018-08-01 17:17:07 +0200

    pid1: bump maximum number of process in user slice to 80% of pid_max
    Related: #1523236

--- src/basic/cgroup-util.h
+++ src/basic/cgroup-util.h
@@ -101,7 +101,7 @@ static inline bool CGROUP_BLKIO_WEIGHT_IS_OK(uint64_t x) {
 
 /* Default resource limits */
 #define DEFAULT_TASKS_MAX_PERCENTAGE            80U /* 80% of PIDs, 26214 on default settings */
-#define DEFAULT_USER_TASKS_MAX_PERCENTAGE       33U /* 33% of PIDs, 10813 on default settings */
+#define DEFAULT_USER_TASKS_MAX_PERCENTAGE       80U /* 80% of PIDs, 26214 on default settings */
 
 typedef enum CGroupUnified {
         CGROUP_UNIFIED_UNKNOWN = -1,
