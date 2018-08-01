0b3833d6c3b751c6dfb40eeb2ef852984c58f546
rpm-build <rpm-build>
2018-08-01 10:58:28 +0200

    logind: set RemoveIPC to false by default
    Resolves: #1523233

--- src/login/logind-core.c
+++ src/login/logind-core.c
@@ -25,7 +25,7 @@ void manager_reset_config(Manager *m) {
 
         m->n_autovts = 6;
         m->reserve_vt = 6;
-        m->remove_ipc = true;
+        m->remove_ipc = false;
         m->inhibit_delay_max = 5 * USEC_PER_SEC;
         m->handle_power_key = HANDLE_POWEROFF;
         m->handle_suspend_key = HANDLE_SUSPEND;
