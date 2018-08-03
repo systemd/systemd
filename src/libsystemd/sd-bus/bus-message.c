8792bdf8a3311f9e629daa0ec592c97c1cfb2a7c
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2018-08-03 16:36:51 +0200

    bus-message: drop asserts in functions which are wrappers for varargs version
    The function does no processing on it's own, and just forwards arguments
    to the other function. Let's just use the asserts there.

--- src/libsystemd/sd-bus/bus-message.c
+++ src/libsystemd/sd-bus/bus-message.c
@@ -2444,11 +2444,6 @@ _public_ int sd_bus_message_append(sd_bus_message *m, const char *types, ...) {
         va_list ap;
         int r;
 
-        assert_return(m, -EINVAL);
-        assert_return(types, -EINVAL);
-        assert_return(!m->sealed, -EPERM);
-        assert_return(!m->poisoned, -ESTALE);
-
         va_start(ap, types);
         r = sd_bus_message_appendv(m, types, ap);
         va_end(ap);
@@ -4500,10 +4495,6 @@ _public_ int sd_bus_message_readv(
 _public_ int sd_bus_message_read(sd_bus_message *m, const char *types, ...) {
         va_list ap;
         int r;
-
-        assert_return(m, -EINVAL);
-        assert_return(m->sealed, -EPERM);
-        assert_return(types, -EINVAL);
 
         va_start(ap, types);
         r = sd_bus_message_readv(m, types, ap);
