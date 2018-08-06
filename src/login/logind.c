a05c1077911652954c8b9e82cfdc0fc643eca782
Lennart Poettering <lennart@poettering.net>
2018-08-06 21:44:45 +0200

    logind: rework how we manage the slice and user-runtime-dir@.service unit for each user
    Instead of managing it explicitly, let's simplify things and rely on
    regular Wants=/Requires= dependencies to pull in these units from
    user@.service and the session scope, and StopWhenUneeded= to stop these
    auxiliary units again. This way, they can be pulled in easily by
    unrelated units too.
    
    This simplifies things quite a bit: for each session we now only need to
    manage the session scope, and for each user the user@.service, the other
    units are not something we need to manage anymore.
    
    This patch also makes sure that if user@.service of a user is masked we
    will continue to work, and user-runtime-dir@.service will still be
    correctly pulled in, as it is now a dependency of the scope unit.
    
    Fixes: #9461
    Replaces: #5546
    (cherry picked from commit 25a1ab4ed48b72e974f77a68dcbe3521014787bb)
    
    Related: #1642460

--- src/login/logind.c
+++ src/login/logind.c
@@ -1158,7 +1158,7 @@ static int manager_startup(Manager *m) {
                 user_start(user);
 
         HASHMAP_FOREACH(session, m->sessions, i)
-                session_start(session, NULL);
+                (void) session_start(session, NULL, NULL);
 
         HASHMAP_FOREACH(inhibitor, m->inhibitors, i)
                 inhibitor_start(inhibitor);
