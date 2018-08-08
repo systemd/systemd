d355618518f26bd045df81a52dade79ac3079f3f
Yu Watanabe <watanabe.yu+github@gmail.com>
2018-08-08 15:06:36 +0900

    journal: fix syslog_parse_identifier()
    Fixes #9829.
    
    (cherry-picked from commit a6aadf4ae0bae185dc4c414d492a4a781c80ffe5)
    
    Resolves: #1664978

--- src/journal/journald-syslog.c
+++ src/journal/journald-syslog.c
@@ -194,7 +194,7 @@ size_t syslog_parse_identifier(const char **buf, char **identifier, char **pid) 
         e = l;
         l--;
 
-        if (p[l-1] == ']') {
+        if (l > 0 && p[l-1] == ']') {
                 size_t k = l-1;
 
                 for (;;) {
@@ -219,8 +219,8 @@ size_t syslog_parse_identifier(const char **buf, char **identifier, char **pid) 
         if (t)
                 *identifier = t;
 
-        if (strchr(WHITESPACE, p[e]))
-                e++;
+        e += strspn(p + e, WHITESPACE);
+
         *buf = p + e;
         return e;
 }
