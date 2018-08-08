a5ee33b951cfa22db53d0274c9c6c0d9d4dae39d
Yu Watanabe <watanabe.yu+github@gmail.com>
2018-08-08 18:27:15 +0900

    syslog: fix segfault in syslog_parse_priority()

--- src/journal/test-journal-syslog.c
+++ src/journal/test-journal-syslog.c
@@ -4,6 +4,7 @@
 #include "journald-syslog.h"
 #include "macro.h"
 #include "string-util.h"
+#include "syslog-util.h"
 
 static void test_syslog_parse_identifier(const char *str,
                                          const char *ident, const char *pid, int ret) {
@@ -18,6 +19,17 @@ static void test_syslog_parse_identifier(const char *str,
         assert_se(pid == pid2 || streq_ptr(pid, pid2));
 }
 
+static void test_syslog_parse_priority(const char *str, int priority, int ret) {
+        const char *buf = str;
+        int priority2, ret2;
+
+        ret2 = syslog_parse_priority(&buf, &priority2, false);
+
+        assert_se(ret == ret2);
+        if (ret2 == 1)
+                assert_se(priority == priority2);
+}
+
 int main(void) {
         test_syslog_parse_identifier("pidu[111]: xxx", "pidu", "111", 11);
         test_syslog_parse_identifier("pidu: xxx", "pidu", NULL, 6);
@@ -28,6 +40,14 @@ int main(void) {
         test_syslog_parse_identifier("pidu:", "pidu", NULL, 5);
         test_syslog_parse_identifier("pidu: ", "pidu", NULL, 6);
         test_syslog_parse_identifier("pidu : ", NULL, NULL, 0);
+
+        test_syslog_parse_priority("<>", 0, 0);
+        test_syslog_parse_priority("<>aaa", 0, 0);
+        test_syslog_parse_priority("<aaaa>", 0, 0);
+        test_syslog_parse_priority("<aaaa>aaa", 0, 0);
+        test_syslog_parse_priority(" <aaaa>", 0, 0);
+        test_syslog_parse_priority(" <aaaa>aaa", 0, 0);
+        /* TODO: add test cases of valid priorities */
 
         return 0;
 }
