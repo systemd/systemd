4b0c83674c45e84eff39d6b8d97bcb8c8d0003b1
Yu Watanabe <watanabe.yu+github@gmail.com>
2018-08-12 17:25:17 +0900

    resolve: do not request DS record when RR does not match the original question
    This effectively reverts 6993d26469ca8deee0eea2b806b3c415deaa2e25.
    
    Fixes #9771.

--- src/resolve/resolved-dns-transaction.c
+++ src/resolve/resolved-dns-transaction.c
@@ -2132,21 +2132,11 @@ int dns_transaction_request_dnssec_keys(DnsTransaction *t) {
                          * this RR matches our original question,
                          * however. */
 
-                        r = dns_resource_key_match_rr(t->key, rr, NULL);
+                        r = dns_transaction_is_primary_response(t, rr);
                         if (r < 0)
                                 return r;
-                        if (r == 0) {
-                                /* Hmm, so this SOA RR doesn't match our original question. In this case, maybe this is
-                                 * a negative reply, and we need the a SOA RR's TTL in order to cache a negative entry?
-                                 * If so, we need to validate it, too. */
-
-                                r = dns_answer_match_key(t->answer, t->key, NULL);
-                                if (r < 0)
-                                        return r;
-                                if (r > 0) /* positive reply, we won't need the SOA and hence don't need to validate
-                                            * it. */
-                                        continue;
-                        }
+                        if (r == 0)
+                                continue;
 
                         r = dnssec_has_rrsig(t->answer, rr->key);
                         if (r < 0)
