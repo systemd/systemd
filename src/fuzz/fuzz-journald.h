231dca5579cfba6175d19eee5347d693893fb5aa
Evgeny Vereshchagin <evvers@ya.ru>
2018-09-07 06:13:17 +0000

    tests: put fuzz_journald_processing_function in a .c file

--- src/fuzz/fuzz-journald.h
+++ src/fuzz/fuzz-journald.h
@@ -1,30 +1,10 @@
 /* SPDX-License-Identifier: LGPL-2.1+ */
 #pragma once
 
-#include "alloc-util.h"
 #include "journald-server.h"
-#include "sd-event.h"
 
-static void fuzz_journald_processing_function(
+void fuzz_journald_processing_function(
                 const uint8_t *data,
                 size_t size,
                 void (*f)(Server *s, const char *buf, size_t raw_len, const struct ucred *ucred, const struct timeval *tv, const char *label, size_t label_len)
-        ) {
-        Server s = {};
-        char *label = NULL;
-        size_t label_len = 0;
-        struct ucred *ucred = NULL;
-        struct timeval *tv = NULL;
-
-        if (size == 0)
-                return;
-
-        assert_se(sd_event_default(&s.event) >= 0);
-        s.syslog_fd = s.native_fd = s.stdout_fd = s.dev_kmsg_fd = s.audit_fd = s.hostname_fd = s.notify_fd = -1;
-        s.buffer = memdup_suffix0(data, size);
-        assert_se(s.buffer);
-        s.buffer_size = size + 1;
-        s.storage = STORAGE_NONE;
-        (*f)(&s, s.buffer, size, ucred, tv, label, label_len);
-        server_done(&s);
-}
+);
