72a4d9f96cbeded61571f3bfddd2010e2dfe8076
Yu Watanabe <watanabe.yu+github@gmail.com>
2018-09-15 01:49:29 +0900

    bus-util: use _printf_ attribute
    Follow-up for eda193578effbc3cee0f6d56ade52434778737c9.
    
    Fixes oss-fuzz#10350.

--- src/shared/bus-util.h
+++ src/shared/bus-util.h
@@ -65,7 +65,7 @@ int bus_connect_transport_systemd(BusTransport transport, const char *host, bool
 
 typedef int (*bus_message_print_t) (const char *name, const char *expected_value, sd_bus_message *m, bool value, bool all);
 
-int bus_print_property_value(const char *name, const char *expected_value, bool only_value, const char *fmt, ...);
+int bus_print_property_value(const char *name, const char *expected_value, bool only_value, const char *fmt, ...) _printf_(4,5);
 int bus_message_print_all_properties(sd_bus_message *m, bus_message_print_t func, char **filter, bool value, bool all, Set **found_properties);
 int bus_print_all_properties(sd_bus *bus, const char *dest, const char *path, bus_message_print_t func, char **filter, bool value, bool all, Set **found_properties);
 
