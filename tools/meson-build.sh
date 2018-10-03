f6d783ac3d145f3f3ffc2023d4ad90f76cf7fe34
Yu Watanabe <watanabe.yu+github@gmail.com>
2018-10-04 04:52:44 +0900

    meson: use same compilers to build fuzzers

--- tools/meson-build.sh
+++ tools/meson-build.sh
@@ -5,8 +5,10 @@ src="$1"
 dst="$2"
 target="$3"
 options="$4"
+CC="$5"
+CXX="$6"
 
-[ -f "$dst/ninja.build" ] || meson "$src" "$dst" $options
+[ -f "$dst/ninja.build" ] || CC="$CC" CXX="$CXX" meson "$src" "$dst" $options
 
 # Locate ninja binary, on CentOS 7 it is called ninja-build, so
 # use that name if available.
