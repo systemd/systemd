5fd93ab2c97029950b5f1ba33d7aad7ab62a2205
Sam Morris <sam@robots.org.uk>
2018-10-08 12:03:28 +0100

    resolved: have the stub resolver listen on both TCP and UDP by default
    RFC7766 section 4 states that in the absence of EDNS0, a response that
    is too large for a 512-byte UDP packet will have the 'truncated' bit
    set. The client is expected to retry the query over TCP.
    
    Fixes #10264.

--- src/resolve/resolved-manager.c
+++ src/resolve/resolved-manager.c
@@ -582,7 +582,7 @@ int manager_new(Manager **ret) {
                 .dnssec_mode = DEFAULT_DNSSEC_MODE,
                 .dns_over_tls_mode = DEFAULT_DNS_OVER_TLS_MODE,
                 .enable_cache = true,
-                .dns_stub_listener_mode = DNS_STUB_LISTENER_UDP,
+                .dns_stub_listener_mode = DNS_STUB_LISTENER_YES,
                 .read_resolv_conf = true,
                 .need_builtin_fallbacks = true,
                 .etc_hosts_last = USEC_INFINITY,
