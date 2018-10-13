ea5c5f680dac5993a6dbce1d40278d4589e03497
Lennart Poettering <lennart@poettering.net>
2018-10-13 15:12:41 +0200

    core: ensure it's not fatal if we cannot watch /etc/localtime
    See: #9602

--- src/core/manager.c
+++ src/core/manager.c
@@ -839,9 +839,7 @@ int manager_new(UnitFileScope scope, ManagerTestRunFlags test_run_flags, Manager
                 if (r < 0)
                         return r;
 
-                r = manager_setup_timezone_change(m);
-                if (r < 0)
-                        return r;
+                (void) manager_setup_timezone_change(m);
 
                 r = manager_setup_sigchld_event_source(m);
                 if (r < 0)
