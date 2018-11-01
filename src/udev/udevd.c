faae64fa3dc22738b3af669579a33055b9d71432
Yu Watanabe <watanabe.yu+github@gmail.com>
2018-11-01 16:54:10 +0900

    udevd: increase default number of workers
    The uevent handling in udevd is not cpu hungry.
    So, let's increase the default number of workers per cpu.
    This decrease the number of queued uevents.
    
    Without this commit (children_max is 16 on my laptop)
    ```
    $ journalctl -b -u systemd-udevd.service | grep reached | wc -l
    1544
    ```
    With this commit (children_max is 30 on my laptop)
    ```
    $ journalctl -b -u systemd-udevd.service | grep reached | wc -l
    7
    ```

--- src/udev/udevd.c
+++ src/udev/udevd.c
@@ -1786,7 +1786,7 @@ int main(int argc, char *argv[]) {
                 arg_children_max = 8;
 
                 if (sched_getaffinity(0, sizeof(cpu_set), &cpu_set) == 0)
-                        arg_children_max += CPU_COUNT(&cpu_set) * 2;
+                        arg_children_max += CPU_COUNT(&cpu_set) * 8;
 
                 mem_limit = physical_memory() / (128LU*1024*1024);
                 arg_children_max = MAX(10U, MIN(arg_children_max, mem_limit));
