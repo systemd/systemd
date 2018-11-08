28c3428df0b7790ab8f55524bd1b60113a620e4e
Yu Watanabe <watanabe.yu+github@gmail.com>
2018-11-08 12:30:00 +0900

    network: adds missing strv_free()
    Fixes oss-fuzz#11285.

--- src/network/networkd-network.c
+++ src/network/networkd-network.c
@@ -369,6 +369,7 @@ void network_free(Network *network) {
         strv_free(network->search_domains);
         strv_free(network->route_domains);
         strv_free(network->bind_carrier);
+        strv_free(network->router_search_domains);
 
         netdev_unref(network->bridge);
         netdev_unref(network->bond);
