e7e70f575840cd021f6429f264911ae0cbff9741
Evgeny Vereshchagin <evvers@ya.ru>
2018-11-15 17:52:57 +0100

    tests: add a fuzzer for dev_kmsg_record
    (cherry picked from commit 8857fb9beb9dcb95a6ce1be14dc94c4dc4cd3ea3)
    
    Resolves: #1764560

--- src/journal/journald-kmsg.h
+++ src/journal/journald-kmsg.h
@@ -9,3 +9,5 @@ int server_flush_dev_kmsg(Server *s);
 void server_forward_kmsg(Server *s, int priority, const char *identifier, const char *message, const struct ucred *ucred);
 
 int server_open_kernel_seqnum(Server *s);
+
+void dev_kmsg_record(Server *s, char *p, size_t l);
