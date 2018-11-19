2fbbbf9a5fbcc71c91a93642f227864568d3a699
Lennart Poettering <lennart@poettering.net>
2018-11-19 12:22:56 +0100

    manager: log on two OOM occasions

--- src/core/manager.c
+++ src/core/manager.c
@@ -623,7 +623,7 @@ static int manager_default_environment(Manager *m) {
                 m->transient_environment = strv_copy(environ);
 
         if (!m->transient_environment)
-                return -ENOMEM;
+                return log_oom();
 
         sanitize_environment(m->transient_environment);
 
@@ -3875,7 +3875,7 @@ int manager_transient_environment_add(Manager *m, char **plus) {
 
         a = strv_env_merge(2, m->transient_environment, plus);
         if (!a)
-                return -ENOMEM;
+                return log_oom();
 
         sanitize_environment(a);
 
