ec5e5948315c856077861b06162ffbc4a83c5b79
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2018-11-20 13:54:12 +0100

    basic/main-func: unify the two macros
    No functional change.

--- src/basic/main-func.h
+++ src/basic/main-func.h
@@ -5,23 +5,21 @@
 
 #include "static-destruct.h"
 
-/* Negative return values from impl are mapped to EXIT_FAILURE, and
- * everything else means success! */
-#define DEFINE_MAIN_FUNCTION(impl)                                      \
+#define _DEFINE_MAIN_FUNCTION(impl, ret)                                \
         int main(int argc, char *argv[]) {                              \
                 int r;                                                  \
                 r = impl(argc, argv);                                   \
                 static_destruct();                                      \
-                return r < 0 ? EXIT_FAILURE : EXIT_SUCCESS;             \
+                return ret;                                             \
         }
+
+/* Negative return values from impl are mapped to EXIT_FAILURE, and
+ * everything else means success! */
+#define DEFINE_MAIN_FUNCTION(impl)                                      \
+        _DEFINE_MAIN_FUNCTION(impl, r < 0 ? EXIT_FAILURE : EXIT_SUCCESS)
 
 /* Zero is mapped to EXIT_SUCCESS, and both negative and positive values
  * are mapped to EXIT_FAILURE.
  * Note: this means "true" maps to EXIT_FAILURE. */
 #define DEFINE_MAIN_FUNCTION_WITH_POSITIVE_FAILURE(impl)                \
-        int main(int argc, char *argv[]) {                              \
-                int r;                                                  \
-                r = impl(argc, argv);                                   \
-                static_destruct();                                      \
-                return r != 0 ? EXIT_FAILURE : EXIT_SUCCESS;            \
-        }
+        _DEFINE_MAIN_FUNCTION(impl, r != 0 ? EXIT_FAILURE : EXIT_SUCCESS)
