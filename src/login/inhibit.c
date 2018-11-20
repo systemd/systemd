1d64b2f1c398e2d703c701ddb5a1487dd267ae7c
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2018-11-20 09:41:32 +0100

    inhibit: fix return value in error path

--- src/login/inhibit.c
+++ src/login/inhibit.c
@@ -303,7 +303,7 @@ static int run(int argc, char *argv[]) {
 
                 fd = inhibit(bus, &error);
                 if (fd < 0)
-                        return log_error("Failed to inhibit: %s", bus_error_message(&error, fd));
+                        return log_error_errno(fd, "Failed to inhibit: %s", bus_error_message(&error, fd));
 
                 r = safe_fork("(inhibit)", FORK_RESET_SIGNALS|FORK_DEATHSIG|FORK_CLOSE_ALL_FDS|FORK_LOG, &pid);
                 if (r < 0)
