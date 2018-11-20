9541f5ff5c637bb1b3e3c69706cb73e68ff06813
Evgeny Vereshchagin <evvers@ya.ru>
2018-11-20 01:20:32 +0100

    tests: add a fuzzer for journald streams

--- src/journal/journald-stream.h
+++ src/journal/journald-stream.h
@@ -10,4 +10,6 @@ int server_open_stdout_socket(Server *s);
 int server_restore_streams(Server *s, FDSet *fds);
 
 void stdout_stream_free(StdoutStream *s);
+int stdout_stream_install(Server *s, int fd, StdoutStream **ret);
+void stdout_stream_destroy(StdoutStream *s);
 void stdout_stream_send_notify(StdoutStream *s);
