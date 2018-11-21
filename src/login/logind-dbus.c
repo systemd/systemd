909a3d84ab9d0efc305bf081b29f15971dffb7fd
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2018-11-21 10:21:28 +0100

    Revert 5fdf2d51c244288ac41443d1bd81365fab7b7b81
    This reverts 5fdf2d51c244288ac41443d1bd81365fab7b7b81, except for one improved
    log message.
    
    Fixes #10613.
    
    Checking if resume= is configured is a good idea, but it turns out we cannot do
    it reliably:
    - the code only supported boot options with sd-boot, and it's not very widely
      used. This means that for most systemd we could only check the current
      commandline, not the next one.
    - Various systems resume without, e.g. Debian has
      /etc/initramfs-tools/conf.d/resume in the initramfs.
    
    Making those checks better would be possible with enough effort, but there'll
    be always new systems that boot in a slightly different way and we would need
    to keep adding new cases. Longer term, we want to rely on autodetecting the
    resume partition, and then checks like this will not be necessary at all. It is
    quite clear from the number of bug reports that the number of poeple impacted
    by this is quite high now, so let's just drop this.

--- src/login/logind-dbus.c
+++ src/login/logind-dbus.c
@@ -1779,8 +1779,6 @@ static int method_do_shutdown_or_sleep(
                         return sd_bus_error_set(error, BUS_ERROR_SLEEP_VERB_NOT_SUPPORTED, "Not enough swap space for hibernation");
                 if (r == -ENOMEDIUM)
                         return sd_bus_error_set(error, BUS_ERROR_SLEEP_VERB_NOT_SUPPORTED, "Kernel image has been removed, can't hibernate");
-                if (r == -EADV)
-                        return sd_bus_error_set(error, BUS_ERROR_SLEEP_VERB_NOT_SUPPORTED, "Resume not configured, can't hibernate");
                 if (r == 0)
                         return sd_bus_error_setf(error, BUS_ERROR_SLEEP_VERB_NOT_SUPPORTED, "Sleep verb \"%s\" not supported", sleep_verb);
                 if (r < 0)
@@ -2209,7 +2207,7 @@ static int method_can_shutdown_or_sleep(
 
         if (sleep_verb) {
                 r = can_sleep(sleep_verb);
-                if (IN_SET(r,  0, -ENOSPC, -ENOMEDIUM, -EADV))
+                if (IN_SET(r,  0, -ENOSPC, -ENOMEDIUM))
                         return sd_bus_reply_method_return(message, "s", "na");
                 if (r < 0)
                         return r;
