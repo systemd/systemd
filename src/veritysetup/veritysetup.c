6b9306b29c0360030fc4ecd4d0f739502bc459d3
Yu Watanabe <watanabe.yu+github@gmail.com>
2018-11-23 04:21:08 +0900

    veritysetup: use static destructors and define main through macro
    This also changes the return value when crypt_init_by_name() fails in 'detach'.

--- src/veritysetup/veritysetup.c
+++ src/veritysetup/veritysetup.c
@@ -8,6 +8,7 @@
 #include "crypt-util.h"
 #include "hexdecoct.h"
 #include "log.h"
+#include "main-func.h"
 #include "pretty-print.h"
 #include "string-util.h"
 #include "terminal-util.h"
@@ -15,6 +16,10 @@
 static char *arg_root_hash = NULL;
 static char *arg_data_what = NULL;
 static char *arg_hash_what = NULL;
+
+STATIC_DESTRUCTOR_REGISTER(arg_root_hash, freep);
+STATIC_DESTRUCTOR_REGISTER(arg_data_what, freep);
+STATIC_DESTRUCTOR_REGISTER(arg_hash_what, freep);
 
 static int help(void) {
         _cleanup_free_ char *link = NULL;
@@ -36,20 +41,15 @@ static int help(void) {
         return 0;
 }
 
-int main(int argc, char *argv[]) {
+static int run(int argc, char *argv[]) {
         _cleanup_(crypt_freep) struct crypt_device *cd = NULL;
         int r;
 
-        if (argc <= 1) {
-                r = help();
-                goto finish;
-        }
+        if (argc <= 1)
+                return help();
 
-        if (argc < 3) {
-                log_error("This program requires at least two arguments.");
-                r = -EINVAL;
-                goto finish;
-        }
+        if (argc < 3)
+                return log_error_errno(SYNTHETIC_ERRNO(EINVAL), "This program requires at least two arguments.");
 
         log_setup_service();
 
@@ -60,82 +60,57 @@ int main(int argc, char *argv[]) {
                 crypt_status_info status;
                 size_t l;
 
-                if (argc < 6) {
-                        log_error("attach requires at least two arguments.");
-                        r = -EINVAL;
-                        goto finish;
-                }
+                if (argc < 6)
+                        return log_error_errno(SYNTHETIC_ERRNO(EINVAL), "attach requires at least two arguments.");
 
                 r = unhexmem(argv[5], strlen(argv[5]), &m, &l);
-                if (r < 0) {
-                        log_error("Failed to parse root hash.");
-                        goto finish;
-                }
+                if (r < 0)
+                        return log_error_errno(r, "Failed to parse root hash: %m");
 
                 r = crypt_init(&cd, argv[4]);
-                if (r < 0) {
-                        log_error_errno(r, "Failed to open verity device %s: %m", argv[4]);
-                        goto finish;
-                }
+                if (r < 0)
+                        return log_error_errno(r, "Failed to open verity device %s: %m", argv[4]);
 
                 crypt_set_log_callback(cd, cryptsetup_log_glue, NULL);
 
                 status = crypt_status(cd, argv[2]);
                 if (IN_SET(status, CRYPT_ACTIVE, CRYPT_BUSY)) {
                         log_info("Volume %s already active.", argv[2]);
-                        r = 0;
-                        goto finish;
+                        return 0;
                 }
 
                 r = crypt_load(cd, CRYPT_VERITY, NULL);
-                if (r < 0) {
-                        log_error_errno(r, "Failed to load verity superblock: %m");
-                        goto finish;
-                }
+                if (r < 0)
+                        return log_error_errno(r, "Failed to load verity superblock: %m");
 
                 r = crypt_set_data_device(cd, argv[3]);
-                if (r < 0) {
-                        log_error_errno(r, "Failed to configure data device: %m");
-                        goto finish;
-                }
+                if (r < 0)
+                        return log_error_errno(r, "Failed to configure data device: %m");
 
                 r = crypt_activate_by_volume_key(cd, argv[2], m, l, CRYPT_ACTIVATE_READONLY);
-                if (r < 0) {
-                        log_error_errno(r, "Failed to set up verity device: %m");
-                        goto finish;
-                }
+                if (r < 0)
+                        return log_error_errno(r, "Failed to set up verity device: %m");
 
         } else if (streq(argv[1], "detach")) {
 
                 r = crypt_init_by_name(&cd, argv[2]);
                 if (r == -ENODEV) {
                         log_info("Volume %s already inactive.", argv[2]);
-                        goto finish;
-                } else if (r < 0) {
-                        log_error_errno(r, "crypt_init_by_name() failed: %m");
-                        goto finish;
+                        return 0;
                 }
+                if (r < 0)
+                        return log_error_errno(r, "crypt_init_by_name() failed: %m");
 
                 crypt_set_log_callback(cd, cryptsetup_log_glue, NULL);
 
                 r = crypt_deactivate(cd, argv[2]);
-                if (r < 0) {
-                        log_error_errno(r, "Failed to deactivate: %m");
-                        goto finish;
-                }
+                if (r < 0)
+                        return log_error_errno(r, "Failed to deactivate: %m");
 
-        } else {
-                log_error("Unknown verb %s.", argv[1]);
-                r = -EINVAL;
-                goto finish;
-        }
+        } else
+                return log_error_errno(SYNTHETIC_ERRNO(EINVAL), "Unknown verb %s.", argv[1]);
 
-        r = 0;
-
-finish:
-        free(arg_root_hash);
-        free(arg_data_what);
-        free(arg_hash_what);
-
-        return r < 0 ? EXIT_FAILURE : EXIT_SUCCESS;
+        return 0;
 }
+
+DEFINE_MAIN_FUNCTION(run);
