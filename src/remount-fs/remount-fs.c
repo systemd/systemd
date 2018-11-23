59f13dd6f801b2df4faea277a5de0115ed598e35
Lennart Poettering <lennart@poettering.net>
2018-11-23 19:47:41 +0100

    remount-fs: optionally remount / writable, if we are told through an env var

--- src/remount-fs/remount-fs.c
+++ src/remount-fs/remount-fs.c
@@ -8,6 +8,7 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+#include "env-util.h"
 #include "exit-status.h"
 #include "log.h"
 #include "main-func.h"
@@ -49,6 +50,7 @@ static int track_pid(Hashmap **h, const char *path, pid_t pid) {
 static int run(int argc, char *argv[]) {
         _cleanup_hashmap_free_free_ Hashmap *pids = NULL;
         _cleanup_endmntent_ FILE *f = NULL;
+        bool has_root = false;
         struct mntent* me;
         int r;
 
@@ -62,44 +64,72 @@ static int run(int argc, char *argv[]) {
 
         f = setmntent("/etc/fstab", "re");
         if (!f) {
-                if (errno == ENOENT)
-                        return 0;
+                if (errno != ENOENT)
+                        return log_error_errno(errno, "Failed to open /etc/fstab: %m");
+        } else {
+                while ((me = getmntent(f))) {
+                        pid_t pid;
 
-                return log_error_errno(errno, "Failed to open /etc/fstab: %m");
+                        /* Remount the root fs, /usr and all API VFS */
+                        if (!mount_point_is_api(me->mnt_dir) &&
+                            !PATH_IN_SET(me->mnt_dir, "/", "/usr"))
+                                continue;
+
+                        log_debug("Remounting %s...", me->mnt_dir);
+
+                        if (path_equal(me->mnt_dir, "/"))
+                                has_root = true;
+
+                        r = safe_fork("(remount)", FORK_RESET_SIGNALS|FORK_DEATHSIG|FORK_RLIMIT_NOFILE_SAFE|FORK_LOG, &pid);
+                        if (r < 0)
+                                return r;
+                        if (r == 0) {
+                                /* Child */
+                                execv(MOUNT_PATH, STRV_MAKE(MOUNT_PATH, me->mnt_dir, "-o", "remount"));
+                                log_error_errno(errno, "Failed to execute " MOUNT_PATH ": %m");
+                                _exit(EXIT_FAILURE);
+                        }
+
+                        /* Parent */
+                        r = track_pid(&pids, me->mnt_dir, pid);
+                        if (r < 0)
+                                return r;
+                }
         }
 
-        while ((me = getmntent(f))) {
-                pid_t pid;
+        if (!has_root) {
+                /* The $SYSTEMD_REMOUNT_ROOT_RW environment variable is set by systemd-gpt-auto-generator to tell us
+                 * whether to remount things. We honour it only if there's no explicit line in /etc/fstab configured
+                 * which takes precedence. */
 
-                /* Remount the root fs, /usr and all API VFS */
-                if (!mount_point_is_api(me->mnt_dir) &&
-                    !PATH_IN_SET(me->mnt_dir, "/", "/usr"))
-                        continue;
+                r = getenv_bool("SYSTEMD_REMOUNT_ROOT_RW");
+                if (r > 0) {
+                        pid_t pid;
 
-                log_debug("Remounting %s", me->mnt_dir);
+                        log_debug("Remounting / writable...");
 
-                r = safe_fork("(remount)", FORK_RESET_SIGNALS|FORK_DEATHSIG|FORK_RLIMIT_NOFILE_SAFE|FORK_LOG, &pid);
-                if (r < 0)
-                        return r;
-                if (r == 0) {
-                        /* Child */
+                        r = safe_fork("(remount-rw)", FORK_RESET_SIGNALS|FORK_DEATHSIG|FORK_LOG, &pid);
+                        if (r < 0)
+                                return r;
+                        if (r == 0) {
+                                /* Child */
+                                execv(MOUNT_PATH, STRV_MAKE(MOUNT_PATH, "/", "-o", "remount,rw"));
+                                log_error_errno(errno, "Failed to execute " MOUNT_PATH ": %m");
+                                _exit(EXIT_FAILURE);
+                        }
 
-                        execv(MOUNT_PATH, STRV_MAKE(MOUNT_PATH, me->mnt_dir, "-o", "remount"));
+                        r = track_pid(&pids, "/", pid);
+                        if (r < 0)
+                                return r;
 
-                        log_error_errno(errno, "Failed to execute " MOUNT_PATH ": %m");
-                        _exit(EXIT_FAILURE);
-                }
-
-                /* Parent */
-                r = track_pid(&pids, me->mnt_dir, pid);
-                if (r < 0)
-                        return r;
+                } else if (r < 0 && r != -ENXIO)
+                        log_warning_errno(r, "Failed to parse $SYSTEMD_REMOUNT_ROOT_RW, ignoring: %m");
         }
 
         r = 0;
         while (!hashmap_isempty(pids)) {
-                siginfo_t si = {};
                 _cleanup_free_ char *s = NULL;
+                siginfo_t si = {};
 
                 if (waitid(P_ALL, 0, &si, WEXITED) < 0) {
                         if (errno == EINTR)
