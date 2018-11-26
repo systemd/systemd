dbe6c4b657aa5c58bfc049d869b94f00b41b7d95
Lennart Poettering <lennart@poettering.net>
2018-11-26 21:06:19 +0100

    dbus-execute: fix indentation

--- src/core/dbus-execute.c
+++ src/core/dbus-execute.c
@@ -1844,9 +1844,9 @@ int bus_exec_context_set_transient_property(
                                         c->std_error = EXEC_OUTPUT_FILE;
                                         unit_write_settingf(u, flags|UNIT_ESCAPE_SPECIFIERS, name, "StandardOutput=file:%s", s);
                                 } else {
-                                      assert(streq(name, "StandardErrorFileToAppend"));
-                                      c->std_error = EXEC_OUTPUT_FILE_APPEND;
-                                      unit_write_settingf(u, flags|UNIT_ESCAPE_SPECIFIERS, name, "StandardOutput=append:%s", s);
+                                        assert(streq(name, "StandardErrorFileToAppend"));
+                                        c->std_error = EXEC_OUTPUT_FILE_APPEND;
+                                        unit_write_settingf(u, flags|UNIT_ESCAPE_SPECIFIERS, name, "StandardOutput=append:%s", s);
                                 }
                         }
                 }
