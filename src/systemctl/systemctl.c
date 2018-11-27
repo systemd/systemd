46efc9780d3f955294cf1807e7e2f7f32c180bfd
Lennart Poettering <lennart@poettering.net>
2018-11-27 13:40:19 +0100

    systemctl: downgrade log message to LOG_NOTICE
    This log message is not problematic at all, it is merely explanatory,
    hence LOG_WARN is too high for this.

--- src/systemctl/systemctl.c
+++ src/systemctl/systemctl.c
@@ -6297,18 +6297,18 @@ static int enable_unit(int argc, char *argv[], void *userdata) {
         }
 
         if (carries_install_info == 0 && !ignore_carries_install_info)
-                log_warning("The unit files have no installation config (WantedBy=, RequiredBy=, Also=,\n"
-                            "Alias= settings in the [Install] section, and DefaultInstance= for template\n"
-                            "units). This means they are not meant to be enabled using systemctl.\n"
-                            "Possible reasons for having this kind of units are:\n"
-                            "1) A unit may be statically enabled by being symlinked from another unit's\n"
-                            "   .wants/ or .requires/ directory.\n"
-                            "2) A unit's purpose may be to act as a helper for some other unit which has\n"
-                            "   a requirement dependency on it.\n"
-                            "3) A unit may be started when needed via activation (socket, path, timer,\n"
-                            "   D-Bus, udev, scripted systemctl call, ...).\n"
-                            "4) In case of template units, the unit is meant to be enabled with some\n"
-                            "   instance name specified.");
+                log_notice("The unit files have no installation config (WantedBy=, RequiredBy=, Also=,\n"
+                           "Alias= settings in the [Install] section, and DefaultInstance= for template\n"
+                           "units). This means they are not meant to be enabled using systemctl.\n"
+                           "Possible reasons for having this kind of units are:\n"
+                           "1) A unit may be statically enabled by being symlinked from another unit's\n"
+                           "   .wants/ or .requires/ directory.\n"
+                           "2) A unit's purpose may be to act as a helper for some other unit which has\n"
+                           "   a requirement dependency on it.\n"
+                           "3) A unit may be started when needed via activation (socket, path, timer,\n"
+                           "   D-Bus, udev, scripted systemctl call, ...).\n"
+                           "4) In case of template units, the unit is meant to be enabled with some\n"
+                           "   instance name specified.");
 
         if (arg_now && STR_IN_SET(argv[0], "enable", "disable", "mask")) {
                 sd_bus *bus;
