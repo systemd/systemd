95ccf1aa5f440d8310050a71234ab57386ec223a
Evgeny Vereshchagin <evvers@ya.ru>
2018-11-27 21:57:33 +0100

    tests: suppress "unwanted log lines" in several fuzzers
    According to https://oss-fuzz.com/fuzzer-stats/by-fuzzer/fuzzer/libFuzzer/job/libfuzzer_asan_systemd,
    fuzz-network-parser, fuzz-netdev-parser and fuzz-journal-remote produce
    a lot of unwanted log lines. Let's set the maximum log level to LOG_CRIT
    as we do in the other fuzzers.

--- src/network/fuzz-netdev-parser.c
+++ src/network/fuzz-netdev-parser.c
@@ -11,6 +11,9 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
         _cleanup_fclose_ FILE *f = NULL;
         _cleanup_(unlink_tempfilep) char netdev_config[] = "/tmp/fuzz-networkd.XXXXXX";
 
+        if (!getenv("SYSTEMD_LOG_LEVEL"))
+                log_set_max_level(LOG_CRIT);
+
         assert_se(fmkostemp_safe(netdev_config, "r+", &f) == 0);
         if (size != 0)
                 assert_se(fwrite(data, size, 1, f) == 1);
