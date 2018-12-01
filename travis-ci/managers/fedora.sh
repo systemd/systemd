3e003ef4bce0094caf80ddd409e33128135fd101
Evgeny Vereshchagin <evvers@ya.ru>
2018-12-01 02:42:14 +0100

    travis: run test-execute directly

--- travis-ci/managers/fedora.sh
+++ travis-ci/managers/fedora.sh
@@ -15,7 +15,7 @@ CONT_NAME="${CONT_NAME:-fedora-$FEDORA_RELEASE-$RANDOM}"
 DOCKER_EXEC="${DOCKER_EXEC:-docker exec -it $CONT_NAME}"
 DOCKER_RUN="${DOCKER_RUN:-docker run}"
 REPO_ROOT="${REPO_ROOT:-$PWD}"
-ADDITIONAL_DEPS=(dnf-plugins-core python2 iputils hostname libasan python3-pyparsing python3-evdev libubsan)
+ADDITIONAL_DEPS=(dnf-plugins-core python2 iputils hostname libasan python3-pyparsing python3-evdev libubsan strace)
 
 function info() {
     echo -e "\033[33;1m$1\033[0m"
@@ -47,10 +47,9 @@ for phase in "${PHASES[@]}"; do
         RUN)
             info "Run phase"
             # Build systemd
-            $DOCKER_EXEC meson --werror -Dtests=unsafe -Dslow-tests=true build
+            $DOCKER_EXEC meson -Dtests=unsafe -Dslow-tests=true build
             $DOCKER_EXEC ninja -v -C build
-            $DOCKER_EXEC ninja -C build test
-            $DOCKER_EXEC tools/check-directives.sh
+            $DOCKER_EXEC strace -tt -T -s500 -f -v -yy ./build/test-execute
             ;;
         RUN_ASAN)
             $DOCKER_EXEC git clean -dxff
