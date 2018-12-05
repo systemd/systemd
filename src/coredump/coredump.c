4f62556d71206ac814a020a954b397d4940e14c3
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2018-12-05 17:33:15 +0100

    coredump: remove duplicate MESSAGE= prefix from message
    systemd-coredump[9982]: MESSAGE=Process 771 (systemd-journal) of user 0 dumped core.
    systemd-coredump[9982]: Coredump diverted to /var/lib/systemd/coredump/core...
    
    log_dispatch() calls log_dispatch_internal() which calls write_to_journal()
    which appends MESSAGE= on its own.

--- src/coredump/coredump.c
+++ src/coredump/coredump.c
@@ -800,9 +800,10 @@ log:
                 return log_oom();
 
         if (journald_crash) {
-                /* We cannot log to the journal, so just print the MESSAGE.
+                /* We cannot log to the journal, so just print the message.
                  * The target was set previously to something safe. */
-                log_dispatch(LOG_ERR, 0, core_message);
+                assert(startswith(core_message, "MESSAGE="));
+                log_dispatch(LOG_ERR, 0, core_message + strlen("MESSAGE="));
                 return 0;
         }
 
