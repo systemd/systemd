503f480f8e4f66963900d0e58b9030c4a9387c41
Yu Watanabe <watanabe.yu+github@gmail.com>
2018-12-06 06:57:27 +0100

    missing: move fs or mount related definitions to missing_fs.h
    This also fixes errnous definition MS_REC -> MS_SLAVE.

--- src/basic/missing.h
+++ src/basic/missing.h
@@ -7,70 +7,11 @@
 #include <inttypes.h>
 #include <sys/syscall.h>
 
-#ifndef FS_NOCOW_FL
-#define FS_NOCOW_FL 0x00800000
-#endif
-
-#ifndef MS_MOVE
-#define MS_MOVE 8192
-#endif
-
-#ifndef MS_REC
-#define MS_REC 16384
-#endif
-
-#ifndef MS_PRIVATE
-#define MS_PRIVATE      (1<<18)
-#endif
-
-#ifndef MS_REC
-#define MS_REC          (1<<19)
-#endif
-
-#ifndef MS_SHARED
-#define MS_SHARED       (1<<20)
-#endif
-
-#ifndef MS_RELATIME
-#define MS_RELATIME     (1<<21)
-#endif
-
-#ifndef MS_KERNMOUNT
-#define MS_KERNMOUNT    (1<<22)
-#endif
-
-#ifndef MS_I_VERSION
-#define MS_I_VERSION    (1<<23)
-#endif
-
-#ifndef MS_STRICTATIME
-#define MS_STRICTATIME  (1<<24)
-#endif
-
-#ifndef MS_LAZYTIME
-#define MS_LAZYTIME     (1<<25)
-#endif
-
-#ifndef CIFS_MAGIC_NUMBER
-#  define CIFS_MAGIC_NUMBER 0xFF534D42
-#endif
-
-#ifndef RENAME_NOREPLACE
-#define RENAME_NOREPLACE (1 << 0)
-#endif
-
-#ifndef EXT4_IOC_RESIZE_FS
-#  define EXT4_IOC_RESIZE_FS              _IOW('f', 16, __u64)
-#endif
-
-#ifndef NS_GET_NSTYPE
-#define NS_GET_NSTYPE _IO(0xb7, 0x3)
-#endif
-
 #include "missing_audit.h"
 #include "missing_btrfs_tree.h"
 #include "missing_capability.h"
 #include "missing_fcntl.h"
+#include "missing_fs.h"
 #include "missing_input.h"
 #include "missing_magic.h"
 #include "missing_mman.h"
