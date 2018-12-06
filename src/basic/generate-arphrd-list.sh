0b7d48f5f4b1187cef617ced8b8ba6d8cb7c38b0
Yu Watanabe <watanabe.yu+github@gmail.com>
2018-12-06 17:42:21 +0100

    af-list,arphrd-list: also include relevant missing_*.h headers

--- src/basic/generate-arphrd-list.sh
+++ src/basic/generate-arphrd-list.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eu
 
-$1 -dM -include net/if_arp.h - </dev/null | \
+$1 -dM -include net/if_arp.h -include "$2" -include "$3" - </dev/null | \
         awk '/^#define[ \t]+ARPHRD_[^ \t]+[ \t]+[^ \t]/ { print $2; }' | \
         sed -e 's/ARPHRD_//'
