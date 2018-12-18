5ead13ecdb3dbe3ccb0a8204238e929d0f20d96b
Yu Watanabe <watanabe.yu+github@gmail.com>
2018-12-18 15:29:37 +0900

    Revert "udevd: do not call udev_ctrl_enable_receiving() if the socket is passed from pid1"
    This reverts commit 6acf1ccba5ef1741649bf858fac5afabb2328fff.
    
    As udev_ctrl_enable_receiving() does nothing if the socket is passed
    from pid1. Let's simplify the logic.

--- src/udev/udevd.c
+++ src/udev/udevd.c
@@ -1611,11 +1611,9 @@ static int manager_new(Manager **ret, int fd_ctrl, int fd_uevent, const char *cg
         if (!manager->ctrl)
                 return log_error_errno(SYNTHETIC_ERRNO(EINVAL), "Failed to initialize udev control socket");
 
-        if (fd_ctrl < 0) {
-                r = udev_ctrl_enable_receiving(manager->ctrl);
-                if (r < 0)
-                        return log_error_errno(r, "Failed to bind udev control socket: %m");
-        }
+        r = udev_ctrl_enable_receiving(manager->ctrl);
+        if (r < 0)
+                return log_error_errno(r, "Failed to bind udev control socket: %m");
 
         fd_ctrl = udev_ctrl_get_fd(manager->ctrl);
         if (fd_ctrl < 0)
