e4d5753bd094cf542f54edbbd58417a5394f5384
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2018-12-19 13:25:07 +0100

    meson-vcs-tag: add work-around for git bug

--- tools/meson-vcs-tag.sh
+++ tools/meson-vcs-tag.sh
@@ -3,7 +3,10 @@
 set -eu
 set -o pipefail
 
-gitdir="$1"
+dir="$1"
 fallback="$2"
 
-git --git-dir="$gitdir" describe --abbrev=7 --dirty=+ 2>/dev/null | sed 's/^v//' || echo "$fallback"
+# Apparently git describe has a bug where it always considers the work-tree
+# dirty when invoked with --git-dir (even though 'git status' is happy). Work
+# around this issue by cd-ing to the source directory.
+cd "$dir" && git describe --abbrev=7 --dirty=+ 2>/dev/null | sed 's/^v//' || echo "$fallback"
