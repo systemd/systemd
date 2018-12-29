03475e2232d655e9a1c584080ca4ed592b1bd623
Yu Watanabe <watanabe.yu+github@gmail.com>
2018-12-29 22:00:07 +0900

    meson: check whether C.UTF-8 exists or not and use it if exists
    If C.UTF-8 does not exist, then fallback to en_US.UTF-8 or C.

---
+++ tools/choose-default-locale.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+# Fedora uses C.utf8 but Debian uses C.UTF-8
+if locale -a | grep -xq -E 'C\.(utf8|UTF-8)'; then
+        echo 'C.UTF-8'
+elif locale -a | grep -xqF 'en_US.utf8'; then
+        echo 'en_US.UTF-8'
+else
+        echo 'C'
+fi
