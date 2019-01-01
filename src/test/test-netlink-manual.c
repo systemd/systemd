0912ca08a0fc98bf7fb99080aa218ca3100a216b
Yu Watanabe <watanabe.yu+github@gmail.com>
2019-01-02 02:29:49 +0900

    tree-wide: drop libkmod.h if module-util.h is included

--- src/test/test-netlink-manual.c
+++ src/test/test-netlink-manual.c
@@ -1,7 +1,6 @@
 /* SPDX-License-Identifier: LGPL-2.1+ */
 
 #include <arpa/inet.h>
-#include <libkmod.h>
 #include <linux/ip.h>
 #include <net/if.h>
 #include <linux/if_tunnel.h>
--- src/udev/udev-builtin-kmod.c
+++ src/udev/udev-builtin-kmod.c
@@ -6,7 +6,6 @@
  */
 
 #include <errno.h>
-#include <libkmod.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
