44ce4255147ab308c1f13580147c693204c322e8
Lennart Poettering <lennart@poettering.net>
2019-01-21 20:19:57 +0100

    cryptsetup: downgrade a log message we ignore

--- src/cryptsetup/cryptsetup.c
+++ src/cryptsetup/cryptsetup.c
@@ -623,7 +623,7 @@ static int run(int argc, char *argv[]) {
                     !streq(argv[4], "none")) {
 
                         if (!path_is_absolute(argv[4]))
-                                log_error("Password file path '%s' is not absolute. Ignoring.", argv[4]);
+                                log_warning("Password file path '%s' is not absolute. Ignoring.", argv[4]);
                         else
                                 key_file = argv[4];
                 }
