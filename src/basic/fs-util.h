71f514169b14e2bd67e9465f132bae502b51af98
Lennart Poettering <lennart@poettering.net>
2019-01-23 16:08:55 +0100

    fs-util: add new helper syncfs_path()

--- src/basic/fs-util.h
+++ src/basic/fs-util.h
@@ -108,4 +108,6 @@ int unlinkat_deallocate(int fd, const char *name, int flags);
 int fsync_directory_of_file(int fd);
 int fsync_path_at(int at_fd, const char *path);
 
+int syncfs_path(int atfd, const char *path);
+
 int open_parent(const char *path, int flags, mode_t mode);
