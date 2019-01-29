8605a4b9eb1081a2f56745b57dc5d13a7637048e
Evgeny Vereshchagin <evvers@ya.ru>
2019-01-29 03:19:43 +0100

    travis: switch to the "official" systemd-ci repository
    Now that add-apt-repository hasn't failed for almost two days on Semaphore
    it should be safe to assume that the key has been propagated properly
    and the repository is ready to be used on Travis CI.

--- travis-ci/managers/xenial.sh
+++ travis-ci/managers/xenial.sh
@@ -3,7 +3,7 @@
 set -e
 set -x
 
-add-apt-repository ppa:pitti/systemd-semaphore -y
+add-apt-repository ppa:upstream-systemd-ci/systemd-ci -y
 apt-get update
 apt-get build-dep systemd -y
 apt-get install -y util-linux libmount-dev libblkid-dev liblzma-dev libqrencode-dev libmicrohttpd-dev iptables-dev liblz4-dev libcurl4-gnutls-dev unifont itstool kbd cryptsetup-bin net-tools isc-dhcp-client iputils-ping strace qemu-system-x86 linux-image-virtual mount libgpg-error-dev libxkbcommon-dev python-lxml python3-lxml python3-pip libcap-dev
@@ -11,8 +11,6 @@ apt-get install -y gettext python3-evdev python3-pyparsing libmount-dev python3-
 pip3 install meson
 
 cd $REPO_ROOT
-
-sed -i 's/2\.30/2.27/' meson.build
 
 meson --werror -Db_sanitize=address,undefined -Dsplit-usr=true build
 ninja -v -C build
