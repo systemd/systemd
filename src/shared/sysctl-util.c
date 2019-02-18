583052110384154eb3fd605ae1e6df51dc5d9c2e
Yu Watanabe <watanabe.yu+github@gmail.com>
2019-02-18 14:41:43 +0900

    sysctl-util: add sysctl_read_ip_property()

--- src/shared/sysctl-util.c
+++ src/shared/sysctl-util.c
@@ -85,3 +85,25 @@ int sysctl_read(const char *property, char **content) {
         p = strjoina("/proc/sys/", property);
         return read_full_file(p, content, NULL);
 }
+
+int sysctl_read_ip_property(int af, const char *ifname, const char *property, char **ret) {
+        _cleanup_free_ char *value = NULL;
+        const char *p;
+        int r;
+
+        assert(IN_SET(af, AF_INET, AF_INET6));
+        assert(property);
+
+        p = strjoina("/proc/sys/net/ipv", af == AF_INET ? "4" : "6",
+                     ifname ? "/conf/" : "", strempty(ifname),
+                     property[0] == '/' ? "" : "/", property);
+
+        r = read_one_line_file(p, &value);
+        if (r < 0)
+                return r;
+
+        if (ret)
+                *ret = TAKE_PTR(value);
+
+        return r;
+}
