12c8bce2723416820b5e7129e7bb3d9a6d5f5bc6
Yu Watanabe <watanabe.yu+github@gmail.com>
2019-02-25 14:56:21 +0800

    glob-util: filter out . and .. even if GLOB_ALTDIRFUNC is not supported
    musl neither support GLOB_ALTDIRFUNC nor GLOB_BRACE.
    Let's make safe_glob() work even when GLOB_ALTDIRFUNC is not supported.
    Currently, GLOB_BRACE is simply ignored when it is not supported.
    
    Upstream-Status: Inappropriate [musl specific]
    Signed-off-by: Yu Watanabe <watanabe.yu+github@gmail.com>

---
+++ src/basic/include/glob.h
@@ -0,0 +1,22 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#pragma once
+
+#include_next <glob.h>
+
+/* musl does not have GLOB_ALTDIRFUNC */
+#ifndef GLOB_ALTDIRFUNC
+#define HAVE_GLOB_ALTDIRFUNC 0
+#define GLOB_ALTDIRFUNC      (1 << 9)
+#else
+#define HAVE_GLOB_ALTDIRFUNC 1
+assert_cc(GLOB_ALTDIRFUNC == (1 << 9));
+#endif
+
+/* musl does not have GLOB_BRACE */
+#ifndef GLOB_BRACE
+#define HAVE_GLOB_BRACE 0
+#define GLOB_BRACE      (1 << 10)
+#else
+#define HAVE_GLOB_BRACE 1
+assert_cc(GLOB_BRACE == (1 << 10));
+#endif
