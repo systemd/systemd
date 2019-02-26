eafadd069c4e30ed62173123326a7237448615d1
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2019-02-26 13:00:35 +0100

    fuzz-journal-stream: avoid assertion failure on samples which don't fit in pipe
    Fixes https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=11587.
    We had a sample which was large enough that write(2) failed to push all the
    data into the pipe, and an assert failed. The code could be changed to use
    a loop, but then we'd need to interleave writes and sd_event_run (to process
    the journal). I don't think the complexity is worth it — fuzzing works best
    if the sample is not too huge anyway. So let's just reject samples above 64k,
    and tell oss-fuzz about this limit.

--- src/fuzz/fuzz-journald-stream.c
+++ src/fuzz/fuzz-journald-stream.c
@@ -15,7 +15,7 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
         StdoutStream *stream;
         int v;
 
-        if (size == 0)
+        if (size == 0 || size > 65536)
                 return 0;
 
         if (!getenv("SYSTEMD_LOG_LEVEL"))
