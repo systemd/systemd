96b86e5fd7436a61ba4e3765d93a9e508f0ab762
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2019-03-01 09:10:03 +0100

    tests: move various definitions from test-helper.[ch] to tests.h
    test-helper.c predates tests.h, but nowadays the latter is more appropriate.

--- src/shared/tests.h
+++ src/shared/tests.h
@@ -3,6 +3,27 @@
 
 #include <stdbool.h>
 
+#include "sd-daemon.h"
+
+#include "string-util.h"
+
+#define TEST_REQ_RUNNING_SYSTEMD(x)                                 \
+        if (sd_booted() > 0) {                                      \
+                x;                                                  \
+        } else {                                                    \
+                printf("systemd not booted skipping '%s'\n", #x);   \
+        }
+
+#define MANAGER_SKIP_TEST(r)                                    \
+        IN_SET(r,                                               \
+               -EPERM,                                          \
+               -EACCES,                                         \
+               -EADDRINUSE,                                     \
+               -EHOSTDOWN,                                      \
+               -ENOENT,                                         \
+               -ENOMEDIUM /* cannot determine cgroup */         \
+               )
+
 char* setup_fake_runtime_dir(void);
 const char* get_testdata_dir(void);
 const char* get_catalog_dir(void);
@@ -12,3 +33,8 @@ int log_tests_skipped(const char *message);
 int log_tests_skipped_errno(int r, const char *message);
 
 bool have_namespaces(void);
+
+/* https://docs.travis-ci.com/user/environment-variables#default-environment-variables */
+static inline bool is_run_on_travis_ci(void) {
+        return streq_ptr(getenv("TRAVIS"), "true");
+}
