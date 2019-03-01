0a4bd519e4a9d9fff35b6c077d34545a89df4b24
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2019-03-01 09:03:37 +0100

    test-execute: simplify test skipping

--- src/test/test-execute.c
+++ src/test/test-execute.c
@@ -799,12 +799,8 @@ int main(int argc, char *argv[]) {
 
         test_setup_logging(LOG_DEBUG);
 
-#if HAS_FEATURE_ADDRESS_SANITIZER
-        if (is_run_on_travis_ci()) {
-                log_notice("Running on TravisCI under ASan, skipping, see https://github.com/systemd/systemd/issues/10696");
-                return EXIT_TEST_SKIP;
-        }
-#endif
+        if (HAS_FEATURE_ADDRESS_SANITIZER && is_run_on_travis_ci())
+                return log_tests_skipped("Running on TravisCI under ASan, skipping, see https://github.com/systemd/systemd/issues/10696");
 
         (void) unsetenv("USER");
         (void) unsetenv("LOGNAME");
