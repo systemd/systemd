ae65d7dbe3fec6f6c0d10915f415675df5ed1967
Yu Watanabe <watanabe.yu+github@gmail.com>
2019-03-04 17:54:48 +0900

    network: introduce new bus method Renew() and RenewLink()

--- src/network/networkd-link-bus.h
+++ src/network/networkd-link-bus.h
@@ -30,3 +30,4 @@ int bus_link_method_set_dnssec(sd_bus_message *message, void *userdata, sd_bus_e
 int bus_link_method_set_dnssec_negative_trust_anchors(sd_bus_message *message, void *userdata, sd_bus_error *error);
 int bus_link_method_revert_ntp(sd_bus_message *message, void *userdata, sd_bus_error *error);
 int bus_link_method_revert_dns(sd_bus_message *message, void *userdata, sd_bus_error *error);
+int bus_link_method_renew(sd_bus_message *message, void *userdata, sd_bus_error *error);
