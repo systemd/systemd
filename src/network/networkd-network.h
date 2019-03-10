d3282a4b6f33ba727eec54ec04afda4637037fc5
Tobias Jungel <tobias.jungel@bisdn.de>
2019-03-10 21:25:24 +0100

    networkd: Add bridge port capabilities
    This PR adds the configuration switches for multicast flooding, neighbor
    suppression and learning of a bridge port.

--- src/network/networkd-network.h
+++ src/network/networkd-network.h
@@ -189,7 +189,10 @@ struct Network {
         int fast_leave;
         int allow_port_to_be_root;
         int unicast_flood;
+        int multicast_flood;
         int multicast_to_unicast;
+        int neighbor_suppression;
+        int learning;
         uint32_t cost;
         uint16_t priority;
 
