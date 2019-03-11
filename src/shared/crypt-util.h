9e12bcff9930acb0ea76cfb873ed9c6494c37962
Yu Watanabe <watanabe.yu+github@gmail.com>
2019-03-11 14:04:06 +0900

    cryptsetup: add same-cpu-crypt and submit-from-crypt-cpus options
    Closes #11946.

--- src/shared/crypt-util.h
+++ src/shared/crypt-util.h
@@ -11,6 +11,14 @@
 #define CRYPT_LUKS NULL
 #endif
 
+#ifndef CRYPT_ACTIVATE_SAME_CPU_CRYPT
+#define CRYPT_ACTIVATE_SAME_CPU_CRYPT (1 << 6)
+#endif
+
+#ifndef CRYPT_ACTIVATE_SUBMIT_FROM_CRYPT_CPUS
+#define CRYPT_ACTIVATE_SUBMIT_FROM_CRYPT_CPUS (1 << 7)
+#endif
+
 DEFINE_TRIVIAL_CLEANUP_FUNC(struct crypt_device *, crypt_free);
 
 void cryptsetup_log_glue(int level, const char *msg, void *usrptr);
