bcaabf481d2f12659463e97177eb1ba700ff3988
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2019-03-16 10:57:12 +0100

    fuzz-calendarspec: actually run the second part of the fuzzer
    https://github.com/systemd/systemd/pull/11975#issuecomment-473467475

--- src/fuzz/fuzz-calendarspec.c
+++ src/fuzz/fuzz-calendarspec.c
@@ -14,7 +14,7 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
 
         str = memdup_suffix0(data, size);
 
-        if (calendar_spec_from_string(str, &cspec) > 0) {
+        if (calendar_spec_from_string(str, &cspec) >= 0) {
                 (void) calendar_spec_valid(cspec);
                 (void) calendar_spec_normalize(cspec);
                 (void) calendar_spec_to_string(cspec, &p);
