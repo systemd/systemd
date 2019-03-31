7df5c6ba904c81fd27acba480400d06110e97ab5
Yu Watanabe <watanabe.yu+github@gmail.com>
2019-03-31 22:35:44 +0900

    network: make FooOverUDP.Protocol= support name of ipproto

--- src/network/netdev/fou-tunnel.c
+++ src/network/netdev/fou-tunnel.c
@@ -5,6 +5,7 @@
 #include <linux/ip.h>
 
 #include "conf-parser.h"
+#include "ip-protocol-list.h"
 #include "missing.h"
 #include "netdev/fou-tunnel.h"
 #include "netlink-util.h"
@@ -109,6 +110,43 @@ static int netdev_fou_tunnel_create(NetDev *netdev) {
                 return log_netdev_error_errno(netdev, r, "Failed to create FooOverUDP tunnel: %m");
 
         netdev_ref(netdev);
+        return 0;
+}
+
+int config_parse_ip_protocol(
+                const char *unit,
+                const char *filename,
+                unsigned line,
+                const char *section,
+                unsigned section_line,
+                const char *lvalue,
+                int ltype,
+                const char *rvalue,
+                void *data,
+                void *userdata) {
+
+        uint8_t *protocol = data;
+        int r;
+
+        assert(filename);
+        assert(section);
+        assert(lvalue);
+        assert(rvalue);
+        assert(data);
+
+        assert_cc(IPPROTO_MAX-1 <= UINT8_MAX);
+
+        r = parse_ip_protocol(rvalue);
+        if (r < 0) {
+                r = safe_atou8(rvalue, protocol);
+                if (r < 0)
+                        log_syntax(unit, LOG_ERR, filename, line, r,
+                                   "Failed to parse IP protocol '%s' for Foo over UDP tunnel, "
+                                   "ignoring assignment: %m", rvalue);
+                return 0;
+        }
+
+        *protocol = r;
         return 0;
 }
 
