ae695a9eda65a8e9114be714e8cb5950775eb92d
Yu Watanabe <watanabe.yu+github@gmail.com>
2019-04-11 14:36:18 +0900

    arp-util: use net/ethernet.h instead of netinet/if_ether.h
    The header net/ethernet.h is used at all other places where
    'struct ether_addr' is required.

--- src/libsystemd-network/arp-util.h
+++ src/libsystemd-network/arp-util.h
@@ -5,7 +5,7 @@
   Copyright © 2014 Axis Communications AB. All rights reserved.
 ***/
 
-#include <netinet/if_ether.h>
+#include <net/ethernet.h>
 
 #include "socket-util.h"
 #include "sparse-endian.h"
