50ccd8645379e2c36f6437f6ef6ddb4d49e59342
Lennart Poettering <lennart@poettering.net>
2019-04-24 18:42:13 +0200

    fileio: add new WRITE_STRING_FILE_MKDIR_0755 flag for mkdir'ing parent

--- src/basic/fileio.c
+++ src/basic/fileio.c
@@ -21,6 +21,7 @@
 #include "log.h"
 #include "macro.h"
 #include "missing.h"
+#include "mkdir.h"
 #include "parse-util.h"
 #include "path-util.h"
 #include "stdio-util.h"
@@ -173,6 +174,12 @@ int write_string_file_ts(
 
         /* We don't know how to verify whether the file contents was already on-disk. */
         assert(!((flags & WRITE_STRING_FILE_VERIFY_ON_FAILURE) && (flags & WRITE_STRING_FILE_SYNC)));
+
+        if (flags & WRITE_STRING_FILE_MKDIR_0755) {
+                r = mkdir_parents(fn, 0755);
+                if (r < 0)
+                        return r;
+        }
 
         if (flags & WRITE_STRING_FILE_ATOMIC) {
                 assert(flags & WRITE_STRING_FILE_CREATE);
