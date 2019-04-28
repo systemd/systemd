4a4eaade6072d5358f9b8cc7e1a6fca22e7d15e6
Frantisek Sumsal <frantisek@sumsal.cz>
2019-04-28 20:43:00 +0200

    coccinelle: exclude certain paths from the transformations
    There's no point in running these transformation for certain files,
    mainly anything from src/boot/efi and src/shared/linux, as this code
    doesn't have access to our internal utility functions

--- coccinelle/run-coccinelle.sh
+++ coccinelle/run-coccinelle.sh
@@ -1,9 +1,24 @@
 #!/bin/bash -e
 
+# Exclude following paths from the Coccinelle transformations
+EXCLUDED_PATHS=(
+    "src/boot/efi/*"
+    "src/shared/linux/*"
+    "src/basic/linux/*"
+    # Symlinked to test-bus-vtable-cc.cc, which causes issues with the IN_SET macro
+    "src/libsystemd/sd-bus/test-bus-vtable.c"
+)
+
 top="$(git rev-parse --show-toplevel)"
-files="$(git ls-files ':/*.[ch]')"
 iso_defs="$top/coccinelle/systemd-definitions.iso"
 args=
+
+# Create an array from files tracked by git...
+mapfile -t files < <(git ls-files ':/*.[ch]')
+# ...and filter everything that matches patterns from EXCLUDED_PATHS
+for excl in "${EXCLUDED_PATHS[@]}"; do
+    files=(${files[@]//$excl})
+done
 
 case "$1" in
     -i)
@@ -22,7 +37,7 @@ for SCRIPT in ${@-$top/coccinelle/*.cocci} ; do
     TMPFILE=`mktemp`
     echo "+ spatch --sp-file $SCRIPT $args ..."
     parallel --halt now,fail=1 --keep-order --noswap --max-args=20 \
-             spatch --iso-file $iso_defs --sp-file $SCRIPT $args ::: $files \
+             spatch --iso-file $iso_defs --sp-file $SCRIPT $args ::: "${files[@]}" \
              2>"$TMPFILE" || cat "$TMPFILE"
     echo -e "--x-- Processed $SCRIPT --x--\n"
 done
