8c6637bab2a4a3caee28a5337f7c2fcedf43c246
Evgeny Vereshchagin <evvers@ya.ru>
2019-05-08 23:23:55 +0200

    travis: make sure the fuzzers can be built in "local" mode

--- travis-ci/managers/fuzzbuzz.sh
+++ travis-ci/managers/fuzzbuzz.sh
@@ -6,7 +6,16 @@ set -u
 
 REPO_ROOT=${REPO_ROOT:-$(pwd)}
 
+sudo apt-get update -y
+sudo apt-get build-dep systemd -y
+sudo apt-get install -y ninja-build python3-pip python3-setuptools
+pip3 install meson
+
 cd $REPO_ROOT
+tools/oss-fuzz.sh
+timeout --preserve-status 5 ./out/fuzz-unit-file
+git clean -dxff
+
 wget https://app.fuzzbuzz.io/releases/cli/latest/linux/fuzzbuzz
 chmod +x fuzzbuzz
 ./fuzzbuzz validate
