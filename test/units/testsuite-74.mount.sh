53938f78d5ea0f19753ae5b968f47d76b4f96d96
Topi Miettinen <toiwoton@gmail.com>
2019-05-08 15:17:04 +0300

    mount: handle remounting
    Detect remounting operation and change transient unit name accordingly. Allow
    remounting root directory.
    
    Closes #12493

--- test/units/testsuite-74.mount.sh
+++ test/units/testsuite-74.mount.sh
@@ -71,6 +71,10 @@ systemd-mount --options=ro,x-foo-bar "$LOOP" "$WORK_DIR/mnt"
 test -e "$WORK_DIR/mnt/foo.bar"
 systemctl show -P Options "$WORK_DIR/mnt" | grep -Eq "(^ro|,ro)"
 systemctl show -P Options "$WORK_DIR/mnt" | grep -q "x-foo-bar"
+# Change mount options with remount
+systemd-mount --remount --options=rw,x-foo-bar "$WORK_DIR/mnt"
+systemctl show -P Options "$WORK_DIR/mnt" | grep -Eq "(^rw|,rw)"
+systemctl show -P Options "$WORK_DIR/mnt" | grep -q "x-foo-bar"
 systemd-umount "$WORK_DIR/mnt"
 
 # Mount with only source set
