63dc544b6f048b68cad57b04e1ad757f983c4223
Evgeny Vereshchagin <evvers@ya.ru>
2019-05-16 17:53:57 +0200

    tests: make docker-run and helper.py happy by not using colons in filenames
    This should address https://github.com/google/oss-fuzz/issues/2428

--- travis-ci/managers/fuzzbuzz.sh
+++ travis-ci/managers/fuzzbuzz.sh
@@ -16,7 +16,13 @@ git clone https://github.com/google/oss-fuzz /tmp/oss-fuzz
 cd /tmp/oss-fuzz
 sudo ./infra/helper.py pull_images
 
+# docker doesn't like colons in filenames so let's create a directory
+# whose name can be consumed by the -v option.
+# https://github.com/google/oss-fuzz/issues/2428
+t=$(mktemp -d)
+sudo mount --bind "$REPO_ROOT" "$t"
+
 # helper.py is wrapped in script to trick it into thinking it's "interactive"
 # See https://github.com/systemd/systemd/pull/12542#issuecomment-491563572
-sudo script -e -c "./infra/helper.py build_fuzzers --clean --sanitizer=memory systemd $REPO_ROOT"
+sudo script -e -c "./infra/helper.py build_fuzzers --clean --sanitizer=memory systemd $t"
 sudo script -e -c "./infra/helper.py check_build --sanitizer=memory systemd"
