f3f0d873e2046877a72809bac922cef2eb85dd65
Yu Watanabe <watanabe.yu+github@gmail.com>
2019-06-19 21:02:47 +0900

    util: introduce siphash24_compress_boolean()

--- src/basic/siphash24.h
+++ src/basic/siphash24.h
@@ -17,6 +17,7 @@ struct siphash {
 
 void siphash24_init(struct siphash *state, const uint8_t k[static 16]);
 void siphash24_compress(const void *in, size_t inlen, struct siphash *state);
+void siphash24_compress_boolean(bool in, struct siphash *state);
 #define siphash24_compress_byte(byte, state) siphash24_compress((const uint8_t[]) { (byte) }, 1, (state))
 
 uint64_t siphash24_finalize(struct siphash *state);
