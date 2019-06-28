5fc5e2f524f5ce519214ca4cd961c71e52efc017
Yu Watanabe <watanabe.yu+github@gmail.com>
2019-06-29 03:30:53 +0900

    sd-netlink: introduce sd_netlink_message_enter_array()

--- src/systemd/sd-netlink.h
+++ src/systemd/sd-netlink.h
@@ -105,6 +105,7 @@ int sd_netlink_message_read_cache_info(sd_netlink_message *m, unsigned short typ
 int sd_netlink_message_read_in_addr(sd_netlink_message *m, unsigned short type, struct in_addr *data);
 int sd_netlink_message_read_in6_addr(sd_netlink_message *m, unsigned short type, struct in6_addr *data);
 int sd_netlink_message_enter_container(sd_netlink_message *m, unsigned short type);
+int sd_netlink_message_enter_array(sd_netlink_message *m, unsigned short type);
 int sd_netlink_message_exit_container(sd_netlink_message *m);
 
 int sd_netlink_message_open_array(sd_netlink_message *m, uint16_t type);
