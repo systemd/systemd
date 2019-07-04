412ac780acdc9ba431dc0d1a0bd0e3a81cce1f33
Yu Watanabe <watanabe.yu+github@gmail.com>
2019-07-05 05:48:21 +0900

    network: fix the initial value of the counter for brvlan
    Fixes #12958.

--- src/network/networkd-brvlan.c
+++ src/network/networkd-brvlan.c
@@ -53,7 +53,7 @@ static int append_vlan_info_data(Link *const link, sd_netlink_message *req, uint
         assert(br_vid_bitmap);
         assert(br_untagged_bitmap);
 
-        i = cnt = -1;
+        cnt = 0;
 
         begin = end = UINT16_MAX;
         for (k = 0; k < BRIDGE_VLAN_BITMAP_LEN; k++) {
@@ -129,9 +129,8 @@ static int append_vlan_info_data(Link *const link, sd_netlink_message *req, uint
                         i = j;
                 } while (!done);
         }
-        if (!cnt)
-                return -EINVAL;
 
+        assert(cnt > 0);
         return cnt;
 }
 
