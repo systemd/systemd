ad932b156c159094148bfbb508641fb818f932f1
Yu Watanabe <watanabe.yu+github@gmail.com>
2019-07-04 13:52:03 +0900

    network: introduce link_reconfigure()
    Will be used in later commits.

--- src/network/networkd-link.h
+++ src/network/networkd-link.h
@@ -204,6 +204,8 @@ uint32_t link_get_ipv6_accept_ra_route_table(Link *link);
 int link_request_set_routes(Link *link);
 int link_request_set_nexthop(Link *link);
 
+int link_reconfigure(Link *link);
+
 #define ADDRESS_FMT_VAL(address)                   \
         be32toh((address).s_addr) >> 24,           \
         (be32toh((address).s_addr) >> 16) & 0xFFu, \
