df2452c553da3004b1c3d276fa7e3de24aac330f
Ivan Shapovalov <intelfx@intelfx.name>
2019-07-06 13:47:06 +0300

    basic/log: fix SYSTEMD_LOG_* parsing error messages
    (likely a copy-paste gone wrong)

--- src/basic/log.c
+++ src/basic/log.c
@@ -1110,11 +1110,11 @@ void log_parse_environment_realm(LogRealm realm) {
 
         e = getenv("SYSTEMD_LOG_COLOR");
         if (e && log_show_color_from_string(e) < 0)
-                log_warning("Failed to parse bool '%s'. Ignoring.", e);
+                log_warning("Failed to parse log color '%s'. Ignoring.", e);
 
         e = getenv("SYSTEMD_LOG_LOCATION");
         if (e && log_show_location_from_string(e) < 0)
-                log_warning("Failed to parse bool '%s'. Ignoring.", e);
+                log_warning("Failed to parse log location '%s'. Ignoring.", e);
 }
 
 LogTarget log_get_target(void) {
