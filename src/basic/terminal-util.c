15bba61325df06bc3d56c770c5afebd64168df11
Lennart Poettering <lennart@poettering.net>
2019-07-18 16:01:05 +0200

    Revert "Add check to switch VTs only between K_XLATE or K_UNICODE"
    This reverts commit 13a43c73d8cbac4b65472de04bb88ea1bacdeb89.

--- src/basic/terminal-util.c
+++ src/basic/terminal-util.c
@@ -1278,17 +1278,10 @@ int vt_verify_kbmode(int fd) {
 }
 
 int vt_reset_keyboard(int fd) {
-        int kb, r;
+        int kb;
 
         /* If we can't read the default, then default to unicode. It's 2017 after all. */
         kb = vt_default_utf8() != 0 ? K_UNICODE : K_XLATE;
-
-        r = vt_verify_kbmode(fd);
-        if (r == -EBUSY) {
-                log_debug_errno(r, "Keyboard is not in XLATE or UNICODE mode, not resetting: %m");
-                return 0;
-        } else if (r < 0)
-                return r;
 
         if (ioctl(fd, KDSKBMODE, kb) < 0)
                 return -errno;
