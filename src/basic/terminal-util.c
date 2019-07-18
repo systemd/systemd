26382cab1b1a030f68b987fdd6cd374490f2b484
Lennart Poettering <lennart@poettering.net>
2019-07-18 16:01:07 +0200

    Revert "Move verify_vc_kbmode() to terminal-util.c as vt_verify_kbmode()"
    This reverts commit bb5ac84d79ac3aef606a4a9eeaafef94a1f199be.

--- src/basic/terminal-util.c
+++ src/basic/terminal-util.c
@@ -1260,23 +1260,6 @@ int vt_default_utf8(void) {
         return parse_boolean(b);
 }
 
-int vt_verify_kbmode(int fd) {
-        int curr_mode;
-
-        /*
-         * Make sure we only adjust consoles in K_XLATE or K_UNICODE mode.
-         * Otherwise we would (likely) interfere with X11's processing of the
-         * key events.
-         *
-         * http://lists.freedesktop.org/archives/systemd-devel/2013-February/008573.html
-         */
-
-        if (ioctl(fd, KDGKBMODE, &curr_mode) < 0)
-                return -errno;
-
-        return IN_SET(curr_mode, K_XLATE, K_UNICODE) ? 0 : -EBUSY;
-}
-
 int vt_reset_keyboard(int fd) {
         int kb;
 
