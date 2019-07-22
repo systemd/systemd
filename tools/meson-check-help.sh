cdfa3f0c76f733b34cae75a277ba621716b33f2f
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2019-07-22 10:59:19 +0200

    meson: catch programs which ignore arguments
    Don't try to use <<<. It seems to not work properly with old bash
    versions or something.

--- tools/meson-check-help.sh
+++ tools/meson-check-help.sh
@@ -10,14 +10,20 @@ if "$1"  --help | grep -v 'default:' | grep -E -q '.{80}.'; then
     exit 1
 fi
 
+# --help prints something. Also catches case where args are ignored.
+if ! "$1"  --help | grep -q .; then
+    echo "$(basename "$1") --help output is empty."
+    exit 2
+fi
+
 # no --help output to stdout
 if "$1" --help 2>&1 1>/dev/null | grep .; then
     echo "$(basename "$1") --help prints to stderr"
-    exit 2
+    exit 3
 fi
 
 # error output to stderr
 if ! "$1" --no-such-parameter 2>&1 1>/dev/null | grep -q .; then
     echo "$(basename "$1") with an unknown parameter does not print to stderr"
-    exit 3
+    exit 4
 fi
