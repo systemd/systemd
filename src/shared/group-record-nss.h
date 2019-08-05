9b2d907877ab10202fdfb7db7a5ce6e8b32c70b8
Lennart Poettering <lennart@poettering.net>
2019-08-05 18:21:30 +0200

    shared: add helpers for converting NSS passwd/group structures to new JSON objects
    These new calls may be used to convert classic UNIX/glibc NSS struct
    passwd and struct group records into new-style JSON-based user/group
    objects.

---
+++ src/shared/group-record-nss.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: LGPL-2.1+ */
+#pragma once
+
+#include <grp.h>
+#include <gshadow.h>
+
+#include "group-record.h"
+
+/* Synthesize GroupRecord objects from NSS data */
+
+int nss_group_to_group_record(const struct group *grp, const struct sgrp *sgrp, GroupRecord **ret);
+int nss_sgrp_for_group(const struct group *grp, struct sgrp *ret_sgrp, char **ret_buffer);
+
+int nss_group_record_by_name(const char *name, GroupRecord **ret);
+int nss_group_record_by_gid(gid_t gid, GroupRecord **ret);
