4445c4d43a63dea4b9c8af76876fe67683d1b679
Ronan Pigott <rpigott@berkeley.edu>
2019-08-05 19:07:31 -0700

    systemctl: Add unit file states to state help message

--- src/systemctl/systemctl.c
+++ src/systemctl/systemctl.c
@@ -83,6 +83,7 @@
 #include "terminal-util.h"
 #include "tmpfile-util.h"
 #include "unit-def.h"
+#include "unit-file.h"
 #include "unit-name.h"
 #include "user-util.h"
 #include "utf8.h"
@@ -7902,6 +7903,10 @@ static void help_states(void) {
         if (!arg_no_legend)
                 puts("\nAvailable unit active states:");
         DUMP_STRING_TABLE(unit_active_state, UnitActiveState, _UNIT_ACTIVE_STATE_MAX);
+
+        if (!arg_no_legend)
+                puts("\nAvailable unit file states:");
+        DUMP_STRING_TABLE(unit_file_state, UnitFileState, _UNIT_FILE_STATE_MAX);
 
         if (!arg_no_legend)
                 puts("\nAvailable automount unit substates:");
