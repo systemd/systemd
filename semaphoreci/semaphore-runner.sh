f9b14148b0761b8bb9de63a999906cb832ce0982
Evgeny Vereshchagin <evvers@ya.ru>
2019-08-24 02:51:53 +0000

    semaphore: switch back to the master branch

--- semaphoreci/semaphore-runner.sh
+++ semaphoreci/semaphore-runner.sh
@@ -5,7 +5,7 @@ set -eux
 # default to Debian testing
 DISTRO=${DISTRO:-debian}
 RELEASE=${RELEASE:-buster}
-BRANCH=${BRANCH:-experimental}
+BRANCH=${BRANCH:-master}
 ARCH=${ARCH:-amd64}
 CONTAINER=${RELEASE}-${ARCH}
 CACHE_DIR=${SEMAPHORE_CACHE_DIR:=/tmp}
