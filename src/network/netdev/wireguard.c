0bae857564e5189bed600f0a243d96b81b5dcb09
Yu Watanabe <watanabe.yu+github@gmail.com>
2019-09-11 18:09:55 +0900

    network: also check the permission of key file

--- src/network/netdev/wireguard.c
+++ src/network/netdev/wireguard.c
@@ -901,6 +901,8 @@ static int wireguard_read_key_file(const char *filename, uint8_t dest[static WG_
 
         assert(dest);
 
+        (void) warn_file_is_world_accessible(filename, NULL, NULL, 0);
+
         r = read_full_file_full(filename, READ_FULL_FILE_SECURE | READ_FULL_FILE_UNBASE64, &key, &key_len);
         if (r < 0)
                 return r;
