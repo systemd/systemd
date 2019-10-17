e4a524c051ae4df4dc60ec4da03939761732ace5
Jonas Jelten <jj@sft.mx>
2019-10-17 12:10:13 +0200

    ceph is a network filesystem

--- src/basic/mountpoint-util.c
+++ src/basic/mountpoint-util.c
@@ -297,6 +297,7 @@ bool fstype_is_network(const char *fstype) {
 
         return STR_IN_SET(fstype,
                           "afs",
+                          "ceph",
                           "cifs",
                           "smb3",
                           "smbfs",
