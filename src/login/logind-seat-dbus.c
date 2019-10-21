8cc64c2a3640121745fdfaccc6eae896ac25a911
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2019-10-21 15:05:58 +0200

    logind: fix emission of PropertiesChanged on seats
    The story is the same as in 471cffcfb0e005b7c4044b3b52cc4f25d217efac:
    device_attach() → seat_send_changed() → sd_bus_emit_properties_changed_strv()
    → emit_properties_changed_on_interface() → node_vtable_get_userdata()
    → seat_object_find(), which returns 0 because message == NULL.
    But when we are emitting a signal, message is always NULL. Removing the
    overeager check and assert in the called function allow the signal to be
    emitted.
    
    Fixes #13769.

--- src/login/logind-seat-dbus.c
+++ src/login/logind-seat-dbus.c
@@ -280,8 +280,6 @@ int seat_object_find(sd_bus *bus, const char *path, const char *interface, void 
                 return -ENOMEM;
 
         message = sd_bus_get_current_message(bus);
-        if (!message)
-                return 0;
 
         r = manager_get_seat_from_creds(m, message, e, error, &seat);
         if (r == -ENXIO) {
