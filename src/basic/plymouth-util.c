048f9da1a4b3bc6d7eecf7ebe3625febb1f278e6
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2019-10-23 22:56:24 +0200

    Remove unused plymouth_running() function
    (cherry picked from commit 8a2c82ac882f4adfb0f7bdc039c84e880ddd689a)

--- src/basic/plymouth-util.c
+++
@@ -1,9 +0,0 @@
-/* SPDX-License-Identifier: LGPL-2.1+ */
-
-#include <unistd.h>
-
-#include "plymouth-util.h"
-
-bool plymouth_running(void) {
-        return access("/run/plymouth/pid", F_OK) >= 0;
-}
