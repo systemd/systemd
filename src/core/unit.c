29a743f99346ca0eae0fbf7389307bad873fb1be
Franck Bui <fbui@suse.com>
2019-11-26 11:33:08 +0100

    core: explicit mention of unit ID is redundant with log_unit_*()

--- src/core/unit.c
+++ src/core/unit.c
@@ -5318,7 +5318,7 @@ static void unit_update_dependency_mask(Unit *u, UnitDependency d, Unit *other, 
         if (di.origin_mask == 0 && di.destination_mask == 0) {
                 /* No bit set anymore, let's drop the whole entry */
                 assert_se(hashmap_remove(u->dependencies[d], other));
-                log_unit_debug(u, "%s lost dependency %s=%s", u->id, unit_dependency_to_string(d), other->id);
+                log_unit_debug(u, "lost dependency %s=%s", unit_dependency_to_string(d), other->id);
         } else
                 /* Mask was reduced, let's update the entry */
                 assert_se(hashmap_update(u->dependencies[d], other, di.data) == 0);
