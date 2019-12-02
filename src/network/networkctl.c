systemd/systemd

commit 78404d22cca9cbbc8adb9dd7248da2f2725b1dbc

network: support matching based on wifi interfece type

src/network/networkctl.c
@@ -167,6 +167,7 @@ typedef struct LinkInfo {
         NetDevPort port;
 
         /* wlan info */
+        enum nl80211_iftype wlan_iftype;
         char *ssid;
         struct ether_addr bssid;
 
@@ -384,7 +385,7 @@ static void acquire_ether_link_info(int *fd, LinkInfo *link) {
 static void acquire_wlan_link_info(LinkInfo *link) {
         _cleanup_(sd_netlink_unrefp) sd_netlink *genl = NULL;
         const char *type = NULL;
-        int r, k;
+        int r, k = 0;
 
         if (link->sd_device)
                 (void) sd_device_get_devtype(link->sd_device, &type);
@@ -399,13 +400,15 @@ static void acquire_wlan_link_info(LinkInfo *link) {
 
         (void) sd_netlink_inc_rcvbuf(genl, RCVBUF_SIZE);
 
-        r = wifi_get_ssid(genl, link->ifindex, &link->ssid);
+        r = wifi_get_interface(genl, link->ifindex, &link->wlan_iftype, &link->ssid);
         if (r < 0)
                 log_debug_errno(r, "%s: failed to query ssid: %m", link->name);
 
-        k = wifi_get_bssid(genl, link->ifindex, &link->bssid);
-        if (k < 0)
-                log_debug_errno(k, "%s: failed to query bssid: %m", link->name);
+        if (link->iftype == NL80211_IFTYPE_STATION) {
+                k = wifi_get_station(genl, link->ifindex, &link->bssid);
+                if (k < 0)
+                        log_debug_errno(k, "%s: failed to query bssid: %m", link->name);
+        }
 
         link->has_wlan_link_info = r > 0 || k > 0;
 }

man/systemd.network.xml
@@ -154,6 +154,18 @@
           </listitem>
         </varlistentry>
         <varlistentry>
+          <term><varname>WirelessType=</varname></term>
+          <listitem>
+            <para>A whitespace-separated list of wireless network type. Supported values are
+            <literal>ad-hoc</literal>, <literal>station</literal>, <literal>ap</literal>,
+            <literal>ap-vlan</literal>, <literal>wds</literal>, <literal>monitor</literal>,
+            <literal>mesh-point</literal>, <literal>p2p-client</literal>, <literal>p2p-go</literal>,
+            <literal>p2p-device</literal>, <literal>ocb</literal>, and <literal>nan</literal>. If the
+            list is prefixed with a "!", the test is inverted.
+            </para>
+          </listitem>
+        </varlistentry>
+        <varlistentry>
           <term><varname>SSID=</varname></term>
           <listitem>
             <para>A whitespace-separated list of shell-style globs matching the SSID of the currently

src/libsystemd-network/network-internal.c
@@ -20,6 +20,7 @@
 #include "parse-util.h"
 #include "siphash24.h"
 #include "socket-util.h"
+#include "string-table.h"
 #include "string-util.h"
 #include "strv.h"
 #include "utf8.h"
@@ -136,17 +137,36 @@ static int net_condition_test_property(char * const *match_property, sd_device *
         return true;
 }
 
+static const char *const wifi_iftype_table[NL80211_IFTYPE_MAX+1] = {
+        [NL80211_IFTYPE_ADHOC] = "ad-hoc",
+        [NL80211_IFTYPE_STATION] = "station",
+        [NL80211_IFTYPE_AP] = "ap",
+        [NL80211_IFTYPE_AP_VLAN] = "ap-vlan",
+        [NL80211_IFTYPE_WDS] = "wds",
+        [NL80211_IFTYPE_MONITOR] = "monitor",
+        [NL80211_IFTYPE_MESH_POINT] = "mesh-point",
+        [NL80211_IFTYPE_P2P_CLIENT] = "p2p-client",
+        [NL80211_IFTYPE_P2P_GO] = "p2p-go",
+        [NL80211_IFTYPE_P2P_DEVICE] = "p2p-device",
+        [NL80211_IFTYPE_OCB] = "ocb",
+        [NL80211_IFTYPE_NAN] = "nan",
+};
+
+DEFINE_PRIVATE_STRING_TABLE_LOOKUP_TO_STRING(wifi_iftype, enum nl80211_iftype);
+
 bool net_match_config(Set *match_mac,
                       char * const *match_paths,
                       char * const *match_drivers,
                       char * const *match_types,
                       char * const *match_names,
                       char * const *match_property,
+                      char * const *match_wifi_iftype,
                       char * const *match_ssid,
                       Set *match_bssid,
                       sd_device *device,
                       const struct ether_addr *dev_mac,
                       const char *dev_name,
+                      enum nl80211_iftype wifi_iftype,
                       const char *ssid,
                       const struct ether_addr *bssid) {
 
@@ -180,6 +200,9 @@ bool net_match_config(Set *match_mac,
                 return false;
 
         if (!net_condition_test_property(match_property, device))
+                return false;
+
+        if (!net_condition_test_strv(match_wifi_iftype, wifi_iftype_to_string(wifi_iftype)))
                 return false;
 
         if (!net_condition_test_strv(match_ssid, ssid))

src/libsystemd-network/network-internal.h
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: LGPL-2.1+ */
 #pragma once
 
+#include <linux/nl80211.h>
 #include <stdbool.h>
 
 #include "sd-device.h"
@@ -20,11 +21,13 @@ bool net_match_config(Set *match_mac,
                       char * const *match_type,
                       char * const *match_name,
                       char * const *match_property,
+                      char * const *match_wifi_iftype,
                       char * const *match_ssid,
                       Set *match_bssid,
                       sd_device *device,
                       const struct ether_addr *dev_mac,
                       const char *dev_name,
+                      enum nl80211_iftype wifi_iftype,
                       const char *ssid,
                       const struct ether_addr *bssid);
 

src/libsystemd/sd-netlink/netlink-types.c
@@ -993,6 +993,7 @@ static const NLType genl_nl80211_types[] = {
         [NL80211_ATTR_IFINDEX] = { .type = NETLINK_TYPE_U32 },
         [NL80211_ATTR_MAC]     = { .type = NETLINK_TYPE_ETHER_ADDR },
         [NL80211_ATTR_SSID]    = { .type = NETLINK_TYPE_STRING },
+        [NL80211_ATTR_IFTYPE]  = { .type = NETLINK_TYPE_U32 },
 };
 
 static const NLTypeSystem genl_nl80211_type_system = {

src/network/networkd-link.c
@@ -2865,7 +2865,7 @@ int link_reconfigure(Link *link, bool force) {
                 return 0;
 
         r = network_get(link->manager, link->sd_device, link->ifname,
-                        &link->mac, link->ssid, &link->bssid, &network);
+                        &link->mac, link->wlan_iftype, link->ssid, &link->bssid, &network);
         if (r == -ENOENT) {
                 link_enter_unmanaged(link);
                 return 0;
@@ -2959,7 +2959,7 @@ static int link_initialized_and_synced(Link *link) {
                         return r;
 
                 r = network_get(link->manager, link->sd_device, link->ifname,
-                                &link->mac, link->ssid, &link->bssid, &network);
+                                &link->mac, link->wlan_iftype, link->ssid, &link->bssid, &network);
                 if (r == -ENOENT) {
                         link_enter_unmanaged(link);
                         return 0;

src/network/networkd-link.h
@@ -2,6 +2,7 @@
 #pragma once
 
 #include <endian.h>
+#include <linux/nl80211.h>
 
 #include "sd-bus.h"
 #include "sd-device.h"
@@ -56,6 +57,7 @@ typedef struct Link {
         sd_device *sd_device;
 
         /* wlan */
+        enum nl80211_iftype wlan_iftype;
         char *ssid;
         struct ether_addr bssid;
 

src/network/networkd-network-gperf.gperf
@@ -29,6 +29,7 @@ Match.MACAddress,                       config_parse_hwaddrs,                   
 Match.Path,                             config_parse_match_strv,                         0,                             offsetof(Network, match_path)
 Match.Driver,                           config_parse_match_strv,                         0,                             offsetof(Network, match_driver)
 Match.Type,                             config_parse_match_strv,                         0,                             offsetof(Network, match_type)
+Match.WLANInterfaceType,                config_parse_match_strv,                         0,                             offsetof(Network, match_wlan_iftype)
 Match.SSID,                             config_parse_match_strv,                         0,                             offsetof(Network, match_ssid)
 Match.BSSID,                            config_parse_hwaddrs,                            0,                             offsetof(Network, match_bssid)
 Match.Name,                             config_parse_match_ifnames,                      0,                             offsetof(Network, match_name)

commit d8b7029a001468fea3247fee310e2d5555327a9d

networkctl: fix to show BSSID
This fixes an issue caused by a typo in
78404d22cca9cbbc8adb9dd7248da2f2725b1dbc.

src/network/networkctl.c
@@ -404,7 +404,7 @@ static void acquire_wlan_link_info(LinkInfo *link) {
         if (r < 0)
                 log_debug_errno(r, "%s: failed to query ssid: %m", link->name);
 
-        if (link->iftype == NL80211_IFTYPE_STATION) {
+        if (link->wlan_iftype == NL80211_IFTYPE_STATION) {
                 k = wifi_get_station(genl, link->ifindex, &link->bssid);
                 if (k < 0)
                         log_debug_errno(k, "%s: failed to query bssid: %m", link->name);