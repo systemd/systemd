3ac189d8aa3ca08e2ad76426f86e8135b2b45d95
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2019-12-09 20:53:38 +0100

    test: move TEST-11-ISSUE-3166 setup to static files

--- test/TEST-11-ISSUE-3166/test.sh
+++ test/TEST-11-ISSUE-3166/test.sh
@@ -15,48 +15,7 @@ test_setup() {
 
         setup_basic_environment
         mask_supporting_services
-        dracut_install false touch
-
-        # setup the testsuite service
-        cat >$initdir/etc/systemd/system/testsuite.service <<EOF
-[Unit]
-Description=Testsuite service
-
-[Service]
-ExecStart=/test-fail-on-restart.sh
-Type=oneshot
-EOF
-
-        cat >$initdir/etc/systemd/system/fail-on-restart.service <<EOF
-[Unit]
-Description=Fail on restart
-StartLimitIntervalSec=1m
-StartLimitBurst=3
-
-[Service]
-Type=simple
-ExecStart=/bin/false
-Restart=always
-EOF
-
-
-        cat >$initdir/test-fail-on-restart.sh <<'EOF'
-#!/usr/bin/env bash
-set -x
-
-systemctl start fail-on-restart.service
-active_state=$(systemctl show --property ActiveState fail-on-restart.service)
-while [[ "$active_state" == "ActiveState=activating" || "$active_state" == "ActiveState=active" ]]; do
-    sleep 1
-    active_state=$(systemctl show --property ActiveState fail-on-restart.service)
-done
-systemctl is-failed fail-on-restart.service || exit 1
-touch /testok
-EOF
-
-        chmod 0755 $initdir/test-fail-on-restart.sh
-        setup_testsuite
     )
 }
 
-do_test "$@"
+do_test "$@" 11
