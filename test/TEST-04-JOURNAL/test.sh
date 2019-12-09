9901a6ad2e18420d65c8b3628f605b2ef8b86ac7
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2019-12-09 19:19:05 +0100

    test: convert TEST-04-JOURNAL to generic image

--- test/TEST-04-JOURNAL/test.sh
+++ test/TEST-04-JOURNAL/test.sh
@@ -14,31 +14,8 @@ test_setup() {
 
         setup_basic_environment
         mask_supporting_services
-
-        # setup the testsuite service
-        cat >$initdir/etc/systemd/system/testsuite.service <<EOF
-[Unit]
-Description=Testsuite service
-
-[Service]
-ExecStart=/test-journal.sh
-Type=oneshot
-EOF
-
-        cat >$initdir/etc/systemd/system/forever-print-hola.service <<EOF
-[Unit]
-Description=ForeverPrintHola service
-
-[Service]
-Type=simple
-ExecStart=/bin/sh -x -c 'while :; do printf "Hola\n" || touch /i-lose-my-logs; sleep 1; done'
-EOF
-
-        cp test-journal.sh $initdir/
-
-        setup_testsuite
     )
     setup_nspawn_root
 }
 
-do_test "$@"
+do_test "$@" 04
