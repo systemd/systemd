8c6d58f65d94f081a7e869383facaee4980400ac
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2019-12-09 20:42:48 +0100

    test: move TEST-10-ISSUE-2467 setup to static files

--- test/TEST-10-ISSUE-2467/test.sh
+++ test/TEST-10-ISSUE-2467/test.sh
@@ -14,35 +14,8 @@ test_setup() {
 
         setup_basic_environment
         mask_supporting_services
-        dracut_install true rm socat
-
-        # setup the testsuite service
-        cat >$initdir/etc/systemd/system/testsuite.service <<'EOF'
-[Unit]
-Description=Testsuite service
-
-[Service]
-Type=oneshot
-ExecStart=/bin/sh -e -x -c 'rm -f /tmp/nonexistent; systemctl start test.socket; printf x > test.file; socat -t20 OPEN:test.file UNIX-CONNECT:/run/test.ctl; >/testok'
-EOF
-
-	cat  >$initdir/etc/systemd/system/test.socket <<'EOF'
-[Socket]
-ListenStream=/run/test.ctl
-EOF
-
-	cat > $initdir/etc/systemd/system/test.service <<'EOF'
-[Unit]
-Requires=test.socket
-ConditionPathExistsGlob=/tmp/nonexistent
-
-[Service]
-ExecStart=/bin/true
-EOF
-
-        setup_testsuite
     )
     setup_nspawn_root
 }
 
-do_test "$@"
+do_test "$@" 10
