c8937bed08701875e4c1540ffb28a0d34243b94c
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2019-12-10 09:43:14 +0100

    test: move TEST-32-OOMPOLICY setup to static files

--- test/units/testsuite-32.sh
+++ test/units/testsuite-32.sh
@@ -8,17 +8,19 @@ set -o pipefail
 # an easier thing to test for, and also: let's not get confused by older
 # kernels where the concept was still new.
 
-if test -f /sys/fs/cgroup/system.slice/testsuite.service/memory.oom.group ; then
+if test -f /sys/fs/cgroup/system.slice/testsuite-32.service/memory.oom.group; then
 
     systemd-analyze log-level debug
     systemd-analyze log-target console
 
     # Run a service that is guaranteed to be the first candidate for OOM killing
-    systemd-run --unit=oomtest.service -p Type=exec -p OOMScoreAdjust=1000 -p OOMPolicy=stop -p MemoryAccounting=yes /bin/sleep infinity
+    systemd-run --unit=oomtest.service \
+                -p Type=exec -p OOMScoreAdjust=1000 -p OOMPolicy=stop -p MemoryAccounting=yes \
+                sleep infinity
 
     # Trigger an OOM killer run
-    echo 1 > /proc/sys/kernel/sysrq
-    echo f > /proc/sysrq-trigger
+    echo 1 >/proc/sys/kernel/sysrq
+    echo f >/proc/sysrq-trigger
 
     while : ; do
         STATE=`systemctl show -p ActiveState --value oomtest.service`
@@ -32,6 +34,6 @@ if test -f /sys/fs/cgroup/system.slice/testsuite.service/memory.oom.group ; then
     systemd-analyze log-level info
 fi
 
-echo OK > /testok
+echo OK >/testok
 
 exit 0
