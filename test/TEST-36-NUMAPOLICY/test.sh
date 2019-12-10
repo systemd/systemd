097d8c6eb7909d9ecd49562855bb8c9a9e59cefb
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2019-12-10 09:54:36 +0100

    test: move TEST-36-NUMAPOLICY setup to static files
    No need to call setup_nspawn_root() if we plan to only run under qemu.

--- test/TEST-36-NUMAPOLICY/test.sh
+++ test/TEST-36-NUMAPOLICY/test.sh
@@ -16,22 +16,7 @@ test_setup() {
 
         setup_basic_environment
         mask_supporting_services
-        dracut_install mktemp
-
-        # setup the testsuite service
-        cat >$initdir/etc/systemd/system/testsuite.service <<EOF
-[Unit]
-Description=Testsuite service
-
-[Service]
-ExecStart=/bin/bash -x /testsuite.sh
-Type=oneshot
-EOF
-        cp testsuite.sh $initdir/
-
-        setup_testsuite
     )
-    setup_nspawn_root
 }
 
-do_test "$@"
+do_test "$@" 36
