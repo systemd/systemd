7eeeab205d8066d3516b2bc552ff87a72eab824e
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2019-12-10 13:45:48 +0100

    test: move TEST-30-ONCLOCKCHANGE setup to static files
    The two timezone files are now installed in the global setup. I am not too
    happy about this, but it still seems better than to create a completely
    separate image just for this.

--- test/TEST-30-ONCLOCKCHANGE/test.sh
+++ test/TEST-30-ONCLOCKCHANGE/test.sh
@@ -12,32 +12,9 @@ test_setup() {
         LOG_LEVEL=5
         eval $(udevadm info --export --query=env --name=${LOOPDEV}p2)
 
-        inst_any /usr/share/zoneinfo/Europe/Kiev
-        inst_any /usr/share/zoneinfo/Europe/Berlin
-
         setup_basic_environment
         mask_supporting_services
-
-        # extend the watchdog
-        mkdir -p $initdir/etc/systemd/system/systemd-timedated.service.d
-        cat >$initdir/etc/systemd/system/systemd-timedated.service.d/watchdog.conf <<EOF
-[Service]
-WatchdogSec=10min
-EOF
-
-        # setup the testsuite service
-        cat >$initdir/etc/systemd/system/testsuite.service <<EOF
-[Unit]
-Description=Testsuite service
-
-[Service]
-ExecStart=/testsuite.sh
-Type=oneshot
-EOF
-        cp testsuite.sh $initdir/
-
-        setup_testsuite
     )
 }
 
-do_test "$@"
+do_test "$@" 30
