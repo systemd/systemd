50b836518c1f031a33b255f6684ded0422d5a8ef
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2019-12-12 08:35:20 +0100

    test: drop cargo-cult line
    p2 is only used in the cryptsetup test...

--- test/TEST-08-ISSUE-2730/test.sh
+++ test/TEST-08-ISSUE-2730/test.sh
@@ -13,8 +13,6 @@ test_setup() {
     # Create what will eventually be our root filesystem onto an overlay
     (
         LOG_LEVEL=5
-        eval $(udevadm info --export --query=env --name=${LOOPDEV}p2)
-
         setup_basic_environment
 
         rm $initdir/etc/fstab
--- test/TEST-14-MACHINE-ID/test.sh
+++ test/TEST-14-MACHINE-ID/test.sh
@@ -11,8 +11,6 @@ test_setup() {
     # Create what will eventually be our root filesystem onto an overlay
     (
         LOG_LEVEL=5
-        eval $(udevadm info --export --query=env --name=${LOOPDEV}p2)
-
         setup_basic_environment
         mask_supporting_services
         printf "556f48e837bc4424a710fa2e2c9d3e3c\ne3d\n" >$initdir/etc/machine-id
