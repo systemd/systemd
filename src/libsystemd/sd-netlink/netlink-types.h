eb34f4b3d2a9e111b9f13429128af49ed16abbc6
Yu Watanabe <watanabe.yu+github@gmail.com>
2019-12-12 16:21:49 +0900

    sd-netlink: add attributes for FQ

--- src/libsystemd/sd-netlink/netlink-types.h
+++ src/libsystemd/sd-netlink/netlink-types.h
@@ -92,8 +92,9 @@ const char *nl_union_link_info_data_to_string(NLUnionLinkInfoData p) _const_;
 NLUnionLinkInfoData nl_union_link_info_data_from_string(const char *p) _pure_;
 
 typedef enum NLUnionTCAOptionData {
-        NL_UNION_TCA_OPTION_DATA_TBF,
+        NL_UNION_TCA_OPTION_DATA_FQ,
         NL_UNION_TCA_OPTION_DATA_FQ_CODEL,
+        NL_UNION_TCA_OPTION_DATA_TBF,
         _NL_UNION_TCA_OPTION_DATA_MAX,
         _NL_UNION_TCA_OPTION_DATA_INVALID = -1,
 } NLUnionTCAOptionData;
