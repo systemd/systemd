9d84eb2053afe9ba7df14007d7d982d742601893
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2019-12-17 12:52:35 +0100

    test/TEST-01: allow running w/o -Dinstall-tests=true
    This is useful for sanitizer builds under CI, see
    https://github.com/systemd/systemd-centos-ci/pull/202#issuecomment-566476592.

--- test/TEST-01-BASIC/test.sh
+++ test/TEST-01-BASIC/test.sh
@@ -3,6 +3,7 @@ set -e
 TEST_DESCRIPTION="Basic systemd setup"
 IMAGE_NAME="basic"
 RUN_IN_UNPRIVILEGED_CONTAINER=${RUN_IN_UNPRIVILEGED_CONTAINER:-yes}
+TEST_REQUIRE_INSTALL_TESTS=0
 
 . $TEST_BASE_DIR/test-functions
 
@@ -13,6 +14,10 @@ test_create_image() {
     (
         LOG_LEVEL=5
         setup_basic_environment
+
+        # install tests manually so the test is functional even when -Dinstall-tests=false
+        mkdir -p $initdir/usr/lib/systemd/tests/testdata/units/
+        cp -v $(dirname $0)/../units/{testsuite-01,end}.service $initdir/usr/lib/systemd/tests/testdata/units/
     )
     setup_nspawn_root
 }
