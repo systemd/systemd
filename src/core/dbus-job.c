4fd9bb36236634280d7a1095942c9f06f1b3dcde
Christian Göttsche <cgzones@googlemail.com>
2019-12-18 14:57:10 +0100

    core: add notes to D-Bus interfaces about adding SELinux checks

--- src/core/dbus-job.c
+++ src/core/dbus-job.c
@@ -121,6 +121,9 @@ int bus_job_method_get_waiting_jobs(sd_bus_message *message, void *userdata, sd_
         return sd_bus_send(NULL, reply, NULL);
 }
 
+/* Note: when adding a SD_BUS_WRITABLE_PROPERTY or SD_BUS_METHOD add a TODO(selinux),
+ *       so the SELinux people can add a permission check.
+ */
 const sd_bus_vtable bus_job_vtable[] = {
         SD_BUS_VTABLE_START(0),
 
