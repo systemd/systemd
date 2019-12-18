ea45b51350d39147ca710af0598e02553aedc4f2
Christian Göttsche <cgzones@googlemail.com>
2019-12-18 14:57:10 +0100

    core: add notes to D-Bus interfaces about adding SELinux checks

--- src/core/dbus-socket.c
+++ src/core/dbus-socket.c
@@ -76,6 +76,9 @@ static int property_get_listen(
         return sd_bus_message_close_container(reply);
 }
 
+/* Note: when adding a SD_BUS_WRITABLE_PROPERTY or SD_BUS_METHOD add a TODO(selinux),
+ *       so the SELinux people can add a permission check.
+ */
 const sd_bus_vtable bus_socket_vtable[] = {
         SD_BUS_VTABLE_START(0),
         SD_BUS_PROPERTY("BindIPv6Only", "s", property_get_bind_ipv6_only, offsetof(Socket, bind_ipv6_only), SD_BUS_VTABLE_PROPERTY_CONST),
--- src/core/dbus-target.c
+++ src/core/dbus-target.c
@@ -3,6 +3,9 @@
 #include "dbus-target.h"
 #include "unit.h"
 
+/* Note: when adding a SD_BUS_WRITABLE_PROPERTY or SD_BUS_METHOD add a TODO(selinux),
+ *       so the SELinux people can add a permission check.
+ */
 const sd_bus_vtable bus_target_vtable[] = {
         SD_BUS_VTABLE_START(0),
         SD_BUS_VTABLE_END
