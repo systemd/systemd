a827c41851d9a065823c9fa444944952c11969ac
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2020-01-16 15:53:57 +0100

    shared/sysctl-util: normalize repeated slashes or dots to a single value
    We use those strings as hash keys. While writing "a...b" looks strange,
    "a///b" does not look so strange. Both syntaxes would actually result in the
    value being correctly written to the file, but they would confuse our
    de-deplication over keys. So let's normalize. Output also becomes nicer.
    
    Add test.
    
    (cherry picked from commit f3b136a4847a0993e2dc1197779160dca4da6dac)
    (cherry picked from commit c2e304681929fea79ce8e9c5a1e00cd2f293a72d)

---
+++ src/test/test-sysctl-util.c
@@ -0,0 +1,44 @@
+/* SPDX-License-Identifier: LGPL-2.1+ */
+
+#include "strv.h"
+#include "sysctl-util.h"
+#include "tests.h"
+
+static const char* cases[] = {
+        "a.b.c", "a/b/c",
+        "a/b/c", "a/b/c",
+        "a/b.c/d", "a/b.c/d",
+        "a.b/c.d", "a/b.c/d",
+
+        "net.ipv4.conf.enp3s0/200.forwarding", "net/ipv4/conf/enp3s0.200/forwarding",
+        "net/ipv4/conf/enp3s0.200/forwarding", "net/ipv4/conf/enp3s0.200/forwarding",
+
+        "a...b...c", "a/b/c",
+        "a///b///c", "a/b/c",
+        ".a...b...c", "a/b/c",
+        "/a///b///c", "a/b/c",
+        NULL,
+};
+
+static void test_sysctl_normalize(void) {
+        log_info("/* %s */", __func__);
+
+        const char **s, **expected;
+        STRV_FOREACH_PAIR(s, expected, cases) {
+                _cleanup_free_ char *t;
+
+                assert_se(t = strdup(*s));
+                assert_se(sysctl_normalize(t) == t);
+
+                log_info("\"%s\" → \"%s\", expected \"%s\"", *s, t, *expected);
+                assert_se(streq(t, *expected));
+        }
+}
+
+int main(int argc, char *argv[]) {
+        test_setup_logging(LOG_INFO);
+
+        test_sysctl_normalize();
+
+        return 0;
+}
