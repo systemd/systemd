a3e42c468fc18e6713622b7e4af5d45c5887e54c
Frantisek Sumsal <frantisek@sumsal.cz>
2020-01-25 17:48:23 +0100

    test: unpin meson from v0.52.1
    The compatibility issue in meson v0.53 has been fixed in v0.53.1, which
    is already available through pip, so let's remove the pin for meson
    introduced before.
    
    Reverts: 514793658c499821383bdc8bbf1d33048c18dd40

--- travis-ci/managers/fuzzbuzz.sh
+++ travis-ci/managers/fuzzbuzz.sh
@@ -12,9 +12,7 @@ sudo apt-get build-dep systemd -y
 sudo apt-get install -y ninja-build python3-pip python3-setuptools quota
 # The following should be dropped when debian packaging has been updated to include them
 sudo apt-get install -y libfdisk-dev libp11-kit-dev libssl-dev
-# FIXME: temporarily pin the meson version as 0.53 doesn't work with older python 3.5
-# See: https://github.com/mesonbuild/meson/issues/6427
-pip3 install meson==0.52.1
+pip3 install meson
 
 cd $REPO_ROOT
 export PATH="$HOME/.local/bin/:$PATH"
