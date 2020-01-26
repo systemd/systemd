f6a5c02d26b1687bdc34ed595c64b7c7c24b6525
Yu Watanabe <watanabe.yu+github@gmail.com>
2020-01-26 13:05:13 +0900

    bootspec: parse random-seed-mode line in loader.conf
    Fixes #14657.
    
    (cherry picked from commit fe5a698f7646735335a97cc429ebe5d79f67fb70)

--- src/shared/bootspec.h
+++ src/shared/bootspec.h
@@ -42,6 +42,7 @@ typedef struct BootConfig {
         char *auto_entries;
         char *auto_firmware;
         char *console_mode;
+        char *random_seed_mode;
 
         char *entry_oneshot;
         char *entry_default;
