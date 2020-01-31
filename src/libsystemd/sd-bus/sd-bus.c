8a0e3305133460aae7a9810204bc19605c27ccb5
Jan Synacek <jsynacek@redhat.com>
2020-01-31 11:34:45 +0100

    sd-bus: introduce API for re-enqueuing incoming messages
    When authorizing via PolicyKit we want to process incoming method calls
    twice: once to process and figure out that we need PK authentication,
    and a second time after we aquired PK authentication to actually execute
    the operation. With this new call sd_bus_enqueue_for_read() we have a
    way to put an incoming message back into the read queue for this
    purpose.
    
    This might have other uses too, for example debugging.
    Related: CVE-2020-1712

--- src/libsystemd/sd-bus/sd-bus.c
+++ src/libsystemd/sd-bus/sd-bus.c
@@ -4076,3 +4076,27 @@ _public_ int sd_bus_get_n_queued_write(sd_bus *bus, uint64_t *ret) {
         *ret = bus->wqueue_size;
         return 0;
 }
+
+_public_ int sd_bus_enqueue_for_read(sd_bus *bus, sd_bus_message *m) {
+        int r;
+
+        assert_return(bus, -EINVAL);
+        assert_return(bus = bus_resolve(bus), -ENOPKG);
+        assert_return(m, -EINVAL);
+        assert_return(m->sealed, -EINVAL);
+        assert_return(!bus_pid_changed(bus), -ECHILD);
+
+        if (!BUS_IS_OPEN(bus->state))
+                return -ENOTCONN;
+
+        /* Re-enqeue a message for reading. This is primarily useful for PolicyKit-style authentication,
+         * where we want accept a message, then determine we need to interactively authenticate the user, and
+         * when we have that process the message again. */
+
+        r = bus_rqueue_make_room(bus);
+        if (r < 0)
+                return r;
+
+        bus->rqueue[bus->rqueue_size++] = bus_message_ref_queued(m, bus);
+        return 0;
+}
