40079d18ffdc874d5390ab83a7cfb14265f0a8e8
Evgeny Vereshchagin <evvers@ya.ru>
2020-02-01 12:53:09 +0000

    tests: temporarily bring back the ASan+UBsan stage to run TEST-44-LOG-NAMESPACE under ASan+UBsan

--- travis-ci/managers/fuzzbuzz.sh
+++ travis-ci/managers/fuzzbuzz.sh
@@ -1,41 +1,18 @@
 #!/bin/bash
-
 set -e
 set -x
-set -u
 
-REPO_ROOT=${REPO_ROOT:-$(pwd)}
-
-sudo bash -c "echo 'deb-src http://archive.ubuntu.com/ubuntu/ xenial main restricted universe multiverse' >>/etc/apt/sources.list"
-sudo apt-get update -y
-sudo apt-get build-dep systemd -y
-sudo apt-get install -y ninja-build python3-pip python3-setuptools quota
-# The following should be dropped when debian packaging has been updated to include them
-sudo apt-get install -y libfdisk-dev libp11-kit-dev libssl-dev libpwquality-dev
+bash -c "echo 'deb-src http://archive.ubuntu.com/ubuntu/ xenial main restricted universe multiverse' >>/etc/apt/sources.list"
+apt-get update
+apt-get build-dep systemd -y
+apt-get install -y util-linux libmount-dev libblkid-dev liblzma-dev libqrencode-dev libmicrohttpd-dev iptables-dev liblz4-dev libcurl4-gnutls-dev unifont itstool kbd cryptsetup-bin net-tools isc-dhcp-client iputils-ping strace qemu-system-x86 linux-image-virtual mount libgpg-error-dev libxkbcommon-dev python-lxml python3-lxml python3-pip libcap-dev
+apt-get install -y gettext python3-evdev python3-pyparsing libmount-dev python3-setuptools ninja-build quota
 pip3 install meson
 
-cd $REPO_ROOT
-export PATH="$HOME/.local/bin/:$PATH"
-tools/oss-fuzz.sh
-timeout --preserve-status 5 ./out/fuzz-unit-file
-git clean -dxff
+cd ${REPO_ROOT:-$(pwd)}
 
-wget https://app.fuzzbuzz.io/releases/cli/latest/linux/fuzzbuzz
-chmod +x fuzzbuzz
-./fuzzbuzz validate
-./fuzzbuzz target test fuzz-unit-file --all
+sed -i 's/2\.30/2.27/' meson.build
 
-git clone https://github.com/google/oss-fuzz /tmp/oss-fuzz
-cd /tmp/oss-fuzz
-sudo ./infra/helper.py pull_images
-
-# docker doesn't like colons in filenames so let's create a directory
-# whose name can be consumed by the -v option.
-# https://github.com/google/oss-fuzz/issues/2428
-t=$(mktemp -d)
-sudo mount --bind "$REPO_ROOT" "$t"
-
-# helper.py is wrapped in script to trick it into thinking it's "interactive"
-# See https://github.com/systemd/systemd/pull/12542#issuecomment-491563572
-sudo script -e -c "./infra/helper.py build_fuzzers --clean --sanitizer=memory systemd $t"
-sudo script -e -c "./infra/helper.py check_build --sanitizer=memory -e ALLOWED_BROKEN_TARGETS_PERCENTAGE=0 systemd"
+meson --werror -Db_sanitize=address,undefined -Dsplit-usr=true build
+ninja -v -C build
+make -C test/TEST-44-LOG-NAMESPACE clean setup run TEST_NO_QEMU=yes NSPAWN_ARGUMENTS=--keep-unit RUN_IN_UNPRIVILEGED_CONTAINER=no
