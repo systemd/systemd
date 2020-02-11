407a2bf37df7ad3b496b5037edfd48fd52faef5c
Evgeny Vereshchagin <evvers@ya.ru>
2020-02-11 19:56:55 +0000

    turn off ASan/UBSan and switch to gcc to resemble Ubuntu CI

--- travis-ci/managers/ubuntu.sh
+++ travis-ci/managers/ubuntu.sh
@@ -48,20 +48,24 @@ cd ${REPO_ROOT:-$PWD}
 sed -i 's/2\.30/2.27/' meson.build
 
 # Sanitizer-specific options
-export ASAN_OPTIONS=strict_string_checks=1:detect_stack_use_after_return=1:check_initialization_order=1:strict_init_order=1
-export UBSAN_OPTIONS=print_stacktrace=1:print_summary=1:halt_on_error=1
-export CC=clang
-export CXX=clang++
+#export ASAN_OPTIONS=strict_string_checks=1:detect_stack_use_after_return=1:check_initialization_order=1:strict_init_order=1
+#export UBSAN_OPTIONS=print_stacktrace=1:print_summary=1:halt_on_error=1
+#export CC=clang
+#export CXX=clang++
+
+systemd-detect-virt
+cat /proc/1/mountinfo
+unshare -m bash -x -c "
+mount -t tmpfs tmpfs /tmp/
+mount -t tmpfs tmpfs /var/tmp/
 
 meson --werror \
       --optimization=0 \
       --buildtype=debug \
       -Dc_args='-fno-omit-frame-pointer -ftrapv' \
-      -Db_sanitize=address,undefined \
-      -Db_lundef=false \
       -Dtests=unsafe \
       -Dsplit-usr=true \
       -Dman=true \
       build
 ninja -v -C build
-meson test -C build --print-errorlogs --timeout-multiplier=4
+meson test -C build --print-errorlogs --timeout-multiplier=4"
