1f8cb091ade5ec14ff4c2b68dce213809877e0bb
Mike Gilbert <floppym@gentoo.org>
2020-02-22 13:30:48 -0500

    userdb: allow dots in username
    This prevents an error in pam_systemd when logging in.
    
    sshd[2623165]: pam_unix(sshd:session): session opened for user tony.stark(uid=10001) by (uid=0)
    sshd[2623165]: pam_systemd(sshd:session): Failed to get user record: Invalid argument
    
    Bug: https://bugs.gentoo.org/708824

--- src/shared/userdb.c
+++ src/shared/userdb.c
@@ -587,7 +587,7 @@ int userdb_by_name(const char *name, UserDBFlags flags, UserRecord **ret) {
         _cleanup_(json_variant_unrefp) JsonVariant *query = NULL;
         int r;
 
-        if (!valid_user_group_name(name))
+        if (!valid_user_group_name_compat(name))
                 return -EINVAL;
 
         r = json_build(&query, JSON_BUILD_OBJECT(
@@ -801,7 +801,7 @@ int groupdb_by_name(const char *name, UserDBFlags flags, GroupRecord **ret) {
         _cleanup_(json_variant_unrefp) JsonVariant *query = NULL;
         int r;
 
-        if (!valid_user_group_name(name))
+        if (!valid_user_group_name_compat(name))
                 return -EINVAL;
 
         r = json_build(&query, JSON_BUILD_OBJECT(
@@ -994,7 +994,7 @@ int membershipdb_by_user(const char *name, UserDBFlags flags, UserDBIterator **r
 
         assert(ret);
 
-        if (!valid_user_group_name(name))
+        if (!valid_user_group_name_compat(name))
                 return -EINVAL;
 
         r = json_build(&query, JSON_BUILD_OBJECT(
@@ -1037,7 +1037,7 @@ int membershipdb_by_group(const char *name, UserDBFlags flags, UserDBIterator **
 
         assert(ret);
 
-        if (!valid_user_group_name(name))
+        if (!valid_user_group_name_compat(name))
                 return -EINVAL;
 
         r = json_build(&query, JSON_BUILD_OBJECT(
