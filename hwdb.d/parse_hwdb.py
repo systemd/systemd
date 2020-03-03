1c5b427f5d3649cfa1bbd0371430bf062251da26
Guido Günther <agx@sigxcpu.org>
2020-03-03 14:40:00 +0100

    hwdb: 60-sensor.hwdb: Add proximity sensor udev property (#14845)
    Devices with proximity sensors can specify a near level that
    indicates when an object is considered close to the device.

--- hwdb.d/parse_hwdb.py
+++ hwdb.d/parse_hwdb.py
@@ -128,6 +128,7 @@ def property_grammar():
              ('KEYBOARD_LED_CAPSLOCK', Literal('0')),
              ('ACCEL_MOUNT_MATRIX', mount_matrix),
              ('ACCEL_LOCATION', Or(('display', 'base'))),
+             ('PROXIMITY_NEAR_LEVEL', INTEGER),
             )
     fixed_props = [Literal(name)('NAME') - Suppress('=') - val('VALUE')
                    for name, val in props]
