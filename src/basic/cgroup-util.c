5fc2d94fbf8271bb340e834f832af5d890c267bf
Michal Sekletár <msekleta@redhat.com>
2020-03-03 11:45:00 +0100

    cgroup: make sure that cpuset is supported on cgroup v2 and disabled with v1
    Resolves: #1808940
    
    (rhel-only)

--- src/basic/cgroup-util.c
+++ src/basic/cgroup-util.c
@@ -2353,10 +2353,10 @@ int cg_mask_supported(CGroupMask *ret) {
                 if (r < 0)
                         return r;
 
-                /* Currently, we support the cpu, memory, io and pids
+                /* Currently, we support the cpu, memory, io, pids and cpuset
                  * controller in the unified hierarchy, mask
                  * everything else off. */
-                mask &= CGROUP_MASK_CPU | CGROUP_MASK_MEMORY | CGROUP_MASK_IO | CGROUP_MASK_PIDS;
+                mask &= CGROUP_MASK_CPU | CGROUP_MASK_MEMORY | CGROUP_MASK_IO | CGROUP_MASK_PIDS | CGROUP_MASK_CPUSET;
 
         } else {
                 CGroupController c;
@@ -2366,6 +2366,9 @@ int cg_mask_supported(CGroupMask *ret) {
 
                 for (c = 0; c < _CGROUP_CONTROLLER_MAX; c++) {
                         const char *n;
+
+                        if (c == CGROUP_CONTROLLER_CPUSET)
+                                continue;
 
                         n = cgroup_controller_to_string(c);
                         if (controller_is_accessible(n) >= 0)
