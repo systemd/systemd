9b4992fb6b53a7c11313f0ed7e763dc683439772
Valerii Chernous <vchernou@cisco.com>
2020-03-10 08:34:48 -0700

    udev: fix SECLABEL{selinux} issue
    Add SECLABEL{selinux}="some value" cause udevadm crash
    systemd-udevd[x]: Worker [x] terminated by signal 11 (SEGV)
    
    It happens since 25de7aa7b90 (Yu Watanabe 2019-04-25 01:21:11 +0200)
    when udev rules processing changed to token model. Yu forgot store
    attr to SECLABEL token so fix it.
    
    Signed-off-by: Valerii Chernous <vchernou@cisco.com>

--- src/udev/udev-rules.c
+++ src/udev/udev-rules.c
@@ -921,7 +921,7 @@ static int parse_token(UdevRules *rules, const char *key, char *attr, UdevRuleOp
                         op = OP_ASSIGN;
                 }
 
-                r = rule_line_add_token(rule_line, TK_A_SECLABEL, op, value, NULL);
+                r = rule_line_add_token(rule_line, TK_A_SECLABEL, op, value, attr);
         } else if (streq(key, "RUN")) {
                 if (is_match || op == OP_REMOVE)
                         return log_token_invalid_op(rules, key);
