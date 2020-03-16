b453122789ec4c6f39e6ceb9900e0e80a6abeb99
Yu Watanabe <watanabe.yu+github@gmail.com>
2020-03-16 18:55:10 +0900

    network: add a flag to ignore gateway provided by DHCP server
    Closes #15117.

--- src/network/networkd-network.c
+++ src/network/networkd-network.c
@@ -384,6 +384,7 @@ int network_load_one(Manager *manager, OrderedHashmap **networks, const char *fi
                 .dhcp_use_dns = true,
                 .dhcp_use_hostname = true,
                 .dhcp_use_routes = true,
+                .dhcp_use_gateway = true,
                 /* NOTE: this var might be overwritten by network_apply_anonymize_if_set */
                 .dhcp_send_hostname = true,
                 .dhcp_send_release = true,
