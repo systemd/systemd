1c76b5b5bbef7803916903ccc97a840245cce343
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2020-03-19 16:57:50 +0100

    test/TEST-06: skip test if no selinux
    I put SELINUX=disabled on my laptop, and the test fails with ENOENT when trying
    to write to /sys/fs/selinux/enforce. It's a bit of a special case, but let's
    avoid the failure.

--- test/units/testsuite-06.sh
+++ test/units/testsuite-06.sh
@@ -3,7 +3,12 @@ set -x
 set -e
 set -o pipefail
 
-echo 1 >/sys/fs/selinux/enforce
+echo 1 >/sys/fs/selinux/enforce || {
+    echo "Can't make selinux enforcing, skipping test"
+    touch /testok
+    exit
+}
+
 runcon -t systemd_test_start_t systemctl start hola
 runcon -t systemd_test_reload_t systemctl reload hola
 runcon -t systemd_test_stop_t systemctl stop hola
