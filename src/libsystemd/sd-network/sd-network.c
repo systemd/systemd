8102b9e710eb64cf07fb9d591a0de94b5e37e850
Susant Sahani <ssahani@vmware.com>
2020-03-23 04:16:47 +0100

    sd-network: Add support to emit and receive pop3 server information

--- src/libsystemd/sd-network/sd-network.c
+++ src/libsystemd/sd-network/sd-network.c
@@ -256,6 +256,10 @@ _public_ int sd_network_link_get_sip_servers(int ifindex, char ***ret) {
         return network_link_get_strv(ifindex, "SIP", ret);
 }
 
+_public_ int sd_network_link_get_pop3_servers(int ifindex, char ***pop3) {
+        return network_link_get_strv(ifindex, "POP3_SERVERS", pop3);
+}
+
 _public_ int sd_network_link_get_dns_default_route(int ifindex) {
         char path[STRLEN("/run/systemd/netif/links/") + DECIMAL_STR_MAX(ifindex) + 1];
         _cleanup_free_ char *s = NULL;
