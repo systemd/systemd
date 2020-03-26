1b7d1cae0f19c35bb9497f133eabb87e19383c91
Christian Göttsche <cgzones@googlemail.com>
2020-03-26 20:15:17 +0100

    selinux: print enforcing state in access check debug message

--- src/core/selinux-access.c
+++ src/core/selinux-access.c
@@ -272,8 +272,8 @@ int mac_selinux_generic_access_check(
                         sd_bus_error_setf(error, SD_BUS_ERROR_ACCESS_DENIED, "SELinux policy denies access.");
         }
 
-        log_debug_errno(r, "SELinux access check scon=%s tcon=%s tclass=%s perm=%s path=%s cmdline=%s: %m",
-                        scon, fcon, tclass, permission, path, cl);
+        log_debug_errno(r, "SELinux access check scon=%s tcon=%s tclass=%s perm=%s state=%s path=%s cmdline=%s: %m",
+                        scon, fcon, tclass, permission, enforce ? "enforcing" : "permissive", path, cl);
         return enforce ? r : 0;
 }
 
