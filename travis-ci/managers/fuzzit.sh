c4ae2704b7e921a0b05486a7b201be6770a04ea7
Evgeny Vereshchagin <evvers@ya.ru>
2020-03-29 23:32:00 +0000

    travis: install ninja with pip
    https://mesonbuild.com/Release-notes-for-0-54-0.html#ninja-version-requirement-bumped-to-17
    https://github.com/mesonbuild/meson/issues/6867

--- travis-ci/managers/fuzzit.sh
+++ travis-ci/managers/fuzzit.sh
@@ -13,10 +13,10 @@ REPO_ROOT=${REPO_ROOT:-$(pwd)}
 sudo bash -c "echo 'deb-src http://archive.ubuntu.com/ubuntu/ xenial main restricted universe multiverse' >>/etc/apt/sources.list"
 sudo apt-get update -y
 sudo apt-get build-dep systemd -y
-sudo apt-get install -y ninja-build python3-pip python3-setuptools
+sudo apt-get install -y python3-pip python3-setuptools
 # The following should be dropped when debian packaging has been updated to include them
 sudo apt-get install -y libfdisk-dev libp11-kit-dev libssl-dev libpwquality-dev
-pip3 install meson
+pip3 install ninja meson
 
 cd $REPO_ROOT
 export PATH="$HOME/.local/bin/:$PATH"
