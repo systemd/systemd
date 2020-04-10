7c9b26900cc33daf080627daf5904de74c1ef267
Susant Sahani <ssahani@vmware.com>
2020-04-10 15:29:10 +0200

    network: VXLan - fix adding Group address

--- src/network/netdev/vxlan.c
+++ src/network/netdev/vxlan.c
@@ -38,11 +38,11 @@ static int netdev_vxlan_fill_message_create(NetDev *netdev, Link *link, sd_netli
                         return log_netdev_error_errno(netdev, r, "Could not append IFLA_VXLAN_ID attribute: %m");
         }
 
-        if (in_addr_is_null(v->remote_family, &v->remote) == 0) {
-                if (v->remote_family == AF_INET)
-                        r = sd_netlink_message_append_in_addr(m, IFLA_VXLAN_GROUP, &v->remote.in);
+        if (in_addr_is_null(v->group_family, &v->group) == 0) {
+                if (v->group_family == AF_INET)
+                        r = sd_netlink_message_append_in_addr(m, IFLA_VXLAN_GROUP, &v->group.in);
                 else
-                        r = sd_netlink_message_append_in6_addr(m, IFLA_VXLAN_GROUP6, &v->remote.in6);
+                        r = sd_netlink_message_append_in6_addr(m, IFLA_VXLAN_GROUP6, &v->group.in6);
                 if (r < 0)
                         return log_netdev_error_errno(netdev, r, "Could not append IFLA_VXLAN_GROUP attribute: %m");
         }
