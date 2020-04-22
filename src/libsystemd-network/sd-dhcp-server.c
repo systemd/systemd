a385dc910d3e819ef41d71a9e0cef6a4f2ccb520
Borna Blažević <borna.blazevic@sartura.hr>
2020-04-22 09:10:11 +0200

    add static leases to DHCP ack

--- src/libsystemd-network/sd-dhcp-server.c
+++ src/libsystemd-network/sd-dhcp-server.c
@@ -873,7 +873,58 @@ int dhcp_server_handle_message(sd_dhcp_server *server, DHCPMessage *message,
 
                 /* verify that the requested address is from the pool, and either
                    owned by the current client or free */
-                if (pool_offset >= 0 &&
+                if(static_lease) {
+                        DHCPLease *lease;
+                        usec_t time_now = 0;
+                        lease = new0(DHCPLease, 1);
+                        if (!lease)
+                          return -ENOMEM;
+                        lease->address = static_lease->address;
+                        lease->client_id.data = memdup(req->client_id.data,
+                                                       req->client_id.length);
+                        if (!lease->client_id.data) {
+                          free(lease);
+                          return -ENOMEM;
+                        }
+                        lease->client_id.length = req->client_id.length;
+                        memcpy(&lease->chaddr, &req->message->chaddr,
+                               ETH_ALEN);
+                        lease->gateway = req->message->giaddr;
+                        printf("static address from req %u\n", lease->address);
+
+                                                r = sd_event_now(server->event,
+                                         clock_boottime_or_monotonic(),
+                                         &time_now);
+                        if (r < 0) {
+                                if (!existing_lease)
+                                        dhcp_lease_free(lease);
+                                return r;
+                        }
+
+                        lease->expiration = req->lifetime * USEC_PER_SEC + time_now;
+                        printf("sending ack\n");
+                        r = server_send_ack(server, req, address);
+                        if (r < 0) {
+                                /* this only fails on critical errors */
+                                log_dhcp_server_errno(server, r, "Could not send ack: %m");
+                                printf("could not send ack\n");
+
+                                if (!existing_lease)
+                                        dhcp_lease_free(lease);
+
+                                return r;
+                        } else {
+                                log_dhcp_server(server, "ACK (0x%x)",
+                                                be32toh(req->message->xid));
+
+                                server->bound_leases[pool_offset] = lease;
+                                hashmap_put(server->leases_by_client_id,
+                                            &lease->client_id, lease);
+
+                                return DHCP_ACK;
+                        }
+                }
+                else if (pool_offset >= 0 &&
                     server->bound_leases[pool_offset] == existing_lease) {
                         DHCPLease *lease;
                         usec_t time_now = 0;
@@ -906,11 +957,12 @@ int dhcp_server_handle_message(sd_dhcp_server *server, DHCPMessage *message,
                         }
 
                         lease->expiration = req->lifetime * USEC_PER_SEC + time_now;
-
+                        printf("sending ack\n");
                         r = server_send_ack(server, req, address);
                         if (r < 0) {
                                 /* this only fails on critical errors */
                                 log_dhcp_server_errno(server, r, "Could not send ack: %m");
+                                printf("could not send ack\n");
 
                                 if (!existing_lease)
                                         dhcp_lease_free(lease);
