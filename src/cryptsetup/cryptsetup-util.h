23769fb3711e268386453ae3b4d8ed3feadeb516
Lennart Poettering <lennart@poettering.net>
2020-04-29 14:50:38 +0200

    cryptsetup: split out key loading from pkcs11 code and teach search path logic
    Let's do some rearrangements, so that we can later on use this to
    automatically search for a key file.

---
+++ src/cryptsetup/cryptsetup-util.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: LGPL-2.1+ */
+#pragma once
+
+#include <inttypes.h>
+#include <sys/types.h>
+
+int load_key_file(
+                const char *key_file,
+                char **search_path,
+                size_t key_file_size,
+                uint64_t key_file_offset,
+                void **ret_key,
+                size_t *ret_key_size);
