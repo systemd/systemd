d904afc730268d50502f764dfd55b8cf4906c46f
Luca Boccassi <luca.boccassi@microsoft.com>
2020-05-07 23:26:53 +0100

    core: reload cache if it's dirty when starting a UNIT_NOT_FOUND unit
    The time-based cache allows starting a new unit without an expensive
    daemon-reload, unless there was already a reference to it because of
    a dependency or ordering from another unit.
    If the cache is out of date, check again if we can load the
    fragment.

---
+++ test/TEST-48-START-STOP-NO-RELOAD/test.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
+# ex: ts=8 sw=4 sts=4 et filetype=sh
+set -e
+TEST_DESCRIPTION="test StartStopNoReload"
+. $TEST_BASE_DIR/test-functions
+
+do_test "$@" 48
