d2916409edb7bea9963c71052375a9919df28dc1
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2020-05-24 12:51:51 +0200

    sd-bus: make name validation functions public
    Those are fairly trivial to reimplement, but any non-trivial user of sd-bus
    is likely to need them. So let's expose them to save everyone the trouble.
    
    I'm keeping the internal functions and making the public ones thin wrappers,
    because for the internal uses we don't need the additional asserts, and also we
    can't expose _pure_ annotation easily, and dropping it would likely make the
    compiled code a bit less efficient.

--- src/libsystemd/sd-bus/bus-type.c
+++ src/libsystemd/sd-bus/bus-type.c
@@ -4,6 +4,7 @@
 
 #include "sd-bus.h"
 
+#include "bus-internal.h"
 #include "bus-type.h"
 
 bool bus_type_is_valid(char c) {
@@ -134,4 +135,28 @@ int bus_type_get_size(char c) {
         }
 
         return -EINVAL;
+}
+
+_public_ int sd_bus_interface_name_is_valid(const char *p) {
+        assert_return(p, -EINVAL);
+
+        return interface_name_is_valid(p);
+}
+
+_public_ int sd_bus_service_name_is_valid(const char *p) {
+        assert_return(p, -EINVAL);
+
+        return service_name_is_valid(p);
+}
+
+_public_ int sd_bus_member_name_is_valid(const char *p) {
+        assert_return(p, -EINVAL);
+
+        return member_name_is_valid(p);
+}
+
+_public_ int sd_bus_object_path_is_valid(const char *p) {
+        assert_return(p, -EINVAL);
+
+        return object_path_is_valid(p);
 }
