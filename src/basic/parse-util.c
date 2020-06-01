fe43ccf978ea477c01e6c3aa3329fddf2a037d0f
Lennart Poettering <lennart@poettering.net>
2020-06-01 17:08:38 +0200

    parse-util: allow '-0' as alternative to '0' and '+0'
    Let's allow "-0" as alternative to "+0" and "0" when parsing integers,
    unless the new SAFE_ATO_REFUSE_PLUS_MINUS flag is specified.
    
    In cases where allowing the +/- syntax shall not be allowed
    SAFE_ATO_REFUSE_PLUS_MINUS is the right flag to use, but this also means
    that -0 as only negative integer that fits into an unsigned value should
    be acceptable if the flag is not specified.
    
    (cherry picked from commit c78eefc13562a8fc0c22c00a6d3001af89860258)

--- src/basic/parse-util.c
+++ src/basic/parse-util.c
@@ -378,7 +378,7 @@ int safe_atou_full(const char *s, unsigned base, unsigned *ret_u) {
                 return -errno;
         if (!x || x == s || *x != 0)
                 return -EINVAL;
-        if (s[0] == '-')
+        if (l != 0 && s[0] == '-')
                 return -ERANGE;
         if ((unsigned long) (unsigned) l != l)
                 return -ERANGE;
@@ -437,7 +437,7 @@ int safe_atollu_full(const char *s, unsigned base, long long unsigned *ret_llu) 
                 return -errno;
         if (!x || x == s || *x != 0)
                 return -EINVAL;
-        if (*s == '-')
+        if (l != 0 && s[0] == '-')
                 return -ERANGE;
 
         if (ret_llu)
@@ -479,7 +479,7 @@ int safe_atou8(const char *s, uint8_t *ret) {
                 return -errno;
         if (!x || x == s || *x != 0)
                 return -EINVAL;
-        if (s[0] == '-')
+        if (l != 0 && s[0] == '-')
                 return -ERANGE;
         if ((unsigned long) (uint8_t) l != l)
                 return -ERANGE;
@@ -516,7 +516,7 @@ int safe_atou16_full(const char *s, unsigned base, uint16_t *ret) {
                 return -errno;
         if (!x || x == s || *x != 0)
                 return -EINVAL;
-        if (s[0] == '-')
+        if (l != 0 && s[0] == '-')
                 return -ERANGE;
         if ((unsigned long) (uint16_t) l != l)
                 return -ERANGE;
