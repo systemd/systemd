a055076988cd5b3b6cf392797da930c7ecf6274e
Evgeny Vereshchagin <evvers@ya.ru>
2020-06-25 02:46:21 +0200

    ci: free up some resources on Pipelines
    Now that CIFuzz supports all the sanitizers we use and ALLOWED_BROKEN_TARGETS_PERCENTAGE
    we no longer need that bash script.

--- travis-ci/managers/fuzzbuzz.sh
+++ travis-ci/managers/fuzzbuzz.sh
@@ -6,7 +6,7 @@ set -u
 
 REPO_ROOT=${REPO_ROOT:-$(pwd)}
 
-sudo bash -c "echo 'deb-src http://archive.ubuntu.com/ubuntu/ xenial main restricted universe multiverse' >>/etc/apt/sources.list"
+sudo bash -c "echo 'deb-src http://archive.ubuntu.com/ubuntu/ $(lsb_release -cs) main restricted universe multiverse' >>/etc/apt/sources.list"
 sudo apt-get update -y
 sudo apt-get build-dep systemd -y
 sudo apt-get install -y ninja-build python3-pip python3-setuptools quota
@@ -19,18 +19,3 @@ export PATH="$HOME/.local/bin/:$PATH"
 tools/oss-fuzz.sh
 ./out/fuzz-unit-file -max_total_time=5
 git clean -dxff
-
-git clone https://github.com/google/oss-fuzz /tmp/oss-fuzz
-cd /tmp/oss-fuzz
-sudo ./infra/helper.py pull_images
-
-# docker doesn't like colons in filenames so let's create a directory
-# whose name can be consumed by the -v option.
-# https://github.com/google/oss-fuzz/issues/2428
-t=$(mktemp -d)
-sudo mount --bind "$REPO_ROOT" "$t"
-
-# helper.py is wrapped in script to trick it into thinking it's "interactive"
-# See https://github.com/systemd/systemd/pull/12542#issuecomment-491563572
-sudo script -e -c "./infra/helper.py build_fuzzers --clean --sanitizer=memory systemd $t"
-sudo script -e -c "./infra/helper.py check_build --sanitizer=memory -e ALLOWED_BROKEN_TARGETS_PERCENTAGE=0 systemd"
