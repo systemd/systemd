systemd/systemd

commit 8d9cbd809db492df9d94c0c664bd0d2e53416531

selinux: create standard user-runtime nodes with default context
Currently systemd-user-runtime-dir does not create the files in
/run/user/$UID/systemd/inaccessible with the default SELinux label.
The user and role part of these labels should be based on the user
related to $UID and not based on the process context of
systemd-user-runtime-dir.

Since v246-rc1 (9664be199af6) /run/user/$UID/systemd is also created by
systemd-user-runtime-dir and should also be created with the default
SELinux context.

src/shared/dev-setup.c
@@ -103,9 +103,9 @@ int make_inaccessible_nodes(
                         return log_oom();
 
                 if (S_ISDIR(table[i].mode))
-                        r = mkdir(path, table[i].mode & 07777);
+                        r = mkdir_label(path, table[i].mode & 07777);
                 else
-                        r = mknod(path, table[i].mode, makedev(0, 0));
+                        r = mknod_label(path, table[i].mode, makedev(0, 0));
                 if (r < 0) {
                         if (errno != EEXIST)
                                 log_debug_errno(errno, "Failed to create '%s', ignoring: %m", path);

commit db06c162e8c077e6c6844e14bdf70c5ef8a40a4e

shared: fix error handling in make_inaccessible_nodes
_label wrappers return -errno on failure instead of returning -1 and
setting global errno.

Fixes: 8d9cbd809db492df9d94c0c664bd0d2e53416531
Follow up: #16426

src/shared/dev-setup.c
@@ -107,8 +107,8 @@ int make_inaccessible_nodes(
                 else
                         r = mknod_label(path, table[i].mode, makedev(0, 0));
                 if (r < 0) {
-                        if (errno != EEXIST)
-                                log_debug_errno(errno, "Failed to create '%s', ignoring: %m", path);
+                        if (r != -EEXIST)
+                                log_debug_errno(r, "Failed to create '%s', ignoring: %m", path);
                         continue;
                 }
 