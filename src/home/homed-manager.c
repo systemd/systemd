84e1ab74d274de2a43bfa4f67a81da97635fd27c
Lennart Poettering <lennart@poettering.net>
2020-08-17 18:19:30 +0200

    homed: downgrade quota message in containers
    quota syscalls and operations are typically prohibited in containers.
    Let's not make noise about that, needlessly.
    
    (cherry picked from commit 5e5e11b8744fbe9dcd87a2d165f342dc9bcd4008)

--- src/home/homed-manager.c
+++ src/home/homed-manager.c
@@ -512,6 +512,8 @@ static int search_quota(uid_t uid, const char *exclude_quota_path) {
                 if (r < 0) {
                         if (ERRNO_IS_NOT_SUPPORTED(r))
                                 log_debug_errno(r, "No UID quota support on %s, ignoring.", where);
+                        else if (ERRNO_IS_PRIVILEGE(r))
+                                log_debug_errno(r, "UID quota support for %s prohibited, ignoring.", where);
                         else
                                 log_warning_errno(r, "Failed to query quota on %s, ignoring: %m", where);
 
