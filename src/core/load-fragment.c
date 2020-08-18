6cf2ec5da11488c31415f94180ad799a0187ce6c
Alyssa Ross <hi@alyssa.is>
2020-08-18 20:56:59 +0000

    load-fragment: fix grammar in error messages
    (cherry picked from commit 556a7bbed607ec8cbbf4affc5d862ef92403418e)
    (cherry picked from commit 76331f86f630bd884f2e16a36d66c55b2b22c8e1)

--- src/core/load-fragment.c
+++ src/core/load-fragment.c
@@ -4284,7 +4284,7 @@ int config_parse_bind_paths(
                 r = unit_full_printf(u, source, &sresolved);
                 if (r < 0) {
                         log_syntax(unit, LOG_ERR, filename, line, r,
-                                   "Failed to resolved unit specifiers in \"%s\", ignoring: %m", source);
+                                   "Failed to resolve unit specifiers in \"%s\", ignoring: %m", source);
                         continue;
                 }
 
@@ -4315,7 +4315,7 @@ int config_parse_bind_paths(
                         r = unit_full_printf(u, destination, &dresolved);
                         if (r < 0) {
                                 log_syntax(unit, LOG_ERR, filename, line, r,
-                                           "Failed to resolved specifiers in \"%s\", ignoring: %m", destination);
+                                           "Failed to resolve specifiers in \"%s\", ignoring: %m", destination);
                                 continue;
                         }
 
