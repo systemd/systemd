752fedbea7c02c82287c7ff2a4139f528b3f7ba8
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2020-08-19 17:46:30 +0200

    shared/syscall-list: filter out some obviously platform-specific syscalls

--- src/shared/generate-syscall-list.py
+++ src/shared/generate-syscall-list.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
 import sys
+import os
+
+s390 = 's390' in os.uname().machine
+arm = 'arm' in os.uname().machine
 
 for line in open(sys.argv[1]):
+    if line.startswith('s390_') and not s390:
+        continue
+    if line.startswith('arm_') and not arm:
+        continue
+
     print('"{}\\0"'.format(line.strip()))
