becee21066c719698562c20f7cb994b0ece7917c
Michal Sekletár <msekleta@redhat.com>
2020-08-24 11:21:44 +0200

    udev: add basic set of user-space defined tracepoints (USDT)
    Debugging udev issues especially during the early boot is fairly
    difficult. Currently, you need to enable (at least) debug logging and
    start monitoring uevents, try to reproduce the issue and then analyze
    and correlate two (usually) huge log files. This is not ideal.
    
    This patch aims to provide much more focused debugging tool,
    tracepoints. More often then not we tend to have at least the basic idea
    about the issue we are trying to debug further, e.g. we know it is
    storage related. Hence all of the debug data generated for network
    devices is useless, adds clutter to the log files and generally
    slows things down.
    
    Using this set of tracepoints you can start asking very specific
    questions related to event processing for given device or subsystem.
    Tracepoints can be used with various tracing tools but I will provide
    examples using bpftrace.
    
    Another important aspect to consider is that using tracepoints you can
    debug production systems. There is no need to install test packages with
    added logging, no debuginfo packages, etc...
    
    Example usage (you might be asking such questions during the debug session),
    
    Q: How can I list all tracepoints?
    A: bpftrace -l 'usdt:/usr/lib/systemd/systemd-udevd:udev:*'
    
    Q: What are the arguments for each tracepoint?
    A: In bpftrace program you need to refer to them as arg0, arg1. For
       details about types have a look at their definition in
       src/udev/udev-tracepoints.d.
    
    Q: How many times we have executed external binary?
    A: bpftrace -e 'usdt:/usr/lib/systemd/systemd-udevd:udev:spawn_exec { @cnt = count; }'
    
    Q: What binaries where executed while handling events for "dm-0" device?
    A  bfptrace -e 'usdt:/usr/lib/systemd/systemd-udevd:udev:spawn_exec / str(arg0) == "dm-0"/ { @cmds[str(arg2)] = count(); }'

--- src/udev/udev-event.c
+++ src/udev/udev-event.c
@@ -34,6 +34,11 @@
 #include "udev-watch.h"
 #include "user-util.h"
 
+#if HAVE_STAP
+#include <sys/sdt.h>
+#include "udev-probes.h"
+#endif
+
 typedef struct Spawn {
         sd_device *device;
         const char *cmd;
@@ -603,6 +608,7 @@ static int on_spawn_timeout(sd_event_source *s, uint64_t usec, void *userdata) {
 
         assert(spawn);
 
+        DEVICE_TRACE_POINT(SPAWN_TIMEOUT, spawn->device, spawn->cmd);
         kill_and_sigcont(spawn->pid, spawn->timeout_signal);
 
         log_device_error(spawn->device, "Spawned process '%s' ["PID_FMT"] timed out after %s, killing",
@@ -647,6 +653,8 @@ static int on_spawn_sigchld(sd_event_source *s, const siginfo_t *si, void *userd
         default:
                 log_device_error(spawn->device, "Process '%s' failed due to unknown reason.", spawn->cmd);
         }
+
+        DEVICE_TRACE_POINT(SPAWN_EXIT, spawn->device, spawn->cmd);
 
         sd_event_exit(sd_event_source_get_event(s), ret);
         return 1;
@@ -784,6 +792,8 @@ int udev_event_spawn(UdevEvent *event,
 
                 (void) close_all_fds(NULL, 0);
                 (void) rlimit_nofile_safe();
+
+                DEVICE_TRACE_POINT(SPAWN_EXEC, event->dev, cmd);
 
                 execve(argv[0], argv, envp);
                 _exit(EXIT_FAILURE);
@@ -1017,9 +1027,13 @@ int udev_event_execute_rules(UdevEvent *event,
                         return r;
         }
 
+        DEVICE_TRACE_POINT(RULES_START, dev);
+
         r = udev_rules_apply_to_event(rules, event, timeout_usec, timeout_signal, properties_list);
         if (r < 0)
                 return log_device_debug_errno(dev, r, "Failed to apply udev rules: %m");
+
+        DEVICE_TRACE_POINT(RULES_FINISHED, dev);
 
         r = rename_netif(event);
         if (r < 0)
