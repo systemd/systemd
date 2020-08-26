4139509426902d5c4a69695474028f38b9b7d4ac
Lennart Poettering <lennart@poettering.net>
2020-08-26 23:10:50 +0200

    tty-ask-pw-agent: properly propagate error
    (cherry picked from commit a4fd6cd3f575eb7e1452a74c2e35548739e3b252)
    (cherry picked from commit 8b29c4a4f9dff904985293193c5a5b8f9f369afd)

--- src/tty-ask-password-agent/tty-ask-password-agent.c
+++ src/tty-ask-password-agent/tty-ask-password-agent.c
@@ -143,8 +143,7 @@ static int agent_ask_password_tty(
                 const char *flag_file,
                 char ***ret) {
 
-        int tty_fd = -1;
-        int r;
+        int tty_fd = -1, r;
 
         if (arg_console) {
                 const char *con = arg_device ?: "/dev/console";
@@ -166,7 +165,7 @@ static int agent_ask_password_tty(
                 release_terminal();
         }
 
-        return 0;
+        return r;
 }
 
 static int process_one_password_file(const char *filename) {
