1c5950bd61e3e1ea67ad7428a32929c9768e2d8e
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2020-09-08 13:19:53 +0200

    sleep: reword some debug messages
    I think the sentences sound more natural this way.

--- src/sleep/sleep.c
+++ src/sleep/sleep.c
@@ -160,14 +160,13 @@ static int lock_all_homes(void) {
 
         r = sd_bus_call(bus, m, DEFAULT_TIMEOUT_USEC, &error, NULL);
         if (r < 0) {
-                if (bus_error_is_unknown_service(&error))
-                        return log_debug("systemd-homed is not running, skipping locking of home directories.");
+                if (!bus_error_is_unknown_service(&error))
+                        return log_error_errno(r, "Failed to lock home directories: %s", bus_error_message(&error, r));
 
-                return log_error_errno(r, "Failed to lock home directories: %s", bus_error_message(&error, r));
+                return log_debug("systemd-homed is not running, locking of home directories skipped.");
         }
 
-        log_debug("Successfully requested for all home directories to be locked.");
-        return 0;
+        return log_debug("Successfully requested locking of all home directories.");
 }
 
 static int execute(char **modes, char **states) {
