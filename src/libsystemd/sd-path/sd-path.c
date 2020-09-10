6e65df89c348242dbd10036abc7dd5e8181cf733
Lennart Poettering <lennart@poettering.net>
2020-09-10 18:27:18 +0200

    pkg-config: prefix is not really configurable, don't pretend it was
    We generally don't support prefix being != /usr, and this is hardcoded
    all over the place. In the systemd.pc file it wasn't so far. Let's
    adjust this to match the rest of the codebase.

--- src/libsystemd/sd-path/sd-path.c
+++ src/libsystemd/sd-path/sd-path.c
@@ -364,8 +364,6 @@ static int get_path(uint64_t type, char **buffer, const char **ret) {
                 *ret = ROOTPREFIX_NOSLASH "/lib/systemd/system-shutdown";
                 return 0;
 
-        /* FIXME: systemd.pc uses ${prefix}, but CONF_PATHS_NULSTR doesn't.
-         *        Should ${prefix} use in systemd.pc be removed? */
         case SD_PATH_TMPFILES:
                 *ret = "/usr/lib/tmpfiles.d";
                 return 0;
