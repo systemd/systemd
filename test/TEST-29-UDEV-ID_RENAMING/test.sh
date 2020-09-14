efdaeb88f02aeb406068d45ae7687abf1bd4a8a3
Yu Watanabe <watanabe.yu+github@gmail.com>
2020-09-14 15:44:30 +0900

    test: add test for device renaming issue #16967

--- test/TEST-29-UDEV-ID_RENAMING/test.sh
+++ test/TEST-29-UDEV-ID_RENAMING/test.sh
@@ -1,9 +1,24 @@
 #!/usr/bin/env bash
 set -e
 TEST_DESCRIPTION="UDEV ID_RENAMING property"
+IMAGE_NAME="udev-id-renaming"
 TEST_NO_NSPAWN=1
 
 . $TEST_BASE_DIR/test-functions
 QEMU_TIMEOUT=300
+
+test_create_image() {
+    create_empty_image_rootdir
+
+    # Create what will eventually be our root filesystem onto an overlay
+    (
+        LOG_LEVEL=5
+        setup_basic_environment
+        mask_supporting_services
+
+        instmods dummy
+        generate_module_dependencies
+    )
+}
 
 do_test "$@" 29
