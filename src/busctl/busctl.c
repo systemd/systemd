277b8bb5981160b3cc6b0525414df6a5e1841b19
Juergen Hoetzel <juergen@hoetzel.info>
2020-09-15 08:09:51 +0200

    busctl: add missing shortopt -l
    (cherry picked from commit 150c430fd499082164b6ddbd2f501e2333261a78)
    (cherry picked from commit d08f6fddc0a04a212dff9da14f77e3923414c233)

--- src/busctl/busctl.c
+++ src/busctl/busctl.c
@@ -2378,7 +2378,7 @@ static int parse_argv(int argc, char *argv[]) {
         assert(argc >= 0);
         assert(argv);
 
-        while ((c = getopt_long(argc, argv, "hH:M:qj", options, NULL)) >= 0)
+        while ((c = getopt_long(argc, argv, "hH:M:qjl", options, NULL)) >= 0)
 
                 switch (c) {
 
