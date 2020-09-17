dc5c5cd5c864aaae8c2695c2b833d556d8df0f98
Yu Watanabe <watanabe.yu+github@gmail.com>
2020-09-17 14:49:47 +0900

    util: wireguard is merged into upstream kernel
    (cherry picked from commit e29e5db7dff1442ef73cbcd0c898e11078e1042d)
    (cherry picked from commit 1d17c00260a923188363fc32363ff8b6bae06230)
    (cherry picked from commit bc321efb7a1c253f1bf584d97303c447db68a8e8)

--- src/basic/linux/update.sh
+++ src/basic/linux/update.sh
@@ -3,11 +3,7 @@
 set -eu
 
 for i in *.h */*.h; do
-    if [[ $i == 'wireguard.h' ]]; then
-        curl https://raw.githubusercontent.com/WireGuard/WireGuard/master/src/uapi/$i -o $i
-    else
-        curl https://raw.githubusercontent.com/torvalds/linux/master/include/uapi/linux/$i -o $i
-    fi
+    curl https://raw.githubusercontent.com/torvalds/linux/master/include/uapi/linux/$i -o $i
 
     sed -i -e 's/__user //g' -e '/^#include <linux\/compiler.h>/ d' $i
 done
