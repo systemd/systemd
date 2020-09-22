325e9f1f9b205c29e36f41591a7d2aa8ec73295e
Luca Boccassi <luca.boccassi@microsoft.com>
2020-09-22 16:43:30 +0100

    Add ExtensionImages directive to form overlays
    Add support for overlaying the image for services on top of their
    root fs.
    
    The overlay is not done at the root, but instead if individual
    files or directories are overridden, they are bind mounted
    individually.
    Overlayfs is used if a directory exists both in the root and
    in the image, with different content, and it is set up
    read-only unconditionally.
    If /usr and/or /opt are empty and not writable in the root, a tmpfs
    is mounted on top.

--- test/TEST-50-DISSECT/test.sh
+++ test/TEST-50-DISSECT/test.sh
@@ -9,6 +9,8 @@ TEST_INSTALL_VERITY_MINIMAL=1
 
 . $TEST_BASE_DIR/test-functions
 
+command -v mksquashfs >/dev/null 2>&1 || exit 0
+command -v veritysetup >/dev/null 2>&1 || exit 0
 command -v sfdisk >/dev/null 2>&1 || exit 0
 
 # Need loop devices for systemd-dissect
@@ -25,6 +27,7 @@ test_create_image() {
         instmods loop =block
         instmods squashfs =squashfs
         instmods dm_verity =md
+        instmods overlay =overlayfs
         install_dmevent
         generate_module_dependencies
         inst_binary losetup
