9ee806d1a80d7aa9d85fc91dca48e7b37efcb053
Lennart Poettering <lennart@poettering.net>
2020-09-29 13:06:12 +0200

    log: optionally display thread ID in log output to tty
    This is very useful when playing around with threaded tests, but should
    be useful in other cases too.

--- src/basic/log.h
+++ src/basic/log.h
@@ -61,10 +61,13 @@ void log_show_location(bool b);
 bool log_get_show_location(void) _pure_;
 void log_show_time(bool b);
 bool log_get_show_time(void) _pure_;
+void log_show_tid(bool b);
+bool log_get_show_tid(void) _pure_;
 
 int log_show_color_from_string(const char *e);
 int log_show_location_from_string(const char *e);
 int log_show_time_from_string(const char *e);
+int log_show_tid_from_string(const char *e);
 
 LogTarget log_get_target(void) _pure_;
 int log_get_max_level_realm(LogRealm realm) _pure_;
