9e3d90671edbdb69393818d2b755597f84518975
Lennart Poettering <lennart@poettering.net>
2020-10-12 18:18:33 +0200

    udev-util: use absolute rather than relative timeout when waiting for devices
    This makes it easier to accurately wait for a overall deadline.

--- src/udev/udevadm-info.c
+++ src/udev/udevadm-info.c
@@ -493,7 +493,11 @@ int info_main(int argc, char *argv[], void *userdata) {
                 if (arg_wait_for_initialization_timeout > 0) {
                         sd_device *d;
 
-                        r = device_wait_for_initialization(device, NULL, arg_wait_for_initialization_timeout, &d);
+                        r = device_wait_for_initialization(
+                                        device,
+                                        NULL,
+                                        usec_add(now(CLOCK_MONOTONIC), arg_wait_for_initialization_timeout),
+                                        &d);
                         if (r < 0)
                                 return r;
 
