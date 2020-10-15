ad2d8e61eeab27f3e36572c7981362227357f420
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2020-10-15 13:54:31 +0200

    parse_hwdb: enforce the ":*" suffix

--- hwdb.d/parse_hwdb.py
+++ hwdb.d/parse_hwdb.py
@@ -201,8 +201,9 @@ def check_matches(groups):
             except ParseBaseException as e:
                 error('Pattern {!r} is invalid: {}', rest, e)
                 continue
-            if rest[-1] not in '*:':
-                error('pattern {} does not end with "*" or ":"', match)
+
+        if not rest.endswith(':*'):
+            error("pattern {!r} does not end with ':*'", match)
 
     matches.sort()
     prev = None
