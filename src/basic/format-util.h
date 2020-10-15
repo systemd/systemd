67bd5620f6cf481c0a59cedbcf63ddcab355cc55
Lennart Poettering <lennart@poettering.net>
2020-10-15 17:33:04 +0200

    util: make size macros unsigned
    By making them unsigned comparing them with other sizes is less likely
    to trigger compiler warnings regarding signed/unsigned comparisons.
    After all sizes (i.e. size_t) are generally assumed to be unsigned, so
    these should be too.
    
    Prompted-by: https://github.com/systemd/systemd/pull/17345#issuecomment-709402332

--- src/basic/format-util.h
+++ src/basic/format-util.h
@@ -72,11 +72,14 @@ typedef enum {
         FORMAT_BYTES_TRAILING_B  = 1 << 2,
 } FormatBytesFlag;
 
-#define FORMAT_BYTES_MAX 16
+#define FORMAT_BYTES_MAX 16U
+
 char *format_bytes_full(char *buf, size_t l, uint64_t t, FormatBytesFlag flag);
+
 static inline char *format_bytes(char *buf, size_t l, uint64_t t) {
         return format_bytes_full(buf, l, t, FORMAT_BYTES_USE_IEC | FORMAT_BYTES_BELOW_POINT | FORMAT_BYTES_TRAILING_B);
 }
+
 static inline char *format_bytes_cgroup_protection(char *buf, size_t l, uint64_t t) {
         if (t == CGROUP_LIMIT_MAX) {
                 (void) snprintf(buf, l, "%s", "infinity");
