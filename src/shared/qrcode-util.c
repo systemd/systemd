f1b823596f8a4dc6b1f76206b7473bac71e3840b
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2020-10-25 12:11:21 +0100

    journal,homectl: unify implementations of libqrencode loading and fss key printing
    We had two of each: both homectl and journalctl had the whole dlopen()
    wrapper, and journalctl had two implementations (slightly different) of the
    code to print the fss:// pattern.
    
    print_qrcode() now returns -EOPNOTSUPP when compiled with qrcode support. Both
    callers ignore the return value, so this changes nothing.
    
    No functional change.

--- src/shared/qrcode-util.c
+++ src/shared/qrcode-util.c
@@ -1,4 +1,10 @@
 #include "qrcode-util.h"
+
+#if HAVE_QRENCODE
+#include <qrencode.h>
+
+#include "dlfcn-util.h"
+#include "locale-util.h"
 #include "terminal-util.h"
 
 #define ANSI_WHITE_ON_BLACK "\033[40;37;1m"
@@ -15,7 +21,7 @@ static void print_border(FILE *output, unsigned width) {
         }
 }
 
-void write_qrcode(FILE *output, QRcode *qr) {
+static void write_qrcode(FILE *output, QRcode *qr) {
         assert(qr);
 
         if (!output)
@@ -55,3 +61,45 @@ void write_qrcode(FILE *output, QRcode *qr) {
         print_border(output, qr->width);
         fflush(output);
 }
+
+int print_qrcode(FILE *out, const char *header, const char *string) {
+        QRcode* (*sym_QRcode_encodeString)(const char *string, int version, QRecLevel level, QRencodeMode hint, int casesensitive);
+        void (*sym_QRcode_free)(QRcode *qrcode);
+        _cleanup_(dlclosep) void *dl = NULL;
+        QRcode* qr;
+        int r;
+
+        /* If this is not an UTF-8 system or ANSI colors aren't supported/disabled don't print any QR
+         * codes */
+        if (!is_locale_utf8() || !colors_enabled())
+                return -EOPNOTSUPP;
+
+        dl = dlopen("libqrencode.so.4", RTLD_LAZY);
+        if (!dl)
+                return log_debug_errno(SYNTHETIC_ERRNO(EOPNOTSUPP),
+                                       "QRCODE support is not installed: %s", dlerror());
+
+        r = dlsym_many_and_warn(
+                        dl,
+                        LOG_DEBUG,
+                        &sym_QRcode_encodeString, "QRcode_encodeString",
+                        &sym_QRcode_free, "QRcode_free",
+                        NULL);
+        if (r < 0)
+                return r;
+
+        qr = sym_QRcode_encodeString(string, 0, QR_ECLEVEL_L, QR_MODE_8, 0);
+        if (!qr)
+                return -ENOMEM;
+
+        if (header)
+                fprintf(out, "\n%s:\n\n", header);
+
+        write_qrcode(out, qr);
+
+        fputc('\n', out);
+
+        sym_QRcode_free(qr);
+        return 0;
+}
+#endif
