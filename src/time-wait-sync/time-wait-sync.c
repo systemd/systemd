59a711c2cdb547e80c6f1fd18ab0dc412687cf6c
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2020-10-27 18:18:57 +0100

    time-wait-sync: drop gpl snail mail boilerplate
    No idea why this chunk was left when we removed the other copies.

--- src/time-wait-sync/time-wait-sync.c
+++ src/time-wait-sync/time-wait-sync.c
@@ -1,12 +1,5 @@
 /* SPDX-License-Identifier: LGPL-2.1+ */
-/*
- * systemd service to wait until kernel realtime clock is synchronized
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
- * USA
- */
+/* systemd service to wait until kernel realtime clock is synchronized */
 
 #include <errno.h>
 #include <stdbool.h>
