dbf4e471926efb000bacd17b34c58af6062a5caf
Lennart Poettering <lennart@poettering.net>
2020-11-06 14:56:16 +0100

    resolved: use stat_inode_unmodified() to detect /etc/hosts changes

--- src/resolve/resolved-manager.h
+++ src/resolve/resolved-manager.h
@@ -129,9 +129,8 @@ struct Manager {
 
         /* Data from /etc/hosts */
         EtcHosts etc_hosts;
-        usec_t etc_hosts_last, etc_hosts_mtime;
-        ino_t etc_hosts_ino;
-        dev_t etc_hosts_dev;
+        usec_t etc_hosts_last;
+        struct stat etc_hosts_stat;
         bool read_etc_hosts;
 
         OrderedSet *dns_extra_stub_listeners;
