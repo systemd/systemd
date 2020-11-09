dfcda3fcb5eed73b9ae68a86fabed18c45856f68
Lennart Poettering <lennart@poettering.net>
2020-11-09 22:22:56 +0100

    resolved: let's preferably route reverse lookups for local subnets to matching interfaces
    Let's preferably route traffic for reverse lookups to LLMNR/mDNS/DNS on
    the matching interface if the IP address is in the local subnet. Also,
    if looking up an IP address of our own host, let's avoid doing
    LLMNR/mDNS at all.
    
    This is useful if "~." is a routing domain to DNS, as it means, local
    reverse lookups still go to LLMNR/mDNS, too.
    
    Fixes: #16243 #10081

--- src/resolve/resolved-link.c
+++ src/resolve/resolved-link.c
@@ -834,6 +834,7 @@ int link_address_new(Link *l, LinkAddress **ret, int family, const union in_addr
                 .family = family,
                 .in_addr = *in_addr,
                 .link = l,
+                .prefixlen = UCHAR_MAX,
         };
 
         LIST_PREPEND(addresses, l->addresses, a);
@@ -1126,6 +1127,7 @@ fail:
 
 int link_address_update_rtnl(LinkAddress *a, sd_netlink_message *m) {
         int r;
+
         assert(a);
         assert(m);
 
@@ -1133,7 +1135,8 @@ int link_address_update_rtnl(LinkAddress *a, sd_netlink_message *m) {
         if (r < 0)
                 return r;
 
-        sd_rtnl_message_addr_get_scope(m, &a->scope);
+        (void) sd_rtnl_message_addr_get_prefixlen(m, &a->prefixlen);
+        (void) sd_rtnl_message_addr_get_scope(m, &a->scope);
 
         link_allocate_scopes(a->link);
         link_add_rrs(a->link, false);
