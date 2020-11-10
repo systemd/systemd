53eedd50117acda5ee4bd81e6129052f5d44a44f
Lennart Poettering <lennart@poettering.net>
2020-11-10 14:12:27 +0100

    git-contrib: exclude -rc tags

--- tools/git-contrib.sh
+++ tools/git-contrib.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eu
 
-git shortlog -s `git describe --abbrev=0`.. | \
+git shortlog -s `git describe --abbrev=0 --match 'v[0-9][0-9][0-9]'`.. | \
     awk '{ $1=""; print $0 "," }' | \
     sort -u
