cbf23f385381f1df2149b18d1fa2411356d76ad8
Lennart Poettering <lennart@poettering.net>
2020-11-18 13:52:59 +0100

    resolved: optimize change notification handling away if bus calls set the same values as were already set
    Prompted-by: #17577 (but doesn't fix this, since this commit only
    handles D-Bus-induced changes, not the ones made via networkd)

--- src/resolve/resolved-dns-search-domain.h
+++ src/resolve/resolved-dns-search-domain.h
@@ -44,7 +44,7 @@ void dns_search_domain_unlink(DnsSearchDomain *d);
 void dns_search_domain_move_back_and_unmark(DnsSearchDomain *d);
 
 void dns_search_domain_unlink_all(DnsSearchDomain *first);
-void dns_search_domain_unlink_marked(DnsSearchDomain *first);
+bool dns_search_domain_unlink_marked(DnsSearchDomain *first);
 void dns_search_domain_mark_all(DnsSearchDomain *first);
 
 int dns_search_domain_find(DnsSearchDomain *first, const char *name, DnsSearchDomain **ret);
