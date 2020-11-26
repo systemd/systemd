e77fed207a41a77f88853a89a8408fbfa9a17ddd
Yu Watanabe <watanabe.yu+github@gmail.com>
2020-11-27 05:59:56 +0900

    hwdb: enable diagnostic switches

--- hwdb.d/parse_hwdb.py
+++ hwdb.d/parse_hwdb.py
@@ -33,7 +33,7 @@ try:
                            OneOrMore, Combine, Or, Optional, Suppress, Group,
                            nums, alphanums, printables,
                            stringEnd, pythonStyleComment,
-                           ParseBaseException)
+                           ParseBaseException, __diag__)
 except ImportError:
     print('pyparsing is not available')
     sys.exit(77)
@@ -49,6 +49,12 @@ try:
 except ImportError:
     # don't do caching on old python
     lru_cache = lambda: (lambda f: f)
+
+__diag__.warn_multiple_tokens_in_named_alternation = True
+__diag__.warn_ungrouped_named_tokens_in_collection = True
+__diag__.warn_name_set_on_empty_Forward = True
+__diag__.warn_on_multiple_string_args_to_oneof = True
+__diag__.enable_debug_on_named_expressions = True
 
 EOL = LineEnd().suppress()
 EMPTYLINE = LineEnd()
