8c47d1103fb5afe8cd19d214d50ebe47a6860e02
Yu Watanabe <watanabe.yu+github@gmail.com>
2020-12-03 11:07:52 +0900

    basic/linux: update kernel headers
    From kernel commit 34816d20f173a90389c8a7e641166d8ea9dce70a.

--- src/basic/linux/update.sh
+++ src/basic/linux/update.sh
@@ -3,7 +3,11 @@
 set -eu
 
 for i in *.h */*.h; do
-    curl https://raw.githubusercontent.com/torvalds/linux/master/include/uapi/linux/$i -o $i
+    if [[ $i == 'loadavg.h' ]]; then
+        curl https://raw.githubusercontent.com/torvalds/linux/master/include/linux/sched/$i -o $i
+    else
+        curl https://raw.githubusercontent.com/torvalds/linux/master/include/uapi/linux/$i -o $i
+    fi
 
     sed -i -e 's/__user //g' -e '/^#include <linux\/compiler.h>/ d' $i
 done
