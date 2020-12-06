09422f9a28481eb5c49d375a6b5b7ca2b773c1ae
Daan De Meyer <daan.j.demeyer@gmail.com>
2020-12-06 18:16:59 +0000

    meson: Respect MESON_INSTALL_QUIET
    MESON_INSTALL_QUIET is set when --quiet is passed to meson install.
    Make sure we check the variable in our custom install scripts and
    don't output anything if it is set.

--- tools/meson-make-symlink.sh
+++ tools/meson-make-symlink.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 set -eu
 
+if [ "${MESON_INSTALL_QUIET:-0}" = 1 ] ; then
+    VERBOSE=""
+else
+    VERBOSE="v"
+fi
+
 # this is needed mostly because $DESTDIR is provided as a variable,
 # and we need to create the target directory...
 
-mkdir -vp "$(dirname "${DESTDIR:-}$2")"
+mkdir -${VERBOSE}p "$(dirname "${DESTDIR:-}$2")"
 if [ "$(dirname $1)" = . -o "$(dirname $1)" = .. ]; then
-    ln -vfs -T -- "$1" "${DESTDIR:-}$2"
+    ln -${VERBOSE}fs -T -- "$1" "${DESTDIR:-}$2"
 else
-    ln -vfs -T --relative -- "${DESTDIR:-}$1" "${DESTDIR:-}$2"
+    ln -${VERBOSE}fs -T --relative -- "${DESTDIR:-}$1" "${DESTDIR:-}$2"
 fi
