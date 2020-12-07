systemd/systemd

commit 87a4d416e5126b6fb2528ae192a6a6a8033539ce

sd-device: use extract_first_word()

src/libsystemd/sd-device/device-private.c
@@ -316,34 +316,33 @@ static int device_amend(sd_device *device, const char *key, const char *value) {
                 if (r < 0)
                         return log_device_debug_errno(device, r, "sd-device: Failed to set SEQNUM to '%s': %m", value);
         } else if (streq(key, "DEVLINKS")) {
-                const char *word, *state;
-                size_t l;
+                for (const char *p = value;;) {
+                        _cleanup_free_ char *word = NULL;
 
-                FOREACH_WORD(word, l, value, state) {
-                        char devlink[l + 1];
-
-                        strncpy(devlink, word, l);
-                        devlink[l] = '\0';
-
-                        r = device_add_devlink(device, devlink);
+                        r = extract_first_word(&p, &word, NULL, 0);
                         if (r < 0)
-                                return log_device_debug_errno(device, r, "sd-device: Failed to add devlink '%s': %m", devlink);
+                                return r;
+                        if (r == 0)
+                                break;
+
+                        r = device_add_devlink(device, word);
+                        if (r < 0)
+                                return log_device_debug_errno(device, r, "sd-device: Failed to add devlink '%s': %m", word);
                 }
         } else if (STR_IN_SET(key, "TAGS", "CURRENT_TAGS")) {
-                const char *word, *state;
-                size_t l;
+                for (const char *p = value;;) {
+                        _cleanup_free_ char *word = NULL;
 
-                FOREACH_WORD_SEPARATOR(word, l, value, ":", state) {
-                        char tag[l + 1];
-
-                        (void) strncpy(tag, word, l);
-                        tag[l] = '\0';
-
-                        r = device_add_tag(device, tag, streq(key, "CURRENT_TAGS"));
+                        r = extract_first_word(&p, &word, ":", EXTRACT_DONT_COALESCE_SEPARATORS);
                         if (r < 0)
-                                return log_device_debug_errno(device, r, "sd-device: Failed to add tag '%s': %m", tag);
-                }
+                                return r;
+                        if (r == 0)
+                                break;
 
+                        r = device_add_tag(device, word, streq(key, "CURRENT_TAGS"));
+                        if (r < 0)
+                                return log_device_debug_errno(device, r, "sd-device: Failed to add tag '%s': %m", word);
+                }
         } else {
                 r = device_add_property_internal(device, key, value);
                 if (r < 0)

commit 726f4d1267c4bef173addf57bf713ebccb1566de

sd-device: Retain escapes when parsing devlinks
Filesystem labels with forward slashes are generally fairly well
supported in Unix, and have been for a long time, so they are not
entirely uncommon. To that effect, there are systems with fstab entries
like the following:

    LABEL=rouji-/boot /boot vfat  defaults,noatime 0 1

These entries get represented with backslash escapes in
`/dev/disk/by-label` -- in this case,
`/dev/disk/by-label/rouji-\x2fboot` -- as you'd expect, / is the path
separator, so it needs to be encoded.

87a4d416 broke this, as it changes the way that DEVLINKS values with
backslashes are parsed -- previously backslashes were passed through
intact, but now they are silently dropped on the floor since we believe
them to be invalid escape sequences.

This all results in being dropped to an emergency shell on boot. To fix
that, turn off backslash munging, since DEVLINKS values should be passed
through intact.

src/libsystemd/sd-device/device-private.c
@@ -319,7 +319,9 @@ static int device_amend(sd_device *device, const char *key, const char *value) {
                 for (const char *p = value;;) {
                         _cleanup_free_ char *word = NULL;
 
-                        r = extract_first_word(&p, &word, NULL, 0);
+                        /* /dev/disk/by-label entries contain backslash escape sequences which must be
+                         * retained verbatim if the label contains forward slashes */
+                        r = extract_first_word(&p, &word, NULL, EXTRACT_RETAIN_ESCAPE);
                         if (r < 0)
                                 return r;
                         if (r == 0)