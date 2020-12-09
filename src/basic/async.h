1d9aa4d572b661fd6500c55ab524141332f76230
Lennart Poettering <lennart@poettering.net>
2020-12-09 10:07:12 +0100

    async: add trivial cleanup wrapper for asynchronous_close()

--- src/basic/async.h
+++ src/basic/async.h
@@ -1,7 +1,13 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 #pragma once
 
+#include <sys/types.h>
+
+#include "macro.h"
+
 int asynchronous_job(void* (*func)(void *p), void *arg);
 
 int asynchronous_sync(pid_t *ret_pid);
 int asynchronous_close(int fd);
+
+DEFINE_TRIVIAL_CLEANUP_FUNC(int, asynchronous_close);
