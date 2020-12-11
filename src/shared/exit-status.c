abcd208840069cc9e30fdad6b447cc9f28740cd1
Iago López Galeiras <iago@kinvolk.io>
2020-12-11 13:21:54 +0100

    exit-status: fix mappings comment
    It didn't reflect the current status.

--- src/shared/exit-status.c
+++ src/shared/exit-status.c
@@ -18,8 +18,8 @@ const ExitStatusMapping exit_status_mappings[256] = {
          *   8…63  │ (Currently unmapped)
          *  64…78  │ BSD defined exit codes
          *  79…199 │ (Currently unmapped)
-         * 200…242 │ systemd's private error codes (might be extended to 254 in future development)
-         * 243…254 │ (Currently unmapped, but see above)
+         * 200…243 │ systemd's private error codes (might be extended to 254 in future development)
+         * 244…254 │ (Currently unmapped, but see above)
          *
          *   255   │ EXIT_EXCEPTION (We use this to propagate exit-by-signal events. It's frequently used by others apps (like bash)
          *         │ to indicate exit reason that cannot really be expressed in a single exit status value — such as a propagated
