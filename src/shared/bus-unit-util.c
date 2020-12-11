386702f470ca1486c4ef6c8f14c203d4b91c2168
Iago López Galeiras <iago@kinvolk.io>
2020-12-11 13:20:23 +0100

    core: add dbus RestrictFileSystems= properties

--- src/shared/bus-unit-util.c
+++ src/shared/bus-unit-util.c
@@ -1323,6 +1323,7 @@ static int bus_append_execute_property(sd_bus_message *m, const char *field, con
         }
 
         if (STR_IN_SET(field, "RestrictAddressFamilies",
+                              "RestrictFileSystems",
                               "SystemCallFilter",
                               "SystemCallLog")) {
                 int allow_list = 1;
