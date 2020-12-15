9bd53aa872f0a08eb1e38475f52bccc18df14992
Christian Ehrhardt <christian.ehrhardt@canonical.com>
2020-12-15 12:05:14 +0100

    test: use modern qemu numa arguments
    Upgrading to qemu 5.2 breaks TEST-36-NUMAPOLICY like:
      qemu-system-x86_64: total memory for NUMA nodes (0x0) should
      equal RAM size (0x20000000)
    
    Use the new (as in >=2014) form of memdev in test 36:
     -object memory-backend-ram,id=mem0,size=512M -numa node,memdev=mem0,nodeid=0
    
    Since some target systems are as old as qemu 1.5.3 (CentOS7) but the new
    kind to specify was added in qemu 2.1 this needs to add version parsing and
    add the argument only when qemu is >=5.2.
    
    Fixes #17986.
    
    Signed-off-by: Christian Ehrhardt <christian.ehrhardt@canonical.com>

--- test/TEST-36-NUMAPOLICY/test.sh
+++ test/TEST-36-NUMAPOLICY/test.sh
@@ -3,7 +3,11 @@ set -e
 
 TEST_DESCRIPTION="test MUMAPolicy= and NUMAMask= options"
 TEST_NO_NSPAWN=1
-QEMU_OPTIONS="-numa node,nodeid=0"
 . $TEST_BASE_DIR/test-functions
+if qemu_min_version "5.2.0"; then
+    QEMU_OPTIONS="-object memory-backend-ram,id=mem0,size=512M -numa node,memdev=mem0,nodeid=0"
+else
+    QEMU_OPTIONS="-numa node,nodeid=0"
+fi
 
 do_test "$@" 36
