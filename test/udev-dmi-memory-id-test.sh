2b7b142e97b42e08bf7386e2afe69dfa12306daa
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2020-12-16 12:21:43 +0100

    meson: make each dmidecode a separate test
    This allows them to be executed in parallel and also gives us
    better reporting.
    
    The dump files are renamed to avoid repeating "dmidecode-dump", since that
    string is already present in the subdirectory name.

--- test/udev-dmi-memory-id-test.sh
+++ test/udev-dmi-memory-id-test.sh
@@ -1,31 +1,17 @@
 #!/bin/sh
 # SPDX-License-Identifier: LGPL-2.1-or-later
-#
 
 set -e
 
-export SYSTEMD_LOG_LEVEL=info
-ROOTDIR=$(dirname $(dirname $(readlink -f $0)))
-UDEV_DMI_MEMORY_ID=./src/udev/dmi_memory_id
+dmi_memory_id="$1"
+input="$2"
+expected="$3"
 
-if [ ! -x "$UDEV_DMI_MEMORY_ID" ]; then
-    echo "$UDEV_DMI_MEMORY_ID does not exist, please build first"
-    exit 1
-fi
+output=$(mktemp --tmpdir "test-udev-dmi-memory-id.XXXXXXXXXX")
+trap "rm '$output'" EXIT INT QUIT PIPE
 
-D=$(mktemp --tmpdir --directory "udev-dmi-memory-id.XXXXXXXXXX")
-trap "rm -rf '$D'" EXIT INT QUIT PIPE
-
-for i in $ROOTDIR/test/dmidecode-dumps/*.bin ; do
-    $("$UDEV_DMI_MEMORY_ID" -F "$i" 2>&1 > "$D"/out.txt) && rc= || rc=$?
-    if [ -n "$rc" ]; then
-        echo "$UDEV_DMI_MEMORY_ID returned $rc"
-        exit $rc
-    fi
-    err=$(diff -u "$D"/out.txt "$i.txt" 2>&1) && rc= || rc=$?
-    if [ -n "$rc" ]; then
-        echo "Parsing DMI memory information from \"$i\" didn't match expected:"
-        echo "$err"
-        exit $rc
-    fi
-done
+(
+    set -x
+    "$dmi_memory_id" -F "$input" >$output
+    diff -u "$output" "$expected"
+)
