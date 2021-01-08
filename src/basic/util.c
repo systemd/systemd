96cceb35e7985f5ee6c9b17e129a76259273cdde
Kairui Song <kasong@redhat.com>
2021-01-08 14:52:26 +0800

    util: rework in_initrd() to make use of path_is_temporary_fs()

--- src/basic/util.c
+++ src/basic/util.c
@@ -52,7 +52,6 @@ int prot_from_flags(int flags) {
 }
 
 bool in_initrd(void) {
-        struct statfs s;
         int r;
 
         if (saved_in_initrd >= 0)
@@ -76,8 +75,7 @@ bool in_initrd(void) {
                 saved_in_initrd = r > 0;
         else
                 saved_in_initrd = access("/etc/initrd-release", F_OK) >= 0 &&
-                                  statfs("/", &s) >= 0 &&
-                                  is_temporary_fs(&s);
+                                  path_is_temporary_fs("/") > 0;
 
         return saved_in_initrd;
 }
