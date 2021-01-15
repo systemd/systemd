79758570798dd82ab60cdae3f4ae1fa3f48823a1
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2021-01-15 13:02:44 +0100

    tree-wide: use curl --fail
    curl will save the 404 response page (or another error) if the page
    download fails, which we never want. Let it error out instead.

--- tools/syscall-table-update.sh
+++ tools/syscall-table-update.sh
@@ -3,8 +3,8 @@ set -eu
 
 cd "$1" && shift
 
-curl -L -o syscall-names.text 'https://raw.githubusercontent.com/hrw/syscalls-table/master/syscall-names.text'
+curl --fail -L -o syscall-names.text 'https://raw.githubusercontent.com/hrw/syscalls-table/master/syscall-names.text'
 
 for arch in "$@"; do
-    curl -L -o syscalls-$arch "https://raw.githubusercontent.com/hrw/syscalls-table/master/tables/syscalls-$arch"
+    curl --fail -L -o syscalls-$arch "https://raw.githubusercontent.com/hrw/syscalls-table/master/tables/syscalls-$arch"
 done
