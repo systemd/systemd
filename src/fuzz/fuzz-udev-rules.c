70ae2553accb5cc5b0035e24aa1ecceb551fb71c
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-01-17 13:37:14 +0900

    fuzz: set the default log level in fuzz-main.c

--- src/fuzz/fuzz-udev-rules.c
+++ src/fuzz/fuzz-udev-rules.c
@@ -15,9 +15,6 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
         _cleanup_(unlink_tempfilep) char filename[] = "/tmp/fuzz-udev-rules.XXXXXX";
         int r;
 
-        if (!getenv("SYSTEMD_LOG_LEVEL"))
-                log_set_max_level(LOG_CRIT);
-
         assert_se(fmkostemp_safe(filename, "r+", &f) == 0);
         if (size != 0)
                 assert_se(fwrite(data, size, 1, f) == 1);
--- src/fuzz/fuzz-unit-file.c
+++ src/fuzz/fuzz-unit-file.c
@@ -60,11 +60,6 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
 
         assert_se(fseek(f, offset, SEEK_SET) == 0);
 
-        /* We don't want to fill the logs with messages about parse errors.
-         * Disable most logging if not running standalone */
-        if (!getenv("SYSTEMD_LOG_LEVEL"))
-                log_set_max_level(LOG_CRIT);
-
         assert_se(manager_new(UNIT_FILE_SYSTEM, MANAGER_TEST_RUN_MINIMAL, &m) >= 0);
 
         name = strjoina("a.", unit_type_to_string(t));
