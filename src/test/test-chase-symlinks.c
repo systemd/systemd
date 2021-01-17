aa32a72111760863c59a97b4cb6048ab9f392419
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-01-17 15:12:28 +0900

    tree-wide: enable colorized logging for daemons

--- src/test/test-chase-symlinks.c
+++ src/test/test-chase-symlinks.c
@@ -84,7 +84,7 @@ static int parse_argv(int argc, char *argv[]) {
 static int run(int argc, char **argv) {
         int r;
 
-        log_setup_cli();
+        log_setup();
 
         r = parse_argv(argc, argv);
         if (r <= 0)
