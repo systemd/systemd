7f5e48cd3ef7e88c054aeaf8cdd7a30acbd0c811
bharathdoopati <sribharath.doopati@hpe.com>
2021-01-17 23:54:35 -0800

    Adding per daemon core-dump rate limit

---
+++ src/coredump/coredump-ratelimit.h
@@ -0,0 +1,20 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#pragma once
+
+#include <inttypes.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "time-util.h"
+
+/* The default core dump rate-limit interval */
+#define DEFAULT_COREDUMP_RATELIMIT_INTERVAL 0
+
+/* The default core dump rate-limit burst */
+#define DEFAULT_COREDUMP_RATELIMIT_BURST    0
+
+/* The default core dumps per boot max */
+#define DEFAULT_COREDUMPS_PER_BOOT_MAX      5
+
+int coredump_ratelimit(const char* comm, usec_t interval, unsigned burst, unsigned coredumpsperbootmax);
