84171ba17926c43b1aaabbcead8327824606cbf2
Benjamin Berg <bberg@redhat.com>
2021-01-19 14:51:44 +0100

    tools: Pass source directory to autosuspend-update.sh
    This makes sense so that we can extend the script to also update other
    files.

--- tools/autosuspend-update.sh
+++ tools/autosuspend-update.sh
@@ -4,4 +4,4 @@ set -eu
 cd "$1"
 
 (curl --fail -L 'https://chromium.googlesource.com/chromiumos/platform2/+/master/power_manager/udev/gen_autosuspend_rules.py?format=TEXT'; echo) \
-    | base64 -d > gen_autosuspend_rules.py
+    | base64 -d > tools/chromiumos/gen_autosuspend_rules.py
