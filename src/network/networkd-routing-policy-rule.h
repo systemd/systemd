7441ec938317216201574cc5628b73c0757870d0
Susant Sahani <ssahani@vmware.com>
2021-01-19 14:06:22 +0100

    network: Allow to configure RPDB rules from and to from DHCP ip or gw

--- src/network/networkd-routing-policy-rule.h
+++ src/network/networkd-routing-policy-rule.h
@@ -15,6 +15,13 @@ typedef struct Network Network;
 typedef struct Link Link;
 typedef struct Manager Manager;
 
+typedef enum DHCP4AddressType {
+        DHCP4_ADDRESS_TYPE_DHCP4_IP,
+        DHCP4_ADDRESS_TYPE_DHCP4_GATEWAY,
+        _DHCP4_ADDRESS_TYPE_MAX,
+        _DHCP4_ADDRESS_TYPE_INVALID = -1,
+} DHCP4AddressType;
+
 typedef struct RoutingPolicyRule {
         Manager *manager;
         Network *network;
@@ -34,6 +41,9 @@ typedef struct RoutingPolicyRule {
         uint32_t fwmark;
         uint32_t fwmask;
         uint32_t priority;
+
+        DHCP4AddressType to_type;
+        DHCP4AddressType from_type;
 
         AddressFamily address_family; /* Specified by Family= */
         int family; /* Automatically determined by From= or To= */
@@ -56,6 +66,7 @@ RoutingPolicyRule *routing_policy_rule_free(RoutingPolicyRule *rule);
 void network_drop_invalid_routing_policy_rules(Network *network);
 
 int link_set_routing_policy_rules(Link *link);
+int routing_policy_rule_configure(const RoutingPolicyRule *rule, Link *link);
 
 int manager_rtnl_process_rule(sd_netlink *rtnl, sd_netlink_message *message, Manager *m);
 int manager_drop_routing_policy_rules_internal(Manager *m, bool foreign, const Link *except);
