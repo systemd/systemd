efb37c0566f3ce04e14d8c49dc8d2f4220c55bf1
Benjamin Berg <bberg@redhat.com>
2021-01-19 14:59:42 +0100

    hwdb: Pull autosuspend rules from upstream libfprint
    libfprint generates a hwdb from drivers and known unsupported devices at
    build time. Pull this hwdb file from the artefacts generated by the
    upstream CI.
    
    Closes: #17663

--- tools/autosuspend-update.sh
+++ tools/autosuspend-update.sh
@@ -5,3 +5,12 @@ cd "$1"
 
 (curl --fail -L 'https://chromium.googlesource.com/chromiumos/platform2/+/master/power_manager/udev/gen_autosuspend_rules.py?format=TEXT'; echo) \
     | base64 -d > tools/chromiumos/gen_autosuspend_rules.py
+
+(cat <<%EOF
+# This file is part of systemd.
+#
+# Pulled from libfprint upstream in order to autosuspend fingerprint readers
+#
+%EOF
+curl -L 'https://gitlab.freedesktop.org/libfprint/libfprint/-/jobs/artifacts/master/raw/60-autosuspend-libfprint.hwdb?job=build') \
+    > hwdb.d/60-autosuspend-libfprint.hwdb
