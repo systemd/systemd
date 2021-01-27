fa3a225eeec0dd5cf21a73c5e33826ce449a2745
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2021-01-27 12:16:40 +0100

    generate-dns_type-gperf: modernize python syntax

--- src/resolve/generate-dns_type-gperf.py
+++ src/resolve/generate-dns_type-gperf.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
-"""Generate %-from-name.gperf from %-list.txt
-"""
+"Generate %-from-name.gperf from %-list.txt"
 
 import sys
 
@@ -13,12 +12,12 @@ print("""\
 _Pragma("GCC diagnostic ignored \\"-Wimplicit-fallthrough\\"")
 #endif
 %}""")
-print("""\
-struct {}_name {{ const char* name; int id; }};
+print(f"""\
+struct {name}_name {{ const char* name; int id; }};
 %null-strings
-%%""".format(name))
+%%""")
 
 for line in open(input):
     line = line.rstrip()
     s = line.replace('_', '-')
-    print("{}, {}{}".format(s, prefix, line))
+    print(f'{s}, {prefix}{line}')
