984b529684bf5597984b8d7ed8317b7d239b8dd3
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2021-01-27 13:56:12 +0100

    Use .txt as the extension for syscall list file
    Upstream uses .text, but this is rather unusual. Let's use .txt as the usual
    suffix for text files. This tells various editors and such that the file should
    be treated as plain text. I also want to a script to summarize license status,
    and having an easy-to-recognize suffix makes this easier.

--- tools/syscall-table-update.sh
+++ tools/syscall-table-update.sh
@@ -4,7 +4,7 @@ set -eu
 
 cd "$1" && shift
 
-curl --fail -L -o syscall-names.text 'https://raw.githubusercontent.com/hrw/syscalls-table/master/syscall-names.text'
+curl --fail -L -o syscall-list.txt 'https://raw.githubusercontent.com/hrw/syscalls-table/master/syscall-names.text'
 
 for arch in "$@"; do
     curl --fail -L -o syscalls-$arch "https://raw.githubusercontent.com/hrw/syscalls-table/master/tables/syscalls-$arch"
