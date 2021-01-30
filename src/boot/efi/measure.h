1863118f5b298e3d01d29a7dd68c11f53213c3e8
Daan De Meyer <daan.j.demeyer@gmail.com>
2021-01-30 17:21:48 +0000

    sd-boot: Add missing includes
    Let's make all headers self-sufficient by including the necessary
    EFI headers in the headers themselves.

--- src/boot/efi/measure.h
+++ src/boot/efi/measure.h
@@ -1,4 +1,6 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 #pragma once
 
+#include <efi.h>
+
 EFI_STATUS tpm_log_event(UINT32 pcrindex, const EFI_PHYSICAL_ADDRESS buffer, UINTN buffer_size, const CHAR16 *description);
