4e8bb1f34df25a96bab6e50a6f0267cf8c227bd8
Mauricio Vásquez <mauricio@kinvolk.io>
2021-02-02 07:48:52 -0500

    shared: add object name parameter to bpf_object_new
    The name is used for the names of the maps and other BPF objects, make
    it a parameter to make them deterministic when needed.
    
    Signed-off-by: Mauricio Vásquez <mauricio@kinvolk.io>

--- src/shared/bpf-object.h
+++ src/shared/bpf-object.h
@@ -10,7 +10,11 @@ typedef struct Set Set;
 
 int bpf_object_cpu_arch_supported(int arch) _const_;
 
-int bpf_object_new(const unsigned char *mem_buf, size_t size, struct bpf_object **object);
+int bpf_object_new(
+                const unsigned char *mem_buf,
+                size_t size,
+                const char *object_name,
+                struct bpf_object **object);
 struct bpf_object *bpf_object_free(struct bpf_object *object);
 
 int bpf_object_load(struct bpf_object *object);
