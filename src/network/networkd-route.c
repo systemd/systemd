d672b81bce633ee266b66820ed923f48a6526967
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-02-05 00:51:39 +0900

    network: add nexthop ID in debugging logs

--- src/network/networkd-route.c
+++ src/network/networkd-route.c
@@ -623,12 +623,13 @@ static void log_route_debug(const Route *route, const char *str, const Link *lin
                         (void) in_addr_to_string(route->family, &route->prefsrc, &prefsrc);
 
                 log_link_debug(link,
-                               "%s route: dst: %s%s, src: %s, gw: %s, prefsrc: %s, scope: %s, table: %s, proto: %s, type: %s",
+                               "%s route: dst: %s%s, src: %s, gw: %s, prefsrc: %s, scope: %s, table: %s, proto: %s, type: %s, nexthop: %"PRIu32,
                                str, strna(dst), strempty(dst_prefixlen), strna(src), strna(gw), strna(prefsrc),
                                format_route_scope(route->scope, scope, sizeof(scope)),
                                format_route_table(route->table, table, sizeof(table)),
                                format_route_protocol(route->protocol, protocol, sizeof(protocol)),
-                               strna(route_type_to_string(route->type)));
+                               strna(route_type_to_string(route->type)),
+                               route->nexthop_id);
         }
 }
 
