4f3f8242908f65ae345f0c6df6bcc13669792c29
Julia Kartseva <hex@fb.com>
2021-02-04 00:02:07 -0800

    shared: bpf_attach_type {from,to} string
    Introduce bpf_cgroup_attach_type_table with accustomed attached type
    names also used in bpftool.
    Add bpf_cgroup_attach_type_{from|to}_string helpers to convert from|to
    string representation of pinned bpf program, e.g.
    "egress:/sys/fs/bpf/egress-hook" for
    /sys/fs/bpf/egress-hook path and BPF_CGROUP_INET_EGRESS attach type.

--- src/shared/bpf-program.h
+++ src/shared/bpf-program.h
@@ -43,4 +43,7 @@ int bpf_map_new(enum bpf_map_type type, size_t key_size, size_t value_size, size
 int bpf_map_update_element(int fd, const void *key, void *value);
 int bpf_map_lookup_element(int fd, const void *key, void *value);
 
+int bpf_cgroup_attach_type_from_string(const char *str) _pure_;
+const char *bpf_cgroup_attach_type_to_string(int attach_type) _const_;
+
 DEFINE_TRIVIAL_CLEANUP_FUNC(BPFProgram*, bpf_program_unref);
