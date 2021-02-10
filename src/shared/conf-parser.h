1bf47ac06c49d1a2099b2cd15389aaf45cbfe6f4
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-02-11 03:27:22 +0900

    conf-parser: use return codes in xxx_from_string()
    Follow-up for #11484.

--- src/shared/conf-parser.h
+++ src/shared/conf-parser.h
@@ -207,7 +207,7 @@ typedef enum Disabled {
                                                                         \
                 x = from_string(rvalue);                                \
                 if (x < 0) {                                            \
-                        log_syntax(unit, LOG_WARNING, filename, line, 0, \
+                        log_syntax(unit, LOG_WARNING, filename, line, x, \
                                    msg ", ignoring: %s", rvalue);       \
                         return 0;                                       \
                 }                                                       \
@@ -235,7 +235,7 @@ typedef enum Disabled {
                                                                         \
                 x = name##_from_string(rvalue);                         \
                 if (x < 0) {                                            \
-                        log_syntax(unit, LOG_WARNING, filename, line, 0, \
+                        log_syntax(unit, LOG_WARNING, filename, line, x, \
                                    msg ", ignoring: %s", rvalue);       \
                         return 0;                                       \
                 }                                                       \
@@ -270,13 +270,13 @@ typedef enum Disabled {
                         if (r == -ENOMEM)                                      \
                                 return log_oom();                              \
                         if (r < 0)                                             \
-                                return log_syntax(unit, LOG_ERR, filename, line, 0,   \
+                                return log_syntax(unit, LOG_ERR, filename, line, r, \
                                                   msg ": %s", en);             \
                         if (r == 0)                                            \
                                 break;                                         \
                                                                                \
                         if ((x = name##_from_string(en)) < 0) {                \
-                                log_syntax(unit, LOG_WARNING, filename, line, 0,        \
+                                log_syntax(unit, LOG_WARNING, filename, line, x, \
                                            msg ", ignoring: %s", en);          \
                                 continue;                                      \
                         }                                                      \
