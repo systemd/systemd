2a74827b748114db01aabfde8610032389ba1e57
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2021-02-14 11:45:01 +0100

    Move and rename parse_json_argument() function
    json.[ch] is a very generic implementation, and cmdline argument parsing
    doesn't fit there.

--- src/shared/parse-argument.c
+++ src/shared/parse-argument.c
@@ -9,6 +9,27 @@
 
 /* All functions in this file emit warnigs. */
 
+int parse_json_argument(const char *s, JsonFormatFlags *ret) {
+        assert(s);
+        assert(ret);
+
+        if (streq(s, "pretty"))
+                *ret = JSON_FORMAT_PRETTY|JSON_FORMAT_COLOR_AUTO;
+        else if (streq(s, "short"))
+                *ret = JSON_FORMAT_NEWLINE;
+        else if (streq(s, "off"))
+                *ret = JSON_FORMAT_OFF;
+        else if (streq(s, "help")) {
+                puts("pretty\n"
+                     "short\n"
+                     "off");
+                return 0; /* 0 means → we showed a brief help, exit now */
+        } else
+                return log_error_errno(SYNTHETIC_ERRNO(EINVAL), "Unknown argument to --json= switch: %s", s);
+
+        return 1; /* 1 means → properly parsed */
+}
+
 int parse_path_argument(const char *path, bool suppress_root, char **arg) {
         char *p;
         int r;
