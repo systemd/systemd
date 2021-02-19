3f855b71e840a1a457d5be1c89fc6d804584410e
Mauricio Vásquez <mauricio@kinvolk.io>
2021-02-19 15:05:40 -0500

    updates for bpf link
    Partial cherry-pick from https://github.com/systemd/systemd/commit/d83120ab510b388e08610423d3ee3aba17adfaae.

--- src/shared/bpf-link.c
+++ src/shared/bpf-link.c
@@ -7,6 +7,19 @@
 #include "bpf-link.h"
 
 #if HAVE_LIBBPF
+int serialize_bpf_link(FILE *f, FDSet *fds, const char *key, BPFLink *link) {
+        int fd;
+
+        assert(key);
+        assert(link);
+
+        fd = bpf_link__fd(link);
+        if (fd < 0)
+                return -errno;
+
+        return serialize_fd(f, fds, key, fd);
+}
+
 BPFLink *bpf_link_free(BPFLink *link) {
         if (link && libbpf_get_error(link) == 0)
                (void) bpf_link__destroy(link);
@@ -14,6 +27,9 @@ BPFLink *bpf_link_free(BPFLink *link) {
         return NULL;
 }
 #else
+int serialize_bpf_link(FILE *f, FDSet *fds, const char *key, BPFLink *link) {
+        return 0;
+}
 BPFLink *bpf_link_free(BPFLink *link) {
         return NULL;
 }
