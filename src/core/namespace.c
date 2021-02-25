b33cd6b3eec52fc50c6c34d6f07a41cc6254c27f
Luca Boccassi <luca.boccassi@microsoft.com>
2021-02-25 18:49:53 +0000

    core: do not set noexec on sysfs/procfs
    It causes a regression in certain running environments (networkd under LXC),
    so avoid enabling for now.
    
    Fixes #18795
    
    Suggested-by: Topi Miettinen <toiwoton@gmail.com>

--- src/core/namespace.c
+++ src/core/namespace.c
@@ -272,7 +272,7 @@ static bool mount_entry_read_only(const MountEntry *p) {
 static bool mount_entry_noexec(const MountEntry *p) {
         assert(p);
 
-        return p->noexec || IN_SET(p->mode, NOEXEC, INACCESSIBLE, SYSFS, PROCFS);
+        return p->noexec || IN_SET(p->mode, NOEXEC, INACCESSIBLE);
 }
 
 static bool mount_entry_exec(const MountEntry *p) {
