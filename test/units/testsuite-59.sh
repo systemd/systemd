6a792a31fb9cb577fea5475ff69f9a4f8e9a4c33
Mauricio Vásquez <mauricio@kinvolk.io>
2021-02-25 19:59:36 -0500

    tests: add integration test for RestrictNetworkInterfaces=
    Signed-off-by: Mauricio Vásquez <mauricio@kinvolk.io>

---
+++ test/units/testsuite-59.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+set -ex
+set -o pipefail
+
+setup() {
+    systemd-analyze log-level debug
+    systemd-analyze log-target console
+
+    for i in `seq 0 3`;
+    do
+        ip netns del ns${i} || true
+        ip link del veth${i} || true
+        ip netns add ns${i}
+        ip link add veth${i} type veth peer name veth${i}_
+        ip link set veth${i}_ netns ns${i}
+        ip -n ns${i} link set dev veth${i}_ up
+        ip -n ns${i} link set dev lo up
+        ip -n ns${i} addr add "192.168.113."$((4*i+1))/30 dev veth${i}_
+        ip link set dev veth${i} up
+        ip addr add "192.168.113."$((4*i+2))/30 dev veth${i}
+    done
+}
+
+teardown() {
+    set +e
+
+    for i in `seq 0 3`;
+    do
+        ip netns del ns${i}
+        ip link del veth${i}
+    done
+
+    systemd-analyze log-level info
+}
+
+trap teardown EXIT
+setup
+
+systemctl start --wait testsuite-59-1.service
+systemctl start --wait testsuite-59-2.service
+systemctl start --wait testsuite-59-3.service
+systemctl start --wait testsuite-59-4.service
+systemctl start --wait testsuite-59-5.service
+
+echo OK > /testok
+
+exit 0
