systemd/systemd

commit 28a5065149c31a8689738ddcd1100502371a7c34

udev: use strv_split_newlines() to parse result of spawned command

src/udev/udev-rules.c
@@ -1730,7 +1730,8 @@ static int udev_rule_apply_token_to_event(
                 return token->op == OP_MATCH;
         }
         case TK_M_IMPORT_PROGRAM: {
-                char result[UDEV_LINE_SIZE], *line, *pos;
+                _cleanup_strv_free_ char **lines = NULL;
+                char result[UDEV_LINE_SIZE], **line;
 
                 (void) udev_event_apply_format(event, token->value, buf, sizeof(buf), false);
                 log_rule_debug(dev, rules, "Importing properties from results of '%s'", buf);
@@ -1744,18 +1745,18 @@ static int udev_rule_apply_token_to_event(
                         return token->op == OP_NOMATCH;
                 }
 
-                for (line = result; !isempty(line); line = pos) {
+                lines = strv_split_newlines(result);
+                if (!lines)
+                        return log_oom();
+
+                STRV_FOREACH(line, lines) {
                         char *key, *value;
 
-                        pos = strchr(line, '\n');
-                        if (pos)
-                                *pos++ = '\0';
-
-                        r = get_property_from_string(line, &key, &value);
+                        r = get_property_from_string(*line, &key, &value);
                         if (r < 0) {
                                 log_rule_debug_errno(dev, rules, r,
                                                      "Failed to parse key and value from '%s', ignoring: %m",
-                                                     line);
+                                                     *line);
                                 continue;
                         }
                         if (r == 0)

commit 86c783d93dafde1d175170b6366725ebfd148de6

udev: do not unescape command result
This fixes a bug introduced by 28a5065149c31a8689738ddcd1100502371a7c34.

Fixes RHBZ#1935062 (https://bugzilla.redhat.com/show_bug.cgi?id=1935062).

src/udev/udev-rules.c
@@ -1746,9 +1746,10 @@ static int udev_rule_apply_token_to_event(
                         return token->op == OP_NOMATCH;
                 }
 
-                lines = strv_split_newlines(result);
-                if (!lines)
-                        return log_oom();
+                r = strv_split_newlines_full(&lines, result, EXTRACT_RETAIN_ESCAPE);
+                if (r < 0)
+                        log_rule_warning_errno(dev, rules, r,
+                                               "Failed to extract lines from result of command \"%s\", ignoring: %m", buf);
 
                 STRV_FOREACH(line, lines) {
                         char *key, *value;