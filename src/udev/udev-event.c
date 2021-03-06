b7e0f227be0ad38e78aa0458f5f40438bea15725
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-03-06 20:45:17 +0900

    udev: refuse to enable inotify watch on remove event
    Some udev rule may erroneously set inotify watch on remove event.
    For safety, silently ignore such an inotify watch enablement.
    
    This also moves inotify watch enablement code to udev-event.c.

--- src/udev/udev-event.c
+++ src/udev/udev-event.c
@@ -1082,3 +1082,36 @@ void udev_event_execute_run(UdevEvent *event, usec_t timeout_usec, int timeout_s
                 }
         }
 }
+
+int udev_event_process_inotify_watch(UdevEvent *event, int inotify_fd) {
+        sd_device *dev;
+        int r;
+
+        assert(event);
+        assert(inotify_fd >= 0);
+
+        dev = event->dev;
+
+        assert(dev);
+
+        if (device_for_action(dev, SD_DEVICE_REMOVE))
+                return 0;
+
+        if (sd_device_get_devname(dev, NULL) < 0)
+                return 0;
+
+        if (!event->inotify_watch) {
+                (void) udev_watch_end(inotify_fd, dev);
+                return 0;
+        }
+
+        r = udev_watch_begin(inotify_fd, dev);
+        if (r < 0)
+                return r;
+
+        r = device_update_db(dev);
+        if (r < 0)
+                return log_device_debug_errno(dev, r, "Failed to update database under /run/udev/data/: %m");
+
+        return 0;
+}
