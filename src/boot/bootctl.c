fd981ff95cdd4c5bafe4846e1090bfda110c3833
наб <nabijaczleweli@nabijaczleweli.xyz>
2021-03-17 23:31:17 +0100

    squash! bootctl

--- src/boot/bootctl.c
+++ src/boot/bootctl.c
@@ -52,9 +52,7 @@ static bool arg_print_dollar_boot_path = false;
 static bool arg_touch_variables = true;
 static PagerFlags arg_pager_flags = 0;
 static bool arg_graceful = false;
-static enum { MID_NO, MID_YES, MID_AUTO, } arg_make_mid = MID_AUTO;
-
-static sd_id128_t machine_id;
+static int arg_make_machine_id_directory = -1;
 
 STATIC_DESTRUCTOR_REGISTER(arg_esp_path, freep);
 STATIC_DESTRUCTOR_REGISTER(arg_xbootldr_path, freep);
@@ -115,22 +113,18 @@ static int acquire_xbootldr(bool unprivileged_mode, sd_id128_t *ret_uuid) {
         return 1;
 }
 
-static int settle_make_mid_dir(void) {
+static void settle_make_machine_id_directory(void) {
         struct statfs stf;
-        int r;
 
-        if (arg_make_mid == MID_AUTO)
-                arg_make_mid = statfs("/etc/machine-id", &stf) >= 0 &&
-                                stf.f_type != TMPFS_MAGIC;
-        assert(arg_make_mid == MID_NO || arg_make_mid == MID_YES);
+        if (arg_make_machine_id_directory != -1)
+                return;
 
-        if (arg_make_mid) {
-                r = sd_id128_get_machine(&machine_id);
-                if (r < 0)
-                        return log_error_errno(r, "Failed to get machine id: %m");
-        }
+        arg_make_machine_id_directory = false;
 
-        return 0;
+        if (statfs("/etc/machine-id", &stf) < 0)
+                return;
+
+        arg_make_machine_id_directory = !is_temporary_fs(&stf);
 }
 
 /* search for "#### LoaderInfo: systemd-boot 218 ####" string inside the binary */
@@ -910,12 +904,18 @@ static int remove_subdirs(const char *root, const char *const *subdirs) {
 }
 
 static int remove_machine_id_directory(const char *root) {
+        sd_id128_t machine_id;
         char buf[SD_ID128_STRING_MAX];
+        int r;
 
         assert(root);
 
-        if (!arg_make_mid)
+        if (!arg_make_machine_id_directory)
                 return 0;
+
+        r = sd_id128_get_machine(&machine_id);
+        if (r != 0)
+                return log_error_errno(r, "Failed to get machine id: %m");
 
         return rmdir_one(root, sd_id128_to_string(machine_id, buf));
 }
@@ -1003,10 +1003,11 @@ static int remove_loader_variables(void) {
 }
 
 static int install_loader_config(const char *esp_path) {
-        char machine_string[SD_ID128_STRING_MAX];
         _cleanup_(unlink_and_freep) char *t = NULL;
         _cleanup_fclose_ FILE *f = NULL;
         _cleanup_close_ int fd = -1;
+        sd_id128_t machine_id;
+        char machine_string[SD_ID128_STRING_MAX];
         const char *p;
         int r;
 
@@ -1024,8 +1025,13 @@ static int install_loader_config(const char *esp_path) {
 
         fprintf(f, "#timeout 3\n"
                    "#console-mode keep\n");
-        if (arg_make_mid)
+        if (arg_make_machine_id_directory) {
+                r = sd_id128_get_machine(&machine_id);
+                if (r != 0)
+                        return log_error_errno(r, "Failed to get machine id: %m");
+
                 fprintf(f, "default %s-*\n", sd_id128_to_string(machine_id, machine_string));
+        }
 
         r = fflush_sync_and_check(f);
         if (r < 0)
@@ -1042,12 +1048,18 @@ static int install_loader_config(const char *esp_path) {
 }
 
 static int install_machine_id_directory(const char *root) {
+        sd_id128_t machine_id;
         char buf[SD_ID128_STRING_MAX];
+        int r;
 
         assert(root);
 
-        if (!arg_make_mid)
+        if (!arg_make_machine_id_directory)
                 return 0;
+
+        r = sd_id128_get_machine(&machine_id);
+        if (r != 0)
+                return log_error_errno(r, "Failed to get machine id: %m");
 
         return mkdir_one(root, sd_id128_to_string(machine_id, buf));
 }
@@ -1089,7 +1101,8 @@ static int help(int argc, char *argv[], void *userdata) {
                "     --no-pager        Do not pipe output into a pager\n"
                "     --graceful        Don't fail when the ESP cannot be found or EFI\n"
                "                       variables cannot be written\n"
-               "     --make-machine-id-directory[=yes|no|auto] (Don't) create \\$MACHINE_ID\n"
+               "     --make-machine-id-directory[=yes|no|auto]\n"
+               "                       (Don't) create $BOOT/$MACHINE_ID\n"
                "\nSee the %2$s for details.\n",
                program_invocation_short_name,
                link,
@@ -1109,22 +1122,22 @@ static int parse_argv(int argc, char *argv[]) {
                 ARG_NO_VARIABLES,
                 ARG_NO_PAGER,
                 ARG_GRACEFUL,
-                ARG_MAKE_MID,
+                ARG_MAKE_MACHINE_ID_DIRECTORY,
         };
 
         static const struct option options[] = {
-                { "help",                      no_argument,       NULL, 'h'                 },
-                { "version",                   no_argument,       NULL, ARG_VERSION         },
-                { "esp-path",                  required_argument, NULL, ARG_ESP_PATH        },
-                { "path",                      required_argument, NULL, ARG_ESP_PATH        }, /* Compatibility alias */
-                { "boot-path",                 required_argument, NULL, ARG_BOOT_PATH       },
-                { "print-esp-path",            no_argument,       NULL, 'p'                 },
-                { "print-path",                no_argument,       NULL, 'p'                 }, /* Compatibility alias */
-                { "print-boot-path",           no_argument,       NULL, 'x'                 },
-                { "no-variables",              no_argument,       NULL, ARG_NO_VARIABLES    },
-                { "no-pager",                  no_argument,       NULL, ARG_NO_PAGER        },
-                { "graceful",                  no_argument,       NULL, ARG_GRACEFUL        },
-                { "make-machine-id-directory", optional_argument, NULL, ARG_MAKE_MID        },
+                { "help",                      no_argument,       NULL, 'h'                           },
+                { "version",                   no_argument,       NULL, ARG_VERSION                   },
+                { "esp-path",                  required_argument, NULL, ARG_ESP_PATH                  },
+                { "path",                      required_argument, NULL, ARG_ESP_PATH                  }, /* Compatibility alias */
+                { "boot-path",                 required_argument, NULL, ARG_BOOT_PATH                 },
+                { "print-esp-path",            no_argument,       NULL, 'p'                           },
+                { "print-path",                no_argument,       NULL, 'p'                           }, /* Compatibility alias */
+                { "print-boot-path",           no_argument,       NULL, 'x'                           },
+                { "no-variables",              no_argument,       NULL, ARG_NO_VARIABLES              },
+                { "no-pager",                  no_argument,       NULL, ARG_NO_PAGER                  },
+                { "graceful",                  no_argument,       NULL, ARG_GRACEFUL                  },
+                { "make-machine-id-directory", optional_argument, NULL, ARG_MAKE_MACHINE_ID_DIRECTORY },
                 {}
         };
 
@@ -1181,16 +1194,18 @@ static int parse_argv(int argc, char *argv[]) {
                         arg_graceful = true;
                         break;
 
-                case ARG_MAKE_MID:
-                        if (!optarg || !strncmp(optarg, "yes", strlen("yes")))
-                                arg_make_mid = MID_YES;
-                        else if (!strncmp(optarg, "no", strlen("no")))
-                                arg_make_mid = MID_NO;
-                        else if (!strncmp(optarg, "auto", strlen("auto")))
-                                arg_make_mid = MID_AUTO;  /* default */
-                        else
-                                return log_error_errno(SYNTHETIC_ERRNO(EINVAL),
-                                                       "--make-machine-id-directory takes nothing/yes/no/auto");
+                case ARG_MAKE_MACHINE_ID_DIRECTORY:
+                        if (!optarg)
+                                arg_make_machine_id_directory = true;
+                        else if (streq(optarg, "auto"))
+                                arg_make_machine_id_directory = -1;  /* default */
+                        else {
+                                r = parse_boolean(optarg);
+                                if (r < 0)
+                                        return log_error_errno(r, "--make-machine-id-directory= takes yes/no/auto");
+
+                                arg_make_machine_id_directory = r;
+                        }
                         break;
 
                 case '?':
@@ -1591,9 +1606,7 @@ static int verb_install(int argc, char *argv[], void *userdata) {
         if (r < 0)
                 return r;
 
-        r = settle_make_mid_dir();
-        if (r < 0)
-                return r;
+        settle_make_machine_id_directory();
 
         install = streq(argv[0], "install");
 
@@ -1653,9 +1666,7 @@ static int verb_remove(int argc, char *argv[], void *userdata) {
         if (r < 0)
                 return r;
 
-        r = settle_make_mid_dir();
-        if (r < 0)
-                return r;
+        settle_make_machine_id_directory();
 
         r = remove_binaries(arg_esp_path);
 
