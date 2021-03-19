systemd/systemd

commit 0b1f3c768ce1bd1490a5e53f539976dcef8ca765

tree-wide: reopen log when we need to log in FORK_CLOSE_ALL_FDS children
In a number of occasions we use FORK_CLOSE_ALL_FDS when forking off a
child, since we don't want to pass fds to the processes spawned (either
because we later want to execve() some other process there, or because
our child might hang around for longer than expected, in which case it
shouldn't keep our fd pinned). This also closes any logging fds, and
thus means logging is turned off in the child. If we want to do proper
logging, explicitly reopen the logs hence in the child at the right
time.

This is particularly crucial in the umount/remount children we fork off
the shutdown binary, as otherwise the children can't log, which is
why #8155 is harder to debug than necessary: the log messages we
generate about failing mount() system calls aren't actually visible on
screen, as they done in the child processes where the log fds are
closed.

src/coredump/coredumpctl.c
@@ -932,6 +932,7 @@ static int run_gdb(sd_journal *j) {
                 goto finish;
         if (r == 0) {
                 execlp("gdb", "gdb", exe, path, NULL);
+                log_open();
                 log_error_errno(errno, "Failed to invoke gdb: %m");
                 _exit(EXIT_FAILURE);
         }

src/delta/delta.c
@@ -191,6 +191,7 @@ static int found_override(const char *top, const char *bottom) {
                 return r;
         if (r == 0) {
                 execlp("diff", "diff", "-us", "--", bottom, top, NULL);
+                log_open();
                 log_error_errno(errno, "Failed to execute diff: %m");
                 _exit(EXIT_FAILURE);
         }

src/login/inhibit.c
@@ -272,6 +272,7 @@ int main(int argc, char *argv[]) {
                 if (r == 0) {
                         /* Child */
                         execvp(argv[optind], argv + optind);
+                        log_open();
                         log_error_errno(errno, "Failed to execute %s: %m", argv[optind]);
                         _exit(EXIT_FAILURE);
                 }

src/core/umount.c
@@ -390,7 +390,7 @@ static int remount_with_timeout(MountPoint *m, char *options, int *n_failed) {
          * fork a child process and set a timeout. If the timeout
          * lapses, the assumption is that that particular remount
          * failed. */
-        r = safe_fork("(sd-remount)", FORK_RESET_SIGNALS|FORK_CLOSE_ALL_FDS|FORK_LOG, &pid);
+        r = safe_fork("(sd-remount)", FORK_RESET_SIGNALS|FORK_CLOSE_ALL_FDS|FORK_LOG|FORK_REOPEN_LOG, &pid);
         if (r < 0)
                 return r;
         if (r == 0) {
@@ -426,7 +426,7 @@ static int umount_with_timeout(MountPoint *m, bool *changed) {
          * fork a child process and set a timeout. If the timeout
          * lapses, the assumption is that that particular umount
          * failed. */
-        r = safe_fork("(sd-umount)", FORK_RESET_SIGNALS|FORK_CLOSE_ALL_FDS|FORK_LOG, &pid);
+        r = safe_fork("(sd-umount)", FORK_RESET_SIGNALS|FORK_CLOSE_ALL_FDS|FORK_LOG|FORK_REOPEN_LOG, &pid);
         if (r < 0)
                 return r;
         if (r == 0) {

commit 3525e0327a21afa393a84c090d6e4ee26c7e7608

tree-wide: save errno over log_open() calls
Commit 0b1f3c768ce1bd1490a5e53f539976dcef8ca765 has introduced log_open()
calls after exec fails post-fork. However, the log_open() call itself can
change the value of errno, which, for me, manifested in:

$ coredumpctl gdb
...
Failed to invoke gdb: Success

Fix this by saving errno over the log_open() calls. An alternative solution
would be to add PROTECT_ERRNO to log_open().

src/coredump/coredumpctl.c
@@ -1120,9 +1120,13 @@ static int run_debug(int argc, char **argv, void *userdata) {
         if (r < 0)
                 goto finish;
         if (r == 0) {
+                int saved_errno;
+
                 execvp(debugger_call[0], debugger_call);
+
+                saved_errno = errno;
                 log_open();
-                log_error_errno(errno, "Failed to invoke %s: %m", debugger_call[0]);
+                log_error_errno(saved_errno, "Failed to invoke %s: %m", debugger_call[0]);
                 _exit(EXIT_FAILURE);
         }
 

src/delta/delta.c
@@ -173,9 +173,13 @@ static int found_override(const char *top, const char *bottom) {
         if (r < 0)
                 return r;
         if (r == 0) {
+                int saved_errno;
+
                 execlp("diff", "diff", "-us", "--", bottom, top, NULL);
+
+                saved_errno = errno;
                 log_open();
-                log_error_errno(errno, "Failed to execute diff: %m");
+                log_error_errno(saved_errno, "Failed to execute diff: %m");
                 _exit(EXIT_FAILURE);
         }
 

src/login/inhibit.c
@@ -321,9 +321,13 @@ static int run(int argc, char *argv[]) {
                         return r;
                 if (r == 0) {
                         /* Child */
+                        int saved_errno;
+
                         execvp(arguments[0], arguments);
+
+                        saved_errno = errno;
                         log_open();
-                        log_error_errno(errno, "Failed to execute %s: %m", argv[optind]);
+                        log_error_errno(saved_errno, "Failed to execute %s: %m", argv[optind]);
                         _exit(EXIT_FAILURE);
                 }
 