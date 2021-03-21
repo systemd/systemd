a74bc3a51fe432c7e70f2cc8dd909a88c9f4adf9
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-03-21 11:37:04 +0900

    resolve: dns_answer_contains() does not return negative errno

--- src/resolve/resolved-dns-answer.h
+++ src/resolve/resolved-dns-answer.h
@@ -51,9 +51,9 @@ int dns_answer_add_extend(DnsAnswer **a, DnsResourceRecord *rr, int ifindex, Dns
 int dns_answer_add_soa(DnsAnswer *a, const char *name, uint32_t ttl, int ifindex);
 
 int dns_answer_match_key(DnsAnswer *a, const DnsResourceKey *key, DnsAnswerFlags *ret_flags);
-int dns_answer_contains_nsec_or_nsec3(DnsAnswer *a);
+bool dns_answer_contains_nsec_or_nsec3(DnsAnswer *a);
 int dns_answer_contains_zone_nsec3(DnsAnswer *answer, const char *zone);
-int dns_answer_contains(DnsAnswer *answer, DnsResourceRecord *rr);
+bool dns_answer_contains(DnsAnswer *answer, DnsResourceRecord *rr);
 
 int dns_answer_find_soa(DnsAnswer *a, const DnsResourceKey *key, DnsResourceRecord **ret, DnsAnswerFlags *ret_flags);
 int dns_answer_find_cname_or_dname(DnsAnswer *a, const DnsResourceKey *key, DnsResourceRecord **ret, DnsAnswerFlags *ret_flags);
