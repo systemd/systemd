22592b6837c691ba2f8cacca6e305208f3ecf837
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-03-21 11:09:11 +0900

    resolve: rename function argument in prototype to match its declaration

--- src/resolve/resolved-dns-answer.h
+++ src/resolve/resolved-dns-answer.h
@@ -50,7 +50,7 @@ int dns_answer_add(DnsAnswer *a, DnsResourceRecord *rr, int ifindex, DnsAnswerFl
 int dns_answer_add_extend(DnsAnswer **a, DnsResourceRecord *rr, int ifindex, DnsAnswerFlags flags, DnsResourceRecord *rrsig);
 int dns_answer_add_soa(DnsAnswer *a, const char *name, uint32_t ttl, int ifindex);
 
-int dns_answer_match_key(DnsAnswer *a, const DnsResourceKey *key, DnsAnswerFlags *combined_flags);
+int dns_answer_match_key(DnsAnswer *a, const DnsResourceKey *key, DnsAnswerFlags *ret_flags);
 int dns_answer_contains_nsec_or_nsec3(DnsAnswer *a);
 int dns_answer_contains_zone_nsec3(DnsAnswer *answer, const char *zone);
 int dns_answer_contains(DnsAnswer *answer, DnsResourceRecord *rr);
