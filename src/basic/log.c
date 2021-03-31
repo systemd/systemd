01337a74eb6d143426c7d4f929456c8f746ec871
Lennart Poettering <lennart@poettering.net>
2021-03-31 10:25:55 +0200

    log: drop some unused function arguments

--- src/basic/log.c
+++ src/basic/log.c
@@ -367,10 +367,8 @@ void log_set_facility(int facility) {
 
 static int write_to_console(
                 int level,
-                int error,
                 const char *file,
                 int line,
-                const char *func,
                 const char *buffer) {
 
         char location[256],
@@ -444,10 +442,6 @@ static int write_to_console(
 
 static int write_to_syslog(
                 int level,
-                int error,
-                const char *file,
-                int line,
-                const char *func,
                 const char *buffer) {
 
         char header_priority[2 + DECIMAL_STR_MAX(int) + 1],
@@ -504,10 +498,6 @@ static int write_to_syslog(
 
 static int write_to_kmsg(
                 int level,
-                int error,
-                const char *file,
-                int line,
-                const char *func,
                 const char *buffer) {
 
         /* Set a ratelimit on the amount of messages logged to /dev/kmsg. This is mostly supposed to be a
@@ -713,7 +703,7 @@ int log_dispatch_internal(
                 if (IN_SET(log_target, LOG_TARGET_SYSLOG_OR_KMSG,
                                        LOG_TARGET_SYSLOG)) {
 
-                        k = write_to_syslog(level, error, file, line, func, buffer);
+                        k = write_to_syslog(level, buffer);
                         if (k < 0 && k != -EAGAIN)
                                 log_close_syslog();
                 }
@@ -727,7 +717,7 @@ int log_dispatch_internal(
                         if (k < 0)
                                 log_open_kmsg();
 
-                        k = write_to_kmsg(level, error, file, line, func, buffer);
+                        k = write_to_kmsg(level, buffer);
                         if (k < 0) {
                                 log_close_kmsg();
                                 (void) log_open_console();
@@ -735,7 +725,7 @@ int log_dispatch_internal(
                 }
 
                 if (k <= 0)
-                        (void) write_to_console(level, error, file, line, func, buffer);
+                        (void) write_to_console(level, file, line, buffer);
 
                 buffer = e;
         } while (buffer);
