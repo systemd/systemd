4de5ad8e715555274c08eb9a1567579b64c2e528
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-04-13 12:41:23 +0900

    network: reindent and rebreak comment

--- src/network/networkd-network.c
+++ src/network/networkd-network.c
@@ -45,38 +45,28 @@
 void network_apply_anonymize_if_set(Network *network) {
         if (!network->dhcp_anonymize)
                 return;
-        /* RFC7844 3.7
-         SHOULD NOT send the Host Name option */
+
+        /* RFC7844 3.7: SHOULD NOT send the Host Name option */
         network->dhcp_send_hostname = false;
-        /* RFC7844 section 3.:
-         MAY contain the Client Identifier option
-         Section 3.5:
-         clients MUST use client identifiers based solely
-         on the link-layer address */
-        /* NOTE: Using MAC, as it does not reveal extra information,
-        * and some servers might not answer if this option is not sent */
+        /* RFC7844 section 3.: MAY contain the Client Identifier option
+         * Section 3.5: clients MUST use client identifiers based solely on the link-layer address
+         * NOTE: Using MAC, as it does not reveal extra information, and some servers might not answer
+         * if this option is not sent */
         network->dhcp_client_identifier = DHCP_CLIENT_ID_MAC;
-        /* RFC 7844 3.10:
-         SHOULD NOT use the Vendor Class Identifier option */
+        /* RFC7844 3.10: SHOULD NOT use the Vendor Class Identifier option */
         network->dhcp_vendor_class_identifier = mfree(network->dhcp_vendor_class_identifier);
-        /* RFC7844 section 3.6.:
-         The client intending to protect its privacy SHOULD only request a
-         minimal number of options in the PRL and SHOULD also randomly shuffle
-         the ordering of option codes in the PRL. If this random ordering
-         cannot be implemented, the client MAY order the option codes in the
-         PRL by option code number (lowest to highest).
-        */
-        /* NOTE: dhcp_use_mtu is false by default,
-        * though it was not initiallized to any value in network_load_one.
-        * Maybe there should be another var called *send*?
-        * (to use the MTU sent by the server but to do not send
-        * the option in the PRL). */
+        /* RFC7844 section 3.6.: The client intending to protect its privacy SHOULD only request a
+         * minimal number of options in the PRL and SHOULD also randomly shuffle the ordering of option
+         * codes in the PRL. If this random ordering cannot be implemented, the client MAY order the
+         * option codes in the PRL by option code number (lowest to highest).
+         *
+         * TODO: Maybe there should be another variable called dhcp_request_mtu (to use the MTU sent by
+         * the server but to do not request the option in the PRL)? */
         network->dhcp_use_mtu = false;
-        /* NOTE: when Anonymize=yes, the PRL route options are sent by default,
-         * but this is needed to use them. */
+        /* NOTE: when Anonymize=yes, the PRL route options are sent by default, but this is needed to
+         * use them. */
         network->dhcp_use_routes = true;
-        /* RFC7844 section 3.6.
-        * same comments as previous option */
+        /* RFC7844 section 3.6: same comments as previous option */
         network->dhcp_use_timezone = false;
 }
 
