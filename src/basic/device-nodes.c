e8ed567a4b43def389364f1ada15535ec629ad27
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-04-14 15:07:12 +0900

    util: shorten allow_listed_char_for_devnode()

--- src/basic/device-nodes.c
+++ src/basic/device-nodes.c
@@ -8,15 +8,12 @@
 #include "utf8.h"
 
 int allow_listed_char_for_devnode(char c, const char *white) {
-
-        if ((c >= '0' && c <= '9') ||
-            (c >= 'A' && c <= 'Z') ||
-            (c >= 'a' && c <= 'z') ||
-            strchr("#+-.:=@_", c) != NULL ||
-            (white != NULL && strchr(white, c) != NULL))
-                return 1;
-
-        return 0;
+        return
+                (c >= '0' && c <= '9') ||
+                (c >= 'A' && c <= 'Z') ||
+                (c >= 'a' && c <= 'z') ||
+                strchr("#+-.:=@_", c) ||
+                (white && strchr(white, c));
 }
 
 int encode_devnode_name(const char *str, char *str_enc, size_t len) {
@@ -31,7 +28,7 @@ int encode_devnode_name(const char *str, char *str_enc, size_t len) {
                 seqlen = utf8_encoded_valid_unichar(str + i, SIZE_MAX);
                 if (seqlen > 1) {
 
-                        if (len-j < (size_t)seqlen)
+                        if (len-j < (size_t) seqlen)
                                 return -EINVAL;
 
                         memcpy(&str_enc[j], &str[i], seqlen);
