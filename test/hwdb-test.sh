8bc7fed84e548387337db3d253e032674bb71673
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2021-04-15 08:20:31 +0200

    hwdb-test: pass an explit path to systemd-hwdb
    https://github.com/systemd/systemd/pull/19316 failed with:
    [1065/1670] Linking target systemd-hwdb
    --- command ---
    14:28:29 /root/src/test/hwdb-test.sh
    --- stdout ---
    ./systemd-hwdb does not exist, please build first
    
    I'm not sure what is going on here… In principle meson says that tests may be
    called from any directory, but in practice is was always the build directory.
    So far we were relying on systemd-hwdb being present in '.', and this worked.
    Either way, it's nicer to pass the exact path, so let's do that.

--- test/hwdb-test.sh
+++ test/hwdb-test.sh
@@ -11,10 +11,10 @@ set -e
 
 export SYSTEMD_LOG_LEVEL=info
 ROOTDIR=$(dirname $(dirname $(readlink -f $0)))
-SYSTEMD_HWDB=./systemd-hwdb
+SYSTEMD_HWDB="${1:?missing argument}"
 
 if [ ! -x "$SYSTEMD_HWDB" ]; then
-    echo "$SYSTEMD_HWDB does not exist, please build first"
+    echo "$SYSTEMD_HWDB is not executable" >&2
     exit 1
 fi
 
