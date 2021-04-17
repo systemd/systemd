9fe0b7b493bc3f878b047eba7d9360bbf9a24357
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-04-18 02:32:51 +0900

    network: dhcp6-pd: add RouteMetric= setting in [DHCPv6PrefixDelegation]

--- src/network/networkd-network.h
+++ src/network/networkd-network.h
@@ -219,6 +219,7 @@ struct Network {
         bool dhcp6_pd_assign;
         bool dhcp6_pd_manage_temporary_address;
         int64_t dhcp6_pd_subnet_id;
+        uint32_t dhcp6_pd_route_metric;
         union in_addr_union dhcp6_pd_token;
 
         /* Bridge Support */
