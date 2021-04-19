a6a58f273def870acca09db7e18bd526543837dd
Frantisek Sumsal <frantisek@sumsal.cz>
2021-04-19 13:01:59 +0200

    test: make the test entrypoint scripts shellcheck-compliant

--- test/TEST-09-ISSUE-2691/test.sh
+++ test/TEST-09-ISSUE-2691/test.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 set -e
+
 TEST_DESCRIPTION="https://github.com/systemd/systemd/issues/2691"
 TEST_NO_NSPAWN=1
 
-. $TEST_BASE_DIR/test-functions
+# shellcheck source=test/test-functions
+. "${TEST_BASE_DIR:?}/test-functions"
+
 QEMU_TIMEOUT=300
 
 do_test "$@" 09
--- test/TEST-44-LOG-NAMESPACE/test.sh
+++ test/TEST-44-LOG-NAMESPACE/test.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -e
+
 TEST_DESCRIPTION="test log namespaces"
 
-. $TEST_BASE_DIR/test-functions
+# shellcheck source=test/test-functions
+. "${TEST_BASE_DIR:?}/test-functions"
 
 do_test "$@" 44
