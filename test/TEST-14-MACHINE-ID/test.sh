3f161ba9bc43e68e5ac3dd9ef18309e6e88bee89
Frantisek Sumsal <frantisek@sumsal.cz>
2021-04-19 13:01:59 +0200

    test: make the test entrypoint scripts shellcheck-compliant

--- test/TEST-14-MACHINE-ID/test.sh
+++ test/TEST-14-MACHINE-ID/test.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 set -e
+
 TEST_DESCRIPTION="/etc/machine-id testing"
 IMAGE_NAME="badid"
 TEST_NO_NSPAWN=1
 
-. $TEST_BASE_DIR/test-functions
+# shellcheck source=test/test-functions
+. "${TEST_BASE_DIR:?}/test-functions"
 
 test_append_files() {
-    printf "556f48e837bc4424a710fa2e2c9d3e3c\ne3d\n" >$1/etc/machine-id
+    printf "556f48e837bc4424a710fa2e2c9d3e3c\ne3d\n" >"${1:?}/etc/machine-id"
 }
 
 do_test "$@" 14
