8535421f232dda742e8f2bcb01a0f9c83f3d7906
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-04-20 21:12:22 +0900

    in-addr-util: add assertions

--- src/basic/in-addr-util.c
+++ src/basic/in-addr-util.c
@@ -803,12 +803,18 @@ int in_addr_prefix_from_string_auto_internal(
 }
 
 static void in_addr_data_hash_func(const struct in_addr_data *a, struct siphash *state) {
+        assert(a);
+        assert(state);
+
         siphash24_compress(&a->family, sizeof(a->family), state);
         siphash24_compress(&a->address, FAMILY_ADDRESS_SIZE(a->family), state);
 }
 
 static int in_addr_data_compare_func(const struct in_addr_data *x, const struct in_addr_data *y) {
         int r;
+
+        assert(x);
+        assert(y);
 
         r = CMP(x->family, y->family);
         if (r != 0)
@@ -821,11 +827,15 @@ DEFINE_HASH_OPS(in_addr_data_hash_ops, struct in_addr_data, in_addr_data_hash_fu
 
 void in6_addr_hash_func(const struct in6_addr *addr, struct siphash *state) {
         assert(addr);
+        assert(state);
 
         siphash24_compress(addr, sizeof(*addr), state);
 }
 
 int in6_addr_compare_func(const struct in6_addr *a, const struct in6_addr *b) {
+        assert(a);
+        assert(b);
+
         return memcmp(a, b, sizeof(*a));
 }
 
