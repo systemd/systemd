b24eff80a4808ce15a17da963c99d07d0a631c75
Frantisek Sumsal <frantisek@sumsal.cz>
2021-04-22 16:26:25 +0200

    test: configure swap for TEST-55-OOMD
    oomd works way better with swap, so let's make the test less flaky by
    configuring a swap device for it. This also allows us to drop the ugly
    `cat`s from the load-generating script.

--- test/TEST-55-OOMD/test.sh
+++ test/TEST-55-OOMD/test.sh
@@ -2,9 +2,22 @@
 set -e
 
 TEST_DESCRIPTION="systemd-oomd Memory Pressure Test"
+IMAGE_NAME="oomd"
 
 # shellcheck source=test/test-functions
 . "${TEST_BASE_DIR:?}/test-functions"
+
+test_append_files() {
+    # Create a swap device
+    (
+        mkswap "${LOOPDEV:?}p2"
+        dracut_install swapon swapoff
+
+        cat >>"${initdir:?}/etc/fstab" <<EOF
+UUID=$(blkid -o value -s UUID "${LOOPDEV}p2")    none    swap    defaults 0 0
+EOF
+    )
+}
 
 check_result_nspawn() {
     local workspace="${1:?}"
