50fbea50d7e23d2776e2bb2f2fd679a2741e6530
Julia Kartseva <hex@fb.com>
2021-04-25 19:10:40 -0700

    core, bpf: add socket-bind feature to unit
    Add supported and install unit interface for socket-bind feature.
    
    supported verifies that
    - unified cgroup hierarchy (cgroup v2) is used
    - BPF_FRAMEWORK (libbpf + clang + llvm + bpftool) was available in
    compile time
    - kernel supports BPF_PROG_TYPE_CGROUP_SOCK_ADDR
    - bpf programs can be loaded into kernel
    - bpf link can be used
    
    install:
    - load bpf_object from bpf skeleton
    - resize rules map to fit socket_bind_allow and socket_bind deny rules
    from cgroup context
    - populate cgroup-bpf maps with rules
    - get bpf programs from bpf skeleton
    - attach programs to unit cgroup using bpf link
    - save bpf link in the unit

--- src/core/unit.h
+++ src/core/unit.h
@@ -309,6 +309,14 @@ typedef struct Unit {
          * attached to unit cgroup by provided program fd and attach type. */
         Hashmap *bpf_foreign_by_key;
 
+#if BPF_FRAMEWORK
+        /* BPF links to BPF programs attached to cgroup/bind{4|6} hooks and
+         * responsible for allowing or denying a unit to bind(2) to a socket
+         * address. */
+        struct bpf_link *ipv4_socket_bind_link;
+        struct bpf_link *ipv6_socket_bind_link;
+#endif
+
         uint64_t ip_accounting_extra[_CGROUP_IP_ACCOUNTING_METRIC_MAX];
 
         /* Low-priority event source which is used to remove watched PIDs that have gone away, and subscribe to any new
