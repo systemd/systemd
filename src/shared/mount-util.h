71ab95cde4ac79e34ea91f57c29dc55c0619c496
Lennart Poettering <lennart@poettering.net>
2021-04-27 17:27:45 +0200

    mount-util: add a helper that can add an idmap to an existing mount
    This makes use of the new kernel 5.12 APIs to add an idmap to a mount
    point. It does so by cloning the mountpoint, changing it, and then
    unmounting the old mountpoint, replacing it later with the new one.

--- src/shared/mount-util.h
+++ src/shared/mount-util.h
@@ -105,3 +105,5 @@ int bind_mount_in_namespace(pid_t target, const char *propagate_path, const char
 int mount_image_in_namespace(pid_t target, const char *propagate_path, const char *incoming_path, const char *src, const char *dest, bool read_only, bool make_file_or_directory, const MountOptions *options);
 
 int make_mount_point(const char *path);
+
+int remount_idmap(const char *p, uid_t uid_shift, uid_t uid_range);
