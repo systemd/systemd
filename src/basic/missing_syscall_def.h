10125d16abb833b641721da60a2159dc12dc1edb
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2021-04-28 03:34:38 +0800

    basic/missing-syscall: add numbers for riscv (64-bit)

--- src/basic/missing_syscall_def.h
+++ src/basic/missing_syscall_def.h
@@ -18,6 +18,7 @@
 #      error "Unknown MIPS ABI"
 #    endif
 #  elif defined(__powerpc__)
+#  elif defined(__riscv) && defined(__LP64__)
 #  elif defined(__s390__)
 #  elif defined(__sparc__)
 #  elif defined(__x86_64__)
@@ -56,6 +57,8 @@
 #    endif
 #  elif defined(__powerpc__)
 #    define systemd_NR_bpf 361
+#  elif defined(__riscv) && defined(__LP64__)
+#    define systemd_NR_bpf 280
 #  elif defined(__s390__)
 #    define systemd_NR_bpf 351
 #  elif defined(__sparc__)
@@ -111,6 +114,8 @@ assert_cc(__NR_bpf == systemd_NR_bpf);
 #      error "Unknown MIPS ABI"
 #    endif
 #  elif defined(__powerpc__)
+#    define systemd_NR_close_range 436
+#  elif defined(__riscv) && defined(__LP64__)
 #    define systemd_NR_close_range 436
 #  elif defined(__s390__)
 #    define systemd_NR_close_range 436
@@ -168,6 +173,8 @@ assert_cc(__NR_close_range == systemd_NR_close_range);
 #    endif
 #  elif defined(__powerpc__)
 #    define systemd_NR_copy_file_range 379
+#  elif defined(__riscv) && defined(__LP64__)
+#    define systemd_NR_copy_file_range 285
 #  elif defined(__s390__)
 #    define systemd_NR_copy_file_range 375
 #  elif defined(__sparc__)
@@ -224,6 +231,8 @@ assert_cc(__NR_copy_file_range == systemd_NR_copy_file_range);
 #    endif
 #  elif defined(__powerpc__)
 #    define systemd_NR_getrandom 359
+#  elif defined(__riscv) && defined(__LP64__)
+#    define systemd_NR_getrandom 278
 #  elif defined(__s390__)
 #    define systemd_NR_getrandom 349
 #  elif defined(__sparc__)
@@ -280,6 +289,8 @@ assert_cc(__NR_getrandom == systemd_NR_getrandom);
 #    endif
 #  elif defined(__powerpc__)
 #    define systemd_NR_memfd_create 360
+#  elif defined(__riscv) && defined(__LP64__)
+#    define systemd_NR_memfd_create 279
 #  elif defined(__s390__)
 #    define systemd_NR_memfd_create 350
 #  elif defined(__sparc__)
@@ -336,6 +347,8 @@ assert_cc(__NR_memfd_create == systemd_NR_memfd_create);
 #    endif
 #  elif defined(__powerpc__)
 #    define systemd_NR_name_to_handle_at 345
+#  elif defined(__riscv) && defined(__LP64__)
+#    define systemd_NR_name_to_handle_at 264
 #  elif defined(__s390__)
 #    define systemd_NR_name_to_handle_at 335
 #  elif defined(__sparc__)
@@ -391,6 +404,8 @@ assert_cc(__NR_name_to_handle_at == systemd_NR_name_to_handle_at);
 #      error "Unknown MIPS ABI"
 #    endif
 #  elif defined(__powerpc__)
+#    define systemd_NR_pidfd_open 434
+#  elif defined(__riscv) && defined(__LP64__)
 #    define systemd_NR_pidfd_open 434
 #  elif defined(__s390__)
 #    define systemd_NR_pidfd_open 434
@@ -448,6 +463,8 @@ assert_cc(__NR_pidfd_open == systemd_NR_pidfd_open);
 #    endif
 #  elif defined(__powerpc__)
 #    define systemd_NR_pidfd_send_signal 424
+#  elif defined(__riscv) && defined(__LP64__)
+#    define systemd_NR_pidfd_send_signal 424
 #  elif defined(__s390__)
 #    define systemd_NR_pidfd_send_signal 424
 #  elif defined(__sparc__)
@@ -504,6 +521,8 @@ assert_cc(__NR_pidfd_send_signal == systemd_NR_pidfd_send_signal);
 #    endif
 #  elif defined(__powerpc__)
 #    define systemd_NR_pkey_mprotect 386
+#  elif defined(__riscv) && defined(__LP64__)
+#    define systemd_NR_pkey_mprotect 288
 #  elif defined(__s390__)
 #    define systemd_NR_pkey_mprotect 384
 #  elif defined(__sparc__)
@@ -560,6 +579,8 @@ assert_cc(__NR_pkey_mprotect == systemd_NR_pkey_mprotect);
 #    endif
 #  elif defined(__powerpc__)
 #    define systemd_NR_renameat2 357
+#  elif defined(__riscv) && defined(__LP64__)
+#    define systemd_NR_renameat2 276
 #  elif defined(__s390__)
 #    define systemd_NR_renameat2 347
 #  elif defined(__sparc__)
@@ -616,6 +637,8 @@ assert_cc(__NR_renameat2 == systemd_NR_renameat2);
 #    endif
 #  elif defined(__powerpc__)
 #    define systemd_NR_setns 350
+#  elif defined(__riscv) && defined(__LP64__)
+#    define systemd_NR_setns 268
 #  elif defined(__s390__)
 #    define systemd_NR_setns 339
 #  elif defined(__sparc__)
@@ -672,6 +695,8 @@ assert_cc(__NR_setns == systemd_NR_setns);
 #    endif
 #  elif defined(__powerpc__)
 #    define systemd_NR_statx 383
+#  elif defined(__riscv) && defined(__LP64__)
+#    define systemd_NR_statx 291
 #  elif defined(__s390__)
 #    define systemd_NR_statx 379
 #  elif defined(__sparc__)
@@ -727,6 +752,8 @@ assert_cc(__NR_statx == systemd_NR_statx);
 #      error "Unknown MIPS ABI"
 #    endif
 #  elif defined(__powerpc__)
+#    define systemd_NR_epoll_pwait2 441
+#  elif defined(__riscv) && defined(__LP64__)
 #    define systemd_NR_epoll_pwait2 441
 #  elif defined(__s390__)
 #    define systemd_NR_epoll_pwait2 441
