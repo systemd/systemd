a43cc984b4e23efe6a470c7769d0a9828700764d
Dimitri John Ledkov <xnox@ubuntu.com>
2021-04-27 16:53:53 +0100

    boot: add optional EFI SBAT support
    Add SBAT support, when -Dsbat-distro value is specified. One can use
    -Dsbat-distro=auto for autodetection of all sbat options. Many meson configure
    options added to customize SBAT CSV values, but sensible defaults are auto
    detected by default. SBAT support is required if shim v15+ is used to load
    systemd-boot binary or kernel.efi (Type II BootLoaderSpec).
    
    Fixes #19247

--- src/boot/efi/secure-boot.c
+++ src/boot/efi/secure-boot.c
@@ -11,3 +11,13 @@ BOOLEAN secure_boot_enabled(void) {
 
         return !EFI_ERROR(err) && secure;
 }
+
+#ifdef SBAT_DISTRO
+static const char sbat[]
+__attribute__((section (".sbat")))
+__attribute__((__used__))
+__attribute__((aligned(512))) = ""
+"sbat,1,SBAT Version,sbat,1,https://github.com/rhboot/shim/blob/main/SBAT.md\n"
+SBAT_PROJECT ",1,systemd EFI boot," SBAT_PROJECT "," PROJECT_VERSION "," PROJECT_URL "\n"
+SBAT_PROJECT "." SBAT_DISTRO "," SBAT_DISTRO_GENERATION "," SBAT_DISTRO_SUMMARY "," SBAT_DISTRO_PKGNAME "," SBAT_DISTRO_VERSION "," SBAT_DISTRO_URL ;
+#endif
