c8da77f8c3855a579be560005fce105e6eb0a181
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-04-28 22:18:45 +0900

    network: neighbor: downgrade log level
    As commented in the code, kernel sends messages about neighbors after
    a link is removed.
    
    (cherry picked from commit 27a213392f642fdd2a9dbce914bbfda9a72aafc1)
    (cherry picked from commit beaae1f8d1d958e95117550604aa6462d1a636b8)

--- src/network/networkd-neighbor.c
+++ src/network/networkd-neighbor.c
@@ -496,10 +496,9 @@ int manager_rtnl_process_neighbor(sd_netlink *rtnl, sd_netlink_message *message,
 
         r = link_get(m, ifindex, &link);
         if (r < 0 || !link) {
-                /* when enumerating we might be out of sync, but we will get the neighbor again, so just
-                 * ignore it */
-                if (!m->enumerating)
-                        log_warning("rtnl: received neighbor for link '%d' we don't know about, ignoring.", ifindex);
+                /* when enumerating we might be out of sync, but we will get the neighbor again. Also,
+                 * kernel sends messages about neighbors after a link is removed. So, just ignore it. */
+                log_debug("rtnl: received neighbor for link '%d' we don't know about, ignoring.", ifindex);
                 return 0;
         }
 
