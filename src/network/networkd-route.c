d2c5ebf8ebc00712cc31e8948c669e215f4a9fd9
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-04-28 18:42:26 +0900

    fix dropping routes logic

--- src/network/networkd-route.c
+++ src/network/networkd-route.c
@@ -956,7 +956,7 @@ static bool links_have_static_route(const Manager *manager, const Route *route, 
                 if (link == except)
                         continue;
 
-                if (link_has_route(link, route))
+                if (link_has_static_route(link, route))
                         return true;
         }
 
