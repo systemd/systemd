179a92c9d8b2df688764095ce1d242de20ee72fa
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-04-30 06:57:15 +0900

    network: drop duplicated link_up_can()
    (cherry picked from commit f282ce20aaa767f5395b065f2be587cdef3e5491)

--- src/network/networkd-link.h
+++ src/network/networkd-link.h
@@ -204,6 +204,7 @@ DEFINE_TRIVIAL_DESTRUCTOR(link_netlink_destroy_callback, Link, link_unref);
 
 int link_get(Manager *m, int ifindex, Link **ret);
 
+int link_up(Link *link);
 int link_down(Link *link, link_netlink_message_handler_t callback);
 
 void link_enter_failed(Link *link);
