0b32cb46e4630d9a37288e469fb7e0f4e37addba
Yegor Alexeyev <yegor.alexeyev@gmail.com>
2021-04-29 19:03:47 +0300

    unique network and hardware addresses for relay agent test

--- test/test-network/systemd-networkd-tests.py
+++ test/test-network/systemd-networkd-tests.py
@@ -3719,12 +3719,11 @@ class NetworkdDHCPServerRelayAgentTests(unittest.TestCase, Utilities):
         copy_unit_to_networkd_unit_path(*self.units)
         start_networkd()
 
-        #Test is disabled until BindToInterface DHCP server configuration option is supported
         self.wait_online(['client:routable'])
 
         output = check_output(*networkctl_cmd, '-n', '0', 'status', 'client', env=env)
         print(output)
-        self.assertRegex(output, 'Address: 192.168.5.150 \(DHCP4 via 192.168.5.1\)')
+        self.assertRegex(output, 'Address: 192.168.235.150 \(DHCP4 via 192.168.235.1\)')
 
 class NetworkdDHCPClientTests(unittest.TestCase, Utilities):
     links = [
