484cd43cae536d952cd84abfda3260235dc893cc
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-05-03 23:47:57 +0900

    path-util: introduce path_find_last_component()

--- src/basic/path-util.h
+++ src/basic/path-util.h
@@ -153,6 +153,7 @@ int fsck_exists(const char *fstype);
 
 char* dirname_malloc(const char *path);
 int path_find_first_component(const char **p, bool accept_dot_dot, const char **ret);
+int path_find_last_component(const char *path, bool accept_dot_dot, const char **next, const char **ret);
 const char *last_path_component(const char *path);
 int path_extract_filename(const char *p, char **ret);
 int path_extract_directory(const char *p, char **ret);
