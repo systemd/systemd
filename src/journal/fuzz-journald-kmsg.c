9dcc62ee9464ff3441e07584d4e7a008d53a76c7
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2021-05-05 16:44:52 +0200

    fuzz-journald-kmsg: silence output as usual

--- src/journal/fuzz-journald-kmsg.c
+++ src/journal/fuzz-journald-kmsg.c
@@ -10,6 +10,11 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
         if (size == 0)
                 return 0;
 
+        /* We don't want to fill the logs with assert warnings.
+         * Disable most logging if not running standalone */
+        if (!getenv("SYSTEMD_LOG_LEVEL"))
+                log_set_max_level(LOG_CRIT);
+
         dummy_server_init(&s, data, size);
         dev_kmsg_record(&s, s.buffer, size);
         server_done(&s);
