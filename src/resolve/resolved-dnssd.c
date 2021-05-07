f6ce8ad3d9143cabd0c8be4ff9eb9c2442b6eb03
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-05-08 01:56:24 +0900

    resolve: check that bus is ready before emitting signal or property change

--- src/resolve/resolved-dnssd.c
+++ src/resolve/resolved-dnssd.c
@@ -329,9 +329,12 @@ int dnssd_txt_item_new_from_data(const char *key, const void *data, const size_t
         return 0;
 }
 
-void dnssd_signal_conflict(Manager *manager, const char *name) {
+int dnssd_signal_conflict(Manager *manager, const char *name) {
         DnssdService *s;
         int r;
+
+        if (sd_bus_is_ready(manager->bus) <= 0)
+                return 0;
 
         HASHMAP_FOREACH(s, manager->dnssd_services) {
                 if (s->withdrawn)
@@ -343,22 +346,20 @@ void dnssd_signal_conflict(Manager *manager, const char *name) {
                         s->withdrawn = true;
 
                         r = sd_bus_path_encode("/org/freedesktop/resolve1/dnssd", s->name, &path);
-                        if (r < 0) {
-                                log_error_errno(r, "Can't get D-BUS object path: %m");
-                                return;
-                        }
+                        if (r < 0)
+                                return log_error_errno(r, "Can't get D-BUS object path: %m");
 
                         r = sd_bus_emit_signal(manager->bus,
                                                path,
                                                "org.freedesktop.resolve1.DnssdService",
                                                "Conflicted",
                                                NULL);
-                        if (r < 0) {
-                                log_error_errno(r, "Cannot emit signal: %m");
-                                return;
-                        }
+                        if (r < 0)
+                                return log_error_errno(r, "Cannot emit signal: %m");
 
                         break;
                 }
         }
+
+        return 0;
 }
