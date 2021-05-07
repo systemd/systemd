0e3f79890123a59b25be5d7ccfb4721b9ff92bf5
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-05-07 16:05:31 +0900

    network: use request queue to configure neighbors

--- src/network/networkd-queue.c
+++ src/network/networkd-queue.c
@@ -10,6 +10,9 @@
 
 static void request_free_object(RequestType type, void *object) {
         switch(type) {
+        case REQUEST_TYPE_NEIGHBOR:
+                neighbor_free(object);
+                break;
         case REQUEST_TYPE_ROUTING_POLICY_RULE:
                 routing_policy_rule_free(object);
                 break;
@@ -104,6 +107,9 @@ int manager_process_requests(sd_event_source *s, void *userdata) {
 
                 ORDERED_SET_FOREACH(req, manager->request_queue) {
                         switch(req->type) {
+                        case REQUEST_TYPE_NEIGHBOR:
+                                r = request_process_neighbor(req);
+                                break;
                         case REQUEST_TYPE_ROUTING_POLICY_RULE:
                                 r = request_process_routing_policy_rule(req);
                                 break;
