5dcadb4c8320f6a7b8a9353404874d43668e4648
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2021-05-10 10:22:07 +0200

    core: disable event sources before unreffing them
    This mirrors the change done for systemd-resolved in
    97935302283729c9206b84f5e00b1aff0f78ad19. Quoting that patch:
    
    > We generally operate on the assumption that a source is "gone" as soon as we
    > unref it. This is generally true because we have the only reference. But if
    > something else holds the reference, our unref doesn't really stop the source
    > and it could fire again.
    
    In particular, we take temporary references from sd-event code, and when called
    from an sd-event callback, we could temporarily see this elevated reference
    count. This patch doesn't seem to change anything, but I think it's nicer to do
    the same change as in other places and not rely on _unref() immediately
    disabling the source.

--- src/core/automount.c
+++ src/core/automount.c
@@ -84,7 +84,7 @@ static void unmount_autofs(Automount *a) {
         if (a->pipe_fd < 0)
                 return;
 
-        a->pipe_event_source = sd_event_source_unref(a->pipe_event_source);
+        a->pipe_event_source = sd_event_source_disable_unref(a->pipe_event_source);
         a->pipe_fd = safe_close(a->pipe_fd);
 
         /* If we reload/reexecute things we keep the mount point around */
@@ -113,7 +113,7 @@ static void automount_done(Unit *u) {
         a->tokens = set_free(a->tokens);
         a->expire_tokens = set_free(a->expire_tokens);
 
-        a->expire_event_source = sd_event_source_unref(a->expire_event_source);
+        a->expire_event_source = sd_event_source_disable_unref(a->expire_event_source);
 }
 
 static int automount_add_trigger_dependencies(Automount *a) {
