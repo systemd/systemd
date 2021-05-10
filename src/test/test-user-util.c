243dc61b5f85a344eecc908bf55aa0da190e3a0c
Lennart Poettering <lennart@poettering.net>
2021-05-10 19:19:02 +0200

    test-user-util: fix line break confusion
    This fixes some line-break confusion introduced by #11199
    (c6cecb744b53561efd329309af7d02a3f9979ed1). It also restores a test with
    GID_INVALID that was dropped, presumably by accident.

--- src/test/test-user-util.c
+++ src/test/test-user-util.c
@@ -379,7 +379,9 @@ static void test_make_salt(void) {
 
 static void test_in_gid(void) {
         assert(in_gid(getgid()) >= 0);
-        assert(in_gid(getegid()) >= 0);        assert(in_gid(TTY_GID) == 0); /* The TTY gid is for owning ttys, it would be really really weird if we were in it. */
+        assert(in_gid(getegid()) >= 0);
+        assert(in_gid(GID_INVALID) < 0);
+        assert(in_gid(TTY_GID) == 0); /* The TTY gid is for owning ttys, it would be really really weird if we were in it. */
 }
 
 static void test_gid_lists_ops(void) {
