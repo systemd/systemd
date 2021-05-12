aaac5c618184a22395c0b25e91c16e1c9e396eca
Danilo Krummrich <danilo.krummrich@bmw.de>
2021-05-12 18:36:23 +0200

    core: device: re-trigger uevents missed by udevd
    This patch addresses a potential performance optimization targeting
    well-defined environments such as embedded environments, e.g. automotive
    control units, where everything is controlled by systemd and all
    existing dependencies are known and properly modelled.
    
    While booting up it is likely that systemd-udevd is started after a
    couple of uevents were already triggered by the kernel and therefore
    were missed.
    
    Usually, this is resolved by systemd-udev-trigger.service, which simply
    re-triggers all devices and subsystems, including also the ones that
    were already observed by systemd-udevd.
    
    However, this can take quite some time and is needless in well-defined
    environments where everything is executed as systemd unit and all
    dependencies are modelled properly.
    
    To speed things up in such environments device_start() is introduced by
    this patch and used to re-trigger the corresponding uevent of a device
    unit. The device unit is pulled in as a dependency of another unit
    depending on the particular device represented by this device unit.
    
    The re-trigger is only done in case the device is not following another
    device, was never in plugged state before, is in the dead state, but is
    actually plugged (corresponding sysfs entries are existent).
    
    This way we can re-trigger only those uevents that were actually missed
    by systemd-udevd. This is much faster than re-triggering all devices and
    subsystems and balances the work of processing re-triggered, missed
    uevents to the point of time where they're actually needed by another
    unit.
    
    This accounts only for device units which are explicitly referenced as
    dependency by other units and are tagged in udev rules (TAG+="systemd").
    As a consequence device units for all used devices in the system must be
    created and a proper dependency tree must be built in order to do
    without systemd-udev-trigger.service.

--- src/core/unit.c
+++ src/core/unit.c
@@ -1674,12 +1674,18 @@ int unit_start(Unit *u) {
         if (!UNIT_VTABLE(u)->start)
                 return -EBADR;
 
+        /* Units of type UNIT_DEVICE do not actually start anything in start(), but do trigger their
+         * corresponding uevent in case it was sent by the kernel in the time before systemd-udevd was
+         * running.
+         */
+        if (!(u->type == UNIT_DEVICE)) {
+                unit_add_to_dbus_queue(u);
+                unit_cgroup_freezer_action(u, FREEZER_THAW);
+        }
+
         /* We don't suppress calls to ->start() here when we are already starting, to allow this request to
          * be used as a "hurry up" call, for example when the unit is in some "auto restart" state where it
          * waits for a holdoff timer to elapse before it will start again. */
-
-        unit_add_to_dbus_queue(u);
-        unit_cgroup_freezer_action(u, FREEZER_THAW);
 
         return UNIT_VTABLE(u)->start(u);
 }
