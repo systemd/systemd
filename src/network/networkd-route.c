05f228c3117f092a7ec811d0b185ad66ec09f94e
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-05-13 15:14:00 +0900

    network: route: also show weight of multipath route in debugging logs

--- src/network/networkd-route.c
+++ src/network/networkd-route.c
@@ -773,13 +773,15 @@ static void log_route_debug(const Route *route, const char *str, const Link *lin
                 MultipathRoute *m;
 
                 ORDERED_SET_FOREACH(m, route->multipath_routes) {
-                        _cleanup_free_ char *buf = NULL, *joined = NULL;
+                        _cleanup_free_ char *buf = NULL;
                         union in_addr_union a = m->gateway.address;
 
                         (void) in_addr_to_string(m->gateway.family, &a, &buf);
-                        joined = strjoin(gw_alloc, gw_alloc ? "," : "", strna(buf), m->ifname ? "@" : "", strempty(m->ifname));
-                        if (joined)
-                                free_and_replace(gw_alloc, joined);
+                        /* Increase the weight by one to match the range which MultiPathRoute= uses.
+                         * See also comment in config_parse_multipath_route(). */
+                        (void) strextendf(&gw_alloc, "%s%s%s%s(weight:%u)",
+                                          gw_alloc ? "," : "", strna(buf), m->ifname ? "@" : "",
+                                          strempty(m->ifname), m->weight+1);
                 }
                 gw = gw_alloc;
         }
