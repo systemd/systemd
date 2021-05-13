876f455e9328923b5a83243572a24bd4c8de5355
Luca Boccassi <luca.boccassi@microsoft.com>
2021-05-13 16:06:05 +0100

    PoC: add Rust submodule as libbasic_rust
    Proof-of-concept to test integrating Rust submodules.
    
    This adds a (very naive) rewrite of string_extract_line from string-util.h
    in Rust, and compiles it in so that the rest of our C code, including the
    unit tests, call into it instead.
    
    Given we are only using the standard library, there is no need for a
    (complicated) integration with Cargo, and the only additional
    requirement is the Rust compiler. Meson abstracts it quite nicely.

--- .github/workflows/build_test.sh
+++ .github/workflows/build_test.sh
@@ -16,6 +16,22 @@ ARGS=(
     "--optimization=3 -Dfexecve=true -Dstandalone-binaries=true -Dstatic-libsystemd=true -Dstatic-libudev=true"
     "-Db_ndebug=true"
 )
+ARGS=(
+    "--optimization=0 -Drust_args='--deny warnings -Copt-level=0'"
+    "--optimization=2 -Drust_args='--deny warnings -Copt-level=2'"
+    "--optimization=s -Drust_args='--deny warnings -Copt-level=s'"
+    "--optimization=3 -Db_lto=false -Drust_args='--deny warnings -Copt-level=3'"
+    "--optimization=3 -Ddns-over-tls=openssl -Drust_args='--deny warnings -Copt-level=3'"
+    "--optimization=3 -Dfexecve=true -Dstandalone-binaries=true -Dstatic-libsystemd=true -Dstatic-libudev=true -Drust_args='--deny warnings -Copt-level=3'"
+    "-Db_ndebug=true -Drust_args='--deny warnings -Cdebug-assertions=false'"
+)
+
+if [[ "$COMPILER" == "clang" ]]; then
+      ARGS+=("--optimization=3 -Db_lto=true -Drust_args='--deny warnings -Copt-level=3 -Clinker-plugin-lto'")
+else
+      ARGS+=("--optimization=3 -Db_lto=true -Drust_args='--deny warnings -Copt-level=3'")
+fi
+
 PACKAGES=(
     cryptsetup-bin
     expect
@@ -88,6 +104,11 @@ else
     fatal "Unknown compiler: $COMPILER"
 fi
 
+if [ -n "${RUST}" ]; then
+    PACKAGES+=(rustc)
+    RUST="-Dbuild-rust=true"
+fi
+
 # PPA with some newer build dependencies (like zstd)
 add-apt-repository -y ppa:upstream-systemd-ci/systemd-ci
 apt-get -y update
@@ -108,7 +129,7 @@ for args in "${ARGS[@]}"; do
     SECONDS=0
 
     info "Checking build with $args"
-    if ! AR="$AR" CC="$CC" CXX="$CXX" CFLAGS="-Werror" CXXFLAGS="-Werror" meson -Dtests=unsafe -Dslow-tests=true -Dfuzz-tests=true --werror $args build; then
+    if ! AR="$AR" CC="$CC" CXX="$CXX" CFLAGS="-Werror" CXXFLAGS="-Werror" eval "meson -Dtests=unsafe -Dslow-tests=true -Dfuzz-tests=true --werror ${RUST} $args build"; then
         fatal "meson failed with $args"
     fi
 
