716dfeca577daf4457bf3a05e51c59597e5ad645
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-05-14 17:33:32 +0900

    network: address: introduce link_get_ipv4/ipv6_address()

--- src/network/networkd-address.h
+++ src/network/networkd-address.h
@@ -66,7 +66,8 @@ DEFINE_NETWORK_SECTION_FUNCTIONS(Address, address_free);
 int link_drop_addresses(Link *link);
 int link_drop_foreign_addresses(Link *link);
 bool link_address_is_dynamic(const Link *link, const Address *address);
-int link_has_ipv6_address(Link *link, const struct in6_addr *address);
+int link_get_ipv6_address(Link *link, const struct in6_addr *address, Address **ret);
+int link_get_ipv4_address(Link *link, const struct in_addr *address, unsigned char prefixlen, Address **ret);
 int manager_has_address(Manager *manager, int family, const union in_addr_union *address, bool check_ready);
 
 void ipv4_dad_unref(Link *link);
