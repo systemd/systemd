1956f07206ecab9cb00fe92cfa041b030296367b
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-05-18 05:09:59 +0900

    network: rename MdbEntry -> BridgeMDB

--- src/network/networkd-bridge-mdb.h
+++ src/network/networkd-bridge-mdb.h
@@ -7,21 +7,21 @@
 #include "in-addr-util.h"
 #include "networkd-util.h"
 
-typedef struct Network Network;
 typedef struct Link Link;
+typedef struct Network Network;
 
-typedef struct MdbEntry {
+typedef struct BridgeMDB {
         Network *network;
         NetworkConfigSection *section;
 
         int family;
         union in_addr_union group_addr;
         uint16_t vlan_id;
-} MdbEntry;
+} BridgeMDB;
 
-MdbEntry *mdb_entry_free(MdbEntry *mdb_entry);
+BridgeMDB *bridge_mdb_free(BridgeMDB *mdb);
 
-void network_drop_invalid_mdb_entries(Network *network);
+void network_drop_invalid_bridge_mdb_entries(Network *network);
 
 int link_set_bridge_mdb(Link *link);
 
