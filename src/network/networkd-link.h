6c166efa20364e21ff214c70b5367311a01d9520
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-05-18 14:21:42 +0900

    network: use request queue to configure IPv6 proxy NDP addresses

--- src/network/networkd-link.h
+++ src/network/networkd-link.h
@@ -82,6 +82,7 @@ typedef struct Link {
         unsigned static_address_label_messages;
         unsigned static_bridge_fdb_messages;
         unsigned static_bridge_mdb_messages;
+        unsigned static_ipv6_proxy_ndp_messages;
         unsigned static_neighbor_messages;
         unsigned static_nexthop_messages;
         unsigned static_route_messages;
@@ -125,6 +126,7 @@ typedef struct Link {
         bool static_address_labels_configured:1;
         bool static_bridge_fdb_configured:1;
         bool static_bridge_mdb_configured:1;
+        bool static_ipv6_proxy_ndp_configured:1;
         bool static_neighbors_configured:1;
         bool static_nexthops_configured:1;
         bool static_routes_configured:1;
