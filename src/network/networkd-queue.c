d9248dabf445ddbd0e9424ad88aabbc180d2f4d1
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-05-18 13:24:32 +0900

    network: address label: use request queue to configure address labels

--- src/network/networkd-queue.c
+++ src/network/networkd-queue.c
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 
 #include "networkd-address.h"
+#include "networkd-address-label.h"
 #include "networkd-bridge-fdb.h"
 #include "networkd-bridge-mdb.h"
 #include "networkd-dhcp-server.h"
@@ -15,6 +16,9 @@ static void request_free_object(RequestType type, void *object) {
         switch(type) {
         case REQUEST_TYPE_ADDRESS:
                 address_free(object);
+                break;
+        case REQUEST_TYPE_ADDRESS_LABEL:
+                address_label_free(object);
                 break;
         case REQUEST_TYPE_BRIDGE_FDB:
                 bridge_fdb_free(object);
@@ -131,6 +135,9 @@ int manager_process_requests(sd_event_source *s, void *userdata) {
                         switch(req->type) {
                         case REQUEST_TYPE_ADDRESS:
                                 r = request_process_address(req);
+                                break;
+                        case REQUEST_TYPE_ADDRESS_LABEL:
+                                r = request_process_address_label(req);
                                 break;
                         case REQUEST_TYPE_BRIDGE_FDB:
                                 r = request_process_bridge_fdb(req);
