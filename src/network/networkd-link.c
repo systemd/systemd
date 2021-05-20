5cc8c9e2e6c44597282e5e293e4e918014027a35
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-05-20 16:14:17 +0900

    network: fix an infinite loop
    Fixes #19467.

--- src/network/networkd-link.c
+++ src/network/networkd-link.c
@@ -179,6 +179,12 @@ static void link_update_master_operstate(Link *link, NetDev *netdev) {
         if (netdev->ifindex <= 0)
                 return;
 
+        /* If an interface is self-mentioned in Bridge= or friends, then it introduces an infinite loop.
+         * FIXME: there still exits a possibility of an infinite loop when two or more interfaces
+         * mention each other in Bridge= or so. We need to detect such a loop. */
+        if (link->ifindex == netdev->ifindex)
+                return;
+
         if (link_get(link->manager, netdev->ifindex, &master) < 0)
                 return;
 
