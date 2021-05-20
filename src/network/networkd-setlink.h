b5bc9ec2d4458ec45a5078938c31079e43c31078
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-05-21 04:51:07 +0900

    network: use request queue to set mtu

---
+++ src/network/networkd-setlink.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#pragma once
+
+#include <inttypes.h>
+
+typedef struct Link Link;
+typedef struct Request Request;
+
+typedef enum SetLinkMode {
+        SET_LINK_MTU, /* Setting MTU. */
+        _SET_LINK_MODE_MAX,
+        _SET_LINK_MODE_INVALID = -EINVAL,
+} SetLinkMode;
+
+int link_request_to_set_mtu(Link *link, uint32_t mtu);
+
+int request_process_set_link(Request *req);
