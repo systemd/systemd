e5e99f170c0b9fe4b7393b9ad8d04343ddcb2b36
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-05-21 13:50:39 +0900

    network: use request queue to set link flags

--- src/network/networkd-setlink.h
+++ src/network/networkd-setlink.h
@@ -7,11 +7,13 @@ typedef struct Link Link;
 typedef struct Request Request;
 
 typedef enum SetLinkOperation {
+        SET_LINK_FLAGS,                   /* Setting IFF_NOARP or friends. */
         SET_LINK_MTU,                     /* Setting MTU. */
         _SET_LINK_OPERATION_MAX,
         _SET_LINK_OPERATION_INVALID = -EINVAL,
 } SetLinkOperation;
 
+int link_request_to_set_flags(Link *link);
 int link_request_to_set_mtu(Link *link, uint32_t mtu);
 
 int link_configure_mtu(Link *link);
