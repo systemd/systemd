ac2fc7e876620aaa538b0bbcc61c8a2abd0f7941
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-05-25 15:00:33 +0900

    network: introduce link_request_to_set_master()
    The function will be used later.

--- src/network/networkd-setlink.h
+++ src/network/networkd-setlink.h
@@ -11,6 +11,7 @@ typedef enum SetLinkOperation {
         SET_LINK_FLAGS,                   /* Setting IFF_NOARP or friends. */
         SET_LINK_GROUP,                   /* Setting interface group. */
         SET_LINK_MAC,                     /* Setting MAC address. */
+        SET_LINK_MASTER,                  /* Setting IFLA_MASTER. */
         SET_LINK_MTU,                     /* Setting MTU. */
         _SET_LINK_OPERATION_MAX,
         _SET_LINK_OPERATION_INVALID = -EINVAL,
@@ -20,6 +21,7 @@ int link_request_to_set_addrgen_mode(Link *link);
 int link_request_to_set_flags(Link *link);
 int link_request_to_set_group(Link *link);
 int link_request_to_set_mac(Link *link);
+int link_request_to_set_master(Link *link);
 int link_request_to_set_mtu(Link *link, uint32_t mtu);
 
 int link_configure_mtu(Link *link);
