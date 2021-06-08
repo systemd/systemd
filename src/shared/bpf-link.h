d63a3d350f2e5b8bdcbd4eb6b042d61859705ac0
Lennart Poettering <lennart@poettering.net>
2021-06-08 17:00:48 +0200

    bpf-link: prefix function names with "bpf_"
    we generally do this for all bpf functions, do so here too.

--- src/shared/bpf-link.h
+++ src/shared/bpf-link.h
@@ -8,9 +8,9 @@
 #include "fdset.h"
 #include "macro.h"
 
-bool can_link_bpf_program(struct bpf_program *prog);
+bool bpf_can_link_program(struct bpf_program *prog);
 
-int serialize_bpf_link(FILE *f, FDSet *fds, const char *key, struct bpf_link *link);
+int bpf_serialize_link(FILE *f, FDSet *fds, const char *key, struct bpf_link *link);
 
 struct bpf_link *bpf_link_free(struct bpf_link *p);
 DEFINE_TRIVIAL_CLEANUP_FUNC(struct bpf_link *, bpf_link_free);
