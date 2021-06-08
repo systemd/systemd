9a6a6e011ee885cf12496fce2df393f09ffd4702
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2021-06-08 09:06:11 +0200

    man/50-xdg-data-dirs: add quotes as suggested by shellcheck

--- man/50-xdg-data-dirs.sh
+++ man/50-xdg-data-dirs.sh
@@ -5,8 +5,8 @@ XDG_DATA_DIRS="${XDG_DATA_DIRS:-/usr/local/share/:/usr/share}"
 
 # add a directory if it exists
 if [[ -d /opt/foo/share ]]; then
-    XDG_DATA_DIRS=/opt/foo/share:${XDG_DATA_DIRS}
+    XDG_DATA_DIRS="/opt/foo/share:${XDG_DATA_DIRS}"
 fi
 
 # write our output
-echo XDG_DATA_DIRS=$XDG_DATA_DIRS
+echo "XDG_DATA_DIRS=${XDG_DATA_DIRS}"
