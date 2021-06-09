e24901d344dd0d629c7bcb8674dde3ca4cc2b42b
Dan Streetman <ddstreet@canonical.com>
2021-06-09 07:57:01 -0400

    networkctl: add field 'Required For Online'

--- src/network/networkctl.c
+++ src/network/networkctl.c
@@ -2226,6 +2226,16 @@ static int link_status_one(
                         return table_log_add_error(r);
         }
 
+        r = sd_network_link_get_required_for_online(info->ifindex);
+        if (r >= 0) {
+                r = table_add_many(table,
+                                   TABLE_EMPTY,
+                                   TABLE_STRING, "Required For Online:",
+                                   TABLE_BOOLEAN, r);
+                if (r < 0)
+                        return table_log_add_error(r);
+        }
+
         if (lease) {
                 const void *client_id;
                 size_t client_id_len;
