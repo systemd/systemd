89f4f6926153cec3aae6583082112b6abb4888ed
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2021-06-11 17:11:34 +0200

    bootctl: print SystemdOptions from efivarfs if newer than our cache
    The logic is that if the options are updated after boot, we *don't* use
    the new value. But we still want to print out the changed contents in
    bootctl as to not confuse people.
    
    Fixes #19597, https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=988450.
    
    $ build/bootctl systemd-efi-options
    quiet
    Note: SystemdOptions EFI variable has been modified since boot. New value: debug
    
    The hint is printed to stderr, so scripts should not be confused.

--- src/basic/efivars.h
+++ src/basic/efivars.h
@@ -95,6 +95,7 @@ bool is_efi_secure_boot_setup_mode(void);
 
 int cache_efi_options_variable(void);
 int systemd_efi_options_variable(char **line);
+int systemd_efi_options_efivarfs_if_newer(char **line);
 
 #else
 
@@ -131,6 +132,10 @@ static inline int cache_efi_options_variable(void) {
 }
 
 static inline int systemd_efi_options_variable(char **line) {
+        return -ENODATA;
+}
+
+static inline int systemd_efi_options_efivarfs_if_newer(char **line) {
         return -ENODATA;
 }
 #endif
--- src/boot/bootctl.c
+++ src/boot/bootctl.c
@@ -1860,13 +1860,22 @@ static int verb_systemd_efi_options(int argc, char *argv[], void *userdata) {
         int r;
 
         if (argc == 1) {
-                _cleanup_free_ char *line = NULL;
+                _cleanup_free_ char *line = NULL, *new = NULL;
 
                 r = systemd_efi_options_variable(&line);
-                if (r < 0)
-                        return log_error_errno(r, "Failed to query SystemdOptions EFI variable: %m");
+                if (r == -ENODATA)
+                        log_debug("No SystemdOptions EFI variable present in cache.");
+                else if (r < 0)
+                        return log_error_errno(r, "Failed to read SystemdOptions EFI variable from cache: %m");
+                else
+                        puts(line);
 
-                puts(line);
+                r = systemd_efi_options_efivarfs_if_newer(&new);
+                if (r < 0 && r != -ENODATA)
+                        log_warning_errno(r, "Failed to check EFI variable in efivarfs, ignoring: %m");
+                if (new)
+                        log_notice("Note: SystemdOptions EFI variable has been modified since boot. New value: %s",
+                                   new);
 
         } else {
                 r = efi_set_variable_string(EFI_VARIABLE_SystemdOptions, argv[1]);
