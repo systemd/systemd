149fe9f986d49addde2ebbf998ef64f8619f0140
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-06-12 10:21:41 +0900

    Revert "core: Avoid spurious realization of unit cgroups"
    This reverts commit cc815b7fea0ade5331e8dd22ef6b5183edb77608.

--- src/core/unit-serialize.c
+++ src/core/unit-serialize.c
@@ -526,10 +526,8 @@ int unit_deserialize(Unit *u, FILE *f, FDSet *fds) {
         /* Let's make sure that everything that is deserialized also gets any potential new cgroup settings
          * applied after we are done. For that we invalidate anything already realized, so that we can
          * realize it again. */
-        if (u->cgroup_realized) {
-                unit_invalidate_cgroup(u, _CGROUP_MASK_ALL);
-                unit_invalidate_cgroup_bpf(u);
-        }
+        unit_invalidate_cgroup(u, _CGROUP_MASK_ALL);
+        unit_invalidate_cgroup_bpf(u);
 
         return 0;
 }
