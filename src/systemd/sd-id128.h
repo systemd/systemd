39d02a175f08c8cc9e4507e1abce26912d00fac3
Lennart Poettering <lennart@poettering.net>
2021-06-15 17:55:17 +0200

    sd-id128: document everywhere that we treat all UUIDs as Variant 1
    So in theory UUID Variant 2 (i.e. microsoft GUIDs) are supposed to be
    displayed in native endian. That is of course a bad idea, and Linux
    userspace generally didn't implement that, i.e. uuidd and similar.
    Hence, let's not bother either, but let's document that we treat
    everything the same as Variant 1, even if it declares something else.

--- src/systemd/sd-id128.h
+++ src/systemd/sd-id128.h
@@ -63,7 +63,9 @@ int sd_id128_get_boot_app_specific(sd_id128_t app_id, sd_id128_t *ret);
 #define SD_ID128_FORMAT_STR "%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x"
 #define SD_ID128_FORMAT_VAL(x) (x).bytes[0], (x).bytes[1], (x).bytes[2], (x).bytes[3], (x).bytes[4], (x).bytes[5], (x).bytes[6], (x).bytes[7], (x).bytes[8], (x).bytes[9], (x).bytes[10], (x).bytes[11], (x).bytes[12], (x).bytes[13], (x).bytes[14], (x).bytes[15]
 
-/* Like SD_ID128_FORMAT_STR, but formats as UUID, not in plain format */
+/* Like SD_ID128_FORMAT_STR, but formats as UUID, not in plain format (Strictly Big Endian byte order,
+ * i.e. treats everything as RFC4122 Variant 1 UUIDs, even if variant says otherwise, but matching other
+ * Linux userspace behaviour.) */
 #define SD_ID128_UUID_FORMAT_STR "%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x"
 
 #define SD_ID128_CONST_STR(x)                                           \
