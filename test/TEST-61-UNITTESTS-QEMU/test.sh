441d0d9e284916916ba2273d4418e06250d79338
Luca Boccassi <bluca@debian.org>
2021-06-17 22:53:16 +0100

    test-loop-block: run in qemu
    test-loop-block needs to run in qemu, so we are currently not
    testing it in the CI. Run it by itself in a separate job from
    TEST-02-UNITTESTS to avoid slowing that suite down.
    
    Fixes https://github.com/systemd/systemd/issues/19966

---
+++ test/TEST-61-UNITTESTS-QEMU/test.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+set -e
+
+TEST_DESCRIPTION="Run unit tests under qemu"
+# this subset of unit tests requires qemu, so they are ran here to avoid slowing down TEST-02
+TEST_NO_NSPAWN=1
+
+# embed some newlines in the kernel command line to stress our test suite
+KERNEL_APPEND="
+
+frobnicate!
+
+$KERNEL_APPEND
+"
+
+# shellcheck source=test/test-functions
+. "${TEST_BASE_DIR:?}/test-functions"
+
+check_result_nspawn() {
+    local workspace="${1:?}"
+    local ret=1
+
+    [[ -e "$workspace/testok" ]] && ret=0
+
+    if [[ -s "$workspace/failed" ]]; then
+        ret=$((ret + 1))
+        echo "=== Failed test log ==="
+        cat "$workspace/failed"
+    else
+        if [[ -s "$workspace/skipped" ]]; then
+            echo "=== Skipped test log =="
+            cat "$workspace/skipped"
+        fi
+        if [[ -s "$workspace/testok" ]]; then
+            echo "=== Passed tests ==="
+            cat "$workspace/testok"
+        fi
+    fi
+
+    save_journal "$workspace/var/log/journal"
+    _umount_dir "${initdir:?}"
+
+    [[ -n "${TIMED_OUT:=}" ]] && ret=1
+    return $ret
+}
+
+check_result_qemu() {
+    local ret=1
+
+    mount_initdir
+    [[ -e "${initdir:?}/testok" ]] && ret=0
+
+    if [[ -s "$initdir/failed" ]]; then
+        ret=$((ret + 1))
+        echo "=== Failed test log ==="
+        cat "$initdir/failed"
+    else
+        if [[ -s "$initdir/skipped" ]]; then
+            echo "=== Skipped test log =="
+            cat "$initdir/skipped"
+        fi
+        if [[ -s "$initdir/testok" ]]; then
+            echo "=== Passed tests ==="
+            cat "$initdir/testok"
+        fi
+    fi
+
+    save_journal "$initdir/var/log/journal"
+    _umount_dir "$initdir"
+
+    [[ -n "${TIMED_OUT:=}" ]] && ret=1
+    return $ret
+}
+
+do_test "$@"
