14808007fa037bcf4fd68dd987808a119a18cc23
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-06-18 16:02:46 +0900

    arp-util: use struct in_addr

--- src/libsystemd-network/arp-util.h
+++ src/libsystemd-network/arp-util.h
@@ -6,29 +6,30 @@
 ***/
 
 #include <net/ethernet.h>
+#include <netinet/in.h>
 
 #include "socket-util.h"
 #include "sparse-endian.h"
 
-int arp_network_bind_raw_socket(int index, be32_t address, const struct ether_addr *eth_mac);
+int arp_network_bind_raw_socket(int index, const struct in_addr *a, const struct ether_addr *eth_mac);
 
 int arp_send_packet(
                 int fd,
                 int ifindex,
-                be32_t pa,
+                const struct in_addr *pa,
                 const struct ether_addr *ha,
                 bool announce);
 static inline int arp_send_probe(
                 int fd,
                 int ifindex,
-                be32_t pa,
+                const struct in_addr *pa,
                 const struct ether_addr *ha) {
         return arp_send_packet(fd, ifindex, pa, ha, false);
 }
 static inline int arp_send_announcement(
                 int fd,
                 int ifindex,
-                be32_t pa,
+                const struct in_addr *pa,
                 const struct ether_addr *ha) {
         return arp_send_packet(fd, ifindex, pa, ha, true);
 }
