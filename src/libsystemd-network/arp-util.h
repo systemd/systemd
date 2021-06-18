d4b186877fade542515282383f25545583dc871a
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-06-18 15:40:38 +0900

    arp-util: make arp_send_{probe,announcement}() inline

--- src/libsystemd-network/arp-util.h
+++ src/libsystemd-network/arp-util.h
@@ -12,7 +12,23 @@
 
 int arp_network_bind_raw_socket(int index, be32_t address, const struct ether_addr *eth_mac);
 
-int arp_send_probe(int fd, int ifindex,
-                   be32_t pa, const struct ether_addr *ha);
-int arp_send_announcement(int fd, int ifindex,
-                          be32_t pa, const struct ether_addr *ha);
+int arp_send_packet(
+                int fd,
+                int ifindex,
+                be32_t pa,
+                const struct ether_addr *ha,
+                bool announce);
+static inline int arp_send_probe(
+                int fd,
+                int ifindex,
+                be32_t pa,
+                const struct ether_addr *ha) {
+        return arp_send_packet(fd, ifindex, pa, ha, false);
+}
+static inline int arp_send_announcement(
+                int fd,
+                int ifindex,
+                be32_t pa,
+                const struct ether_addr *ha) {
+        return arp_send_packet(fd, ifindex, pa, ha, true);
+}
