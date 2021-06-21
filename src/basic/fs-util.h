b25a930f0e2ebe77bc8b0f0acfac8a3b27ef1f0a
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2021-06-21 23:13:10 +0200

    basic,shared: move a bunch of files to src/shared/
    The goal is to move everything that requires selinux or smack
    away from src/basic/. This means that src/basic/label.[ch] must move,
    which implies btrfs-util.[ch], copy.[ch], and a bunch of other files
    which form a cluster of internal use.
    
    This is just moving text around, so there should be no functional difference.
    
    test-blockdev-util is new, because path_is_encrypted() is moved to
    blockdev-util.c, and so far we didn't have any tests for code there.

--- src/basic/fs-util.h
+++ src/basic/fs-util.h
@@ -145,8 +145,6 @@ int syncfs_path(int atfd, const char *path);
 
 int open_parent(const char *path, int flags, mode_t mode);
 
-int path_is_encrypted(const char *path);
-
 int conservative_renameat(int olddirfd, const char *oldpath, int newdirfd, const char *newpath);
 static inline int conservative_rename(const char *oldpath, const char *newpath) {
         return conservative_renameat(AT_FDCWD, oldpath, AT_FDCWD, newpath);
