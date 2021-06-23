f1433b5df5b4a3b255f3286448d7cc19d73ff0d3
David Tardon <dtardon@redhat.com>
2021-06-23 11:51:53 +0200

    udev-builtin-usb_id: replace strtoul by safe_ato*

--- src/udev/udev-builtin-usb_id.c
+++ src/udev/udev-builtin-usb_id.c
@@ -104,12 +104,10 @@ static int set_usb_mass_storage_ifsubtype(char *to, const char *from, size_t len
 }
 
 static void set_scsi_type(char *to, const char *from, size_t len) {
-        int type_num;
-        char *eptr;
+        unsigned type_num;
         const char *type = "generic";
 
-        type_num = strtoul(from, &eptr, 0);
-        if (eptr != from) {
+        if (safe_atou(from, &type_num) >= 0) {
                 switch (type_num) {
                 case 0:
                 case 0xe:
