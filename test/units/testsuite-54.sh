32a078ce88c99b87aa95a8c0a74cde7cc4e07438
Lennart Poettering <lennart@poettering.net>
2021-06-24 10:28:28 +0200

    test: extend credentials test to cover encrypted credentials

--- test/units/testsuite-54.sh
+++ test/units/testsuite-54.sh
@@ -27,6 +27,26 @@ systemd-run -p LoadCredential=passwd:/etc/passwd \
             rm '${CREDENTIALS_DIRECTORY}/passwd' \
     && { echo 'unexpected success'; exit 1; }
 
+echo -n $RANDOM > /tmp/test-54-plaintext
+systemd-creds encrypt --name=test-54 /tmp/test-54-plaintext /tmp/test-54-ciphertext
+systemd-creds decrypt --name=test-54 /tmp/test-54-ciphertext | cmp /tmp/test-54-plaintext
+
+systemd-run -p LoadCredentialEncrypted=test-54:/tmp/test-54-ciphertext \
+            --wait \
+            --pipe \
+            cat '${CREDENTIALS_DIRECTORY}/test-54' | cmp /tmp/test-54-plaintext
+
+echo -n $RANDOM > /tmp/test-54-plaintext
+systemd-creds encrypt --name=test-54 /tmp/test-54-plaintext /tmp/test-54-ciphertext
+systemd-creds decrypt --name=test-54 /tmp/test-54-ciphertext | cmp /tmp/test-54-plaintext
+
+systemd-run -p SetCredentialEncrypted=test-54:"`cat /tmp/test-54-ciphertext`" \
+            --wait \
+            --pipe \
+            cat '${CREDENTIALS_DIRECTORY}/test-54' | cmp /tmp/test-54-plaintext
+
+rm /tmp/test-54-plaintext /tmp/test-54-ciphertext
+
 systemd-analyze log-level info
 
 echo OK >/testok
