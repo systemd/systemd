66eeb8e85371d0c18044b5e7764d579b4a88a591
Curtis Klein <curtis.klein@hpe.com>
2021-06-27 15:36:49 -0700

    watchdog: small fixes for kernel compatibility
    Internally, the watchdog kernel infrastructure expects to get unsigned
    integers as the parameter for the watchdog ioctls. Additionally, since
    version 4.5, the max possible timeout is UINT_MAX / 1000 since it does
    calculations in milliseconds. A small helper function is added to make
    this conversion and saturation and will be used more in the next commit.

--- src/shared/watchdog.c
+++ src/shared/watchdog.c
@@ -19,6 +19,16 @@ static char *watchdog_device = NULL;
 static usec_t watchdog_timeout = USEC_INFINITY;
 static usec_t watchdog_last_ping = USEC_INFINITY;
 
+/* Starting from kernel version 4.5, the maximum allowable watchdog timeout is
+ * UINT_MAX/1000U seconds (since internal calculations are done in milliseconds).
+ */
+#define WATCHDOG_TIMEOUT_MAX_SEC (UINT_MAX/1000U)
+
+static unsigned saturated_usec_to_sec(usec_t val) {
+        usec_t t = DIV_ROUND_UP(val, USEC_PER_SEC);
+        return (unsigned) t >= WATCHDOG_TIMEOUT_MAX_SEC ? WATCHDOG_TIMEOUT_MAX_SEC : t; /* Saturate to watchdog max */
+}
+
 static int update_timeout(void) {
         if (watchdog_fd < 0)
                 return 0;
@@ -26,19 +36,17 @@ static int update_timeout(void) {
                 return 0;
 
         if (watchdog_timeout == 0) {
-                int flags;
+                unsigned flags;
 
                 flags = WDIOS_DISABLECARD;
                 if (ioctl(watchdog_fd, WDIOC_SETOPTIONS, &flags) < 0)
                         return log_warning_errno(errno, "Failed to disable hardware watchdog: %m");
         } else {
-                int sec, flags;
-                usec_t t;
+                unsigned sec, flags;
 
-                t = DIV_ROUND_UP(watchdog_timeout, USEC_PER_SEC);
-                sec = (int) t >= INT_MAX ? INT_MAX : t; /* Saturate */
+                sec = saturated_usec_to_sec(watchdog_timeout);
                 if (ioctl(watchdog_fd, WDIOC_SETTIMEOUT, &sec) < 0)
-                        return log_warning_errno(errno, "Failed to set timeout to %is: %m", sec);
+                        return log_warning_errno(errno, "Failed to set timeout to %us: %m", sec);
 
                 log_info("Set hardware watchdog to %s.", FORMAT_TIMESPAN(sec * USEC_PER_SEC, 0));
 
@@ -164,7 +172,7 @@ void watchdog_close(bool disarm) {
                 return;
 
         if (disarm) {
-                int flags;
+                unsigned flags;
 
                 /* Explicitly disarm it */
                 flags = WDIOS_DISABLECARD;
