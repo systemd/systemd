6a4a2646c63a59118a0659a4ae107fb3455c1884
Aditi Ambadkar <Aditi.Ambadkar@philips.com>
2021-06-28 13:53:24 +0400

    resolve: Add support to read mDNS cache via D-Bus method
    -"ShowCaches" takes no parameters and returns an array of structures
    - Each structure represents a cache entry and comprises - Interface, Protocol, Name, Type and Domain
    - Emit D-Bus signals ItemNew, ItemRemove, CacheExhausted and AllForNow on the basis of events
    - sd-bus API implements the D-Bus methods and signals

--- src/resolve/resolved-dns-scope.c
+++ src/resolve/resolved-dns-scope.c
@@ -1275,7 +1275,7 @@ void dns_scope_check_conflicts(DnsScope *scope, DnsPacket *p) {
 
                 /* Check for conflicts against the local cache. If so,
                  * send out an advisory query, to inform everybody */
-                r = dns_cache_check_conflicts(&scope->cache, rr, p->family, &p->sender);
+                r = dns_cache_check_conflicts(&scope->cache, rr, p->family, &p->sender, scope->link->ifname, scope->protocol);
                 if (r <= 0)
                         continue;
 
