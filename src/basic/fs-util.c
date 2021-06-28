831d14adcc252780f1eaf0bf2a54d6e3aced9c1b
Albert Brox <albert@exypno.tech>
2021-06-28 11:50:38 -0400

    ignore uid_to_name on fstat failure

--- src/basic/fs-util.c
+++ src/basic/fs-util.c
@@ -751,13 +751,10 @@ static int log_unsafe_transition(int a, int b, const char *path, unsigned flags)
         (void) fd_get_path(a, &n1);
         (void) fd_get_path(b, &n2);
 
-        if (fstat(a, &stat_a) < 0)
-                return -errno;
-        if (fstat(b, &stat_b) < 0)
-                return -errno;
-
-        user_a = uid_to_name(stat_a.st_uid);
-        user_b = uid_to_name(stat_b.st_uid);
+        if (fstat(a, &stat_a) == 0)
+            user_a = uid_to_name(stat_a.st_uid);
+        if (fstat(b, &stat_b) == 0)
+            user_b = uid_to_name(stat_b.st_uid);
 
         return log_warning_errno(SYNTHETIC_ERRNO(ENOLINK),
                                  "Detected unsafe path transition %s (owned by %s) %s %s (owned by %s) during canonicalization of %s.",
