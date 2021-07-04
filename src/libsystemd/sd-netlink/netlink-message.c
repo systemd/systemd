d8ad5d832d5f052a31bc358339da2d5ff208498f
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-07-04 14:58:20 +0900

    sd-netlink: rename functions

--- src/libsystemd/sd-netlink/netlink-message.c
+++ src/libsystemd/sd-netlink/netlink-message.c
@@ -557,9 +557,9 @@ int sd_netlink_message_open_container(sd_netlink_message *m, unsigned short type
                 if (r < 0)
                         return r;
 
-                r = type_system_union_protocol_get_type_system(type_system_union,
-                                                               &m->containers[m->n_containers + 1].type_system,
-                                                               family);
+                r = type_system_union_get_type_system_by_protocol(type_system_union,
+                                                                  &m->containers[m->n_containers + 1].type_system,
+                                                                  family);
                 if (r < 0)
                         return r;
         } else {
@@ -591,9 +591,9 @@ int sd_netlink_message_open_container_union(sd_netlink_message *m, unsigned shor
         if (r < 0)
                 return r;
 
-        r = type_system_union_get_type_system(type_system_union,
-                                              &m->containers[m->n_containers + 1].type_system,
-                                              key);
+        r = type_system_union_get_type_system_by_string(type_system_union,
+                                                        &m->containers[m->n_containers + 1].type_system,
+                                                        key);
         if (r < 0)
                 return r;
 
@@ -1138,9 +1138,9 @@ int sd_netlink_message_enter_container(sd_netlink_message *m, unsigned short typ
                         if (r < 0)
                                 return r;
 
-                        r = type_system_union_get_type_system(type_system_union,
-                                                              &type_system,
-                                                              key);
+                        r = type_system_union_get_type_system_by_string(type_system_union,
+                                                                        &type_system,
+                                                                        key);
                         if (r < 0)
                                 return r;
 
@@ -1154,9 +1154,9 @@ int sd_netlink_message_enter_container(sd_netlink_message *m, unsigned short typ
                         if (r < 0)
                                 return r;
 
-                        r = type_system_union_protocol_get_type_system(type_system_union,
-                                                                       &type_system,
-                                                                       family);
+                        r = type_system_union_get_type_system_by_protocol(type_system_union,
+                                                                          &type_system,
+                                                                          family);
                         if (r < 0)
                                 return r;
 
