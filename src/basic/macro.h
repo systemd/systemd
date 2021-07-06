4a7ccfdd551aac21c5bc95556be9d42a67e706a4
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2021-07-06 15:16:15 +0200

    basic/{time,format}-util: warn when format result is unused
    Now that anonymous buffers are used in almost all cases, code which
    does not use the return value is usually broken.

--- src/basic/macro.h
+++ src/basic/macro.h
@@ -30,6 +30,7 @@
 #define _weakref_(x) __attribute__((__weakref__(#x)))
 #define _alignas_(x) __attribute__((__aligned__(__alignof(x))))
 #define _alignptr_ __attribute__((__aligned__(sizeof(void*))))
+#define _warn_unused_result_ __attribute__((__warn_unused_result__))
 #if __GNUC__ >= 7
 #define _fallthrough_ __attribute__((__fallthrough__))
 #else
