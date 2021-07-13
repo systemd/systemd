184b4f78cfbded54a6e06bbe1152256c204a7a73
Iago Lopez Galeiras <iagol@microsoft.com>
2021-07-13 09:51:06 +0200

    core: add BPF LSM functions
    This adds 6 functions to implement RestrictFileSystems=
    
    * lsm_bpf_supported() checks if LSM BPF is supported. It checks that
      cgroupv2 is used, that BPF LSM is enabled, and tries to load the BPF
      LSM program which makes sure BTF and hash of maps are supported, and
      BPF LSM programs can be loaded.
    * lsm_bpf_setup() loads and attaches the LSM BPF program.
    * lsm_bpf_unit_restrict_filesystems() populates the hash of maps BPF map with the
      cgroupID and the set of allowed or denied filesystems.
    * lsm_bpf_cleanup() removes a cgroupID entry from the hash of maps.
    * lsm_bpf_map_restrict_fs_fd() is a helper function to get the file
      descriptor of the BPF map.
    * lsm_bpf_destroy() is a wrapper around the destroy function of the BPF
      skeleton file.

--- src/core/cgroup.c
+++ src/core/cgroup.c
@@ -2102,6 +2102,8 @@ static int unit_update_cgroup(
 
         bool created, is_root_slice;
         CGroupMask migrate_mask = 0;
+        _cleanup_free_ char *cgroup_full_path = NULL;
+        uint64_t cgroup_id = 0;
         int r;
 
         assert(u);
@@ -2119,6 +2121,18 @@ static int unit_update_cgroup(
         if (r < 0)
                 return log_unit_error_errno(u, r, "Failed to create cgroup %s: %m", empty_to_root(u->cgroup_path));
         created = r;
+
+        if (cg_unified_controller(SYSTEMD_CGROUP_CONTROLLER) > 0) {
+                r = cg_get_path(SYSTEMD_CGROUP_CONTROLLER, u->cgroup_path, NULL, &cgroup_full_path);
+                if (r == 0) {
+                        r = cg_path_get_cgroupid(cgroup_full_path, &cgroup_id);
+                        if (r < 0)
+                                log_unit_warning_errno(u, r, "Failed to get cgroup ID on cgroup %s, ignoring: %m", cgroup_full_path);
+                } else
+                        log_unit_warning_errno(u, r, "Failed to get full cgroup path on cgroup %s, ignoring: %m", empty_to_root(u->cgroup_path));
+
+                u->cgroup_id = cgroup_id;
+        }
 
         /* Start watching it */
         (void) unit_watch_cgroup(u);
