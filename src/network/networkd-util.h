8bce077047885f8f42100efcf9f6a391611d99c0
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-07-14 16:50:26 +0900

    network: introduce NetworkConfigSource and NetworkConfigState
    These will be used in later commits.

--- src/network/networkd-util.h
+++ src/network/networkd-util.h
@@ -19,10 +19,47 @@ typedef struct NetworkConfigSection {
         char filename[];
 } NetworkConfigSection;
 
+typedef enum NetworkConfigSource {
+        NETWORK_CONFIG_SOURCE_FOREIGN, /* configured by kernel */
+        NETWORK_CONFIG_SOURCE_STATIC,
+        NETWORK_CONFIG_SOURCE_IPV4LL,
+        NETWORK_CONFIG_SOURCE_DHCP4,
+        NETWORK_CONFIG_SOURCE_DHCP6,
+        NETWORK_CONFIG_SOURCE_DHCP6PD,
+        NETWORK_CONFIG_SOURCE_NDISC,
+        _NETWORK_CONFIG_SOURCE_MAX,
+        _NETWORK_CONFIG_SOURCE_INVALID = -EINVAL,
+} NetworkConfigSource;
+
+typedef enum NetworkConfigState {
+        NETWORK_CONFIG_STATE_REQUESTING  = 1 << 0, /* request is queued */
+        NETWORK_CONFIG_STATE_CONFIGURING = 1 << 1, /* e.g. address_configure() is called, but no responce is received yet */
+        NETWORK_CONFIG_STATE_CONFIGURED  = 1 << 2, /* e.g. address_configure() is called and received a response from kernel.
+                                                    * Note that address may not be ready yet, so please use address_is_ready()
+                                                    * to check whether the address can be usable or not. */
+        NETWORK_CONFIG_STATE_MARKED      = 1 << 3, /* used GC'ing the old config */
+        NETWORK_CONFIG_STATE_REMOVING    = 1 << 4, /* e.g. address_remove() is called, but no responce is received yet */
+} NetworkConfigState;
+
 CONFIG_PARSER_PROTOTYPE(config_parse_link_local_address_family);
 CONFIG_PARSER_PROTOTYPE(config_parse_address_family_with_kernel);
 CONFIG_PARSER_PROTOTYPE(config_parse_ip_masquerade);
 CONFIG_PARSER_PROTOTYPE(config_parse_mud_url);
+
+const char *network_config_source_to_string(NetworkConfigSource s) _const_;
+
+int network_config_state_to_string_alloc(NetworkConfigState s, char **ret);
+
+#define DEFINE_UPDATE_NETWORK_CONFIG_STATE(type, name)                  \
+        static inline void name##_update_state(                         \
+                        type *t,                                        \
+                        NetworkConfigState unset,                       \
+                        NetworkConfigState set) {                       \
+                                                                        \
+                assert(t);                                              \
+                                                                        \
+                t->state = (t->state & ~unset) | set;                   \
+        }
 
 const char *address_family_to_string(AddressFamily b) _const_;
 AddressFamily address_family_from_string(const char *s) _pure_;
