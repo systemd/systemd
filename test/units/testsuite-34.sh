7ced525afd0871a27a90fa0177235d20aa6bc5a7
Luca Boccassi <luca.boccassi@microsoft.com>
2021-07-14 18:22:21 +0100

    core: add [State|Runtime]Directory symlink as second parameter
    When combined with a tmpfs on /run or /var/lib, allows to create
    arbitrary and ephemeral symlinks for StateDirectory or RuntimeDirectory.
    This is especially useful when sharing these directories between
    different services, to make the same state/runtime directory 'backend'
    appear as different names to each service, so that they can be added/removed
    to a sharing agreement transparently, without code changes.
    
    An example (simplified, but real) use case:
    
    foo.service:
    StateDirectory=foo
    
    bar.service:
    StateDirectory=bar
    
    foo.service.d/shared.conf:
    StateDirectory=
    StateDirectory=shared:foo
    
    bar.service.d/shared.conf:
    StateDirectory=
    StateDirectory=shared:bar
    
    foo and bar use respectively /var/lib/foo and /var/lib/bar. Then
    the orchestration layer decides to stop this sharing, the drop-in
    can be removed. The services won't need any update and will keep
    working and being able to store state, transparently.
    
    To keep backward compatibility, new DBUS messages StateDirectorySymlink
    and RuntimeDirectorySymlink are added.

--- test/units/testsuite-34.sh
+++ test/units/testsuite-34.sh
@@ -11,6 +11,9 @@ systemd-analyze log-target console
 systemd-run --wait -p DynamicUser=0 -p StateDirectory=zzz touch /var/lib/zzz/test
 systemd-run --wait -p DynamicUser=0 -p StateDirectory=zzz test -f /var/lib/zzz/test
 systemd-run --wait -p DynamicUser=0 -p StateDirectory=zzz -p TemporaryFileSystem=/var/lib test -f /var/lib/zzz/test
+systemd-run --wait -p DynamicUser=0 -p StateDirectory=zzz:yyy test -f /var/lib/yyy/test
+systemd-run --wait -p DynamicUser=0 -p StateDirectory=zzz:xxx -p TemporaryFileSystem=/var/lib test -f /var/lib/xxx/test
+systemd-run --wait -p DynamicUser=0 -p StateDirectory=zzz:xxx -p TemporaryFileSystem=/var/lib:ro test -f /var/lib/xxx/test
 systemd-run --wait -p DynamicUser=0 -p StateDirectory=zzz test -f /var/lib/zzz/test-missing \
     && { echo 'unexpected success'; exit 1; }
 
@@ -24,11 +27,17 @@ test ! -f /var/lib/zzz/test-missing
 
 systemd-run --wait -p DynamicUser=1 -p StateDirectory=zzz test -f /var/lib/zzz/test
 systemd-run --wait -p DynamicUser=1 -p StateDirectory=zzz -p TemporaryFileSystem=/var/lib test -f /var/lib/zzz/test
+systemd-run --wait -p DynamicUser=1 -p StateDirectory=zzz:yyy test -f /var/lib/yyy/test
+systemd-run --wait -p DynamicUser=1 -p StateDirectory=zzz:xxx -p TemporaryFileSystem=/var/lib test -f /var/lib/xxx/test
+systemd-run --wait -p DynamicUser=1 -p StateDirectory=zzz:xxx -p TemporaryFileSystem=/var/lib:ro test -f /var/lib/xxx/test
 systemd-run --wait -p DynamicUser=1 -p StateDirectory=zzz test -f /var/lib/zzz/test-missing \
     && { echo 'unexpected success'; exit 1; }
 
 test -L /var/lib/zzz
+test -L /var/lib/yyy
 test -d /var/lib/private/zzz
+test ! -L /var/lib/private/xxx
+test ! -L /var/lib/xxx
 
 test -f /var/lib/zzz/test
 test ! -f /var/lib/zzz/test-missing
@@ -37,14 +46,38 @@ test ! -f /var/lib/zzz/test-missing
 
 systemd-run --wait -p DynamicUser=0 -p StateDirectory=zzz test -f /var/lib/zzz/test
 systemd-run --wait -p DynamicUser=0 -p StateDirectory=zzz -p TemporaryFileSystem=/var/lib test -f /var/lib/zzz/test
+systemd-run --wait -p DynamicUser=0 -p StateDirectory=zzz:yyy test -f /var/lib/yyy/test
+systemd-run --wait -p DynamicUser=0 -p StateDirectory=zzz:xxx -p TemporaryFileSystem=/var/lib test -f /var/lib/xxx/test
+systemd-run --wait -p DynamicUser=0 -p "StateDirectory=zzz:xxx zzz:xxx2" -p TemporaryFileSystem=/var/lib bash -c "test -f /var/lib/xxx/test && test -f /var/lib/xxx2/test"
+systemd-run --wait -p DynamicUser=0 -p StateDirectory=zzz:xxx -p TemporaryFileSystem=/var/lib:ro test -f /var/lib/xxx/test
 systemd-run --wait -p DynamicUser=0 -p StateDirectory=zzz test -f /var/lib/zzz/test-missing \
     && { echo 'unexpected success'; exit 1; }
 
+# Exercise the unit parsing paths too
+cat >/run/systemd/system/testservice-34.service <<EOF
+[Service]
+Type=oneshot
+TemporaryFileSystem=/var/lib
+StateDirectory=zzz:x\:yz zzz:x\:yz2
+ExecStart=test -f /var/lib/x:yz2/test
+ExecStart=test -f /var/lib/x:yz/test
+ExecStart=test -f /var/lib/zzz/test
+EOF
+systemctl daemon-reload
+systemctl start --wait testservice-34.service
+
 test -d /var/lib/zzz
+test ! -L /var/lib/xxx
+test ! -L /var/lib/xxx2
+test ! -L /var/lib/private/xxx
+test ! -L /var/lib/private/xxx2
+test -L /var/lib/yyy
 test ! -L /var/lib/zzz
 test ! -e /var/lib/private/zzz
 test -f /var/lib/zzz/test
 test ! -f /var/lib/zzz/test-missing
+test ! -L /var/lib/x:yz
+test ! -L /var/lib/x:yz2
 
 systemd-analyze log-level info
 
