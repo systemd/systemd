9b22b65c0c991e178db727d0645f180c7d5cc5ff
Christian Göttsche <cgzones@googlemail.com>
2021-07-28 16:59:57 +0200

    selinux: improve debug log format
    path might be NULL when checking against the system permissions, so wrap
    with strna().
    
    The command line might not be available over D-Bus and thus cl might be
    empty.  To improve readability quote the corresponding value.

--- src/core/selinux-access.c
+++ src/core/selinux-access.c
@@ -279,8 +279,8 @@ int mac_selinux_generic_access_check(
         }
 
         log_full_errno_zerook(LOG_DEBUG, r,
-                              "SELinux access check scon=%s tcon=%s tclass=%s perm=%s state=%s func=%s path=%s cmdline=%s: %m",
-                              scon, fcon, tclass, permission, enforce ? "enforcing" : "permissive", func, path, cl);
+                              "SELinux access check scon=%s tcon=%s tclass=%s perm=%s state=%s func=%s path=%s cmdline='%s': %m",
+                              scon, fcon, tclass, permission, enforce ? "enforcing" : "permissive", func, strna(path), cl);
         return enforce ? r : 0;
 }
 
