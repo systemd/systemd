08ed2fa2e1908fd91f819c43c791a7616cc2279f
Jan Janssen <medhefgo@web.de>
2021-07-28 13:07:26 +0200

    sd-boot: Add assert implementation
    There is a ASSERT() macro from gnu-efi, but that does not show any
    output to ConOut. Having to do some additional setup just to get
    some debug output is tedious and outright difficult on real hardware.
    
    The EFI_DEBUG define is repurposed from gnu-efi.

--- src/fundamental/macro-fundamental.h
+++ src/fundamental/macro-fundamental.h
@@ -50,7 +50,12 @@
 #define CONCATENATE(x, y) XCONCATENATE(x, y)
 
 #ifdef SD_BOOT
-#define assert(expr) do {} while (false)
+        #ifdef EFI_DEBUG
+                void efi_assert(const char *expr, const char *file, unsigned line) _noreturn_;
+                #define assert(expr) if (_unlikely_(!(expr))) { efi_assert(#expr, __FILE__, __LINE__); }
+        #else
+                #define assert(expr) do {} while (false)
+        #endif
 #endif
 
 #if defined(static_assert)
