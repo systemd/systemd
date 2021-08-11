33b3b9f59d4897263d12357b91c8d92f2394f60d
Jan Janssen <medhefgo@web.de>
2021-08-11 14:59:46 +0200

    sd-boot: Add assert implementation
    There is a ASSERT() macro from gnu-efi, but that does not show any
    output to ConOut. Having to do some additional setup just to get
    some debug output is tedious and outright difficult on real hardware.

---
+++ src/boot/efi/assert.c
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+
+#ifndef NDEBUG
+
+#include <efi.h>
+#include <efilib.h>
+#include "util.h"
+
+void efi_assert(const char *expr, const char *file, unsigned line, const char *function) {
+      log_error_stall(L"systemd-boot assertion '%a' failed at %a:%u, function %a(). Halting.", expr, file, line, function);
+      for (;;)
+            uefi_call_wrapper(BS->Stall, 1, 60 * 1000 * 1000);
+}
+
+#endif
