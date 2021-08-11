872d101c2308e07c523fd6ca84bd774447f05b7e
Jan Janssen <medhefgo@web.de>
2021-08-11 14:59:46 +0200

    sd-boot: Fix possible null pointer dereference
    Auto entries are showing garbage for the version in print_status()
    because StrDuplicate does not expect null pointers.
    
    (cherry picked from commit b52fafb26d90b77cfc259fcbdab3c95a571bacb1)

--- src/boot/efi/boot.c
+++ src/boot/efi/boot.c
@@ -1724,7 +1724,7 @@ static ConfigEntry *config_entry_add_loader(
         *entry = (ConfigEntry) {
                 .type = type,
                 .title = StrDuplicate(title),
-                .version = StrDuplicate(version),
+                .version = version ? StrDuplicate(version) : NULL,
                 .device = device,
                 .loader = StrDuplicate(loader),
                 .id = StrDuplicate(id),
