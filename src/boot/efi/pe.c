fbea71b90d96e794ca1abf2eab292236f2c1ef79
Jan Janssen <medhefgo@web.de>
2021-08-13 18:55:32 +0200

    sd-boot: Use PE definitions from gnu-efi
    No need for duplicate effort.
    
    There was also a bug in finding sections: The name field is 8 bytes
    long and padded with null bytes. If a section name is exactly that
    long, there is no null terminator, but we would still tell CompareMem
    to look that due to strlena().
    Also, since the sections we are searching for are listed as string
    literals, they are unlikely to be null-padded to 8 bytes, which would
    makes a successful CompareMem unlikely.
    
    Honestly, I have no idea how this was working so far...

--- src/boot/efi/pe.c
+++ src/boot/efi/pe.c
@@ -2,177 +2,165 @@
 
 #include <efi.h>
 #include <efilib.h>
+#include <pe.h>
 
 #include "pe.h"
 #include "util.h"
 
-struct DosFileHeader {
-        UINT8   Magic[2];
-        UINT16  LastSize;
-        UINT16  nBlocks;
-        UINT16  nReloc;
-        UINT16  HdrSize;
-        UINT16  MinAlloc;
-        UINT16  MaxAlloc;
-        UINT16  ss;
-        UINT16  sp;
-        UINT16  Checksum;
-        UINT16  ip;
-        UINT16  cs;
-        UINT16  RelocPos;
-        UINT16  nOverlay;
-        UINT16  reserved[4];
-        UINT16  OEMId;
-        UINT16  OEMInfo;
-        UINT16  reserved2[10];
-        UINT32  ExeHeader;
-} __attribute__((packed));
+#define MAX_SECTIONS 96
 
-#define PE_HEADER_MACHINE_I386          0x014c
-#define PE_HEADER_MACHINE_X64           0x8664
-#define PE_HEADER_MACHINE_ARM64         0xaa64
-#define PE_HEADER_MACHINE_ARM           0x01c2
-#define PE_HEADER_MACHINE_RISCV64       0x5064
-struct PeFileHeader {
-        UINT16  Machine;
-        UINT16  NumberOfSections;
-        UINT32  TimeDateStamp;
-        UINT32  PointerToSymbolTable;
-        UINT32  NumberOfSymbols;
-        UINT16  SizeOfOptionalHeader;
-        UINT16  Characteristics;
-} __attribute__((packed));
+#if defined(__i386__)
+        #define TARGET_MACHINE_TYPE EFI_IMAGE_MACHINE_IA32
+#elif defined(__x86_64__)
+        #define TARGET_MACHINE_TYPE EFI_IMAGE_MACHINE_X64
+#elif defined(__aarch64__)
+        #define TARGET_MACHINE_TYPE EFI_IMAGE_MACHINE_AARCH64
+#elif defined(__arm__)
+        #define TARGET_MACHINE_TYPE EFI_IMAGE_MACHINE_ARMTHUMB_MIXED
+#elif defined(__riscv) && __riscv_xlen == 64
+        #define TARGET_MACHINE_TYPE EFI_IMAGE_MACHINE_RISCV64
+#else
+        #error Unknown EFI arch
+#endif
 
-struct PeHeader {
-        UINT8   Magic[4];
-        struct PeFileHeader FileHeader;
-} __attribute__((packed));
+static inline BOOLEAN verify_dos(const IMAGE_DOS_HEADER *dos) {
+        assert(dos);
+        return dos->e_magic == IMAGE_DOS_SIGNATURE;
+}
 
-struct PeSectionHeader {
-        UINT8   Name[8];
-        UINT32  VirtualSize;
-        UINT32  VirtualAddress;
-        UINT32  SizeOfRawData;
-        UINT32  PointerToRawData;
-        UINT32  PointerToRelocations;
-        UINT32  PointerToLinenumbers;
-        UINT16  NumberOfRelocations;
-        UINT16  NumberOfLinenumbers;
-        UINT32  Characteristics;
-} __attribute__((packed));
+static inline BOOLEAN verify_pe(const IMAGE_NT_HEADERS *pe) {
+        assert(pe);
+        return pe->Signature == IMAGE_NT_SIGNATURE &&
+               pe->FileHeader.Machine == TARGET_MACHINE_TYPE &&
+               pe->FileHeader.NumberOfSections > 0 &&
+               pe->FileHeader.NumberOfSections <= MAX_SECTIONS;
+}
 
-EFI_STATUS pe_memory_locate_sections(CHAR8 *base, CHAR8 **sections, UINTN *addrs, UINTN *offsets, UINTN *sizes) {
-        struct DosFileHeader *dos;
-        struct PeHeader *pe;
+static inline UINTN section_table_offset(const IMAGE_DOS_HEADER *dos, const IMAGE_NT_HEADERS *pe) {
+        assert(dos);
+        assert(pe);
+        return dos->e_lfanew +
+               OFFSETOF(IMAGE_NT_HEADERS, OptionalHeader) +
+               pe->FileHeader.SizeOfOptionalHeader;
+}
+
+static VOID locate_sections(
+                const IMAGE_SECTION_HEADER section_table[],
+                UINTN n_table,
+                const CHAR8 **sections,
+                UINTN *addrs,
+                UINTN *offsets,
+                UINTN *sizes) {
+
+        assert(section_table);
+        assert(sections);
+        assert(sizes);
+
+        for (UINTN i = 0; i < n_table; i++) {
+                const IMAGE_SECTION_HEADER *sect = &section_table[i];
+
+                for (UINTN j = 0; sections[j]; j++) {
+                        /* If section name length is exactly 8, the name is *not* null terminated!. */
+                        if (strncmpa(sect->Name, sections[j], ELEMENTSOF(sect->Name)) != 0)
+                                continue;
+
+                        if (addrs)
+                                addrs[j] = sect->VirtualAddress;
+                        if (offsets)
+                                offsets[j] = sect->PointerToRawData;
+                        sizes[j] = sect->Misc.VirtualSize;
+                }
+        }
+}
+
+EFI_STATUS pe_memory_locate_sections(
+                const CHAR8 *base,
+                const CHAR8 **sections,
+                UINTN *addrs,
+                UINTN *offsets,
+                UINTN *sizes) {
+        IMAGE_DOS_HEADER *dos;
+        IMAGE_NT_HEADERS *pe;
         UINTN offset;
 
         assert(base);
         assert(sections);
+        assert(sizes);
 
-        dos = (struct DosFileHeader *)base;
-
-        if (CompareMem(dos->Magic, "MZ", 2) != 0)
+        dos = (IMAGE_DOS_HEADER*)base;
+        if (!verify_dos(dos))
                 return EFI_LOAD_ERROR;
 
-        pe = (struct PeHeader *)&base[dos->ExeHeader];
-        if (CompareMem(pe->Magic, "PE\0\0", 4) != 0)
+        pe = (IMAGE_NT_HEADERS*)&base[dos->e_lfanew];
+        if (!verify_pe(pe))
                 return EFI_LOAD_ERROR;
 
-        /* PE32+ Subsystem type */
-        if (pe->FileHeader.Machine != PE_HEADER_MACHINE_X64 &&
-            pe->FileHeader.Machine != PE_HEADER_MACHINE_ARM64 &&
-            pe->FileHeader.Machine != PE_HEADER_MACHINE_I386 &&
-            pe->FileHeader.Machine != PE_HEADER_MACHINE_ARM &&
-            pe->FileHeader.Machine != PE_HEADER_MACHINE_RISCV64)
-                return EFI_LOAD_ERROR;
-
-        if (pe->FileHeader.NumberOfSections > 96)
-                return EFI_LOAD_ERROR;
-
-        offset = dos->ExeHeader + sizeof(*pe) + pe->FileHeader.SizeOfOptionalHeader;
-
-        for (UINTN i = 0; i < pe->FileHeader.NumberOfSections; i++) {
-                struct PeSectionHeader *sect;
-
-                sect = (struct PeSectionHeader *)&base[offset];
-                for (UINTN j = 0; sections[j]; j++) {
-                        if (CompareMem(sect->Name, sections[j], strlena(sections[j])) != 0)
-                                continue;
-
-                        if (addrs)
-                                addrs[j] = (UINTN)sect->VirtualAddress;
-                        if (offsets)
-                                offsets[j] = (UINTN)sect->PointerToRawData;
-                        if (sizes)
-                                sizes[j] = (UINTN)sect->VirtualSize;
-                }
-                offset += sizeof(*sect);
-        }
+        offset = section_table_offset(dos, pe);
+        locate_sections((IMAGE_SECTION_HEADER*)&base[offset], pe->FileHeader.NumberOfSections,
+                        sections, addrs, offsets, sizes);
 
         return EFI_SUCCESS;
 }
 
-EFI_STATUS pe_file_locate_sections(EFI_FILE *dir, CHAR16 *path, CHAR8 **sections, UINTN *addrs, UINTN *offsets, UINTN *sizes) {
-        EFI_FILE_HANDLE handle;
-        struct DosFileHeader dos;
-        struct PeHeader pe;
-        UINTN len;
-        UINTN headerlen;
+EFI_STATUS pe_file_locate_sections(
+                EFI_FILE *dir,
+                const CHAR16 *path,
+                const CHAR8 **sections,
+                UINTN *addrs,
+                UINTN *offsets,
+                UINTN *sizes) {
+        _cleanup_(FileHandleClosep) EFI_FILE_HANDLE handle = NULL;
+        IMAGE_DOS_HEADER dos;
+        IMAGE_NT_HEADERS pe;
+        UINTN len, section_table_len;
         EFI_STATUS err;
-        _cleanup_freepool_ CHAR8 *header = NULL;
+        _cleanup_freepool_ IMAGE_SECTION_HEADER *section_table = NULL;
 
         assert(dir);
         assert(path);
+        assert(sections);
+        assert(sizes);
 
-        err = uefi_call_wrapper(dir->Open, 5, dir, &handle, path, EFI_FILE_MODE_READ, 0ULL);
+        err = uefi_call_wrapper(dir->Open, 5, dir, &handle, (CHAR16*)path, EFI_FILE_MODE_READ, 0ULL);
         if (EFI_ERROR(err))
                 return err;
 
-        /* MS-DOS stub */
         len = sizeof(dos);
         err = uefi_call_wrapper(handle->Read, 3, handle, &len, &dos);
         if (EFI_ERROR(err))
-                goto out;
-        if (len != sizeof(dos)) {
-                err = EFI_LOAD_ERROR;
-                goto out;
-        }
+                return err;
+        if (len != sizeof(dos) || !verify_dos(&dos))
+                return EFI_LOAD_ERROR;
 
-        err = uefi_call_wrapper(handle->SetPosition, 2, handle, dos.ExeHeader);
+        err = uefi_call_wrapper(handle->SetPosition, 2, handle, dos.e_lfanew);
         if (EFI_ERROR(err))
-                goto out;
+                return err;
 
         len = sizeof(pe);
         err = uefi_call_wrapper(handle->Read, 3, handle, &len, &pe);
         if (EFI_ERROR(err))
-                goto out;
-        if (len != sizeof(pe)) {
-                err = EFI_LOAD_ERROR;
-                goto out;
-        }
+                return err;
+        if (len != sizeof(pe) || !verify_pe(&pe))
+                return EFI_LOAD_ERROR;
 
-        headerlen = sizeof(dos) + sizeof(pe) + pe.FileHeader.SizeOfOptionalHeader + pe.FileHeader.NumberOfSections * sizeof(struct PeSectionHeader);
-        header = AllocatePool(headerlen);
-        if (!header) {
-                err = EFI_OUT_OF_RESOURCES;
-                goto out;
-        }
-        len = headerlen;
-        err = uefi_call_wrapper(handle->SetPosition, 2, handle, 0);
+        section_table_len = pe.FileHeader.NumberOfSections * sizeof(IMAGE_SECTION_HEADER);
+        section_table = AllocatePool(section_table_len);
+        if (!section_table)
+                return EFI_OUT_OF_RESOURCES;
+
+        err = uefi_call_wrapper(handle->SetPosition, 2, handle, section_table_offset(&dos, &pe));
         if (EFI_ERROR(err))
-                goto out;
+                return err;
 
-        err = uefi_call_wrapper(handle->Read, 3, handle, &len, header);
+        len = section_table_len;
+        err = uefi_call_wrapper(handle->Read, 3, handle, &len, section_table);
         if (EFI_ERROR(err))
-                goto out;
+                return err;
+        if (len != section_table_len)
+                return EFI_LOAD_ERROR;
 
-        if (len != headerlen) {
-                err = EFI_LOAD_ERROR;
-                goto out;
-        }
+        locate_sections(section_table, pe.FileHeader.NumberOfSections,
+                        sections, addrs, offsets, sizes);
 
-        err = pe_memory_locate_sections(header, sections, addrs, offsets, sizes);
-out:
-        uefi_call_wrapper(handle->Close, 1, handle);
-        return err;
+        return EFI_SUCCESS;
 }
