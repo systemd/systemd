58ca99d20b13091ddbe2cea171026ec65fde6af7
Jan Janssen <medhefgo@web.de>
2021-08-15 13:44:42 +0200

    sd-boot: Simplify setting console mode

--- src/boot/efi/console.h
+++ src/boot/efi/console.h
@@ -9,12 +9,15 @@
 #define KEYCHAR(k) ((k) & 0xffff)
 #define CHAR_CTRL(c) ((c) - 'a' + 1)
 
-enum console_mode_change_type {
-        CONSOLE_MODE_KEEP = 0,
-        CONSOLE_MODE_SET,
+enum console_mode_special {
+        /* Valid UEFI console modes are 0..INT32_MAX. */
+        CONSOLE_MODE_KEEP = INT32_MAX + 1U,
         CONSOLE_MODE_AUTO,
-        CONSOLE_MODE_MAX,
+        CONSOLE_MODE_AUTO_MAX,
+        /* UEFI indicates -1 if the device is not in a valid text mode. */
+        _CONSOLE_MODE_INVALID = (UINT32)-1,
 };
 
 EFI_STATUS console_key_read(UINT64 *key, UINT64 timeout_usec);
-EFI_STATUS console_set_mode(UINTN *mode, enum console_mode_change_type how);
+EFI_STATUS console_set_mode(UINT32 mode);
+EFI_STATUS console_query_mode(UINTN *x_max, UINTN *y_max);
