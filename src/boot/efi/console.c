c368a1df5fde38b1020c4c2ecd3a5d1d3fb19d07
Jan Janssen <medhefgo@web.de>
2021-08-15 13:44:47 +0200

    sd-boot: Add support for changing console mode at runtime

--- src/boot/efi/console.c
+++ src/boot/efi/console.c
@@ -193,6 +193,22 @@ EFI_STATUS console_set_mode(UINT32 mode) {
                         return change_mode(CONSOLE_MODE_FIRMWARE_RANGE_MIN);
                 return EFI_SUCCESS;
 
+        case CONSOLE_MODE_NEXT:
+                if (ST->ConOut->Mode->MaxMode <= CONSOLE_MODE_FIRMWARE_RANGE_MIN)
+                        return EFI_UNSUPPORTED;
+
+                /* If mode is invalid (-1) here we'll end up switching to mode 0, which is desired. */
+                mode = (UINT32)ST->ConOut->Mode->Mode;
+                do {
+                        mode = (mode + 1) % ST->ConOut->Mode->MaxMode;
+                        if (!EFI_ERROR(change_mode(mode)))
+                                break;
+                        /* If this mode is broken/unsupported, try the next.
+                         * If mode is 0, we wrapped around and should stop. */
+                } while (mode > CONSOLE_MODE_FIRMWARE_RANGE_MIN);
+
+                return EFI_SUCCESS;
+
         case CONSOLE_MODE_AUTO:
                 return change_mode(get_auto_mode());
 
