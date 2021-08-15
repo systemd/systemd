3af74397f0d87fb04ea2db41a6a64fa34248543a
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-08-16 01:03:25 +0900

    network: can: introduce config_parse_can_control_mode()

--- src/network/networkd-can.h
+++ src/network/networkd-can.h
@@ -1,6 +1,8 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 #pragma once
 
+#include <linux/can/netlink.h>
+
 #include "sd-netlink.h"
 
 #include "conf-parser.h"
@@ -11,3 +13,4 @@ int can_set_netlink_message(Link *link, sd_netlink_message *m);
 
 CONFIG_PARSER_PROTOTYPE(config_parse_can_bitrate);
 CONFIG_PARSER_PROTOTYPE(config_parse_can_restart_usec);
+CONFIG_PARSER_PROTOTYPE(config_parse_can_control_mode);
