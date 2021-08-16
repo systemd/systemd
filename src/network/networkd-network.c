634815762d837e25ed4640cb8a12fa742a13d9ac
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-08-16 12:41:34 +0900

    network: rebreak conf parser arguments

--- src/network/networkd-network.c
+++ src/network/networkd-network.c
@@ -719,7 +719,8 @@ bool network_has_static_ipv6_configurations(Network *network) {
         return false;
 }
 
-int config_parse_stacked_netdev(const char *unit,
+int config_parse_stacked_netdev(
+                const char *unit,
                 const char *filename,
                 unsigned line,
                 const char *section,
@@ -729,6 +730,7 @@ int config_parse_stacked_netdev(const char *unit,
                 const char *rvalue,
                 void *data,
                 void *userdata) {
+
         _cleanup_free_ char *name = NULL;
         NetDevKind kind = ltype;
         Hashmap **h = data;
