b259f772b2ebc04873fc9d5cfc69207b8b539c0a
Jiri Pirko <jiri@nvidia.com>
2021-08-19 14:13:32 +0200

    devlinkd: add devlinkd base
    Introduce a skeleton of devlinkd. This prepares the infrastructure in
    order to introduce individual matches and objects in follow-up patchset.
    
    There is one configuration file per object. Devlinkd first loads
    configuration files. Then it processes incoming devlink netlink messages,
    matches against the "Match*" sections in configuration and processes
    per object and per message type actions.

---
+++ src/devlink/devlinkd-manager.h
@@ -0,0 +1,43 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#pragma once
+
+#include "sd-event.h"
+#include "sd-netlink.h"
+#include "hashmap.h"
+
+#include "devlink-key.h"
+
+#include <stdbool.h>
+
+typedef struct KindInfo {
+        unsigned count;
+        bool dirty;
+} KindInfo;
+
+typedef struct Manager Manager;
+
+struct Manager {
+        sd_netlink *genl;
+        sd_netlink *rtnl;
+        sd_event *event;
+        sd_event_source *periodic_enumeration_event_source;
+        sd_bus *bus;
+        Hashmap *devlink_objs;
+        KindInfo kind_info[_DEVLINK_KIND_MAX];
+};
+
+int manager_rtnl_query_one(Manager *m, uint32_t ifindex);
+int manager_setup(Manager *m);
+int manager_new(Manager **ret);
+Manager* manager_free(Manager *m);
+
+int manager_start(Manager *m);
+int manager_config_load(Manager *m);
+
+void manager_kind_inc(Manager *m, DevlinkKind kind);
+void manager_kind_dec(Manager *m, DevlinkKind kind);
+
+int manager_enumerate(Manager *m);
+void manager_enumerate_by_key(Manager *m, DevlinkKey *key);
+
+DEFINE_TRIVIAL_CLEANUP_FUNC(Manager *, manager_free);
--- src/network/networkd-manager.c
+++ src/network/networkd-manager.c
@@ -777,7 +777,7 @@ int manager_start(Manager *m) {
         return 0;
 }
 
-int manager_load_config(Manager *m) {
+int manager_config_load(Manager *m) {
         int r;
 
         log_debug("Loading...");
