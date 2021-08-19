b121886ee76a82f8fd101af88c9a8c03460c300d
Jiri Pirko <jiri@nvidia.com>
2021-08-19 14:13:32 +0200

    devlinkd: add devlinkd base
    Introduce a skeleton of devlinkd. This prepares the infrastructure in
    order to introduce individual matches and objects in follow-up patchset.
    
    There is one configuration file per object. Devlinkd first loads
    configuration files. Then it processes incoming devlink netlink messages,
    matches against the "Match*" sections in configuration and processes
    per object and per message type actions.

---
+++ src/shared/devlink-util.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#pragma once
+
+#include <errno.h>
+#include <linux/devlink.h>
+
+#include "macro.h"
+
+const char *devlink_cmd_to_string(int cmd) _const_;
