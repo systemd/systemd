3dd0ec5bba6af5099aa2131c74c09b2c6d824a57
Lennart Poettering <lennart@poettering.net>
2021-08-20 10:51:53 +0200

    sd-id128: add compound literal love to sd_id128_to_string() + id128_to_uuid_string()

--- src/libsystemd/sd-id128/id128-util.h
+++ src/libsystemd/sd-id128/id128-util.h
@@ -12,6 +12,8 @@
 
 char *id128_to_uuid_string(sd_id128_t id, char s[static ID128_UUID_STRING_MAX]);
 
+#define ID128_TO_UUID_STRING(id) id128_to_uuid_string((id), (char[ID128_UUID_STRING_MAX]) {})
+
 bool id128_is_valid(const char *s) _pure_;
 
 typedef enum Id128Format {
