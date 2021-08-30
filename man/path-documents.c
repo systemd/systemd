899db31fa267505223f3f6233fec32326e5db6d6
Thomas Mühlbacher <tmuehlbacher@posteo.net>
2021-08-30 16:16:30 +0200

    man: Don't leak memory in path-documents example
    The `sd_path_lookup(3)` man page states that the returned string shall be
    `free(3)`'d but then doesn't do so in the example code.
    
    Also add basic error handling as well.

--- man/path-documents.c
+++ man/path-documents.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sd-path.h>
 
 int main(void) {
+  int r;
   char *t;
 
-  sd_path_lookup(SD_PATH_USER_DOCUMENTS, NULL, &t);
+  r = sd_path_lookup(SD_PATH_USER_DOCUMENTS, NULL, &t);
+  if (r < 0)
+    return EXIT_FAILURE;
+
   printf("~/Documents: %s\n", t);
+  free(t);
+
+  return EXIT_SUCCESS;
 }
