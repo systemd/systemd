c50a647598665694f107602d76ddc9da4db2c626
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-09-02 12:05:14 +0900

    fs-util: drop unnecessary initialization

--- src/basic/fs-util.c
+++ src/basic/fs-util.c
@@ -375,7 +375,7 @@ int fd_warn_permissions(const char *path, int fd) {
 
 int touch_file(const char *path, bool parents, usec_t stamp, uid_t uid, gid_t gid, mode_t mode) {
         _cleanup_close_ int fd = -1;
-        int r, ret = 0;
+        int r, ret;
 
         assert(path);
 
