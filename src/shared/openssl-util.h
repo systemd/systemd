5d67527e68975a1144a90054a64caca474520ebe
Lennart Poettering <lennart@poettering.net>
2021-09-09 17:55:36 +0200

    dissect-image: optionally, validate dm-verity signatures in userspace
    Getting certificates for dm-verity roothash signing into the trusted
    kernel keychain is a royal PITA (means recompiling or rebooting with
    shim), hence let's add a minimal userspace PKCS7 validation as well.
    
    The mechanism is really simple and compatible with the verification the
    kernel does. The only difference is that the certificates are searched
    in /etc/verity.d/*.crt (and similar dirs in /usr/lib/, …).
    
    We'll first try validation by passing the PKCS#7 data to the kernel, but
    if that doesn't work we'll see if one of the certificates found that way
    works and then attempt to attach the image without passing the PKCS#7
    data to the kernel.
    
    This makes it very easy to have fully validated GPT disk images. For
    example, just copy the 'mkosi.secure-boot.crt' file you have in your
    mkosi build dir to /etc/verity.d/ and things should just work.

--- src/shared/openssl-util.h
+++ src/shared/openssl-util.h
@@ -4,13 +4,26 @@
 #include "macro.h"
 
 #if HAVE_OPENSSL
+#  include <openssl/bio.h>
 #  include <openssl/evp.h>
-#  include <openssl/x509.h>
+#  include <openssl/pkcs7.h>
+#  include <openssl/ssl.h>
+#  include <openssl/x509v3.h>
 
 DEFINE_TRIVIAL_CLEANUP_FUNC_FULL(X509*, X509_free, NULL);
 DEFINE_TRIVIAL_CLEANUP_FUNC_FULL(X509_NAME*, X509_NAME_free, NULL);
 DEFINE_TRIVIAL_CLEANUP_FUNC_FULL(EVP_PKEY_CTX*, EVP_PKEY_CTX_free, NULL);
 DEFINE_TRIVIAL_CLEANUP_FUNC_FULL(EVP_CIPHER_CTX*, EVP_CIPHER_CTX_free, NULL);
+DEFINE_TRIVIAL_CLEANUP_FUNC_FULL(PKCS7*, PKCS7_free, NULL);
+DEFINE_TRIVIAL_CLEANUP_FUNC_FULL(SSL*, SSL_free, NULL);
+DEFINE_TRIVIAL_CLEANUP_FUNC_FULL(BIO*, BIO_free, NULL);
+
+static inline void sk_X509_free_allp(STACK_OF(X509) **sk) {
+        if (!sk || !*sk)
+                return;
+
+        sk_X509_pop_free(*sk, X509_free);
+}
 
 int rsa_encrypt_bytes(EVP_PKEY *pkey, const void *decrypted_key, size_t decrypted_key_size, void **ret_encrypt_key, size_t *ret_encrypt_key_size);
 
