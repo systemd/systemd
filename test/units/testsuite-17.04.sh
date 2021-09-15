e70f57796d29c1c538f77c8678d58e57f64eb7fb
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-09-16 06:34:45 +0900

    test: use --settle option for udevadm trigger
    Then, we can drop the loops.
    This also drops unnecessary `udevadm control --reload`.

--- test/units/testsuite-17.04.sh
+++ test/units/testsuite-17.04.sh
@@ -15,42 +15,34 @@ ACTION=="change", SUBSYSTEM=="mem", KERNEL=="null", TAG+="changed"
 EOF
 
 udevadm control --reload
-udevadm trigger -c add /dev/null
+udevadm trigger --settle --action add /dev/null
 
-while   test ! -f /run/udev/tags/added/c1:3 ||
-        test -f /run/udev/tags/changed/c1:3 ||
-        ! udevadm info /dev/null | grep -q 'E: TAGS=.*:added:.*' ||
-        ! udevadm info /dev/null | grep -q 'E: CURRENT_TAGS=.*:added:.*' ||
-        udevadm info /dev/null | grep -q 'E: TAGS=.*:changed:.*' ||
-        udevadm info /dev/null | grep -q 'E: CURRENT_TAGS=.*:changed:.*'
-do
-    sleep .5
-done
+test -f /run/udev/tags/added/c1:3
+test ! -f /run/udev/tags/changed/c1:3
+udevadm info /dev/null | grep -q 'E: TAGS=.*:added:.*'
+udevadm info /dev/null | grep -q 'E: CURRENT_TAGS=.*:added:.*'
+udevadm info /dev/null | grep -q 'E: TAGS=.*:changed:.*' && { echo 'unexpected TAGS='; exit 1; }
+udevadm info /dev/null | grep -q 'E: CURRENT_TAGS=.*:changed:.*' && { echo 'unexpected CURRENT_TAGS='; exit 1; }
 
+udevadm trigger --settle --action change /dev/null
+
+test -f /run/udev/tags/added/c1:3
+test -f /run/udev/tags/changed/c1:3
+udevadm info /dev/null | grep -q 'E: TAGS=.*:added:.*'
+udevadm info /dev/null | grep -q 'E: CURRENT_TAGS=.*:added:.*' && { echo 'unexpected CURRENT_TAGS='; exit 1; }
+udevadm info /dev/null | grep -q 'E: TAGS=.*:changed:.*'
+udevadm info /dev/null | grep -q 'E: CURRENT_TAGS=.*:changed:.*'
+
+udevadm trigger --settle --action add /dev/null
+
+test -f /run/udev/tags/added/c1:3
+test -f /run/udev/tags/changed/c1:3
+udevadm info /dev/null | grep -q 'E: TAGS=.*:added:.*'
+udevadm info /dev/null | grep -q 'E: CURRENT_TAGS=.*:added:.*'
+udevadm info /dev/null | grep -q 'E: TAGS=.*:changed:.*'
+udevadm info /dev/null | grep -q 'E: CURRENT_TAGS=.*:changed:.*' && { echo 'unexpected CURRENT_TAGS='; exit 1; }
+
+rm /run/udev/rules.d/50-testsuite.rules
 udevadm control --reload
-udevadm trigger -c change /dev/null
-
-while   test ! -f /run/udev/tags/added/c1:3 ||
-        test ! -f /run/udev/tags/changed/c1:3 ||
-        ! udevadm info /dev/null | grep -q 'E: TAGS=.*:added:.*' ||
-        udevadm info /dev/null | grep -q 'E: CURRENT_TAGS=.*:added:.*' ||
-        ! udevadm info /dev/null | grep -q 'E: TAGS=.*:changed:.*' ||
-        ! udevadm info /dev/null | grep -q 'E: CURRENT_TAGS=.*:changed:.*'
-do
-    sleep .5
-done
-
-udevadm control --reload
-udevadm trigger -c add /dev/null
-
-while   test ! -f /run/udev/tags/added/c1:3 ||
-        test ! -f /run/udev/tags/changed/c1:3 ||
-        ! udevadm info /dev/null | grep -q 'E: TAGS=.*:added:.*' ||
-        ! udevadm info /dev/null | grep -q 'E: CURRENT_TAGS=.*:added:.*' ||
-        ! udevadm info /dev/null | grep -q 'E: TAGS=.*:changed:.*' ||
-        udevadm info /dev/null | grep -q 'E: CURRENT_TAGS=.*:changed:.*'
-do
-    sleep .5
-done
 
 exit 0
