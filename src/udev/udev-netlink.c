e439e92318e38ec238f42a103c2958514ad2d51b
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-09-15 12:20:04 +0900

    udev: rename udev-builtin-net_id-netlink.[ch]

--- src/udev/udev-netlink.c
+++ src/udev/udev-netlink.c
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 
 #include "netlink-util.h"
-#include "udev-builtin-net_id-netlink.h"
+#include "udev-netlink.h"
 
 void link_info_clear(LinkInfo *info) {
         if (!info)
