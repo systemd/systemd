52d8baf3c569d26e76bfa7bfd62c34c715b6aced
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-09-21 05:05:39 +0900

    sd-netlink: allow to set dump flag for genl or netfilter messages

--- src/libsystemd/sd-netlink/netlink-message.c
+++ src/libsystemd/sd-netlink/netlink-message.c
@@ -117,9 +117,10 @@ int sd_netlink_message_request_dump(sd_netlink_message *m, int dump) {
         assert_return(m, -EINVAL);
         assert_return(m->hdr, -EINVAL);
 
-        assert_return(IN_SET(m->hdr->nlmsg_type,
-                             RTM_GETLINK, RTM_GETLINKPROP, RTM_GETADDR, RTM_GETROUTE, RTM_GETNEIGH,
-                             RTM_GETRULE, RTM_GETADDRLABEL, RTM_GETNEXTHOP), -EINVAL);
+        if (m->protocol == NETLINK_ROUTE)
+                assert_return(IN_SET(m->hdr->nlmsg_type,
+                                     RTM_GETLINK, RTM_GETLINKPROP, RTM_GETADDR, RTM_GETROUTE, RTM_GETNEIGH,
+                                     RTM_GETRULE, RTM_GETADDRLABEL, RTM_GETNEXTHOP), -EINVAL);
 
         SET_FLAG(m->hdr->nlmsg_flags, NLM_F_DUMP, dump);
 
