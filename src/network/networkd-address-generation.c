1d4508dc4236993a382ab605e7618761d2bde02e
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-09-25 04:04:09 +0900

    network: introduce Token=eui64
    So, now user can explicitly request EUI-64 algorithm to generate addresses.

--- src/network/networkd-address-generation.c
+++ src/network/networkd-address-generation.c
@@ -25,6 +25,7 @@
 #define NDISC_APP_ID   SD_ID128_MAKE(13,ac,81,a7,d5,3f,49,78,92,79,5d,0c,29,3a,bc,7e)
 
 typedef enum AddressGenerationType {
+        ADDRESS_GENERATION_EUI64,
         ADDRESS_GENERATION_STATIC,
         ADDRESS_GENERATION_PREFIXSTABLE,
         _ADDRESS_GENERATION_TYPE_MAX,
@@ -190,6 +191,10 @@ static int generate_addresses(
                 struct in6_addr addr, *copy;
 
                 switch (j->type) {
+                case ADDRESS_GENERATION_EUI64:
+                        generate_eui64_address(link, &masked, &addr);
+                        break;
+
                 case ADDRESS_GENERATION_STATIC:
                         memcpy(addr.s6_addr, masked.s6_addr, 8);
                         memcpy(addr.s6_addr + 8, j->address.s6_addr + 8, 8);
@@ -323,6 +328,9 @@ int config_parse_address_generation_type(
                         return 0;
                 }
 
+        } else if (streq(rvalue, "eui64")) {
+                type = ADDRESS_GENERATION_EUI64;
+                p = NULL;
         } else {
                 type = ADDRESS_GENERATION_STATIC;
 
@@ -342,6 +350,10 @@ int config_parse_address_generation_type(
         }
 
         switch (type) {
+        case ADDRESS_GENERATION_EUI64:
+                assert(in6_addr_is_null(&buffer.in6));
+                break;
+
         case ADDRESS_GENERATION_STATIC:
                 /* Only last 64 bits are used. */
                 memzero(buffer.in6.s6_addr, 8);
