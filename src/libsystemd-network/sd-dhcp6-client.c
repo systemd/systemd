95c053b9b59eb0e6d152a410356370d3b1c7d708
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-09-24 15:00:43 +0900

    sd-dhcp6-client: do not merge NTP and SNTP options
    Previously, SNTP option is ignored when it appears after NTP option(s),
    but merged later NTP options when it appears first.
    This makes split the NTP and SNTP addresses, and use SNTP addresses only
    when no NTP option is provided.

--- src/libsystemd-network/sd-dhcp6-client.c
+++ src/libsystemd-network/sd-dhcp6-client.c
@@ -1264,7 +1264,7 @@ static int client_parse_message(
                         break;
 
                 case SD_DHCP6_OPTION_SNTP_SERVERS:
-                        r = dhcp6_lease_set_sntp(lease, optval, optlen);
+                        r = dhcp6_lease_add_sntp(lease, optval, optlen);
                         if (r < 0)
                                 return r;
 
