681cb84a63c14410449469c580ea3cc54f590ca4
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2021-09-28 10:12:36 +0200

    meson: allow extra net naming schemes to be defined during configuration
    In upstream, we have a linearly-growing list of net-naming-scheme defines;
    we add a new one for every release where we make user-visible changes to the
    naming scheme.
    
    But the general idea was that downstream distributions could define their
    own combinations (or even just their own names for existing combinations),
    so provide stability for their users. So far this required patching of the
    netif-naming-scheme.c and .h files to add the new lines.
    
    With this patch, patching is not required:
    
    $ meson configure build \
      -Dextra-net-naming-schemes=gargoyle=v238+npar_ari+allow_rerenames,gargoyle2=gargoyle+nspawn_long_hash \
      -Ddefault-net-naming-scheme=gargoyle2
    
    or even
    
    $ meson configure build \
      -Dextra-net-naming-schemes=gargoyle=v238+npar_ari+allow_rerenames,gargoyle2=gargoyle+nspawn_long_hash,latest=v249 \
      -Ddefault-net-naming-scheme=gargoyle2
    
    The syntax is a comma-separated list of NAME=name+name+…
    This syntax is a bit scary, but any typos result in compilation errors,
    so I think it should be OK in practice.
    
    With this approach, we don't allow users to define arbitrary combinations:
    what is allowed is still defined at compilation time, so it's up to the
    distribution maintainers to provide reasonable combinations. In this regard,
    the only difference from status quo is that it's much easier to do (and harder
    to do incorrectly, for example by forgetting to add a name to one of the
    maps).

--- src/shared/netif-naming-scheme.c
+++ src/shared/netif-naming-scheme.c
@@ -22,15 +22,21 @@ static const NamingScheme naming_schemes[] = {
         { "v247", NAMING_V247 },
         { "v249", NAMING_V249 },
         /* … add more schemes here, as the logic to name devices is updated … */
+
+        EXTRA_NET_NAMING_MAP
 };
 
 const NamingScheme* naming_scheme_from_name(const char *name) {
-        if (streq(name, "latest"))
-                return naming_schemes + ELEMENTSOF(naming_schemes) - 1;
+        /* "latest" may either be defined explicitly by the extra map, in which case we we will find it in
+         * the table like any other name. After iterating through the table, we check for "latest" again,
+         * which means that if not mapped explicitly, it maps to the last defined entry, whatever that is. */
 
         for (size_t i = 0; i < ELEMENTSOF(naming_schemes); i++)
                 if (streq(naming_schemes[i].name, name))
                         return naming_schemes + i;
+
+        if (streq(name, "latest"))
+                return naming_schemes + ELEMENTSOF(naming_schemes) - 1;
 
         return NULL;
 }
