8f4400d158f24df511089b38798593e4db4caa22
Luca Boccassi <luca.boccassi@microsoft.com>
2021-09-29 18:16:09 +0100

    tree-wide: fix SPDX short identifier for LGPL-2.1-or-later
    https://spdx.dev/ids/#:~:text=Allowing%20later%20versions%20of%20a%20license
    https://spdx.org/licenses/LGPL-2.1-or-later.html

--- src/core/restrict-ifaces.c
+++ src/core/restrict-ifaces.c
@@ -1,4 +1,4 @@
-/* SPDX-License-Identifier: LGPL-2.1+ */
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
 
 #include "fd-util.h"
 #include "restrict-ifaces.h"
--- src/login/logind-polkit.c
+++ src/login/logind-polkit.c
@@ -1,4 +1,4 @@
-/* SPDX-License-Identifier: LGPL-2.1+ */
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
 
 #include "bus-polkit.h"
 #include "logind-polkit.h"
