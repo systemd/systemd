e076de98167cf46d99974dc60b2e7cf4f9a4364a
Frantisek Sumsal <frantisek@sumsal.cz>
2021-09-29 12:46:11 +0200

    basic: introduce test_strv_split_and_extend()

--- src/basic/strv.c
+++ src/basic/strv.c
@@ -300,6 +300,27 @@ int strv_split_full(char ***t, const char *s, const char *separators, ExtractFla
         return (int) n;
 }
 
+int strv_split_and_extend_full(char ***t, const char *s, const char *separators, ExtractFlags flags) {
+        _cleanup_strv_free_ char **l = NULL;
+        char * const *x = NULL;
+        int r;
+
+        assert(t);
+        assert(s);
+
+        r = strv_split_full(&l, s, separators, flags);
+        if (r < 0)
+                return r;
+
+        STRV_FOREACH(x, l) {
+                r = strv_extend(t, *x);
+                if (r < 0)
+                        return r;
+        }
+
+        return (int) strv_length(*t);
+}
+
 int strv_split_colon_pairs(char ***t, const char *s) {
         _cleanup_strv_free_ char **l = NULL;
         size_t n = 0;
