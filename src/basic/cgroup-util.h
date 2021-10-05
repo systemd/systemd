b1994387d3cb50b212fc4815941a8ff40d60cd85
Iago Lopez Galeiras <iagol@microsoft.com>
2021-10-05 13:18:49 +0200

    core: use LSM BPF functions to implement RestrictFileSystems=
    It attaches the LSM BPF program when the system manager starts up.
    
    It populates the hash of maps BPF map when services that have
    RestrictFileSystems= set start.
    
    It cleans up the hash of maps when the unit cgroup is pruned.
    
    To pass the file descriptor of the BPF map we add it to the keep_fds
    array.

--- src/basic/cgroup-util.h
+++ src/basic/cgroup-util.h
@@ -33,6 +33,9 @@ typedef enum CGroupController {
         CGROUP_CONTROLLER_BPF_FOREIGN,
         CGROUP_CONTROLLER_BPF_SOCKET_BIND,
         CGROUP_CONTROLLER_BPF_RESTRICT_NETWORK_INTERFACES,
+        /* The BPF hook implementing RestrictFileSystems= is not defined here.
+         * It's applied as late as possible in exec_child() so we don't block
+         * our own unit setup code. */
 
         _CGROUP_CONTROLLER_MAX,
         _CGROUP_CONTROLLER_INVALID = -EINVAL,
