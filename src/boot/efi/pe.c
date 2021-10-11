dc4679283153463f81ba3f96f80960bb2c476378
Max Resch <resch.max@gmail.com>
2021-10-11 15:15:30 +0200

    [sd-stub] Add support for aarch64 booting via pe-entry point

--- src/boot/efi/pe.c
+++ src/boot/efi/pe.c
@@ -56,10 +56,46 @@ struct CoffFileHeader {
         UINT16  Characteristics;
 } _packed_;
 
+#define OPTHDR32_MAGIC 0x10B /* PE32  OptionalHeader */
+#define OPTHDR64_MAGIC 0x20B /* PE32+ OptionalHeader */
+
+struct PeOptionalHeader {
+        UINT16  Magic;
+        UINT8   LinkerMajor;
+        UINT8   LinkerMinor;
+        UINT32  SizeOfCode;
+        UINT32  SizeOfInitializedData;
+        UINT32  SizeOfUninitializeData;
+        UINT32  AddressOfEntryPoint;
+        UINT32  BaseOfCode;
+        union {
+                struct { /* PE32 */
+                        UINT32 BaseOfData;
+                        UINT32 ImageBase32;
+                };
+                UINT64 ImageBase64; /* PE32+ */
+        };
+        UINT32 SectionAlignment;
+        UINT32 FileAlignment;
+        UINT16 MajorOperatingSystemVersion;
+        UINT16 MinorOperatingSystemVersion;
+        UINT16 MajorImageVersion;
+        UINT16 MinorImageVersion;
+        UINT16 MajorSubsystemVersion;
+        UINT16 MinorSubsystemVersion;
+        UINT32 Win32VersionValue;
+        UINT32 SizeOfImage;
+        UINT32 SizeOfHeaders;
+        UINT32 CheckSum;
+        UINT16 Subsystem;
+        UINT16 DllCharacteristics;
+        /* fields with different sizes for 32/64 omitted */
+} _packed_;
+
 struct PeFileHeader {
         UINT8   Magic[4];
         struct CoffFileHeader FileHeader;
-        /* OptionalHeader omitted */
+        struct PeOptionalHeader OptionalHeader;
 } _packed_;
 
 struct PeSectionHeader {
@@ -91,7 +127,7 @@ static inline BOOLEAN verify_pe(const struct PeFileHeader *pe) {
 static inline UINTN section_table_offset(const struct DosFileHeader *dos, const struct PeFileHeader *pe) {
         assert(dos);
         assert(pe);
-        return dos->ExeHeader + sizeof(struct PeFileHeader) + pe->FileHeader.SizeOfOptionalHeader;
+        return dos->ExeHeader + OFFSETOF(struct PeFileHeader, OptionalHeader) + pe->FileHeader.SizeOfOptionalHeader;
 }
 
 static VOID locate_sections(
@@ -120,6 +156,41 @@ static VOID locate_sections(
                         sizes[j] = sect->VirtualSize;
                 }
         }
+}
+
+EFI_STATUS pe_alignment_info(
+                const VOID *base,
+                UINT32 *ret_entry_point_address,
+                UINT32 *ret_size_of_image,
+                UINT32 *ret_section_alignment) {
+
+        const struct DosFileHeader *dos;
+        const struct PeFileHeader *pe;
+
+        assert(base);
+        assert(ret_entry_point_address);
+        assert(ret_size_of_image);
+        assert(ret_section_alignment);
+
+        dos = (const struct DosFileHeader *) base;
+        if (!verify_dos(dos))
+                return EFI_LOAD_ERROR;
+
+        pe = (const struct PeFileHeader*) ((const UINT8 *)base + dos->ExeHeader);
+        if (!verify_pe(pe))
+                return EFI_LOAD_ERROR;
+
+        *ret_entry_point_address = pe->OptionalHeader.AddressOfEntryPoint;
+
+        if (pe->OptionalHeader.Magic == OPTHDR32_MAGIC) {
+                *ret_size_of_image = pe->OptionalHeader.SizeOfImage;
+                *ret_section_alignment = pe->OptionalHeader.SectionAlignment;
+        } else if (pe->OptionalHeader.Magic == OPTHDR64_MAGIC) {
+                *ret_size_of_image = pe->OptionalHeader.SizeOfImage;
+                *ret_section_alignment = pe->OptionalHeader.SectionAlignment;
+        } else
+                return EFI_UNSUPPORTED;
+        return EFI_SUCCESS;
 }
 
 EFI_STATUS pe_memory_locate_sections(
