4fa9d8f14523982482386d398d2b2669902f2098
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-10-18 14:11:53 +0900

    Revert "core: Check unit start rate limiting earlier"
    This reverts commit ed8fbbf1745c6a2dc0b8cd560ac8a3353f72e979.
    
    This was causing problems during boot, see
    https://bodhi.fedoraproject.org/updates/FEDORA-2021-a1a52487e6,
    https://bugzilla.redhat.com/show_bug.cgi?id=2013386.
    https://github.com/systemd/systemd/issues/21025

--- test/TEST-63-ISSUE-17433/test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-TEST_DESCRIPTION="https://github.com/systemd/systemd/issues/17433"
-
-# shellcheck source=test/test-functions
-. "${TEST_BASE_DIR:?}/test-functions"
-
-do_test "$@"
