907e56d4c733826b88298253841c08ba6dc5469d
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-10-19 01:49:53 +0900

    tree-wide: use AF_NETLINK instead of PF_NETLINK

--- src/libsystemd/sd-netlink/netlink-socket.c
+++ src/libsystemd/sd-netlink/netlink-socket.c
@@ -20,7 +20,7 @@
 int socket_open(int family) {
         int fd;
 
-        fd = socket(PF_NETLINK, SOCK_RAW|SOCK_CLOEXEC|SOCK_NONBLOCK, family);
+        fd = socket(AF_NETLINK, SOCK_RAW|SOCK_CLOEXEC|SOCK_NONBLOCK, family);
         if (fd < 0)
                 return -errno;
 
