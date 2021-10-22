a4e70ef7ba3b5e2c050cce5ed2bbf58807505596
Lennart Poettering <lennart@poettering.net>
2021-10-23 00:28:24 +0200

    dirent-util: add FOREACH macro for iterating through getdents64() buffers
    We already have a similar loop twice, let's make it easier to read via
    an iteration macro.
    
    (The new macro is a bit more careful even, as it verifies the full
    dirent fits into the remaining buffer when returning it)

--- src/basic/dirent-util.h
+++ src/basic/dirent-util.h
@@ -51,3 +51,8 @@ assert_cc(offsetof(struct dirent, d_type) == offsetof(struct dirent64, d_type));
 assert_cc(sizeof_field(struct dirent, d_type) == sizeof_field(struct dirent64, d_type));
 assert_cc(offsetof(struct dirent, d_name) == offsetof(struct dirent64, d_name));
 assert_cc(sizeof_field(struct dirent, d_name) == sizeof_field(struct dirent64, d_name));
+
+#define FOREACH_DIRENT_IN_BUFFER(de, buf, sz)                           \
+        for (void *_end = (uint8_t*) ({ (de) = (buf); }) + (sz);        \
+             (uint8_t*) (de) < (uint8_t*) _end;                         \
+             (de) = (struct dirent*) ((uint8_t*) (de) + (de)->d_reclen))
