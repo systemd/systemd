6aded2df8631329a82943c980117f4ee277d07cd
Daan De Meyer <daan.j.demeyer@gmail.com>
2021-10-23 22:36:47 +0100

    journal: Use 32-bit entry array offsets in compact mode
    Before:
    
    ```
    OBJECT TYPE      ENTRIES SIZE
    Unused           0       0B
    Data             1003715 142.3M
    Field            235     11.6K
    Entry            1001522 366.6M
    Data Hash Table  7       21.3M
    Field Hash Table 7       36.5K
    Entry Array      2265    234.5M
    Tag              0       0B
    ```
    
    After:
    
    ```
    OBJECT TYPE      ENTRIES SIZE
    Unused           0       0B
    Data             1003719 142.3M
    Field            235     11.6K
    Entry            1001526 366.6M
    Data Hash Table  7       21.3M
    Field Hash Table 7       36.5K
    Entry Array      2268    117.3M
    Tag              0       0B
    ```

--- src/libsystemd/sd-journal/journal-file.h
+++ src/libsystemd/sd-journal/journal-file.h
@@ -208,7 +208,8 @@ static inline bool VALID_EPOCH(uint64_t u) {
 int journal_file_move_to_object(JournalFile *f, ObjectType type, uint64_t offset, Object **ret);
 
 uint64_t journal_file_entry_n_items(Object *o) _pure_;
-uint64_t journal_file_entry_array_n_items(Object *o) _pure_;
+uint64_t journal_file_entry_array_n_items(JournalFile *f, Object *o) _pure_;
+uint64_t journal_file_entry_array_item(JournalFile *f, Object *o, size_t i) _pure_;
 uint64_t journal_file_hash_table_n_items(Object *o) _pure_;
 
 int journal_file_append_object(JournalFile *f, ObjectType type, uint64_t size, Object **ret, uint64_t *offset);
