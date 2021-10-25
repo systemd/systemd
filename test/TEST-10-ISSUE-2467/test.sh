8624d160c86530d1804d553255662aabeb6c83f1
Frantisek Sumsal <frantisek@sumsal.cz>
2021-10-25 11:02:22 +0200

    test: tweak TriggerLimitIntervalSec= when built with coverage
    Collecting coverage causes a significant slowdown in general, but since
    this test requires certain timing, we need to tweak the defaults to make
    it reliably pass.

--- test/TEST-10-ISSUE-2467/test.sh
+++ test/TEST-10-ISSUE-2467/test.sh
@@ -7,4 +7,16 @@ TEST_DESCRIPTION="https://github.com/systemd/systemd/issues/2467"
 # shellcheck source=test/test-functions
 . "${TEST_BASE_DIR:?}/test-functions"
 
+test_append_files() {
+    (
+        # Collecting coverage slows this particular test quite a bit, causing
+        # it to fail with the default settings (20 triggers per 2 secs)
+        # to trip over the default limit. Let's help it a bit in such case.
+        if get_bool "$IS_BUILT_WITH_COVERAGE"; then
+            mkdir -p "${initdir:?}/etc/systemd/system/test10.socket.d"
+            printf "[Socket]\nTriggerLimitIntervalSec=10\n" >"${initdir:?}/etc/systemd/system/test10.socket.d/coverage-override.conf"
+        fi
+    )
+}
+
 do_test "$@"
