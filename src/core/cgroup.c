8d3e4ac7cd37200d1431411a4b98925a24b7d9b3
Lennart Poettering <lennart@poettering.net>
2021-10-27 23:17:50 +0200

    scope: refuse activation of scopes if no PIDs to add are left
    If all processes we are supposed to add are gone by the time we are
    ready to do so, let's fail.
    
    THis is heavily based on Cunlong Li's work, who thankfully tracked this
    down.
    
    Replaces: #20577

--- src/core/cgroup.c
+++ src/core/cgroup.c
@@ -2291,7 +2291,8 @@ int unit_attach_pids_to_cgroup(Unit *u, Set *pids, const char *suffix_path) {
                                 ret = r; /* Remember first error */
 
                         continue;
-                }
+                } else if (ret >= 0)
+                        ret++; /* Count successful additions */
 
                 r = cg_all_unified();
                 if (r < 0)
