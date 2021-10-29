508d26db6d722f6cd40c1375c6153f35e040fa6d
Lennart Poettering <lennart@poettering.net>
2021-10-29 09:44:31 +0200

    homework: rework how we disassemble a home dir in home_deactivate()
    Let's first move the home dir to a new mount point that is only visible
    in our own private namespace. Then, do FITRIM and stuff there, so that
    we know the regular userspace can't interfere with that, and we know
    that the home fs is not used anymore.
    
    (This will become even more important once we add auto-grow/auto-shrink
    for home dirs)

--- src/home/homework-luks.h
+++ src/home/homework-luks.h
@@ -9,7 +9,7 @@ int home_setup_luks(UserRecord *h, HomeSetupFlags flags, const char *force_image
 
 int home_activate_luks(UserRecord *h, HomeSetup *setup, PasswordCache *cache, UserRecord **ret_home);
 int home_deactivate_luks(UserRecord *h, HomeSetup *setup);
-int home_trim_luks(UserRecord *h);
+int home_trim_luks(UserRecord *h, HomeSetup *setup);
 
 int home_store_header_identity_luks(UserRecord *h, HomeSetup *setup, UserRecord *old_home);
 
@@ -39,7 +39,6 @@ static inline uint64_t luks_volume_key_size_convert(struct crypt_device *cd) {
 }
 
 int run_fitrim(int root_fd);
-int run_fitrim_by_path(const char *root_path);
 int run_fallocate(int backing_fd, const struct stat *st);
 int run_fallocate_by_path(const char *backing_path);
 int run_mark_dirty(int fd, bool b);
