8d0a57e0f90ce1f2f8373cf28455f48b0831c120
Jan Janssen <medhefgo@web.de>
2021-11-01 10:36:12 +0100

    sd-boot: Add some link flags
    These flags are used by gnu-efi. Adding --no-undefined even allows
    us to drop the no-undefined-symbols.sh test in favor of immediate
    compile time errors.

--- src/boot/efi/no-undefined-symbols.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# SPDX-License-Identifier: LGPL-2.1-or-later
-set -eu
-set -o pipefail
-
-if nm -D -u "${1:?}" | grep ' U '; then
-    echo "Undefined symbols detected!"
-    exit 1
-fi
