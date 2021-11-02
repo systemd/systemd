67ab888965e06428a4bec4c0a00e3dfc237ea554
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-11-03 04:50:00 +0900

    network: tc/cake: introduce CompensationMode= setting

--- src/network/tc/cake.h
+++ src/network/tc/cake.h
@@ -2,8 +2,18 @@
  * Copyright © 2020 VMware, Inc. */
 #pragma once
 
+#include <linux/pkt_sched.h>
+
 #include "conf-parser.h"
 #include "qdisc.h"
+
+typedef enum CakeCompensationMode {
+        CAKE_COMPENSATION_MODE_NONE = CAKE_ATM_NONE,
+        CAKE_COMPENSATION_MODE_ATM  = CAKE_ATM_ATM,
+        CAKE_COMPENSATION_MODE_PTM  = CAKE_ATM_PTM,
+        _CAKE_COMPENSATION_MODE_MAX,
+        _CAKE_COMPENSATION_MODE_INVALID = -EINVAL,
+} CakeCompensationMode;
 
 typedef struct CommonApplicationsKeptEnhanced {
         QDisc meta;
@@ -15,6 +25,7 @@ typedef struct CommonApplicationsKeptEnhanced {
         /* Overhead compensation parameters */
         bool overhead_set;
         int overhead;
+        CakeCompensationMode compensation_mode;
 
 } CommonApplicationsKeptEnhanced;
 
@@ -24,3 +35,4 @@ extern const QDiscVTable cake_vtable;
 CONFIG_PARSER_PROTOTYPE(config_parse_cake_bandwidth);
 CONFIG_PARSER_PROTOTYPE(config_parse_cake_overhead);
 CONFIG_PARSER_PROTOTYPE(config_parse_cake_tristate);
+CONFIG_PARSER_PROTOTYPE(config_parse_cake_compensation_mode);
