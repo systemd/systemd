f9cdaba52c96fadca009c2bec8e73682d5aa5900
Lennart Poettering <lennart@poettering.net>
2021-11-02 18:24:02 +0100

    homework: upload home password into kernel keyring if needed
    If we do automatic disk space rebalancing, we must be able to unlock the
    encrypted volume for that in the background, thus we need to decryption
    key around in userspace. Let's do this via the kernel keyring. This
    allows us to do this in a relatively secure way, so that it sticks
    around between homework invocations, but still is destroyed
    automatically when homed goes down.

--- src/home/homed-manager.c
+++ src/home/homed-manager.c
@@ -1947,7 +1947,7 @@ static int manager_rebalance_apply(Manager *m) {
 
                 h->rebalance_pending = false;
 
-                r = home_resize(h, h->rebalance_goal, NULL, &error);
+                r = home_resize(h, h->rebalance_goal, /* secret= */ NULL, /* automatic= */ true, &error);
                 if (r < 0)
                         log_warning_errno(r, "Failed to resize home '%s' for rebalancing, ignoring: %s",
                                           h->user_name, bus_error_message(&error, r));
