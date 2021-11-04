0cadadb5d79f8f6d13658cb8f1c0e369e36c986c
Tony Asleson <tasleson@redhat.com>
2021-11-04 14:19:56 -0500

    test: exercise sytemd-integritysetup & generator
    Ensures we can open a dm-integrity volume formated with
    integritysetup.

---
+++ test/TEST-67-INTEGRITY/test.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: LGPL-2.1-or-later
+set -e
+
+TEST_DESCRIPTION="dm-integrity test"
+
+TEST_NO_NSPAWN=1
+QEMU_TIMEOUT=300
+
+# shellcheck source=test/test-functions
+. "${TEST_BASE_DIR:?}/test-functions"
+
+test_append_files() {(
+
+    instmods loop =block
+    instmods dm_integrity =md
+
+    inst_binary losetup
+    inst_binary integritysetup
+    inst_binary blkid
+    install_dmevent
+
+    generate_module_dependencies
+
+)}
+
+do_test "$@"
