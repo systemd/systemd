efe6a921df09e620de556353651baa32d6385ecd
Xiaotian Wu <wuxiaotian@loongson.cn>
2021-11-06 16:52:37 +0800

    architecture: Add support for the LoongArch architecture.

--- src/basic/architecture.c
+++ src/basic/architecture.c
@@ -118,6 +118,8 @@ int uname_architecture(void) {
 #elif defined(__arc__)
                 { "arc",        ARCHITECTURE_ARC      },
                 { "arceb",      ARCHITECTURE_ARC_BE   },
+#elif defined(__loongarch64)
+                { "loongarch64", ARCHITECTURE_LOONGARCH64 },
 #else
 #error "Please register your architecture here!"
 #endif
@@ -173,6 +175,7 @@ static const char *const architecture_table[_ARCHITECTURE_MAX] = {
         [ARCHITECTURE_RISCV64] = "riscv64",
         [ARCHITECTURE_ARC] = "arc",
         [ARCHITECTURE_ARC_BE] = "arc-be",
+        [ARCHITECTURE_LOONGARCH64] = "loongarch64",
 };
 
 DEFINE_STRING_TABLE_LOOKUP(architecture, int);
