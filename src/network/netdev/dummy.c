38192e7db4088d572f4dcb8fa6a904d58fccbf44
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-11-07 16:27:33 +0900

    network/netdev: introduce .iftype to netdev vtable

--- src/network/netdev/dummy.c
+++ src/network/netdev/dummy.c
@@ -1,10 +1,13 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 
+#include <linux/if_arp.h>
+
 #include "dummy.h"
 
 const NetDevVTable dummy_vtable = {
         .object_size = sizeof(Dummy),
         .sections = NETDEV_COMMON_SECTIONS,
         .create_type = NETDEV_CREATE_INDEPENDENT,
+        .iftype = ARPHRD_ETHER,
         .generate_mac = true,
 };
