0c74ee6c36b7d408fbf4b726d4790e073b364888
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-11-07 15:23:38 +0900

    arphrd-util: introduce arphrd_to_hw_addr_len()

--- src/basic/arphrd-util.c
+++ src/basic/arphrd-util.c
@@ -1,7 +1,9 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 
 #include <errno.h>
+#include <netinet/in.h>
 #include <linux/if_arp.h>
+#include <linux/if_infiniband.h>
 #include <string.h>
 
 #include "arphrd-util.h"
@@ -22,4 +24,22 @@ int arphrd_from_name(const char *name) {
                 return -EINVAL;
 
         return sc->id;
+}
+
+size_t arphrd_to_hw_addr_len(uint16_t arphrd) {
+        switch (arphrd) {
+        case ARPHRD_ETHER:
+                return ETH_ALEN;
+        case ARPHRD_INFINIBAND:
+                return INFINIBAND_ALEN;
+        case ARPHRD_TUNNEL:
+        case ARPHRD_SIT:
+        case ARPHRD_IPGRE:
+                return sizeof(struct in_addr);
+        case ARPHRD_TUNNEL6:
+        case ARPHRD_IP6GRE:
+                return sizeof(struct in6_addr);
+        default:
+                return 0;
+        }
 }
