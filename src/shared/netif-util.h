f45e66b33fbc993df4544d04e14d892ebcdf1f3d
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-11-08 11:01:46 +0900

    netif-util: split net_get_unique_predictable_data() into two

--- src/shared/netif-util.h
+++ src/shared/netif-util.h
@@ -5,7 +5,9 @@
 #include <stdbool.h>
 
 #include "sd-device.h"
+#include "sd-id128.h"
 
 int net_get_type_string(sd_device *device, uint16_t iftype, char **ret);
 const char *net_get_persistent_name(sd_device *device);
 int net_get_unique_predictable_data(sd_device *device, bool use_sysname, uint64_t *ret);
+int net_get_unique_predictable_data_from_name(const char *name, const sd_id128_t *key, uint64_t *ret);
