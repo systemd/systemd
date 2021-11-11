9b6e44076bc027c868617681e66abc33d982491d
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-11-11 13:48:53 +0900

    network: json: make network_build_json() and device_build_json() accept NULL

--- src/network/networkd-network-json.c
+++ src/network/networkd-network-json.c
@@ -4,8 +4,12 @@
 #include "networkd-network.h"
 
 int network_build_json(Network *network, JsonVariant **ret) {
-        assert(network);
         assert(ret);
+
+        if (!network) {
+                *ret = NULL;
+                return 0;
+        }
 
         return json_build(ret, JSON_BUILD_OBJECT(
                                 JSON_BUILD_PAIR("NetworkFile", JSON_BUILD_STRING(network->filename))));
