f22abf384d1d2a6d1a303502a57366df7dd4b08e
Lennart Poettering <lennart@poettering.net>
2021-11-11 15:31:17 +0100

    types-fundamental: introduce sd_true + sd_false
    I think we should stick to the rule that stuff defined in
    types-fundamental.h either:
    
    1. adds a prefixed concept "sd_xyz" that maps differently in the two
       environments
    
    2. adds a non-prefixed concept "xyz" that adds a type otherwise missing
       in one of the two environments but with the same definition as in the
       other.
    
    i.e. if have have some concept that might differ the way its set up in
    the two environments it really should be prefixed by "sd_" to make clear
    it has semantics we defined. Only drop the prefix if it really means the
    exact same thin in all environments.
    
    Now, sd_bool is defined prefixed, because its either mapped to "BOOLEAN"
    (which is an integer) in UEFI or "bool" (which is C99 _Bool) in
    userspace. size_t is not defined prefixed, because it's mapped to the
    same thing ultimately (on the UEFI its mapped to UINTN, but that in turn
    is defined as being the type for the size of memory objects, thus it's
    really the same as userspace size_t).
    
    So far "true" and "false" where defined unprefixed even though they map
    to values of different types. typeof(true) in userspace would reveal
    _Bool, but typeof(false) in UEFI would reveal BOOLEAN. The distinction
    actually does matter in comparisons (i.e. (_Bool) 1 == (_Bool) 2 holds
    while (BOOLEAN) 1 == (BOOLEAN) 2 does not hold).
    
    Hence, let's add sd_true and sd_false, thus indicating we defined our
    own concept here, and it has similar but different semantics in UEFI and
    in userspace.

--- src/fundamental/types-fundamental.h
+++ src/fundamental/types-fundamental.h
@@ -1,6 +1,20 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 #pragma once
 
+/* This defines a number of basic types that are one thing in userspace and another in the UEFI environment,
+ * but mostly the same in concept and behaviour.
+ *
+ * Note: if the definition of these types/values has slightly different semantics in userspace and in the
+ * UEFI environment then please prefix its name with "sd_" to make clear these types have special semantics,
+ * and *we* defined them. Otherwise, if the types are effectively 100% identical in behaviour in userspace
+ * and UEFI environment you can omit the prefix. (Examples: sd_char is 8 bit in userspace and 16 bit in UEFI
+ * space hence it should have the sd_ prefix; but size_t in userspace and UINTN in UEFI environment are 100%
+ * defined the same way ultimately, hence it's OK to just define size_t as alias to UINTN in UEFI
+ * environment, so that size_t can be used everywhere, without any "sd_" prefix.)
+ *
+ * Note: we generally prefer the userspace names of types and concepts. i.e. if in doubt please name types
+ * after the userspace vocabulary, and let's keep UEFI vocabulary specific to the UEFI build environment. */
+
 #ifdef SD_BOOT
 #include <efi.h>
 
@@ -9,8 +23,8 @@ typedef CHAR16  sd_char;
 typedef INTN    sd_int;
 typedef UINTN   size_t;
 
-#define true    TRUE
-#define false   FALSE
+#define sd_true  TRUE
+#define sd_false FALSE
 #else
 #include <stdbool.h>
 #include <stdint.h>
@@ -18,4 +32,8 @@ typedef UINTN   size_t;
 typedef bool    sd_bool;
 typedef char    sd_char;
 typedef int     sd_int;
+
+#define sd_true  true
+#define sd_false false
+
 #endif
