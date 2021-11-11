systemd/systemd

commit 8d3e4ac7cd37200d1431411a4b98925a24b7d9b3

scope: refuse activation of scopes if no PIDs to add are left
If all processes we are supposed to add are gone by the time we are
ready to do so, let's fail.

THis is heavily based on Cunlong Li's work, who thankfully tracked this
down.

Replaces: #20577

src/core/cgroup.c
@@ -2291,7 +2291,8 @@ int unit_attach_pids_to_cgroup(Unit *u, Set *pids, const char *suffix_path) {
                                 ret = r; /* Remember first error */
 
                         continue;
-                }
+                } else if (ret >= 0)
+                        ret++; /* Count successful additions */
 
                 r = cg_all_unified();
                 if (r < 0)

src/core/scope.c
@@ -391,6 +391,12 @@ static int scope_start(Unit *u) {
                 scope_enter_dead(s, SCOPE_FAILURE_RESOURCES);
                 return r;
         }
+        if (r == 0) {
+                log_unit_warning(u, "No PIDs left to attach to the scope's control group, refusing: %m");
+                scope_enter_dead(s, SCOPE_FAILURE_RESOURCES);
+                return -ECHILD;
+        }
+        log_unit_debug(u, "%i %s added to scope's control group.", r, r == 1 ? "process" : "processes");
 
         s->result = SCOPE_SUCCESS;
 

commit fd78de361a682e989136b27d3ea8d336284b9733

scope: fix activation from a user instance
Commit 8d3e4ac7cd3 introduce a feature to count how many PIDs were
added to a scope, and fails to activate the scope if the number is 0
(i.e. there are no PIDs).

Unfortunately the counting code has a bug -- those PIDs that are
activated via a fallback (see if (again) in the code) are not counted.
As a result, rootless runc fails to work with

        No PIDs left to attach to the scope's control group, refusing: Success

This commit fixes the issue. Tested on my Fedora 35 machine.

Fixes: 8d3e4ac7cd37200d1431411a4b98925a24b7d9b3
Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

src/core/cgroup.c
@@ -2283,8 +2283,12 @@ int unit_attach_pids_to_cgroup(Unit *u, Set *pids, const char *suffix_path) {
                                 z = unit_attach_pid_to_cgroup_via_bus(u, pid, suffix_path);
                                 if (z < 0)
                                         log_unit_info_errno(u, z, "Couldn't move process "PID_FMT" to requested cgroup '%s' (directly or via the system bus): %m", pid, empty_to_root(p));
-                                else
+                                else {
+                                        if (ret >= 0)
+                                                ret++; /* Count successful additions */
+
                                         continue; /* When the bus thing worked via the bus we are fully done for this PID. */
+                                }
                         }
 
                         if (ret >= 0)