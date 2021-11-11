1cc8db6985aa7f46f385ca3ef0019659d1817fb3
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-11-12 08:56:34 +0900

    network: include information about routing policy rules into json

--- src/network/networkd-manager-json.c
+++ src/network/networkd-manager-json.c
@@ -5,6 +5,7 @@
 #include "networkd-manager.h"
 #include "networkd-nexthop-json.h"
 #include "networkd-route-json.h"
+#include "networkd-routing-policy-rule-json.h"
 
 int manager_build_json(Manager *manager, JsonVariant **ret) {
         _cleanup_(json_variant_unrefp) JsonVariant *v = NULL, *w = NULL;
@@ -28,6 +29,16 @@ int manager_build_json(Manager *manager, JsonVariant **ret) {
         w = json_variant_unref(w);
 
         r = nexthops_build_json(manager->nexthops, &w);
+        if (r < 0)
+                return r;
+
+        r = json_variant_merge(&v, w);
+        if (r < 0)
+                return r;
+
+        w = json_variant_unref(w);
+
+        r = routing_policy_rules_build_json(manager->rules, &w);
         if (r < 0)
                 return r;
 
