ae192c84468d691f25ac50050d6af666badc9fe7
Evgeny Vereshchagin <evvers@ya.ru>
2021-11-13 15:02:26 +0000

    trigger a memory leak

--- src/fuzz/fuzz-env-file.c
+++ src/fuzz/fuzz-env-file.c
@@ -11,7 +11,7 @@
 
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
         _cleanup_fclose_ FILE *f = NULL;
-        _cleanup_strv_free_ char **rl = NULL, **rlp =  NULL;
+        char **rl = NULL, **rlp =  NULL;
 
         if (size == 0 || size > 65535)
                 return 0;
