ec7c3de8ec8b88a492a0a34c41ae5e4067269bf9
Vincent Dagonneau <vincentdagonneau@gmail.com>
2021-11-15 09:01:32 -0500

    sd-boot: Add support for opt-in auto-enrollement of secure boot keys
    This patch adds support for enrolling secure boot boot keys from sd-boot.
    
    If secure boot keys are found under $ESP/loader/keys then sd-boot will generate
    enrollement entries named after the directories they are in.
    
    Additionally, if the auto-enroll option is set then the keys located in the directory
    named 'auto' are going to be enrolled without user input.
    
    After keys are enrolled, the system reboots with secure boot enabled therefore, it is
    ***critical*** to ensure that everything needed for the system to boot is signed
    properly (sd-boot itself, kernel, initramfs, PCI option ROMs).
    
    This feature currently only allows loading the most simple set of variables: PK, KEK
    and db.
    
    The files need to be prepared with cert-to-efi-sig-list and then signed with
    sign-efi-sig-list.
    
    Here is a short example to generate your own keys and the right files for
    auto-enrollement.
    
    `
    keys="PK KEK DB"
    for key in ${keys}; do
    	openssl req -new -x509 -subj "/CN=${key}/ -keyout "${key}.key" -out "${key}.crt"
    	openssl x509 -outform DER -in "${key}.crt" -out "${key}.cer"
    	cert-to-efi-sig-list -g "${uuid}" "${key}.crt" "${key}.esl.nosign"
    done
    
    sign-efi-sig-list -c PK.crt -k PK.key PK PK.esl.nosign PK.esl
    sign-efi-sig-list -c PK.crt -k PK.key KEK KEK.esl.nosign KEK.esl
    sign-efi-sig-list -c KEK.crt -k KEK.key db db.esl.nosign db.esl
    `
    
    Once these keys are enrolled, all the files needed for boot ***NEED*** to be signed in
    order to run. You can sign the binaries with the sbsign tool, for example:
    
    `
    sbsign --key db.key --cert db.crt bzImage --output $ESP/bzImage
    `
    
    Example:
    
    Assuming the system has been put in Setup Mode:
    
    `
    $ESP/loader/keys/auto/db.esl
    $ESP/loader/keys/auto/KEK.esl
    $ESP/loader/keys/auto/PK.esl
    $ESP/loader/keys/Linux Only/db.esl
    $ESP/loader/keys/Linux Only/KEK.esl
    $ESP/loader/keys/Linux Only/PK.esl
    $ESP/loader/keys/Linux and Windows/db.esl
    $ESP/loader/keys/Linux and Windows/KEK.esl
    $ESP/loader/keys/Linux and Windows/PK.esl
    `
    
    If auto-enroll is set, then the db, KEK and then PK are enrolled from the 'auto'
    directory.
    
    If not, three new boot entries are available to the user in order to enroll either the
    'Linux Only', 'Linux And Windows' or 'auto' set of keys.
    
    :wq

--- src/boot/efi/secure-boot.h
+++ src/boot/efi/secure-boot.h
@@ -4,5 +4,11 @@
 #include <efi.h>
 #include "efivars-fundamental.h"
 
+#define EFI_IMAGE_SECURITY_DATABASE_VARIABLE \
+        { 0xd719b2cb, 0x3d3a, 0x4596, {0xa3, 0xbc, 0xda, 0xd0,  0xe, 0x67, 0x65, 0x6f }}
+
+#define EFI_IMAGE_SECURITY_DATABASE_GUID \
+        &(const EFI_GUID) EFI_IMAGE_SECURITY_DATABASE_VARIABLE
+
 BOOLEAN secure_boot_enabled(void);
 SecureBootMode secure_boot_mode(void);
