bb684a2b1eacc922c7a1d5c05cd71d73902df8e0
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2021-11-19 17:02:09 +0100

    shared/base-filesystem: use LIB_ARCH_TUPLE instead of string

--- src/shared/base-filesystem.c
+++ src/shared/base-filesystem.c
@@ -9,6 +9,7 @@
 #include <unistd.h>
 
 #include "alloc-util.h"
+#include "architecture.h"
 #include "base-filesystem.h"
 #include "fd-util.h"
 #include "log.h"
@@ -49,7 +50,7 @@ static const BaseFilesystem table[] = {
         /* aarch64 ELF ABI actually says dynamic loader is in /lib/, but Fedora puts it in /lib64/ anyway and
          * just symlinks /lib/ld-linux-aarch64.so.1 to ../lib64/ld-linux-aarch64.so.1. For this to work
          * correctly, /lib64/ must be symlinked to /usr/lib64/. */
-        { "lib64",    0, "usr/lib/aarch64-linux-gnu\0"
+        { "lib64",    0, "usr/lib/"LIB_ARCH_TUPLE"\0"
                          "usr/lib64\0",                "ld-linux-aarch64.so.1" },
 #  define KNOW_LIB64_DIRS 1
 #elif defined(__alpha__)
@@ -58,7 +59,7 @@ static const BaseFilesystem table[] = {
         /* No /lib64 on arm. The linker is /lib/ld-linux-armhf.so.3. */
 #  define KNOW_LIB64_DIRS 1
 #elif defined(__i386__) || defined(__x86_64__)
-        { "lib64",    0, "usr/lib/x86_64-linux-gnu\0"
+        { "lib64",    0, "usr/lib/"LIB_ARCH_TUPLE"\0"
                          "usr/lib64\0",                "ld-linux-x86-64.so.2" },
 #  define KNOW_LIB64_DIRS 1
 #elif defined(__ia64__)
@@ -74,7 +75,7 @@ static const BaseFilesystem table[] = {
 #  endif
 #elif defined(__powerpc__)
 #  if defined(__PPC64__) && __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-        { "lib64",    0, "usr/lib/powerpc64le-linux-gnu\0"
+        { "lib64",    0, "usr/lib/"LIB_ARCH_TUPLE"\0"
                          "usr/lib64\0",                "ld64.so.2" },
 #    define KNOW_LIB64_DIRS 1
 #  elif defined(__powerpc64__) && __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
@@ -86,7 +87,7 @@ static const BaseFilesystem table[] = {
 #  if __riscv_xlen == 32
 #  elif __riscv_xlen == 64
         /* Same situation as for aarch64 */
-        { "lib64",    0, "usr/lib/riscv64-linux-gnu\0"
+        { "lib64",    0, "usr/lib/"LIB_ARCH_TUPLE"\0"
                          "usr/lib64\0",                "ld-linux-riscv64-lp64d.so.1" },
 #    define KNOW_LIB64_DIRS 1
 #  else
@@ -95,7 +96,7 @@ static const BaseFilesystem table[] = {
 #elif defined(__s390__)
         /* s390-linux-gnu */
 #elif defined(__s390x__)
-        { "lib64",    0, "usr/lib/s390x-linux-gnu\0"
+        { "lib64",    0, "usr/lib/"LIB_ARCH_TUPLE"\0"
                          "usr/lib64",                  "ld-lsb-s390x.so.3" },
 #    define KNOW_LIB64_DIRS 1
 #elif defined(__sparc__)
