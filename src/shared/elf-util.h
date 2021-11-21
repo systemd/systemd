ad2540c13522e6eb03d677f6198bb363642d4c14
Luca Boccassi <luca.boccassi@microsoft.com>
2021-11-21 17:31:20 +0000

    elf-util: add stub for builds without libdw

--- src/shared/elf-util.h
+++ src/shared/elf-util.h
@@ -3,6 +3,12 @@
 
 #include "json.h"
 
+#if HAVE_ELFUTILS
 /* Parse an ELF object in a forked process, so that errors while iterating over
  * untrusted and potentially malicious data do not propagate to the main caller's process. */
 int parse_elf_object(int fd, const char *executable, char **ret, JsonVariant **ret_package_metadata);
+#else
+static inline int parse_elf_object(int fd, const char *executable, char **ret, JsonVariant **ret_package_metadata) {
+        return log_error_errno(SYNTHETIC_ERRNO(EOPNOTSUPP), "elfutils disabled, parsing ELF objects not supported");
+}
+#endif
