43ae4f6f33ac2962e7dd5f5d9bba45de6c3b3af8
Luca Boccassi <luca.boccassi@microsoft.com>
2021-11-21 17:31:20 +0000

    elf-util: add stub for builds without libdw

--- src/coredump/coredump.c
+++ src/coredump/coredump.c
@@ -813,7 +813,6 @@ static int submit_coredump(
         if (r < 0)
                 return log_error_errno(r, "Failed to drop privileges: %m");
 
-#if HAVE_ELFUTILS
         /* Try to get a stack trace if we can */
         if (coredump_size > arg_process_size_max) {
                 log_debug("Not generating stack trace: core size %"PRIu64" is greater "
@@ -825,7 +824,6 @@ static int submit_coredump(
                                         /* fork_disable_dump= */endswith(context->meta[META_EXE], "systemd-coredump"), /* avoid loops */
                                         &stacktrace,
                                         &json_metadata);
-#endif
 
 log:
         core_message = strjoina("Process ", context->meta[META_ARGV_PID],
