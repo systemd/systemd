ad1e3ca17e8cb9679429ca597e43a43d0dea7e82
Julia Kartseva <hex@fb.com>
2021-11-23 15:38:11 -0800

    bpf: fix memleak in restrict_fs_bpf
    Memory allocated in bpf skeleton is not freed. Wrap ptr in _cleanup_.
    
    Fixes: #21471

--- src/core/bpf-lsm.c
+++ src/core/bpf-lsm.c
@@ -52,7 +52,7 @@ static bool bpf_can_link_lsm_program(struct bpf_program *prog) {
 }
 
 static int prepare_restrict_fs_bpf(struct restrict_fs_bpf **ret_obj) {
-        struct restrict_fs_bpf *obj = 0;
+        _cleanup_(restrict_fs_bpf_freep) struct restrict_fs_bpf *obj = NULL;
         _cleanup_close_ int inner_map_fd = -1;
         int r;
 
