f4ada1b42ffd14a12bbad48b40d65332673bc869
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2021-11-26 11:46:54 +0100

    basic/util: add macro variants of log2 functions
    The macro variants can be used in static initializers.
    
    The same guard against calling __builtin_clz(0) is added as for
    __builtin_clzll(0), since that's undefined behaviour too. Our code
    wouldn't call it, but this avoids a potential pitfall with the macro.
    All variants map 0→0. Otherwise we'd often have to handle 0 specially
    in callers.
    
    __builtin_clz takes unsigned as the argument, so there's no LOG2I macro.

--- src/test/test-util.c
+++ src/test/test-util.c
@@ -17,14 +17,72 @@
 #include "tests.h"
 #include "util.h"
 
+TEST(LOG2ULL) {
+        assert_se(LOG2ULL(0) == 0);
+        assert_se(LOG2ULL(1) == 0);
+        assert_se(LOG2ULL(8) == 3);
+        assert_se(LOG2ULL(9) == 3);
+        assert_se(LOG2ULL(15) == 3);
+        assert_se(LOG2ULL(16) == 4);
+        assert_se(LOG2ULL(1024*1024) == 20);
+        assert_se(LOG2ULL(1024*1024+5) == 20);
+}
+
+TEST(CONST_LOG2ULL) {
+        assert_se(CONST_LOG2ULL(0) == 0);
+        assert_se(CONST_LOG2ULL(1) == 0);
+        assert_se(CONST_LOG2ULL(8) == 3);
+        assert_se(CONST_LOG2ULL(9) == 3);
+        assert_se(CONST_LOG2ULL(15) == 3);
+        assert_se(CONST_LOG2ULL(16) == 4);
+        assert_se(CONST_LOG2ULL(1024*1024) == 20);
+        assert_se(CONST_LOG2ULL(1024*1024+5) == 20);
+}
+
+TEST(NONCONST_LOG2ULL) {
+        assert_se(NONCONST_LOG2ULL(0) == 0);
+        assert_se(NONCONST_LOG2ULL(1) == 0);
+        assert_se(NONCONST_LOG2ULL(8) == 3);
+        assert_se(NONCONST_LOG2ULL(9) == 3);
+        assert_se(NONCONST_LOG2ULL(15) == 3);
+        assert_se(NONCONST_LOG2ULL(16) == 4);
+        assert_se(NONCONST_LOG2ULL(1024*1024) == 20);
+        assert_se(NONCONST_LOG2ULL(1024*1024+5) == 20);
+}
+
 TEST(log2u64) {
         assert_se(log2u64(0) == 0);
+        assert_se(log2u64(1) == 0);
         assert_se(log2u64(8) == 3);
         assert_se(log2u64(9) == 3);
         assert_se(log2u64(15) == 3);
         assert_se(log2u64(16) == 4);
         assert_se(log2u64(1024*1024) == 20);
         assert_se(log2u64(1024*1024+5) == 20);
+}
+
+TEST(log2u) {
+        assert_se(log2u(0) == 0);
+        assert_se(log2u(1) == 0);
+        assert_se(log2u(2) == 1);
+        assert_se(log2u(3) == 1);
+        assert_se(log2u(4) == 2);
+        assert_se(log2u(32) == 5);
+        assert_se(log2u(33) == 5);
+        assert_se(log2u(63) == 5);
+        assert_se(log2u(INT_MAX) == sizeof(int)*8-2);
+}
+
+TEST(log2i) {
+        assert_se(log2i(0) == 0);
+        assert_se(log2i(1) == 0);
+        assert_se(log2i(2) == 1);
+        assert_se(log2i(3) == 1);
+        assert_se(log2i(4) == 2);
+        assert_se(log2i(32) == 5);
+        assert_se(log2i(33) == 5);
+        assert_se(log2i(63) == 5);
+        assert_se(log2i(INT_MAX) == sizeof(int)*8-2);
 }
 
 TEST(protect_errno) {
@@ -56,17 +114,6 @@ TEST(unprotect_errno) {
         test_unprotect_errno_inner_function();
 
         assert_se(errno == 4711);
-}
-
-TEST(log2i) {
-        assert_se(log2i(1) == 0);
-        assert_se(log2i(2) == 1);
-        assert_se(log2i(3) == 1);
-        assert_se(log2i(4) == 2);
-        assert_se(log2i(32) == 5);
-        assert_se(log2i(33) == 5);
-        assert_se(log2i(63) == 5);
-        assert_se(log2i(INT_MAX) == sizeof(int)*8-2);
 }
 
 TEST(eqzero) {
