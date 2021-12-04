900c9d744abd4cd5a09affe1dd4b3933c6ffa5bb
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-12-05 07:29:05 +0900

    network: dhcp-pd: rename [DHCPv6PrefixDelegation] -> [DHCPPrefixDelegation]
    Preparation for supporting 6rd.

--- src/network/networkd-dhcp-prefix-delegation.h
+++ src/network/networkd-dhcp-prefix-delegation.h
@@ -3,17 +3,19 @@
 
 #include <stdbool.h>
 
+#include "sd-dhcp6-lease.h"
+
 #include "conf-parser.h"
 
 typedef struct Link Link;
 
-bool link_dhcp6_pd_is_enabled(Link *link);
-bool dhcp6_pd_is_uplink(Link *link, Link *target, bool accept_auto);
-int dhcp6_pd_find_uplink(Link *link, Link **ret);
+bool link_dhcp_pd_is_enabled(Link *link);
+bool dhcp_pd_is_uplink(Link *link, Link *target, bool accept_auto);
+int dhcp_pd_find_uplink(Link *link, Link **ret);
 bool dhcp6_lease_has_pd_prefix(sd_dhcp6_lease *lease);
-int dhcp6_pd_remove(Link *link, bool only_marked);
-int dhcp6_request_prefix_delegation(Link *link);
-int dhcp6_pd_prefix_acquired(Link *dhcp6_link);
-void dhcp6_pd_prefix_lost(Link *dhcp6_link);
+int dhcp_pd_remove(Link *link, bool only_marked);
+int dhcp_request_prefix_delegation(Link *link);
+int dhcp6_pd_prefix_acquired(Link *uplink);
+void dhcp_pd_prefix_lost(Link *uplink);
 
-CONFIG_PARSER_PROTOTYPE(config_parse_dhcp6_pd_subnet_id);
+CONFIG_PARSER_PROTOTYPE(config_parse_dhcp_pd_subnet_id);
