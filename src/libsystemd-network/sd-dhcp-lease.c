e9641c0c150976f5b3719a60575d79255346e2dd
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-12-08 05:47:11 +0900

    dhcp: fix assertion failure
    Fixes #21671.

--- src/libsystemd-network/sd-dhcp-lease.c
+++ src/libsystemd-network/sd-dhcp-lease.c
@@ -775,7 +775,7 @@ int dhcp_lease_parse_options(uint8_t code, uint8_t len, const void *option, void
                 }
 
                 if (!timezone_is_valid(tz, LOG_DEBUG)) {
-                        log_debug_errno(r, "Timezone is not valid, ignoring: %m");
+                        log_debug("Timezone is not valid, ignoring.");
                         return 0;
                 }
 
