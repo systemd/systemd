808c2762a96008e557754f46565895aab20687be
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-12-08 00:45:31 +0900

    network: l2tp: use link_get_local_address()

--- src/network/netdev/netdev-util.c
+++ src/network/netdev/netdev-util.c
@@ -11,6 +11,9 @@ static const char * const netdev_local_address_type_table[_NETDEV_LOCAL_ADDRESS_
         [NETDEV_LOCAL_ADDRESS_DHCP4]   = "dhcp4",
         [NETDEV_LOCAL_ADDRESS_DHCP6]   = "dhcp6",
         [NETDEV_LOCAL_ADDRESS_SLAAC]   = "slaac",
+        [NETDEV_LOCAL_ADDRESS_AUTO]    = "auto",
+        [NETDEV_LOCAL_ADDRESS_STATIC]  = "static",
+        [NETDEV_LOCAL_ADDRESS_DYNAMIC] = "dynamic",
 };
 
 DEFINE_STRING_TABLE_LOOKUP(netdev_local_address_type, NetDevLocalAddressType);
@@ -47,6 +50,11 @@ int link_get_local_address(
                 assert(IN_SET(family, AF_UNSPEC, AF_INET6));
                 family = AF_INET6;
                 break;
+        case NETDEV_LOCAL_ADDRESS_AUTO:
+        case NETDEV_LOCAL_ADDRESS_STATIC:
+        case NETDEV_LOCAL_ADDRESS_DYNAMIC:
+                assert(IN_SET(family, AF_INET, AF_INET6));
+                break;
         default:
                 assert_not_reached();
         }
@@ -80,6 +88,16 @@ int link_get_local_address(
                         break;
                 case NETDEV_LOCAL_ADDRESS_SLAAC:
                         if (a->source != NETWORK_CONFIG_SOURCE_NDISC)
+                                continue;
+                        break;
+                case NETDEV_LOCAL_ADDRESS_AUTO:
+                        break;
+                case NETDEV_LOCAL_ADDRESS_STATIC:
+                        if (!FLAGS_SET(a->flags, IFA_F_PERMANENT))
+                                continue;
+                        break;
+                case NETDEV_LOCAL_ADDRESS_DYNAMIC:
+                        if (FLAGS_SET(a->flags, IFA_F_PERMANENT))
                                 continue;
                         break;
                 default:
