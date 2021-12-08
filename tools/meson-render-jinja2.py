888ba24fffd7cd100ca645d62859f744c113d717
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2021-12-08 18:00:35 +0100

    meson: add missing final newline in jinja2 outputs
    I think this might be caused by trim_blocks=True. We were
    missing the final newline everywhere.

--- tools/meson-render-jinja2.py
+++ tools/meson-render-jinja2.py
@@ -31,5 +31,6 @@ if __name__ == '__main__':
     output = render(sys.argv[2], defines)
     with open(sys.argv[3], 'w') as f:
         f.write(output)
+        f.write('\n')
     info = os.stat(sys.argv[2])
     os.chmod(sys.argv[3], info.st_mode)
