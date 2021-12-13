90cf89446c0f9591eeb9e20a230a04d1b5a837be
Frantisek Sumsal <frantisek@sumsal.cz>
2021-12-13 20:50:28 +0100

    test: fuzz our dbus interfaces with dfuzzer
    WIP

---
+++ test/TEST-21-DFUZZER/test.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: LGPL-2.1-or-later
+set -e
+
+TEST_DESCRIPTION="tbd"
+TEST_NO_NSPAWN=1
+TEST_MASK_SERVICES=0
+
+# shellcheck source=test/test-functions
+. "${TEST_BASE_DIR:?}/test-functions"
+
+command -v dfuzzer >/dev/null || exit 0
+
+test_append_files() {
+    local workspace="${1:?}"
+
+    image_install dfuzzer /etc/dfuzzer.conf
+
+    # Enable all systemd-related services, including the D-Bus ones
+    "$SYSTEMCTL" --root="${workspace:?}" preset-all
+}
+
+do_test "$@"
