2f738adf82cc9cb5ea02aef33b45288127745d24
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-12-21 20:34:23 +0900

    errno-name: drop aliases defined for specific arch
    In kernel's arch/parisc/include/uapi/asm/errno.h, ECANCELLED and
    EREFUSED are defined as aliases of ECANCELED and ECONNREFUSED,
    respectively. Let's drop them.
    
    Fixes #21844.

--- src/basic/generate-errno-list.sh
+++ src/basic/generate-errno-list.sh
@@ -3,5 +3,9 @@
 set -eu
 set -o pipefail
 
+# In kernel's arch/parisc/include/uapi/asm/errno.h, ECANCELLED and EREFUSED are defined as aliases of
+# ECANCELED and ECONNREFUSED, respectively. Let's drop them.
+
 ${1:?} -dM -include errno.h - </dev/null | \
+       grep -Ev '^#define[[:space:]]+(ECANCELLED|EREFUSED)' | \
        awk '/^#define[ \t]+E[^ _]+[ \t]+/ { print $2; }'
