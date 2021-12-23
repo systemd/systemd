496388f1f63c29d4b06c40a76e5a569f23106c79
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-12-23 19:36:06 +0900

    hwdb: update hwdb for v250-final
    Generated by `meson compile -C build update-hwdb update-hwdb-autosuspend`.

--- tools/chromiumos/gen_autosuspend_rules.py
+++ tools/chromiumos/gen_autosuspend_rules.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-# SPDX-License-Identifier: BSD-3-Clause
 # -*- coding: utf-8 -*-
 
 # Copyright 2017 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSES/BSD-3-Clause.txt file.
+# found in the LICENSE file.
 
 """Autosuspend udev rule generator
 
