d70f115d3a0868aba447655223bce60ae8981231
Stephen Hemminger <sthemmin@microsoft.com>
2021-12-24 22:07:40 -0800

    bus-dump: change capture output to use pcapng (#21738)
    This patch changes busctl capture to generate pcapng format
    instead of the legacy pcap format files. It includes basic
    meta-data in the file and still uses microsecond time
    resolution. In future, more things can be added such as
    high resolution timestams, statistics, etc.
    
    PCAP Next Generation capture file format is what tshark uses
    and is in process of being standardized in IETF. It is also
    readable with libpcap.
    
    $ capinfos /tmp/new.pcapng
    File name:           /tmp/new.pcapng
    File type:           Wireshark/... - pcapng
    File encapsulation:  D-Bus
    File timestamp precision:  microseconds (6)
    Packet size limit:   file hdr: (not set)
    Packet size limit:   inferred: 4096 bytes
    Number of packets:   22
    File size:           21kB
    Data size:           20kB
    Capture duration:    0.005694 seconds
    First packet time:   2021-12-11 11:57:42.788374
    Last packet time:    2021-12-11 11:57:42.794068
    Data byte rate:      3,671kBps
    Data bit rate:       29Mbps
    Average packet size: 950.27 bytes
    Average packet rate: 3,863 packets/s
    SHA256:              b85ed8b094af60c64aa6d9db4a91404e841736d36b9e662d707db9e4096148f1
    RIPEMD160:           81f9bac7ec0ec5cd1d55ede136a5c90413894e3a
    SHA1:                8400822ef724b934d6000f5b7604b9e6e91be011
    Strict time order:   True
    Capture oper-sys:    Linux 5.14.0-0.bpo.2-amd64
    Capture application: systemd 250 (250-rc2-33-gdc79ae2+)
    Number of interfaces in file: 1
    Interface #0 info:
                         Encapsulation = D-Bus (146 - dbus)
                         Capture length = 4096
                         Time precision = microseconds (6)
                         Time ticks per second = 1000000
                         Number of stat entries = 0
                         Number of packets = 22

--- src/libsystemd/sd-bus/bus-dump.h
+++ src/libsystemd/sd-bus/bus-dump.h
@@ -8,5 +8,5 @@
 
 int bus_creds_dump(sd_bus_creds *c, FILE *f, bool terse);
 
-int bus_pcap_header(size_t snaplen, FILE *f);
+int bus_pcap_header(size_t snaplen, const char *os, const char *app, FILE *f);
 int bus_message_pcap_frame(sd_bus_message *m, size_t snaplen, FILE *f);
