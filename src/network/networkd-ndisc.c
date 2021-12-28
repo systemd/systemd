04d013808f269a75d8cfcdb9d697b1ff7c4c3eea
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-12-28 22:42:03 +0900

    network: ndisc: ignore route prefix to ::/0
    Fixes #21912.

--- src/network/networkd-ndisc.c
+++ src/network/networkd-ndisc.c
@@ -612,6 +612,11 @@ static int ndisc_router_process_route(Link *link, sd_ndisc_router *rt) {
         if (r < 0)
                 return log_link_error_errno(link, r, "Failed to get route prefix length: %m");
 
+        if (in6_addr_is_null(&dst) && prefixlen == 0) {
+                log_link_debug(link, "Route prefix is ::/0, ignoring");
+                return 0;
+        }
+
         if (in6_prefix_is_filtered(&dst, prefixlen, link->network->ndisc_allow_listed_route_prefix, link->network->ndisc_deny_listed_route_prefix)) {
                 if (DEBUG_LOGGING) {
                         _cleanup_free_ char *buf = NULL;
