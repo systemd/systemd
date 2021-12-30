746e137865e29c473957978cf1e89a32db54fdd0
Luca Boccassi <luca.boccassi@microsoft.com>
2021-12-30 00:53:29 +0000

    dbus-wait-for-jobs: add extra_args to bus_wait_for_jobs_one()
    And pass it through to bus_wait_for_jobs()
    
    (cherry picked from commit 86980de64bf8c03505eec729808f52f3b3042998)
    (cherry picked from commit 0c4fe2e3dcde8225006a36cff643c112bd6c6523)
    (cherry picked from commit 84188acc6fe4a2f04c91c2c4d7b20a3166caa63b)

--- src/shared/bus-wait-for-jobs.h
+++ src/shared/bus-wait-for-jobs.h
@@ -11,6 +11,6 @@ int bus_wait_for_jobs_new(sd_bus *bus, BusWaitForJobs **ret);
 BusWaitForJobs* bus_wait_for_jobs_free(BusWaitForJobs *d);
 int bus_wait_for_jobs_add(BusWaitForJobs *d, const char *path);
 int bus_wait_for_jobs(BusWaitForJobs *d, bool quiet, const char* const* extra_args);
-int bus_wait_for_jobs_one(BusWaitForJobs *d, const char *path, bool quiet);
+int bus_wait_for_jobs_one(BusWaitForJobs *d, const char *path, bool quiet, const char* const* extra_args);
 
 DEFINE_TRIVIAL_CLEANUP_FUNC(BusWaitForJobs*, bus_wait_for_jobs_free);
