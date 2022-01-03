5df66d7d68006615abb4c4d3b1ebad545af4dd72
Evgeny Vereshchagin <evvers@ya.ru>
2022-01-03 12:31:07 +0000

    fuzz: no longer skip empty files
    Empty files and empty strings seem to have triggered various
    issues in the past so it seems they shouldn't be ignore by the
    fuzzers just because fmemopen can't handle them.
    
    Prompted by https://github.com/systemd/systemd/pull/21939#issuecomment-1003113669

--- src/fuzz/fuzz.h
+++ src/fuzz/fuzz.h
@@ -4,5 +4,14 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#include "fileio.h"
+
 /* The entry point into the fuzzer */
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size);
+
+static inline FILE* data_to_file(const uint8_t *data, size_t size) {
+        if (size == 0)
+                return fopen("/dev/null", "re");
+        else
+                return fmemopen_unlocked((char*) data, size, "re");
+}
