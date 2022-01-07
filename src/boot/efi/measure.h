6096f01ebe8576251a504f4e7848c11aed82b49e
Jan Janssen <medhefgo@web.de>
2022-01-07 11:09:08 +0100

    boot: Add TPM to status info

--- src/boot/efi/measure.h
+++ src/boot/efi/measure.h
@@ -5,11 +5,15 @@
 
 #if ENABLE_TPM
 
+BOOLEAN tpm_present(void);
 EFI_STATUS tpm_log_event(UINT32 pcrindex, const EFI_PHYSICAL_ADDRESS buffer, UINTN buffer_size, const CHAR16 *description);
 EFI_STATUS tpm_log_load_options(const CHAR16 *cmdline);
 
 #else
 
+static inline BOOLEAN tpm_present(void) {
+        return FALSE;
+}
 static inline EFI_STATUS tpm_log_event(UINT32 pcrindex, const EFI_PHYSICAL_ADDRESS buffer, UINTN buffer_size, const CHAR16 *description) {
         return EFI_SUCCESS;
 }
