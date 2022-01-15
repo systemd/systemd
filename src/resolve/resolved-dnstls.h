88b4e8f74ed981000ded8e23ead930a6f68eebc8
Joan Bruguera <joanbrugueram@gmail.com>
2022-01-15 17:33:25 +0100

    resolved: Fix DoT timeout on multiple answer records
    When sending multiple DNS questions to a DNS-over-TLS server (e.g. a question
    for A and AAAA records, as is typical) on the same session, the server may
    answer to each question in a separate TLS record, but it may also aggregate
    multiple answers in a single TLS record.
    (Some servers do this very often (e.g. Cloudflare 1.0.0.1), some do it sometimes
    (e.g. Google 8.8.8.8) and some seem to never do it (e.g. Quad9 9.9.9.10)).
    
    Both cases should be handled equivalently, as the byte stream is the same, but
    when multiple answers came in a single TLS record, usually the first answer was
    processed, but the second answer was entirely ignored, which caused a 10s delay
    until the resolution timed out and the missing question was retried.
    This can be reproduced by configuring one of the offending server and running
    `resolvectl query google.com --cache=no` a few times.
    
    To be notified of incoming data, systemd-resolved listens to `EPOLLIN` events
    on the underlying socket. However, when DNS-over-TLS is used, the TLS library
    (OpenSSL or GnuTLS) may read and buffer the entire TLS record when reading the
    first answer, so usually no further `EPOLLIN` events will be generated, and the
    second answer will never be processed.
    
    To avoid this, if there's buffered TLS data, generate a "fake" EPOLLIN event.
    This is hacky, but it makes this case transparent to the rest of the IO code.
    
    (cherry picked from commit 2aaf6bb6e99b0f2bd73e0c49bef9e11a2844bf1a)

--- src/resolve/resolved-dnstls.h
+++ src/resolve/resolved-dnstls.h
@@ -3,6 +3,7 @@
 
 #if ENABLE_DNS_OVER_TLS
 
+#include <stdbool.h>
 #include <stdint.h>
 
 typedef struct DnsServer DnsServer;
@@ -28,6 +29,7 @@ int dnstls_stream_on_io(DnsStream *stream, uint32_t revents);
 int dnstls_stream_shutdown(DnsStream *stream, int error);
 ssize_t dnstls_stream_write(DnsStream *stream, const char *buf, size_t count);
 ssize_t dnstls_stream_read(DnsStream *stream, void *buf, size_t count);
+bool dnstls_stream_has_buffered_data(DnsStream *stream);
 
 void dnstls_server_free(DnsServer *server);
 
