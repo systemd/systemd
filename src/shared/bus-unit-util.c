3e3c7463e578653aaa47098d9bf7e8b22c210c3a
Luca Boccassi <luca.boccassi@microsoft.com>
2022-01-17 01:14:14 +0000

    core: add ExtensionDirectories= setting
    Add a new setting that follows the same principle and implementation
    as ExtensionImages, but using directories as sources.
    It will be used to implement support for extending portable images
    with directories, since portable services can already use a directory
    as root.

--- src/shared/bus-unit-util.c
+++ src/shared/bus-unit-util.c
@@ -973,6 +973,7 @@ static int bus_append_execute_property(sd_bus_message *m, const char *field, con
                               "ExecPaths",
                               "NoExecPaths",
                               "ExecSearchPath",
+                              "ExtensionDirectories",
                               "ConfigurationDirectory",
                               "SupplementaryGroups",
                               "SystemCallArchitectures"))
