5edb24f749aa03abd9e3c09a53ecae1bdc1ae4c9
Yu Watanabe <watanabe.yu+github@gmail.com>
2022-01-19 18:26:44 +0900

    test: tentatively set/request UUID in uevent forcibly
    For issue #21603.

--- test/units/testsuite-17.04.sh
+++ test/units/testsuite-17.04.sh
@@ -16,7 +16,7 @@ ACTION=="change", SUBSYSTEM=="mem", KERNEL=="null", TAG+="changed"
 EOF
 
 udevadm control --reload --log-level=debug
-SYSTEMD_LOG_LEVEL=debug udevadm trigger --verbose --settle --action add /dev/null
+SYSTEMD_LOG_LEVEL=debug udevadm trigger --verbose --uuid --settle --action add /dev/null
 
 test -f /run/udev/tags/added/c1:3
 test ! -f /run/udev/tags/changed/c1:3
@@ -25,7 +25,7 @@ udevadm info /dev/null | grep -q 'E: CURRENT_TAGS=.*:added:.*'
 udevadm info /dev/null | grep -q 'E: TAGS=.*:changed:.*' && { echo 'unexpected TAGS='; exit 1; }
 udevadm info /dev/null | grep -q 'E: CURRENT_TAGS=.*:changed:.*' && { echo 'unexpected CURRENT_TAGS='; exit 1; }
 
-SYSTEMD_LOG_LEVEL=debug udevadm trigger --verbose --settle --action change /dev/null
+SYSTEMD_LOG_LEVEL=debug udevadm trigger --verbose --uuid --settle --action change /dev/null
 
 test -f /run/udev/tags/added/c1:3
 test -f /run/udev/tags/changed/c1:3
@@ -34,7 +34,7 @@ udevadm info /dev/null | grep -q 'E: CURRENT_TAGS=.*:added:.*' && { echo 'unexpe
 udevadm info /dev/null | grep -q 'E: TAGS=.*:changed:.*'
 udevadm info /dev/null | grep -q 'E: CURRENT_TAGS=.*:changed:.*'
 
-SYSTEMD_LOG_LEVEL=debug udevadm trigger --verbose --settle --action add /dev/null
+SYSTEMD_LOG_LEVEL=debug udevadm trigger --verbose --uuid --settle --action add /dev/null
 
 test -f /run/udev/tags/added/c1:3
 test -f /run/udev/tags/changed/c1:3
