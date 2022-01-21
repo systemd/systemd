d4b908d70c7b836dff8f568e26d2142c16d3a251
Daan De Meyer <daan.j.demeyer@gmail.com>
2022-01-21 15:19:26 +0000

    journal: Run unit tests with and without compact mode enabled

--- src/journal/test-journal-flush.c
+++ src/journal/test-journal-flush.c
@@ -13,7 +13,7 @@
 #include "path-util.h"
 #include "string-util.h"
 
-int main(int argc, char *argv[]) {
+static void run_test(int argc, char *argv[]) {
         _cleanup_(mmap_cache_unrefp) MMapCache *m = NULL;
         _cleanup_free_ char *fn = NULL;
         char dn[] = "/var/tmp/test-journal-flush.XXXXXX";
@@ -70,6 +70,12 @@ int main(int argc, char *argv[]) {
 
         unlink(fn);
         assert_se(rmdir(dn) == 0);
+}
 
-        return 0;
+int main(int argc, char *argv[]) {
+        assert_se(setenv("SYSTEMD_JOURNAL_COMPACT", "0", 1) >= 0);
+        run_test(argc, argv);
+
+        assert_se(setenv("SYSTEMD_JOURNAL_COMPACT", "1", 1) >= 0);
+        run_test(argc, argv);
 }
