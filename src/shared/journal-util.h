1bce99ad8a4b8d65add867ab77c74f798614e584
Daan De Meyer <daan.j.demeyer@gmail.com>
2022-01-25 17:00:39 +0000

    journal: Add --convert= command to journalctl
    --convert writes the journal files read by journalctl to the given
    location. The location should be specified as a full journal file
    path (e.g. /a/b/c/converted.journal). The directory specifies where
    the converted journal files will be stored. The filename specifies
    the naming convention the converted journal files will follow.

--- src/shared/journal-util.h
+++ src/shared/journal-util.h
@@ -6,5 +6,9 @@
 
 #include "sd-journal.h"
 
+#include "journal-internal.h"
+
 int journal_access_blocked(sd_journal *j);
 int journal_access_check_and_warn(sd_journal *j, bool quiet, bool want_other_users);
+
+bool journal_shall_try_append_again(JournalFile *f, int r);
