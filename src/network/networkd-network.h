4e023b9707c4db80702bb57b1f4adadd9fe80324
Yu Watanabe <watanabe.yu+github@gmail.com>
2022-01-26 13:06:37 +0900

    network: tc: use hashmap to store traffic control settings in .network files
    As now each qdisc or tclass is configured when it is ready.

--- src/network/networkd-network.h
+++ src/network/networkd-network.h
@@ -341,7 +341,7 @@ struct Network {
         Hashmap *route_prefixes_by_section;
         Hashmap *rules_by_section;
         Hashmap *dhcp_static_leases_by_section;
-        OrderedHashmap *tc_by_section;
+        Hashmap *tc_by_section;
         OrderedHashmap *sr_iov_by_section;
 
         /* All kinds of DNS configuration */
