db357e9a89a302649afa3b6dcaefdb158c0a9169
Frantisek Sumsal <frantisek@sumsal.cz>
2022-01-31 16:48:15 +0100

    test: make the code less error-prone

--- test/TEST-69-SHUTDOWN/test.sh
+++ test/TEST-69-SHUTDOWN/test.sh
@@ -9,25 +9,26 @@ TEST_NO_QEMU=1
 # shellcheck source=test/test-functions
 . "${TEST_BASE_DIR:?}/test-functions"
 
-_ORIG_NSPAWN="$SYSTEMD_NSPAWN"
-SYSTEMD_NSPAWN="$STATEDIR/run-nspawn"
+_ORIG_NSPAWN="${SYSTEMD_NSPAWN:?}"
+SYSTEMD_NSPAWN="${STATEDIR:?}/run-nspawn"
 
 setup_nspawn_root_hook() {
-    cat > "$STATEDIR"/run-nspawn <<-EOF
+    cat > "${STATEDIR:?}"/run-nspawn <<-EOF
 	#!/bin/bash
-	exec "$TEST_BASE_DIR"/test-shutdown.py -- "$_ORIG_NSPAWN" "\$@"
+	exec "${TEST_BASE_DIR:?}"/test-shutdown.py -- "$_ORIG_NSPAWN" "\$@"
 	exit 1
 	EOF
-    chmod 755 "$STATEDIR"/run-nspawn
+    chmod 755 "${STATEDIR:?}"/run-nspawn
 }
 
 test_append_files() {
+    local workspace="${1:?}"
     # prevent shutdown in test suite, the expect script does that manually.
-    rm "$1"/usr/lib/systemd/tests/testdata/units/end.service
+    rm "${workspace:?}/usr/lib/systemd/tests/testdata/units/end.service"
     inst /usr/bin/screen
-    echo "PS1='screen\$WINDOW # '" > "$1"/etc/bash.bashrc
-    echo 'startup_message off' > "$1"/etc/screenrc
-    echo 'bell_msg ""' >> "$1"/etc/screenrc
+    echo "PS1='screen\$WINDOW # '" >"$workspace/etc/bash.bashrc"
+    echo 'startup_message off' >"$workspace/etc/screenrc"
+    echo 'bell_msg ""' >>"$1/etc/screenrc"
 }
 
 do_test "$@"
