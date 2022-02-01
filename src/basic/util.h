007e03b284e8ffc0b92edb2122cd9d2d16f049ef
Lennart Poettering <lennart@poettering.net>
2022-02-01 12:06:21 +0100

    util: another set of CVE-2021-4034 assert()s
    It's a good idea that we validate argc/argv when we are supposed to
    store them away.

--- src/basic/util.h
+++ src/basic/util.h
@@ -9,6 +9,12 @@ extern int saved_argc;
 extern char **saved_argv;
 
 static inline void save_argc_argv(int argc, char **argv) {
+
+        /* Protect against CVE-2021-4034 style attacks */
+        assert_se(argc > 0);
+        assert_se(argv);
+        assert_se(argv[0]);
+
         saved_argc = argc;
         saved_argv = argv;
 }
