21437bb7d5edecb8669eee145ced54bd3e3ad84e
Yu Watanabe <watanabe.yu+github@gmail.com>
2022-02-01 15:38:39 +0900

    network: drop outdated TODO comment
    Follow-up for 59aa622013277cfe6349d0789a7a00ad2b884902.

--- src/network/networkd-dhcp-server.c
+++ src/network/networkd-dhcp-server.c
@@ -402,12 +402,6 @@ static int dhcp4_server_configure(Link *link) {
         if (r < 0)
                 return log_link_error_errno(link, r, "Failed to configure address pool for DHCPv4 server instance: %m");
 
-        /* TODO:
-        r = sd_dhcp_server_set_router(link->dhcp_server, &main_address->in_addr.in);
-        if (r < 0)
-                return r;
-        */
-
         if (link->network->dhcp_server_max_lease_time_usec > 0) {
                 r = sd_dhcp_server_set_max_lease_time(link->dhcp_server,
                                                       DIV_ROUND_UP(link->network->dhcp_server_max_lease_time_usec, USEC_PER_SEC));
