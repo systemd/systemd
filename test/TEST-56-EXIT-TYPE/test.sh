d14001fbcca5e3028a1a5a7861390c536dafbe2d
Frantisek Sumsal <frantisek@sumsal.cz>
2022-02-01 20:25:00 +0100

    test: require unified cgroup hierarchy for TEST-56
    since cgroup empty notifications are unreliable in legacy cgroups.
    
    See: systemd/systemd#22320
    Complements: systemd/systemd#22344

--- test/TEST-56-EXIT-TYPE/test.sh
+++ test/TEST-56-EXIT-TYPE/test.sh
@@ -6,4 +6,9 @@ TEST_DESCRIPTION="test ExitType=cgroup"
 # shellcheck source=test/test-functions
 . "${TEST_BASE_DIR:?}/test-functions"
 
+if [[ "$(get_cgroup_hierarchy)" != unified ]]; then
+    echo "This test requires unified cgroup hierarchy, skipping..."
+    exit 0
+fi
+
 do_test "$@"
