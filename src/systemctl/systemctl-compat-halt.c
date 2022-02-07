6635702e758cd44388444b24f05cd2fac5750633
Ludwig Nussel <ludwig.nussel@suse.de>
2022-02-07 10:25:10 +0100

    systemctl: fix halt -f wrt man page
    Commit adefc8789b always asks logind for shutdown first. So I broke halt
    -f which is supposed to issue a direct syscall in that case. At least
    that's what the code did AFAICT.
    
    Commit 5d9adb5 however changed the man page to state that that halt -f
    talks to the service manager and causes a clean shutdown. One is
    supposed to pass -f twice so direct syscall is issued. Adjust behavior
    according to documentation.

--- src/systemctl/systemctl-compat-halt.c
+++ src/systemctl/systemctl-compat-halt.c
@@ -12,6 +12,7 @@
 #include "systemctl-compat-halt.h"
 #include "systemctl-compat-telinit.h"
 #include "systemctl-logind.h"
+#include "systemctl-start-unit.h"
 #include "systemctl-util.h"
 #include "systemctl.h"
 #include "terminal-util.h"
@@ -75,6 +76,8 @@ int halt_parse_argv(int argc, char *argv[]) {
         assert(argc >= 0);
         assert(argv);
 
+        /* called in sysvinit system as last command in
+         * shutdown/reboot so this is always forceful */
         if (utmp_get_runlevel(&runlevel, NULL) >= 0)
                 if (IN_SET(runlevel, '0', '6'))
                         arg_force = 2;
@@ -99,7 +102,7 @@ int halt_parse_argv(int argc, char *argv[]) {
                         break;
 
                 case 'f':
-                        arg_force = 2;
+                        ++arg_force;
                         break;
 
                 case 'w':
@@ -144,29 +147,31 @@ int halt_parse_argv(int argc, char *argv[]) {
 int halt_main(void) {
         int r;
 
-        /* always try logind first */
-        if (arg_when > 0)
-                r = logind_schedule_shutdown();
-        else {
-                r = logind_check_inhibitors(arg_action);
-                if (r < 0)
-                        return r;
+        if (arg_force == 0) {
+                /* always try logind first */
+                if (arg_when > 0)
+                        r = logind_schedule_shutdown();
+                else {
+                        r = logind_check_inhibitors(arg_action);
+                        if (r < 0)
+                                return r;
 
-                r = logind_reboot(arg_action);
+                        r = logind_reboot(arg_action);
+                }
+                if (r >= 0)
+                        return r;
+                if (IN_SET(r, -EACCES, -EOPNOTSUPP, -EINPROGRESS))
+                        /* Requested operation requires auth, is not supported on the local system or already in
+                         * progress */
+                        return r;
+                /* on all other errors, try low-level operation */
         }
-        if (r >= 0)
-                return r;
-        if (IN_SET(r, -EACCES, -EOPNOTSUPP, -EINPROGRESS))
-                /* Requested operation requires auth, is not supported on the local system or already in
-                 * progress */
-                return r;
-        /* on all other errors, try low-level operation */
 
         /* In order to minimize the difference between operation with and without logind, we explicitly
          * enable non-blocking mode for this, as logind's shutdown operations are always non-blocking. */
         arg_no_block = true;
 
-        if (!arg_dry_run && !arg_force)
+        if (!arg_dry_run && arg_force < 2)
                 return start_with_fallback();
 
         if (geteuid() != 0) {
@@ -188,5 +193,5 @@ int halt_main(void) {
                 return 0;
 
         r = halt_now(arg_action);
-        return log_error_errno(r, "Failed to reboot: %m");
+        return log_error_errno(r, "Failed to %s: %m", action_table[arg_action].verb);
 }
