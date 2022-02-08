b7f82551303a5d948bc5ce0e9cc2b4f39456c8f1
Vito Caputo <vcaputo@pengaru.com>
2022-02-08 15:49:30 -0800

    mmap-cache: inline mmap_cache_fd_get() in mmap-cache.h
    This necessarily exports mmap_cache_fd_get_slow() for the slow
    path.
    
    Also mapping_matches{_fd}() were brought along with a mmap_
    prefix rename.
    
    `journalctl -u postfix@-` timings show a ~10%+-2% runtime
    improvement when cached, on a mailserver's journals.  That's for
    an operation where a majority of the CPU time is spent in the
    output_short() side of things:
    
    Before any inlining:
    
    ```
     $ time LD_LIBRARY_PATH=build-main/ build-main/journalctl --directory rel-builds/test-journals/ -u postfix@- > /dev/null
    
      real    0m10.716s
      user    0m10.610s
      sys     0m0.090s
    ```
    
    With just inlined journal_move_to():
    
    ```
     $ time LD_LIBRARY_PATH=build-main/ build-main/journalctl --directory rel-builds/test-journals/ -u postfix@- > /dev/null
    
      real    0m10.470s
      user    0m10.365s
      sys     0m0.097s
    ```
    
    Now, with MMapContextCache hits performed inline too:
    
    ```
     $ time LD_LIBRARY_PATH=build-test/ build-test/journalctl --directory rel-builds/test-journals/ -u postfix@- > /dev/null
    
      real    0m9.987s
      user    0m9.876s
      sys     0m0.097s
    ```
    
    The target to beat, v247, when journal_move_to_object() elided the
    second move_to() call:
    ```
     $ time LD_LIBRARY_PATH=rel-builds/v247/ rel-builds/v247/journalctl --directory rel-builds/test-journals/ -u postfix@- > /dev/null
    
      real    0m10.133s
      user    0m10.015s
      sys     0m0.104s
    ```

--- src/libsystemd/sd-journal/mmap-cache.h
+++ src/libsystemd/sd-journal/mmap-cache.h
@@ -39,7 +39,7 @@ MMapCache* mmap_cache_ref(MMapCache *m);
 MMapCache* mmap_cache_unref(MMapCache *m);
 DEFINE_TRIVIAL_CLEANUP_FUNC(MMapCache*, mmap_cache_unref);
 
-int mmap_cache_fd_get(
+int mmap_cache_fd_get_slow(
         MMapFileDescriptor *f,
         unsigned context,
         bool keep_always,
@@ -47,6 +47,60 @@ int mmap_cache_fd_get(
         size_t size,
         struct stat *st,
         void **ret);
+
+_pure_ static inline bool mmap_mapping_matches(MMapMapping *m, uint64_t offset, size_t size) {
+        assert(m);
+        assert(size > 0);
+
+        return
+                offset >= m->offset &&
+                offset + size <= m->offset + m->size;
+}
+
+_pure_ static inline bool mmap_mapping_matches_fd(MMapMapping *m, MMapFileDescriptor *f, uint64_t offset, size_t size) {
+        assert(m);
+        assert(f);
+
+        return
+                m->fd == f &&
+                mmap_mapping_matches(m, offset, size);
+}
+
+/* XXX: keep this inline as it's quite hot for users like journalctl */
+static inline int mmap_cache_fd_get(
+                MMapFileDescriptor *f,
+                unsigned context,
+                bool keep_always,
+                uint64_t offset,
+                size_t size,
+                struct stat *st,
+                void **ret) {
+
+        MMapContextCache *cc;
+        MMapContext *c;
+
+        assert(f);
+        assert(size > 0);
+        assert(ret);
+        assert(context < MMAP_CACHE_MAX_CONTEXTS);
+
+        cc = *((MMapContextCache **)f);
+        assert(cc);
+
+        /* Check whether the current context is the right one already */
+        c = &cc->contexts[context];
+        if (c->mapping && mmap_mapping_matches_fd(c->mapping, f, offset, size)) {
+                c->mapping->keep_always = c->mapping->keep_always || keep_always;
+
+                *ret = (uint8_t*) c->mapping->ptr + (offset - c->mapping->offset);
+                cc->n_context_cache_hit++;
+
+                return 1;
+        }
+
+        return mmap_cache_fd_get_slow(f, context, keep_always, offset, size, st, ret);
+}
+
 MMapFileDescriptor* mmap_cache_add_fd(MMapCache *m, int fd, int prot);
 MMapCache* mmap_cache_fd_cache(MMapFileDescriptor *f);
 void mmap_cache_fd_free(MMapFileDescriptor *f);
