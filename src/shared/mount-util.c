a9dac7a6dd31225dbe9633061dcade12c0c90a32
Lennart Poettering <lennart@poettering.net>
2022-02-14 17:21:41 +0100

    tree-wide: port various places over to new stat_inode_same() helper

--- src/shared/mount-util.c
+++ src/shared/mount-util.c
@@ -816,7 +816,7 @@ static int mount_in_namespace(
                 return log_debug_errno(errno, "Failed to fstat mount namespace FD of systemd: %m");
 
         /* We can't add new mounts at runtime if the process wasn't started in a namespace */
-        if (st.st_ino == self_mntns_st.st_ino && st.st_dev == self_mntns_st.st_dev)
+        if (stat_inode_same(&st, &self_mntns_st))
                 return log_debug_errno(SYNTHETIC_ERRNO(EINVAL), "Failed to activate bind mount in target, not running in a mount namespace");
 
         /* One day, when bind mounting /proc/self/fd/n works across namespace boundaries we should rework
