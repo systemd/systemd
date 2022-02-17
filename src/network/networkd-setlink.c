125d098464cdbbdba1afcf7126005e3064419ea2
Yu Watanabe <watanabe.yu+github@gmail.com>
2022-02-18 00:57:59 +0900

    network: call post_create() of netdev on reconfigure
    This makes some properties set by post_create() are updated on
    reconfigure.

--- src/network/networkd-setlink.c
+++ src/network/networkd-setlink.c
@@ -469,6 +469,9 @@ static int link_is_ready_to_set_link(Link *link, RequestType type, Request *req)
         if (!IN_SET(link->state, LINK_STATE_CONFIGURING, LINK_STATE_CONFIGURED))
                 return false;
 
+        if (!link->netdev_configured)
+                return false;
+
         switch (type) {
         case REQUEST_TYPE_SET_LINK_BOND:
         case REQUEST_TYPE_SET_LINK_BRIDGE:
