872822fbaf421c401f84df57fdbcc77996ac6b80
Lennart Poettering <lennart@poettering.net>
2022-02-21 10:25:36 +0100

    analyze: split out "filesystems" verb into its own .c/.h file pair

--- src/analyze/analyze.h
+++ src/analyze/analyze.h
@@ -3,9 +3,12 @@
 
 #include <stdbool.h>
 
+#include "pager.h"
 #include "time-util.h"
 
+extern PagerFlags arg_pager_flags;
 extern unsigned arg_iterations;
 extern usec_t arg_base_time;
+extern bool arg_quiet;
 
 void time_parsing_hint(const char *p, bool calendar, bool timestamp, bool timespan);
