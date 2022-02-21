2b04e72cd19286e58a8234e282e6c1a32d473b0d
Lennart Poettering <lennart@poettering.net>
2022-02-21 10:52:49 +0100

    analyze: split out "service-watchdogs" verb

--- src/analyze/analyze.c
+++ src/analyze/analyze.c
@@ -20,6 +20,7 @@
 #include "analyze-elf.h"
 #include "analyze-filesystems.h"
 #include "analyze-security.h"
+#include "analyze-service-watchdogs.h"
 #include "analyze-syscall-filter.h"
 #include "analyze-timespan.h"
 #include "analyze-timestamp.h"
@@ -1411,40 +1412,6 @@ void time_parsing_hint(const char *p, bool calendar, bool timestamp, bool timesp
         if (timespan && parse_time(p, NULL, USEC_PER_SEC) >= 0)
                 log_notice("Hint: this expression is a valid timespan. "
                            "Use 'systemd-analyze timespan \"%s\"' instead?", p);
-}
-
-static int service_watchdogs(int argc, char *argv[], void *userdata) {
-        _cleanup_(sd_bus_error_free) sd_bus_error error = SD_BUS_ERROR_NULL;
-        _cleanup_(sd_bus_flush_close_unrefp) sd_bus *bus = NULL;
-        int b, r;
-
-        assert(IN_SET(argc, 1, 2));
-        assert(argv);
-
-        r = acquire_bus(&bus, NULL);
-        if (r < 0)
-                return bus_log_connect_error(r, arg_transport);
-
-        if (argc == 1) {
-                /* get ServiceWatchdogs */
-                r = bus_get_property_trivial(bus, bus_systemd_mgr, "ServiceWatchdogs", &error, 'b', &b);
-                if (r < 0)
-                        return log_error_errno(r, "Failed to get service-watchdog state: %s", bus_error_message(&error, r));
-
-                printf("%s\n", yes_no(!!b));
-
-        } else {
-                /* set ServiceWatchdogs */
-                b = parse_boolean(argv[1]);
-                if (b < 0)
-                        return log_error_errno(b, "Failed to parse service-watchdogs argument: %m");
-
-                r = bus_set_property(bus, bus_systemd_mgr, "ServiceWatchdogs", &error, "b", b);
-                if (r < 0)
-                        return log_error_errno(r, "Failed to set service-watchdog state: %s", bus_error_message(&error, r));
-        }
-
-        return 0;
 }
 
 static int do_condition(int argc, char *argv[], void *userdata) {
