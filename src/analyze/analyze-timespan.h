30bddc066ac185f28f2a4372d1fece174add0b13
Lennart Poettering <lennart@poettering.net>
2022-02-21 10:09:06 +0100

    analyze: split out "timespan" verb into its own .c/.h file

---
+++ src/analyze/analyze-timespan.h
@@ -0,0 +1,4 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#pragma once
+
+int dump_timespan(int argc, char *argv[], void *userdata);
---
+++ src/analyze/analyze.h
@@ -0,0 +1,6 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#pragma once
+
+#include <stdbool.h>
+
+void time_parsing_hint(const char *p, bool calendar, bool timestamp, bool timespan);
