24b7ff5a38165e3b450cd3efc697b67434ccbd66
Lennart Poettering <lennart@poettering.net>
2022-02-21 11:04:31 +0100

    analyze: split out "capability" verb

--- src/analyze/analyze.c
+++ src/analyze/analyze.c
@@ -14,6 +14,7 @@
 #include "alloc-util.h"
 #include "analyze.h"
 #include "analyze-calendar.h"
+#include "analyze-capability.h"
 #include "analyze-condition.h"
 #include "analyze-dot.h"
 #include "analyze-dump.h"
@@ -1310,51 +1311,6 @@ static int dump_unit_paths(int argc, char *argv[], void *userdata) {
                 puts(*p);
 
         return 0;
-}
-
-static int dump_capabilities(int argc, char *argv[], void *userdata) {
-        _cleanup_(table_unrefp) Table *table = NULL;
-        unsigned last_cap;
-        int r;
-
-        table = table_new("name", "number");
-        if (!table)
-                return log_oom();
-
-        (void) table_set_align_percent(table, table_get_cell(table, 0, 1), 100);
-
-        /* Determine the maximum of the last cap known by the kernel and by us */
-        last_cap = MAX((unsigned) CAP_LAST_CAP, cap_last_cap());
-
-        if (strv_isempty(strv_skip(argv, 1)))
-                for (unsigned c = 0; c <= last_cap; c++) {
-                        r = table_add_many(table,
-                                           TABLE_STRING, capability_to_name(c) ?: "cap_???",
-                                           TABLE_UINT, c);
-                        if (r < 0)
-                                return table_log_add_error(r);
-                }
-        else {
-                for (int i = 1; i < argc; i++) {
-                        int c;
-
-                        c = capability_from_name(argv[i]);
-                        if (c < 0 || (unsigned) c > last_cap)
-                                return log_error_errno(SYNTHETIC_ERRNO(EINVAL), "Capability \"%s\" not known.", argv[i]);
-
-                        r = table_add_many(table,
-                                           TABLE_STRING, capability_to_name(c) ?: "cap_???",
-                                           TABLE_UINT, (unsigned) c);
-                        if (r < 0)
-                                return table_log_add_error(r);
-                }
-
-                (void) table_set_sort(table, (size_t) 1);
-        }
-
-        pager_open(arg_pager_flags);
-
-        return table_print(table, NULL);
 }
 
 void time_parsing_hint(const char *p, bool calendar, bool timestamp, bool timespan) {
