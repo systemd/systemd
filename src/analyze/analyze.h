5229b03c108cc7a32917683e1435dafe70c7262d
Lennart Poettering <lennart@poettering.net>
2022-02-21 10:18:57 +0100

    analyze: split out calendar verb into own .c/.h file

--- src/analyze/analyze.h
+++ src/analyze/analyze.h
@@ -3,4 +3,9 @@
 
 #include <stdbool.h>
 
+#include "time-util.h"
+
+extern unsigned arg_iterations;
+extern usec_t arg_base_time;
+
 void time_parsing_hint(const char *p, bool calendar, bool timestamp, bool timespan);
