3b8fc5c1bc744c5105f5a8e76997d72a829bb06d
Daniel Mack <daniel@zonque.org>
2022-02-24 15:44:48 +0100

    timesync: export manager_is_connected()
    This function use useful from other files, so let's export it.

--- src/timesync/timesyncd-manager.h
+++ src/timesync/timesyncd-manager.h
@@ -124,5 +124,6 @@ void manager_flush_server_names(Manager *m, ServerType t);
 
 int manager_connect(Manager *m);
 void manager_disconnect(Manager *m);
+bool manager_is_connected(Manager *m);
 
 int manager_setup_save_time_event(Manager *m);
