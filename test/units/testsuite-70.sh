47530d417aad0e48a0203440a1439ca7b26cc1c8
Grigori Goronzy <greg@chown.ath.cx>
2022-02-24 01:28:29 +0100

    cryptenroll: add tests for TPM2 unlocking
    Add tests for enrolling and unlocking. Various cases are tested:
    
    - Default PCR 7 policy w/o PIN, good and bad cases (wrong PCR)
    - PCR 7 + PIN policy, good and bad cases (wrong PCR, wrong PIN)
    - Non-default PCR 0+7 policy w/o PIN, good and bad cases (wrong PCR 0)

---
+++ test/units/testsuite-70.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: LGPL-2.1-or-later
+set -ex
+set -o pipefail
+
+# Extend PCRs (they might be invalid, i.e. zeroes)
+tpm2_pcrextend 0:sha256=0000000000000000000000000000000000000000000000000000000000000000
+tpm2_pcrextend 7:sha256=0000000000000000000000000000000000000000000000000000000000000000
+
+# Prepare fresh disk image
+dd if=/dev/zero of=/test.img bs=1024k count=100 status=none
+echo -n passphrase >/tmp/passphrase
+echo YES | cryptsetup luksFormat test.img /tmp/passphrase
+
+# Enroll unlock with default PCR policy
+env PASSWORD=passphrase systemd-cryptenroll --tpm2-device=auto /test.img
+/usr/lib/systemd/systemd-cryptsetup attach test-volume /test.img - tpm2-device=auto,headless=1
+cryptsetup luksClose test-volume
+
+# Check with wrong PCR
+tpm2_pcrextend 7:sha256=0000000000000000000000000000000000000000000000000000000000000000
+! /usr/lib/systemd/systemd-cryptsetup attach test-volume /test.img - tpm2-device=auto,headless=1
+
+# Enroll unlock with PCR+PIN policy
+systemd-cryptenroll --wipe-slot=tpm2 /test.img
+env PASSWORD=passphrase NEWPIN=123456 systemd-cryptenroll --tpm2-device=auto --tpm2-with-pin=true /test.img
+env PIN=123456 /usr/lib/systemd/systemd-cryptsetup attach test-volume /test.img - tpm2-device=auto,headless=1
+cryptsetup luksClose test-volume
+
+# Check failure with wrong PIN
+! env PIN=123457 /usr/lib/systemd/systemd-cryptsetup attach test-volume /test.img - tpm2-device=auto,headless=1
+
+# Check failure with wrong PCR (and correct PIN)
+tpm2_pcrextend 7:sha256=0000000000000000000000000000000000000000000000000000000000000000
+! env PIN=123456 /usr/lib/systemd/systemd-cryptsetup attach test-volume /test.img - tpm2-device=auto,headless=1
+
+# Enroll unlock with PCR 0+7
+systemd-cryptenroll --wipe-slot=tpm2 /test.img
+env PASSWORD=passphrase systemd-cryptenroll --tpm2-device=auto --tpm2-pcrs=0+7 /test.img
+/usr/lib/systemd/systemd-cryptsetup attach test-volume /test.img - tpm2-device=auto,headless=1
+cryptsetup luksClose test-volume
+
+# Check with wrong PCR 0
+tpm2_pcrextend 0:sha256=0000000000000000000000000000000000000000000000000000000000000000
+! /usr/lib/systemd/systemd-cryptsetup attach test-volume /test.img - tpm2-device=auto,headless=1
+
+echo OK >/testok
+
+exit 0
