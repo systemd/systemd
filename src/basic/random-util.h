ff29f243ba74195950ee2d888ad3e274a773fc06
Jason A. Donenfeld <Jason@zx2c4.com>
2022-03-06 22:15:44 -0700

    random-util: remove RDRAND usage
    /dev/urandom is seeded with RDRAND. Calling genuine_random_bytes(...,
    ..., 0) will use /dev/urandom as a last resort. Hence, we gain nothing
    here by having our own RDRAND wrapper, because /dev/urandom already is
    based on RDRAND output, even before /dev/urandom has fully initialized.
    
    Furthermore, RDRAND is not actually fast! And on each successive
    generation of new x86 CPUs, from both AMD and Intel, it just gets
    slower.
    
    This commit simplifies things by just using /dev/urandom in cases where
    we before might use RDRAND, since /dev/urandom will always have RDRAND
    mixed in as part of it.
    
    And above where I say "/dev/urandom", what I actually mean is
    GRND_INSECURE, which is the same thing but won't generate warnings in
    dmesg.

--- src/basic/random-util.h
+++ src/basic/random-util.h
@@ -8,7 +8,6 @@
 typedef enum RandomFlags {
         RANDOM_EXTEND_WITH_PSEUDO = 1 << 0, /* If we can't get enough genuine randomness, but some, fill up the rest with pseudo-randomness */
         RANDOM_BLOCK              = 1 << 1, /* Rather block than return crap randomness (only if the kernel supports that) */
-        RANDOM_ALLOW_RDRAND       = 1 << 2, /* Allow usage of the CPU RNG */
 } RandomFlags;
 
 int genuine_random_bytes(void *p, size_t n, RandomFlags flags); /* returns "genuine" randomness, optionally filled up with pseudo random, if not enough is available */
@@ -28,8 +27,6 @@ static inline uint32_t random_u32(void) {
         random_bytes(&u, sizeof(u));
         return u;
 }
-
-int rdrand(unsigned long *ret);
 
 /* Some limits on the pool sizes when we deal with the kernel random pool */
 #define RANDOM_POOL_SIZE_MIN 512U
