9aac19ea7aa2b8fdab4ceae3dcf8920401866e45
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2022-03-08 11:49:19 +0100

    shared/install-printf: drop now-unused install_path_printf()

--- src/shared/install-printf.h
+++ src/shared/install-printf.h
@@ -4,11 +4,4 @@
 #include "install.h"
 #include "unit-name.h"
 
-int install_full_printf_internal(const UnitFileInstallInfo *i, const char *format, size_t max_length, const char *root, char **ret);
-
-static inline int install_name_printf(const UnitFileInstallInfo *i, const char *format, const char *root, char **ret) {
-        return install_full_printf_internal(i, format, UNIT_NAME_MAX, root, ret);
-}
-static inline int install_path_printf(const UnitFileInstallInfo *i, const char *format, const char *root, char **ret) {
-        return install_full_printf_internal(i, format, PATH_MAX-1, root, ret);
-}
+int install_name_printf(const UnitFileInstallInfo *i, const char *format, const char *root, char **ret);
