50e0022fd7d7b5f7f889e09cf921e118e03b451b
Frantisek Sumsal <frantisek@sumsal.cz>
2022-03-13 14:45:03 +0100

    macro: account for negative values in DECIMAL_STR_WIDTH()
    With negative numbers we wouldn't account for the minus sign, thus
    returning a string with one character too short, triggering buffer
    overflows in certain situations.

--- src/basic/macro.h
+++ src/basic/macro.h
@@ -319,13 +319,14 @@ static inline int __coverity_check_and_return__(int condition) {
              sizeof(type) <= 4 ? 10U :                                  \
              sizeof(type) <= 8 ? 20U : sizeof(int[-2*(sizeof(type) > 8)])))
 
-#define DECIMAL_STR_WIDTH(x)                            \
-        ({                                              \
-                typeof(x) _x_ = (x);                    \
-                size_t ans = 1;                         \
-                while ((_x_ /= 10) != 0)                \
-                        ans++;                          \
-                ans;                                    \
+#define DECIMAL_STR_WIDTH(x)                                                     \
+        ({                                                                       \
+                typeof(x) _x_ = (x);                                             \
+                size_t ans = 1;                                                  \
+                ans += __builtin_choose_expr(IS_SIGNED_INTEGER_TYPE(_x_), 1, 0); \
+                while ((_x_ /= 10) != 0)                                         \
+                        ans++;                                                   \
+                ans;                                                             \
         })
 
 #define SWAP_TWO(x, y) do {                        \
