d6c9411072901556176ac130f2ce71a33107aa93
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2022-03-16 10:17:32 +0100

    shared/install: create relative symlinks for enablement and aliasing
    This is a fairly noticable change, but I think it needs to be done.
    So far we'd create an absolute symlink to the target unit file:
      .wants/foo.service → /usr/lib/systemd/system/foo.service
    or
      alias.service → /etc/systemd/system/aliased.service.
    
    This works reasonably well, except in one case: where the unit file
    is linked. When we look at a file link, the name of the physical file
    isn't used, and we only take the account the symlink source name.
    (In fact, the destination filename may not even be a well-formed unit name,
    so we couldn't use it, even if we wanted to.) But this means that if
    a file is linked, and specifies aliases, we'd create absolute links for
    those aliases, and systemd would consider each "alias" to be a separate
    unit. This isn't checked by the tests here, because we don't have a running
    systemd instance, but it is easy enough to check manually.
    
    The most reasonable way to fix this is to create relative links to the
    unit file:
      .wants/foo.service → ../foo.service
      alias.service → aliased.service.
    
    I opted to use no prefix for aliases, both normal and 'default.target',
    and to add "../" for .wants/ and .requires/. Note that the link that is
    created doesn't necessarily point to the file. E.g. if we're enabling
    a file under /usr/lib/systemd/system, and create a symlink in /etc/systemd/system,
    it'll still be "../foo.service", not "../../usr/lib/systemd/system/foo.service".
    For our unit loading logic this doesn't matter, and figuring out a path
    that actually leads somewhere would be more work. Since the user is allowed
    to move the unit file, or add a new unit file in a different location, and
    we don't actually follow the symlink, I think it's OK to create a dangling
    symlink. The prefix of "../" is useful to give a hint that the link points
    to files that are conceptually "one level up" in the directory hierarchy.
    
    With the relative symlinks, systemd knows that those are aliases.
    
    The tests are adjusted to use the new forms. There were a few tests that
    weren't really testing something useful: 'test -e x' fails if 'x' is a
    a dangling symlink. Absolute links in the chroot would be dangling, even
    though the target existed in the expected path, but become non-dangling
    when made relative and the test fails.
    
    This should be described in NEWS, but I'm not adding that here, because
    it'd likely result in conflicts.

--- src/shared/install.c
+++ src/shared/install.c
@@ -1837,7 +1837,7 @@ static int install_info_symlink_alias(
                 if (!alias_path)
                         return -ENOMEM;
 
-                q = create_symlink(lp, info->path, alias_path, force, changes, n_changes);
+                q = create_symlink(lp, info->name, alias_path, force, changes, n_changes);
                 r = r < 0 ? r : q;
         }
 
@@ -1906,7 +1906,7 @@ static int install_info_symlink_wants(
         }
 
         STRV_FOREACH(s, list) {
-                _cleanup_free_ char *path = NULL, *dst = NULL;
+                _cleanup_free_ char *dst = NULL;
 
                 q = install_name_printf(scope, info, *s, info->root, &dst);
                 if (q < 0) {
@@ -1936,11 +1936,15 @@ static int install_info_symlink_wants(
                         continue;
                 }
 
-                path = strjoin(config_path, "/", dst, suffix, n);
+                _cleanup_free_ char *path = strjoin(config_path, "/", dst, suffix, n);
                 if (!path)
                         return -ENOMEM;
 
-                q = create_symlink(lp, info->path, path, true, changes, n_changes);
+                _cleanup_free_ char *target = strjoin("../", info->name);
+                if (!target)
+                        return -ENOMEM;
+
+                q = create_symlink(lp, target, path, true, changes, n_changes);
                 if (r == 0)
                         r = q;
 
@@ -2848,7 +2852,7 @@ int unit_file_set_default(
                 return r;
 
         new_path = strjoina(lp.persistent_config, "/" SPECIAL_DEFAULT_TARGET);
-        return create_symlink(&lp, info->path, new_path, flags & UNIT_FILE_FORCE, changes, n_changes);
+        return create_symlink(&lp, info->name, new_path, flags & UNIT_FILE_FORCE, changes, n_changes);
 }
 
 int unit_file_get_default(
