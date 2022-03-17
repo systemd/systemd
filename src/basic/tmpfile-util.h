261a18322eb46242e1ef2b75ee188652648124fa
Lennart Poettering <lennart@poettering.net>
2022-03-17 18:18:04 +0100

    bootctl: generalize open_tmpfile_linkable() use a bit
    We want FILE* here, instead of a plain fd. Let's generalize this in
    tmpfile-util.c, so we can reuse it later easily.

--- src/basic/tmpfile-util.h
+++ src/basic/tmpfile-util.h
@@ -13,7 +13,9 @@ int tempfn_random_child(const char *p, const char *extra, char **ret);
 
 int open_tmpfile_unlinkable(const char *directory, int flags);
 int open_tmpfile_linkable(const char *target, int flags, char **ret_path);
+int fopen_tmpfile_linkable(const char *target, int flags, char **ret_path, FILE **ret_file);
 
 int link_tmpfile(int fd, const char *path, const char *target);
+int flink_tmpfile(FILE *f, const char *path, const char *target);
 
 int mkdtemp_malloc(const char *template, char **ret);
