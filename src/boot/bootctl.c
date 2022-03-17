1c2b6177031f665f2117b8683a5d1a067b05ad7d
Lennart Poettering <lennart@poettering.net>
2022-03-17 18:44:14 +0100

    bootctl: fix typo

--- src/boot/bootctl.c
+++ src/boot/bootctl.c
@@ -2044,7 +2044,7 @@ static int verb_remove(int argc, char *argv[], void *userdata) {
 
         q = remove_entry_directory(arg_esp_path);
         if (q < 0 && r >= 0)
-                r = 1;
+                r = q;
 
         if (arg_xbootldr_path) {
                 /* Remove the latter two also in the XBOOTLDR partition if it exists */
