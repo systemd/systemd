7db3b4824c5d6b03ccb18fe62fd7e0f2c92a6272
Yu Watanabe <watanabe.yu+github@gmail.com>
2022-03-22 22:01:08 +0900

    network: do not enable IPv4 ACD for IPv4 link-local address if ACD is disabled explicitly
    The commit 1cf4ed142d6c1e2b9dc6a0bc74b6a83ae30b0f8e makes the IPv4 ACD
    enabled unconditionally for IPv4 link-local addresses even if users
    explicitly disable ACD.
    
    This makes the IPv4 ACD is enabled by default, but honor user setting.
    
    Fixes #22763.

--- src/network/networkd-address.h
+++ src/network/networkd-address.h
@@ -53,6 +53,9 @@ struct Address {
 
         bool scope_set:1;
         bool ip_masquerade_done:1;
+
+        /* duplicate_address_detection is only used by static or IPv4 dynamic addresses.
+         * To control DAD for IPv6 dynamic addresses, set IFA_F_NODAD to flags. */
         AddressFamily duplicate_address_detection;
         sd_ipv4acd *acd;
 
