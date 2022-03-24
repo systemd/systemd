fc52249d456d59cce7b73d6eab24258bbf23facf
наб <nabijaczleweli@nabijaczleweli.xyz>
2022-03-24 17:15:39 +0100

    Use new default-user-shell option instead of hard-coding bash in nspawn and user-record
    Defaults to /bin/bash, no changes in the default configuration
    
    The fallback shell for non-root users is as-specified,
    and the interactive shell for nspawn sessions is started as
      exec(default-user-shell, "-" + basename(default-user-shell), ...)

--- src/nspawn/nspawn.c
+++ src/nspawn/nspawn.c
@@ -3550,10 +3550,10 @@ static int inner_child(
                         /* If we cannot change the directory, we'll end up in /, that is expected. */
                         (void) chdir(home ?: "/root");
 
-                execle("/bin/bash", "-bash", NULL, env_use);
+                execle(DEFAULT_USER_SHELL, "-" DEFAULT_USER_SHELL_NAME, NULL, env_use);
                 execle("/bin/sh", "-sh", NULL, env_use);
 
-                exec_target = "/bin/bash, /bin/sh";
+                exec_target = DEFAULT_USER_SHELL ", /bin/sh";
         }
 
         return log_error_errno(errno, "execv(%s) failed: %m", exec_target);
