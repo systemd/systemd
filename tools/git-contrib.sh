e0a5fc7267d3ed3b8792019761d761246bb0ec81
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2022-03-29 19:42:01 +0200

    tools/git-contrib: list contributions not only from Weblate
    It seems that --invert-grep used to affect --author, but now it doesn't (with
    git-2.35.1-1.fc36.x86_64), so effectively we would only show the one entry that
    was supposed to be filtered out.

--- tools/git-contrib.sh
+++ tools/git-contrib.sh
@@ -3,7 +3,8 @@
 set -eu
 
 tag="$(git describe --abbrev=0 --match 'v[0-9][0-9][0-9]')"
-git log --pretty=tformat:%aN --author=noreply@weblate.org --invert-grep -s "${tag}.." | \
+git log --pretty=tformat:%aN -s "${tag}.." |
+    grep -v noreply@weblate.org |
     sed 's/ / /g; s/--/-/g; s/.*/\0,/' |
-    sort -u | tr '\n' ' ' | sed -e "s/^/Contributions from: /g" -e "s/,\s*$/\n/g" | fold -w 72 -s | \
+    sort -u | tr '\n' ' ' | sed -e "s/^/Contributions from: /g" -e "s/,\s*$/\n/g" | fold -w 72 -s |
     sed -e "s/^/        /g" -e "s/\s*$//g"
