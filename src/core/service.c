f07f1f4c581913234dba7d14e0bb460d26862123
Michal Koutný <mkoutny@suse.com>
2022-03-30 18:39:47 +0200

    service: Implement common cgroup suffix with Delegate=yes
    This makes both control and payload commands nested into a subcgroup
    that is configurable per-unit. Thanks to this users can specify
    subcgroup where systemd's control commands are placed into and resources
    can be allocated for it in advance.
    
    The default remains backwards compatible with the implicit .control
    subcgroup for control commands.
    
    ExecStartPre= commands will run in bare cgroup even with configured
    suffix (they don't compete with anything).

--- src/core/service.c
+++ src/core/service.c
@@ -575,6 +575,14 @@ static int service_verify(Service *s) {
                                 return log_unit_error_errno(UNIT(s), SYNTHETIC_ERRNO(ENOEXEC),
                                                             "Service has an empty argv in %s=. Refusing.",
                                                             service_exec_command_to_string(c));
+                        /* XXX We call cg_set_access on one level, so reject more levels that can be
+                         * configured. */
+                        if (c == SERVICE_EXEC_START &&
+                            s->cgroup_context.delegate && s->cgroup_context.delegate_suffix &&
+                            s->exec_context.user &&
+                            strchr(s->cgroup_context.delegate_suffix, '/'))
+                                return log_unit_error_errno(UNIT(s), SYNTHETIC_ERRNO(ENOEXEC),
+                                                            "DelegateControlGroupSuffix= is too deep. Refusing");
                 }
 
         if (!s->exec_command[SERVICE_EXEC_START] && !s->exec_command[SERVICE_EXEC_STOP] &&
@@ -2235,7 +2243,7 @@ static void service_enter_start(Service *s) {
         r = service_spawn(s,
                           c,
                           timeout,
-                          EXEC_PASS_FDS|EXEC_APPLY_SANDBOXING|EXEC_APPLY_CHROOT|EXEC_APPLY_TTY_STDIN|EXEC_SET_WATCHDOG|EXEC_WRITE_CREDENTIALS|EXEC_SETENV_MONITOR_RESULT,
+                          EXEC_PASS_FDS|EXEC_APPLY_SANDBOXING|EXEC_APPLY_CHROOT|EXEC_APPLY_TTY_STDIN|EXEC_SET_WATCHDOG|EXEC_WRITE_CREDENTIALS|EXEC_SETENV_MONITOR_RESULT|EXEC_CONTROL_CGROUP,
                           &pid);
         if (r < 0)
                 goto fail;
@@ -2500,7 +2508,7 @@ static void service_run_next_main(Service *s) {
         r = service_spawn(s,
                           s->main_command,
                           s->timeout_start_usec,
-                          EXEC_PASS_FDS|EXEC_APPLY_SANDBOXING|EXEC_APPLY_CHROOT|EXEC_APPLY_TTY_STDIN|EXEC_SET_WATCHDOG|EXEC_SETENV_MONITOR_RESULT,
+                          EXEC_PASS_FDS|EXEC_APPLY_SANDBOXING|EXEC_APPLY_CHROOT|EXEC_APPLY_TTY_STDIN|EXEC_SET_WATCHDOG|EXEC_SETENV_MONITOR_RESULT|EXEC_CONTROL_CGROUP,
                           &pid);
         if (r < 0)
                 goto fail;
