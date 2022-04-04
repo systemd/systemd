systemd/systemd

commit 9202b567bcdd0c1f6a1fc2a5f36602e619960813

network: do not ignore critical errors like OOM

src/network/networkd-network.c
@@ -535,14 +535,17 @@ int network_load_one(Manager *manager, OrderedHashmap **networks, const char *fi
 
         r = network_add_ipv4ll_route(network);
         if (r < 0)
-                log_warning_errno(r, "%s: Failed to add IPv4LL route, ignoring: %m", network->filename);
+                return log_warning_errno(r, "%s: Failed to add IPv4LL route: %m", network->filename);
 
         r = network_add_default_route_on_device(network);
         if (r < 0)
-                log_warning_errno(r, "%s: Failed to add default route on device, ignoring: %m",
-                                  network->filename);
+                return log_warning_errno(r, "%s: Failed to add default route on device: %m",
+                                         network->filename);
 
-        if (network_verify(network) < 0)
+        r = network_verify(network);
+        if (r == -ENOMEM)
+                return r;
+        if (r < 0)
                 /* Ignore .network files that do not match the conditions. */
                 return 0;
 
@@ -570,7 +573,7 @@ int network_load(Manager *manager, OrderedHashmap **networks) {
         STRV_FOREACH(f, files) {
                 r = network_load_one(manager, networks, *f);
                 if (r < 0)
-                        log_error_errno(r, "Failed to load %s, ignoring: %m", *f);
+                        return log_error_errno(r, "Failed to load %s: %m", *f);
         }
 
         return 0;

commit c3a2a8423bdff53d8bf8cd50ae3ac9b0f53ec038

network: ignore missing permission for .network or .netdev files
Fixes a bug introduced by 9202b567bcdd0c1f6a1fc2a5f36602e619960813.

Fixes #22954.

src/network/netdev/netdev.c
@@ -873,8 +873,8 @@ int netdev_load(Manager *manager, bool reload) {
 
         STRV_FOREACH(f, files) {
                 r = netdev_load_one(manager, *f);
-                if (r < 0)
-                        log_error_errno(r, "Failed to load %s, ignoring: %m", *f);
+                if (r < 0 && !ERRNO_IS_PRIVILEGE(r))
+                        return log_error_errno(r, "Failed to load %s: %m", *f);
         }
 
         return 0;

src/network/networkd-network.c
@@ -592,7 +592,7 @@ int network_load(Manager *manager, OrderedHashmap **networks) {
 
         STRV_FOREACH(f, files) {
                 r = network_load_one(manager, networks, *f);
-                if (r < 0)
+                if (r < 0 && !ERRNO_IS_PRIVILEGE(r))
                         return log_error_errno(r, "Failed to load %s: %m", *f);
         }
 