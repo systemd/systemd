cfdd74051e22eb0ffdf50713568fc97398ba4ca4
Daan De Meyer <daan.j.demeyer@gmail.com>
2022-04-05 19:06:48 +0200

    journal: Store entry timestamps as 32-bit offsets to the head entry
    Reduces the size of the entry object by 8 bytes. To make this work,
    we initialize the header head entry timestamps a little earlier so
    that the entry timestamps are always calculated correctly. A side
    effect is that if we fail to append the head entry, the head entry
    timestamps will be "wrong" but the same already happens for the
    head entry seqnum so this doesn't seem like a big issue.

--- src/journal/journald-server.c
+++ src/journal/journald-server.c
@@ -809,6 +809,10 @@ static bool shall_try_append_again(JournalFile *f, int r) {
                 log_warning("%s: Journal file is from the future, rotating.", f->path);
                 return true;
 
+        case -ETIME:
+                log_warning("%s: Journal entry is too old/new, rotating.", f->path);
+                return true;
+
         case -EAFNOSUPPORT:
                 log_warning("%s: underlying file system does not support memory mapping or another required file system feature.", f->path);
                 return false;
