10b9ea32fb2776b1267e635c0d0b69512f20aa3f
Yu Watanabe <watanabe.yu+github@gmail.com>
2022-04-06 01:08:35 +0900

    hwdb: fix parsing options
    Fixes #22976.

--- src/hwdb/hwdb.c
+++ src/hwdb/hwdb.c
@@ -73,7 +73,7 @@ static int parse_argv(int argc, char *argv[]) {
         assert(argc >= 0);
         assert(argv);
 
-        while ((c = getopt_long(argc, argv, "ust:r:h", options, NULL)) >= 0)
+        while ((c = getopt_long(argc, argv, "sr:h", options, NULL)) >= 0)
                 switch (c) {
 
                 case 'h':
