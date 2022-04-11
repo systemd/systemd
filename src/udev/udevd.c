1055172804e660df2e1c498dafa998ffd65e0c88
Yu Watanabe <watanabe.yu+github@gmail.com>
2022-04-11 21:52:49 +0900

    udev: cleanup stack directory /run/udev/links when all workers exited
    By the previous commit, the stack directories are not removed even if
    it is empty. To reduce the inode usage of /run, let's cleanup the
    directories.

--- src/udev/udevd.c
+++ src/udev/udevd.c
@@ -65,6 +65,7 @@
 #include "udev-builtin.h"
 #include "udev-ctrl.h"
 #include "udev-event.h"
+#include "udev-node.h"
 #include "udev-util.h"
 #include "udev-watch.h"
 #include "user-util.h"
@@ -111,6 +112,7 @@ typedef struct Manager {
 
         usec_t last_usec;
 
+        bool udev_node_needs_cleanup;
         bool stop_exec_queue;
         bool exit;
 } Manager;
@@ -976,6 +978,9 @@ static int event_queue_start(Manager *manager) {
         if (!manager->events || manager->exit || manager->stop_exec_queue)
                 return 0;
 
+        /* To make the stack directory /run/udev/links cleaned up later. */
+        manager->udev_node_needs_cleanup = true;
+
         r = event_source_disable(manager->kill_workers_event);
         if (r < 0)
                 log_warning_errno(r, "Failed to disable event source for cleaning up idle workers, ignoring: %m");
@@ -1606,6 +1611,11 @@ static int on_post(sd_event_source *s, void *userdata) {
         }
 
         /* There are no idle workers. */
+
+        if (manager->udev_node_needs_cleanup) {
+                (void) udev_node_cleanup();
+                manager->udev_node_needs_cleanup = false;
+        }
 
         if (manager->exit)
                 return sd_event_exit(manager->event, 0);
