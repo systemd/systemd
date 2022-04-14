fb79b78f52cf59462f37aa3d6b0de13f5a255882
Yu Watanabe <watanabe.yu+github@gmail.com>
2022-04-15 06:31:21 +0900

    udev-watch: resolve race in saving inotify watch handle
    Unfortunately, the previous solution did not solve the race completely.
    The root of the race is multiple workers access may a same symlink from
    the watch handle to device ID. E.g.
    1. worker A removes a watch handle,
    2. worker B reuse the watch handle, and save it to a symlink,
    3. worker A removes the symlink.
    
    By this commit, the symlink from watch handle to device ID will never
    be created. But, only symlink of opposite direction, i.e. from device ID
    to watch handle is created. As only one worker accesses a symlink
    simultaneously, now the logic should be completely race-free, and we can
    avoid the try-and-wait loop.
    The change makes `device_new_from_watch_handle()` slightly complecated,
    but it always returns the correct device if it succeeds.

--- src/udev/udev-watch.h
+++ src/udev/udev-watch.h
@@ -3,6 +3,8 @@
 
 #include "sd-device.h"
 
+int device_new_from_watch_handle(sd_device **ret, int wd);
+
 int udev_watch_restore(int inotify_fd);
 int udev_watch_begin(int inotify_fd, sd_device *dev);
 int udev_watch_end(int inotify_fd, sd_device *dev);
