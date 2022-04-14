485e7848600a9b5ba6b625818f2f944d12d4357b
Yu Watanabe <watanabe.yu+github@gmail.com>
2022-04-15 06:38:33 +0900

    sd-device: move device_new_from_watch_handle_at() to udev-watch.c
    And drop unused watch handle related functions.

--- src/udev/udev-watch.h
+++ src/udev/udev-watch.h
@@ -3,6 +3,11 @@
 
 #include "sd-device.h"
 
+int device_new_from_watch_handle_at(sd_device **ret, int dirfd, int wd);
+static inline int device_new_from_watch_handle(sd_device **ret, int wd) {
+        return device_new_from_watch_handle_at(ret, -1, wd);
+}
+
 int udev_watch_restore(int inotify_fd);
 int udev_watch_begin(int inotify_fd, sd_device *dev);
 int udev_watch_end(int inotify_fd, sd_device *dev);
