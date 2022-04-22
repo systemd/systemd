d69275bd54dabc973d3cd3d774e5f77f02691a8e
Lennart Poettering <lennart@poettering.net>
2022-04-22 21:44:26 +0200

    test: test new credential features

--- test/TEST-54-CREDS/test.sh
+++ test/TEST-54-CREDS/test.sh
@@ -3,9 +3,16 @@
 set -e
 
 TEST_DESCRIPTION="test credentials"
-NSPAWN_ARGUMENTS="--set-credential=mynspawncredential:strangevalue"
+NSPAWN_ARGUMENTS="${NSPAWN_ARGUMENTS:-} --set-credential=mynspawncredential:strangevalue"
+QEMU_OPTIONS="${QEMU_OPTIONS:-} -fw_cfg  name=opt/io.systemd.credentials/myqemucredential,string=othervalue"
+KERNEL_APPEND="${KERNEL_APPEND:-} systemd.set_credential=kernelcmdlinecred:uff rd.systemd.import_credentials=no"
 
 # shellcheck source=test/test-functions
 . "${TEST_BASE_DIR:?}/test-functions"
+
+test_append_files() {
+    instmods qemu_fw_cfg
+    generate_module_dependencies
+}
 
 do_test "$@"
