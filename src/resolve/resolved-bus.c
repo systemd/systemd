1fa8157143779f49a7e7ce64657a8e70b6ea95d0
Suraj Krishnan <72937403+surajkrishnan14@users.noreply.github.com>
2022-04-26 17:09:02 -0500

    Implement DNS notifications from resolved via varlink
    * The new varlink interface exposes a method to subscribe to DNS
    resolutions on the system. The socket permissions are open for owner and
    group only.
    * Notifications are sent to subscriber(s), if any, after successful
    resolution of A and AAAA records.
    
    This feature could be used by applications for auditing/logging services
    downstream of the resolver. It could also be used to asynchronously
    update the firewall. For example, a system that has a tightly configured
    firewall could open up connections selectively to known good hosts based
    on a known allow-list of hostnames. Of course, updating the firewall
    asynchronously will require other design considerations (such as
    queueing packets in the user space while a verdict is made).
    
    See also:
    https://lists.freedesktop.org/archives/systemd-devel/2022-August/048202.html
    https://lists.freedesktop.org/archives/systemd-devel/2022-February/047441.html

--- src/resolve/resolved-bus.c
+++ src/resolve/resolved-bus.c
@@ -467,7 +467,7 @@ static int bus_method_resolve_hostname(sd_bus_message *message, void *userdata, 
         _cleanup_(dns_question_unrefp) DnsQuestion *question_idna = NULL, *question_utf8 = NULL;
         _cleanup_(dns_query_freep) DnsQuery *q = NULL;
         Manager *m = userdata;
-        const char *hostname;
+        char *hostname;
         int family, ifindex;
         uint64_t flags;
         int r;
@@ -517,6 +517,9 @@ static int bus_method_resolve_hostname(sd_bus_message *message, void *userdata, 
 
         q->bus_request = sd_bus_message_ref(message);
         q->request_family = family;
+        q->request_name = strdup(hostname);
+        if (!q->request_name)
+                return log_oom();
         q->complete = bus_method_resolve_hostname_complete;
 
         r = dns_query_bus_track(q, message);
@@ -839,6 +842,9 @@ static int bus_method_resolve_record(sd_bus_message *message, void *userdata, sd
 
         q->bus_request = sd_bus_message_ref(message);
         q->complete = bus_method_resolve_record_complete;
+        q->request_name = strdup(name);
+        if (!q->request_name)
+                return log_oom();
 
         r = dns_query_bus_track(q, message);
         if (r < 0)
@@ -1196,6 +1202,9 @@ static int resolve_service_hostname(DnsQuery *q, DnsResourceRecord *rr, int ifin
                 return r;
 
         aux->request_family = q->request_family;
+        aux->request_name = strdup(rr->srv.name);
+        if (!aux->request_name)
+                return log_oom();
         aux->complete = resolve_service_hostname_complete;
 
         r = dns_query_make_auxiliary(aux, q);
@@ -1710,6 +1719,7 @@ static int bus_property_get_ntas(
 
 static BUS_DEFINE_PROPERTY_GET_ENUM(bus_property_get_dns_stub_listener_mode, dns_stub_listener_mode, DnsStubListenerMode);
 static BUS_DEFINE_PROPERTY_GET(bus_property_get_dnssec_supported, "b", Manager, manager_dnssec_supported);
+static BUS_DEFINE_PROPERTY_GET(bus_property_get_monitor_enabled, "b", Manager, manager_monitor_is_enabled);
 static BUS_DEFINE_PROPERTY_GET2(bus_property_get_dnssec_mode, "s", Manager, manager_get_dnssec_mode, dnssec_mode_to_string);
 static BUS_DEFINE_PROPERTY_GET2(bus_property_get_dns_over_tls_mode, "s", Manager, manager_get_dns_over_tls_mode, dns_over_tls_mode_to_string);
 
@@ -2115,6 +2125,7 @@ static const sd_bus_vtable resolve_vtable[] = {
         SD_BUS_PROPERTY("DNSSECNegativeTrustAnchors", "as", bus_property_get_ntas, 0, 0),
         SD_BUS_PROPERTY("DNSStubListener", "s", bus_property_get_dns_stub_listener_mode, offsetof(Manager, dns_stub_listener_mode), 0),
         SD_BUS_PROPERTY("ResolvConfMode", "s", bus_property_get_resolv_conf_mode, 0, 0),
+        SD_BUS_PROPERTY("Monitor", "b", bus_property_get_monitor_enabled, 0, 0),
 
         SD_BUS_METHOD_WITH_ARGS("ResolveHostname",
                                 SD_BUS_ARGS("i", ifindex, "s", name, "i", family, "t", flags),
