93cdc6a9d385cecc2f29f20297fa428b102ba4ee
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2022-04-26 22:08:02 +0200

    oomd: "descendent" → "descendant"
    The latter is the common spelling apparently.

--- src/oom/oomd-manager.c
+++ src/oom/oomd-manager.c
@@ -180,13 +180,13 @@ finish:
         return r;
 }
 
-/* Fill `new_h` with `path`'s descendent OomdCGroupContexts. Only include descendent cgroups that are possible
+/* Fill 'new_h' with 'path's descendant OomdCGroupContexts. Only include descendant cgroups that are possible
  * candidates for action. That is, only leaf cgroups or cgroups with memory.oom.group set to "1".
  *
- * This function ignores most errors in order to handle cgroups that may have been cleaned up while populating
- * the hashmap.
+ * This function ignores most errors in order to handle cgroups that may have been cleaned up while
+ * populating the hashmap.
  *
- * `new_h` is of the form { key: cgroup paths -> value: OomdCGroupContext } */
+ * 'new_h' is of the form { key: cgroup paths -> value: OomdCGroupContext } */
 static int recursively_get_cgroup_context(Hashmap *new_h, const char *path) {
         _cleanup_free_ char *subpath = NULL;
         _cleanup_closedir_ DIR *d = NULL;
