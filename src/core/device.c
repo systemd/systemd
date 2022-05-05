e6b22ab332c96f0e1a1ad4686dcf2f8801f01a8e
Franck Bui <fbui@suse.com>
2022-05-05 09:47:27 +0200

    core: forget (almost) all devices during initrd transition
    This is a slightly different approach than the one taken by commit
    75d7b5989f99125e52d5c0e5656fa1cd0fae2405 to fix issue #12953 and #23208.
    
    This patch forces PID1 to forget all devices (except those with the
    "db_persist" option see below) that were known by PID1 before switching root by
    pretending that the devices were in DEAD state before being serialized. Hence
    no artificial "plugged->dead" state transitions happen when PID1 is reexecuting
    from a switch root followed by "dead->plugged" state transitions when all
    devices are coldplugged with the new set of udev rule from the host.
    
    As mentioned previously, devices with the "db_persistent" option are exceptions
    of the previously described mechanism. Since these devices remain in the udev
    DB even after the DB has been cleared, they still continue to be deserialized
    in plugged state and remain in this state hence following the description of
    the option. This should fix the regression introduced by
    75d7b5989f99125e52d5c0e5656fa1cd0fae2405.
    
    Fixes: #23429
    Replaces: #23218

--- src/core/device.c
+++ src/core/device.c
@@ -158,59 +158,38 @@ static void device_set_state(Device *d, DeviceState state) {
 
 static int device_coldplug(Unit *u) {
         Device *d = DEVICE(u);
+        DeviceState state;
+        DeviceFound found;
 
         assert(d);
         assert(d->state == DEVICE_DEAD);
 
         /* First, let's put the deserialized state and found mask into effect, if we have it. */
+
+        /* If the device wasn't serialized then it appeared during the reexec/reload process. Keep
+         * DEAD state and let device_catchup() set it in its final state. */
         if (d->deserialized_state < 0)
                 return 0;
 
-        Manager *m = u->manager;
-        DeviceFound found = d->deserialized_found;
-        DeviceState state = d->deserialized_state;
+        found = d->deserialized_found;
+        state = d->deserialized_state;
 
-        /* On initial boot, switch-root, reload, reexecute, the following happen:
-         * 1. MANAGER_IS_RUNNING() == false
-         * 2. enumerate devices: manager_enumerate() -> device_enumerate()
-         *    Device.enumerated_found is set.
-         * 3. deserialize devices: manager_deserialize() -> device_deserialize()
-         *    Device.deserialize_state and Device.deserialized_found are set.
-         * 4. coldplug devices: manager_coldplug() -> device_coldplug()
-         *    deserialized properties are copied to the main properties.
-         * 5. MANAGER_IS_RUNNING() == true: manager_ready()
-         * 6. catchup devices: manager_catchup() -> device_catchup()
-         *    Device.enumerated_found is applied to Device.found, and state is updated based on that.
-         *
-         * Notes:
-         * - On initial boot, no udev database exists. Hence, no devices are enumerated in the step 2.
-         *   Also, there is no deserialized device. Device units are (a) generated based on dependencies of
-         *   other units, or (b) generated when uevents are received.
-         *
-         * - On switch-root, the udev database may be cleared, except for devices with sticky bit, i.e.
-         *   OPTIONS="db_persist". Hence, almost no devices are enumerated in the step 2. However, in general,
-         *   we have several serialized devices. So, DEVICE_FOUND_UDEV bit in the deserialized_found must be
-         *   ignored, as udev rules in initramfs and the main system are often different. If the deserialized
-         *   state is DEVICE_PLUGGED, we need to downgrade it to DEVICE_TENTATIVE (or DEVICE_DEAD if nobody
-         *   sees the device). Unlike the other starting mode, Manager.honor_device_enumeration == false
-         *   (maybe, it is better to rename the flag) when device_coldplug() and device_catchup() are called.
-         *   Hence, let's conditionalize the operations by using the flag. After switch-root, systemd-udevd
-         *   will (re-)process all devices, and the Device.found and Device.state will be adjusted.
-         *
-         * - On reload or reexecute, we can trust enumerated_found, deserialized_found, and deserialized_state.
-         *   Of course, deserialized parameters may be outdated, but the unit state can be adjusted later by
-         *   device_catchup() or uevents. */
+        if (!FLAGS_SET(d->enumerated_found, DEVICE_FOUND_UDEV) &&
+            MANAGER_IS_SWITCHING_ROOT(u->manager)) {
 
-        if (!m->honor_device_enumeration && !MANAGER_IS_USER(m)) {
-                found &= ~DEVICE_FOUND_UDEV; /* ignore DEVICE_FOUND_UDEV bit */
-                if (state == DEVICE_PLUGGED)
-                        state = DEVICE_TENTATIVE; /* downgrade state */
-                if (found == DEVICE_NOT_FOUND)
-                        state = DEVICE_DEAD; /* If nobody sees the device, downgrade more */
+                /* The device is no more present in udev DB. When this happens (due to PID1
+                 * switching root), udev is inactive and its DB has been cleared, hence we can't
+                 * trust the enumeration step anymore until udev DB is repopulated again. Until
+                 * this mechanism is used during initrd transitions, we have no other choice than
+                 * forgetting all devices and relying on a full retriggering of events (based on
+                 * the host set of udev rules this time). Hence we avoid generating wrong device
+                 * state transitions by not assuming wrongly that devices missing in udev DB were
+                 * unplugged. The downside is that PID1 won't generate plugged->dead transitions
+                 * for devices that were unplugged during the initrd transition. */
+
+                found = DEVICE_NOT_FOUND;
+                state = DEVICE_DEAD;
         }
-
-        if (d->found == found && d->state == state)
-                return 0;
 
         d->found = found;
         device_set_state(d, state);
