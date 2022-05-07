7d4c51e7e3eb2b6a6bb74b55f5feda227220789e
Daan De Meyer <daan.j.demeyer@gmail.com>
2022-05-07 14:19:40 +0200

    core/device: Use DEVICE_NOT_FOUND instead of 0.
    Use the enum name instead of the value zero. This is a noop change
    as DEVICE_NOT_FOUND == 0.

--- src/core/device.c
+++ src/core/device.c
@@ -924,7 +924,7 @@ static int device_dispatch_io(sd_device_monitor *monitor, sd_device *dev, void *
 
                 /* If we get notified that a device was removed by udev, then it's completely gone, hence
                  * unset all found bits */
-                device_update_found_by_sysfs(m, sysfs, 0, DEVICE_FOUND_MASK);
+                device_update_found_by_sysfs(m, sysfs, DEVICE_NOT_FOUND, DEVICE_FOUND_MASK);
 
         } else if (device_is_ready(dev)) {
 
@@ -941,7 +941,7 @@ static int device_dispatch_io(sd_device_monitor *monitor, sd_device *dev, void *
         } else
                 /* The device is nominally around, but not ready for us. Hence unset the udev bit, but leave
                  * the rest around. */
-                device_update_found_by_sysfs(m, sysfs, 0, DEVICE_FOUND_UDEV);
+                device_update_found_by_sysfs(m, sysfs, DEVICE_NOT_FOUND, DEVICE_FOUND_UDEV);
 
         return 0;
 }
