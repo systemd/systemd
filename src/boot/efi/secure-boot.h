4f344a18cb434df8a9c5a95be1352d017d2f99e3
Vincent Dagonneau <v@vda.io>
2022-05-09 14:13:28 -0400

    This patch adds support for enrolling secure boot boot keys from sd-boot.
    ***DANGER*** NOTE ***DANGER***
    
    This feature might result in your device becoming soft-brick as outlined
    below, please use this feature carefully.
    
    ***DANGER*** NOTE ***DANGER***
    
    If secure-boot-enrollment is set to no, then no action whatsoever is performed,
    no matter the files on the ESP.
    
    If secure boot keys are found under $ESP/loader/keys and secure-boot-enrollment
    is set to either manual or force then sd-boot will generate enrollment entries
    named after the directories they are in. The entries are shown at the very bottom
    of the list and can be selected by the user from the menu. If the user selects it,
    the user is shown a screen allowing for cancellation before a timeout. The enrollment
    proceeds if the action is not cancelled after the timeout.
    
    Additionally, if the secure-boot-enroll option is set to 'force' then the keys
    located in the directory named 'auto' are going to be enrolled automatically. The user
    is still going to be shown a screen allowing them to cancel the action if they want to,
    however the enrollment will proceed automatically after a timeout without
    user cancellation.
    
    After keys are enrolled, the system reboots with secure boot enabled therefore, it is
    ***critical*** to ensure that everything needed for the system to boot is signed
    properly (sd-boot itself, kernel, initramfs, PCI option ROMs).
    
    This feature currently only allows loading the most simple set of variables: PK, KEK
    and db.
    
    The files need to be prepared with cert-to-efi-sig-list and then signed with
    sign-efi-sig-list.
    
    Here is a short example to generate your own keys and the right files for
    auto-enrollement.
    
    `
    keys="PK KEK DB"
    uuid="{$(systemd-id128 new -u)}"
    for key in ${keys}; do
    	openssl req -new -x509 -subj "/CN=${key}/ -keyout "${key}.key" -out "${key}.crt"
    	openssl x509 -outform DER -in "${key}.crt" -out "${key}.cer"
    	cert-to-efi-sig-list -g "${uuid}" "${key}.crt" "${key}.esl.nosign"
    done
    
    sign-efi-sig-list -c PK.crt -k PK.key PK PK.esl.nosign PK.esl
    sign-efi-sig-list -c PK.crt -k PK.key KEK KEK.esl.nosign KEK.esl
    sign-efi-sig-list -c KEK.crt -k KEK.key db db.esl.nosign db.esl
    `
    
    Once these keys are enrolled, all the files needed for boot ***NEED*** to be signed in
    order to run. You can sign the binaries with the sbsign tool, for example:
    
    `
    sbsign --key db.key --cert db.crt bzImage --output $ESP/bzImage
    `
    
    Example:
    
    Assuming the system has been put in Setup Mode:
    
    `
    $ESP/loader/keys/auto/db.esl
    $ESP/loader/keys/auto/KEK.esl
    $ESP/loader/keys/auto/PK.esl
    $ESP/loader/keys/Linux Only/db.esl
    $ESP/loader/keys/Linux Only/KEK.esl
    $ESP/loader/keys/Linux Only/PK.esl
    $ESP/loader/keys/Linux and Windows/db.esl
    $ESP/loader/keys/Linux and Windows/KEK.esl
    $ESP/loader/keys/Linux and Windows/PK.esl
    `
    
    If auto-enroll is set, then the db, KEK and then PK are enrolled from the 'auto'
    directory.
    
    If not, three new boot entries are available to the user in order to enroll either the
    'Linux Only', 'Linux And Windows' or 'auto' set of keys.
    
    .

--- src/boot/efi/secure-boot.h
+++ src/boot/efi/secure-boot.h
@@ -2,7 +2,11 @@
 #pragma once
 
 #include <efi.h>
+
+#include "boot.h"
 #include "efivars-fundamental.h"
 
 bool secure_boot_enabled(void);
 SecureBootMode secure_boot_mode(void);
+
+EFI_STATUS secure_boot_enroll_at(EFI_FILE *root_dir, const char16_t *path);
