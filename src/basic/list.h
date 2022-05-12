3cb57f133ee648d72b7fd4d5caba624614c70bcd
Daan De Meyer <daan.j.demeyer@gmail.com>
2022-05-12 13:50:51 +0200

    basic: Add log context
    This commit adds support for attaching extra metadata to log
    messages written to the journal via log.h. We keep track of a
    thread local log context in log.c onto which we can push extra
    metadata fields that should be logged. Once a field is no longer
    relevant, it can be popped again from the log context.
    
    On top of this, we then add macros to allow pushing extra fields
    onto the log context.
    
    LOG_CONTEXT_PUSH() will push the provided field onto the log context
    and pop the last field from the log context when the current block
    ends. LOG_CONTEXT_PUSH_STRV() will do the same but for all fields in
    the given strv.
    
    Using the macros is as simple as putting them anywhere inside a block
    to add a field to all following log messages logged from inside that
    block.
    
    void myfunction(...) {
    	...
    
    	LOG_CONTEXT_PUSH("MYMETADATA=abc");
    
    	// Every journal message logged will now have the MYMETADATA=abc
            // field included.
    }
    One special case to note is async code, where we use callbacks that
    are invoked to continue processing when some event occurs. For async
    code, there's usually an associated "userdata" struct containing
    all the information associated with the async request. In this
    "userdata" struct, we can add an extra "log_fields" field which is
    a strv containing extra fields to be attached to log messages. At
    the start of each callback, we can then call
    "LOG_CONTEXT_PUSH_STRV(userdata->log_fields)" which makes sure the
    fields are attached for the duration of the callback.

--- src/basic/list.h
+++ src/basic/list.h
@@ -1,8 +1,6 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 #pragma once
 
-#include "macro.h"
-
 /* The head of the linked list. Use this in the structure that shall
  * contain the head of the linked list */
 #define LIST_HEAD(t,name)                                               \
