345607230562fa7abdcdcfec1ab2eb8bfbcf057b
Yu Watanabe <watanabe.yu+github@gmail.com>
2022-05-13 20:48:18 +0900

    network: use link_is_rady_to_configure() for configuring DHCP client or friends
    Otherwise, DHCP client or friends may started before link-layer properties,
    e.g. MAC address, being configured or the link being activated.

--- src/network/networkd-dhcp4.c
+++ src/network/networkd-dhcp4.c
@@ -1584,7 +1584,7 @@ static int dhcp4_process_request(Request *req, Link *link, void *userdata) {
 
         assert(link);
 
-        if (!IN_SET(link->state, LINK_STATE_CONFIGURING, LINK_STATE_CONFIGURED))
+        if (!link_is_ready_to_configure(link, /* allow_unmanaged = */ false))
                 return 0;
 
         if (!IN_SET(link->hw_addr.length, ETH_ALEN, INFINIBAND_ALEN) ||
