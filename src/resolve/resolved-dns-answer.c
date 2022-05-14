6530ca0d2b0255eec8e5620555f50961d11759eb
Yu Watanabe <watanabe.yu+github@gmail.com>
2022-05-14 15:09:11 +0900

    resolve: fix memleak
    Fixes a bug introduced by 71aee23dba7faeef68e7232f444626267a6c90d7.
    
    Fixes CID#1488763.

--- src/resolve/resolved-dns-answer.c
+++ src/resolve/resolved-dns-answer.c
@@ -69,7 +69,7 @@ static int dns_answer_reserve_internal(DnsAnswer *a, size_t n) {
 
 DnsAnswer *dns_answer_new(size_t n) {
         _cleanup_ordered_set_free_ OrderedSet *s = NULL;
-        DnsAnswer *a;
+        _cleanup_(dns_answer_unrefp) DnsAnswer *a = NULL;
 
         if (n > UINT16_MAX)
                 n = UINT16_MAX;
@@ -90,7 +90,7 @@ DnsAnswer *dns_answer_new(size_t n) {
         if (dns_answer_reserve_internal(a, n) < 0)
                 return NULL;
 
-        return a;
+        return TAKE_PTR(a);
 }
 
 static DnsAnswer *dns_answer_free(DnsAnswer *a) {
