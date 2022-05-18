182ab5501b38839215950870cc25c8187b77ab45
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2022-05-18 10:40:54 +0200

    test-compare-versions: basic test for systemd-analyze compare-versions

---
+++ test/test-compare-versions.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: LGPL-2.1-or-later
+set -e
+
+ANALYZE="${1:-systemd-analyze}"
+
+$ANALYZE compare-versions 1 lt 2
+$ANALYZE compare-versions 1 '<' 2
+$ANALYZE compare-versions 1 le 2
+$ANALYZE compare-versions 1 '<=' 2
+$ANALYZE compare-versions 1 ne 2
+$ANALYZE compare-versions 1 '!=' 2
+( ! $ANALYZE compare-versions 1 ge 2 )
+( ! $ANALYZE compare-versions 1 '>=' 2 )
+( ! $ANALYZE compare-versions 1 eq 2 )
+( ! $ANALYZE compare-versions 1 '==' 2 )
+( ! $ANALYZE compare-versions 1 gt 2 )
+( ! $ANALYZE compare-versions 1 '>' 2 )
+
+$ANALYZE compare-versions 1 2 | grep ' < '
+$ANALYZE compare-versions 2 2 | grep ' == '
+$ANALYZE compare-versions 2 1 | grep ' > '
+
+set +e
+
+$ANALYZE compare-versions 1 2; ret1=$?
+$ANALYZE compare-versions 2 2; ret2=$?
+$ANALYZE compare-versions 2 1; ret3=$?
+
+set -e
+
+test $ret1 == 12
+test $ret2 == 0
+test $ret3 == 11
