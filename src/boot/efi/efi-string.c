38d3a74e1532b9f1936f2d05d492367605e0ae1c
Jan Janssen <medhefgo@web.de>
2022-05-23 13:09:03 +0200

    boot: Add strcmp8/16

--- src/boot/efi/efi-string.c
+++ src/boot/efi/efi-string.c
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 
 #include "efi-string.h"
+#include "macro-fundamental.h"
 
 /* String functions for both char and char16_t that should behave the same way as their respective
  * counterpart in userspace. Where it makes sense, these accept NULL and do something sensible whereas
@@ -41,4 +42,68 @@ size_t strlen8(const char *s) {
 
 size_t strlen16(const char16_t *s) {
         return strnlen16(s, SIZE_MAX);
+}
+
+#define TOLOWER(c) ((c >= 'A' && c <= 'Z') ? c + ('a' - 'A') : c)
+
+char tolower8(char c) {
+        return TOLOWER(c);
+}
+
+char16_t tolower16(char16_t c) {
+        return TOLOWER(c);
+}
+
+#define STRNCASECMP_U(cmp_type, case)                \
+        ({                                           \
+                if (!s1 || !s2)                      \
+                        return CMP(s1, s2);          \
+                                                     \
+                while (n > 0) {                      \
+                        cmp_type c1 = *s1, c2 = *s2; \
+                        if (case) {                  \
+                                c1 = TOLOWER(c1);    \
+                                c2 = TOLOWER(c2);    \
+                        }                            \
+                        if (!c1 || c1 != c2)         \
+                                return c1 - c2;      \
+                                                     \
+                        s1++;                        \
+                        s2++;                        \
+                        n--;                         \
+                }                                    \
+                                                     \
+                return 0;                            \
+        })
+
+int strncmp8(const char *s1, const char *s2, size_t n) {
+        STRNCASECMP_U(unsigned char, false);
+}
+
+int strncmp16(const char16_t *s1, const char16_t *s2, size_t n) {
+        STRNCASECMP_U(char16_t, false);
+}
+
+int strcmp8(const char *s1, const char *s2) {
+        return strncmp8(s1, s2, SIZE_MAX);
+}
+
+int strcmp16(const char16_t *s1, const char16_t *s2) {
+        return strncmp16(s1, s2, SIZE_MAX);
+}
+
+int strncasecmp8(const char *s1, const char *s2, size_t n) {
+        STRNCASECMP_U(unsigned char, true);
+}
+
+int strncasecmp16(const char16_t *s1, const char16_t *s2, size_t n) {
+        STRNCASECMP_U(char16_t, true);
+}
+
+int strcasecmp8(const char *s1, const char *s2) {
+        return strncasecmp8(s1, s2, SIZE_MAX);
+}
+
+int strcasecmp16(const char16_t *s1, const char16_t *s2) {
+        return strncasecmp16(s1, s2, SIZE_MAX);
 }
