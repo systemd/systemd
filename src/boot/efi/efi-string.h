533d477f50ed1526ca5a598dfe099143c2f94a51
Jan Janssen <medhefgo@web.de>
2022-05-23 11:23:40 +0200

    boot: Add strlen8/16

---
+++ src/boot/efi/efi-string.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#pragma once
+
+#include <stddef.h>
+#include <stdint.h>
+#include <uchar.h>
+
+size_t strnlen8(const char *s, size_t n);
+size_t strnlen16(const char16_t *s, size_t n);
+
+size_t strlen8(const char *s);
+size_t strlen16(const char16_t *s);
