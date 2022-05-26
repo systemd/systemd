ea8a991e95449489adea7df5700b7554a0860609
Jan Janssen <medhefgo@web.de>
2022-05-26 13:07:30 +0200

    boot: Drop use of DevicePathFromHandle

--- src/boot/efi/disk.c
+++ src/boot/efi/disk.c
@@ -7,16 +7,18 @@
 #include "util.h"
 
 EFI_STATUS disk_get_part_uuid(EFI_HANDLE *handle, CHAR16 uuid[static 37]) {
+        EFI_STATUS err;
         EFI_DEVICE_PATH *device_path;
         _cleanup_freepool_ EFI_DEVICE_PATH *paths = NULL;
+
+        /* export the device path this image is started from */
 
         if (!handle)
                 return EFI_NOT_FOUND;
 
-        /* export the device path this image is started from */
-        device_path = DevicePathFromHandle(handle);
-        if (!device_path)
-                return EFI_NOT_FOUND;
+        err = BS->HandleProtocol(handle, &DevicePathProtocol, (void **) &device_path);
+        if (err != EFI_SUCCESS)
+                return err;
 
         paths = UnpackDevicePath(device_path);
         for (EFI_DEVICE_PATH *path = paths; !IsDevicePathEnd(path); path = NextDevicePathNode(path)) {
