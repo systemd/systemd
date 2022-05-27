31a758b1b1ff557e6b2c3564f8660f8821aef435
Christian Göttsche <cgzones@googlemail.com>
2022-05-27 13:47:23 +0200

    login: do not issue wall messages on ptys for suspend and hibernate
    Fixes: #23520
    
    [zjs: I added the comment and rebased the patch.]

--- src/login/logind-utmp.c
+++ src/login/logind-utmp.c
@@ -44,13 +44,23 @@ _const_ static usec_t when_wall(usec_t n, usec_t elapse) {
 
 bool logind_wall_tty_filter(const char *tty, bool is_local, void *userdata) {
         Manager *m = ASSERT_PTR(userdata);
+        assert(m->scheduled_shutdown_action);
 
         const char *p = path_startswith(tty, "/dev/");
         if (!p)
                 return true;
 
-        /* Do not write to local pseudo-terminals */
-        if (startswith(p, "pts/") && is_local)
+        /* Do not information about events which do not destroy local sessions to local pseudo-terminals.
+         * We assume that the if the system enters sleep or hibernation, this will be visible in an
+         * obvious way for any local user. And once the systems exits sleep or hibernation, the notication
+         * would be just noise. */
+        if (is_local &&
+            startswith(p, "pts/") &&
+            IN_SET(m->scheduled_shutdown_action->handle,
+                   HANDLE_SUSPEND,
+                   HANDLE_HIBERNATE,
+                   HANDLE_HYBRID_SLEEP,
+                   HANDLE_SUSPEND_THEN_HIBERNATE))
                 return false;
 
         return !streq_ptr(p, m->scheduled_shutdown_tty);
