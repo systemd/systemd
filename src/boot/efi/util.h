ad66963c0b6d14a76c7794eaa8b7ef84f51a6572
Jan Janssen <medhefgo@web.de>
2022-05-28 19:36:21 +0200

    boot: Use xmalloc
    This drops the unused xnew0 and xallocate_zero_pool as there is only two
    users of it. _cleanup_freepool_ will be phased out once the types in the
    declarations are changed/renamed.

--- src/boot/efi/util.h
+++ src/boot/efi/util.h
@@ -72,19 +72,8 @@ static inline void *xrealloc(void *p, size_t old_size, size_t new_size) {
         return r;
 }
 
-#define xnew_alloc(type, n, alloc)                                           \
-        ({                                                                   \
-                UINTN _alloc_size;                                           \
-                assert_se(!__builtin_mul_overflow(sizeof(type), (n), &_alloc_size)); \
-                (type *) alloc(_alloc_size);                                 \
-        })
-
-#define xallocate_pool(size) ASSERT_SE_PTR(AllocatePool(size))
-#define xallocate_zero_pool(size) ASSERT_SE_PTR(AllocateZeroPool(size))
-#define xreallocate_pool(p, old_size, new_size) ASSERT_SE_PTR(ReallocatePool((p), (old_size), (new_size)))
 #define xpool_print(fmt, ...) ((CHAR16 *) ASSERT_SE_PTR(PoolPrint((fmt), ##__VA_ARGS__)))
-#define xnew(type, n) xnew_alloc(type, (n), xallocate_pool)
-#define xnew0(type, n) xnew_alloc(type, (n), xallocate_zero_pool)
+#define xnew(type, n) xmalloc_multiply((n), sizeof(type))
 
 EFI_STATUS parse_boolean(const CHAR8 *v, BOOLEAN *b);
 
