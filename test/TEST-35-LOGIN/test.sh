66ca0d9a5c8a548a336ad1297f4f55aa0520c303
Yu Watanabe <watanabe.yu+github@gmail.com>
2022-06-01 08:56:08 +0900

    test: import logind test from debian/ubuntu test suite

---
+++ test/TEST-35-LOGIN/test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: LGPL-2.1-or-later
+set -e
+
+TEST_DESCRIPTION="LOGIN"
+IMAGE_NAME="default"
+
+# shellcheck source=test/test-functions
+. "${TEST_BASE_DIR:?}/test-functions"
+
+QEMU_TIMEOUT=800
+
+do_test "$@"
