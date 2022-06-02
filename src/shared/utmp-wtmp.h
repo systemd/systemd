30a0e11edc3ac73d5c00debde98cb82ada54a33b
Christian Hesse <mail@eworm.de>
2022-06-02 20:49:46 +0200

    shared/utmp-wtmp: fix build without utmp
    Commit 16618332388442f2f1c3e52b0a9fde00121564a3 changed a function to
    add an extra argument. The data types used when building without utmp
    missed the change.

--- src/shared/utmp-wtmp.h
+++ src/shared/utmp-wtmp.h
@@ -59,7 +59,7 @@ static inline int utmp_wall(
                 const char *message,
                 const char *username,
                 const char *origin_tty,
-                bool (*match_tty)(const char *tty, void *userdata),
+                bool (*match_tty)(const char *tty, bool is_local, void *userdata),
                 void *userdata) {
         return 0;
 }
