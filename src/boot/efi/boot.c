1d278ad7d4635eed33bad73dd3fbd563d3ce0209
Jan Janssen <medhefgo@web.de>
2022-06-05 17:35:03 +0200

    boot: Stop linking against libefi.a
    libefi.a just provided the c helper API that was slowly removed. As we
    do not depend on anything provided by it anymore, it is safe to drop
    now.
    
    Since the ST/BS/RT pointers are very convenient and needed everywhere,
    they are retained and initialized by us.

--- src/boot/efi/boot.c
+++ src/boot/efi/boot.c
@@ -2748,3 +2748,8 @@ out:
 }
 
 DEFINE_EFI_MAIN_FUNCTION(run, "systemd-boot", /*wait_for_debugger=*/false);
+
+/* Fedora has a heavily patched gnu-efi that supports elf constructors. It calls into _entry instead. */
+EFI_STATUS _entry(EFI_HANDLE image, EFI_SYSTEM_TABLE *system_table) {
+        return efi_main(image, system_table);
+}
