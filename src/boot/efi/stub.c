2ffbc67c86a9d206ab96efc41bbb9cbdb294774c
Jan Janssen <medhefgo@web.de>
2022-06-05 13:19:21 +0200

    boot: Introduce log_wait
    Instead of waiting after every error message that is shown, we now just
    count the logs and do the wait either before starting an image or exit
    from main.

--- src/boot/efi/stub.c
+++ src/boot/efi/stub.c
@@ -147,7 +147,7 @@ static void export_variables(EFI_LOADED_IMAGE *loaded_image) {
                 efivar_set(LOADER_GUID, L"StubInfo", L"systemd-stub " GIT_VERSION, 0);
 }
 
-EFI_STATUS efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE *sys_table) {
+EFI_STATUS main(EFI_HANDLE image) {
 
         enum {
                 SECTION_CMDLINE,
@@ -179,11 +179,6 @@ EFI_STATUS efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE *sys_table) {
         CHAR8 *cmdline = NULL;
         _cleanup_freepool_ CHAR8 *cmdline_owned = NULL;
         EFI_STATUS err;
-
-        InitializeLib(image, sys_table);
-        debug_hook(L"systemd-stub");
-        /* Uncomment the next line if you need to wait for debugger. */
-        // debug_break();
 
         err = BS->OpenProtocol(
                         image,
@@ -302,4 +297,16 @@ EFI_STATUS efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE *sys_table) {
                          PHYSICAL_ADDRESS_TO_POINTER(initrd_base), initrd_size);
         graphics_mode(FALSE);
         return log_error_status(err, "Execution of embedded linux image failed: %m");
+}
+
+EFI_STATUS efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE *sys_table) {
+        InitializeLib(image, sys_table);
+
+        debug_hook(L"systemd-stub");
+        /* Uncomment the next line if you need to wait for debugger. */
+        // debug_break();
+
+        EFI_STATUS err = main(image);
+        log_wait();
+        return err;
 }
