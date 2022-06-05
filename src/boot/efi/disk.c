5e71453699b1d3fbb7b89ea2502322f35d5b8fab
Jan Janssen <medhefgo@web.de>
2022-06-05 16:01:40 +0200

    boot: Drop use of GuidToString

--- src/boot/efi/disk.c
+++ src/boot/efi/disk.c
@@ -29,11 +29,18 @@ EFI_STATUS disk_get_part_uuid(EFI_HANDLE *handle, CHAR16 uuid[static 37]) {
                 if (hd->SignatureType != SIGNATURE_TYPE_GUID)
                         continue;
 
-                /* Use memcpy in case the device path node is misaligned. */
-                EFI_GUID sig;
-                memcpy(&sig, hd->Signature, sizeof(hd->Signature));
+                snprintf(uuid, 37,
+                         "%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x",
+                         hd->Signature[3], hd->Signature[2],
+                         hd->Signature[1], hd->Signature[0],
 
-                GuidToString(uuid, &sig);
+                         hd->Signature[5], hd->Signature[4],
+                         hd->Signature[7], hd->Signature[6],
+
+                         hd->Signature[8], hd->Signature[9],
+                         hd->Signature[10], hd->Signature[11],
+                         hd->Signature[12], hd->Signature[13],
+                         hd->Signature[14], hd->Signature[15]);
                 return EFI_SUCCESS;
         }
 
