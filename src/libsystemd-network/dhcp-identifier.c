1ffb05050103586e899353bea6d94b24a209da29
Nick Rosbrook <nick.rosbrook@canonical.com>
2022-06-08 15:53:30 -0400

    libsystemd-network: use udev_available()

--- src/libsystemd-network/dhcp-identifier.c
+++ src/libsystemd-network/dhcp-identifier.c
@@ -11,7 +11,6 @@
 #include "netif-util.h"
 #include "siphash24.h"
 #include "sparse-endian.h"
-#include "stat-util.h"
 #include "string-table.h"
 #include "udev-util.h"
 
@@ -213,7 +212,7 @@ int dhcp_identifier_set_iaid(
         uint64_t id;
         int r;
 
-        if (path_is_read_only_fs("/sys") <= 0 && !use_mac) {
+        if (udev_available() && !use_mac) {
                 /* udev should be around */
 
                 r = sd_device_new_from_ifindex(&device, ifindex);
