7d321754bb4c8257b18ea39ac765979e046fba51
Yu Watanabe <watanabe.yu+github@gmail.com>
2022-06-10 13:55:54 +0900

    test: import timedated test from debian/ubuntu test suite

---
+++ test/units/assert.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+# utility functions for shell tests
+
+assert_true() {
+    local rc
+
+    set +e
+    "$@"
+    rc=$?
+    set -e
+    if [[ "$rc" != "0" ]]; then
+        echo "FAIL: command '$*' failed with exit code $rc" >&2
+        exit 1
+    fi
+}
+
+
+assert_eq() {
+    if [[ "$1" != "$2" ]]; then
+        echo "FAIL: expected: '$2' actual: '$1'" >&2
+        exit 1
+    fi
+}
+
+assert_in() {
+    if ! echo "$2" | grep -q "$1"; then
+        echo "FAIL: '$1' not found in:" >&2
+        echo "$2" >&2
+        exit 1
+    fi
+}
+
+assert_rc() {
+    local exp=$1
+    local rc
+    shift
+    set +e
+    "$@"
+    rc=$?
+    set -e
+    assert_eq "$rc" "$exp"
+}
