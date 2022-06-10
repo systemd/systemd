0e60569cb10626c9db62c811d8bb1290a8c2a34b
Jan Janssen <medhefgo@web.de>
2022-06-10 19:06:57 +0200

    boot: Use printf for error logging
    This also drops the _stall suffix in anticipation of the next commit.

--- src/boot/efi/stub.c
+++ src/boot/efi/stub.c
@@ -60,7 +60,7 @@ static EFI_STATUS combine_initrd(
                         EFI_SIZE_TO_PAGES(n),
                         &base);
         if (EFI_ERROR(err))
-                return log_error_status_stall(err, L"Failed to allocate space for combined initrd: %r", err);
+                return log_error_status(err, "Failed to allocate space for combined initrd: %m");
 
         p = PHYSICAL_ADDRESS_TO_POINTER(base);
         if (initrd_base != 0) {
@@ -193,13 +193,13 @@ EFI_STATUS efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE *sys_table) {
                         NULL,
                         EFI_OPEN_PROTOCOL_GET_PROTOCOL);
         if (EFI_ERROR(err))
-                return log_error_status_stall(err, L"Error getting a LoadedImageProtocol handle: %r", err);
+                return log_error_status(err, "Error getting a LoadedImageProtocol handle: %m");
 
         err = pe_memory_locate_sections(loaded_image->ImageBase, (const CHAR8**) sections, addrs, szs);
         if (EFI_ERROR(err) || szs[SECTION_LINUX] == 0) {
                 if (!EFI_ERROR(err))
                         err = EFI_NOT_FOUND;
-                return log_error_status_stall(err, L"Unable to locate embedded .linux section: %r", err);
+                return log_error_status(err, "Unable to locate embedded .linux section: %m");
         }
 
         /* Show splash screen as early as possible */
@@ -294,12 +294,12 @@ EFI_STATUS efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE *sys_table) {
                 err = devicetree_install_from_memory(
                                 &dt_state, PHYSICAL_ADDRESS_TO_POINTER(dt_base), dt_size);
                 if (EFI_ERROR(err))
-                        log_error_stall(L"Error loading embedded devicetree: %r", err);
+                        log_error_status(err, "Error loading embedded devicetree: %m");
         }
 
         err = linux_exec(image, cmdline, cmdline_len,
                          PHYSICAL_ADDRESS_TO_POINTER(linux_base), linux_size,
                          PHYSICAL_ADDRESS_TO_POINTER(initrd_base), initrd_size);
         graphics_mode(FALSE);
-        return log_error_status_stall(err, L"Execution of embedded linux image failed: %r", err);
+        return log_error_status(err, "Execution of embedded linux image failed: %m");
 }
