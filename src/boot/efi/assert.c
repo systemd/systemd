2b8c2fe70e26c3e4e277ca83d8dedfd00a0ab424
Jan Janssen <medhefgo@web.de>
2022-06-10 19:06:57 +0200

    boot: Use printf for error logging
    This also drops the _stall suffix in anticipation of the next commit.

--- src/boot/efi/assert.c
+++ src/boot/efi/assert.c
@@ -6,7 +6,11 @@
 #include "util.h"
 
 void efi_assert(const char *expr, const char *file, unsigned line, const char *function) {
-        log_error_stall(L"systemd-boot assertion '%a' failed at %a:%u, function %a(). Halting.", expr, file, line, function);
+        log_error("systemd-boot assertion '%s' failed at %s:%u, function %s(). Halting.",
+                  expr,
+                  file,
+                  line,
+                  function);
         for (;;)
                 BS->Stall(60 * 1000 * 1000);
 }
