aeed247aa524a7846c9144e9a471c0558aab034f
David Tardon <dtardon@redhat.com>
2022-06-10 15:07:01 +0200

    logind-session-dbus: allow to set display name via dbus
    Currently, the only way to set display name of a graphical session is to
    pass it to CreateSession(). But modern display managers like gdm start
    the display server as part of the user session, which means that the
    display name isn't known yet when the session is being created. Hence,
    let's make it possible to set it later.

--- src/login/logind-session.h
+++ src/login/logind-session.h
@@ -137,6 +137,7 @@ int session_set_idle_hint(Session *s, bool b);
 int session_get_locked_hint(Session *s);
 void session_set_locked_hint(Session *s, bool b);
 void session_set_type(Session *s, SessionType t);
+int session_set_display(Session *s, const char *display);
 int session_create_fifo(Session *s);
 int session_start(Session *s, sd_bus_message *properties, sd_bus_error *error);
 int session_stop(Session *s, bool force);
