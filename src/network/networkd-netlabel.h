9bce9ba4e8a1be9a8b507110af805a62fd958b70
Yu Watanabe <watanabe.yu+github@gmail.com>
2022-06-17 22:54:04 +0900

    network: add missing headers and type declaration

--- src/network/networkd-netlabel.h
+++ src/network/networkd-netlabel.h
@@ -1,6 +1,11 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 #pragma once
 
+#include "conf-parser.h"
+#include "set.h"
+
+typedef struct Address Address;
+
 void address_add_netlabel(const Address *address);
 void address_del_netlabel(const Address *address);
 
