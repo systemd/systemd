b01f76c0727e17029cb955fc0272eca1bd8178c3
Vishal Chillara Srinivas <vishal.chillarasrinivas@philips.com>
2022-06-17 12:07:19 +0530

    resolve: mDNS transaction max attempts fix
    Maximum attempts to send mDNS requests is one except for probe requests, which should be attempted thrice.
    Implemented fix to account for the difference between regular queries and probe requests, and prevent
    even regular queries from being attempted thrice.
    See RFC 6762 Section 8.1

--- src/resolve/resolved-dns-transaction.c
+++ src/resolve/resolved-dns-transaction.c
@@ -1629,7 +1629,7 @@ static int dns_transaction_prepare(DnsTransaction *t, usec_t ts) {
                 return 0;
         }
 
-        if (t->n_attempts >= TRANSACTION_ATTEMPTS_MAX(t->scope->protocol)) {
+        if (t->n_attempts >= dns_transaction_attempts_max(t->scope->protocol, t->probing)) {
                 DnsTransactionState result;
 
                 if (t->scope->protocol == DNS_PROTOCOL_LLMNR)
