d64e9bc5a686a74cc2c2be4802d5f30df5c00da0
Frantisek Sumsal <frantisek@sumsal.cz>
2022-06-17 14:44:49 +0200

    test: make the busy loop in TEST-02 less verbose
    as it unnecessarily clogs the logs.

--- test/units/testsuite-02.sh
+++ test/units/testsuite-02.sh
@@ -49,6 +49,7 @@ function report_result() {
     systemd-cat cat "/$name.log"
 }
 
+set +x
 # Associative array for running tasks, where running[test-path]=PID
 declare -A running=()
 for task in "${TEST_LIST[@]}"; do
@@ -72,6 +73,7 @@ for task in "${TEST_LIST[@]}"; do
     done
 
     if [[ -x $task ]]; then
+        echo "Executing test '$task'"
         log_file="/${task##*/}.log"
         $task &>"$log_file" &
         running[$task]=$!
@@ -80,10 +82,13 @@ done
 
 # Wait for remaining running tasks
 for key in "${!running[@]}"; do
+    echo "Waiting for test '$key' to finish"
     wait ${running[$key]} && ec=0 || ec=$?
     report_result "$key" $ec
     unset running["$key"]
 done
+
+set -x
 
 # Test logs are sometimes lost, as the system shuts down immediately after
 journalctl --sync
