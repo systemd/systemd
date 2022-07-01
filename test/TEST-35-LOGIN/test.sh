dc94edcc64863c0dbc1c88267ef0c4fcfece17fd
Michal Sekletar <msekleta@redhat.com>
2022-07-01 12:59:57 +0200

    tests: verify that Lock D-Bus signal is sent when IdleAction=lock

--- test/TEST-35-LOGIN/test.sh
+++ test/TEST-35-LOGIN/test.sh
@@ -7,4 +7,14 @@ TEST_DESCRIPTION="LOGIN"
 # shellcheck source=test/test-functions
 . "${TEST_BASE_DIR:?}/test-functions"
 
+test_append_files() {
+    if command -v expect >/dev/null && command -v tclsh >/dev/null ; then
+        # shellcheck disable=SC2016
+        version="$(tclsh <<< 'puts $tcl_version')"
+
+        image_install expect
+        inst_recursive /usr/lib64/tcl"$version" /usr/share/tcl"$version"
+    fi
+}
+
 do_test "$@"
