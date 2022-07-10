e854bce69b33b382739e66b68eb8a19ca17c4762
matoro <matoro@users.noreply.github.com>
2022-07-10 00:54:58 -0400

    journal: replace __sync intrinsics with __atomic

--- src/basic/sigbus.c
+++ src/basic/sigbus.c
@@ -28,7 +28,7 @@ static void sigbus_push(void *addr) {
         assert(addr);
 
         /* Find a free place, increase the number of entries and leave, if we can */
-        for (size_t u = 0; u < SIGBUS_QUEUE_MAX; u++)
+        for (size_t u = 0; u < SIGBUS_QUEUE_MAX; u++) {
                 /* OK to initialize this here since we haven't started the atomic ops yet */
                 void *tmp = NULL;
                 if (__atomic_compare_exchange_n(&sigbus_queue[u], &tmp, addr,
@@ -36,6 +36,7 @@ static void sigbus_push(void *addr) {
                         __atomic_fetch_add(&n_sigbus_queue, 1, __ATOMIC_SEQ_CST);
                         return;
                 }
+        }
 
         /* If we can't, make sure the queue size is out of bounds, to
          * mark it as overflow */
@@ -80,8 +81,8 @@ int sigbus_pop(void **ret) {
 
                         /* OK if we clobber addr here, since we either immediately return
                          * or it will be immediately reinitialized on next loop */
-                        if (__atomic_compare_exchange_n(&sigbus_queue[u], &addr, NULL),
-                            false, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST) {
+                        if (__atomic_compare_exchange_n(&sigbus_queue[u], &addr, NULL,
+                            false, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST)) {
                                 __atomic_fetch_sub(&n_sigbus_queue, 1, __ATOMIC_SEQ_CST);
                                 /* If we successfully entered this if condition, addr won't
                                  * have been modified since its assignment, so safe to use it */
