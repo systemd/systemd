1538fb7f8997c49f5e3c73fb69f61eff931a7b3f
Daan De Meyer <daan.j.demeyer@gmail.com>
2022-07-13 14:46:33 +0200

    coredump: Try to write journald coredump metadata to the journal
    Currently, if journald coredumps, the coredump is written to
    /var/lib/systemd/coredump but the coredump metadata is not written
    to  the journal meaning we can't find out about the coredump's
    existence via the journal. This means that coredumpctl can't be
    used to work with journald coredumps, as well as any other tools
    that rely on journald to know about coredumps.
    
    To solve the issue, let's have systemd-coredump try to write
    systemd-journald coredump metadata to the journal. We have to be
    careful though, since if journald coredumps, there's no active
    reader on the receive end of the journal socket, so we have to make
    sure we don't deadlock trying to write to the socket. To avoid the
    deadlock, we put the socket in nonblocking mode before trying to
    write to it.

--- src/libsystemd/sd-journal/journal-send.h
+++ src/libsystemd/sd-journal/journal-send.h
@@ -1,6 +1,10 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 #pragma once
 
+#include <stdbool.h>
+
+int journal_fd_nonblock(bool nonblock);
+
 #if VALGRIND
 void close_journal_fd(void);
 #else
