2829fca249bd58a8cd52013d625701e2b8e31737
Yu Watanabe <watanabe.yu+github@gmail.com>
2022-07-15 09:08:10 +0900

    sd-device: introduce device_get_sysattr_bool()

--- src/libsystemd/sd-device/device-private.h
+++ src/libsystemd/sd-device/device-private.h
@@ -18,6 +18,7 @@ static inline int device_new_from_watch_handle(sd_device **ret, int wd) {
 }
 
 int device_get_property_bool(sd_device *device, const char *key);
+int device_get_sysattr_bool(sd_device *device, const char *sysattr);
 int device_get_device_id(sd_device *device, const char **ret);
 int device_get_devlink_priority(sd_device *device, int *ret);
 int device_get_watch_handle(sd_device *device);
