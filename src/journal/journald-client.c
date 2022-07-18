c37e1a9503a4d9c8bf063708c4ea59c650272b1c
Quentin Deslandes <qdeslandes@naccy.de>
2022-07-18 18:23:53 +0200

    journal: filter log based on LogIncludeRegex and LogExcludeRegex
    Use LogIncludeRegex and LogExcludeRegex from the unit's cgroup xattr in
    order to keep or discard log messages before writting them to the journal.
    When a log message is discarded, it won't be written to syslog, console...
    either.
    
    When a native, syslog, or standard output log message is received, journald
    will try to match it against LogIncludeRegex and LogExcludeRegex defined in
    the unit's configuration file. The matching is defined as follow:
    - If LogIncludeRegex is defined but doesn't match, discard the message,
      otherwise continue.
    - If LogExcludeRegex is defined and matches, discard the messages,
      otherwise continue.
    - Write the log message.
    
    This behaviour allow for all combination of LogIncludeRegex and LogExcludeRegex
    to be defined, or not. Also, this allows for explicit definition of allowed and
    not allowed log messages, without having to define more complex regexes.

---
+++ src/journal/journald-client.c
@@ -0,0 +1,36 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+
+#include "journald-client.h"
+#include "pcre2-util.h"
+
+int client_context_check_keep_log(ClientContext *c, const char *message)
+{
+        int r;
+
+        if (!c)
+                return 1;
+
+        assert(message);
+
+        if (c->log_include_regex) {
+                r = pattern_matches_and_log(c->log_include_regex, message, strlen(message), NULL);
+                if (r < 0)
+                        return r;
+                if (r == 0)
+                        /* If the include pattern does not matches the message,
+                         * it should be discarded. */
+                        return 0;
+        }
+
+        if (c->log_exclude_regex) {
+                r = pattern_matches_and_log(c->log_exclude_regex, message, strlen(message), NULL);
+                if (r < 0)
+                        return r;
+                if (r == 1)
+                        /* If the exclude pattern matches the message, it
+                         * should be discarded. */
+                        return 0;
+        }
+
+        return 1;
+}
--- src/journal/journald-context.h
+++ src/journal/journald-context.h
@@ -7,6 +7,7 @@
 
 #include "sd-id128.h"
 
+#include "pcre2-util.h"
 #include "time-util.h"
 
 typedef struct ClientContext ClientContext;
@@ -55,6 +56,9 @@ struct ClientContext {
 
         usec_t log_ratelimit_interval;
         unsigned log_ratelimit_burst;
+
+        pcre2_code *log_include_regex;
+        pcre2_code *log_exclude_regex;
 };
 
 int client_context_get(
