55699a77e168957b340ec1c81f42a20231a3df33
Ludwig Nussel <ludwig.nussel@suse.de>
2022-07-19 17:29:45 +0200

    pull: fix PullFlags numbering

--- src/import/pull-common.h
+++ src/import/pull-common.h
@@ -9,15 +9,15 @@
 typedef enum PullFlags {
         PULL_FORCE              = 1 << 0, /* replace existing image */
         PULL_READ_ONLY          = 1 << 1, /* make generated image read-only */
-        PULL_SETTINGS           = 1 << 1, /* download .nspawn settings file */
-        PULL_ROOTHASH           = 1 << 2, /* only for raw: download .roothash file for verity */
-        PULL_ROOTHASH_SIGNATURE = 1 << 3, /* only for raw: download .roothash.p7s file for verity */
-        PULL_VERITY             = 1 << 4, /* only for raw: download .verity file for verity */
-        PULL_BTRFS_SUBVOL       = 1 << 2, /* tar: preferably create images as btrfs subvols */
-        PULL_BTRFS_QUOTA        = 1 << 3, /* tar: set up btrfs quota for new subvolume as child of parent subvolume */
-        PULL_CONVERT_QCOW2      = 1 << 4, /* raw: if we detect a qcow2 image, unpack it */
-        PULL_DIRECT             = 1 << 5, /* download without rename games */
-        PULL_SYNC               = 1 << 6, /* fsync() right before we are done */
+        PULL_SETTINGS           = 1 << 2, /* download .nspawn settings file */
+        PULL_ROOTHASH           = 1 << 3, /* only for raw: download .roothash file for verity */
+        PULL_ROOTHASH_SIGNATURE = 1 << 4, /* only for raw: download .roothash.p7s file for verity */
+        PULL_VERITY             = 1 << 5, /* only for raw: download .verity file for verity */
+        PULL_BTRFS_SUBVOL       = 1 << 6, /* tar: preferably create images as btrfs subvols */
+        PULL_BTRFS_QUOTA        = 1 << 7, /* tar: set up btrfs quota for new subvolume as child of parent subvolume */
+        PULL_CONVERT_QCOW2      = 1 << 8, /* raw: if we detect a qcow2 image, unpack it */
+        PULL_DIRECT             = 1 << 9, /* download without rename games */
+        PULL_SYNC               = 1 << 10, /* fsync() right before we are done */
 
         /* The supported flags for the tar and the raw pulling */
         PULL_FLAGS_MASK_TAR     = PULL_FORCE|PULL_READ_ONLY|PULL_SETTINGS|PULL_BTRFS_SUBVOL|PULL_BTRFS_QUOTA|PULL_DIRECT|PULL_SYNC,
