7fc254429faf522696873f3e65e2ec4f60965d71
Daan De Meyer <daan.j.demeyer@gmail.com>
2022-07-22 14:49:42 +0200

    journal: Move more pattern matching logic into pcre2-util
    To avoid having "#if HAVE_PCRE2" all throughout the code, let's
    confine the pcre2 header specific stuff to pcre2-util.c. Instead of
    exposing all the individual symbols from pcre2, let's only expose
    three high level functions that do all we need:
    
    - pcre2_pattern_compile(): Compile the regex
    - pcre2_pattern_matches(): Check if the compiled regex matches a message
    - pcre2_pattern_free(): Free the compiled regex
    
    We expose the compiled pcre2 pattern (which is of type pcre2_code *) as
    a void pointer to avoid having to include pcre2.h in all code where we
    work with compiled pcre2 patterns. For readability, we typedef void
    to pcre2_pattern and use that as the type specifier for compiled pcre2
    patterns.

--- src/shared/pcre2-util.h
+++ src/shared/pcre2-util.h
@@ -18,6 +18,24 @@ extern PCRE2_SIZE* (*sym_pcre2_get_ovector_pointer)(pcre2_match_data *);
 
 DEFINE_TRIVIAL_CLEANUP_FUNC_FULL(pcre2_match_data*, sym_pcre2_match_data_free, NULL);
 DEFINE_TRIVIAL_CLEANUP_FUNC_FULL(pcre2_code*, sym_pcre2_code_free, NULL);
+#else
+
+typedef struct {} pcre2_code;
+
 #endif
+
+typedef enum {
+        PATTERN_COMPILE_CASE_AUTO,
+        PATTERN_COMPILE_CASE_SENSITIVE,
+        PATTERN_COMPILE_CASE_INSENSITIVE,
+        _PATTERN_COMPILE_CASE_MAX,
+        _PATTERN_COMPILE_CASE_INVALID = -EINVAL,
+} PatternCompileCase;
+
+int pattern_compile_and_log(const char *pattern, PatternCompileCase case_, pcre2_code **ret);
+int pattern_matches_and_log(pcre2_code *compiled_pattern, const char *message, size_t size, size_t *ret_ovec);
+void *pattern_free(pcre2_code *p);
+
+DEFINE_TRIVIAL_CLEANUP_FUNC(pcre2_code*, pattern_free);
 
 int dlopen_pcre2(void);
