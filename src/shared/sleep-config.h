203a4df6c11014071e1ef5947ee630dbad93d305
Sonali Srivastava <srivastava.sonali1@gmail.com>
2022-07-23 17:30:42 +0530

    wip

--- src/shared/sleep-config.h
+++ src/shared/sleep-config.h
@@ -64,6 +64,11 @@ int estimate_battery_discharge_rate_per_hour(
                                         Hashmap *current_capacity,
                                         usec_t before_timestamp,
                                         usec_t after_timestamp);
+int get_battery_discharge_rate(void);
+int put_battery_discharge_rate(int estimated_battery_discharge_rate);
+int get_dmi_wakeup_type(void);
+
+bool battery_trip_point_alarm_exists(void);
 
 const char* sleep_operation_to_string(SleepOperation s) _const_;
 SleepOperation sleep_operation_from_string(const char *s) _pure_;
