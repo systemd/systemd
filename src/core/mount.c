systemd/systemd

commit e4de58c8231e47509ffeb3aa47620ca42f22d7f6

core/mount: fail early if directory cannot be created
Prompted by #22334.

src/core/mount.c
@@ -1027,8 +1027,10 @@ static void mount_enter_mounting(Mount *m) {
                 r = mkdir_p_label(p->what, m->directory_mode);
                 /* mkdir_p_label() can return -EEXIST if the target path exists and is not a directory - which is
                  * totally OK, in case the user wants us to overmount a non-directory inode. */
-                if (r < 0 && r != -EEXIST)
+                if (r < 0 && r != -EEXIST) {
                         log_unit_error_errno(UNIT(m), r, "Failed to make bind mount source '%s': %m", p->what);
+                        goto fail;
+                }
         }
 
         if (p) {

commit b2ca3739e18a535e83a4480be85d1220b0410d9a

core/mount: ignore -EACCES from mkdir_p_label() on NFS
This fixes a bug introduced by e4de58c8231e47509ffeb3aa47620ca42f22d7f6.

Fixes #24120.

src/core/mount.c
@@ -1053,8 +1053,9 @@ static void mount_enter_mounting(Mount *m) {
         if (p && mount_is_bind(p)) {
                 r = mkdir_p_label(p->what, m->directory_mode);
                 /* mkdir_p_label() can return -EEXIST if the target path exists and is not a directory - which is
-                 * totally OK, in case the user wants us to overmount a non-directory inode. */
-                if (r < 0 && r != -EEXIST) {
+                 * totally OK, in case the user wants us to overmount a non-directory inode. Also it may returns
+                 * -EACCES when the path is on NFS and the kernel cache about the path is cold. See issue #24120. */
+                if (r < 0 && !IN_SET(r, -EEXIST, -EACCES)) {
                         log_unit_error_errno(UNIT(m), r, "Failed to make bind mount source '%s': %m", p->what);
                         goto fail;
                 }