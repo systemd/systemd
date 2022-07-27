bfba706ffe72e738ab85bcadc2302acad4796a19
Lennart Poettering <lennart@poettering.net>
2022-07-27 16:39:13 +0200

    boot: add three new boot loader feature flags
    Report whether the devicetree + sort-key boot loader spec type #1
    fields are supported, and whether the "@saved" pseudo-entry is
    supported.
    
    Strictly speaking, thes features have been added in versions that are
    already released (250+), so by adding this those version even though
    they support the features will be considered not supporting them, but
    that should be OK (the opposite would be a problem though, i.e. if we'd
    assume a boot loader had a feature it actually does not).
    
    These three features are features relevant to userspace, as it allows
    userspace to tweak/genereate BLS entries or set EFI vars correctly.
    Other features (i.e. that have no impliciations to userspace) are not
    reported.

--- src/boot/efi/boot.c
+++ src/boot/efi/boot.c
@@ -2443,6 +2443,9 @@ static void export_variables(
                 EFI_LOADER_FEATURE_XBOOTLDR |
                 EFI_LOADER_FEATURE_RANDOM_SEED |
                 EFI_LOADER_FEATURE_LOAD_DRIVER |
+                EFI_LOADER_FEATURE_SORT_KEY |
+                EFI_LOADER_FEATURE_SAVED_ENTRY |
+                EFI_LOADER_FEATURE_DEVICETREE |
                 0;
 
         _cleanup_free_ char16_t *infostr = NULL, *typestr = NULL;
