dacf35f57b25876fd88be14965edec97fae1aec3
Sonali Srivastava <srivastava.sonali1@gmail.com>
2022-07-27 11:53:21 +0530

    sleep: support multiple battery instead of only BAT0

--- src/shared/sleep-config.h
+++ src/shared/sleep-config.h
@@ -2,6 +2,7 @@
 #pragma once
 
 #include <linux/fiemap.h>
+#include "hashmap.h"
 #include "time-util.h"
 
 typedef enum SleepOperation {
@@ -55,11 +56,17 @@ int find_hibernate_location(HibernateLocation **ret_hibernate_location);
 int can_sleep(SleepOperation operation);
 int can_sleep_disk(char **types);
 int can_sleep_state(char **types);
-int read_battery_capacity_percentage(void);
 int battery_is_low(void);
 int get_battery_discharge_rate(void);
 int put_battery_discharge_rate(int estimated_battery_discharge_rate);
 int get_dmi_wakeup_type(void);
+int get_total_suspend_interval(Hashmap *last_capacity, usec_t *ret);
+int fetch_batteries_name_with_capacity(Hashmap **ret_current_capacity);
+int estimate_battery_discharge_rate_per_hour(
+                                        Hashmap *last_capacity,
+                                        Hashmap *current_capacity,
+                                        usec_t before_timestamp,
+                                        usec_t after_timestamp);
 
 bool battery_trip_point_alarm_exists(void);
 
