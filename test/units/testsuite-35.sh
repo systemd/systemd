fe9d58b25a8b8b1029dcdfbae89fac59fb429707
Yu Watanabe <watanabe.yu+github@gmail.com>
2022-07-31 00:11:59 +0900

    test: terminate session and user on cleanup

--- test/units/testsuite-35.sh
+++ test/units/testsuite-35.sh
@@ -221,13 +221,24 @@ test_shutdown() {
 cleanup_session() (
     set +ex
 
-    local uid
+    local uid s
 
     uid=$(id -u logind-test-user)
 
     loginctl disable-linger logind-test-user
 
     systemctl stop getty@tty2.service
+
+    for s in $(loginctl --no-legend list-sessions | awk '$3 == "logind-test-user" { print $1 }'); do
+        echo "INFO: stopping session $s"
+        loginctl terminate-session "$s"
+    done
+
+    loginctl terminate-user logind-test-user
+
+    if ! timeout 30 bash -c "while loginctl --no-legend | grep -q logind-test-user; do sleep 1; done"; then
+        echo "WARNING: session for logind-test-user still active, ignoring."
+    fi
 
     pkill -u "$uid"
     sleep 1
