739bf4580c042bf9203f0cd62735268fea655a27
Luca Boccassi <bluca@debian.org>
2022-08-02 20:07:35 +0100

    service: set TRIGGER_UNIT= and TRIGGER_PATH= on activation by path unit
    When a service is triggered by a path unit, pass the
    path unit name and the path that triggered it via env vars
    to the spawned processes.
    Note that this is best-effort, as there might be many triggers
    at the same time, but we only get woken up by one.

--- src/core/unit.c
+++ src/core/unit.c
@@ -5921,6 +5921,7 @@ int unit_get_dependency_array(const Unit *u, UnitDependencyAtom atom, Unit ***re
 }
 
 const ActivationEventInfoVTable * const activation_event_info_vtable[_UNIT_TYPE_MAX] = {
+        [UNIT_PATH] = &activation_event_info_path_vtable,
 };
 
 ActivationEventInfo *activation_event_info_new(Unit *trigger_unit) {
