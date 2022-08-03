f22ee84d2fc533ff5e0f8dfd0db020939ceda371
Jan Janssen <medhefgo@web.de>
2022-08-03 11:27:38 +0200

    man: Add instructions for Microsoft secure boot keys
    Adding Microsoft keys by default is recommended because firmware drivers
    might be signed by it.
    
    This also changes the file ending from .esl to .auth as that is used by
    sign-efi-sig-list manpage and other sources.

--- src/boot/efi/secure-boot.c
+++ src/boot/efi/secure-boot.c
@@ -81,9 +81,9 @@ EFI_STATUS secure_boot_enroll_at(EFI_FILE *root_dir, const char16_t *path) {
                 char *buffer;
                 size_t size;
         } sb_vars[] = {
-                { u"db",  u"db.esl",  EFI_IMAGE_SECURITY_DATABASE_VARIABLE, NULL, 0 },
-                { u"KEK", u"KEK.esl", EFI_GLOBAL_VARIABLE, NULL, 0 },
-                { u"PK",  u"PK.esl",  EFI_GLOBAL_VARIABLE, NULL, 0 },
+                { u"db",  u"db.auth",  EFI_IMAGE_SECURITY_DATABASE_VARIABLE, NULL, 0 },
+                { u"KEK", u"KEK.auth", EFI_GLOBAL_VARIABLE, NULL, 0 },
+                { u"PK",  u"PK.auth",  EFI_GLOBAL_VARIABLE, NULL, 0 },
         };
 
         /* Make sure all keys files exist before we start enrolling them by loading them from the disk first. */
