be4991d5722cc2e37adf96597bc13094de9931bf
Yu Watanabe <watanabe.yu+github@gmail.com>
2022-08-17 05:33:18 +0900

    test: use assertions in sysctl tests

--- test/units/testsuite-76.sh
+++ test/units/testsuite-76.sh
@@ -3,14 +3,17 @@
 set -eux
 set -o pipefail
 
+# shellcheck source=test/units/assert.sh
+. "$(dirname "$0")"/assert.sh
+
 export SYSTEMD_LOG_LEVEL=debug
 
 echo "foo.bar=42" > /tmp/foo.conf
-[[ $(/usr/lib/systemd/systemd-sysctl /tmp/foo.conf)$? -eq 0 ]]
-[[ $(/usr/lib/systemd/systemd-sysctl --strict /tmp/foo.conf)$? -ne 0 ]]
+assert_rc 0 /usr/lib/systemd/systemd-sysctl /tmp/foo.conf
+assert_rc 1 /usr/lib/systemd/systemd-sysctl --strict /tmp/foo.conf
 
 echo "-foo.foo=42" > /tmp/foo.conf
-[[ $(/usr/lib/systemd/systemd-sysctl /tmp/foo.conf)$? -eq 0 ]]
-[[ $(/usr/lib/systemd/systemd-sysctl --strict /tmp/foo.conf)$? -eq 0 ]]
+assert_rc 0 /usr/lib/systemd/systemd-sysctl /tmp/foo.conf
+assert_rc 0 /usr/lib/systemd/systemd-sysctl --strict /tmp/foo.conf
 
 touch /testok
