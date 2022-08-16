eea59048bcd65b5c6003c4462bb5469102e0bf10
Yu Watanabe <watanabe.yu+github@gmail.com>
2022-08-17 05:20:27 +0900

    test: do not use sysctl.d to store test conf
    Otherwise, late invocations of systemd-sysctl, especially through udev
    rules may fail.

--- test/units/testsuite-76.sh
+++ test/units/testsuite-76.sh
@@ -5,12 +5,12 @@ set -o pipefail
 
 export SYSTEMD_LOG_LEVEL=debug
 
-echo "foo.bar=42" > /etc/sysctl.d/foo.conf
-[[ $(/usr/lib/systemd/systemd-sysctl /etc/sysctl.d/foo.conf)$? -eq 0 ]]
-[[ $(/usr/lib/systemd/systemd-sysctl --strict /etc/sysctl.d/foo.conf)$? -ne 0 ]]
+echo "foo.bar=42" > /tmp/foo.conf
+[[ $(/usr/lib/systemd/systemd-sysctl /tmp/foo.conf)$? -eq 0 ]]
+[[ $(/usr/lib/systemd/systemd-sysctl --strict /tmp/foo.conf)$? -ne 0 ]]
 
-echo "-foo.foo=42" > /etc/sysctl.d/foo.conf
-[[ $(/usr/lib/systemd/systemd-sysctl /etc/sysctl.d/foo.conf)$? -eq 0 ]]
-[[ $(/usr/lib/systemd/systemd-sysctl --strict /etc/sysctl.d/foo.conf)$? -eq 0 ]]
+echo "-foo.foo=42" > /tmp/foo.conf
+[[ $(/usr/lib/systemd/systemd-sysctl /tmp/foo.conf)$? -eq 0 ]]
+[[ $(/usr/lib/systemd/systemd-sysctl --strict /tmp/foo.conf)$? -eq 0 ]]
 
 touch /testok
