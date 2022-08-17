d77fdd883eeadce347ca517d21b8cf2b1a2a6f04
Richard Phibel <rphibel@googlemail.com>
2022-08-17 22:24:52 +0200

    journald: use rate-limited logging to log 'Failed to write entry' message

--- src/basic/ratelimit.h
+++ src/basic/ratelimit.h
@@ -4,7 +4,6 @@
 #include <stdbool.h>
 
 #include "time-util.h"
-#include "util.h"
 
 typedef struct RateLimit {
         usec_t interval; /* Keep those two fields first so they can be initialized easily: */
