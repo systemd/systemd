222a951fa4a625e2d7d9c2799d9873377029d1bf
Lennart Poettering <lennart@poettering.net>
2022-08-17 17:00:27 +0200

    tpm2-util: introduce tpm2_parse_pcr_argument() helper
    Add a new tpm2_parse_pcr_argument() helper that unifies how we merge PCR
    masks in a single function, we can use all over the place. Previously we
    had basically the same code for this at 4 places.

--- src/shared/tpm2-util.c
+++ src/shared/tpm2-util.c
@@ -1508,3 +1508,28 @@ Tpm2Support tpm2_support(void) {
 
         return support;
 }
+
+int tpm2_parse_pcr_argument(const char *arg, uint32_t *mask) {
+        uint32_t m;
+        int r;
+
+        assert(mask);
+
+        /* For use in getopt_long() command line parsers: merges masks specified on the command line */
+
+        if (isempty(arg)) {
+                *mask = 0;
+                return 0;
+        }
+
+        r = tpm2_parse_pcrs(arg, &m);
+        if (r < 0)
+                return r;
+
+        if (*mask == UINT32_MAX)
+                *mask = m;
+        else
+                *mask |= m;
+
+        return 0;
+}
