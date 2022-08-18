37a49fdec5d434e1a48f6124b005d19e08acc4ae
Jan Janssen <medhefgo@web.de>
2022-08-18 13:41:49 +0200

    boot: Add log_trace debugging helper

--- src/boot/efi/log.h
+++ src/boot/efi/log.h
@@ -8,3 +8,4 @@ _printf_(2, 3) EFI_STATUS log_internal(EFI_STATUS status, const char *format, ..
 #define log_error_status(status, ...) log_internal(status, __VA_ARGS__)
 #define log_error(...) log_internal(EFI_INVALID_PARAMETER, __VA_ARGS__)
 #define log_oom() log_internal(EFI_OUT_OF_RESOURCES, "Out of memory.")
+#define log_trace() log_internal(EFI_SUCCESS, "%s:%u@%s", __FILE__, __LINE__, __func__)
