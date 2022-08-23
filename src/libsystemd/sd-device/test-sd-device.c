bb00ca3209411076a17ec82365fef33003e355d4
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2022-08-23 16:24:02 +0200

    sd-device: add helper to read a unsigned int attribute
    There are dozens of places where this could be used, but I don't
    want to do the conversion now because it's quite a bit of work.
    I think we could export this function later on, because reading
    numerical attributes is so common. But for now, I'm just adding the
    helper to use it one place.

--- src/libsystemd/sd-device/test-sd-device.c
+++ src/libsystemd/sd-device/test-sd-device.c
@@ -180,6 +180,13 @@ static void test_sd_device_one(sd_device *d) {
 
         r = sd_device_get_sysattr_value(d, "name_assign_type", &val);
         assert_se(r >= 0 || ERRNO_IS_PRIVILEGE(r) || IN_SET(r, -ENOENT, -EINVAL));
+
+        if (r > 0) {
+                unsigned x;
+
+                assert_se(device_get_sysattr_unsigned(d, "name_assign_type", NULL) >= 0);
+                assert_se(device_get_sysattr_unsigned(d, "name_assign_type", &x) >= 0);
+        }
 }
 
 TEST(sd_device_enumerator_devices) {
