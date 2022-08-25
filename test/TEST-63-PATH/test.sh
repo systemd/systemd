8463787f23d6793b4e493fc7a78bab1dc95a3323
Frantisek Sumsal <frantisek@sumsal.cz>
2022-08-25 12:05:53 +0200

    test: make TEST-63 more reliable on slower machines
    Otherwise we might never hit the trigger limit and wait indefinitely.
    
    Found when trying to run the test on an EC2 xen machine without a nested
    virt in CentOS CI (in preparations for some ... unforseseen consequences).

--- test/TEST-63-PATH/test.sh
+++ test/TEST-63-PATH/test.sh
@@ -7,17 +7,4 @@ TEST_DESCRIPTION="https://github.com/systemd/systemd/issues/17433"
 # shellcheck source=test/test-functions
 . "${TEST_BASE_DIR:?}/test-functions"
 
-test_append_files() {
-    (
-        # Tweak the trigger limit interval in case we are collecting coverage
-        # or running without KVM - in both cases we might be slow enough that
-        # we could miss the default rate-limit window and cause the test to fail
-        # unexpectedly.
-        if get_bool "$IS_BUILT_WITH_COVERAGE" || ! get_bool "$QEMU_KVM"; then
-            mkdir -p "${initdir:?}/etc/systemd/system/test63.path.d"
-            printf "[Path]\nTriggerLimitIntervalSec=10\n" >"${initdir:?}/etc/systemd/system/test63.path.d/triggerlimitinterval-override.conf"
-        fi
-    )
-}
-
 do_test "$@"
