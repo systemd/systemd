ad6bbef9d46e34f445eb9ea71c9985577a62f0ef
Yu Watanabe <watanabe.yu+github@gmail.com>
2022-08-28 04:30:23 +0900

    sd-device: introduce device_open_from_devnum()

--- src/libsystemd/sd-device/device-util.c
+++ src/libsystemd/sd-device/device-util.c
@@ -3,6 +3,7 @@
 #include "device-private.h"
 #include "device-util.h"
 #include "devnum-util.h"
+#include "fd-util.h"
 #include "string-util.h"
 
 int devpath_from_devnum(mode_t mode, dev_t devnum, char **ret) {
@@ -30,4 +31,35 @@ int devpath_from_devnum(mode_t mode, dev_t devnum, char **ret) {
 
         *ret = TAKE_PTR(s);
         return 0;
+}
+
+int device_open_from_devnum(mode_t mode, dev_t devnum, int flags, char **ret) {
+        _cleanup_(sd_device_unrefp) sd_device *dev = NULL;
+        _cleanup_close_ int fd = -1;
+        int r;
+
+        r = device_new_from_mode_and_devnum(&dev, mode, devnum);
+        if (r < 0)
+                return r;
+
+        fd = sd_device_open(dev, flags);
+        if (fd < 0)
+                return fd;
+
+        if (ret) {
+                const char *devname;
+                char *s;
+
+                r = sd_device_get_devname(dev, &devname);
+                if (r < 0)
+                        return r;
+
+                s = strdup(devname);
+                if (!s)
+                        return -ENOMEM;
+
+                *ret = s;
+        }
+
+        return TAKE_FD(fd);
 }
