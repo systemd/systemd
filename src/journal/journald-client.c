eb9c167df9de60fe7cde07997b360dc4ed1e529c
Quentin Deslandes <qdeslandes@naccy.de>
2022-08-29 15:42:59 +0200

    journal: filter log based on LogFilterPatterns
    Use LogFilterPatterns from the unit's cgroup xattr in order to keep or
    discard log messages before writing them to the journal.
    When a log message is discarded, it won't be written to syslog, console...
    either.
    
    When a native, syslog, or standard output log message is received,
    systemd-journald will process it if it matches against at least one
    allowed pattern (if any) and none of the denied patterns (if any).

---
+++ src/journal/journald-client.c
@@ -0,0 +1,27 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+
+#include "journald-client.h"
+#include "pcre2-util.h"
+
+int client_context_check_keep_log(ClientContext *c, const char *message, size_t len) {
+        bool matched_allowlist = false;
+        pcre2_code *regex;
+        int r;
+
+        if (!c || !message)
+                return true;
+
+        SET_FOREACH(regex, c->log_filter_denied_patterns) {
+                r = pattern_matches_and_log(regex, message, len, NULL);
+                if (r > 0)
+                        return false;
+        }
+
+        SET_FOREACH(regex, c->log_filter_allowed_patterns) {
+                r = pattern_matches_and_log(regex, message, len, NULL);
+                if (r > 0)
+                        matched_allowlist = true;
+        }
+
+        return set_isempty(c->log_filter_allowed_patterns) || matched_allowlist;
+}
