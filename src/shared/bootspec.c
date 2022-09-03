932f6b2755784f5f6cd425b47276e3b5d8d9f6be
Jan Janssen <medhefgo@web.de>
2022-09-03 23:07:44 +0200

    bootctl: Use tree output for entry paths
    People apparently expect the bootloader to magically know the userspace
    filesystem tree and get confused when boot files aren't found because of
    this. Printing the root of entry paths will hopefully make it easier for
    people to understand a bad configuration.
    
    Example output:
             type: Boot Loader Specification Type #1 (.conf)
            title: Arch Linux (selected)
               id: arch.conf
           source: /boot/loader/entries/arch.conf
            linux: /boot
                   └─/vmlinuz-linux
           initrd: /boot
                   └─/intel-ucode.img
                   └─/initramfs-linux.img
    
             type: Boot Loader Specification Type #1 (.conf)
            title: Arch Linux (LTS) (not reported/new)
               id: arch-lts.conf
           source: /boot/loader/entries/arch-lts.conf
          version: LTS
            linux: /boot
                   └─/vmlinuz-linux-lts (No such file or directory)
           initrd: /boot
                   └─/intel-ucode.img
                   └─/initramfs-linux-lts.img (No such file or directory)

--- src/shared/bootspec.c
+++ src/shared/bootspec.c
@@ -727,7 +727,7 @@ static int boot_entry_load_unified(
         if (!tmp.root)
                 return log_oom();
 
-        tmp.kernel = strdup(skip_leading_chars(k, "/"));
+        tmp.kernel = path_make_absolute(k, "/");
         if (!tmp.kernel)
                 return log_oom();
 
@@ -1288,7 +1288,11 @@ BootEntry* boot_config_find_entry(BootConfig *config, const char *id) {
 static void boot_entry_file_list(const char *field, const char *root, const char *p, int *ret_status) {
         int status = boot_entry_file_check(root, p);
 
-        printf("%13s%s ", strempty(field), field ? ":" : " ");
+        if (field)
+                printf("%13s: %s\n", field, root);
+
+        printf("%13s  %s", "", special_glyph(SPECIAL_GLYPH_TREE_RIGHT));
+
         if (status < 0) {
                 errno = -status;
                 printf("%s%s%s (%m)\n", ansi_highlight_red(), p, ansi_normal());
