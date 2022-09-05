a307686e1cc49493f8840160677fb817de38e1dd
Thierry Martin <tmartin@haproxy.com>
2022-09-05 15:02:06 +0200

    nspawn: container network interface naming
    systemd-nspawn now optionally supports colon-separated pair of
    host interface name and container interface name for --network-macvlan, --network-ipvlan and --network-interface options.
    Also supported in .nspawn configuration files (i.e Interface=, MACVLAN=, IPVLAN= parameters).

--- src/nspawn/nspawn-network.h
+++ src/nspawn/nspawn-network.h
@@ -13,11 +13,17 @@ int setup_veth_extra(const char *machine_name, pid_t pid, char **pairs);
 int setup_bridge(const char *veth_name, const char *bridge_name, bool create);
 int remove_bridge(const char *bridge_name);
 
-int setup_macvlan(const char *machine_name, pid_t pid, char **ifaces);
-int setup_ipvlan(const char *machine_name, pid_t pid, char **ifaces);
+int setup_macvlan(const char *machine_name, pid_t pid, char **pairs);
+int setup_ipvlan(const char *machine_name, pid_t pid, char **pairs);
 
-int move_network_interfaces(int netns_fd, char **ifaces);
+int move_network_interfaces(int netns_fd, char **pairs);
 
 int veth_extra_parse(char ***l, const char *p);
 
 int remove_veth_links(const char *primary, char **pairs);
+
+int network_interface_parse_and_test(char ***l, const char *p);
+
+int network_macvlan_parse_and_test(char ***l, const char *p);
+
+int network_ipvlan_parse_and_test(char ***l, const char *p);
