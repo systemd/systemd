76a81e995cf8e5d72e81d0086b374bf7464b293d
Daan De Meyer <daan.j.demeyer@gmail.com>
2022-09-06 00:45:32 +0200

    repart: Add support for formatting verity partitions
    This commit adds a new Verity= setting to repart definition files
    with two possible values: "data" and "hash".
    
    If Verity= is set to "data", repart works as before, and populates
    the partition with the content from CopyBlocks= or CopyFiles=.
    
    If Verity= is set to "hash", repart will try to find a matching
    data partition with Verity=data and equal values for CopyBlocks=
    or CopyFiles=, Format= and MakeDirectories=. If a matching data
    partition is found, repart will generate verity hashes for that
    data partition in the verity partition. The UUID of the data
    partition is set to the first 128 bits of the verity root hash. The
    UUID of the hashes partition is set to the final 128 bits of the
    verity root hash.
    
    Fixes #24559

--- test/units/testsuite-58.sh
+++ test/units/testsuite-58.sh
@@ -598,6 +598,46 @@ EOF
     assert_in "$imgs/zero1 : start=        2048, size=       20480, type=${root_guid}, uuid=00000000-0000-0000-0000-000000000000" "$output"
 }
 
+test_verity() {
+    local defs imgs output
+
+    defs="$(mktemp --directory "/tmp/test-repart.XXXXXXXXXX")"
+    imgs="$(mktemp --directory "/var/tmp/test-repart.XXXXXXXXXX")"
+    # shellcheck disable=SC2064
+    trap "rm -rf '$defs' '$imgs'" RETURN
+
+    cat >"$defs/root.conf" <<EOF
+[Partition]
+Type=root-${architecture}
+CopyFiles=${defs}
+Verity=data
+VerityName=root
+EOF
+
+    cat >"$defs/verity.conf" <<EOF
+[Partition]
+Type=root-${architecture}-verity
+Verity=hash
+VerityName=root
+EOF
+
+    output=$(systemd-repart --definitions="$defs" \
+                            --seed="$seed" \
+                            --dry-run=no \
+                            --empty=create \
+                            --size=auto \
+                            --json=pretty \
+                            "$imgs/verity" | tee proc/self/fd/2)
+
+    roothash=$(echo "$output" | jq -r ".[] | select(.type == 'root-${architecture}-verity') | .roothash")
+
+     # Check that we can dissect, mount and unmount a repart verity image.
+
+    systemd-dissect "$imgs/verity" --root-hash "$roothash"
+    systemd-dissect "$imgs/verity" --root-hash "$roothash" -M "$imgs/mnt"
+    systemd-dissect "$imgs/verity" --root-hash "$roothash" -U "$imgs/mnt"
+}
+
 test_sector() {
     local defs imgs output loop
     local start size ratio
@@ -664,6 +704,7 @@ test_copy_blocks
 test_unaligned_partition
 test_issue_21817
 test_zero_uuid
+test_verity
 
 # Valid block sizes on the Linux block layer are >= 512 and <= PAGE_SIZE, and
 # must be powers of 2. Which leaves exactly four different ones to test on
