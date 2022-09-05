df7cdf3eaf57f10cda139ac693efc52f8ca2d927
Yu Watanabe <watanabe.yu+github@gmail.com>
2022-09-06 05:01:34 +0900

    loop-util: move device_has_block_children() to blockdev-util.c
    As the function is not only for loopback block device.
    
    No actual code changes, just refactoring.

--- src/shared/blockdev-util.h
+++ src/shared/blockdev-util.h
@@ -37,3 +37,4 @@ int block_device_add_partition(int fd, const char *name, int nr, uint64_t start,
 int block_device_remove_partition(int fd, const char *name, int nr);
 int block_device_resize_partition(int fd, int nr, uint64_t start, uint64_t size);
 int block_device_remove_all_partitions(sd_device *dev, int fd);
+int block_device_has_partitions(sd_device *dev);
