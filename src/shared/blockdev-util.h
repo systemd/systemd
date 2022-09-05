fa48cbb3355ae556840a6022d5c7f73446dee60e
Yu Watanabe <watanabe.yu+github@gmail.com>
2022-09-06 04:46:04 +0900

    blockdev-util: make block_device_remove_all_partitions() take sd_device object
    Then, it is not necessary to recreate sd_device object when we already
    have.

--- src/shared/blockdev-util.h
+++ src/shared/blockdev-util.h
@@ -3,6 +3,8 @@
 
 #include <sys/types.h>
 
+#include "sd-device.h"
+
 #include "macro.h"
 #include "stdio-util.h"
 #include "string-util.h"
@@ -34,4 +36,4 @@ int path_get_whole_disk(const char *path, bool backing, dev_t *ret);
 int block_device_add_partition(int fd, const char *name, int nr, uint64_t start, uint64_t size);
 int block_device_remove_partition(int fd, const char *name, int nr);
 int block_device_resize_partition(int fd, int nr, uint64_t start, uint64_t size);
-int block_device_remove_all_partitions(int fd);
+int block_device_remove_all_partitions(sd_device *dev, int fd);
