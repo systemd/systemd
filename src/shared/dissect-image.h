f94b759fda859c578ded138c86c57d7e1a4786ae
Yu Watanabe <watanabe.yu+github@gmail.com>
2022-09-10 05:44:27 +0900

    dissect-image: open dissected or decrypted partitions and mount through the file descriptor
    If multiple services with the same encrypted image are simultaneously
    starting, one may deactivate the dm device while others using it.
    Or, similary, after (regular) partitions are dissected, another process
    may try to remove them before we mount them.
    
    To prevent such situations, let's keep the dissected and decrypted
    partitions opened. Then, use the file descriptors when we mount the
    partitions.
    
    Fixes #24617.

--- src/shared/dissect-image.h
+++ src/shared/dissect-image.h
@@ -29,6 +29,7 @@ struct DissectedPartition {
         char *decrypted_node;
         char *decrypted_fstype;
         char *mount_options;
+        int mount_node_fd;
         uint64_t size;
         uint64_t offset;
 };
@@ -37,6 +38,7 @@ struct DissectedPartition {
         ((DissectedPartition) {                                         \
                 .partno = -1,                                           \
                 .architecture = _ARCHITECTURE_INVALID,                  \
+                .mount_node_fd = -1,                                    \
         })
 
 typedef enum PartitionDesignator {
@@ -207,7 +209,9 @@ typedef enum DissectImageFlags {
         DISSECT_IMAGE_GROWFS                   = 1 << 18, /* Grow file systems in partitions marked for that to the size of the partitions after mount */
         DISSECT_IMAGE_MOUNT_IDMAPPED           = 1 << 19, /* Mount mounts with kernel 5.12-style userns ID mapping, if file system type doesn't support uid=/gid= */
         DISSECT_IMAGE_MANAGE_PARTITION_DEVICES = 1 << 20, /* Manage partition devices, e.g. probe each partition in more detail */
-        DISSECT_IMAGE_BLOCK_DEVICE             = DISSECT_IMAGE_MANAGE_PARTITION_DEVICES,
+        DISSECT_IMAGE_OPEN_PARTITION_DEVICES   = 1 << 21, /* Open dissected partitions and decrypted partitions */
+        DISSECT_IMAGE_BLOCK_DEVICE             = DISSECT_IMAGE_MANAGE_PARTITION_DEVICES |
+                                                 DISSECT_IMAGE_OPEN_PARTITION_DEVICES,
 } DissectImageFlags;
 
 struct DissectedImage {
