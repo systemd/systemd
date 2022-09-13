2e7700788e200f497d998e6b7f953d57a4ac2e5c
Quentin Deslandes <qd@naccy.de>
2022-09-13 16:15:13 +0100

    journal: filter log based on LogFilterPatterns
    Use LogFilterPatterns from the unit's cgroup xattr in order to keep or
    discard log messages before writing them to the journal.
    When a log message is discarded, it won't be written to syslog, console...
    either.
    
    When a native, syslog, or standard output log message is received,
    systemd-journald will process it if it matches against at least one
    allowed pattern (if any) and none of the denied patterns (if any).

--- src/journal/journald-context.h
+++ src/journal/journald-context.h
@@ -7,6 +7,7 @@
 
 #include "sd-id128.h"
 
+#include "set.h"
 #include "time-util.h"
 
 typedef struct ClientContext ClientContext;
@@ -55,6 +56,9 @@ struct ClientContext {
 
         usec_t log_ratelimit_interval;
         unsigned log_ratelimit_burst;
+
+        Set *log_filter_allowed_patterns;
+        Set *log_filter_denied_patterns;
 };
 
 int client_context_get(
