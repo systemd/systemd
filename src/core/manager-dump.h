610a518fdea65caa6ccbf98e4443e380d5ac7d3f
Franck Bui <fbui@suse.com>
2022-09-13 16:13:16 +0200

    analyze: extend the dump command to accept patterns
    When passing patterns, the dump command skip the version of the manager as well
    as the features and the timestamps.

--- src/core/manager-dump.h
+++ src/core/manager-dump.h
@@ -5,8 +5,24 @@
 
 #include "manager.h"
 
-void manager_dump_jobs(Manager *s, FILE *f, const char *prefix);
-void manager_dump_units(Manager *s, FILE *f, const char *prefix);
-void manager_dump(Manager *s, FILE *f, const char *prefix);
-int manager_get_dump_string(Manager *m, char **ret);
+void manager_dump_jobs_by_patterns(Manager *s, FILE *f, char **patterns, const char *prefix);
+void manager_dump_units_by_patterns(Manager *s, FILE *f, char **patterns, const char *prefix);
+void manager_dump_by_patterns(Manager *s, FILE *f, char **patterns, const char *prefix);
+int manager_get_dump_string_by_patterns(Manager *m, char **patterns, char **ret);
 void manager_test_summary(Manager *m);
+
+static inline void manager_dump_jobs(Manager *s, FILE *f, const char *prefix) {
+        manager_dump_jobs_by_patterns(s, f, NULL, prefix);
+}
+
+static inline void manager_dump_units(Manager *s, FILE *f, const char *prefix) {
+        manager_dump_units_by_patterns(s, f, NULL, prefix);
+}
+
+static inline void manager_dump(Manager *m, FILE *f, const char *prefix) {
+        manager_dump_by_patterns(m, f, NULL, prefix);
+}
+
+static inline int manager_get_dump_string(Manager *m, char **ret) {
+        return manager_get_dump_string_by_patterns(m, NULL, ret);
+}
