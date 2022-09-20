6848f21b8e1e2998c0c47cf4441bc6c17f8d4469
Frantisek Sumsal <frantisek@sumsal.cz>
2022-09-20 19:12:18 +0200

    test: don't overwrite existing $QEMU_OPTIONS

--- test/TEST-53-ISSUE-16347/test.sh
+++ test/TEST-53-ISSUE-16347/test.sh
@@ -5,7 +5,7 @@ set -e
 TEST_DESCRIPTION="test timer units when initial clock is ahead"
 TEST_NO_NSPAWN=1
 
-QEMU_OPTIONS="-rtc base=$(date -u +%Y-%m-%dT%H:%M:%S -d '+3 days')"
+QEMU_OPTIONS+=" -rtc base=$(date -u +%Y-%m-%dT%H:%M:%S -d '+3 days')"
 
 # shellcheck source=test/test-functions
 . "${TEST_BASE_DIR:?}/test-functions"
