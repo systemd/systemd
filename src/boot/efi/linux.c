dcc726020bd7b5b2e02d61addfd64fe64b680978
Jan Janssen <medhefgo@web.de>
2022-09-21 12:56:20 +0200

    stub: Allow loading unsigned kernel images

--- src/boot/efi/linux.c
+++ src/boot/efi/linux.c
@@ -15,9 +15,61 @@
 #include "linux.h"
 #include "missing_efi.h"
 #include "pe.h"
+#include "secure-boot.h"
 #include "util.h"
 
-EFI_STATUS load_trusted_image(EFI_HANDLE parent, const void *source, size_t len, EFI_HANDLE *ret_image) {
+static struct TrustedState {
+        EFI_SECURITY_FILE_AUTHENTICATION_STATE orig_auth;
+        EFI_SECURITY2_FILE_AUTHENTICATION orig_auth2;
+        const void *buffer;
+        size_t size;
+        const EFI_DEVICE_PATH *device_path;
+} *trusted = NULL;
+
+static EFIAPI EFI_STATUS security_hook(
+                const EFI_SECURITY_ARCH_PROTOCOL *this,
+                uint32_t authentication_status,
+                const EFI_DEVICE_PATH *file) {
+
+        EFI_STATUS err;
+
+        assert(trusted);
+        assert(trusted->orig_auth);
+
+        err = trusted->orig_auth(this, authentication_status, file);
+        if (err == EFI_SUCCESS)
+                return EFI_SUCCESS;
+
+        if (file == trusted->device_path)
+                return EFI_SUCCESS;
+
+        return err;
+}
+
+static EFIAPI EFI_STATUS security2_hook(
+                const EFI_SECURITY2_ARCH_PROTOCOL *this,
+                const EFI_DEVICE_PATH *device_path,
+                void *file_buffer,
+                size_t file_size,
+                BOOLEAN boot_policy) {
+
+        EFI_STATUS err;
+
+        assert(trusted);
+        assert(trusted->orig_auth2);
+
+        err = trusted->orig_auth2(this, device_path, file_buffer, file_size, boot_policy);
+        if (err == EFI_SUCCESS)
+                return EFI_SUCCESS;
+
+        if (file_buffer == trusted->buffer && file_size == trusted->size && device_path == trusted->device_path)
+                return EFI_SUCCESS;
+
+        return err;
+}
+
+EFI_STATUS load_trusted_image(
+                EFI_HANDLE parent, const void *source, size_t len, EFI_HANDLE *ret_image) {
         EFI_STATUS err;
 
         /* We want to be nice and provide a device path. Since the kernel is embedded into our stub we can
@@ -28,7 +80,46 @@ EFI_STATUS load_trusted_image(EFI_HANDLE parent, const void *source, size_t len,
         if (err != EFI_SUCCESS)
                 return log_error_status_stall(err, u"Error getting loaded image device path: %r", err);
 
-        return BS->LoadImage(false, parent, loaded_dp, (void *) source, len, ret_image);
+        if (!secure_boot_enabled())
+                return BS->LoadImage(false, parent, loaded_dp, (void *) source, len, ret_image);
+
+        /* We want to support unsigned kernel images (which is safe to do since it is embedded in this stub,
+         * which is already running and therefore trusted). We hook into security arch protocol to trick the
+         * firmware into trusting our image.
+         *
+         * This is really a nasty hack, but better than seeing a security violation error. Note that these
+         * protocols are technically internal to the platform and not some kind of public API. They may not
+         * even be available (hence why we do this opportunistically). */
+
+        EFI_SECURITY_ARCH_PROTOCOL *security = NULL;
+        EFI_SECURITY2_ARCH_PROTOCOL *security2 = NULL;
+
+        (void) BS->LocateProtocol((EFI_GUID *) EFI_SECURITY_ARCH_PROTOCOL_GUID, NULL, (void **) &security);
+        (void) BS->LocateProtocol((EFI_GUID *) EFI_SECURITY2_ARCH_PROTOCOL_GUID, NULL, (void **) &security2);
+
+        struct TrustedState state = {
+                .buffer = source,
+                .size = len,
+                .device_path = loaded_dp,
+                .orig_auth = security ? security->FileAuthenticationState : NULL,
+                .orig_auth2 = security2 ? security2->FileAuthentication : NULL,
+        };
+
+        trusted = &state;
+        if (security)
+                security->FileAuthenticationState = security_hook;
+        if (security2)
+                security2->FileAuthentication = security2_hook;
+
+        err = BS->LoadImage(false, parent, loaded_dp, (void *) source, len, ret_image);
+
+        if (security)
+                security->FileAuthenticationState = state.orig_auth;
+        if (security2)
+                security2->FileAuthentication = state.orig_auth2;
+        trusted = NULL;
+
+        return err;
 }
 
 EFI_STATUS linux_exec(
