498918f8c8c241b1171613907d8bdbd8b008d0ed
Lennart Poettering <lennart@poettering.net>
2022-09-22 14:21:46 +0200

    run: make --working-directory= work for --scope too
    This sounds like a more user-friendly alternative to #24780

--- src/run/run.c
+++ src/run/run.c
@@ -1529,6 +1529,9 @@ static int start_transient_scope(sd_bus *bus) {
                         return log_error_errno(errno, "Failed to change UID to " UID_FMT ": %m", uid);
         }
 
+        if (arg_working_directory && chdir(arg_working_directory) < 0)
+                return log_error_errno(errno, "Failed to change directory to '%s': %m", arg_working_directory);
+
         env = strv_env_merge(environ, user_env, arg_environment);
         if (!env)
                 return log_oom();
