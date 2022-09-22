d3dadc2c1e472f24e0e28fa080f2c3a89bf6a756
Yu Watanabe <watanabe.yu+github@gmail.com>
2022-09-22 13:08:32 +0900

    test: add more tests for StateDirectory= with DynamicUser=
    This also moves the check for writable paths from test-execute to TEST-34.
    
    Closes #10337.

--- test/units/testsuite-34.sh
+++ test/units/testsuite-34.sh
@@ -5,9 +5,14 @@ set -o pipefail
 
 systemd-analyze log-level debug
 
-function test_directory() {
+test_directory() {
     local directory="$1"
     local path="$2"
+
+    # cleanup for previous invocation
+    for i in xxx xxx2 yyy zzz x:yz x:yz2; do
+        rm -rf "${path:?}/${i}" "${path:?}/private/${i}"
+    done
 
     # Set everything up without DynamicUser=1
 
@@ -15,7 +20,7 @@ function test_directory() {
     systemd-run --wait -p RuntimeDirectoryPreserve=yes -p DynamicUser=0 -p "${directory}"=zzz test -f "${path}"/zzz/test
     systemd-run --wait -p RuntimeDirectoryPreserve=yes -p DynamicUser=0 -p "${directory}"=zzz -p TemporaryFileSystem="${path}" test -f "${path}"/zzz/test
     systemd-run --wait -p RuntimeDirectoryPreserve=yes -p DynamicUser=0 -p "${directory}"=zzz:yyy test -f "${path}"/yyy/test
-    systemd-run --wait -p RuntimeDirectoryPreserve=yes -p DynamicUser=0 -p "${directory}"=zzz:xxx -p TemporaryFileSystem="${path}" test -f "${path}"/xxx/test
+    systemd-run --wait -p RuntimeDirectoryPreserve=yes -p DynamicUser=0 -p "${directory}=zzz:xxx zzz:xxx2" -p TemporaryFileSystem="${path}" bash -c "test -f ${path}/xxx/test && test -f ${path}/xxx2/test"
     systemd-run --wait -p RuntimeDirectoryPreserve=yes -p DynamicUser=0 -p "${directory}"=zzz:xxx -p TemporaryFileSystem="${path}":ro test -f "${path}"/xxx/test
     systemd-run --wait -p RuntimeDirectoryPreserve=yes -p DynamicUser=0 -p "${directory}"=zzz test -f "${path}"/zzz/test-missing \
         && { echo 'unexpected success'; exit 1; }
@@ -23,27 +28,40 @@ function test_directory() {
     test -d "${path}"/zzz
     test ! -L "${path}"/zzz
     test ! -e "${path}"/private/zzz
+
+    test ! -e "${path}"/xxx
+    test ! -e "${path}"/private/xxx
+    test ! -e "${path}"/xxx2
+    test ! -e "${path}"/private/xxx2
+    test -L "${path}"/yyy
+    test ! -e "${path}"/private/yyy
+
     test -f "${path}"/zzz/test
-    test ! -f "${path}"/zzz/test-missing
+    test ! -e "${path}"/zzz/test-missing
 
     # Convert to DynamicUser=1
 
     systemd-run --wait -p RuntimeDirectoryPreserve=yes -p DynamicUser=1 -p "${directory}"=zzz test -f "${path}"/zzz/test
     systemd-run --wait -p RuntimeDirectoryPreserve=yes -p DynamicUser=1 -p "${directory}"=zzz -p TemporaryFileSystem="${path}" test -f "${path}"/zzz/test
     systemd-run --wait -p RuntimeDirectoryPreserve=yes -p DynamicUser=1 -p "${directory}"=zzz:yyy test -f "${path}"/yyy/test
-    systemd-run --wait -p RuntimeDirectoryPreserve=yes -p DynamicUser=1 -p "${directory}"=zzz:xxx -p TemporaryFileSystem="${path}" test -f "${path}"/xxx/test
+    systemd-run --wait -p RuntimeDirectoryPreserve=yes -p DynamicUser=1 -p "${directory}=zzz:xxx zzz:xxx2" \
+                -p TemporaryFileSystem="${path}" -p EnvironmentFile=-/usr/lib/systemd/systemd-asan-env bash -c "test -f ${path}/xxx/test && test -f ${path}/xxx2/test"
     systemd-run --wait -p RuntimeDirectoryPreserve=yes -p DynamicUser=1 -p "${directory}"=zzz:xxx -p TemporaryFileSystem="${path}":ro test -f "${path}"/xxx/test
     systemd-run --wait -p RuntimeDirectoryPreserve=yes -p DynamicUser=1 -p "${directory}"=zzz test -f "${path}"/zzz/test-missing \
         && { echo 'unexpected success'; exit 1; }
 
     test -L "${path}"/zzz
-    test -L "${path}"/yyy
     test -d "${path}"/private/zzz
-    test ! -L "${path}"/private/xxx
-    test ! -L "${path}"/xxx
+
+    test ! -e "${path}"/xxx
+    test ! -e "${path}"/private/xxx
+    test ! -e "${path}"/xxx2
+    test ! -e "${path}"/private/xxx2
+    test -L "${path}"/yyy # previous symlink is not removed
+    test ! -e "${path}"/private/yyy
 
     test -f "${path}"/zzz/test
-    test ! -f "${path}"/zzz/test-missing
+    test ! -e "${path}"/zzz/test-missing
 
     # Convert back
 
@@ -55,6 +73,20 @@ function test_directory() {
     systemd-run --wait -p RuntimeDirectoryPreserve=yes -p DynamicUser=0 -p "${directory}"=zzz:xxx -p TemporaryFileSystem="${path}":ro test -f "${path}"/xxx/test
     systemd-run --wait -p RuntimeDirectoryPreserve=yes -p DynamicUser=0 -p "${directory}"=zzz test -f "${path}"/zzz/test-missing \
         && { echo 'unexpected success'; exit 1; }
+
+    test -d "${path}"/zzz
+    test ! -L "${path}"/zzz
+    test ! -e "${path}"/private/zzz
+
+    test ! -e "${path}"/xxx
+    test ! -e "${path}"/private/xxx
+    test ! -e "${path}"/xxx2
+    test ! -e "${path}"/private/xxx2
+    test -L "${path}"/yyy
+    test ! -e "${path}"/private/yyy
+
+    test -f "${path}"/zzz/test
+    test ! -e "${path}"/zzz/test-missing
 
     # Exercise the unit parsing paths too
     cat >/run/systemd/system/testservice-34.service <<EOF
@@ -71,23 +103,60 @@ EOF
     systemctl start --wait testservice-34.service
 
     test -d "${path}"/zzz
-    test ! -L "${path}"/xxx
-    test ! -L "${path}"/xxx2
-    test ! -L "${path}"/private/xxx
-    test ! -L "${path}"/private/xxx2
-    test -L "${path}"/yyy
     test ! -L "${path}"/zzz
     test ! -e "${path}"/private/zzz
-    test -f "${path}"/zzz/test
-    test ! -f "${path}"/zzz/test-missing
+
     test ! -L "${path}"/x:yz
     test ! -L "${path}"/x:yz2
+}
+
+test_check_writable() {
+    # cleanup for previous invocation
+    for i in aaa quux waldo xxx; do
+        rm -rf "/var/lib/$i" "/var/lib/private/$i"
+    done
+
+    cat >/run/systemd/system/testservice-34-check-writable.service <<EOF
+[Unit]
+Description=Check writable directories when DynamicUser= with StateDirectory=
+
+[Service]
+# Relevant only for sanitizer runs
+EnvironmentFile=-/usr/lib/systemd/systemd-asan-env
+
+Type=oneshot
+DynamicUser=yes
+StateDirectory=waldo quux/pief aaa/bbb aaa aaa/ccc xxx/yyy:aaa/111 xxx:aaa/222 xxx/zzz:aaa/333
+
+# Make sure that the state directories are really the only writable directory besides the obvious candidates
+ExecStart=bash -c ' \
+    set -eux; \
+    set -o pipefail; \
+    declare -a writable_dirs; \
+    readarray -t writable_dirs < <(find / \\( -path /var/tmp -o -path /tmp -o -path /proc -o -path /dev/mqueue -o -path /dev/shm -o \
+                                              -path /sys/fs/bpf -o -path /dev/.lxc -o -path /sys/devices/system/cpu \\) \
+                                   -prune -o -type d -writable -print 2>/dev/null | sort -u); \
+    [[ "\$\${#writable_dirs[@]}" == "8" ]]; \
+    [[ "\$\${writable_dirs[0]}" == "/var/lib/private/aaa" ]]; \
+    [[ "\$\${writable_dirs[1]}" == "/var/lib/private/aaa/bbb" ]]; \
+    [[ "\$\${writable_dirs[2]}" == "/var/lib/private/aaa/ccc" ]]; \
+    [[ "\$\${writable_dirs[3]}" == "/var/lib/private/quux/pief" ]]; \
+    [[ "\$\${writable_dirs[4]}" == "/var/lib/private/waldo" ]]; \
+    [[ "\$\${writable_dirs[5]}" == "/var/lib/private/xxx" ]]; \
+    [[ "\$\${writable_dirs[6]}" == "/var/lib/private/xxx/yyy" ]]; \
+    [[ "\$\${writable_dirs[7]}" == "/var/lib/private/xxx/zzz" ]]; \
+'
+EOF
+    systemctl daemon-reload
+    systemctl start testservice-34-check-writable.service
 }
 
 test_directory "StateDirectory" "/var/lib"
 test_directory "RuntimeDirectory" "/run"
 test_directory "CacheDirectory" "/var/cache"
 test_directory "LogsDirectory" "/var/log"
+
+test_check_writable
 
 systemd-analyze log-level info
 
