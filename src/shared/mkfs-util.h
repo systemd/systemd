e3131645ba6928a57ecbc0b17bb5f325756592ab
Daan De Meyer <daan.j.demeyer@gmail.com>
2022-09-22 21:28:58 +0200

    repart: Extend squashfs logic to all read-only filesystems
    The same logic will apply to every read-only filesystem that we
    might add support for in the future, so let's make this a bit more
    future proof.

--- src/shared/mkfs-util.h
+++ src/shared/mkfs-util.h
@@ -5,6 +5,13 @@
 
 #include "sd-id128.h"
 
+#include "strv.h"
+
 int mkfs_exists(const char *fstype);
+
+static inline bool filesystem_is_writable(const char *fstype) {
+        assert(fstype);
+        return !STR_IN_SET(fstype, "cramfs", "squashfs", "erofs");
+}
 
 int make_filesystem(const char *node, const char *fstype, const char *label, const char *root, sd_id128_t uuid, bool discard);
