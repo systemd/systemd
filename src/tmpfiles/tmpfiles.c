0e52e8c76d1b5f5351309010a433bc44a7bf110a
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2022-09-26 14:22:49 +0200

    tmpfiles: fix detection of unitialized-/etc
    In 6ec4c852c910b1aca649e87ba3143841334f01fa I changed specifier_machine_id() to
    return EUNATCH, but the code catching in tmpfiles wasn't adjusted.
    
    Instead of translating errors at various levels, let the "original" errno value
    (whatever was returned by the low-level reading function) propagate all the way
    to the logging function, and only check which errnos to suppress there. This
    makes it easier to follow the flow of data through all the layers. Also, we
    don't need to provide wrapper functions for each place where we want to do the
    supression.

--- src/tmpfiles/tmpfiles.c
+++ src/tmpfiles/tmpfiles.c
@@ -225,17 +225,11 @@ static const char *const creation_mode_verb_table[_CREATION_MODE_MAX] = {
 
 DEFINE_PRIVATE_STRING_TABLE_LOOKUP_TO_STRING(creation_mode_verb, CreationMode);
 
-static int specifier_machine_id_safe(char specifier, const void *data, const char *root, const void *userdata, char **ret) {
-        int r;
-
-        /* If /etc/machine_id is missing or empty (e.g. in a chroot environment) return a recognizable error
-         * so that the caller can skip the rule gracefully. */
-
-        r = specifier_machine_id(specifier, data, root, userdata, ret);
-        if (IN_SET(r, -ENOENT, -ENOMEDIUM))
-                return -ENXIO;
-
-        return r;
+static inline bool ERRNO_IS_NOINFO(int r) {
+        return IN_SET(abs(r),
+                      EUNATCH,
+                      ENOMEDIUM,
+                      ENXIO);
 }
 
 static int specifier_directory(char specifier, const void *data, const char *root, const void *userdata, char **ret) {
@@ -320,15 +314,15 @@ static int user_config_paths(char*** ret) {
                 return r;
 
         r = xdg_user_config_dir(&persistent_config, "/user-tmpfiles.d");
-        if (r < 0 && r != -ENXIO)
+        if (r < 0 && !ERRNO_IS_NOINFO(r))
                 return r;
 
         r = xdg_user_runtime_dir(&runtime_config, "/user-tmpfiles.d");
-        if (r < 0 && r != -ENXIO)
+        if (r < 0 && !ERRNO_IS_NOINFO(r))
                 return r;
 
         r = xdg_user_data_dir(&data_home, "/user-tmpfiles.d");
-        if (r < 0 && r != -ENXIO)
+        if (r < 0 && !ERRNO_IS_NOINFO(r))
                 return r;
 
         r = strv_extend_strv_concat(&res, config_dirs, "/user-tmpfiles.d");
@@ -3151,7 +3145,7 @@ static int parse_line(
                 { 'B', specifier_os_build_id,     NULL },
                 { 'H', specifier_hostname,        NULL },
                 { 'l', specifier_short_hostname,  NULL },
-                { 'm', specifier_machine_id_safe, NULL },
+                { 'm', specifier_machine_id,      NULL },
                 { 'o', specifier_os_id,           NULL },
                 { 'v', specifier_kernel_release,  NULL },
                 { 'w', specifier_os_version_id,   NULL },
@@ -3231,7 +3225,7 @@ static int parse_line(
         i.try_replace = try_replace;
 
         r = specifier_printf(path, PATH_MAX-1, specifier_table, arg_root, NULL, &i.path);
-        if (r == -ENXIO)
+        if (ERRNO_IS_NOINFO(r))
                 return log_unresolvable_specifier(fname, line);
         if (r < 0) {
                 if (IN_SET(r, -EINVAL, -EBADSLT))
@@ -3419,7 +3413,7 @@ static int parse_line(
         if (!unbase64) {
                 /* Do specifier expansion except if base64 mode is enabled */
                 r = specifier_expansion_from_arg(specifier_table, &i);
-                if (r == -ENXIO)
+                if (ERRNO_IS_NOINFO(r))
                         return log_unresolvable_specifier(fname, line);
                 if (r < 0) {
                         if (IN_SET(r, -EINVAL, -EBADSLT))
@@ -3435,9 +3429,10 @@ static int parse_line(
                         return log_syntax(NULL, LOG_ERR, fname, line, SYNTHETIC_ERRNO(EINVAL), "Credential name not valid: %s", i.argument);
 
                 r = read_credential(i.argument, &i.binary_argument, &i.binary_argument_size);
-                if (IN_SET(r, -ENXIO, -ENOENT)) {
+                if (ERRNO_IS_NOINFO(r)) {
                         /* Silently skip over lines that have no credentials passed */
-                        log_syntax(NULL, LOG_INFO, fname, line, 0, "Credential '%s' not specified, skipping line.", i.argument);
+                        log_syntax(NULL, LOG_INFO, fname, line, 0,
+                                   "Credential '%s' not specified, skipping line.", i.argument);
                         return 0;
                 }
                 if (r < 0)
