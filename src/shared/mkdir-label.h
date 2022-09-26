2b0257ee377eaebed6d4b9848c735c29536c9ab6
Daan De Meyer <daan.j.demeyer@gmail.com>
2022-09-26 11:19:03 +0200

    mkdir: Add mkdirat_parents()

--- src/shared/mkdir-label.h
+++ src/shared/mkdir-label.h
@@ -16,5 +16,8 @@ int mkdirat_safe_label(int dir_fd, const char *path, mode_t mode, uid_t uid, gid
 static inline int mkdir_safe_label(const char *path, mode_t mode, uid_t uid, gid_t gid, MkdirFlags flags) {
         return mkdirat_safe_label(AT_FDCWD, path, mode, uid, gid, flags);
 }
-int mkdir_parents_label(const char *path, mode_t mod);
+int mkdirat_parents_label(int dir_fd, const char *path, mode_t mod);
+static inline int mkdir_parents_label(const char *path, mode_t mod) {
+        return mkdirat_parents_label(AT_FDCWD, path, mod);
+}
 int mkdir_p_label(const char *path, mode_t mode);
