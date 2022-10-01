4124b03b8c8f4942358abaf3153bf55bbddd539c
Yu Watanabe <watanabe.yu+github@gmail.com>
2022-10-01 15:22:23 +0900

    sd-dhcp6-client: do not use implicit cast from integer to boolean

--- src/libsystemd-network/sd-dhcp6-client.c
+++ src/libsystemd-network/sd-dhcp6-client.c
@@ -779,7 +779,7 @@ int dhcp6_client_send_message(sd_dhcp6_client *client) {
         if (r < 0)
                 return r;
 
-        assert(client->duid_len);
+        assert(client->duid_len > 0);
         r = dhcp6_option_append(&opt, &optlen, SD_DHCP6_OPTION_CLIENTID,
                                 client->duid_len, &client->duid);
         if (r < 0)
