4a220ab0627212c5b5fa5045eecb29de064114fe
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2022-10-04 14:19:12 +0200

    shared/pam-util: add pam_syslog_errno() wrapper that sets errno
    So far our pam code was using strerror_safe(). But that's not a good approach,
    because strerror_safe() is not thread-safe, and the pam code is "library code"
    that should be thread-safe. In fact, the whole effort to use strerror() is
    unnecessary, because pam_syslog() is documented to support %m. The
    implementation in linux-pam simply uses vasprintf(). If we use %m too, we get
    rid of the issue. The wrapper sets errno temporarily from the argument.
    
    Apparently some PAM consumers run multiple PAM stacks in threads, so we should
    avoid non-thread-safe code.
    
    The new helper returns PAM_BUF_ERR for ENOMEM, and PAM_SERVICE_ERR in other
    cases. This may change the returned code in some cases, but I think a) it
    doesn't matter much, b) it's probably for the better. E.g. we might now return
    PAM_SERVICE_ERR if the dbus message is borked, and PAM_SERVICE_ERR seems
    appropriate.

--- src/shared/pam-util.h
+++ src/shared/pam-util.h
@@ -5,9 +5,22 @@
 
 #include "sd-bus.h"
 
-int pam_log_oom(pam_handle_t *handle);
-int pam_bus_log_create_error(pam_handle_t *handle, int r);
-int pam_bus_log_parse_error(pam_handle_t *handle, int r);
+int pam_syslog_errno(pam_handle_t *handle, int level, int error, const char *format, ...) _printf_(4,5);
+
+static inline int pam_log_oom(pam_handle_t *handle) {
+        /* This is like log_oom(), but uses PAM logging */
+        return pam_syslog_errno(handle, LOG_ERR, ENOMEM, "Out of memory.");
+}
+
+static inline int pam_bus_log_create_error(pam_handle_t *handle, int r) {
+        /* This is like bus_log_create_error(), but uses PAM logging */
+        return pam_syslog_errno(handle, LOG_ERR, r, "Failed to create bus message: %m");
+}
+
+static inline int pam_bus_log_parse_error(pam_handle_t *handle, int r) {
+        /* This is like bus_log_parse_error(), but uses PAM logging */
+        return pam_syslog_errno(handle, LOG_ERR, r, "Failed to parse bus message: %m");
+}
 
 int pam_acquire_bus_connection(pam_handle_t *handle, sd_bus **ret);
 int pam_release_bus_connection(pam_handle_t *handle);
