f5f5bc314d6c43bcabded6cbbea7c4df902a0ded
Michal Koutný <mkoutny@suse.com>
2022-10-07 18:21:36 +0200

    service: Do not apply oomd xattrs in delegated subtree
    This provision existed for the auxiliary `.control` subcgroups.
    We want users to provide explicit subcgroup path (also for control
    commands) and they should also specify oomd policy themselves in the
    delegated subtree. That is responsibility of a cgroup manager in the
    subtree though. Therefore don't configure (oomd) xattrs inside the
    delegated subtree when we create the subcgroup.

--- src/core/execute.c
+++ src/core/execute.c
@@ -5239,10 +5239,6 @@ int exec_spawn(Unit *unit,
                                         return log_unit_error_errno(unit, r, "Failed to switch control group '%s' to threaded mode: %m",
                                                                     subcgroup_path);
                         }
-
-                        /* Normally we would not propagate the oomd xattrs to children but since we created this
-                         * sub-cgroup internally we should do it. */
-                        cgroup_oomd_xattr_apply(unit, subcgroup_path);
                 }
         }
 
