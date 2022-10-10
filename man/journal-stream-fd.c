739e8b5b761a1797ebf96c0900135f9a836e9345
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2022-10-10 09:18:26 +0200

    man: use external .c files for three examples
    This way it's much easier to test that the code compiles without issues.
    It's also easier to edit the code.
    
    Indentation in one of the examples is reduced to two spaces. This is what we
    use in man pages to make them fit on screen better.

---
+++ man/journal-stream-fd.c
@@ -0,0 +1,28 @@
+/* SPDX-License-Identifier: CC0-1.0 */
+
+#include <syslog.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <systemd/sd-journal.h>
+#include <systemd/sd-daemon.h>
+
+int main(int argc, char *argv[]) {
+  int fd;
+  FILE *log;
+  fd = sd_journal_stream_fd("test", LOG_INFO, 1);
+  if (fd < 0) {
+    fprintf(stderr, "Failed to create stream fd: %s\n", strerror(-fd));
+    return 1;
+  }
+  log = fdopen(fd, "w");
+  if (!log) {
+    fprintf(stderr, "Failed to create file object: %m\n");
+    close(fd);
+    return 1;
+  }
+  fprintf(log, "Hello World!\n");
+  fprintf(log, SD_WARNING "This is a warning!\n");
+  fclose(log);
+  return 0;
+}
