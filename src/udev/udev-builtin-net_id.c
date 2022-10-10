eabace4496309717104cbf25545fff72410f9922
Charles Hardin <charles.hardin@chargepoint.com>
2022-10-10 13:30:10 -0700

    udev-builtin-net_id: support getting usb path and slot without pci
    Several asics have usb controllers that are platform devices and not
    children of a pci interface. These embedded systems should be able
    to enumerate interfaces by usb slot or path as well to support
    configurations based upon predictable interface names as needed.
    
    Signed-off-by: Charles Hardin <charles.hardin@chargepoint.com>

--- src/udev/udev-builtin-net_id.c
+++ src/udev/udev-builtin-net_id.c
@@ -1202,9 +1202,25 @@ static int builtin_net_id(sd_device *dev, sd_netlink **rtnl, int argc, char *arg
                 return 0;
         }
 
-        /* get PCI based path names, we compose only PCI based paths */
-        if (names_pci(dev, &info, &names) < 0)
+        /* get PCI based path names */
+        if (names_pci(dev, &info, &names) < 0) {
+                /*
+                 * check for usb devices that are not off pci interfaces to
+                 * support various on-chip asics that have usb ports
+                 */
+                if (names_usb(dev, &names) >= 0 && names.type == NET_USB) {
+                        char str[ALTIFNAMSIZ];
+
+                        if (snprintf_ok(str, sizeof str, "%s%s", prefix, names.usb_ports))
+                                udev_builtin_add_property(dev, test, "ID_NET_NAME_PATH", str);
+
+                        if (snprintf_ok(str, sizeof str, "%s%s", prefix, names.usb_ports))
+                                udev_builtin_add_property(dev, test, "ID_NET_NAME_SLOT", str);
+                        return 0;
+                }
+
                 return 0;
+        }
 
         /* plain PCI device */
         if (names.type == NET_PCI) {
