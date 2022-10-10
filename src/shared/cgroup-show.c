527f18d2f6d374ce9243445c1519fa69adb6c8c7
Lennart Poettering <lennart@poettering.net>
2022-10-10 11:06:56 +0200

    tree-wide: add ERRNO_IS_XATTR_ABSENT() helper
    We check the same list of error codes on various xattr operations, and
    we should on some more. Add a common helper for this purpose.

--- src/shared/cgroup-show.c
+++ src/shared/cgroup-show.c
@@ -135,12 +135,12 @@ static int is_delegated(int cgfd, const char *path) {
         assert(cgfd >= 0 || path);
 
         r = getxattr_malloc(cgfd < 0 ? path : FORMAT_PROC_FD_PATH(cgfd), "trusted.delegate", &b);
-        if (r == -ENODATA) {
+        if (r < 0 && ERRNO_IS_XATTR_ABSENT(r)) {
                 /* If the trusted xattr isn't set (preferred), then check the untrusted one. Under the
                  * assumption that whoever is trusted enough to own the cgroup, is also trusted enough to
                  * decide if it is delegated or not this should be safe. */
                 r = getxattr_malloc(cgfd < 0 ? path : FORMAT_PROC_FD_PATH(cgfd), "user.delegate", &b);
-                if (r == -ENODATA)
+                if (r < 0 && ERRNO_IS_XATTR_ABSENT(r))
                         return false;
         }
         if (r < 0)
