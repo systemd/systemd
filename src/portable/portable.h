f858843d01a673baeaa86952d32b45748708fbc5
Luca Boccassi <bluca@debian.org>
2022-10-11 18:58:33 +0100

    portable: allow caller to override extension-release name check
    When the --force flag is used, do not insist that the extension-release
    file has to match the extension image name

--- src/portable/portable.h
+++ src/portable/portable.h
@@ -23,10 +23,11 @@ typedef struct PortableMetadata {
 typedef enum PortableFlags {
         PORTABLE_RUNTIME        = 1 << 0, /* Public API via DBUS, do not change */
         PORTABLE_FORCE_ATTACH   = 1 << 1, /* Public API via DBUS, do not change */
-        PORTABLE_PREFER_COPY    = 1 << 2,
-        PORTABLE_PREFER_SYMLINK = 1 << 3,
-        PORTABLE_REATTACH       = 1 << 4,
-        _PORTABLE_MASK_PUBLIC   = PORTABLE_RUNTIME | PORTABLE_FORCE_ATTACH,
+        PORTABLE_FORCE_SYSEXT   = 1 << 2, /* Public API via DBUS, do not change */
+        PORTABLE_PREFER_COPY    = 1 << 3,
+        PORTABLE_PREFER_SYMLINK = 1 << 4,
+        PORTABLE_REATTACH       = 1 << 5,
+        _PORTABLE_MASK_PUBLIC   = PORTABLE_RUNTIME | PORTABLE_FORCE_ATTACH | PORTABLE_FORCE_SYSEXT,
         _PORTABLE_TYPE_MAX,
         _PORTABLE_TYPE_INVALID  = -EINVAL,
 } PortableFlags;
@@ -66,7 +67,7 @@ DEFINE_TRIVIAL_CLEANUP_FUNC(PortableMetadata*, portable_metadata_unref);
 
 int portable_metadata_hashmap_to_sorted_array(Hashmap *unit_files, PortableMetadata ***ret);
 
-int portable_extract(const char *image, char **matches, char **extension_image_paths, PortableMetadata **ret_os_release, OrderedHashmap **ret_extension_releases, Hashmap **ret_unit_files, char ***ret_valid_prefixes, sd_bus_error *error);
+int portable_extract(const char *image, char **matches, char **extension_image_paths, PortableFlags flags, PortableMetadata **ret_os_release, OrderedHashmap **ret_extension_releases, Hashmap **ret_unit_files, char ***ret_valid_prefixes, sd_bus_error *error);
 
 int portable_attach(sd_bus *bus, const char *name_or_path, char **matches, const char *profile, char **extension_images, PortableFlags flags, PortableChange **changes, size_t *n_changes, sd_bus_error *error);
 int portable_detach(sd_bus *bus, const char *name_or_path, char **extension_image_paths, PortableFlags flags, PortableChange **changes, size_t *n_changes, sd_bus_error *error);
