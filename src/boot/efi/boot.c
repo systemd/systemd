1a9874905a900dc7d02bf4ef4c0a87931a5ad75c
Jan Janssen <medhefgo@web.de>
2022-10-12 09:39:59 +0200

    boot: Always reconnect all drivers
    Fixes: #23442

--- src/boot/efi/boot.c
+++ src/boot/efi/boot.c
@@ -2660,6 +2660,12 @@ EFI_STATUS efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE *sys_table) {
         /* Uncomment the next line if you need to wait for debugger. */
         // debug_break();
 
+        /* The firmware may skip initializing some devices for the sake of a faster boot. This is especially
+         * true for fastboot enabled firmwares. But this means that things we use like input devices or the
+         * xbootldr partition may not be available yet. Reconnect all drivers should hopefully make the
+         * firmware initialize everything we need. */
+        (void) reconnect_all_drivers();
+
         err = BS->OpenProtocol(image,
                         &LoadedImageProtocol,
                         (void **)&loaded_image,
