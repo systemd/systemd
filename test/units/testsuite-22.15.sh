2ea9204dc2ddf6348f4d7680acfd5d3023fcaa19
Luca Boccassi <bluca@debian.org>
2022-10-12 23:46:28 +0100

    tmpfiles: add factory reset type modifier
    If '@' is prefixed to an entry type, consider it only if --factory-reset=yes
    is passed to systemd-tmpfiles. Useful to remove files/directories only
    when a system is being reset.

---
+++ test/units/testsuite-22.15.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# SPDX-License-Identifier: LGPL-2.1-or-later
+#
+# Tests for the "@" factory reset modifier
+#
+set -eux
+set -o pipefail
+
+rm -rf /tmp/somedir
+mkdir -p /tmp/somedir
+echo bar > /tmp/somedir/somefile
+
+systemd-tmpfiles --remove - <<EOF
+R@ /tmp/somedir
+f /tmp/somedir/somefile - - - - baz
+EOF
+
+test -f /tmp/somedir/somefile
+grep -q bar /tmp/somedir/somefile
+
+systemd-tmpfiles --remove --create --factory-reset=true - <<EOF
+R@ /tmp/somedir
+f /tmp/somedir/somefile - - - - baz
+EOF
+
+test -f /tmp/somedir/somefile
+grep -q baz /tmp/somedir/somefile
+
+systemd-tmpfiles --remove --factory-reset=true - <<EOF
+R@ /tmp/somedir
+EOF
+
+test ! -f /tmp/somedir/somefile
+test ! -d /tmp/somedir/
