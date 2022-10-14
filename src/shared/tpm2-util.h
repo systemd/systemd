d00990548c8c12036bcce9b8461c8a6eafa1bf92
Lennart Poettering <lennart@poettering.net>
2022-10-14 14:38:35 +0200

    tpm2-util: optionally do HMAC in tpm2_extend_bytes() in case we process sensitive data
    When measuring data into a PCR we are supposed to hash the data on the
    CPU and then pass the hash value over the wire to the TPM2. That's all
    good as long as the data we intend to measure is not sensitive.
    
    Let's be extra careful though if we want to measure sensitive data, for
    example the root file system volume key. Instead of just hashing that
    and passing it over the wire to the TPM2, let's do a HMAC signature
    instead. It's also a hash operation, but should protect our secret
    reasonably well and not leak direct information about it to wiretappers.

--- src/shared/tpm2-util.h
+++ src/shared/tpm2-util.h
@@ -70,7 +70,7 @@ static inline void Esys_Freep(void *p) {
 int tpm2_get_good_pcr_banks(ESYS_CONTEXT *c, uint32_t pcr_mask, TPMI_ALG_HASH **ret_banks);
 int tpm2_get_good_pcr_banks_strv(ESYS_CONTEXT *c, uint32_t pcr_mask, char ***ret);
 
-int tpm2_extend_bytes(ESYS_CONTEXT *c, char **banks, unsigned pcr_index, const void *data, size_t sz);
+int tpm2_extend_bytes(ESYS_CONTEXT *c, char **banks, unsigned pcr_index, const void *data, size_t data_size, const void *secret, size_t secret_size);
 
 #else
 struct tpm2_context;
