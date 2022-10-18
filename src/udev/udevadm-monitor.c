f0acdeba72886213da590505da1f1c041c487357
Yu Watanabe <watanabe.yu+github@gmail.com>
2022-10-19 04:46:24 +0900

    tree-wide: set description for device manager

--- src/udev/udevadm-monitor.c
+++ src/udev/udevadm-monitor.c
@@ -91,8 +91,7 @@ static int setup_monitor(MonitorNetlinkGroup sender, sd_event *event, sd_device_
         if (r < 0)
                 return log_error_errno(r, "Failed to start device monitor: %m");
 
-        (void) sd_event_source_set_description(sd_device_monitor_get_event_source(monitor),
-                                               sender == MONITOR_GROUP_UDEV ? "device-monitor-udev" : "device-monitor-kernel");
+        (void) sd_device_monitor_set_description(monitor, sender == MONITOR_GROUP_UDEV ? "udev" : "kernel");
 
         *ret = TAKE_PTR(monitor);
         return 0;
