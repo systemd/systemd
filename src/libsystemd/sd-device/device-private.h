389954987adb22aca606a7db38d276cd0ec88787
Yu Watanabe <watanabe.yu+github@gmail.com>
2022-10-26 05:12:54 +0900

    sd-device: make device_shallow_clone() static

--- src/libsystemd/sd-device/device-private.h
+++ src/libsystemd/sd-device/device-private.h
@@ -54,7 +54,6 @@ int device_get_properties_nulstr(sd_device *device, const char **ret_nulstr, siz
 int device_get_properties_strv(sd_device *device, char ***ret);
 
 int device_rename(sd_device *device, const char *name);
-int device_shallow_clone(sd_device *device, sd_device **ret);
 int device_clone_with_db(sd_device *device, sd_device **ret);
 
 int device_tag_index(sd_device *dev, sd_device *dev_old, bool add);
