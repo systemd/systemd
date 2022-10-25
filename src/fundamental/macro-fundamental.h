5531dbab1184b150797ba4804017d462adbea85a
Yu Watanabe <watanabe.yu+github@gmail.com>
2022-10-26 03:28:08 +0900

    macro: fix indentation

--- src/fundamental/macro-fundamental.h
+++ src/fundamental/macro-fundamental.h
@@ -288,7 +288,7 @@
                 switch (x) {                                            \
                 FOR_EACH_MAKE_CASE(__VA_ARGS__)                         \
                         _found = true;                                  \
-                       break;                                           \
+                        break;                                          \
                 default:                                                \
                         break;                                          \
                 }                                                       \
