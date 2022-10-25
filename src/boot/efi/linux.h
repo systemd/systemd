8d1cce7ae6f7d5acd147a9753b2a984328302791
Jan Janssen <medhefgo@web.de>
2022-10-25 13:04:41 +0200

    stub: Drop kernel EFI handover code
    There is no need for the handover code now that the generic path can
    deliver the initrd to older kernels too.

--- src/boot/efi/linux.h
+++ src/boot/efi/linux.h
@@ -8,8 +8,3 @@ EFI_STATUS linux_exec(
                 const char *cmdline, UINTN cmdline_len,
                 const void *linux_buffer, UINTN linux_length,
                 const void *initrd_buffer, UINTN initrd_length);
-EFI_STATUS linux_exec_efi_handover(
-                EFI_HANDLE parent,
-                const char *cmdline, UINTN cmdline_len,
-                const void *linux_buffer, UINTN linux_length,
-                const void *initrd_buffer, UINTN initrd_length);
