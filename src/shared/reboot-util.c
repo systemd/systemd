2e18a6bd729dd62a272e45a69a2ea7346f936f67
Yu Watanabe <watanabe.yu+github@gmail.com>
2022-11-03 09:19:07 +0900

    reboot-util: drop unnecessary cast
    Follow-up for ede5a78f50ed2d5f86dc7a117de2a51b397d52d4.

--- src/shared/reboot-util.c
+++ src/shared/reboot-util.c
@@ -122,9 +122,9 @@ int shall_restore_state(void) {
 
 static int xen_kexec_loaded(void) {
 #if HAVE_XENCTRL
-        size_t size;
         _cleanup_close_ int privcmd_fd = -1, buf_fd = -1;
-        void *buffer;
+        xen_kexec_status_t *buffer;
+        size_t size;
         int r;
 
         if (access("/proc/xen", F_OK) < 0) {
@@ -149,7 +149,7 @@ static int xen_kexec_loaded(void) {
         if (buffer == MAP_FAILED)
                 return log_debug_errno(errno, "Cannot allocate buffer for hypercall: %m");
 
-        *(xen_kexec_status_t*) buffer = (xen_kexec_status_t) {
+        *buffer = (xen_kexec_status_t) {
                 .type = KEXEC_TYPE_DEFAULT,
         };
 
