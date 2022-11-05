64d2d377a04e8af8bcc3beea4dc2f2f430e9efff
David Tardon <dtardon@redhat.com>
2022-11-05 16:30:19 +0100

    udev-varlink: add some varlink client helper functions

--- src/udev/udev-varlink.c
+++ src/udev/udev-varlink.c
@@ -3,6 +3,41 @@
 #include "udev-manager.h"
 #include "udev-varlink.h"
 
+int udev_varlink_connect(sd_varlink **ret) {
+        _cleanup_(sd_varlink_flush_close_unrefp) sd_varlink *link = NULL;
+        int r;
+
+        assert(ret);
+
+        r = sd_varlink_connect_address(&link, UDEV_VARLINK_ADDRESS);
+        if (r < 0)
+                return log_error_errno(r, "Failed to connect to " UDEV_VARLINK_ADDRESS ": %m");
+
+        (void) sd_varlink_set_description(link, "udev");
+        (void) sd_varlink_set_relative_timeout(link, USEC_INFINITY);
+
+        *ret = TAKE_PTR(link);
+
+        return 0;
+}
+
+int udev_varlink_call(sd_varlink *link, const char *method, sd_json_variant *parameters, sd_json_variant **ret_parameters) {
+        const char *error;
+        int r;
+
+        assert(link);
+        assert(method);
+
+        r = sd_varlink_call(link, method, parameters, ret_parameters, &error);
+        if (r < 0)
+                return log_error_errno(r, "Failed to execute varlink call: %m");
+        if (error)
+                return log_error_errno(SYNTHETIC_ERRNO(EBADE),
+                                       "Failed to execute varlink call: %s", error);
+
+        return 0;
+}
+
 int manager_open_varlink(Manager *m) {
         int r;
 
