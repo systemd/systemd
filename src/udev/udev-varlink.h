5da660ca1f548f4e96906ba93bc4575709211abe
David Tardon <dtardon@redhat.com>
2022-11-05 16:30:19 +0100

    udev-varlink: add some varlink client helper functions

--- src/udev/udev-varlink.h
+++ src/udev/udev-varlink.h
@@ -1,4 +1,9 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 #pragma once
 
+#include "varlink.h"
+
 #define UDEV_VARLINK_ADDRESS "/run/udev/io.systemd.udev"
+
+int udev_varlink_connect(Varlink **ret);
+int udev_varlink_call(Varlink *link, const char *method, JsonVariant *parameters, JsonVariant **ret_parameters);
