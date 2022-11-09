a245225e2d3f1193389c7dc35d2da02543fd8ac8
Lennart Poettering <lennart@poettering.net>
2022-11-09 11:37:42 +0100

    dissect: simplify flags for recurse_dir()
    We are not interested in the struct dirent data, hence there's no point
    in passing RECURSE_DIR_ENSURE_TYPE. Let's drop it, and thus avoid some
    extrac work on file systems where readdir() does not report .d_type.
    
    Also drop RECURSE_DIR_SAME_MOUNT, because DDIs after all may contain
    multiple partitions, and we mount them all together. The --list command
    really should report the full set of files in an image.

--- src/dissect/dissect.c
+++ src/dissect/dissect.c
@@ -896,7 +896,7 @@ static int action_list_or_copy(DissectedImage *m, LoopDevice *d) {
                 if (dfd < 0)
                         return log_error_errno(errno, "Failed to open mount directory: %m");
 
-                r = recurse_dir(dfd, NULL, 0, UINT_MAX, RECURSE_DIR_SORT|RECURSE_DIR_ENSURE_TYPE|RECURSE_DIR_SAME_MOUNT, list_print_item, NULL);
+                r = recurse_dir(dfd, NULL, 0, UINT_MAX, RECURSE_DIR_SORT, list_print_item, NULL);
                 if (r < 0)
                         return log_error_errno(r, "Failed to list image: %m");
         }
