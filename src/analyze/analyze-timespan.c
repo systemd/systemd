d8d184eaa00b43cff2f068adc1cbe0ef6655f255
David Tardon <dtardon@redhat.com>
2022-11-11 09:42:52 +0100

    analyze-timespan: port to vertical table

--- src/analyze/analyze-timespan.c
+++ src/analyze/analyze-timespan.c
@@ -22,18 +22,12 @@ int verb_timespan(int argc, char *argv[], void *userdata) {
                         return r;
                 }
 
-                table = table_new("name", "value");
+                table = table_new_vertical();
                 if (!table)
                         return log_oom();
 
-                table_set_header(table, false);
-
                 assert_se(cell = table_get_cell(table, 0, 0));
                 r = table_set_ellipsize_percent(table, cell, 100);
-                if (r < 0)
-                        return r;
-
-                r = table_set_align_percent(table, cell, 100);
                 if (r < 0)
                         return r;
 
@@ -43,18 +37,18 @@ int verb_timespan(int argc, char *argv[], void *userdata) {
                         return r;
 
                 r = table_add_many(table,
-                                   TABLE_STRING, "Original:",
+                                   TABLE_FIELD, "Original",
                                    TABLE_STRING, *input_timespan);
                 if (r < 0)
                         return table_log_add_error(r);
 
-                r = table_add_cell_stringf(table, NULL, "%ss:", special_glyph(SPECIAL_GLYPH_MU));
+                r = table_add_cell_stringf_full(table, NULL, TABLE_FIELD, "%ss", special_glyph(SPECIAL_GLYPH_MU));
                 if (r < 0)
                         return table_log_add_error(r);
 
                 r = table_add_many(table,
                                    TABLE_UINT64, output_usecs,
-                                   TABLE_STRING, "Human:",
+                                   TABLE_FIELD, "Human",
                                    TABLE_TIMESPAN, output_usecs,
                                    TABLE_SET_COLOR, ansi_highlight());
                 if (r < 0)
