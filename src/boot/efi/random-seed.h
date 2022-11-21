caaed5a47d2e19ffa82479e8e6272fcb606cf14b
Jason A. Donenfeld <Jason@zx2c4.com>
2022-11-21 16:40:24 +0100

    boot: remove random-seed-mode
    Now that the random seed is used on virtualized systems, there's no
    point in having a random-seed-mode toggle switch. Let's just always
    require it now, with the existing logic already being there to allow not
    having it if EFI itself has an RNG. In other words, the logic for this
    can now be automatic.

--- src/boot/efi/random-seed.h
+++ src/boot/efi/random-seed.h
@@ -2,21 +2,5 @@
 #pragma once
 
 #include <efi.h>
-#include <errno.h>
-#include <uchar.h>
 
-typedef enum RandomSeedMode {
-        RANDOM_SEED_OFF,
-        RANDOM_SEED_WITH_SYSTEM_TOKEN,
-        RANDOM_SEED_ALWAYS,
-        _RANDOM_SEED_MODE_MAX,
-        _RANDOM_SEED_MODE_INVALID = -EINVAL,
-} RandomSeedMode;
-
-static const char16_t * const random_seed_modes_table[_RANDOM_SEED_MODE_MAX] = {
-        [RANDOM_SEED_OFF]               = L"off",
-        [RANDOM_SEED_WITH_SYSTEM_TOKEN] = L"with-system-token",
-        [RANDOM_SEED_ALWAYS]            = L"always",
-};
-
-EFI_STATUS process_random_seed(EFI_FILE *root_dir, RandomSeedMode mode);
+EFI_STATUS process_random_seed(EFI_FILE *root_dir);
