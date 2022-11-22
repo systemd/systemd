9bac299483e59ddd1e687fd0c9bea006834f1cad
наб <nabijaczleweli@nabijaczleweli.xyz>
2022-11-22 05:14:01 +0100

    Add basic systemctl edit test

---
+++ test/test-systemctl-edit.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# SPDX-License-Identifier: LGPL-2.1-or-later
+set -ex
+
+systemctl=${1:-systemctl}
+
+trap 'rm -rf "$root"' EXIT
+root=$(mktemp -dt systemctl-test.XXXXXX)
+
+mkdir -p "$root/etc/systemd/system"
+: >"$root/etc/systemd/system/test1.service"
+
+EDITOR='cat' "$systemctl" --root="$root" edit test1.service
+[ -e "$root/etc/systemd/system/test1.service.d/override.conf" ] && exit 1
+
+printf '%s\n' 3a '[Service]' 'ExecStart=' . w | EDITOR='ed' "$systemctl" --root="$root" edit test1.service
+printf '%s\n'    '[Service]' 'ExecStart='     | cmp - "$root/etc/systemd/system/test1.service.d/override.conf"
