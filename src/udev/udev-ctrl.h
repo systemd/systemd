609d528caa5bf6805e28b034ad9ac70e4c5428bc
David Tardon <dtardon@redhat.com>
2022-11-25 10:23:51 +0100

    udev: stop listening on /run/udev/control

--- src/udev/udev-ctrl.h
+++ src/udev/udev-ctrl.h
@@ -9,18 +9,6 @@
 
 typedef struct UdevCtrl UdevCtrl;
 
-typedef enum UdevCtrlMessageType {
-        _UDEV_CTRL_END_MESSAGES,
-} UdevCtrlMessageType;
-
-typedef union UdevCtrlMessageValue {
-        int intval;
-        char buf[256];
-} UdevCtrlMessageValue;
-
-typedef int (*udev_ctrl_handler_t)(UdevCtrl *udev_ctrl, UdevCtrlMessageType type,
-                                   const UdevCtrlMessageValue *value, void *userdata);
-
 int udev_ctrl_new_from_fd(UdevCtrl **ret, int fd);
 int udev_ctrl_new_with_link(UdevCtrl **ret, Varlink *link);
 
@@ -28,7 +16,6 @@ int udev_ctrl_enable_receiving(UdevCtrl *uctrl);
 UdevCtrl *udev_ctrl_ref(UdevCtrl *uctrl);
 UdevCtrl *udev_ctrl_unref(UdevCtrl *uctrl);
 int udev_ctrl_attach_event(UdevCtrl *uctrl, sd_event *event);
-int udev_ctrl_start(UdevCtrl *uctrl, udev_ctrl_handler_t callback, void *userdata);
 sd_event_source *udev_ctrl_get_event_source(UdevCtrl *uctrl);
 
 DEFINE_TRIVIAL_CLEANUP_FUNC(UdevCtrl*, udev_ctrl_unref);
