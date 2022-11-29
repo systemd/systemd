02e32aa62958eb49d97540985f95bd79b2f59ccc
Lennart Poettering <lennart@poettering.net>
2022-11-29 11:54:22 +0100

    fdisk-util: add fdisk_partition_get_uuid_as_id128() helper
    Inspired by: #25534

--- src/shared/fdisk-util.c
+++ src/shared/fdisk-util.c
@@ -26,4 +26,17 @@ int fdisk_new_context_fd(int fd, bool read_only, struct fdisk_context **ret) {
         return 0;
 }
 
+int fdisk_partition_get_uuid_as_id128(struct fdisk_partition *p, sd_id128_t *ret) {
+        const char *ids;
+
+        assert(p);
+        assert(ret);
+
+        ids = fdisk_partition_get_uuid(p);
+        if (!ids)
+                return -ENXIO;
+
+        return sd_id128_from_string(ids, ret);
+}
+
 #endif
