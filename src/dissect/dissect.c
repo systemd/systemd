de11f27c01d0c8146b928fa2f6f92be64e770cce
Lennart Poettering <lennart@poettering.net>
2022-12-02 15:02:44 +0100

    dissect: show color in log output

--- src/dissect/dissect.c
+++ src/dissect/dissect.c
@@ -1330,8 +1330,7 @@ static int run(int argc, char *argv[]) {
         _cleanup_(loop_device_unrefp) LoopDevice *d = NULL;
         int r;
 
-        log_parse_environment();
-        log_open();
+        log_setup();
 
         r = parse_argv(argc, argv);
         if (r <= 0)
