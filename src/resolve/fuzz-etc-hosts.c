b35e8c65838e5732d6f896c414b43462be97653f
Yu Watanabe <watanabe.yu+github@gmail.com>
2022-12-07 22:30:45 +0900

    resolve: rename etc_hosts_free() -> etc_hosts_clear()

--- src/resolve/fuzz-etc-hosts.c
+++ src/resolve/fuzz-etc-hosts.c
@@ -6,7 +6,7 @@
 
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
         _cleanup_fclose_ FILE *f = NULL;
-        _cleanup_(etc_hosts_free) EtcHosts h = {};
+        _cleanup_(etc_hosts_clear) EtcHosts h = {};
 
         if (!getenv("SYSTEMD_LOG_LEVEL"))
                 log_set_max_level(LOG_CRIT);
