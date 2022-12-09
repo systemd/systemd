97ccd4ba372a5608595634f8ebe8b36a652462c9
Dan Streetman <ddstreet@ieee.org>
2022-12-09 14:59:05 -0500

    tpm2: add tpm2_policy_auth_value()

--- src/shared/tpm2-util.c
+++ src/shared/tpm2-util.c
@@ -1792,6 +1792,50 @@ static int tpm2_policy_authorize(
         return tpm2_get_policy_digest(c, session, ret_policy_digest);
 }
 
+static int tpm2_calculate_policy_auth_value(TPM2B_DIGEST *digest) {
+        static const TPM2_CC command = TPM2_CC_PolicyAuthValue;
+        int r;
+
+        assert(digest);
+
+        uint8_t buf[sizeof(command)];
+        size_t offset = 0;
+        r = tpm2_marshal("PolicyAuthValue command", command, buf, sizeof(command), &offset);
+        if (r < 0)
+                return r;
+
+        tpm2_digest_extend(digest, buf, offset);
+
+        tpm2_log_debug_digest(digest, "PolicyAuthValue calculated digest");
+
+        return 0;
+}
+
+static int tpm2_policy_auth_value(
+                struct tpm2_context *c,
+                struct tpm2_handle session,
+                TPM2B_DIGEST **ret_policy_digest) {
+
+        TSS2_RC rc;
+
+        assert(c);
+
+        log_debug("Adding authValue policy.");
+
+        rc = sym_Esys_PolicyAuthValue(
+                        c->esys_context,
+                        session.handle,
+                        ESYS_TR_NONE,
+                        ESYS_TR_NONE,
+                        ESYS_TR_NONE);
+        if (rc != TSS2_RC_SUCCESS)
+                return log_error_errno(SYNTHETIC_ERRNO(ENOTRECOVERABLE),
+                                       "Failed to add authValue policy to TPM: %s",
+                                       sym_Tss2_RC_Decode(rc));
+
+        return tpm2_get_policy_digest(c, session, ret_policy_digest);
+}
+
 static int tpm2_build_policy(
                 struct tpm2_context *c,
                 struct tpm2_handle session,
@@ -1858,18 +1902,12 @@ static int tpm2_build_policy(
         }
 
         if (use_pin) {
-                log_debug("Configuring PIN policy.");
-
-                rc = sym_Esys_PolicyAuthValue(
-                                c->esys_context,
-                                session.handle,
-                                ESYS_TR_NONE,
-                                ESYS_TR_NONE,
-                                ESYS_TR_NONE);
-                if (rc != TSS2_RC_SUCCESS)
-                        return log_error_errno(SYNTHETIC_ERRNO(ENOTRECOVERABLE),
-                                               "Failed to add authValue policy to TPM: %s",
-                                               sym_Tss2_RC_Decode(rc));
+                r = tpm2_policy_auth_value(
+                                c,
+                                session,
+                                /* ret_policy_digest= */ NULL);
+                if (r < 0)
+                        return r;
         }
 
         r = tpm2_get_policy_digest(c, session, ret_policy_digest);
