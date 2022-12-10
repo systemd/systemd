5e3a64babf68e0b46a75b1091b70e2889b212cd2
Berend De Schouwer <berend@deschouwer.co.za>
2022-12-10 15:51:28 +0200

    Revert a change
    Revert a change that causes a memory leak in the fuzzer

--- src/journal-remote/journal-remote-write.c
+++ src/journal-remote/journal-remote-write.c
@@ -23,6 +23,7 @@ static int do_rotate(ManagedJournalFile **f, MMapCache *m, JournalFileFlags file
 
 Writer* writer_new(RemoteServer *server) {
         Writer *w;
+        _cleanup_free_ char *dirname = NULL;
         int r;
 
         w = new0(Writer, 1);
@@ -43,11 +44,12 @@ Writer* writer_new(RemoteServer *server) {
                 if (!w->output)
                         return NULL;
         } else {
-                r = path_extract_directory(server->output, &w->output);
+                r = path_extract_directory(server->output, &dirname);
                 if (r < 0) {
                         log_error_errno(r, "Failed to find directory of file %s", server->output);
                         return NULL;
                 }
+                w->output = dirname;
         }
 
         return w;
