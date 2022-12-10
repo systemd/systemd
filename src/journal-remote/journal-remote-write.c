5390b5b2586adddfcebcc2ad9127776ce711dddf
Berend De Schouwer <berend@deschouwer.co.za>
2022-12-10 10:13:46 +0200

    Cleanup journal-remote-write.c/writer_new()
    Cleanup the calls to path_extract_directory()

--- src/journal-remote/journal-remote-write.c
+++ src/journal-remote/journal-remote-write.c
@@ -23,7 +23,6 @@ static int do_rotate(ManagedJournalFile **f, MMapCache *m, JournalFileFlags file
 
 Writer* writer_new(RemoteServer *server) {
         Writer *w;
-        _cleanup_free_ char *dirname = NULL;
         int r;
 
         w = new0(Writer, 1);
@@ -39,15 +38,16 @@ Writer* writer_new(RemoteServer *server) {
         w->n_ref = 1;
         w->server = server;
 
-        if (is_dir(server->output, /* follow = */ true) > 0)
-                w->output = server->output;
-        else {
-                r = path_extract_directory(server->output, &dirname);
+        if (is_dir(server->output, /* follow = */ true) > 0) {
+                w->output = strdup(server->output);
+                if (!w->output)
+                        return NULL;
+        } else {
+                r = path_extract_directory(server->output, &w->output);
                 if (r < 0) {
                         log_error_errno(r, "Failed to find directory of file %s", server->output);
                         return NULL;
                 }
-                w->output = dirname;
         }
 
         return w;
--- src/journal-remote/journal-remote-write.h
+++ src/journal-remote/journal-remote-write.h
@@ -9,7 +9,7 @@ typedef struct RemoteServer RemoteServer;
 typedef struct Writer {
         ManagedJournalFile *journal;
         JournalMetrics metrics;
-        const char *output;          /* directory where we write, for vacuuming */
+        char *output;          /* directory where we write, for vacuuming */
 
         MMapCache *mmap;
         RemoteServer *server;
