8f30c00c50d7fd569853dfca5b9c1ffbcbad2b32
Aidan Dang <dang@aidan.gg>
2022-12-10 21:58:27 +1100

    Implement SYSTEMD_HOME_MKFS_OPTIONS_* envvars to configure mkfs options for homed LUKS directories

--- src/partition/repart.c
+++ src/partition/repart.c
@@ -4032,7 +4032,7 @@ static int context_mkfs(Context *context) {
                 }
 
                 r = make_filesystem(partition_target_path(t), p->format, strempty(p->new_label), root,
-                                    p->fs_uuid, arg_discard);
+                                    p->fs_uuid, arg_discard, NULL);
                 if (r < 0)
                         return r;
 
@@ -5362,7 +5362,7 @@ static int context_minimize(Context *context) {
                                 return r;
                 }
 
-                r = make_filesystem(d ? d->node : temp, p->format, strempty(p->new_label), root, fs_uuid, arg_discard);
+                r = make_filesystem(d ? d->node : temp, p->format, strempty(p->new_label), root, fs_uuid, arg_discard, NULL);
                 if (r < 0)
                         return r;
 
@@ -5415,7 +5415,7 @@ static int context_minimize(Context *context) {
                 if (r < 0 && r != -ENOENT && !ERRNO_IS_PRIVILEGE(r))
                         return log_error_errno(r, "Failed to make loopback device of %s: %m", temp);
 
-                r = make_filesystem(d ? d->node : temp, p->format, strempty(p->new_label), root, p->fs_uuid, arg_discard);
+                r = make_filesystem(d ? d->node : temp, p->format, strempty(p->new_label), root, p->fs_uuid, arg_discard, NULL);
                 if (r < 0)
                         return r;
 
