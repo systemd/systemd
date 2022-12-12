ca7b590283f803866e44f7c0cfe24022a955ec16
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2022-12-12 14:11:46 +0100

    udevadm: emit deprecation notice in udevadm hwdb
    This tool was "deprecated" back in 65eb4378c3e1de25383d8cd606909e64c71edc80,
    but only by removing documentation. This is somewhat surprising, but udevadm
    hwdb --update and systemd-hwdb update generate different databases. udevadm
    runs in compat mode and (as far as I have been able to figure out from a quick
    look), it omits filename information and does some other changes to the
    datastructures. The consuming code (udev) is the same in both cases, so this
    "compatibility mode" seems very strange. But I don't think it's worth trying to
    figure out why things were done this way. Let's just push people towards the
    new code.
    
    Inspired by https://github.com/systemd/systemd/issues/25698#issuecomment-1346298094.

--- src/udev/udevadm-hwdb.c
+++ src/udev/udevadm-hwdb.c
@@ -87,6 +87,8 @@ int hwdb_main(int argc, char *argv[], void *userdata) {
                 return log_error_errno(SYNTHETIC_ERRNO(EINVAL),
                                        "Either --update or --test must be used.");
 
+        log_notice("udevadm hwdb is deprecated. Use systemd-hwdb instead.");
+
         if (arg_update) {
                 r = hwdb_update(arg_root, arg_hwdb_bin_dir, arg_strict, true);
                 if (r < 0)
