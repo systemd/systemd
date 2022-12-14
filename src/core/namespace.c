2326c6188940206d001b943739118826bbed1483
Yu Watanabe <watanabe.yu+github@gmail.com>
2022-12-14 15:38:54 +0900

    mount-util: make mount_switch_root() take a mount propagation flag

--- src/core/namespace.c
+++ src/core/namespace.c
@@ -2488,7 +2488,7 @@ int setup_namespace(
                 goto finish;
 
         /* MS_MOVE does not work on MS_SHARED so the remount MS_SHARED will be done later */
-        r = mount_switch_root(root, MOUNT_ATTR_PROPAGATION_INHERIT);
+        r = mount_switch_root(root, /* mount_propagation_flag = */ 0);
         if (r == -EINVAL && root_directory) {
                 /* If we are using root_directory and we don't have privileges (ie: user manager in a user
                  * namespace) and the root_directory is already a mount point in the parent namespace,
@@ -2498,7 +2498,7 @@ int setup_namespace(
                 r = mount_nofollow_verbose(LOG_DEBUG, root, root, NULL, MS_BIND|MS_REC, NULL);
                 if (r < 0)
                         goto finish;
-                r = mount_switch_root(root, MOUNT_ATTR_PROPAGATION_INHERIT);
+                r = mount_switch_root(root, /* mount_propagation_flag = */ 0);
         }
         if (r < 0) {
                 log_debug_errno(r, "Failed to mount root with MS_MOVE: %m");
