17528e012f33b8644cb2485ad42339046994ddc7
Lennart Poettering <lennart@poettering.net>
2022-12-16 18:51:25 +0100

    systemd-efi-options

--- src/boot/bootctl-systemd-efi-options.c
+++ src/boot/bootctl-systemd-efi-options.c
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 
-#include "bootctl-systemd-efi-options.h"
 #include "alloc-util.h"
+#include "bootctl-systemd-efi-options.h"
 #include "efi-loader.h"
 
 int verb_systemd_efi_options(int argc, char *argv[], void *userdata) {
