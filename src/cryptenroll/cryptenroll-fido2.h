af5a56151d00e920d573b0216782ea979caad262
Peter Cai <peter@typeblog.net>
2022-12-17 20:33:05 -0500

    cryptenroll: Implement support for unlocking via FIDO2 tokens
    This allows FIDO2 users to wipe out password slots and still be able to
    enroll new key slots via systemd-cryptenroll. Note that when the user
    wants to both unlock with a FIDO2 token and enroll a new FIDO2 token,
    they cannot be set to automatic discovery. This is to safeguard against
    confusion, because there will be multiple tokens connected to the system
    when doing so -- and we require users to explicitly confirm which one to
    use for unlocking and which one to use for enrollment.

--- src/cryptenroll/cryptenroll-fido2.h
+++ src/cryptenroll/cryptenroll-fido2.h
@@ -9,7 +9,14 @@
 
 #if HAVE_LIBFIDO2
 int enroll_fido2(struct crypt_device *cd, const void *volume_key, size_t volume_key_size, const char *device, Fido2EnrollFlags lock_with, int cred_alg);
+int prepare_luks_fido2(struct crypt_device *cd, const char *device, void *ret_vk, size_t *ret_vks);
+
 #else
+static inline int prepare_luks_fido2(struct crypt_device *cd, const char *device, void *ret_vk, size_t *ret_vks) {
+        return log_debug_errno(SYNTHETIC_ERRNO(EOPNOTSUPP),
+                               "FIDO2 unlocking not supported.");
+}
+
 static inline int enroll_fido2(struct crypt_device *cd, const void *volume_key, size_t volume_key_size, const char *device, Fido2EnrollFlags lock_with, int cred_alg) {
         return log_debug_errno(SYNTHETIC_ERRNO(EOPNOTSUPP),
                                "FIDO2 key enrollment not supported.");
