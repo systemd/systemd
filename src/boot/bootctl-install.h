db1c80ca156d97f855ad13c96ac7970ea29e2d51
Lennart Poettering <lennart@poettering.net>
2022-12-19 11:42:15 +0100

    bootctl: split out "install" verb too

---
+++ src/boot/bootctl-install.h
@@ -0,0 +1,6 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#pragma once
+
+int verb_install(int argc, char *argv[], void *userdata);
+int verb_remove(int argc, char *argv[], void *userdata);
+int verb_is_installed(int argc, char *argv[], void *userdata);
