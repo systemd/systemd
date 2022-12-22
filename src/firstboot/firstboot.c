1b21c54ed5b7e4858452f2901c10eda74d405654
Daan De Meyer <daan.j.demeyer@gmail.com>
2022-12-22 13:00:31 +0100

    dissect-image: Return mount point fd if requested

--- src/firstboot/firstboot.c
+++ src/firstboot/firstboot.c
@@ -1390,6 +1390,7 @@ static int run(int argc, char *argv[]) {
                                 DISSECT_IMAGE_FSCK |
                                 DISSECT_IMAGE_GROWFS,
                                 &unlink_dir,
+                                NULL,
                                 &loop_device);
                 if (r < 0)
                         return r;
