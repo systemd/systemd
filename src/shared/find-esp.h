63105f33edad423691e2d53bf7071f99c83799ba
Daan De Meyer <daan.j.demeyer@gmail.com>
2022-12-22 14:59:56 +0100

    find-esp: Add openat() like helpers that operate on fds
    We also rework the internals of find-esp to work on directory file
    descriptors instead of absolute paths and do a lot of general cleanups.
    
    By passing the parent directory file descriptor to verify_fsroot_dir()
    along with the name of the directory we're operating on, we can get rid
    of the fallback that goes via path to open the parent directory if '..'
    fails due to permission errors.

--- src/shared/find-esp.h
+++ src/shared/find-esp.h
@@ -8,5 +8,8 @@
 
 #include "sd-id128.h"
 
+int find_esp_and_warn_at(int rfd, const char *path, bool unprivileged_mode, char **ret_path, uint32_t *ret_part, uint64_t *ret_pstart, uint64_t *ret_psize, sd_id128_t *ret_uuid, dev_t *ret_devid);
 int find_esp_and_warn(const char *root, const char *path, bool unprivileged_mode, char **ret_path, uint32_t *ret_part, uint64_t *ret_pstart, uint64_t *ret_psize, sd_id128_t *ret_uuid, dev_t *ret_devid);
+
+int find_xbootldr_and_warn_at(int rfd, const char *path, bool unprivileged_mode, char **ret_path, sd_id128_t *ret_uuid, dev_t *ret_devid);
 int find_xbootldr_and_warn(const char *root, const char *path, bool unprivileged_mode, char **ret_path, sd_id128_t *ret_uuid, dev_t *ret_devid);
