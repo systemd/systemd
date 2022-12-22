7369ddb879123c6e62d53ef86eb0847de3eed70c
Daan De Meyer <daan.j.demeyer@gmail.com>
2022-12-22 14:26:29 +0100

    env-file: Add write_env_file_at()

--- src/basic/env-file.h
+++ src/basic/env-file.h
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 #pragma once
 
+#include <fcntl.h>
 #include <stdarg.h>
 #include <stdio.h>
 
@@ -16,4 +17,7 @@ int load_env_file_pairs(FILE *f, const char *fname, char ***ret);
 
 int merge_env_file(char ***env, FILE *f, const char *fname);
 
-int write_env_file(const char *fname, char **l);
+int write_env_file_at(int dir_fd, const char *fname, char **l);
+static inline int write_env_file(const char *fname, char **l) {
+        return write_env_file_at(AT_FDCWD, fname, l);
+}
