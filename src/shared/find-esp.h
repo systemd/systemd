dd34ff838c176a9dccef81706bd2f871ae831adb
Daan De Meyer <daan.j.demeyer@gmail.com>
2022-12-22 14:59:56 +0100

    find-esp: Add openat() like helpers that operate on fds
    Also add ret_fd arguments to return a file descriptor to the ESP
    and rework the internals of find-esp to use file descriptors more.

--- src/shared/find-esp.h
+++ src/shared/find-esp.h
@@ -2,11 +2,16 @@
 
 #pragma once
 
+#include <fcntl.h>
 #include <inttypes.h>
 #include <stdbool.h>
 #include <sys/types.h>
 
+#include "fd-util.h"
 #include "sd-id128.h"
 
-int find_esp_and_warn(const char *root, const char *path, bool unprivileged_mode, char **ret_path, uint32_t *ret_part, uint64_t *ret_pstart, uint64_t *ret_psize, sd_id128_t *ret_uuid, dev_t *ret_devid);
-int find_xbootldr_and_warn(const char *root, const char *path, bool unprivileged_mode, char **ret_path, sd_id128_t *ret_uuid, dev_t *ret_devid);
+int find_esp_at_and_warn(int dir_fd, const char *path, bool unprivileged_mode, char **ret_path, int *ret_fd, uint32_t *ret_part, uint64_t *ret_pstart, uint64_t *ret_psize, sd_id128_t *ret_uuid, dev_t *ret_devid);
+int find_esp_and_warn(const char *root, const char *path, bool unprivileged_mode, char **ret_path, int *ret_fd, uint32_t *ret_part, uint64_t *ret_pstart, uint64_t *ret_psize, sd_id128_t *ret_uuid, dev_t *ret_devid);
+
+int find_xbootldr_at_and_warn(int dir_fd, const char *path, bool unprivileged_mode, char **ret_path, int *ret_fd, sd_id128_t *ret_uuid, dev_t *ret_devid);
+int find_xbootldr_and_warn(const char *root, const char *path, bool unprivileged_mode, char **ret_path, int *ret_fd, sd_id128_t *ret_uuid, dev_t *ret_devid);
