aa8cb4995615678b50f1e181c1c92e753aeb393f
Jan Janssen <medhefgo@web.de>
2023-01-06 18:07:18 +0100

    boot: Provide our own EFI API headers
    We want to get away from gnu-efi and the only really usable source of
    EFI headers would be EDK2, which is somewhat impractical to use and
    quite large to require to be around just for some headers.
    
    As a bonus point, the new headers are safe to be included in userspace
    code.
    
    This should not have any behavior changes as it is mostly changing
    header includes. There are some renames to conform to standard names
    and a few minor device path fixups as the struct is defined slightly
    different.
    
    Of note is that this removes usage of uchar.h and wchar.h as they are
    not guaranteed to be available in a freestanding environment. Instead
    efi.h will provide the needed types.

--- src/boot/efi/devicetree.c
+++ src/boot/efi/devicetree.c
@@ -1,9 +1,7 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 
-#include <efi.h>
-
 #include "devicetree.h"
-#include "missing_efi.h"
+#include "proto-dt-fixup.h"
 #include "util.h"
 
 #define FDT_V1_SIZE (7*4)
