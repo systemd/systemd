4453ebe4db0511d25bed1040930ea6430c1bed91
Lennart Poettering <lennart@poettering.net>
2023-01-23 16:23:45 +0100

    build: add some coloring to --version output
    Make it easier to discern enabled and disabled build options.

--- src/basic/log.h
+++ src/basic/log.h
@@ -298,6 +298,7 @@ int log_emergency_level(void);
 
 #define log_oom() log_oom_internal(LOG_ERR, PROJECT_FILE, __LINE__, __func__)
 #define log_oom_debug() log_oom_internal(LOG_DEBUG, PROJECT_FILE, __LINE__, __func__)
+#define log_oom_warning() log_oom_internal(LOG_WARNING, PROJECT_FILE, __LINE__, __func__)
 
 bool log_on_console(void) _pure_;
 
