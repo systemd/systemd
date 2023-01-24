7a47fcad869d8e5be078f8ab8f3d2254e24371f5
William Roberts <william.c.roberts@intel.com>
2023-01-24 15:18:40 -0600

    tpm2: add support for a trusted SRK
    Prevent attackers from spoofing the tpmKey portion of the AuthSession by
    adding a trusted key to the LUKS header metadata. Also, use a persistent
    object rather than a transient object.
    
    This provides the following benifits:
    1. No way to MITM the tpmKey portion of the session, see [1] for
    details.
    
    2. Strengthens the encrypted sessions, note that the bindKey could be
    dropped now.
    
    3. Speed, once it's created we just use it.
    
    This is a "first to set" model, in where the first person to set the key
    in the LUKS header wins. Thus, setup should be done in a known good
    state. If an SRK, which is a primary key at a special persistent
    address, is found, it will use whatever is there. If not, it creates an
    SRK. The SRK follows the convetions used through the tpm2-software
    organization code on GitHub [2], however, a split has occured between
    Windows and Linux with respect to SRK templates. The Linux SRK is
    generated with the unique field size set to 0, in Windows, it properly
    sets the size to key size in bytes and the unique data to all 0's of that
    size. Note the proper templates for SRKs is covered in spec [3].
    However, the most important thing, is that both SRKs are passwordless,
    and thus they should be interchangable. If Windows is the first to make
    the SRK, systemd will gladly accept it and vice-versa.
    
    1. Without the bindKey being utilized, an attacker was able to intercept
    this and fake a key, thus being able to decrypt and encrypt traffic as
    needed. Introduction of the bindKey strengthened this, but allows for
    the attacker to brute force AES128CFB using pin guesses. Introduction of
    the salt increases the difficulty of this attack as well as DA attacks
    on the TPM objects itself.
    
    2. https://github.com/tpm2-software
    
    3. https://trustedcomputinggroup.org/wp-content/uploads/TCG-TPM-v2.0-Provisioning-Guidance-Published-v1r1.pdf
    
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- src/shared/tpm2-util.h
+++ src/shared/tpm2-util.h
@@ -50,8 +50,8 @@ extern TSS2_RC (*sym_Tss2_MU_TPM2B_PUBLIC_Unmarshal)(uint8_t const buffer[], siz
 
 int dlopen_tpm2(void);
 
-int tpm2_seal(const char *device, uint32_t hash_pcr_mask, const void *pubkey, size_t pubkey_size, uint32_t pubkey_pcr_mask, const char *pin, void **ret_secret, size_t *ret_secret_size, void **ret_blob, size_t *ret_blob_size, void **ret_pcr_hash, size_t *ret_pcr_hash_size, uint16_t *ret_pcr_bank, uint16_t *ret_primary_alg);
-int tpm2_unseal(const char *device, uint32_t hash_pcr_mask, uint16_t pcr_bank, const void *pubkey, size_t pubkey_size, uint32_t pubkey_pcr_mask, JsonVariant *signature, const char *pin, uint16_t primary_alg, const void *blob, size_t blob_size, const void *policy_hash, size_t policy_hash_size, void **ret_secret, size_t *ret_secret_size);
+int tpm2_seal(const char *device, uint32_t hash_pcr_mask, const void *pubkey, size_t pubkey_size, uint32_t pubkey_pcr_mask, const char *pin, void **ret_secret, size_t *ret_secret_size, void **ret_blob, size_t *ret_blob_size, void **ret_pcr_hash, size_t *ret_pcr_hash_size, uint16_t *ret_pcr_bank, uint16_t *ret_primary_alg, void **ret_primary_buf, size_t *ret_primary_buf_size);
+int tpm2_unseal(const char *device, uint32_t hash_pcr_mask, uint16_t pcr_bank, const void *pubkey, size_t pubkey_size, uint32_t pubkey_pcr_mask, JsonVariant *signature, const char *pin, uint16_t primary_alg, const void *blob, size_t blob_size, const void *policy_hash, size_t policy_hash_size, const void *primary_buffer, size_t primary_buf_size, void **ret_secret, size_t *ret_secret_size);
 
 struct tpm2_context {
         void *tcti_dl;
@@ -88,8 +88,10 @@ int tpm2_parse_pcrs(const char *s, uint32_t *ret);
 int tpm2_make_pcr_json_array(uint32_t pcr_mask, JsonVariant **ret);
 int tpm2_parse_pcr_json_array(JsonVariant *v, uint32_t *ret);
 
-int tpm2_make_luks2_json(int keyslot, uint32_t hash_pcr_mask, uint16_t pcr_bank, const void *pubkey, size_t pubkey_size, uint32_t pubkey_pcr_mask, uint16_t primary_alg, const void *blob, size_t blob_size, const void *policy_hash, size_t policy_hash_size, const void *salt, size_t salt_size, TPM2Flags flags, JsonVariant **ret);
-int tpm2_parse_luks2_json(JsonVariant *v, int *ret_keyslot, uint32_t *ret_hash_pcr_mask, uint16_t *ret_pcr_bank, void **ret_pubkey, size_t *ret_pubkey_size, uint32_t *ret_pubkey_pcr_mask, uint16_t *ret_primary_alg, void **ret_blob, size_t *ret_blob_size, void **ret_policy_hash, size_t *ret_policy_hash_size, void **ret_salt, size_t *ret_salt_size, TPM2Flags *ret_flags);
+int tpm2_make_luks2_json(int keyslot, uint32_t hash_pcr_mask, uint16_t pcr_bank, const void *pubkey, size_t pubkey_size, uint32_t pubkey_pcr_mask, uint16_t primary_alg, const void *blob, size_t blob_size, const void *policy_hash, size_t policy_hash_size, const void *salt, size_t salt_size, const void *primary_buf, size_t primary_buf_size, TPM2Flags flags, JsonVariant **ret);
+int tpm2_parse_luks2_json(JsonVariant *v, int *ret_keyslot, uint32_t *ret_hash_pcr_mask, uint16_t *ret_pcr_bank, void **ret_pubkey, size_t *ret_pubkey_size, uint32_t *ret_pubkey_pcr_mask, uint16_t *ret_primary_alg, void **ret_blob, size_t *ret_blob_size, void **ret_policy_hash, size_t *ret_policy_hash_size, void **ret_salt, size_t *ret_salt_size, void **ret_primary_buf, size_t *ret_primary_buf_size, TPM2Flags *ret_flags);
+
+const TPM2B_PUBLIC *tpm2_get_primary_template(bool use_srk_model, bool is_ecc);
 
 #define TPM2_PCRS_MAX 24U
 
