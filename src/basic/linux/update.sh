4f6e3281914adbee93b9eec95ab3bcaf7c7cbbeb
Frantisek Sumsal <frantisek@sumsal.cz>
2023-01-25 12:37:49 +0100

    basic/linux: update linux uapi headers
    IPPROTO_L2TP was moved from linux/l2tp.h to linux/in.h [0], so let's
    reflect that change to fix build with newer kernels:
    
    ```
    In file included from ../src/libsystemd/sd-netlink/netlink-types-genl.c:10:
    ../src/basic/linux/l2tp.h:16: error: "IPPROTO_L2TP" redefined [-Werror]
       16 | #define IPPROTO_L2TP            115
          |
    In file included from ../src/libsystemd/sd-netlink/netlink-types-genl.c:3:
    /usr/include/netinet/in.h:85: note: this is the location of the previous definition
       85 | #define IPPROTO_L2TP            IPPROTO_L2TP
          |
    cc1: all warnings being treated as errors
    ```
    
    When at it, update the rest of the headers we ship as well.
    
    [0] https://github.com/torvalds/linux/commit/65b32f801bfbc54dc98144a6ec26082b59d131ee

--- src/basic/linux/update.sh
+++ src/basic/linux/update.sh
@@ -6,5 +6,5 @@ set -o pipefail
 for i in *.h */*.h; do
     curl --fail "https://raw.githubusercontent.com/torvalds/linux/master/include/uapi/linux/$i" -o "$i"
 
-    sed -i -e 's/__user //g' -e '/^#include <linux\/compiler.h>/ d' "$i"
+    sed -r -i -e 's/__user //g' -e '/^#include <linux\/compiler(_types)?.h>/ d' "$i"
 done
