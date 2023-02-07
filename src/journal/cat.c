f5b639157c8d9cbd59f28b413a4f71958917fbc6
Mike Yuan <me@yhndnzj.com>
2023-02-08 00:21:33 +0800

    journal: cat: set JOURNAL_STREAM before exec-ing
    Makes the behavior of systemd-cat match
    that of core/execute.
    
    Closes #25880

--- src/journal/cat.c
+++ src/journal/cat.c
@@ -5,6 +5,7 @@
 #include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
 #include "sd-journal.h"
@@ -12,6 +13,7 @@
 #include "alloc-util.h"
 #include "build.h"
 #include "fd-util.h"
+#include "format-util.h"
 #include "main-func.h"
 #include "parse-argument.h"
 #include "parse-util.h"
@@ -144,15 +146,37 @@ static int run(int argc, char *argv[]) {
         saved_stderr = fcntl(STDERR_FILENO, F_DUPFD_CLOEXEC, 3);
 
         r = rearrange_stdio(STDIN_FILENO, outfd, errfd < 0 ? outfd : errfd); /* Invalidates fd on success + error! */
-        TAKE_FD(outfd);
-        TAKE_FD(errfd);
-        if (r < 0)
+        if (r < 0) {
+                TAKE_FD(outfd);
+                TAKE_FD(errfd);
                 return log_error_errno(r, "Failed to rearrange stdout/stderr: %m");
+        }
 
-        if (argc <= optind)
+        if (argc <= optind) {
+                TAKE_FD(outfd);
+                TAKE_FD(errfd);
+
                 (void) execl("/bin/cat", "/bin/cat", NULL);
-        else
+        } else {
+                _cleanup_free_ char *s = NULL;
+                struct stat st;
+
+                if (fstat(errfd < 0 ? STDOUT_FILENO : STDERR_FILENO, &st) < 0)
+                        return log_error_errno(errno,
+                                               "Failed to fstat(%s): %m",
+                                               FORMAT_PROC_FD_PATH(errfd < 0 ? STDOUT_FILENO : STDERR_FILENO));
+
+                if (asprintf(&s, DEV_FMT ":" INO_FMT, st.st_dev, st.st_ino) < 0)
+                        return log_oom();
+
+                if (setenv("JOURNAL_STREAM", s, 1) < 0)
+                        log_error_errno(errno, "Failed to set environment variable JOURNAL_STREAM: %m");
+
+                TAKE_FD(outfd);
+                TAKE_FD(errfd);
+
                 (void) execvp(argv[optind], argv + optind);
+        }
         r = -errno;
 
         /* Let's try to restore a working stderr, so we can print the error message */
