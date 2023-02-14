d5b3a0497f62eac2374ff9639394be51dd555c53
Daan De Meyer <daan.j.demeyer@gmail.com>
2023-02-14 15:09:54 +0100

    repart: Remove outdated comment

--- src/partition/repart.c
+++ src/partition/repart.c
@@ -3813,10 +3813,6 @@ static int do_copy_files(Partition *p, const char *root, const Set *denylist) {
                                 if (pfd < 0)
                                         return log_error_errno(pfd, "Failed to open parent directory of target: %m");
 
-                                /* Make sure everything is owned by the user running repart so that
-                                 * make_filesystem() can map the user running repart to "root" in a user
-                                 * namespace to have the files owned by root in the final image. */
-
                                 r = copy_tree_at(
                                                 sfd, ".",
                                                 pfd, fn,
