941a2cfbb4efb46a431e3b6d7df4337d0d35de8f
Luca Boccassi <bluca@debian.org>
2023-02-15 00:44:01 +0000

    cryptsetup: do not assert when unsealing token without salt
    Salt was added in v253. We are not checking whether it was actually found
    (non-zero size), so when an old tpm+pin enrollment is opened things go boom.
    For good measure, check both the buffer and the size in both places.
    
    Assertion 'saltlen > 0' failed at src/shared/tpm2-util.c:2490, function tpm2_util_pbkdf2_hmac_sha256(). Aborting.

--- src/cryptsetup/cryptsetup-tpm2.c
+++ src/cryptsetup/cryptsetup-tpm2.c
@@ -86,6 +86,8 @@ int acquire_tpm2_key(
         const void *blob;
         int r;
 
+        assert(salt || salt_size == 0);
+
         if (!device) {
                 r = tpm2_find_device_auto(LOG_DEBUG, &auto_device);
                 if (r == -ENODEV)
@@ -152,7 +154,7 @@ int acquire_tpm2_key(
                 if (r < 0)
                         return r;
 
-                if (salt) {
+                if (salt_size > 0) {
                         uint8_t salted_pin[SHA256_DIGEST_SIZE] = {};
                         CLEANUP_ERASE(salted_pin);
 
