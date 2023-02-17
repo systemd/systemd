d84ca3b80bef9b5d756a976b91f711ccf1b587da
Dan Streetman <ddstreet@ieee.org>
2023-02-17 12:59:18 -0500

    tpm2: verify symmetric parms in tpm2_context_new()
    This adds tpm2_capability_algs(), tpm2_supports_alg(), and
    tpm2_test_parms(). These functions cache the TPM's supported algs and provides
    functions to check if a specific alg is supported and to ask the TPM if the
    provdied parameters are all supported by the TPM.
    
    This also replaces hardcoding of symmetric alg parameters with an initial check
    when creating a TPM context. If the TPM does not support the symmetric algs
    and/or parameters we require, we log and return error.

--- src/shared/tpm2-util.c
+++ src/shared/tpm2-util.c
@@ -52,6 +52,7 @@ static TSS2_RC (*sym_Esys_PolicyPCR)(ESYS_CONTEXT *esysContext, ESYS_TR policySe
 static TSS2_RC (*sym_Esys_ReadPublic)(ESYS_CONTEXT *esysContext, ESYS_TR objectHandle, ESYS_TR shandle1, ESYS_TR shandle2, ESYS_TR shandle3, TPM2B_PUBLIC **outPublic, TPM2B_NAME **name, TPM2B_NAME **qualifiedName) = NULL;
 static TSS2_RC (*sym_Esys_StartAuthSession)(ESYS_CONTEXT *esysContext, ESYS_TR tpmKey, ESYS_TR bind, ESYS_TR shandle1, ESYS_TR shandle2, ESYS_TR shandle3, const TPM2B_NONCE *nonceCaller, TPM2_SE sessionType, const TPMT_SYM_DEF *symmetric, TPMI_ALG_HASH authHash, ESYS_TR *sessionHandle) = NULL;
 static TSS2_RC (*sym_Esys_Startup)(ESYS_CONTEXT *esysContext, TPM2_SU startupType) = NULL;
+static TSS2_RC (*sym_Esys_TestParms)(ESYS_CONTEXT *esysContext, ESYS_TR shandle1, ESYS_TR shandle2, ESYS_TR shandle3, const TPMT_PUBLIC_PARMS *parameters) = NULL;
 static TSS2_RC (*sym_Esys_TR_Deserialize)(ESYS_CONTEXT *esys_context, uint8_t const *buffer, size_t buffer_size, ESYS_TR *esys_handle) = NULL;
 static TSS2_RC (*sym_Esys_TR_FromTPMPublic)(ESYS_CONTEXT *esysContext, TPM2_HANDLE tpm_handle, ESYS_TR optionalSession1, ESYS_TR optionalSession2, ESYS_TR optionalSession3, ESYS_TR *object) = NULL;
 static TSS2_RC (*sym_Esys_TR_GetName)(ESYS_CONTEXT *esysContext, ESYS_TR handle, TPM2B_NAME **name) = NULL;
@@ -98,6 +99,7 @@ int dlopen_tpm2(void) {
                         DLSYM_ARG(Esys_ReadPublic),
                         DLSYM_ARG(Esys_StartAuthSession),
                         DLSYM_ARG(Esys_Startup),
+                        DLSYM_ARG(Esys_TestParms),
                         DLSYM_ARG(Esys_TR_Deserialize),
                         DLSYM_ARG(Esys_TR_FromTPMPublic),
                         DLSYM_ARG(Esys_TR_GetName),
@@ -182,6 +184,24 @@ static int tpm2_cache_capabilities(Tpm2Context *c) {
         TPMI_YES_NO more;
         int r;
 
+        /* Cache the algorithm capabilities, which *should be* safe to cache, as the only way they can change
+         * is TPM2_SetAlgorithmSet(). While the TPM2 spec is pretty vague on the exact use of this function,
+         * it does note in the example that objects and sessions "may" be flushed. The reference
+         * implementation does not support this command, and I can only hope that no real TPM would allow
+         * disabling (or enabling) algorithms while running. */
+        r = tpm2_get_capability(
+                        c,
+                        TPM2_CAP_ALGS,
+                        TPM2_ALG_FIRST,
+                        TPM2_MAX_CAP_ALGS,
+                        &more,
+                        &capability);
+        if (r < 0)
+                return r;
+        if (more == TPM2_YES)
+                log_error("TPM bug: reported more algs, ignoring.");
+        c->capability_algs = capability.algorithms;
+
         /* Cache the PCR capabilities, which are safe to cache, as the only way they can change is
          * TPM2_PCR_Allocate(), which changes the allocation after the next _TPM_Init(). If the TPM is
          * reinitialized while we are using it, all our context and sessions will be invalid, so we can
@@ -202,7 +222,58 @@ static int tpm2_cache_capabilities(Tpm2Context *c) {
         return 0;
 }
 
+#define tpm2_capability_algs(c) ((c)->capability_algs)
 #define tpm2_capability_pcrs(c) ((c)->capability_pcrs)
+
+static int tpm2_capability_alg(Tpm2Context *c, TPM2_ALG_ID alg, TPMA_ALGORITHM *ret) {
+        TPML_ALG_PROPERTY algs = tpm2_capability_algs(c);
+        for (uint32_t i = 0; i < algs.count; i++)
+                if (algs.algProperties[i].alg == alg) {
+                        if (ret)
+                                *ret = algs.algProperties[i].algProperties;
+                        return 0;
+                }
+
+        return log_debug_errno(SYNTHETIC_ERRNO(ENOENT), "TPM does not support alg 0x%02x.", alg);
+}
+#define tpm2_supports_alg(c, alg) (tpm2_capability_alg(c, alg, NULL) == 0)
+
+static bool tpm2_test_parms(Tpm2Context *c, TPMI_ALG_PUBLIC alg, const TPMU_PUBLIC_PARMS *parms) {
+        TSS2_RC rc;
+
+        assert(c);
+        assert(parms);
+
+        const TPMT_PUBLIC_PARMS parameters = {
+                .type = alg,
+                .parameters = *parms,
+        };
+
+        rc = sym_Esys_TestParms(c->esys_context, ESYS_TR_NONE, ESYS_TR_NONE, ESYS_TR_NONE, &parameters);
+        if (rc != TSS2_RC_SUCCESS)
+                log_debug("TPM does not support tested parms: %s", sym_Tss2_RC_Decode(rc));
+
+        return rc == TSS2_RC_SUCCESS;
+}
+
+static inline bool tpm2_supports_tpmt_sym_def_object(Tpm2Context *c, const TPMT_SYM_DEF_OBJECT *parameters) {
+        assert(parameters);
+
+        return tpm2_test_parms(c, TPM2_ALG_SYMCIPHER, &(const TPMU_PUBLIC_PARMS){ .symDetail.sym = *parameters, });
+}
+
+static inline bool tpm2_supports_tpmt_sym_def(Tpm2Context *c, const TPMT_SYM_DEF *parameters) {
+        assert(parameters);
+
+        /* Unfortunately, TPMT_SYM_DEF and TPMT_SYM_DEF_OBEJECT are separately defined, even though they are
+         * functionally identical. */
+        const TPMT_SYM_DEF_OBJECT object = {
+                .algorithm = parameters->algorithm,
+                .keyBits = parameters->keyBits,
+                .mode = parameters->mode,
+        };
+        return tpm2_supports_tpmt_sym_def_object(c, &object);
+}
 
 static Tpm2Context *tpm2_context_free(Tpm2Context *c) {
         if (!c)
@@ -329,6 +400,21 @@ int tpm2_context_new(const char *device, Tpm2Context **ret_context) {
         r = tpm2_cache_capabilities(context);
         if (r < 0)
                 return r;
+
+        /* We require AES and CFB support for session encryption. */
+        if (!tpm2_supports_alg(context, TPM2_ALG_AES))
+                return log_error_errno(SYNTHETIC_ERRNO(ENOTRECOVERABLE), "TPM does not support AES.");
+        if (!tpm2_supports_alg(context, TPM2_ALG_CFB))
+                return log_error_errno(SYNTHETIC_ERRNO(ENOTRECOVERABLE), "TPM does not support CFB.");
+
+        context->session_symmetric_template = (TPMT_SYM_DEF){
+                .algorithm = TPM2_ALG_AES,
+                .keyBits.aes = 128,
+                .mode.aes = TPM2_ALG_CFB, /* The spec requires sessions to use CFB. */
+        };
+
+        if (!tpm2_supports_tpmt_sym_def(context, &context->session_symmetric_template))
+                return log_error_errno(SYNTHETIC_ERRNO(ENOTRECOVERABLE), "TPM does not support AES-128-CFB.");
 
         *ret_context = TAKE_PTR(context);
 
@@ -1564,11 +1650,6 @@ static int tpm2_make_encryption_session(
                 const Tpm2Handle *bind_key,
                 Tpm2Handle **ret_session) {
 
-        static const TPMT_SYM_DEF symmetric = {
-                .algorithm = TPM2_ALG_AES,
-                .keyBits.aes = 128,
-                .mode.aes = TPM2_ALG_CFB,
-        };
         const TPMA_SESSION sessionAttributes = TPMA_SESSION_DECRYPT | TPMA_SESSION_ENCRYPT |
                         TPMA_SESSION_CONTINUESESSION;
         TSS2_RC rc;
@@ -1596,7 +1677,7 @@ static int tpm2_make_encryption_session(
                         ESYS_TR_NONE,
                         NULL,
                         TPM2_SE_HMAC,
-                        &symmetric,
+                        &c->session_symmetric_template,
                         TPM2_ALG_SHA256,
                         &session->esys_handle);
         if (rc != TSS2_RC_SUCCESS)
@@ -1625,11 +1706,6 @@ static int tpm2_make_policy_session(
                 bool trial,
                 Tpm2Handle **ret_session) {
 
-        static const TPMT_SYM_DEF symmetric = {
-                .algorithm = TPM2_ALG_AES,
-                .keyBits.aes = 128,
-                .mode.aes = TPM2_ALG_CFB,
-        };
         TPM2_SE session_type = trial ? TPM2_SE_TRIAL : TPM2_SE_POLICY;
         TSS2_RC rc;
         int r;
@@ -1659,7 +1735,7 @@ static int tpm2_make_policy_session(
                         ESYS_TR_NONE,
                         NULL,
                         session_type,
-                        &symmetric,
+                        &c->session_symmetric_template,
                         TPM2_ALG_SHA256,
                         &session->esys_handle);
         if (rc != TSS2_RC_SUCCESS)
