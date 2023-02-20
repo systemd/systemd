64aa0c9f0f03150d49f857a4dd9ce3a0ffcb098c
Lennart Poettering <lennart@poettering.net>
2023-02-20 11:43:13 +0100

    cap-list: add CAPABILITY_TO_STRING() macro using compound initialization to allocate fallback buffer
    Let's add a helper that can return a numeric string in case we don't
    recognize a name for a capability.

--- src/basic/cap-list.h
+++ src/basic/cap-list.h
@@ -3,7 +3,12 @@
 
 #include <inttypes.h>
 
+#define CAPABILITY_TO_STRING_MAX (2 + 8 + 1)
+
 const char *capability_to_name(int id);
+const char *capability_to_string(int id, char buf[static CAPABILITY_TO_STRING_MAX]);
+#define CAPABILITY_TO_STRING(id) capability_to_string(id, (char[CAPABILITY_TO_STRING_MAX]) {})
+
 int capability_from_name(const char *name);
 int capability_list_length(void);
 
