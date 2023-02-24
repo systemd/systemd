73036f4f4fe43573a505ae855d0aced251fc4090
maanyagoenka <maanyagoenka@microsoft.com>
2023-02-24 01:25:46 +0000

    extension-release: establish compatibility between host file and syscfg-release file
    The syscfg-release file that accompanies the syscfg images needs to be
    host compatible to be able to be merged into the host /etc/ directory.
    This commit checks for version compatibility between the image file and
    the host file.

--- src/portable/portable.c
+++ src/portable/portable.c
@@ -623,7 +623,7 @@ static int extract_image_and_extensions(
                         return r;
 
                 if (validate_sysext) {
-                        r = extension_release_validate(ext->path, id, version_id, sysext_level, "portable", extension_release);
+                        r = extension_release_validate_internal(ext->path, id, version_id, sysext_level, "portable", extension_release, "SYSEXT_LEVEL", "SYSEXT_SCOPE");
                         if (r == 0)
                                 return sd_bus_error_set_errnof(error, SYNTHETIC_ERRNO(ESTALE), "Image %s extension-release metadata does not match the root's", ext->path);
                         if (r < 0)
