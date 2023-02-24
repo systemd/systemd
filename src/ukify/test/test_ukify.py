552c9588c1d651b0d4f264eb3b004d6d25c08981
Jan Janssen <medhefgo@web.de>
2023-02-24 19:23:01 +0100

    tree-wide: Use correct SPDX license identifier

--- src/ukify/test/test_ukify.py
+++ src/ukify/test/test_ukify.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# SPDX-License-Identifier: LGPL-2.1+
+# SPDX-License-Identifier: LGPL-2.1-or-later
 
 # pylint: disable=missing-docstring,redefined-outer-name,invalid-name
 # pylint: disable=unused-import,import-outside-toplevel,useless-else-on-loop
