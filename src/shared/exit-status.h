9cf7aeea47ab86a8366f963c23fb1921793fd6a1
Stefan Roesch <shr@devkernel.io>
2023-02-28 12:39:35 -0800

    add support for KSM
    This adds support for KSM (kernel samepage merging). It adds a new
    boolean parameter called MemoryKSM to enable the feature. The feature is
    can only be enabled with newer Linux kernels.

--- src/shared/exit-status.h
+++ src/shared/exit-status.h
@@ -72,6 +72,7 @@ enum {
         EXIT_NUMA_POLICY,
         EXIT_CREDENTIALS,
         EXIT_BPF,
+	EXIT_KSM,
 
         EXIT_EXCEPTION = 255,  /* Whenever we want to propagate an abnormal/signal exit, in line with bash */
 };
