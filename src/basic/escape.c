c9c7e9c8025441a0d1b8287b04b40635ca521c7e
msizanoen1 <msizanoen@qtmlabs.xyz>
2023-03-02 18:37:02 +0700

    escape: add missing non-NULL parameter assertions

--- src/basic/escape.c
+++ src/basic/escape.c
@@ -473,6 +473,8 @@ char* octescape(const char *s, size_t len) {
 
 static char* strcpy_backslash_escaped(char *t, const char *s, const char *bad) {
         assert(bad);
+        assert(t);
+        assert(s);
 
         while (*s) {
                 int l = utf8_encoded_valid_unichar(s, SIZE_MAX);
