1b77df43ac3b2793b2d3b804c68bff6911c9789b
Mike Yuan <me@yhndnzj.com>
2023-03-05 23:27:44 +0800

    systemctl: add option --when for scheduled shutdown
    Pass an empty string or "cancel" will cancel the action.
    Pass "show" will show the scheduled actions.
    
    Replaces #17258

--- src/systemctl/systemctl-logind.c
+++ src/systemctl/systemctl-logind.c
@@ -356,7 +356,7 @@ int logind_show_shutdown(void) {
         _cleanup_(sd_bus_error_free) sd_bus_error error = SD_BUS_ERROR_NULL;
         _cleanup_(sd_bus_message_unrefp) sd_bus_message *reply = NULL;
         sd_bus *bus;
-        const char *action = NULL;
+        const char *action, *pretty_action;
         uint64_t elapse;
         int r;
 
@@ -376,17 +376,23 @@ int logind_show_shutdown(void) {
                 return log_error_errno(SYNTHETIC_ERRNO(ENODATA), "No scheduled shutdown.");
 
         if (STR_IN_SET(action, "halt", "poweroff", "exit"))
-                action = "Shutdown";
+                pretty_action = "Shutdown";
         else if (streq(action, "kexec"))
-                action = "Reboot via kexec";
+                pretty_action = "Reboot via kexec";
         else if (streq(action, "reboot"))
-                action = "Reboot";
+                pretty_action = "Reboot";
+        else /* If we don't recognize the action string, we'll show it as-is */
+                pretty_action = action;
 
-        /* If we don't recognize the action string, we'll show it as-is */
-
-        log_info("%s scheduled for %s, use 'shutdown -c' to cancel.",
-                 action,
-                 FORMAT_TIMESTAMP_STYLE(elapse, arg_timestamp_style));
+        if (arg_action == ACTION_SYSTEMCTL)
+                log_info("%s scheduled for %s, use 'systemctl %s --when=cancel' to cancel.",
+                         pretty_action,
+                         FORMAT_TIMESTAMP_STYLE(elapse, arg_timestamp_style),
+                         action);
+        else
+                log_info("%s scheduled for %s, use 'shutdown -c' to cancel.",
+                         pretty_action,
+                         FORMAT_TIMESTAMP_STYLE(elapse, arg_timestamp_style));
 
         return 0;
 #else
