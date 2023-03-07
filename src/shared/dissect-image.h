c571dee74bf5bea67bbe94e3734d19b1092765dd
Lennart Poettering <lennart@poettering.net>
2023-03-07 14:39:47 +0100

    dissect: add new helper dissected_partition_fstype()
    Initially we only have one user, but following patches will add more.

--- src/shared/dissect-image.h
+++ src/shared/dissect-image.h
@@ -193,3 +193,9 @@ int dissect_fstype_ok(const char *fstype);
 
 int probe_sector_size(int fd, uint32_t *ret);
 int probe_sector_size_prefer_ioctl(int fd, uint32_t *ret);
+
+static inline const char *dissected_partition_fstype(const DissectedPartition *m) {
+        assert(m);
+
+        return m->decrypted_node ? m->decrypted_fstype : m->fstype;
+}
