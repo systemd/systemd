58d9c66ed58bafd0447e7ef74d60daa1b42da397
Daan De Meyer <daan.j.demeyer@gmail.com>
2023-03-09 12:16:54 +0100

    lock-util: Add unposix_lock()
    Let's add an interface to UNPOSIX locks that mimicks the flock()
    interface for BSD locks.

--- src/shared/lock-util.h
+++ src/shared/lock-util.h
@@ -1,6 +1,8 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 #pragma once
 
+#include <fcntl.h>
+
 typedef struct LockFile {
         char *path;
         int fd;
@@ -12,3 +14,6 @@ int make_lock_file_for(const char *p, int operation, LockFile *ret);
 void release_lock_file(LockFile *f);
 
 #define LOCK_FILE_INIT { .fd = -EBADF, .path = NULL }
+
+/* Open File Description locks with the same interface as flock(). */
+int unposix_lock(int fd, int operation);
