b1237f5f4c671fc1b40021941a8dd345be0aff19
Mike Yuan <me@yhndnzj.com>
2023-03-13 01:44:51 +0800

    edit-util: include the correct header

--- src/shared/edit-util.h
+++ src/shared/edit-util.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 #pragma once
 
-#include "path-lookup.h"
+#include <stdbool.h>
 
 typedef struct EditFile EditFile;
 typedef struct EditFileContext EditFileContext;
