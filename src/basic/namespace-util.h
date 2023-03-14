9dda59522ac2b85f75e317baaecb241bf182dd58
Lennart Poettering <lennart@poettering.net>
2023-03-14 17:22:18 +0100

    namespace-util: add helper for allocating an empty userns fd

--- src/basic/namespace-util.h
+++ src/basic/namespace-util.h
@@ -51,8 +51,11 @@ static inline bool userns_shift_range_valid(uid_t shift, uid_t range) {
         return true;
 }
 
+int userns_acquire_empty(void);
 int userns_acquire(const char *uid_map, const char *gid_map);
+
 int netns_acquire(void);
+
 int in_same_namespace(pid_t pid1, pid_t pid2, NamespaceType type);
 
 int parse_userns_uid_range(const char *s, uid_t *ret_uid_shift, uid_t *ret_uid_range);
