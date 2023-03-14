19ecb5adc3a103551b955d0eefbd2896d8fe2841
Lennart Poettering <lennart@poettering.net>
2023-03-14 11:09:57 +0100

    mount-util: add make_fsmount()

--- src/shared/mount-util.h
+++ src/shared/mount-util.h
@@ -116,3 +116,5 @@ int remount_sysfs(const char *where);
 /* Creates a mount point (not parents) based on the source path or stat - ie, a file or a directory */
 int make_mount_point_inode_from_stat(const struct stat *st, const char *dest, mode_t mode);
 int make_mount_point_inode_from_path(const char *source, const char *dest, mode_t mode);
+
+int make_fsmount(int error_log_level, const char *what, const char *type, unsigned long flags, const char *options, int userns_fd);
