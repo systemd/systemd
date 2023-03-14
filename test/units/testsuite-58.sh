add9e42a778b3e54be55837e9ee4e1a5462d0ed0
Thomas Blume <Thomas.Blume@suse.com>
2023-03-14 15:21:29 +0100

    test: use setpriv instead of su for user switch from root
    systemd-repart needs to find mkfs.ext4 for the test.
    This is located in the directory /usr/sbin on openSUSE Tumbleweed.
    But since the variable ALWAYS_SET_PATH in /etc/login.defs is set to yes,
    su re-initializes the $PATH variable and removes /usr/sbin.
    Hence, mkfs.ext4 is not found and the test fails.
    Using setpriv instead of su fixes this issue and is more appropriate to
    do the switch user task from root.

--- test/units/testsuite-58.sh
+++ test/units/testsuite-58.sh
@@ -7,7 +7,7 @@ runas() {
     declare userid=$1
     shift
     # shellcheck disable=SC2016
-    su "$userid" -s /bin/sh -c 'XDG_RUNTIME_DIR=/run/user/$UID exec "$@"' -- sh "$@"
+    XDG_RUNTIME_DIR=/run/user/$(id -u "$userid") setpriv --reuid="$userid" --init-groups "$@"
 }
 
 if ! command -v systemd-repart &>/dev/null; then
