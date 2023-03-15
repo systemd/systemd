systemd/systemd

commit 017a7ba4f406adcf69d6b3ec15b9f2d9ed5ad853

core: propagate stop too if restart is issued
The restart of a unit should also be considered as that
a stop job happens, and thus gets propagated to units
specified using PropagatesStopTo=.

Fixes #24068

src/core/transaction.c
@@ -1047,27 +1047,39 @@ int transaction_add_job_and_dependencies(
                 }
 
                 if (IN_SET(type, JOB_STOP, JOB_RESTART)) {
-                        UnitDependencyAtom atom;
-                        JobType ptype;
-
+                        _cleanup_set_free_ Set *propagated_restart = NULL;
                         /* We propagate STOP as STOP, but RESTART only as TRY_RESTART, in order not to start
                          * dependencies that are not around. */
-                        if (type == JOB_RESTART) {
-                                atom = UNIT_ATOM_PROPAGATE_RESTART;
-                                ptype = JOB_TRY_RESTART;
-                        } else {
-                                ptype = JOB_STOP;
-                                atom = UNIT_ATOM_PROPAGATE_STOP;
-                        }
 
-                        UNIT_FOREACH_DEPENDENCY(dep, ret->unit, atom) {
-                                JobType nt;
+                        if (type == JOB_RESTART)
+                                UNIT_FOREACH_DEPENDENCY(dep, ret->unit, UNIT_ATOM_PROPAGATE_RESTART) {
+                                        JobType nt;
 
-                                nt = job_type_collapse(ptype, dep);
-                                if (nt == JOB_NOP)
+                                        r = set_ensure_put(&propagated_restart, NULL, dep);
+                                        if (r < 0)
+                                                return r;
+
+                                        nt = job_type_collapse(JOB_TRY_RESTART, dep);
+                                        if (nt == JOB_NOP)
+                                                continue;
+
+                                        r = transaction_add_job_and_dependencies(tr, nt, dep, ret, true, false, false, ignore_order, e);
+                                        if (r < 0) {
+                                                if (r != -EBADR) /* job type not applicable */
+                                                        return r;
+
+                                                sd_bus_error_free(e);
+                                        }
+                                }
+
+                        /* The 'stop' part of a restart job is also propagated to
+                         * units with UNIT_ATOM_PROPAGATE_STOP */
+                        UNIT_FOREACH_DEPENDENCY(dep, ret->unit, UNIT_ATOM_PROPAGATE_STOP) {
+                                /* Units experienced restart propagation are skipped */
+                                if (set_contains(propagated_restart, dep))
                                         continue;
 
-                                r = transaction_add_job_and_dependencies(tr, nt, dep, ret, true, false, false, ignore_order, e);
+                                r = transaction_add_job_and_dependencies(tr, JOB_STOP, dep, ret, true, false, false, ignore_order, e);
                                 if (r < 0) {
                                         if (r != -EBADR) /* job type not applicable */
                                                 return r;

commit e37799a325959ac4a99a0758f24e5e9f2ec02157

core: propagate stop as restart if there's pending (re)start job
When a restart is being propagated as stop, the unit that
issues propagation could also depend on the unit to be stopped.
This results in a job type conflict and failure to pull in
the dependent unit.

After this commit, we store the units that has a (re)start job
in a set. If they shall get stop propagation, a restart job is
added instead.

Fixes #26839 (bug introduced in 017a7ba4f406adcf69d6b3ec15b9f2d9ed5ad853)

src/core/transaction.c
@@ -902,6 +902,7 @@ int transaction_add_job_and_dependencies(
                 bool ignore_order,
                 sd_bus_error *e) {
 
+        _cleanup_set_free_ Set *to_start = NULL;
         bool is_new;
         Unit *dep;
         Job *ret;
@@ -994,6 +995,12 @@ int transaction_add_job_and_dependencies(
                 /* Finally, recursively add in all dependencies. */
                 if (IN_SET(type, JOB_START, JOB_RESTART)) {
                         UNIT_FOREACH_DEPENDENCY(dep, ret->unit, UNIT_ATOM_PULL_IN_START) {
+                                if (type == JOB_RESTART) {
+                                        r = set_ensure_put(&to_start, NULL, dep);
+                                        if (r < 0)
+                                                return r;
+                                }
+
                                 r = transaction_add_job_and_dependencies(tr, JOB_START, dep, ret, true, false, false, ignore_order, e);
                                 if (r < 0) {
                                         if (r != -EBADR) /* job type not applicable */
@@ -1004,6 +1011,12 @@ int transaction_add_job_and_dependencies(
                         }
 
                         UNIT_FOREACH_DEPENDENCY(dep, ret->unit, UNIT_ATOM_PULL_IN_START_IGNORED) {
+                                if (type == JOB_RESTART) {
+                                        r = set_ensure_put(&to_start, NULL, dep);
+                                        if (r < 0)
+                                                return r;
+                                }
+
                                 r = transaction_add_job_and_dependencies(tr, JOB_START, dep, ret, false, false, false, ignore_order, e);
                                 if (r < 0) {
                                         /* unit masked, job type not applicable and unit not found are not considered as errors. */
@@ -1079,7 +1092,14 @@ int transaction_add_job_and_dependencies(
                                 if (set_contains(propagated_restart, dep))
                                         continue;
 
-                                r = transaction_add_job_and_dependencies(tr, JOB_STOP, dep, ret, true, false, false, ignore_order, e);
+                                if (set_contains(to_start, dep))
+                                        /* dep has a pending start job in this transaction, which is pulled
+                                         * in due to UNIT_ATOM_PULL_IN_START(_IGNORED). In this case, let's
+                                         * add a restart job directly, which fits the ultimate goal and
+                                         * avoids job type conflict. */
+                                        r = transaction_add_job_and_dependencies(tr, JOB_RESTART, dep, ret, true, false, false, ignore_order, e);
+                                else
+                                        r = transaction_add_job_and_dependencies(tr, JOB_STOP, dep, ret, true, false, false, ignore_order, e);
                                 if (r < 0) {
                                         if (r != -EBADR) /* job type not applicable */
                                                 return r;