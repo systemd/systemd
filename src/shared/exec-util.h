7b847590e27f1ea4b0c794fadcbfba3d51b18530
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-03-16 11:34:14 +0900

    exec-util: introduce EXEC_DIR_SKIP_REMAINING flag
    Will be used in later commits.

--- src/shared/exec-util.h
+++ src/shared/exec-util.h
@@ -5,6 +5,8 @@
 
 #include "time-util.h"
 
+#define EXIT_SKIP_REMAINING 77
+
 typedef int (*gather_stdout_callback_t) (int fd, void *arg);
 
 enum {
@@ -19,6 +21,7 @@ typedef enum {
         EXEC_DIR_PARALLEL             = 1 << 0, /* Execute scripts in parallel, if possible */
         EXEC_DIR_IGNORE_ERRORS        = 1 << 1, /* Ignore non-zero exit status of scripts */
         EXEC_DIR_SET_SYSTEMD_EXEC_PID = 1 << 2, /* Set $SYSTEMD_EXEC_PID environment variable */
+        EXEC_DIR_SKIP_REMAINING       = 1 << 3, /* Ignore remaining executions when one exit with 77. */
 } ExecDirFlags;
 
 typedef enum ExecCommandFlags {
