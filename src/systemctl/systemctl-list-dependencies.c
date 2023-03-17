a9f2f486f3d1dd1cd2a58cd384b0e88970e2fda9
David Tardon <dtardon@redhat.com>
2023-03-17 09:53:23 +0100

    systemctl: fix indentation of circular deps
    We're still at level `level` here.
    
    Before:
    b.target
    ○ ├─a.target
      │   └─...
    ● └─paths.target
    
    After:
    b.target
    ○ ├─a.target
      │ └─...
    ● └─paths.target
    
    Fixes #26052.

--- src/systemctl/systemctl-list-dependencies.c
+++ src/systemctl/systemctl-list-dependencies.c
@@ -86,7 +86,7 @@ static int list_dependencies_one(
                 if (strv_contains(*units, *c)) {
                         if (!arg_plain) {
                                 printf("  ");
-                                r = list_dependencies_print("...", level + 1, (branches << 1) | (c[1] == NULL ? 0 : 1), /* last = */ true);
+                                r = list_dependencies_print("...", level, branches, /* last = */ true);
                                 if (r < 0)
                                         return r;
                         }
