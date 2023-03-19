e6745e0fa584086c8c4f197bddd8da51036a8523
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-03-20 02:55:56 +0900

    kernel-install: add --esp-path= and --boot-path= options
    Then, kernel-install takes one more step for compatibility with bootctl.

--- src/kernel-install/kernel-install.c
+++ src/kernel-install/kernel-install.c
@@ -17,6 +17,7 @@
 #include "kernel-image.h"
 #include "main-func.h"
 #include "mkdir.h"
+#include "parse-argument.h"
 #include "path-util.h"
 #include "pretty-print.h"
 #include "rm-rf.h"
@@ -27,6 +28,11 @@
 #include "verbs.h"
 
 static bool arg_verbose = false;
+static char *arg_esp_path = NULL;
+static char *arg_xbootldr_path = NULL;
+
+STATIC_DESTRUCTOR_REGISTER(arg_esp_path, freep);
+STATIC_DESTRUCTOR_REGISTER(arg_xbootldr_path, freep);
 
 typedef enum Action {
         ACTION_ADD,
@@ -409,7 +415,7 @@ static int context_acquire_xbootldr(Context *c) {
 
         r = find_xbootldr_and_warn(
                         /* root = */ NULL,
-                        /* path = */ NULL,
+                        /* path = */ arg_xbootldr_path,
                         /* unprivileged_mode= */ geteuid() != 0,
                         /* ret_path = */ &c->boot_root,
                         /* ret_uuid = */ NULL,
@@ -433,7 +439,7 @@ static int context_acquire_esp(Context *c) {
 
         r = find_esp_and_warn(
                         /* root = */ NULL,
-                        /* path = */ NULL,
+                        /* path = */ arg_esp_path,
                         /* unprivileged_mode= */ geteuid() != 0,
                         /* ret_path = */ &c->boot_root,
                         /* ret_part = */ NULL,
@@ -1062,6 +1068,8 @@ static int help(void) {
                "  -h --help              Show this help\n"
                "     --version           Show package version\n"
                "  -v --verbose           Increase verbosity\n"
+               "     --esp-path=PATH     Path to the EFI System Partition (ESP)\n"
+               "     --boot-path=PATH    Path to the $BOOT partition\n"
                "\nSee the %4$s for details.\n",
                program_invocation_short_name,
                ansi_highlight(),
@@ -1074,14 +1082,18 @@ static int help(void) {
 static int parse_argv(int argc, char *argv[]) {
         enum {
                 ARG_VERSION = 0x100,
+                ARG_ESP_PATH,
+                ARG_BOOT_PATH,
         };
         static const struct option options[] = {
                 { "help",                 no_argument,       NULL, 'h'                      },
                 { "version",              no_argument,       NULL, ARG_VERSION              },
                 { "verbose",              no_argument,       NULL, 'v'                      },
+                { "esp-path",             required_argument, NULL, ARG_ESP_PATH             },
+                { "boot-path",            required_argument, NULL, ARG_BOOT_PATH            },
                 {}
         };
-        int t;
+        int t, r;
 
         assert(argc >= 0);
         assert(argv);
@@ -1097,6 +1109,18 @@ static int parse_argv(int argc, char *argv[]) {
                 case 'v':
                         log_set_max_level(LOG_DEBUG);
                         arg_verbose = true;
+                        break;
+
+                case ARG_ESP_PATH:
+                        r = parse_path_argument(optarg, /* suppress_root = */ false, &arg_esp_path);
+                        if (r < 0)
+                                return log_oom();
+                        break;
+
+                case ARG_BOOT_PATH:
+                        r = parse_path_argument(optarg, /* suppress_root = */ false, &arg_xbootldr_path);
+                        if (r < 0)
+                                return log_oom();
                         break;
 
                 case '?':
