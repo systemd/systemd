3c3493fcc528c24a772eef4ae252fc6bdd221fd1
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-03-21 16:44:50 +0900

    uki-util: rename KernelType -> KernelImageType
    The name KernelType is slightly ambiguous. The type is about kernel
    image. Let's rename the enum to clarify that.

--- src/boot/bootctl-uki.c
+++ src/boot/bootctl-uki.c
@@ -5,27 +5,27 @@
 #include "uki-util.h"
 
 int verb_kernel_identify(int argc, char *argv[], void *userdata) {
-        KernelType t;
+        KernelImageType t;
         int r;
 
         r = inspect_kernel(argv[1], &t, NULL, NULL, NULL);
         if (r < 0)
                 return r;
 
-        puts(kernel_type_to_string(t));
+        puts(kernel_image_type_to_string(t));
         return 0;
 }
 
 int verb_kernel_inspect(int argc, char *argv[], void *userdata) {
         _cleanup_free_ char *cmdline = NULL, *uname = NULL, *pname = NULL;
-        KernelType t;
+        KernelImageType t;
         int r;
 
         r = inspect_kernel(argv[1], &t, &cmdline, &uname, &pname);
         if (r < 0)
                 return r;
 
-        printf("Kernel Type: %s\n", kernel_type_to_string(t));
+        printf("Kernel Type: %s\n", kernel_image_type_to_string(t));
         if (cmdline)
                 printf("    Cmdline: %s\n", cmdline);
         if (uname)
--- src/shared/uki-util.h
+++ src/shared/uki-util.h
@@ -5,19 +5,19 @@
 
 #include "macro.h"
 
-typedef enum KernelType {
-        KERNEL_TYPE_UNKNOWN,
-        KERNEL_TYPE_UKI,
-        KERNEL_TYPE_PE,
-        _KERNEL_TYPE_MAX,
-        _KERNEL_TYPE_INVALID = -EINVAL,
-} KernelType;
+typedef enum KernelImageType {
+        KERNEL_IMAGE_TYPE_UNKNOWN,
+        KERNEL_IMAGE_TYPE_UKI,
+        KERNEL_IMAGE_TYPE_PE,
+        _KERNEL_IMAGE_TYPE_MAX,
+        _KERNEL_IMAGE_TYPE_INVALID = -EINVAL,
+} KernelImageType;
 
-const char* kernel_type_to_string(KernelType t) _const_;
+const char* kernel_image_type_to_string(KernelImageType t) _const_;
 
 int inspect_kernel(
                 const char *filename,
-                KernelType *ret_type,
+                KernelImageType *ret_type,
                 char **ret_cmdline,
                 char **ret_uname,
                 char **ret_pretty_name);
