ae556492cb8a73acace7a594649d024bca34a20e
Mike Yuan <me@yhndnzj.com>
2023-03-22 03:40:52 +0800

    test: add tests for NOTIFYACCESS override through sd_notify

---
+++ test/testsuite-80.units/test.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: LGPL-2.1-or-later
+# shellcheck disable=SC2016
+set -eux
+set -o pipefail
+
+systemd-notify --status="Test starts, waiting for 5 seconds"
+sleep 5
+
+(
+    systemd-notify "MAINPID=${BASHPID}"
+    systemd-notify "NOTIFYACCESS=main"
+
+    systemd-notify --status="Sending READY=1 in an unpriviledged process"
+    ( systemd-notify --ready )
+    sleep 10
+
+    systemd-notify "MAINPID=$$"
+)
+
+systemd-notify --ready --status="OK"
+sleep infinity
