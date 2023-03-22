87a572cc75b6d7b4c88c633719253154091bbd8b
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2023-03-22 13:42:40 +0100

    test-coredump-util: add tests for parse_aux()
    The test files are /proc//auxv files copies from various architecutres
    signified by the file name suffix.
    
    Those tests are fairly simple, but when we run them on n architectures, we do
    ~n² cross-arch tests.

--- src/test/test-coredump-util.c
+++ src/test/test-coredump-util.c
@@ -2,6 +2,9 @@
 
 #include "alloc-util.h"
 #include "coredump-util.h"
+#include "fileio.h"
+#include "fd-util.h"
+#include "format-util.h"
 #include "macro.h"
 #include "tests.h"
 
@@ -62,6 +65,60 @@ TEST(coredump_filter_mask_from_string) {
                                 1 << COREDUMP_FILTER_SHARED_HUGE |
                                 1 << COREDUMP_FILTER_PRIVATE_DAX |
                                 1 << COREDUMP_FILTER_SHARED_DAX)));
+}
+
+static void test_parse_auxv_one(
+                int dir_fd,
+                const char *filename,
+                bool elf64bit,
+                int expect_at_secure,
+                uid_t expect_uid,
+                uid_t expect_euid,
+                gid_t expect_gid,
+                gid_t expect_egid) {
+
+        _cleanup_free_ char *data;
+        size_t data_size;
+        log_info("Parsing %s…", filename);
+        assert_se(read_full_file_at(dir_fd, filename, &data, &data_size) >= 0);
+
+        int at_secure;
+        uid_t uid, euid;
+        gid_t gid, egid;
+        assert_se(parse_auxv(LOG_ERR, elf64bit, data, data_size,
+                             &at_secure, &uid, &euid, &gid, &egid) == 0);
+
+        log_info("at_secure=%d, uid="UID_FMT", euid="UID_FMT", gid="GID_FMT", egid="GID_FMT,
+                 at_secure, uid, euid, gid, egid);
+
+        assert_se(uid == expect_uid);
+        assert_se(euid == expect_euid);
+        assert_se(gid == expect_gid);
+        assert_se(egid == expect_egid);
+}
+
+TEST(test_parse_auxv) {
+        _cleanup_free_ char *dir;
+        _cleanup_close_ int dir_fd;
+
+        assert_se(get_testdata_dir("auxv", &dir) >= 0);
+        dir_fd = open(dir, O_RDONLY | O_CLOEXEC | O_DIRECTORY | O_PATH);
+        assert_se(dir_fd >= 0);
+
+        test_parse_auxv_one(dir_fd, "resolved.arm32", false, 0, 193, 193, 193, 193);
+        test_parse_auxv_one(dir_fd, "bash.riscv64", true, 0, 1001, 1001, 1001, 1001);
+        test_parse_auxv_one(dir_fd, "sleep.i686", false, 0, 1000, 1000, 1000, 1000);
+        /* after chgrp and chmod g+s */
+        test_parse_auxv_one(dir_fd, "sleep32.i686", false, 1, 1000, 1000, 1000, 10);
+        test_parse_auxv_one(dir_fd, "sleep64.amd64", true, 1, 1000, 1000, 1000, 10);
+
+        test_parse_auxv_one(dir_fd, "sudo.aarch64", true, 1, 1494200408, 0, 1494200408, 1494200408);
+        test_parse_auxv_one(dir_fd, "sudo.amd64", true, 1, 1000, 0, 1000, 1000);
+
+        /* Those run unprivileged, but start as root. */
+        test_parse_auxv_one(dir_fd, "dbus-broker-launch.amd64", true, 0, 0, 0, 0, 0);
+        test_parse_auxv_one(dir_fd, "dbus-broker-launch.aarch64", true, 0, 0, 0, 0, 0);
+        test_parse_auxv_one(dir_fd, "polkitd.aarch64", true, 0, 0, 0, 0, 0);
 }
 
 DEFINE_TEST_MAIN(LOG_INFO);
