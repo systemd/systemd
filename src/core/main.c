systemd/systemd

commit 26abdc73a212b90f7c4b71808a1028d2e87ab09f

fix off by one error in array index assertion
Since the index is already post-incremented when the array is appended
to, this assertion can be wrongly reached when the array is at capacity
(with the NULL terminator). The bug is reproducible on shutdown with
the following settings in /etc/systemd/system.conf:

  LogTarget=journal-or-kmsg
  LogColor=yes
  LogLocation=yes

Reported by Thermi on IRC.

src/core/main.c
@@ -1994,7 +1994,7 @@ finish:
                 if (log_get_show_location())
                         command_line[pos++] = "--log-location";
 
-                assert(pos + 1 < ELEMENTSOF(command_line));
+                assert(pos < ELEMENTSOF(command_line));
 
                 if (arm_reboot_watchdog && arg_shutdown_watchdog > 0) {
                         char *e;

commit 1fefac9ddae1513a6a46e792ee8660e3638fe010

core/main: restore the correct assert about array position
'pos' is incremented after each assignment. If we use the maximum number
of arguments, we end up with pos==9 after all the assignments, and it
points to where the next value would be assigned. This position must remain
NULL.

The assert I "fixed" was intentionally introduced in
26abdc73a212b90f7c4b71808a1028d2e87ab09f as a bugfix. So my "fix" repeated
the same error that was fixed back then.

src/core/main.c
@@ -1572,7 +1572,7 @@ static int become_shutdown(int objective, int retval) {
                 command_line[pos++] = exit_code;
         }
 
-        assert(pos < ELEMENTSOF(command_line) - 1);
+        assert(pos < ELEMENTSOF(command_line));
 
         /* The watchdog: */
 