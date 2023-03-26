801330273c3892c2d5537270f590d1898ba627c0
Daan De Meyer <daan.j.demeyer@gmail.com>
2023-03-26 18:20:41 +0200

    label: Introduce LabelOps to do pre/post labelling operations
    By default, label_ops is initialized with a NULL pointer which translates
    to noop labelling operations. In the new mac_init() function, we initialize
    label_ops with a pair of pre/post functions that do SELINUX/SMACK labelling.
    
    mac_init() also calls mac_selinux_init() and all usages of mac_selinux_init()
    are replaced with mac_init().

--- src/hwdb/hwdb.c
+++ src/hwdb/hwdb.c
@@ -124,7 +124,7 @@ static int run(int argc, char *argv[]) {
         if (r <= 0)
                 return r;
 
-        r = mac_selinux_init();
+        r = mac_init();
         if (r < 0)
                 return r;
 
