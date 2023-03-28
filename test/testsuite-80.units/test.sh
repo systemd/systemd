66a8598aef4054bf0f7f5ce90ebe704639dec3b1
Lennart Poettering <lennart@poettering.net>
2023-03-28 16:35:35 +0200

    TEST-80: synchronize explicitly instead of by time
    This removes "sleep" invocations, and makes the notify access testcase a
    lot more robust to runtime jitter. We use a pair of fifos in the fs to
    sync instead.
    
    Also various other improvoements, including comments.
    
    (Also removes the unnecessary "no-qemu" restriction)

--- test/testsuite-80.units/test.sh
+++ test/testsuite-80.units/test.sh
@@ -4,23 +4,60 @@
 set -eux
 set -o pipefail
 
-systemd-notify --status="Test starts, waiting for 5 seconds"
-sleep 5
+sync_in() {
+    read x < /tmp/syncfifo2
+    test "$x" = "$1"
+}
 
+sync_out() {
+    echo "$1" > /tmp/syncfifo1
+}
+
+export SYSTEMD_LOG_LEVEL=debug
+
+echo "toplevel PID: $BASHPID"
+
+systemd-notify --status="Test starts"
+sync_out a
+sync_in b
 (
-    systemd-notify --pid=auto
+    echo "subshell PID: $BASHPID"
+
+    # Make us main process
+    systemd-notify --pid="$BASHPID"
+
+    # Lock down access to just us
     systemd-notify "NOTIFYACCESS=main"
 
-    systemd-notify --status="Sending READY=1 in an unpriviledged process"
-    (
-        sleep 0.1
-        systemd-notify --ready
-    )
-    sleep 10
+    # This should still work
+    systemd-notify --status="Sending READY=1 in an unprivileged process"
 
-    systemd-notify "MAINPID=$$"
+    # Send as subprocess of the subshell, this should not work
+    systemd-notify --ready --pid=self --status "BOGUS1"
+
+    sync_out c
+    sync_in d
+
+    # Move main process back to toplevel
+    systemd-notify --pid=parent "MAINPID=$$"
+
+    # Should be dropped again
+    systemd-notify --status="BOGUS2" --pid=parent
+
+    # Apparently, bash will automatically invoke the last command in a subshell
+    # via a simple execve() rather than fork()ing first. But we want that the
+    # previous command uses the subshell's PID, hence let's insert a final,
+    # bogus redundant command as last command to run in the subshell, so that
+    # bash can't optimize things like that.
+    echo "bye"
 )
+
+echo "toplevel again: $BASHPID"
 
 systemd-notify --ready --status="OK"
 systemd-notify "NOTIFYACCESS=none"
-sleep infinity
+systemd-notify --status="BOGUS3"
+
+sync_out e
+
+exec sleep infinity
