2868be9479a31299e2d3fbef515b4ea645fe68ac
Daan De Meyer <daan.j.demeyer@gmail.com>
2023-03-28 12:47:06 +0200

    man: Add note about BSD locking to systemd-tmpfiles manpage.

--- src/tmpfiles/tmpfiles.c
+++ src/tmpfiles/tmpfiles.c
@@ -521,24 +521,6 @@ static DIR* opendir_nomod(const char *path) {
         return xopendirat_nomod(AT_FDCWD, path);
 }
 
-static int xopenat_nomod(int dir_fd, const char *path) {
-        int fd;
-
-        fd = xopenat(dir_fd, path, O_RDONLY|O_CLOEXEC|O_NOFOLLOW|O_NOATIME, 0);
-        if (fd >= 0)
-                return fd;
-
-        log_debug_errno(fd, "Cannot open %sfile \"%s\": %m", dir_fd == AT_FDCWD ? "" : "sub", path);
-        if (fd != -EPERM)
-                return fd;
-
-        fd = xopenat(dir_fd, path, O_RDONLY|O_CLOEXEC|O_NOFOLLOW, 0);
-        if (fd < 0)
-                log_debug_errno(fd, "Cannot open %sfile \"%s\": %m", dir_fd == AT_FDCWD ? "" : "sub", path);
-
-        return fd;
-}
-
 static inline nsec_t load_statx_timestamp_nsec(const struct statx_timestamp *ts) {
         assert(ts);
 
@@ -814,15 +796,10 @@ static int dir_cleanup(
                                            cutoff_nsec, sub_path, age_by_file, false))
                                 continue;
 
-                        fd = xopenat_nomod(dirfd(d), de->d_name);
-                        if (fd < 0) {
-                                if (fd != -ENOENT)
-                                        r = log_warning_errno(fd, "Opening file \"%s\" failed, ignoring: %m", sub_path);
-
-                                continue;
-                        }
-
-                        if (flock(fd, LOCK_EX|LOCK_NB) < 0) {
+                        fd = xopenat(dirfd(d), de->d_name, O_RDONLY|O_CLOEXEC|O_NOFOLLOW|O_NOATIME, 0);
+                        if (fd < 0 && fd != -ENOENT)
+                                log_warning_errno(fd, "Opening file \"%s\" failed, ignoring: %m", sub_path);
+                        if (fd >= 0 && flock(fd, LOCK_EX|LOCK_NB) < 0 && errno == EAGAIN) {
                                 log_debug_errno(errno, "Couldn't acquire shared BSD lock on file \"%s\", skipping: %m", p);
                                 continue;
                         }
