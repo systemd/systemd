208d9ad16840aa781efb68bb9c7b1d8f7d03760a
Franck Bui <fbui@suse.com>
2023-03-28 20:41:38 +0200

    test: fix regexp in testsuite-74.mount.sh
    Several whitespaces can separate "ext4" and "sd-mount-test" strings.

--- test/units/testsuite-74.mount.sh
+++ test/units/testsuite-74.mount.sh
@@ -122,7 +122,7 @@ systemd-mount --discover "$WORK_DIR/simple.img"
 test -e /run/media/system/simple.img/foo.bar
 # systemd-mount --list and systemd-umount require the loopback block device is initialized by udevd.
 udevadm settle --timeout 30
-assert_in "/dev/loop.* ext4 sd-mount-test" "$(systemd-mount --list --full)"
+assert_in "/dev/loop.* ext4 +sd-mount-test" "$(systemd-mount --list --full)"
 systemd-umount "$WORK_DIR/simple.img"
 
 # --owner + vfat
