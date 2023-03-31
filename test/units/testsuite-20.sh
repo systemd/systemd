d68ecba9eed479a1eb15289a0116768b93630dfc
Frantisek Sumsal <frantisek@sumsal.cz>
2023-03-31 18:42:38 +0200

    test: set ReadWritePaths= for test-.services when built w/ coverage
    Let's make the dropin, to make the build dir writable for gcov, a bit
    more generic, so it can be used by all units starting with prefix test-.
    This should help with a bunch of recent reports about missing coverage I
    got, as well as with existing test units using DynamicUser=true.
    
    This might feel a bit like a magic trick from behind the curtains, but I
    want to touch the actual tests as little as possible, since it would
    make the tests hard to read (see the various workarounds for
    sanitizers), and the coverage reports are generated only in a specific
    CI job anyway.

--- test/units/testsuite-20.sh
+++ test/units/testsuite-20.sh
@@ -13,8 +13,8 @@ INTERNALPID=$!
 disown
 
 # Start a test process outside of our own cgroup
-systemd-run -p DynamicUser=1 --unit=test20-sleep.service /bin/sleep infinity
-EXTERNALPID="$(systemctl show -P MainPID test20-sleep.service)"
+systemd-run -p DynamicUser=1 --unit=test-sleep.service /bin/sleep infinity
+EXTERNALPID="$(systemctl show -P MainPID test-sleep.service)"
 
 # Update our own main PID to the external test PID, this should work
 systemd-notify MAINPID="$EXTERNALPID"
@@ -54,7 +54,7 @@ test "$(systemctl show -P MainPID testsuite-20.service)" -eq "$INTERNALPID"
 systemd-notify --uid=1000 MAINPID=$$
 test "$(systemctl show -P MainPID testsuite-20.service)" -eq $$
 
-cat >/tmp/test20-mainpid.sh <<EOF
+cat >/tmp/test-mainpid.sh <<EOF
 #!/usr/bin/env bash
 
 set -eux
@@ -73,12 +73,12 @@ disown
 
 echo \$MAINPID >/run/mainpidsh/pid
 EOF
-chmod +x /tmp/test20-mainpid.sh
+chmod +x /tmp/test-mainpid.sh
 
-systemd-run --unit=test20-mainpidsh.service -p StandardOutput=tty -p StandardError=tty -p Type=forking -p RuntimeDirectory=mainpidsh -p PIDFile=/run/mainpidsh/pid /tmp/test20-mainpid.sh
-test "$(systemctl show -P MainPID test20-mainpidsh.service)" -eq "$(cat /run/mainpidsh/pid)"
+systemd-run --unit=test-mainpidsh.service -p StandardOutput=tty -p StandardError=tty -p Type=forking -p RuntimeDirectory=mainpidsh -p PIDFile=/run/mainpidsh/pid /tmp/test-mainpid.sh
+test "$(systemctl show -P MainPID test-mainpidsh.service)" -eq "$(cat /run/mainpidsh/pid)"
 
-cat >/tmp/test20-mainpid2.sh <<EOF
+cat >/tmp/test-mainpid2.sh <<EOF
 #!/usr/bin/env bash
 
 set -eux
@@ -98,12 +98,12 @@ disown
 echo \$MAINPID >/run/mainpidsh2/pid
 chown 1001:1001 /run/mainpidsh2/pid
 EOF
-chmod +x /tmp/test20-mainpid2.sh
+chmod +x /tmp/test-mainpid2.sh
 
-systemd-run --unit=test20-mainpidsh2.service -p StandardOutput=tty -p StandardError=tty -p Type=forking -p RuntimeDirectory=mainpidsh2 -p PIDFile=/run/mainpidsh2/pid /tmp/test20-mainpid2.sh
-test "$(systemctl show -P MainPID test20-mainpidsh2.service)" -eq "$(cat /run/mainpidsh2/pid)"
+systemd-run --unit=test-mainpidsh2.service -p StandardOutput=tty -p StandardError=tty -p Type=forking -p RuntimeDirectory=mainpidsh2 -p PIDFile=/run/mainpidsh2/pid /tmp/test-mainpid2.sh
+test "$(systemctl show -P MainPID test-mainpidsh2.service)" -eq "$(cat /run/mainpidsh2/pid)"
 
-cat >/dev/shm/test20-mainpid3.sh <<EOF
+cat >/dev/shm/test-mainpid3.sh <<EOF
 #!/usr/bin/env bash
 
 set -eux
@@ -124,11 +124,11 @@ ln -s ../mainpidsh/pid /run/mainpidsh3/pid
 # Quick assertion that the link isn't dead
 test -f /run/mainpidsh3/pid
 EOF
-chmod 755 /dev/shm/test20-mainpid3.sh
+chmod 755 /dev/shm/test-mainpid3.sh
 
 # This has to fail, as we shouldn't accept the dangerous PID file, and then
 # inotify-wait on it to be corrected which we never do.
-systemd-run --unit=test20-mainpidsh3.service \
+systemd-run --unit=test-mainpidsh3.service \
             -p StandardOutput=tty \
             -p StandardError=tty \
             -p Type=forking \
@@ -136,15 +136,15 @@ systemd-run --unit=test20-mainpidsh3.service \
             -p PIDFile=/run/mainpidsh3/pid \
             -p DynamicUser=1 \
             -p TimeoutStartSec=2s \
-            /dev/shm/test20-mainpid3.sh \
+            /dev/shm/test-mainpid3.sh \
     && { echo 'unexpected success'; exit 1; }
 
 # Test that this failed due to timeout, and not some other error
-test "$(systemctl show -P Result test20-mainpidsh3.service)" = timeout
+test "$(systemctl show -P Result test-mainpidsh3.service)" = timeout
 
 # Test that scope units work
-systemd-run --scope --unit test20-true.scope /bin/true
-test "$(systemctl show -P Result test20-true.scope)" = success
+systemd-run --scope --unit test-true.scope /bin/true
+test "$(systemctl show -P Result test-true.scope)" = success
 
 # Test that user scope units work as well
 
@@ -156,8 +156,8 @@ runas() {
 }
 
 systemctl start user@4711.service
-runas testuser systemd-run --scope --user --unit test20-true.scope /bin/true
-test "$(systemctl show -P Result test20-true.scope)" = success
+runas testuser systemd-run --scope --user --unit test-true.scope /bin/true
+test "$(systemctl show -P Result test-true.scope)" = success
 
 systemd-analyze log-level info
 
