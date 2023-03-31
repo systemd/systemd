17699a784aaa558d254ee786798c4c944dad4d64
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-03-31 16:07:23 +0900

    machine-id-setup: use id128_read_at()

--- src/shared/machine-id-setup.c
+++ src/shared/machine-id-setup.c
@@ -27,22 +27,17 @@
 #include "virt.h"
 
 static int generate_machine_id(const char *root, sd_id128_t *ret) {
-        const char *dbus_machine_id;
-        _cleanup_close_ int fd = -EBADF;
+        _cleanup_close_ int dir_fd = -EBADF;
         int r;
 
         assert(ret);
 
         /* First, try reading the D-Bus machine id, unless it is a symlink */
-        dbus_machine_id = prefix_roota(root, "/var/lib/dbus/machine-id");
-        fd = open(dbus_machine_id, O_RDONLY|O_CLOEXEC|O_NOCTTY|O_NOFOLLOW);
-        if (fd >= 0) {
-                if (id128_read_fd(fd, ID128_FORMAT_PLAIN, ret) >= 0) {
-                        log_info("Initializing machine ID from D-Bus machine ID.");
-                        return 0;
-                }
-
-                fd = safe_close(fd);
+        dir_fd = open(empty_to_root(root), O_CLOEXEC|O_DIRECTORY|O_PATH);
+        if (dir_fd >= 0 &&
+            id128_read_at(dir_fd, "/var/lib/dbus/machine-id", ID128_FORMAT_PLAIN | ID128_NOFOLLOW, ret) >= 0) {
+                log_info("Initializing machine ID from D-Bus machine ID.");
+                return 0;
         }
 
         if (isempty(root) && running_in_chroot() <= 0) {
