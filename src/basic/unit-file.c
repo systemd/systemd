4d86a0be9cbbb05748da4490ba6348e4995fef4e
OMOJOLA JOSHUA DAMILOLA <omojolajoshua@gmail.com>
2023-04-01 18:52:32 +0000

    src: changed usage of basename() to path_extract_filename(). TODO.

--- src/basic/unit-file.c
+++ src/basic/unit-file.c
@@ -71,7 +71,7 @@ int unit_symlink_name_compatible(const char *symlink, const char *target, bool i
 }
 
 int unit_validate_alias_symlink_or_warn(int log_level, const char *filename, const char *target) {
-        const char *src, *dst;
+        _cleanup_free_ const char *src = NULL, *dst = NULL;
         _cleanup_free_ char *src_instance = NULL, *dst_instance = NULL;
         UnitType src_unit_type, dst_unit_type;
         UnitNameFlags src_name_type, dst_name_type;
@@ -87,8 +87,8 @@ int unit_validate_alias_symlink_or_warn(int log_level, const char *filename, con
          * -ELOOP for an alias to self.
          */
 
-        src = basename(filename);
-        dst = basename(target);
+        path_extract_filename(filename, (char**)&src);
+        path_extract_filename(target, (char**)&dst);
 
         /* src checks */
 
@@ -581,7 +581,6 @@ int unit_file_build_name_map(
         HASHMAP_FOREACH_KEY(dummy, src, ids) {
                 _cleanup_free_ char *inst = NULL, *dst_inst = NULL;
                 const char *dst;
-
                 r = unit_ids_map_get(ids, src, &dst);
                 if (r < 0)
                         continue;
@@ -762,7 +761,8 @@ int unit_file_find_fragment(
         }
 
         if (fragment && ret_names) {
-                const char *fragment_basename = basename(fragment);
+                _cleanup_free_ const char *fragment_basename = NULL;
+                path_extract_filename(fragment, (char **)&fragment_basename);
 
                 if (!streq(fragment_basename, unit_name)) {
                         /* Add names based on the fragment name to the set of names */
