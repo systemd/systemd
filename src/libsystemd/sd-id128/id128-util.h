249e330e8d681208718df649df5303d171a24454
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-04-03 10:39:00 +0900

    Revert "sd-id128: make id128_read() optionally take root directory"
    This reverts commit 830e52caa2bf1a29f56cb93e7ed85acb1bda11c3.

--- src/libsystemd/sd-id128/id128-util.h
+++ src/libsystemd/sd-id128/id128-util.h
@@ -19,7 +19,7 @@ typedef enum Id128FormatFlag {
 } Id128FormatFlag;
 
 int id128_read_fd(int fd, Id128FormatFlag f, sd_id128_t *ret);
-int id128_read(const char *root, const char *p, Id128FormatFlag f, sd_id128_t *ret);
+int id128_read(const char *p, Id128FormatFlag f, sd_id128_t *ret);
 
 int id128_write_fd(int fd, Id128FormatFlag f, sd_id128_t id);
 int id128_write(const char *p, Id128FormatFlag f, sd_id128_t id);
