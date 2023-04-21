6f01094cfc11b6f4a2995bfc2e0d1360f661ab7c
David Tardon <dtardon@redhat.com>
2023-04-21 15:50:21 +0200

    logind-session-device: modernize session_device_free()

--- src/login/logind-session-device.h
+++ src/login/logind-session-device.h
@@ -28,7 +28,7 @@ struct SessionDevice {
 };
 
 int session_device_new(Session *s, dev_t dev, bool open_device, SessionDevice **out);
-void session_device_free(SessionDevice *sd);
+SessionDevice *session_device_free(SessionDevice *sd);
 void session_device_complete_pause(SessionDevice *sd);
 
 void session_device_resume_all(Session *s);
