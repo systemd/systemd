0ef3f0d96e1b033cdb29abf8750d44f6f67fb280
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-04-29 11:51:57 +0900

    missing_fs: also define struct file_clone_range
    Follow-up for b640e274a7c363a2b6394c9dce5671d9404d2e2a.
    
    Addresses https://github.com/systemd/systemd/commit/b640e274a7c363a2b6394c9dce5671d9404d2e2a#r110996661.

--- src/basic/missing_fs.h
+++ src/basic/missing_fs.h
@@ -16,6 +16,12 @@
 
 #ifndef FICLONERANGE /* 04b38d601239b4d9be641b412cf4b7456a041c67 (4.5) */
 #define FICLONERANGE _IOW(0x94, 13, struct file_clone_range)
+struct file_clone_range {
+       __s64 src_fd;
+       __u64 src_offset;
+       __u64 src_length;
+       __u64 dest_offset;
+};
 #endif
 
 /* linux/fs.h or sys/mount.h */
