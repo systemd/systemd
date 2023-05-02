eb449c4fb614b0c5e261f629c3a2a6d992aed088
Nick Rosbrook <nick.rosbrook@canonical.com>
2023-05-02 12:30:31 -0400

    basic/audit-util: make a test request before enabling use of audit
    If a container manager does not follow the guidance in
    https://systemd.io/CONTAINER_INTERFACE/ regarding audit capabilities,
    then the current check may not be sufficient to determine that audit
    will function properly. In particular, when calling bind() on the audit
    fd, we will get EPERM if running in a user-namespaced container.
    
    Expand the check to make an AUDIT_GET_FEATURE request on the audit fd to
    test if it is working. If this fails with ECONNREFUSED, we know it is
    because the kernel does not support the use of audit outside of the
    initial user namespace.
    
    Note that the approach of this patch was suggested here:
    https://github.com/systemd/systemd/pull/19443#issuecomment-829566659
    
    Fixes: #6519

--- src/basic/audit-util.c
+++ src/basic/audit-util.c
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 
 #include <errno.h>
+#include <linux/audit.h>
 #include <linux/netlink.h>
 #include <stdio.h>
 #include <sys/socket.h>
@@ -12,6 +13,7 @@
 #include "macro.h"
 #include "parse-util.h"
 #include "process-util.h"
+#include "socket-util.h"
 #include "user-util.h"
 
 int audit_session_from_pid(pid_t pid, uint32_t *id) {
@@ -68,6 +70,75 @@ int audit_loginuid_from_pid(pid_t pid, uid_t *uid) {
         return 0;
 }
 
+static int try_audit_request(int fd) {
+        struct nlmsgerr *nlerr = NULL;
+        struct nlmsghdr nlh;
+        struct iovec iov;
+        struct msghdr mh;
+        struct sockaddr_nl nladdr;
+        ssize_t n;
+
+        nlh = (struct nlmsghdr) {
+                .nlmsg_len = NLMSG_LENGTH(0),
+                .nlmsg_type = AUDIT_GET_FEATURE,
+                .nlmsg_flags = NLM_F_REQUEST | NLM_F_ACK,
+        };
+        iov = (struct iovec) {
+                .iov_base = &nlh,
+                .iov_len = NLMSG_LENGTH(0),
+        };
+        mh = (struct msghdr) {
+                .msg_iov = &iov,
+                .msg_iovlen = 1,
+        };
+
+        if (sendmsg(fd, &mh, MSG_NOSIGNAL) < 0) {
+                log_debug_errno(errno, "Failed to send AUDIT_GET_FEATURE request, ignoring: %m");
+                return 0;
+        }
+
+        nlh = (struct nlmsghdr){};
+        iov = (struct iovec) {
+                .iov_base = &nlh,
+                .iov_len = NLMSG_LENGTH(sizeof(struct nlmsgerr)),
+        };
+        mh = (struct msghdr) {
+                .msg_name = &nladdr,
+                .msg_namelen = sizeof(nladdr),
+                .msg_iov = &iov,
+                .msg_iovlen = 1,
+        };
+
+        n = recvmsg_safe(fd, &mh, 0);
+        if (n < 0) {
+                log_debug_errno(errno, "Failed to recv AUDIT_GET_FEATURE request ack, ignoring: %m");
+                return 0;
+        }
+
+        if (!NLMSG_OK(&nlh, n)) {
+                log_debug("AUDIT_GET_FEATURE reply too large, ignoring.");
+                return 0;
+        }
+
+        if (nlh.nlmsg_type != NLMSG_ERROR) {
+                log_debug("Expected NLMSG_ERROR message but got %d, ignoring.", nlh.nlmsg_type);
+                return 0;
+        }
+
+        /* If we try and use the audit fd but get ECONNREFUSED, it is because
+         * we are not in the initial user namespace, and the kernel does not
+         * have support for audit outside of the initial user namespace. */
+        nlerr = NLMSG_DATA(&nlh);
+        if (nlerr->error == 0)
+                return 1;
+        else if (nlerr->error == -ECONNREFUSED)
+                return log_debug_errno(nlerr->error, "Won't talk to audit: %m");
+        else {
+                log_debug_errno(nlerr->error, "AUDIT_GET_FEATURE request failed, ignoring: %m");
+                return 0;
+        }
+}
+
 bool use_audit(void) {
         static int cached_use = -1;
 
@@ -80,7 +151,7 @@ bool use_audit(void) {
                         if (!cached_use)
                                 log_debug_errno(errno, "Won't talk to audit: %m");
                 } else {
-                        cached_use = true;
+                        cached_use = try_audit_request(fd) >= 0;
                         safe_close(fd);
                 }
         }
