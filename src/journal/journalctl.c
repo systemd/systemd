6ea4705e8cb2db430b7358aee3784682b9f3facd
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-05-04 10:09:37 +0900

    journalctl: fix --no-tail handling
    Fixes a bug introduced by 62f21ec91ad8e7e24079962f4df066b0094fe68d.

--- src/journal/journalctl.c
+++ src/journal/journalctl.c
@@ -1048,7 +1048,10 @@ static int parse_argv(int argc, char *argv[]) {
                         assert_not_reached();
                 }
 
-        if (arg_follow && !arg_no_tail && !arg_since_set && arg_lines == ARG_LINES_DEFAULT)
+        if (arg_no_tail)
+                arg_lines = ARG_LINES_ALL;
+
+        if (arg_follow && !arg_since_set && arg_lines == ARG_LINES_DEFAULT)
                 arg_lines = 10;
 
         if (arg_follow && !arg_merge && !arg_boot) {
