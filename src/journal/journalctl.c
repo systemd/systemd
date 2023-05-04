systemd/systemd

commit 62f21ec91ad8e7e24079962f4df066b0094fe68d

journalctl: properly honour -n when -f is passed, too

src/journal/journalctl.c
@@ -152,7 +152,7 @@ static int parse_argv(int argc, char *argv[]) {
                 }
         }
 
-        if (arg_follow && !arg_no_tail)
+        if (arg_follow && !arg_no_tail && arg_lines < 0)
                 arg_lines = 10;
 
         return 1;

TODO
@@ -21,6 +21,14 @@ Bugfixes:
 
 Features:
 
+* systemctl journal command
+
+* journalctl: --cursor support, priority filtering
+
+* systemctl status: show coredumps
+
+* systemctl status: show whether journal was rotated since service started
+
 * save coredump in Windows/Mozilla minidump format
 
 * support crash reporting operation modes (https://live.gnome.org/GnomeOS/Design/Whiteboards/ProblemReporting)

commit cfb828d95a7c7747664d3f78a5bfd85f3ae2d866

journalctl: fix --no-tail handling
Fixes a bug introduced by 62f21ec91ad8e7e24079962f4df066b0094fe68d.

src/journal/journalctl.c
@@ -1052,7 +1052,10 @@ static int parse_argv(int argc, char *argv[]) {
                         assert_not_reached();
                 }
 
-        if (arg_follow && !arg_no_tail && !arg_since_set && arg_lines == ARG_LINES_DEFAULT)
+        if (arg_no_tail)
+                arg_lines = ARG_LINES_ALL;
+
+        if (arg_follow && !arg_since_set && arg_lines == ARG_LINES_DEFAULT)
                 arg_lines = 10;
 
         if (!!arg_directory + !!arg_file + !!arg_machine + !!arg_root + !!arg_image > 1)