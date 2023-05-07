2e28fc77a32e7a39f1a522125a1a797c8953db14
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-05-07 18:38:58 +0900

    static-destruct: introduce STATIC_ARRAY_DESTRUCTOR_REGISTER()

--- src/basic/static-destruct.h
+++ src/basic/static-destruct.h
@@ -4,6 +4,7 @@
 
 #include "alloc-util.h"
 #include "macro.h"
+#include "memory-util.h"
 
 /* A framework for registering static variables that shall be freed on shutdown of a process. It's a bit like gcc's
  * destructor attribute, but allows us to precisely schedule when we want to free the variables. This is supposed to
@@ -45,10 +46,26 @@ typedef struct StaticDestructor {
                 .destroy = UNIQ_T(static_destructor_wrapper, uq),       \
         }
 
+#define STATIC_ARRAY_DESTRUCTOR_REGISTER(array, n, func)        \
+        _STATIC_ARRAY_DESTRUCTOR_REGISTER(UNIQ, array, n, func)
+
+#define _STATIC_ARRAY_DESTRUCTOR_REGISTER(uq, array, n, func)           \
+        _unused_ static void (* UNIQ_T(static_array_destructor_wrapper, uq))(typeof(array[0]) *a, size_t b) = (func); \
+        _common_static_destruct_attrs_                                  \
+        /* The actual destructor structure we place in a special section to find it. */ \
+        _section_("SYSTEMD_STATIC_ARRAY_DESTRUCT")                      \
+        static const ArrayCleanup UNIQ_T(_cleanup_array_, uq) = {       \
+                .parray = (void**) &(array),                            \
+                .pn = &(n),                                             \
+                .pfunc = (free_array_func_t) (func),                    \
+        };
+
 /* Beginning and end of our section listing the destructors. We define these as weak as we want this to work
  * even if no destructors are defined and the section is missing. */
 extern const StaticDestructor _weak_ __start_SYSTEMD_STATIC_DESTRUCT[];
 extern const StaticDestructor _weak_ __stop_SYSTEMD_STATIC_DESTRUCT[];
+extern const ArrayCleanup _weak_ __start_SYSTEMD_STATIC_ARRAY_DESTRUCT[];
+extern const ArrayCleanup _weak_ __stop_SYSTEMD_STATIC_ARRAY_DESTRUCT[];
 
 /* The function to destroy everything. (Note that this must be static inline, as it's key that it remains in
  * the same linking unit as the variables we want to destroy.) */
@@ -58,4 +75,10 @@ static inline void static_destruct(void) {
                      d < __stop_SYSTEMD_STATIC_DESTRUCT;
                      d = ALIGN_PTR(d + 1))
                         d->destroy(d->data);
+
+        if (__start_SYSTEMD_STATIC_ARRAY_DESTRUCT)
+                for (const ArrayCleanup *d = ALIGN_PTR(__start_SYSTEMD_STATIC_ARRAY_DESTRUCT);
+                     d < __stop_SYSTEMD_STATIC_ARRAY_DESTRUCT;
+                     d = ALIGN_PTR(d + 1))
+                        array_cleanup(d);
 }
