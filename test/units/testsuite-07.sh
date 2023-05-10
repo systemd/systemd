cd62ba42a40e5d87a83a98bc421419bd2778d4ec
Frantisek Sumsal <frantisek@sumsal.cz>
2023-05-10 13:07:26 +0200

    test: merge TEST-08-ISSUE-2730 into TEST-07-PID1

--- test/units/testsuite-07.sh
+++ test/units/testsuite-07.sh
@@ -5,6 +5,10 @@ set -o pipefail
 
 : >/failed
 
+# Issue: https://github.com/systemd/systemd/issues/2730
+# See TEST-07-PID1/test.sh for the first "half" of the test
+mountpoint /issue2730
+
 for script in "${0%.sh}".*.sh; do
     echo "Running $script"
     "./$script"
