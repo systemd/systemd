35354480d5d77221e40cd9a5262dcc532ddd5695
Frantisek Sumsal <frantisek@sumsal.cz>
2023-05-10 13:07:26 +0200

    test: merge TEST-08-ISSUE-2730 into TEST-07-PID1

--- test/TEST-07-PID1/test.sh
+++ test/TEST-07-PID1/test.sh
@@ -7,4 +7,23 @@ TEST_DESCRIPTION="Tests for core PID1 functionality"
 # shellcheck source=test/test-functions
 . "${TEST_BASE_DIR:?}/test-functions"
 
+test_append_files() {
+    local workspace="${1:?}"
+
+    # Issue: https://github.com/systemd/systemd/issues/2730
+    mkdir -p "$workspace/etc/systemd/system/"
+    cat >"$workspace/etc/systemd/system/issue2730.mount" <<EOF
+[Mount]
+What=tmpfs
+Where=/issue2730
+Type=tmpfs
+
+[Install]
+WantedBy=local-fs.target
+Alias=issue2730-alias.mount
+EOF
+    "${SYSTEMCTL:?}" enable --root="$workspace" issue2730.mount
+    ln -svrf "$workspace/etc/systemd/system/issue2730.mount" "$workspace/etc/systemd/system/issue2730-alias.mount"
+}
+
 do_test "$@"
