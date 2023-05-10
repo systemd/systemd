09c033a2763fe7f0e625901f199e646b3f132e84
Frantisek Sumsal <frantisek@sumsal.cz>
2023-05-10 12:38:57 +0200

    test: rename TEST-07-ISSUE-1981 to TEST-07-PID1

--- test/TEST-07-PID1/test.sh
+++ test/TEST-07-PID1/test.sh
@@ -2,12 +2,9 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
 set -e
 
-TEST_DESCRIPTION="https://github.com/systemd/systemd/issues/1981"
-TEST_NO_QEMU=1
+TEST_DESCRIPTION="Tests for core PID1 functionality"
 
 # shellcheck source=test/test-functions
 . "${TEST_BASE_DIR:?}/test-functions"
-
-NSPAWN_TIMEOUT=30
 
 do_test "$@"
--- test/units/testsuite-07.sh
+++ test/units/testsuite-07.sh
@@ -5,35 +5,10 @@ set -o pipefail
 
 : >/failed
 
-cat >/lib/systemd/system/my.service <<EOF
-[Service]
-Type=oneshot
-ExecStartPre=sh -c 'test "\$TRIGGER_UNIT" = my.timer'
-ExecStartPre=sh -c 'test -n "\$TRIGGER_TIMER_REALTIME_USEC"'
-ExecStartPre=sh -c 'test -n "\$TRIGGER_TIMER_MONOTONIC_USEC"'
-ExecStart=/bin/echo Timer runs me
-EOF
-
-cat >/lib/systemd/system/my.timer <<EOF
-[Timer]
-OnBootSec=10s
-OnUnitInactiveSec=1h
-EOF
-
-systemctl unmask my.timer
-
-systemctl start my.timer
-
-mkdir -p /etc/systemd/system/my.timer.d/
-cat >/etc/systemd/system/my.timer.d/override.conf <<EOF
-[Timer]
-OnBootSec=10s
-OnUnitInactiveSec=1h
-EOF
-
-systemctl daemon-reload
-
-systemctl mask my.timer
+for script in "${0%.sh}".*.sh; do
+    echo "Running $script"
+    "./$script"
+done
 
 touch /testok
 rm /failed
