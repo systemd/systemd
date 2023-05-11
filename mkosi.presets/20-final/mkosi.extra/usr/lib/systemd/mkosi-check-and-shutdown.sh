146f883823df62e8eed54337e959f9a771a73ff0
Luca Boccassi <bluca@debian.org>
2023-05-12 00:55:58 +0100

    stub: allow loading and verifying cmdline addons
    Files placed in /EFI/Linux/UKI.efi.extra.d/ and /loader/addons/ are
    opened and verified using the LoadImage protocol, and will thus get
    verified via shim/firmware.
    If they are valid signed PE files, the .cmdline section will be
    extracted and appended. If there are multiple addons in each directory,
    they will be parsed in alphanumerical order.
    
    The files must have a .addon.efi suffix.
    
    Files in the per-UKI directory are parsed, sorted, measured and
    appended first. Then, files in the generic directory are processed.

--- mkosi.presets/20-final/mkosi.extra/usr/lib/systemd/mkosi-check-and-shutdown.sh
+++ mkosi.presets/20-final/mkosi.extra/usr/lib/systemd/mkosi-check-and-shutdown.sh
@@ -7,6 +7,8 @@ systemctl --failed --no-legend | tee /failed-services
 if [[ -d /sys/firmware/efi/efivars/ ]]; then
     cmp /sys/firmware/efi/efivars/SecureBoot-8be4df61-93ca-11d2-aa0d-00e098032b8c <(printf '\6\0\0\0\1')
     cmp /sys/firmware/efi/efivars/SetupMode-8be4df61-93ca-11d2-aa0d-00e098032b8c <(printf '\6\0\0\0\0')
+    grep -q this_should_be_here /proc/cmdline && exit 1
+    grep -q this_should_not_be_here /proc/cmdline && exit 1
 fi
 
 # Exit with non-zero EC if the /failed-services file is not empty (we have -e set)
