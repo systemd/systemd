c5a427b765e09f02a0a427fb7c4931ac5e2213de
Frantisek Sumsal <frantisek@sumsal.cz>
2023-05-16 19:56:30 +0200

    test: clean up test artifacts
    So we don't run into unexpected fails when two tests use the same paths.

--- test/units/testsuite-23.clean-unit.sh
+++ test/units/testsuite-23.clean-unit.sh
@@ -7,6 +7,17 @@ set -o pipefail
 
 # Test unit configuration/state/cache/log/runtime data cleanup
 
+at_exit() {
+    set +e
+
+    rm -fr /{etc,run,var/lib,var/cache,var/log}/test-service
+    rm -fr /{etc,run,var/lib,var/cache,var/log}/private/test-service
+    rm -fr /{etc,run,var/lib,var/cache,var/log}/hoge
+    rm -fr /{etc,run,var/lib,var/cache,var/log}/test-socket
+}
+
+trap at_exit EXIT
+
 cat >/run/systemd/system/test-service.service <<EOF
 [Service]
 ConfigurationDirectory=test-service
@@ -113,6 +124,7 @@ test -L /var/log/test-service
 (! systemctl clean test-service)
 
 systemctl stop test-service
+
 
 test -d /etc/test-service
 test -d /run/private/test-service
@@ -260,6 +272,7 @@ LogsDirectory=test-socket
 EOF
 
 systemctl daemon-reload
+
 
 test ! -e /etc/test-socket
 test ! -e /run/test-socket
