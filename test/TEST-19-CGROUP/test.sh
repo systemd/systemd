3999ea001a70c8efc48115737e9c5b3c03d3640f
Frantisek Sumsal <frantisek@sumsal.cz>
2023-05-16 19:17:40 +0200

    test: rename TEST-19-DELEGATE to TEST-19-CGROUP
    And clean it up a bit.

--- test/TEST-19-CGROUP/test.sh
+++ test/TEST-19-CGROUP/test.sh
@@ -2,8 +2,7 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
 set -e
 
-TEST_DESCRIPTION="test cgroup delegation in the unified hierarchy"
-TEST_NO_NSPAWN=1
+TEST_DESCRIPTION="Various cgroup-related tests"
 
 # shellcheck source=test/test-functions
 . "${TEST_BASE_DIR:?}/test-functions"
