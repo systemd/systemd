17915ea5b3d11790a8bd1f09335aa10bdf3c2a96
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-05-16 18:48:29 +0900

    memfd-util: memfd may also have F_SEAL_EXEC flag
    Follow-up for c29715a8f77d96cd731b4a3083b3a852b3b61eb8.
    
    Fixes #27608.

--- src/basic/memfd-util.c
+++ src/basic/memfd-util.c
@@ -106,7 +106,8 @@ int memfd_get_sealed(int fd) {
         if (r < 0)
                 return -errno;
 
-        return r == (F_SEAL_SHRINK | F_SEAL_GROW | F_SEAL_WRITE | F_SEAL_SEAL);
+        /* We ignore F_SEAL_EXEC here to support older kernels. */
+        return FLAGS_SET(r, F_SEAL_SHRINK | F_SEAL_GROW | F_SEAL_WRITE | F_SEAL_SEAL);
 }
 
 int memfd_get_size(int fd, uint64_t *sz) {
