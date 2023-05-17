systemd/systemd

commit 206f0f397edf1144c63a158fb30f496c3e89f256

journal-file: journal-file: extend journal header to always carry offset of most recent entry
This way we can quickly find the most recent entry, without searching or
traversing entry array chains.

This is relevant later, as it it allows us to quickly determine the most
recent timestamps of each journal file, in a roughly atomic way.

src/libsystemd/sd-journal/journal-file.c
@@ -505,6 +505,11 @@ static int journal_file_verify_header(JournalFile *f) {
             !VALID64(le64toh(f->header->entry_array_offset)))
                 return -ENODATA;
 
+        if (JOURNAL_HEADER_CONTAINS(f->header, tail_entry_offset) &&
+            le64toh(f->header->tail_entry_offset) != 0 &&
+            !VALID64(le64toh(f->header->tail_entry_offset)))
+                return -ENODATA;
+
         if (journal_file_writable(f)) {
                 sd_id128_t machine_id;
                 uint8_t state;
@@ -2053,6 +2058,7 @@ static int journal_file_link_entry(
 
         f->header->tail_entry_realtime = o->entry.realtime;
         f->header->tail_entry_monotonic = o->entry.monotonic;
+        f->header->tail_entry_offset = offset;
 
         /* Link up the items */
         for (uint64_t i = 0; i < n_items; i++) {

src/libsystemd/sd-journal/journal-def.h
@@ -239,12 +239,14 @@ enum {
         /* Added in 252 */                              \
         le32_t tail_entry_array_offset;                 \
         le32_t tail_entry_array_n_entries;              \
+        /* Added in 254 */                              \
+        le64_t tail_entry_offset;                       \
         }
 
 struct Header struct_Header__contents;
 struct Header__packed struct_Header__contents _packed_;
 assert_cc(sizeof(struct Header) == sizeof(struct Header__packed));
-assert_cc(sizeof(struct Header) == 264);
+assert_cc(sizeof(struct Header) == 272);
 
 #define FSS_HEADER_SIGNATURE                                            \
         ((const char[]) { 'K', 'S', 'H', 'H', 'R', 'H', 'L', 'P' })

commit c7b38d3fc9c7f642425611029aaeff4f938777ca

sd-journal: fix assignment of tail entry offset
Fixes a bug introduced by 206f0f397edf1144c63a158fb30f496c3e89f256.

src/libsystemd/sd-journal/journal-file.c
@@ -2122,7 +2122,8 @@ static int journal_file_link_entry(
 
         f->header->tail_entry_realtime = o->entry.realtime;
         f->header->tail_entry_monotonic = o->entry.monotonic;
-        f->header->tail_entry_offset = offset;
+        if (JOURNAL_HEADER_CONTAINS(f->header, tail_entry_offset))
+                f->header->tail_entry_offset = htole64(offset);
         f->newest_mtime = 0; /* we have a new tail entry now, explicitly invalidate newest boot id/timestamp info */
 
         /* Link up the items */