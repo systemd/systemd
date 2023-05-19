563e684689db9c810e22646c879afb5157c396c5
Lennart Poettering <lennart@poettering.net>
2023-05-19 14:47:37 +0200

    stat-util: rename files_same() → inode_same()
    Let's be more accurate about what this function does: it checks whether
    the underlying reported inode is the same. Internally, this already uses
    a better named stat_inode_same() call, hence let's similarly name the
    wrapping function following the same logic.
    
    Similar for files_same_at() and path_equal_or_same_files().
    
    No code changes, just some renaming.

--- src/basic/stat-util.h
+++ src/basic/stat-util.h
@@ -37,10 +37,10 @@ static inline int null_or_empty_path(const char *fn) {
 
 int path_is_read_only_fs(const char *path);
 
-int files_same_at(int fda, const char *filea, int fdb, const char *fileb, int flags);
+int inode_same_at(int fda, const char *filea, int fdb, const char *fileb, int flags);
 
-static inline int files_same(const char *filea, const char *fileb, int flags) {
-        return files_same_at(AT_FDCWD, filea, AT_FDCWD, fileb, flags);
+static inline int inode_same(const char *filea, const char *fileb, int flags) {
+        return inode_same_at(AT_FDCWD, filea, AT_FDCWD, fileb, flags);
 }
 
 /* The .f_type field of struct statfs is really weird defined on
