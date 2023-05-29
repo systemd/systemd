9e7d91ed97a008a47e347d99b02de376d55d3449
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-05-29 12:56:30 +0900

    network: do not request dynamic addressing protocols finished when at least one static address is configured
    The setting IPv6AcceptRA= is defaults to yes, hence, even if a .network
    file for an interface has static IP address configuration, it may takes
    few seconds for the interface being configured state, as NDisc for the
    interface needs to be finished. That makes wait-online.service
    needlessly slow. Typically, such delay is not necessary for statically
    configured networks.
    
    Let's make the required condition slightly relaxed; if a .network file
    has static IP address configurations, then let's make the matching
    interface enter the 'configured' state soon after the static addresses
    configured on the interface.
    
    Note, this does not change the default for IPv6AcceptRA=, hence, NDisc
    still runs on interfaces by default. So, addresses, routes, DNS servers,
    and so on based on RA will be assigned on interfaces later.
    
    Strictly speaking, this breaks backward compatibility, but the previous
    behavior is not clearly documented. If a user requested both static
    IPv4 address and IPv6 SLAAC address configured before an interface being
    entered to the 'configured' state, then '--ipv6' for wait-online can be
    used. So, the behavior change should not cause severe regression.
    
    Closes #27779.

--- src/network/networkd-link.c
+++ src/network/networkd-link.c
@@ -398,11 +398,6 @@ void link_check_ready(Link *link) {
         if (!link->static_addresses_configured)
                 return (void) log_link_debug(link, "%s(): static addresses are not configured.", __func__);
 
-        SET_FOREACH(a, link->addresses)
-                if (!address_is_ready(a))
-                        return (void) log_link_debug(link, "%s(): address %s is not ready.", __func__,
-                                                     IN_ADDR_PREFIX_TO_STRING(a->family, &a->in_addr, a->prefixlen));
-
         if (!link->static_address_labels_configured)
                 return (void) log_link_debug(link, "%s(): static address labels are not configured.", __func__);
 
@@ -435,6 +430,21 @@ void link_check_ready(Link *link) {
             link_ipv6ll_enabled(link) &&
             !in6_addr_is_set(&link->ipv6ll_address))
                 return (void) log_link_debug(link, "%s(): IPv6LL is not configured yet.", __func__);
+
+        /* All static addresses must be ready. */
+        bool has_static_address = false;
+        SET_FOREACH(a, link->addresses) {
+                if (a->source != NETWORK_CONFIG_SOURCE_STATIC)
+                        continue;
+                if (!address_is_ready(a))
+                        return (void) log_link_debug(link, "%s(): static address %s is not ready.", __func__,
+                                                     IN_ADDR_PREFIX_TO_STRING(a->family, &a->in_addr, a->prefixlen));
+                has_static_address = true;
+        }
+
+        /* If at least one static address is requested, do not request that dynamic addressing protocols are finished. */
+        if (has_static_address)
+                goto ready;
 
         /* If no dynamic addressing protocol enabled, assume the interface is ready.
          * Note, ignore NDisc when ConfigureWithoutCarrier= is enabled, as IPv6AcceptRA= is enabled by default. */
