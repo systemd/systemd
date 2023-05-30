4041a100d5e256a676387fa7e9ba6ec0fc7fdf32
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2023-05-30 13:02:57 +0200

    man: generate link mode list dynamically
    The entries are sorted by speed. Some fields are left empty when there is no
    clear value to use. The table is much longer now, but I think it's better to
    document the allowed values, even if some are not terribly useful.
    
    Fixes #26256.

--- src/shared/ethtool-link-mode.py
+++ src/shared/ethtool-link-mode.py
@@ -7,27 +7,49 @@ import sys
 OVERRIDES = {
     'autoneg' : 'autonegotiation',
 }
-count = 0
 
-f = open(sys.argv[1])
+xml = sys.argv[1] == '--xml'
+
+f = open(sys.argv[-1])
 for line in f:
     if line.startswith('enum ethtool_link_mode_bit_indices {'):
         break
+
+entries = []
 for line in f:
     if line.startswith('}'):
         break
     # ETHTOOL_LINK_MODE_10baseT_Half_BIT	= 0,
-    m = re.match(r'^\s*(ETHTOOL_LINK_MODE_(.*)_BIT)\s*=\s*(\d+),', line)
+    m = re.match(r'^\s*(ETHTOOL_LINK_MODE_((\d*).*)_BIT)\s*=\s*(\d+),', line)
     if not m:
         continue
-    enum, name, value = m.groups()
+    enum, name, speed, value = m.groups()
 
     name = name.lower().replace('_', '-')
     name = OVERRIDES.get(name, name)
 
-    enum = f'[{enum}]'
+    duplex = name.split('-')[-1].lower()
+    if duplex not in {'half', 'full'}:
+        duplex = ''
 
-    print(f'        {enum:50} = "{name}",')
-    count += 1
+    entries += [(enum, name, speed, value, duplex)]
 
-assert count >= 99
+if xml:
+    print('              <tbody>')
+
+    entries.sort(key=lambda entry: (int(entry[2]) if entry[2] else 1e20, entry[4], entry[1], entry[3]))
+
+for enum, name, speed, value, duplex in entries:
+    if xml:
+        print(f'''\
+                <row><entry><option>{name}</option></entry>
+                <entry>{speed}</entry><entry>{duplex}</entry></row>
+        ''')
+    else:
+        enum = f'[{enum}]'
+        print(f'        {enum:50} = "{name}",')
+
+if xml:
+    print('              </tbody>')
+
+assert len(entries) >= 99
