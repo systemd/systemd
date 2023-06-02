141df0352b6bf82ceaaf27754e2a08d4043a2787
Lennart Poettering <lennart@poettering.net>
2023-06-02 10:25:19 +0200

    umount: split out MD detaching code from umount.c
    umount.c does so much stuff, and MD detaching is relatively separate,
    hence split it out into its own .c/.h file pair.

--- src/shutdown/umount.c
+++ src/shutdown/umount.c
@@ -6,8 +6,6 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <linux/dm-ioctl.h>
-#include <linux/major.h>
-#include <linux/raid/md_u.h>
 #include <linux/loop.h>
 #include <sys/mount.h>
 #include <sys/swap.h>
@@ -50,7 +48,7 @@
 #include "umount.h"
 #include "virt.h"
 
-static void mount_point_free(MountPoint **head, MountPoint *m) {
+void mount_point_free(MountPoint **head, MountPoint *m) {
         assert(head);
         assert(m);
 
@@ -343,74 +341,6 @@ static int dm_list_get(MountPoint **head) {
         return 0;
 }
 
-static int md_list_get(MountPoint **head) {
-        _cleanup_(sd_device_enumerator_unrefp) sd_device_enumerator *e = NULL;
-        sd_device *d;
-        int r;
-
-        assert(head);
-
-        r = sd_device_enumerator_new(&e);
-        if (r < 0)
-                return r;
-
-        r = sd_device_enumerator_allow_uninitialized(e);
-        if (r < 0)
-                return r;
-
-        r = sd_device_enumerator_add_match_subsystem(e, "block", true);
-        if (r < 0)
-                return r;
-
-        r = sd_device_enumerator_add_match_sysname(e, "md*");
-        if (r < 0)
-                return r;
-
-        /* Filter out partitions. */
-        r = sd_device_enumerator_add_match_property(e, "DEVTYPE", "disk");
-        if (r < 0)
-                return r;
-
-        FOREACH_DEVICE(e, d) {
-                _cleanup_free_ char *p = NULL;
-                const char *dn, *md_level;
-                MountPoint *m;
-                dev_t devnum;
-
-                if (sd_device_get_devnum(d, &devnum) < 0 ||
-                    sd_device_get_devname(d, &dn) < 0)
-                        continue;
-
-                r = sd_device_get_property_value(d, "MD_LEVEL", &md_level);
-                if (r < 0) {
-                        log_warning_errno(r, "Failed to get MD_LEVEL property for %s, ignoring: %m", dn);
-                        continue;
-                }
-
-                /* MD "containers" are a special type of MD devices, used for external metadata.  Since it
-                 * doesn't provide RAID functionality in itself we don't need to stop it. */
-                if (streq(md_level, "container"))
-                        continue;
-
-                p = strdup(dn);
-                if (!p)
-                        return -ENOMEM;
-
-                m = new(MountPoint, 1);
-                if (!m)
-                        return -ENOMEM;
-
-                *m = (MountPoint) {
-                        .path = TAKE_PTR(p),
-                        .devnum = devnum,
-                };
-
-                LIST_PREPEND(mount_point, *head, m);
-        }
-
-        return 0;
-}
-
 static int delete_loopback(const char *device) {
         _cleanup_close_ int fd = -EBADF;
         struct loop_info64 info;
@@ -511,23 +441,6 @@ static int delete_dm(MountPoint *m) {
                 .data_size = sizeof(struct dm_ioctl),
                 .dev = m->devnum,
         }));
-}
-
-static int delete_md(MountPoint *m) {
-        _cleanup_close_ int fd = -EBADF;
-
-        assert(m);
-        assert(major(m->devnum) != 0);
-        assert(m->path);
-
-        fd = open(m->path, O_RDONLY|O_CLOEXEC|O_EXCL);
-        if (fd < 0)
-                return -errno;
-
-        if (fsync(fd) < 0)
-                log_debug_errno(errno, "Failed to sync MD block device %s, ignoring: %m", m->path);
-
-        return RET_NERRNO(ioctl(fd, STOP_ARRAY, NULL));
 }
 
 static bool nonunmountable_path(const char *path) {
@@ -906,36 +819,6 @@ static int dm_points_list_detach(MountPoint **head, bool *changed, bool last_try
         return n_failed;
 }
 
-static int md_points_list_detach(MountPoint **head, bool *changed, bool last_try) {
-        int n_failed = 0, r;
-        dev_t rootdev = 0;
-
-        assert(head);
-        assert(changed);
-
-        (void) get_block_device("/", &rootdev);
-
-        LIST_FOREACH(mount_point, m, *head) {
-                if (major(rootdev) != 0 && rootdev == m->devnum) {
-                        n_failed ++;
-                        continue;
-                }
-
-                log_info("Stopping MD %s (" DEVNUM_FORMAT_STR ").", m->path, DEVNUM_FORMAT_VAL(m->devnum));
-                r = delete_md(m);
-                if (r < 0) {
-                        log_full_errno(last_try ? LOG_ERR : LOG_INFO, r, "Could not stop MD %s: %m", m->path);
-                        n_failed++;
-                        continue;
-                }
-
-                *changed = true;
-                mount_point_free(head, m);
-        }
-
-        return n_failed;
-}
-
 static int umount_all_once(bool *changed, bool last_try) {
         _cleanup_(mount_points_list_free) LIST_HEAD(MountPoint, mp_list_head);
         int r;
@@ -1012,19 +895,4 @@ int dm_detach_all(bool *changed, bool last_try) {
                 return r;
 
         return dm_points_list_detach(&dm_list_head, changed, last_try);
-}
-
-int md_detach_all(bool *changed, bool last_try) {
-        _cleanup_(mount_points_list_free) LIST_HEAD(MountPoint, md_list_head);
-        int r;
-
-        assert(changed);
-
-        LIST_HEAD_INIT(md_list_head);
-
-        r = md_list_get(&md_list_head);
-        if (r < 0)
-                return r;
-
-        return md_points_list_detach(&md_list_head, changed, last_try);
 }
