c95b4d6e0dfa69e4db72226e107d2713e97963d4
David Tardon <dtardon@redhat.com>
2023-06-02 15:15:41 +0200

    test: use wait_while

--- test/units/testsuite-74.mount.sh
+++ test/units/testsuite-74.mount.sh
@@ -108,13 +108,13 @@ test -e "$WORK_DIR/mnt/foo.bar"
 # Wait until it's idle again
 sleep 1.5
 # Safety net for slower/overloaded systems
-timeout 10s bash -c "while systemctl is-active -q $WORK_DIR/mnt; do sleep .2; done"
+wait_while -t 10 systemctl is-active -q $WORK_DIR/mnt
 systemctl status "$(systemd-escape --path "$WORK_DIR/mnt").automount"
 # Disassemble the underlying block device
 losetup -d "$LOOP"
 unset LOOP
 # The automount unit should disappear once the underlying blockdev is gone
-timeout 10s bash -c "while systemctl status '$(systemd-escape --path "$WORK_DIR/mnt".automount)'; do sleep .2; done"
+wait_while -t 10 systemctl status "$(systemd-escape --path "$WORK_DIR/mnt".automount)"
 
 # Mount a disk image
 systemd-mount --discover "$WORK_DIR/simple.img"
