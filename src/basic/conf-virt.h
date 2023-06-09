849b7849ecb41b71976f937ecc2ee2fb9ba9ad83
Daniel P. Berrangé <berrange@redhat.com>
2023-06-09 15:37:18 +0100

    add APIs for detecting confidential virtualization
    This code uses various CPUID checks to be able to identify
    
     * AMD SEV
     * AMD SEV-ES
     * AMD SEV-SNP
     * Intel TDX
    
    On HyperV/Azure, it has special checks for detecting SEV-SNP
    since the normal CPUID is blocked.
    
    Related: https://github.com/systemd/systemd/issues/27604
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

---
+++ src/basic/conf-virt.h
@@ -0,0 +1,25 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#pragma once
+
+#include <stdbool.h>
+
+#include "errno-list.h"
+#include "macro.h"
+
+typedef enum ConfidentialVirtualization {
+        CONFIDENTIAL_VIRTUALIZATION_NONE = 0,
+
+        CONFIDENTIAL_VIRTUALIZATION_SEV,
+        CONFIDENTIAL_VIRTUALIZATION_SEV_ES,
+        CONFIDENTIAL_VIRTUALIZATION_SEV_SNP,
+        CONFIDENTIAL_VIRTUALIZATION_TDX,
+
+        _CONFIDENTIAL_VIRTUALIZATION_MAX,
+        _CONFIDENTIAL_VIRTUALIZATION_INVALID = -EINVAL,
+        _CONFIDENTIAL_VIRTUALIZATION_ERRNO_MAX = -ERRNO_MAX, /* ensure full range of errno fits into this enum */
+} ConfidentialVirtualization;
+
+ConfidentialVirtualization detect_confidential_virtualization(void);
+
+const char *confidential_virtualization_to_string(ConfidentialVirtualization v) _const_;
+ConfidentialVirtualization confidential_virtualization_from_string(const char *s) _pure_;
