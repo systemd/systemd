5cc29666313a6ddd45a340457181019d8a30a0bc
David Tardon <dtardon@redhat.com>
2023-06-13 15:31:20 +0200

    udevadm-control: fix error message
    Extracted from #25523 by Lennart.

--- src/udev/udevadm-control.c
+++ src/udev/udevadm-control.c
@@ -142,7 +142,7 @@ int control_main(int argc, char *argv[], void *userdata) {
 
                         r = safe_atou(optarg, &i);
                         if (r < 0)
-                                return log_error_errno(r, "Failed to parse maximum number of events '%s': %m", optarg);
+                                return log_error_errno(r, "Failed to parse maximum number of children '%s': %m", optarg);
 
                         r = udev_ctrl_send_set_children_max(uctrl, i);
                         if (r == -ENOANO)
