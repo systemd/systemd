11346cc93f5617b5857b81b04e85378b36953c48
Luca Boccassi <bluca@debian.org>
2023-06-17 00:07:32 +0100

    TEST-55-OOMD: use 'stress' to create high pressure
    The test fails on my machine, running Debian stable, because
    testsuite-55-testbloat.service just swaps and never goes over the
    limit, so it's not killed. Use 'stress' instead which seems to be
    able to overwhelm the swap too.

--- test/units/testsuite-55.sh
+++ test/units/testsuite-55.sh
@@ -89,12 +89,15 @@ while [[ $(date -u +%s) -le $timeout ]]; do
     if ! systemctl status testsuite-55-testbloat.service; then
         break
     fi
+    oomctl
     sleep 2
 done
 
 # testbloat should be killed and testchill should be fine
 if systemctl status testsuite-55-testbloat.service; then exit 42; fi
 if ! systemctl status testsuite-55-testchill.service; then exit 24; fi
+
+sleep 120 # wait for systemd-oomd kill cool down and elevated memory pressure to come down
 
 # Make sure we also work correctly on user units.
 
@@ -126,6 +129,7 @@ while [[ $(date -u +%s) -le $timeout ]]; do
     if ! systemctl --machine "testuser@.host" --user status testsuite-55-testbloat.service; then
         break
     fi
+    oomctl
     sleep 2
 done
 
@@ -153,6 +157,7 @@ EOF
         if ! systemctl status testsuite-55-testmunch.service; then
             break
         fi
+        oomctl
         sleep 2
     done
 
