3474c173f646179357935e1a779f94c91fa47948
Raito Bezarius <masterancpp@gmail.com>
2023-06-16 14:03:35 +0200

    systemd-boot: load addons from type 1 and expose them via EFI protocol
    This make it possible to attach addons to any Type #1 entry
    and get them back in the next bootable stage via an EFI protocol.

---
+++ src/boot/efi/addon-util.c
@@ -0,0 +1,17 @@
+#include "addon-util.h"
+#include "proto/device-path.h"
+#include "util.h"
+#include "log.h"
+
+EFI_STATUS addons_install(EFI_LOADED_IMAGE_PROTOCOL *stub_image, const char16_t **addons) {
+        EFI_STATUS err;
+        EFI_DEVICE_PATH **dps;
+
+        err = make_multiple_file_device_path(stub_image->DeviceHandle, addons, &dps);
+        if (err != EFI_SUCCESS)
+                return err;
+
+        return BS->InstallMultipleProtocolInterfaces(&stub_image->DeviceHandle,
+                                            MAKE_GUID_PTR(SYSTEMD_ADDON_MEDIA),
+                                            dps, NULL);
+}
