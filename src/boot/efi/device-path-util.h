0570ec1b7418c7ba15a17d6029e31319e160c8ff
Raito Bezarius <masterancpp@gmail.com>
2023-06-17 19:42:43 +0200

    libefi(device-path): introduce `make_multiple_file_device_path`
    This build an `EfiDevicePath**` of the form:
    
    `FilePath(\files[0])\EndNode()FilePath(\files[1])\EndNode()…\FilePath(\files[n])\EndNode()`
    
    which should really be read as a list:
    
    [ FilePath(\files[0])\EndNode() ; FilePath(\files(1))\EndNode() ; … ;
    FilePath(\files[n])\EndNode() ]

--- src/boot/efi/device-path-util.h
+++ src/boot/efi/device-path-util.h
@@ -3,6 +3,8 @@
 
 #include "proto/device-path.h"
 
+EFI_STATUS make_multiple_file_device_path(EFI_HANDLE device, const char16_t **files, EFI_DEVICE_PATH
+                ***ret_dp);
 EFI_STATUS make_file_device_path(EFI_HANDLE device, const char16_t *file, EFI_DEVICE_PATH **ret_dp);
 EFI_STATUS device_path_to_str(const EFI_DEVICE_PATH *dp, char16_t **ret);
 bool device_path_startswith(const EFI_DEVICE_PATH *dp, const EFI_DEVICE_PATH *start);
@@ -19,9 +21,21 @@ static inline bool device_path_is_end(const EFI_DEVICE_PATH *dp) {
         return dp->Type == END_DEVICE_PATH_TYPE && dp->SubType == END_ENTIRE_DEVICE_PATH_SUBTYPE;
 }
 
+static inline bool device_path_is_end_instance(const EFI_DEVICE_PATH *dp) {
+        assert(dp);
+        return dp->Type == END_DEVICE_PATH_TYPE && dp->SubType == END_INSTANCE_DEVICE_PATH_SUBTYPE;
+}
+
 #define DEVICE_PATH_END_NODE                               \
         (EFI_DEVICE_PATH) {                                \
                 .Type = END_DEVICE_PATH_TYPE,              \
                 .SubType = END_ENTIRE_DEVICE_PATH_SUBTYPE, \
                 .Length = sizeof(EFI_DEVICE_PATH)          \
+        }
+
+#define DEVICE_PATH_END_INSTANCE                             \
+        (EFI_DEVICE_PATH) {                                  \
+                .Type = END_DEVICE_PATH_TYPE,                \
+                .SubType = END_INSTANCE_DEVICE_PATH_SUBTYPE, \
+                .Length = sizeof(EFI_DEVICE_PATH)            \
         }
