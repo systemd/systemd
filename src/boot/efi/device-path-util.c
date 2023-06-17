efe208ef46e6df5fe64d0ab2a1bcce94b46733cb
Raito Bezarius <masterancpp@gmail.com>
2023-06-17 19:42:43 +0200

    libefi(device-path): introduce `make_multiple_file_device_path`
    … and `split_multiple_file_into_single`.
    
    This build an EfiDevicePath of the form:
    
    `FilePath(\files[0])\EndInstance()FilePath(\files[1])\EndInstance()…\FilePath(\files[n])\EndDevicePath`
    
    For this, it seems easier to build single version from multiple
    versions.
    
    And then, we offer the reverse primitive that gives you an array of
    single instance device paths.

--- src/boot/efi/device-path-util.c
+++ src/boot/efi/device-path-util.c
@@ -3,39 +3,71 @@
 #include "device-path-util.h"
 #include "util.h"
 
-EFI_STATUS make_file_device_path(EFI_HANDLE device, const char16_t *file, EFI_DEVICE_PATH **ret_dp) {
+EFI_STATUS make_multiple_file_device_path(EFI_HANDLE device, const char16_t **files, EFI_DEVICE_PATH
+                                          **ret_dp)
+{
         EFI_STATUS err;
         EFI_DEVICE_PATH *dp;
+        EFI_DEVICE_PATH *original_device_path;
+        size_t total_file_sizes = 0, n_files = 0;
 
-        assert(file);
+        assert(files);
         assert(ret_dp);
 
-        err = BS->HandleProtocol(device, MAKE_GUID_PTR(EFI_DEVICE_PATH_PROTOCOL), (void **) &dp);
+        err = BS->HandleProtocol(device, MAKE_GUID_PTR(EFI_DEVICE_PATH_PROTOCOL), (void **)
+                                 &original_device_path);
         if (err != EFI_SUCCESS)
                 return err;
 
-        EFI_DEVICE_PATH *end_node = dp;
+        EFI_DEVICE_PATH *end_node = original_device_path;
         while (!device_path_is_end(end_node))
                 end_node = device_path_next_node(end_node);
 
-        size_t file_size = strsize16(file);
-        size_t dp_size = (uint8_t *) end_node - (uint8_t *) dp;
 
-        /* Make a copy that can also hold a file media device path. */
-        *ret_dp = xmalloc(dp_size + file_size + sizeof(FILEPATH_DEVICE_PATH) + sizeof(EFI_DEVICE_PATH));
-        dp = mempcpy(*ret_dp, dp, dp_size);
+        size_t o_dp_size = (uint8_t *) end_node - (uint8_t *) original_device_path;
+        STRV_FOREACH(file, files) {
+                size_t file_size = strsize16(*file);
 
-        FILEPATH_DEVICE_PATH *file_dp = (FILEPATH_DEVICE_PATH *) dp;
-        file_dp->Header = (EFI_DEVICE_PATH) {
-                .Type = MEDIA_DEVICE_PATH,
-                .SubType = MEDIA_FILEPATH_DP,
-                .Length = sizeof(FILEPATH_DEVICE_PATH) + file_size,
-        };
-        memcpy(file_dp->PathName, file, file_size);
+                total_file_sizes += file_size;
+                n_files += 1;
+        }
 
-        dp = device_path_next_node(dp);
-        *dp = DEVICE_PATH_END_NODE;
+        /* We need to allocate the original device path for each file
+         * + the file path name size for each file
+         * + the actual filepath device path structure for each file
+         * + the efi device path structure for each file (the end marker) */
+        *ret_dp = xmalloc(total_file_sizes + (o_dp_size + sizeof(FILEPATH_DEVICE_PATH) +
+                                                        sizeof(EFI_DEVICE_PATH)) * n_files);
+
+        dp = *ret_dp;
+        STRV_FOREACH(file, files) {
+                /* Prepend the original device path */
+                dp = mempcpy(dp, original_device_path, o_dp_size);
+
+                size_t file_size = strsize16(*file);
+                const char16_t **peek = file;
+                peek++;
+                bool is_last_file = (*peek == NULL || isempty(*peek));
+
+                FILEPATH_DEVICE_PATH *file_dp = (FILEPATH_DEVICE_PATH *) dp;
+                file_dp->Header = (EFI_DEVICE_PATH) {
+                        .Type = MEDIA_DEVICE_PATH,
+                        .SubType = MEDIA_FILEPATH_DP,
+                        .Length = sizeof(FILEPATH_DEVICE_PATH) + file_size,
+                };
+                memcpy(file_dp->PathName, *file, file_size);
+
+                dp = device_path_next_node(dp);
+                *dp = is_last_file ? DEVICE_PATH_END_NODE : DEVICE_PATH_END_INSTANCE;
+                dp = device_path_next_node(dp);
+        }
+
         return EFI_SUCCESS;
+}
+
+EFI_STATUS make_file_device_path(EFI_HANDLE device, const char16_t *file, EFI_DEVICE_PATH **ret_dp) {
+        const char16_t *singleton[2] = {file, NULL};
+        return make_multiple_file_device_path(device, singleton, ret_dp);
 }
 
 EFI_STATUS device_path_to_str(const EFI_DEVICE_PATH *dp, char16_t **ret) {
@@ -54,7 +86,7 @@ EFI_STATUS device_path_to_str(const EFI_DEVICE_PATH *dp, char16_t **ret) {
                 size_t size = 0;
                 for (const EFI_DEVICE_PATH *node = dp; !device_path_is_end(node);
                      node = device_path_next_node(node)) {
-
+                        log_internal(EFI_SUCCESS, "%hu %hu %hu", node->Type, node->SubType, node->Length);
                         if (node->Type != MEDIA_DEVICE_PATH || node->SubType != MEDIA_FILEPATH_DP)
                                 return err;
 
@@ -125,4 +157,96 @@ EFI_DEVICE_PATH *device_path_replace_node(
 
         *end = DEVICE_PATH_END_NODE;
         return ret;
+}
+
+const EFI_DEVICE_PATH **split_multiple_file_device_path(const EFI_DEVICE_PATH *dp) {
+        /* Split a multi-instance device path
+        * into an array of single-instance device path
+        * */
+
+        /* To achieve this, we need to:
+         * (1) determine how many instances is there in this multi-instance device path
+         * (2) we will copy the entire multi instance device
+         * (3) arrange an array of size n_instances of type EFI_DEVICE_PATH*
+         *      containing for each index the start pointer of an instance
+         * (4) each time, we will encounter an END INSTANCE, we will replace it
+         *      by an END NODE
+         * (5) return an array of EFI_DEVICE_PATH*, i.e. EFI_DEVICE_PATH**
+         * */
+        const EFI_DEVICE_PATH *end = dp;
+        const EFI_DEVICE_PATH **instances = NULL;
+        EFI_DEVICE_PATH *next = NULL;
+        EFI_DEVICE_PATH *ret_dp = NULL;
+        size_t n_dps = 0;
+        size_t n_instances = 0;
+        size_t total_dp_size = 0;
+
+        assert(dp);
+
+        /* We determine (1) */
+        while (!device_path_is_end(end)) {
+                n_instances++;
+                while (!device_path_is_end_instance(end)) {
+                        n_dps++;
+                        end = device_path_next_node(end);
+                        if (device_path_is_end(end))
+                                break;
+                }
+                if (device_path_is_end(end))
+                        break;
+                end = device_path_next_node(end);
+        }
+        total_dp_size = (uint8_t *)end - (uint8_t *)dp;
+
+        /* We count the final device path this way */
+        n_dps += 1;
+        total_dp_size += sizeof(EFI_DEVICE_PATH);
+
+        log_internal(EFI_SUCCESS, "total dp size: %lu, n_dps: %lu, n_instances: %lu", total_dp_size, n_dps,
+                     n_instances);
+
+        /* We perform the copy mentioned in (2) */
+        ret_dp = xmalloc(total_dp_size);
+        mempcpy(ret_dp, dp, total_dp_size);
+
+        log_internal(EFI_SUCCESS, "copied dp (%p) into ret_dp (%p)", dp, ret_dp);
+
+        /* We arrange the array mentioned in (3) */
+        log_internal(EFI_SUCCESS, "allocating %lu instances", n_instances);
+        instances = xmalloc(sizeof(EFI_DEVICE_PATH*) * (n_instances + 1));
+        end = ret_dp;
+        for (size_t instance_index = 0 ;; instance_index++) {
+                assert(instance_index < n_instances);
+
+                instances[instance_index] = end;
+                log_internal(EFI_SUCCESS, "wrote %lu-instance, now at %p", instance_index, end);
+
+                while (!device_path_is_end_instance(end)) {
+                        log_internal(EFI_SUCCESS, "chasing %p -> %p, type: %hu, subtype: %hu", end, device_path_next_node(end), end->Type, end->SubType);
+                        end = device_path_next_node(end);
+                        if (device_path_is_end(end))
+                                break;
+                }
+
+                if (device_path_is_end(end))
+                        break;
+                else
+                        end = device_path_next_node(end);
+        }
+
+        instances[n_instances] = NULL;
+
+        log_internal(EFI_SUCCESS, "allocated %lu instances", n_instances);
+
+        /* We replace all END_INSTANCE by END_NODE as mentioned in (4) */
+        next = ret_dp;
+        while (!device_path_is_end(next)) {
+                if (device_path_is_end_instance(next)) {
+                        *next = DEVICE_PATH_END_NODE;
+                        log_internal(EFI_SUCCESS, "wrote END NODE instead of END INSTANCE");
+                }
+                next = device_path_next_node(next);
+        }
+
+        return instances;
 }
