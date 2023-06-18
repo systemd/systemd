bc3563ada3c847830d46efb9fcc543646f16491b
Raito Bezarius <masterancpp@gmail.com>
2023-06-19 00:30:44 +0200

    debug: cleanup logs - SQUASHME AFTER REVIEW

--- src/boot/efi/addon-util.c
+++ src/boot/efi/addon-util.c
@@ -7,9 +7,6 @@ EFI_STATUS addons_install(EFI_LOADED_IMAGE_PROTOCOL *stub_image, const char16_t 
         EFI_STATUS err;
         EFI_DEVICE_PATH **dps;
 
-        STRV_FOREACH(addon, addons)
-                log_internal(EFI_SUCCESS, "installing addon %ls", *addon);
-
         err = make_multiple_file_device_path(stub_image->DeviceHandle, addons, &dps);
         if (err != EFI_SUCCESS)
                 return err;
