e97e03814dbb785d11219d524c78dedf52ea16ab
OMOJOLA JOSHUA <omojolajoshua@gmail.com>
2023-06-19 15:16:23 +0100

    PID1: Display emergency log message full-sceen on boot failure.

--- src/shared/qrcode-util.h
+++ src/shared/qrcode-util.h
@@ -7,9 +7,14 @@
 #if HAVE_QRENCODE
 int dlopen_qrencode(void);
 
+int print_positioned_qrcode(FILE *out, const char *header, const char *string, unsigned row, unsigned column, unsigned tty_width, unsigned tty_height);
 int print_qrcode(FILE *out, const char *header, const char *string);
 #else
 static inline int print_qrcode(FILE *out, const char *header, const char *string) {
+        return -EOPNOTSUPP;
+}
+
+static inline int print_positioned_qrcode(FILE *out, const char *header, const char *string, unsigned row, unsigned column, unsigned tty_width, unsigned tty_height) {
         return -EOPNOTSUPP;
 }
 #endif
