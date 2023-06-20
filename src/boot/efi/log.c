57ad8e84b0fd61ed37f69fdf2fe6ad026f357474
Jan Janssen <medhefgo@web.de>
2023-06-20 16:51:17 +0200

    boot: Add device path logging helper

--- src/boot/efi/log.c
+++ src/boot/efi/log.c
@@ -1,5 +1,6 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 
+#include "device-path-util.h"
 #include "log.h"
 #include "proto/rng.h"
 #include "proto/simple-text-io.h"
@@ -69,6 +70,17 @@ void log_hexdump(const char16_t *prefix, const void *data, size_t size) {
 
         _cleanup_free_ char16_t *hex = hexdump(data, size);
         log_debug("%ls[%zu]: %ls", prefix, size, hex);
+}
+
+void log_device_path(const char16_t *prefix, const EFI_DEVICE_PATH *dp) {
+        _cleanup_free_ char16_t *str = NULL;
+        EFI_STATUS err;
+
+        err = device_path_to_str(dp, &str);
+        if (err == EFI_SUCCESS)
+                log_debug("%ls: %ls", prefix, str);
+        else
+                log_error_status(err, "%ls: %m", prefix);
 }
 #endif
 
