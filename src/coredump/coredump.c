99ae54da5d898bff44e6daf99563b5e7e3d5352b
Lennart Poettering <lennart@poettering.net>
2023-06-22 17:11:00 +0200

    coredump: clamp JournalSizeMax= to maximum journald would accept
    See: #26748

--- src/coredump/coredump.c
+++ src/coredump/coredump.c
@@ -172,9 +172,27 @@ static int parse_config(void) {
                 {}
         };
 
-        return config_parse_config_file("coredump.conf", "Coredump\0",
-                                        config_item_table_lookup, items,
-                                        CONFIG_PARSE_WARN, NULL);
+        int r;
+
+        r = config_parse_config_file(
+                        "coredump.conf",
+                        "Coredump\0",
+                        config_item_table_lookup,
+                        items,
+                        CONFIG_PARSE_WARN,
+                        /* userdata= */ NULL);
+        if (r < 0)
+                return r;
+
+        /* Let's make sure we fix up the maximum size we send to the journal here on the client side, for
+         * efficiency reasons. journald wouldn't accept anything larger anyway. */
+        if (arg_journal_size_max > JOURNAL_SIZE_MAX) {
+                log_warning("JournalSizeMax= set to larger value (%s) than journald would accept (%s), lowering automatically.",
+                            FORMAT_BYTES(arg_journal_size_max), FORMAT_BYTES(JOURNAL_SIZE_MAX));
+                arg_journal_size_max = JOURNAL_SIZE_MAX;
+        }
+
+        return 0;
 }
 
 static uint64_t storage_size_max(void) {
