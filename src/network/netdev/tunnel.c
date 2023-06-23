b9ce57e99dd7ea687a6d1dbd0cdd4a4e218b5627
Yuxiang Zhu <vfreex@gmail.com>
2023-06-23 18:58:45 +0800

    Address review suggestions

--- src/network/netdev/tunnel.c
+++ src/network/netdev/tunnel.c
@@ -749,9 +749,12 @@ static int netdev_tunnel_verify(NetDev *netdev, const char *filename) {
                 return log_netdev_error_errno(netdev, SYNTHETIC_ERRNO(EINVAL),
                                               "The local address cannot be '%s' when Independent= or AssignToLoopback= is enabled, ignoring.",
                                               strna(netdev_local_address_type_to_string(t->local_type)));
-        if (t->ignore_df && t->pmtudisc)
+
+        if (t->pmtudisc > 0 && t->ignore_df)
                 return log_netdev_error_errno(netdev, SYNTHETIC_ERRNO(EINVAL),
-                                              "IgnoreDF cannot be enabled when DiscoverPathMTU= is enabled, ignoring.");
+                                              "IgnoreDF cannot be enabled when DiscoverPathMTU= is enabled");
+        if (t->pmtudisc < 0)
+                t->pmtudisc = !t->ignore_df;
         return 0;
 }
 
@@ -1195,8 +1198,7 @@ static void netdev_tunnel_init(NetDev *netdev) {
         assert(t);
 
         t->local_type = _NETDEV_LOCAL_ADDRESS_TYPE_INVALID;
-        t->pmtudisc = true;
-        t->ignore_df = false;
+        t->pmtudisc = -1;
         t->fou_encap_type = NETDEV_FOO_OVER_UDP_ENCAP_DIRECT;
         t->isatap = -1;
         t->gre_erspan_sequence = -1;
