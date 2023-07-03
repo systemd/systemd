03f7d10811e6ed62796ae0d013ab8f737a1a9901
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-07-03 15:15:27 +0900

    test: update test for free area calculation in repart

--- test/units/testsuite-58.sh
+++ test/units/testsuite-58.sh
@@ -1085,7 +1085,7 @@ EOF
                                               --json=pretty \
                                               "$imgs/zzz")
 
-    # FIXME: the second invocation fails.
+    # The second invocation
     output2=$(SYSTEMD_REPART_MKFS_OPTIONS_VFAT="-S 512" systemd-repart \
                                               --definitions="$defs" \
                                               --seed="$seed" \
@@ -1095,7 +1095,11 @@ EOF
                                               --sector-size=4096 \
                                               --defer-partitions=esp \
                                               --json=pretty \
-                                              "$imgs/zzz") || :
+                                              "$imgs/zzz")
+
+    diff -u <(echo "$output1" | grep -E "(offset|raw_size|raw_padding)") <(echo "$output2" | grep -E "(offset|raw_size|raw_padding)")
+    assert_eq "$(echo "$output1" | grep "activity" | sort -u | awk -F: "{ print $2 }")" "created"
+    assert_eq "$(echo "$output2" | grep "activity" | sort -u | awk -F: "{ print $2 }")" "unchanged"
 }
 
 test_sector() {
