48ef3c86b43cc518d2b870e323469ca492c70f54
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2023-07-09 13:33:50 -0600

    gpt-auto-generator: use RET_GATHER, return first error

--- src/gpt-auto-generator/gpt-auto-generator.c
+++ src/gpt-auto-generator/gpt-auto-generator.c
@@ -806,7 +806,7 @@ static int enumerate_partitions(dev_t devnum) {
         _cleanup_(dissected_image_unrefp) DissectedImage *m = NULL;
         _cleanup_(loop_device_unrefp) LoopDevice *loop = NULL;
         _cleanup_free_ char *devname = NULL;
-        int r, k;
+        int r;
 
         r = block_get_whole_disk(devnum, &devnum);
         if (r < 0)
@@ -846,45 +846,29 @@ static int enumerate_partitions(dev_t devnum) {
                 return ok ? 0 : r;
         }
 
-        if (m->partitions[PARTITION_SWAP].found) {
-                k = add_partition_swap(m->partitions + PARTITION_SWAP);
-                if (k < 0)
-                        r = k;
-        }
+        if (m->partitions[PARTITION_SWAP].found)
+                RET_GATHER(r, add_partition_swap(m->partitions + PARTITION_SWAP));
 
-        k = process_loader_partitions(m->partitions + PARTITION_ESP, m->partitions + PARTITION_XBOOTLDR);
-        if (k < 0)
-                r = k;
+        RET_GATHER(r, process_loader_partitions(m->partitions + PARTITION_ESP, m->partitions + PARTITION_XBOOTLDR));
 
-        if (m->partitions[PARTITION_HOME].found) {
-                k = add_partition_mount(PARTITION_HOME, m->partitions + PARTITION_HOME, "home", "/home", "Home Partition");
-                if (k < 0)
-                        r = k;
-        }
+        if (m->partitions[PARTITION_HOME].found)
+                RET_GATHER(r, add_partition_mount(PARTITION_HOME, m->partitions + PARTITION_HOME,
+                                                  "home", "/home", "Home Partition"));
 
-        if (m->partitions[PARTITION_SRV].found) {
-                k = add_partition_mount(PARTITION_SRV, m->partitions + PARTITION_SRV, "srv", "/srv", "Server Data Partition");
-                if (k < 0)
-                        r = k;
-        }
+        if (m->partitions[PARTITION_SRV].found)
+                RET_GATHER(r, add_partition_mount(PARTITION_SRV, m->partitions + PARTITION_SRV,
+                                                  "srv", "/srv", "Server Data Partition"));
 
-        if (m->partitions[PARTITION_VAR].found) {
-                k = add_partition_mount(PARTITION_VAR, m->partitions + PARTITION_VAR, "var", "/var", "Variable Data Partition");
-                if (k < 0)
-                        r = k;
-        }
+        if (m->partitions[PARTITION_VAR].found)
+                RET_GATHER(r, add_partition_mount(PARTITION_VAR, m->partitions + PARTITION_VAR,
+                                                  "var", "/var", "Variable Data Partition"));
 
-        if (m->partitions[PARTITION_TMP].found) {
-                k = add_partition_mount(PARTITION_TMP, m->partitions + PARTITION_TMP, "var-tmp", "/var/tmp", "Temporary Data Partition");
-                if (k < 0)
-                        r = k;
-        }
+        if (m->partitions[PARTITION_TMP].found)
+                RET_GATHER(r, add_partition_mount(PARTITION_TMP, m->partitions + PARTITION_TMP,
+                                                  "var-tmp", "/var/tmp", "Temporary Data Partition"));
 
-        if (m->partitions[PARTITION_ROOT].found) {
-                k = add_partition_root_rw(m->partitions + PARTITION_ROOT);
-                if (k < 0)
-                        r = k;
-        }
+        if (m->partitions[PARTITION_ROOT].found)
+                RET_GATHER(r, add_partition_root_rw(m->partitions + PARTITION_ROOT));
 
         return r;
 }
