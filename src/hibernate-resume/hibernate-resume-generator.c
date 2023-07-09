4427194292311fb9899eae1543effdcb09efa19b
Mike Yuan <me@yhndnzj.com>
2023-07-10 00:40:13 +0800

    hibernate-resume: compare device nodes using path_equal_or_inode_same
    Follow-up for 9deeca127520b1098c3dfab9cdfd3b9c6bf983a4

--- src/hibernate-resume/hibernate-resume-generator.c
+++ src/hibernate-resume/hibernate-resume-generator.c
@@ -21,6 +21,7 @@
 #include "main-func.h"
 #include "os-util.h"
 #include "parse-util.h"
+#include "path-util.h"
 #include "proc-cmdline.h"
 #include "special.h"
 #include "string-util.h"
@@ -169,7 +170,7 @@ static int parse_efi_hibernate_location(void) {
                 arg_resume_device = TAKE_PTR(device);
                 arg_resume_offset = location.offset;
         } else {
-                if (!streq(arg_resume_device, device))
+                if (!path_equal_or_inode_same(arg_resume_device, device, 0))
                         log_warning("resume=%s doesn't match with HibernateLocation device '%s', proceeding anyway with resume=.",
                                     arg_resume_device, device);
 
