01d43c8129042813e19ef6bc2f44e89708b185e0
Jan Janssen <medhefgo@web.de>
2023-07-09 11:27:28 +0200

    boot: Add SYSTEMD_BOOT_LOG_LEVEL UEFI shell variable
    Using the UEFI shell namespace allows easily setting a log level from
    within a UEFI shell using `set SYSTEMD_BOOT_LOG_LEVEL`. This is far
    superior to using the LOADER_GUID namespace as `setvar` would require
    spelling out the UUID.

--- src/boot/efi/log.h
+++ src/boot/efi/log.h
@@ -31,6 +31,7 @@ typedef enum {
 extern LogLevel max_log_level;
 
 _noreturn_ void freeze(void);
+void log_init(void);
 void log_wait(void);
 _gnu_printf_(3, 4) EFI_STATUS log_internal(LogLevel level, EFI_STATUS status, const char *format, ...);
 
