28be12b6b8df4f09b1b8a8c340cf0f9595a0d7ff
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2023-07-09 13:32:55 -0600

    bootctl: use RET_GATHER, return first error

--- src/boot/bootctl-util.c
+++ src/boot/bootctl-util.c
@@ -4,26 +4,27 @@
 
 #include "bootctl.h"
 #include "bootctl-util.h"
+#include "errno-util.h"
 #include "fileio.h"
 #include "stat-util.h"
 #include "sync-util.h"
 
 int sync_everything(void) {
-        int ret = 0, k;
+        int r = 0, k;
 
         if (arg_esp_path) {
                 k = syncfs_path(AT_FDCWD, arg_esp_path);
                 if (k < 0)
-                        ret = log_error_errno(k, "Failed to synchronize the ESP '%s': %m", arg_esp_path);
+                        RET_GATHER(r, log_error_errno(k, "Failed to synchronize the ESP '%s': %m", arg_esp_path));
         }
 
         if (arg_xbootldr_path) {
                 k = syncfs_path(AT_FDCWD, arg_xbootldr_path);
                 if (k < 0)
-                        ret = log_error_errno(k, "Failed to synchronize $BOOT '%s': %m", arg_xbootldr_path);
+                        RET_GATHER(r, log_error_errno(k, "Failed to synchronize $BOOT '%s': %m", arg_xbootldr_path));
         }
 
-        return ret;
+        return r;
 }
 
 const char *get_efi_arch(void) {
