60a18d4a0796f4caa0240ec0e7dac9e7ea7af5ae
Daan De Meyer <daan.j.demeyer@gmail.com>
2023-07-10 13:50:30 +0200

    ukify: Derive public key from private key if not specified

--- src/ukify/ukify.py
+++ src/ukify/ukify.py
@@ -729,11 +729,17 @@ def make_uki(opts):
     uki = UKI(opts.stub)
     initrd = join_initrds(opts.initrd)
 
-    # TODO: derive public key from opts.pcr_private_keys?
     pcrpkey = opts.pcrpkey
     if pcrpkey is None:
         if opts.pcr_public_keys and len(opts.pcr_public_keys) == 1:
             pcrpkey = opts.pcr_public_keys[0]
+        elif opts.pcr_private_keys and len(opts.pcr_private_keys) == 1:
+            import cryptography.hazmat.primitives.serialization as serialization
+            privkey = serialization.load_pem_private_key(opts.pcr_private_keys[0].read_bytes(), password=None)
+            pcrpkey = privkey.public_key().public_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PublicFormat.SubjectPublicKeyInfo,
+            )
 
     sections = [
         # name,      content,         measure?
