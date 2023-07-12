6f2747c920f3dd39da1986838d156fa7415353d4
Dan Streetman <ddstreet@ieee.org>
2023-07-11 21:23:36 -0400

    tpm2: add Tpm2PCRValue struct and associated functions
    Add a new struct that can represent a PCR index, hash, and value all
    together. This replaces code (e.g. the tpm2_pcr_read() parameters) that
    required using both a TPML_PCR_SELECTION as well as array of TPM2B_DIGEST
    entries, which was difficult to correlate the selection hash/index to each
    digest.

--- src/cryptsetup/cryptsetup.c
+++ src/cryptsetup/cryptsetup.c
@@ -435,7 +435,7 @@ static int parse_one_option(const char *option) {
                         }
 
                         pcr = r ? TPM_PCR_INDEX_VOLUME_KEY : UINT_MAX;
-                } else if (!TPM2_PCR_VALID(pcr)) {
+                } else if (!TPM2_PCR_INDEX_VALID(pcr)) {
                         log_warning("Selected TPM index for measurement %u outside of allowed range 0…%u, ignoring.", pcr, TPM2_PCRS_MAX-1);
                         return 0;
                 }
