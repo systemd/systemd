89b7c4b1132d0fef0f130ca185f32e9773cf8279
Lennart Poettering <lennart@poettering.net>
2023-07-12 22:50:25 +0200

    format-table: add new table_get_current_column() helper

--- src/shared/format-table.c
+++ src/shared/format-table.c
@@ -2577,6 +2577,14 @@ size_t table_get_columns(Table *t) {
         return t->n_columns;
 }
 
+size_t table_get_current_column(Table *t) {
+        if (!t)
+                return 0;
+
+        assert(t->n_columns > 0);
+        return t->n_cells % t->n_columns;
+}
+
 int table_set_reverse(Table *t, size_t column, bool b) {
         assert(t);
         assert(column < t->n_columns);
