6f7bec0be56cdad401d493ba7b1b4af26732292b
Dmitry V. Levin <ldv@strace.io>
2023-07-14 08:00:00 +0000

    cryptsetup: cleanup use of ERRNO_IS_NOT_SUPPORTED()
    Given that ERRNO_IS_NOT_SUPPORTED() also matches positive values,
    make sure this macro is not called with arguments that do not have
    errno semantics.
    
    In this case the argument passed to ERRNO_IS_NOT_SUPPORTED() is the
    value returned by find_tpm2_auto_data() which is not expected to return
    any positive values, but let's be consistent anyway and move the
    ERRNO_IS_NOT_SUPPORTED() invocation to the branch where
    the return value is known to be negative.

--- src/cryptsetup/cryptsetup.c
+++ src/cryptsetup/cryptsetup.c
@@ -1728,10 +1728,11 @@ static int attach_luks_or_plain_or_bitlk_by_tpm2(
                                                               found_some
                                                               ? "No TPM2 metadata matching the current system state found in LUKS2 header, falling back to traditional unlocking."
                                                               : "No TPM2 metadata enrolled in LUKS2 header, falling back to traditional unlocking.");
-                                if (ERRNO_IS_NOT_SUPPORTED(r))  /* TPM2 support not compiled in? */
-                                        return log_debug_errno(SYNTHETIC_ERRNO(EAGAIN), "TPM2 support not available, falling back to traditional unlocking.");
-                                if (r < 0)
+                                if (r < 0) {
+                                        if (ERRNO_IS_NOT_SUPPORTED(r))  /* TPM2 support not compiled in? */
+                                                return log_debug_errno(SYNTHETIC_ERRNO(EAGAIN), "TPM2 support not available, falling back to traditional unlocking.");
                                         return r;
+                                }
 
                                 found_some = true;
 
