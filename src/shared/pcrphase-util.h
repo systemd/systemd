9aed22f26b73e59ad75d35f34230c3a2c8fc1782
Lennart Poettering <lennart@poettering.net>
2023-07-16 11:09:30 +0200

    pcrphase: split out word to measure code into shared helper file
    Let's split out the logic that actually generates the word to measure to
    PCRs into a new helper file pcrphase-util.[ch].
    
    This we can later reuse to calculate PCR measurement predictions ahead
    of time.

---
+++ src/shared/pcrphase-util.h
@@ -0,0 +1,5 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#pragma once
+
+int pcrphase_file_system_word(const char *path, char **ret, char **ret_normalized_path);
+int pcrphase_machine_id_word(char **ret);
