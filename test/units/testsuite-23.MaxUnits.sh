281c8d5d31ab21713128b1d33bb1dc2de203734f
David Tardon <dtardon@redhat.com>
2023-07-17 09:55:44 +0200

    test: add test for org.freedesktop.systemd1.Slice.Units

--- test/units/testsuite-23.MaxUnits.sh
+++ test/units/testsuite-23.MaxUnits.sh
@@ -28,30 +28,46 @@ systemctl start testsuite-23.MaxUnits.slice
 : Check that DBus property is exported properly
 
 assert_property "$(get_unit -.slice)" MaxUnits $[2**32 - 1]
-assert_property "$(get_unit testsuite-23.MaxUnits.slice)" MaxUnits 4
+slice="$(get_unit testsuite-23.MaxUnits.slice)"
+assert_property "$slice" MaxUnits 4
+assert_property "$slice" Units 0
 
 : Check that unit count capping works
 
 systemd-run --unit testsuite-23.MaxUnits-1.service --slice testsuite-23.MaxUnits.slice sleep infinity
+assert_property "$slice" Units 1
 systemd-run --unit testsuite-23.MaxUnits-2.service --slice testsuite-23.MaxUnits.slice sleep infinity
+assert_property "$slice" Units 2
 systemd-run --unit testsuite-23.MaxUnits-3.service --slice testsuite-23.MaxUnits.slice sleep infinity
+assert_property "$slice" Units 3
 systemd-run --unit testsuite-23.MaxUnits-4.service --slice testsuite-23.MaxUnits.slice sleep infinity
+assert_property "$slice" Units 4
 (! systemd-run --unit testsuite-23.MaxUnits-5.service --slice testsuite-23.MaxUnits.slice sleep infinity)
 systemctl stop testsuite-23.MaxUnits-1.service
+assert_property "$slice" Units 3
 systemd-run --unit testsuite-23.MaxUnits-5.service --slice testsuite-23.MaxUnits.slice sleep infinity
+assert_property "$slice" Units 4
 (! systemd-run --unit testsuite-23.MaxUnits-6.service --slice testsuite-23.MaxUnits.slice sleep infinity)
 
 : Check that the capping is recursive
 
 systemctl stop testsuite-23.MaxUnits-2.service
-systemctl start testsuite-23.MaxUnits-child.slice
+assert_property "$slice" Units 3
 
+systemctl start testsuite-23.MaxUnits-child.slice
+child_slice="$(get_unit testsuite-23.MaxUnits-child.slice)"
+assert_property "$slice" Units 4
+assert_property "$child_slice" Units 0
 # testsuite-23.MaxUnits-child.slice is not capped itself
-assert_property "$(get_unit testsuite-23.MaxUnits-child.slice)" MaxUnits $[2**32 - 1]
+assert_property "$child_slice" MaxUnits $[2**32 - 1]
+
 
 (! systemd-run --slice testsuite-23.MaxUnits-child.slice sleep infinity)
 systemctl stop testsuite-23.MaxUnits-3.service
+assert_property "$slice" Units 3
 systemd-run --slice testsuite-23.MaxUnits-child.slice sleep infinity
+assert_property "$slice" Units 4
+assert_property "$child_slice" Units 1
 (! systemd-run --slice testsuite-23.MaxUnits-child.slice sleep infinity)
 
 : Check that the capping works for templates too # the original use case
