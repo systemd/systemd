987b90f93f59ac5f4a4240c17f7bb285e2f0859a
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-07-19 02:11:16 +0900

    udev: split-out worker code from udevd.c
    While doing that, the Manager object allocated by the main process
    is now _not_ freed in the spawned worker process, to follow our usual
    coding style. See discussion in #24043.

--- src/udev/udevd.h
+++ src/udev/udevd.h
@@ -1,6 +1,8 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 #pragma once
 
+#include <stdbool.h>
+
 int run_udevd(int argc, char *argv[]);
 
 bool devpath_conflict(const char *a, const char *b);
