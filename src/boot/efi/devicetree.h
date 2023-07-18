f46a2ef87a621d6f5caad0aea34b3a5ea61ee4f2
Diogo Ivo <diogo.ivo@siemens.com>
2023-07-18 13:44:29 +0200

    boot: add matching against FW-provided DeviceTree blob
    Add support for matching the DT contained in a .dtb section of the
    UKI image against the FW provided FDT.

--- src/boot/efi/devicetree.h
+++ src/boot/efi/devicetree.h
@@ -9,6 +9,26 @@ struct devicetree_state {
         void *orig;
 };
 
+#define FDT_BEGIN_NODE  0x00000001
+#define FDT_END_NODE    0x00000002
+#define FDT_PROP        0x00000003
+#define FDT_NOP         0x00000004
+#define FDT_END         0x00000009
+
+struct fdt_header {
+        uint32_t Magic;
+        uint32_t TotalSize;
+        uint32_t OffDTStruct;
+        uint32_t OffDTStrings;
+        uint32_t OffMemRsvMap;
+        uint32_t Version;
+        uint32_t LastCompVersion;
+        uint32_t BootCPUIDPhys;
+        uint32_t SizeDTStrings;
+        uint32_t SizeDTStruct;
+};
+
+EFI_STATUS devicetree_match(const void *dtb_buffer, size_t dtb_length);
 EFI_STATUS devicetree_install(struct devicetree_state *state, EFI_FILE *root_dir, char16_t *name);
 EFI_STATUS devicetree_install_from_memory(
                 struct devicetree_state *state, const void *dtb_buffer, size_t dtb_length);
