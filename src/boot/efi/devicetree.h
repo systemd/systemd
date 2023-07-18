773909514a611000a08494b057927c2c17324b7f
Diogo Ivo <diogo.ivo@siemens.com>
2023-07-18 13:44:29 +0200

    boot: add matching against several device trees
    Add support for the case where the .dtb section of the UKI image
    contains more than one FDT by matching against the FW provided FDT.

--- src/boot/efi/devicetree.h
+++ src/boot/efi/devicetree.h
@@ -9,6 +9,25 @@ struct devicetree_state {
         void *orig;
 };
 
+#define FDT_BEGIN_NODE  0x00000001
+#define FDT_END_NODE    0x00000002
+#define FDT_PROP        0x00000003
+#define FDT_NOP         0x00000004
+#define FDT_END         0x00000009
+
+struct fdt_header {
+    uint32_t magic;
+    uint32_t totalsize;
+    uint32_t off_dt_struct;
+    uint32_t off_dt_strings;
+    uint32_t off_mem_rsvmap;
+    uint32_t version;
+    uint32_t last_comp_version;
+    uint32_t boot_cpuid_phys;
+    uint32_t size_dt_strings;
+    uint32_t size_dt_struct;
+};
+
 EFI_STATUS devicetree_install(struct devicetree_state *state, EFI_FILE *root_dir, char16_t *name);
 EFI_STATUS devicetree_install_from_memory(
                 struct devicetree_state *state, const void *dtb_buffer, size_t dtb_length);
