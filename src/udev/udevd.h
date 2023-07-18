81c32b1d418626c301e661965104675b6b939d68
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-07-19 05:17:03 +0900

    udev: split udevd.c into two

--- src/udev/udevd.h
+++ src/udev/udevd.h
@@ -1,8 +1,4 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 #pragma once
 
-#include <stdbool.h>
-
 int run_udevd(int argc, char *argv[]);
-
-bool devpath_conflict(const char *a, const char *b);
