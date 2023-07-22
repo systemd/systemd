b41ab9b3f44663b0ac0471de7e3c87db47b28c01
Luca Boccassi <bluca@debian.org>
2023-07-22 23:35:40 +0100

    softreboot: ensure all processes are killed
    Having surviving processes is not ready yet as a feature, so ensure
    everything is killed on the transition for now

--- src/core/main.c
+++ src/core/main.c
@@ -1871,6 +1871,9 @@ static int do_reexecute(
          * SIGCHLD for them after deserializing. */
         if (IN_SET(objective, MANAGER_SWITCH_ROOT, MANAGER_SOFT_REBOOT))
                 broadcast_signal(SIGTERM, /* wait_for_exit= */ false, /* send_sighup= */ true, arg_default_timeout_stop_usec);
+        /* On soft reboot really make sure nothing is left */
+        if (objective == MANAGER_SOFT_REBOOT)
+                broadcast_signal(SIGKILL, /* wait_for_exit= */ false, /* send_sighup= */ false, arg_default_timeout_stop_usec);
 
         if (!switch_root_dir && objective == MANAGER_SOFT_REBOOT) {
                 /* If no switch root dir is specified, then check if /run/nextroot/ qualifies and use that */
