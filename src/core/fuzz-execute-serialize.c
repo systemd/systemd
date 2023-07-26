8daf507dff7313f2e92bdd92763c11d13543d796
Luca Boccassi <bluca@debian.org>
2023-07-26 16:19:25 +0100

    core: support serializing to FD array for sendmsg/recvmsg
    When serializing across a fork, a fdset can be used, and the serialization writes down
    the FD number. The child process can then deserialize by adding all FDs it spawns with
    to a set, and assigning to the right object based on the FD number from the serialized
    key=value strings.
    
    When sending over via sendmsg/recvmsg (SCM_RIGHTS) however, the serialized key=value
    string needs to list not the FD number, but the index of the array in which the
    receiver shall find the corresponding FD, as delivered by SCM_RIGHTS.

--- src/core/fuzz-execute-serialize.c
+++ src/core/fuzz-execute-serialize.c
@@ -25,60 +25,36 @@
 #include "fuzz.h"
 #include "service.h"
 
-static void exec_fuzz_one(FILE *f, FDSet *fdset) {
-        _cleanup_(exec_params_serialized_done) ExecParameters params = {
-                .stdin_fd         = -EBADF,
-                .stdout_fd        = -EBADF,
-                .stderr_fd        = -EBADF,
-                .exec_fd          = -EBADF,
-                .user_lookup_fd   = -EBADF,
-                .bpf_outer_map_fd = -EBADF,
-        };
+static void exec_fuzz_one(FILE *f, FDSet *fdset, bool store_index) {
+        _cleanup_(exec_params_serialized_done) ExecParameters params = EXEC_PARAMETERS_INIT(/* flags= */ 0);
         _cleanup_(exec_context_done) ExecContext exec_context = {};
+        _cleanup_(exec_command_done) ExecCommand command = {};
         _cleanup_(cgroup_context_done) CGroupContext cgroup_context = {};
-        DynamicCreds dynamic_creds = {};
-        ExecCommand command = {};
-        ExecSharedRuntime shared = {
+        _cleanup_(dynamic_creds_done) DynamicCreds dynamic_creds = {};
+        _cleanup_(exec_shared_runtime_done) ExecSharedRuntime shared = {
                 .netns_storage_socket = PIPE_EBADF,
                 .ipcns_storage_socket = PIPE_EBADF,
         };
-        ExecRuntime runtime = {
+        _cleanup_(exec_runtime_clear) ExecRuntime runtime = {
                 .ephemeral_storage_socket = PIPE_EBADF,
                 .shared = &shared,
                 .dynamic_creds = &dynamic_creds,
         };
+        int fd_index = 0;
 
         exec_context_init(&exec_context);
         cgroup_context_init(&cgroup_context);
 
-        (void) exec_deserialize_invocation(f, fdset, &exec_context, &command, &params, &runtime, &cgroup_context);
-        (void) exec_serialize_invocation(f, fdset, &exec_context, &command, &params, &runtime, &cgroup_context);
-        (void) exec_deserialize_invocation(f, fdset, &exec_context, &command, &params, &runtime, &cgroup_context);
+        (void) exec_deserialize_invocation(f, fdset, NULL, 0, &exec_context, &command, &params, &runtime, &cgroup_context);
+        (void) exec_serialize_invocation(f, fdset, store_index ? &fd_index : NULL, &exec_context, &command, &params, &runtime, &cgroup_context);
+        (void) exec_deserialize_invocation(f, fdset, NULL, 0, &exec_context, &command, &params, &runtime, &cgroup_context);
 
-        /* We definitely didn't provide valid FDs during deserialization, so
-         * wipe the FDs before exec_params_serialized_clear() kicks in, otherwise
-         * we'll hit the assert in safe_close() */
-        params.stdin_fd = -EBADF;
-        params.stdout_fd = -EBADF;
-        params.stderr_fd = -EBADF;
-        params.exec_fd = -EBADF;
-        params.user_lookup_fd = -EBADF;
-        params.bpf_outer_map_fd = -EBADF;
-        if (!params.fds)
-                params.n_socket_fds = params.n_storage_fds = 0;
-        for (size_t i = 0; params.fds && i < params.n_socket_fds + params.n_storage_fds; i++)
-                params.fds[i] = -EBADF;
-
-        exec_command_done_array(&command, /* n= */ 1);
-        exec_shared_runtime_done(&shared);
-        if (dynamic_creds.group != dynamic_creds.user)
-                dynamic_user_free(dynamic_creds.group);
-        dynamic_user_free(dynamic_creds.user);
-        free(runtime.ephemeral_copy);
-        safe_close_pair(runtime.ephemeral_storage_socket);
+        /* Asserts on cleanup */
+        params.n_socket_fds = params.n_storage_fds = 0;
 }
 
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+        _cleanup_close_ int devnull = -EBADF;
         _cleanup_fclose_ FILE *f = NULL;
         _cleanup_fdset_free_ FDSet *fdset = NULL;
 
@@ -89,10 +65,23 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
                 log_set_target(LOG_TARGET_NULL);
         }
 
+        assert_se((devnull = open("/dev/null", O_RDWR | O_CLOEXEC)) >= 0);
         assert_se(fdset = fdset_new());
         assert_se(f = data_to_file(data, size));
 
-        exec_fuzz_one(f, fdset);
+        for (size_t i = 0; i < 64; i++)
+                fdset_put_dup_indexed(fdset, devnull, i);
+
+        log_info("/* %s - serializing FDs by index */", __func__);
+        exec_fuzz_one(f, fdset, /* store_index= */ true);
+
+        fdset = fdset_free(fdset);
+        assert_se(fdset = fdset_new());
+        for (size_t i = 0; i < 64; i++)
+                fdset_put_dup(fdset, devnull);
+
+        log_info("/* %s - serializing FDs by value */", __func__);
+        exec_fuzz_one(f, fdset, /* store_index= */ false);
 
         return 0;
 }
