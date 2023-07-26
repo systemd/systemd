16dbe9773e659ae2505829a32031a62e1b29ca1a
Luca Boccassi <bluca@debian.org>
2023-07-26 16:19:25 +0100

    core: support serializing to FD array for sendmsg/recvmsg
    When serializing across a fork, a fdset can be used, and the serialization writes down
    the FD number. The child process can then deserialize by adding all FDs it spawns with
    to a set, and assigning to the right object based on the FD number from the serialized
    key=value strings.
    
    When sending over via sendmsg/recvmsg (SCM_RIGHTS) however, the serialized key=value
    string needs to list not the FD number, but the index of the array in which the
    receiver shall find the corresponding FD, as delivered by SCM_RIGHTS.

--- src/core/execute.c
+++ src/core/execute.c
@@ -401,7 +401,15 @@ int exec_spawn(Unit *unit,
         if (!fdset)
                 return log_oom();
 
-        r = exec_serialize_invocation(f, fdset, context, command, params, runtime, cgroup_context);
+        r = exec_serialize_invocation(
+                        f,
+                        fdset,
+                        /* index= */ NULL,
+                        context,
+                        command,
+                        params,
+                        runtime,
+                        cgroup_context);
         if (r < 0)
                 return log_unit_error_errno(unit, r, "Failed to serialize parameters: %m");
 
@@ -2004,14 +2012,14 @@ int exec_shared_runtime_deserialize_compat(Unit *u, const char *key, const char 
         } else if (streq(key, "netns-socket-0")) {
 
                 safe_close(rt->netns_storage_socket[0]);
-                rt->netns_storage_socket[0] = deserialize_fd(fds, value);
+                rt->netns_storage_socket[0] = deserialize_fd_from_set(fds, value);
                 if (rt->netns_storage_socket[0] < 0)
                         return 0;
 
         } else if (streq(key, "netns-socket-1")) {
 
                 safe_close(rt->netns_storage_socket[1]);
-                rt->netns_storage_socket[1] = deserialize_fd(fds, value);
+                rt->netns_storage_socket[1] = deserialize_fd_from_set(fds, value);
                 if (rt->netns_storage_socket[1] < 0)
                         return 0;
         } else
@@ -2079,7 +2087,7 @@ int exec_shared_runtime_deserialize_one(Manager *m, const char *value, FDSet *fd
                 n = strcspn(v, " ");
                 buf = strndupa_safe(v, n);
 
-                netns_fdpair[0] = deserialize_fd(fds, buf);
+                netns_fdpair[0] = deserialize_fd_from_set(fds, buf);
                 if (netns_fdpair[0] < 0)
                         return netns_fdpair[0];
                 if (v[n] != ' ')
@@ -2094,7 +2102,7 @@ int exec_shared_runtime_deserialize_one(Manager *m, const char *value, FDSet *fd
                 n = strcspn(v, " ");
                 buf = strndupa_safe(v, n);
 
-                netns_fdpair[1] = deserialize_fd(fds, buf);
+                netns_fdpair[1] = deserialize_fd_from_set(fds, buf);
                 if (netns_fdpair[1] < 0)
                         return netns_fdpair[1];
                 if (v[n] != ' ')
@@ -2109,7 +2117,7 @@ int exec_shared_runtime_deserialize_one(Manager *m, const char *value, FDSet *fd
                 n = strcspn(v, " ");
                 buf = strndupa_safe(v, n);
 
-                ipcns_fdpair[0] = deserialize_fd(fds, buf);
+                ipcns_fdpair[0] = deserialize_fd_from_set(fds, buf);
                 if (ipcns_fdpair[0] < 0)
                         return ipcns_fdpair[0];
                 if (v[n] != ' ')
@@ -2124,7 +2132,7 @@ int exec_shared_runtime_deserialize_one(Manager *m, const char *value, FDSet *fd
                 n = strcspn(v, " ");
                 buf = strndupa_safe(v, n);
 
-                ipcns_fdpair[1] = deserialize_fd(fds, buf);
+                ipcns_fdpair[1] = deserialize_fd_from_set(fds, buf);
                 if (ipcns_fdpair[1] < 0)
                         return ipcns_fdpair[1];
         }
