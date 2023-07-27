ec570129e6694256e5f4335a78e1ab1e52014690
Luca Boccassi <bluca@debian.org>
2023-07-27 23:14:31 +0100

    softreboot: add survivors.slice for surviving services
    Add a new special top-level slice, survivors.slice. Units nested under
    this slice will not be stopped/restarted during a soft reboot
    operation. By defining this interface, we make the promise to users
    that we will do what's necessary to ensure the functionality is
    provided as expected, regardless of what changes under the hood.
    
    Implement the killing spree on soft reboot at the cgroup level,
    iterating over the remaining top-level cgroups and sending SIGTERM and
    SIGKILL to all apart from init.scope and survivors.slice. For this
    reason, this is supported only with cgroupsv2, but we intend to
    deprecated cgroupsv1 anyway.
    
    At the moment this interface implies the following:
    
    - not being sent SIGTERM/SIGKILL on final shutdown
    - no conflict/ordering with shutdown/softreboot targets
    - conflict/ordering with poweroff/halt/reboot/kexec targets
    - ignored on isolate when booting/shutting down the manager

--- src/basic/special.h
+++ src/basic/special.h
@@ -111,6 +111,7 @@
 #define SPECIAL_USER_SLICE "user.slice"
 #define SPECIAL_MACHINE_SLICE "machine.slice"
 #define SPECIAL_ROOT_SLICE "-.slice"
+#define SPECIAL_SOFT_REBOOT_SURVIVORS_SLICE "survivors.slice"
 
 /* The scope unit systemd itself lives in. */
 #define SPECIAL_INIT_SCOPE "init.scope"
