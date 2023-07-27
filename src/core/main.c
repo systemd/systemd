3a4ea2753e4b2a4619d9a3013f14cee80c9e5723
Luca Boccassi <bluca@debian.org>
2023-07-27 23:14:31 +0100

    pid1: add SurviveFinalKillSignal= to skip units on final sigterm/sigkill spree
    Add a new boolean for units, SurviveFinalKillSignal=yes/no. Units that
    set it will not have their process receive the final sigterm/sigkill in
    the shutdown phase.
    
    This is implemented by checking if a process is part of a cgroup marked
    with a user.survive_final_kill_signal xattr (or a trusted xattr if we
    can't set a user one, which were added only in kernel v5.7 and are not
    supported in CentOS 8).

--- src/core/main.c
+++ src/core/main.c
@@ -1766,7 +1766,8 @@ static void finish_remaining_processes(ManagerObjective objective) {
         if (IN_SET(objective, MANAGER_SWITCH_ROOT, MANAGER_SOFT_REBOOT))
                 broadcast_signal(SIGTERM, /* wait_for_exit= */ false, /* send_sighup= */ true, arg_defaults.timeout_stop_usec);
 
-        /* On soft reboot really make sure nothing is left */
+        /* On soft reboot really make sure nothing is left. Note that this will skip cgroups
+         * of units that were configured with SurviveFinalKillSignal=yes. */
         if (objective == MANAGER_SOFT_REBOOT)
                 broadcast_signal(SIGKILL, /* wait_for_exit= */ false, /* send_sighup= */ false, arg_defaults.timeout_stop_usec);
 }
