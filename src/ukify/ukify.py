4f21c68133d1bb6cb7632cc2f23c3b869fe90889
Daan De Meyer <daan.j.demeyer@gmail.com>
2023-07-31 21:35:02 +0200

    ukify: Only run systemd-measure after adding all sections
    We were running systemd-measure before adding the sbat section,
    let's fix that.
    
    Also make sure we only pass --linux to systemd-measure once instead
    of twice.

--- src/ukify/ukify.py
+++ src/ukify/ukify.py
@@ -792,6 +792,16 @@ def make_uki(opts):
     for section in opts.sections:
         uki.add_section(section)
 
+    if linux is not None:
+        # Merge the .sbat sections from stub, kernel and parameter, so that revocation can be done on either.
+        uki.add_section(Section.create('.sbat', merge_sbat([opts.stub, linux], opts.sbat), measure=True))
+    else:
+        if not opts.sbat:
+            opts.sbat = ["""sbat,1,SBAT Version,sbat,1,https://github.com/rhboot/shim/blob/main/SBAT.md
+uki,1,UKI,uki,1,https://www.freedesktop.org/software/systemd/man/systemd-stub.html
+"""]
+        uki.add_section(Section.create('.sbat', merge_sbat([], opts.sbat), measure=False))
+
     # PCR measurement and signing
 
     call_systemd_measure(uki, linux, opts=opts)
@@ -799,15 +809,7 @@ def make_uki(opts):
     # UKI or addon creation - addons don't use the stub so we add SBAT manually
 
     if linux is not None:
-        # Merge the .sbat sections from stub, kernel and parameter, so that revocation can be done on either.
-        uki.add_section(Section.create('.sbat', merge_sbat([opts.stub, linux], opts.sbat), measure=True))
         uki.add_section(Section.create('.linux', linux, measure=True))
-    else:
-        if not opts.sbat:
-            opts.sbat = ["""sbat,1,SBAT Version,sbat,1,https://github.com/rhboot/shim/blob/main/SBAT.md
-uki,1,UKI,uki,1,https://www.freedesktop.org/software/systemd/man/systemd-stub.html
-"""]
-        uki.add_section(Section.create('.sbat', merge_sbat([], opts.sbat), measure=False))
 
     if sign_args_present:
         unsigned = tempfile.NamedTemporaryFile(prefix='uki')
