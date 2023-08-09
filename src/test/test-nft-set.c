65a9b01086d35fff661ff80b4e21145e12194d9f
Topi Miettinen <toiwoton@gmail.com>
2023-08-09 23:07:21 +0300

    network: firewall integration with NFT sets
    New directive `NFTSet=` provides a method for integrating network configuration
    into firewall rules with NFT sets. The benefit of using this setting is that
    static network configuration or dynamically obtained network addresses can be
    used in firewall rules with the indirection of NFT set types. For example,
    access could be granted for hosts in the local subnetwork only. Firewall rules
    using IP address of an interface are also instantly updated when the network
    configuration changes, for example via DHCP.
    
    This option expects a whitespace separated list of NFT set definitions. Each
    definition consists of a colon-separated tuple of source type (one of
    "hostaddr", "network", or "ifindex"), NFT address family (one of "arp",
    "bridge", "inet", "ip", "ip6", or "netdev"), table name and set name. The names
    of tables and sets must conform to lexical restrictions of NFT table names. The
    type implied by the directive ("hostaddr", "network" or "ifindex") and address
    type (IPv4 or IPv6) must also match the set definition.
    
    When an interface is configured with IP addresses, the addresses and subnetwork
    masks will be appended to the NFT sets. They will be removed when the interface
    is deconfigured. Failures to manage the sets will be ignored.
    
    /etc/systemd/network/eth.network
    ```
    [DHCPv4]
    ...
    NFTSet=network:netdev:filter:eth_ipv4_network
    ```
    
    Example NFT rules:
    ```
    table netdev filter {
            set eth_ipv4_network {
                    type ipv4_addr
                    flags interval
            }
            chain eth_ingress {
                    type filter hook ingress device "eth0" priority filter; policy drop;
                    ip saddr != @eth_ipv4_network drop
                    accept
            }
    }
    ```
    ```
    $ sudo nft list set netdev filter eth_ipv4_network
    table netdev filter {
            set eth_ipv4_network {
                    type ipv4_addr
                    flags interval
                    elements = { 10.0.0.0/24 }
            }
    }
    ```

---
+++ src/test/test-nft-set.c
@@ -0,0 +1,70 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+
+#include <assert.h>
+#include <unistd.h>
+
+#include "firewall-util.h"
+#include "in-addr-util.h"
+#include "log.h"
+#include "netlink-internal.h"
+#include "parse-util.h"
+#include "string-util.h"
+#include "tests.h"
+
+int main(int argc, char **argv) {
+        int r;
+
+        assert_se(argc == 7);
+
+        test_setup_logging(LOG_DEBUG);
+
+        if (getuid() != 0)
+                return log_tests_skipped("not root");
+
+        int nfproto;
+        nfproto = nfproto_from_string(argv[2]);
+        assert_se(nfproto_is_valid(nfproto));
+
+        const char *table = argv[3], *set = argv[4];
+
+        FirewallContext *ctx;
+        r = fw_ctx_new(&ctx);
+        assert_se(r == 0);
+
+        bool add;
+        if (streq(argv[1], "add"))
+                add = true;
+        else
+                add = false;
+
+        if (streq(argv[5], "uint32")) {
+                uint32_t element;
+
+                r = safe_atou32(argv[6], &element);
+                assert_se(r == 0);
+
+                r = nft_set_element_modify_any(ctx, add, nfproto, table, set, &element, sizeof(element));
+                assert_se(r == 0);
+        } else if (streq(argv[5], "in_addr")) {
+                union in_addr_union addr;
+                int af;
+
+                r = in_addr_from_string_auto(argv[6], &af, &addr);
+                assert_se(r == 0);
+
+                r = nft_set_element_modify_ip(ctx, add, nfproto, af, table, set, &addr);
+                assert_se(r == 0);
+        } else if (streq(argv[5], "network")) {
+                union in_addr_union addr;
+                int af;
+                unsigned char prefixlen;
+
+                r = in_addr_prefix_from_string_auto(argv[6], &af, &addr, &prefixlen);
+                assert_se(r == 0);
+
+                r = nft_set_element_modify_iprange(ctx, add, nfproto, af, table, set, &addr, prefixlen);
+                assert_se(r == 0);
+        }
+
+        return 0;
+}
