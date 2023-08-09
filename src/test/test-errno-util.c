b06c94c8a13279b9d7784a9b4be77679571789b5
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2023-08-09 18:21:13 +0200

    errno-util: allow ERRNO_IS_* to accept types wider than int
    This is useful if the variable is ssize_t and we don't want to trigger a
    warning or truncation.
    
    With gcc (gcc-13.2.1-1.fc38.x86_64), the resulting systemd binary is identical,
    so I assume that the compiler is able to completely optimize away the type.

--- src/test/test-errno-util.c
+++ src/test/test-errno-util.c
@@ -94,6 +94,19 @@ TEST(ERRNO_IS_TRANSIENT) {
         assert_se(!ERRNO_IS_NEG_TRANSIENT(EINTR));
         assert_se( ERRNO_IS_TRANSIENT(-EINTR));
         assert_se( ERRNO_IS_TRANSIENT(EINTR));
+
+        /* Test with type wider than int */
+        ssize_t r = -EAGAIN;
+        assert_se( ERRNO_IS_NEG_TRANSIENT(r));
+
+        /* On 64-bit arches, now (int) r == EAGAIN */
+        r = SSIZE_MAX - EAGAIN + 1;
+        assert_se(!ERRNO_IS_NEG_TRANSIENT(r));
+
+        assert_se(!ERRNO_IS_NEG_TRANSIENT(INT_MAX));
+        assert_se(!ERRNO_IS_NEG_TRANSIENT(INT_MIN));
+        assert_se(!ERRNO_IS_NEG_TRANSIENT(INTMAX_MAX));
+        assert_se(!ERRNO_IS_NEG_TRANSIENT(INTMAX_MIN));
 }
 
 DEFINE_TEST_MAIN(LOG_INFO);
