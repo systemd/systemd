e6b8be355dc4bba5b23e978d945e114778e759e1
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-08-11 04:46:00 +0900

    io-util: introduce iovw_append() helper function

--- src/basic/io-util.h
+++ src/basic/io-util.h
@@ -111,5 +111,6 @@ int iovw_put_string_field(struct iovec_wrapper *iovw, const char *field, const c
 int iovw_put_string_field_free(struct iovec_wrapper *iovw, const char *field, char *value);
 void iovw_rebase(struct iovec_wrapper *iovw, char *old, char *new);
 size_t iovw_size(struct iovec_wrapper *iovw);
+int iovw_append(struct iovec_wrapper *target, const struct iovec_wrapper *source);
 
 void iovec_array_free(struct iovec *iov, size_t n);
