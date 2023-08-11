8a672c93f7083deec2ab1598729f74846a06873c
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-08-11 13:59:20 +0900

    units: introduce systemd-tmpfiles-setup-dev-early.service
    This makes tmpfiles, sysusers, and udevd invoked in the following
    order:
    1. systemd-tmpfiles-setup-dev-early.service
       Create device nodes gracefully, that is, create device nodes anyway
       by ignoring unknown users and groups.
    2. systemd-sysusers.service
       Create users and groups, to make later invocations of tmpfiles and
       udevd can resolve necessary users and groups.
    3. systemd-tmpfiles-setup-dev.service
       Adjust owners of previously created device nodes.
    4. systemd-udevd.service
       Process all devices. Especially to make block devices active and can
       be mountable.
    5. systemd-tmpfiles-setup.service
       Setup basic filesystem.
    
    Follow-up for b42482af904ae0b94a6e4501ec595448f0ba1c06.
    
    Fixes #28653.
    Replaces #28681 and #28732.

--- test/units/testsuite-17.00.sh
+++ test/units/testsuite-17.00.sh
@@ -8,11 +8,50 @@ set -o pipefail
 
 # Tests for issue #28588 and #28653.
 
-assert_in "systemd-tmpfiles-setup-dev.service" "$(systemctl show --property After --value systemd-udevd.service)"
-assert_in "systemd-udevd.service" "$(systemctl show --property Before --value systemd-tmpfiles-setup-dev.service)"
+# On boot, services need to be started in the following order:
+# 1. systemd-tmpfiles-setup-dev-early.service
+# 2. systemd-sysusers.service
+# 3. systemd-tmpfiles-setup-dev.service
+# 4. systemd-udevd.service
 
-if [[ -f /dev/vfio/vfio ]]; then
-   assert_in "crw-rw-rw-" "$(stat --format=%A /dev/vfio/vfio)"
-fi
+output="$(systemctl show --property After --value systemd-udevd.service)"
+assert_in "systemd-tmpfiles-setup-dev-early.service" "$output"
+assert_in "systemd-sysusers.service" "$output"
+assert_in "systemd-tmpfiles-setup-dev.service" "$output"
+
+output="$(systemctl show --property After --value systemd-tmpfiles-setup-dev.service)"
+assert_in "systemd-tmpfiles-setup-dev-early.service" "$output"
+assert_in "systemd-sysusers.service" "$output"
+
+output="$(systemctl show --property After --value systemd-sysusers.service)"
+assert_in "systemd-tmpfiles-setup-dev-early.service" "$output"
+
+check_owner_and_mode() {
+    local dev=${1?}
+    local user=${2?}
+    local group=${3?}
+    local mode=${4:-}
+
+    if [[ -e "$dev" ]]; then
+        assert_in "$user" "$(stat --format=%U "$dev")"
+        assert_in "$group" "$(stat --format=%G "$dev")"
+        if [[ -n "$mode" ]]; then
+            assert_in "$mode" "$(stat --format=%#0a "$dev")"
+        fi
+    fi
+
+    return 0
+}
+
+# Check owner and access mode specified in static-nodes-permissions.conf
+check_owner_and_mode /dev/snd/seq      root audio 0660
+check_owner_and_mode /dev/snd/timer    root audio 0660
+check_owner_and_mode /dev/loop-control root disk  0660
+check_owner_and_mode /dev/net/tun      root root  0666
+check_owner_and_mode /dev/fuse         root root  0666
+check_owner_and_mode /dev/vfio/vfio    root root  0666
+check_owner_and_mode /dev/kvm          root kvm
+check_owner_and_mode /dev/vhost-net    root kvm
+check_owner_and_mode /dev/vhost-vsock  root kvm
 
 exit 0
