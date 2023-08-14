2af02e61657aa513aa7b6df7fd1a04b13f853cef
Daan De Meyer <daan.j.demeyer@gmail.com>
2023-08-14 12:54:40 +0200

    tree-wide: Always include <net/if.h> before related linux headers
    Otherwise, we get redefinition errors if <net/if.h> is included later
    on by another header.

--- src/network/networkd-manager.c
+++ src/network/networkd-manager.c
@@ -3,7 +3,8 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#include <linux/if.h>
+#include <net/if.h>
+#include <linux/if.h> /* Must be included after <net/if.h> */
 #include <linux/fib_rules.h>
 #include <linux/nexthop.h>
 #include <linux/nl80211.h>
