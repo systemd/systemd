a9c19eae03e8292140e2a39c5c8fd3de361026ea
Daan De Meyer <daan.j.demeyer@gmail.com>
2023-08-15 10:32:45 +0200

    repart: Rework read-only logic
    verity sig partitions can't actually be marked read-only, so let's
    not do that to avoid an ugly warning about that. Instead, let's just
    make sure that we mark verity hash and verity data partitions as
    read-only.

--- src/partition/repart.c
+++ src/partition/repart.c
@@ -1759,9 +1759,7 @@ static int partition_read_definition(Partition *p, const char *path, const char 
         /* Verity partitions are read only, let's imply the RO flag hence, unless explicitly configured otherwise. */
         if ((IN_SET(p->type.designator,
                     PARTITION_ROOT_VERITY,
-                    PARTITION_ROOT_VERITY_SIG,
-                    PARTITION_USR_VERITY,
-                    PARTITION_USR_VERITY_SIG) || p->verity != VERITY_OFF) && p->read_only < 0)
+                    PARTITION_USR_VERITY) || p->verity == VERITY_DATA) && p->read_only < 0)
                 p->read_only = true;
 
         /* Default to "growfs" on, unless read-only */
