4e458aa11e9524ce0b9c930b0d90c98b8cb80c70
Daan De Meyer <daan.j.demeyer@gmail.com>
2023-08-15 12:09:31 +0200

    repart: Put function call closer to its error handling

--- src/partition/repart.c
+++ src/partition/repart.c
@@ -2277,8 +2277,8 @@ static int context_load_partition_table(Context *context) {
                 return log_oom();
 
         if (arg_sector_size > 0) {
-                r = fdisk_save_user_sector_size(c, /* phy= */ 0, arg_sector_size);
                 fs_secsz = arg_sector_size;
+                r = fdisk_save_user_sector_size(c, /* phy= */ 0, arg_sector_size);
         } else {
                 uint32_t ssz;
                 struct stat st;
