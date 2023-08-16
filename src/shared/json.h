8ab96f1fbfe62bd5ca0939342176366bb8fd201a
Lennart Poettering <lennart@poettering.net>
2023-08-16 09:35:07 +0200

    json: add json_variant_append_arrayb() helper

--- src/shared/json.h
+++ src/shared/json.h
@@ -213,6 +213,7 @@ int json_variant_set_field_strv(JsonVariant **v, const char *field, char **l);
 JsonVariant *json_variant_find(JsonVariant *haystack, JsonVariant *needle);
 
 int json_variant_append_array(JsonVariant **v, JsonVariant *element);
+int json_variant_append_arrayb(JsonVariant **v, ...);
 int json_variant_append_array_nodup(JsonVariant **v, JsonVariant *element);
 
 int json_variant_merge(JsonVariant **v, JsonVariant *m);
