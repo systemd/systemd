20caa85b84ecb371f55f07a06cbcec70d0840f1c
Susant Sahani <ssahani@gmail.com>
2023-08-16 22:37:48 +0530

    network: static route - Allow to configure per route hop liimt

--- src/network/networkd-route.h
+++ src/network/networkd-route.h
@@ -125,6 +125,7 @@ CONFIG_PARSER_PROTOTYPE(config_parse_ipv6_route_preference);
 CONFIG_PARSER_PROTOTYPE(config_parse_route_protocol);
 CONFIG_PARSER_PROTOTYPE(config_parse_route_type);
 CONFIG_PARSER_PROTOTYPE(config_parse_route_tcp_window);
+CONFIG_PARSER_PROTOTYPE(config_parse_route_hop_limit);
 CONFIG_PARSER_PROTOTYPE(config_parse_tcp_window);
 CONFIG_PARSER_PROTOTYPE(config_parse_route_tcp_rto);
 CONFIG_PARSER_PROTOTYPE(config_parse_route_mtu);
