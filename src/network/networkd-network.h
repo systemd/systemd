fcec61feb1924022ef42a46f57ca835dbed5a55a
Susant Sahani <ssahani@gmail.com>
2023-08-22 17:16:56 +0530

    network: radv - Allow to send hop limit

--- src/network/networkd-network.h
+++ src/network/networkd-network.h
@@ -228,6 +228,7 @@ struct Network {
         usec_t router_lifetime_usec;
         uint8_t router_preference;
         usec_t router_retransmit_usec;
+        uint8_t router_hop_limit;
         bool router_managed;
         bool router_other_information;
         bool router_emit_dns;
