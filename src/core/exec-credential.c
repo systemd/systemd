b95829633a98f907f7ae0c1c7fdc895d5684fac9
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-08-24 23:41:05 +0900

    core/credential,mount: re-read /proc/self/mountinfo before invoking umount command
    When a unit has credentials, stopping the service unmounts the credentials
    directory. On shutdown, stopping the service and the corresponding mount
    unit may be done mostly simultaneously, and if we invoke umount command soon
    after umount() being called on stopping the service, the mount unit will
    fail.
    
    This makes Mount.dirty flag set when umount() is called for a path, and
    re-read /proc/self/mouninfo before invoking umount command if the flag is set.
    
    Fixes #25527.
    Replaces #26959.

--- src/core/exec-credential.c
+++ src/core/exec-credential.c
@@ -12,6 +12,7 @@
 #include "label-util.h"
 #include "mkdir-label.h"
 #include "mount-util.h"
+#include "mount.h"
 #include "mountpoint-util.h"
 #include "process-util.h"
 #include "random-util.h"
@@ -138,19 +139,21 @@ int unit_add_default_credential_dependencies(Unit *u, const ExecContext *c) {
         return unit_add_dependency_by_name(u, UNIT_AFTER, m, /* add_reference= */ true, UNIT_DEPENDENCY_FILE);
 }
 
-int exec_context_destroy_credentials(const ExecContext *c, const char *runtime_prefix, const char *unit) {
+int exec_context_destroy_credentials(Unit *u) {
         _cleanup_free_ char *p = NULL;
         int r;
 
-        assert(c);
+        assert(u);
 
-        r = get_credential_directory(runtime_prefix, unit, &p);
+        r = get_credential_directory(u->manager->prefix[EXEC_DIRECTORY_RUNTIME], u->id, &p);
         if (r <= 0)
                 return r;
 
         /* This is either a tmpfs/ramfs of its own, or a plain directory. Either way, let's first try to
          * unmount it, and afterwards remove the mount point */
-        (void) umount2(p, MNT_DETACH|UMOUNT_NOFOLLOW);
+        if (umount2(p, MNT_DETACH|UMOUNT_NOFOLLOW) >= 0)
+                (void) mount_set_dirty_by_path(u->manager, p);
+
         (void) rm_rf(p, REMOVE_ROOT|REMOVE_CHMOD);
 
         return 0;
