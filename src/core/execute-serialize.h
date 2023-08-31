c677538a48d8894d17589f68e0cad03d2788e82b
Luca Boccassi <bluca@debian.org>
2023-09-01 00:31:26 +0100

    core: add serialization/deserialization for ExecContext

---
+++ src/core/execute-serialize.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#pragma once
+
+#include "execute.h"
+
+/* These functions serialize/deserialize for invocation purposes (i.e.: serialized object is passed to a
+ * child process) rather than to save state across reload/reexec. */
+
+int exec_serialize_invocation(FILE *f,
+        FDSet *fds,
+        const ExecContext *ctx);
+
+int exec_deserialize_invocation(FILE *f,
+        FDSet *fds,
+        ExecContext *ctx);
