b0d2cf602c74eb3c901c3cc61bad2b1ba92c6c6a
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-09-05 03:12:46 +0900

    sd-dhcp6-client: rework IA_NA or IA_PD getters
    This splits sd_dhcp6_lease_get_address() into small pieces,
    and introduce FOREACH_DHCP6_ADDRESS() macro.
    Also, the lifetimes provided by _get_address_lifetime() are now in usec,
    and _get_address_lifetime_timestamp() provides timestamp.
    
    The same change is also applied for IA_PD.

--- src/libsystemd-network/network-common.c
+++ src/libsystemd-network/network-common.c
@@ -13,3 +13,19 @@ int get_ifname(int ifindex, char **ifname) {
 
         return format_ifname_alloc(ifindex, ifname);
 }
+
+usec_t be32_sec_to_usec(be32_t t) {
+        uint32_t s = be32toh(t);
+
+        if (s == UINT32_MAX)
+                return USEC_INFINITY;
+
+        return s * USEC_PER_SEC;
+}
+
+usec_t time_span_to_stamp(usec_t span, usec_t base) {
+        if (span == 0)
+                return 0;
+
+        return usec_add(base, span);
+}
