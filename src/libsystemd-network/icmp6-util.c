55bdfc467309b54f85a52fb7134702888e175c75
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-09-04 17:00:31 +0900

    icmp6-util: several cleanups for icmp6_receive()
    - Rename the argument for storing the sendor address,
    - allow to call it with NULL for ret_xyz,
    - reduce needless copy of timestamp when the message does not have
      timestamp.

--- src/libsystemd-network/icmp6-util.c
+++ src/libsystemd-network/icmp6-util.c
@@ -144,8 +144,12 @@ int icmp6_send_router_solicitation(int s, const struct ether_addr *ether_addr) {
         return 0;
 }
 
-int icmp6_receive(int fd, void *buffer, size_t size, struct in6_addr *ret_dst,
-                  triple_timestamp *ret_timestamp) {
+int icmp6_receive(
+                int fd,
+                void *buffer,
+                size_t size,
+                struct in6_addr *ret_sendor,
+                triple_timestamp *ret_timestamp) {
 
         /* This needs to be initialized with zero. See #20741. */
         CMSG_BUFFER_TYPE(CMSG_SPACE(sizeof(int)) + /* ttl */
@@ -206,10 +210,14 @@ int icmp6_receive(int fd, void *buffer, size_t size, struct in6_addr *ret_dst,
                 }
         }
 
-        if (!triple_timestamp_is_set(&t))
-                triple_timestamp_get(&t);
+        if (ret_timestamp) {
+                if (triple_timestamp_is_set(&t))
+                        *ret_timestamp = t;
+                else
+                        triple_timestamp_get(ret_timestamp);
+        }
 
-        *ret_dst = addr;
-        *ret_timestamp = t;
+        if (ret_sendor)
+                *ret_sendor = addr;
         return 0;
 }
