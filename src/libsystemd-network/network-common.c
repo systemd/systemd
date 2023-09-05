7efec5d3c02d0e38805b1ca513e2b43e7adbf114
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-09-05 16:51:39 +0900

    sd-radv: make sd_radv always take timespan in usec

--- src/libsystemd-network/network-common.c
+++ src/libsystemd-network/network-common.c
@@ -23,6 +23,45 @@ usec_t be32_sec_to_usec(be32_t t) {
         return s * USEC_PER_SEC;
 }
 
+be32_t usec_to_be32_sec(usec_t t) {
+        if (t == USEC_INFINITY)
+                /* UINT32_MAX may be handled as infinity. */
+                return htobe32(UINT32_MAX);
+
+        t = DIV_ROUND_UP(t, USEC_PER_SEC);
+        if (t >= UINT32_MAX)
+                /* Finite but too large. Let's use the largest finite value. */
+                return htobe32(UINT32_MAX - 1);
+
+        return htobe32(t);
+}
+
+be32_t usec_to_be32_msec(usec_t t) {
+        if (t == USEC_INFINITY)
+                /* UINT32_MAX is handled as infinity. */
+                return htobe32(UINT32_MAX);
+
+        t = DIV_ROUND_UP(t, USEC_PER_MSEC);
+        if (t >= UINT32_MAX)
+                /* Finite but too large. Let's use the largest finite value. */
+                return htobe32(UINT32_MAX - 1);
+
+        return htobe32(t);
+}
+
+be16_t usec_to_be16_sec(usec_t t) {
+        if (t == USEC_INFINITY)
+                /* UINT32_MAX may be handled as infinity. */
+                return htobe32(UINT16_MAX);
+
+        t = DIV_ROUND_UP(t, USEC_PER_SEC);
+        if (t >= UINT16_MAX)
+                /* Finite but too large. Let's use the largest finite value. */
+                return htobe32(UINT16_MAX - 1);
+
+        return htobe16(t);
+}
+
 usec_t time_span_to_stamp(usec_t span, usec_t base) {
         if (span == 0)
                 return 0;
