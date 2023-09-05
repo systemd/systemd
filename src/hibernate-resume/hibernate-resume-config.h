e47c269e936c9881b99f7d3cdeeeefef762bef47
Mike Yuan <me@yhndnzj.com>
2023-09-05 21:50:04 +0800

    hibernate-resume: split out the logic of finding hibernate location
    Before this commit, the hibernate location logic only exists in
    the generator. Also, we compare device nodes (devnode_same()) and
    clear EFI variable HibernateLocation in the generator too. This is
    not ideal though: when the generator gets to run, udev hasn't yet
    started, so effectively devnode_same() always fails. Moreover, if
    the boot process is interrupted by e.g. battery-check, the hibernate
    information is lost.
    
    Therefore, let's split out the logic of finding hibernate location.
    The generator only does the initial validation of system info and
    enables systemd-hibernate-resume.service, and when the service
    actually runs we validate everything again, which includes comparing
    the device nodes and clearing the EFI variable. This should make
    things more robust, plus systems that don't utilize a systemd-enabled
    initrd can use the exact same logic to resume using the EFI variable.
    I.e., systemd-hibernate-resume can be used standalone.

---
+++ src/hibernate-resume/hibernate-resume-config.h
@@ -0,0 +1,39 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+
+#include <stdbool.h>
+
+#include "sd-id128.h"
+
+typedef struct KernelHibernateLocation {
+        char *device;
+        uint64_t offset;
+        bool offset_set;
+} KernelHibernateLocation;
+
+typedef struct EFIHibernateLocation {
+        char *device;
+
+        sd_id128_t uuid;
+        uint64_t offset;
+
+        char *kernel_version;
+        char *id;
+        char *image_id;
+        char *version_id;
+        char *image_version;
+} EFIHibernateLocation;
+
+typedef struct HibernateInfo {
+        const char *device;
+        uint64_t offset; /* in memory pages */
+        bool from_efi;
+
+        KernelHibernateLocation *k;
+        EFIHibernateLocation *e;
+} HibernateInfo;
+
+void hibernate_info_done(HibernateInfo *info);
+
+int acquire_hibernate_info(HibernateInfo *ret);
+
+void clear_efi_hibernate_location(void);
