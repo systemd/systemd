14d865e178a3b46b272b8a68caf3238f6a0f054f
Mike Yuan <me@yhndnzj.com>
2023-09-08 19:16:46 +0800

    shared/wall: several cleanups
    Follow-up for 53c0397b1dbc95f144d9a551c2086d132933e8ce
    
    * Split do_wall into wall_utmp and wall_logind
    * Don't pass unused arguments
    * Add missing asserts
    * Modernize error handling
    * Don't do anything if neither utmp nor logind is enabled

--- src/shared/wall.h
+++ src/shared/wall.h
@@ -3,9 +3,25 @@
 
 #include <stdbool.h>
 
+#if ENABLE_UTMP || ENABLE_LOGIND
+
 int wall(
         const char *message,
         const char *username,
         const char *origin_tty,
         bool (*match_tty)(const char *tty, bool is_local, void *userdata),
         void *userdata);
+
+#else
+
+static inline int wall(
+        const char *message,
+        const char *username,
+        const char *origin_tty,
+        bool (*match_tty)(const char *tty, bool is_local, void *userdata),
+        void *userdata) {
+
+        return 0;
+}
+
+#endif
