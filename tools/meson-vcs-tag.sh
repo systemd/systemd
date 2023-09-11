00d52863c735371ae01d0d79e165b82c2fcf766a
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2023-09-11 16:46:04 +0200

    meson: restore tools/meson-vcs-tag.sh
    This conceptually reverts e95acdfe1d3a790e18617bb992a712b34f41800d,
    but the actual contents of the script are taken from the command invocation
    in meson with all the updates that happened in the meantime.
    
    One small change is that I replaced () by {}: this avoids one subprocess spawn.
    People were worried about the cost of vcs_tag(), and this microoptimization may
    help a bit. I measured the speed on machine, and noop rebuilds are still about
    100–120 ms.
    
    The logic is entirely moved to the script. This makes the meson config simpler
    and also makes it easier to use it externally.
    
    The script is needed for in-place rpm builds, see README.build-in-place.md [1],
    where it is invoked from the spec file to determine the project version.
    
    [1] https://src.fedoraproject.org/rpms/systemd/blob/rawhide/f/README.build-in-place.md

---
+++ tools/meson-vcs-tag.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+set -eu
+set -o pipefail
+
+dir="${1:?}"
+fallback="${2:?}"
+version_tag="$3"
+
+if [ -n "${version_tag}" ]; then
+    # If -Dversion_tag= was used, just use that without further changes.
+    echo "${version_tag}"
+else
+    # Check that we have either .git/ (a normal clone) or a .git file (a work-tree)
+    # and that we don't get confused if a tarball is extracted in a higher-level
+    # git repository.
+    #
+    # If the working tree has no tags (CI builds), the first git-describe will fail
+    # and we fall back to project_version-commitid instead.
+    {
+        [ -e .git ] && git -C "$dir" describe --abbrev=7 --dirty=^ 2>/dev/null ||
+                echo "$fallback-$(git -C "$1" describe --always --abbrev=7)"
+    } | sed 's/^v//; s/-rc/~rc/'
+fi
