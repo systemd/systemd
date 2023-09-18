1aa8e8026bec53cf29942b6afaa22188584249ae
Mike Yuan <me@yhndnzj.com>
2023-09-18 20:31:59 +0800

    test: introduce test-btrfs-physical-offset

---
+++ test/test-btrfs-physical-offset.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+set -eu
+
+if ! command -v btrfs >/dev/null; then
+    echo "Missing btrfs tools, skipping the test"
+    exit 77
+fi
+
+if [[ "$1" ]]; then
+    TEST_BTRFS="$1"
+else
+    TEST_BTRFS="$(basename "$0")"/manual/test-btrfs-physical-offset
+fi
+
+[[ "$(stat -f -c %T /var/tmp/)" = "btrfs" ]] || exit 0
+
+SWAPFILE=/var/tmp/swapfile
+
+btrfs filesystem mkswapfile -s 1m "$SWAPFILE"
+sync -f "$SWAPFILE"
+
+offset_btrfs_progs="$(btrfs inspect-internal map-swapfile -r "$SWAPFILE")"
+echo "btrfs-progs: $offset_btrfs_progs"
+
+offset_btrfs_util="$("$TEST_BTRFS" "$SWAPFILE")"
+echo "btrfs-util: $offset_btrfs_util"
+
+(( offset_btrfs_progs == offset_btrfs_util )) || exit 1
