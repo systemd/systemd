78c19ad7a855aee5412fd5b0c8c231a1c6bf09a9
Nick Rosbrook <enr0n@ubuntu.com>
2023-09-19 17:09:58 -0400

    test: add a test for container support in coredump

--- test/TEST-74-AUX-UTILS/test.sh
+++ test/TEST-74-AUX-UTILS/test.sh
@@ -16,6 +16,23 @@ test_append_files() {
         # the QEMU test, as nspawn refuses the invalid machine ID with -EUCLEAN
         printf "556f48e837bc4424a710fa2e2c9d3e3c\ne3d\n" >"$workspace/etc/machine-id"
     fi
+
+    # This container will be used to test that we can forward coredumps back to containers.
+    local container="$workspace/var/lib/machines/testsuite-74-container"
+
+    mkdir -p "$container"
+    initdir="$container" setup_basic_dirs
+    initdir="$container" install_systemd
+    initdir="$container" install_missing_libraries
+    initdir="$container" install_config_files
+    initdir="$container" install_zoneinfo
+    initdir="$container" create_rc_local
+    initdir="$container" install_basic_tools
+    initdir="$container" install_libnss
+    initdir="$container" install_pam
+    initdir="$container" install_dbus
+    initdir="$container" install_debug_tools
+    initdir="$container" install_execs
 }
 
 do_test "$@"
