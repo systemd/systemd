08744157a49a1c6cc546b0a3f4a0a62af4a2821c
RoepLuke <contact@roeper-luke.de>
2023-09-21 23:52:04 +0200

    Add recommended initramfs regeneration
    I recently tried adding a FIDO2-Device as an unlocking method to the LUKS2 partition containing my Fedora install.
    When trying to do this, I stumbled upon the here edited man files detailing how to do this.
    I however could not unlock my partition with my FIDO2-Device after editing /etc/crypttab and rebooting.
    As I found out after a while, I needed to regenerate / update my currently running / used initramfs (https://unix.stackexchange.com/a/705809).
    This would have most likely solved itself for me with the next kernel update install (as far as I understand).
    So I propose changing the files edited here to recommend or at least inform the user about this.

--- man/fido2-crypttab.sh
+++ man/fido2-crypttab.sh
@@ -10,3 +10,12 @@ sudo /usr/lib/systemd/systemd-cryptsetup attach mytest /dev/sdXn - fido2-device=
 # If that worked, let's now add the same line persistently to /etc/crypttab,
 # for the future.
 sudo bash -c 'echo "mytest /dev/sdXn - fido2-device=auto" >>/etc/crypttab'
+
+# Depending on your distribution and encryption setup, you may need
+# to manually regenerate your initramfs to be able to use
+# a FIDO2-Device to unlock the partition during early boot.
+# More information at https://unix.stackexchange.com/a/705809
+# On Fedora based systems:
+sudo dracut --force
+# On Debian based systems:
+sudo update-initramfs -u
