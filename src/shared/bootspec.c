1031019cacbda5ab8b45a8b1fad26de4e69580c6
Emanuele Giuseppe Esposito <eesposit@redhat.com>
2023-09-21 07:59:24 -0400

    bootctl: discover and pring global UKI PE addons
    An UKI final command line is not just made of the content of .cmdline,
    but also from the addons that are inserted in
    /boot/efi/EFI/Linux/<UKI_NAME>.efi.extra.d (local addons) and /boot/efi/loader/addons
    (global addons).
    
    Therefore bootclt "status" and "list" should also include these addons
    when printing the UKI command line.
    
    Support for /boot/efi/EFI/Linux/<UKI_NAME>.efi.extra.d is already present,
    so now cover addons present in /boot/efi/loader/addons (global addons).

--- src/shared/bootspec.c
+++ src/shared/bootspec.c
@@ -446,6 +446,7 @@ void boot_config_free(BootConfig *config) {
         for (size_t i = 0; i < config->n_entries; i++)
                 boot_entry_free(config->entries + i);
         free(config->entries);
+        free(config->global_addons.arr);
 
         set_free(config->inodes_seen);
 }
@@ -962,6 +963,23 @@ static int boot_entries_find_unified_addons(
         return 0;
 }
 
+static int boot_entries_find_unified_global_addons(
+                BootConfig *config,
+                const char *root,
+                const char *d_name) {
+
+        int r;
+        _cleanup_closedir_ DIR *d = NULL;
+
+        r = chase_and_opendir(root, NULL, CHASE_PROHIBIT_SYMLINKS, NULL, &d);
+        if (r == -ENOENT)
+                return 0;
+        if (r < 0)
+                return log_error_errno(r, "Failed to open '%s/%s': %m", root, d_name);
+
+        return boot_entries_find_unified_addons(config, dirfd(d), d_name, root, &config->global_addons);
+}
+
 static int boot_entries_find_unified_local_addons(
                 BootConfig *config,
                 int d_fd,
@@ -1255,6 +1273,8 @@ int boot_config_load(
         int r;
 
         assert(config);
+        config->global_addons.arr = NULL;
+        config->global_addons.size = 0;
 
         if (esp_path) {
                 r = boot_loader_read_conf_path(config, esp_path, "/loader/loader.conf");
@@ -1266,6 +1286,10 @@ int boot_config_load(
                         return r;
 
                 r = boot_entries_find_unified(config, esp_path, "/EFI/Linux/");
+                if (r < 0)
+                        return r;
+
+                r = boot_entries_find_unified_global_addons(config, esp_path, "/loader/addons/");
                 if (r < 0)
                         return r;
         }
@@ -1437,7 +1461,10 @@ static void print_addon(
         printf("      cmdline: %s%s\n", special_glyph(SPECIAL_GLYPH_TREE_RIGHT), addon->cmdline);
 }
 
-static int print_cmdline(const BootEntry *e) {
+static int print_cmdline(
+                const BootEntry *e,
+                const BootEntryAddonArray *global_arr) {
+
         _cleanup_free_ char *final_cmdline = NULL;
 
         assert(e);
@@ -1460,6 +1487,12 @@ static int print_cmdline(const BootEntry *e) {
 
                 printf("  defCmdline: %s\n", t2);
                 final_cmdline = TAKE_PTR(t2);
+        }
+
+        FOREACH_ARRAY(addon, global_arr->arr, global_arr->size) {
+                print_addon(addon, "globalAddon");
+                if (!strextend(&final_cmdline, " ", addon->cmdline))
+                        return log_oom();
         }
 
         FOREACH_ARRAY(addon, e->local_addons.arr, e->local_addons.size) {
@@ -1492,7 +1525,11 @@ static int json_addon(
         return 0;
 }
 
-static int json_cmdline(const BootEntry *e, JsonVariant **v) {
+static int json_cmdline(
+                const BootEntry *e,
+                const BootEntryAddonArray *global_arr,
+                JsonVariant **v) {
+
         _cleanup_free_ char *final_cmdline = NULL, *def_cmdline = NULL;
         _cleanup_(json_variant_unrefp) JsonVariant *addons_array = NULL;
         int r;
@@ -1504,6 +1541,14 @@ static int json_cmdline(const BootEntry *e, JsonVariant **v) {
                 if (!def_cmdline)
                         return log_oom();
                 final_cmdline = TAKE_PTR(def_cmdline);
+        }
+
+        FOREACH_ARRAY(addon, global_arr->arr, global_arr->size) {
+                r = json_addon(addon, "globalAddon", &addons_array);
+                if (r < 0)
+                        return r;
+                if (!strextend(&final_cmdline, " ", addon->cmdline))
+                        return log_oom();
         }
 
         FOREACH_ARRAY(addon, e->local_addons.arr, e->local_addons.size) {
@@ -1526,6 +1571,7 @@ static int json_cmdline(const BootEntry *e, JsonVariant **v) {
 
 int show_boot_entry(
                 const BootEntry *e,
+                const BootEntryAddonArray *global_addons,
                 bool show_as_default,
                 bool show_as_selected,
                 bool show_reported) {
@@ -1609,7 +1655,7 @@ int show_boot_entry(
                                      *s,
                                      &status);
 
-        r = print_cmdline(e);
+        r = print_cmdline(e, global_addons);
         if (r < 0)
                 return r;
 
@@ -1657,7 +1703,7 @@ int show_boot_entries(const BootConfig *config, JsonFormatFlags json_format) {
                         if (r < 0)
                                 return log_oom();
 
-                        r = json_cmdline(e, &v);
+                        r = json_cmdline(e, &config->global_addons, &v);
                         if (r < 0)
                                 return log_oom();
 
@@ -1686,6 +1732,7 @@ int show_boot_entries(const BootConfig *config, JsonFormatFlags json_format) {
                 for (size_t n = 0; n < config->n_entries; n++) {
                         r = show_boot_entry(
                                         config->entries + n,
+                                        &config->global_addons,
                                         /* show_as_default= */  n == (size_t) config->default_entry,
                                         /* show_as_selected= */ n == (size_t) config->selected_entry,
                                         /* show_discovered= */  true);
