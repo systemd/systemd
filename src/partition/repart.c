1476e2150e487dcd31baaf119d671df74594aa2c
Daan De Meyer <daan.j.demeyer@gmail.com>
2023-09-22 13:47:14 +0200

    repart: Don't fail on boot if we can't find the root block device
    When booting from virtiofs, we won't be able to find a root block
    device. Let's gracefully handle this similar to how we don't fail
    if we can't find a GPT partition table.

--- src/partition/repart.c
+++ src/partition/repart.c
@@ -7301,6 +7301,9 @@ static int run(int argc, char *argv[]) {
                 return r;
 
         r = find_root(context);
+        if (r == -ENODEV)
+                return 76; /* Special return value which means "Root block device not found, so not doing
+                            * anything". This isn't really an error when called at boot. */
         if (r < 0)
                 return r;
 
