2d28e7cec4737f5085d133b8174695d96a74279f
Arthur Shau <arthurshau@meta.com>
2023-09-26 13:07:10 -0700

    bpfd: Add new service that pulls and logs bpf program/map information
    BPF service, a.k.a. bpfd, is a new service that’s meant to serve the
    needs of deploying, monitoring, auditing, and facilitating the  adoption
    of BPF kernel technology. It logs information to the journal periodically
    about currently running bpf programs and maps, as well as exposes a
    varlink interface to retrieve that information on demand.

---
+++ src/bpf/bpfd-manager.h
@@ -0,0 +1,24 @@
+/* SPDX-License-Identifier: LGPL-2.1+ */
+#pragma once
+
+#include "sd-event.h"
+
+#include "varlink.h"
+
+/* Polling interval for BPF timer  */
+#define BPF_TIMER_INTERVAL_USEC USEC_PER_MINUTE /* 1 minute */
+#define BPFD_VARLINK_ADDRESS "/run/systemd/bpf/io.systemd.Bpf"
+
+typedef struct Manager {
+        sd_event *event;
+        sd_event_source *bpf_timer_event_source;
+
+        bool enable_logging;
+
+        VarlinkServer *varlink_server;
+} Manager;
+
+int manager_new(Manager **ret);
+Manager* manager_free(Manager *m);
+int manager_start(Manager *m);
+DEFINE_TRIVIAL_CLEANUP_FUNC(Manager*, manager_free);
