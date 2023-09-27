1277575408493eb67e04be6f3f8187d67933e7ff
Emanuele Giuseppe Esposito <eesposit@redhat.com>
2023-09-27 04:28:45 -0400

    ukify: override default option value with config file
    If an option like SecureBootCertificateDir is given, it should override
    the default '/etc/pki/pesign'. Until now the config file option were
    always ignored if they had a default.
    
    So from now on, every ConfigItem with a config_key and default field
    should also give config_push = ConfigItem.config_set.

--- src/ukify/ukify.py
+++ src/ukify/ukify.py
@@ -1037,6 +1037,19 @@ class ConfigItem:
             setattr(namespace, dest, value)
 
     @staticmethod
+    def config_set(
+            namespace: argparse.Namespace,
+            group: Optional[str],
+            dest: str,
+            value: Any,
+    ) -> None:
+        "Set namespace.<dest> to value only if it was None"
+
+        assert not group
+
+        setattr(namespace, dest, value)
+
+    @staticmethod
     def config_set_group(
             namespace: argparse.Namespace,
             group: Optional[str],
@@ -1300,6 +1313,7 @@ CONFIG_ITEMS = [
         default = '/etc/pki/pesign',
         help = 'required by --signtool=pesign. Path to nss certificate database directory for PE signing. Default is /etc/pki/pesign',
         config_key = 'UKI/SecureBootCertificateDir',
+        config_push = ConfigItem.config_set
     ),
     ConfigItem(
         '--secureboot-certificate-name',
@@ -1314,6 +1328,7 @@ CONFIG_ITEMS = [
         default = 365 * 10,
         help = "period of validity (in days) for a certificate created by 'genkey'",
         config_key = 'UKI/SecureBootCertificateValidity',
+        config_push = ConfigItem.config_set
     ),
 
     ConfigItem(
