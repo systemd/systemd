63a06193f1caf823408c6cb05a2a059b3d0223c6
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-09-28 06:04:53 +0900

    journalctl: find boot ID more gracefully in corrupted journal
    In discover_next_boot(), first we find a new boot ID based on the value
    stored in the entry object. Then, find the tail (or head when we are going
    upwards) entry of the boot based on the _BOOT_ID= field data.
    
    If boot IDs of an entry in the entry object and _BOOT_ID field data
    are inconsistent, which may happen on corrupted journal, then previously
    discover_next_boot() failed with -ENODATA.
    
    This makes the function check if the two boot IDs in each entry are
    consistent, and skip the entry if not.
    
    Fixes the failure of `journalctl -b -1` for 'truncated' journal:
    https://github.com/systemd/systemd/pull/29334#issuecomment-1736567951

--- test/units/testsuite-04.journal.sh
+++ test/units/testsuite-04.journal.sh
@@ -233,6 +233,6 @@ done < <(find /test-journals/no-rtc -name "*.zst")
 
 journalctl --directory="$JOURNAL_DIR" --list-boots --output=json >/tmp/lb1
 diff -u /tmp/lb1 - <<'EOF'
-[{"index":-3,"boot_id":"5ea5fc4f82a14186b5332a788ef9435e","first_entry":1666569600994371,"last_entry":1666584266223608},{"index":-2,"boot_id":"bea6864f21ad4c9594c04a99d89948b0","first_entry":1666584266731785,"last_entry":1666584347230411},{"index":-1,"boot_id":"4c708e1fd0744336be16f3931aa861fb","first_entry":1666584348378271,"last_entry":1666584354649355},{"index":0,"boot_id":"35e8501129134edd9df5267c49f744a4","first_entry":1666584356661527,"last_entry":1666584438086856}]
+[{"index":-3,"boot_id":"5ea5fc4f82a14186b5332a788ef9435e","first_entry":1666569600994371,"last_entry":1666584266223608},{"index":-2,"boot_id":"bea6864f21ad4c9594c04a99d89948b0","first_entry":1666569601005945,"last_entry":1666584347230411},{"index":-1,"boot_id":"4c708e1fd0744336be16f3931aa861fb","first_entry":1666584293354007,"last_entry":1666584354649355},{"index":0,"boot_id":"35e8501129134edd9df5267c49f744a4","first_entry":1666569601009823,"last_entry":1666584438086856}]
 EOF
 rm -rf "$JOURNAL_DIR" /tmp/lb1
