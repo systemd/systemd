91bf541120e4909e666c7a2cb8089ac38e461d93
Lennart Poettering <lennart@poettering.net>
2023-09-29 18:55:31 +0200

    pe-binary: actually check if PE binary is UEFI binary when determining if UKI
    If it's not a UEFI binary, then it's not a UKI.

--- src/shared/pe-binary.c
+++ src/shared/pe-binary.c
@@ -231,6 +231,9 @@ bool pe_is_uki(const PeHeader *pe_header, const IMAGE_SECTION_HEADER *sections) 
         assert(pe_header);
         assert(sections || le16toh(pe_header->pe.NumberOfSections) == 0);
 
+        if (le16toh(pe_header->optional.Subsystem) != IMAGE_SUBSYSTEM_EFI_APPLICATION)
+                return false;
+
         return
                 pe_header_find_section(pe_header, sections, ".osrel") &&
                 pe_header_find_section(pe_header, sections, ".linux") &&
