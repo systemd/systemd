b42ecf3efe9dd211e01962042f15fd33efeca06d
Vladimir Stoiakin <VStoiakin@lavabit.com>
2023-10-02 16:37:26 +0300

    cryptenroll: allow to use a public key on a token
    This patch allows systemd-cryptenroll to enroll directly with a public key if a certificate is missing on a token.
    
    Fixes: #30675

--- src/home/homectl-pkcs11.h
+++ src/home/homectl-pkcs11.h
@@ -5,7 +5,13 @@
 
 int identity_add_token_pin(JsonVariant **v, const char *pin);
 
+#if HAVE_P11KIT
 int identity_add_pkcs11_key_data(JsonVariant **v, const char *token_uri);
+#else
+static inline int identity_add_pkcs11_key_data(JsonVariant **v, const char *token_uri) {
+        return log_error_errno(SYNTHETIC_ERRNO(EOPNOTSUPP), "PKCS#11 tokens not supported on this build.");
+}
+#endif
 
 int list_pkcs11_tokens(void);
 int find_pkcs11_token_auto(char **ret);
