d13a6729414310bd59ab8530317653072c5e845d
Luca Boccassi <bluca@debian.org>
2023-10-02 23:35:26 +0100

    serialize: add serialize_image_policy()

--- src/shared/serialize.c
+++ src/shared/serialize.c
@@ -218,6 +218,27 @@ int serialize_string_set(FILE *f, const char *key, Set *s) {
         return 1;
 }
 
+int serialize_image_policy(FILE *f, const char *key, const ImagePolicy *p) {
+        _cleanup_free_ char *policy = NULL;
+        int r;
+
+        assert(f);
+        assert(key);
+
+        if (!p)
+                return 0;
+
+        r = image_policy_to_string(p, false, &policy);
+        if (r < 0)
+                return r;
+
+        r = serialize_item(f, key, policy);
+        if (r < 0)
+                return r;
+
+        return 1;
+}
+
 int deserialize_read_line(FILE *f, char **ret) {
         _cleanup_free_ char *line = NULL;
         int r;
