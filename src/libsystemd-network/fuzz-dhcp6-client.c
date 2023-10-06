0cbdb21f203f20018c0840babef1c86b5d55f078
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-10-06 13:26:16 +0900

    fuzz: suppress log messages

--- src/libsystemd-network/fuzz-dhcp6-client.c
+++ src/libsystemd-network/fuzz-dhcp6-client.c
@@ -75,6 +75,9 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
 
         assert_se(setenv("SYSTEMD_NETWORK_TEST_MODE", "1", 1) >= 0);
 
+        if (!getenv("SYSTEMD_LOG_LEVEL"))
+                log_set_max_level(LOG_CRIT);
+
         if (outside_size_range(size, 0, 65536))
                 return 0;
 
