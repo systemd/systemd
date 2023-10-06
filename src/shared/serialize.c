1c22995bc61848eb88787dcda2be9f0937ad1807
Lennart Poettering <lennart@poettering.net>
2023-10-06 17:56:30 +0200

    serialize: change order of deserialize_strv() parameters
    The other deserializers put value first, and return parameter second,
    let's do so here too.

--- src/shared/serialize.c
+++ src/shared/serialize.c
@@ -302,7 +302,7 @@ int deserialize_fd(FDSet *fds, const char *value) {
         return TAKE_FD(our_fd);
 }
 
-int deserialize_strv(char ***l, const char *value) {
+int deserialize_strv(const char *value, char ***l) {
         ssize_t unescaped_len;
         char *unescaped;
 
