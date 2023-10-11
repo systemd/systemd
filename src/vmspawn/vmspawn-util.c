fc2476205e4299136a005d49310c945753020bc0
Sam Leonard <sam.leonard@codethink.co.uk>
2023-10-11 10:21:25 +0100

    vmspawn: fixed issues from review

--- src/vmspawn/vmspawn-util.c
+++ src/vmspawn/vmspawn-util.c
@@ -3,25 +3,24 @@
 #include "log.h"
 #include "macro-fundamental.h"
 #include "path-util.h"
+#include "string-util.h"
 #include "strv.h"
 #include "vmspawn-util.h"
 #include <stdio.h>
 
 bool qemu_check_kvm_support(void) {
-        _cleanup_close_ int fd = -EBADF;
-        fd = open("/dev/kvm", O_RDONLY | O_CLOEXEC);
-        if (fd >= 0)
+        int r;
+        r = access("/dev/kvm", F_OK);
+        if (r == 0)
                 return true;
 
         switch (errno) {
         case ENOENT:
-                log_warning_errno(errno, "/dev/kvm not found. Not using KVM acceleration.");
+                log_debug_errno(errno, "/dev/kvm not found. Not using KVM acceleration.");
                 break;
 
-        case EPERM:
-                _fallthrough_;
         case EACCES:
-                log_warning_errno(errno, "Permission denied to access /dev/kvm. Not using KVM acceleration.");
+                log_debug_errno(errno, "Permission denied to access /dev/kvm. Not using KVM acceleration.");
                 break;
         }
 
@@ -38,9 +37,7 @@ int find_ovmf_firmware(const char **ret_firmware_path) {
 #endif
 
         FOREACH_STRING(firmware, FIRMWARE_LOCATIONS) {
-                _cleanup_close_ int fd = -EBADF;
-                fd = open(firmware, O_CLOEXEC | O_RDONLY);
-                if (fd >= 0) {
+                if (access(firmware, F_OK) == 0) {
                         if (ret_firmware_path)
                                 *ret_firmware_path = firmware;
                         return 0;
@@ -61,12 +58,7 @@ int find_ovmf_firmware(const char **ret_firmware_path) {
 #endif
 
         FOREACH_STRING(firmware, FIRMWARE_LOCATIONS) {
-                _cleanup_close_ int fd = -EBADF;
-                fd = open(firmware, O_CLOEXEC | O_RDONLY);
-                if (fd >= 0) {
-                        log_warning("Couldn't find OVMF firmware blob with secure boot support, "
-                                    "falling back to OVMF firmware blobs without secure boot support.");
-
+                if (access(firmware, F_OK) == 0) {
                         if (ret_firmware_path)
                                 *ret_firmware_path = firmware;
                         return 1;
@@ -82,9 +74,7 @@ int find_ovmf_firmware(const char **ret_firmware_path) {
                 "/usr/share/OVMF/OVMF_CODE.secboot.fd"
 
         FOREACH_STRING(firmware, FIRMWARE_LOCATIONS) {
-                _cleanup_close_ int fd = -EBADF;
-                fd = open(firmware, O_CLOEXEC | O_RDONLY);
-                if (fd >= 0) {
+                if (access(firmware, F_OK) == 0) {
                         if (ret_firmware_path)
                                 *ret_firmware_path = firmware;
                         return 0;
@@ -100,12 +90,7 @@ int find_ovmf_firmware(const char **ret_firmware_path) {
                 "/usr/share/OVMF/OVMF_CODE.fd"
 
         FOREACH_STRING(firmware, FIRMWARE_LOCATIONS) {
-                _cleanup_close_ int fd = -EBADF;
-                fd = open(firmware, O_CLOEXEC | O_RDONLY);
-                if (fd >= 0) {
-                        log_warning("Couldn't find OVMF firmware blob with secure boot support, "
-                                    "falling back to OVMF firmware blobs without secure boot support.");
-
+                if (access(firmware, F_OK) == 0) {
                         if (ret_firmware_path)
                                 *ret_firmware_path = firmware;
                         return 1;
@@ -113,27 +98,31 @@ int find_ovmf_firmware(const char **ret_firmware_path) {
         }
 
 #undef FIRMWARE_LOCATIONS
-        if (ret_firmware_path)
-                *ret_firmware_path = NULL;
-        log_error("Couldn't find OVMF UEFI firmware blob.");
         return -ENOENT;
 }
 
 int find_qemu_binary(char **ret_qemu_binary) {
         int r;
 
+        /*
+         * On success the path to the qemu binary will be stored in `req_qemu_binary`
+         *
+         * If the qemu binary cannot be found -ENOENT will be returned.
+         * If the native architecture is not supported by qemu -ESRCH will be returned;
+         */
+
         static const char *architecture_to_qemu_table[_ARCHITECTURE_MAX] = {
-                [ARCHITECTURE_ARM64]       = "aarch64",
+                [ARCHITECTURE_ARM64]       = "aarch64",     /* differs from our name */
                 [ARCHITECTURE_ARM]         = "arm",
                 [ARCHITECTURE_ALPHA]       = "alpha",
-                [ARCHITECTURE_X86_64]      = "x86_64",
-                [ARCHITECTURE_X86]         = "i386",
+                [ARCHITECTURE_X86_64]      = "x86_64",      /* differs from our name */
+                [ARCHITECTURE_X86]         = "i386",        /* differs from our name */
                 [ARCHITECTURE_LOONGARCH64] = "loongarch64",
-                [ARCHITECTURE_MIPS64_LE]   = "mips",
-                [ARCHITECTURE_MIPS_LE]     = "mips",
-                [ARCHITECTURE_PARISC]      = "hppa",
-                [ARCHITECTURE_PPC64_LE]    = "ppc",
-                [ARCHITECTURE_PPC64]       = "ppc",
+                [ARCHITECTURE_MIPS64_LE]   = "mips",        /* differs from our name */
+                [ARCHITECTURE_MIPS_LE]     = "mips",        /* differs from our name */
+                [ARCHITECTURE_PARISC]      = "hppa",        /* differs from our name */
+                [ARCHITECTURE_PPC64_LE]    = "ppc",         /* differs from our name */
+                [ARCHITECTURE_PPC64]       = "ppc",         /* differs from our name */
                 [ARCHITECTURE_PPC]         = "ppc",
                 [ARCHITECTURE_RISCV32]     = "riscv32",
                 [ARCHITECTURE_RISCV64]     = "riscv64",
@@ -144,16 +133,20 @@ int find_qemu_binary(char **ret_qemu_binary) {
                 r = find_executable(s, ret_qemu_binary);
                 if (r == 0)
                         return 0;
+
+                if (r != -ENOENT)
+                        return r;
         }
 
         const char *arch_qemu = architecture_to_qemu_table[native_architecture()];
         if (!arch_qemu) {
-                log_error("Architecture %s not supported by qemu", architecture_to_string(native_architecture()));
-                return -ENOENT;
+                return log_debug_errno(SYNTHETIC_ERRNO(ESRCH), "Architecture %s not supported by qemu", architecture_to_string(native_architecture()));
         }
 
         _cleanup_free_ char *qemu_arch_specific = NULL;
-        asprintf(&qemu_arch_specific, "qemu-system-%s", arch_qemu);
+        qemu_arch_specific = strjoin("qemu-system-", arch_qemu);
+        if (!qemu_arch_specific)
+                return -ENOMEM;
 
         r = find_executable(qemu_arch_specific, ret_qemu_binary);
         if (r == 0)
@@ -181,9 +174,7 @@ int find_ovmf_vars(const char **ret_ovmf_vars) {
         "/usr/share/OVMF/OVMF_VARS.fd"
 
         FOREACH_STRING(location, OVMF_VARS_LOCATIONS , GENERIC_OVMF_VARS_LOCATIONS) {
-                _cleanup_close_ int fd = -EBADF;
-                fd = open(location, O_CLOEXEC | O_RDONLY);
-                if (fd >= 0) {
+                if (access(location, F_OK) == 0) {
                         if (ret_ovmf_vars)
                                 *ret_ovmf_vars = location;
                         return 0;
