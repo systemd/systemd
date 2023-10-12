787ef0f0ef66865bb63ac2c593b5fbd8cb4c929b
Sam Leonard <sam.leonard@codethink.co.uk>
2023-10-12 13:13:19 +0100

    vmspawn: added initial code for vmspawn
    vmspawn-settings.c is currently empty but this will be used in future to
    house code for parsing settings from a file

---
+++ src/vmspawn/vmspawn-settings.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#pragma once
+
+#include <stdint.h>
+
+typedef enum SettingsMask {
+        SETTING_START_MODE        = UINT64_C(1) << 0,
+        SETTING_DIRECTORY         = UINT64_C(1) << 26,
+        SETTING_CREDENTIALS       = UINT64_C(1) << 30,
+        _SETTING_FORCE_ENUM_WIDTH = UINT64_MAX
+} SettingsMask;
---
+++ src/vmspawn/vmspawn-util.h
@@ -0,0 +1,23 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#pragma once
+
+#include <stdbool.h>
+#include "macro.h"
+
+#if defined(__x86_64__) || defined(__i386__) || defined(__arm__) || defined(__aarch64__)
+#define ARCHITECTURE_SUPPORTS_SMBIOS
+#endif
+
+typedef struct OvmfFirmware {
+        char *path;
+        char *vars;
+        bool supports_sb;
+} OvmfConfig;
+
+OvmfConfig* ovmf_config_new(void);
+OvmfConfig* ovmf_config_free(OvmfConfig *ovmf_config);
+DEFINE_TRIVIAL_CLEANUP_FUNC(OvmfConfig*, ovmf_config_free);
+
+bool qemu_check_kvm_support(void);
+int find_ovmf_config(int search_sb, OvmfConfig **ret_ovmf_config);
+int find_qemu_binary(char **ret_qemu_binary);
