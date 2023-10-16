0ee0fac794f8a6e3f95a59ac8c4d3a6a3881da6d
Mike Yuan <me@yhndnzj.com>
2023-10-16 13:10:01 +0800

    hibernate-util: introduce hibernation_is_safe
    After 7470b80763ac0f598ca1ef73d44763967119c18d, we refuse
    to hibernate if we fail to write HibernateLocation EFI
    variable and resume= is not set. Let's teach sleep_supported
    to follow the practice too.

--- src/shared/hibernate-util.c
+++ src/shared/hibernate-util.c
@@ -14,6 +14,7 @@
 #include "btrfs-util.h"
 #include "device-util.h"
 #include "devnum-util.h"
+#include "efivars.h"
 #include "env-util.h"
 #include "errno-util.h"
 #include "fd-util.h"
@@ -402,36 +403,52 @@ int find_suitable_hibernation_device_full(HibernationDevice *ret_device, uint64_
         return resume_config_devno > 0;
 }
 
-bool enough_swap_for_hibernation(void) {
+static int get_proc_meminfo_active(unsigned long long *ret) {
         _cleanup_free_ char *active_str = NULL;
         unsigned long long active;
-        uint64_t size, used;
         int r;
+
+        r = get_proc_field("/proc/meminfo", "Active(anon)", WHITESPACE, &active_str);
+        if (r < 0)
+                return log_debug_errno(r, "Failed to retrieve Active(anon) from /proc/meminfo: %m");
+
+        r = safe_atollu(active_str, &active);
+        if (r < 0)
+                return log_debug_errno(r, "Failed to parse Active(anon) '%s' from /proc/meminfo: %m", active_str);
+
+        *ret = active;
+        return 0;
+}
+
+int hibernation_is_safe(void) {
+        unsigned long long active;
+        uint64_t size, used;
+        bool resume_set;
+        int r;
+
+        r = find_suitable_hibernation_device_full(NULL, &size, &used);
+        if (r < 0)
+                return r;
+        resume_set = r > 0;
+
+        if (!resume_set && !is_efi_boot())
+                return log_debug_errno(SYNTHETIC_ERRNO(ENOTRECOVERABLE),
+                                       "Not running on EFI and resume= is not set. Hibernation is not safe.");
 
         if (getenv_bool("SYSTEMD_BYPASS_HIBERNATION_MEMORY_CHECK") > 0)
                 return true;
 
-        r = find_suitable_hibernation_device_full(NULL, &size, &used);
+        r = get_proc_meminfo_active(&active);
         if (r < 0)
-                return false;
-
-        r = get_proc_field("/proc/meminfo", "Active(anon)", WHITESPACE, &active_str);
-        if (r < 0) {
-                log_debug_errno(r, "Failed to retrieve Active(anon) from /proc/meminfo: %m");
-                return false;
-        }
-
-        r = safe_atollu(active_str, &active);
-        if (r < 0) {
-                log_debug_errno(r, "Failed to parse Active(anon) '%s' from /proc/meminfo: %m", active_str);
-                return false;
-        }
+                return r;
 
         r = active <= (size - used) * HIBERNATION_SWAP_THRESHOLD;
         log_debug("Detected %s swap for hibernation: Active(anon)=%llu kB, size=%" PRIu64 " kB, used=%" PRIu64 " kB, threshold=%.2g%%",
                   r ? "enough" : "not enough", active, size, used, 100 * HIBERNATION_SWAP_THRESHOLD);
+        if (!r)
+                return -ENOSPC;
 
-        return r;
+        return resume_set;
 }
 
 int write_resume_config(dev_t devno, uint64_t offset, const char *device) {
