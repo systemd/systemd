1dcd484999745d08d096da698a374937083f83aa
Lennart Poettering <lennart@poettering.net>
2023-10-19 17:04:04 +0200

    iovec-util: add some useful helpers for dealing with iovecs that refer to dynamic memory

--- src/basic/iovec-util.h
+++ src/basic/iovec-util.h
@@ -5,6 +5,7 @@
 #include <sys/types.h>
 #include <sys/uio.h>
 
+#include "alloc-util.h"
 #include "macro.h"
 
 size_t iovec_total_size(const struct iovec *i, size_t n);
@@ -19,6 +20,27 @@ bool iovec_increment(struct iovec *i, size_t n, size_t k);
                 const char *_s = (string);              \
                 IOVEC_MAKE((char*) _s, strlen(_s));     \
         })
+
+#define TAKE_IOVEC(p) TAKE_GENERIC((p), struct iovec, IOVEC_NULL)
+
+static inline void iovec_done(struct iovec *iovec) {
+        /* A _cleanup_() helper that frees the iov_base in the iovec */
+        assert(iovec);
+
+        iovec->iov_base = mfree(iovec->iov_base);
+        iovec->iov_len = 0;
+}
+
+static inline void iovec_done_erase(struct iovec *iovec) {
+        assert(iovec);
+
+        iovec->iov_base = erase_and_free(iovec->iov_base);
+        iovec->iov_len = 0;
+}
+
+static inline bool iovec_is_set(const struct iovec *iov) {
+        return iov && iov->iov_len > 0 && iov->iov_base;
+}
 
 char* set_iovec_string_field(struct iovec *iovec, size_t *n_iovec, const char *field, const char *value);
 char* set_iovec_string_field_free(struct iovec *iovec, size_t *n_iovec, const char *field, char *value);
