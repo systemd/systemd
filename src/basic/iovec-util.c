7b899304ac347b59941b18553c78dc757d6b6326
Lennart Poettering <lennart@poettering.net>
2023-10-19 16:36:43 +0200

    iovec-util: make IOVEC_INCREMENT a regular function too
    Even more than with the previous commit, this is not a trivial function
    and there's no reason to believe this will actually be inlined nor that
    it would be beneficial.

--- src/basic/iovec-util.c
+++ src/basic/iovec-util.c
@@ -14,6 +14,29 @@ size_t iovec_total_size(const struct iovec *i, size_t n) {
         return sum;
 }
 
+bool iovec_increment(struct iovec *i, size_t n, size_t k) {
+        /* Returns true if there is nothing else to send (bytes written cover all of the iovec),
+         * false if there's still work to do. */
+
+        for (size_t j = 0; j < n; j++) {
+                size_t sub;
+
+                if (i[j].iov_len == 0)
+                        continue;
+                if (k == 0)
+                        return false;
+
+                sub = MIN(i[j].iov_len, k);
+                i[j].iov_len -= sub;
+                i[j].iov_base = (uint8_t*) i[j].iov_base + sub;
+                k -= sub;
+        }
+
+        assert(k == 0); /* Anything else would mean that we wrote more bytes than available,
+                         * or the kernel reported writing more bytes than sent. */
+        return true;
+}
+
 char* set_iovec_string_field(struct iovec *iovec, size_t *n_iovec, const char *field, const char *value) {
         char *x;
 
