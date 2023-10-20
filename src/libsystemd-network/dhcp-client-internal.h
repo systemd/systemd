78fd8c6bba2d2a1d11a3a28f1aaa6839269e63aa
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-10-21 00:46:39 +0900

    dhcp: move DHCPState to dhcp-client-internal.h
    All other definitions in dhcp-protocol.h are common for client and
    server. Let's move DHCP client specific definitions.

--- src/libsystemd-network/dhcp-client-internal.h
+++ src/libsystemd-network/dhcp-client-internal.h
@@ -1,7 +1,27 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 #pragma once
 
+#include <errno.h>
+
 #include "sd-dhcp-client.h"
+
+#include "macro.h"
+
+typedef enum DHCPState {
+        DHCP_STATE_STOPPED,
+        DHCP_STATE_INIT,
+        DHCP_STATE_SELECTING,
+        DHCP_STATE_INIT_REBOOT,
+        DHCP_STATE_REBOOTING,
+        DHCP_STATE_REQUESTING,
+        DHCP_STATE_BOUND,
+        DHCP_STATE_RENEWING,
+        DHCP_STATE_REBINDING,
+        _DHCP_STATE_MAX,
+        _DHCP_STATE_INVALID                     = -EINVAL,
+} DHCPState;
+
+const char *dhcp_state_to_string(DHCPState s) _const_;
 
 extern const struct hash_ops dhcp_option_hash_ops;
 
