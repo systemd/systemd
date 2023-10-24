a70c27b834bfc712340c2fa3de9ec3b13e6662b1
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-10-24 18:25:47 +0900

    network: make link_save() static

--- src/network/networkd-state-file.h
+++ src/network/networkd-state-file.h
@@ -6,7 +6,6 @@ typedef struct Manager Manager;
 
 void link_dirty(Link *link);
 void link_clean(Link *link);
-int link_save(Link *link);
 int link_save_and_clean(Link *link);
 
 int manager_save(Manager *m);
