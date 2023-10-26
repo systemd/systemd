73c32bdc257f4d0cf0a0c2afbcb3ae2c5ab124e5
Mate Kukri <mate.kukri@canonical.com>
2023-10-26 13:48:04 +0100

    {stub,boot}: Store PE sections in binary-searchable sorted array
    This is in preparation for allowing multiple sections with the same
    name.

--- src/boot/efi/pe.h
+++ src/boot/efi/pe.h
@@ -3,17 +3,26 @@
 
 #include "efi.h"
 
-EFI_STATUS pe_memory_locate_sections(
-                const void *base,
-                const char * const sections[],
-                size_t *addrs,
-                size_t *sizes);
+EFI_STATUS pe_kernel_info(const void *base, uint32_t *ret_compat_address);
+
+struct PeSectionDescriptor {
+  char name[9];
+  size_t offset;
+  size_t size;
+};
 
 EFI_STATUS pe_file_locate_sections(
                 EFI_FILE *dir,
                 const char16_t *path,
-                const char * const sections[],
-                size_t *offsets,
-                size_t *sizes);
+                struct PeSectionDescriptor **out_sections,
+                size_t *out_nsections);
 
-EFI_STATUS pe_kernel_info(const void *base, uint32_t *ret_compat_address);
+EFI_STATUS pe_memory_locate_sections(
+                const void *base,
+                struct PeSectionDescriptor **out_sections,
+                size_t *out_nsections);
+
+struct PeSectionDescriptor *pe_bsearch_section(
+                struct PeSectionDescriptor *sections,
+                size_t nsections,
+                const char *section_name);
