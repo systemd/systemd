27cf94b600014102d1eadd9ab34bbd9c46182aed
David Tardon <dtardon@redhat.com>
2023-10-27 13:13:05 +0200

    udev-connection: connect to varlink too
    UdevConnection now connects to varlink in addition to the legacy
    protocol, as we need both until the conversion to varlink is finished.

--- src/udev/udev-connection.c
+++ src/udev/udev-connection.c
@@ -1,9 +1,14 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 
 #include "udev-connection.h"
+#include "udev-varlink.h"
 
 int udev_connection_init(UdevConnection *conn) {
         int r;
+
+        r = udev_varlink_connect(&conn->link);
+        if (r < 0)
+                return log_error_errno(r, "Failed to initialize varlink connection: %m");
 
         r = udev_ctrl_new(&conn->uctrl);
         if (r < 0)
@@ -16,5 +21,6 @@ void udev_connection_done(UdevConnection *conn) {
         if (!conn)
                 return;
 
+        conn->link = varlink_flush_close_unref(conn->link);
         conn->uctrl = udev_ctrl_unref(conn->uctrl);
 }
