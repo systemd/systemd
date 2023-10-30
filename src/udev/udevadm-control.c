415feaafff0ab26b71dbe6ea9ad0976bea3f9e7a
David Tardon <dtardon@redhat.com>
2023-10-30 16:46:14 +0100

    udev: initialize either varlink or legacy protocol
    ... but never both of them.

--- src/udev/udevadm-control.c
+++ src/udev/udevadm-control.c
@@ -31,7 +31,7 @@
 
 static int send_reload(UdevConnection *conn) {
         assert(conn);
-        assert(conn->link || conn->uctrl);
+        assert((bool) conn->link ^ (bool) conn->uctrl);
 
         if (!conn->link)
                 return udev_ctrl_send_reload(conn->uctrl);
@@ -44,7 +44,7 @@ static int send_set_log_level(UdevConnection *conn, int level) {
         int r;
 
         assert(conn);
-        assert(conn->link || conn->uctrl);
+        assert((bool) conn->link ^ (bool) conn->uctrl);
 
         if (!conn->link)
                 return udev_ctrl_send_set_log_level(conn->uctrl, level);
@@ -58,7 +58,7 @@ static int send_set_log_level(UdevConnection *conn, int level) {
 
 static int send_stop_exec_queue(UdevConnection *conn) {
         assert(conn);
-        assert(conn->link || conn->uctrl);
+        assert((bool) conn->link ^ (bool) conn->uctrl);
 
         if (!conn->link)
                 return udev_ctrl_send_stop_exec_queue(conn->uctrl);
@@ -68,7 +68,7 @@ static int send_stop_exec_queue(UdevConnection *conn) {
 
 static int send_start_exec_queue(UdevConnection *conn) {
         assert(conn);
-        assert(conn->link || conn->uctrl);
+        assert((bool) conn->link ^ (bool) conn->uctrl);
 
         if (!conn->link)
                 return udev_ctrl_send_start_exec_queue(conn->uctrl);
@@ -81,7 +81,7 @@ static int send_set_env(UdevConnection *conn, const char *env) {
         int r;
 
         assert(conn);
-        assert(conn->link || conn->uctrl);
+        assert((bool) conn->link ^ (bool) conn->uctrl);
 
         if (!conn->link)
                 return udev_ctrl_send_set_env(conn->uctrl, env);
@@ -98,7 +98,7 @@ static int send_set_children_max(UdevConnection *conn, unsigned n) {
         int r;
 
         assert(conn);
-        assert(conn->link || conn->uctrl);
+        assert((bool) conn->link ^ (bool) conn->uctrl);
 
         if (!conn->link)
                 return udev_ctrl_send_set_children_max(conn->uctrl, n);
@@ -112,7 +112,7 @@ static int send_set_children_max(UdevConnection *conn, unsigned n) {
 
 static int send_exit(UdevConnection *conn) {
         assert(conn);
-        assert(conn->link || conn->uctrl);
+        assert((bool) conn->link ^ (bool) conn->uctrl);
 
         if (!conn->link)
                 return udev_ctrl_send_exit(conn->uctrl);
