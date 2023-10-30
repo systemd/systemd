b5d06ca49c726f9474bd95e69cd7506c7364ea92
David Tardon <dtardon@redhat.com>
2023-10-30 13:04:11 +0100

    varlink: define interface io.systemd.udev

---
+++ src/shared/varlink-io.systemd.udev.c
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include "varlink-io.systemd.udev.h"
+
+VARLINK_DEFINE_INTERFACE(
+                io_systemd_udev,
+                "io.systemd.udev");
