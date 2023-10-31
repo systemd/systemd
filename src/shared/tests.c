678fbf7eef9e6ef9865c7845ab4b9983f754e208
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-10-31 14:46:38 +0900

    tests: make log_tests_skipped() and friend accept format string

--- src/shared/tests.c
+++ src/shared/tests.c
@@ -118,18 +118,6 @@ void test_setup_logging(int level) {
         log_open();
 }
 
-int log_tests_skipped(const char *message) {
-        log_notice("%s: %s, skipping tests.",
-                   program_invocation_short_name, message);
-        return EXIT_TEST_SKIP;
-}
-
-int log_tests_skipped_errno(int r, const char *message) {
-        log_notice_errno(r, "%s: %s, skipping tests: %m",
-                         program_invocation_short_name, message);
-        return EXIT_TEST_SKIP;
-}
-
 int write_tmpfile(char *pattern, const char *contents) {
         _cleanup_close_ int fd = -EBADF;
 
