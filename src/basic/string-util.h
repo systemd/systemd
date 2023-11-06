4ca780957b34e98bf0a00c49981619c572fc2a14
Lennart Poettering <lennart@poettering.net>
2023-11-06 11:33:51 +0100

    string-util: add strdup_or_null() helper

--- src/basic/string-util.h
+++ src/basic/string-util.h
@@ -301,3 +301,24 @@ bool version_is_valid(const char *s);
 bool version_is_valid_versionspec(const char *s);
 
 ssize_t strlevenshtein(const char *x, const char *y);
+
+static inline int strdup_or_null(const char *s, char **ret) {
+        char *c;
+
+        assert(ret);
+
+        /* This is a lot like strdup(), but is happy with NULL strings, and does not treat that as error, but
+         * copies the NULL value. */
+
+        if (!s) {
+                *ret = NULL;
+                return 0;
+        }
+
+        c = strdup(s);
+        if (!c)
+                return -ENOMEM;
+
+        *ret = c;
+        return 1;
+}
