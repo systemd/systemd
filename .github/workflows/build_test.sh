b475e1e6e05659e8a442237e266bfb96d4df4ae3
Luca Boccassi <bluca@debian.org>
2023-11-08 18:41:09 +0000

    ci: add -Dutmp=false coverage

--- .github/workflows/build_test.sh
+++ .github/workflows/build_test.sh
@@ -9,7 +9,7 @@ success() { echo >&2 -e "\033[32;1m$1\033[0m"; }
 
 ARGS=(
     "--optimization=0 -Dopenssl=disabled -Dcryptolib=gcrypt -Ddns-over-tls=gnutls -Dtpm=true -Dtpm2=enabled"
-    "--optimization=s"
+    "--optimization=s -Dutmp=false"
     "--optimization=3 -Db_lto=true -Ddns-over-tls=false"
     "--optimization=3 -Db_lto=false -Dtpm2=disabled -Dlibfido2=false -Dp11kit=disabled"
     "--optimization=3 -Ddns-over-tls=openssl"
