485354627576be3a8e81c9b3ae7e3028d09b2a7a
Luca Boccassi <bluca@debian.org>
2023-11-10 13:10:13 +0000

    selinux: change fd check into assertion

--- src/shared/selinux-util.c
+++ src/shared/selinux-util.c
@@ -585,8 +585,7 @@ int mac_selinux_create_file_prepare_at(
         _cleanup_free_ char *abspath = NULL;
         int r;
 
-        if (dir_fd < 0 && dir_fd != AT_FDCWD)
-                return -EBADF;
+        assert(dir_fd >= 0 || dir_fd == AT_FDCWD);
 
         r = selinux_init(/* is_lazy= */ true);
         if (r <= 0)
