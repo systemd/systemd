79378b29f87e7e7b8e313174087795ae139fc576
Florian Schmaus <flo@geekplace.eu>
2023-11-11 11:12:49 +0100

    systemctl-show: only show current swap if ever swapped or non-zero
    This reduces the noise on swapless systems, that would userwise always show a
    swap value that never has the chance to become non-zero. It further reduces the
    noise for services that never swapped.

--- src/systemctl/systemctl-show.c
+++ src/systemctl/systemctl-show.c
@@ -705,11 +705,11 @@ static void print_status_info(
         if (i->memory_current != UINT64_MAX) {
                 printf("     Memory: %s", FORMAT_BYTES(i->memory_current));
 
-                bool show_memory_swap_current = !IN_SET(i->memory_swap_current, 0, CGROUP_LIMIT_MAX),
-                        show_memory_swap_peak = !IN_SET(i->memory_swap_peak, 0, CGROUP_LIMIT_MAX);
+                /* Only show current swap if it ever was non-zero or is currently non-zero. In both cases
+                   memory_swap_peak will be non-zero (and not CGROUP_LIMIT_MAX). */
+                bool show_memory_swap = !IN_SET(i->memory_swap_peak, 0, CGROUP_LIMIT_MAX);
                 if (i->memory_peak != CGROUP_LIMIT_MAX ||
-                    show_memory_swap_current ||
-                    show_memory_swap_peak ||
+                    show_memory_swap ||
                     i->memory_min > 0 ||
                     i->memory_low > 0 || i->startup_memory_low > 0 ||
                     i->memory_high != CGROUP_LIMIT_MAX || i->startup_memory_high != CGROUP_LIMIT_MAX ||
@@ -777,11 +777,11 @@ static void print_status_info(
                                 printf("%speak: %s", prefix, FORMAT_BYTES(i->memory_peak));
                                 prefix = " ";
                         }
-                        if (show_memory_swap_current) {
+                        if (show_memory_swap) {
                                 printf("%sswap: %s", prefix, FORMAT_BYTES(i->memory_swap_current));
                                 prefix = " ";
                         }
-                        if (show_memory_swap_peak) {
+                        if (show_memory_swap) {
                                 printf("%sswap peak: %s", prefix, FORMAT_BYTES(i->memory_swap_peak));
                                 prefix = " ";
                         }
