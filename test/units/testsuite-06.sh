eed73eaeda144999d26802a0c7b79be1e3a24d09
Frantisek Sumsal <frantisek@sumsal.cz>
2023-11-14 10:52:24 +0100

    test: switch SELinux to permissive in the config file
    The config file has (unfortunately) precedence over the kernel command
    line, so let's tweak the config file if necessary.

--- test/units/testsuite-06.sh
+++ test/units/testsuite-06.sh
@@ -3,12 +3,6 @@
 set -eux
 set -o pipefail
 
-echo 1 >/sys/fs/selinux/enforce || {
-    echo "Can't make selinux enforcing, skipping test"
-    touch /testok
-    exit
-}
-
 runcon -t systemd_test_start_t systemctl start hola
 runcon -t systemd_test_reload_t systemctl reload hola
 runcon -t systemd_test_stop_t systemctl stop hola
