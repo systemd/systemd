f2e08cc79fa9a56e1f416224e4244d199d917aa0
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2023-11-15 15:24:34 +0100

    hwdb/acpi-update.py: streamline python code
    Use f-strings and simplify the code a bit.
    
    When I call 'acpi-update.py' after those changes, the resulting .hwdb files are
    the same except for two additions that appeared in the meantime. I don't think
    it makes sense to update them again, because the ma-*.txt files changed and we
    don't want to store big blobs unnecessarilly.

--- hwdb.d/acpi-update.py
+++ hwdb.d/acpi-update.py
@@ -2,31 +2,25 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
 from csv import reader
-from enum import Enum
 
-def read_table(a):
+# pylint: disable=consider-using-with
 
-    table = []
-
-    with open(a, newline='') as csvfile:
-        for row in reader(csvfile):
-            if row[0] == "Company":
-                # Skip header
-                continue
-            table.append(row)
-
+def read_table(filename):
+    table = list(reader(open(filename, newline='')))
+    table = table[1:]  # Skip header
     table.sort(key=lambda x: x[1])
 
     for row in table:
         # Some IDs end with whitespace, while they didn't in the old HTML table, so it's probably
         # a mistake, strip it.
-        print("\nacpi:{0}*:\n ID_VENDOR_FROM_DATABASE={1}".format(row[1].strip(), row[0].strip()))
+        print(f'\nacpi:{row[1].strip()}*:\n ID_VENDOR_FROM_DATABASE={row[0].strip()}')
 
-print('# This file is part of systemd.\n'
-      '#\n'
-      '# Data imported from:\n'
-      '#     https://uefi.org/uefi-pnp-export\n'
-      '#     https://uefi.org/uefi-acpi-export')
+print('''\
+# This file is part of systemd.
+#
+# Data imported from:
+#     https://uefi.org/uefi-pnp-export
+#     https://uefi.org/uefi-acpi-export''')
 
 read_table('acpi_id_registry.csv')
 read_table('pnp_id_registry.csv')
