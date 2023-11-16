07aef3c9f07663de8461eb7805de39c4a62347cf
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-11-16 10:48:37 +0900

    log: introduce a knob to make assert_return() critical
    These can be used to check if we trigger assert_return()
    unexpectedly.
    
    Co-authored-by: Frantisek Sumsal <frantisek@sumsal.cz>

--- src/basic/log.h
+++ src/basic/log.h
@@ -331,6 +331,9 @@ void log_set_open_when_needed(bool b);
  * stderr, the console or kmsg */
 void log_set_prohibit_ipc(bool b);
 
+void log_set_assert_return_is_critical(bool b);
+bool log_get_assert_return_is_critical(void) _pure_;
+
 int log_dup_console(void);
 
 int log_syntax_internal(
