a50135ec56c2e41ca598ebe0a2b5f7f1cab07553
Mike Yuan <me@yhndnzj.com>
2023-11-17 22:17:06 +0800

    hibernate-util: report no available swap as no enough swap space
    Otherwise, ENOENT or ENXIO may be directly returned as error through
    bus.
    
    Should help to generate clearer error message for
    https://bugs.debian.org/1056135

--- src/shared/hibernate-util.c
+++ src/shared/hibernate-util.c
@@ -349,7 +349,7 @@ int find_suitable_hibernation_device_full(HibernationDevice *ret_device, uint64_
         if (r < 0)
                 return r;
         if (entries.n_swaps == 0)
-                return log_debug_errno(SYNTHETIC_ERRNO(ENOENT), "No swap space available for hibernation.");
+                return log_debug_errno(SYNTHETIC_ERRNO(ENOSR), "No swap space available for hibernation.");
 
         FOREACH_ARRAY(swap, entries.swaps, entries.n_swaps) {
                 r = swap_entry_get_resume_config(swap);
@@ -385,7 +385,7 @@ int find_suitable_hibernation_device_full(HibernationDevice *ret_device, uint64_
         if (!entry) {
                 /* No need to check n_swaps == 0, since it's rejected early */
                 assert(resume_config_devno > 0);
-                return log_debug_errno(SYNTHETIC_ERRNO(ENXIO), "Cannot find swap entry corresponding to /sys/power/resume.");
+                return log_debug_errno(SYNTHETIC_ERRNO(ENOSR), "Cannot find swap entry corresponding to /sys/power/resume.");
         }
 
         if (ret_device)
@@ -429,6 +429,9 @@ int hibernation_is_safe(void) {
         int r;
 
         r = find_suitable_hibernation_device_full(NULL, &size, &used);
+        if (r == -ENOSR)
+                /* sleep_supported translates ENOSPC to SLEEP_NOT_ENOUGH_SWAP_SPACE */
+                return -ENOSPC;
         if (r < 0)
                 return r;
         resume_set = r > 0;
