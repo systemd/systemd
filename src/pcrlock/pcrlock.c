57d1938a06c38c89b4ef5cdf2e2b719a48e48e83
Lennart Poettering <lennart@poettering.net>
2023-11-20 14:26:18 +0100

    pcrlock: check for embedded NUL bytes

--- src/pcrlock/pcrlock.c
+++ src/pcrlock/pcrlock.c
@@ -1125,6 +1125,11 @@ static int event_log_load_userspace(EventLog *el) {
 
                 b[bn] = 0; /* Turn it into a string */
 
+                if (memchr(b, 0, bn)) {
+                        log_warning("Found record with embedded NUL byte, skipping.");
+                        continue;
+                }
+
                 r = json_parse(b, 0, &j, NULL, NULL);
                 if (r < 0)
                         return log_error_errno(r, "Failed to parse local TPM measurement log file: %m");
