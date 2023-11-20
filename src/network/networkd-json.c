7621b70faaacb204b1b763f25b067993ace1f08b
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-11-20 11:56:07 +0900

    sd-dhcp-server: rename DHCPLease -> sd_dhcp_server_lease
    Then, move basic functions for the object to sd-dhcp-server-lease.[ch].
    
    No effective funcional changes.

--- src/network/networkd-json.c
+++ src/network/networkd-json.c
@@ -2,7 +2,7 @@
 
 #include <linux/nexthop.h>
 
-#include "dhcp-server-internal.h"
+#include "dhcp-server-lease-internal.h"
 #include "dhcp6-internal.h"
 #include "dhcp6-lease-internal.h"
 #include "dns-domain.h"
@@ -892,7 +892,7 @@ static int pref64_append_json(Link *link, JsonVariant **v) {
 
 static int dhcp_server_offered_leases_append_json(Link *link, JsonVariant **v) {
         _cleanup_(json_variant_unrefp) JsonVariant *array = NULL;
-        DHCPLease *lease;
+        sd_dhcp_server_lease *lease;
         int r;
 
         assert(link);
@@ -924,7 +924,7 @@ static int dhcp_server_offered_leases_append_json(Link *link, JsonVariant **v) {
 
 static int dhcp_server_static_leases_append_json(Link *link, JsonVariant **v) {
         _cleanup_(json_variant_unrefp) JsonVariant *array = NULL;
-        DHCPLease *lease;
+        sd_dhcp_server_lease *lease;
         int r;
 
         assert(link);
