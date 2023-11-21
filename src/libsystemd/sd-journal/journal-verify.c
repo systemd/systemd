6bbc7f59a2d6c7e99b913476be3b87267806217e
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-11-22 08:44:44 +0900

    sd-journal: warn about seals may not be continuous only when the file is sealed
    Follow-up for e375bc5fcc080815ce849261eb505d084c2fde3e.
    
    Fixes #30118.

--- src/libsystemd/sd-journal/journal-verify.c
+++ src/libsystemd/sd-journal/journal-verify.c
@@ -925,7 +925,7 @@ int journal_file_verify(
                         goto fail;
                 }
 
-        if (!JOURNAL_HEADER_SEALED_CONTINUOUS(f->header))
+        if (JOURNAL_HEADER_SEALED(f->header) && !JOURNAL_HEADER_SEALED_CONTINUOUS(f->header))
                 warning(p,
                         "This log file was sealed with an old journald version where the sequence of seals might not be continuous. We cannot guarantee completeness.");
 
