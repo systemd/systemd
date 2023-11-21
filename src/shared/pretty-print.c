d61a4dbba93a2b34dc85522e432c30ca152d4dda
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-11-22 07:01:50 +0900

    pid1: move draw_cylong() to pretty-print.[ch]

--- src/shared/pretty-print.c
+++ src/shared/pretty-print.c
@@ -17,6 +17,42 @@
 #include "strv.h"
 #include "terminal-util.h"
 
+void draw_cylon(char buffer[], size_t buflen, unsigned width, unsigned pos) {
+        char *p = buffer;
+
+        assert(buflen >= CYLON_BUFFER_EXTRA + width + 1);
+        assert(pos <= width+1); /* 0 or width+1 mean that the center light is behind the corner */
+
+        if (pos > 1) {
+                if (pos > 2)
+                        p = mempset(p, ' ', pos-2);
+                if (log_get_show_color())
+                        p = stpcpy(p, ANSI_RED);
+                *p++ = '*';
+        }
+
+        if (pos > 0 && pos <= width) {
+                if (log_get_show_color())
+                        p = stpcpy(p, ANSI_HIGHLIGHT_RED);
+                *p++ = '*';
+        }
+
+        if (log_get_show_color())
+                p = stpcpy(p, ANSI_NORMAL);
+
+        if (pos < width) {
+                if (log_get_show_color())
+                        p = stpcpy(p, ANSI_RED);
+                *p++ = '*';
+                if (pos < width-1)
+                        p = mempset(p, ' ', width-1-pos);
+                if (log_get_show_color())
+                        p = stpcpy(p, ANSI_NORMAL);
+        }
+
+        *p = '\0';
+}
+
 bool urlify_enabled(void) {
 #if ENABLE_URLIFY
         static int cached_urlify_enabled = -1;
