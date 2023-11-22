bf704ca7b2325b4ea4d2fbc06299cbd7752f36e9
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-11-23 05:03:43 +0900

    parse-util: accept arbitrary MTU size when AF_UNSPEC
    When [Link] MTU= is specified in a .network file, we have no idea about
    that what kind of interface will be configured with the .network file.
    The maximum and minimum MTU size depend on the kind of interface.
    So, we should not filter MTU eagerly in the parser.
    
    Closes #30140.

--- src/basic/parse-util.c
+++ src/basic/parse-util.c
@@ -123,8 +123,7 @@ int parse_ifindex(const char *s) {
 }
 
 int parse_mtu(int family, const char *s, uint32_t *ret) {
-        uint64_t u;
-        size_t m;
+        uint64_t u, m;
         int r;
 
         r = parse_size(s, 1024, &u);
@@ -134,10 +133,16 @@ int parse_mtu(int family, const char *s, uint32_t *ret) {
         if (u > UINT32_MAX)
                 return -ERANGE;
 
-        if (family == AF_INET6)
+        switch (family) {
+        case AF_INET:
+                m = IPV4_MIN_MTU; /* This is 68 */
+                break;
+        case AF_INET6:
                 m = IPV6_MIN_MTU; /* This is 1280 */
-        else
-                m = IPV4_MIN_MTU; /* For all other protocols, including 'unspecified' we assume the IPv4 minimal MTU */
+                break;
+        default:
+                m = 0;
+        }
 
         if (u < m)
                 return -ERANGE;
