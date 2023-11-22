7b36fb9f96fd5c1f63b9f0f9e75194e3e4dd6a8d
Lennart Poettering <lennart@poettering.net>
2023-11-22 18:56:19 +0100

    polkit: simplify bus_verify_polkit_async() + drop auth-by-cap dbus feature
    This simplifies bus_verify_polkit_async() and related calls quite a bit:
    
    1. This removes any support for authentication-by-Linux-capability. This
       is ultimately a kdbus leftover: with classic AF_UNIX transports we
       cannot authenticate by capabilities securely (because we cannot
       acquire it from the peer without races), hence we never actually did.
       Since the necessary kernel work didn't materialize in the last 10y,
       and is unlikely to be added, let's just kill this context. We cannot
       quite remove the caps stuff from sd-bus for API compat, but for our
       polkit logic let's kill it.
    
    2. The "good_uid" and "interactive" params are only necessary in very
       few cases, hence let's move them to a new call
       bus_verify_polkit_async_full() and make bus_verify_polkit_async() a
       wrapper around it without those two parameters.
    
    This also fixes a bunch of wrong uses of the "interactive" bool. The
    bool makes no sense today as the ALLOW_INTERACTIVE_AUTHORIZATION field
    in the D-Bus message header replaces it fully. We only need it to
    implement method calls we introduced prior to that header field becoming
    available in D-Bus. And it should only be used on such old method calls,
    and otherwise always be set to false.
    
    This does not change behaviour in any way. Just simplifies stuff.
    
    Fixes: #21586

--- src/shared/bus-polkit.h
+++ src/shared/bus-polkit.h
@@ -4,8 +4,13 @@
 #include "sd-bus.h"
 
 #include "hashmap.h"
+#include "user-util.h"
 
-int bus_test_polkit(sd_bus_message *call, int capability, const char *action, const char **details, uid_t good_user, bool *_challenge, sd_bus_error *e);
+int bus_test_polkit(sd_bus_message *call, const char *action, const char **details, uid_t good_user, bool *_challenge, sd_bus_error *e);
 
-int bus_verify_polkit_async(sd_bus_message *call, int capability, const char *action, const char **details, bool interactive, uid_t good_user, Hashmap **registry, sd_bus_error *error);
+int bus_verify_polkit_async_full(sd_bus_message *call, const char *action, const char **details, bool interactive, uid_t good_user, Hashmap **registry, sd_bus_error *error);
+static inline int bus_verify_polkit_async(sd_bus_message *call, const char *action, const char **details, Hashmap **registry, sd_bus_error *ret_error) {
+        return bus_verify_polkit_async_full(call, action, details, false, UID_INVALID, registry, ret_error);
+}
+
 Hashmap *bus_verify_polkit_async_registry_free(Hashmap *registry);
