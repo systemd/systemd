4e96bf0b5b3eed775e80e314b8094e579cab2e82
Lennart Poettering <lennart@poettering.net>
2023-11-23 18:21:21 +0100

    bus-polkit: add support for authenticating varlink peers via polkit
    This extends our current polkit logic, so that we can in a very similar
    fashion as we already can authenticate dbus peers authenticate varlink
    connection peers.
    
    polkit natively speaks dbus and can authentication dbus peers. To get
    the same level of support for varlink we'll use authentication by
    pid+starttime+uid. Which is not quite as good. Sooner or later we want
    to switch to this:
    
    https://gitlab.freedesktop.org/polkit/polkit/-/merge_requests/200
    
    But so far that has not been merged.

--- src/shared/bus-polkit.h
+++ src/shared/bus-polkit.h
@@ -5,6 +5,7 @@
 
 #include "hashmap.h"
 #include "user-util.h"
+#include "varlink.h"
 
 int bus_test_polkit(sd_bus_message *call, const char *action, const char **details, uid_t good_user, bool *_challenge, sd_bus_error *e);
 
@@ -14,3 +15,13 @@ static inline int bus_verify_polkit_async(sd_bus_message *call, const char *acti
 }
 
 Hashmap *bus_verify_polkit_async_registry_free(Hashmap *registry);
+
+int varlink_verify_polkit_async(Varlink *link, sd_bus *bus, const char *action, const char **details, uid_t good_user, Hashmap **registry);
+
+/* A JsonDispatch initializer that makes sure the allowInteractiveAuthentication boolean field we want for
+ * polkit support in Varlink calls is ignored while regular dispatching (and does not result in errors
+ * regarding unexpected fields) */
+#define VARLINK_DISPATCH_POLKIT_FIELD {                          \
+                .name = "allowInteractiveAuthentication",        \
+                .type = JSON_VARIANT_BOOLEAN,                    \
+        }
