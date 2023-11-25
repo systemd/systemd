92e149291eee36ee9f46351b5f8f210cba48c213
Frantisek Sumsal <frantisek@sumsal.cz>
2023-11-25 21:36:10 +0100

    systemctl: include unit ID in the property map
    Otherwise we get a not very nice message when trying to display a
    non-existent man page:
    
    ~# systemctl cat test.service
    [Unit]
    Description=Hello
    [Service]
    ExecStart=true
    ~# systemctl help test.service
    Documentation for (null) not known.

--- src/systemctl/systemctl-show.c
+++ src/systemctl/systemctl-show.c
@@ -1969,6 +1969,7 @@ static int show_one(
                 bool *ellipsized) {
 
         static const struct bus_properties_map property_map[] = {
+                { "Id",                             "s",               NULL,           offsetof(UnitStatusInfo, id)                                },
                 { "LoadState",                      "s",               NULL,           offsetof(UnitStatusInfo, load_state)                        },
                 { "ActiveState",                    "s",               NULL,           offsetof(UnitStatusInfo, active_state)                      },
                 { "FreezerState",                   "s",               NULL,           offsetof(UnitStatusInfo, freezer_state)                     },
