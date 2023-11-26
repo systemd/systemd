ee7159d7eba403750915e35d93f0f635691f3a17
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-11-27 06:26:07 +0900

    test: verify json format passed through varlink

---
+++ test/units/testsuite-post.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+set -eux
+set -o pipefail
+
+(! journalctl -q -o short-monotonic --grep "didn't pass validation" >> /failed)
+
+exit 0
