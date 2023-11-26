2193b49fb1c15ca21019dcc4f2509d0695ee8b57
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-11-26 09:06:09 +0900

    async: do not kill sd-close on soft-reboot
    The fds passed to asynchronous_clonse() may be slow to close.
    Let's make the processes not killed on soft-reboot.

--- src/shared/killall.h
+++ src/shared/killall.h
@@ -1,6 +1,9 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 #pragma once
 
+#include <signal.h>
+
 #include "time-util.h"
 
+int pid_set_survivor_cgroup(pid_t pid);
 int broadcast_signal(int sig, bool wait_for_exit, bool send_sighup, usec_t timeout);
