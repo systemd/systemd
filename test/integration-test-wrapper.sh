bea69125e859196a6538a70cb0f5a4df12965f34
Richard Maw <richard.maw@codethink.co.uk>
2023-11-27 17:52:29 +0000

    WIP: test: Run integration tests with mkosi-built image
    TODO:
    1. Get all the tests running
       1. Support customising the image by activating overlays
       2. Support passing extra options to vmspawn and qemu and kernel
    2. Support running a prebuilt image instead of building with mkosi
    3. Support parallel test running

---
+++ test/integration-test-wrapper.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -eux
+
+system_mkosi="$(readlink -f "$1")"
+testunit="$2"
+shift 2
+
+mkosi_output_path="$(dirname "$system_mkosi")/$testunit"
+cp -a --reflink=always "$system_mkosi" "$mkosi_output_path"
+
+runmkosi() {
+    mkosi -C .. --image=system --output-dir="$mkosi_output_path" "$@"
+}
+
+image_path="$(runmkosi --json summary | jq --raw-output --arg image system '.Images[]|select(.Image==$image)|@text "\(.OutputDirectory)/\(.Output)"')"
+runmkosi --qemu-smp=1 --qemu-mem=1G "--kernel-command-line-extra=$*" qemu
+
+if systemd-dissect --with "$image_path" test -e testok; then
+    # Remove on success so that less space is required to run the whole suite
+    rm -rf "$mkosi_output_path"
+    exit 0
+else
+    exit_code="$?"
+    runmkosi journalctl -b -u "$testunit"
+    exit "$exit_code"
+fi
