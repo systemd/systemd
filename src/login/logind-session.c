93c68cf294a45da6eeb4bb19ade93a026b09938c
Lennart Poettering <lennart@poettering.net>
2023-11-29 11:21:21 +0100

    logind: use unlink_and_free() at once more place

--- src/login/logind-session.c
+++ src/login/logind-session.c
@@ -1246,11 +1246,7 @@ static void session_remove_fifo(Session *s) {
 
         s->fifo_event_source = sd_event_source_unref(s->fifo_event_source);
         s->fifo_fd = safe_close(s->fifo_fd);
-
-        if (s->fifo_path) {
-                (void) unlink(s->fifo_path);
-                s->fifo_path = mfree(s->fifo_path);
-        }
+        s->fifo_path = unlink_and_free(s->fifo_path);
 }
 
 static int session_dispatch_leader_pidfd(sd_event_source *es, int fd, uint32_t revents, void *userdata) {
