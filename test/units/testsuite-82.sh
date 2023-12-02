77f62eb55ccc6eac49856fe7710376d0de3a0266
Luca Boccassi <bluca@debian.org>
2023-12-02 20:11:57 +0000

    logind: add EnforceInhibitors=
    Currently inhibitors are bypassed unless an explicit request is made to
    check for them, or even in that case when the requestor is root or the
    same uid as the holder of the lock. This is done to ensure backward
    compatibility.
    
    But in many cases this makes it impractical to rely on inhibitor locks.
    For example, in Debian there are several convoluted and archaic
    workarounds that divert systemctl/reboot to some hacky custom scripts
    to try and enforce blocking accidental reboots, when it's not expected
    that the requestor will remember to specify the command line option
    to enable checking for active inhibitor locks.
    
    Add a new EnforceInhibitors= option in logind.conf, disabled by
    default, that when enabled ensures locks are always taken into account,
    regardless of the privilege levels or the options selected by the
    requestor. systemctl --force --force (i.e., specified twice) will still
    be able to bypass the locks, as it reboots via syscalls rather than
    going through D-Bus, but that is acceptable and a useful workaround
    for when a system is deadlocked or non-functional in some way.

--- test/units/testsuite-82.sh
+++ test/units/testsuite-82.sh
@@ -203,6 +203,12 @@ EOF
 
     # Check that we can set up an inhibitor, and that busctl monitor sees the
     # PrepareForShutdownWithMetadata signal and that it says 'soft-reboot'.
+    mkdir -p /run/systemd/logind.conf.d
+    cat >/run/systemd/logind.conf.d/lock.conf <<EOF
+[Login]
+EnforceInhibitors=yes
+EOF
+    systemctl reload systemd-logind.service
     systemd-run --unit busctl.service --service-type=exec --property StandardOutput=file:/run/testsuite82.signal \
         busctl monitor --json=pretty --match 'sender=org.freedesktop.login1,path=/org/freedesktop/login1,interface=org.freedesktop.login1.Manager,member=PrepareForShutdownWithMetadata,type=signal'
     systemd-run --unit inhibit.service --service-type=exec \
