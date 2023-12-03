b4eb13b9697f32cab617bfe0c5ac376f75b8fd2c
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2023-12-03 14:53:49 +0100

    tmpfiles: split out helper to open and read a "config file"
    No functional change.
    
    Note that this function will be modified in subsequent commits, and the API
    will change.

--- src/basic/conf-files.h
+++ src/basic/conf-files.h
@@ -1,6 +1,8 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 #pragma once
 
+#include <stdbool.h>
+
 #include "macro.h"
 
 enum {
@@ -29,3 +31,19 @@ int conf_files_list_dropins(
                 const char *dropin_dirname,
                 const char *root,
                 const char * const *dirs);
+
+typedef int parse_line_t(
+                void *data,
+                const char *fname,
+                unsigned line,
+                const char *buffer,
+                bool *invalid_config);
+
+int conf_file_read(
+                const char *root,
+                const char **config_dirs,
+                const char *fn,
+                parse_line_t parse_line,
+                void *userdata,
+                bool ignore_enoent,
+                bool *invalid_config);
