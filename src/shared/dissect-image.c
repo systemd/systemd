c44c4e441ee4c45818e32eb100f9f2be7a24e007
Lennart Poettering <lennart@poettering.net>
2023-12-04 18:20:36 +0100

    dissect-image: don't try to validate an extension release file with no image name
    Otherwise we might validate the OS release file instead…

--- src/shared/dissect-image.c
+++ src/shared/dissect-image.c
@@ -2201,7 +2201,7 @@ int dissected_image_mount(
                         if (r > 0)
                                 ok = true;
                 }
-                if (!ok && FLAGS_SET(flags, DISSECT_IMAGE_VALIDATE_OS_EXT)) {
+                if (!ok && FLAGS_SET(flags, DISSECT_IMAGE_VALIDATE_OS_EXT) && m->image_name) {
                         r = extension_has_forbidden_content(where);
                         if (r < 0)
                                 return r;
@@ -3460,6 +3460,9 @@ int dissected_image_acquire_metadata(DissectedImage *m, DissectImageFlags extra_
                         switch (k) {
 
                         case META_SYSEXT_RELEASE:
+                                if (!m->image_name)
+                                        goto next;
+
                                 /* As per the os-release spec, if the image is an extension it will have a
                                  * file named after the image name in extension-release.d/ - we use the image
                                  * name and try to resolve it with the extension-release helpers, as
@@ -3480,6 +3483,9 @@ int dissected_image_acquire_metadata(DissectedImage *m, DissectImageFlags extra_
                                 break;
 
                         case META_CONFEXT_RELEASE:
+                                if (!m->image_name)
+                                        goto next;
+
                                 /* As above */
                                 r = open_extension_release(
                                                 t,
