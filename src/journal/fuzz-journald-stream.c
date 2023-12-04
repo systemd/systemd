727ffcee4125ae4495cd70d42bf7dddc60e0c24a
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-12-04 16:06:20 +0900

    journal: split out server_new() from server_init()
    Then, use DEFINE_MAIN_FUNCTION() macro in journald.
    No functional change, just refactoring to follow our usual coding style.

--- src/journal/fuzz-journald-stream.c
+++ src/journal/fuzz-journald-stream.c
@@ -12,7 +12,7 @@
 static int stream_fds[2] = EBADF_PAIR;
 
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
-        Server s;
+        _cleanup_free_ Server *s = NULL;
         StdoutStream *stream;
         int v;
 
@@ -22,14 +22,14 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
         fuzz_setup_logging();
 
         assert_se(socketpair(AF_UNIX, SOCK_STREAM|SOCK_CLOEXEC|SOCK_NONBLOCK, 0, stream_fds) >= 0);
-        dummy_server_init(&s, NULL, 0);
-        assert_se(stdout_stream_install(&s, stream_fds[0], &stream) >= 0);
+        assert_se(server_new(&s) >= 0);
+        dummy_server_init(s, NULL, 0);
+        assert_se(stdout_stream_install(s, stream_fds[0], &stream) >= 0);
         assert_se(write(stream_fds[1], data, size) == (ssize_t) size);
         while (ioctl(stream_fds[0], SIOCINQ, &v) == 0 && v)
-                sd_event_run(s.event, UINT64_MAX);
-        if (s.n_stdout_streams)
+                sd_event_run(s->event, UINT64_MAX);
+        if (s->n_stdout_streams)
                 stdout_stream_destroy(stream);
-        server_done(&s);
         stream_fds[1] = safe_close(stream_fds[1]);
 
         return 0;
