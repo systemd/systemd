e155643aa987ca27a6a55ef0f7c1f6af16a769c9
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-12-04 16:06:20 +0900

    journal: split out server_new() from server_init()
    Then, use DEFINE_MAIN_FUNCTION() macro in journald.
    No functional change, just refactoring to follow our usual coding style.

--- src/journal/fuzz-journald-stream.c
+++ src/journal/fuzz-journald-stream.c
@@ -9,12 +9,11 @@
 #include "fuzz-journald.h"
 #include "journald-stream.h"
 
-static int stream_fds[2] = EBADF_PAIR;
-
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
-        Server s;
+        _cleanup_close_pair_ int stream_fds[2] = EBADF_PAIR;
+        _cleanup_(server_freep) Server *s = NULL;
         StdoutStream *stream;
-        int v;
+        int v, fd0;
 
         if (outside_size_range(size, 1, 65536))
                 return 0;
@@ -22,15 +21,18 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
         fuzz_setup_logging();
 
         assert_se(socketpair(AF_UNIX, SOCK_STREAM|SOCK_CLOEXEC|SOCK_NONBLOCK, 0, stream_fds) >= 0);
-        dummy_server_init(&s, NULL, 0);
-        assert_se(stdout_stream_install(&s, stream_fds[0], &stream) >= 0);
+        assert_se(server_new(&s) >= 0);
+        dummy_server_init(s, NULL, 0);
+
+        assert_se(stdout_stream_install(s, stream_fds[0], &stream) >= 0);
+        fd0 = TAKE_FD(stream_fds[0]); /* avoid double close */
+
         assert_se(write(stream_fds[1], data, size) == (ssize_t) size);
-        while (ioctl(stream_fds[0], SIOCINQ, &v) == 0 && v)
-                sd_event_run(s.event, UINT64_MAX);
-        if (s.n_stdout_streams)
+        while (ioctl(fd0, SIOCINQ, &v) == 0 && v)
+                sd_event_run(s->event, UINT64_MAX);
+
+        if (s->n_stdout_streams > 0)
                 stdout_stream_destroy(stream);
-        server_done(&s);
-        stream_fds[1] = safe_close(stream_fds[1]);
 
         return 0;
 }
