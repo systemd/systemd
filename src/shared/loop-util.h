8c44dd6b3cfa4fa83387648a5ccd8df1d0c9d5de
Lennart Poettering <lennart@poettering.net>
2023-12-04 18:02:45 +0100

    loop-util: remember if we created the LoopDevice
    Let's store in a bool whether a LoopDevice object was created via
    loop_device_open() or loop_device_make().

--- src/shared/loop-util.h
+++ src/shared/loop-util.h
@@ -22,6 +22,7 @@ struct LoopDevice {
         sd_device *dev;
         char *backing_file;
         bool relinquished;
+        bool created;        /* If we created the device */
         dev_t backing_devno; /* The backing file's dev_t */
         ino_t backing_inode; /* The backing file's ino_t */
         uint64_t diskseq; /* Block device sequence number, monothonically incremented by the kernel on create/attach, or 0 if we don't know */
