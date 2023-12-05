54b0f615a77f8390f3d3b75c79fa4ec0524f9640
Lennart Poettering <lennart@poettering.net>
2023-12-05 11:51:26 +0100

    namespace-util: add namespace_open_by_type() helper

--- src/basic/namespace-util.h
+++ src/basic/namespace-util.h
@@ -50,3 +50,5 @@ int userns_acquire_empty(void);
 int userns_acquire(const char *uid_map, const char *gid_map);
 
 int in_same_namespace(pid_t pid1, pid_t pid2, NamespaceType type);
+
+int namespace_open_by_type(NamespaceType type);
