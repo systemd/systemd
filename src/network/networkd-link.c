601a7bfbd075286b39074b28aa2509f15ddfddb6
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-12-07 18:57:13 +0900

    network: allow to configure interface MTU for CAN devices
    Previously, even if MTUBytes= is specified in matching .network file,
    the setting was ignored for CAN devices.

--- src/network/networkd-link.c
+++ src/network/networkd-link.c
@@ -1048,6 +1048,10 @@ static int link_configure(Link *link) {
         if (r < 0)
                 return r;
 
+        r = link_configure_mtu(link);
+        if (r < 0)
+                return r;
+
         if (link->iftype == ARPHRD_CAN) {
                 /* let's shortcut things for CAN which doesn't need most of what's done below. */
                 r = link_request_to_set_can(link);
@@ -1078,10 +1082,6 @@ static int link_configure(Link *link) {
                 return r;
 
         r = link_request_to_set_group(link);
-        if (r < 0)
-                return r;
-
-        r = link_configure_mtu(link);
         if (r < 0)
                 return r;
 
