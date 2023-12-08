systemd/systemd

commit 2f6181ad4d6c126e3ebf6880ba30b3b0059c6fc8

analyze: do not prepend the current working directory to SYSTEMD_UNIT_PATH needlessly
If the requested unit is not in the current working directory, it is not
necessary to include the current working directory in SYSTEMD_UNIT_PATH.

Follow-up for 45519d13a4f2a3c3585e672595762ca621abe65e.

Fixes #30166.

src/analyze/analyze-verify-util.c
@@ -86,6 +86,9 @@ int verify_set_unit_path(char **filenames) {
                 if (r < 0)
                         return r;
 
+                if (access(a, F_OK) < 0)
+                        continue;
+
                 r = path_extract_directory(a, &t);
                 if (r < 0)
                         return r;
@@ -98,6 +101,9 @@ int verify_set_unit_path(char **filenames) {
         joined = strv_join(strv_uniq(ans), ":");
         if (!joined)
                 return -ENOMEM;
+
+        if (isempty(joined))
+                return 0;
 
         /* First, prepend our directories. Second, if some path was specified, use that, and
          * otherwise use the defaults. Any duplicates will be filtered out in path-lookup.c.

commit b3c27cf7ac675f827e8b729f8213f8d98ae2429f

analyze: also find template unit when a template instance is specified
Fixes a regression caused by 2f6181ad4d6c126e3ebf6880ba30b3b0059c6fc8.

Fixes #30357.

src/analyze/analyze-verify-util.c
@@ -72,6 +72,43 @@ int verify_prepare_filename(const char *filename, char **ret) {
         return 0;
 }
 
+static int find_unit(const char *p, char **ret) {
+        _cleanup_free_ char *a = NULL, *t = NULL;
+        int r;
+
+        assert(p);
+        assert(ret);
+
+        r = path_make_absolute_cwd(p, &a);
+        if (r < 0)
+                return r;
+
+        r = RET_NERRNO(access(a, F_OK));
+        if (r >= 0) {
+                *ret = TAKE_PTR(a);
+                return 0;
+        }
+
+        if (!unit_name_is_valid(p, UNIT_NAME_INSTANCE))
+                return r; /* return the original error code given by the above access(). */
+
+        /* If the specified unit is an instance of a template unit, then let's try to find the template unit. */
+        r = unit_name_template(p, &t);
+        if (r < 0)
+                return r;
+
+        a = mfree(a);
+        r = path_make_absolute_cwd(t, &a);
+        if (r < 0)
+                return r;
+
+        if (access(a, F_OK) < 0)
+                return -errno;
+
+        *ret = TAKE_PTR(a);
+        return 0;
+}
+
 int verify_set_unit_path(char **filenames) {
         _cleanup_strv_free_ char **ans = NULL;
         _cleanup_free_ char *joined = NULL;
@@ -82,11 +119,10 @@ int verify_set_unit_path(char **filenames) {
                 _cleanup_free_ char *a = NULL;
                 char *t;
 
-                r = path_make_absolute_cwd(*filename, &a);
-                if (r < 0)
+                r = find_unit(*filename, &a);
+                if (r == -ENOMEM)
                         return r;
-
-                if (access(a, F_OK) < 0)
+                if (r < 0)
                         continue;
 
                 r = path_extract_directory(a, &t);