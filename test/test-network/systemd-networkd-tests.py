5ae3c4c959f554213e985541941dbf967714f62b
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-12-12 03:16:49 +0900

    test-network: drop redundant call of tearDown()

--- test/test-network/systemd-networkd-tests.py
+++ test/test-network/systemd-networkd-tests.py
@@ -3232,8 +3232,6 @@ class NetworkdNetworkTests(unittest.TestCase, Utilities):
         print(output)
         self.assertEqual(output, '')
 
-        self.tearDown()
-
     def test_route_static(self):
         first = True
         for manage_foreign_routes in [True, False]:
