4d9a30f14f0ce4d01722d332f17a20798464eab0
Frantisek Sumsal <frantisek@sumsal.cz>
2023-12-11 14:50:32 +0100

    network: add a missing space

--- src/network/networkd-address.c
+++ src/network/networkd-address.c
@@ -675,7 +675,7 @@ static void address_modify_nft_set_context(Address *address, bool add, NFTSetCon
 
                 if (r < 0)
                         log_warning_errno(r, "Failed to %s NFT set: family %s, table %s, set %s, IP address %s, ignoring",
-                                          add? "add" : "delete",
+                                          add ? "add" : "delete",
                                           nfproto_to_string(nft_set->nfproto), nft_set->table, nft_set->set,
                                           IN_ADDR_PREFIX_TO_STRING(address->family, &address->in_addr, address->prefixlen));
                 else
