3e496cdab3733f738a49221dff765064f5020734
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-12-20 00:16:16 +0900

    udev: make UdevEvent take reference to UdevWorker
    Many functions take timeout_usec and friends, but those are equivalent
    to the Worker elements.
    No functional change, just refactoring.

--- src/udev/udev-worker.c
+++ src/udev/udev-worker.c
@@ -176,7 +176,7 @@ static int worker_process_device(UdevWorker *worker, sd_device *dev) {
 
         log_device_uevent(dev, "Processing device");
 
-        udev_event = udev_event_new(dev, worker->exec_delay_usec, worker->rtnl, worker->log_level);
+        udev_event = udev_event_new(dev, worker);
         if (!udev_event)
                 return -ENOMEM;
 
@@ -200,16 +200,11 @@ static int worker_process_device(UdevWorker *worker, sd_device *dev) {
                 log_device_warning_errno(dev, r, "Failed to remove inotify watch, ignoring: %m");
 
         /* apply rules, create node, symlinks */
-        r = udev_event_execute_rules(
-                          udev_event,
-                          worker->timeout_usec,
-                          worker->timeout_signal,
-                          worker->properties,
-                          worker->rules);
+        r = udev_event_execute_rules(udev_event, worker->rules);
         if (r < 0)
                 return r;
 
-        udev_event_execute_run(udev_event, worker->timeout_usec, worker->timeout_signal);
+        udev_event_execute_run(udev_event);
 
         if (!worker->rtnl)
                 /* in case rtnl was initialized */
