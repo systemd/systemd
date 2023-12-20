447bcbfc903d56615809d7b578ddbec53704ccc5
Lennart Poettering <lennart@poettering.net>
2023-12-20 12:08:32 +0100

    color-util: add helper to convert RGB → HSV
    We already have HSV → RGB, add the opposite operation.

--- src/shared/color-util.h
+++ src/shared/color-util.h
@@ -3,6 +3,9 @@
 
 #include <inttypes.h>
 
+void rgb_to_hsv(double r, double g, double b,
+                double *ret_h, double *ret_s, double *ret_v);
+
 void hsv_to_rgb(
                 double h, double s, double v,
                 uint8_t* ret_r, uint8_t *ret_g, uint8_t *ret_b);
