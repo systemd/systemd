aa8212a33d2afe124fd845099b7e792dc62a09dd
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-12-26 05:31:38 +0900

    network/queue: make request_detach() return if the request is called or not
    Then, the caller can know if the requested configuration (e.g. address)
    will soon assigned.
    
    The return value is not used currently, but will be used in later
    commits.

--- src/network/networkd-queue.h
+++ src/network/networkd-queue.h
@@ -88,7 +88,7 @@ Request *request_ref(Request *req);
 Request *request_unref(Request *req);
 DEFINE_TRIVIAL_CLEANUP_FUNC(Request*, request_unref);
 
-void request_detach(Manager *manager, Request *req);
+bool request_detach(Manager *manager, Request *req);
 
 int netdev_queue_request(
                 NetDev *netdev,
