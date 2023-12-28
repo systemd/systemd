bfd6c9f6cdc2335471a1076407ab793817ada8a9
Mike Yuan <me@yhndnzj.com>
2023-12-28 18:07:12 +0800

    fdset: use FOREACH_ARRAY at one more place

--- src/shared/fdset.c
+++ src/shared/fdset.c
@@ -40,8 +40,8 @@ int fdset_new_array(FDSet **ret, const int fds[], size_t n_fds) {
         if (!s)
                 return -ENOMEM;
 
-        for (size_t i = 0; i < n_fds; i++) {
-                r = fdset_put(s, fds[i]);
+        FOREACH_ARRAY(fd, fds, n_fds) {
+                r = fdset_put(s, *fd);
                 if (r < 0)
                         return r;
         }
