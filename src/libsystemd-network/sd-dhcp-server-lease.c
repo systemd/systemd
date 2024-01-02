af8edf2c15d5757e196f06c2f1394e4394b5c617
Yu Watanabe <watanabe.yu+github@gmail.com>
2024-01-03 06:07:05 +0900

    sd-dhcp-server-lease: move functions to build json format

--- src/libsystemd-network/sd-dhcp-server-lease.c
+++ src/libsystemd-network/sd-dhcp-server-lease.c
@@ -204,3 +204,69 @@ int sd_dhcp_server_set_static_lease(
         TAKE_PTR(lease);
         return 0;
 }
+
+static int dhcp_server_lease_append_json(sd_dhcp_server_lease *lease, JsonVariant **array) {
+        assert(lease);
+        assert(array);
+
+        return json_variant_append_arrayb(
+                        array,
+                        JSON_BUILD_OBJECT(
+                                        JSON_BUILD_PAIR_BYTE_ARRAY(
+                                                        "ClientId",
+                                                        lease->client_id.raw,
+                                                        lease->client_id.size),
+                                        JSON_BUILD_PAIR_IN4_ADDR_NON_NULL("Address", &(struct in_addr) { .s_addr = lease->address }),
+                                        JSON_BUILD_PAIR_STRING_NON_EMPTY("Hostname", lease->hostname),
+                                        JSON_BUILD_PAIR_FINITE_USEC(
+                                                        "ExpirationUSec", lease->expiration)));
+}
+
+static int dhcp_server_bound_leases_build_json(sd_dhcp_server *server, JsonVariant **ret) {
+        _cleanup_(json_variant_unrefp) JsonVariant *array = NULL;
+        sd_dhcp_server_lease *lease;
+        int r;
+
+        assert(server);
+        assert(ret);
+
+        HASHMAP_FOREACH(lease, server->bound_leases_by_client_id) {
+                r = dhcp_server_lease_append_json(lease, &array);
+                if (r < 0)
+                        return r;
+        }
+
+        *ret = TAKE_PTR(array);
+        return 0;
+}
+
+int dhcp_server_bound_leases_append_json(sd_dhcp_server *server, JsonVariant **v) {
+        _cleanup_(json_variant_unrefp) JsonVariant *array = NULL;
+        int r;
+
+        assert(server);
+        assert(v);
+
+        r = dhcp_server_bound_leases_build_json(server, &array);
+        if (r < 0)
+                return r;
+
+        return json_variant_set_field_non_null(v, "Leases", array);
+}
+
+int dhcp_server_static_leases_append_json(sd_dhcp_server *server, JsonVariant **v) {
+        _cleanup_(json_variant_unrefp) JsonVariant *array = NULL;
+        sd_dhcp_server_lease *lease;
+        int r;
+
+        assert(server);
+        assert(v);
+
+        HASHMAP_FOREACH(lease, server->static_leases_by_client_id) {
+                r = dhcp_server_lease_append_json(lease, &array);
+                if (r < 0)
+                        return r;
+        }
+
+        return json_variant_set_field_non_null(v, "StaticLeases", array);
+}
