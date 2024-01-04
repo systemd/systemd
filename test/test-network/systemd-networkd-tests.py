10f317f8bd32f995bc58733bfccfdd35e706d0ac
Yu Watanabe <watanabe.yu+github@gmail.com>
2024-01-05 03:49:32 +0900

    test-network: add test for removal of nexthops that we do not receive reply from the kernel

--- test/test-network/systemd-networkd-tests.py
+++ test/test-network/systemd-networkd-tests.py
@@ -4014,7 +4014,11 @@ class NetworkdNetworkTests(unittest.TestCase, Utilities):
 
         remove_network_unit('25-nexthop-nothing.network', '25-nexthop-dummy-2.network')
         copy_network_unit('25-nexthop-1.network', '25-nexthop-dummy-1.network')
-        networkctl_reload()
+        # Of course, networkctl_reconfigure() below is unnecessary in normal operation, but it is intentional
+        # here to test reconfiguring with different .network files does not trigger race.
+        # See also comments in link_drop_requests().
+        networkctl_reconfigure('dummy98') # reconfigured with 25-nexthop-dummy-2.network
+        networkctl_reload()               # reconfigured with 25-nexthop-dummy-1.network
 
         self.check_nexthop(manage_foreign_nexthops, first=True)
 
