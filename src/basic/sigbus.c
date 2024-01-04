87af56452a3070134423aad8c4621c35bdd1ea09
Rose <83477269+AtariDreams@users.noreply.github.com>
2024-01-04 11:55:05 -0500

    basic: make sigbus thread safe
    Make the queue purely atomic and always access it atomically for safety.

--- src/basic/sigbus.c
+++ src/basic/sigbus.c
@@ -22,8 +22,8 @@ static unsigned n_installed = 0;
    don't have to deal with locks between signal handler and main
    programs in possibly multiple threads. */
 
-static void* volatile sigbus_queue[SIGBUS_QUEUE_MAX];
-static volatile sig_atomic_t n_sigbus_queue = 0;
+static _Atomic(void *) sigbus_queue[SIGBUS_QUEUE_MAX];
+static _Atomic(sig_atomic_t) n_sigbus_queue = 0;
 
 static void sigbus_push(void *addr) {
         assert(addr);
@@ -41,18 +41,15 @@ static void sigbus_push(void *addr) {
 
         /* If we can't, make sure the queue size is out of bounds, to
          * mark it as overflowed */
+
+        sig_atomic_t c = __atomic_load_n(&n_sigbus_queue, __ATOMIC_SEQ_CST);
         for (;;) {
-                sig_atomic_t c;
-
-                __atomic_thread_fence(__ATOMIC_SEQ_CST);
-                c = n_sigbus_queue;
-
                 if (c > SIGBUS_QUEUE_MAX) /* already overflowed */
                         return;
 
                 /* OK if we clobber c here, since we either immediately return
                  * or it will be immediately reinitialized on next loop */
-                if (__atomic_compare_exchange_n(&n_sigbus_queue, &c, c + SIGBUS_QUEUE_MAX, false,
+                if (__atomic_compare_exchange_n(&n_sigbus_queue, &c, c + SIGBUS_QUEUE_MAX, true,
                                                 __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST))
                         return;
         }
@@ -64,8 +61,7 @@ int sigbus_pop(void **ret) {
         for (;;) {
                 unsigned u, c;
 
-                __atomic_thread_fence(__ATOMIC_SEQ_CST);
-                c = n_sigbus_queue;
+                c = __atomic_load_n(n_sigbus_queue, __ATOMIC_SEQ_CST);
 
                 if (_likely_(c == 0))
                         return 0;
@@ -76,14 +72,14 @@ int sigbus_pop(void **ret) {
                 for (u = 0; u < SIGBUS_QUEUE_MAX; u++) {
                         void *addr;
 
-                        addr = sigbus_queue[u];
+                        addr = __atomic_load_n(&sigbus_queue[u], __ATOMIC_SEQ_CST);
                         if (!addr)
                                 continue;
 
                         /* OK if we clobber addr here, since we either immediately return
                          * or it will be immediately reinitialized on next loop */
                         if (__atomic_compare_exchange_n(&sigbus_queue[u], &addr, NULL, false,
-                                                        __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST)) {
+                                                        __ATOMIC_SEQ_CST, __ATOMIC_RELAXED)) {
                                 __atomic_fetch_sub(&n_sigbus_queue, 1, __ATOMIC_SEQ_CST);
                                 /* If we successfully entered this if condition, addr won't
                                  * have been modified since its assignment, so safe to use it */
