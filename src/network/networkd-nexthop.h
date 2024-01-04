0dcefaf1aae0351d3b2e76da250344a65072abb3
Yu Watanabe <watanabe.yu+github@gmail.com>
2024-01-05 04:58:37 +0900

    network/nexthop: wait for requests for group members being processed
    This also split out the check as nexthop_is_ready().

--- src/network/networkd-nexthop.h
+++ src/network/networkd-nexthop.h
@@ -56,6 +56,7 @@ void link_foreignize_nexthops(Link *link);
 int link_request_static_nexthops(Link *link, bool only_ipv4);
 
 int nexthop_get_by_id(Manager *manager, uint32_t id, NextHop **ret);
+int nexthop_is_ready(Manager *manager, uint32_t id, NextHop **ret);
 int manager_rtnl_process_nexthop(sd_netlink *rtnl, sd_netlink_message *message, Manager *m);
 int manager_build_nexthop_ids(Manager *manager);
 
