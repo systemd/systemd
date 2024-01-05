5c9fc46aaf0c112969f3dffa5937f6cb7b5cdf89
David Tardon <dtardon@redhat.com>
2024-01-05 16:11:24 +0100

    udev: factor out config parser call into function
    ... which is then called from both places. This makes sure that the
    configuration is parsed by udevd and other tools in exactly the same
    way.

--- src/shared/udev-util.h
+++ src/shared/udev-util.h
@@ -3,9 +3,11 @@
 
 #include "sd-device.h"
 
+#include "conf-parser.h"
 #include "hashmap.h"
 #include "time-util.h"
 
+int udev_parse_config_full(const ConfigTableItem config_table[]);
 int udev_parse_config(void);
 
 int device_wait_for_initialization(sd_device *device, const char *subsystem, usec_t timeout_usec, sd_device **ret);
