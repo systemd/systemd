a7434417d6af694e03db83c3ee83e9c23fbc01b7
Frantisek Sumsal <frantisek@sumsal.cz>
2024-01-05 18:06:08 +0100

    journalctl: implement --list-namespaces
    Apart from being useful on its own, this will be used in the following
    commit for shell completions.

--- test/units/testsuite-44.sh
+++ test/units/testsuite-44.sh
@@ -4,11 +4,19 @@ set -eux
 
 systemd-analyze log-level debug
 
+journalctl --list-namespaces -o json | jq .
+
 systemd-run --wait -p LogNamespace=foobar echo "hello world"
+systemd-run --wait -p LogNamespace=foobaz echo "hello world"
 
 journalctl --namespace=foobar --sync
 journalctl -o cat --namespace=foobar >/tmp/hello-world
 journalctl -o cat >/tmp/no-hello-world
+
+journalctl --list-namespaces | grep foobar
+journalctl --list-namespaces | grep foobaz
+journalctl --list-namespaces -o json | jq .
+[[ "$(journalctl --root=/tmp --list-namespaces --quiet)" == "" ]]
 
 grep "^hello world$" /tmp/hello-world
 (! grep "^hello world$" /tmp/no-hello-world)
