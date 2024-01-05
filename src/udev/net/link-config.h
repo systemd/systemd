55225610a92de529111149b04f90e32e31bad956
Yu Watanabe <watanabe.yu+github@gmail.com>
2024-01-05 20:36:52 +0900

    udev/net: split out udev property assignment logic
    No functional change, just refactoring.

--- src/udev/net/link-config.h
+++ src/udev/net/link-config.h
@@ -100,7 +100,7 @@ Link *link_free(Link *link);
 DEFINE_TRIVIAL_CLEANUP_FUNC(Link*, link_free);
 
 int link_get_config(LinkConfigContext *ctx, Link *link);
-int link_apply_config(LinkConfigContext *ctx, sd_netlink **rtnl, Link *link);
+int link_apply_config(LinkConfigContext *ctx, sd_netlink **rtnl, Link *link, bool test);
 
 const char *mac_address_policy_to_string(MACAddressPolicy p) _const_;
 MACAddressPolicy mac_address_policy_from_string(const char *p) _pure_;
