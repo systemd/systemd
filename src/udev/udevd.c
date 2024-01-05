bdc903e21dcf903419136f3c7d764a452efd4616
David Tardon <dtardon@redhat.com>
2024-01-05 16:11:24 +0100

    udev: factor out config parser call into function
    ... which is then called from both places. This makes sure that the
    configuration is parsed by udevd and other tools in exactly the same
    way.

--- src/udev/udevd.c
+++ src/udev/udevd.c
@@ -82,16 +82,7 @@ static int manager_parse_udev_config(Manager *manager) {
         };
         int r;
 
-        r = config_parse_config_file_full(
-                        "udev.conf",
-                        "udev",
-                        NULL,
-                        config_item_table_lookup,
-                        config_table,
-                        CONFIG_PARSE_WARN,
-                        NULL);
-        if (r == -ENOENT)
-                return 0;
+        r = udev_parse_config_full(config_table);
         if (r < 0)
                 return r;
 
