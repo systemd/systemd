bdb1c1fbd51352f6f52ab142f9e453e2e958ee2d
Yu Watanabe <watanabe.yu+github@gmail.com>
2024-01-06 05:08:40 +0900

    network/route: move several conf parsers to networkd-route-metric.c and networkd-route-nexthop.c
    This also split config_parse_route_boolean() into two:
    for GatewayOnline= and boolean route metrics.
    
    No functional change, just refactoring and preparation for later commits.

---
+++ src/network/networkd-route-metric.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#pragma once
+
+#include "conf-parser.h"
+
+CONFIG_PARSER_PROTOTYPE(config_parse_route_mtu);
+CONFIG_PARSER_PROTOTYPE(config_parse_tcp_advmss);
+CONFIG_PARSER_PROTOTYPE(config_parse_route_hop_limit);
+CONFIG_PARSER_PROTOTYPE(config_parse_route_tcp_window);
+CONFIG_PARSER_PROTOTYPE(config_parse_route_tcp_rto);
+CONFIG_PARSER_PROTOTYPE(config_parse_route_boolean);
+CONFIG_PARSER_PROTOTYPE(config_parse_tcp_congestion);
+CONFIG_PARSER_PROTOTYPE(config_parse_tcp_window);
