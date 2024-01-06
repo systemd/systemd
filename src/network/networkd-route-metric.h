52170875a78c610574ea8f0dca1e10e0c0d6f06c
Yu Watanabe <watanabe.yu+github@gmail.com>
2024-01-06 13:07:11 +0900

    network/route-metric: pass attribute type to conf parsers
    The passed attribute type will be used later.
    
    This also
    - rename conf parsers,
    - sort gperf entries by the attr type.
    
    No functional change, just refactoring and preparation for later commits.

--- src/network/networkd-route-metric.h
+++ src/network/networkd-route-metric.h
@@ -3,11 +3,11 @@
 
 #include "conf-parser.h"
 
-CONFIG_PARSER_PROTOTYPE(config_parse_route_mtu);
-CONFIG_PARSER_PROTOTYPE(config_parse_tcp_advmss);
-CONFIG_PARSER_PROTOTYPE(config_parse_route_hop_limit);
-CONFIG_PARSER_PROTOTYPE(config_parse_route_tcp_window);
-CONFIG_PARSER_PROTOTYPE(config_parse_route_tcp_rto);
-CONFIG_PARSER_PROTOTYPE(config_parse_route_boolean);
-CONFIG_PARSER_PROTOTYPE(config_parse_tcp_congestion);
+CONFIG_PARSER_PROTOTYPE(config_parse_route_metric_mtu);
+CONFIG_PARSER_PROTOTYPE(config_parse_route_metric_advmss);
+CONFIG_PARSER_PROTOTYPE(config_parse_route_metric_hop_limit);
+CONFIG_PARSER_PROTOTYPE(config_parse_route_metric_tcp_window);
+CONFIG_PARSER_PROTOTYPE(config_parse_route_metric_tcp_rto);
+CONFIG_PARSER_PROTOTYPE(config_parse_route_metric_boolean);
+CONFIG_PARSER_PROTOTYPE(config_parse_route_metric_tcp_congestion);
 CONFIG_PARSER_PROTOTYPE(config_parse_tcp_window);
