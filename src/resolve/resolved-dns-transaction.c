36d6488c8f4f82b913a2e9d7edcc87a54107b9bd
Yu Watanabe <watanabe.yu+github@gmail.com>
2024-01-07 03:45:47 +0900

    resolve: do not assign -EINVAL to DnsTransaction.answer_ede_rcode
    Yes, the element is initialized with _DNS_EDE_RCODE_INVALID,
    so this does not change behavior. But, assigning invalid value looks
    spurious. Just refactoring.
    
    Follow-up for 9ca133e97a0c8795b1f293ccea4965b4ad1accc4.

--- src/resolve/resolved-dns-transaction.c
+++ src/resolve/resolved-dns-transaction.c
@@ -1223,18 +1223,17 @@ void dns_transaction_process_reply(DnsTransaction *t, DnsPacket *p, bool encrypt
         switch (t->scope->protocol) {
 
         case DNS_PROTOCOL_DNS: {
-                int ede_rcode;
                 _cleanup_free_ char *ede_msg = NULL;
+                int ede_rcode;
 
                 assert(t->server);
 
                 ede_rcode = dns_packet_ede_rcode(p, &ede_msg);
-                if (ede_rcode < 0 && ede_rcode != -EINVAL)
-                        log_debug_errno(ede_rcode, "Unable to extract EDE error code from packet, ignoring: %m");
-                else {
+                if (ede_rcode >= 0) {
                         t->answer_ede_rcode = ede_rcode;
                         t->answer_ede_msg = TAKE_PTR(ede_msg);
-                }
+                } else if (ede_rcode != _DNS_EDE_RCODE_INVALID)
+                        log_debug_errno(ede_rcode, "Unable to extract EDE error code from packet, ignoring: %m");
 
                 if (!t->bypass &&
                     IN_SET(DNS_PACKET_RCODE(p), DNS_RCODE_FORMERR, DNS_RCODE_SERVFAIL, DNS_RCODE_NOTIMP)) {
