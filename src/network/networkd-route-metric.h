ebf4fa1e82b71cb743518fa22dafb6860fd78bcf
Yu Watanabe <watanabe.yu+github@gmail.com>
2024-01-07 10:36:41 +0900

    network/route-metric: manage uint32_t RTAX_XYZ attributes in the same way
    Except for RTAX_CC_ALGO, all RTAX_XYZ attributes take uint32_t and
    the kernel's default value is zero. So, let's unify handling of the
    attributes.
    
    This should not change any effective behavior. Just refactoring.

--- src/network/networkd-route-metric.h
+++ src/network/networkd-route-metric.h
@@ -10,28 +10,28 @@
 #include "hash-funcs.h"
 
 typedef struct RouteMetric {
-        int quickack;
-        int fast_open_no_cookie;
-        uint32_t mtu;
-        uint32_t initcwnd;
-        uint32_t initrwnd;
-        uint32_t advmss;
-        uint32_t hop_limit;
-        char *tcp_congestion_control_algo;
-        usec_t tcp_rto_usec;
+        size_t n_metrics; /* maximum metric attr type with non-zero value */
+        uint32_t *metrics; /* RTAX_*, except for RTAX_CC_ALGO */
+
+        size_t n_metrics_set;
+        bool *metrics_set; /* used by conf parsers */
+
+        char *tcp_congestion_control_algo; /* RTAX_CC_ALGO */
 } RouteMetric;
 
-#define ROUTE_METRIC_NULL                       \
-        ((const RouteMetric) {                  \
-                .quickack = -1,                 \
-                .fast_open_no_cookie = -1,      \
-        })
+#define ROUTE_METRIC_NULL ((const RouteMetric) {})
 
 void route_metric_done(RouteMetric *metric);
 int route_metric_copy(const RouteMetric *src, RouteMetric *dest);
 
 void route_metric_hash_func(const RouteMetric *metric, struct siphash *state);
 int route_metric_compare_func(const RouteMetric *a, const RouteMetric *b);
+
+int route_metric_set_full(RouteMetric *metric, uint16_t attr, uint32_t value, bool force);
+static inline int route_metric_set(RouteMetric *metric, uint16_t attr, uint32_t value) {
+        return route_metric_set_full(metric, attr, value, false);
+}
+uint32_t route_metric_get(const RouteMetric *metric, uint16_t attr);
 
 int route_metric_set_netlink_message(const RouteMetric *metric, sd_netlink_message *m);
 int route_metric_read_netlink_message(RouteMetric *metric, sd_netlink_message *message);
