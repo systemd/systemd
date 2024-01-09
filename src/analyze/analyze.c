7c0e0bbb6b13d70500da79ce0270ed6da09327a0
Antonio Alvarez Feijoo <antonio.feijoo@suse.com>
2024-01-09 09:05:50 +0100

    analyze: fix -q option
    Follow-up to 52117f5af831a816c47ceebb83c8244ee93b72fe

--- src/analyze/analyze.c
+++ src/analyze/analyze.c
@@ -362,7 +362,7 @@ static int parse_argv(int argc, char *argv[]) {
         assert(argc >= 0);
         assert(argv);
 
-        while ((c = getopt_long(argc, argv, "hH:M:U:", options, NULL)) >= 0)
+        while ((c = getopt_long(argc, argv, "hH:M:U:q", options, NULL)) >= 0)
                 switch (c) {
 
                 case 'h':
