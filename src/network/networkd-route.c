338f3fe41c4394399292ad2c7b8f1d32705c50d5
Yu Watanabe <watanabe.yu+github@gmail.com>
2024-01-09 13:10:43 +0900

    network/route: use specified error message
    Previously, specified error message was not used.

--- src/network/networkd-route.c
+++ src/network/networkd-route.c
@@ -1129,7 +1129,7 @@ int route_configure_handler_internal(sd_netlink *rtnl, sd_netlink_message *m, Li
 
         r = sd_netlink_message_get_errno(m);
         if (r < 0 && r != -EEXIST) {
-                log_link_message_warning_errno(link, m, r, "Could not set route");
+                log_link_message_warning_errno(link, m, r, error_msg);
                 link_enter_failed(link);
                 return 0;
         }
