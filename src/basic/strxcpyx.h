e7fcdc4f6b1f497c1567582797a30272a5713acb
Rose <83477269+AtariDreams@users.noreply.github.com>
2024-01-10 11:03:28 -0500

    restrict
    Mark string functions derived from libc as restrict
    
    These are not guaranteed to behave if there is any overlap between the pointers passed to these functions anyway, so we may as well go all the way with telling the compiler it is an optimization hint.

--- src/basic/strxcpyx.h
+++ src/basic/strxcpyx.h
@@ -6,15 +6,16 @@
 
 #include "macro.h"
 
-size_t strnpcpy_full(char **dest, size_t size, const char *src, size_t len, bool *ret_truncated);
-static inline size_t strnpcpy(char **dest, size_t size, const char *src, size_t len) {
+size_t strnpcpy_full(char ** restrict dest, size_t size, const char * restrict src, size_t len, bool *ret_truncated);
+static inline size_t strnpcpy(char ** restrict dest, size_t size, const char * restrict src, size_t len) {
         return strnpcpy_full(dest, size, src, len, NULL);
 }
-size_t strpcpy_full(char **dest, size_t size, const char *src, bool *ret_truncated);
-static inline size_t strpcpy(char **dest, size_t size, const char *src) {
+size_t strpcpy_full(char ** restrict dest, size_t size, const char * restrict src, bool *ret_truncated);
+static inline size_t strpcpy(char ** restrict dest, size_t size, const char * restrict src) {
         return strpcpy_full(dest, size, src, NULL);
 }
-size_t strpcpyf_full(char **dest, size_t size, bool *ret_truncated, const char *src, ...) _printf_(4, 5);
+size_t strpcpyf_full(char ** restrict dest, size_t size, bool *ret_truncated, const char *src, ...)
+                _printf_(4, 5);
 #define strpcpyf(dest, size, src, ...) \
         strpcpyf_full((dest), (size), NULL, (src), ##__VA_ARGS__)
 size_t strpcpyl_full(char **dest, size_t size, bool *ret_truncated, const char *src, ...) _sentinel_;
@@ -24,10 +25,11 @@ size_t strnscpy_full(char *dest, size_t size, const char *src, size_t len, bool 
 static inline size_t strnscpy(char *dest, size_t size, const char *src, size_t len) {
         return strnscpy_full(dest, size, src, len, NULL);
 }
-size_t strscpy_full(char *dest, size_t size, const char *src, bool *ret_truncated);
-static inline size_t strscpy(char *dest, size_t size, const char *src) {
+size_t strscpy_full(char * restrict dest, size_t size, const char * restrict src, bool *ret_truncated);
+static inline size_t strscpy(char * restrict dest, size_t size, const char * restrict src) {
         return strscpy_full(dest, size, src, NULL);
 }
-size_t strscpyl_full(char *dest, size_t size, bool *ret_truncated, const char *src, ...) _sentinel_;
+size_t strscpyl_full(char * restrict dest, size_t size, bool *ret_truncated, const char * restrict src, ...)
+                _sentinel_;
 #define strscpyl(dest, size, src, ...) \
         strscpyl_full(dest, size, NULL, src, ##__VA_ARGS__)
