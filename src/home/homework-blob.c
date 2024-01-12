9b59baeccf1f4842173c572fb082f62a89407815
Adrian Vovk <adrianvovk@gmail.com>
2024-01-12 16:42:58 -0500

    fixup! homework: Reconcile blob directories

--- src/home/homework-blob.c
+++ src/home/homework-blob.c
@@ -9,7 +9,7 @@
 #include "fd-util.h"
 #include "fs-util.h"
 #include "home-util.h"
-#include "homework-bulk.h"
+#include "homework-blob.h"
 #include "homework.h"
 #include "macro.h"
 #include "path-util.h"
@@ -21,13 +21,13 @@
 
 #define BULK_MAX_SIZE (64U*1024U*1024U)
 
-struct bulk_overwrite_data {
+struct blob_overwrite_data {
         int dest_dfd;
         uint64_t total_size;
         uid_t uid;
 };
 
-static int bulk_overwrite_callback(
+static int blob_overwrite_callback(
                 RecurseDirEvent event,
                 const char *path,
                 int dfd,
@@ -37,23 +37,23 @@ static int bulk_overwrite_callback(
                 void *userdata) {
 
         _cleanup_close_ int src = -EBADF, dest = -EBADF;
-        struct bulk_overwrite_data *d = ASSERT_PTR(userdata);
+        struct blob_overwrite_data *d = ASSERT_PTR(userdata);
         int r;
 
         if (event == RECURSE_DIR_ENTER) {
-                log_warning("Entry %s in bulk directory is a directory. Skipping.", de->d_name);
+                log_warning("Entry %s in blob directory is a directory. Skipping.", de->d_name);
                 return RECURSE_DIR_SKIP_ENTRY;
         }
         if (event != RECURSE_DIR_ENTRY)
                 return RECURSE_DIR_CONTINUE;
 
         if (!S_ISREG(sx->stx_mode)) {
-                log_warning("Entry %s in bulk directory is not a regular file. Skipping.", de->d_name);
+                log_warning("Entry %s in blob directory is not a regular file. Skipping.", de->d_name);
                 return RECURSE_DIR_CONTINUE;
         }
 
         if (!ascii_is_valid(de->d_name) || string_has_cc(de->d_name, NULL)) {
-                log_warning("File %s in bulk directory has invalid filename. Skipping.", de->d_name);
+                log_warning("File %s in blob directory has invalid filename. Skipping.", de->d_name);
                 return RECURSE_DIR_CONTINUE;
         }
 
@@ -65,12 +65,12 @@ static int bulk_overwrite_callback(
 
         src = openat(dfd, de->d_name, O_RDONLY|O_CLOEXEC|O_NOCTTY|O_NOFOLLOW);
         if (src < 0)
-                return log_debug_errno(errno, "Failed to open %s in src bulk: %m", path);
+                return log_debug_errno(errno, "Failed to open %s in src blob: %m", path);
 
         WITH_UMASK(0000) {
                 dest = openat(d->dest_dfd, path, O_WRONLY|O_CREAT|O_EXCL|O_CLOEXEC|O_NOCTTY|O_NOFOLLOW, 0644);
                 if (dest < 0)
-                        return log_debug_errno(errno, "Failed to create/open %s in dest bulk: %m", path);
+                        return log_debug_errno(errno, "Failed to create/open %s in dest blob: %m", path);
         }
 
         r = copy_bytes(src, dest, UINT64_MAX, 0);
@@ -78,14 +78,14 @@ static int bulk_overwrite_callback(
                 return r;
 
         if (fchown(dest, d->uid, d->uid) < 0)
-                return log_debug_errno(errno, "Failed to chown %s in dest bulk: %m", path);
+                return log_debug_errno(errno, "Failed to chown %s in dest blob: %m", path);
 
         return RECURSE_DIR_CONTINUE;
 }
 
-static int overwrite_bulk(int src_fd, int dest_fd, uid_t uid) {
+static int overwrite_blob(int src_fd, int dest_fd, uid_t uid) {
         _cleanup_close_ int dest_dup = -EBADF;
-        struct bulk_overwrite_data userdata = {
+        struct blob_overwrite_data userdata = {
                 .dest_dfd = dest_fd,
                 .total_size = 0,
                 .uid = uid,
@@ -97,20 +97,20 @@ static int overwrite_bulk(int src_fd, int dest_fd, uid_t uid) {
 
         dest_dup = fcntl(dest_fd, F_DUPFD_CLOEXEC, 3);
         if (dest_dup < 0)
-                return log_debug_errno(errno, "Failed to dupliate dest bulk dir fd: %m");
+                return log_debug_errno(errno, "Failed to dupliate dest blob dir fd: %m");
 
         r = rm_rf_children(TAKE_FD(dest_dup), REMOVE_PHYSICAL|REMOVE_SUBVOLUME, NULL);
         if (r < 0)
-                return log_debug_errno(r, "Failed to clear dest bulk dir: %m");
+                return log_debug_errno(r, "Failed to clear dest blob dir: %m");
 
         if (fchown(dest_fd, uid, uid) < 0)
-                return log_debug_errno(r, "Failed to chown dest bulk: %m");
+                return log_debug_errno(r, "Failed to chown dest blob: %m");
 
         return recurse_dir_at(src_fd, ".", STATX_TYPE|STATX_SIZE, UINT_MAX, RECURSE_DIR_SORT,
-                              bulk_overwrite_callback, &userdata);
+                              blob_overwrite_callback, &userdata);
 }
 
-int home_reconcile_bulk_dirs(UserRecord *h, int root_fd, int reconciled) {
+int home_reconcile_blob_dirs(UserRecord *h, int root_fd, int reconciled) {
         _cleanup_close_ int sys_fd = -EBADF, embedded_fd = -EBADF;
         _cleanup_free_ char *sys_path = NULL;
         int r;
