e2a42c0c43f6cdd7cb2b89521392248056eeac49
Mike Yuan <me@yhndnzj.com>
2024-01-14 02:38:11 +0800

    logind-user: track user started/stopping state through user-runtime-dir@.service
    Before #30884, the user state is tied to user@.service (user service
    manager). However, #30884 introduced sessions that need no manager,
    and we can no longer rely on that.
    
    Consider the following situation:
    
    1. A 'background-light' session '1' is created (i.e. no user service manager
       is needed)
    2. Session '1' scope unit pulls in user-runtime-dir@.service
    3. Session '1' exits. A stop job is enqueued for user-runtime-dir@.service
       due to StopWhenUnneeded=yes
    4. At the same time, another session '2' which requires user manager is started.
       However, session scope units have JobMode=fail, therefore the start job
       for user-runtime-dir@.service that was pulled in by session '2' scope job
       is deleted as it conflicts with the stop job.
    
    We want session scope units to continue using JobMode=fail, but we still need
    the dependencies to be started correctly, i.e. explicitly requested by logind
    beforehand. Therefore, let's stop using StopWhenUnneeded=yes for
    user-runtime-dir@.service, and track users' `started` and `stopping` state
    based on that when user@.service is not needed. Then, for every invocation
    of user_start(), we'll recheck if we need the service manager and start it
    if so.
    
    Also, the dependency type on user-runtime-dir@.service from user@.service
    is upgraded to `BindsTo=`, in order to ensure that when logind stops the
    former, the latter is stopped as well.

--- src/login/logind-user.h
+++ src/login/logind-user.h
@@ -34,11 +34,17 @@ struct User {
         char *state_file;
         char *runtime_path;
 
-        char *slice;                     /* user-UID.slice */
-        char *service;                   /* user@UID.service */
-        char *runtime_dir_service;       /* user-runtime-dir@UID.service */
+        /* user-UID.slice */
+        char *slice;
 
-        char *service_job;
+        /* user-runtime-dir@UID.service */
+        char *runtime_dir_unit;
+        char *runtime_dir_job;
+
+        /* user@UID.service */
+        bool service_manager_started;
+        char *service_manager_unit;
+        char *service_manager_job;
 
         Session *display;
 
@@ -51,7 +57,8 @@ struct User {
         UserGCMode gc_mode;
         bool in_gc_queue:1;
 
-        bool started:1;       /* Whenever the user being started, has been started or is being stopped again. */
+        bool started:1;       /* Whenever the user being started, has been started or is being stopped again
+                                 (tracked through user-runtime-dir@.service) */
         bool stopping:1;      /* Whenever the user is being stopped or has been stopped. */
 
         LIST_HEAD(Session, sessions);
@@ -63,10 +70,11 @@ User *user_free(User *u);
 
 DEFINE_TRIVIAL_CLEANUP_FUNC(User *, user_free);
 
+int user_start_service_manager(User *u);
+int user_start(User *u);
+
 bool user_may_gc(User *u, bool drop_not_started);
 void user_add_to_gc_queue(User *u);
-void user_start_service_manager(User *u);
-int user_start(User *u);
 int user_stop(User *u, bool force);
 int user_finalize(User *u);
 UserState user_get_state(User *u);
