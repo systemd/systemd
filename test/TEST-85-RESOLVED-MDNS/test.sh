882fac220eb45264291d890b3e4784dbc146111f
Frantisek Sumsal <frantisek@sumsal.cz>
2024-01-16 19:36:50 +0100

    test: run the mDNS tests in a VM

--- test/TEST-85-RESOLVED-MDNS/test.sh
+++ test/TEST-85-RESOLVED-MDNS/test.sh
@@ -2,34 +2,11 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
 set -e
 
-TEST_DESCRIPTION="resolved-mdns testing"
-IMAGE_NAME="resolved_mdns"
-TEST_NO_QEMU=1
+TEST_DESCRIPTION="Test for systemd-resolved's mDNS functionality"
+IMAGE_NAME="resolved-mdns"
+TEST_NO_NSPAWN=1
 
 # shellcheck source=test/test-functions
 . "${TEST_BASE_DIR:?}/test-functions"
-
-_ORIG_NSPAWN="${SYSTEMD_NSPAWN:?}"
-SYSTEMD_NSPAWN="${STATEDIR:?}/run-nspawn"
-
-setup_nspawn_root_hook() {
-    cat >"${STATEDIR:?}/run-nspawn" <<EOF
-#!/bin/bash
-exec "${TEST_BASE_DIR:?}/test-resolved-mdns.py" -v -- "$_ORIG_NSPAWN" "\$@"
-exit 1
-EOF
-    chmod 755 "${STATEDIR:?}"/run-nspawn
-}
-
-test_append_files() {
-    local workspace="${1:?}"
-    # prevent shutdown in test suite, the expect script does that manually.
-    mkdir -p "${workspace:?}/etc/systemd/system/end.service.d"
-    cat >"$workspace/etc/systemd/system/end.service.d/99-override.conf" <<EOF
-[Service]
-ExecStart=
-ExecStart=/bin/true
-EOF
-}
 
 do_test "$@"
