58e7ef2e9b8c190e763f28ea3b02939e990e8588
Yu Watanabe <watanabe.yu+github@gmail.com>
2024-01-17 04:20:06 +0900

    namespace-util: rename arguments to prefix 'ret_'
    Also reorder opening namespace fd to make it match with the order of the
    arguments.

--- src/basic/namespace-util.h
+++ src/basic/namespace-util.h
@@ -22,7 +22,13 @@ extern const struct namespace_info {
         unsigned int clone_flag;
 } namespace_info[_NAMESPACE_TYPE_MAX + 1];
 
-int namespace_open(pid_t pid, int *pidns_fd, int *mntns_fd, int *netns_fd, int *userns_fd, int *root_fd);
+int namespace_open(
+                pid_t pid,
+                int *ret_pidns_fd,
+                int *ret_mntns_fd,
+                int *ret_netns_fd,
+                int *ret_userns_fd,
+                int *ret_root_fd);
 int namespace_enter(int pidns_fd, int mntns_fd, int netns_fd, int userns_fd, int root_fd);
 
 int fd_is_ns(int fd, unsigned long nsflag);
