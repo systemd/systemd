b7ddcc6921b9b8e30f36ab255baccf3d41930f8e
Ronan Pigott <ronan@rjp.ie>
2024-01-16 00:04:41 -0700

    network: Serialize DNR servers
    Implement serialization/deserialization for DNR servers. This re-uses
    the string format in place for user configuration of DoT servers, and as
    a consequence non-DoT servers are discarded when recording the link
    configuration, for correctness.
    
    This also enables sd-resolved to use these servers as it would other DNS
    servers.

--- src/libsystemd-network/sd-dhcp-lease.c
+++ src/libsystemd-network/sd-dhcp-lease.c
@@ -1276,6 +1276,14 @@ int dhcp_lease_save(sd_dhcp_lease *lease, const char *lease_file) {
                 fputc('\n', f);
         }
 
+        sd_dns_resolver *resolvers;
+        r = sd_dhcp_lease_get_dnr(lease, &resolvers);
+        if (r > 0) {
+                fputs("DNR=", f);
+                serialize_dnr(f, resolvers, r, NULL);
+                fputc('\n', f);
+        }
+
         r = sd_dhcp_lease_get_ntp(lease, &addresses);
         if (r > 0) {
                 fputs("NTP=", f);
@@ -1384,6 +1392,7 @@ int dhcp_lease_load(sd_dhcp_lease **ret, const char *lease_file) {
                 *next_server = NULL,
                 *broadcast = NULL,
                 *dns = NULL,
+                *dnr = NULL,
                 *ntp = NULL,
                 *sip = NULL,
                 *pop3 = NULL,
@@ -1421,6 +1430,7 @@ int dhcp_lease_load(sd_dhcp_lease **ret, const char *lease_file) {
                            "NEXT_SERVER", &next_server,
                            "BROADCAST", &broadcast,
                            "DNS", &dns,
+                           "DNR", &dnr,
                            "NTP", &ntp,
                            "SIP", &sip,
                            "POP3", &pop3,
@@ -1521,6 +1531,13 @@ int dhcp_lease_load(sd_dhcp_lease **ret, const char *lease_file) {
                         log_debug_errno(r, "Failed to deserialize DNS servers %s, ignoring: %m", dns);
                 else
                         lease->servers[SD_DHCP_LEASE_DNS].size = r;
+        }
+
+        if (dnr) {
+                r = deserialize_dnr(&lease->dnr, dnr);
+                if (r < 0)
+                        log_debug_errno(r, "Failed to deserialize DNR servers %s, ignoring: %m", dnr);
+                lease->n_dnr = r;
         }
 
         if (ntp) {
