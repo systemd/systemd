3976fd538f4c0212c99f6c32718fa6c70ff858f9
Ronan Pigott <ronan@rjp.ie>
2024-01-16 00:01:46 -0700

    network: parse RFC9463 DHCPv4 DNR option
    This option is another way for DHCP servers to indicate preferred DNS
    servers for the network, but includes more detailed info like the server
    name, transport (DoT/DoH/DoQ etc.), and port.
    
    Allow our DHCPv4 client to parse this option.

--- src/libsystemd-network/dhcp-lease-internal.h
+++ src/libsystemd-network/dhcp-lease-internal.h
@@ -55,6 +55,8 @@ struct sd_dhcp_lease {
 
         DHCPServerData servers[_SD_DHCP_LEASE_SERVER_TYPE_MAX];
 
+        ResolverData *resolvers;
+
         struct sd_dhcp_route *static_routes;
         size_t n_static_routes;
         struct sd_dhcp_route *classless_routes;
