373018b064bf5f6cb8ff89514be3f3bc6d821c21
Yu Watanabe <watanabe.yu+github@gmail.com>
2024-01-17 12:57:53 +0900

    TEST-13-NSPAWN: test for passing wlan interface to container

--- test/units/testsuite-13.nspawn.sh
+++ test/units/testsuite-13.nspawn.sh
@@ -332,7 +332,7 @@ nspawn_settings_cleanup() {
 }
 
 testcase_nspawn_settings() {
-    local root container dev private_users
+    local root container dev private_users wlan_names='' wlan_checks=''
 
     mkdir -p /run/systemd/nspawn
     root="$(mktemp -d /var/lib/machines/testsuite-13.nspawn-settings.XXX)"
@@ -340,6 +340,12 @@ testcase_nspawn_settings() {
     create_dummy_container "$root"
     rm -f "/etc/systemd/nspawn/$container.nspawn"
     mkdir -p "$root/tmp" "$root"/opt/{tmp,inaccessible,also-inaccessible}
+
+    # add virtual wlan interfaces
+    if modprobe mac80211_hwsim radios=2; then
+        wlan_names='wlan0 wlan1:wl-renamed1'
+        wlan_checks='ip link | grep wlan0\nip link | grep wl-renamed1'
+    fi
 
     for dev in sd-host-only sd-shared{1,2,3} sd-macvlan{1,2} sd-macvlanloong sd-ipvlan{1,2} sd-ipvlanlooong; do
         ip link add "$dev" type dummy
@@ -395,7 +401,7 @@ Private=yes
 VirtualEthernet=yes
 VirtualEthernetExtra=my-fancy-veth1
 VirtualEthernetExtra=fancy-veth2:my-fancy-veth2
-Interface=sd-shared1 sd-shared2:sd-renamed2 sd-shared3:sd-altname3
+Interface=sd-shared1 sd-shared2:sd-renamed2 sd-shared3:sd-altname3 ${wlan_names}
 MACVLAN=sd-macvlan1 sd-macvlan2:my-macvlan2 sd-macvlanloong
 IPVLAN=sd-ipvlan1 sd-ipvlan2:my-ipvlan2 sd-ipvlanlooong
 Zone=sd-zone0
@@ -447,6 +453,8 @@ ip link | grep my-macvlan2@
 ip link | grep iv-sd-ipvlan1@
 ip link | grep my-ipvlan2@
 EOF
+    echo -e "$wlan_checks" >>"$root/entrypoint.sh"
+
     timeout 30 systemd-nspawn --directory="$root"
 
     # And now for stuff that needs to run separately
