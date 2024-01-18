18c298b184027b1af63154b7479ddd9870e175c0
Ronan Pigott <ronan@rjp.ie>
2024-01-17 19:27:21 -0700

    network: Serialize DHCPv6 DNR servers
    This serializes DNR servers acquired by V6_DNR option, equivalent to the
    V4_DNR option.

--- src/network/networkd-state-file.c
+++ src/network/networkd-state-file.c
@@ -143,6 +143,26 @@ static int link_put_dns(Link *link, OrderedSet **s) {
                 }
         }
 
+        if (link->dhcp6_lease && network_dhcp6_use_dnr(link->network)) {
+                sd_dns_resolver *resolvers;
+
+                r = sd_dhcp6_lease_get_dnr(link->dhcp6_lease, &resolvers);
+                if (r >= 0 ) {
+                        struct in_addr_full **dot_servers;
+                        size_t n = 0;
+                        CLEANUP_ARRAY(dot_servers, n, in_addr_full_array_free);
+
+                        r = sd_dns_resolvers_to_dot_addrs(resolvers, r, &dot_servers, &n);
+                        if (r < 0)
+                                return r;
+
+                        r = ordered_set_put_dns_servers(s, link->ifindex, dot_servers, n);
+                        if (r < 0)
+                                return r;
+                }
+
+        }
+
         if (link->network->ndisc_use_dns) {
                 NDiscRDNSS *a;
 
@@ -553,7 +573,9 @@ static void serialize_resolvers(
                 const char *lvalue,
                 bool *space,
                 sd_dhcp_lease *lease,
-                bool conditional) {
+                bool conditional,
+                sd_dhcp6_lease *lease6,
+                bool conditional6) {
 
         bool _space = false;
         if (!space)
@@ -568,6 +590,20 @@ static void serialize_resolvers(
                 int r;
 
                 r = sd_dhcp_lease_get_dnr(lease, &resolvers);
+                if (r < 0)
+                        return;
+
+                r = sd_dns_resolvers_to_dot_strv(resolvers, r, &names);
+                if (r > 0)
+                        fputstrv(f, names, NULL, space);
+        }
+
+        if (lease6 && conditional6) {
+                sd_dns_resolver *resolvers;
+                _cleanup_strv_free_ char **names = NULL;
+                int r;
+
+                r = sd_dhcp6_lease_get_dnr(lease6, &resolvers);
                 if (r < 0)
                         return;
 
@@ -722,7 +758,9 @@ static int link_save(Link *link) {
                          * assumed. */
                         serialize_resolvers(f, NULL, &space,
                                             link->dhcp_lease,
-                                            network_dhcp_use_dnr(link->network));
+                                            network_dhcp_use_dnr(link->network),
+                                            link->dhcp6_lease,
+                                            network_dhcp6_use_dnr(link->network));
 
                         serialize_addresses(f, NULL, &space,
                                             NULL,
