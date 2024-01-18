d38dd40e926e1168e1390424199553ea5acd5187
Ronan Pigott <ronan@rjp.ie>
2024-01-17 18:16:47 -0700

    network: Introduce UseDNR DHCPv6 option
    This is equivalent to the DHCPv4 option introduced earlier.

--- src/network/networkd-network.c
+++ src/network/networkd-network.c
@@ -407,6 +407,7 @@ int network_load_one(Manager *manager, OrderedHashmap **networks, const char *fi
                 .dhcp6_use_address = true,
                 .dhcp6_use_pd_prefix = true,
                 .dhcp6_use_dns = -1,
+                .dhcp6_use_dnr = -1,
                 .dhcp6_use_domains = _USE_DOMAINS_INVALID,
                 .dhcp6_use_hostname = true,
                 .dhcp6_use_ntp = -1,
