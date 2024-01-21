7a0b79a0bd78903269d6a5741287102f2003efcb
Yu Watanabe <watanabe.yu+github@gmail.com>
2024-01-21 13:14:15 +0900

    test: check pam warning message

--- test/units/end.sh
+++ test/units/end.sh
@@ -5,6 +5,7 @@ set -eux
 set -o pipefail
 
 (! journalctl -q -o short-monotonic --grep "didn't pass validation" >>/failed)
+(! journalctl -q -o short-monotonic --grep "Attempted to close sd-bus after fork, this should not happen." >>/failed)
 
 systemctl poweroff --no-block
 exit 0
