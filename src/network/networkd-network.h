b3552eb45f639d61893d06bc56a766b104275f51
Ronan Pigott <ronan@rjp.ie>
2024-01-22 14:37:42 -0700

    network: Introduce IPv6RA UseDNR= option
    Same as the DHCP v4/v6 options, this controls the use of DNR received
    from ipv6ra.

--- src/network/networkd-network.h
+++ src/network/networkd-network.h
@@ -339,6 +339,7 @@ struct Network {
 
         /* NDisc support */
         int ndisc;
+        int ndisc_use_dnr;
         bool ndisc_use_dns;
         bool ndisc_use_gateway;
         bool ndisc_use_route_prefix;
@@ -422,6 +423,10 @@ static inline int network_dhcp_use_dnr(Network *network) {
 static inline int network_dhcp6_use_dnr(Network *network) {
         assert(network);
         return network->dhcp6_use_dnr < 0 ? network->dhcp6_use_dns : network->dhcp6_use_dnr;
+}
+static inline int network_ndisc_use_dnr(Network *network) {
+        assert(network);
+        return network->ndisc_use_dnr < 0 ? network->ndisc_use_dns : network->ndisc_use_dnr;
 }
 
 int manager_build_dhcp_pd_subnet_ids(Manager *manager);
