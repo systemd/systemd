fe26c71b2706abc8321e52262c1be664fd817333
Lennart Poettering <lennart@poettering.net>
2024-01-23 10:22:27 +0100

    varlink: add varlink_peek_dup_fd() helper
    This new call is like varlink_peek_fd() (i.e. gets an fd out of the
    connection but leaving it also in there), and combines ith with
    F_DUPFD_CLOEXEC to make a copy of it.
    
    We previously already had varlink_dup_fd() which was a duplicating
    version for pushing an fd *into* the connection. To reduce confusion,
    let's rename that one varlink_push_dup_fd() to make the symmetry to
    valrink_push_fd() clear so that we have no:
    
    varlink_peer_push_fd()        → put fd in without dup'ing
    varlink_peer_push_dup_fd()    → same with F_DUPFD_CLOEXEC
    varlink_peer_peek_fd()        → get fd out without dup'ing
    varlink_peer_peek_dup_fd()    → same with F_DUPFD_CLOEXEC

--- src/shared/varlink.c
+++ src/shared/varlink.c
@@ -3089,7 +3089,7 @@ int varlink_push_fd(Varlink *v, int fd) {
         return i;
 }
 
-int varlink_dup_fd(Varlink *v, int fd) {
+int varlink_push_dup_fd(Varlink *v, int fd) {
         _cleanup_close_ int dp = -1;
         int r;
 
@@ -3135,6 +3135,16 @@ int varlink_peek_fd(Varlink *v, size_t i) {
                 return -ENXIO;
 
         return v->input_fds[i];
+}
+
+int varlink_peek_dup_fd(Varlink *v, size_t i) {
+        int fd;
+
+        fd = varlink_peek_fd(v, i);
+        if (fd < 0)
+                return fd;
+
+        return RET_NERRNO(fcntl(fd, F_DUPFD_CLOEXEC, 3));
 }
 
 int varlink_take_fd(Varlink *v, size_t i) {
