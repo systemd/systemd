c6ee9484304dcc7167fea7fa1fceab944709ec92
Yu Watanabe <watanabe.yu+github@gmail.com>
2024-01-24 14:52:46 +0900

    core/exec-invoke: use PidRef at one more place
    No functional change, just refactoring.

--- src/core/exec-invoke.c
+++ src/core/exec-invoke.c
@@ -1101,7 +1101,6 @@ static int setup_pam(
         pam_handle_t *handle = NULL;
         int pam_code = PAM_SUCCESS, r;
         bool close_session = false;
-        pid_t parent_pid;
         int flags = 0;
 
         assert(name);
@@ -1114,6 +1113,11 @@ static int setup_pam(
          * session again. The parent process will exec() the actual
          * daemon. We do things this way to ensure that the main PID
          * of the daemon is the one we initially fork()ed. */
+
+        _cleanup_(pidref_done) PidRef parent_pid = PIDREF_NULL;
+        r = pidref_set_self(&parent_pid);
+        if (r < 0)
+                goto fail;
 
         r = barrier_create(&barrier);
         if (r < 0)
@@ -1170,8 +1174,6 @@ static int setup_pam(
                 goto fail;
         }
 
-        parent_pid = getpid_cached();
-
         r = safe_fork("(sd-pam)", FORK_DEATHSIG_SIGTERM, NULL);
         if (r < 0)
                 goto fail;
@@ -1209,7 +1211,7 @@ static int setup_pam(
                 (void) barrier_place(&barrier);
 
                 /* Check if our parent process might already have died? */
-                if (getppid() == parent_pid) {
+                if (getppid() == parent_pid.pid) {
                         sigset_t ss;
                         int sig;
 
@@ -1221,7 +1223,7 @@ static int setup_pam(
                 }
 
                 /* If our parent died we'll end the session */
-                if (getppid() != parent_pid) {
+                if (getppid() != parent_pid.pid) {
                         pam_code = pam_close_session_and_delete_credentials(handle, flags);
                         if (pam_code != PAM_SUCCESS)
                                 goto child_finish;
