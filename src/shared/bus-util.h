4e0177b2062f7aa7defdc0f0e6e09066634b5c2d
Lennart Poettering <lennart@poettering.net>
2024-01-26 18:40:09 +0100

    bus-util: add helper for getting PidRef structs from bus
    This adds two helpers: one for extracting a PidRef from an sd_bus_creds
    object, and one from doing this from and sd_bus_message object.

--- src/shared/bus-util.h
+++ src/shared/bus-util.h
@@ -11,6 +11,7 @@
 
 #include "errno-util.h"
 #include "macro.h"
+#include "pidref.h"
 #include "runtime-scope.h"
 #include "set.h"
 #include "string-util.h"
@@ -73,3 +74,6 @@ extern const struct hash_ops bus_message_hash_ops;
 int bus_message_append_string_set(sd_bus_message *m, Set *s);
 
 int bus_property_get_string_set(sd_bus *bus, const char *path, const char *interface, const char *property, sd_bus_message *reply, void *userdata, sd_bus_error *error);
+
+int bus_creds_get_pidref(sd_bus_creds *c, PidRef *ret);
+int bus_query_sender_pidref(sd_bus_message *m, PidRef *ret);
