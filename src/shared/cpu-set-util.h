7b5c452f0124a076cffc699fc42da9d2235fd68e
Brennan Lamoreaux <brennan.lamoreaux@broadcom.com>
2024-01-30 00:33:11 +0000

    core: add option for DefaultAllowedCPUs
    While "AllowedCPUs" can be specified for individual units, there
    is not an overarching default that can be specified. Adding a default
    option for AllowedCPUs allows easier control over cpu affinity of
    all processes running under systemd (essentially all userspace processes).
    
    Previously this same effect could be achieved by setting the global
    "CPUAffinity" parameter, however this is no longer effective with
    cgroups v2. When processes are placed inside different cgroups while/and
    the cpuset controller is activated, even if no CPUs are specified,
    the per-process cpu affinity is obliterated by the kernel function "cpuset_attach_task()".
    
    Theoretically, this problem would also be fixed by manually assigning
    a per-process affinity after each process has been placed in its' cgroup.
    However, it seems to me a better solution is to control CPU placement through
    the control group hierarchy.
    
    In order to allow local control, the default is only applied if AllowedCPUs
    is not specified for the unit. The unit cpuset is not merged with the default.

--- src/shared/cpu-set-util.h
+++ src/shared/cpu-set-util.h
@@ -22,6 +22,8 @@ static inline void cpu_set_reset(CPUSet *a) {
 int cpu_set_add_all(CPUSet *a, const CPUSet *b);
 int cpu_set_add(CPUSet *a, unsigned cpu);
 
+int cpu_set_copy(CPUSet *dst, CPUSet *src);
+
 char* cpu_set_to_string(const CPUSet *a);
 char *cpu_set_to_range_string(const CPUSet *a);
 int cpu_set_realloc(CPUSet *cpu_set, unsigned ncpus);
