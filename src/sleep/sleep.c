2311fcdc69aa68ddc6a55a9b506f8e253dd481a9
Lennart Poettering <lennart@poettering.net>
2024-01-30 11:32:41 +0100

    sleep: upgrade some unexpected errors to LOG_WARNING log messages

--- src/sleep/sleep.c
+++ src/sleep/sleep.c
@@ -484,18 +484,18 @@ static int execute_s2h(const SleepConfig *sleep_config) {
 
         r = freeze_thaw_user_slice(&(const char*) { "FreezeUnit" });
         if (r < 0)
-                log_debug_errno(r, "Failed to freeze unit user.slice, ignoring: %m");
+                log_warning_errno(r, "Failed to freeze unit user.slice, ignoring: %m");
 
         /* Only check if we have automated battery alarms if HibernateDelaySec= is not set, as in that case
          * we'll busy poll for the configured interval instead */
         if (!timestamp_is_set(sleep_config->hibernate_delay_usec)) {
                 r = check_wakeup_type();
                 if (r < 0)
-                        log_debug_errno(r, "Failed to check hardware wakeup type, ignoring: %m");
+                        log_warning_errno(r, "Failed to check hardware wakeup type, ignoring: %m");
                 else {
                         r = battery_trip_point_alarm_exists();
                         if (r < 0)
-                                log_debug_errno(r, "Failed to check whether acpi_btp support is enabled or not, ignoring: %m");
+                                log_warning_errno(r, "Failed to check whether acpi_btp support is enabled or not, ignoring: %m");
                 }
         } else
                 r = 0;  /* Force fallback path */
