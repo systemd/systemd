65a22768199640f07557ff76f1f6d6e026b09274
Lennart Poettering <lennart@poettering.net>
2024-01-30 14:50:35 +0100

    libkmod: turn into dlopen() dependency
    As it turns out libkmod has quite a bunch of deps, including various
    compressing libs and similar. By turning this into a dlopen()
    dependency, we can make our depchain during install time quite a bit
    smaller. In particular as inside of containers kmod doesn't help anyway
    as CAP_SYS_MODULE is not available anyway.
    
    While we are at it, also share the code that sets up logging/kmod
    context.
    
    After:
    
    $ lddtree ./build/systemd
    systemd => ./build/systemd (interpreter => /lib64/ld-linux-x86-64.so.2)
        libsystemd-core-255.so => ./build/src/core/libsystemd-core-255.so
            libaudit.so.1 => /lib64/libaudit.so.1
                libcap-ng.so.0 => /lib64/libcap-ng.so.0
                    ld-linux-x86-64.so.2 => /lib64/ld-linux-x86-64.so.2
            libm.so.6 => /lib64/libm.so.6
            libmount.so.1 => /lib64/libmount.so.1
                libblkid.so.1 => /lib64/libblkid.so.1
            libseccomp.so.2 => /lib64/libseccomp.so.2
            libselinux.so.1 => /lib64/libselinux.so.1
                libpcre2-8.so.0 => /lib64/libpcre2-8.so.0
        libsystemd-shared-255.so => /home/lennart/projects/systemd/build/src/shared/libsystemd-shared-255.so
            libacl.so.1 => /lib64/libacl.so.1
                libattr.so.1 => /lib64/libattr.so.1
            libcap.so.2 => /lib64/libcap.so.2
            libcrypt.so.2 => /lib64/libcrypt.so.2
            libgcrypt.so.20 => /lib64/libgcrypt.so.20
                libgpg-error.so.0 => /lib64/libgpg-error.so.0
            liblz4.so.1 => /lib64/liblz4.so.1
            libcrypto.so.3 => /lib64/libcrypto.so.3
                libz.so.1 => /lib64/libz.so.1
            libpam.so.0 => /lib64/libpam.so.0
                libeconf.so.0 => /lib64/libeconf.so.0
            liblzma.so.5 => /lib64/liblzma.so.5
            libzstd.so.1 => /lib64/libzstd.so.1
        libc.so.6 => /lib64/libc.so.6
    
    Before:
    
    $ lddtree ./build/systemd
    systemd => ./build/systemd (interpreter => /lib64/ld-linux-x86-64.so.2)
        libsystemd-core-255.so => ./build/src/core/libsystemd-core-255.so
            libaudit.so.1 => /lib64/libaudit.so.1
                libcap-ng.so.0 => /lib64/libcap-ng.so.0
                    ld-linux-x86-64.so.2 => /lib64/ld-linux-x86-64.so.2
            libkmod.so.2 => /lib64/libkmod.so.2
                libzstd.so.1 => /lib64/libzstd.so.1
                liblzma.so.5 => /lib64/liblzma.so.5
                libz.so.1 => /lib64/libz.so.1
                libcrypto.so.3 => /lib64/libcrypto.so.3
                libgcc_s.so.1 => /lib64/libgcc_s.so.1
            libm.so.6 => /lib64/libm.so.6
            libmount.so.1 => /lib64/libmount.so.1
                libblkid.so.1 => /lib64/libblkid.so.1
            libseccomp.so.2 => /lib64/libseccomp.so.2
            libselinux.so.1 => /lib64/libselinux.so.1
                libpcre2-8.so.0 => /lib64/libpcre2-8.so.0
        libsystemd-shared-255.so => /home/lennart/projects/systemd/build/src/shared/libsystemd-shared-255.so
            libacl.so.1 => /lib64/libacl.so.1
                libattr.so.1 => /lib64/libattr.so.1
            libcap.so.2 => /lib64/libcap.so.2
            libcrypt.so.2 => /lib64/libcrypt.so.2
            libgcrypt.so.20 => /lib64/libgcrypt.so.20
                libgpg-error.so.0 => /lib64/libgpg-error.so.0
            liblz4.so.1 => /lib64/liblz4.so.1
            libpam.so.0 => /lib64/libpam.so.0
                libeconf.so.0 => /lib64/libeconf.so.0
        libc.so.6 => /lib64/libc.so.6

--- src/shared/module-util.h
+++ src/shared/module-util.h
@@ -1,12 +1,56 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 #pragma once
 
+#include "dlfcn-util.h"
+
+#if HAVE_KMOD
+
 #include <libkmod.h>
 
 #include "macro.h"
 
-DEFINE_TRIVIAL_CLEANUP_FUNC(struct kmod_ctx*, kmod_unref);
-DEFINE_TRIVIAL_CLEANUP_FUNC(struct kmod_module*, kmod_module_unref);
-DEFINE_TRIVIAL_CLEANUP_FUNC_FULL(struct kmod_list*, kmod_module_unref_list, NULL);
+DLSYM_PROTOTYPE(kmod_list_next);
+DLSYM_PROTOTYPE(kmod_load_resources);
+DLSYM_PROTOTYPE(kmod_module_get_initstate);
+DLSYM_PROTOTYPE(kmod_module_get_module);
+DLSYM_PROTOTYPE(kmod_module_get_name);
+DLSYM_PROTOTYPE(kmod_module_new_from_lookup);
+DLSYM_PROTOTYPE(kmod_module_probe_insert_module);
+DLSYM_PROTOTYPE(kmod_module_unref);
+DLSYM_PROTOTYPE(kmod_module_unref_list);
+DLSYM_PROTOTYPE(kmod_new);
+DLSYM_PROTOTYPE(kmod_set_log_fn);
+DLSYM_PROTOTYPE(kmod_unref);
+DLSYM_PROTOTYPE(kmod_validate_resources);
+
+int dlopen_libkmod(void);
+
+DEFINE_TRIVIAL_CLEANUP_FUNC(struct kmod_ctx*, sym_kmod_unref);
+DEFINE_TRIVIAL_CLEANUP_FUNC(struct kmod_module*, sym_kmod_module_unref);
+DEFINE_TRIVIAL_CLEANUP_FUNC_FULL(struct kmod_list*, sym_kmod_module_unref_list, NULL);
+
+#define sym_kmod_list_foreach(list_entry, first_entry) \
+        for (list_entry = first_entry; \
+                list_entry != NULL; \
+                list_entry = sym_kmod_list_next(first_entry, list_entry))
 
 int module_load_and_warn(struct kmod_ctx *ctx, const char *module, bool verbose);
+int module_setup_context(struct kmod_ctx **ret);
+
+#else
+
+struct kmod_ctx;
+
+static inline int dlopen_libkmod(void) {
+        return -EOPNOTSUPP;
+}
+
+static inline int module_setup_context(struct kmod_ctx **ret) {
+        return -EOPNOTSUPP;
+}
+
+static inline int module_load_and_warn(struct kmod_ctx *ctx, const char *module, bool verbose) {
+        return -EOPNOTSUPP;
+}
+
+#endif
