bb1a391fe8db0c0218a3b599f6702f41d01f010d
Adrian Vovk <adrianvovk@gmail.com>
2024-02-01 13:35:03 -0500

    homework: Implement offline updates
    This makes it possible to update a home record (and blob directory) of a
    home area that's either completely absent (i.e. on a USB stick that's
    unplugged) or just inaccessible due to lack of authentication

--- src/home/homed-home-bus.c
+++ src/home/homed-home-bus.c
@@ -6,6 +6,7 @@
 #include "bus-polkit.h"
 #include "fd-util.h"
 #include "format-util.h"
+#include "home-util.h"
 #include "homed-bus.h"
 #include "homed-home-bus.h"
 #include "homed-home.h"
@@ -432,8 +433,8 @@ int bus_home_method_update_record(
         if (r < 0)
                 return r;
 
-        if (flags != 0)
-                return sd_bus_error_setf(error, SD_BUS_ERROR_NOT_SUPPORTED, "Provided flags are unsupported.");
+        if ((flags & ~SD_HOMED_UPDATE_FLAGS_ALL) != 0)
+                return sd_bus_error_setf(error, SD_BUS_ERROR_INVALID_ARGS, "Invalid flags provided.");
 
         r = home_verify_polkit_async(
                         h,
@@ -456,6 +457,8 @@ int bus_home_method_update_record(
         r = home_set_current_message(h, message);
         if (r < 0)
                 return r;
+
+        h->current_operation->call_flags = flags;
 
         return 1;
 }
