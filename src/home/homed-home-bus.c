8824fb3fb2d462f5083ab27cb4d4c985ee6afab2
Adrian Vovk <adrianvovk@gmail.com>
2024-02-01 22:53:09 -0500

    homed: Allow unpriv user to change parts of record
    This introduces an allowlist of fields that an unprivileged user that is
    active at the console is allowed to change about their own user record
    without needing authentication from a system administrator.
    
    Similarly, an unprivileged user that is active at the console is now
    allowed to change their own password.
    
    Should an administrator wish to disable this behavior, they can do so by
    changing the policy of the org.freedesktop.home1.update-own-home and
    o.f.h.passwd-own-home Polkit actions.

--- src/home/homed-home-bus.c
+++ src/home/homed-home-bus.c
@@ -380,7 +380,7 @@ int bus_home_method_update_record(
                 Hashmap *blobs,
                 uint64_t flags,
                 sd_bus_error *error) {
-        int r;
+        int r, safe;
 
         assert(h);
         assert(message);
@@ -393,9 +393,31 @@ int bus_home_method_update_record(
         if ((flags & ~SD_HOMED_UPDATE_FLAGS_ALL) != 0)
                 return sd_bus_error_setf(error, SD_BUS_ERROR_INVALID_ARGS, "Invalid flags provided.");
 
+        if (blobs) {
+                const char *failed = NULL;
+                r = user_record_ensure_blob_manifest(hr, blobs, &failed);
+                if (r == -EINVAL)
+                        return sd_bus_error_set(error, SD_BUS_ERROR_INVALID_ARGS, "Provided blob files do not correspond to blob manifest.");
+                if (r < 0)
+                        return sd_bus_error_set_errnof(error, r, "Failed to generate hash for blob %s: %m", strnull(failed));
+        }
+
+        safe = user_record_changes_are_safe(h->record, hr);
+        if (safe < 0) {
+                log_warning_errno(safe, "Failed to determine if changes to user record are safe, assuming not: %m");
+                safe = false;
+        } else if (safe) {
+                safe = bus_home_client_is_trusted(h, message);
+                if (safe < 0) {
+                        log_warning_errno(safe, "Failed to determine whether client is trusted, assuming not: %m");
+                        safe = false;
+                }
+        }
+
         r = bus_verify_polkit_async(
                         message,
-                        "org.freedesktop.home1.update-home",
+                        safe ? "org.freedesktop.home1.update-own-home"
+                             : "org.freedesktop.home1.update-home",
                         /* details= */ NULL,
                         &h->manager->polkit_registry,
                         error);
@@ -502,7 +524,7 @@ int bus_home_method_change_password(
 
         _cleanup_(user_record_unrefp) UserRecord *new_secret = NULL, *old_secret = NULL;
         Home *h = ASSERT_PTR(userdata);
-        int r;
+        int r, trusted;
 
         assert(message);
 
@@ -514,9 +536,16 @@ int bus_home_method_change_password(
         if (r < 0)
                 return r;
 
+        trusted = bus_home_client_is_trusted(h, message);
+        if (trusted < 0) {
+                log_warning_errno(trusted, "Failed to determine whether client is trusted, assuming not: %m");
+                trusted = false;
+        }
+
         r = bus_verify_polkit_async_full(
                         message,
-                        "org.freedesktop.home1.passwd-home",
+                        trusted ? "org.freedesktop.home1.passwd-own-home"
+                                : "org.freedesktop.home1.passwd-home",
                         /* details= */ NULL,
                         /* interactive= */ false,
                         h->uid,
