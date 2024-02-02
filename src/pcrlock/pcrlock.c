ce5fe16c01af154f68e1ba902fe2276a02e47d7d
Lennart Poettering <lennart@poettering.net>
2024-02-02 13:30:43 +0100

    pcrlock: use log_setup()

--- src/pcrlock/pcrlock.c
+++ src/pcrlock/pcrlock.c
@@ -5109,9 +5109,7 @@ static int pcrlock_main(int argc, char *argv[]) {
 static int run(int argc, char *argv[]) {
         int r;
 
-        log_show_color(true);
-        log_parse_environment();
-        log_open();
+        log_setup();
 
         r = parse_argv(argc, argv);
         if (r <= 0)
