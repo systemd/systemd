04124ad11ec774d6e6ead693e8d72f42ed7cce4f
Lennart Poettering <lennart@poettering.net>
2024-02-07 17:15:20 +0100

    ptyfwd: automatically turn off tinting/window title logic on dumb terminals
    If we are not talking to a reasonable terminal let's not try to set the
    window title or tint the background.

--- src/shared/ptyfwd.h
+++ src/shared/ptyfwd.h
@@ -10,13 +10,17 @@
 typedef struct PTYForward PTYForward;
 
 typedef enum PTYForwardFlags {
-        PTY_FORWARD_READ_ONLY = 1,
+        /* Only output to STDOUT, never try to read from STDIN */
+        PTY_FORWARD_READ_ONLY              = 1 << 0,
 
         /* Continue reading after hangup? */
-        PTY_FORWARD_IGNORE_VHANGUP = 2,
+        PTY_FORWARD_IGNORE_VHANGUP         = 1 << 1,
 
         /* Continue reading after hangup but only if we never read anything else? */
-        PTY_FORWARD_IGNORE_INITIAL_VHANGUP = 4,
+        PTY_FORWARD_IGNORE_INITIAL_VHANGUP = 1 << 2,
+
+        /* Don't tint the background, or set window title */
+        PTY_FORWARD_DUMB_TERMINAL          = 1 << 3,
 } PTYForwardFlags;
 
 typedef int (*PTYForwardHandler)(PTYForward *f, int rcode, void *userdata);
