79ec39958d70e3eeb141f7ca1f57ea52533727b6
Lennart Poettering <lennart@poettering.net>
2024-02-12 17:30:31 +0100

    bootctl: add a Varlink interface
    For now, just super basic functionality: return the list of boot menu
    entries, and read/write the reboot to firmware flag

--- src/boot/bootctl-status.h
+++ src/boot/bootctl-status.h
@@ -1,5 +1,9 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 
+#include "varlink.h"
+
 int verb_status(int argc, char *argv[], void *userdata);
 int verb_list(int argc, char *argv[], void *userdata);
 int verb_unlink(int argc, char *argv[], void *userdata);
+
+int vl_method_list_boot_entries(Varlink *link, JsonVariant *parameters, VarlinkMethodFlags flags, void *userdata);
