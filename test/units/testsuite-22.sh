039c25cfc4e8c3085e11500f37a5c6c01ebd7b56
Richard Maw <richard.maw@codethink.co.uk>
2024-02-13 16:21:41 +0000

    TEST-22-TMPFILES: mount /usr overlay if necessary

--- test/units/testsuite-22.sh
+++ test/units/testsuite-22.sh
@@ -5,6 +5,12 @@ set -o pipefail
 
 # shellcheck source=test/units/test-control.sh
 . "$(dirname "$0")"/test-control.sh
+# shellcheck source=test/units/util.sh
+. "$(dirname "$0")"/util.sh
+
+# Needed to write /usr/lib/tmpfiles.d to test /etc overrides in testsuite-22.13.sh
+maybe_mount_usr_overlay
+trap 'maybe_umount_usr_overlay' EXIT
 
 run_subtests
 
