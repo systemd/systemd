d19181c04ba4eb561ac63edf569daf5e4151c664
Renjaya Raga Zenta <ragazenta@gmail.com>
2024-02-13 21:15:24 +0700

    udevd: Add ReceivePacketSteeringCPUMask for systemd.link
    Takes a list of CPU indices or ranges separated by either whitespace or commas. Alternatively,
    takes a special "all" value in which will include all available CPUs in the mask.
    CPU ranges are specified by the lower and upper CPU indices separated by a dash. This option may
    be specified more than once, in which case the specified CPU affinity masks are merged. If the
    empty string is assigned, the mask is reset, all assignments prior to this will have no effect.
    Defaults to unset and RPS CPU list is unchanged.
    
    Currently, this will set CPU mask to all `rx` queue of matched device (if it has multiple queues).
    
    The `/sys/class/net/<dev>/queues/rx-<n>/rps_cpus` only accept cpu bitmap mask in hexadecimal.
    
    Fix: #30323

--- src/test/test-cpu-set-util.c
+++ src/test/test-cpu-set-util.c
@@ -25,6 +25,10 @@ TEST(parse_cpu_set) {
         log_info("cpu_set_to_range_string: %s", str);
         assert_se(streq(str, "0"));
         str = mfree(str);
+        assert_se(str = cpu_set_to_mask_string(&c));
+        log_info("cpu_set_to_mask_string: %s", str);
+        assert_se(streq(str, "1"));
+        str = mfree(str);
         cpu_set_reset(&c);
 
         /* Simple range (from CPUAffinity example) */
@@ -42,6 +46,10 @@ TEST(parse_cpu_set) {
         assert_se(str = cpu_set_to_range_string(&c));
         log_info("cpu_set_to_range_string: %s", str);
         assert_se(streq(str, "1-2 4"));
+        str = mfree(str);
+        assert_se(str = cpu_set_to_mask_string(&c));
+        log_info("cpu_set_to_mask_string: %s", str);
+        assert_se(streq(str, "16"));
         str = mfree(str);
         cpu_set_reset(&c);
 
@@ -61,6 +69,10 @@ TEST(parse_cpu_set) {
         log_info("cpu_set_to_range_string: %s", str);
         assert_se(streq(str, "0-3 8-11"));
         str = mfree(str);
+        assert_se(str = cpu_set_to_mask_string(&c));
+        log_info("cpu_set_to_mask_string: %s", str);
+        assert_se(streq(str, "f0f"));
+        str = mfree(str);
         cpu_set_reset(&c);
 
         /* Quoted strings */
@@ -75,6 +87,10 @@ TEST(parse_cpu_set) {
         assert_se(str = cpu_set_to_range_string(&c));
         log_info("cpu_set_to_range_string: %s", str);
         assert_se(streq(str, "8-11"));
+        str = mfree(str);
+        assert_se(str = cpu_set_to_mask_string(&c));
+        log_info("cpu_set_to_mask_string: %s", str);
+        assert_se(streq(str, "f00"));
         str = mfree(str);
         cpu_set_reset(&c);
 
@@ -106,6 +122,10 @@ TEST(parse_cpu_set) {
         log_info("cpu_set_to_range_string: %s", str);
         assert_se(streq(str, "0-7 63"));
         str = mfree(str);
+        assert_se(str = cpu_set_to_mask_string(&c));
+        log_info("cpu_set_to_mask_string: %s", str);
+        assert_se(streq(str, "80000000000000ff"));
+        str = mfree(str);
         cpu_set_reset(&c);
 
         /* Ranges */
@@ -136,12 +156,20 @@ TEST(parse_cpu_set) {
         log_info("cpu_set_to_range_string: %s", str);
         assert_se(streq(str, "0-3 8-11"));
         str = mfree(str);
+        assert_se(str = cpu_set_to_mask_string(&c));
+        log_info("cpu_set_to_mask_string: %s", str);
+        assert_se(streq(str, "f0f"));
+        str = mfree(str);
         cpu_set_reset(&c);
 
         /* Negative range (returns empty cpu_set) */
         assert_se(parse_cpu_set_full("3-0", &c, true, NULL, "fake", 1, "CPUAffinity") >= 0);
         assert_se(c.allocated >= DIV_ROUND_UP(sizeof(__cpu_mask), 8));
         assert_se(CPU_COUNT_S(c.allocated, c.set) == 0);
+        assert_se(str = cpu_set_to_mask_string(&c));
+        log_info("cpu_set_to_mask_string: %s", str);
+        assert_se(streq(str, "0"));
+        str = mfree(str);
         cpu_set_reset(&c);
 
         /* Overlapping ranges */
@@ -156,6 +184,10 @@ TEST(parse_cpu_set) {
         assert_se(str = cpu_set_to_range_string(&c));
         log_info("cpu_set_to_range_string: %s", str);
         assert_se(streq(str, "0-11"));
+        str = mfree(str);
+        assert_se(str = cpu_set_to_mask_string(&c));
+        log_info("cpu_set_to_mask_string: %s", str);
+        assert_se(streq(str, "fff"));
         str = mfree(str);
         cpu_set_reset(&c);
 
@@ -174,6 +206,10 @@ TEST(parse_cpu_set) {
         log_info("cpu_set_to_range_string: %s", str);
         assert_se(streq(str, "0 2 4-11"));
         str = mfree(str);
+        assert_se(str = cpu_set_to_mask_string(&c));
+        log_info("cpu_set_to_mask_string: %s", str);
+        assert_se(streq(str, "ff5"));
+        str = mfree(str);
         cpu_set_reset(&c);
 
         /* Garbage */
@@ -190,6 +226,10 @@ TEST(parse_cpu_set) {
         assert_se(parse_cpu_set_full("", &c, true, NULL, "fake", 1, "CPUAffinity") == 0);
         assert_se(!c.set);                /* empty string returns NULL */
         assert_se(c.allocated == 0);
+        assert_se(str = cpu_set_to_mask_string(&c));
+        log_info("cpu_set_to_mask_string: %s", str);
+        assert_se(streq(str, "0"));
+        str = mfree(str);
 
         /* Runaway quoted string */
         assert_se(parse_cpu_set_full("0 1 2 3 \"4 5 6 7 ", &c, true, NULL, "fake", 1, "CPUAffinity") == -EINVAL);
@@ -205,6 +245,15 @@ TEST(parse_cpu_set) {
         assert_se(str = cpu_set_to_range_string(&c));
         log_info("cpu_set_to_range_string: %s", str);
         assert_se(streq(str, "8000-8191"));
+        str = mfree(str);
+        assert_se(str = cpu_set_to_mask_string(&c));
+        log_info("cpu_set_to_mask_string: %s", str);
+        for (size_t i = 0; i < strlen(str); i++) {
+                if (i < 48)
+                        assert_se(str[i] == 'f');
+                else
+                        assert_se(str[i] == '0');
+        }
         str = mfree(str);
         cpu_set_reset(&c);
 }
