dbb9a6ad6952685c58352e9344976a7d4d21a2e5
Daan De Meyer <daan.j.demeyer@gmail.com>
2024-02-14 19:56:30 +0100

    meson-vcs-tag: Say version format specification
    That's the official name so let's use it.

--- tools/meson-vcs-tag.sh
+++ tools/meson-vcs-tag.sh
@@ -37,6 +37,6 @@ else
     [ -z "$c" ] && c="${project_version}"
     # Replace any hyphens with carets which are allowed in versions by pacman whereas hyphens are not. Git
     # versions with carets will also sort higher than their non-git version counterpart both in pacman
-    # versioning and in version comparison spec versioning.
+    # versioning and in version format specification versioning.
     echo "$c" | sed 's/^v//; s/-/^/g'
 fi
