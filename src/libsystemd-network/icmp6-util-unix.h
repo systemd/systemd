5d633020b9da8ffa423ea7b4c71e219d68cc3af7
Yu Watanabe <watanabe.yu+github@gmail.com>
2024-02-14 21:17:00 +0900

    test-ndisc: add basic tests for Neighbor Advertisement handling

--- src/libsystemd-network/icmp6-util-unix.h
+++ src/libsystemd-network/icmp6-util-unix.h
@@ -4,7 +4,9 @@
 #include "icmp6-util.h"
 
 typedef int (*send_ra_t)(uint8_t flags);
+typedef int (*send_na_t)(uint32_t flags);
 
 extern send_ra_t send_ra_function;
+extern send_na_t send_na_function;
 extern int test_router_fd[2];
 extern int test_neighbor_fd[2];
