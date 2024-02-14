ebf5bb5477a167ee2f5c231b8ab0c0cfd48cbb1d
Luca Boccassi <bluca@debian.org>
2024-02-14 17:14:21 +0000

    man: enchance sd_bus_set_watch_bind() example to handle one more failure
    In case the D-Bus policy is not set up correctly the example just
    loops forever. Check the return of sd_bus_request_name_async() in
    a callback and exit if the error is not temporary.
    
    Follow-up for 34bbda18a5f07fa5a52e8d85d20637ce1c00c4ec

--- man/sd_bus_service_reconnect.c
+++ man/sd_bus_service_reconnect.c
@@ -92,6 +92,22 @@ static int on_disconnect(sd_bus_message *message, void *userdata, sd_bus_error *
   return 0;
 }
 
+/* Ensure the event loop exits with a clear error if acquiring the well-known service name fails */
+static int request_name_callback(sd_bus_message *m, void *userdata, sd_bus_error *ret_error) {
+  if (sd_bus_message_is_method_error(m, NULL)) {
+    int r = sd_bus_message_get_errno(m);
+
+    if (r == ETIMEDOUT)
+      return 1; /* The bus is not available, try again later */
+
+    printf("Failed to request name: %s\n", strerror(r));
+    object *o = userdata;
+    check(sd_event_exit(*o->event, -r));
+  }
+
+  return 1;
+}
+
 static int setup(object *o) {
   /* If we are reconnecting, then the bus object needs to be closed, detached from
    * the event loop and recreated.
@@ -135,15 +151,16 @@ static int setup(object *o) {
                                  o));
   /* By default the service is only assigned an ephemeral name. Also add a well-known
    * one, so that clients know whom to call. This needs to be asynchronous, as
-   * D-Bus might not be yet available.
+   * D-Bus might not be yet available. The callback will check whether the error is
+   * expected or not, in case it fails.
    * https://www.freedesktop.org/software/systemd/man/sd_bus_request_name.html
    */
   check(sd_bus_request_name_async(*o->bus,
                                   NULL,
                                   "org.freedesktop.ReconnectExample",
                                   0,
-                                  NULL,
-                                  NULL));
+                                  request_name_callback,
+                                  o));
   /* When D-Bus is disconnected this callback will be invoked, which will
    * set up the connection again. This needs to be asynchronous, as D-Bus might not
    * yet be available.
