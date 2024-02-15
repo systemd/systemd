09270ab92081d4672b7206eb0b3daa80d108cd61
Krzesimir Nowak <knowak@microsoft.com>
2024-02-15 15:32:43 +0100

    test: Initial systemd-sysext tests
    The follow-up commit will refactor some code in systemd-sysext, so add some
    tests to make sure that things didn't break. The tests will be later extended
    with cases for new features added.

---
+++ test/TEST-85-SYSEXT/test.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: LGPL-2.1-or-later
+# -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
+# ex: ts=8 sw=4 sts=4 et filetype=sh
+set -e
+
+TEST_DESCRIPTION="test systemd-sysext"
+IMAGE_NAME="sysext"
+
+# TODO: I have no idea about those.
+TEST_NO_NSPAWN=1
+TEST_INSTALL_VERITY_MINIMAL=1
+
+# shellcheck source=test/test-functions
+. "${TEST_BASE_DIR:?}/test-functions"
+
+do_test "$@"
