93c7f3c5b4ab4f892982f4e0d097fbb5f9b9a821
Ludwig Nussel <ludwig.nussel@suse.de>
2024-02-15 17:59:53 +0100

    pcrlock: add --strict option
    Add a --strict option that makes sure all PCRs for which components were
    found are indeed included in the set of predicted PCRs. Instead throw an
    error. Avoids creating a policy less secure than requested.

--- src/pcrlock/pcrlock.c
+++ src/pcrlock/pcrlock.c
@@ -66,6 +66,7 @@ static bool arg_recovery_pin = false;
 static char *arg_policy_path = NULL;
 static bool arg_force = false;
 static BootEntryTokenType arg_entry_token_type = BOOT_ENTRY_TOKEN_AUTO;
+static bool arg_strict = false;
 static char *arg_entry_token = NULL;
 static bool arg_varlink = false;
 /* abbreviate to 7 chars, just like git */
@@ -3752,6 +3753,7 @@ static int verb_lock_uki(int argc, char *argv[], void *userdata) {
 
 static int event_log_reduce_to_safe_pcrs(EventLog *el, uint32_t *pcrs) {
         _cleanup_free_ char *dropped = NULL, *kept = NULL;
+        uint32_t dropped_relevant_pcr = 0;
 
         assert(el);
         assert(pcrs);
@@ -3800,6 +3802,9 @@ static int event_log_reduce_to_safe_pcrs(EventLog *el, uint32_t *pcrs) {
                 continue;
 
         drop:
+                if (arg_strict && FLAGS_SET(*pcrs, UINT32_C(1) << pcr))
+                        dropped_relevant_pcr |= pcr;
+
                 *pcrs &= ~(UINT32_C(1) << pcr);
 
                 if (strextendf_with_separator(&dropped, ", ", "%" PRIu32 " (%s)", pcr, tpm2_pcr_index_to_string(pcr)) < 0)
@@ -3807,7 +3812,7 @@ static int event_log_reduce_to_safe_pcrs(EventLog *el, uint32_t *pcrs) {
         }
 
         if (dropped)
-                log_notice("PCRs dropped from protection mask: %s", dropped);
+                log_full(dropped_relevant_pcr != 0 ? LOG_ERR : LOG_NOTICE, "PCRs dropped from protection mask: %s", dropped);
         else
                 log_debug("No PCRs dropped from protection mask.");
 
@@ -3816,7 +3821,7 @@ static int event_log_reduce_to_safe_pcrs(EventLog *el, uint32_t *pcrs) {
         else
                 log_notice("No PCRs kept in protection mask.");
 
-        return 0;
+        return arg_strict && dropped_relevant_pcr != 0 ? -ENOEXEC : 0;
 }
 
 static int verb_lock_kernel_cmdline(int argc, char *argv[], void *userdata) {
@@ -4950,6 +4955,7 @@ static int help(int argc, char *argv[], void *userdata) {
                "     --force                  Write policy even if it matches existing policy\n"
                "     --entry-token=machine-id|os-id|os-image-id|auto|literal:…\n"
                "                              Boot entry token to use for this installation\n"
+               "     --strict                 Require all PCRs configured via --pcr= included in policy\n"
                "\nSee the %2$s for details.\n",
                program_invocation_short_name,
                link,
@@ -4976,6 +4982,7 @@ static int parse_argv(int argc, char *argv[]) {
                 ARG_POLICY,
                 ARG_FORCE,
                 ARG_ENTRY_TOKEN,
+                ARG_STRICT,
         };
 
         static const struct option options[] = {
@@ -4994,6 +5001,7 @@ static int parse_argv(int argc, char *argv[]) {
                 { "policy",          required_argument, NULL, ARG_POLICY          },
                 { "force",           no_argument,       NULL, ARG_FORCE           },
                 { "entry-token",     required_argument, NULL, ARG_ENTRY_TOKEN     },
+                { "strict",          required_argument, NULL, ARG_STRICT          },
                 {}
         };
 
@@ -5147,6 +5155,12 @@ static int parse_argv(int argc, char *argv[]) {
 
                 case ARG_ENTRY_TOKEN:
                         r = parse_boot_entry_token_type(optarg, &arg_entry_token_type, &arg_entry_token);
+                        if (r < 0)
+                                return r;
+                        break;
+
+                case ARG_STRICT:
+                        r = parse_boolean_argument("--strict", optarg, &arg_strict);
                         if (r < 0)
                                 return r;
                         break;
