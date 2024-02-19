07770f6591bb10d6f4970035f11d3e302a9312e3
Yu Watanabe <watanabe.yu+github@gmail.com>
2024-02-19 12:02:04 +0900

    pam-util: include cache ID of bus connection in the log message
    To make it easier to debug issues like #31375.

--- src/shared/pam-util.c
+++ src/shared/pam-util.c
@@ -96,7 +96,9 @@ static void pam_bus_data_destroy(pam_handle_t *handle, void *data, int error_sta
         if (FLAGS_SET(error_status, PAM_DATA_SILENT) &&
             d->bus && bus_origin_changed(d->bus))
                 /* Please adjust test/units/end.sh when updating the log message. */
-                pam_syslog(handle, LOG_DEBUG, "Attempted to close sd-bus after fork whose connection is opened before the fork, this should not happen.");
+                pam_syslog(handle, LOG_DEBUG,
+                           "Attempted to close sd-bus (%s) after fork whose connection is opened before the fork, this should not happen.",
+                           strna(d->cache_id));
 
         pam_bus_data_free(data);
 }
