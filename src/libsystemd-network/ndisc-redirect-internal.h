ee040c8f8e1a9d6a03145607ffd3b4fad9470851
Yu Watanabe <watanabe.yu+github@gmail.com>
2024-02-22 12:31:49 +0900

    sd-ndisc: add basic support of Redirect message
    This makes sd-ndisc support Redirect message defined in RFC 4861.

---
+++ src/libsystemd-network/ndisc-redirect-internal.h
@@ -0,0 +1,23 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#pragma once
+
+#include <stdbool.h>
+
+#include "sd-ndisc.h"
+
+#include "icmp6-packet.h"
+#include "time-util.h"
+
+struct sd_ndisc_redirect {
+        unsigned n_ref;
+
+        ICMP6Packet *packet;
+
+        struct in6_addr target_address;
+        struct in6_addr destination_address;
+        struct ether_addr target_mac;
+        struct ip6_hdr redirected_header;
+};
+
+sd_ndisc_redirect* ndisc_redirect_new(ICMP6Packet *packet);
+int ndisc_redirect_parse(sd_ndisc *nd, sd_ndisc_redirect *rd);
