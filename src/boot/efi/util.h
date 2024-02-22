89ec0185b2ca19c5cfeebbaa7c5725f15f0712bc
Luca Boccassi <bluca@debian.org>
2024-02-22 14:23:06 +0000

    efi: add workaround for compilation error with gcc 12.2.0
    ../src/boot/efi/stub.c: In function ‘load_addons.constprop’:03:06
    ../src/boot/efi/stub.c:475:40: error: using a dangling pointer to ‘p’ [-Werror=dangling-pointer=]03:06
      475 |                         dt_bases[n_dt] = xmemdup((uint8_t*)loaded_addon->ImageBase + addrs[UNIFIED_SECTION_DTB],03:06
          |                         ~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~03:06
      476 |                                                  dt_sizes[n_dt]);03:06
          |                                                  ~~~~~~~~~~~~~~~03:06
    In file included from ../src/boot/efi/stub.c:20:03:06
    ../src/boot/efi/util.h:33:15: note: ‘p’ declared here03:06
       33 |         void *p;03:06
          |               ^

--- src/boot/efi/util.h
+++ src/boot/efi/util.h
@@ -54,7 +54,8 @@ static inline void *xrealloc(void *p, size_t old_size, size_t new_size) {
 
 _malloc_ _alloc_(2) _returns_nonnull_ _warn_unused_result_
 static inline void* xmemdup(const void *p, size_t l) {
-        return memcpy(xmalloc(l), p, l);
+        void *t = xmalloc(l);
+        return memcpy(t, p, l);
 }
 
 #define xnew(type, n) ((type *) xmalloc_multiply((n), sizeof(type)))
