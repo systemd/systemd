a49ed4ad72b8e62c94389b8e1515b215d9365382
Richard Maw <richard.maw@codethink.co.uk>
2024-02-22 13:01:13 +0000

    test-74-ssh: Support credential-provided root password
    When root authorized keys are provided by mkosi
    they are not newline-terminated
    so appending a public key to the file results in a corrupt key,
    so just to be safe we can add an empty line.

--- test/units/testsuite-74.ssh.sh
+++ test/units/testsuite-74.ssh.sh
@@ -34,6 +34,8 @@ removesshid() {
 ssh-keygen -N '' -C '' -t rsa -f "$ROOTID"
 
 mkdir -p 0700 /root/.ssh
+# Add a newline in case authorized_keys wasn't terminated correctly
+echo >> /root/.ssh/authorized_keys
 cat "$ROOTID".pub >> /root/.ssh/authorized_keys
 
 # set root pw to "foo", just to set it to something valid
