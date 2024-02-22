428913f05db933f7cbdcfc4921f3c2b34d40917b
Richard Maw <richard.maw@codethink.co.uk>
2024-02-22 13:10:02 +0000

    test-74: support read-only /usr

--- test/units/testsuite-74.sh
+++ test/units/testsuite-74.sh
@@ -5,6 +5,15 @@ set -o pipefail
 
 # shellcheck source=test/units/test-control.sh
 . "$(dirname "$0")"/test-control.sh
+# shellcheck source=test/units/util.sh
+. "$(dirname "$0")"/util.sh
+
+mkdir -p /usr/share/empty.sshd
+test/units/testsuite-74.networkctl.sh
+# Writable /usr overlay required to possibly make /usr/share/empty.sshd
+# and write networkd config to /usr to test runtime masking.
+maybe_mount_usr_overlay
+trap 'maybe_umount_usr_overlay' EXIT
 
 run_subtests
 
