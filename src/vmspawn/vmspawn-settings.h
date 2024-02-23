e82fdc0b98e4a8056250fecb40a8c8dc0a2c707b
Lennart Poettering <lennart@poettering.net>
2024-02-23 12:20:55 +0100

    vmspawn: use our own ptyfwd code for the console of a VM
    Let's make systemd-nspawn use our own ptyfwd logic to handle the TTY by
    default.
    
    This adds a new setting --console=, inspired by nspawn's setting of the
    same name. If --console=interactive= is used, then we'll do the TTY
    dance on our own via ptyfwd, and thus get tinting, our usual hotkey
    handling and similar.
    
    Since qemu's own console is useful too, let's keep it around via
    --console=native.
    
    FInally, replace the --qemu-gui switch by --console=gui.

--- src/vmspawn/vmspawn-settings.h
+++ src/vmspawn/vmspawn-settings.h
@@ -1,7 +1,19 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 #pragma once
 
+#include <errno.h>
 #include <stdint.h>
+
+#include "macro.h"
+
+typedef enum ConsoleMode {
+        CONSOLE_INTERACTIVE,    /* ptyfwd */
+        CONSOLE_READ_ONLY,      /* ptyfwd, but in read-only mode */
+        CONSOLE_NATIVE,         /* qemu's native TTY handling */
+        CONSOLE_GUI,            /* qemu's graphical UI */
+        _CONSOLE_MODE_MAX,
+        _CONSOLE_MODE_INVALID = -EINVAL,
+} ConsoleMode;
 
 typedef enum SettingsMask {
         SETTING_START_MODE        = UINT64_C(1) << 0,
@@ -10,3 +22,6 @@ typedef enum SettingsMask {
         SETTING_CREDENTIALS       = UINT64_C(1) << 30,
         _SETTING_FORCE_ENUM_WIDTH = UINT64_MAX
 } SettingsMask;
+
+const char *console_mode_to_string(ConsoleMode m) _const_;
+ConsoleMode console_mode_from_string(const char *s) _pure_;
