22f2941cfca17258717cd6f61b6d31af3d65809c
Ronan Pigott <ronan@rjp.ie>
2024-02-24 15:23:54 -0700

    network: Introduce UseDNR IPv6AcceptRA option
    This is equivalent to the DHCP options

--- src/network/networkd-ndisc.c
+++ src/network/networkd-ndisc.c
@@ -1356,6 +1356,9 @@ static int ndisc_router_process_encrypted_dns(Link *link, sd_ndisc_router *rt) {
 
         _cleanup_(sd_dns_resolver_done) sd_dns_resolver res = {};
 
+        if (!network_ndisc_use_dnr(link->network))
+                return 0;
+
         usec_t lifetime_usec;
         r = sd_ndisc_router_encrypted_dns_get_lifetime_timestamp(rt, CLOCK_BOOTTIME, &lifetime_usec);
         if (r < 0)
