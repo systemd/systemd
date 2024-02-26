eaa260cd748591dcae22368a5ea1b37c5351aae1
Yu Watanabe <watanabe.yu+github@gmail.com>
2024-02-26 13:42:01 +0900

    varlink/network: reindent methods

--- src/shared/varlink-io.systemd.Network.c
+++ src/shared/varlink-io.systemd.Network.c
@@ -2,17 +2,19 @@
 
 #include "varlink-io.systemd.Network.h"
 
-static VARLINK_DEFINE_METHOD(GetStates,
-                             VARLINK_DEFINE_OUTPUT(AddressState, VARLINK_STRING, 0),
-                             VARLINK_DEFINE_OUTPUT(IPv4AddressState, VARLINK_STRING, 0),
-                             VARLINK_DEFINE_OUTPUT(IPv6AddressState, VARLINK_STRING, 0),
-                             VARLINK_DEFINE_OUTPUT(CarrierState, VARLINK_STRING, 0),
-                             VARLINK_DEFINE_OUTPUT(OnlineState, VARLINK_STRING, VARLINK_NULLABLE),
-                             VARLINK_DEFINE_OUTPUT(OperationalState, VARLINK_STRING, 0));
+static VARLINK_DEFINE_METHOD(
+                GetStates,
+                VARLINK_DEFINE_OUTPUT(AddressState, VARLINK_STRING, 0),
+                VARLINK_DEFINE_OUTPUT(IPv4AddressState, VARLINK_STRING, 0),
+                VARLINK_DEFINE_OUTPUT(IPv6AddressState, VARLINK_STRING, 0),
+                VARLINK_DEFINE_OUTPUT(CarrierState, VARLINK_STRING, 0),
+                VARLINK_DEFINE_OUTPUT(OnlineState, VARLINK_STRING, VARLINK_NULLABLE),
+                VARLINK_DEFINE_OUTPUT(OperationalState, VARLINK_STRING, 0));
 
-static VARLINK_DEFINE_METHOD(GetNamespaceId,
-                             VARLINK_DEFINE_OUTPUT(NamespaceId, VARLINK_INT, 0),
-                             VARLINK_DEFINE_OUTPUT(NamespaceNSID, VARLINK_INT, VARLINK_NULLABLE));
+static VARLINK_DEFINE_METHOD(
+                GetNamespaceId,
+                VARLINK_DEFINE_OUTPUT(NamespaceId, VARLINK_INT, 0),
+                VARLINK_DEFINE_OUTPUT(NamespaceNSID, VARLINK_INT, VARLINK_NULLABLE));
 
 VARLINK_DEFINE_INTERFACE(
                 io_systemd_Network,
