af18c8eb49db2652023a694eda841562ebd0343b
Yu Watanabe <watanabe.yu+github@gmail.com>
2024-02-29 12:43:03 +0900

    ndisc-option: drop unused function

--- src/libsystemd-network/ndisc-option.c
+++ src/libsystemd-network/ndisc-option.c
@@ -647,16 +647,6 @@ static const uint8_t prefix_length_code_to_prefix_length[_PREFIX_LENGTH_CODE_MAX
         [PREFIX_LENGTH_CODE_32] = 32,
 };
 
-int pref64_plc_to_prefix_length(uint16_t plc, uint8_t *ret) {
-        plc &= PREF64_PLC_MASK;
-        if (plc >= _PREFIX_LENGTH_CODE_MAX)
-                return -EINVAL;
-
-        if (ret)
-                *ret = prefix_length_code_to_prefix_length[plc];
-        return 0;
-}
-
 int pref64_prefix_length_to_plc(uint8_t prefixlen, uint8_t *ret) {
         for (size_t i = 0; i < ELEMENTSOF(prefix_length_code_to_prefix_length); i++)
                 if (prefix_length_code_to_prefix_length[i] == prefixlen) {
