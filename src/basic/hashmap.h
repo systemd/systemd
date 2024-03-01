ab3f56491e64177f85f7f150d31ba231b7cfd792
Lennart Poettering <lennart@poettering.net>
2024-03-01 21:43:21 +0100

    hashmap: reorder fields to pack structure better
    When building with ENABLE_DEBUG_HASHMAP we can pack the hashmap iterator
    structure a bit better.
    
    Fixes: #31558

--- src/basic/hashmap.h
+++ src/basic/hashmap.h
@@ -39,8 +39,8 @@ typedef struct IteratedCache IteratedCache;   /* Caches the iterated order of on
  * by hashmap users, so the definition has to be here. Do not use its fields
  * directly. */
 typedef struct {
-        unsigned idx;         /* index of an entry to be iterated next */
         const void *next_key; /* expected value of that entry's key pointer */
+        unsigned idx;         /* index of an entry to be iterated next */
 #if ENABLE_DEBUG_HASHMAP
         unsigned put_count;   /* hashmap's put_count recorded at start of iteration */
         unsigned rem_count;   /* hashmap's rem_count in previous iteration */
