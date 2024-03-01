fd5264e3096af0a344179dfb41bb79edd0275675
Lennart Poettering <lennart@poettering.net>
2024-03-01 14:47:52 +0100

    bus-util: add generic parser for extracting id128 values from bus messages

--- src/shared/bus-util.c
+++ src/shared/bus-util.c
@@ -754,3 +754,32 @@ int bus_query_sender_pidref(
 
         return bus_creds_get_pidref(creds, ret);
 }
+
+int bus_message_read_id128(sd_bus_message *m, sd_id128_t *ret) {
+        const void *a;
+        size_t sz;
+        int r;
+
+        assert(m);
+
+        r = sd_bus_message_read_array(m, 'y', &a, &sz);
+        if (r < 0)
+                return r;
+
+        switch (sz) {
+        case 0:
+                if (ret)
+                        *ret = SD_ID128_NULL;
+                break;
+
+        case sizeof(sd_id128_t):
+                if (ret)
+                        memcpy(ret, a, sz);
+                break;
+
+        default:
+                return -EINVAL;
+        }
+
+        return 0;
+}
