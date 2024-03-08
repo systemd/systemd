f90db5c22847013f19cd051d82e4421c90074e56
Unique-Usman <usmanakinyemi202@gmail.com>
2024-03-08 17:29:39 +0530

    Sorted the header files alphabetically

--- src/test/test-login-util.c
+++ src/test/test-login-util.c
@@ -1,8 +1,8 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 
-#include "tests.h"
 #include "login-util.h"
 #include "string-util.h"
+#include "tests.h"
 
 TEST(session_id_valid) {
         /* Invalid Session */
@@ -18,4 +18,5 @@ TEST(session_id_valid) {
         assert_se(session_id_valid("1234567890"));
         assert_se(session_id_valid("ABCDEFGHI"));
 }
+
 DEFINE_TEST_MAIN(LOG_DEBUG);
