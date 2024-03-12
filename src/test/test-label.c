74d96d3b927c0c74c823cc28ce1ff503fe7ba19b
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2024-03-12 15:34:50 +0100

    refactor code to implement systemd coding styles

--- src/test/test-label.c
+++ src/test/test-label.c
@@ -4,72 +4,89 @@
 #include <stddef.h>
 #include <string.h>
 
+#include "fs-util.h"
 #include "label.h"
+#include "path-util.h"
 #include "string-util.h"
 #include "tests.h"
 
+static int check_path(const char *path) {
 
-int pre_labelling_func(int dir_fd, const char *path, mode_t mode);
-int post_labelling_func(int dir_fd, const char *path);
-LabelOps test_label_ops = {
-        .pre = NULL,
-        .post = NULL
+        if (!is_path(path) || !path_is_valid(path) || isempty(path))
+                return -EINVAL;
+
+        if (strlen(path) > 40)
+                return -ENAMETOOLONG;
+
+        if (!path_is_safe(path))
+                return -ENOTDIR;
+
+        /*assume a case where a specific label isn't allowed*/
+        if (path_equal(path, "/restricted_directory"))
+                return -EACCES;
+        return 0;
+}
+static int pre_labelling_func(int dir_fd, const char *path, mode_t mode) {
+        int ret;
+
+        /*sample pre_labelling task*/
+        assert(dir_fd >= 0);
+        assert(path);
+        assert(mode != MODE_INVALID);
+        ret = check_path(path);
+        if (ret < 0) {
+                return ret;
+        }
+
+        /* custom pre labelling logic here */
+        return 0; /* on success */
+}
+
+static int post_labelling_func(int dir_fd, const char *path) {
+        /*sample post_labelling task*/
+        int ret;
+
+        assert(dir_fd >= 0);
+        assert(path);
+
+        /*assume label policies that restrict certain labels*/
+
+        ret = check_path(path);
+        if (ret < 0)
+                return ret;
+
+        /* custom post labelling logic */
+        return 0; /*on sucess*/
+}
+
+static const LabelOps test_label_ops = {
+        .pre = pre_labelling_func,
+        .post = post_labelling_func,
     };
 
 TEST(label_ops_set) {
 
-    assert_se(label_ops_set(&test_label_ops) == 0);
-
-    /*attempt to reset label_ops when already set*/
-    assert_se(label_ops_set(&test_label_ops) == -EBUSY);
-
-}
-
-int pre_labelling_func(int dir_fd, const char *path, mode_t mode) {
-    /*sample pre_labelling task*/
-    if (dir_fd < 0 || !path || mode == 0)
-        return -1;
-
-    /*assume a case where a specific label isn't allowed*/
-    if (streq(path, "/restricted_directory"))
-        return -1;
-
-    /* custom pre labelling logic here */
-    return 0; /* on success */
+        assert_se(label_ops_set(&test_label_ops) == 0);
+        /*attempt to reset label_ops when already set*/
+        assert_se(label_ops_set(&test_label_ops) == -EBUSY);
 
 }
 
 TEST(label_ops_pre) {
 
-    assert_se(label_ops_pre(1, "abcd", 0755) == 0);
-    test_label_ops.pre = pre_labelling_func;
-    assert_se(label_ops_pre(1, "abcd", 0755) == 0);
-    assert_se(label_ops_pre(1, "/restricted_directory", 0755) == -1);
-    assert_se(label_ops_pre(1, "abcd", 0) == -1);
+            assert_se(label_ops_pre(1, "/abcd", 0755) == 0);
+            assert_se(label_ops_pre(1, "/restricted_directory", 0755) == -EACCES);
+            assert_se(label_ops_pre(2, "abcd", 0644) == -EINVAL);
+            assert_se(label_ops_pre(1, "/wekrgoierhgoierhqgherhgwklegnlweehgorwfkryrit", 0755) == -ENAMETOOLONG);
 
-}
-
-int post_labelling_func(int dir_fd, const char *path) {
-    /*sample post_labelling task*/
-
-    if (dir_fd < 0 || !path)
-    return -1;
-
-    /*assume label policies that restrict certain labels*/
-    if (strlen(path) > 100 || streq(path, "/restricted_directory"))
-        return -1;
-
-    /* custom post labelling logic */
-    return 0; /*on sucess*/
 
 }
 
 TEST(label_ops_post) {
 
-    assert_se(label_ops_post(1, "abcd") == 0);
-    test_label_ops.post = post_labelling_func;
-    assert_se(label_ops_post(1, "abc") == 0);
-    label_ops_set(NULL);
+        assert_se(label_ops_post(1, "/abcd") == 0);
+        assert_se(label_ops_post(1, "/restricted_directory") == -EACCES);
+        assert_se(label_ops_post(2, "") == -EINVAL);
 }
 
 DEFINE_TEST_MAIN(LOG_INFO)
