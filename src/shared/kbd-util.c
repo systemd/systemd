8b9bbeda04960abde50701b55cd62b621086b408
Yu Watanabe <watanabe.yu+github@gmail.com>
2024-03-14 18:40:14 +0900

    kbd-util: allow to override the default keymap directories
    This introduces $SYSTEMD_KEYMAP_DIRECTORIES environment variable to
    override the hardcoded keymap directories.
    
    Prompted by #31759.
    
    Closes #31759.

--- src/shared/kbd-util.c
+++ src/shared/kbd-util.c
@@ -3,13 +3,61 @@
 #include "errno-util.h"
 #include "kbd-util.h"
 #include "log.h"
-#include "nulstr-util.h"
 #include "path-util.h"
 #include "recurse-dir.h"
 #include "set.h"
 #include "string-util.h"
 #include "strv.h"
 #include "utf8.h"
+
+#define KBD_KEYMAP_DIRS                         \
+        "/usr/share/keymaps/",                  \
+        "/usr/share/kbd/keymaps/",              \
+        "/usr/lib/kbd/keymaps/"
+
+int keymap_directories(char ***ret) {
+        _cleanup_strv_free_ char **paths = NULL;
+        const char *e;
+        int r;
+
+        assert(ret);
+
+        e = getenv("SYSTEMD_KEYMAP_DIRECTORIES");
+        if (!e)
+                goto use_default;
+
+        r = strv_split_full(&paths, e, NULL, EXTRACT_UNQUOTE);
+        if (r == 0) /* empty string? */
+                goto use_default;
+        if (r < 0) {
+                log_debug_errno(r, "Failed to parse $SYSTEMD_KEYMAP_DIRECTORIES environment variable, ignoring: %m");
+                goto use_default;
+        }
+
+        STRV_FOREACH(p, paths) {
+                if (!path_is_absolute(*p)) {
+                        log_debug("Path specified in $SYSTEMD_KEYMAP_DIRECTORIES environment variable is not absolute, ignoring: %m");
+                        goto use_default;
+                }
+                if (!path_is_normalized(*p)) {
+                        log_debug("Path specified in $SYSTEMD_KEYMAP_DIRECTORIES environment variable is not normalized, ignoring: %m");
+                        goto use_default;
+                }
+        }
+
+        *ret = TAKE_PTR(paths);
+        return 0;
+
+use_default:
+        strv_free(paths);
+
+        paths = strv_new(KBD_KEYMAP_DIRS);
+        if (!paths)
+                return log_oom_debug();
+
+        *ret = TAKE_PTR(paths);
+        return 0;
+}
 
 struct recurse_dir_userdata {
         const char *keymap_name;
@@ -65,16 +113,21 @@ static int keymap_recurse_dir_callback(
 
 int get_keymaps(char ***ret) {
         _cleanup_set_free_free_ Set *keymaps = NULL;
+        _cleanup_strv_free_ char **keymap_dirs = NULL;
         int r;
+
+        r = keymap_directories(&keymap_dirs);
+        if (r < 0)
+                return r;
 
         keymaps = set_new(&string_hash_ops);
         if (!keymaps)
                 return -ENOMEM;
 
-        NULSTR_FOREACH(dir, KBD_KEYMAP_DIRS) {
+        STRV_FOREACH(dir, keymap_dirs) {
                 r = recurse_dir_at(
                                 AT_FDCWD,
-                                dir,
+                                *dir,
                                 /* statx_mask= */ 0,
                                 /* n_depth_max= */ UINT_MAX,
                                 RECURSE_DIR_IGNORE_DOT|RECURSE_DIR_ENSURE_TYPE,
@@ -85,9 +138,9 @@ int get_keymaps(char ***ret) {
                 if (r == -ENOENT)
                         continue;
                 if (ERRNO_IS_NEG_RESOURCE(r))
-                        return log_warning_errno(r, "Failed to read keymap list from %s: %m", dir);
+                        return log_warning_errno(r, "Failed to read keymap list from %s: %m", *dir);
                 if (r < 0)
-                        log_debug_errno(r, "Failed to read keymap list from %s, ignoring: %m", dir);
+                        log_debug_errno(r, "Failed to read keymap list from %s, ignoring: %m", *dir);
         }
 
         _cleanup_strv_free_ char **l = set_get_strv(keymaps);
@@ -127,15 +180,20 @@ bool keymap_is_valid(const char *name) {
 }
 
 int keymap_exists(const char *name) {
+        _cleanup_strv_free_ char **keymap_dirs = NULL;
         int r;
 
         if (!keymap_is_valid(name))
                 return -EINVAL;
 
-        NULSTR_FOREACH(dir, KBD_KEYMAP_DIRS) {
+        r = keymap_directories(&keymap_dirs);
+        if (r < 0)
+                return r;
+
+        STRV_FOREACH(dir, keymap_dirs) {
                 r = recurse_dir_at(
                                 AT_FDCWD,
-                                dir,
+                                *dir,
                                 /* statx_mask= */ 0,
                                 /* n_depth_max= */ UINT_MAX,
                                 RECURSE_DIR_IGNORE_DOT|RECURSE_DIR_ENSURE_TYPE,
@@ -148,7 +206,7 @@ int keymap_exists(const char *name) {
                 if (ERRNO_IS_NEG_RESOURCE(r))
                         return r;
                 if (r < 0 && r != -ENOENT)
-                        log_debug_errno(r, "Failed to read keymap list from %s, ignoring: %m", dir);
+                        log_debug_errno(r, "Failed to read keymap list from %s, ignoring: %m", *dir);
         }
 
         return false;
