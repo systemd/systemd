e28cc892846fa48d6c6df4400fb7bb9e28eebcf7
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2024-03-15 15:22:31 +0100

    basic/missing_*.h: add asserts that the values are as expected
    It's great that we provide fallback values, but if we got one of those wrong,
    it could be a long time before anyone noticed. So let's add asserts that the
    our internal defines actually match the official ones, when the latter are
    available.
    
    I did not add '#include "macro.h"' to missing_{audit,capability}, because
    those are processed by an awk script that would need additional include
    directories and could be confused by the additional lines. We don't include
    those headers standalone anyway, so this is not necessary anyway.

--- src/basic/missing_audit.h
+++ src/basic/missing_audit.h
@@ -4,21 +4,29 @@
 #include <linux/audit.h>
 
 #if HAVE_AUDIT
-#include <libaudit.h>
+#  include <libaudit.h>
 #endif
 
 #ifndef AUDIT_SERVICE_START
-#define AUDIT_SERVICE_START 1130 /* Service (daemon) start */
+#  define AUDIT_SERVICE_START 1130 /* Service (daemon) start */
+#else
+static_assert(AUDIT_SERVICE_START == 1130);
 #endif
 
 #ifndef AUDIT_SERVICE_STOP
-#define AUDIT_SERVICE_STOP 1131 /* Service (daemon) stop */
+#  define AUDIT_SERVICE_STOP 1131 /* Service (daemon) stop */
+#else
+static_assert(AUDIT_SERVICE_STOP == 1131);
 #endif
 
 #ifndef MAX_AUDIT_MESSAGE_LENGTH
-#define MAX_AUDIT_MESSAGE_LENGTH 8970
+#  define MAX_AUDIT_MESSAGE_LENGTH 8970
+#else
+static_assert(MAX_AUDIT_MESSAGE_LENGTH == 8970);
 #endif
 
 #ifndef AUDIT_NLGRP_MAX
-#define AUDIT_NLGRP_READLOG 1
+#  define AUDIT_NLGRP_READLOG 1
+#else
+static_assert(AUDIT_NLGRP_MAX == 1);
 #endif
--- src/basic/missing_mman.h
+++ src/basic/missing_mman.h
@@ -3,18 +3,28 @@
 
 #include <sys/mman.h>
 
+#include "macro.h"
+
 #ifndef MFD_ALLOW_SEALING
-#define MFD_ALLOW_SEALING 0x0002U
+#  define MFD_ALLOW_SEALING 0x0002U
+#else
+static_assert(MFD_ALLOW_SEALING == 0x0002U);
 #endif
 
 #ifndef MFD_CLOEXEC
-#define MFD_CLOEXEC 0x0001U
+#  define MFD_CLOEXEC 0x0001U
+#else
+static_assert(MFD_CLOEXEC == 0x0001U);
 #endif
 
 #ifndef MFD_NOEXEC_SEAL
-#define MFD_NOEXEC_SEAL 0x0008U
+#  define MFD_NOEXEC_SEAL 0x0008U
+#else
+static_assert(MFD_NOEXEC_SEAL == 0x0008U);
 #endif
 
 #ifndef MFD_EXEC
-#define MFD_EXEC 0x0010U
+#  define MFD_EXEC 0x0010U
+#else
+static_assert(MFD_EXEC == 0x0010U);
 #endif
