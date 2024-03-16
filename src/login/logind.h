8b782277b921f0093fa6a51260dd7d2ebd53d990
Adrian Vovk <adrianvovk@gmail.com>
2024-03-16 17:24:23 -0400

    logind: Add SecureLock()
    This is a special lock operation that wipes sensitive data out of
    memory, makes files inaccessible, and freezes the user session (i.e.
    homed's lock feature).
    
    This is implemented in a backend-agnostic manner, to loosen the coupling
    between systemd components and to allow desktop environments to support
    this feature no matter what service is managing the user.
    
    This impl. also supports warning clients about an imminent secure lock,
    and letting them delay the lock for a short while so that they can
    prepare themselves. This will allow session services to additionally
    wipe their own secrets from memory to further improve the security of a
    secure lock.

--- src/login/logind.h
+++ src/login/logind.h
@@ -38,14 +38,16 @@ struct Manager {
         Hashmap *buttons;
         Hashmap *brightness_writers;
 
+        Set *secure_lock_backends;
+
         LIST_HEAD(Seat, seat_gc_queue);
         LIST_HEAD(Session, session_gc_queue);
         LIST_HEAD(User, user_gc_queue);
 
         sd_device_monitor *device_seat_monitor, *device_monitor, *device_vcsa_monitor, *device_button_monitor;
 
+        sd_event_source *secure_lock_backends_event_source;
         sd_event_source *console_active_event_source;
-
 #if ENABLE_UTMP
         sd_event_source *utmp_event_source;
 #endif
