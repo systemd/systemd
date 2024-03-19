8f6ea3e8279c85e75f2fac75b62062d2789e68a6
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2024-03-19 23:21:06 +0100

    sd-journal/catalog: use strdup_to(), rename output param

--- src/libsystemd/sd-journal/catalog.h
+++ src/libsystemd/sd-journal/catalog.h
@@ -11,7 +11,7 @@
 
 int catalog_import_file(OrderedHashmap *h, const char *path);
 int catalog_update(const char* database, const char* root, const char* const* dirs);
-int catalog_get(const char* database, sd_id128_t id, char **data);
+int catalog_get(const char* database, sd_id128_t id, char **ret_text);
 int catalog_list(FILE *f, const char* database, bool oneline);
 int catalog_list_items(FILE *f, const char* database, bool oneline, char **items);
 int catalog_file_lang(const char *filename, char **lang);
