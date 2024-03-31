473928cadac2384b48ff4018df96efef188dfdc1
Mike Yuan <me@yhndnzj.com>
2024-03-31 19:47:34 +0800

    hibernate-resume: add options handling

--- src/hibernate-resume/hibernate-resume.c
+++ src/hibernate-resume/hibernate-resume.c
@@ -3,6 +3,7 @@
 #include <errno.h>
 #include <sys/stat.h>
 
+#include "build.h"
 #include "devnum-util.h"
 #include "hibernate-resume-config.h"
 #include "hibernate-util.h"
@@ -11,10 +12,69 @@
 #include "main-func.h"
 #include "parse-util.h"
 #include "static-destruct.h"
+#include "terminal-util.h"
 
 static HibernateInfo arg_info = {};
 
 STATIC_DESTRUCTOR_REGISTER(arg_info, hibernate_info_done);
+
+static int help(void) {
+        _cleanup_free_ char *link = NULL;
+        int r;
+
+        r = terminal_urlify_man("systemd-hibernate-resume", "8", &link);
+        if (r < 0)
+                return log_oom();
+
+        printf("%s [OPTIONS...] [DEVICE [OFFSET]]\n"
+               "\n%sInitiate resume from hibernation.%s\n\n"
+               "  -h --help            Show this help\n"
+               "     --version         Show package version\n"
+               "\nSee the %s for details.\n",
+               program_invocation_short_name,
+               ansi_highlight(),
+               ansi_normal(),
+               link);
+
+        return 0;
+}
+
+static int parse_argv(int argc, char *argv[]) {
+
+        enum {
+                ARG_VERSION = 0x100,
+        };
+
+        static const struct option options[] = {
+                { "help",      no_argument,       NULL, 'h'           },
+                { "version",   no_argument,       NULL, ARG_VERSION   },
+                {}
+        };
+
+        int r, c;
+
+        assert(argc >= 0);
+        assert(argv);
+
+        while ((c = getopt_long(argc, argv, "h", options, NULL)) >= 0)
+
+                switch (c) {
+
+                case 'h':
+                        return help();
+
+                case ARG_VERSION:
+                        return version();
+
+                case '?':
+                        return -EINVAL;
+
+                default:
+                        assert_not_reached();
+                }
+
+        return 1;
+}
 
 static int setup_hibernate_info_and_warn(void) {
         int r;
@@ -38,7 +98,11 @@ static int run(int argc, char *argv[]) {
 
         log_setup();
 
-        if (argc < 1 || argc > 3)
+        r = parse_argv(argc, argv);
+        if (r <= 0)
+                return r;
+
+        if (argc - optind > 2)
                 return log_error_errno(SYNTHETIC_ERRNO(EINVAL), "This program expects zero, one, or two arguments.");
 
         umask(0022);
@@ -46,13 +110,13 @@ static int run(int argc, char *argv[]) {
         if (!in_initrd())
                 return 0;
 
-        if (argc > 1) {
-                arg_info.device = argv[1];
+        if (argc > optind) {
+                arg_info.device = argv[optind];
 
-                if (argc == 3) {
-                        r = safe_atou64(argv[2], &arg_info.offset);
+                if (argc - optind == 2) {
+                        r = safe_atou64(argv[optind + 1], &arg_info.offset);
                         if (r < 0)
-                                return log_error_errno(r, "Failed to parse resume offset %s: %m", argv[2]);
+                                return log_error_errno(r, "Failed to parse resume offset %s: %m", argv[optind + 1]);
                 }
         } else {
                 r = setup_hibernate_info_and_warn();
