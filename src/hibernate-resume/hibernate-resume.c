84026ceca9dee4278b37d702fecfe9e7343b4ad2
Mike Yuan <me@yhndnzj.com>
2024-03-31 20:30:50 +0800

    hibernate-resume: introduce --clear-efi for clearing stale EFI var

--- src/hibernate-resume/hibernate-resume.c
+++ src/hibernate-resume/hibernate-resume.c
@@ -15,6 +15,7 @@
 #include "terminal-util.h"
 
 static HibernateInfo arg_info = {};
+static bool arg_clear_efi = false;
 
 STATIC_DESTRUCTOR_REGISTER(arg_info, hibernate_info_done);
 
@@ -30,6 +31,7 @@ static int help(void) {
                "\n%sInitiate resume from hibernation.%s\n\n"
                "  -h --help            Show this help\n"
                "     --version         Show package version\n"
+               "     --clear-efi       Clear stale HibernateLocation EFI variable and exit\n"
                "\nSee the %s for details.\n",
                program_invocation_short_name,
                ansi_highlight(),
@@ -43,11 +45,13 @@ static int parse_argv(int argc, char *argv[]) {
 
         enum {
                 ARG_VERSION = 0x100,
+                ARG_CLEAR_EFI,
         };
 
         static const struct option options[] = {
                 { "help",      no_argument,       NULL, 'h'           },
                 { "version",   no_argument,       NULL, ARG_VERSION   },
+                { "clear-efi", no_argument,       NULL, ARG_CLEAR_EFI },
                 {}
         };
 
@@ -65,6 +69,10 @@ static int parse_argv(int argc, char *argv[]) {
 
                 case ARG_VERSION:
                         return version();
+
+                case ARG_CLEAR_EFI:
+                        arg_clear_efi = true;
+                        break;
 
                 case '?':
                         return -EINVAL;
@@ -87,7 +95,8 @@ static int setup_hibernate_info_and_warn(void) {
         if (r < 0)
                 return r;
 
-        compare_hibernate_location_and_warn(&arg_info);
+        if (!arg_clear_efi)
+                compare_hibernate_location_and_warn(&arg_info);
 
         return 1;
 }
@@ -107,31 +116,44 @@ static int run(int argc, char *argv[]) {
 
         umask(0022);
 
-        if (!in_initrd())
+        if (!in_initrd() && !arg_clear_efi)
+                return log_error_errno(SYNTHETIC_ERRNO(ENOTRECOVERABLE),
+                                       "Not running in initrd, refusing to initiate resume from hibernation.");
+        if (in_initrd() && arg_clear_efi) {
+                log_info("Running in initrd, --clear-efi has no effect, exiting.");
                 return 0;
+        }
 
-        if (argc > optind) {
-                arg_info.device = argv[optind];
+        if (argc <= optind || arg_clear_efi) {
+                r = setup_hibernate_info_and_warn();
+                if (r <= 0)
+                        return r;
+
+                if (arg_info.efi)
+                        r = clear_efi_hibernate_location_and_warn();
+                else
+                        r = 0;
+
+                if (arg_clear_efi) {
+                        if (r > 0)
+                                log_notice("Successfully cleared stale HibernateLocation EFI variable.");
+                        return r;
+                }
+        } else {
+                arg_info.device = ASSERT_PTR(argv[optind]);
 
                 if (argc - optind == 2) {
                         r = safe_atou64(argv[optind + 1], &arg_info.offset);
                         if (r < 0)
                                 return log_error_errno(r, "Failed to parse resume offset %s: %m", argv[optind + 1]);
                 }
-        } else {
-                r = setup_hibernate_info_and_warn();
-                if (r <= 0)
-                        return r;
-
-                if (arg_info.efi)
-                        (void) clear_efi_hibernate_location_and_warn();
         }
 
         if (stat(arg_info.device, &st) < 0)
                 return log_error_errno(errno, "Failed to stat resume device '%s': %m", arg_info.device);
 
         if (!S_ISBLK(st.st_mode))
-                return log_error_errno(SYNTHETIC_ERRNO(EINVAL),
+                return log_error_errno(SYNTHETIC_ERRNO(ENOTBLK),
                                        "Resume device '%s' is not a block device.", arg_info.device);
 
         /* The write shall not return if a resume takes place. */
