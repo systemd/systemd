14142173d955fc4aa23256eb7c6742fcd5bd31a1
Mike Yuan <me@yhndnzj.com>
2024-04-03 00:40:38 +0800

    time-util: drop unneeded 'struct'

--- src/basic/time-util.h
+++ src/basic/time-util.h
@@ -71,8 +71,8 @@ typedef enum TimestampStyle {
 
 #define TIME_T_MAX (time_t)((UINTMAX_C(1) << ((sizeof(time_t) << 3) - 1)) - 1)
 
-#define DUAL_TIMESTAMP_NULL ((struct dual_timestamp) {})
-#define TRIPLE_TIMESTAMP_NULL ((struct triple_timestamp) {})
+#define DUAL_TIMESTAMP_NULL ((dual_timestamp) {})
+#define TRIPLE_TIMESTAMP_NULL ((triple_timestamp) {})
 
 usec_t now(clockid_t clock);
 nsec_t now_nsec(clockid_t clock);
