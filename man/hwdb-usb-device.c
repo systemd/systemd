55e52f920e6cf3d15fa416f76b864dab83df0ad9
Yu Watanabe <watanabe.yu+github@gmail.com>
2024-04-03 02:21:19 +0900

    man/example: fix build failure of hwdb-usb-device.c
    STRLEN() and xsprintf() is our internal macros.

--- man/hwdb-usb-device.c
+++ man/hwdb-usb-device.c
@@ -5,13 +5,13 @@
 #include <sd-hwdb.h>
 
 int print_usb_properties(uint16_t vid, uint16_t pid) {
-  char match[STRLEN("usb:vp") + DECIMAL_STR_MAX(uint16_t) * 2];
+  char match[128];
   sd_hwdb *hwdb;
   const char *key, *value;
   int r;
 
   /* Match this USB vendor and product ID combination */
-  xsprintf(match, "usb:v%04Xp%04X", vid, pid);
+  snprintf(match, sizeof match, "usb:v%04Xp%04X", vid, pid);
 
   r = sd_hwdb_new(&hwdb);
   if (r < 0)
