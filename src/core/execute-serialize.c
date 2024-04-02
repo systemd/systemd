c6e727901edf87fe03f9d43dc3d26ba7ec00d451
Frantisek Sumsal <frantisek@sumsal.cz>
2024-04-02 18:31:03 +0200

    core: correctly deserialize credentials with empty payload
    For example with SetCredential=mycred: the data payload is empty, but it
    is still a valid credential.

--- src/core/execute-serialize.c
+++ src/core/execute-serialize.c
@@ -3671,7 +3671,8 @@ static int exec_context_deserialize(ExecContext *c, FILE *f) {
                         r = extract_many_words(&val, " ", 0, &id, &encrypted, &data);
                         if (r < 0)
                                 return r;
-                        if (r != 3)
+                        /* The "data" part of the credential can be empty, e.g. with SetCredential=mycred: */
+                        if (r < 2 || r > 3)
                                 return -EINVAL;
 
                         r = parse_boolean(encrypted);
@@ -3687,7 +3688,7 @@ static int exec_context_deserialize(ExecContext *c, FILE *f) {
                                 .encrypted = r,
                         };
 
-                        r = unbase64mem(data, &sc->data, &sc->size);
+                        r = unbase64mem(data ?: "", &sc->data, &sc->size);
                         if (r < 0)
                                 return r;
 
