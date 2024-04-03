f859939ae627f34bf4cec9592245459a5012f113
Daan De Meyer <daan.j.demeyer@gmail.com>
2024-04-03 16:06:14 +0200

    core: Serialize both pid and pidfd to keep downgrades working
    Currently, when downgrading from a version with pidfd support to a
    version without pidfd support, all information about running processes
    is lost as the newer systemd will serialized pidfds which are not recognized
    by the older systemd when deserializing.
    
    To improve the situation, let's serialize both the pid and the pidfd.
    This is safe because existing versions will either replace the first
    deserialized pidref with the second one or discard the second one in
    favor of the first one depending on the unit and field. Older versions
    that don't support pidfd's will silently discard any fields that contain
    a pidfd as those will try to parse the field as a pid and since a pidfd
    field will start with '@', those versions will debug error log and ignore
    the value.
    
    To make sure we reuse the existing pidfd as much as possible, the pidfd
    is serialized first. Both for scopes and service main pids, if the same
    pid is seen multiple times, the first pidref is kept. So by serializing
    the pidfd first we make sure the original pidfd is used instead of the
    new one which is opened when deserializing the first pid field.
    
    For other control units, older versions with pidfd support will discard
    the first pidfd and replace it with a new pidfd from the second pid field.
    This is a slight regression on downgrades, but we make sure it doesn't
    happen for future versions (and older versions when this commit is
    backported) by modifying the logic to only use the first successfully
    deserialized pidref so that the raw pid without pidfd is discarded instead
    of it replacing the existing pidfd.

--- src/core/socket.c
+++ src/core/socket.c
@@ -2631,8 +2631,9 @@ static int socket_deserialize_item(Unit *u, const char *key, const char *value, 
                 else
                         s->n_refused += k;
         } else if (streq(key, "control-pid")) {
-                pidref_done(&s->control_pid);
-                (void) deserialize_pidref(fds, value, &s->control_pid);
+
+                if (!pidref_is_set(&s->control_pid))
+                        (void) deserialize_pidref(fds, value, &s->control_pid);
 
         } else if (streq(key, "control-command")) {
                 SocketExecCommand id;
