23df17f133326742954b8b1ab982875752b4d2cf
Luca Boccassi <bluca@debian.org>
2024-04-03 12:28:26 +0100

    idn: use dlopen_many_sym_or_warn

--- src/shared/idn-util.c
+++ src/shared/idn-util.c
@@ -42,23 +42,20 @@ int dlopen_idn(void) {
         if (idn_dl)
                 return 0; /* Already loaded */
 
-        dl = dlopen("libidn.so.12", RTLD_LAZY);
-        if (!dl) {
-                /* libidn broke ABI in 1.34, but not in a way we care about (a new field got added to an
-                 * open-coded struct we do not use), hence support both versions. */
-                dl = dlopen("libidn.so.11", RTLD_LAZY);
-                if (!dl)
-                        return log_debug_errno(SYNTHETIC_ERRNO(EOPNOTSUPP),
-                                               "libidn support is not installed: %s", dlerror());
-        }
-
-        r = dlsym_many_or_warn(
-                        dl,
-                        LOG_DEBUG,
+        r = dlopen_many_sym_or_warn(&dl, "libidn.so.12", LOG_DEBUG,
                         DLSYM_ARG(idna_to_ascii_4i),
                         DLSYM_ARG(idna_to_unicode_44i),
                         DLSYM_ARG(stringprep_ucs4_to_utf8),
                         DLSYM_ARG(stringprep_utf8_to_ucs4));
+        if (r == -EOPNOTSUPP) {
+                /* libidn broke ABI in 1.34, but not in a way we care about (a new field got added to an
+                 * open-coded struct we do not use), hence support both versions. */
+                r = dlopen_many_sym_or_warn(&dl, "libidn.so.11", LOG_DEBUG,
+                                DLSYM_ARG(idna_to_ascii_4i),
+                                DLSYM_ARG(idna_to_unicode_44i),
+                                DLSYM_ARG(stringprep_ucs4_to_utf8),
+                                DLSYM_ARG(stringprep_utf8_to_ucs4));
+        }
         if (r < 0)
                 return r;
 
