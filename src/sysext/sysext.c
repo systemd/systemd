cc18bb986022aff9cd14c73ec8cb04c3ffb3d54a
Krzesimir Nowak <knowak@microsoft.com>
2024-04-11 14:40:35 +0200

    sysext: Use the new path-util helper

--- src/sysext/sysext.c
+++ src/sysext/sysext.c
@@ -898,18 +898,6 @@ static int overlayfs_paths_new(const char *hierarchy, const char *workspace_path
         return 0;
 }
 
-static int resolved_paths_equal(const char *resolved_a, const char *resolved_b) {
-        /* Returns true if paths are of the same entry, false if not, <0 on error. */
-
-        if (path_equal(resolved_a, resolved_b))
-                return 1;
-
-        if (!resolved_a || !resolved_b)
-                return 0;
-
-        return inode_same(resolved_a, resolved_b, 0);
-}
-
 static int maybe_import_mutable_directory(OverlayFSPaths *op) {
         int r;
 
@@ -921,7 +909,7 @@ static int maybe_import_mutable_directory(OverlayFSPaths *op) {
         if (arg_mutable != MUTABLE_IMPORT || !op->resolved_mutable_directory)
                 return 0;
 
-        r = resolved_paths_equal(op->resolved_hierarchy, op->resolved_mutable_directory);
+        r = path_equal_or_inode_same_checked(op->resolved_hierarchy, op->resolved_mutable_directory, 0);
         if (r < 0)
                 return log_error_errno(r, "Failed to check equality of hierarchy %s and its mutable directory %s: %m", op->resolved_hierarchy, op->resolved_mutable_directory);
         if (r > 0)
@@ -962,7 +950,7 @@ static int maybe_import_ignored_mutable_directory(OverlayFSPaths *op) {
                 return log_error_errno(r, "Failed to resolve mutable directory '%s': %m", path);
         }
 
-        r = resolved_paths_equal(op->resolved_hierarchy, resolved_path);
+        r = path_equal_or_inode_same_checked(op->resolved_hierarchy, resolved_path, 0);
         if (r < 0)
                 return log_error_errno(r, "Failed to check equality of hierarchy %s and its mutable directory %s: %m", op->resolved_hierarchy, op->resolved_mutable_directory);
 
@@ -1071,7 +1059,7 @@ static int hierarchy_as_lower_dir(OverlayFSPaths *op) {
                 return 0;
         }
 
-        r = resolved_paths_equal(op->resolved_hierarchy, op->resolved_mutable_directory);
+        r = path_equal_or_inode_same_checked(op->resolved_hierarchy, op->resolved_mutable_directory, 0);
         if (r < 0)
                 return log_error_errno(r, "Failed to check equality of hierarchy %s and its mutable directory %s: %m", op->resolved_hierarchy, op->resolved_mutable_directory);
         if (r > 0) {
