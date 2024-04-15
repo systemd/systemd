85e139d8897189a5f1d6824b7ddb8bc6e6d92847
Yu Watanabe <watanabe.yu+github@gmail.com>
2024-04-15 14:55:18 +0900

    network: introduce network- and protocol-independent default for UseDomains=
    Follow-up for fb573007430ab0dbe45517b58837d2fa5cfa1a48.
    
    Prompted by #32273.

--- src/network/networkd-network.c
+++ src/network/networkd-network.c
@@ -377,6 +377,8 @@ int network_load_one(Manager *manager, OrderedHashmap **networks, const char *fi
 
                 .keep_configuration = manager->keep_configuration,
 
+                .use_domains = _USE_DOMAINS_INVALID,
+
                 .compat_dhcp_use_domains = _USE_DOMAINS_INVALID,
                 .compat_dhcp_use_dns = -1,
                 .compat_dhcp_use_ntp = -1,
@@ -489,6 +491,7 @@ int network_load_one(Manager *manager, OrderedHashmap **networks, const char *fi
                 .ndisc_use_hop_limit = true,
                 .ndisc_use_reachable_time = true,
                 .ndisc_use_retransmission_time = true,
+                .ndisc_use_domains = _USE_DOMAINS_INVALID,
                 .ndisc_route_table = RT_TABLE_MAIN,
                 .ndisc_route_metric_high = IPV6RA_ROUTE_METRIC_HIGH,
                 .ndisc_route_metric_medium = IPV6RA_ROUTE_METRIC_MEDIUM,
