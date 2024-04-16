01b99d13fcc3f9d2052a292acbb2d2da7744c60e
Matteo Croce <teknoraver@meta.com>
2024-04-17 01:09:46 +0200

    emit a warning in networkd if managed sysctls are changed
    Monitor the sysctl set by networkd for writes. If a sysctl is
    overwritten with a different value than the one we set, emit a warning.

--- src/network/networkd-sysctl.c
+++ src/network/networkd-sysctl.c
@@ -13,6 +13,8 @@
 #include "socket-util.h"
 #include "string-table.h"
 #include "sysctl-util.h"
+#include "fileio.h"
+#include "path-util.h"
 
 static void manager_set_ip_forwarding(Manager *manager, int family) {
         int r, t;
@@ -40,9 +42,79 @@ static void manager_set_ip_forwarding(Manager *manager, int family) {
                                   enable_disable(t), af_to_ipv4_ipv6(family));
 }
 
+static void sysctl_overwrite_check(sd_event_source *source, const struct inotify_event *event, Hashmap *sysctls) {
+        const char *desc = NULL, *value;
+        _cleanup_free_ char *buf = NULL;
+        char *path;
+        size_t len;
+
+        if (sd_event_source_get_description(source, &desc) < 0)
+                return;
+
+        path = path_join(desc, event->name);
+
+        value = hashmap_get(sysctls, path);
+
+        /* We don't handle this sysctl */
+        if (!value)
+                return;
+
+        if (read_full_file(path, &buf, &len) < 0)
+                return;
+
+        delete_trailing_chars(buf, "\n");
+
+        if (!streq(value, buf)) {
+                path += sizeof("/proc/sys/") - 1;
+                for (char *p = path; *p; p++)
+                        if (*p == '/')
+                                *p = '.';
+
+                log_warning("sysctl %s was changed from '%s' to '%s'", path, value, buf);
+        }
+}
+
+static int sysctl_inotify_manager(sd_event_source *source, const struct inotify_event *event, void *userdata) {
+        Manager *manager = ASSERT_PTR(userdata);
+
+        sysctl_overwrite_check(source, event, manager->sysctls);
+
+        return 0;
+}
+
+static int sysctl_inotify_link(sd_event_source *source, const struct inotify_event *event, void *userdata) {
+        Link *link = ASSERT_PTR(userdata);
+
+        sysctl_overwrite_check(source, event, link->sysctls);
+
+        return 0;
+}
+
+static int manager_inotify_add(Manager *manager) {
+        sd_event_source **source;
+
+        assert(manager);
+
+        source = manager->sysctl_event_sources;
+
+        FOREACH_STRING(p,
+                "/proc/sys/net/ipv4/conf/all",
+                "/proc/sys/net/ipv4/conf/default",
+                "/proc/sys/net/ipv6/conf/all",
+                "/proc/sys/net/ipv6/conf/default") {
+                if (sd_event_add_inotify(manager->event, source, p, IN_CLOSE_WRITE, sysctl_inotify_manager, manager) < 0)
+                        return log_error_errno(errno, "Failed to watch sysctl %s: %m", p);
+                source++;
+        }
+
+        return 0;
+}
+
 void manager_set_sysctl(Manager *manager) {
         assert(manager);
         assert(!manager->test_mode);
+
+        manager_inotify_add(manager);
 
         manager_set_ip_forwarding(manager, AF_INET);
         manager_set_ip_forwarding(manager, AF_INET6);
@@ -315,10 +387,30 @@ static int link_set_ipv4_promote_secondaries(Link *link) {
         return sysctl_write_ip_property_boolean(AF_INET, link->ifname, "promote_secondaries", true, link->sysctls);
 }
 
+static void link_watch_sysctl(Link *link) {
+        sd_event_source **source;
+
+        assert(link);
+
+        source = link->sysctl_event_sources;
+
+        FOREACH_STRING(p,
+                "/proc/sys/net/ipv4/conf",
+                "/proc/sys/net/ipv6/conf") {
+                char *path = path_join(p, link->ifname);
+
+                if (sd_event_add_inotify(link->manager->event, source, path, IN_CLOSE_WRITE, sysctl_inotify_link, link) < 0)
+                        log_link_warning_errno(link, errno, "Failed to watch sysctl %s: %m", path);
+                source++;
+        }
+}
+
 int link_set_sysctl(Link *link) {
         int r;
 
         assert(link);
+
+        link_watch_sysctl(link);
 
         /* If IPv6 configured that is static IPv6 address and IPv6LL autoconfiguration is enabled
          * for this interface, then enable IPv6 */
