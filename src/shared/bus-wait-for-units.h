0917bd415769827a695f3472c4f61703dce0de8e
Mike Yuan <me@yhndnzj.com>
2024-04-16 20:02:33 +0800

    bus-wait-for-units: check for existing unit first, use hashmap_ensure_put

--- src/shared/bus-wait-for-units.h
+++ src/shared/bus-wait-for-units.h
@@ -19,6 +19,7 @@ typedef enum BusWaitForUnitsFlags {
         BUS_WAIT_FOR_INACTIVE        = 1 << 1, /* Wait until the unit is back in inactive or dead state */
         BUS_WAIT_NO_JOB              = 1 << 2, /* Wait until there's no more job pending */
         BUS_WAIT_REFFED              = 1 << 3, /* The unit is already reffed with RefUnit() */
+        _BUS_WAIT_FOR_TARGET         = BUS_WAIT_FOR_MAINTENANCE_END|BUS_WAIT_FOR_INACTIVE|BUS_WAIT_NO_JOB,
 } BusWaitForUnitsFlags;
 
 typedef void (*bus_wait_for_units_unit_callback_t)(BusWaitForUnits *d, const char *unit_path, bool good, void *userdata);
