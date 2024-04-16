15ab89dc0004eb54a326ef19cd7b011035cd9f73
Yu Watanabe <watanabe.yu+github@gmail.com>
2024-04-16 09:58:25 +0900

    network: move NTP related conf parsers to networkd-ntp.[ch]
    No functional change, just refactoring.

---
+++ src/network/networkd-ntp.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#pragma once
+
+#include "conf-parser.h"
+
+CONFIG_PARSER_PROTOTYPE(config_parse_ntp);
+CONFIG_PARSER_PROTOTYPE(config_parse_dhcp_use_ntp);
