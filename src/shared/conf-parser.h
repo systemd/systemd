c5e6f9832b8bc18f03e7c266ba22bc6dc6408079
Yu Watanabe <watanabe.yu+github@gmail.com>
2024-04-16 10:28:06 +0900

    conf-parser: move config_parse_timezone() to conf-parser.[ch]
    Even though it is currently only used by networkd, the parser itself
    is quite generic. Let's move it to the shared library.

--- src/shared/conf-parser.h
+++ src/shared/conf-parser.h
@@ -250,6 +250,7 @@ CONFIG_PARSER_PROTOTYPE(config_parse_percent);
 CONFIG_PARSER_PROTOTYPE(config_parse_permyriad);
 CONFIG_PARSER_PROTOTYPE(config_parse_pid);
 CONFIG_PARSER_PROTOTYPE(config_parse_sec_fix_0);
+CONFIG_PARSER_PROTOTYPE(config_parse_timezone);
 
 typedef enum Disabled {
         DISABLED_CONFIGURATION,
