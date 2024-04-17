7f1a21c04a333dda3a338586f8430f912e918fd3
Richard Maw <richard.maw@codethink.co.uk>
2024-04-17 16:12:17 +0100

    TEST-07-PID1: Make TriggerLimitInterval a credential
    Since unit drop-in credentials were added this customisation is no
    longer required and is a step closer to sharing an image.

--- test/TEST-07-PID1/test.sh
+++ test/TEST-07-PID1/test.sh
@@ -10,17 +10,15 @@ NSPAWN_ARGUMENTS="--capability=CAP_NET_ADMIN"
 # shellcheck source=test/test-functions
 . "${TEST_BASE_DIR:?}/test-functions"
 
+# We might not be fast enough to hit the limit (20 triggers per 2 secs)
+# in certain environments, i.e. when running without KVM or when collecting
+# coverage. Let's help it a bit in such case.
+if ! get_bool "$QEMU_KVM" || get_bool "$IS_BUILT_WITH_COVERAGE"; then
+    NSPAWN_ARGUMENTS="$NSPAWN_ARGUMENTS --load-credential systemd.unit-dropin.issue2467.socket:$(readlink -f systemd.unit-dropin.issue2467.socket)"
+    QEMU_OPTIONS="${QEMU_OPTIONS:-} -smbios type=11,value=io.systemd.credential.binary:systemd.unit-dropin.issue2467.socket=$(base64 systemd.unit-dropin.issue2467.socket)"
+fi
+
 test_append_files() {
-    local workspace="${1:?}"
-
-    # We might not be fast enough to hit the limit (20 triggers per 2 secs)
-    # in certain environments, i.e. when running without KVM or when collecting
-    # coverage. Let's help it a bit in such case.
-    if ! get_bool "$QEMU_KVM" || get_bool "$IS_BUILT_WITH_COVERAGE"; then
-        mkdir -p "$workspace/etc/systemd/system/issue2467.socket.d"
-        printf "[Socket]\nTriggerLimitIntervalSec=10\n" >"$workspace/etc/systemd/system/issue2467.socket.d/TriggerLimitInterval.conf"
-    fi
-
     image_install logger socat
 }
 
