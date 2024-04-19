df040180d5340bae23f5c6b5f65b2e6207bfdcd7
Luca Boccassi <bluca@debian.org>
2024-04-20 00:50:16 +0100

    core: add ExecMainHandoverTimestamp property recording time-of-execve
    Enable the exec_fd logic for Type=notify* services too, and change it
    to send a timestamp instead of a '1' byte. Record the timestamp in a
    new ExecMainHandoverTimestamp property so that users can track accurately
    when control is handed over from systemd to the service payload, so
    that latency and startup performance can be trivially and accurately
    tracked and attributed.

--- src/core/exec-invoke.c
+++ src/core/exec-invoke.c
@@ -5265,12 +5265,14 @@ int exec_invoke(
         log_command_line(context, params, "Executing", executable, final_argv);
 
         if (params->exec_fd >= 0) {
-                uint8_t hot = 1;
+                usec_t t = now(CLOCK_MONOTONIC);
 
                 /* We have finished with all our initializations. Let's now let the manager know that. From this point
-                 * on, if the manager sees POLLHUP on the exec_fd, then execve() was successful. */
+                 * on, if the manager sees POLLHUP on the exec_fd, then execve() was successful. We send a
+                 * timestamp so that the service manager and users can track the precise moment we handed
+                 * over execution of the service to the kernel. */
 
-                if (write(params->exec_fd, &hot, sizeof(hot)) < 0) {
+                if (write(params->exec_fd, &t, sizeof(t)) < 0) {
                         *exit_status = EXIT_EXEC;
                         return log_exec_error_errno(context, params, errno, "Failed to enable exec_fd: %m");
                 }
