949ca25941716604f27745dab0180ca44495fe3a
Moritz Sanft <58110325+msanft@users.noreply.github.com>
2024-04-26 14:59:31 +0200

    repart: respect `SOURCE_DATE_EPOCH` on `mkdir_p_root`
    This let's systemd-repart respect the `SOURCE_DATE_EPOCH` environment
    variable when creating directories in the local tree through `CopyFiles`
    or `MakeDirectories`.
    
    To do this, we pass a timestamp `ts` to `mkdir_p_root`, which it will
    use to fix up `mtime` and `atime` of the directory it creates as
    well as the `mtime` of the directory it creates the other directory *in*,
    as the `mtime` of the latter is modified when creating a directory in it.
    
    For the same reason, it also needs to fixup the `mtime` of the upper
    directory when copying a file into it through `CopyFiles`.
    
    If `SOURCE_DATE_EPOCH`, times are left as is. (`UTIME_OMIT`)

--- src/basic/mkdir.h
+++ src/basic/mkdir.h
@@ -4,6 +4,8 @@
 #include <fcntl.h>
 #include <sys/types.h>
 
+#include "time-util.h"
+
 typedef enum MkdirFlags {
         MKDIR_FOLLOW_SYMLINK  = 1 << 0,
         MKDIR_IGNORE_EXISTING = 1 << 1,  /* Quietly accept a preexisting directory (or file) */
@@ -24,6 +26,7 @@ int mkdir_parents_safe(const char *prefix, const char *path, mode_t mode, uid_t 
 int mkdir_p(const char *path, mode_t mode);
 int mkdir_p_safe(const char *prefix, const char *path, mode_t mode, uid_t uid, gid_t gid, MkdirFlags flags);
 int mkdir_p_root(const char *root, const char *p, uid_t uid, gid_t gid, mode_t m, char **subvolumes);
+int mkdir_p_root_full(const char *root, const char *p, uid_t uid, gid_t gid, mode_t m, char **subvolumes, usec_t ts);
 
 /* The following are used to implement the mkdir_xyz_label() calls, don't use otherwise. */
 typedef int (*mkdirat_func_t)(int dir_fd, const char *pathname, mode_t mode);
