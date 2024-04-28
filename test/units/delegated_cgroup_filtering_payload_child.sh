5ec3acd8fd6b3b488c8ec8804d2fab29fb53bfe5
Daan De Meyer <daan.j.demeyer@gmail.com>
2024-04-28 22:52:59 +0200

    TEST-04-JOURNAL: Make LogFilterPatterns= tests more robust
    Let's use oneshot services as we don't need long running services
    for the tests we're doing. Let's also increase the sleeps a little
    as the current values weren't sufficient when running the test locally
    on my machine with mkosi.

--- test/units/delegated_cgroup_filtering_payload_child.sh
+++ test/units/delegated_cgroup_filtering_payload_child.sh
@@ -3,9 +3,6 @@
 
 echo $$ >/sys/fs/cgroup/system.slice/delegated-cgroup-filtering.service/the_child/cgroup.procs
 
-while true
-do
-	echo "child_process: hello, world!"
-	echo "child_process: hello, people!"
-    sleep .15
-done
+echo "child_process: hello, world!"
+echo "child_process: hello, people!"
+sleep .15
