04863d84d201b7776c552797197925ba0591eac3
Luca Boccassi <bluca@debian.org>
2024-04-29 16:14:12 +0100

    core: do BindMount/MountImage operations in async control process
    These operations might require slow I/O, and thus might block PID1's main
    loop for an undeterminated amount of time. Instead of performing them
    inline, fork a worker process and stash away the D-Bus message, and reply
    once we get a SIGCHILD indicating they have completed. That way we don't
    break compatibility and callers can continue to rely on the fact that when
    they get the method reply the operation either succeeded or failed.
    
    To keep backward compatibility, unlike reload control processes, these
    are ran inside init.scope and not the target cgroup. Unlike ExecReload,
    this is under our control and is not defined by the unit. This is necessary
    because previously the operation also wasn't ran from the target cgroup,
    so suddenly forking a copy-on-write copy of pid1 into the target cgroup
    will make memory usage spike, and if there is a MemoryMax= or MemoryHigh=
    set and the cgroup is already close to the limit, it will cause an OOM
    kill, where previously it would have worked fine.

--- src/shared/bus-wait-for-units.c
+++ src/shared/bus-wait-for-units.c
@@ -24,6 +24,7 @@ typedef struct WaitForItem {
         char *active_state;
         uint32_t job_id;
         char *clean_result;
+        char *mount_result;
 } WaitForItem;
 
 typedef struct BusWaitForUnits {
@@ -67,6 +68,7 @@ static WaitForItem *wait_for_item_free(WaitForItem *item) {
         free(item->bus_path);
         free(item->active_state);
         free(item->clean_result);
+        free(item->mount_result);
 
         return mfree(item);
 }
@@ -178,6 +180,9 @@ static void wait_for_item_check_ready(WaitForItem *item) {
                 if (item->clean_result && !streq(item->clean_result, "success"))
                         d->has_failed = true;
 
+                if (item->mount_result && !streq(item->mount_result, "success"))
+                        d->has_failed = true;
+
                 if (!item->active_state || streq(item->active_state, "maintenance"))
                         return;
         }
@@ -217,6 +222,7 @@ static int wait_for_item_parse_properties(WaitForItem *item, sd_bus_message *m) 
                 { "ActiveState", "s",    NULL,                offsetof(WaitForItem, active_state) },
                 { "Job",         "(uo)", property_map_job_id, offsetof(WaitForItem, job_id)       },
                 { "CleanResult", "s",    NULL,                offsetof(WaitForItem, clean_result) },
+                { "MountResult", "s",    NULL,                offsetof(WaitForItem, mount_result) },
                 {}
         };
 
