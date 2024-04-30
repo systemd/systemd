447e936428abfea4beeeb951ccc0885ce8730320
Daan De Meyer <daan.j.demeyer@gmail.com>
2024-04-30 16:12:35 +0200

    test: Drop /usr overlay workaround
    /usr is not erofs anymore in the mkosi images so let's drop the
    workaround where we mount a writable tmpfs on top of /usr.

--- test/units/testsuite-26.sh
+++ test/units/testsuite-26.sh
@@ -12,15 +12,9 @@ at_exit() {
         rm -fvr "/usr/lib/systemd/system/$UNIT_NAME" "/etc/systemd/system/$UNIT_NAME.d" "+4"
     fi
 
-    maybe_umount_usr_overlay
-
     rm -f /etc/init.d/issue-24990
     return 0
 }
-
-# Needed for /usr/lib/systemd/system/$UNIT_NAME to test overrides in /etc and /run
-maybe_mount_usr_overlay
-trap at_exit EXIT
 
 # Create a simple unit file for testing
 # Note: the service file is created under /usr on purpose to test
