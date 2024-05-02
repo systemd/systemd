339ab61350e7b4a1651f5237564d92b770bd1e20
Luca Boccassi <bluca@debian.org>
2024-05-02 01:51:31 +0100

    debug

--- test/TEST-50-DISSECT/test.sh
+++ test/TEST-50-DISSECT/test.sh
@@ -3,6 +3,7 @@
 # -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
 # ex: ts=8 sw=4 sts=4 et filetype=sh
 set -e
+set -x
 
 TEST_DESCRIPTION="test systemd-dissect"
 IMAGE_NAME="dissect"
