733d0594f024fad76453a1ac38ea68eb0377b064
Daan De Meyer <daan.j.demeyer@gmail.com>
2024-05-04 13:22:53 +0200

    TEST-07-PID1: Lower TriggerLimitIntervalSec= unconditionally
    It shouldn't hurt to do this when KVM is enabled or we're not collecting
    coverage so let's just always lower the trigger limit interval.

--- test/TEST-07-PID1/test.sh
+++ test/TEST-07-PID1/test.sh
@@ -11,16 +11,6 @@ NSPAWN_ARGUMENTS="--capability=CAP_NET_ADMIN"
 . "${TEST_BASE_DIR:?}/test-functions"
 
 test_append_files() {
-    local workspace="${1:?}"
-
-    # We might not be fast enough to hit the limit (20 triggers per 2 secs)
-    # in certain environments, i.e. when running without KVM or when collecting
-    # coverage. Let's help it a bit in such case.
-    if ! get_bool "$QEMU_KVM" || get_bool "$IS_BUILT_WITH_COVERAGE"; then
-        mkdir -p "$workspace/etc/systemd/system/issue2467.socket.d"
-        printf "[Socket]\nTriggerLimitIntervalSec=10\n" >"$workspace/etc/systemd/system/issue2467.socket.d/TriggerLimitInterval.conf"
-    fi
-
     image_install logger socat
 }
 
