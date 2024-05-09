cc74b0348d6b2bf4ac99eb44e3fee54640d8224e
MaxHearnden <maxoscarhearnden@gmail.com>
2024-05-09 14:28:40 +0100

    Allow an entry to suppress initrd= options
    Currently the a kernel initrd can come from the following places:
     - The LOAD_FILE2 protocol where the initrd is measure into PCR 9
     - The initrd= where the initrd is not measured
     - The linux boot protocols which systemd-boot doesn't use
     - A builtin initrd which is measured as part of the kernel
     - A initrdless setup where the behaivour comes from both the kernel and
       cmdline, both of which are measured
    
    Out of these, the initrd= method is the only one which isn't measured.
    By suppressing the initrd= option, we can get a fully measured boot.
    
    This only suppresses the automatic addition of the initrd= option by
    systemd-boot, however this allows for verification that the initrd was
    not loaded through initrd= which is not currently possible.

--- src/boot/efi/boot.c
+++ src/boot/efi/boot.c
@@ -63,6 +63,7 @@ typedef struct {
         char16_t *options;
         bool options_implied; /* If true, these options are implied if we invoke the PE binary without any parameters (as in: UKI). If false we must specify these options explicitly. */
         char16_t **initrd;
+        bool initrd_arg;
         char16_t key;
         EFI_STATUS (*call)(void);
         int tries_done;
@@ -1497,6 +1498,9 @@ static void boot_entry_add_type1(
                                 entry->options = s;
                         } else
                                 entry->options = TAKE_PTR(new);
+
+                } else if (streq8(key, "initrd-arg")) {
+                        entry->initrd_arg = streq8(value, "yes");
                 }
 
         if (entry->type == LOADER_UNDEFINED)
@@ -2374,7 +2378,7 @@ static EFI_STATUS image_start(
         /* If we had to append an initrd= entry to the command line, we have to pass it, and measure it.
          * Otherwise, only pass/measure it if it is not implicit anyway (i.e. embedded into the UKI or
          * so). */
-        _cleanup_free_ char16_t *options = xstrdup16(options_initrd ?: entry->options_implied ? NULL : entry->options);
+        _cleanup_free_ char16_t *options = xstrdup16((entry->initrd_arg && options_initrd) ? options_initrd : entry->options_implied ? NULL : entry->options);
 
         if (entry->type == LOADER_LINUX && !is_confidential_vm()) {
                 const char *extra = smbios_find_oem_string("io.systemd.boot.kernel-cmdline-extra");
