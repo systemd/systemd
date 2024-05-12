d28c6ee5912f4c9637610e30320be13040c05983
Yu Watanabe <watanabe.yu+github@gmail.com>
2024-05-12 16:15:22 +0900

    test: add test for journalctl --list-invocations and --invocation=

---
+++ test/units/testsuite-04.invocation.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: LGPL-2.1-or-later
+set -eux
+set -o pipefail
+
+SERVICE_NAME=invocation-id-test-"$RANDOM".service
+
+TMP_DIR=$(mktemp -d)
+
+for i in {1..10}; do
+    systemd-run --wait -u "$SERVICE_NAME" bash -c "echo invocation ${i} \$INVOCATION_ID; journalctl --sync"
+done
+
+journalctl --list-invocation -u "$SERVICE_NAME" | tee "$TMP_DIR"/10
+journalctl --list-invocation -u "$SERVICE_NAME" --reverse | tee "$TMP_DIR"/10-r
+journalctl --list-invocation -u "$SERVICE_NAME" -n +10| tee "$TMP_DIR"/p10
+journalctl --list-invocation -u "$SERVICE_NAME" -n +10 --reverse | tee "$TMP_DIR"/p10-r
+journalctl --list-invocation -u "$SERVICE_NAME" -n 5 | tee "$TMP_DIR"/5
+journalctl --list-invocation -u "$SERVICE_NAME" -n 5 --reverse | tee "$TMP_DIR"/5-r
+journalctl --list-invocation -u "$SERVICE_NAME" -n +5 | tee "$TMP_DIR"/p5
+journalctl --list-invocation -u "$SERVICE_NAME" -n +5 --reverse | tee "$TMP_DIR"/p5-r
+
+[[ $(cat "$TMP_DIR"/10 | wc -l) == 11 ]]
+[[ $(cat "$TMP_DIR"/10-r | wc -l) == 11 ]]
+[[ $(cat "$TMP_DIR"/p10 | wc -l) == 11 ]]
+[[ $(cat "$TMP_DIR"/p10-r | wc -l) == 11 ]]
+[[ $(cat "$TMP_DIR"/5 | wc -l) == 6 ]]
+[[ $(cat "$TMP_DIR"/5-r | wc -l) == 6 ]]
+[[ $(cat "$TMP_DIR"/p5 | wc -l) == 6 ]]
+[[ $(cat "$TMP_DIR"/p5-r | wc -l) == 6 ]]
+
+diff <(tail -n 10 "$TMP_DIR"/10 | tac) <(tail -n 10 "$TMP_DIR"/10-r)
+diff <(tail -n 5 "$TMP_DIR"/10) <(tail -n 5 "$TMP_DIR"/5)
+diff <(tail -n 5 "$TMP_DIR"/10 | tac) <(tail -n 5 "$TMP_DIR"/5-r)
+diff <(tail -n 10 "$TMP_DIR"/p10 | tac) <(tail -n 10 "$TMP_DIR"/p10-r)
+diff <(tail -n 10 "$TMP_DIR"/p10 | head -n 5) <(tail -n 5 "$TMP_DIR"/p5)
+diff <(tail -n 10 "$TMP_DIR"/p10 | head -n 5 | tac) <(tail -n 5 "$TMP_DIR"/p5-r)
+
+tail -n 10 "$TMP_DIR"/10 |
+    while read -r idx invocation remaining; do
+        i="$(( idx + 10 ))"
+        [[ $(journalctl --no-hostname -n 1 -t bash --invocation="${i}" -u "$SERVICE_NAME") =~ "invocation ${i} ${invocation}" ]]
+        [[ $(journalctl --no-hostname -n 1 -t bash --invocation="${idx}" -u "$SERVICE_NAME") =~ "invocation ${i} ${invocation}" ]]
+        [[ $(journalctl --no-hostname -n 1 -t bash --invocation="${invocation}") =~ "invocation ${i} ${invocation}" ]]
+    done
+
+tail -n 10 "$TMP_DIR"/p10 |
+    while read -r i invocation remaining; do
+        idx="$(( i - 10 ))"
+        [[ $(journalctl --no-hostname -n 1 -t bash --invocation="${i}" -u "$SERVICE_NAME") =~ "invocation ${i} ${invocation}" ]]
+        [[ $(journalctl --no-hostname -n 1 -t bash --invocation="${idx}" -u "$SERVICE_NAME") =~ "invocation ${i} ${invocation}" ]]
+        [[ $(journalctl --no-hostname -n 1 -t bash --invocation="${invocation}") =~ "invocation ${i} ${invocation}" ]]
+    done
