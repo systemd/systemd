45e2faec33ea14732f6eb05bcc46cc99c3fd0b6f
Daan De Meyer <daan.j.demeyer@gmail.com>
2024-05-13 11:26:58 +0200

    TEST-01-BASIC: Drop logic to run test without install-tests=true
    We don't support this for any other tests either so let's drop the
    support for running TEST-01-BASIC without installing as well to make
    the upcoming commit easier to implement.

--- test/TEST-01-BASIC/test.sh
+++ test/TEST-01-BASIC/test.sh
@@ -16,14 +16,4 @@ KERNEL_APPEND="foo -- -z bar --- baz $KERNEL_APPEND"
 # shellcheck source=test/test-functions
 . "${TEST_BASE_DIR:?}/test-functions"
 
-test_append_files() {
-    local workspace="${1:?}"
-    local dst
-
-    # Install tests manually so the test is functional even when -Dinstall-tests=false
-    dst="$workspace/usr/lib/systemd/tests/testdata/units/"
-    mkdir -p "$dst"
-    cp -v "$TEST_UNITS_DIR"/{testsuite-01,end}.service "$TEST_UNITS_DIR/testsuite.target" "$dst"
-}
-
 do_test "$@"
