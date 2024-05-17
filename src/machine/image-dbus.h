b6facdd847bf63a1245fc7c46a3b6e9a3af539a1
Yu Watanabe <watanabe.yu+github@gmail.com>
2024-05-18 05:31:16 +0900

    machine: split out manager_acquire_image() from image_object_find()
    Preparation for the next commit. No functional change.

--- src/machine/image-dbus.h
+++ src/machine/image-dbus.h
@@ -2,10 +2,12 @@
 #pragma once
 
 #include "bus-object.h"
+#include "discover-image.h"
 #include "machined.h"
 
 extern const BusObjectImplementation image_object;
 
+int manager_acquire_image(Manager *m, const char *name, Image **ret);
 char *image_bus_path(const char *name);
 
 int bus_image_method_remove(sd_bus_message *message, void *userdata, sd_bus_error *error);
