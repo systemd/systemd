64b0aaac170d2b13bb06e6388b76e038c3b38d47
Łukasz Stelmach <l.stelmach@samsung.com>
2024-05-20 16:51:55 +0200

    core: drop ambient capabilities in systemd-executor
    Since the commit 963b6b906e ("core: drop ambient capabilities in
    user manager") systemd running as the session manager has dropped ambient
    capabilities retaining other sets allowing user services to be started
    with elevated capabilities. This, worked fine until the introduction of
    sd-executor. For a non-root process to be started with elevated
    capabilities by a non-root parent it either needs file capabilities or
    ambient capabilities in the parent process. Thus, systemd needs to allow
    sd-executor to inherit its ambient capabilities and sd-executor should
    drop them as systemd did before.
    
    Fixes: bb5232b6a3 ("core: add systemd-executor binary")

--- src/core/main.c
+++ src/core/main.c
@@ -3130,11 +3130,6 @@ int main(int argc, char *argv[]) {
                 /* clear the kernel timestamp, because we are not PID 1 */
                 kernel_timestamp = DUAL_TIMESTAMP_NULL;
 
-                /* Clear ambient capabilities, so services do not inherit them implicitly. Dropping them does
-                 * not affect the permitted and effective sets which are important for the manager itself to
-                 * operate. */
-                capability_ambient_set_apply(0, /* also_inherit= */ false);
-
                 if (mac_init() < 0) {
                         error_message = "Failed to initialize MAC support";
                         goto finish;
