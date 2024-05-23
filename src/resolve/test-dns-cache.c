607415fed3c4b4f5d39bd2ba4af20041244af9c1
James Coglan <james@neighbourhood.ie>
2024-05-23 09:53:42 +0100

    resolved: tests for dns_cache_lookup() for NXDOMAIN

--- src/resolve/test-dns-cache.c
+++ src/resolve/test-dns-cache.c
@@ -281,7 +281,7 @@ TEST(dns_a_to_cname_success_escaped_name_returns_error) {
  * dns_cache_lookup()
  * ================================================================ */
 
-TEST(dns_cache_lookup_single) {
+TEST(dns_cache_lookup_success) {
         _cleanup_(dns_cache_unrefp) DnsCache cache = new_cache();
         _cleanup_(put_args_unrefp) PutArgs put_args = mk_put_args();
         _cleanup_(dns_answer_unrefp) DnsAnswer *ret_answer = NULL;
@@ -312,6 +312,46 @@ TEST(dns_cache_lookup_single) {
 
         rr = dns_resource_record_new_full(DNS_CLASS_IN, DNS_TYPE_A, "www.example.com");
         rr->a.in_addr.s_addr = htobe32(0xc0a8017f);
+        ASSERT_TRUE(dns_answer_contains(ret_answer, rr));
+}
+
+TEST(dns_cache_lookup_nxdomain) {
+        _cleanup_(dns_cache_unrefp) DnsCache cache = new_cache();
+        _cleanup_(put_args_unrefp) PutArgs put_args = mk_put_args();
+        _cleanup_(dns_answer_unrefp) DnsAnswer *ret_answer = NULL;
+        _cleanup_(dns_packet_unrefp) DnsPacket *ret_full_packet = NULL;
+        _cleanup_(dns_resource_record_unrefp) DnsResourceRecord *rr = NULL;
+        _cleanup_(dns_resource_key_unrefp) DnsResourceKey *key = NULL;
+        int query_flags, ret_rcode;
+        uint64_t ret_query_flags;
+
+        put_args.key = dns_resource_key_new(DNS_CLASS_IN, DNS_TYPE_A, "www.example.com");
+        put_args.rcode = DNS_RCODE_NXDOMAIN;
+        dns_answer_add_soa(put_args.answer, "example.com", 3600, 0);
+        cache_put(&cache, &put_args);
+
+        ASSERT_EQ(dns_cache_size(&cache), 1u);
+
+        key = dns_resource_key_new(DNS_CLASS_IN, DNS_TYPE_A, "www.example.com");
+        query_flags = 0;
+        ASSERT_TRUE(dns_cache_lookup(&cache, key, query_flags, &ret_rcode, &ret_answer, &ret_full_packet, &ret_query_flags, NULL));
+
+        ASSERT_EQ(cache.n_hit, 1u);
+        ASSERT_EQ(cache.n_miss, 0u);
+
+        ASSERT_EQ(ret_rcode, DNS_RCODE_NXDOMAIN);
+        ASSERT_EQ(ret_query_flags, (SD_RESOLVED_AUTHENTICATED | SD_RESOLVED_CONFIDENTIAL));
+
+        ASSERT_EQ(dns_answer_size(ret_answer), 1u);
+
+        rr = dns_resource_record_new_full(DNS_CLASS_IN, DNS_TYPE_SOA, "example.com");
+        rr->soa.mname = strdup("example.com");
+        rr->soa.rname = strdup("root.example.com");
+        rr->soa.serial = 1;
+        rr->soa.refresh = 1;
+        rr->soa.retry = 1;
+        rr->soa.expire = 1;
+        rr->soa.minimum = 3600;
         ASSERT_TRUE(dns_answer_contains(ret_answer, rr));
 }
 
