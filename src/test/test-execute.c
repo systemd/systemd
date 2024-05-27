4bad7ec8ac56045c51a36c381ca5fd043e8cea4e
Daan De Meyer <daan.j.demeyer@gmail.com>
2024-05-27 13:33:51 +0200

    test-execute: Skip system call filter tests when sanitizers are used
    System call filtering is incompatible with sanitizers so let's skip
    these tests when we're built with sanitizers.

--- src/test/test-execute.c
+++ src/test/test-execute.c
@@ -632,7 +632,7 @@ static void test_exec_temporaryfilesystem(Manager *m) {
 }
 
 static void test_exec_systemcallfilter(Manager *m) {
-#if HAVE_SECCOMP
+#if HAVE_SECCOMP && !HAS_FEATURE_ADDRESS_SANITIZER
         int r;
 
         if (!is_seccomp_available()) {
@@ -675,7 +675,7 @@ static void test_exec_systemcallfilter(Manager *m) {
 }
 
 static void test_exec_systemcallerrornumber(Manager *m) {
-#if HAVE_SECCOMP
+#if HAVE_SECCOMP && !HAS_FEATURE_ADDRESS_SANITIZER
         int r;
 
         if (!is_seccomp_available()) {
