c7791557a88f9007a4ed875bc99632d05f1e30bf
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2024-05-28 20:14:48 +0200

    sd-event: use consistent style for '*' in signatures

--- src/systemd/sd-event.h
+++ src/systemd/sd-event.h
@@ -123,7 +123,7 @@ sd_event_source* sd_event_source_ref(sd_event_source *s);
 sd_event_source* sd_event_source_unref(sd_event_source *s);
 sd_event_source* sd_event_source_disable_unref(sd_event_source *s);
 
-sd_event *sd_event_source_get_event(sd_event_source *s);
+sd_event* sd_event_source_get_event(sd_event_source *s);
 void* sd_event_source_get_userdata(sd_event_source *s);
 void* sd_event_source_set_userdata(sd_event_source *s, void *userdata);
 
