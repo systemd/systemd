e7bcd68b6417bd0c840f55d06156f5f588a3d5f8
James Coglan <james@neighbourhood.ie>
2024-05-30 17:22:30 +0100

    resolved: tests for dns_question_new_address()

--- src/resolve/test-dns-question.c
+++ src/resolve/test-dns-question.c
@@ -44,4 +44,47 @@ TEST(dns_question_add_with_space) {
         assert(dns_question_isempty(question) == 0);
 }
 
+/* ================================================================
+ * dns_question_new_address()
+ * ================================================================ */
+
+TEST(dns_question_new_address_ipv4) {
+        _cleanup_(dns_question_unrefp) DnsQuestion *question = NULL;
+        _cleanup_(dns_resource_key_unrefp) DnsResourceKey *key = NULL;
+
+        ASSERT_OK(dns_question_new_address(&question, AF_INET, "www.example.com", 0));
+        ASSERT_NOT_NULL(question);
+
+        key = dns_resource_key_new(DNS_CLASS_IN, DNS_TYPE_A, "www.example.com");
+
+        ASSERT_EQ(dns_question_size(question), 1u);
+        ASSERT_TRUE(dns_question_contains_key(question, key));
+}
+
+TEST(dns_question_new_address_ipv6) {
+        _cleanup_(dns_question_unrefp) DnsQuestion *question = NULL;
+        _cleanup_(dns_resource_key_unrefp) DnsResourceKey *key = NULL;
+
+        ASSERT_OK(dns_question_new_address(&question, AF_INET6, "www.example.com", 0));
+        ASSERT_NOT_NULL(question);
+
+        key = dns_resource_key_new(DNS_CLASS_IN, DNS_TYPE_AAAA, "www.example.com");
+
+        ASSERT_EQ(dns_question_size(question), 1u);
+        ASSERT_TRUE(dns_question_contains_key(question, key));
+}
+
+TEST(dns_question_new_address_convert_idna) {
+        _cleanup_(dns_question_unrefp) DnsQuestion *question = NULL;
+        _cleanup_(dns_resource_key_unrefp) DnsResourceKey *key = NULL;
+
+        ASSERT_OK(dns_question_new_address(&question, AF_INET, "www.\xF0\x9F\x98\xB1.com", 1));
+        ASSERT_NOT_NULL(question);
+
+        key = dns_resource_key_new(DNS_CLASS_IN, DNS_TYPE_A, "www.xn--s38h.com");
+
+        ASSERT_EQ(dns_question_size(question), 1u);
+        ASSERT_TRUE(dns_question_contains_key(question, key));
+}
+
 DEFINE_TEST_MAIN(LOG_DEBUG);
