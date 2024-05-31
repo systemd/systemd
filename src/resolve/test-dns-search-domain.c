2fef64b21a52f019766f6d6be4a612d656898b54
James Coglan <james@neighbourhood.ie>
2024-05-31 14:50:20 +0100

    resolved: extract function for checking search domains

--- src/resolve/test-dns-search-domain.c
+++ src/resolve/test-dns-search-domain.c
@@ -7,6 +7,17 @@
 #include "log.h"
 #include "tests.h"
 
+static void check_domains(DnsSearchDomain *head, const char **expected, size_t n) {
+        size_t i = 0;
+
+        LIST_FOREACH(domains, d, head) {
+                ASSERT_STREQ(DNS_SEARCH_DOMAIN_NAME(d), expected[i]);
+                i++;
+        }
+
+        ASSERT_EQ(i, n);
+}
+
 /* ================================================================
  * dns_search_domain_new()
  * ================================================================ */
@@ -71,8 +82,6 @@ TEST(dns_search_domain_new_link_limit) {
 TEST(dns_search_domain_unlink_system) {
         Manager manager = {};
         _cleanup_(dns_search_domain_unrefp) DnsSearchDomain *sd1 = NULL, *sd2 = NULL, *sd3 = NULL;
-        const char *names[2];
-        unsigned int i = 0;
 
         dns_search_domain_new(&manager, &sd1, DNS_SEARCH_DOMAIN_SYSTEM, NULL, "local");
         dns_search_domain_new(&manager, &sd2, DNS_SEARCH_DOMAIN_SYSTEM, NULL, "vpn.example.com");
@@ -86,20 +95,14 @@ TEST(dns_search_domain_unlink_system) {
         ASSERT_FALSE(sd2->linked);
         ASSERT_EQ(manager.n_search_domains, 2u);
 
-        LIST_FOREACH(domains, d, manager.search_domains) {
-                names[i++] = DNS_SEARCH_DOMAIN_NAME(d);
-        }
-
-        ASSERT_STREQ(names[0], "local");
-        ASSERT_STREQ(names[1], "org");
+        const char *expected[] = { "local", "org" };
+        check_domains(manager.search_domains, expected, 2);
 }
 
 TEST(dns_search_domain_unlink_link) {
         Manager manager = {};
         _cleanup_(link_freep) Link *link = NULL;
         _cleanup_(dns_search_domain_unrefp) DnsSearchDomain *sd1 = NULL, *sd2 = NULL, *sd3 = NULL;
-        const char *names[2];
-        unsigned int i = 0;
 
         link_new(&manager, &link, 1);
 
@@ -115,12 +118,8 @@ TEST(dns_search_domain_unlink_link) {
         ASSERT_FALSE(sd2->linked);
         ASSERT_EQ(link->n_search_domains, 2u);
 
-        LIST_FOREACH(domains, d, link->search_domains) {
-                names[i++] = DNS_SEARCH_DOMAIN_NAME(d);
-        }
-
-        ASSERT_STREQ(names[0], "local");
-        ASSERT_STREQ(names[1], "org");
+        const char *expected[] = { "local", "org" };
+        check_domains(link->search_domains, expected, 2);
 }
 
 DEFINE_TEST_MAIN(LOG_DEBUG);
