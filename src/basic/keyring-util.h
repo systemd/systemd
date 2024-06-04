375489f82ed437f176be28ea1514a488bb5c79c5
Luca Boccassi <bluca@debian.org>
2024-06-04 13:23:30 +0100

    util: add keyring_describe helper and move to basic
    So that it can be used from libsystemd. No external dependencies.

--- src/basic/keyring-util.h
+++ src/basic/keyring-util.h
@@ -9,3 +9,4 @@
 #define TAKE_KEY_SERIAL(key_serial) TAKE_GENERIC(key_serial, key_serial_t, -1)
 
 int keyring_read(key_serial_t serial, void **ret, size_t *ret_size);
+int keyring_describe(key_serial_t serial, char **ret);
