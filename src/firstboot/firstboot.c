8f9034de916955b07062b2d43354a5901800d4ca
Michael Ferrari <nekkodroid404@gmail.com>
2024-06-06 10:33:53 +0200

    firstboot: add similar input suggestion

--- src/firstboot/firstboot.c
+++ src/firstboot/firstboot.c
@@ -203,6 +203,7 @@ static int prompt_loop(const char *text, char **l, const char* fallback, unsigne
 
         for (;;) {
                 _cleanup_free_ char *p = NULL;
+                char *best_match = NULL;
                 unsigned u;
 
                 r = ask_string(&p, "%s %s (empty to default to \"%s\", \"-\" to skip, \"list\" to list options): ",
@@ -240,12 +241,20 @@ static int prompt_loop(const char *text, char **l, const char* fallback, unsigne
                         return free_and_strdup_warn(ret, l[u-1]);
                 }
 
-                if (!is_valid(p)) {
-                        log_error("Entered data invalid.");
-                        continue;
-                }
+                if (is_valid(p))
+                        return free_and_replace(*ret, p);
 
-                return free_and_replace(*ret, p);
+                /* Be helperful to the user, and give a hint what the user might have wanted to
+                 * type. We search with two mechanisms: a simple prefix match and – if that didn't
+                 * yield results –, a Levenshtein word distance based match. */
+                best_match = strv_find_prefix(l, p);
+                if (!best_match)
+                        best_match = strv_find_closest(l, p);
+
+                if (best_match)
+                        log_error("Invalid data '%s', did you mean '%s'?", p, best_match);
+                else
+                        log_error("Invalid data '%s'.", p);
         }
 }
 
