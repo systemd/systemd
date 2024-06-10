dadde1fef2e2eecb3b6504852f8adbc2209ad3ba
James Coglan <james@neighbourhood.ie>
2024-06-10 10:16:01 +0100

    resolved: tests for dns_packet_set_flags()

---
+++ src/resolve/test-dns-packet-append.c
@@ -0,0 +1,135 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+
+#include "dns-type.h"
+#include "resolved-dns-packet.h"
+#include "resolved-dns-rr.h"
+
+#include "log.h"
+#include "tests.h"
+
+#define BIT_QR (1 << 7)
+#define BIT_AA (1 << 2)
+#define BIT_TC (1 << 1)
+#define BIT_RD (1 << 0)
+
+#define BIT_RA (1 << 7)
+#define BIT_AD (1 << 5)
+#define BIT_CD (1 << 4)
+
+/* ================================================================
+ * dns_packet_set_flags()
+ * ================================================================ */
+
+TEST(packet_set_flags_dns_checking_enabled) {
+        _cleanup_(dns_packet_unrefp) DnsPacket *packet = NULL;
+
+        dns_packet_new(&packet, DNS_PROTOCOL_DNS, 0, DNS_PACKET_SIZE_MAX);
+        dns_packet_set_flags(packet, false, false);
+
+        ASSERT_EQ(DNS_PACKET_QR(packet), 0);
+        ASSERT_EQ(DNS_PACKET_OPCODE(packet), 0);
+        ASSERT_EQ(DNS_PACKET_AA(packet), 0);
+        ASSERT_EQ(DNS_PACKET_TC(packet), 0);
+        ASSERT_EQ(DNS_PACKET_RD(packet), 1);
+
+        ASSERT_EQ(DNS_PACKET_RA(packet), 0);
+        ASSERT_EQ(DNS_PACKET_AD(packet), 0);
+        ASSERT_EQ(DNS_PACKET_CD(packet), 0);
+        ASSERT_EQ(DNS_PACKET_RCODE(packet), 0);
+}
+
+TEST(packet_set_flags_dns_checking_disabled) {
+        _cleanup_(dns_packet_unrefp) DnsPacket *packet = NULL;
+
+        dns_packet_new(&packet, DNS_PROTOCOL_DNS, 0, DNS_PACKET_SIZE_MAX);
+        dns_packet_set_flags(packet, true, false);
+
+        ASSERT_EQ(DNS_PACKET_QR(packet), 0);
+        ASSERT_EQ(DNS_PACKET_OPCODE(packet), 0);
+        ASSERT_EQ(DNS_PACKET_AA(packet), 0);
+        ASSERT_EQ(DNS_PACKET_TC(packet), 0);
+        ASSERT_EQ(DNS_PACKET_RD(packet), 1);
+
+        ASSERT_EQ(DNS_PACKET_RA(packet), 0);
+        ASSERT_EQ(DNS_PACKET_AD(packet), 0);
+        ASSERT_EQ(DNS_PACKET_CD(packet), 1);
+        ASSERT_EQ(DNS_PACKET_RCODE(packet), 0);
+}
+
+TEST(packet_set_flags_llmnr) {
+        _cleanup_(dns_packet_unrefp) DnsPacket *packet = NULL;
+
+        dns_packet_new(&packet, DNS_PROTOCOL_LLMNR, 0, DNS_PACKET_SIZE_MAX);
+        dns_packet_set_flags(packet, true, false);
+
+        ASSERT_EQ(DNS_PACKET_QR(packet), 0);
+        ASSERT_EQ(DNS_PACKET_OPCODE(packet), 0);
+        ASSERT_EQ(DNS_PACKET_AA(packet), 0);
+        ASSERT_EQ(DNS_PACKET_TC(packet), 0);
+        ASSERT_EQ(DNS_PACKET_RD(packet), 0);
+
+        ASSERT_EQ(DNS_PACKET_RA(packet), 0);
+        ASSERT_EQ(DNS_PACKET_AD(packet), 0);
+        ASSERT_EQ(DNS_PACKET_CD(packet), 0);
+        ASSERT_EQ(DNS_PACKET_RCODE(packet), 0);
+}
+
+TEST(packet_set_flags_mdns_not_truncated) {
+        _cleanup_(dns_packet_unrefp) DnsPacket *packet = NULL;
+
+        dns_packet_new(&packet, DNS_PROTOCOL_MDNS, 0, DNS_PACKET_SIZE_MAX);
+        dns_packet_set_flags(packet, true, false);
+
+        ASSERT_EQ(DNS_PACKET_QR(packet), 0);
+        ASSERT_EQ(DNS_PACKET_OPCODE(packet), 0);
+        ASSERT_EQ(DNS_PACKET_AA(packet), 0);
+        ASSERT_EQ(DNS_PACKET_TC(packet), 0);
+        ASSERT_EQ(DNS_PACKET_RD(packet), 0);
+
+        ASSERT_EQ(DNS_PACKET_RA(packet), 0);
+        ASSERT_EQ(DNS_PACKET_AD(packet), 0);
+        ASSERT_EQ(DNS_PACKET_CD(packet), 0);
+        ASSERT_EQ(DNS_PACKET_RCODE(packet), 0);
+}
+
+TEST(packet_set_flags_mdns_truncated) {
+        _cleanup_(dns_packet_unrefp) DnsPacket *packet = NULL;
+
+        dns_packet_new(&packet, DNS_PROTOCOL_MDNS, 0, DNS_PACKET_SIZE_MAX);
+        dns_packet_set_flags(packet, true, true);
+
+        ASSERT_EQ(DNS_PACKET_QR(packet), 0);
+        ASSERT_EQ(DNS_PACKET_OPCODE(packet), 0);
+        ASSERT_EQ(DNS_PACKET_AA(packet), 0);
+        ASSERT_EQ(DNS_PACKET_TC(packet), 1);
+        ASSERT_EQ(DNS_PACKET_RD(packet), 0);
+
+        ASSERT_EQ(DNS_PACKET_RA(packet), 0);
+        ASSERT_EQ(DNS_PACKET_AD(packet), 0);
+        ASSERT_EQ(DNS_PACKET_CD(packet), 0);
+        ASSERT_EQ(DNS_PACKET_RCODE(packet), 0);
+}
+
+/* ================================================================
+ * dns_packet_new_query()
+ * ================================================================ */
+
+TEST(packet_new_query_checking_enabled) {
+        _cleanup_(dns_packet_unrefp) DnsPacket *packet = NULL;
+
+        dns_packet_new_query(&packet, DNS_PROTOCOL_DNS, 0, false);
+
+        ASSERT_EQ(DNS_PACKET_TC(packet), 0);
+        ASSERT_EQ(DNS_PACKET_CD(packet), 0);
+}
+
+TEST(packet_new_query_checking_disabled) {
+        _cleanup_(dns_packet_unrefp) DnsPacket *packet = NULL;
+
+        dns_packet_new_query(&packet, DNS_PROTOCOL_DNS, 0, true);
+
+        ASSERT_EQ(DNS_PACKET_TC(packet), 0);
+        ASSERT_EQ(DNS_PACKET_CD(packet), 1);
+}
+
+DEFINE_TEST_MAIN(LOG_DEBUG)
