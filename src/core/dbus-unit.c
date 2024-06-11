dd22c315462f806589e5e305f6145b95575fcff9
Mike Yuan <me@yhndnzj.com>
2024-06-11 19:09:05 +0200

    core/dbus-unit: add an explicit bus error when unit is frozen by parent

--- src/core/dbus-unit.c
+++ src/core/dbus-unit.c
@@ -756,7 +756,7 @@ static int bus_unit_method_freezer_generic(sd_bus_message *message, void *userda
         if (r == -EALREADY)
                 return sd_bus_error_set(error, BUS_ERROR_UNIT_BUSY, "Previously requested freezer operation for unit is still in progress");
         if (r == -ECHILD)
-                return sd_bus_error_set(error, SD_BUS_ERROR_FAILED, "Unit is frozen by a parent slice");
+                return sd_bus_error_set(error, BUS_ERROR_FROZEN_BY_PARENT, "Unit is frozen by a parent slice");
         if (r < 0)
                 return r;
 
