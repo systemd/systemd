58d4e82262586f43f04d15c5ee800ab1f264b2d2
Lennart Poettering <lennart@poettering.net>
2024-06-20 09:24:18 +0200

    json: make it easy to serialize our enums to json
    Most of our enums are mapped to strings that use dashes ("-") as word
    separators, i.e. "foo-bar-baz". However, Varlink enums do not allow "-"
    as separator, see:
    
    https://varlink.org/Interface-Definition
    
    Hence, let's add some simple glue to automatucally turn "-" into "_" for
    use when serializing our enums.

--- src/libsystemd/sd-json/sd-json.c
+++ src/libsystemd/sd-json/sd-json.c
@@ -3528,6 +3528,40 @@ _public_ int sd_json_buildv(sd_json_variant **ret, va_list ap) {
                         break;
                 }
 
+                case _JSON_BUILD_STRING_UNDERSCORIFY: {
+                        const char *p;
+
+                        if (!IN_SET(current->expect, EXPECT_TOPLEVEL, EXPECT_OBJECT_VALUE, EXPECT_ARRAY_ELEMENT)) {
+                                r = -EINVAL;
+                                goto finish;
+                        }
+
+                        p = va_arg(ap, const char *);
+
+                        if (current->n_suppress == 0) {
+                                _cleanup_free_ char *c = strreplace(p, "-", "_");
+                                if (!c) {
+                                        r = -ENOMEM;
+                                        goto finish;
+                                }
+
+                                r = sd_json_variant_new_string(&add, c);
+                                if (r < 0)
+                                        goto finish;
+                        }
+
+                        n_subtract = 1;
+
+                        if (current->expect == EXPECT_TOPLEVEL)
+                                current->expect = EXPECT_END;
+                        else if (current->expect == EXPECT_OBJECT_VALUE)
+                                current->expect = EXPECT_OBJECT_KEY;
+                        else
+                                assert(current->expect == EXPECT_ARRAY_ELEMENT);
+
+                        break;
+                }
+
                 case _SD_JSON_BUILD_INTEGER: {
                         int64_t j;
 
