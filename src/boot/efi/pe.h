51be9143452e178817f7fa69f44e6a09a4128e14
Lennart Poettering <lennart@poettering.net>
2024-07-01 17:50:56 +0200

    boot: only open type2 ukis once when parsing

--- src/boot/efi/pe.h
+++ src/boot/efi/pe.h
@@ -52,10 +52,4 @@ EFI_STATUS pe_memory_locate_sections(
                 const char *const section_names[],
                 PeSectionVector sections[]);
 
-EFI_STATUS pe_file_locate_sections(
-                EFI_FILE *dir,
-                const char16_t *path,
-                const char *const section_names[],
-                PeSectionVector sections[]);
-
 EFI_STATUS pe_kernel_info(const void *base, uint32_t *ret_compat_address);
