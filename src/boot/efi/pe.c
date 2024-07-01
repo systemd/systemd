3da1a4654337f93bb6679f10bde04f56f22e15a0
Lennart Poettering <lennart@poettering.net>
2024-07-01 17:39:49 +0200

    pe: don't non-chalantly drop const from memory buffer

--- src/boot/efi/pe.c
+++ src/boot/efi/pe.c
@@ -261,7 +261,7 @@ static uint32_t get_compatibility_entry_address(const DosFileHeader *dos, const 
         size_t addr = vector.memory_offset, size = vector.size;
 
         while (size >= sizeof(LinuxPeCompat1) && addr % alignof(LinuxPeCompat1) == 0) {
-                LinuxPeCompat1 *compat = (LinuxPeCompat1 *) ((uint8_t *) dos + addr);
+                const LinuxPeCompat1 *compat = (const LinuxPeCompat1 *) ((const uint8_t *) dos + addr);
 
                 if (compat->type == 0 || compat->size == 0 || compat->size > size)
                         break;
