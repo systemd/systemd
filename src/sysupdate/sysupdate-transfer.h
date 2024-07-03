33dd98f33e729aa26a61a3aa94b2a7cc811accbb
Adrian Vovk <adrianvovk@gmail.com>
2024-07-03 17:49:36 -0400

    sysupdate: Add verb to inspect features

--- src/sysupdate/sysupdate-transfer.h
+++ src/sysupdate/sysupdate-transfer.h
@@ -15,11 +15,14 @@ typedef struct Transfer Transfer;
 #include "sysupdate.h"
 
 struct Transfer {
+        char *id;
+
         char *min_version;
         char **protected_versions;
         char *current_symlink;
         bool verify;
 
+        char **member_of_features;
         bool enabled;
 
         Resource source, target;
