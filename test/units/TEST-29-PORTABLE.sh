92d1fe3efac7b3a700317ec71b64cab5ebc17b42
Luca Boccassi <bluca@debian.org>
2024-07-03 21:27:28 +0100

    os-util: avoid matching on the wrong extension-release file
    The previous commit tries to extract a substring from the
    extension-release suffix, but that is not right, it's only the
    images that need to be versioned and extracted, use the extension-release
    suffix as-is. Otherwise if it happens to contain a prefix that
    matches the wrong image, it will be taken into account.
    
    Follow-up for 37543971aff79f3a37646ffc2bb5845c9394797b

--- test/units/TEST-29-PORTABLE.sh
+++ test/units/TEST-29-PORTABLE.sh
@@ -354,6 +354,17 @@ portablectl detach --now --runtime --enable /tmp/rootdir minimal-app0
 portablectl "${ARGS[@]}" attach --copy=symlink --now --runtime /tmp/rootdir minimal-app0
 portablectl detach --now --runtime --enable /tmp/rootdir minimal-app0
 
+# The wrong file should be ignored, given the right one has the xattr set
+mkdir -p /tmp/wrongext/usr/lib/extension-release.d /tmp/wrongext/usr/lib/systemd/system/
+echo "[Service]" > /tmp/wrongext/usr/lib/systemd/system/app0.service
+touch /tmp/wrongext/usr/lib/extension-release.d/extension-release.wrongext_somethingwrong.txt
+cp /tmp/rootdir/usr/lib/os-release /tmp/wrongext/usr/lib/extension-release.d/extension-release.app0
+setfattr -n user.extension-release.strict -v "false" /tmp/wrongext/usr/lib/extension-release.d/extension-release.app0
+portablectl "${ARGS[@]}" attach --runtime --extension /tmp/wrongext /tmp/rootdir app0
+status="$(portablectl is-attached --extension wrongext rootdir)"
+[[ "${status}" == "attached-runtime" ]]
+portablectl detach --runtime --extension /tmp/wrongext /tmp/rootdir app0
+
 umount /tmp/rootdir
 umount /tmp/app0
 umount /tmp/app1
