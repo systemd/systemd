db0c00d005c92631af5991f16a7e6abd71fd3c04
Matteo Croce <teknoraver@meta.com>
2024-07-04 03:14:22 +0200

    use O_PATH when getting cgroup fd
    O_PATH mode is enough to attach a bpf program to a cgroup, use it
    instead of O_RDONLY

--- src/core/restrict-ifaces.c
+++ src/core/restrict-ifaces.c
@@ -123,7 +123,7 @@ static int restrict_network_interfaces_install_impl(Unit *u) {
         if (r < 0)
                 return r;
 
-        cgroup_fd = open(cgroup_path, O_RDONLY | O_CLOEXEC | O_DIRECTORY, 0);
+        cgroup_fd = open(cgroup_path, O_PATH | O_CLOEXEC | O_DIRECTORY, 0);
         if (cgroup_fd < 0)
                 return -errno;
 
