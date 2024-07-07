d1ff4bb3195d09b831b42196957617d9f527c43d
Mike Yuan <me@yhndnzj.com>
2024-07-07 19:35:40 +0200

    core: dlopen()'ify libapparmor
    In Arch Linux we currently have a half-baked apparmor support,
    in particular we cannot link systemd to libapparmor for service
    context integration, since that will pull apparmor into base system.
    Hence, let's turn this into a dlopen dep.
    
    Ref: https://gitlab.archlinux.org/archlinux/packaging/packages/systemd/-/issues/22

--- src/shared/apparmor-util.c
+++ src/shared/apparmor-util.c
@@ -7,6 +7,39 @@
 #include "fileio.h"
 #include "parse-util.h"
 
+#if HAVE_APPARMOR
+static void *libapparmor_dl = NULL;
+
+DLSYM_PROTOTYPE(aa_change_onexec) = NULL;
+DLSYM_PROTOTYPE(aa_change_profile) = NULL;
+DLSYM_PROTOTYPE(aa_features_new_from_kernel) = NULL;
+DLSYM_PROTOTYPE(aa_features_unref) = NULL;
+DLSYM_PROTOTYPE(aa_policy_cache_dir_path_preview) = NULL;
+DLSYM_PROTOTYPE(aa_policy_cache_new) = NULL;
+DLSYM_PROTOTYPE(aa_policy_cache_replace_all) = NULL;
+DLSYM_PROTOTYPE(aa_policy_cache_unref) = NULL;
+
+int dlopen_libapparmor(void) {
+        ELF_NOTE_DLOPEN("apparmor",
+                        "Support for AppArmor policies",
+                        ELF_NOTE_DLOPEN_PRIORITY_RECOMMENDED,
+                        "libapparmor.so.1");
+
+        return dlopen_many_sym_or_warn(
+                        &libapparmor_dl,
+                        "libapparmor.so.1",
+                        LOG_DEBUG,
+                        DLSYM_ARG(aa_change_onexec),
+                        DLSYM_ARG(aa_change_profile),
+                        DLSYM_ARG(aa_features_new_from_kernel),
+                        DLSYM_ARG(aa_features_unref),
+                        DLSYM_ARG(aa_policy_cache_dir_path_preview),
+                        DLSYM_ARG(aa_policy_cache_new),
+                        DLSYM_ARG(aa_policy_cache_replace_all),
+                        DLSYM_ARG(aa_policy_cache_unref));
+}
+#endif
+
 bool mac_apparmor_use(void) {
         static int cached_use = -1;
 
--- src/shared/apparmor-util.h
+++ src/shared/apparmor-util.h
@@ -3,4 +3,28 @@
 
 #include <stdbool.h>
 
+#include "dlfcn-util.h"
+
+#if HAVE_APPARMOR
+#  include <sys/apparmor.h>
+
+extern DLSYM_PROTOTYPE(aa_change_onexec);
+extern DLSYM_PROTOTYPE(aa_change_profile);
+extern DLSYM_PROTOTYPE(aa_features_new_from_kernel);
+extern DLSYM_PROTOTYPE(aa_features_unref);
+extern DLSYM_PROTOTYPE(aa_policy_cache_dir_path_preview);
+extern DLSYM_PROTOTYPE(aa_policy_cache_new);
+extern DLSYM_PROTOTYPE(aa_policy_cache_replace_all);
+extern DLSYM_PROTOTYPE(aa_policy_cache_unref);
+
+DEFINE_TRIVIAL_CLEANUP_FUNC_FULL(aa_features*, sym_aa_features_unref, NULL);
+DEFINE_TRIVIAL_CLEANUP_FUNC_FULL(aa_policy_cache*, sym_aa_policy_cache_unref, NULL);
+
+int dlopen_libapparmor(void);
+#else
+static inline int dlopen_libapparmor(void) {
+        return -EOPNOTSUPP;
+}
+#endif
+
 bool mac_apparmor_use(void);
