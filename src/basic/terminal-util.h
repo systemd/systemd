aab4bb07e1d1899d51036c7099814f9d547e79d3
Lennart Poettering <lennart@poettering.net>
2024-07-11 10:47:01 +0200

    terminal-util: don't export vt_reset_keyboard() + vt_default_utf8()

--- src/basic/terminal-util.h
+++ src/basic/terminal-util.h
@@ -277,8 +277,6 @@ int openpt_allocate(int flags, char **ret_slave);
 int openpt_allocate_in_namespace(pid_t pid, int flags, char **ret_slave);
 int open_terminal_in_namespace(pid_t pid, const char *name, int mode);
 
-int vt_default_utf8(void);
-int vt_reset_keyboard(int fd);
 int vt_restore(int fd);
 int vt_release(int fd, bool restore_vt);
 
