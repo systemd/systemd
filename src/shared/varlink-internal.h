b825a8be0b7b857a715e982cee861e8ae6995ee8
Mike Yuan <me@yhndnzj.com>
2024-07-23 17:55:12 +0200

    core: reliably check if varlink socket has been deserialized
    Follow-up for 6906c028e83b77b35eaaf87b27d0fe5c6e1984b7
    
    The mentioned commit uses access() to check if varlink socket
    already exists in the filesystem, but that isn't sufficient.
    
    > Varlink sockets are not serialized until v252, so upgrading from
    > v251 or older means we will not listen anymore on the varlink sockets.
    >
    > See https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=1074789
    > for more details as this was found when updating from Debian Bullseye to a new version.
    
    After this commit, the set up of varlink_server is effectively
    split into two steps. manager_varlink_init_system(), which is
    called after deserialization, would no longer skip listening
    even if Manager.varlink_server is in place, but actually
    check if we're listening on desired sockets.
    Then, manager_deserialize() can be switched back to using
    manager_setup_varlink_server().
    
    Alternative to #33817
    
    Co-authored-by: Luca Boccassi <bluca@debian.org>
    (cherry picked from commit d4e5c66ed469c822ca5346c7a445ec1446b1d17f)

--- src/shared/varlink-internal.h
+++ src/shared/varlink-internal.h
@@ -6,5 +6,45 @@
 #include "fdset.h"
 #include "varlink.h"
 
+typedef struct VarlinkServerSocket VarlinkServerSocket;
+
+struct VarlinkServerSocket {
+        VarlinkServer *server;
+
+        int fd;
+        char *address;
+
+        sd_event_source *event_source;
+
+        LIST_FIELDS(VarlinkServerSocket, sockets);
+};
+
+struct VarlinkServer {
+        unsigned n_ref;
+        VarlinkServerFlags flags;
+
+        LIST_HEAD(VarlinkServerSocket, sockets);
+
+        Hashmap *methods;              /* Fully qualified symbol name of a method → VarlinkMethod */
+        Hashmap *interfaces;           /* Fully qualified interface name → VarlinkInterface* */
+        Hashmap *symbols;              /* Fully qualified symbol name of method/error → VarlinkSymbol* */
+        VarlinkConnect connect_callback;
+        VarlinkDisconnect disconnect_callback;
+
+        sd_event *event;
+        int64_t event_priority;
+
+        unsigned n_connections;
+        Hashmap *by_uid;               /* UID_TO_PTR(uid) → UINT_TO_PTR(n_connections) */
+
+        void *userdata;
+        char *description;
+
+        unsigned connections_max;
+        unsigned connections_per_uid_max;
+
+        bool exit_on_idle;
+};
+
 int varlink_server_serialize(VarlinkServer *s, FILE *f, FDSet *fds);
 int varlink_server_deserialize_one(VarlinkServer *s, const char *value, FDSet *fds);
