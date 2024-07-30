3d90344e941f10b6fe7b1a315b79ca09c4451a86
Daan De Meyer <daan.j.demeyer@gmail.com>
2024-07-30 11:53:32 +0200

    cgroup-util: Ignore kernel threads in cg_kill_items()
    Similar to the implementation of cgroup.kill in the kernel, let's
    skip kernel threads in cg_kill_items() as trying to kill kernel
    threads as an unprivileged process will fail with EPERM and doesn't
    do anything when running privileged.
    
    (cherry picked from commit 0fbb569de1dcc06118dba006cf7a40caf6cd94d0)

--- src/basic/cgroup-util.c
+++ src/basic/cgroup-util.c
@@ -369,6 +369,12 @@ static int cg_kill_items(
                         if (set_get(s, PID_TO_PTR(pidref.pid)) == PID_TO_PTR(pidref.pid))
                                 continue;
 
+                        /* Ignore kernel threads to mimick the behavior of cgroup.kill. */
+                        if (pidref_is_kernel_thread(&pidref) > 0) {
+                                log_debug("Ignoring kernel thread with pid " PID_FMT " in cgroup '%s'", pidref.pid, path);
+                                continue;
+                        }
+
                         if (log_kill)
                                 ret_log_kill = log_kill(&pidref, sig, userdata);
 
