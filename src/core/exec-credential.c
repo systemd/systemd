e4f9a858e8297db3c8cac35c999863221f923974
Daan De Meyer <daan.j.demeyer@gmail.com>
2024-07-31 15:21:17 +0200

    Drop EEXIST handling in load_credential_glob()
    Credentials are written to a temporary file and renamed to the
    destination with renameat() which will replace existing files so
    EEXIST should not happen so drop the handling for EEXIST.

--- src/core/exec-credential.c
+++ src/core/exec-credential.c
@@ -528,8 +528,6 @@ static int load_credential_glob(
                                         ownership_ok,
                                         data, size,
                                         left);
-                        if (r == -EEXIST)
-                                continue;
                         if (r < 0)
                                 return r;
                 }
