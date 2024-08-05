e0436b783bb887ec2fd265630ab2cdab07d37be3
Yu Watanabe <watanabe.yu+github@gmail.com>
2024-08-05 12:29:46 +0900

    udevadm-info: support json output for --attribute-walk
    Closes #33852.

--- test/units/testsuite-17.10.sh
+++ test/units/testsuite-17.10.sh
@@ -70,6 +70,9 @@ udevadm info -q name -r /dev/null
 udevadm info --query symlink --root /sys/class/net/$netdev
 (! udevadm info -q hello -r /sys/class/net/$netdev)
 udevadm info -a /sys/class/net/$netdev
+udevadm info -a --json=off /sys/class/net/$netdev
+udevadm info -a --json=pretty /sys/class/net/$netdev | jq . >/dev/null
+udevadm info -a --json=short /sys/class/net/$netdev | jq . >/dev/null
 udevadm info -t >/dev/null
 udevadm info --tree /sys/class/net/$netdev
 udevadm info -x /sys/class/net/$netdev
