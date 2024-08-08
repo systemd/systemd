75f33d52890954cdc605f8b83864771cdf091802
Daan De Meyer <daan.j.demeyer@gmail.com>
2024-08-08 15:46:49 +0200

    core: Expose Manager object information via varlink
    Let's extend pid1's varlink interface and add a Describe method to
    get the global Manager object information as a JSON object
    (io.systemd.Manager.Describe).
    
    Because the new varlink interface should be available on both the
    user managers and the system manager, we also make the necessary
    changes to expose a varlink server on user managers.

--- src/shared/varlink-serialize.h
+++ src/shared/varlink-serialize.h
@@ -9,3 +9,5 @@
 
 int varlink_server_serialize(sd_varlink_server *s, FILE *f, FDSet *fds);
 int varlink_server_deserialize_one(sd_varlink_server *s, const char *value, FDSet *fds);
+
+int varlink_server_contains_socket(sd_varlink_server *s, const char *address);
