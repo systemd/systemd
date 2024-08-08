e4e4ef27081accf59e3ad114a124c8ffdc888122
Daan De Meyer <daan.j.demeyer@gmail.com>
2024-08-08 15:46:49 +0200

    core: Expose Manager object information via varlink
    Let's extend pid1's varlink interface and add a Describe method to
    get the global Manager object information as a JSON object
    (io.systemd.Manager.Describe).
    
    Because the new varlink interface should be available on both the
    user managers and the system manager, we also make the necessary
    changes to expose a varlink server on user managers.

--- src/test/test-varlink-idl.c
+++ src/test/test-varlink-idl.c
@@ -16,6 +16,7 @@
 #include "varlink-io.systemd.Journal.h"
 #include "varlink-io.systemd.Machine.h"
 #include "varlink-io.systemd.MachineImage.h"
+#include "varlink-io.systemd.Manager.h"
 #include "varlink-io.systemd.ManagedOOM.h"
 #include "varlink-io.systemd.MountFileSystem.h"
 #include "varlink-io.systemd.NamespaceResource.h"
@@ -193,6 +194,8 @@ TEST(parse_format) {
         test_parse_format_one(&vl_interface_io_systemd_Machine);
         print_separator();
         test_parse_format_one(&vl_interface_io_systemd_MachineImage);
+        print_separator();
+        test_parse_format_one(&vl_interface_io_systemd_Manager);
         print_separator();
         test_parse_format_one(&vl_interface_xyz_test);
 }
