0e88e150c53859a77a283e040aa6f3a4dd277a55
Daan De Meyer <daan.j.demeyer@gmail.com>
2024-08-08 13:51:30 +0200

    json-util: Add JSON_BUILD_RATELIMIT()

--- src/libsystemd/sd-json/sd-json.c
+++ src/libsystemd/sd-json/sd-json.c
@@ -4160,6 +4160,40 @@ _public_ int sd_json_buildv(sd_json_variant **ret, va_list ap) {
                         break;
                 }
 
+                case _JSON_BUILD_RATELIMIT: {
+                        const RateLimit *rl;
+
+                        if (!IN_SET(current->expect, EXPECT_TOPLEVEL, EXPECT_OBJECT_VALUE, EXPECT_ARRAY_ELEMENT)) {
+                                r = -EINVAL;
+                                goto finish;
+                        }
+
+                        rl = va_arg(ap, const RateLimit*);
+
+                        if (current->n_suppress == 0) {
+                                if (ratelimit_configured(rl)) {
+                                        r = sd_json_buildo(
+                                                        &add,
+                                                        SD_JSON_BUILD_PAIR("intervalUSec", SD_JSON_BUILD_UNSIGNED(rl->interval)),
+                                                        SD_JSON_BUILD_PAIR("burst", SD_JSON_BUILD_UNSIGNED(rl->burst)));
+                                        if (r < 0)
+                                                goto finish;
+                                } else
+                                        add = JSON_VARIANT_MAGIC_NULL;
+                        }
+
+                        n_subtract = 1;
+
+                        if (current->expect == EXPECT_TOPLEVEL)
+                                current->expect = EXPECT_END;
+                        else if (current->expect == EXPECT_OBJECT_VALUE)
+                                current->expect = EXPECT_OBJECT_KEY;
+                        else
+                                assert(current->expect == EXPECT_ARRAY_ELEMENT);
+
+                        break;
+                }
+
                 case _SD_JSON_BUILD_CALLBACK: {
                         sd_json_build_callback_t cb;
                         void *userdata;
@@ -4565,6 +4599,36 @@ _public_ int sd_json_buildv(sd_json_variant **ret, va_list ap) {
                                         goto finish;
 
                                 r = sd_json_variant_new_array_bytes(&add_more, a->bytes, a->length);
+                                if (r < 0)
+                                        goto finish;
+                        }
+
+                        n_subtract = 2; /* we generated two item */
+
+                        current->expect = EXPECT_OBJECT_KEY;
+                        break;
+                }
+
+                case _JSON_BUILD_PAIR_RATELIMIT_ENABLED: {
+                        const RateLimit *rl;
+                        const char *n;
+
+                        if (current->expect != EXPECT_OBJECT_KEY) {
+                                r = -EINVAL;
+                                goto finish;
+                        }
+
+                        n = va_arg(ap, const char*);
+                        rl = va_arg(ap, const RateLimit*);
+
+                        if (rl && ratelimit_configured(rl) && current->n_suppress == 0) {
+                                r = sd_json_variant_new_string(&add, n);
+                                if (r < 0)
+                                        goto finish;
+
+                                r = sd_json_buildo(&add_more,
+                                                SD_JSON_BUILD_PAIR("intervalUSec", SD_JSON_BUILD_UNSIGNED(rl->interval)),
+                                                SD_JSON_BUILD_PAIR("burst", SD_JSON_BUILD_UNSIGNED(rl->burst)));
                                 if (r < 0)
                                         goto finish;
                         }
