1eb818249c896e7d0c22ee1522c6312e1e2a1a38
Daan De Meyer <daan.j.demeyer@gmail.com>
2024-08-13 16:39:04 +0200

    json-util: Add JSON_BUILD_TRISTATE() and friends

--- src/libsystemd/sd-json/sd-json.c
+++ src/libsystemd/sd-json/sd-json.c
@@ -4194,6 +4194,37 @@ _public_ int sd_json_buildv(sd_json_variant **ret, va_list ap) {
                         break;
                 }
 
+                case _JSON_BUILD_TRISTATE: {
+                        int tristate;
+
+                        if (!IN_SET(current->expect, EXPECT_TOPLEVEL, EXPECT_OBJECT_VALUE, EXPECT_ARRAY_ELEMENT)) {
+                                r = -EINVAL;
+                                goto finish;
+                        }
+
+                        tristate = va_arg(ap, int);
+
+                        if (current->n_suppress == 0) {
+                                if (tristate >= 0) {
+                                        r = sd_json_variant_new_boolean(&add, tristate);
+                                        if (r < 0)
+                                                goto finish;
+                                } else
+                                        add = JSON_VARIANT_MAGIC_NULL;
+                        }
+
+                        n_subtract = 1;
+
+                        if (current->expect == EXPECT_TOPLEVEL)
+                                current->expect = EXPECT_END;
+                        else if (current->expect == EXPECT_OBJECT_VALUE)
+                                current->expect = EXPECT_OBJECT_KEY;
+                        else
+                                assert(current->expect == EXPECT_ARRAY_ELEMENT);
+
+                        break;
+                }
+
                 case _SD_JSON_BUILD_CALLBACK: {
                         sd_json_build_callback_t cb;
                         void *userdata;
@@ -4796,6 +4827,34 @@ _public_ int sd_json_buildv(sd_json_variant **ret, va_list ap) {
                                     command == _SD_JSON_BUILD_BASE32HEX ? sd_json_variant_new_base32hex(&add_more, p, sz) :
                                     command == _SD_JSON_BUILD_HEX       ? sd_json_variant_new_hex(&add_more, p, sz) :
                                                                           sd_json_variant_new_octescape(&add_more, p, sz);
+                                if (r < 0)
+                                        goto finish;
+                        }
+
+                        n_subtract = 2; /* we generated two item */
+
+                        current->expect = EXPECT_OBJECT_KEY;
+                        break;
+                }
+
+                case _JSON_BUILD_PAIR_TRISTATE_NON_NULL: {
+                        int tristate;
+                        const char *n;
+
+                        if (current->expect != EXPECT_OBJECT_KEY) {
+                                r = -EINVAL;
+                                goto finish;
+                        }
+
+                        n = va_arg(ap, const char*);
+                        tristate = va_arg(ap, int);
+
+                        if (tristate >= 0 && current->n_suppress == 0) {
+                                r = sd_json_variant_new_string(&add, n);
+                                if (r < 0)
+                                        goto finish;
+
+                                r = sd_json_variant_new_boolean(&add_more, tristate);
                                 if (r < 0)
                                         goto finish;
                         }
