93efb8d1a2496c5d9a30048a121eeee5246e3c1f
Daan De Meyer <daan.j.demeyer@gmail.com>
2024-08-14 17:31:30 +0200

    repart: Don't add same dir to MakeDirectories= or Subvolumes= twice

--- src/partition/repart.c
+++ src/partition/repart.c
@@ -1668,6 +1668,9 @@ static int config_parse_make_dirs(
                 if (r < 0)
                         continue;
 
+                if (strv_contains(*sv, d))
+                        continue;
+
                 r = strv_consume(sv, TAKE_PTR(d));
                 if (r < 0)
                         return log_oom();
