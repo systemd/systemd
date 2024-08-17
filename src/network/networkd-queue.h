b5ba9cf500dd29aca235a7aaedac923289facb7e
Yu Watanabe <watanabe.yu+github@gmail.com>
2024-08-17 13:04:03 +0900

    network/queue: introduce manager_queue_request_full()
    Currently it is not used, but will be used later.

--- src/network/networkd-queue.h
+++ src/network/networkd-queue.h
@@ -107,6 +107,18 @@ int link_queue_request_full(
                 request_netlink_handler_t netlink_handler,
                 Request **ret);
 
+int manager_queue_request_full(
+                Manager *manager,
+                RequestType type,
+                void *userdata,
+                mfree_func_t free_func,
+                hash_func_t hash_func,
+                compare_func_t compare_func,
+                request_process_func_t process,
+                unsigned *counter,
+                request_netlink_handler_t netlink_handler,
+                Request **ret);
+
 int link_requeue_request(Link *link, Request *req, void *userdata, Request **ret);
 
 static inline int link_queue_request(
