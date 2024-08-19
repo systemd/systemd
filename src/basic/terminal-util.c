systemd/systemd

commit e60a4a3c46df1abc6570a026d716c71a6cb1bf5c

terminal-util: add extra validity checks that we operate on a TTY before doing so
Prompted by #19213, but not fixing it.

This is mostly paranoia that we don't do stuff on inodes that aren't
actually ttys.

src/basic/terminal-util.c
@@ -240,9 +240,11 @@ int reset_terminal_fd(int fd, bool switch_to_text) {
 
         assert(fd >= 0);
 
-        /* We leave locked terminal attributes untouched, so that
-         * Plymouth may set whatever it wants to set, and we don't
-         * interfere with that. */
+        if (isatty(fd) < 1)
+                return log_debug_errno(errno, "Asked to reset a terminal that actually isn't a terminal: %m");
+
+        /* We leave locked terminal attributes untouched, so that Plymouth may set whatever it wants to set,
+         * and we don't interfere with that. */
 
         /* Disable exclusive mode, just in case */
         if (ioctl(fd, TIOCNXCL) < 0)
@@ -1329,6 +1331,9 @@ int vt_restore(int fd) {
         };
         int r, q = 0;
 
+        if (isatty(fd) < 1)
+                return log_debug_errno(errno, "Asked to restore the VT for an fd that does not refer to a terminal: %m");
+
         if (ioctl(fd, KDSETMODE, KD_TEXT) < 0)
                 q = log_debug_errno(errno, "Failed to set VT in text mode, ignoring: %m");
 
@@ -1361,6 +1366,9 @@ int vt_release(int fd, bool restore) {
         /* This function releases the VT by acknowledging the VT-switch signal
          * sent by the kernel and optionally reset the VT in text and auto
          * VT-switching modes. */
+
+        if (isatty(fd) < 1)
+                return log_debug_errno(errno, "Asked to release the VT for an fd that does not refer to a terminal: %m");
 
         if (ioctl(fd, VT_RELDISP, 1) < 0)
                 return -errno;

commit 153e608ce6faf748e73547164cb9cc1ef5f9d031

terminal-util: don't call isatty() before ioctl
This fixes an issue introduced in e60a4a3, where -EIO is no longer returned when isatty() is POSIX compliant.

src/basic/terminal-util.c
@@ -1501,11 +1501,12 @@ int vt_restore(int fd) {
 
         assert(fd >= 0);
 
-        if (!isatty_safe(fd))
-                return log_debug_errno(errno, "Asked to restore the VT for an fd that does not refer to a terminal: %m");
+        if (ioctl(fd, KDSETMODE, KD_TEXT) < 0) {
+                if (errno == ENOTTY || errno == EBADF)
+                        return log_debug_errno(errno, "Asked to restore the VT for an fd that does not refer to a terminal: %m");
 
-        if (ioctl(fd, KDSETMODE, KD_TEXT) < 0)
                 RET_GATHER(ret, log_debug_errno(errno, "Failed to set VT to text mode, ignoring: %m"));
+        }
 
         r = vt_reset_keyboard(fd);
         if (r < 0)
@@ -1528,11 +1529,12 @@ int vt_release(int fd, bool restore) {
          * sent by the kernel and optionally reset the VT in text and auto
          * VT-switching modes. */
 
-        if (!isatty_safe(fd))
-                return log_debug_errno(errno, "Asked to release the VT for an fd that does not refer to a terminal: %m");
+        if (ioctl(fd, VT_RELDISP, 1) < 0) {
+                if (errno == ENOTTY || errno == EBADF)
+                        return log_debug_errno(errno, "Asked to release the VT for an fd that does not refer to a terminal: %m");
 
-        if (ioctl(fd, VT_RELDISP, 1) < 0)
                 return -errno;
+        }
 
         if (restore)
                 return vt_restore(fd);