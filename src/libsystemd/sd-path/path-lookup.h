72ece0b6d8b3493d2672749751ea204e765bf695
Mike Yuan <me@yhndnzj.com>
2024-08-24 15:33:53 +0200

    path-lookup: modernize runtime_directory() too

--- src/libsystemd/sd-path/path-lookup.h
+++ src/libsystemd/sd-path/path-lookup.h
@@ -55,6 +55,8 @@ typedef struct LookupPaths {
 int lookup_paths_init(LookupPaths *lp, RuntimeScope scope, LookupPathsFlags flags, const char *root_dir);
 int lookup_paths_init_or_warn(LookupPaths *lp, RuntimeScope scope, LookupPathsFlags flags, const char *root_dir);
 
+int runtime_directory(RuntimeScope scope, const char *suffix, char **ret);
+
 int xdg_user_dirs(char ***ret_config_dirs, char ***ret_data_dirs);
 
 /* We don't treat /etc/xdg/systemd in these functions as the xdg base dir spec suggests because we assume
@@ -69,7 +71,6 @@ static inline int xdg_user_config_dir(const char *suffix, char **ret) {
 static inline int xdg_user_data_dir(const char *suffix, char **ret) {
         return sd_path_lookup(SD_PATH_USER_SHARED, suffix, ret);
 }
-int runtime_directory(char **ret, RuntimeScope scope, const char *suffix);
 
 bool path_is_user_data_dir(const char *path);
 bool path_is_user_config_dir(const char *path);
