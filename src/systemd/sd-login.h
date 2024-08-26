441c6fb76cfa0a5632446c06cc41b42fe18c25c4
Allison Karlitskaya <allison.karlitskaya@redhat.com>
2024-08-26 10:32:47 +0200

    man: document "web" session type
    This has been supported since e9e74f28d783 but never got documented.
    Add it to the man pages (plus one comment in a header).
    
    Closes #34127.

--- src/systemd/sd-login.h
+++ src/systemd/sd-login.h
@@ -172,7 +172,7 @@ int sd_session_get_start_time(const char *session, uint64_t *usec);
 /* Determine the (PAM) service name this session was registered by. */
 int sd_session_get_service(const char *session, char **service);
 
-/* Determine the type of this session, i.e. one of "tty", "x11", "wayland", "mir" or "unspecified". */
+/* Determine the type of this session, i.e. one of "tty", "x11", "wayland", "mir", "web", or "unspecified". */
 int sd_session_get_type(const char *session, char **type);
 
 /* Determine the class of this session, i.e. one of "user", "greeter" or "lock-screen". */
