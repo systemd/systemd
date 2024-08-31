63b2f33764b7da3d32b267d3c554ad1a4a32ff65
anonymix007 <48598263+anonymix007@users.noreply.github.com>
2024-08-31 21:38:21 +0300

    boot: Add smbios_populate_raw_info
    This function will be used to gather information for DeviceTree matching

--- src/boot/efi/smbios.h
+++ src/boot/efi/smbios.h
@@ -6,3 +6,14 @@
 bool smbios_in_hypervisor(void);
 
 const char* smbios_find_oem_string(const char *name);
+
+typedef struct RawSmbiosInfo {
+        const char *manufacturer;
+        const char *product_name;
+        const char *product_sku;
+        const char *family;
+        const char *baseboard_product;
+        const char *baseboard_manufacturer;
+} RawSmbiosInfo;
+
+void smbios_raw_info_populate(RawSmbiosInfo *ret_info);
