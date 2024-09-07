45b45dcb2cc495ab00031568711e1c3ef0463c65
Mike Yuan <me@yhndnzj.com>
2024-09-07 15:27:04 +0200

    path-lookup: unify *_generator_binary_paths()

--- src/basic/path-lookup.h
+++ src/basic/path-lookup.h
@@ -80,8 +80,16 @@ bool path_is_user_config_dir(const char *path);
 void lookup_paths_log(LookupPaths *p);
 void lookup_paths_done(LookupPaths *p);
 
-char **generator_binary_paths(RuntimeScope scope);
-char **env_generator_binary_paths(RuntimeScope scope);
+char** generator_binary_paths_internal(RuntimeScope scope, bool env_generator);
+static inline char** generator_binary_paths(RuntimeScope runtime_scope) {
+        return generator_binary_paths_internal(runtime_scope, false);
+}
+static inline char** env_generator_binary_paths(RuntimeScope runtime_scope) {
+        return generator_binary_paths_internal(runtime_scope, true);
+}
+
+char** generator_binary_paths(RuntimeScope runtime_scope);
+char** env_generator_binary_paths(RuntimeScope runtime_scope);
 
 #define NETWORK_DIRS ((const char* const*) CONF_PATHS_STRV("systemd/network"))
 #define NETWORK_DIRS_NULSTR CONF_PATHS_NULSTR("systemd/network")
