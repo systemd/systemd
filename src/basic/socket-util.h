85fd8df03df56027645a52c91e9dac6712d86195
Yu Watanabe <watanabe.yu+github@gmail.com>
2024-09-08 00:15:38 +0900

    missing_socket: drop unnecessary definitions
    Now, we have copy of vm_sockets.h, hence these definitions are not
    necessary anymore.

--- src/basic/socket-util.h
+++ src/basic/socket-util.h
@@ -2,15 +2,16 @@
 #pragma once
 
 #include <inttypes.h>
-#include <linux/netlink.h>
 #include <linux/if_ether.h>
 #include <linux/if_infiniband.h>
 #include <linux/if_packet.h>
+#include <linux/netlink.h>
+#include <sys/socket.h> /* linux/vms_sockets.h requires 'struct sockaddr' */
+#include <linux/vm_sockets.h>
 #include <netinet/in.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <string.h>
-#include <sys/socket.h>
 #include <sys/types.h>
 #include <sys/un.h>
 
