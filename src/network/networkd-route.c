14a499ed77dc0f0048494971155cde9a95cbcf41
Yu Watanabe <watanabe.yu+github@gmail.com>
2024-09-07 22:22:41 +0900

    network/route: fix typo
    Follow-up for 195bb6f97ebc5cd5e9932c62aa80df5427c30d67.

--- src/network/networkd-route.c
+++ src/network/networkd-route.c
@@ -720,7 +720,7 @@ static int route_update_on_existing(Request *req) {
         int r;
 
         if (!req->manager)
-                /* Already detached? At least there are two posibilities then.
+                /* Already detached? At least there are two possibilities then.
                  * 1) The interface is removed, and all queued requests for the interface are cancelled.
                  * 2) networkd is now stopping, hence all queued requests are cancelled.
                  * Anyway, we can ignore the request, and there is nothing we can do. */
