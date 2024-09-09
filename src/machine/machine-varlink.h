4fb0dabd927f9ba6679e1faca9f51810fc4f8be9
Ivan Kruglov <mail@ikruglov.com>
2024-09-09 16:03:31 +0200

    varlinkctl: io.systemd.Machine.Unregister & io.systemd.Machine.Terminate

--- src/machine/machine-varlink.h
+++ src/machine/machine-varlink.h
@@ -2,5 +2,11 @@
 #pragma once
 
 #include "sd-varlink.h"
+#include "machine.h"
 
+int lookup_machine_by_name(sd_varlink *link, Manager *manager, const char *machine_name, Machine **ret_machine);
+
+typedef int (*vl_method_handler_t) (sd_varlink *link, sd_json_variant *parameters, sd_varlink_method_flags_t flags, void *userdata) ;
 int vl_method_register(sd_varlink *link, sd_json_variant *parameters, sd_varlink_method_flags_t flags, void *userdata);
+int vl_method_unregister_internal(sd_varlink *link, sd_json_variant *parameters, sd_varlink_method_flags_t flags, void *userdata);
+int vl_method_terminate_internal(sd_varlink *link, sd_json_variant *parameters, sd_varlink_method_flags_t flags, void *userdata);
