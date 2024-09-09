84649a256d9d1374780a0b2e9e437281e10b32ae
Daan De Meyer <daan.j.demeyer@gmail.com>
2024-09-09 12:25:28 +0200

    core: Add support for PrivateUsers=identity
    This configures an indentity mapping similar to
    systemd-nspawn --private-users=identity.

--- src/core/execute.h
+++ src/core/execute.h
@@ -318,7 +318,7 @@ struct ExecContext {
         PrivateTmp private_tmp;
         bool private_network;
         bool private_devices;
-        bool private_users;
+        PrivateUsers private_users;
         bool private_ipc;
         bool protect_kernel_tunables;
         bool protect_kernel_modules;
