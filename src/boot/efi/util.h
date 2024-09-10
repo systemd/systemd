8b309469fd62e4a35e49005d605ada7e1fa9150c
anonymix007 <48598263+anonymix007@users.noreply.github.com>
2024-09-10 14:40:41 +0300

    fundamental: Add bswap_* macros

--- src/boot/efi/util.h
+++ src/boot/efi/util.h
@@ -188,3 +188,9 @@ static inline bool efi_guid_equal(const EFI_GUID *a, const EFI_GUID *b) {
 void *find_configuration_table(const EFI_GUID *guid);
 
 char16_t *get_extra_dir(const EFI_DEVICE_PATH *file_path);
+
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+#  define be32toh(x) __builtin_bswap32(x)
+#else
+#  error "Unexpected byte order in EFI mode?"
+#endif
