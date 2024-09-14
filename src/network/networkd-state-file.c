09d5cb71b4b99b8480efc1197b5f4c6924540104
Ronan Pigott <ronan@rjp.ie>
2024-09-13 22:14:17 -0700

    fixup! network: Add serialization for DoT resolvers
    move dnr include

--- src/network/networkd-state-file.c
+++ src/network/networkd-state-file.c
@@ -3,10 +3,9 @@
 #include <netinet/in.h>
 #include <linux/if.h>
 
-#include "dns-resolver-internal.h"
-
 #include "alloc-util.h"
 #include "dns-domain.h"
+#include "dns-resolver-internal.h"
 #include "escape.h"
 #include "fd-util.h"
 #include "fileio.h"
