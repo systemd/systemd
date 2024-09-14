14e8e73b28c2bce103667c4748b6fe7d65c00de7
Michael Ferrari <nekkodroid404@gmail.com>
2024-09-14 02:01:52 +0200

    firstboot: add similar input suggestion
    This uses the same logic as similar verb suggestion for command line
    utilities. Try to be helpful when the user entered something invalid
    instead of just showing the prompt again.

--- src/firstboot/firstboot.c
+++ src/firstboot/firstboot.c
@@ -187,6 +187,7 @@ static int prompt_loop(const char *text, char **l, unsigned percentage, bool (*i
 
         for (;;) {
                 _cleanup_free_ char *p = NULL;
+                char *best_match = NULL;
                 unsigned u;
 
                 r = ask_string(&p, "%s %s (empty to skip, \"list\" to list options): ",
@@ -219,12 +220,20 @@ static int prompt_loop(const char *text, char **l, unsigned percentage, bool (*i
                         return free_and_strdup_warn(ret, l[u-1]);
                 }
 
-                if (!is_valid(p)) {
-                        log_error("Entered data invalid.");
-                        continue;
-                }
+                if (is_valid(p))
+                        return free_and_replace(*ret, p);
 
-                return free_and_replace(*ret, p);
+                /* Be helperful to the user, and give a hint what the user might have wanted to
+                 * type. We search with two mechanisms: a simple prefix match and – if that didn't
+                 * yield results –, a Levenshtein word distance based match. */
+                best_match = strv_find_prefix(l, p);
+                if (!best_match)
+                        best_match = strv_find_closest_by_levenshtein(l, p);
+
+                if (best_match)
+                        log_error("Invalid data '%s', did you mean '%s'?", p, best_match);
+                else
+                        log_error("Invalid data '%s'.", p);
         }
 }
 
