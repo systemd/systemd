8300fa4dcf0c96ec3ff818a9af2ca1de7eb87d46
Mike Yuan <me@yhndnzj.com>
2024-09-15 21:17:20 +0200

    path-lookup: move network/portable conf helpers to basic/conf-files

--- src/basic/conf-files.c
+++ src/basic/conf-files.c
@@ -469,3 +469,30 @@ int conf_file_read(
 
         return r;
 }
+
+int find_portable_profile(const char *name, const char *unit, char **ret_path) {
+        const char *dot;
+
+        assert(name);
+        assert(ret_path);
+
+        assert_se(dot = strrchr(unit, '.'));
+
+        NULSTR_FOREACH(p, PORTABLE_PROFILE_DIRS) {
+                _cleanup_free_ char *joined = NULL;
+
+                joined = strjoin(p, "/", name, "/", dot + 1, ".conf");
+                if (!joined)
+                        return -ENOMEM;
+
+                if (laccess(joined, F_OK) >= 0) {
+                        *ret_path = TAKE_PTR(joined);
+                        return 0;
+                }
+
+                if (errno != ENOENT)
+                        return -errno;
+        }
+
+        return -ENOENT;
+}
