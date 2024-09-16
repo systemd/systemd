systemd/systemd

commit 6d9ef22acdeac4b429efb75164341233955484af

emit a warning in networkd if managed sysctls are changed
Monitor the sysctl set by networkd for writes, if a sysctl is
overwritten with a different value than the one we set, emit a warning.
Writes are detected with an eBPF program attached as BPF_CGROUP_SYSCTL
which reports the sysctl writes only in net/.

The eBPF program only reports sysctl writes from a different cgroup than networkd.
To do this, it uses the `bpf_current_task_under_cgroup_proto()` helper,
which will be available allowed in BPF_CGROUP_SYSCTL from kernel 6.12[1].

Loading a BPF_CGROUP_SYSCTL program requires the CAP_SYS_ADMIN capability,
so drop it just after the program load, whether it loads successfully or not.

Writes are logged but permitted, in future the functionality can be
extended to also deny writes to managed sysctls.

[1] https://lore.kernel.org/bpf/20240819162805.78235-3-technoboy85@gmail.com/

src/network/networkd-manager.c
@@ -16,6 +16,7 @@
 #include "bus-log-control-api.h"
 #include "bus-polkit.h"
 #include "bus-util.h"
+#include "capability-util.h"
 #include "common-signal.h"
 #include "conf-parser.h"
 #include "constants.h"
@@ -603,6 +604,7 @@ int manager_new(Manager **ret, bool test_mode) {
                 .duid_product_uuid.type = DUID_TYPE_UUID,
                 .dhcp_server_persist_leases = true,
                 .ip_forwarding = { -1, -1, },
+                .cgroup_fd = -EBADF,
         };
 
         *ret = TAKE_PTR(m);
@@ -614,6 +616,8 @@ Manager* manager_free(Manager *m) {
 
         if (!m)
                 return NULL;
+
+        sysctl_remove_monitor(m);
 
         free(m->state_file);
 
@@ -693,6 +697,18 @@ int manager_start(Manager *m) {
         int r;
 
         assert(m);
+
+        (void) sysctl_add_monitor(m);
+
+        /* Loading BPF programs requires CAP_SYS_ADMIN and CAP_BPF.
+         * Drop the capabilities here, regardless if the load succeeds or not. */
+        r = drop_capability(CAP_SYS_ADMIN);
+        if (r < 0)
+                log_warning_errno(r, "Failed to drop CAP_SYS_ADMIN: %m, ignoring.");
+
+        r = drop_capability(CAP_BPF);
+        if (r < 0)
+                log_warning_errno(r, "Failed to drop CAP_BPF: %m, ignoring.");
 
         manager_set_sysctl(m);
 

catalog/systemd.catalog.in
@@ -794,3 +794,12 @@ the TPM.
 
 Automatic SRK enrollment on TPMs in such scenarios is not supported. In order to unset the PIN/password
 protection on the owner hierarchy issue a command like the following: 'tpm2_changeauth -c o -p <OLDPW> ""'.
+
+-- 9cf56b8baf9546cf9478783a8de42113
+Subject: A foreign process changed a sysctl we manage
+Defined-By: systemd
+Support: %SUPPORT_URL%
+
+A sysctl handle under /proc/sys/net, which is managed by systemd-networkd, has been changed by another process.
+The event is raised only if the written value differs from the current one.
+The program name, the written value, the previous value, and the value initially set by networkd have been logged.

src/network/bpf/sysctl_monitor/meson.build
@@ -1,0 +1,25 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+if conf.get('HAVE_VMLINUX_H') != 1
+        subdir_done()
+endif
+
+sysctl_monitor_bpf_o_unstripped = custom_target(
+        'sysctl-monitor.bpf.unstripped.o',
+        input : 'sysctl-monitor.bpf.c',
+        output : 'sysctl-monitor.bpf.unstripped.o',
+        command : bpf_o_unstripped_cmd,
+        depends : vmlinux_h_dependency)
+
+sysctl_monitor_bpf_o = custom_target(
+        'sysctl-monitor.bpf.o',
+        input : sysctl_monitor_bpf_o_unstripped,
+        output : 'sysctl-monitor.bpf.o',
+        command : bpf_o_cmd)
+
+sysctl_monitor_skel_h = custom_target(
+        'sysctl-monitor.skel.h',
+        input : sysctl_monitor_bpf_o,
+        output : 'sysctl-monitor.skel.h',
+        command : skel_h_cmd,
+        capture : true)

src/network/bpf/sysctl_monitor/sysctl-monitor-skel.h
@@ -1,0 +1,16 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+
+/* The SPDX header above is actually correct in claiming this was
+ * LGPL-2.1-or-later, because it is. Since the kernel doesn't consider that
+ * compatible with GPL we will claim this to be GPL however, which should be
+ * fine given that LGPL-2.1-or-later downgrades to GPL if needed.
+ */
+
+#include "bpf-dlopen.h"
+
+/* libbpf is used via dlopen(), so rename symbols */
+#define bpf_object__destroy_skeleton sym_bpf_object__destroy_skeleton
+#define bpf_object__load_skeleton sym_bpf_object__load_skeleton
+#define bpf_object__open_skeleton sym_bpf_object__open_skeleton
+
+#include "bpf/sysctl_monitor/sysctl-monitor.skel.h"

src/network/bpf/sysctl_monitor/sysctl-monitor.bpf.c
@@ -1,0 +1,134 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+
+#include "vmlinux.h"
+
+#include <bpf/bpf_helpers.h>
+
+#include "sysctl-write-event.h"
+
+struct {
+        __uint(type, BPF_MAP_TYPE_CGROUP_ARRAY);
+        __type(key, u32);
+        __type(value, u32);
+        __uint(max_entries, 1);
+} cgroup_map SEC(".maps");
+
+struct {
+        __uint(type, BPF_MAP_TYPE_RINGBUF);
+        __uint(max_entries, 256 * 1024);
+} written_sysctls SEC(".maps");
+
+static bool my_streq(const char *s1, const char *s2, size_t l) {
+        for (size_t i = 0; i < l; i++) {
+                if (s1[i] != s2[i])
+                        return false;
+                if (s1[i] == 0)
+                        return true;
+        }
+        return true;
+}
+
+struct str {
+        char *s;
+        size_t l;
+};
+
+static long cut_last(u32 i, struct str *str) {
+        char *s;
+
+        i = str->l - i - 1;
+        s = str->s + i;
+
+        /* Sanity check for the preverifier */
+        if (i >= str->l)
+                return 1;
+
+        if (*s == 0)
+                return 0;
+
+        if (*s == '\n' || *s == '\r' || *s == ' ' || *s == '\t') {
+                *s = 0;
+
+                return 0;
+        }
+
+        return 1;
+}
+
+/* Cut off trailing whitespace and newlines */
+static void chop(char *s, size_t l) {
+        struct str str = { s, l };
+
+        bpf_loop(l, cut_last, &str, 0);
+}
+
+SEC("cgroup/sysctl")
+int sysctl_monitor(struct bpf_sysctl *ctx) {
+        int r;
+
+        /* Ignore events generated by us */
+        if (bpf_current_task_under_cgroup(&cgroup_map, 0))
+                return 1;
+
+        /* Allow reads */
+        if (!ctx->write)
+                return 1;
+
+        /* Declare the struct without contextually initializing it.
+         * This avoid zero-filling the struct, which would be a waste of
+         * resource and code size. Since we're sending an event even on failure,
+         * truncate the strings to zero size, in case we don't populate them. */
+        struct sysctl_write_event we;
+        we.version = 1;
+        we.errorcode = 0;
+        we.path[0] = 0;
+        we.comm[0] = 0;
+        we.current[0] = 0;
+        we.newvalue[0] = 0;
+
+        /* Set the simple values first */
+        we.pid = bpf_get_current_pid_tgid() >> 32;
+        we.cgroup_id = bpf_get_current_cgroup_id();
+
+        /* Only monitor /proc/sys/net/ */
+        r = bpf_sysctl_get_name(ctx, we.path, sizeof(we.path), 0);
+        if (r < 0) {
+                we.errorcode = r;
+                goto send_event;
+        }
+
+        if (bpf_strncmp(we.path, 4, "net/") != 0)
+                return 1;
+
+        r = bpf_get_current_comm(we.comm, sizeof(we.comm));
+        if (r < 0) {
+                we.errorcode = r;
+                goto send_event;
+        }
+
+        r = bpf_sysctl_get_current_value(ctx, we.current, sizeof(we.current));
+        if (r < 0) {
+                we.errorcode = r;
+                goto send_event;
+        }
+
+        r = bpf_sysctl_get_new_value(ctx, we.newvalue, sizeof(we.newvalue));
+        if (r < 0) {
+                we.errorcode = r;
+                goto send_event;
+        }
+
+        /* Both the kernel and userspace applications add a newline at the end,
+         * remove it from both strings */
+        chop(we.current, sizeof(we.current));
+        chop(we.newvalue, sizeof(we.newvalue));
+
+send_event:
+        /* If new value differs or we encountered an error, send the event */
+        if (r < 0 || !my_streq(we.current, we.newvalue, sizeof(we.current)))
+                bpf_ringbuf_output(&written_sysctls, &we, sizeof(we), 0);
+
+        return 1;
+}
+
+char _license[] SEC("license") = "GPL";

src/network/bpf/sysctl_monitor/sysctl-write-event.h
@@ -1,0 +1,46 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+
+#pragma once
+
+#ifndef TASK_COMM_LEN
+#define TASK_COMM_LEN 16
+#endif
+
+/* It would be nice to size these members to bigger values, but the stack
+ * in BPF programs is limited to 512 bytes, and allocating bigger structures
+ * leads to this compile time error:
+ *   error: Looks like the BPF stack limit is exceeded.
+ *   Please move large on stack variables into BPF per-cpu array map.
+ *   For non-kernel uses, the stack can be increased using -mllvm -bpf-stack-size. */
+struct sysctl_write_event {
+        /* Used to track changes in the struct layout */
+        int version;
+
+        /* Error code returned to userspace to handle eventual failures. */
+        int errorcode;
+
+        /* The PID of the process which is writing the sysctl. */
+        pid_t pid;
+
+        /* The cgroup id of the process. */
+        uint64_t cgroup_id;
+
+        /* The name of the binary. */
+        char comm[TASK_COMM_LEN];
+
+        /* The path of the sysctl, relative to /proc/sys/.
+         * The longest path observed is 64 bytes:
+         * net/ipv4/conf/123456789012345/igmpv3_unsolicited_report_interval
+         * so set it to 100 gives us lot of headroom */
+        char path[100];
+
+        /* The value of the sysctl just before the write.
+         * The longest value observed is net.core.netdev_rss_key which
+         * contains 155 bytes, so set it to 160 to have some headroom
+         * even in this corner case. */
+        char current[160];
+
+        /* The new value being written into the sysctl.
+         * same sizing as 'current' */
+        char newvalue[160];
+};

src/network/meson.build
@@ -1,5 +1,7 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
+subdir('bpf/sysctl_monitor')
+
 sources = files(
         'netdev/bareudp.c',
         'netdev/batadv.c',
@@ -139,6 +141,10 @@ network_generator_sources = files(
 
 networkd_network_gperf_gperf = files('networkd-network-gperf.gperf')
 networkd_netdev_gperf_gperf = files('netdev/netdev-gperf.gperf')
+
+if conf.get('HAVE_VMLINUX_H') == 1
+        sources += sysctl_monitor_skel_h
+endif
 
 sources += custom_target(
         'networkd-gperf.c',

src/network/networkd-link.c
@@ -252,6 +252,8 @@ static void link_free_engines(Link *link) {
 static Link *link_free(Link *link) {
         assert(link);
 
+        (void) sysctl_clear_link_shadows(link);
+
         link_ntp_settings_clear(link);
         link_dns_settings_clear(link);
 

commit a2fbe9f3f9938cf3e6fcfad9fe6fc9044f990fe8

network/sysctl-monitor: fix use-after-free
Previously, manager_free() did not assign NULL to Manager.sysctl_shadow,
hence sysctl_clear_link_shadows() called by link_free() will causes
use-after-free. To fix the issue, this makes Manager.sysctl_shadow will be
set to NULL after it is freed,

Fixes a bug introduced by 6d9ef22acdeac4b429efb75164341233955484af.

src/network/networkd-manager.c
@@ -624,7 +624,7 @@ Manager* manager_free(Manager *m) {
         HASHMAP_FOREACH(link, m->links_by_index)
                 (void) link_stop_engines(link, true);
 
-        hashmap_free(m->sysctl_shadow);
+        m->sysctl_shadow = hashmap_free(m->sysctl_shadow);
 
         m->request_queue = ordered_set_free(m->request_queue);
         m->remove_request_queue = ordered_set_free(m->remove_request_queue);