feae34d64ea3e037a2a6b631b768ea36c90d30bb
Mike Yuan <me@yhndnzj.com>
2024-10-04 12:57:23 +0200

    path-lookup: move find_portable_profile() to portable-util

--- src/basic/path-lookup.h
+++ src/basic/path-lookup.h
@@ -3,8 +3,6 @@
 
 #include <stdbool.h>
 
-#include "constants.h"
-#include "macro.h"
 #include "runtime-scope.h"
 
 typedef enum LookupPathsFlags {
@@ -69,6 +67,3 @@ void lookup_paths_done(LookupPaths *p);
 
 char **generator_binary_paths(RuntimeScope scope);
 char **env_generator_binary_paths(RuntimeScope scope);
-
-#define PORTABLE_PROFILE_DIRS CONF_PATHS_NULSTR("systemd/portable/profile")
-int find_portable_profile(const char *name, const char *unit, char **ret_path);
