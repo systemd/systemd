928fbdbde997e7bc6bd9c3410c630fb0b7493e35
Ivan Kruglov <mail@ikruglov.com>
2024-10-09 13:25:24 +0200

    sd-json: add sd_json_dispatch_pid()

--- src/libsystemd/sd-json/sd-json.c
+++ src/libsystemd/sd-json/sd-json.c
@@ -29,6 +29,7 @@
 #include "memory-util.h"
 #include "memstream-util.h"
 #include "path-util.h"
+#include "process-util.h"
 #include "set.h"
 #include "string-table.h"
 #include "string-util.h"
@@ -5504,6 +5505,38 @@ _public_ int sd_json_dispatch_uid_gid(const char *name, sd_json_variant *variant
 
         *uid = k;
         return 0;
+}
+
+_public_ int sd_json_dispatch_pid(const char *name, sd_json_variant *variant, sd_json_dispatch_flags_t flags, void *userdata) {
+        /* pid_t is a signed type, but we don't consider negative values as valid.
+         * There is a special treatment for 0 though. */
+
+        pid_t *pid = userdata;
+        uint64_t k;
+
+        assert_return(variant, -EINVAL);
+        assert_cc(sizeof(pid_t) == sizeof(uint32_t));
+
+        if (sd_json_variant_is_null(variant)) {
+                *pid = PID_INVALID;
+                return 0;
+        }
+
+        if (!sd_json_variant_is_unsigned(variant))
+                return json_log(variant, flags, SYNTHETIC_ERRNO(EINVAL), "JSON field '%s' is not an integer.", strna(name));
+
+        k = sd_json_variant_unsigned(variant);
+        if (k == 0) {
+            if (!FLAGS_SET(flags, SD_JSON_RELAX))
+                goto invalid_value;
+        } else if (k > UINT32_MAX || !pid_is_valid(k))
+                goto invalid_value;
+
+        *pid = k;
+        return 0;
+
+invalid_value:
+        return json_log(variant, flags, SYNTHETIC_ERRNO(EINVAL), "JSON field '%s' is not a valid PID.", strna(name));
 }
 
 _public_ int sd_json_dispatch_id128(const char *name, sd_json_variant *variant, sd_json_dispatch_flags_t flags, void *userdata) {
