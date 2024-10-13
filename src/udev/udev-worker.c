systemd/systemd

commit f6bda694f908cc227b002570b893029aa4c9e173

udev: introduce ID_PROCESSING=1 boolean property
This indicates that the device is being processed by udevd.

src/udev/udev-worker.c
@@ -212,6 +212,15 @@ static int worker_process_device(UdevWorker *worker, sd_device *dev) {
                         log_device_warning_errno(dev, r, "Failed to add inotify watch, ignoring: %m");
         }
 
+        /* Finalize database. */
+        r = device_add_property(dev, "ID_PROCESSING", NULL);
+        if (r < 0)
+                return log_device_warning_errno(dev, r, "Failed to remove 'ID_PROCESSING' property: %m");
+
+        r = device_update_db(dev);
+        if (r < 0)
+                return log_device_warning_errno(dev, r, "Failed to update database under /run/udev/data/: %m");
+
         log_device_uevent(dev, "Device processed");
         return 0;
 }

src/udev/udev-event.c
@@ -171,6 +171,12 @@ static int rename_netif(UdevEvent *event) {
                 goto revert;
         }
 
+        r = device_add_property(event->dev_db_clone, "ID_PROCESSING", "1");
+        if (r < 0) {
+                log_device_warning_errno(event->dev_db_clone, r, "Failed to add 'ID_PROCESSING' property: %m");
+                goto revert;
+        }
+
         r = device_update_db(event->dev_db_clone);
         if (r < 0) {
                 log_device_debug_errno(event->dev_db_clone, r, "Failed to update database under /run/udev/data/: %m");
@@ -197,6 +203,7 @@ static int rename_netif(UdevEvent *event) {
 revert:
         /* Restore 'dev_db_clone' */
         (void) device_add_property(event->dev_db_clone, "ID_RENAMING", NULL);
+        (void) device_add_property(event->dev_db_clone, "ID_PROCESSING", NULL);
         (void) device_update_db(event->dev_db_clone);
 
         /* Restore 'dev' */
@@ -348,6 +355,18 @@ int udev_event_execute_rules(UdevEvent *event, UdevRules *rules) {
         if (r < 0)
                 return log_device_debug_errno(dev, r, "Failed to remove 'ID_RENAMING' property: %m");
 
+        /* If the database file already exists, append ID_PROCESSING property to the existing database,
+         * to indicate that the device is being processed by udevd. */
+        if (device_has_db(event->dev_db_clone) > 0) {
+                r = device_add_property(event->dev_db_clone, "ID_PROCESSING", "1");
+                if (r < 0)
+                        return log_device_warning_errno(event->dev_db_clone, r, "Failed to add 'ID_PROCESSING' property: %m");
+
+                r = device_update_db(event->dev_db_clone);
+                if (r < 0)
+                        return log_device_warning_errno(event->dev_db_clone, r, "Failed to update database under /run/udev/data/: %m");
+        }
+
         DEVICE_TRACE_POINT(rules_start, dev);
 
         r = udev_rules_apply_to_event(rules, event);
@@ -377,6 +396,15 @@ int udev_event_execute_rules(UdevEvent *event, UdevRules *rules) {
         r = device_tag_index(dev, event->dev_db_clone, true);
         if (r < 0)
                 return log_device_debug_errno(dev, r, "Failed to update tags under /run/udev/tag/: %m");
+
+        /* If the database file for the device will be created below, add ID_PROCESSING=1 to indicate that
+         * the device is still being processed by udevd, as commands specified in RUN are invoked after
+         * the database is created. See issue #30056. */
+        if (device_should_have_db(dev) && !ordered_hashmap_isempty(event->run_list)) {
+                r = device_add_property(dev, "ID_PROCESSING", "1");
+                if (r < 0)
+                        return log_device_warning_errno(dev, r, "Failed to add 'ID_PROCESSING' property: %m");
+        }
 
         r = device_update_db(dev);
         if (r < 0)

commit 25cc34b77b8aec7edb79e63d83654d3d707230ea

udev: do not re-create database on remove event
Fixes a bug introduced by f6bda694f908cc227b002570b893029aa4c9e173 (v256).

With the offending commit, on remove event, database file for a device is once
removed in event_execute_rules_on_remove(), but later re-created here.
This fixes the issue, and makes the database file not re-created on remove event.

Also, for safety, do not try to enable inotify watch on remove event,
as another device may be created with the same device node while udevd
is processing the remove event of the previous owner of the device node.

src/udev/udev-worker.c
@@ -206,20 +206,24 @@ static int worker_process_device(UdevWorker *worker, sd_device *dev) {
                 /* in case rtnl was initialized */
                 worker->rtnl = sd_netlink_ref(udev_event->rtnl);
 
-        if (udev_event->inotify_watch) {
-                r = udev_watch_begin(worker->inotify_fd, dev);
-                if (r < 0 && r != -ENOENT) /* The device may be already removed, ignore -ENOENT. */
-                        log_device_warning_errno(dev, r, "Failed to add inotify watch, ignoring: %m");
+        if (!device_for_action(dev, SD_DEVICE_REMOVE)) {
+
+                /* Enable watch if requested. */
+                if (udev_event->inotify_watch) {
+                        r = udev_watch_begin(worker->inotify_fd, dev);
+                        if (r < 0 && r != -ENOENT) /* The device may be already removed, ignore -ENOENT. */
+                                log_device_warning_errno(dev, r, "Failed to add inotify watch, ignoring: %m");
+                }
+
+                /* Finalize database. */
+                r = device_add_property(dev, "ID_PROCESSING", NULL);
+                if (r < 0)
+                        return log_device_warning_errno(dev, r, "Failed to remove 'ID_PROCESSING' property: %m");
+
+                r = device_update_db(dev);
+                if (r < 0)
+                        return log_device_warning_errno(dev, r, "Failed to update database under /run/udev/data/: %m");
         }
-
-        /* Finalize database. */
-        r = device_add_property(dev, "ID_PROCESSING", NULL);
-        if (r < 0)
-                return log_device_warning_errno(dev, r, "Failed to remove 'ID_PROCESSING' property: %m");
-
-        r = device_update_db(dev);
-        if (r < 0)
-                return log_device_warning_errno(dev, r, "Failed to update database under /run/udev/data/: %m");
 
         log_device_uevent(dev, "Device processed");
         return 0;