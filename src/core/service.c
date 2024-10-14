72e4f92597245c7dbb7919ee00cd9c46f6b7bcbf
Mike Yuan <me@yhndnzj.com>
2024-10-14 19:59:47 +0200

    core/service: don't propagate stop jobs if RestartMode=direct
    The goal of RestartMode=direct is to make restarts invisible
    to dependents, so auto restart jobs shouldn't bring them down
    at all. So far we only skipped going through failed/dead states
    in service_enter_dead(), but when adding jobs to restart transaction,
    the stop job would be propagated to dependents. Let's introduce
    a TRANSACTION_DONT_PROPAGATE_STOP flag to suppress it, and set it
    in service_enter_restart().
    
    Fixes #34758

--- src/core/service.c
+++ src/core/service.c
@@ -47,6 +47,7 @@
 #include "string-table.h"
 #include "string-util.h"
 #include "strv.h"
+#include "transaction.h"
 #include "unit-name.h"
 #include "unit.h"
 #include "utf8.h"
@@ -2648,7 +2649,11 @@ static void service_enter_restart(Service *s, bool shortcut) {
 
         /* Any units that are bound to this service must also be restarted. We use JOB_START for ourselves
          * but then set JOB_RESTART_DEPENDENCIES which will enqueue JOB_RESTART for those dependency jobs. */
-        r = manager_add_job(UNIT(s)->manager, JOB_START, UNIT(s), JOB_RESTART_DEPENDENCIES, &error, /* ret = */ NULL);
+        r = manager_add_job_full(UNIT(s)->manager,
+                                 JOB_START, UNIT(s), JOB_RESTART_DEPENDENCIES,
+                                 s->restart_mode == SERVICE_RESTART_MODE_DIRECT ? TRANSACTION_DONT_PROPAGATE_STOP : 0,
+                                 /* affected_jobs = */ NULL,
+                                 &error, /* ret = */ NULL);
         if (r < 0) {
                 log_unit_warning(UNIT(s), "Failed to schedule restart job: %s", bus_error_message(&error, r));
                 return service_enter_dead(s, SERVICE_FAILURE_RESOURCES, /* allow_restart= */ false);
