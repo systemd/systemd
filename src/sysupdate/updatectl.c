adacffd8c87102e441169f57eebcd15b7adfd6b6
Antonio Alvarez Feijoo <antonio.feijoo@suse.com>
2024-10-14 12:02:04 +0200

    updatectl: remove unimplemented option

--- src/sysupdate/updatectl.c
+++ src/sysupdate/updatectl.c
@@ -1240,7 +1240,7 @@ static int parse_argv(int argc, char *argv[]) {
         assert(argc >= 0);
         assert(argv);
 
-        while ((c = getopt_long(argc, argv, "hH:M:", options, NULL)) >= 0) {
+        while ((c = getopt_long(argc, argv, "hH:", options, NULL)) >= 0) {
                 switch (c) {
 
                 case 'h':
