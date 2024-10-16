104481e354e857e9d5eb4d6adad75d76b22ec828
Mike Yuan <me@yhndnzj.com>
2024-10-16 16:30:10 +0200

    core: don't pass live mount internal errors as bus error
    These aren't expected by peer, and cannot be handled.
    We don't really propagate such error elsewhere.

--- src/core/unit.c
+++ src/core/unit.c
@@ -6403,8 +6403,8 @@ int unit_can_live_mount(const Unit *u, sd_bus_error *error) {
         if (!UNIT_VTABLE(u)->live_mount)
                 return sd_bus_error_setf(
                                 error,
-                                SD_BUS_ERROR_INVALID_ARGS,
-                                "Live mounting not supported for unit type '%s' of unit '%s'.",
+                                SD_BUS_ERROR_NOT_SUPPORTED,
+                                "Live mounting not supported for unit type '%s' of unit '%s'",
                                 unit_type_to_string(u->type),
                                 u->id);
 
@@ -6412,7 +6412,7 @@ int unit_can_live_mount(const Unit *u, sd_bus_error *error) {
                 return sd_bus_error_setf(
                                 error,
                                 BUS_ERROR_NO_SUCH_UNIT,
-                                "Unit '%s' not loaded, cannot live mount.",
+                                "Unit '%s' not loaded, cannot live mount",
                                 u->id);
 
         if (!UNIT_VTABLE(u)->can_live_mount)
@@ -6434,7 +6434,7 @@ int unit_live_mount(
         assert(UNIT_VTABLE(u)->live_mount);
 
         if (!UNIT_IS_ACTIVE_OR_RELOADING(unit_active_state(u))) {
-                log_unit_debug(u, "Unit not active");
+                log_unit_debug(u, "Unit not active, cannot perform live mount.");
                 return sd_bus_error_setf(
                                 error,
                                 BUS_ERROR_UNIT_INACTIVE,
@@ -6445,7 +6445,7 @@ int unit_live_mount(
         }
 
         if (unit_active_state(u) == UNIT_REFRESHING) {
-                log_unit_debug(u, "Unit already live mounting");
+                log_unit_debug(u, "Unit already live mounting, refusing further requests.");
                 return sd_bus_error_setf(
                                 error,
                                 BUS_ERROR_UNIT_BUSY,
