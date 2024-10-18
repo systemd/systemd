95b9e070b37983889b79834a417a6e35b469361a
Ryan Wilson <ryantimwilson@meta.com>
2024-10-18 11:41:09 -0700

    cgroup: Add support for ProtectControlGroups= private and strict
    This commit adds two settings private and strict to
    the ProtectControlGroups= property. Private will unshare the cgroup
    namespace and mount a read-write private cgroup2 filesystem at /sys/fs/cgroup.
    Strict does the same except the mount is read-only. Since the unit is
    running in a cgroup namespace, the new root of /sys/fs/cgroup is the unit's
    own cgroup.
    
    We also add a new dbus property ProtectControlGroupsEx which accepts strings
    instead of boolean. This will allow users to use private/strict via dbus
    and systemd-run in addition to service files.
    
    Note private and strict fall back to no and yes respectively if the kernel
    doesn't support cgroup2 or system is not using unified hierarchy.
    
    Fixes: #34634

--- src/core/execute.h
+++ src/core/execute.h
@@ -616,6 +616,11 @@ bool exec_needs_mount_namespace(const ExecContext *context, const ExecParameters
 bool exec_needs_network_namespace(const ExecContext *context);
 bool exec_needs_ipc_namespace(const ExecContext *context);
 
+ProtectControlGroups get_adjusted_protect_control_groups(const ExecContext *context, const ExecParameters *params);
+bool exec_needs_cgroup_namespace(const ExecContext *context, const ExecParameters *params);
+bool exec_needs_cgroup_mount(const ExecContext *context, const ExecParameters *params);
+bool exec_is_cgroup_mount_read_only(const ExecContext *context, const ExecParameters *params);
+
 /* These logging macros do the same logging as those in unit.h, but using ExecContext and ExecParameters
  * instead of the unit object, so that it can be used in the sd-executor context (where the unit object is
  * not available). */
