69135a7dca2eb35b8d4a90423f04bff4f77d00e4
Lennart Poettering <lennart@poettering.net>
2024-10-22 10:09:20 +0200

    fileio: port write_string_file() to LabelOps, and thus add WRITE_STRING_FILE_LABEL flag
    Given that we have the LabelOps abstraction these days, we can teach
    write_string_file() to use it, which means we can get rid of
    fileio-label.[ch] as a separate concept.
    
    (The only reason that fileio-label.[ch] exists independently of
    fileio.[ch] was that the former linekd to libselinux potentially, and
    thus had to be in src/shared/ while the other always was in src/basic/.
    But the LabelOps vtable provides us with a nice work-around)

--- src/shared/fileio-label.c
+++ src/shared/fileio-label.c
@@ -6,36 +6,23 @@
 #include "fileio.h"
 #include "selinux-util.h"
 
-int write_string_file_full_label(int atfd, const char *fn, const char *line, WriteStringFileFlags flags, struct timespec *ts) {
-        int r;
-
-        r = mac_selinux_create_file_prepare_at(atfd, fn, S_IFREG);
-        if (r < 0)
-                return r;
-
-        r = write_string_file_full(atfd, fn, line, flags, ts);
-
-        mac_selinux_create_file_clear();
-
-        return r;
-}
-
 int create_shutdown_run_nologin_or_warn(void) {
         int r;
 
         /* This is used twice: once in systemd-user-sessions.service, in order to block logins when we
          * actually go down, and once in systemd-logind.service when shutdowns are scheduled, and logins are
          * to be turned off a bit in advance. We use the same wording of the message in both cases.
-         *
+         *"
          * Traditionally, there was only /etc/nologin, and we managed that. Then, in PAM 1.1
          * support for /run/nologin was added as alternative
          * (https://github.com/linux-pam/linux-pam/commit/e9e593f6ddeaf975b7fe8446d184e6bc387d450b).
          * 13 years later we stopped managing /etc/nologin, leaving it for the administrator to manage.
          */
 
-        r = write_string_file_atomic_label("/run/nologin",
-                                           "System is going down. Unprivileged users are not permitted to log in anymore. "
-                                           "For technical details, see pam_nologin(8).");
+        r = write_string_file("/run/nologin",
+                              "System is going down. Unprivileged users are not permitted to log in anymore. "
+                              "For technical details, see pam_nologin(8).",
+                              WRITE_STRING_FILE_CREATE|WRITE_STRING_FILE_ATOMIC|WRITE_STRING_FILE_LABEL);
         if (r < 0)
                 return log_error_errno(r, "Failed to create /run/nologin: %m");
 
