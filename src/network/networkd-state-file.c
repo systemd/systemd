4bffc2bdbc1d373f7cd3e05035e70ff54bd0a43a
Ronan Pigott <ronan@rjp.ie>
2024-10-22 08:20:17 -0700

    network: remove log message about DNR
    The only possible error return in this position is -ENODATA, which is
    not interesting.

--- src/network/networkd-state-file.c
+++ src/network/networkd-state-file.c
@@ -601,7 +601,7 @@ static void serialize_resolvers(
 
                 r = sd_dhcp_lease_get_dnr(lease, &resolvers);
                 if (r < 0)
-                        return (void) log_warning_errno(r, "Failed to get DNR from DHCP lease, ignoring.");
+                        return;
 
                 r = dns_resolvers_to_dot_strv(resolvers, r, &names);
                 if (r < 0)
@@ -617,7 +617,7 @@ static void serialize_resolvers(
 
                 r = sd_dhcp6_lease_get_dnr(lease6, &resolvers);
                 if (r < 0)
-                        return (void) log_warning_errno(r, "Failed to get DNR from DHCPv6 lease, ignoring.");
+                        return;
 
                 r = dns_resolvers_to_dot_strv(resolvers, r, &names);
                 if (r < 0)
