1e0f03e7a1b797a777ade6d2d0ec80119b5d15a5
Igor Opaniuk <igor.opaniuk@foundries.io>
2024-10-22 15:22:15 +0200

    sd-boot: add support for a sysfail entry
    Add support for a sysfail boot entry. Sysfail boot entries can be used
    for optional tweaking the automatic selection order in case a
    failure state of the system in some form is detected (boot firmware failure
    etc).
    
    The EFI variable `LoaderEntrySysFail` contains the sysfail boot loader
    entry to use. It can be set using bootctl:
    $ bootctl set-sysfail sysfail.conf
    
    The `LoaderEntrySysFail` EFI variable would be unset automatically
    during next boot by `systemd-boot-clear-sysfail.service` if no
    system failure occured, otherwise it would be kept as it is and a system
    failure reason will be saved to `LoaderSysFailReason` EFI variable.
    
    sysfail_check() expected to be extented to support possible
    conditions when we should boot sysfail("recovery") boot entry.
    
    Signed-off-by: Igor Opaniuk <igor.opaniuk@foundries.io>

---
+++ src/boot/sysfail.c
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+
+#include "sysfail.h"
+#include "util.h"
+
+SysFailType sysfail_check(void) {
+        return SYSFAIL_OK;
+}
+
+const char16_t* sysfail_get_error_str(SysFailType fail_type) {
+        return NULL;
+}
