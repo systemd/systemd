e72d0bb2f316ef97d379475c04a2023fe7a2a919
Integral <integral@member.fsf.org>
2024-10-22 23:56:06 +0800

    analyze: refactor version comparison code

--- src/analyze/analyze-compare-versions.c
+++ src/analyze/analyze-compare-versions.c
@@ -9,26 +9,28 @@
 #include "strv.h"
 
 int verb_compare_versions(int argc, char *argv[], void *userdata) {
-        const char *v1 = ASSERT_PTR(argv[1]), *v2 = ASSERT_PTR(argv[argc-1]);
         int r;
 
         assert(IN_SET(argc, 3, 4));
         assert(argv);
 
+        const char *v[] = { ASSERT_PTR(argv[1]), ASSERT_PTR(argv[argc - 1]) };
+
         /* We only output a warning on invalid version strings (instead of failing), since the comparison
          * functions try to handle invalid strings gracefully and it's still interesting to see what the
          * comparison result will be. */
-        if (!version_is_valid_versionspec(v1))
-                log_warning("Version string 1 contains disallowed characters, they will be treated as separators: %s", v1);
-        if (!version_is_valid_versionspec(v2))
-                log_warning("Version string 2 contains disallowed characters, they will be treated as separators: %s", v2);
+        for (size_t i = 0; i < sizeof(v) / sizeof(v[0]); i++) {
+                log_warning("Version string %zu contains disallowed characters, they will be treated as separators: %s",
+                            i + 1,
+                            v[i]);
+        }
 
         if (argc == 3) {
-                r = strverscmp_improved(v1, v2);
+                r = strverscmp_improved(v[0], v[1]);
                 printf("%s %s %s\n",
-                       isempty(v1) ? "''" : v1,
+                       isempty(v[0]) ? "''" : v[0],
                        comparison_operator(r),
-                       isempty(v2) ? "''" : v2);
+                       isempty(v[1]) ? "''" : v[1]);
 
                 /* This matches the exit convention used by rpmdev-vercmp.
                  * We don't use named values because 11 and 12 don't have names. */
@@ -43,7 +45,7 @@ int verb_compare_versions(int argc, char *argv[], void *userdata) {
                 if (operator < 0 || !isempty(op))
                         return log_error_errno(SYNTHETIC_ERRNO(EINVAL), "Unknown operator \"%s\".", op);
 
-                r = version_or_fnmatch_compare(operator, v1, v2);
+                r = version_or_fnmatch_compare(operator, v[0], v[1]);
                 if (r < 0)
                         return log_error_errno(r, "Failed to compare versions: %m");
 
