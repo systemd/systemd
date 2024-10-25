4f151fc776000d144021cd0ad223f376727fea51
Daan De Meyer <daan.j.demeyer@gmail.com>
2024-10-25 17:03:37 +0200

    import: Draw progress bars
    Currently every progress update results in a new progress message
    which is extremely verbose. Instead, let's use the progress bar infra
    to draw a proper progress bar similar to what we do in systemd-repart
    now.

--- src/import/export-tar.c
+++ src/import/export-tar.c
@@ -7,6 +7,7 @@
 #include "export-tar.h"
 #include "fd-util.h"
 #include "import-common.h"
+#include "pretty-print.h"
 #include "process-util.h"
 #include "ratelimit.h"
 #include "string-util.h"
@@ -132,7 +133,19 @@ static void tar_export_report_progress(TarExport *e) {
                 return;
 
         sd_notifyf(false, "X_IMPORT_PROGRESS=%u%%", percent);
-        log_info("Exported %u%%.", percent);
+
+        if (isatty_safe(STDERR_FILENO)) {
+                _cleanup_free_ char *s = NULL;
+
+                if (asprintf(&s, "%s %s/%s",
+                             special_glyph(SPECIAL_GLYPH_ARROW_RIGHT),
+                             FORMAT_BYTES(e->written_uncompressed),
+                             FORMAT_BYTES(e->quota_referenced)) < 0)
+                        return;
+
+                draw_progress_bar(s, percent);
+        } else
+                log_info("Exported %u%%.", percent);
 
         e->last_percent = percent;
 }
@@ -229,6 +242,9 @@ static int tar_export_process(TarExport *e) {
         return 0;
 
 finish:
+        if (r >= 0 && isatty_safe(STDERR_FILENO))
+                clear_progress_bar(/* prefix= */ NULL);
+
         if (e->on_finished)
                 e->on_finished(e, r, e->userdata);
         else
