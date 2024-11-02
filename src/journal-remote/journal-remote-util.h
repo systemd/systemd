3d044ee1e3252103d3636b3951f628af791ea9e9
Andrii Chubatiuk <andrew.chubatiuk@gmail.com>
2024-11-02 22:03:26 +0200

    journal-remote: added compression, compression-level and content-encoding negotiation

---
+++ src/journal-remote/journal-remote-util.h
@@ -0,0 +1,16 @@
+#include "compress.h"
+#include "conf-parser.h"
+
+typedef struct CompressionOpts {
+        Compression algorithm;
+        int level;
+} CompressionOpts;
+
+typedef struct CompressionArgs {
+        CompressionOpts *opts;
+        size_t size;
+} CompressionArgs;
+
+CONFIG_PARSER_PROTOTYPE(config_parse_compression);
+
+void compression_args_clear(CompressionArgs *args);
