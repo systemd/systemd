5b3cd749fed5b83e8da92c6349d1f5d013456e73
Andrii Chubatiuk <andrew.chubatiuk@gmail.com>
2024-11-02 22:03:26 +0200

    journal-remote: added compression, compression-level and content-encoding negotiation

--- src/basic/compress.h
+++ src/basic/compress.h
@@ -22,8 +22,20 @@ typedef enum Compression {
         _COMPRESSION_INVALID = -EINVAL,
 } Compression;
 
+typedef struct CompressionOpts {
+        Compression algorithm;
+        int level;
+} CompressionOpts;
+
+typedef struct CompressionArgs {
+        CompressionOpts *opts;
+        size_t size;
+} CompressionArgs;
+
 const char* compression_to_string(Compression compression);
 Compression compression_from_string(const char *compression);
+const char* compression_lowercase_to_string(Compression compression);
+Compression compression_lowercase_from_string(const char *compression);
 
 bool compression_supported(Compression c);
 
