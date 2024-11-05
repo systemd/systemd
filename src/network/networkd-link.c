7759f27b4c7d71cf822f9442aa999c5247757a7a
Yu Watanabe <watanabe.yu+github@gmail.com>
2024-11-05 11:41:31 +0900

    network: introduce LINK_RECONFIGURE_CLEANLY flag
    And use it when explicit reconfiguration is requested by Reconfigure() DBus method
    or networkd certainly detects that connected network is changed.
    Otherwise do not use the flag especially when we come back from sleep mode.

--- src/network/networkd-link.c
+++ src/network/networkd-link.c
@@ -1371,7 +1371,7 @@ int link_reconfigure_impl(Link *link, LinkReconfigurationFlag flags) {
 
         /* Dropping old .network file */
 
-        if (FLAGS_SET(flags, LINK_RECONFIGURE_UNCONDITIONALLY)) {
+        if (FLAGS_SET(flags, LINK_RECONFIGURE_CLEANLY)) {
                 /* Remove all static configurations. Note, dynamic configurations are dropped by
                  * link_stop_engines(), and foreign configurations will be removed later by
                  * link_configure() -> link_drop_unmanaged_config(). */
@@ -1384,7 +1384,7 @@ int link_reconfigure_impl(Link *link, LinkReconfigurationFlag flags) {
                 if (r < 0)
                         return r;
 
-                /* Free DHCP client and friends. Note, if LINK_RECONFIGURE_UNCONDITIONALLY is unset, unnecessary
+                /* Free DHCP client and friends. Note, if LINK_RECONFIGURE_CLEANLY is unset, unnecessary
                  * engines (e.g. in the case that previously DHCP=yes, but now DHCP=no), will be freed later
                  * in link_configure(). */
                 link_free_engines(link);
@@ -1723,7 +1723,7 @@ static int link_carrier_gained(Link *link) {
          * For non-wireless interfaces, we have no way to detect the connected network change. So,
          * we do not set any flags here. Note, both ssid and previous_ssid are NULL in that case. */
         if (link->previous_ssid && !streq_ptr(link->previous_ssid, link->ssid))
-                flags |= LINK_RECONFIGURE_UNCONDITIONALLY;
+                flags |= LINK_RECONFIGURE_UNCONDITIONALLY | LINK_RECONFIGURE_CLEANLY;
         link->previous_ssid = mfree(link->previous_ssid);
 
         /* AP and P2P-GO interfaces may have a new SSID - update the link properties in case a new .network
