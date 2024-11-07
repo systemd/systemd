215292d09e8cc997e9034aabe5b37a75ad69d8cd
Antonio Alvarez Feijoo <antonio.feijoo@suse.com>
2024-11-07 08:57:39 +0100

    sbsign: remove unimplemented options

--- src/boot/sbsign.c
+++ src/boot/sbsign.c
@@ -90,7 +90,7 @@ static int parse_argv(int argc, char *argv[]) {
         assert(argc >= 0);
         assert(argv);
 
-        while ((c = getopt_long(argc, argv, "hjc", options, NULL)) >= 0)
+        while ((c = getopt_long(argc, argv, "h", options, NULL)) >= 0)
                 switch (c) {
 
                 case 'h':
