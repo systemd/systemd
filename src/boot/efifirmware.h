cfa5d00d17693dbada633a7dae98324f331c36a2
Ani Sinha <anisinha@redhat.com>
2024-11-08 12:01:51 +0530

    uki: introduce support for a .efifw section
    UKIs can be used to bundle uefi firmwares that can be measured and
    used on a confidential computing environment. There can be more than one
    firmware blob bundle, each one for a specific platform. Also firmware images
    can themselves be containers like IGVM files that can in turn bundle the
    actual firmware blob. This change is specifically for uefi firmwares, not
    IGVM container files.
    
    This change adds support to introduce a .efifw section in UKI that can be
    used for firmware blobs/images. There can be multiple such sections and each
    section can contain a single firmware image.
    
    The matching .hwids entry for a specific platform can be used to select the
    most appropriate firmware blob.
    
    ukify tool has been also changed to support addition of a firmware image
    in UKI.
    
    Since firmware gets measured automatically, we do not need to measure it
    separately as a part of the UKI.

---
+++ src/boot/efifirmware.h
@@ -0,0 +1,28 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#pragma once
+
+#include "efi.h"
+
+#define FWHEADERMAGIC (UINT32_C(0xfeeddead))
+
+/*  The structure of the efifw UKI blob is the following:
+ *  ---------------------------------------------------------
+ *  EfiFw header|fwid|payload| reserved for future attributes
+ *  ---------------------------------------------------------
+ *  The base header defines the length of full header, fwid and payload.
+ *  The fwid is a NULL terminated string.
+ *  The payload contains the actual efi firmware.
+ */
+typedef struct EfiFwHeader {
+        uint32_t magic; /* magic number that defines Efifw */
+        uint32_t header_len; /* total length of header including all attributes */
+        uint32_t fwid_len; /* length including the NULL terminator */
+        uint32_t payload_len; /* actual length of the efi firmware binary image */
+
+        /* The header might be extended in the future to add additional
+         * parameters. header_len will increase to indicate presence of these
+         * additional attributes.
+         */
+} EfiFwHeader;
+
+EFI_STATUS efifirmware_match_by_fwid(const void *uki_efifw, size_t uki_efifw_length, const char *fwid);
