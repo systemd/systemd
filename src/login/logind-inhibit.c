systemd/systemd

commit c7b5eb98e8eeafe63a079ee3c51e9670872437ae

logind: hook up inhibit logic with idle hint logic

src/login/logind-inhibit.c
@@ -150,6 +150,8 @@ int inhibitor_start(Inhibitor *i) {
         if (i->started)
                 return 0;
 
+        dual_timestamp_get(&i->since);
+
         log_debug("Inhibitor %s (%s) pid=%lu uid=%lu started.",
                   strna(i->who), strna(i->why),
                   (unsigned long) i->pid, (unsigned long) i->uid);
@@ -323,6 +325,32 @@ InhibitWhat manager_inhibit_what(Manager *m) {
                 what |= i->what;
 
         return what;
+}
+
+bool manager_is_inhibited(Manager *m, InhibitWhat w, dual_timestamp *since) {
+        Inhibitor *i;
+        Iterator j;
+        struct dual_timestamp ts = { 0, 0 };
+        bool inhibited = false;
+
+        assert(m);
+        assert(w > 0 && w < _INHIBIT_WHAT_MAX);
+
+        HASHMAP_FOREACH(i, m->inhibitor_fds, j) {
+                if (!(i->what & w))
+                        continue;
+
+                if (!inhibited ||
+                    i->since.monotonic < ts.monotonic)
+                        ts = i->since;
+
+                inhibited = true;
+        }
+
+        if (since)
+                *since = ts;
+
+        return inhibited;
 }
 
 const char *inhibit_what_to_string(InhibitWhat w) {

src/login/logind-dbus.c
@@ -1649,7 +1649,7 @@ static DBusHandlerResult manager_message_handler(
                         return bus_send_error_reply(connection, message, &error, r);
 
                 multiple_sessions = r > 0;
-                inhibit = !!(manager_inhibit_what(m) & INHIBIT_SHUTDOWN);
+                inhibit = manager_is_inhibited(m, INHIBIT_SHUTDOWN, NULL);
 
                 if (multiple_sessions) {
                         action = streq(dbus_message_get_member(message), "PowerOff") ?
@@ -1723,7 +1723,7 @@ static DBusHandlerResult manager_message_handler(
                         return bus_send_error_reply(connection, message, &error, r);
 
                 multiple_sessions = r > 0;
-                inhibit = !!(manager_inhibit_what(m) & INHIBIT_SHUTDOWN);
+                inhibit = manager_is_inhibited(m, INHIBIT_SHUTDOWN, NULL);
 
                 if (multiple_sessions) {
                         action = streq(dbus_message_get_member(message), "CanPowerOff") ?

src/login/logind-inhibit.h
@@ -52,6 +52,8 @@ struct Inhibitor {
         pid_t pid;
         uid_t uid;
 
+        dual_timestamp since;
+
         char *fifo_path;
         int fifo_fd;
 };
@@ -69,6 +71,7 @@ int inhibitor_create_fifo(Inhibitor *i);
 void inhibitor_remove_fifo(Inhibitor *i);
 
 InhibitWhat manager_inhibit_what(Manager *m);
+bool manager_is_inhibited(Manager *m, InhibitWhat w, dual_timestamp *since);
 
 const char *inhibit_what_to_string(InhibitWhat k);
 InhibitWhat inhibit_what_from_string(const char *s);

src/login/logind.c
@@ -1157,11 +1157,13 @@ void manager_gc(Manager *m, bool drop_not_started) {
 
 int manager_get_idle_hint(Manager *m, dual_timestamp *t) {
         Session *s;
-        bool idle_hint = true;
+        bool idle_hint;
         dual_timestamp ts = { 0, 0 };
         Iterator i;
 
         assert(m);
+
+        idle_hint = !manager_is_inhibited(m, INHIBIT_IDLE, t);
 
         HASHMAP_FOREACH(s, m->sessions, i) {
                 dual_timestamp k;

commit d5f3dea471915aee2311c784d040e5ba9a408f87

logind: fix 'since' timestamp of manager_is_inhibited()
Instead of initializing ts with DUAL_TIMESTAMP_NULL, initialize it with
DUAL_TIMESTAMP_INIFNITY.

The function later compares the inhibitor's monotonic timestamp with
'ts'. However, if 'ts' is initialized with DUAL_TIMESTAMP_NULL, then
"since.monotonic < ts.monotonic" will never be true. As a result, the function
returns just the "since period" of the first found inhibitor and not the oldest
"since period" over all inhibitors.

Fixes: c7b5eb98e8ee ("logind: hook up inhibit logic with idle hint logic")

src/login/logind-inhibit.c
@@ -407,7 +407,7 @@ bool manager_is_inhibited(
                 Inhibitor **ret_offending) {
 
         Inhibitor *i, *offending = NULL;
-        struct dual_timestamp ts = DUAL_TIMESTAMP_NULL;
+        struct dual_timestamp ts = DUAL_TIMESTAMP_INFINITY;
         bool inhibited = false;
 
         assert(m);