cb2bdaecfaebcbf47f567d612e91054af763710d
Ivan Kruglov <mail@ikruglov.com>
2024-11-18 16:31:35 +0100

    core: simple tests for io.systemd.Unit.List

--- test/units/TEST-74-AUX-UTILS.varlinkctl.sh
+++ test/units/TEST-74-AUX-UTILS.varlinkctl.sh
@@ -170,3 +170,13 @@ systemctl start user@4711
 varlinkctl info /run/user/4711/systemd/io.systemd.Manager
 varlinkctl introspect /run/user/4711/systemd/io.systemd.Manager
 varlinkctl call /run/user/4711/systemd/io.systemd.Manager io.systemd.Manager.Describe '{}'
+
+# test io.system.Unit
+varlinkctl info /run/systemd/unit/io.systemd.Unit
+varlinkctl introspect /run/systemd/unit/io.systemd.Unit io.systemd.Unit
+varlinkctl --more call /run/systemd/unit/io.systemd.Unit io.systemd.Unit.List '{}'
+
+systemctl start user@4711
+varlinkctl info /run/user/4711/systemd/unit/io.systemd.Unit
+varlinkctl introspect /run/user/4711/systemd/unit/io.systemd.Unit
+varlinkctl --more call /run/user/4711/systemd/unit/io.systemd.Unit io.systemd.Unit.List '{}'
