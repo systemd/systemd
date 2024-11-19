b047a329c957c150062082e5f9ec61cabdb0d49f
Mike Yuan <me@yhndnzj.com>
2024-11-19 18:50:13 +0100

    sd-varlink: mark sd_varlink_server_{ref,unref} as _public_
    Prompted by #35240

--- src/libsystemd/sd-varlink/sd-varlink.c
+++ src/libsystemd/sd-varlink/sd-varlink.c
@@ -3265,7 +3265,7 @@ static sd_varlink_server* varlink_server_destroy(sd_varlink_server *s) {
         return mfree(s);
 }
 
-DEFINE_TRIVIAL_REF_UNREF_FUNC(sd_varlink_server, sd_varlink_server, varlink_server_destroy);
+DEFINE_PUBLIC_TRIVIAL_REF_UNREF_FUNC(sd_varlink_server, sd_varlink_server, varlink_server_destroy);
 
 static int validate_connection(sd_varlink_server *server, const struct ucred *ucred) {
         int allowed = -1;
