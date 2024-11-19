59880831f1bd86418bc5ab94f245c5ce7cfa6380
Mike Yuan <me@yhndnzj.com>
2024-11-19 20:03:55 +0100

    basic: extract pidfd-related funcs from process-util into pidfd-util.[ch]

---
+++ src/basic/pidfd-util.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#pragma once
+
+#include <stdint.h>
+#if HAVE_PIDFD_OPEN
+#include <sys/pidfd.h>
+#endif
+#include <sys/types.h>
+
+int pidfd_get_pid(int fd, pid_t *ret);
+int pidfd_verify_pid(int pidfd, pid_t pid);
