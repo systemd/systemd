e161972c47a0a8ffa818766b8c19f7ec8bf23a74
Ani Sinha <anisinha@redhat.com>
2024-11-21 16:30:04 +0530

    hwids: add a new firmware type of device entry
    This change adds a new firmware type device entry for the .hwids section.
    It also adds necessary unit test changes.

--- src/boot/test-chid-match.c
+++ src/boot/test-chid-match.c
@@ -44,6 +44,7 @@ static struct {
         { "Device 1", "test,device-1" },
         { "Device 2", "test,device-2" },
         { "Device 3", "test,device-3" },
+        { "Device 4", "test,vmware" },
 };
 
 static RawSmbiosInfo current_info = {};
