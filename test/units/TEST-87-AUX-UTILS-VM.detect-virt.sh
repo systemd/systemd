c6cb6aee4d297f38588202bdf12884362f706908
Luca Boccassi <luca.boccassi@gmail.com>
2024-11-21 09:51:14 +0000

    test: split VM-only subtests from TEST-74-AUX-UTILS to new VM-only test
    TEST-74-AUX-UTILS was switched for the mkosi-based jobs, but not for the shell
    based jobs, so there's the new runner doing one thing, and the legacy runner
    doing something different.
    
    Move the VM-only subtests to TEST-87-AUX-UTILS-VM that is configured to only
    run in VMs under both runners. This way we keep the existing tests as-is, and
    we can add new VM-only tests without worrying.
    
    Follow-up for f4faac20730cbb339ae05ed6e20da687a2868e76

---
+++ test/units/TEST-87-AUX-UTILS-VM.detect-virt.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: LGPL-2.1-or-later
+set -eux
+set -o pipefail
+
+(! systemd-detect-virt -cq)
+
+SYSTEMD_IN_CHROOT=1 systemd-detect-virt --chroot
+(! SYSTEMD_IN_CHROOT=0 systemd-detect-virt --chroot)
