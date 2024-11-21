systemd/systemd

commit 0c5d667932f8abaf02814ee9ada6d0e63d63f8bb

user-util: Add helper functions for gid lists operations

src/basic/user-util.c
@@ -404,11 +404,16 @@ char* gid_to_name(gid_t gid) {
         return ret;
 }
 
+static bool gid_list_has(const gid_t *list, size_t size, gid_t val) {
+        for (size_t i = 0; i < size; i++)
+                if (list[i] == val)
+                        return true;
+        return false;
+}
+
 int in_gid(gid_t gid) {
-        _cleanup_free_ gid_t *allocated = NULL;
-        gid_t local[16], *p = local;
-        int ngroups = ELEMENTSOF(local);
-        unsigned attempt = 0;
+        _cleanup_free_ gid_t *gids = NULL;
+        int ngroups;
 
         if (getgid() == gid)
                 return 1;
@@ -418,6 +423,57 @@ int in_gid(gid_t gid) {
 
         if (!gid_is_valid(gid))
                 return -EINVAL;
+
+        ngroups = getgroups_alloc(&gids);
+        if (ngroups < 0)
+                return ngroups;
+
+        return gid_list_has(gids, ngroups, gid);
+}
+
+int in_group(const char *name) {
+        int r;
+        gid_t gid;
+
+        r = get_group_creds(&name, &gid, 0);
+        if (r < 0)
+                return r;
+
+        return in_gid(gid);
+}
+
+int merge_gid_lists(const gid_t *list1, size_t size1, const gid_t *list2, size_t size2, gid_t **ret) {
+        size_t nresult = 0;
+        assert(ret);
+
+        if (size2 > INT_MAX - size1)
+                return -ENOBUFS;
+
+        gid_t *buf = new(gid_t, size1 + size2);
+        if (!buf)
+                return -ENOMEM;
+
+        /* Duplicates need to be skipped on merging, otherwise they'll be passed on and stored in the kernel. */
+        for (size_t i = 0; i < size1; i++)
+                if (!gid_list_has(buf, nresult, list1[i]))
+                        buf[nresult++] = list1[i];
+        for (size_t i = 0; i < size2; i++)
+                if (!gid_list_has(buf, nresult, list2[i]))
+                        buf[nresult++] = list2[i];
+        *ret = buf;
+        return (int)nresult;
+}
+
+int getgroups_alloc(gid_t** gids) {
+        gid_t *allocated;
+        _cleanup_free_  gid_t *p = NULL;
+        int ngroups = 8;
+        unsigned attempt = 0;
+
+        allocated = new(gid_t, ngroups);
+        if (!allocated)
+                return -ENOMEM;
+        p = allocated;
 
         for (;;) {
                 ngroups = getgroups(ngroups, p);
@@ -447,22 +503,8 @@ int in_gid(gid_t gid) {
                 p = allocated;
         }
 
-        for (int i = 0; i < ngroups; i++)
-                if (p[i] == gid)
-                        return true;
-
-        return false;
-}
-
-int in_group(const char *name) {
-        int r;
-        gid_t gid;
-
-        r = get_group_creds(&name, &gid, 0);
-        if (r < 0)
-                return r;
-
-        return in_gid(gid);
+        *gids = TAKE_PTR(p);
+        return ngroups;
 }
 
 int get_home_dir(char **_h) {

src/basic/user-util.h
@@ -42,6 +42,9 @@ char* gid_to_name(gid_t gid);
 int in_gid(gid_t gid);
 int in_group(const char *name);
 
+int merge_gid_lists(const gid_t *list1, size_t size1, const gid_t *list2, size_t size2, gid_t **result);
+int getgroups_alloc(gid_t** gids);
+
 int get_home_dir(char **ret);
 int get_shell(char **_ret);
 

commit d7f5be7626b482e4407e9141ba5cab1d096e1604

basic/user-util: getgroups_alloc(): Fix bug
If getgroups(2) reports zero groups, our wrapper getgroups_allow() had
erratic behavior:

-  If we succeed in the first iteration.

	If we succeed in the first iteration, we correctly allocate a
	buffer, leave it unused (since getgroups(2) fills zero entries),
	set *gids to the allocated memory, and return 0.

-  If we need to iterate.

	If we fail in the first iteration, we do NOT set *gids, and
	return 'false'.  This is bogus in so many ways.

	First of all, 'false' is a boolean and makes absolutely no sense
	here where we're returning a number of elements or an error
	code.  Being converted to 0, the return value is technically
	valid, but morally still bogus.  And ...

	Secondly, and most importantly, we're not setting *gid, so if
	the caller tried to free(3) it (and it should, since we're
	supposed to have allocated in a successful --nonnegative--
	call), we'll have a big problem.

See an excerpt of the code responsible for the bug:

	 int getgroups_alloc(gid_t** gids) {
		...
		 int ngroups = 8;
		...

		 for (;;) {
			 p = new(gid_t, ngroups);
			...
			 ngroups = getgroups(ngroups, p);
			 if (ngroups >= 0)
				 break;
			...
			 ngroups = getgroups(0, NULL);
			...
	-                if (ngroups == 0)
	-                        return false;
	-
			 free(p);
		 }

		 *gids = TAKE_PTR(p);
		 return ngroups;
	 }

By removing the special case, we handle zero just like any other
non-negative value, which is just fine.

Fixes: 0c5d667932f8 (2020-01-08, "user-util: Add helper functions for gid lists operations")
Signed-off-by: Alejandro Colomar <alx@kernel.org>

src/basic/user-util.c
@@ -551,9 +551,6 @@ int getgroups_alloc(gid_t** gids) {
                 ngroups = getgroups(0, NULL);
                 if (ngroups < 0)
                         return -errno;
-                if (ngroups == 0)
-                        return false;
-
                 free(p);
         }
 