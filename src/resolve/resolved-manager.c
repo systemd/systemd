235ca87e91f032a8159ef3747e441cbe952f8151
Vishal Chillara Srinivas <vishal.chillarasrinivas@philips.com>
2024-11-26 18:06:09 +0530

    resolved: Rename 'DnssdService' to 'DnssdRegisteredService'
    Prepare for the upcoming mDNS-discovered services to avoid confusion.

--- src/resolve/resolved-manager.c
+++ src/resolve/resolved-manager.c
@@ -774,7 +774,7 @@ int manager_start(Manager *m) {
 
 Manager *manager_free(Manager *m) {
         Link *l;
-        DnssdService *s;
+        DnssdRegisteredService *s;
 
         if (!m)
                 return NULL;
@@ -1351,7 +1351,7 @@ int manager_find_ifindex(Manager *m, int family, const union in_addr_union *in_a
 
 void manager_refresh_rrs(Manager *m) {
         Link *l;
-        DnssdService *s;
+        DnssdRegisteredService *s;
 
         assert(m);
 
@@ -1775,7 +1775,7 @@ void manager_cleanup_saved_user(Manager *m) {
 }
 
 bool manager_next_dnssd_names(Manager *m) {
-        DnssdService *s;
+        DnssdRegisteredService *s;
         bool tried = false;
         int r;
 
