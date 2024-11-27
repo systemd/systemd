02ab91301c313ebd4981bc3c7105b9590f0a1ffe
Lennart Poettering <lennart@poettering.net>
2024-11-27 10:17:00 +0100

    namespace-util: add process_is_owned_by_uid() helper

--- src/basic/missing_fs.h
+++ src/basic/missing_fs.h
@@ -95,8 +95,16 @@ assert_cc(MS_LAZYTIME == (1<<25));
 #endif
 
 /* linux/nsfs.h */
-#ifndef NS_GET_NSTYPE /* d95fa3c76a66b6d76b1e109ea505c55e66360f3c (4.11) */
+#ifndef NS_GET_USERNS /* 6786741dbf99e44fb0c0ed85a37582b8a26f1c3b (4.9) */
+#define NS_GET_USERNS _IO(0xb7, 0x1)
+#endif
+
+#ifndef NS_GET_NSTYPE /* e5ff5ce6e20ee22511398bb31fb912466cf82a36 (4.11) */
 #define NS_GET_NSTYPE _IO(0xb7, 0x3)
+#endif
+
+#ifndef NS_GET_OWNER_UID /* d95fa3c76a66b6d76b1e109ea505c55e66360f3c (4.11) */
+#define NS_GET_OWNER_UID _IO(0xb7, 0x4)
 #endif
 
 #ifndef FS_PROJINHERIT_FL
