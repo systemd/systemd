3ed8bd2b258e7928939d4334f2f453c208a5ad39
Allison Karlitskaya <allison.karlitskaya@redhat.com>
2024-11-28 14:06:34 +0100

    copy: add support for copying fs-verity status
    Add a new member to CopyFlags to request copying of fs-verity status.
    We already call FS_IOC_GETFLAGS in the copy code, and it tells us if we
    have verity support.  If not, there is no change.
    
    If we do notice a file with fs-verity enabled, and if CopyFlags
    requested that we copy this, we request the descriptor from the source
    file and use it to setup fs-verity on the destination, using the same
    parameters.
    
    Signatures don't seem to be a particularly well-loved (or used) feature
    of fs-verity and we don't bother to query them here.  Support for that
    could be added later, if desired.
    
    This change means that, with the correct combination of flags, we might
    end up calling `fsync()` on a read-only file descriptor.  This is
    permitted by POSIX and supported on Linux.
    
    Nothing uses this yet.
    
    Signed-off-by: Allison Karlitskaya <allison.karlitskaya@redhat.com>

--- src/basic/missing_fs.h
+++ src/basic/missing_fs.h
@@ -116,3 +116,58 @@ assert_cc(FS_KEY_DESCRIPTOR_SIZE == 8);
 #ifndef FILEID_KERNFS
 #define FILEID_KERNFS 0xfe
 #endif
+
+/* linux/fsverity.h */
+#ifndef FS_IOC_ENABLE_VERITY
+/* Introduced in 085771ec14b9 ("fs-verity: add UAPI header") */
+#define FS_IOC_ENABLE_VERITY     _IOW('f', 133, struct fsverity_enable_arg)
+struct fsverity_enable_arg {
+        __u32 version;
+        __u32 hash_algorithm;
+        __u32 block_size;
+        __u32 salt_size;
+        __u64 salt_ptr;
+        __u32 sig_size;
+        __u32 __reserved1;
+        __u64 sig_ptr;
+        __u64 __reserved2[11];
+};
+#endif
+
+#if !HAVE_STRUCT_FSVERITY_DESCRIPTOR
+/* This was exposed on its own in
+ *   bde493349025 ("fs-verity: move structs needed for file signing to UAPI header")
+ */
+struct fsverity_descriptor {
+        __u8 version;           /* must be 1 */
+        __u8 hash_algorithm;    /* Merkle tree hash algorithm */
+        __u8 log_blocksize;     /* log2 of size of data and tree blocks */
+        __u8 salt_size;         /* size of salt in bytes; 0 if none */
+        __le32 __reserved_0x04; /* must be 0 */
+        __le64 data_size;       /* size of file the Merkle tree is built over */
+        __u8 root_hash[64];     /* Merkle tree root hash */
+        __u8 salt[32];          /* salt prepended to each hashed block */
+        __u8 __reserved[144];   /* must be 0's */
+};
+#endif
+
+#ifndef FS_IOC_READ_VERITY_METADATA
+/* All introduced as part of the same patch series:
+ *   e17fe6579de0 ("fs-verity: add FS_IOC_READ_VERITY_METADATA ioctl")
+ *   ...
+ *   07c99001312c ("fs-verity: support reading signature with ioctl")
+ */
+#define FS_VERITY_METADATA_TYPE_MERKLE_TREE     1
+#define FS_VERITY_METADATA_TYPE_DESCRIPTOR      2
+#define FS_VERITY_METADATA_TYPE_SIGNATURE       3
+#define FS_IOC_READ_VERITY_METADATA \
+        _IOWR('f', 135, struct fsverity_read_metadata_arg)
+
+struct fsverity_read_metadata_arg {
+        __u64 metadata_type;
+        __u64 offset;
+        __u64 length;
+        __u64 buf_ptr;
+        __u64 __reserved;
+};
+#endif
