57e13b082f3eeadabc3707037e059d90ea965685
Ryan Wilson <ryantimwilson@meta4.com>
2024-11-30 14:14:35 -0800

    core: Set /proc/pid/setgroups to allow for PrivateUsers=full
    When trying to run dbus-broker in a systemd unit with PrivateUsers=full,
    we see dbus-broker fails with EPERM at `util_audit_drop_permissions`.
    
    The root cause is dbus-broker calls the setgroups() system call and this
    is disallowed via systemd's implementation of PrivateUsers= by setting
    /proc/pid/setgroups = deny. This is done to remediate potential privilege
    escalation vulnerabilities in user namespaces where an attacker can remove
    supplementary groups and gain access to resources where those groups are
    restricted.
    
    However, for OS-like containers, setgroups() is a pretty common API and
    disabling it is not feasible. So we allow setgroups() by setting
    /proc/pid/setgroups to allow in PrivateUsers=full. Note security conscious
    users can still use SystemCallFilter= to disable setgroups() if they want
    to specifically prevent this system call.
    
    Fixes: #35425

--- test/units/TEST-07-PID1.private-users.sh
+++ test/units/TEST-07-PID1.private-users.sh
@@ -6,9 +6,12 @@ set -o pipefail
 
 systemd-run -p PrivateUsers=yes --wait bash -c 'test "$(cat /proc/self/uid_map)" == "         0          0          1"'
 systemd-run -p PrivateUsers=yes --wait bash -c 'test "$(cat /proc/self/gid_map)" == "         0          0          1"'
+systemd-run -p PrivateUsersEx=yes --wait bash -c 'test "$(cat /proc/self/setgroups)" == "deny"'
 systemd-run -p PrivateUsersEx=self --wait bash -c 'test "$(cat /proc/self/uid_map)" == "         0          0          1"'
 systemd-run -p PrivateUsersEx=self --wait bash -c 'test "$(cat /proc/self/gid_map)" == "         0          0          1"'
+systemd-run -p PrivateUsersEx=self --wait bash -c 'test "$(cat /proc/self/setgroups)" == "deny"'
 systemd-run -p PrivateUsersEx=identity --wait bash -c 'test "$(cat /proc/self/uid_map)" == "         0          0      65536"'
 systemd-run -p PrivateUsersEx=identity --wait bash -c 'test "$(cat /proc/self/gid_map)" == "         0          0      65536"'
 systemd-run -p PrivateUsersEx=full --wait bash -c 'test "$(cat /proc/self/uid_map)" == "         0          0 4294967295"'
 systemd-run -p PrivateUsersEx=full --wait bash -c 'test "$(cat /proc/self/gid_map)" == "         0          0 4294967295"'
+systemd-run -p PrivateUsersEx=full --wait bash -c 'test "$(cat /proc/self/setgroups)" == "allow"'
