c7fcb08324774c62bd2a786b03a7104a33049db6
Luca Boccassi <luca.boccassi@gmail.com>
2024-12-12 00:50:45 +0000

    test: add more coverage for extensions and verity

--- test/units/TEST-50-DISSECT.dissect.sh
+++ test/units/TEST-50-DISSECT.dissect.sh
@@ -365,6 +365,12 @@ systemd-run -P \
             --property ExtensionImages=/etc/service-scoped-test.raw \
             --property RootImage="$MINIMAL_IMAGE.raw" \
             cat /etc/systemd/system/some_file | grep -q -F "MARKER_CONFEXT_123"
+systemd-run -P \
+            --property ExtensionImages="/tmp/app0.raw /tmp/conf0.raw" \
+            veritysetup status "$(cat /tmp/app0.roothash)-verity" | grep -q "$(cat /tmp/app0.roothash)"
+systemd-run -P \
+            --property ExtensionImages="/tmp/app0.raw /tmp/conf0.raw" \
+            veritysetup status "$(cat /tmp/conf0.roothash)-verity" | grep -q "$(cat /tmp/conf0.roothash)"
 
 # Check that two identical verity images at different paths do not fail with -ELOOP from OverlayFS
 mkdir -p /tmp/loop
