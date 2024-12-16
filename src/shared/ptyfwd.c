systemd/systemd

commit d848a9499830c530e804a41ffd8aa1bc942fa735

shared/ptyfwd: detect String Terminator or BEL when parsing an OSC sequence

src/shared/ptyfwd.c
@@ -507,9 +507,15 @@ static int pty_forward_ansi_process(PTYForward *f, size_t offset) {
                                 } else if (!strextend(&f->osc_sequence, CHAR_TO_STR(c)))
                                         return -ENOMEM;
                         } else {
-                                /* Otherwise, the OSC sequence is over */
+                                /* Otherwise, the OSC sequence is over
+                                 *
+                                 * There are two allowed ways to end an OSC sequence:
+                                 * BEL '\x07'
+                                 * String Terminator (ST): <Esc>\ - "\x1b\x5c"
+                                 * since we cannot lookahead to see if the Esc is followed by a \
+                                 * we cut a corner here and assume it will be \. */
 
-                                if (c == '\x07') {
+                                if (c == '\x07' || c == '\x1b') {
                                         r = insert_window_title_fix(f, i+1);
                                         if (r < 0)
                                                 return r;

commit 524702dca488f854fb97f8e1369c9910b585eca8

ptyfwd: fix logic of OSC sequence termination
Previously, when an OSC sequence is terminated with \x1b\x5c, then
requested OSC sequence is inserted between the two characters, rather
than after the two.

Fixes a bug introduced by d848a9499830c530e804a41ffd8aa1bc942fa735.
Fixes #35631.

src/shared/ptyfwd.c
@@ -36,6 +36,7 @@ typedef enum AnsiColorState  {
         ANSI_COLOR_STATE_ESC,
         ANSI_COLOR_STATE_CSI_SEQUENCE,
         ANSI_COLOR_STATE_OSC_SEQUENCE,
+        ANSI_COLOR_STATE_OSC_SEQUENCE_TERMINATING,
         _ANSI_COLOR_STATE_MAX,
         _ANSI_COLOR_STATE_INVALID = -EINVAL,
 } AnsiColorState;
@@ -501,7 +502,7 @@ static int pty_forward_ansi_process(PTYForward *f, size_t offset) {
                                         f->ansi_color_state = ANSI_COLOR_STATE_TEXT;
                                 } else if (!strextend(&f->osc_sequence, CHAR_TO_STR(c)))
                                         return -ENOMEM;
-                        } else {
+                        } else if (c == '\x07') {
                                 /* Otherwise, the OSC sequence is over
                                  *
                                  * There are three documented ways to end an OSC sequence:
@@ -515,17 +516,33 @@ static int pty_forward_ansi_process(PTYForward *f, size_t offset) {
                                  * codepoint, and that would create ambiguity. Various terminal emulators
                                  * similar do not support it. */
 
-                                if (IN_SET(c, '\x07', '\x1b')) {
-                                        r = insert_window_title_fix(f, i+1);
-                                        if (r < 0)
-                                                return r;
-
-                                        i += r;
-                                }
+                                r = insert_window_title_fix(f, i+1);
+                                if (r < 0)
+                                        return r;
+                                i += r;
 
                                 f->osc_sequence = mfree(f->osc_sequence);
                                 f->ansi_color_state = ANSI_COLOR_STATE_TEXT;
+                        } else if (c == '\x1b')
+                                /* See the comment above. */
+                                f->ansi_color_state = ANSI_COLOR_STATE_OSC_SEQUENCE_TERMINATING;
+                        else {
+                                /* Unexpected OSC sequence. */
+                                f->osc_sequence = mfree(f->osc_sequence);
+                                f->ansi_color_state = ANSI_COLOR_STATE_TEXT;
                         }
+                        break;
+
+                case ANSI_COLOR_STATE_OSC_SEQUENCE_TERMINATING:
+                        if (c == '\x5c') {
+                                r = insert_window_title_fix(f, i+1);
+                                if (r < 0)
+                                        return r;
+                                i += r;
+                        }
+
+                        f->osc_sequence = mfree(f->osc_sequence);
+                        f->ansi_color_state = ANSI_COLOR_STATE_TEXT;
                         break;
 
                 default: