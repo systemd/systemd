7adac4caecf8a6cfbfcf2e169a803ce170359518
Yu Watanabe <watanabe.yu+github@gmail.com>
2024-12-16 11:50:53 +0900

    meson: allow to customize the access mode for tty/pts devices
    Then, switch the default value to "0600", due to general security
    concerns about terminals being written to by other users.
    
    Closing #35599.
    
    Backport note: the default is changed back to 0620 to keep backward
    compatbility intact for the purpose of backporting. This lets
    security-conscious users fix  the issue if they want to, without
    affecting users that do not want changes in the stable branches.
    
    (cherry picked from commit a4d18914751e687c9e44f22fe4e5f95b843a45c8)

--- src/nspawn/nspawn.c
+++ src/nspawn/nspawn.c
@@ -2399,13 +2399,13 @@ static int setup_pts(const char *dest) {
 #if HAVE_SELINUX
         if (arg_selinux_apifs_context)
                 (void) asprintf(&options,
-                                "newinstance,ptmxmode=0666,mode=620,gid=" GID_FMT ",context=\"%s\"",
+                                "newinstance,ptmxmode=0666,mode=" STRINGIFY(TTY_MODE) ",gid=" GID_FMT ",context=\"%s\"",
                                 arg_uid_shift + TTY_GID,
                                 arg_selinux_apifs_context);
         else
 #endif
                 (void) asprintf(&options,
-                                "newinstance,ptmxmode=0666,mode=620,gid=" GID_FMT,
+                                "newinstance,ptmxmode=0666,mode=" STRINGIFY(TTY_MODE) ",gid=" GID_FMT,
                                 arg_uid_shift + TTY_GID);
 
         if (!options)
