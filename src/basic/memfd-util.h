553d9a71399e0d5a69af32bb16e4fb16568f0c0a
Lennart Poettering <lennart@poettering.net>
2024-12-16 11:32:07 +0100

    memfd-util: trivial modernizations

--- src/basic/memfd-util.h
+++ src/basic/memfd-util.h
@@ -13,19 +13,19 @@ static inline int memfd_new(const char *name) {
         return memfd_new_full(name, 0);
 }
 
-int memfd_new_and_map(const char *name, size_t sz, void **p);
+int memfd_new_and_map(const char *name, size_t sz, void **ret);
 
 int memfd_new_and_seal(const char *name, const void *data, size_t sz);
 static inline int memfd_new_and_seal_string(const char *name, const char *s) {
         return memfd_new_and_seal(name, s, SIZE_MAX);
 }
 
-int memfd_add_seals(int fd, unsigned int seals);
-int memfd_get_seals(int fd, unsigned int *ret_seals);
-int memfd_map(int fd, uint64_t offset, size_t size, void **p);
+int memfd_add_seals(int fd, unsigned seals);
+int memfd_get_seals(int fd, unsigned *ret_seals);
+int memfd_map(int fd, uint64_t offset, size_t size, void **ret);
 
 int memfd_set_sealed(int fd);
 int memfd_get_sealed(int fd);
 
-int memfd_get_size(int fd, uint64_t *sz);
+int memfd_get_size(int fd, uint64_t *ret);
 int memfd_set_size(int fd, uint64_t sz);
