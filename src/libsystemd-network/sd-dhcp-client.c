1888f58471801152e59ae417c0181e5ca9d50b9d
Avram Dorfman <dorfman@est.org>
2024-12-18 15:20:00 -0500

    revert to original check that fails when server_address is missing

--- src/libsystemd-network/sd-dhcp-client.c
+++ src/libsystemd-network/sd-dhcp-client.c
@@ -1548,8 +1548,11 @@ static int client_parse_message(
         }
 
         r = dhcp_option_parse(message, len, dhcp_lease_parse_options, lease, &error_message);
-        if (r == -ENOMSG && client->bootp)
-                r = DHCP_ACK; /* BOOTP messages don't have a DHCP message type option */
+        if (r == -ENOMSG && client->bootp) {
+                /* BOOTP messages don't have a DHCP message type option */
+                log_dhcp_client(client, "received BOOTREQUEST");
+                r = DHCP_ACK;
+        }
         else if (r < 0)
                 return log_dhcp_client_errno(client, r, "Failed to parse DHCP options, ignoring: %m");
 
@@ -1605,14 +1608,11 @@ static int client_parse_message(
         if (client->bootp)
                 lease->lifetime = USEC_INFINITY;
 
-        if (lease->server_address == 0 && !client->bootp)
-                return log_dhcp_client_errno(client, SYNTHETIC_ERRNO(ENOMSG),
-                                             "received lease lacks server address, ignoring.");
-
         if (lease->address == 0 ||
+            lease->server_address == 0 ||
             lease->lifetime == 0)
                 return log_dhcp_client_errno(client, SYNTHETIC_ERRNO(ENOMSG),
-                                             "received lease lacks address or lease lifetime, ignoring.");
+                                             "received lease lacks address, server address or lease lifetime, ignoring.");
 
         r = dhcp_lease_set_default_subnet_mask(lease);
         if (r < 0)
@@ -1649,7 +1649,7 @@ static int client_handle_offer_or_rapid_ack(sd_dhcp_client *client, DHCPMessage 
         dhcp_lease_unref_and_replace(client->lease, lease);
 
         if (client->lease->rapid_commit || client->bootp) {
-                log_dhcp_client(client, "ACK");
+                log_dhcp_client(client, client->bootp ? "BOOTREPLY" : "ACK");
                 return SD_DHCP_CLIENT_EVENT_IP_ACQUIRE;
         }
 
