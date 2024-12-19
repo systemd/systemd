d57ab8457ed9afb93995b35ae307f866ae9bf209
Luca Boccassi <luca.boccassi@gmail.com>
2024-12-19 10:53:12 +0000

    Revert "terminal-util: don't issue "ESC c" sequence on reset, but only when erasing the screen"
    This reverts commit 841eb9c186816acfef8f3f724647a7f0649f214d.

--- src/basic/terminal-util.c
+++ src/basic/terminal-util.c
@@ -647,9 +647,8 @@ int vt_disallocate(const char *name) {
         (void) loop_write(fd2,
                           "\033[r"   /* clear scrolling region */
                           "\033[H"   /* move home */
-                          "\033[3J"  /* clear screen including scrollback, requires Linux 2.6.40 */
-                          "\033c",   /* reset to initial state */
-                          SIZE_MAX);
+                          "\033[3J", /* clear screen including scrollback, requires Linux 2.6.40 */
+                          10);
         return 0;
 }
 
@@ -1650,6 +1649,7 @@ int terminal_reset_ansi_seq(int fd) {
                 return log_debug_errno(r, "Failed to set terminal to non-blocking mode: %m");
 
         k = loop_write_full(fd,
+                            "\033c"        /* reset to initial state */
                             "\033[!p"      /* soft terminal reset */
                             "\033]104\007" /* reset colors */
                             "\033[?7h",    /* enable line-wrapping */
