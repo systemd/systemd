25ceb4f464b70904a3f8510e489b656cacd5f31e
Yu Watanabe <watanabe.yu+github@gmail.com>
2024-12-22 06:03:52 +0900

    udevadm-control: use varlink to send commands

--- src/udev/udev-varlink.c
+++ src/udev/udev-varlink.c
@@ -164,3 +164,23 @@ int manager_start_varlink_server(Manager *manager) {
         manager->varlink_server = TAKE_PTR(v);
         return 0;
 }
+
+int udev_varlink_connect(sd_varlink **ret, usec_t timeout) {
+        _cleanup_(sd_varlink_flush_close_unrefp) sd_varlink *link = NULL;
+        int r;
+
+        assert(ret);
+
+        r = sd_varlink_connect_address(&link, UDEV_VARLINK_ADDRESS);
+        if (r < 0)
+                return r;
+
+        (void) sd_varlink_set_description(link, "udev");
+
+        r = sd_varlink_set_relative_timeout(link, timeout);
+        if (r < 0)
+                return r;
+
+        *ret = TAKE_PTR(link);
+        return 0;
+}
