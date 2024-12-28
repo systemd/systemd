systemd/systemd

commit 804874d26ac73e0af07c4c5d7165c95372f03f6d

logind: always check for inhibitor locks
Currently inhibitors are bypassed unless an explicit request is made to
check for them, or even in that case when the requestor is root or the
same uid as the holder of the lock.

But in many cases this makes it impractical to rely on inhibitor locks.
For example, in Debian there are several convoluted and archaic
workarounds that divert systemctl/reboot to some hacky custom scripts
to try and enforce blocking accidental reboots, when it's not expected
that the requestor will remember to specify the command line option
to enable checking for active inhibitor locks.

Also in many cases one wants to ensure that locks taken by a user are
respected by actions initiated by that same user.

Change logind so that inhibitors checks are not skipped in these
cases, and systemctl so that locks are checked in order to show a
friendly error message rather than "permission denied".

Add new block-weak and delay-weak modes that keep the previous
behaviour unchanged.

src/systemctl/systemctl-logind.c
@@ -82,6 +82,7 @@ int logind_reboot(enum action a) {
                 return 0;
 
         SET_FLAG(flags, SD_LOGIND_ROOT_CHECK_INHIBITORS, arg_check_inhibitors > 0);
+        SET_FLAG(flags, SD_LOGIND_SKIP_INHIBITORS, arg_check_inhibitors == 0);
         SET_FLAG(flags,
                  SD_LOGIND_REBOOT_VIA_KEXEC,
                  a == ACTION_KEXEC || (a == ACTION_REBOOT && getenv_bool("SYSTEMCTL_SKIP_AUTO_KEXEC") <= 0));
@@ -94,17 +95,17 @@ int logind_reboot(enum action a) {
         SET_FLAG(flags, SD_LOGIND_SOFT_REBOOT, a == ACTION_SOFT_REBOOT);
 
         r = bus_call_method(bus, bus_login_mgr, method_with_flags, &error, NULL, "t", flags);
+        if (r < 0 && FLAGS_SET(flags, SD_LOGIND_SKIP_INHIBITORS) &&
+                        sd_bus_error_has_name(&error, SD_BUS_ERROR_INVALID_ARGS)) {
+                sd_bus_error_free(&error);
+                flags &= ~SD_LOGIND_SKIP_INHIBITORS;
+                r = bus_call_method(bus, bus_login_mgr, method_with_flags, &error, NULL, "t", flags);
+        }
         if (r < 0 && FLAGS_SET(flags, SD_LOGIND_SOFT_REBOOT_IF_NEXTROOT_SET_UP) &&
                         sd_bus_error_has_name(&error, SD_BUS_ERROR_INVALID_ARGS)) {
                 sd_bus_error_free(&error);
-                r = bus_call_method(
-                                bus,
-                                bus_login_mgr,
-                                method_with_flags,
-                                &error,
-                                NULL,
-                                "t",
-                                flags & ~SD_LOGIND_SOFT_REBOOT_IF_NEXTROOT_SET_UP);
+                flags &= ~SD_LOGIND_SOFT_REBOOT_IF_NEXTROOT_SET_UP;
+                r = bus_call_method(bus, bus_login_mgr, method_with_flags, &error, NULL, "t", flags);
         }
         if (r >= 0)
                 return 0;
@@ -144,13 +145,8 @@ int logind_check_inhibitors(enum action a) {
         if (arg_when > 0)
                 return 0;
 
-        if (arg_check_inhibitors < 0) {
-                if (geteuid() == 0)
-                        return 0;
-
-                if (!on_tty())
-                        return 0;
-        }
+        if (arg_check_inhibitors < 0 && !on_tty())
+                return 0;
 
         if (arg_transport != BUS_TRANSPORT_LOCAL)
                 return 0;
@@ -172,7 +168,10 @@ int logind_check_inhibitors(enum action a) {
                 _cleanup_free_ char *comm = NULL, *user = NULL;
                 _cleanup_strv_free_ char **sv = NULL;
 
-                if (!streq(mode, "block"))
+                if (!STR_IN_SET(mode, "block", "block-weak"))
+                        continue;
+
+                if (streq(mode, "block-weak") && (geteuid() == 0 || geteuid() == uid || !on_tty()))
                         continue;
 
                 sv = strv_split(what, ":");
@@ -233,6 +232,7 @@ int logind_check_inhibitors(enum action a) {
 
         return log_error_errno(SYNTHETIC_ERRNO(EPERM),
                                "Please retry operation after closing inhibitors and logging out other users.\n"
+                               "'systemd-inhibit' can be used to list active inhibitors.\n"
                                "Alternatively, ignore inhibitors and users with 'systemctl %s -i'.",
                                action_table[a].verb);
 #else

man/org.freedesktop.login1.xml
@@ -665,15 +665,18 @@ node /org/freedesktop/login1 {
 #define SD_LOGIND_KEXEC_REBOOT                   (UINT64_C(1) &lt;&lt; 1)
 #define SD_LOGIND_SOFT_REBOOT                    (UINT64_C(1) &lt;&lt; 2)
 #define SD_LOGIND_SOFT_REBOOT_IF_NEXTROOT_SET_UP (UINT64_C(1) &lt;&lt; 3)
+#define SD_LOGIND_SKIP_INHIBITORS                (UINT64_C(1) &lt;&lt; 4)
       </programlisting>
       <para>When the <varname>flags</varname> is 0 then these methods behave just like the versions without
-      flags. When <constant>SD_LOGIND_ROOT_CHECK_INHIBITORS</constant> (0x01) is set, active inhibitors are
-      honoured for privileged users too. When <constant>SD_LOGIND_KEXEC_REBOOT</constant> (0x02) is set,
-      then <function>RebootWithFlags()</function> performs a kexec reboot if kexec kernel is loaded. When
-      <constant>SD_LOGIND_SOFT_REBOOT</constant> (0x04) is set, or
-      <constant>SD_LOGIND_SOFT_REBOOT_IF_NEXTROOT_SET_UP</constant> (0x08) is set and a new root file system
-      has been set up on <literal>/run/nextroot/</literal>, then <function>RebootWithFlags()</function>
-      performs a userspace reboot only. <constant>SD_LOGIND_SOFT_REBOOT_IF_NEXTROOT_SET_UP</constant> and
+      flags. Since systemd version 256 <constant>SD_LOGIND_ROOT_CHECK_INHIBITORS</constant> (0x01) is deprecated,
+      and active inhibitors are always honoured by default for privileged users too, and a new flag
+      <constant>SD_LOGIND_SKIP_INHIBITORS</constant> (0x04) can be specified to bypass inhibitors. When
+      <constant>SD_LOGIND_KEXEC_REBOOT</constant> (0x02) is set, then <function>RebootWithFlags()</function>
+      performs a kexec reboot if kexec kernel is loaded. When <constant>SD_LOGIND_SOFT_REBOOT</constant>
+      (0x04) is set, or <constant>SD_LOGIND_SOFT_REBOOT_IF_NEXTROOT_SET_UP</constant> (0x08) is set and a
+      new root file system has been set up on <literal>/run/nextroot/</literal>, then
+      <function>RebootWithFlags()</function> performs a userspace reboot only.
+      <constant>SD_LOGIND_SOFT_REBOOT_IF_NEXTROOT_SET_UP</constant> and
       <constant>SD_LOGIND_KEXEC_REBOOT</constant> can be combined, with soft-reboot having precedence.</para>
 
       <para><function>SetRebootParameter()</function> sets a parameter for a subsequent reboot operation.
@@ -731,8 +734,10 @@ node /org/freedesktop/login1 {
       should be a short human readable string identifying the reason why the lock is taken. Finally,
       <varname>mode</varname> is either <literal>block</literal> or <literal>delay</literal> which encodes
       whether the inhibit shall be consider mandatory or whether it should just delay the operation to a
-      certain maximum time. The method returns a file descriptor. The lock is released the moment this file
-      descriptor and all its duplicates are closed. For more information on the inhibition logic see
+      certain maximum time, while the <literal>block-weak</literal> and <literal>delay-weak</literal>
+      variants will create an inhibitor that is automatically ignored in some circumstances. The method
+      returns a file descriptor. The lock is released the moment this file descriptor and all its duplicates
+      are closed. For more information on the inhibition logic see
       <ulink url="https://systemd.io/INHIBITOR_LOCKS">Inhibitor Locks</ulink>.
       </para>
     </refsect2>

man/systemctl.xml
@@ -2288,17 +2288,15 @@ Jan 12 10:46:45 example.com bluetoothd[8900]: gatt-time-server: Input/output err
         <listitem>
           <para>When system shutdown or sleep state is requested, this option controls checking of inhibitor
           locks. It takes one of <literal>auto</literal>, <literal>yes</literal> or
-          <literal>no</literal>. Defaults to <literal>auto</literal>, which will behave like
-          <literal>yes</literal> for interactive invocations (i.e. from a TTY) and <literal>no</literal> for
-          non-interactive invocations. <literal>yes</literal> lets the request respect inhibitor locks.
-          <literal>no</literal> lets the request ignore inhibitor locks.</para>
+          <literal>no</literal>. Defaults to <literal>auto</literal>, which means logind will perform the
+          check and respect active inhibitor locks, but systemctl will only do a client-side check for
+          interactive invocations (i.e. from a TTY), so that a more friendly and informative error can be
+          returned to users. <literal>no</literal> disables both the systemctl and logind checks.</para>
 
           <para>Applications can establish inhibitor locks to prevent certain important operations (such as
           CD burning) from being interrupted by system shutdown or sleep. Any user may take these locks and
           privileged users may override these locks. If any locks are taken, shutdown and sleep state
-          requests will normally fail (unless privileged). However, if <literal>no</literal> is specified or
-          <literal>auto</literal> is specified on a non-interactive requests, the operation will be
-          attempted. If locks are present, the operation may require additional privileges.</para>
+          requests will normally fail (unless explicitly overridden with <literal>no</literal>).</para>
 
           <para>Option <option>--force</option> provides another way to override inhibitors.</para>
 

man/systemd-inhibit.xml
@@ -92,19 +92,17 @@
       <varlistentry>
         <term><option>--mode=</option></term>
 
-        <listitem><para>Takes either <literal>block</literal> or
-        <literal>delay</literal> and describes how the lock is
-        applied. If <literal>block</literal> is used (the default),
-        the lock prohibits any of the requested operations without
-        time limit, and only privileged users may override it. If
-        <literal>delay</literal> is used, the lock can only delay the
-        requested operations for a limited time. If the time elapses,
-        the lock is ignored and the operation executed. The time limit
-        may be specified in
+        <listitem><para>Takes <literal>block</literal>, <literal>delay</literal>,
+        <literal>block-weak</literal> or <literal>delay-weak</literal> and describes how the lock is
+        applied. If <literal>block</literal> is used (the default), the lock prohibits any of the requested
+        operations without time limit, and only privileged users may override it. If
+        <literal>delay</literal> is used, the lock can only delay the requested operations for a limited
+        time. If the time elapses, the lock is ignored and the operation executed. The time limit may be
+        specified in
         <citerefentry><refentrytitle>logind.conf</refentrytitle><manvolnum>5</manvolnum></citerefentry>.
-        Note that <literal>delay</literal> is only available for
-        <literal>sleep</literal> and
-        <literal>shutdown</literal>.</para></listitem>
+        Note that <literal>delay</literal> is only available for <literal>sleep</literal> and
+        <literal>shutdown</literal>. In addition, the weak variants will automatically and silently be
+        bypassed under some circumstances.</para></listitem>
       </varlistentry>
 
       <varlistentry>

shell-completion/zsh/_systemd-inhibit
@@ -33,6 +33,6 @@ _arguments \
     '--what=[Operations to inhibit]:options:_systemd-inhibit_what' \
     '--who=[A descriptive string who is inhibiting]:who is inhibiting:' \
     '--why=[A descriptive string why is being inhibited]:reason for the lock:' \
-    '--mode=[One of block or delay]:lock mode:( block delay )' \
+    '--mode=[One of block, block-weak, delay, or delay-weak]:lock mode:( block block-weak delay delay-weak )' \
     '--list[List active inhibitors]' \
     '*:commands:_systemd-inhibit_commands'

src/basic/login-util.h
@@ -8,11 +8,12 @@
 #define SD_LOGIND_REBOOT_VIA_KEXEC                (UINT64_C(1) << 1)
 #define SD_LOGIND_SOFT_REBOOT                     (UINT64_C(1) << 2)
 #define SD_LOGIND_SOFT_REBOOT_IF_NEXTROOT_SET_UP  (UINT64_C(1) << 3)
+#define SD_LOGIND_SKIP_INHIBITORS                 (UINT64_C(1) << 4)
 
 /* For internal use only */
 #define SD_LOGIND_INTERACTIVE                     (UINT64_C(1) << 63)
 
-#define SD_LOGIND_SHUTDOWN_AND_SLEEP_FLAGS_PUBLIC (SD_LOGIND_ROOT_CHECK_INHIBITORS|SD_LOGIND_REBOOT_VIA_KEXEC|SD_LOGIND_SOFT_REBOOT|SD_LOGIND_SOFT_REBOOT_IF_NEXTROOT_SET_UP)
+#define SD_LOGIND_SHUTDOWN_AND_SLEEP_FLAGS_PUBLIC (SD_LOGIND_ROOT_CHECK_INHIBITORS|SD_LOGIND_REBOOT_VIA_KEXEC|SD_LOGIND_SOFT_REBOOT|SD_LOGIND_SOFT_REBOOT_IF_NEXTROOT_SET_UP|SD_LOGIND_SKIP_INHIBITORS)
 #define SD_LOGIND_SHUTDOWN_AND_SLEEP_FLAGS_ALL    (SD_LOGIND_SHUTDOWN_AND_SLEEP_FLAGS_PUBLIC|SD_LOGIND_INTERACTIVE)
 
 bool session_id_valid(const char *id);

src/login/inhibit.c
@@ -157,7 +157,7 @@ static int help(void) {
                "                          handle-lid-switch\n"
                "     --who=STRING         A descriptive string who is inhibiting\n"
                "     --why=STRING         A descriptive string why is being inhibited\n"
-               "     --mode=MODE          One of block or delay\n"
+               "     --mode=MODE          One of block, block-weak, delay, or delay-weak\n"
                "     --list               List active inhibitors\n"
                "\nSee the %s for details.\n",
                program_invocation_short_name,

src/login/logind-action.c
@@ -234,7 +234,7 @@ static int handle_action_execute(
         /* If the actual operation is inhibited, warn and fail */
         if (inhibit_what_is_valid(inhibit_operation) &&
             !ignore_inhibited &&
-            manager_is_inhibited(m, inhibit_operation, INHIBIT_BLOCK, NULL, false, false, 0, &offending)) {
+            manager_is_inhibited(m, inhibit_operation, /* block= */ true, NULL, false, false, 0, &offending)) {
                 _cleanup_free_ char *comm = NULL, *u = NULL;
 
                 (void) pidref_get_comm(&offending->pid, &comm);
@@ -372,7 +372,7 @@ int manager_handle_action(
 
         /* If the key handling is inhibited, don't do anything */
         if (inhibit_key > 0) {
-                if (manager_is_inhibited(m, inhibit_key, INHIBIT_BLOCK, NULL, true, false, 0, NULL)) {
+                if (manager_is_inhibited(m, inhibit_key, /* block= */ true, NULL, true, false, 0, NULL)) {
                         log_debug("Refusing %s operation, %s is inhibited.",
                                   handle_action_to_string(handle),
                                   inhibit_what_to_string(inhibit_key));

commit d48da487a453ce5389d66d4c107abf2b6ba1fa9a

systemctl: also ignore ENOENT in checking inhibitors
Fixes a bug caused by 804874d26ac73e0af07c4c5d7165c95372f03f6d.
Follow-up for a1417e5563acb73a0accdc12b3af91e4ec1c7f46.
Fixes #35757.

src/systemctl/systemctl-logind.c
@@ -152,9 +152,9 @@ int logind_check_inhibitors(enum action a) {
                 return 0;
 
         r = acquire_bus_full(BUS_FULL, /* graceful = */ true, &bus);
-        if (r == -ECONNREFUSED && geteuid() == 0)
-                return 0; /* When D-Bus is not running, allow root to force a shutdown. E.g. when running at
-                           * the emergency console. */
+        if (ERRNO_IS_NEG_DISCONNECT(r) && geteuid() == 0)
+                return 0; /* When D-Bus is not running (ECONNREFUSED) or D-Bus socket is not created (ENOENT),
+                           * allow root to force a shutdown. E.g. when running at the emergency console. */
         if (r < 0)
                 return r;
 