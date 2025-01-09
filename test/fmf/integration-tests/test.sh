8965d57f4b03dd8979f84f555836287f380e9e29
Daan De Meyer <daan.j.demeyer@gmail.com>
2025-01-09 15:45:41 +0100

    fmf: Use one fewer than number of available CPUs again
    This effectively reverts b8582198ca1e6fe390f7169e623a9130b68a6b36
    as I can not get the testing farm bare metal machines working
    downstream and even if I managed to, without also using the testing
    farm bare metal machines upstream (for which there is no capacity),
    the setup would very quickly bitrot anyway so we'll just run the
    container based tests for now.

--- test/fmf/integration-tests/test.sh
+++ test/fmf/integration-tests/test.sh
@@ -112,13 +112,6 @@ if [[ ! -e /dev/kvm ]]; then
     export TEST_NO_QEMU=1
 fi
 
-NPROC=$(nproc)
-if [[ $NPROC -ge 10 ]]; then
-    NPROC=$((NPROC / 2))
-else
-    NPROC=$((NPROC - 1))
-fi
-
 # Create missing mountpoint for mkosi sandbox.
 mkdir -p /etc/pacman.d/gnupg
 
@@ -134,7 +127,7 @@ mkosi -f sandbox \
     --suite integration-tests \
     --print-errorlogs \
     --no-stdsplit \
-    --num-processes "$NPROC" && EC=0 || EC=$?
+    --num-processes "$(($(nproc) - 1))" && EC=0 || EC=$?
 
 find build/meson-logs -type f -exec mv {} "$TMT_TEST_DATA" \;
 find build/test/journal -type f -exec mv {} "$TMT_TEST_DATA" \;
