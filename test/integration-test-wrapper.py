737bbb796106d12b282b384b7937ff2248290c15
Daan De Meyer <daan.j.demeyer@gmail.com>
2025-01-13 16:56:05 +0100

    test: Use mv instead of shutil.move() in integration-test-wrapper
    WIP

--- test/integration-test-wrapper.py
+++ test/integration-test-wrapper.py
@@ -560,7 +560,7 @@ def main() -> None:
     if os.getenv('TEST_JOURNAL_USE_TMP', '0') == '1':
         dst = args.meson_build_dir / f'test/journal/{name}.journal'
         dst.parent.mkdir(parents=True, exist_ok=True)
-        shutil.move(journal_file, dst)
+        subprocess.run(["mv", journal_file, dst], check=True)
 
     if shell or (result.returncode in (args.exit_code, 77) and not coredumps and not sanitizer):
         exit(0 if shell or result.returncode == args.exit_code else 77)
