systemd/systemd

commit 2f0aa9a80445ef18086260a60fad71920ad9486c

udev-varlink: introduce io.systemd.Udev varlink interface
Co-authored-by: David Tardon <dtardon@redhat.com>

src/udev/udev-manager.c
@@ -32,6 +32,7 @@
 #include "udev-spawn.h"
 #include "udev-trace.h"
 #include "udev-util.h"
+#include "udev-varlink.h"
 #include "udev-watch.h"
 #include "udev-worker.h"
 
@@ -148,6 +149,7 @@ Manager* manager_free(Manager *manager) {
 
         sd_device_monitor_unref(manager->monitor);
         udev_ctrl_unref(manager->ctrl);
+        sd_varlink_server_unref(manager->varlink_server);
 
         sd_event_source_unref(manager->inotify_event);
         sd_event_source_unref(manager->kill_workers_event);
@@ -213,7 +215,7 @@ void manager_kill_workers(Manager *manager, bool force) {
         }
 }
 
-static void manager_exit(Manager *manager) {
+void manager_exit(Manager *manager) {
         assert(manager);
 
         manager->exit = true;
@@ -222,7 +224,7 @@ static void manager_exit(Manager *manager) {
 
         /* close sources of new events and discard buffered events */
         manager->ctrl = udev_ctrl_unref(manager->ctrl);
-
+        manager->varlink_server = sd_varlink_server_unref(manager->varlink_server);
         manager->inotify_event = sd_event_source_disable_unref(manager->inotify_event);
         manager->inotify_fd = safe_close(manager->inotify_fd);
 
@@ -246,7 +248,7 @@ void notify_ready(Manager *manager) {
 }
 
 /* reload requested, HUP signal received, rules changed, builtin changed */
-static void manager_reload(Manager *manager, bool force) {
+void manager_reload(Manager *manager, bool force) {
         _cleanup_(udev_rules_freep) UdevRules *rules = NULL;
         usec_t now_usec;
         int r;
@@ -1150,6 +1152,9 @@ static int listen_fds(int *ret_ctrl, int *ret_netlink) {
         for (int i = 0; i < n; i++) {
                 int fd = SD_LISTEN_FDS_START + i;
 
+                if (streq(names[i], "varlink"))
+                        continue; /* The fd will be handled by sd_varlink_server_listen_auto(). */
+
                 if (sd_is_socket(fd, AF_UNIX, SOCK_SEQPACKET, -1) > 0) {
                         if (ctrl_fd >= 0) {
                                 log_debug("Received multiple seqpacket socket (%s), ignoring.", names[i]);
@@ -1414,6 +1419,10 @@ int manager_main(Manager *manager) {
                 return r;
 
         r = manager_start_ctrl(manager);
+        if (r < 0)
+                return r;
+
+        r = manager_start_varlink_server(manager);
         if (r < 0)
                 return r;
 

src/udev/udev-varlink.c
@@ -1,0 +1,165 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+
+#include "json-util.h"
+#include "strv.h"
+#include "udev-manager.h"
+#include "udev-varlink.h"
+#include "varlink-io.systemd.Udev.h"
+#include "varlink-io.systemd.service.h"
+#include "varlink-util.h"
+
+#define UDEV_VARLINK_ADDRESS "/run/udev/io.systemd.Udev"
+
+static int vl_method_reload(sd_varlink *link, sd_json_variant *parameters, sd_varlink_method_flags_t flags, void *userdata) {
+        assert(link);
+
+        if (sd_json_variant_elements(parameters) > 0)
+                return sd_varlink_error_invalid_parameter(link, parameters);
+
+        log_debug("Received io.systemd.service.Reload()");
+        manager_reload(userdata, /* force = */ true);
+        return sd_varlink_reply(link, NULL);
+}
+
+static int vl_method_set_log_level(sd_varlink *link, sd_json_variant *parameters, sd_varlink_method_flags_t flags, void *userdata) {
+        int r, level;
+
+        static const sd_json_dispatch_field dispatch_table[] = {
+                { "level", _SD_JSON_VARIANT_TYPE_INVALID, json_dispatch_log_level, 0, SD_JSON_MANDATORY },
+                {}
+        };
+
+        assert(link);
+
+        r = sd_varlink_dispatch(link, parameters, dispatch_table, &level);
+        if (r != 0)
+                return r;
+
+        log_debug("Received io.systemd.service.SetLogLevel(%i)", level);
+        manager_set_log_level(userdata, level);
+        return sd_varlink_reply(link, NULL);
+}
+
+static int vl_method_set_children_max(sd_varlink *link, sd_json_variant *parameters, sd_varlink_method_flags_t flags, void *userdata) {
+        unsigned n;
+        int r;
+
+        static const sd_json_dispatch_field dispatch_table[] = {
+                { "number", _SD_JSON_VARIANT_TYPE_INVALID, sd_json_dispatch_uint, 0, SD_JSON_MANDATORY },
+                {}
+        };
+
+        assert(link);
+
+        r = sd_varlink_dispatch(link, parameters, dispatch_table, &n);
+        if (r != 0)
+                return r;
+
+        log_debug("Received io.systemd.Udev.SetChildrenMax(%u)", n);
+        manager_set_children_max(userdata, n);
+        return sd_varlink_reply(link, NULL);
+}
+
+static int vl_method_set_environment(sd_varlink *link, sd_json_variant *parameters, sd_varlink_method_flags_t flags, void *userdata) {
+        int r;
+
+        static const sd_json_dispatch_field dispatch_table[] = {
+                { "assignments", SD_JSON_VARIANT_ARRAY, json_dispatch_strv_environment, 0, SD_JSON_MANDATORY },
+                {}
+        };
+
+        assert(link);
+
+        _cleanup_strv_free_ char **v = NULL;
+        r = sd_varlink_dispatch(link, parameters, dispatch_table, &v);
+        if (r != 0)
+                return r;
+
+        log_debug("Received io.systemd.Udev.SetEnvironment()");
+        manager_set_environment(userdata, v);
+        return sd_varlink_reply(link, NULL);
+}
+
+static int vl_method_start_stop_exec_queue(sd_varlink *link, sd_json_variant *parameters, sd_varlink_method_flags_t flags, void *userdata) {
+        Manager *manager = ASSERT_PTR(userdata);
+        const char *method;
+        int r;
+
+        assert(link);
+
+        if (sd_json_variant_elements(parameters) > 0)
+                return sd_varlink_error_invalid_parameter(link, parameters);
+
+        r = sd_varlink_get_current_method(link, &method);
+        if (r < 0)
+                return r;
+
+        log_debug("Received %s()", method);
+        manager->stop_exec_queue = streq(method, "io.systemd.Udev.StopExecQueue");
+        return sd_varlink_reply(link, NULL);
+}
+
+static int vl_method_exit(sd_varlink *link, sd_json_variant *parameters, sd_varlink_method_flags_t flags, void *userdata) {
+        assert(link);
+
+        if (sd_json_variant_elements(parameters) > 0)
+                return sd_varlink_error_invalid_parameter(link, parameters);
+
+        /* Refuse further connections. */
+        _unused_ _cleanup_(sd_varlink_flush_close_unrefp) sd_varlink *v = sd_varlink_ref(link);
+
+        log_debug("Received io.systemd.udev.Exit()");
+        manager_exit(userdata);
+        return sd_varlink_reply(link, NULL);
+}
+
+int manager_start_varlink_server(Manager *manager) {
+        _cleanup_(sd_varlink_server_unrefp) sd_varlink_server *v = NULL;
+        int r;
+
+        assert(manager);
+        assert(manager->event);
+
+        r = varlink_server_new(&v, SD_VARLINK_SERVER_ROOT_ONLY | SD_VARLINK_SERVER_INHERIT_USERDATA, manager);
+        if (r < 0)
+                return log_error_errno(r, "Failed to allocate Varlink server: %m");
+
+        /* This needs to be after the inotify and uevent handling, to make sure that the ping is send back
+         * after fully processing the pending uevents (including the synthetic ones we may create due to
+         * inotify events). */
+        r = sd_varlink_server_attach_event(v, manager->event, SD_EVENT_PRIORITY_IDLE);
+        if (r < 0)
+                return log_error_errno(r, "Failed to attach Varlink connection to event loop: %m");
+
+        r = sd_varlink_server_listen_auto(v);
+        if (r < 0)
+                return log_error_errno(r, "Failed to bind to passed Varlink socket: %m");
+        if (r == 0) {
+                r = sd_varlink_server_listen_address(v, UDEV_VARLINK_ADDRESS, 0600);
+                if (r < 0)
+                        return log_error_errno(r, "Failed to bind to Varlink socket: %m");
+        }
+
+        r = sd_varlink_server_add_interface_many(
+                        v,
+                        &vl_interface_io_systemd_service,
+                        &vl_interface_io_systemd_Udev);
+        if (r < 0)
+                return log_error_errno(r, "Failed to add Varlink interface: %m");
+
+        r = sd_varlink_server_bind_method_many(
+                        v,
+                        "io.systemd.service.Ping",          varlink_method_ping,
+                        "io.systemd.service.Reload",        vl_method_reload,
+                        "io.systemd.service.SetLogLevel",   vl_method_set_log_level,
+                        "io.systemd.Udev.SetChildrenMax",   vl_method_set_children_max,
+                        "io.systemd.Udev.SetEnvironment",   vl_method_set_environment,
+                        "io.systemd.Udev.StartExecQueue",   vl_method_start_stop_exec_queue,
+                        "io.systemd.Udev.StopExecQueue",    vl_method_start_stop_exec_queue,
+                        "io.systemd.Udev.Exit",             vl_method_exit);
+        if (r < 0)
+                return log_error_errno(r, "Failed to bind Varlink methods: %m");
+
+        manager->varlink_server = TAKE_PTR(v);
+        return 0;
+}

src/udev/udev-varlink.h
@@ -1,0 +1,6 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#pragma once
+
+typedef struct Manager Manager;
+
+int manager_start_varlink_server(Manager *manager);

src/shared/meson.build
@@ -194,6 +194,7 @@ shared_sources = files(
         'varlink-io.systemd.PCRLock.c',
         'varlink-io.systemd.Resolve.c',
         'varlink-io.systemd.Resolve.Monitor.c',
+        'varlink-io.systemd.Udev.c',
         'varlink-io.systemd.UserDatabase.c',
         'varlink-io.systemd.oom.c',
         'varlink-io.systemd.service.c',

src/shared/varlink-io.systemd.Udev.c
@@ -1,0 +1,34 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include "varlink-io.systemd.Udev.h"
+
+static SD_VARLINK_DEFINE_METHOD(
+                SetChildrenMax,
+                SD_VARLINK_FIELD_COMMENT("The maximum number of child processes. When 0 is specified, the maximum is determined based on the system resources."),
+                SD_VARLINK_DEFINE_INPUT(number, SD_VARLINK_INT, 0));
+
+static SD_VARLINK_DEFINE_METHOD(
+                SetEnvironment,
+                SD_VARLINK_FIELD_COMMENT("An array of global udev property assignments. Each string must be in KEY=VALUE style."),
+                SD_VARLINK_DEFINE_INPUT(assignments, SD_VARLINK_STRING, SD_VARLINK_ARRAY));
+
+static SD_VARLINK_DEFINE_METHOD(StartExecQueue);
+
+static SD_VARLINK_DEFINE_METHOD(StopExecQueue);
+
+static SD_VARLINK_DEFINE_METHOD(Exit);
+
+SD_VARLINK_DEFINE_INTERFACE(
+                io_systemd_Udev,
+                "io.systemd.Udev",
+                SD_VARLINK_INTERFACE_COMMENT("An interface for controlling systemd-udevd."),
+                SD_VARLINK_SYMBOL_COMMENT("Sets the maximum number of child processes."),
+                &vl_method_SetChildrenMax,
+                SD_VARLINK_SYMBOL_COMMENT("Sets the global udev properties."),
+                &vl_method_SetEnvironment,
+                SD_VARLINK_SYMBOL_COMMENT("Starts processing of queued events."),
+                &vl_method_StartExecQueue,
+                SD_VARLINK_SYMBOL_COMMENT("Stops processing of queued events."),
+                &vl_method_StopExecQueue,
+                SD_VARLINK_SYMBOL_COMMENT("Terminates systemd-udevd. This exists for backward compatibility. Please consider to use 'systemctl stop systemd-udevd.service'."),
+                &vl_method_Exit);

src/shared/varlink-io.systemd.Udev.h
@@ -1,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#pragma once
+
+#include "sd-varlink.h"
+#include "sd-varlink-idl.h"
+
+extern const sd_varlink_interface vl_interface_io_systemd_Udev;

src/test/test-varlink-idl.c
@@ -25,6 +25,7 @@
 #include "varlink-io.systemd.PCRLock.h"
 #include "varlink-io.systemd.Resolve.h"
 #include "varlink-io.systemd.Resolve.Monitor.h"
+#include "varlink-io.systemd.Udev.h"
 #include "varlink-io.systemd.UserDatabase.h"
 #include "varlink-io.systemd.oom.h"
 #include "varlink-io.systemd.service.h"
@@ -196,6 +197,8 @@ TEST(parse_format) {
         test_parse_format_one(&vl_interface_io_systemd_MachineImage);
         print_separator();
         test_parse_format_one(&vl_interface_io_systemd_AskPassword);
+        print_separator();
+        test_parse_format_one(&vl_interface_io_systemd_Udev);
         print_separator();
         test_parse_format_one(&vl_interface_xyz_test);
 }

src/udev/meson.build
@@ -37,6 +37,7 @@ libudevd_core_sources = files(
         'udev-node.c',
         'udev-rules.c',
         'udev-spawn.c',
+        'udev-varlink.c',
         'udev-watch.c',
         'udev-worker.c',
 )

commit 312e80c152b35058d57de9d58ab08e252610fa26

udev: do not add passed socket as varlink socket
Fixes a bug introduced by 2f0aa9a80445ef18086260a60fad71920ad9486c.

In the offending commit, `sd_varlink_server_listen_auto()` is used.
That may add file descriptors of netlink socket for uevent and legacy
control socket varlink server. Then, udevd may fail to receive uevents.

src/udev/udev-manager.c
@@ -1135,12 +1135,67 @@ Manager* manager_new(void) {
         return manager;
 }
 
-static int listen_fds(int *ret_ctrl, int *ret_netlink) {
-        _cleanup_strv_free_ char **names = NULL;
-        _cleanup_close_ int ctrl_fd = -EBADF, netlink_fd = -EBADF;
+static int manager_init_ctrl(Manager *manager, int fd) {
+        int r;
 
-        assert(ret_ctrl);
-        assert(ret_netlink);
+        assert(manager);
+
+        /* This consumes passed file descriptor on success. */
+
+        if (fd >= 0) {
+                if (manager->ctrl)
+                        return log_warning_errno(SYNTHETIC_ERRNO(EALREADY), "Received multiple control socket (%i), ignoring.", fd);
+
+                r = sd_is_socket(fd, AF_UNIX, SOCK_SEQPACKET, -1);
+                if (r < 0)
+                        return log_warning_errno(r, "Failed to verify socket type of %i, ignoring: %m", fd);
+                if (r == 0)
+                        return log_warning_errno(SYNTHETIC_ERRNO(EINVAL), "Received invalid control socket (%i), ignoring.", fd);
+        } else {
+                if (manager->ctrl)
+                        return 0;
+        }
+
+        r = udev_ctrl_new_from_fd(&manager->ctrl, fd);
+        if (r < 0)
+                return log_error_errno(r, "Failed to initialize udev control socket: %m");
+
+        return 0;
+}
+
+static int manager_init_device_monitor(Manager *manager, int fd) {
+        int r;
+
+        assert(manager);
+
+        /* This consumes passed file descriptor on success. */
+
+        if (fd >= 0) {
+                if (manager->monitor)
+                        return log_warning_errno(SYNTHETIC_ERRNO(EALREADY), "Received multiple netlink socket (%i), ignoring.", fd);
+
+                r = sd_is_socket(fd, AF_NETLINK, SOCK_RAW, /* listening = */ -1);
+                if (r < 0)
+                        return log_warning_errno(r, "Failed to verify socket type of %i, ignoring: %m", fd);
+                if (r == 0)
+                        return log_warning_errno(SYNTHETIC_ERRNO(EINVAL), "Received invalid netlink socket (%i), ignoring.", fd);
+        } else {
+                if (manager->monitor)
+                        return 0;
+        }
+
+        r = device_monitor_new_full(&manager->monitor, MONITOR_GROUP_KERNEL, fd);
+        if (r < 0)
+                return log_error_errno(r, "Failed to initialize device monitor: %m");
+
+        return 0;
+}
+
+static int manager_listen_fds(Manager *manager) {
+        _cleanup_strv_free_ char **names = NULL;
+        int r;
+
+        assert(manager);
 
         int n = sd_listen_fds_with_names(/* unset_environment = */ true, &names);
         if (n < 0)
@@ -1152,101 +1207,32 @@ static int listen_fds(int *ret_ctrl, int *ret_netlink) {
         for (int i = 0; i < n; i++) {
                 int fd = SD_LISTEN_FDS_START + i;
 
-                if (streq(names[i], "varlink"))
-                        continue; /* The fd will be handled by sd_varlink_server_listen_auto(). */
-
-                if (sd_is_socket(fd, AF_UNIX, SOCK_SEQPACKET, -1) > 0) {
-                        if (ctrl_fd >= 0) {
-                                log_debug("Received multiple seqpacket socket (%s), ignoring.", names[i]);
-                                goto unused;
-                        }
-
-                        ctrl_fd = fd;
-                        continue;
-                }
-
-                if (sd_is_socket(fd, AF_NETLINK, SOCK_RAW, -1) > 0) {
-                        if (netlink_fd >= 0) {
-                                log_debug("Received multiple netlink socket (%s), ignoring.", names[i]);
-                                goto unused;
-                        }
-
-                        netlink_fd = fd;
-                        continue;
-                }
-
-                log_debug("Received unexpected fd (%s), ignoring.", names[i]);
-
-        unused:
-                close_and_notify_warn(fd, names[i]);
+                if (streq(names[i], "systemd-udevd-varlink.socket"))
+                        r = manager_init_varlink_server(manager, fd);
+                else if (streq(names[i], "systemd-udevd-control.socket"))
+                        r = manager_init_ctrl(manager, fd);
+                else if (streq(names[i], "systemd-udevd-kernel.socket"))
+                        r = manager_init_device_monitor(manager, fd);
+                else
+                        r = log_debug_errno(SYNTHETIC_ERRNO(EINVAL),
+                                            "Received unexpected fd (%s), ignoring.", names[i]);
+                if (r < 0)
+                        close_and_notify_warn(fd, names[i]);
         }
 
-        *ret_ctrl = TAKE_FD(ctrl_fd);
-        *ret_netlink = TAKE_FD(netlink_fd);
-        return 0;
-}
-
-static int manager_init_ctrl(Manager *manager, int fd_ctrl) {
-        _cleanup_(udev_ctrl_unrefp) UdevCtrl *ctrl = NULL;
-        _cleanup_close_ int fd = fd_ctrl;
-        int r;
-
-        assert(manager);
-
-        /* This consumes passed file descriptor. */
-
-        r = udev_ctrl_new_from_fd(&ctrl, fd);
-        if (r < 0)
-                return log_error_errno(r, "Failed to initialize udev control socket: %m");
-        TAKE_FD(fd);
-
-        r = udev_ctrl_enable_receiving(ctrl);
-        if (r < 0)
-                return log_error_errno(r, "Failed to bind udev control socket: %m");
-
-        manager->ctrl = TAKE_PTR(ctrl);
-        return 0;
-}
-
-static int manager_init_device_monitor(Manager *manager, int fd_uevent) {
-        _cleanup_(sd_device_monitor_unrefp) sd_device_monitor *monitor = NULL;
-        _cleanup_close_ int fd = fd_uevent;
-        int r;
-
-        assert(manager);
-
-        /* This consumes passed file descriptor. */
-
-        r = device_monitor_new_full(&monitor, MONITOR_GROUP_KERNEL, fd);
-        if (r < 0)
-                return log_error_errno(r, "Failed to initialize device monitor: %m");
-        TAKE_FD(fd);
-
-        (void) sd_device_monitor_set_description(monitor, "manager");
-
-        manager->monitor = TAKE_PTR(monitor);
         return 0;
 }
 
 int manager_init(Manager *manager) {
-        _cleanup_close_ int fd_ctrl = -EBADF, fd_uevent = -EBADF;
-        _cleanup_free_ char *cgroup = NULL;
         int r;
 
         assert(manager);
 
-        r = listen_fds(&fd_ctrl, &fd_uevent);
-        if (r < 0)
-                return log_error_errno(r, "Failed to listen on fds: %m");
-
-        r = manager_init_ctrl(manager, TAKE_FD(fd_ctrl));
+        r = manager_listen_fds(manager);
         if (r < 0)
                 return r;
 
-        r = manager_init_device_monitor(manager, TAKE_FD(fd_uevent));
-        if (r < 0)
-                return r;
-
+        _cleanup_free_ char *cgroup = NULL;
         r = cg_pid_get_path(SYSTEMD_CGROUP_CONTROLLER, 0, &cgroup);
         if (r < 0)
                 log_debug_errno(r, "Failed to get cgroup, ignoring: %m");
@@ -1262,7 +1248,14 @@ static int manager_start_ctrl(Manager *manager) {
         int r;
 
         assert(manager);
-        assert(manager->ctrl);
+
+        r = manager_init_ctrl(manager, -EBADF);
+        if (r < 0)
+                return r;
+
+        r = udev_ctrl_enable_receiving(manager->ctrl);
+        if (r < 0)
+                return log_error_errno(r, "Failed to bind udev control socket: %m");
 
         r = udev_ctrl_attach_event(manager->ctrl, manager->event);
         if (r < 0)
@@ -1286,7 +1279,12 @@ static int manager_start_device_monitor(Manager *manager) {
         int r;
 
         assert(manager);
-        assert(manager->monitor);
+
+        r = manager_init_device_monitor(manager, -EBADF);
+        if (r < 0)
+                return r;
+
+        (void) sd_device_monitor_set_description(manager->monitor, "manager");
 
         r = sd_device_monitor_attach_event(manager->monitor, manager->event);
         if (r < 0)

src/udev/udev-varlink.c
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 
 #include "json-util.h"
+#include "socket-util.h"
 #include "strv.h"
 #include "udev-manager.h"
 #include "udev-varlink.h"
@@ -113,6 +114,42 @@ static int vl_method_exit(sd_varlink *link, sd_json_variant *parameters, sd_varl
         return sd_varlink_reply(link, NULL);
 }
 
+int manager_init_varlink_server(Manager *manager, int fd) {
+        int r;
+
+        assert(manager);
+
+        /* This consumes passed file descriptor on success. */
+
+        if (fd >= 0) {
+                if (manager->varlink_server)
+                        return log_warning_errno(SYNTHETIC_ERRNO(EALREADY), "Received multiple varlink socket (%i), ignoring.", fd);
+
+                int b;
+                r = getsockopt_int(fd, SOL_SOCKET, SO_ACCEPTCONN, &b);
+                if (r < 0)
+                        return log_warning_errno(r, "Failed to check if fd is a listening socket, ignoring: %m");
+                if (b == 0)
+                        return log_warning_errno(SYNTHETIC_ERRNO(EINVAL), "Received varlink socket without SO_ACCEPTCONN enabled, ignoring.");
+        } else {
+                if (manager->varlink_server)
+                        return 0;
+        }
+
+        r = varlink_server_new(&manager->varlink_server, SD_VARLINK_SERVER_ROOT_ONLY | SD_VARLINK_SERVER_INHERIT_USERDATA, manager);
+        if (r < 0)
+                return log_error_errno(r, "Failed to allocate Varlink server: %m");
+
+        if (fd >= 0)
+                r = sd_varlink_server_listen_fd(manager->varlink_server, fd);
+        else
+                r = sd_varlink_server_listen_address(manager->varlink_server, UDEV_VARLINK_ADDRESS, 0600);
+        if (r < 0)
+                return log_error_errno(r, "Failed to bind to Varlink socket: %m");
+
+        return 0;
+}
+
 int manager_start_varlink_server(Manager *manager) {
         _cleanup_(sd_varlink_server_unrefp) sd_varlink_server *v = NULL;
         int r;
@@ -120,9 +157,9 @@ int manager_start_varlink_server(Manager *manager) {
         assert(manager);
         assert(manager->event);
 
-        r = varlink_server_new(&v, SD_VARLINK_SERVER_ROOT_ONLY | SD_VARLINK_SERVER_INHERIT_USERDATA, manager);
+        r = manager_init_varlink_server(manager, -EBADF);
         if (r < 0)
-                return log_error_errno(r, "Failed to allocate Varlink server: %m");
+                return r;
 
         /* This needs to be after the inotify and uevent handling, to make sure that the ping is send back
          * after fully processing the pending uevents (including the synthetic ones we may create due to
@@ -130,15 +167,6 @@ int manager_start_varlink_server(Manager *manager) {
         r = sd_varlink_server_attach_event(v, manager->event, SD_EVENT_PRIORITY_IDLE);
         if (r < 0)
                 return log_error_errno(r, "Failed to attach Varlink connection to event loop: %m");
-
-        r = sd_varlink_server_listen_auto(v);
-        if (r < 0)
-                return log_error_errno(r, "Failed to bind to passed Varlink socket: %m");
-        if (r == 0) {
-                r = sd_varlink_server_listen_address(v, UDEV_VARLINK_ADDRESS, 0600);
-                if (r < 0)
-                        return log_error_errno(r, "Failed to bind to Varlink socket: %m");
-        }
 
         r = sd_varlink_server_add_interface_many(
                         v,

src/udev/udev-varlink.h
@@ -7,5 +7,6 @@
 
 typedef struct Manager Manager;
 
+int manager_init_varlink_server(Manager *manager, int fd);
 int manager_start_varlink_server(Manager *manager);
 int udev_varlink_connect(sd_varlink **ret, usec_t timeout);