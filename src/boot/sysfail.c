7f90b29deac013762ee9648604698d0c7f6373d2
Igor Opaniuk <igor.opaniuk@foundries.io>
2025-01-23 13:31:04 +0100

    sd-boot: use sysfail entry for UEFI firmware update failure
    Add support for using a sysfail boot entry in case of UEFI firmware
    capsule update failure [1]. The status of a firmware update is obtained from
    the EFI System Resource Table (ESRT), which provides an optional mechanism
    for identifying device and system firmware resources for the purposes of
    targeting firmware updates to those resources.
    
    Current implementation uses the value of LastAttemptStatus field from
    ESRT, which describes the result of the last firmware update attempt for
    the firmware resource entry. The field is updated each time an
    UpdateCapsule() is attempted for an ESRT entry and is preserved across
    reboots (non-volatile).
    
    This can be be used in setups with support for A/B OTA updates, where
    the boot firmware and Linux/RootFS might be updated synchronously.
    
    The check is activated by adding "sysfail-firmware-upd" to
    loader.conf
    
    [1] https://uefi.org/specs/UEFI/2.10/23_Firmware_Update_and_Reporting.html
    Signed-off-by: Igor Opaniuk <igor.opaniuk@foundries.io>

--- src/boot/sysfail.c
+++ src/boot/sysfail.c
@@ -3,10 +3,37 @@
 #include "sysfail.h"
 #include "util.h"
 
-SysFailType sysfail_check(void) {
+
+static bool check_firmware_update_failure(void) {
+        const EFI_SYSTEM_RESOURCE_TABLE *esrt_table = NULL;
+        const EFI_SYSTEM_RESOURCE_ENTRY *esrt_entry = NULL;
+
+        esrt_table = find_configuration_table(MAKE_GUID_PTR(EFI_SYSTEM_RESOURCE_TABLE));
+        if (!esrt_table)
+                return false;
+
+        esrt_entry = (const EFI_SYSTEM_RESOURCE_ENTRY*) ((const uint8_t*) esrt_table +
+                        sizeof(EFI_SYSTEM_RESOURCE_TABLE));
+
+        for (uint32_t i = 0; i < esrt_table->FwResourceCount; i++, esrt_entry++)
+                        if (esrt_entry->FwType == ESRT_FW_TYPE_SYSTEMFIRMWARE)
+                                return esrt_entry->LastAttemptStatus != LAST_ATTEMPT_STATUS_SUCCESS;
+
+        return false;
+}
+
+SysFailType sysfail_check(SysFailConfig *sysfail_config) {
+        if (sysfail_config->check_firmware_update && check_firmware_update_failure())
+                return SYSFAIL_FIRMWARE_UPDATE;
+
         return SYSFAIL_OK;
 }
 
 const char16_t* sysfail_get_error_str(SysFailType fail_type) {
-        return NULL;
+        switch (fail_type) {
+        case SYSFAIL_FIRMWARE_UPDATE:
+                return u"FirmwareUpdateFailed";
+        default:
+                return u"UnknownError";
+        }
 }
