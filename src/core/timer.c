systemd/systemd

commit 6546045fa0bf84737bd8b2e1e8bf7dd3941d8352

core/timer: Always use inactive_exit_timestamp if it is set
If we're doing a daemon-reload, we'll be going from TIMER_DEAD => TIMER_WAITING,
so we won't use inactive_exit_timestamp because TIMER_DEAD != UNIT_ACTIVE, even
though inactive_exit_timestamp is serialized/deserialized and will be valid after
the daemon-reload.

This issue can lead to timers never firing as we'll always calculate the next
elapse based on the current realtime on daemon-reload, so if daemon-reload happens
often enough, the elapse interval will be moved into the future every time, which
means the timer will never trigger.

To fix the issue, let's always use inactive_exit_timestamp if it is set, and only
fall back to the current realtime if it is not set.

src/core/timer.c
@@ -401,12 +401,10 @@ static void timer_enter_waiting(Timer *t, bool time_change) {
 
                         if (t->last_trigger.realtime > 0)
                                 b = t->last_trigger.realtime;
-                        else {
-                                if (state_translation_table[t->state] == UNIT_ACTIVE)
-                                        b = UNIT(t)->inactive_exit_timestamp.realtime;
-                                else
-                                        b = ts.realtime;
-                        }
+                        else if (dual_timestamp_is_set(&UNIT(t)->inactive_exit_timestamp))
+                                b = UNIT(t)->inactive_exit_timestamp.realtime;
+                        else
+                                b = ts.realtime;
 
                         r = calendar_spec_next_usec(v->calendar_spec, b, &v->next_elapse);
                         if (r < 0)

commit 2048c98160cd43df2a3f8d659cdc6be0fa06908e

core/timer: Don't always use inactive_exit_timestamp if it is set
The timer may have since entered an inactive state. When the timer is
stopped and restarted, the inactive exit timestamp here corresponds to
the last activation of the timer unit, making it effectively impossible
to stop and restart a systemd timer: the timer would always trigger
immedately when started if the target elapsed at any time since the
timer was last activated, even if the timer was stopped for this period.

For example, if a timer that has timespec OnCalendar=hourly was started
at 1:30 and stopped at 3:30 it will have triggered at 2:00 and 3:00
during the period it was active, and not trigger at 4:00 and 5:00 while
the timer is stopped. If the timer were then restarted at 6:45 it would
immediately trigger, considering the interval to have elapsed 5 times
since the inactive exit timestamp currently at 1:30.

This effectively reverts 6546045fa0bf84737bd8b2e1e8bf7dd3941d8352.

Fixes: 6546045fa0bf ("core/timer: Always use inactive_exit_timestamp if it is set")

src/core/timer.c
@@ -407,7 +407,8 @@ static void timer_enter_waiting(Timer *t, bool time_change) {
                                         b = trigger->inactive_enter_timestamp.realtime;
                         } else if (dual_timestamp_is_set(&t->last_trigger))
                                 b = t->last_trigger.realtime;
-                        else if (dual_timestamp_is_set(&UNIT(t)->inactive_exit_timestamp))
+                        else if (dual_timestamp_is_set(&UNIT(t)->inactive_exit_timestamp) &&
+                                        state_translation_table[t->state] == UNIT_ACTIVE)
                                 b = UNIT(t)->inactive_exit_timestamp.realtime;
                         else
                                 b = ts.realtime;