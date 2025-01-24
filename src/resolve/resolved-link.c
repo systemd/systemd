systemd/systemd

commit 331888d165b6c4171db4fac318e773248db2218a

resolved: stop mdns/lnrr when config changes to disabled on reload
Currently on reload mdns/lnrr are enabled if they were disabled,
but not viceversa. Fix it.

Follow-up for 14a52176798bc2d013297b503ac6fa49a64e2725

(cherry picked from commit 2976730a4d34749bfb7a88da341464f1834ab1c6)

src/resolve/resolved-link.c
@@ -651,13 +651,16 @@ int link_update(Link *l) {
                 r = manager_llmnr_start(l->manager);
                 if (r < 0)
                         return r;
-        }
+        } else
+                manager_llmnr_stop(l->manager);
+
 
         if (link_get_mdns_support(l) != RESOLVE_SUPPORT_NO) {
                 r = manager_mdns_start(l->manager);
                 if (r < 0)
                         return r;
-        }
+        } else
+                manager_mdns_stop(l->manager);
 
         link_allocate_scopes(l);
         link_add_rrs(l, false);

mkosi.conf
@@ -103,6 +103,7 @@ Packages=
         kmod
         less
         llvm
+        lsof
         lvm2
         man
         mdadm

test/units/TEST-75-RESOLVED.sh
@@ -339,6 +339,7 @@ manual_testcase_02_mdns_llmnr() {
     # defaults to yes (both the global and per-link settings are yes)
     assert_in 'yes' "$(resolvectl mdns hoge)"
     assert_in 'yes' "$(resolvectl llmnr hoge)"
+    lsof -p "$(systemctl show --property MainPID --value systemd-resolved.service)" | grep -q ":mdns\|:5353"
     # set per-link setting
     resolvectl mdns hoge yes
     resolvectl llmnr hoge yes
@@ -379,6 +380,7 @@ manual_testcase_02_mdns_llmnr() {
         echo "LLMNR=no"
     } >/run/systemd/resolved.conf.d/mdns-llmnr.conf
     systemctl reload systemd-resolved.service
+    (! lsof -p "$(systemctl show --property MainPID --value systemd-resolved.service)" | grep -q ":mdns\|:5353")
     # set per-link setting
     resolvectl mdns hoge yes
     resolvectl llmnr hoge yes

commit ccac0eb90e27fad3a70b8dc17b695528e14eba8c

resolved: do not disable mdns/llmnr globally if it's enabled on any link
Fixes a regression caused by 331888d165b6c4171db4fac318e773248db2218a
Fixes #36078.
Fixes #36126.

Co-authored-by: Luca Boccassi <luca.boccassi@gmail.com>

src/resolve/resolved-link.c
@@ -651,16 +651,13 @@ int link_update(Link *l) {
                 r = manager_llmnr_start(l->manager);
                 if (r < 0)
                         return r;
-        } else
-                manager_llmnr_stop(l->manager);
-
+        }
 
         if (link_get_mdns_support(l) != RESOLVE_SUPPORT_NO) {
                 r = manager_mdns_start(l->manager);
                 if (r < 0)
                         return r;
-        } else
-                manager_mdns_stop(l->manager);
+        }
 
         link_allocate_scopes(l);
         link_add_rrs(l, false);

src/resolve/resolved-llmnr.c
@@ -9,8 +9,15 @@
 #include "resolved-llmnr.h"
 #include "resolved-manager.h"
 
-void manager_llmnr_stop(Manager *m) {
+void manager_llmnr_stop(Manager *m, bool force) {
         assert(m);
+
+        if (!force) {
+                Link *l;
+                HASHMAP_FOREACH(l, m->links)
+                        if (link_get_llmnr_support(l) != RESOLVE_SUPPORT_NO)
+                                return;
+        }
 
         m->llmnr_ipv4_udp_event_source = sd_event_source_disable_unref(m->llmnr_ipv4_udp_event_source);
         m->llmnr_ipv4_udp_fd = safe_close(m->llmnr_ipv4_udp_fd);
@@ -64,7 +71,7 @@ int manager_llmnr_start(Manager *m) {
 eaddrinuse:
         log_warning("Another LLMNR responder prohibits binding the socket to the same port. Turning off LLMNR support.");
         m->llmnr_support = RESOLVE_SUPPORT_NO;
-        manager_llmnr_stop(m);
+        manager_llmnr_stop(m, /* force = */ true);
 
         return 0;
 }

src/resolve/resolved-llmnr.h
@@ -10,5 +10,5 @@ int manager_llmnr_ipv6_udp_fd(Manager *m);
 int manager_llmnr_ipv4_tcp_fd(Manager *m);
 int manager_llmnr_ipv6_tcp_fd(Manager *m);
 
-void manager_llmnr_stop(Manager *m);
+void manager_llmnr_stop(Manager *m, bool force);
 int manager_llmnr_start(Manager *m);

src/resolve/resolved-manager.c
@@ -105,6 +105,9 @@ static int manager_process_link(sd_netlink *rtnl, sd_netlink_message *mm, void *
                 break;
         }
 
+        /* Now check all the links, and if mDNS/llmr are disabled everywhere, stop them globally too. */
+        manager_llmnr_stop(m, /* force = */ false);
+        manager_mdns_stop(m, /* force = */ false);
         return 0;
 
 fail:
@@ -286,6 +289,10 @@ static int on_network_event(sd_event_source *s, int fd, uint32_t revents, void *
 
         (void) manager_write_resolv_conf(m);
         (void) manager_send_changed(m, "DNS");
+
+        /* Now check all the links, and if mDNS/llmr are disabled everywhere, stop them globally too. */
+        manager_llmnr_stop(m, /* force = */ false);
+        manager_mdns_stop(m, /* force = */ false);
 
         return 0;
 }
@@ -806,8 +813,8 @@ Manager *manager_free(Manager *m) {
         sd_event_source_unref(m->rtnl_event_source);
         sd_event_source_unref(m->clock_change_event_source);
 
-        manager_llmnr_stop(m);
-        manager_mdns_stop(m);
+        manager_llmnr_stop(m, /* force = */ true);
+        manager_mdns_stop(m, /* force = */ true);
         manager_dns_stub_stop(m);
         manager_varlink_done(m);
 

src/resolve/resolved-mdns.c
@@ -12,8 +12,15 @@
 
 #define CLEAR_CACHE_FLUSH(x) (~MDNS_RR_CACHE_FLUSH_OR_QU & (x))
 
-void manager_mdns_stop(Manager *m) {
+void manager_mdns_stop(Manager *m, bool force) {
         assert(m);
+
+        if (!force) {
+                Link *l;
+                HASHMAP_FOREACH(l, m->links)
+                        if (link_get_mdns_support(l) != RESOLVE_SUPPORT_NO)
+                                return;
+        }
 
         m->mdns_ipv4_event_source = sd_event_source_disable_unref(m->mdns_ipv4_event_source);
         m->mdns_ipv4_fd = safe_close(m->mdns_ipv4_fd);
@@ -49,7 +56,7 @@ int manager_mdns_start(Manager *m) {
 eaddrinuse:
         log_warning("Another mDNS responder prohibits binding the socket to the same port. Turning off mDNS support.");
         m->mdns_support = RESOLVE_SUPPORT_NO;
-        manager_mdns_stop(m);
+        manager_mdns_stop(m, /* force = */ true);
 
         return 0;
 }

src/resolve/resolved-mdns.h
@@ -9,5 +9,5 @@
 int manager_mdns_ipv4_fd(Manager *m);
 int manager_mdns_ipv6_fd(Manager *m);
 
-void manager_mdns_stop(Manager *m);
+void manager_mdns_stop(Manager *m, bool force);
 int manager_mdns_start(Manager *m);