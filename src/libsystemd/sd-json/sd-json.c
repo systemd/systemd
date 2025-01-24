9d3aae2a80419c326e4c56897cf7dd7d8285220f
Yu Watanabe <watanabe.yu+github@gmail.com>
2025-01-24 22:53:13 +0900

    sd-json: silence false positive warning by coverity
    An object json is a set of key and value pair, hence has its number of
    element is multiple of two, and we know at least two elements there.
    
    Follow-up for b6a2df630701de0bcf77850ced213d7fc3d0c4de.
    Fixes CID#1590724

--- src/libsystemd/sd-json/sd-json.c
+++ src/libsystemd/sd-json/sd-json.c
@@ -2151,7 +2151,9 @@ _public_ int sd_json_variant_unset_field(sd_json_variant **v, const char *field)
 
                 if (streq(sd_json_variant_string(p), field)) {
                         if (!array) {
-                                array = new(sd_json_variant*, sd_json_variant_elements(*v) - 2);
+                                size_t n = sd_json_variant_elements(*v);
+                                assert(n >= 2);
+                                array = new(sd_json_variant*, n - 2);
                                 if (!array)
                                         return -ENOMEM;
 
