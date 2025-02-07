eacf69b02a2d33d573f5be3e3c7da01d3ab5d1db
Lennart Poettering <lennart@poettering.net>
2025-02-07 23:30:13 +0100

    sd-stub: if we are http booted, query source URL and write to EFI variable
    This way userspace can read the field, and use it to retrieve more
    resources from the same place.

--- src/fundamental/efivars-fundamental.h
+++ src/fundamental/efivars-fundamental.h
@@ -24,6 +24,7 @@
 #define EFI_LOADER_FEATURE_RETAIN_SHIM             (UINT64_C(1) << 12)
 #define EFI_LOADER_FEATURE_MENU_DISABLE            (UINT64_C(1) << 13)
 #define EFI_LOADER_FEATURE_MULTI_PROFILE_UKI       (UINT64_C(1) << 14)
+#define EFI_LOADER_FEATURE_REPORT_URL              (UINT64_C(1) << 15)
 
 /* Features of the stub, i.e. systemd-stub */
 #define EFI_STUB_FEATURE_REPORT_BOOT_PARTITION     (UINT64_C(1) << 0)
@@ -37,6 +38,7 @@
 #define EFI_STUB_FEATURE_PICK_UP_CONFEXTS          (UINT64_C(1) << 8)
 #define EFI_STUB_FEATURE_MULTI_PROFILE_UKI         (UINT64_C(1) << 9)
 #define EFI_STUB_FEATURE_REPORT_STUB_PARTITION     (UINT64_C(1) << 10)
+#define EFI_STUB_FEATURE_REPORT_URL                (UINT64_C(1) << 11)
 
 typedef enum SecureBootMode {
         SECURE_BOOT_UNSUPPORTED,
