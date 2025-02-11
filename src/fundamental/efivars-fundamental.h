9e8a0d2b471be7e5f8fd70fe98043d7d1b0caf82
Lennart Poettering <lennart@poettering.net>
2025-02-11 09:18:14 +0100

    boot: add new bls type #1 stanza "uki"
    This one is between "efi" and "linux": we'll recognize such entries as
    linux, but we'll just invoke them as EFI binaries.
    
    This creates a high-level concept for invoking UKIs via indirection of a
    bls type #1 entry, for example to permit invocation from a non-standard
    path or for giving entries a different name.

--- src/fundamental/efivars-fundamental.h
+++ src/fundamental/efivars-fundamental.h
@@ -25,6 +25,7 @@
 #define EFI_LOADER_FEATURE_MENU_DISABLE            (UINT64_C(1) << 13)
 #define EFI_LOADER_FEATURE_MULTI_PROFILE_UKI       (UINT64_C(1) << 14)
 #define EFI_LOADER_FEATURE_REPORT_URL              (UINT64_C(1) << 15)
+#define EFI_LOADER_FEATURE_TYPE1_UKI               (UINT64_C(1) << 16)
 
 /* Features of the stub, i.e. systemd-stub */
 #define EFI_STUB_FEATURE_REPORT_BOOT_PARTITION     (UINT64_C(1) << 0)
