6c49c08c9ce60e2edad2645d64b18123a61d1534
Riccardo Venturini <80196285+ctrlVnt@users.noreply.github.com>
2025-02-11 11:00:37 +0100

    Sobstitute printf to log_warning
    I know, there is no printf on this code, could be weird my proposal

--- src/cryptenroll/cryptenroll-wipe.c
+++ src/cryptenroll/cryptenroll-wipe.c
@@ -355,10 +355,10 @@ int wipe_slots(struct crypt_device *cd,
         case WIPE_EXPLICIT:
                 break; /* Nothing to do here */
 
-        case WIPE_ALL:                
+        case WIPE_ALL:
                 r = find_all_slots(cd, wipe_slots, keep_slots);
                 if (r < 0){
-                        log_warning("You have deleted ALL slots, make sure to register new keys before rebooting");
+                        printf("You have deleted ALL slots, make sure to register new keys before rebooting");
                         return r;
                 }
                 break;
