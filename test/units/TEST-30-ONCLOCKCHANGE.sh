72839f16fb028082332ae1a4d7a5e5e3f11cfa08
Pavithra Barithaya <pavithrabarithaya07@gmail.com>
2025-02-13 12:28:40 +0530

    timedated: Send error when time set is past build date time
    When the user/customer sets the time on the system which is prior
    than that of the systemd build time, as systemd doesn't allow time
    before it's build date after a reboot, systemd is resetting it but
    there is no error or exception present in the setTime method due
    to which user/customer is unaware of why the time is reset back to
    the systemd-build time.
    
    Added a condition check in the set_time() method to return an
    error when tried to set time past the systemd build date.
    
    Tested: Verified that it throws an error when we try to set the
    time prior to systemd build date.
    
    Change-Id: I00b53da75abcfe54c4c1314f0842a537d5c23634
    Signed-off-by: Pavithra Barithaya <pavithrabarithaya07@gmail.com>

--- test/units/TEST-30-ONCLOCKCHANGE.sh
+++ test/units/TEST-30-ONCLOCKCHANGE.sh
@@ -8,7 +8,13 @@ systemd-analyze log-level debug
 systemctl disable --now systemd-timesyncd.service
 
 timedatectl set-timezone Europe/Berlin
-timedatectl set-time 1980-10-15
+
+# Get current time and add a future offset (e.g., 1 year ahead)
+current_time=$(date)
+future_time_berlin=$(date -d "$current_time + 1 year" +"%Y-%m-%d %H:%M:%S")
+
+# Set system time to the calculated future time for Europe/Berlin
+timedatectl set-time "$future_time_berlin"
 
 systemd-run --on-timezone-change touch /tmp/timezone-changed
 systemd-run --on-clock-change touch /tmp/clock-changed
@@ -20,7 +26,14 @@ timedatectl set-timezone Europe/Kyiv
 
 while test ! -f /tmp/timezone-changed ; do sleep .5 ; done
 
-timedatectl set-time 2018-1-1
+# Get current time and add a future offset (e.g., 1 year and 1 month ahead)
+# Using date to calculate the future timestamp
+
+current_time=$(date)
+future_time=$(date -d "$current_time + 1 year + 1 month" +"%Y-%m-%d %H:%M:%S")
+
+# Set system time to the calculated future time
+timedatectl set-time "$future_time"
 
 while test ! -f /tmp/clock-changed ; do sleep .5 ; done
 
