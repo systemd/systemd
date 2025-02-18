systemd/systemd

commit cfaf78001c3451d549bcb1ee4adca3e85b934e56

journal-remote: added compression, compression-level and content-encoding negotiation

src/journal-remote/journal-remote-main.c
@@ -11,6 +11,7 @@
 #include "daemon-util.h"
 #include "fd-util.h"
 #include "fileio.h"
+#include "journal-compression-util.h"
 #include "journal-remote-write.h"
 #include "journal-remote.h"
 #include "logs-show.h"
@@ -37,6 +38,7 @@ static const char *arg_getter = NULL;
 static const char *arg_listen_raw = NULL;
 static const char *arg_listen_http = NULL;
 static const char *arg_listen_https = NULL;
+static CompressionArgs arg_compression = {};
 static char **arg_files = NULL; /* Do not free this. */
 static bool arg_compress = true;
 static bool arg_seal = false;
@@ -65,6 +67,7 @@ STATIC_DESTRUCTOR_REGISTER(arg_key, freep);
 STATIC_DESTRUCTOR_REGISTER(arg_cert, freep);
 STATIC_DESTRUCTOR_REGISTER(arg_trust, freep);
 STATIC_DESTRUCTOR_REGISTER(arg_output, freep);
+STATIC_DESTRUCTOR_REGISTER(arg_compression, compression_args_clear);
 
 static const char* const journal_write_split_mode_table[_JOURNAL_WRITE_SPLIT_MAX] = {
         [JOURNAL_WRITE_SPLIT_NONE] = "none",
@@ -152,6 +155,22 @@ static int dispatch_http_event(sd_event_source *event,
                                uint32_t revents,
                                void *userdata);
 
+static int build_accept_encoding(char **ret) {
+        assert(ret);
+
+        float q = 1.0, step = 1.0 / arg_compression.size;
+        _cleanup_free_ char *buf = NULL;
+        FOREACH_ARRAY(opt, arg_compression.opts, arg_compression.size) {
+                const char *c = compression_lowercase_to_string(opt->algorithm);
+                if (strextendf_with_separator(&buf, ",", "%s;q=%.1f", c, q) < 0)
+                        return -ENOMEM;
+                q -= step;
+        }
+
+        *ret = TAKE_PTR(buf);
+        return 0;
+}
+
 static int request_meta(void **connection_cls, int fd, char *hostname) {
         RemoteSource *source;
         Writer *writer;
@@ -174,6 +193,11 @@ static int request_meta(void **connection_cls, int fd, char *hostname) {
 
         log_debug("Added RemoteSource as connection metadata %p", source);
 
+        r = build_accept_encoding(&source->encoding);
+        if (r < 0)
+                return log_oom();
+
+        source->compression = COMPRESSION_NONE;
         *connection_cls = source;
         return 0;
 }
@@ -212,8 +236,17 @@ static int process_http_upload(
         if (*upload_data_size) {
                 log_trace("Received %zu bytes", *upload_data_size);
 
-                r = journal_importer_push_data(&source->importer,
-                                               upload_data, *upload_data_size);
+                if (source->compression != COMPRESSION_NONE) {
+                        _cleanup_free_ char *buf = NULL;
+                        size_t buf_size;
+
+                        r = decompress_blob(source->compression, upload_data, *upload_data_size, (void **) &buf, &buf_size, 0);
+                        if (r < 0)
+                                return mhd_respondf(connection, r, MHD_HTTP_BAD_REQUEST, "Decompression of received blob falied.");
+
+                        r = journal_importer_push_data(&source->importer, buf, buf_size);
+                } else
+                        r = journal_importer_push_data(&source->importer, upload_data, *upload_data_size);
                 if (r < 0)
                         return mhd_respond_oom(connection);
 
@@ -253,7 +286,7 @@ static int process_http_upload(
                                     remaining);
         }
 
-        return mhd_respond(connection, MHD_HTTP_ACCEPTED, "OK.");
+        return mhd_respond_with_encoding(connection, MHD_HTTP_ACCEPTED, source->encoding, "OK.");
 };
 
 static mhd_result request_handler(
@@ -278,10 +311,20 @@ static mhd_result request_handler(
 
         log_trace("Handling a connection %s %s %s", method, url, version);
 
-        if (*connection_cls)
+        if (*connection_cls) {
+                RemoteSource *source = *connection_cls;
+                header = MHD_lookup_connection_value(connection, MHD_HEADER_KIND, "Content-Encoding");
+                if (header) {
+                        Compression c = compression_lowercase_from_string(header);
+                        if (c < 0 || !compression_supported(c))
+                                return mhd_respondf(connection, 0, MHD_HTTP_UNSUPPORTED_MEDIA_TYPE,
+                                                    "Unsupported Content-Encoding type: %s", header);
+                        source->compression = c;
+                }
                 return process_http_upload(connection,
                                            upload_data, upload_data_size,
-                                           *connection_cls);
+                                           source);
+        }
 
         if (!streq(method, "POST"))
                 return mhd_respond(connection, MHD_HTTP_NOT_ACCEPTABLE, "Unsupported method.");
@@ -722,6 +765,7 @@ static int parse_config(void) {
                 { "Remote",  "MaxFileSize",            config_parse_iec_uint64,       0, &arg_max_size    },
                 { "Remote",  "MaxFiles",               config_parse_uint64,           0, &arg_n_max_files },
                 { "Remote",  "KeepFree",               config_parse_iec_uint64,       0, &arg_keep_free   },
+                { "Remote",  "Compression",            config_parse_compression,      0, &arg_compression },
                 {}
         };
 

man/journal-remote.conf.xml
@@ -59,6 +59,19 @@
 
     <variablelist class='config-directives'>
       <varlistentry>
+        <term><varname>Compression=</varname></term>
+
+        <listitem><para>Acceptable compression algorithms to be used by <command>systemd-journal-upload</command>. Compression algorithms are
+        used for <literal>Accept-Encoding</literal> header contruction with priorities set according to an order in configuration.
+        This parameter takes space separated list of compression algorithms. Example:
+        <programlisting>Compression=zstd lz4</programlisting>
+        This option can be specified multiple times. If an empty string is assigned, then all the previous assignments are cleared.
+        </para>
+
+        <xi:include href="version-info.xml" xpointer="v258"/></listitem>
+      </varlistentry>
+
+      <varlistentry>
         <term><varname>Seal=</varname></term>
 
         <listitem><para>Periodically sign the data in the journal using Forward Secure Sealing.

man/journal-upload.conf.xml
@@ -61,6 +61,37 @@
       </varlistentry>
 
       <varlistentry>
+        <term><varname>Compression=</varname></term>
+
+        <listitem><para>Takes a space separated list of compression algorithms to be applied to logs data before sending.
+        Supported algorithms are <literal>none</literal>, <literal>zstd</literal>, <literal>xz</literal>,
+        or <literal>lz4</literal>. Optionally, each algorithm (except for <literal>none</literal>)
+        followed by a colon (<literal>:</literal>) and its compression level, for example <literal>zstd:4</literal>.
+        The compression level is expected to be a positive integer. This option can be specified multiple times.
+        If an empty string is assigned, then all previous assignments are cleared.
+        Defaults to unset, and data will not be compressed.</para>
+
+        <para>Example:
+        <programlisting>Compression=zstd:4 lz4:2</programlisting></para>
+
+        <para>Even when compression is enabled, the initial requests are sent without compression.
+        It becomes effective either if <literal>ForceCompression=</literal> is enabled,
+        or the server response contains <literal>Accept-Encoding</literal> headers with a list of
+        compression algorithms that contains one of the algorithms specified in this option.</para>
+
+        <xi:include href="version-info.xml" xpointer="v258"/></listitem>
+      </varlistentry>
+
+      <varlistentry>
+        <term><varname>ForceCompression=</varname></term>
+
+        <listitem><para>Takes a boolean value, enforces using compression without content encoding negotiation.
+        Defaults to <literal>false</literal>.</para>
+
+        <xi:include href="version-info.xml" xpointer="v258"/></listitem>
+      </varlistentry>
+
+      <varlistentry>
         <term><varname>ServerKeyFile=</varname></term>
 
         <listitem><para>SSL key in PEM format.</para>

src/basic/compress.c
@@ -122,7 +122,15 @@ static const char* const compression_table[_COMPRESSION_MAX] = {
         [COMPRESSION_ZSTD] = "ZSTD",
 };
 
+static const char* const compression_lowercase_table[_COMPRESSION_MAX] = {
+        [COMPRESSION_NONE] = "none",
+        [COMPRESSION_XZ]   = "xz",
+        [COMPRESSION_LZ4]  = "lz4",
+        [COMPRESSION_ZSTD] = "zstd",
+};
+
 DEFINE_STRING_TABLE_LOOKUP(compression, Compression);
+DEFINE_STRING_TABLE_LOOKUP(compression_lowercase, Compression);
 
 bool compression_supported(Compression c) {
         static const unsigned supported =

src/basic/compress.h
@@ -24,6 +24,8 @@ typedef enum Compression {
 
 const char* compression_to_string(Compression compression);
 Compression compression_from_string(const char *compression);
+const char* compression_lowercase_to_string(Compression compression);
+Compression compression_lowercase_from_string(const char *compression);
 
 bool compression_supported(Compression c);
 

src/journal-remote/journal-compression-util.c
@@ -1,0 +1,91 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+
+#include "extract-word.h"
+#include "journal-compression-util.h"
+#include "parse-util.h"
+
+void compression_args_clear(CompressionArgs *args) {
+        assert(args);
+        args->size = 0;
+        args->opts = mfree(args->opts);
+}
+
+int config_parse_compression(
+                const char *unit,
+                const char *filename,
+                unsigned line,
+                const char *section,
+                unsigned section_line,
+                const char *lvalue,
+                int ltype,
+                const char *rvalue,
+                void *data,
+                void *userdata) {
+
+        CompressionArgs *args = ASSERT_PTR(data);
+        bool parse_level = ltype;
+        int r;
+
+        assert(filename);
+        assert(lvalue);
+        assert(rvalue);
+
+        if (isempty(rvalue)) {
+                compression_args_clear(args);
+                return 1;
+        }
+
+        for (const char *p = rvalue;;) {
+                _cleanup_free_ char *algorithm = NULL, *word = NULL;
+                int level = -1;
+
+                r = extract_first_word(&p, &word, NULL, 0);
+                if (r < 0)
+                        return log_syntax_parse_error(unit, filename, line, r, lvalue, rvalue);
+                if (r == 0)
+                        return 1;
+
+                if (parse_level) {
+                        const char *q = word;
+                        r = extract_first_word(&q, &algorithm, ":", 0);
+                        if (r < 0)
+                                return log_syntax_parse_error(unit, filename, line, r, lvalue, rvalue);
+                        if (!isempty(q)) {
+                                r = safe_atoi(q, &level);
+                                if (r < 0) {
+                                        log_syntax(unit, LOG_WARNING, filename, line, r,
+                                                   "Compression level %s should be positive, ignoring.", q);
+                                        continue;
+                                }
+                        }
+                } else
+                        algorithm = TAKE_PTR(word);
+
+                Compression c = compression_lowercase_from_string(algorithm);
+                if (c < 0 || !compression_supported(c)) {
+                        log_syntax(unit, LOG_WARNING, filename, line, c,
+                                   "Compression=%s is not supported on a system, ignoring.", algorithm);
+                        continue;
+                }
+
+                bool found = false;
+                FOREACH_ARRAY(opt, args->opts, args->size)
+                        if (opt->algorithm == c) {
+                                found = true;
+                                if (parse_level)
+                                        opt->level = level;
+                                break;
+                        }
+
+                if (found)
+                        continue;
+
+                if (!GREEDY_REALLOC(args->opts, args->size + 1))
+                        return log_oom();
+
+                args->opts[args->size++] = (CompressionOpts) {
+                        .algorithm = c,
+                        .level = level,
+                };
+        }
+}

src/journal-remote/journal-compression-util.h
@@ -1,0 +1,19 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#pragma once
+
+#include "compress.h"
+#include "conf-parser.h"
+
+typedef struct CompressionOpts {
+        Compression algorithm;
+        int level;
+} CompressionOpts;
+
+typedef struct CompressionArgs {
+        CompressionOpts *opts;
+        size_t size;
+} CompressionArgs;
+
+CONFIG_PARSER_PROTOTYPE(config_parse_compression);
+
+void compression_args_clear(CompressionArgs *args);

src/journal-remote/journal-remote-parse.c
@@ -18,6 +18,7 @@ void source_free(RemoteSource *source) {
         sd_event_source_unref(source->event);
         sd_event_source_unref(source->buffer_event);
 
+        free(source->encoding);
         free(source);
 }
 

commit 54258df8296b28ea889cc789954b62c96d97dc36

journal-remote: fix memleak
Fixes a bug introduced by cfaf78001c3451d549bcb1ee4adca3e85b934e56.
Fixes CID#1591182.

src/journal-remote/journal-remote-main.c
@@ -186,24 +186,22 @@ static int build_accept_encoding(char **ret) {
 }
 
 static int request_meta(void **connection_cls, int fd, char *hostname) {
-        RemoteSource *source;
-        Writer *writer;
         int r;
 
         assert(connection_cls);
-        if (*connection_cls)
-                return 0;
 
+        if (*connection_cls)
+                return 0; /* already assigned. */
+
+        Writer *writer;
         r = journal_remote_get_writer(journal_remote_server_global, hostname, &writer);
         if (r < 0)
                 return log_warning_errno(r, "Failed to get writer for source %s: %m",
                                          hostname);
 
-        source = source_new(fd, true, hostname, writer);
-        if (!source) {
-                writer_unref(writer);
+        _cleanup_(source_freep) RemoteSource *source = source_new(fd, true, hostname, writer);
+        if (!source)
                 return log_oom();
-        }
 
         log_debug("Added RemoteSource as connection metadata %p", source);
 
@@ -212,7 +210,7 @@ static int request_meta(void **connection_cls, int fd, char *hostname) {
                 return log_oom();
 
         source->compression = COMPRESSION_NONE;
-        *connection_cls = source;
+        *connection_cls = TAKE_PTR(source);
         return 0;
 }
 