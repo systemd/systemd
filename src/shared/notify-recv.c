220d062002cedd714011a34c24578c3558ac160e
Mike Yuan <me@yhndnzj.com>
2025-02-21 15:16:46 +0100

    notify-recv: several followups
    Follow-up for 7f6af95dab037e7d15591a924dbf256460bbf069
    
    - Allocate internal buf on the stack, strdup() only at the end.
      This ensures we're able to handle OOM gracefully, i.e.
      return -EAGAIN on OOM while still emptying socket buffer.
    - Insist on sender being alive when we process the message
      (always go through pidfd_open()/pidfd_get_pid() path).
      pid1's notify receiver enforces this, and with notify event
      priority higher than sigchld it shouldn't create extra churn.
    - Do not treat empty notify message as error
    - Raise log level since all callers log loudly anyway.

--- src/shared/notify-recv.c
+++ src/shared/notify-recv.c
@@ -4,11 +4,20 @@
 #include "notify-recv.h"
 #include "socket-util.h"
 
-int notify_recv(int fd, char **ret_text, struct ucred *ret_ucred, PidRef *ret_pidref) {
+int notify_recv(
+                int fd,
+                char **ret_text,
+                struct ucred *ret_ucred,
+                PidRef *ret_pidref) {
+
+        char buf[NOTIFY_BUFFER_MAX+1];
+        struct iovec iovec = {
+                .iov_base = buf,
+                .iov_len = sizeof(buf)-1,
+        };
         CMSG_BUFFER_TYPE(CMSG_SPACE(sizeof(struct ucred)) +
                          CMSG_SPACE(sizeof(int)) + /* SCM_PIDFD */
                          CMSG_SPACE(sizeof(int) * NOTIFY_FD_MAX)) control;
-        struct iovec iovec;
         struct msghdr msghdr = {
                 .msg_iov = &iovec,
                 .msg_iovlen = 1,
@@ -16,38 +25,32 @@ int notify_recv(int fd, char **ret_text, struct ucred *ret_ucred, PidRef *ret_pi
                 .msg_controllen = sizeof(control),
         };
         ssize_t n;
+        int r;
 
         assert(fd >= 0);
 
-        /* Receives a $NOTIFY_SOCKET message (aka sd_notify()). Does various validations. Returns -EAGAIN in
-         * case an invalid message is received (following the logic that an invalid message shall be ignored,
-         * and treated like no message at all). */
-
-        _cleanup_free_ char *buf = new(char, NOTIFY_BUFFER_MAX+1);
-        if (!buf)
-                return log_oom_debug();
-
-        iovec = (struct iovec) {
-                .iov_base = buf,
-                .iov_len = NOTIFY_BUFFER_MAX,
-        };
+        /* Receives a $NOTIFY_SOCKET message (aka sd_notify()). Does various validations.
+         *
+         * Returns -EAGAIN on recoverable errors (e.g. in case an invalid message is received, following
+         * the logic that an invalid message shall be ignored, and treated like no message at all). */
 
         n = recvmsg_safe(fd, &msghdr, MSG_DONTWAIT|MSG_CMSG_CLOEXEC);
         if (ERRNO_IS_NEG_TRANSIENT(n))
                 return -EAGAIN;
         if (n == -ECHRNG) {
-                log_debug_errno(n, "Got message with truncated control data (unexpected fds sent?), ignoring.");
+                log_warning_errno(n, "Got message with truncated control data (unexpected fds sent?), ignoring.");
                 return -EAGAIN;
         }
         if (n == -EXFULL) {
-                log_debug_errno(n, "Got message with truncated payload data, ignoring.");
+                log_warning_errno(n, "Got message with truncated payload data, ignoring.");
                 return -EAGAIN;
         }
         if (n < 0)
-                return (int) n;
+                return log_error_errno(n, "Failed to receive notification message: %m");
 
         const struct ucred *ucred = NULL;
         _cleanup_close_ int pidfd = -EBADF;
+
         struct cmsghdr *cmsg;
         CMSG_FOREACH(cmsg, &msghdr) {
                 if (cmsg->cmsg_level != SOL_SOCKET)
@@ -63,44 +66,68 @@ int notify_recv(int fd, char **ret_text, struct ucred *ret_ucred, PidRef *ret_pi
 
                 case SCM_PIDFD:
                         assert(cmsg->cmsg_len == CMSG_LEN(sizeof(int)));
+                        assert(pidfd < 0);
+
                         pidfd = *CMSG_TYPED_DATA(cmsg, int);
                         break;
 
                 case SCM_CREDENTIALS:
                         assert(cmsg->cmsg_len == CMSG_LEN(sizeof(struct ucred)));
+                        assert(!ucred);
+
                         ucred = CMSG_TYPED_DATA(cmsg, struct ucred);
                         break;
                 }
         }
 
-        if ((ret_ucred || ret_pidref) && (!ucred || ucred->pid <= 0))
-                return log_debug_errno(SYNTHETIC_ERRNO(EAGAIN), "Got notification datagram lacking valid credential information, ignoring.");
+        _cleanup_(pidref_done) PidRef pidref = PIDREF_NULL;
 
-        if (n == 0)
-                return log_debug_errno(SYNTHETIC_ERRNO(EAGAIN), "Got empty notification message, ignoring.");
-        if (memchr(buf, 0, n - 1))
-                return log_debug_errno(SYNTHETIC_ERRNO(EAGAIN), "Got notification message with embedded NUL, ignoring.");
-
-        if (ret_pidref) {
-                assert(ucred);
-                assert(ucred->pid > 0);
+        if (ret_ucred || ret_pidref) {
+                if (!ucred || !pid_is_valid(ucred->pid))
+                        return log_warning_errno(SYNTHETIC_ERRNO(EAGAIN),
+                                                 "Got notification datagram lacking valid credential information, ignoring.");
 
                 if (pidfd >= 0)
-                        *ret_pidref = (PidRef) {
-                                .pid = ucred->pid,
-                                .fd = TAKE_FD(pidfd),
-                        };
+                        r = pidref_set_pidfd_consume(&pidref, TAKE_FD(pidfd));
                 else
-                        *ret_pidref = PIDREF_MAKE_FROM_PID(ucred->pid);
+                        r = pidref_set_pid(&pidref, ucred->pid);
+                if (r < 0) {
+                        if (r == -ESRCH)
+                                log_debug_errno(r, "Notify sender died before message is processed. Ignoring.");
+                        else
+                                log_warning_errno(r, "Failed to pin notify sender, ignoring message: %m");
+                        return -EAGAIN;
+                }
+
+                if (_unlikely_(pidref.pid != ucred->pid)) {
+                        assert(pidref.fd >= 0);
+
+                        return log_warning_errno(SYNTHETIC_ERRNO(EAGAIN),
+                                                 "Got SCM_PIDFD for process " PID_FMT " but SCM_CREDENTIALS for process " PID_FMT ". Ignoring.",
+                                                 pidref.pid, ucred->pid);
+                }
         }
 
+        /* As extra safety check, let's make sure the string we get doesn't contain embedded NUL bytes.
+         * We permit one trailing NUL byte in the message, but don't expect it. */
+        if (n > 1 && memchr(buf, 0, n - 1))
+                return log_warning_errno(SYNTHETIC_ERRNO(EAGAIN), "Got notification message with embedded NUL, ignoring.");
+
         if (ret_text) {
-                buf[n] = 0;
-                *ret_text = TAKE_PTR(buf);
+                char *s = memdup_suffix0(buf, n);
+                if (!s) {
+                        log_oom_warning();
+                        return -EAGAIN;
+                }
+
+                *ret_text = s;
         }
 
         if (ret_ucred)
                 *ret_ucred = *ucred;
+
+        if (ret_pidref)
+                *ret_pidref = TAKE_PIDREF(pidref);
 
         return 0;
 }
