ec75a2818d2d3642deb36e4c63850ddb835cfcdd
Yu Watanabe <watanabe.yu+github@gmail.com>
2025-03-01 04:47:31 +0900

    missing_socket: drop unnecessary definitions
    All removed definitions are in glibc-2.31

--- src/basic/missing_socket.h
+++ src/basic/missing_socket.h
@@ -3,14 +3,6 @@
 
 #include <sys/socket.h>
 
-#ifndef AF_VSOCK
-#define AF_VSOCK 40
-#endif
-
-#ifndef SO_REUSEPORT
-#define SO_REUSEPORT 15
-#endif
-
 #ifndef SO_PEERGROUPS
 #define SO_PEERGROUPS 59
 #endif
@@ -27,51 +19,24 @@
 #define SO_BINDTOIFINDEX 62
 #endif
 
-#ifndef SOL_NETLINK
-#define SOL_NETLINK 270
-#endif
-
-#ifndef SOL_ALG
-#define SOL_ALG 279
-#endif
-
 /* Not exposed yet. Defined in include/linux/socket.h. */
 #ifndef SOL_SCTP
 #define SOL_SCTP 132
 #endif
 
+/* since glibc-2.39 */
 #ifndef SCM_SECURITY
 #define SCM_SECURITY 0x03
 #endif
 
+/* since glibc-2.39 */
 #ifndef SCM_PIDFD
 #define SCM_PIDFD 0x04
 #endif
 
-/* netinet/in.h */
-#ifndef IP_FREEBIND
-#define IP_FREEBIND 15
-#endif
-
-#ifndef IP_TRANSPARENT
-#define IP_TRANSPARENT 19
-#endif
-
-#ifndef IPV6_FREEBIND
-#define IPV6_FREEBIND 78
-#endif
-
-#ifndef IP_RECVFRAGSIZE
-#define IP_RECVFRAGSIZE 25
-#endif
-
-#ifndef IPV6_RECVFRAGSIZE
-#define IPV6_RECVFRAGSIZE 77
-#endif
-
-/* The maximum number of fds that SCM_RIGHTS accepts. This is an internal kernel constant, but very much
- * useful for userspace too. It's documented in unix(7) these days, hence should be fairly reliable to define
- * here. */
+/* The maximum number of fds that SCM_RIGHTS accepts. This is an internal kernel constant defined in
+ * include/net/scm.h, but very much useful for userspace too. It's documented in unix(7) these days, hence
+ * should be fairly reliable to define here. */
 #ifndef SCM_MAX_FD
 #define SCM_MAX_FD 253U
 #endif
