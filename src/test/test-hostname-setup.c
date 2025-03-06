6f3c3017681fa1cb099f18088699c4c325d9f072
Lennart Poettering <lennart@poettering.net>
2025-03-06 18:31:12 +0100

    hostname: support that /etc/hostname contains ??? as wildcards to be replaced by hash value from /etc/machine-id

--- src/test/test-hostname-setup.c
+++ src/test/test-hostname-setup.c
@@ -3,9 +3,11 @@
 #include <unistd.h>
 
 #include "alloc-util.h"
+#include "fd-util.h"
 #include "fileio.h"
 #include "fs-util.h"
 #include "hostname-setup.h"
+#include "hostname-util.h"
 #include "string-util.h"
 #include "tests.h"
 #include "tmpfile-util.h"
@@ -13,48 +15,63 @@
 TEST(read_etc_hostname) {
         _cleanup_(unlink_tempfilep) char path[] = "/tmp/hostname.XXXXXX";
         char *hostname;
-        int fd;
+        int r;
 
-        fd = mkostemp_safe(path);
-        assert_se(fd > 0);
-        close(fd);
+        safe_close(ASSERT_FD(mkostemp_safe(path)));
 
         /* simple hostname */
-        assert_se(write_string_file(path, "foo", WRITE_STRING_FILE_CREATE) == 0);
-        assert_se(read_etc_hostname(path, &hostname) == 0);
+        ASSERT_OK(write_string_file(path, "foo", WRITE_STRING_FILE_CREATE));
+        ASSERT_OK(read_etc_hostname(path, /* substitute_wildcards= */ false, &hostname));
         ASSERT_STREQ(hostname, "foo");
         hostname = mfree(hostname);
 
         /* with comment */
-        assert_se(write_string_file(path, "# comment\nfoo", WRITE_STRING_FILE_CREATE) == 0);
-        assert_se(read_etc_hostname(path, &hostname) == 0);
-        assert_se(hostname);
+        ASSERT_OK(write_string_file(path, "# comment\nfoo", WRITE_STRING_FILE_CREATE));
+        ASSERT_OK(read_etc_hostname(path, /* substitute_wildcards= */ false, &hostname));
+        ASSERT_NOT_NULL(hostname);
         ASSERT_STREQ(hostname, "foo");
         hostname = mfree(hostname);
 
         /* with comment and extra whitespace */
-        assert_se(write_string_file(path, "# comment\n\n foo ", WRITE_STRING_FILE_CREATE) == 0);
-        assert_se(read_etc_hostname(path, &hostname) == 0);
-        assert_se(hostname);
+        ASSERT_OK(write_string_file(path, "# comment\n\n foo ", WRITE_STRING_FILE_CREATE));
+        ASSERT_OK(read_etc_hostname(path, /* substitute_wildcards= */ false, &hostname));
+        ASSERT_NOT_NULL(hostname);
         ASSERT_STREQ(hostname, "foo");
         hostname = mfree(hostname);
 
         /* cleans up name */
-        assert_se(write_string_file(path, "!foo/bar.com", WRITE_STRING_FILE_CREATE) == 0);
-        assert_se(read_etc_hostname(path, &hostname) == 0);
-        assert_se(hostname);
+        ASSERT_OK(write_string_file(path, "!foo/bar.com", WRITE_STRING_FILE_CREATE));
+        ASSERT_OK(read_etc_hostname(path, /* substitute_wildcards= */ false, &hostname));
+        ASSERT_NOT_NULL(hostname);
         ASSERT_STREQ(hostname, "foobar.com");
         hostname = mfree(hostname);
 
+        /* with wildcards */
+        ASSERT_OK(write_string_file(path, "foo????????x??????????u", WRITE_STRING_FILE_CREATE));
+        ASSERT_OK(read_etc_hostname(path, /* substitute_wildcards= */ false, &hostname));
+        ASSERT_NOT_NULL(hostname);
+        ASSERT_STREQ(hostname, "foo????????x??????????u");
+        hostname = mfree(hostname);
+        r = read_etc_hostname(path, /* substitute_wildcards= */ true, &hostname);
+        if (r == -ENOPKG)
+                log_tests_skipped("skipping wildcard hostname tests, no machine ID defined");
+        else {
+                ASSERT_OK(r);
+                ASSERT_NOT_NULL(hostname);
+                ASSERT_FALSE(streq(hostname, "foo????????x??????????u"));
+                ASSERT_EQ(fnmatch("foo????????x??????????u", hostname, /* flags= */ 0), 0);
+                hostname = mfree(hostname);
+        }
+
         /* no value set */
         hostname = (char*) 0x1234;
-        assert_se(write_string_file(path, "# nothing here\n", WRITE_STRING_FILE_CREATE) == 0);
-        assert_se(read_etc_hostname(path, &hostname) == -ENOENT);
-        assert_se(hostname == (char*) 0x1234);  /* does not touch argument on error */
+        ASSERT_OK(write_string_file(path, "# nothing here\n", WRITE_STRING_FILE_CREATE|WRITE_STRING_FILE_TRUNCATE));
+        ASSERT_ERROR(read_etc_hostname(path, /* substitute_wildcards= */ false, &hostname), ENOENT);
+        assert(hostname == (char*) 0x1234);  /* does not touch argument on error */
 
         /* nonexisting file */
-        assert_se(read_etc_hostname("/non/existing", &hostname) == -ENOENT);
-        assert_se(hostname == (char*) 0x1234);  /* does not touch argument on error */
+        ASSERT_ERROR(read_etc_hostname("/non/existing", /* substitute_wildcards= */ false, &hostname), ENOENT);
+        assert(hostname == (char*) 0x1234);  /* does not touch argument on error */
 }
 
 TEST(hostname_setup) {
@@ -69,6 +86,23 @@ TEST(hostname_malloc) {
 
         assert_se(l = gethostname_short_malloc());
         log_info("hostname_short_malloc: \"%s\"", l);
+}
+
+TEST(default_hostname) {
+        if (!hostname_is_valid(FALLBACK_HOSTNAME, 0)) {
+                log_error("Configured fallback hostname \"%s\" is not valid.", FALLBACK_HOSTNAME);
+                exit(EXIT_FAILURE);
+        }
+
+        _cleanup_free_ char *n = get_default_hostname();
+        assert_se(n);
+        log_info("get_default_hostname: \"%s\"", n);
+        assert_se(hostname_is_valid(n, 0));
+
+        _cleanup_free_ char *m = get_default_hostname_raw();
+        assert_se(m);
+        log_info("get_default_hostname_raw: \"%s\"", m);
+        assert_se(hostname_is_valid(m, VALID_HOSTNAME_QUESTION_MARK));
 }
 
 DEFINE_TEST_MAIN(LOG_DEBUG);
