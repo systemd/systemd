eda75b2cb27f171130191d1613c04c9d5e929efd
Mike Yuan <me@yhndnzj.com>
2025-03-08 20:04:17 +0100

    core/main: log about save_env() error

--- src/core/main.c
+++ src/core/main.c
@@ -3021,7 +3021,7 @@ static int save_env(void) {
 
         l = strv_copy(environ);
         if (!l)
-                return -ENOMEM;
+                return log_oom();
 
         strv_free_and_replace(saved_env, l);
         return 0;
