200501c722c9aa4291a4d8c9cc57c160ef88fec4
anonymix007 <48598263+anonymix007@users.noreply.github.com>
2025-03-09 15:43:47 +0300

    pe: Introduce PeLocateByOffset
    Used to distinguish PE files properly loaded into memory or stored as a file

--- src/boot/pe.h
+++ src/boot/pe.h
@@ -26,6 +26,43 @@ typedef struct PeSectionVector {
         uint64_t file_offset;   /* Offset on disk, relative to beginning of file */
 } PeSectionVector;
 
+typedef enum PeLocateByOffset {
+        PE_LOCATE_BY_OFFSET_FILE, /* Assume PE is stored as a file */
+        PE_LOCATE_BY_OFFSET_BASE, /* Assume PE is loaded into the memory */
+} PeLocateByOffset;
+
+static inline const void* pe_section_header_data(const void *base, const PeSectionHeader *header, PeLocateByOffset location, size_t *ret_size) {
+        assert(header);
+        switch (location) {
+        case PE_LOCATE_BY_OFFSET_FILE:
+                if (ret_size)
+                        *ret_size = header->SizeOfRawData;
+                return (const uint8_t *) base + header->PointerToRawData;
+        case PE_LOCATE_BY_OFFSET_BASE:
+                if (ret_size)
+                        *ret_size = header->VirtualSize;
+                return (const uint8_t *) base + header->VirtualAddress;
+        default:
+                return NULL;
+        }
+}
+
+static inline const void* pe_section_vector_data(const void *base, const PeSectionVector *entry, PeLocateByOffset location, size_t *ret_size) {
+        assert(entry);
+        switch (location) {
+        case PE_LOCATE_BY_OFFSET_FILE:
+                if (ret_size)
+                        *ret_size = entry->file_size;
+                return (const uint8_t *) base + entry->file_offset;
+        case PE_LOCATE_BY_OFFSET_BASE:
+                if (ret_size)
+                        *ret_size = entry->memory_size;
+                return (const uint8_t *) base + entry->memory_size;
+        default:
+                return NULL;
+        }
+}
+
 static inline bool PE_SECTION_VECTOR_IS_SET(const PeSectionVector *v) {
         return v && v->memory_size != 0;
 }
@@ -46,11 +83,13 @@ EFI_STATUS pe_locate_profile_sections(
                 const char* const section_names[],
                 unsigned profile,
                 size_t validate_base,
-                PeSectionVector sections[]);
+                PeSectionVector sections[],
+                PeLocateByOffset location);
 
 EFI_STATUS pe_memory_locate_sections(
                 const void *base,
                 const char *const section_names[],
-                PeSectionVector sections[]);
+                PeSectionVector sections[],
+                PeLocateByOffset location);
 
 EFI_STATUS pe_kernel_info(const void *base, uint32_t *ret_compat_address, size_t *ret_size_in_memory);
