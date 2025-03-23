a18b80b15dc9a575eb3e734062fb762b62961bab
Lennart Poettering <lennart@poettering.net>
2025-03-23 18:39:03 -0400

    fork-journal: add helpers for spawning off journalctl from 'systemctl start' or 'systemd-run'
    This is modelled after the polkit or askpw agents, but simply invokes
    journalctl for the specified unit name, to show logs.

---
+++ src/shared/fork-journal.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#pragma once
+
+#include "macro.h"
+#include "pidref.h"
+#include "runtime-scope.h"
+#include "set.h"
+
+int journal_fork(RuntimeScope scope, const char * const *units, PidRef *ret_pidref);
+
+void journal_terminate(PidRef *pidref);
