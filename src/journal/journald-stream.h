1f96f60f15b82133f0f85856d9c6546fd02f5cba
Lennart Poettering <lennart@poettering.net>
2025-03-25 16:46:18 -0400

    journald: rework the Synchronize() varlink logic
    Previously, if the Synchronize() varlink call is issued we'd wait for
    journald to become idle before returning success. That is problematic
    however: on a busy system journald might never become idle. Hence, let's
    beef up the logic to ensure that we do not wait longer than necessary:
    i.e. we make sure we process any data enqueued before the sync request
    was submitted, but not more.
    
    Implementing this isn't trivial unfortunately. To deal with this
    reasonably, we need to determine somehow for incoming log messages
    whether they are from before or after the point in time where the sync
    requested was received.
    
    For AF_UNIX/SOCK_DGRAM we can use SO_TIMESTAMP to directly compare
    timestamps of incoming messages with the timestamp of the sync request
    (unfortunately only CLOCK_REALTIME).
    
    For AF_UNIX/SOCK_STREAM we can call SIOCINQ at the moment we initiate
    the sync, and then continue processing incoming traffic, counting down
    the bytes until the SIOCINQ returned bytes have been processed. All
    further data must have been enqueued later hence.
    
    With those two mechanisms in place we can relatively reliably
    synchronize the journal.
    
    This also adds a boolean argument "offline" to the Synchronize() call,
    which controls whether to offline the journal after processing the
    pending messages. it defaults to true, for compat with the status quo
    ante. But for most cases the offlining is probably not necessary, and is
    cheaper to do without, hence allow not to do it.

--- src/journal/journald-stream.h
+++ src/journal/journald-stream.h
@@ -5,6 +5,54 @@ typedef struct StdoutStream StdoutStream;
 
 #include "fdset.h"
 #include "journald-server.h"
+#include "journald-sync.h"
+
+typedef enum StdoutStreamState {
+        STDOUT_STREAM_IDENTIFIER,
+        STDOUT_STREAM_UNIT_ID,
+        STDOUT_STREAM_PRIORITY,
+        STDOUT_STREAM_LEVEL_PREFIX,
+        STDOUT_STREAM_FORWARD_TO_SYSLOG,
+        STDOUT_STREAM_FORWARD_TO_KMSG,
+        STDOUT_STREAM_FORWARD_TO_CONSOLE,
+        STDOUT_STREAM_RUNNING,
+} StdoutStreamState;
+
+struct StdoutStream {
+        Server *server;
+        StdoutStreamState state;
+
+        int fd;
+
+        struct ucred ucred;
+        char *label;
+        char *identifier;
+        char *unit_id;
+        int priority;
+        bool level_prefix:1;
+        bool forward_to_syslog:1;
+        bool forward_to_kmsg:1;
+        bool forward_to_console:1;
+
+        bool fdstore:1;
+        bool in_notify_queue:1;
+
+        char *buffer;
+        size_t length;
+
+        sd_event_source *event_source;
+
+        char *state_file;
+
+        ClientContext *context;
+
+        LIST_FIELDS(StdoutStream, stdout_stream);
+        LIST_FIELDS(StdoutStream, stdout_stream_notify_queue);
+
+        char id_field[STRLEN("_STREAM_ID=") + SD_ID128_STRING_MAX];
+
+        LIST_HEAD(StreamSyncReq, stream_sync_reqs);
+};
 
 int server_open_stdout_socket(Server *s, const char *stdout_socket);
 int server_restore_streams(Server *s, FDSet *fds);
