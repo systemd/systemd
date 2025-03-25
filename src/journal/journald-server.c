6da21f943619847835cead30638296d3e485c3dd
Lennart Poettering <lennart@poettering.net>
2025-03-25 16:46:18 -0400

    journald: rework the Synchronize() varlink logic
    Previously, if the Synchronize() varlink call is issued we'd wait for
    journald to become idle before returning success. That is problematic
    however: on a busy system journald might never become idle. Hence, let's
    beef up the logic to ensure that we do not wait longer than necessary:
    i.e. we make sure we process any data enqueued before the sync request
    was submitted, but not more.
    
    Implementing this isn't trivial unfortunately. To deal with this
    reasonably, we need to determine somehow for incoming log messages
    whether they are from before or after the point in time where the sync
    requested was received.
    
    For AF_UNIX/SOCK_DGRAM we can use SO_TIMESTAMP to directly compare
    timestamps of incoming messages with the timestamp of the sync request
    (unfortunately only CLOCK_REALTIME).
    
    For AF_UNIX/SOCK_STREAM we can call SIOCINQ at the moment we initiate
    the sync, and then continue processing incoming traffic, counting down
    the bytes until the SIOCINQ returned bytes have been processed. All
    further data must have been enqueued later hence.
    
    With those two mechanisms in place we can relatively reliably
    synchronize the journal.
    
    This also adds a boolean argument "offline" to the Synchronize() call,
    which controls whether to offline the journal after processing the
    pending messages. it defaults to true, for compat with the status quo
    ante. But for most cases the offlining is probably not necessary, and is
    cheaper to do without, hence allow not to do it.

--- src/journal/journald-server.c
+++ src/journal/journald-server.c
@@ -1594,6 +1594,9 @@ int server_process_datagram(
                         log_ratelimit_warning(JOURNAL_LOG_RATELIMIT,
                                               "Got file descriptors via syslog socket. Ignoring.");
 
+                if (tv)
+                        s->syslog_timestamp = timeval_load(tv);
+
         } else if (fd == s->native_fd) {
                 if (n > 0 && n_fds == 0)
                         server_process_native_message(s, s->buffer, n, ucred, tv, label, label_len);
@@ -1602,6 +1605,9 @@ int server_process_datagram(
                 else if (n_fds > 0)
                         log_ratelimit_warning(JOURNAL_LOG_RATELIMIT,
                                               "Got too many file descriptors via native socket. Ignoring.");
+
+                if (tv)
+                        s->native_timestamp = timeval_load(tv);
 
         } else {
                 assert(fd == s->audit_fd);
@@ -1614,6 +1620,9 @@ int server_process_datagram(
         }
 
         close_many(fds, n_fds);
+
+        if (tv)
+                sync_req_revalidate_by_timestamp(s);
 
         server_refresh_idle_timer(s);
         return 0;
