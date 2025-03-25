4299c8f26fa91c5429b1974f4efb2ec552933069
Lennart Poettering <lennart@poettering.net>
2025-03-25 16:44:30 -0400

    journald: rename stdout_stream_destroy() → stdout_stream_terminate()

--- src/journal/journald-stream.h
+++ src/journal/journald-stream.h
@@ -11,5 +11,5 @@ int server_restore_streams(Server *s, FDSet *fds);
 
 StdoutStream* stdout_stream_free(StdoutStream *s);
 int stdout_stream_install(Server *s, int fd, StdoutStream **ret);
-void stdout_stream_destroy(StdoutStream *s);
+void stdout_stream_terminate(StdoutStream *s);
 void stdout_stream_send_notify(StdoutStream *s);
