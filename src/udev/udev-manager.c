49ee535e2564e584b51dc7140b9212188f586b30
Yu Watanabe <watanabe.yu+github@gmail.com>
2025-03-27 13:52:53 +0900

    udev: adjust event source priorities

--- src/udev/udev-manager.c
+++ src/udev/udev-manager.c
@@ -194,7 +194,16 @@ static int worker_new(Worker **ret, Manager *manager, sd_device_monitor *worker_
         if (r < 0)
                 return r;
 
-        r = event_add_child_pidref(manager->event, NULL, pidref, WEXITED, on_sigchld, worker);
+        _cleanup_(sd_event_source_unrefp) sd_event_source *s = NULL;
+        r = event_add_child_pidref(manager->event, &s, pidref, WEXITED, on_sigchld, worker);
+        if (r < 0)
+                return r;
+
+        r = sd_event_source_set_priority(s, EVENT_PRIORITY_WORKER_SIGCHLD);
+        if (r < 0)
+                return r;
+
+        r = sd_event_source_set_floating(s, true);
         if (r < 0)
                 return r;
 
@@ -1125,6 +1134,10 @@ static int manager_start_device_monitor(Manager *manager) {
         if (r < 0)
                 return log_error_errno(r, "Failed to start device monitor: %m");
 
+        r = sd_event_source_set_priority(sd_device_monitor_get_event_source(manager->monitor), EVENT_PRIORITY_DEVICE_MONITOR);
+        if (r < 0)
+                return log_error_errno(r, "Failed to set priority to device monitor: %m");
+
         return 0;
 }
 
@@ -1136,7 +1149,7 @@ static int manager_start_worker_notify(Manager *manager) {
 
         r = notify_socket_prepare(
                         manager->event,
-                        SD_EVENT_PRIORITY_NORMAL,
+                        EVENT_PRIORITY_WORKER_NOTIFY,
                         on_worker_notify,
                         manager,
                         &manager->worker_notify_socket_path);
