16e0a44d2102ace4a124af19f16f543fd9e5d974
Mike Yuan <me@yhndnzj.com>
2025-03-29 21:33:58 +0100

    socket-util: introduce socket_autobind() helper
    Prompted by https://github.com/systemd/systemd/pull/36858#discussion_r2017719305
    and #36893

--- src/basic/socket-util.c
+++ src/basic/socket-util.c
@@ -28,6 +28,7 @@
 #include "parse-util.h"
 #include "path-util.h"
 #include "process-util.h"
+#include "random-util.h"
 #include "socket-util.h"
 #include "string-table.h"
 #include "string-util.h"
@@ -1452,6 +1453,36 @@ int socket_bind_to_ifindex(int fd, int ifindex) {
                 return RET_NERRNO(setsockopt(fd, SOL_SOCKET, SO_BINDTODEVICE, NULL, 0));
 
         return setsockopt_int(fd, SOL_SOCKET, SO_BINDTOIFINDEX, ifindex);
+}
+
+int socket_autobind(int fd, char **ret_name) {
+        _cleanup_free_ char *name = NULL;
+        uint64_t random;
+        int r;
+
+        /* Generate a random abstract socket name and bind fd to it. This is modeled after the kernel
+         * "autobind" feature, but uses 64-bit random number internally. */
+
+        assert(fd >= 0);
+        assert(ret_name);
+
+        random = random_u64();
+
+        if (asprintf(&name, "@%" PRIu64, random) < 0)
+                return -ENOMEM;
+
+        union sockaddr_union sa = {
+                .un.sun_family = AF_UNIX,
+        };
+        assert_cc(DECIMAL_STR_MAX(uint64_t) < sizeof(sa.un.sun_path));
+        strcpy(sa.un.sun_path, name);
+        sa.un.sun_path[0] = 0;
+
+        if (bind(fd, &sa.sa, SOCKADDR_UN_LEN(sa.un)) < 0)
+                return -errno;
+
+        *ret_name = TAKE_PTR(name);
+        return 0;
 }
 
 ssize_t recvmsg_safe(int sockfd, struct msghdr *msg, int flags) {
