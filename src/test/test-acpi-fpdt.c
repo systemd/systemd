0f0e65dc4e97ff7df339b4308c1b06692c34bd99
Busayo Dada <toyinoluwabusayo@gmail.com>
2025-03-29 17:21:02 +0100

    test: add unit test for ACPI FPDT boot timestamps

--- src/test/test-acpi-fpdt.c
+++ src/test/test-acpi-fpdt.c
@@ -1,40 +1,29 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 
-#include <errno.h>
-#include <fcntl.h>
-#include <stddef.h>
-#include <stdint.h>
-#include <string.h>
-
 #include "acpi-fpdt.h"
-#include "alloc-util.h"
-#include "fd-util.h"
-#include "fileio.h"
 #include "tests.h"
-#include "test-acpi-fpdt.h"
 
 TEST(acpi_get_boot_usec) {
-    struct timespec ts_start;
-    timespec_get(&ts_start, TIME_UTC);
-    ASSERT_OK(ts_start.tv_nsec);
-    usec_t start_time = ts_start.tv_nsec;
-    usec_t *ret_loader_start = &start_time;
+        int r;
+        usec_t *loader_start_time;
+        usec_t *loader_exit_time;
+        struct timespec ts_start;
+        struct timespec ts_exit;
 
-    ASSERT_OK_ZERO(usleep_safe(1));
-    struct timespec ts_end;
-    timespec_get(&ts_end, TIME_UTC);
-    ASSERT_OK(ts_end.tv_nsec);
-    usec_t exit_time = ts_end.tv_nsec;
-    usec_t *ret_loader_exit = &exit_time;
-    log_info("ret_loader start time: %lu\n", *ret_loader_start);
-    log_info("ret_loader exit time:  %lu\n", *ret_loader_exit);
+        timespec_get(&ts_start, TIME_UTC);
+        loader_start_time = (long unsigned int *) &ts_start.tv_nsec;
 
-    int r;
-    r = acpi_get_boot_usec(ret_loader_start, ret_loader_exit);
-    if (r == -ENODATA)
-                ASSERT_OK(r);
+        usleep_safe(1);
 
-    /*The assertion ASSERT_OK(-ENODATA) failed because it expected -ENODATA to succeed, but it is actually an error: No data available.*/
+        timespec_get(&ts_exit, TIME_UTC);
+        loader_exit_time = (long unsigned int *) &ts_exit.tv_nsec;
+
+        r = acpi_get_boot_usec(loader_start_time, loader_exit_time);
+        if (r < 0)
+                return (void) log_error_errno(r, "acpi_get_boot_usec function failed: %m");
+
+        ASSERT_OK(r);
+        /* This assertion fails when the if statement above is commented */
 }
 
 DEFINE_TEST_MAIN(LOG_DEBUG);
