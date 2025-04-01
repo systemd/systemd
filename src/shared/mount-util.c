26b3ddb10b6aa25515a971664020e3eb047b8bbe
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2025-04-01 11:17:04 +0200

    shared/mount-util: do not use a unicode in early boot logs
    In debug logs:
    (sd-mkdcre[299]: Moving mount /dev/shm \xe2\x86\x92 /run/credentials/systemd-network-generator.service (MS_MOVE "")...
    In other places we just say 'Moving … to …', so let's not be cute here and
    avoid hard-to-read logs.

--- src/shared/mount-util.c
+++ src/shared/mount-util.c
@@ -17,7 +17,6 @@
 #include "fileio.h"
 #include "fs-util.h"
 #include "fstab-util.h"
-#include "glyph-util.h"
 #include "hashmap.h"
 #include "initrd-util.h"
 #include "label-util.h"
@@ -713,8 +712,8 @@ int mount_verbose_full(
                 log_debug("Bind-mounting %s on %s (%s \"%s\")...",
                           what, where, strnull(fl), strempty(o));
         else if (f & MS_MOVE)
-                log_debug("Moving mount %s %s %s (%s \"%s\")...",
-                          what, glyph(GLYPH_ARROW_RIGHT), where, strnull(fl), strempty(o));
+                log_debug("Moving mount %s to %s (%s \"%s\")...",
+                          what, where, strnull(fl), strempty(o));
         else
                 log_debug("Mounting %s (%s) on %s (%s \"%s\")...",
                           strna(what), strna(type), where, strnull(fl), strempty(o));
