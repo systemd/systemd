38f3c44762f67f633494c0a134968a22f2745e99
Yu Watanabe <watanabe.yu+github@gmail.com>
2025-04-01 19:00:50 +0900

    test: replace deprecated --hostid= with --hostnqn=
    Fixes #36908.
    
    (cherry picked from commit c70a113a6c2b02f667498c8385bedd1a873edb2c)
    (cherry picked from commit c4eeb99c2192e9318566d045af985712dfd3d02c)

--- test/units/TEST-84-STORAGETM.sh
+++ test/units/TEST-84-STORAGETM.sh
@@ -22,7 +22,7 @@ NVME_UUID="$(cat /proc/sys/kernel/random/uuid)"
 systemd-run -u teststoragetm.service -p Type=notify -p "Environment=SYSTEMD_NVME_UUID=${NVME_UUID:?}" /usr/lib/systemd/systemd-storagetm /var/tmp/storagetm.test --nqn=quux
 NVME_DEVICE="/dev/disk/by-id/nvme-uuid.${NVME_UUID:?}"
 
-nvme connect-all -vv -t tcp -a 127.0.0.1 -s 16858 --hostid="$(cat /proc/sys/kernel/random/uuid)"
+nvme connect-all -vv -t tcp -a 127.0.0.1 -s 16858 --hostnqn="$(nvme gen-hostnqn)"
 udevadm wait --settle "$NVME_DEVICE"
 
 dd if="$NVME_DEVICE" bs=1024 | cmp /var/tmp/storagetm.test -
