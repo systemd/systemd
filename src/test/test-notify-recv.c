9686e94d9a8268f28d5acc5dfb7dbe9254649581
Yu Watanabe <watanabe.yu+github@gmail.com>
2025-04-02 13:23:47 +0900

    test: drop unnecessary '>= 0' in ASSERT_OK()
    Follow-up for 2351bc991e3a4f1fbdaa2d53900980b6703babbd.
    Fixes CID#1596075.

--- src/test/test-notify-recv.c
+++ src/test/test-notify-recv.c
@@ -72,7 +72,7 @@ TEST(notify_socket_prepare) {
                 .pidref = PIDREF_NULL,
         };
         _cleanup_free_ char *path = NULL;
-        ASSERT_OK(notify_socket_prepare(e, SD_EVENT_PRIORITY_NORMAL - 10, on_recv, &c, &path) >= 0);
+        ASSERT_OK(notify_socket_prepare(e, SD_EVENT_PRIORITY_NORMAL - 10, on_recv, &c, &path));
 
         ASSERT_OK(sigprocmask_many(SIG_BLOCK, NULL, SIGCHLD));
 
