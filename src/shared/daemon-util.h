a2abd137a82eb3b14671358d1ea3ab6a16f09a42
Yu Watanabe <watanabe.yu+github@gmail.com>
2025-04-05 22:41:28 +0900

    daemon-util: expose notify_remove_fd_warn()

--- src/shared/daemon-util.h
+++ src/shared/daemon-util.h
@@ -23,6 +23,7 @@ static inline void notify_on_cleanup(const char **p) {
                 (void) sd_notify(false, *p);
 }
 
+int notify_remove_fd_warn(const char *name);
 int notify_remove_fd_warnf(const char *format, ...) _printf_(1, 2);
 int close_and_notify_warn(int fd, const char *name);
 int notify_push_fd(int fd, const char *name);
