7e278b72997b87f8b7c1c3ed1db199bdcea89a8b
Mike Yuan <me@yhndnzj.com>
2025-04-09 17:35:33 +0200

    test: add test cases for ExecStart= shell prefixing

---
+++ test/units/TEST-07-PID1.prefix-shell.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: LGPL-2.1-or-later
+# shellcheck disable=SC2016
+set -eux
+set -o pipefail
+
+# shellcheck source=test/units/util.sh
+. "$(dirname "$0")"/util.sh
+
+systemctl start prefix-shell.service
+journalctl -b -u prefix-shell.service | grep "with login shell: .*" | grep "lvl: 101"
+journalctl -b -u prefix-shell.service | grep "with normal shell baz"
+assert_eq "$(cat /tmp/TEST-07-PID1.prefix-shell.flag)" "YAY!"
