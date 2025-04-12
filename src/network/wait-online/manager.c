cf1f91339f35687c7ec24bc8f3f100f9ce2cee81
Yu Watanabe <watanabe.yu+github@gmail.com>
2025-04-13 00:30:58 +0900

    wait-online: move manager_process_link() to link.c
    Then, rename it to rtnl_process_link().
    
    No functional change, just refactoring.

--- src/network/wait-online/manager.c
+++ src/network/wait-online/manager.c
@@ -234,80 +234,11 @@ bool manager_configured(Manager *m) {
         return !m->any && has_online;
 }
 
-static int manager_process_link(sd_netlink *rtnl, sd_netlink_message *mm, void *userdata) {
-        Manager *m = ASSERT_PTR(userdata);
-        uint16_t type;
-        Link *l;
-        const char *ifname;
-        int ifindex, r;
-
-        assert(rtnl);
-        assert(mm);
-
-        r = sd_netlink_message_get_type(mm, &type);
-        if (r < 0) {
-                log_warning_errno(r, "rtnl: Could not get message type, ignoring: %m");
-                return 0;
-        }
-
-        r = sd_rtnl_message_link_get_ifindex(mm, &ifindex);
-        if (r < 0) {
-                log_warning_errno(r, "rtnl: Could not get ifindex from link, ignoring: %m");
-                return 0;
-        } else if (ifindex <= 0) {
-                log_warning("rtnl: received link message with invalid ifindex %d, ignoring", ifindex);
-                return 0;
-        }
-
-        r = sd_netlink_message_read_string(mm, IFLA_IFNAME, &ifname);
-        if (r < 0) {
-                log_warning_errno(r, "rtnl: Received link message without ifname, ignoring: %m");
-                return 0;
-        }
-
-        l = hashmap_get(m->links_by_index, INT_TO_PTR(ifindex));
-
-        switch (type) {
-
-        case RTM_NEWLINK:
-                if (!l) {
-                        log_debug("Found link %s(%i)", ifname, ifindex);
-
-                        r = link_new(m, &l, ifindex, ifname);
-                        if (r < 0) {
-                                log_warning_errno(r, "Failed to create link object for %s(%i), ignoring: %m", ifname, ifindex);
-                                return 0;
-                        }
-                }
-
-                r = link_update_rtnl(l, mm);
-                if (r < 0)
-                        log_link_warning_errno(l, r, "Failed to process RTNL link message, ignoring: %m");
-
-                r = link_update_monitor(l);
-                if (r < 0)
-                        log_link_full_errno(l, IN_SET(r, -ENODATA, -ENOENT) ? LOG_DEBUG : LOG_WARNING, r,
-                                            "Failed to update link state, ignoring: %m");
-
-                break;
-
-        case RTM_DELLINK:
-                if (l) {
-                        log_link_debug(l, "Removing link");
-                        link_free(l);
-                }
-
-                break;
-        }
-
-        return 0;
-}
-
 static int on_rtnl_event(sd_netlink *rtnl, sd_netlink_message *mm, void *userdata) {
         Manager *m = userdata;
         int r;
 
-        r = manager_process_link(rtnl, mm, m);
+        r = rtnl_process_link(rtnl, mm, m);
         if (r < 0)
                 return r;
 
@@ -354,7 +285,7 @@ static int manager_rtnl_listen(Manager *m) {
                 return r;
 
         for (sd_netlink_message *i = reply; i; i = sd_netlink_message_next(i)) {
-                r = manager_process_link(m->rtnl, i, m);
+                r = rtnl_process_link(m->rtnl, i, m);
                 if (r < 0)
                         return r;
         }
