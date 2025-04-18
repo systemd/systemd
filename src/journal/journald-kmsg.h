d88dc9a5dde6b81ef1bf181c031f09fc8d596280
Daan De Meyer <daan.j.demeyer@gmail.com>
2025-04-18 22:08:04 +0200

    journal: Remove circular header dependencies

--- src/journal/journald-kmsg.h
+++ src/journal/journald-kmsg.h
@@ -1,7 +1,10 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 #pragma once
 
-#include "journald-server.h"
+#include <stddef.h>
+#include <sys/socket.h>
+
+typedef struct Server Server;
 
 int server_open_dev_kmsg(Server *s);
 int server_flush_dev_kmsg(Server *s);
--- src/journal/journald-socket.h
+++ src/journal/journald-socket.h
@@ -1,7 +1,10 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 #pragma once
 
-#include "journald-server.h"
-#include "socket-util.h"
+#include <sys/socket.h>
+
+#include "time-util.h"
+
+typedef struct Server Server;
 
 int server_forward_socket(Server *s, const struct iovec *iovec, size_t n, const dual_timestamp *ts, int priority);
