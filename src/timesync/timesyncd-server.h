01a18e34a572246403f290d725e4b6861e6e6560
Daan De Meyer <daan.j.demeyer@gmail.com>
2025-04-19 23:18:44 +0200

    timesync: Remove circular header dependencies

--- src/timesync/timesyncd-server.h
+++ src/timesync/timesyncd-server.h
@@ -4,10 +4,11 @@
 #include "list.h"
 #include "socket-util.h"
 
+typedef struct Manager Manager;
 typedef struct ServerAddress ServerAddress;
 typedef struct ServerName ServerName;
 
-typedef enum ServerType {
+typedef enum ServerType : int {
         SERVER_SYSTEM,
         SERVER_FALLBACK,
         SERVER_LINK,
@@ -15,8 +16,6 @@ typedef enum ServerType {
         _SERVER_TYPE_MAX,
         _SERVER_TYPE_INVALID = -EINVAL,
 } ServerType;
-
-#include "timesyncd-manager.h"
 
 struct ServerAddress {
         ServerName *name;
