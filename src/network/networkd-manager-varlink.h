0934d894843fdbd1fcd8fad8e31fd262394b402f
Daan De Meyer <daan.j.demeyer@gmail.com>
2025-04-19 12:11:50 +0200

    network: Move networkd-manager.h include to implementation file

--- src/network/networkd-manager-varlink.h
+++ src/network/networkd-manager-varlink.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 #pragma once
 
-#include "networkd-manager.h"
+typedef struct Manager Manager;
 
 int manager_connect_varlink(Manager *m);
 void manager_varlink_done(Manager *m);
