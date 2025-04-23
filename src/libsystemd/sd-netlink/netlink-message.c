775a3bffaf96eedf33a4a326551e43320e8d40aa
Jiri Pirko <jiri@nvidia.com>
2025-04-23 15:15:32 +0200

    sd-netlink: introduce indexed variant of enter container
    Benefit from previously introduced multi-attribute infrastructure
    and allow user to enter multiple containers of same attribute
    by passing index.

--- src/libsystemd/sd-netlink/netlink-message.c
+++ src/libsystemd/sd-netlink/netlink-message.c
@@ -1149,7 +1149,7 @@ static int netlink_container_parse(
         return 0;
 }
 
-int sd_netlink_message_enter_container(sd_netlink_message *m, uint16_t attr_type) {
+int sd_netlink_message_enter_container_indexed(sd_netlink_message *m, uint16_t attr_type, unsigned int index) {
         const NLAPolicy *policy;
         const NLAPolicySet *policy_set;
         void *container;
@@ -1235,7 +1235,7 @@ int sd_netlink_message_enter_container(sd_netlink_message *m, uint16_t attr_type
         if (!policy_set)
                 return -EOPNOTSUPP;
 
-        r = netlink_message_read_internal(m, attr_type, &container, NULL);
+        r = netlink_message_read_internal_indexed(m, attr_type, &container, NULL, index);
         if (r < 0)
                 return r;
 
@@ -1254,6 +1254,10 @@ int sd_netlink_message_enter_container(sd_netlink_message *m, uint16_t attr_type
         m->containers[m->n_containers].policy_set = policy_set;
 
         return 0;
+}
+
+int sd_netlink_message_enter_container(sd_netlink_message *m, uint16_t attr_type) {
+        return sd_netlink_message_enter_container_indexed(m, attr_type, 0);
 }
 
 int sd_netlink_message_enter_array(sd_netlink_message *m, uint16_t attr_type) {
