b630b8f84782417c455cec92c7af83f0d037a1ff
David Tardon <dtardon@redhat.com>
2025-04-25 14:42:34 +0200

    udevadm-info: drop superfluous pager_open() call
    It's already called in advance in info_main().

--- src/udev/udevadm-info.c
+++ src/udev/udevadm-info.c
@@ -454,8 +454,6 @@ static int export_devices(sd_device_enumerator *e) {
         if (r < 0)
                 return log_error_errno(r, "Failed to scan devices: %m");
 
-        pager_open(arg_pager_flags);
-
         FOREACH_DEVICE_AND_SUBSYSTEM(e, d)
                 if (sd_json_format_enabled(arg_json_format_flags)) {
                         _cleanup_(sd_json_variant_unrefp) sd_json_variant *v = NULL;
