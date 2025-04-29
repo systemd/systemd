0c49e0049b7665bb7769a13ef346fef92e1ad4d6
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2025-04-29 14:47:59 +0200

    coredump: use %d in kernel core pattern
    The kernel provides %d which is documented as
    "dump mode—same as value returned by prctl(2) PR_GET_DUMPABLE".
    
    We already query /proc/pid/auxv for this information, but unfortunately this
    check is subject to a race, because the crashed process may be replaced by an
    attacker before we read this data, for example replacing a SUID process that
    was killed by a signal with another process that is not SUID, tricking us into
    making the coredump of the original process readable by the attacker.
    
    With this patch, we effectively add one more check to the list of conditions
    that need be satisfied if we are to make the coredump accessible to the user.
    
    Reportedy-by: Qualys Security Advisory <qsa@qualys.com>
    
    In principle, %d might return a value other than 0, 1, or 2 in the future.
    Thus, we accept those, but emit a notice.

--- test/units/TEST-87-AUX-UTILS-VM.coredump.sh
+++ test/units/TEST-87-AUX-UTILS-VM.coredump.sh
@@ -198,12 +198,17 @@ journalctl -b -n 1 --output=export --output-fields=MESSAGE,COREDUMP COREDUMP_EXE
     /usr/lib/systemd/systemd-coredump --backtrace $$ 0 0 6 1679509900 12345
 journalctl -b -n 1 --output=export --output-fields=MESSAGE,COREDUMP COREDUMP_EXE="/usr/bin/test-dump" |
     /usr/lib/systemd/systemd-coredump --backtrace $$ 0 0 6 1679509901 12345 mymachine
+journalctl -b -n 1 --output=export --output-fields=MESSAGE,COREDUMP COREDUMP_EXE="/usr/bin/test-dump" |
+    /usr/lib/systemd/systemd-coredump --backtrace $$ 0 0 6 1679509902 12345 youmachine 1
 # Wait a bit for the coredumps to get processed
 timeout 30 bash -c "while [[ \$(coredumpctl list -q --no-legend $$ | wc -l) -lt 2 ]]; do sleep 1; done"
 coredumpctl info $$
 coredumpctl info COREDUMP_TIMESTAMP=1679509900000000
 coredumpctl info COREDUMP_TIMESTAMP=1679509901000000
 coredumpctl info COREDUMP_HOSTNAME="mymachine"
+coredumpctl info COREDUMP_TIMESTAMP=1679509902000000
+coredumpctl info COREDUMP_HOSTNAME="youmachine"
+coredumpctl info COREDUMP_DUMPABLE="1"
 
 # This used to cause a stack overflow
 systemd-run -t --property CoredumpFilter=all ls /tmp
