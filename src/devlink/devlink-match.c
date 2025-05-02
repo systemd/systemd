7028a86b0ae7fa65ba48ba013db173685750595a
Jiri Pirko <jiri@nvidia.com>
2025-05-02 17:26:12 +0200

    devlinkd: add param match
    Introduce a devlink param object match, allowing to match on PARAM_NAME
    attribute in devlink netlink messages.

--- src/devlink/devlink-match.c
+++ src/devlink/devlink-match.c
@@ -26,6 +26,7 @@ const DevlinkMatchVTable * const devlink_match_vtable[] = {
         &devlink_match_port_index_vtable,
         &devlink_match_port_split_vtable,
         &devlink_match_port_ifname_vtable,
+        &devlink_match_param_vtable,
 };
 
 #define DEVLINK_MATCH_VTABLE_SIZE ELEMENTSOF(devlink_match_vtable)
