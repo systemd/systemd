92ed063496de2d117801308628bb9476ac9f8bfe
Jiri Pirko <jiri@nvidia.com>
2025-05-02 19:52:53 +0200

    devlinkd: add port ifname match with ifname tracker
    Introduce an ifname tracker that uses RT netlink and stores netdevice
    ifnames. Introduce port ifname match that uses the tracker to get
    devlink port attributes by ifname.

--- src/devlink/devlinkd-manager.c
+++ src/devlink/devlinkd-manager.c
@@ -1,5 +1,7 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 
+#include <linux/filter.h>
+
 #include "sd-netlink.h"
 #include "sd-bus.h"
 
@@ -15,6 +17,7 @@
 #include "devlinkd-manager.h"
 #include "devlink.h"
 #include "devlink-key.h"
+#include "devlink-ifname-tracker.h"
 
 /* use 128 MB for receive socket kernel queue. */
 #define RCVBUF_SIZE    (128*1024*1024)
@@ -273,6 +276,122 @@ static int manager_genl_connect(Manager *m) {
         return 0;
 }
 
+static int manager_setup_rtnl_filter(Manager *m) {
+        struct sock_filter filter[] = {
+                /* Check the packet length. */
+                BPF_STMT(BPF_LD + BPF_W + BPF_LEN, 0),                                      /* A <- packet length */
+                BPF_JUMP(BPF_JMP + BPF_JGE + BPF_K, sizeof(struct nlmsghdr), 1, 0),         /* A (packet length) >= sizeof(struct nlmsghdr) ? */
+                BPF_STMT(BPF_RET + BPF_K, 0),                                               /* reject */
+                /* Accept all messages of types RTM_NEWLINK or RTM_DELLINK. */
+                BPF_STMT(BPF_LD + BPF_H + BPF_ABS, offsetof(struct nlmsghdr, nlmsg_type)),  /* A <- message type */
+                BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, htobe16(RTM_NEWLINK), 2, 0),            /* message type == RTM_NEWLINK ? */
+                BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, htobe16(RTM_DELLINK), 1, 0),            /* message type == RTM_DELLINK ? */
+                BPF_STMT(BPF_RET + BPF_K, 0),                                               /* reject */
+                BPF_STMT(BPF_RET + BPF_K, UINT32_MAX),                                      /* accept */
+        };
+
+        assert(m);
+        assert(m->rtnl);
+
+        return sd_netlink_attach_filter(m->rtnl, ELEMENTSOF(filter), filter);
+}
+
+static int manager_rtnl_process_link(sd_netlink *rtnl, sd_netlink_message *message, Manager *m) {
+        uint16_t type;
+        int ifindex;
+        int r;
+
+        assert(rtnl);
+        assert(message);
+        assert(m);
+
+        if (sd_netlink_message_is_error(message)) {
+                r = sd_netlink_message_get_errno(message);
+                if (r < 0)
+                        log_warning_errno(r, "rtnl: Could not receive link message, ignoring: %m");
+
+                return 0;
+        }
+
+        r = sd_netlink_message_get_type(message, &type);
+        if (r < 0) {
+                log_warning_errno(r, "rtnl: Could not get message type, ignoring: %m");
+                return 0;
+        }
+
+        r = sd_rtnl_message_link_get_ifindex(message, &ifindex);
+        if (r < 0) {
+                log_warning_errno(r, "rtnl: Could not get ifindex from link message, ignoring: %m");
+                return 0;
+        } else if (ifindex <= 0) {
+                log_warning("rtnl: received link message with invalid ifindex %d, ignoring: %m", ifindex);
+                return 0;
+        }
+
+        switch (type) {
+        case RTM_NEWLINK:
+                r = devlink_ifname_tracker_ifindex_update(m, ifindex, message);
+                if (r < 0)
+                        log_warning_errno(r, "rtnl: Could not update ifname tracker, ignoring: %m");
+                return 0;
+        case RTM_DELLINK:
+                devlink_ifname_tracker_ifindex_remove(m, ifindex);
+                return 0;
+        default:
+                log_warning("rtnl: Received unexpected message type %u when processing link, ignoring.", type);
+                return 0;
+        }
+}
+
+int manager_rtnl_query_one(Manager *m, uint32_t ifindex) {
+        _cleanup_(sd_netlink_message_unrefp) sd_netlink_message *req = NULL;
+        _cleanup_(sd_netlink_message_unrefp) sd_netlink_message *rep = NULL;
+        int r;
+
+        r = sd_rtnl_message_new_link(m->rtnl, &req, RTM_GETLINK, ifindex);
+        if (r < 0)
+                return r;
+
+        r = sd_netlink_call(m->rtnl, req, 0, &rep);
+        if (r < 0) {
+                if (r == -EINVAL) /* Device not present. */
+                        return 0;
+                return r;
+        }
+
+        (void) manager_rtnl_process_link(m->rtnl, rep, m);
+
+        return 0;
+}
+
+static int manager_rtnl_connect(Manager *m) {
+        int r;
+
+        assert(m);
+
+        r = sd_netlink_open(&m->rtnl);
+        if (r < 0)
+                return r;
+
+        r = sd_netlink_increase_rxbuf(m->rtnl, RCVBUF_SIZE);
+        if (r < 0)
+                log_warning_errno(r, "Failed to increase receive buffer size for RT netlink socket, ignoring: %m");
+
+        r = sd_netlink_attach_event(m->rtnl, m->event, 0);
+        if (r < 0)
+                return r;
+
+        r = netlink_add_match(m->rtnl, NULL, RTM_NEWLINK, &manager_rtnl_process_link, NULL, m, "devlinkd-rtnl_process_link");
+        if (r < 0)
+                return r;
+
+        r = netlink_add_match(m->rtnl, NULL, RTM_DELLINK, &manager_rtnl_process_link, NULL, m, "devlinkd-rtnl_process_link");
+        if (r < 0)
+                return r;
+
+        return manager_setup_rtnl_filter(m);
+}
+
 static int signal_terminate_callback(sd_event_source *s, const struct signalfd_siginfo *si, void *userdata) {
         Manager *m = ASSERT_PTR(userdata);
 
@@ -374,6 +493,10 @@ int manager_setup(Manager *m) {
         if (r < 0)
                 return r;
 
+        r = manager_rtnl_connect(m);
+        if (r < 0)
+                return r;
+
         return 0;
 }
 
@@ -400,6 +523,8 @@ Manager* manager_free(Manager *m) {
         m->rtnl = sd_netlink_unref(m->rtnl);
         m->devlink_objs = hashmap_free(m->devlink_objs);
         m->reload = hashmap_free(m->reload);
+        m->port_cache_by_ifindex = hashmap_free(m->port_cache_by_ifindex);
+        m->ifname_tracker_by_ifname = hashmap_free(m->ifname_tracker_by_ifname);
         m->port_cache_by_ifindex = hashmap_free(m->port_cache_by_ifindex);
 
         return mfree(m);
