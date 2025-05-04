283b71e34a39d986e49b4681608a8bdd125d9738
Jiri Pirko <jiri@nvidia.com>
2025-05-04 20:36:55 +0200

    devlinkd: add internal port cache object
    In order to be able to support ifname-based matching in a follow-up
    patch, introduce an internal port cache object to store devlink port
    attributes and allow them to be looked-up by ifindex.

--- src/devlink/devlinkd-manager.h
+++ src/devlink/devlinkd-manager.h
@@ -17,6 +17,7 @@ struct Manager {
         sd_event *event;
         sd_event_source *periodic_enumeration_event_source;
         Hashmap *devlink_objs;
+        Hashmap *port_cache_by_ifindex;
         Hashmap *reload;
 };
 
