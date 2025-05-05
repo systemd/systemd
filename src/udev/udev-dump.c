19162f59745bae39c2cfb47f46d928c4b2dc16c6
Daan De Meyer <daan.j.demeyer@gmail.com>
2025-05-05 13:32:55 +0200

    device-util: Reduce transitive includes

--- src/udev/udev-dump.c
+++ src/udev/udev-dump.c
@@ -1,5 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 
+#include "alloc-util.h"
 #include "ansi-color.h"
 #include "device-private.h"
 #include "device-util.h"
@@ -8,6 +9,7 @@
 #include "fs-util.h"
 #include "json-util.h"
 #include "parse-util.h"
+#include "strv.h"
 #include "udev-builtin.h"
 #include "udev-dump.h"
 #include "udev-event.h"
