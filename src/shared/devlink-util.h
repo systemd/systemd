e7a2f715eb9f89b9442bb7888775552b9b98e3bd
Jiri Pirko <jiri@nvidia.com>
2025-05-05 18:36:16 +0200

    devlinkd: add dev object
    Introduce devlink dev object, allowing to work with devlink instance.
    Initially allow to configure eswitch mode.

--- src/shared/devlink-util.h
+++ src/shared/devlink-util.h
@@ -7,3 +7,13 @@
 #include "macro.h"
 
 const char *devlink_cmd_to_string(int cmd) _const_;
+
+typedef enum DevlinkDevESwitchMode {
+        _DEVLINK_DEV_ESWITCH_MODE_LEGACY    = DEVLINK_ESWITCH_MODE_LEGACY,
+        _DEVLINK_DEV_ESWITCH_MODE_SWITCHDEV = DEVLINK_ESWITCH_MODE_SWITCHDEV,
+        _DEVLINK_DEV_ESWITCH_MODE_MAX,
+        _DEVLINK_DEV_ESWITCH_MODE_INVALID   = -EINVAL,
+} DevlinkDevESwitchMode;
+
+const char *devlink_dev_eswitch_mode_to_string(DevlinkDevESwitchMode d) _const_;
+DevlinkDevESwitchMode devlink_dev_eswitch_mode_from_string(const char *d) _pure_;
