c3dddbcd8eee277e465d9bd598de8336ff5ba647
Yu Watanabe <watanabe.yu+github@gmail.com>
2025-05-07 02:32:56 +0900

    udev: split out device_broadcast_on_error()

--- src/udev/udev-error.h
+++ src/udev/udev-error.h
@@ -6,3 +6,4 @@
 int device_add_errno(sd_device *dev, int error);
 int device_add_exit_status(sd_device *dev, int status);
 int device_add_signal(sd_device *dev, int signo);
+int device_broadcast_on_error(sd_device *dev, sd_device_monitor *monitor);
