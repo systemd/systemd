a33ab1112fed2906ace04d4ba0679482bcb03fe4
Tommy Unger <tommyu@fb.com>
2025-05-11 14:00:44 -0700

    Cleanup

--- test/units/TEST-07-PID1.file-write.sh
+++ test/units/TEST-07-PID1.file-write.sh
@@ -5,12 +5,14 @@ set -Eeuo pipefail
 set -x
 
 # -----------------------------------------------------------------------------
+#
 # Test: PID-1 File-Write from Transient Unit Container
 #
 # Verifies that a minimal systemd PID 1 inside a tmpfs root can:
+#   • Bind mount the host's /usr read only
 #   • Bind mount a writable directory
 #   • Run a one-shot service in the container to create and
-#     write to host file in that directory
+#     write to a host file in that directory
 #   • Exit cleanly with systemd-run --wait propagating status
 #
 # -----------------------------------------------------------------------------
@@ -28,68 +30,90 @@ fi
 . "$(dirname "$0")"/util.sh
 
 # Mounts and directories to teardown and cleanup
-TO_UMOUNT=()
-TO_RM=()
+CLEANUP_MOUNTS=()
+CLEANUP_PATHS=()
 
 # Common Config:
-TEST_ID="TEST-07-PID1-file-write"
+TEST_NAME="TEST-07-PID1-file-write"
 OUTPUT_FILE="test-service-output"
-OUTPUT_CONTENTS='Test service is running'
-readonly TEST_ID OUTPUT_FILE OUTPUT_CONTENTS
+EXPECTED_OUTPUT="Test service is running"
+readonly TEST_NAME OUTPUT_FILE EXPECTED_OUTPUT
 
 # Host FS Directories
 # mktemp helps avoid name collision; using dry-run mode
-CONTAINER_ROOT_DIR=$(mktemp -u -d --tmpdir "${TEST_ID}-root-XXXX")
-HOST_MOUNT_DIR=$(mktemp -u -d --tmpdir test-dir-XXXX)
-readonly CONTAINER_ROOT_DIR HOST_MOUNT_DIR
+CONTAINER_ROOT_FS=$(mktemp -u -d --tmpdir "${TEST_NAME}-root-XXXX")
+HOST_OUT_DIR=$(mktemp -u -d --tmpdir test-dir-XXXX)
+readonly CONTAINER_ROOT_FS HOST_OUT_DIR
 
 # Container FS Directories
-CONTAINER_MOUNT_DIR="/${TEST_ID}"
+CONTAINER_MOUNT_DIR="/${TEST_NAME}"
 readonly CONTAINER_MOUNT_DIR
+
+# Mount a dummy /proc FS which will not be passed to the container.
+temporary_mount_hack() {
+    # IMPORTANT: This is modeled after a workaround in TEST-07-PID1.private-pids.sh with
+    # a key difference. In private-pids, it's explained there must be at least 1 unmasked
+    # procfs mount on the host in order for /proc/ to be mounted by an UNPRIVILEGED user
+    # within the pid namespace. Note the host mount is not actually passed through to the
+    # container.
+    #
+    # The key difference here is that systemd-run is NOT launched with --user, it is a
+    # PRIVILEGED environment and should not hit a permissions error when attempting to
+    # mount /proc. Unfortunately, that's exactly what happens if you launch the container
+    # without first mounting a dummy unmasked /proc on the host.
+    #
+    # This likely indicates a bug in systemd. When this is addressed, this function can be
+    # removed.
+
+    local -r helper_proc=$(mktemp -d --tmpdir helper-proc-XXXX)
+    CLEANUP_PATHS+=("$helper_proc")
+
+    mount -t proc proc "$helper_proc"
+    CLEANUP_MOUNTS+=("$helper_proc")
+}
+
+# Mount 1) a writable directory for output; 2) a workaround so the container can
+# mount /proc; 3) a tmpfs to serve as the container's root FS; 4) the host's /usr
+# directory read only.
 
 make_mounts() {
     # Host bind mount for the output file. Systemd will make the container's version.
-    mkdir -p "$HOST_MOUNT_DIR"
-    TO_RM+=("$HOST_MOUNT_DIR")
+    mkdir -p "$HOST_OUT_DIR"
+    CLEANUP_PATHS+=("$HOST_OUT_DIR")
 
-    # Dummy procfs mount
-    # TODO: explain why this is needed
-
-    # This is a dummy procfs mount
-    local -r helper_proc=$(mktemp -d --tmpdir helper-proc-XXXX)
-    TO_RM+=("$helper_proc")
-
-    mount -t proc proc "$helper_proc"
-    TO_UMOUNT+=("$helper_proc")
+    temporary_mount_hack
 
     # Container root tmpfs mount
-    mkdir -p "$CONTAINER_ROOT_DIR"
-    TO_RM+=("$CONTAINER_ROOT_DIR")
-    mount -t tmpfs tmpfs "$CONTAINER_ROOT_DIR"
-    TO_UMOUNT+=("$CONTAINER_ROOT_DIR")
+    mkdir -p "$CONTAINER_ROOT_FS"
+    CLEANUP_PATHS+=("$CONTAINER_ROOT_FS")
+
+    mount -t tmpfs tmpfs "$CONTAINER_ROOT_FS"
+    CLEANUP_MOUNTS+=("$CONTAINER_ROOT_FS")
 
     # Container's /usr will be a read-only bind mount of the host's /usr
     # Tried using -p BindReadOnlyPaths=/usr instead of this, but that didn't work.
     # Debugging that got hairy, so I'm going with this for now.
-    mkdir -p "${CONTAINER_ROOT_DIR}/usr"
+    mkdir -p "${CONTAINER_ROOT_FS}/usr"
 
-    mount --bind /usr "${CONTAINER_ROOT_DIR}/usr"
-    mount -o remount,bind,ro "${CONTAINER_ROOT_DIR}/usr"
+    mount --bind /usr "${CONTAINER_ROOT_FS}/usr"
+    mount -o remount,bind,ro "${CONTAINER_ROOT_FS}/usr"
+
     # Make sure /root/usr is unmounted before /root.
-    # Don't add to TO_RM because it will be removed when /root is.
-    TO_UMOUNT=( "${CONTAINER_ROOT_DIR}/usr" "${TO_UMOUNT[@]}" )
+    # Don't add to CLEANUP_PATHS because it will be removed when /root is.
+    CLEANUP_MOUNTS=( "${CONTAINER_ROOT_FS}/usr" "${CLEANUP_MOUNTS[@]}" )
 
 }
 
 # Create a test-service unit file that will run via the container's systemd and write the output file.
 config_container_service() {
-    local -r container_systemd_dir="${CONTAINER_ROOT_DIR}/etc/systemd/system"
-    local -r service_output="${CONTAINER_MOUNT_DIR}/${OUTPUT_FILE}"
+    local -r container_systemd_dir="${CONTAINER_ROOT_FS}/etc/systemd/system"
+    local -r guest_output="${CONTAINER_MOUNT_DIR}/${OUTPUT_FILE}"
     local -r internal_test_service="${container_systemd_dir}/test-service.service"
 
     mkdir -p "$container_systemd_dir"
-    # Generate a phony machine-id for the container
-    uuidgen -r | tr -d '-' | tr '[:upper:]' '[:lower:]' > "${CONTAINER_ROOT_DIR}/etc/machine-id"
+
+    # Generate a phony random machine-id for the container
+    uuidgen -r | tr -d '-' | tr '[:upper:]' '[:lower:]' > "${CONTAINER_ROOT_FS}/etc/machine-id"
 
     cat <<EOF >"$internal_test_service"
 [Unit]
@@ -98,7 +122,7 @@ After=basic.target
 
 [Service]
 Type=oneshot
-ExecStart=/bin/sh -c 'echo "$OUTPUT_CONTENTS"  > "$service_output"'
+ExecStart=/bin/sh -c 'echo "$EXPECTED_OUTPUT"  > "$guest_output"'
 ExecStartPost=/usr/bin/systemctl --no-block exit 0
 TimeoutStopSec=15s
 
@@ -106,15 +130,16 @@ TimeoutStopSec=15s
 WantedBy=multi-user.target
 EOF
     # NOTE: This warns with "test-service.service is added as a dependency to a non-existent unit multi-user.target."
-    systemctl --root="$CONTAINER_ROOT_DIR" enable test-service.service
+    systemctl --root="$CONTAINER_ROOT_FS" enable test-service.service
 }
 
-# Start the container as a transient unit and wait for it to finish. Check that the output file is written
+# The testcase. Configs cleanup trap, makes mounts, configs internal service unit,
+# kicks off container as a transient unit, waits for it to finish and checks output.
 testcase_container_file_write() {
 
     # Cleanup on exit. Test cases seem to run in a subshell, and only a single
     # testcase is expected in this file. So we tie cleanup to the lifetime of
-    # this subshell, not the global context, allowing for appending to TO_RM and TO_UMOUNT
+    # this subshell, not the global context, allowing for appending to CLEANUP_PATHS and CLEANUP_MOUNTS
     trap file_write_cleanup EXIT ERR INT TERM
 
     make_mounts
@@ -122,14 +147,14 @@ testcase_container_file_write() {
     config_container_service
 
     # Run the container as a transient unit and wait for it to finish
-    local -r bind_mount_arg="${HOST_MOUNT_DIR}:${CONTAINER_MOUNT_DIR}"
-    local -r file_wr_service="${TEST_ID}.service"
+    local -r bind_mount_arg="${HOST_OUT_DIR}:${CONTAINER_MOUNT_DIR}"
+    local -r service_unit_name="${TEST_NAME}.service"
 
     # SYSTEMD_LOG_LEVEL=debug SYSTEMD_LOG_TARGET=console \
     systemd-run \
-    --unit "$file_wr_service" \
+    --unit "$service_unit_name" \
     --wait \
-    -p RootDirectory="$CONTAINER_ROOT_DIR" \
+    -p RootDirectory="$CONTAINER_ROOT_FS" \
     -p PrivatePIDs=yes \
     -p PrivateUsersEx=full \
     -p ProtectHostnameEx=private \
@@ -148,9 +173,9 @@ testcase_container_file_write() {
     -p BindPaths="$bind_mount_arg" \
     /usr/lib/systemd/systemd multi-user.target
 
-    local -r host_output="${HOST_MOUNT_DIR}/${OUTPUT_FILE}"
     # If our service ran, we should be able to read its output here
-    assert_eq "$(cat "${host_output}")" "$OUTPUT_CONTENTS"
+    local -r host_output="${HOST_OUT_DIR}/${OUTPUT_FILE}"
+    assert_eq "$(cat "${host_output}")" "$EXPECTED_OUTPUT"
 }
 
 CLEANUP_DONE=0
@@ -161,8 +186,9 @@ file_write_cleanup() {
     set +e
 
     # Remove all the mounts and directories we created
-    umount "${TO_UMOUNT[@]}"
-    rm -rf "${TO_RM[@]}"
+    # These variables reset to empty arrays when the subprocess concludes.
+    umount "${CLEANUP_MOUNTS[@]}"
+    rm -rf "${CLEANUP_PATHS[@]}"
 
 }
 
