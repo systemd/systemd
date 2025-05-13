4ab07f5050c4a13f4f0b5358cf0f8854d25b417b
Daan De Meyer <daan.j.demeyer@gmail.com>
2025-05-13 11:59:04 +0200

    WIP

--- src/systemctl/systemctl-trivial-method.c
+++ src/systemctl/systemctl-trivial-method.c
@@ -1,7 +1,11 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 
+#include "sd-bus.h"
+
 #include "bus-error.h"
 #include "bus-locator.h"
+#include "log.h"
+#include "string-util.h"
 #include "systemctl.h"
 #include "systemctl-trivial-method.h"
 #include "systemctl-util.h"
