a31edb4e71517e580ec2b2444ee0b6d8a9ba5274
Yu Watanabe <watanabe.yu+github@gmail.com>
2025-05-15 04:02:26 +0900

    TEST-07-PID1: drop journal message flood test
    This partially reverts f9b186c21aa500eb7da3de0dcbd4cc1f07589a5a.
    
    The test case is unstable, and fills debugging logs after journald
    is restarted.

--- test/units/TEST-07-PID1.startv.sh
+++ test/units/TEST-07-PID1.startv.sh
@@ -1,39 +1,7 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: LGPL-2.1-or-later
-# shellcheck disable=SC2016
 set -eux
 set -o pipefail
 
-# shellcheck source=test/units/util.sh
-. "$(dirname "$0")"/util.sh
-
 systemd-run -v --wait echo wampfl | grep wampfl
-
-systemd-run -v -p Type=notify bash -c 'echo brumfl ; systemd-notify --ready ; echo krass' |  grep brumfl
-
-mkdir -p /run/systemd/journald.conf.d/
-
-# Let's disable storage of debug messages, since we want to flood the journal
-# daemon with messages that it will have to process, but we do not actually
-# want to push out our own messages from storage while doing so
-cat >> /run/systemd/journald.conf.d/50-disable-debug.conf <<EOF
-[Journal]
-MaxLevelStore=info
-EOF
-
-systemctl restart systemd-journald
-
-# Now flood the journal via syslog and the stream transport to ensure this finishes correctly even if busy
-( xxd /dev/urandom | logger -p debug ) &
-( xxd /dev/urandom | systemd-cat -p debug ) &
-
-# Verify that this works even if the journal is super busy
-systemd-run -v -p Type=notify bash -c 'echo schmurz ; systemd-notify --ready ; echo kropf' |  grep schmurz
-
-kill %1
-kill %2
-
-rm /run/systemd/journald.conf.d/50-disable-debug.conf
-rmdir --ignore-fail-on-non-empty /run/systemd/journald.conf.d
-
-systemctl restart systemd-journald
+systemd-run -v --service-type=notify bash -c 'echo brumfl ; systemd-notify --ready ; echo krass' | grep brumfl
