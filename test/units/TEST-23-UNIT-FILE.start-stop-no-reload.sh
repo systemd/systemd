6fdc3a6b4a7779389cb13b9b01fb1815dd97e7fc
Yu Watanabe <watanabe.yu+github@gmail.com>
2025-05-15 13:45:13 +0900

    test: add test case for issue #36031

--- test/units/TEST-23-UNIT-FILE.start-stop-no-reload.sh
+++ test/units/TEST-23-UNIT-FILE.start-stop-no-reload.sh
@@ -10,7 +10,12 @@ set -o pipefail
 at_exit() {
     set +e
 
-    rm -f /run/systemd/system/TEST-23-UNIT-FILE-no-reload.{service,target}
+    rm -f /run/systemd/system/TEST-23-UNIT-FILE-no-reload.target
+    rm -f /run/systemd/system/TEST-23-UNIT-FILE-no-reload.service
+    rm -f /run/systemd/system/TEST-23-UNIT-FILE-no-reload-2.service
+    systemctl stop TEST-23-UNIT-FILE-no-reload.target
+    systemctl stop TEST-23-UNIT-FILE-no-reload.service
+    systemctl stop TEST-23-UNIT-FILE-no-reload-2.service
 }
 
 trap at_exit EXIT
@@ -91,3 +96,42 @@ EOF
 systemctl restart TEST-23-UNIT-FILE-no-reload.target
 
 systemctl is-active TEST-23-UNIT-FILE-no-reload.service
+
+# Stop and remove, and try again to exercise https://github.com/systemd/systemd/issues/36031
+systemctl stop TEST-23-UNIT-FILE-no-reload.service TEST-23-UNIT-FILE-no-reload.target
+rm -f /run/systemd/system/TEST-23-UNIT-FILE-no-reload.service /run/systemd/system/TEST-23-UNIT-FILE-no-reload.target
+systemctl daemon-reload
+
+sleep 3.1
+
+cat >/run/systemd/system/TEST-23-UNIT-FILE-no-reload.target <<EOF
+[Unit]
+Conflicts=shutdown.target
+EOF
+
+cat >/run/systemd/system/TEST-23-UNIT-FILE-no-reload.service <<EOF
+[Unit]
+Conflicts=TEST-23-UNIT-FILE-no-reload.target
+Wants=TEST-23-UNIT-FILE-no-reload-2.service
+[Service]
+ExecStart=sleep infinity
+EOF
+
+systemctl daemon-reload
+
+systemctl start TEST-23-UNIT-FILE-no-reload.service
+systemctl is-active TEST-23-UNIT-FILE-no-reload.service
+
+cat >/run/systemd/system/TEST-23-UNIT-FILE-no-reload-2.service <<EOF
+[Unit]
+Conflicts=TEST-23-UNIT-FILE-no-reload.target
+[Service]
+ExecStart=sleep infinity
+EOF
+
+systemctl start TEST-23-UNIT-FILE-no-reload.target
+(! systemctl is-active TEST-23-UNIT-FILE-no-reload.service )
+(! systemctl is-active TEST-23-UNIT-FILE-no-reload-2.service )
+[[ "$(systemctl show --property LoadState --value systemctl is-active TEST-23-UNIT-FILE-no-reload-2.service)" =~ loaded ]]
+[[ "$(systemctl show --property ConflictedBy --value systemctl is-active TEST-23-UNIT-FILE-no-reload.target)" =~ TEST-23-UNIT-FILE-no-reload.service ]]
+[[ "$(systemctl show --property ConflictedBy --value systemctl is-active TEST-23-UNIT-FILE-no-reload.target)" =~ TEST-23-UNIT-FILE-no-reload-2.service ]]
