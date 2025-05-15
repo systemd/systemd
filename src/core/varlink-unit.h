cf89066d7c909cf1f0c01c588c640ea00b7b8be2
Ivan Kruglov <mail@ikruglov.com>
2025-05-15 03:04:28 -0700

    core: add basic lookup by name/pidref in io.systemd.Unit.List

--- src/core/varlink-unit.h
+++ src/core/varlink-unit.h
@@ -3,4 +3,6 @@
 
 #include "sd-varlink.h"
 
+#define VARLINK_ERROR_UNIT_NO_SUCH_UNIT "io.systemd.Unit.NoSuchUnit"
+
 int vl_method_list_units(sd_varlink *link, sd_json_variant *parameters, sd_varlink_method_flags_t flags, void *userdata);
