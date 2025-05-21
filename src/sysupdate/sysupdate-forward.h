8365affeed6cbd11edceea7512892976f9bf9963
Daan De Meyer <daan.j.demeyer@gmail.com>
2025-05-21 13:56:03 +0200

    sysupdate: Clean up includes
    Split out of #37344.

---
+++ src/sysupdate/sysupdate-forward.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#pragma once
+
+#include "forward.h" /* IWYU pragma: export */
+
+typedef struct Context Context;
+typedef struct PartitionInfo PartitionInfo;
+typedef struct Resource Resource;
+typedef struct Instance Instance;
+typedef struct InstanceMetadata InstanceMetadata;
