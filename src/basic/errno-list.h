5a881f7f71c8ac705767db20baa56f7f8db721a0
Mike Yuan <me@yhndnzj.com>
2025-05-26 20:19:59 +0200

    errno-list: errno_from_name() is pure but not const
    Follow-up for 0c15577abe013e07dba47d5aac126a63ab2dfd33

--- src/basic/errno-list.h
+++ src/basic/errno-list.h
@@ -4,8 +4,7 @@
 #include "forward.h"
 
 const char* errno_to_name(int id);
-
-int errno_from_name(const char *name) _const_;
+int errno_from_name(const char *name) _pure_;
 
 static inline bool errno_is_valid(int n) {
         return n > 0 && n <= ERRNO_MAX;
