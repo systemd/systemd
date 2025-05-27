75625d4576290c0d7275944f7a276e38a2ca2f5e
Yu Watanabe <watanabe.yu+github@gmail.com>
2025-05-27 23:31:48 +0900

    test-network: add test case for issue #37629

--- test/test-network/systemd-networkd-tests.py
+++ test/test-network/systemd-networkd-tests.py
@@ -5775,6 +5775,10 @@ class NetworkdBondTests(unittest.TestCase, Utilities):
         print(output)
         self.assertRegex(output, 'master bond199')
 
+        # Test case for #37629
+        check_output('ip link set dummy98 nomaster')
+        self.wait_online('dummy98:carrier')
+
     def test_bond_active_slave(self):
         copy_network_unit('23-active-slave.network', '23-bond199.network', '25-bond-active-backup-slave.netdev', '12-dummy.netdev')
         start_networkd()
