a4f840287ec1a3d98076053ecaadb63672ed7bfe
Mike Yuan <me@yhndnzj.com>
2025-05-28 22:30:39 +0200

    ssh-generator: fix typo
    (cherry picked from commit 0aa4c3f0f90f22b566b3c1002ccdce5ab5fb4062)

--- src/ssh-generator/ssh-generator.c
+++ src/ssh-generator/ssh-generator.c
@@ -455,7 +455,7 @@ static int run(const char *dest, const char *dest_early, const char *dest_late) 
         strv_sort_uniq(arg_listen_extra);
 
         if (!arg_auto && strv_isempty(arg_listen_extra)) {
-                log_debug("Disabling SSH generator logic, because as it has been turned off explicitly.");
+                log_debug("Disabling SSH generator logic, because it has been turned off explicitly.");
                 return 0;
         }
 
