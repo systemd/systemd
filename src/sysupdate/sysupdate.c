5cfc9c34f4a38cc82c233e453373d48231944d81
Luca Boccassi <luca.boccassi@gmail.com>
2025-05-29 00:36:47 +0100

    sysupdate: change status once operation has completed
    Otherwise after the service exits it will still show
    "Installing 'foobar'" as the status, which is confusing

--- src/sysupdate/sysupdate.c
+++ src/sysupdate/sysupdate.c
@@ -1088,6 +1088,9 @@ static int context_apply(
 
         log_info("%s Successfully installed update '%s'.", glyph(GLYPH_SPARKLES), us->version);
 
+        (void) sd_notifyf(/* unset_environment=*/ false,
+                          "STATUS=Installed '%s'.", us->version);
+
         if (ret_applied)
                 *ret_applied = us;
 
