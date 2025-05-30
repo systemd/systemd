6f109efcffa7dc2115f44124a53a6a78d7032355
Luca Boccassi <luca.boccassi@gmail.com>
2025-05-30 22:08:39 +0100

    test: wait for coredump to appear before parsing
    A new core was added to the test, but the loop counter was not increased
    to wait for it, so the test races against systemd-coredump's processing.
    
    This failed at least once in debci:
    
    8015s [   32.227813] TEST-87-AUX-UTILS-VM.sh[1038]: + coredumpctl info COREDUMP_TIMESTAMP=1679509902000000
    8015s [   32.228684] TEST-87-AUX-UTILS-VM.sh[1723]: No coredumps found.
    
    Follow-up for 0c49e0049b7665bb7769a13ef346fef92e1ad4d6
    
    Fixes https://github.com/systemd/systemd/issues/37666
    
    (cherry picked from commit 703fbb0bba7f1a1b46342d6efedda30e51d46135)
    (cherry picked from commit ccbad62c42ab8e00b824aaa22036cc5cc65ab203)

--- test/units/TEST-74-AUX-UTILS.coredump.sh
+++ test/units/TEST-74-AUX-UTILS.coredump.sh
@@ -202,7 +202,7 @@ journalctl -b -n 1 --output=export --output-fields=MESSAGE,COREDUMP COREDUMP_EXE
 journalctl -b -n 1 --output=export --output-fields=MESSAGE,COREDUMP COREDUMP_EXE="/usr/bin/test-dump" |
     /usr/lib/systemd/systemd-coredump --backtrace $$ 0 0 6 1679509902 12345 youmachine 1
 # Wait a bit for the coredumps to get processed
-timeout 30 bash -c "while [[ \$(coredumpctl list -q --no-legend $$ | wc -l) -lt 2 ]]; do sleep 1; done"
+timeout 30 bash -c "while [[ \$(coredumpctl list -q --no-legend $$ | wc -l) -lt 3 ]]; do sleep 1; done"
 coredumpctl info $$
 coredumpctl info COREDUMP_TIMESTAMP=1679509900000000
 coredumpctl info COREDUMP_TIMESTAMP=1679509901000000
