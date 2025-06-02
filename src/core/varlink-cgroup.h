1e5eae89fde5534570e44e097e4eaf623de92387
Ivan Kruglov <mail@ikruglov.com>
2025-06-02 20:04:19 +0200

    core: adding CGroup runtime for io.systemd.Unit.List

--- src/core/varlink-cgroup.h
+++ src/core/varlink-cgroup.h
@@ -3,3 +3,4 @@
 #include "sd-json.h"
 
 int unit_cgroup_context_build_json(sd_json_variant **ret, const char *name, void *userdata);
+int unit_cgroup_runtime_build_json(sd_json_variant **ret, const char *name, void *userdata);
