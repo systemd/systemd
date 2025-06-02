dffde7708a3f731766f529f9b210f094bd494c78
Ivan Kruglov <mail@ikruglov.com>
2025-06-02 14:12:44 +0200

    core: adding CGroup context for io.systemd.Unit.List

---
+++ src/core/varlink-cgroup.h
@@ -0,0 +1,6 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#pragma once
+
+#include "forward.h"
+
+int unit_cgroup_context_build_json(sd_json_variant **ret, const char *name, void *userdata);
