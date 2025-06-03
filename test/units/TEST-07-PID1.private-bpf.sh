cc3513f1552ac2c8cc569d6a825f47eb433c9d66
Matteo Croce <teknoraver@meta.com>
2025-06-03 15:02:50 +0200

    [TEST] add error messages to debug in the CI

--- test/units/TEST-07-PID1.private-bpf.sh
+++ test/units/TEST-07-PID1.private-bpf.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: LGPL-2.1-or-later
-set -eux
+set -ux
 set -o pipefail
 
 systemd-run -p PrivateBPF=no --wait true
-systemd-run -p PrivateBPF=yes --wait true
+if ! systemd-run -p PrivateBPF=yes --wait true; then
+        journalctl --no-pager -p info |tail -40
+        journalctl --no-pager -p warning |tail -10
+        i=10
+        while [ $i -gt 0 ]; do
+                echo "ls bpf, try $i"
+                ls -ld /sys /sys/fs /sys/fs/bpf && break
+                i=$((i - 1))
+                sleep 1
+        done
+        exit 1
+fi
