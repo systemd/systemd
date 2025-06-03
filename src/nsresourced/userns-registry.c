2dd066650f4a7f1525d2498cc3f50e254f165e81
Jelle van der Waa <jvanderwaa@redhat.com>
2025-06-03 10:49:17 +0200

    cgroup-util: rename 'fsfd' -> 'cgroups_fd'
    Makes the header and implementation consistent.

--- src/nsresourced/userns-registry.c
+++ src/nsresourced/userns-registry.c
@@ -639,7 +639,7 @@ static int userns_destroy_cgroup(uint64_t cgroup_id) {
         _cleanup_close_ int cgroup_fd = -EBADF, parent_fd = -EBADF;
         int r;
 
-        cgroup_fd = cg_cgroupid_open(/* cgroupfsfd= */ -EBADF, cgroup_id);
+        cgroup_fd = cg_cgroupid_open(/* cgroupfs_fd= */ -EBADF, cgroup_id);
         if (cgroup_fd == -ESTALE) {
                 log_debug_errno(cgroup_fd, "Control group %" PRIu64 " already gone, ignoring.", cgroup_id);
                 return 0;
