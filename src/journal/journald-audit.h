1b4645d762d0d31e46173a389742978c32700901
Daan De Meyer <daan.j.demeyer@gmail.com>
2025-06-07 19:23:45 +0200

    forward: Drop socklen_t forward declaration
    This is glibc specific and doesn't exist on musl, since removing only
    means adding one more include in selinux-util.h, let's drop it.
    
    Fixes #37779

--- src/journal/journald-audit.h
+++ src/journal/journald-audit.h
@@ -1,6 +1,8 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 #pragma once
 
+#include <sys/socket.h>
+
 #include "journald-forward.h"
 
 void manager_process_audit_message(Manager *m, const void *buffer, size_t buffer_size, const struct ucred *ucred, const union sockaddr_union *sa, socklen_t salen);
