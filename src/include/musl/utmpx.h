5112c2850aa060a58fc32ceecf5a4be16a5c6d43
Yu Watanabe <watanabe.yu+github@gmail.com>
2025-06-09 05:13:16 +0900

    musl: add several missing utmpx related macros
    This is to avoid build failures like the following:
      test-utmp.c:30:39: error: ‘ACCOUNTING’ undeclared (first use in this function)

---
+++ src/include/musl/utmpx.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#pragma once
+
+#include_next <utmpx.h>
+
+#ifndef ACCOUNTING
+#define ACCOUNTING 9
+#endif
+#ifndef UTMPX_FILE
+#define UTMPX_FILE _PATH_UTMP
+#endif
+#ifndef WTMPX_FILE
+#define WTMPX_FILE _PATH_WTMP
+#endif
