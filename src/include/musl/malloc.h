2b28e648182397caa0d66d6a0a4bbab3b9d33861
Yu Watanabe <watanabe.yu+github@gmail.com>
2025-06-10 00:40:59 +0900

    musl: introduce dummy functions for mallinfo2(), malloc_info(), and malloc_trim()
    These functions are not provided by musl.

---
+++ src/include/musl/malloc.h
@@ -0,0 +1,36 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#pragma once
+
+#include <errno.h>
+#include <stdio.h>
+
+#include_next <malloc.h>
+
+struct mallinfo2 {
+        size_t arena;    /* non-mmapped space allocated from system */
+        size_t ordblks;  /* number of free chunks */
+        size_t smblks;   /* number of fastbin blocks */
+        size_t hblks;    /* number of mmapped regions */
+        size_t hblkhd;   /* space in mmapped regions */
+        size_t usmblks;  /* always 0, preserved for backwards compatibility */
+        size_t fsmblks;  /* space available in freed fastbin blocks */
+        size_t uordblks; /* total allocated space */
+        size_t fordblks; /* total free space */
+        size_t keepcost; /* top-most, releasable (via malloc_trim) space */
+};
+
+static inline struct mallinfo2 mallinfo2(void) {
+        return (struct mallinfo2) {};
+}
+
+static inline int malloc_info(int options, FILE *stream) {
+        if (options != 0)
+                errno = EINVAL;
+        else
+                errno = EOPNOTSUPP;
+        return -1;
+}
+
+static inline int malloc_trim(size_t pad) {
+        return 0;
+}
