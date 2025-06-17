695921d7f85353017dd96a1680928d3a7cd535fe
Yu Watanabe <watanabe.yu+github@gmail.com>
2025-06-18 08:58:34 +0900

    basic: move missing_random.h -> include/sys/random.h

--- src/basic/random-util.c
+++ src/basic/random-util.c
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <sys/auxv.h>
 #include <sys/ioctl.h>
+#include <sys/random.h>
 #include <threads.h>
 #include <unistd.h>
 
@@ -15,7 +16,6 @@
 #include "io-util.h"
 #include "iovec-util.h"
 #include "log.h"
-#include "missing_random.h"
 #include "parse-util.h"
 #include "pidfd-util.h"
 #include "process-util.h"
