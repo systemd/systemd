57b6ac5bbf3ad736c0739c65db37f7156101188e
Yu Watanabe <watanabe.yu+github@gmail.com>
2025-06-18 09:02:43 +0900

    basic: move missing_wait.h -> include/sys/wait.h

--- src/basic/include/sys/wait.h
+++ src/basic/include/sys/wait.h
@@ -1,13 +1,11 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 #pragma once
 
-#include <sys/wait.h> /* IWYU pragma: export */
-
-#include "forward.h"
+#include_next <sys/wait.h>
 
 /* since glibc-2.36 */
 #ifndef P_PIDFD
 #  define P_PIDFD 3
 #else
-assert_cc(P_PIDFD == 3);
+_Static_assert(P_PIDFD == 3, "");
 #endif
