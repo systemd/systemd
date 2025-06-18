a87c8937b578ccbe02c0a76aae145507ae7f283b
Yu Watanabe <watanabe.yu+github@gmail.com>
2025-06-19 04:51:04 +0900

    missing_syscall: move prototypes to relevant glibc header wrapper
    This also imports linux/xattr.h and linux/sched/types.h to make
    `struct xattr_args` and `struct sched_attr` defined.

--- src/basic/missing_syscall.c
+++ src/basic/missing_syscall.c
@@ -1,81 +1,72 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
-#pragma once
 
 /* Missing glibc definitions to access certain kernel APIs */
 
 #include <sched.h>
-#include <signal.h>
+#include <sys/pidfd.h>
+#include <sys/quota.h>
+#include <sys/stat.h>
 #include <sys/syscall.h>
+#include <sys/xattr.h>
 #include <unistd.h>
 
 #ifdef ARCH_MIPS
 #include <asm/sgidefs.h>
 #endif
 
-#include "forward.h"
 #include "missing_syscall_def.h"
 
 /* ======================================================================= */
 
 #if !HAVE_FCHMODAT2
 /* since kernel v6.6 (78252deb023cf0879256fcfbafe37022c390762b) */
-static inline int missing_fchmodat2(int dirfd, const char *path, mode_t mode, int flags) {
+int fchmodat2(int dirfd, const char *path, mode_t mode, int flags) {
         return syscall(__NR_fchmodat2, dirfd, path, mode, flags);
 }
-
-#  define fchmodat2 missing_fchmodat2
 #endif
 
 /* ======================================================================= */
 
 #if !HAVE_PIVOT_ROOT
-static inline int missing_pivot_root(const char *new_root, const char *put_old) {
+int pivot_root(const char *new_root, const char *put_old) {
         return syscall(__NR_pivot_root, new_root, put_old);
 }
-
-#  define pivot_root missing_pivot_root
 #endif
 
 /* ======================================================================= */
 
 #if !HAVE_PIDFD_SEND_SIGNAL
 /* since kernel v5.1 (3eb39f47934f9d5a3027fe00d906a45fe3a15fad) */
-static inline int missing_pidfd_send_signal(int fd, int sig, siginfo_t *info, unsigned flags) {
+int pidfd_send_signal(int fd, int sig, siginfo_t *info, unsigned flags) {
         return syscall(__NR_pidfd_send_signal, fd, sig, info, flags);
 }
-
-#  define pidfd_send_signal missing_pidfd_send_signal
 #endif
 
 /* ======================================================================= */
 
 #if !HAVE_PIDFD_OPEN
 /* since kernel v5.3 (7615d9e1780e26e0178c93c55b73309a5dc093d7) */
-static inline int missing_pidfd_open(pid_t pid, unsigned flags) {
+int pidfd_open(pid_t pid, unsigned flags) {
         return syscall(__NR_pidfd_open, pid, flags);
 }
-
-#  define pidfd_open missing_pidfd_open
 #endif
 
 /* ======================================================================= */
 
 #if !HAVE_EXECVEAT
 /* since kernel v3.19 (51f39a1f0cea1cacf8c787f652f26dfee9611874) */
-static inline int missing_execveat(int dirfd, const char *pathname,
+int execveat(int dirfd, const char *pathname,
                                    char *const argv[], char *const envp[],
                                    int flags) {
         return syscall(__NR_execveat, dirfd, pathname, argv, envp, flags);
 }
-
-#  define execveat missing_execveat
 #endif
 
 /* ======================================================================= */
 
 #if !HAVE_CLOSE_RANGE
 /* since kernel v5.9 (9b4feb630e8e9801603f3cab3a36369e3c1cf88d) */
-static inline int missing_close_range(unsigned first_fd, unsigned end_fd, unsigned flags) {
+int close_range(unsigned first_fd, unsigned end_fd, unsigned flags) {
         /* Kernel-side the syscall expects fds as unsigned integers (just like close() actually), while
          * userspace exclusively uses signed integers for fds. glibc chose to expose it 1:1 however, hence we
          * do so here too, even if we end up passing signed fds to it most of the time. */
@@ -84,56 +75,40 @@ static inline int missing_close_range(unsigned first_fd, unsigned end_fd, unsign
                        end_fd,
                        flags);
 }
-
-#  define close_range missing_close_range
 #endif
 
 /* ======================================================================= */
 
 #if !HAVE_SCHED_SETATTR
 /* since kernel 3.14 (e6cfc0295c7d51b008999a8b13a44fb43f8685ea) */
-static inline ssize_t missing_sched_setattr(pid_t pid, struct sched_attr *attr, unsigned int flags) {
+int sched_setattr(pid_t pid, struct sched_attr *attr, unsigned int flags) {
         return syscall(__NR_sched_setattr, pid, attr, flags);
 }
-
-#  define sched_setattr missing_sched_setattr
 #endif
 
 /* ======================================================================= */
 
 #if !HAVE_QUOTACTL_FD
 /* since kernel v5.14 (64c2c2c62f92339b176ea24403d8db16db36f9e6) */
-static inline int missing_quotactl_fd(int fd, int cmd, int id, void *addr) {
+int quotactl_fd(int fd, int cmd, int id, void *addr) {
         return syscall(__NR_quotactl_fd, fd, cmd, id, addr);
 }
-
-#  define quotactl_fd missing_quotactl_fd
 #endif
 
 /* ======================================================================= */
 
 #if !HAVE_SETXATTRAT
 /* since kernel v6.13 (6140be90ec70c39fa844741ca3cc807dd0866394) */
-struct xattr_args {
-        _align_(8) uint64_t value;
-        uint32_t size;
-        uint32_t flags;
-};
-
-static inline int missing_setxattrat(int fd, const char *path, int at_flags, const char *name, const struct xattr_args *args, size_t size) {
+int setxattrat(int fd, const char *path, int at_flags, const char *name, const struct xattr_args *args, size_t size) {
         return syscall(__NR_setxattrat, fd, path, at_flags, name, args, size);
 }
-
-#  define setxattrat missing_setxattrat
 #endif
 
 /* ======================================================================= */
 
 #if !HAVE_REMOVEXATTRAT
 /* since kernel v6.13 (6140be90ec70c39fa844741ca3cc807dd0866394) */
-static inline int missing_removexattrat(int fd, const char *path, int at_flags, const char *name) {
+int removexattrat(int fd, const char *path, int at_flags, const char *name) {
         return syscall(__NR_removexattrat, fd, path, at_flags, name);
 }
-
-#  define removexattrat missing_removexattrat
 #endif
