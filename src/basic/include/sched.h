8e642b3fce3df5e8d9944855aa410eeba474dfd4
Yu Watanabe <watanabe.yu+github@gmail.com>
2025-06-20 05:40:29 +0900

    include: import linux/sched/types.h

--- src/basic/include/sched.h
+++ src/basic/include/sched.h
@@ -1,7 +1,8 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 #pragma once
 
-#include <linux/types.h>
+/* sched.h includes linux/sched/types.h since glibc-2.41. */
+#include <linux/sched/types.h>
 
 #include_next <sched.h>
 
@@ -23,22 +24,4 @@
  * Not exposed yet. Defined at include/linux/sched.h */
 #ifndef TASK_COMM_LEN
 #  define TASK_COMM_LEN 16
-#endif
-
-/* defined in sched.h since glibc-2.41. */
-#if !HAVE_STRUCT_SCHED_ATTR
-struct sched_attr {
-        __u32 size;             /* Size of this structure */
-        __u32 sched_policy;     /* Policy (SCHED_*) */
-        __u64 sched_flags;      /* Flags */
-        __s32  sched_nice;      /* Nice value (SCHED_OTHER,
-                                         SCHED_BATCH) */
-        __u32 sched_priority;   /* Static priority (SCHED_FIFO,
-                                       SCHED_RR) */
-        /* Remaining fields are for SCHED_DEADLINE
-           and potentially soon for SCHED_OTHER/SCHED_BATCH */
-        __u64 sched_runtime;
-        __u64 sched_deadline;
-        __u64 sched_period;
-};
 #endif
