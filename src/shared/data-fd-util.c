5cc22ddceea59908fc02e6c473f41b29a6ab56df
Yu Watanabe <watanabe.yu+github@gmail.com>
2025-06-20 03:24:12 +0900

    missing_fcntl: drop O_ACCMODE_STRICT
    This effectively reverts b1236ce38bb2f918c7150df36b32fcb84b1a23fb.
    
    It is musl specific. Moreover, we may easily use O_ACCMODE in a new
    code by mistake. Regardless if we should fix the musl specific issue in
    upstream, the fix anyway does not scale well.
    
    Let's fix the issue in a different way later.

--- src/shared/data-fd-util.c
+++ src/shared/data-fd-util.c
@@ -149,13 +149,13 @@ int memfd_clone_fd(int fd, const char *name, int mode) {
 
         assert(fd >= 0);
         assert(name);
-        assert(IN_SET(mode & O_ACCMODE_STRICT, O_RDONLY, O_RDWR));
+        assert(IN_SET(mode & O_ACCMODE, O_RDONLY, O_RDWR));
         assert((mode & ~(O_RDONLY|O_RDWR|O_CLOEXEC)) == 0);
 
         if (fstat(fd, &st) < 0)
                 return -errno;
 
-        ro = (mode & O_ACCMODE_STRICT) == O_RDONLY;
+        ro = (mode & O_ACCMODE) == O_RDONLY;
         exec = st.st_mode & 0111;
 
         mfd = memfd_create_wrapper(name,
