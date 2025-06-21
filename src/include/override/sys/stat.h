7029bf2542e9a4b0eeea43385785d3677c564391
Yu Watanabe <watanabe.yu+github@gmail.com>
2025-06-22 00:38:58 +0900

    include: add several missing statx macros for musl
    glibc's sys/stat.h includes linux/stat.h, and we have copy of it from
    the latest kernel, hence all new flags are always defined.
    However, musl's sys/stat.h does not include linux/stat.h, and moreover,
    they conflict with each other, hence we cannot include both header
    simultaneously. Let's define missing macros to support musl.
    
    Upstream-Status: Inappropriate [musl specific]
    Signed-off-by: Yu Watanabe <watanabe.yu+github@gmail.com>

--- src/include/override/sys/stat.h
+++ src/include/override/sys/stat.h
@@ -8,3 +8,19 @@
 int missing_fchmodat2(int dirfd, const char *path, mode_t mode, int flags);
 #  define fchmodat2 missing_fchmodat2
 #endif
+
+/* musl's sys/stat.h does not include linux/stat.h, and unfortunately they conflict with each other.
+ * Hence, some relatively new macros need to be explicitly defined here. */
+#ifndef STATX_SUBVOL
+#define STATX_SUBVOL            0x00008000U
+#endif
+#ifndef STATX_WRITE_ATOMIC
+#define STATX_WRITE_ATOMIC      0x00010000U
+#endif
+#ifndef STATX_DIO_READ_ALIGN
+#define STATX_DIO_READ_ALIGN    0x00020000U
+#endif
+
+#ifndef STATX_ATTR_WRITE_ATOMIC
+#define STATX_ATTR_WRITE_ATOMIC 0x00400000
+#endif
