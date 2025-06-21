0c9322fcc46930d4e19b79776e51a863994681d9
Yu Watanabe <watanabe.yu+github@gmail.com>
2025-06-21 20:29:51 +0900

    libc-wrapper: introduce a tiny libc wrapper
    Then, move syscall definitions to the wrapper, and prototypes are moved
    to relevant headers.
    
    This also adds checks for add_key() and request_key(), as one day
    glibc may be going to add some of them separatedly.
    
    The check for fspick in meson.build is dropped, as it is currently
    unused in our code.
    
    This also moves
    - basic/missing_bpf.h -> include/override/linux/bpf.h,
    - basic/missing_keyctl.h -> include/override/linux/keyctl.h.

--- src/include/override/sys/mount.h
+++ src/include/override/sys/mount.h
@@ -8,8 +8,6 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <sys/ioctl.h>
-#include <sys/syscall.h>
-#include <unistd.h>
 
 /* Since glibc-2.37 (774058d72942249f71d74e7f2b639f77184160a6), sys/mount.h includes linux/mount.h, and
  * we can safely include both headers in the same source file. However, we cannot do that with older glibc.
@@ -41,67 +39,56 @@ extern int umount2(const char *__special_file, int __flags) __THROW;
 
 /* Open the filesystem referenced by FS_NAME so it can be configured for
    mouting.  */
+/* Defined since glibc-2.36.
+ * Supported since kernel v5.2 (24dcb3d90a1f67fe08c68a004af37df059d74005). */
 #if HAVE_FSOPEN
 extern int fsopen(const char *__fs_name, unsigned int __flags) __THROW;
 #else
-static inline int missing_fsopen(const char *fsname, unsigned flags) {
-        return syscall(__NR_fsopen, fsname, flags);
-}
+int missing_fsopen(const char *fsname, unsigned flags);
 #  define fsopen missing_fsopen
 #endif
 
 /* Create a mount representation for the FD created by fsopen using
    FLAGS with ATTR_FLAGS describing how the mount is to be performed.  */
+/* Defined since glibc-2.36.
+ * Supported since kernel v5.2 (93766fbd2696c2c4453dd8e1070977e9cd4e6b6d). */
 #if HAVE_FSMOUNT
 extern int fsmount(int __fd, unsigned int __flags, unsigned int __ms_flags) __THROW;
 #else
-static inline int missing_fsmount(int fd, unsigned flags, unsigned ms_flags) {
-        return syscall(__NR_fsmount, fd, flags, ms_flags);
-}
+int missing_fsmount(int fd, unsigned flags, unsigned ms_flags);
 #  define fsmount missing_fsmount
 #endif
 
 /* Add the mounted FROM_DFD referenced by FROM_PATHNAME filesystem returned
    by fsmount in the hierarchy in the place TO_DFD reference by TO_PATHNAME
    using FLAGS.  */
+/* Defined since glibc-2.36.
+ * Supported since kernel v5.2 (2db154b3ea8e14b04fee23e3fdfd5e9d17fbc6ae). */
 #if HAVE_MOVE_MOUNT
 extern int move_mount(int __from_dfd, const char *__from_pathname, int __to_dfd, const char *__to_pathname, unsigned int flags) __THROW;
 #else
-static inline int missing_move_mount(
-                int from_dfd,
-                const char *from_pathname,
-                int to_dfd,
-                const char *to_pathname,
-                unsigned flags) {
-
-        return syscall(__NR_move_mount, from_dfd, from_pathname, to_dfd, to_pathname, flags);
-}
+int missing_move_mount(int from_dfd, const char *from_pathname, int to_dfd, const char *to_pathname, unsigned flags);
 #  define move_mount missing_move_mount
 #endif
 
 /* Set parameters and trigger CMD action on the FD context.  KEY, VALUE,
    and AUX are used depending ng of the CMD.  */
+/* Defined since glibc-2.36.
+ * Supported since kernel v5.2 (ecdab150fddb42fe6a739335257949220033b782). */
 #if HAVE_FSCONFIG
 extern int fsconfig(int __fd, unsigned int __cmd, const char *__key, const void *__value, int __aux) __THROW;
 #else
-static inline int missing_fsconfig(int fd, unsigned cmd, const char *key, const void *value, int aux) {
-        return syscall(__NR_fsconfig, fd, cmd, key, value, aux);
-}
+int missing_fsconfig(int fd, unsigned cmd, const char *key, const void *value, int aux);
 #  define fsconfig missing_fsconfig
 #endif
 
-/* Equivalent of fopen for an existing mount point.  */
-#if HAVE_FSPICK
-extern int fspick(int __dfd, const char *__path, unsigned int __flags) __THROW;
-#endif
-
 /* Open the mount point FILENAME in directory DFD using FLAGS.  */
+/* Defined since glibc-2.36.
+ * Supported since kernel v5.2 (a07b20004793d8926f78d63eb5980559f7813404). */
 #if HAVE_OPEN_TREE
 extern int open_tree(int __dfd, const char *__filename, unsigned int __flags) __THROW;
 #else
-static inline int missing_open_tree(int dfd, const char *filename, unsigned flags) {
-        return syscall(__NR_open_tree, dfd, filename, flags);
-}
+int missing_open_tree(int dfd, const char *filename, unsigned flags);
 #  define open_tree missing_open_tree
 #endif
 
@@ -110,21 +97,20 @@ static inline int missing_open_tree(int dfd, const char *filename, unsigned flag
    directory referred to by the file descriptor dirfd.  Otherwise if DFD is
    the special value AT_FDCWD then PATH is interpreted relative to the current
    working directory of the calling process.  */
+/* Defined since glibc-2.36.
+ * Supported since kernel v5.12 (2a1867219c7b27f928e2545782b86daaf9ad50bd). */
 #if HAVE_MOUNT_SETATTR
 extern int mount_setattr(int __dfd, const char *__path, unsigned int __flags, struct mount_attr *__uattr, size_t __usize) __THROW;
 #else
-static inline int missing_mount_setattr(int dfd, const char *path, unsigned flags, struct mount_attr *attr, size_t size) {
-        return syscall(__NR_mount_setattr, dfd, path, flags, attr, size);
-}
+int missing_mount_setattr(int dfd, const char *path, unsigned flags, struct mount_attr *attr, size_t size);
 #  define mount_setattr missing_mount_setattr
 #endif
 
+/* Not defined in glibc yet as of glibc-2.41.
+ * Supported since kernel v6.15 (c4a16820d90199409c9bf01c4f794e1e9e8d8fd8). */
 #if HAVE_OPEN_TREE_ATTR
 extern int open_tree_attr(int __dfd, const char *__filename, unsigned int __flags, struct mount_attr *__uattr, size_t __usize) __THROW;
 #else
-static inline int missing_open_tree_attr(int dfd, const char *filename, unsigned int flags, struct mount_attr *attr, size_t size) {
-        return syscall(__NR_open_tree_attr, dfd, filename, flags, attr, size);
-}
-
+int missing_open_tree_attr(int dfd, const char *filename, unsigned int flags, struct mount_attr *attr, size_t size);
 #  define open_tree_attr missing_open_tree_attr
 #endif
