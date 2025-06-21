c80dca37da1bacfabbdcfab811ce93d0f72dfd54
Yu Watanabe <watanabe.yu+github@gmail.com>
2025-06-21 22:30:06 +0900

    include: several tweaks in fcntl.h for musl
    - On musl, fcntl.h does not include bits/stat.h, while glibc does.
    
    - On musl, O_ACCMODE is defined as (03|O_SEARCH), unlike glibc which
      defines it as (O_RDONLY|O_WRONLY|O_RDWR). Additionally, O_SEARCH is
      simply defined as O_PATH.
      This is reimplementation of b1236ce38bb2f918c7150df36b32fcb84b1a23fb.
    
    Upstream-Status: Inappropriate [musl specific]
    Signed-off-by: Yu Watanabe <watanabe.yu+github@gmail.com>

--- src/basic/include/fcntl.h
+++ src/basic/include/fcntl.h
@@ -3,6 +3,17 @@
 
 #include_next <fcntl.h>
 
+/* glibc's fcntl.h includes bits/stat.h, hence struct stat can be used even if sys/stat.h is not included.
+ * However musl's fcntl.h does not. Let's explicitly include sys/stat.h here to make struct stat can be used. */
+#include <sys/stat.h>
+
+/* On musl, O_ACCMODE is defined as (03|O_SEARCH), unlike glibc which defines it as
+ * (O_RDONLY|O_WRONLY|O_RDWR). Additionally, O_SEARCH is simply defined as O_PATH. This changes the behaviour
+ * of O_ACCMODE in certain situations, which we don't want. This definition is copied from glibc and works
+ * around the problems with musl's definition. */
+#undef O_ACCMODE
+#define O_ACCMODE (O_RDONLY|O_WRONLY|O_RDWR)
+
 /* This is defined since glibc-2.41. */
 #ifndef F_DUPFD_QUERY
 #define F_DUPFD_QUERY 1027
