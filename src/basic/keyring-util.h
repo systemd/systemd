f0ad6b315f129d2a9170103d29ef32ff771d377f
Yu Watanabe <watanabe.yu+github@gmail.com>
2025-06-21 20:29:51 +0900

    libc-wrapper: introduce a tiny libc wrapper
    Then, move syscall definitions to the wrapper, and prototypes are moved
    to relevant headers.
    
    This also adds checks for add_key() and request_key(), as one day
    glibc may be going to add some of them separatedly.
    
    The check for fspick in meson.build is dropped, as it is currently
    unused in our code.
    
    This also moves
    - basic/missing_bpf.h -> include/override/linux/bpf.h,
    - basic/missing_keyctl.h -> include/override/linux/keyctl.h.

--- src/basic/keyring-util.h
+++ src/basic/keyring-util.h
@@ -1,8 +1,9 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 #pragma once
 
+#include <sys/keyctl.h>         /* IWYU pragma: export */
+
 #include "forward.h"
-#include "missing_keyctl.h"
 
 /* Like TAKE_PTR() but for key_serial_t, resetting them to -1 */
 #define TAKE_KEY_SERIAL(key_serial) TAKE_GENERIC(key_serial, key_serial_t, -1)
