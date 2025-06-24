bf15b99afd0e35d031e5ad006f472db282b06717
Matteo Croce <teknoraver@meta.com>
2025-06-25 01:33:04 +0200

    [TEST] print errno

--- src/test/test-bpf-token.c
+++ src/test/test-bpf-token.c
@@ -9,9 +9,13 @@ int main(void)
 {
 #if __LIBBPF_CURRENT_VERSION_GEQ(1, 5)
         int bpffs_fd = open("/sys/fs/bpf", O_RDONLY);
+        if (bpffs_fd < 0)
+                log_error_errno(errno, "open bpffs failed: %m");
         assert_se(bpffs_fd >= 0);
 
         int token_fd = bpf_token_create(bpffs_fd, NULL);
+        if (token_fd < 0)
+                log_error_errno(errno, "bpf_token_create failed: %m");
         assert_se(token_fd >= 0);
 
         return 0;
