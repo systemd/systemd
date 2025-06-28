92fca62c75e30e16f878eff432215d1366962eb9
Yu Watanabe <watanabe.yu+github@gmail.com>
2025-06-28 10:39:32 +0900

    TEST-17-UDEV: check the exit code at last
    Otherwise, it is hard to debug when the test failed.

--- test/units/TEST-17-UDEV.verify.sh
+++ test/units/TEST-17-UDEV.verify.sh
@@ -76,7 +76,6 @@ assert_1_impl() {
     udevadm verify "$@" >"${out}" 2>"${err}"
     rc=$?
     set -e
-    assert_eq "$rc" 1
 
     if [ -f "${exp}" ]; then
         diff -u "${exp}" "${err}"
@@ -87,6 +86,7 @@ assert_1_impl() {
     elif [ -f "${rules}" ]; then
         diff -u "${workdir}/default_output_1_fail" "${out}"
     fi
+    assert_eq "$rc" 1
 }
 
 assert_1() {
