892f91773385cc99876f1f3c983d70338e133f20
Lennart Poettering <lennart@poettering.net>
2025-06-30 23:13:26 +0200

    test: add testcase for unpriv machined nspawns reg + killing
    Let's add a superficial test for the code we just added: spawn a
    container unpriv, make sure registration fully worked, then kill it via
    machinectl, to ensure it all works properly.
    
    Not too thorough but a good start.

---
+++ test/units/TEST-13-NSPAWN.unpriv.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: LGPL-2.1-or-later
+# shellcheck disable=SC2016
+set -eux
+set -o pipefail
+
+# shellcheck source=test/units/util.sh
+. "$(dirname "$0")"/util.sh
+
+at_exit() {
+    rm -rf /home/testuser/.local/state/machines/zurps ||:
+    machinectl terminate zurps ||:
+}
+
+run0 -u testuser mkdir -p .local/state/machines
+
+create_dummy_container /home/testuser/.local/state/machines/zurps
+systemd-dissect --shift /home/testuser/.local/state/machines/zurps foreign
+
+# Install a PK rule that allows 'testuser' user to register a machine even
+# though they are not on an fg console, just for testing
+cat >/usr/share/polkit-1/rules.d/registermachinetest.rules <<'EOF'
+polkit.addRule(function(action, subject) {
+    if (action.id == "org.freedesktop.machine1.register-machine" &&
+        subject.user == "testuser") {
+        return polkit.Result.YES;
+    }
+});
+EOF
+
+run0 -u testuser systemctl start --user systemd-nspawn@zurps.service
+
+machinectl status zurps
+machinectl status zurps | grep "UID:" | grep "$(id -u)"
+machinectl status zurps | grep "Unit: user@" | grep "$(id -u)"
+machinectl status zurps | grep "Subgroup: machine.slice/systemd-nspawn@zurps.service/supervisor"
+machinectl terminate zurps
+
+(! run0 -u testuser systemctl is-active --user systemd-nspawn@zurps.service)
