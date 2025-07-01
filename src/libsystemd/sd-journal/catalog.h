1e29a967c76106db6980842b6bfb4b10f5b829c9
Yu Watanabe <watanabe.yu+github@gmail.com>
2025-07-01 11:33:22 +0900

    catalog: do not read catalog files outside of specified root directory

--- src/libsystemd/sd-journal/catalog.h
+++ src/libsystemd/sd-journal/catalog.h
@@ -3,7 +3,7 @@
 
 #include "forward.h"
 
-int catalog_import_file(OrderedHashmap **h, const char *path);
+int catalog_import_file(OrderedHashmap **h, int fd, const char *path);
 int catalog_update(const char *database, const char *root, const char* const *dirs);
 int catalog_get(const char *database, sd_id128_t id, char **ret_text);
 int catalog_list(FILE *f, const char *database, bool oneline);
