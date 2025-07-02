f1f93821f353644d226a03bcb9ae6d6259269294
Myrrh Periwinkle <myrrhperiwinkle@qtmlabs.xyz>
2025-07-02 12:53:15 +0700

    logind: Don't match non-leader processes for utmp TTY determination
    This ensures we don't erroneously assign pseudoterminals created by
    terminal emulators that use utempter to register themselves in utmp when
    run under a GUI session that doesn't have a TTY assigned.
    
    (cherry picked from commit 1a088b564aae220241ca2ef7f0d4ec732f97e29c)

--- src/login/logind-core.c
+++ src/login/logind-core.c
@@ -384,6 +384,28 @@ int manager_get_session_by_pidref(Manager *m, const PidRef *pid, Session **ret) 
         return !!s;
 }
 
+int manager_get_session_by_leader(Manager *m, const PidRef *pid, Session **ret) {
+        Session *s;
+        int r;
+
+        assert(m);
+
+        if (!pidref_is_set(pid))
+                return -EINVAL;
+
+        s = hashmap_get(m->sessions_by_leader, pid);
+        if (s) {
+                r = pidref_verify(pid);
+                if (r < 0)
+                        return r;
+        }
+
+        if (ret)
+                *ret = s;
+
+        return !!s;
+}
+
 int manager_get_user_by_pid(Manager *m, pid_t pid, User **ret) {
         _cleanup_free_ char *unit = NULL;
         User *u = NULL;
@@ -755,7 +777,7 @@ int manager_read_utmp(Manager *m) {
                 if (isempty(t))
                         continue;
 
-                if (manager_get_session_by_pidref(m, &PIDREF_MAKE_FROM_PID(u->ut_pid), &s) <= 0)
+                if (manager_get_session_by_leader(m, &PIDREF_MAKE_FROM_PID(u->ut_pid), &s) <= 0)
                         continue;
 
                 if (s->type != SESSION_TTY)
