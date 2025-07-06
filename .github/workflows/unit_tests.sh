a8f8b3efb51837a01197ad84b21182c6d4923131
Luca Boccassi <luca.boccassi@gmail.com>
2025-07-06 11:02:20 +0100

    ci: add test timeout multiplier for ppc64le
    The slow tests have timed out at least a couple of times,
    so add a multiplier
    
    1252/1633 systemd:libsystemd / test-sd-device      TIMEOUT 30.04s killed by signal 15 SIGTERM
    1633/1633 systemd:libsystemd / test-journal-verify TIMEOUT 90.01s killed by signal 15 SIGTERM
    
    Follow-up for 8a1d1341444aaf143108e0ca85741c779014d8b2

--- .github/workflows/unit_tests.sh
+++ .github/workflows/unit_tests.sh
@@ -91,11 +91,18 @@ for phase in "${PHASES[@]}"; do
                     mv /etc/machine-id /etc/machine-id.bak
                 fi
             fi
+
+            # On ppc64le the workers are slower and some slow tests time out
+            MESON_TEST_ARGS=()
+            if [[ "$(uname -m)" != "x86_64" ]] && [[ "$(uname -m)" != "aarch64" ]]; then
+                MESON_TEST_ARGS+=(--timeout-multiplier=3)
+            fi
+
             MESON_ARGS+=(--fatal-meson-warnings)
             run_meson -Dnobody-group=nogroup --werror -Dtests=unsafe -Dslow-tests=true -Dfuzz-tests=true "${MESON_ARGS[@]}" build
             ninja -C build -v
             # Ensure setting a timezone (like the reproducible build tests do) does not break time/date unit tests
-            TZ=GMT+12 meson test -C build --print-errorlogs
+            TZ=GMT+12 meson test "${MESON_TEST_ARGS[@]}" -C build --print-errorlogs
             ;;
         RUN_ASAN_UBSAN|RUN_GCC_ASAN_UBSAN|RUN_CLANG_ASAN_UBSAN|RUN_CLANG_ASAN_UBSAN_NO_DEPS)
             MESON_ARGS=(--optimization=1)
