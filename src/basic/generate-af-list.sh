135e8c04dfd9039b3673514e607b16ae194251ec
Yu Watanabe <watanabe.yu+github@gmail.com>
2025-07-08 13:07:32 +0900

    tree-wide: several cleanups for generating symbol lists and gperf files
    - pass our system include directories to make generators use our libc
      wrappers and latest kernel headers,
    - include relevant headers in generated gperf file,
    - use files() rather than find_program(), as the result of
      find_program() cannot be passed to 'input' of custom_target(),
    - move generate-bpf-delegate-configs.py to src/core/, as it is only used
      by libcore.

--- src/basic/generate-af-list.sh
+++ src/basic/generate-af-list.sh
@@ -3,6 +3,9 @@
 set -eu
 set -o pipefail
 
-${1:?} -E -dM -include sys/socket.h -include "${2:?}" - </dev/null | \
+CC=${1:?}
+shift
+
+$CC -E -dM -include sys/socket.h "$@" - </dev/null | \
        grep -Ev 'AF_UNSPEC|AF_MAX' | \
        awk '/^#define[ \t]+AF_[^ \t]+[ \t]+[AP]F_[^ \t]/ { print $2; }'
