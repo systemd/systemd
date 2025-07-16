4efbdd89ea7ad6087349d9577e246b6da6f32984
Yu Watanabe <watanabe.yu+github@gmail.com>
2025-07-16 09:01:14 +0900

    test-bpf-token: first check if fsopen()/fsconfig() for bpffs works

--- src/test/test-bpf-token.c
+++ src/test/test-bpf-token.c
@@ -1,13 +1,35 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 
+#if HAVE_LIBBPF
 #include <bpf/bpf.h>
+#endif
 #include <fcntl.h>
+#include <sys/mount.h>
 
 #include "fd-util.h"
+#include "strv.h"
 #include "tests.h"
 
 static int intro(void) {
-#if __LIBBPF_CURRENT_VERSION_GEQ(1, 5)
+        int r;
+
+        /* First, check if fsopen() and fsconfig() for bpffs is supported. */
+        _cleanup_close_ int fs_fd = fsopen("bpf", FSOPEN_CLOEXEC);
+        if (fs_fd < 0)
+                return log_error_errno(errno, "Failed to fsopen bpffs: %m");
+
+        FOREACH_STRING(s, "delegate_cmds", "delegate_maps", "delegate_progs", "delegate_attachs") {
+                r = fsconfig(fs_fd, FSCONFIG_SET_STRING, s, "0xffffffffffffffff", /* aux = */ 0);
+                if (r < 0)
+                        return log_error_errno(errno, "Failed to FSCONFIG_SET_STRING (%s): %m", s);
+        }
+
+        r = fsconfig(fs_fd, FSCONFIG_CMD_CREATE, /* key = */ NULL, /* value = */ NULL, /* aux = */ 0);
+        if (r < 0)
+                return log_error_errno(errno, "Failed to FSCONFIG_CMD_CREATE: %m");
+
+#if HAVE_LIBBPF && __LIBBPF_CURRENT_VERSION_GEQ(1, 5)
+        /* Then, check if bpf token can be created. */
         _cleanup_close_ int bpffs_fd = open("/sys/fs/bpf", O_RDONLY);
         if (bpffs_fd < 0)
                 return log_error_errno(errno, "Failed to open '/sys/fs/bpf': %m");
