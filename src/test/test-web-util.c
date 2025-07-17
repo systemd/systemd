967aba59526948e26dd128ee11778ca402378722
Yaping Li <202858510+YapingLi04@users.noreply.github.com>
2025-07-16 18:48:58 -0700

    test-web-util.c: Migrate to new assertion MACROs
    We recently added a new set of assertion macros such as ASSERT_GE, ASSERT_OK, ASSERT_EQ, ... which show not
    only the expression that failed but also the values of the arguments of the expression. Let's use them.

--- src/test/test-web-util.c
+++ src/test/test-web-util.c
@@ -4,17 +4,17 @@
 #include "web-util.h"
 
 TEST(is_valid_documentation_url) {
-        assert_se(documentation_url_is_valid("https://www.freedesktop.org/wiki/Software/systemd"));
-        assert_se(documentation_url_is_valid("https://www.kernel.org/doc/Documentation/binfmt_misc.txt"));  /* dead */
-        assert_se(documentation_url_is_valid("https://www.kernel.org/doc/Documentation/admin-guide/binfmt-misc.rst"));
-        assert_se(documentation_url_is_valid("https://docs.kernel.org/admin-guide/binfmt-misc.html"));
-        assert_se(documentation_url_is_valid("file:/foo/foo"));
-        assert_se(documentation_url_is_valid("man:systemd.special(7)"));
-        assert_se(documentation_url_is_valid("info:bar"));
+        ASSERT_TRUE(documentation_url_is_valid("https://www.freedesktop.org/wiki/Software/systemd"));
+        ASSERT_TRUE(documentation_url_is_valid("https://www.kernel.org/doc/Documentation/binfmt_misc.txt"));  /* dead */
+        ASSERT_TRUE(documentation_url_is_valid("https://www.kernel.org/doc/Documentation/admin-guide/binfmt-misc.rst"));
+        ASSERT_TRUE(documentation_url_is_valid("https://docs.kernel.org/admin-guide/binfmt-misc.html"));
+        ASSERT_TRUE(documentation_url_is_valid("file:/foo/foo"));
+        ASSERT_TRUE(documentation_url_is_valid("man:systemd.special(7)"));
+        ASSERT_TRUE(documentation_url_is_valid("info:bar"));
 
-        assert_se(!documentation_url_is_valid("foo:"));
-        assert_se(!documentation_url_is_valid("info:"));
-        assert_se(!documentation_url_is_valid(""));
+        ASSERT_TRUE(!documentation_url_is_valid("foo:"));
+        ASSERT_TRUE(!documentation_url_is_valid("info:"));
+        ASSERT_TRUE(!documentation_url_is_valid(""));
 }
 
 DEFINE_TEST_MAIN(LOG_INFO);
