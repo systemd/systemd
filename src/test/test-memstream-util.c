7ba28367cf1342f0d82cafd0b312ea96a17b3702
Yaping Li <202858510+YapingLi04@users.noreply.github.com>
2025-07-23 14:16:03 -0700

    test-memstream-util.c: Migrate to new assertion macros
    We recently added a new set of assertion macros such as ASSERT_GE, ASSERT_OK, ASSERT_EQ, ... which show not
    only the expression that failed but also the values of the arguments of the expression. Let's use them.

--- src/test/test-memstream-util.c
+++ src/test/test-memstream-util.c
@@ -6,7 +6,7 @@
 TEST(memstream_done) {
         _cleanup_(memstream_done) MemStream m = {};
 
-        assert_se(memstream_init(&m));
+        ASSERT_NOT_NULL(memstream_init(&m));
 }
 
 TEST(memstream_empty) {
@@ -14,10 +14,10 @@ TEST(memstream_empty) {
         _cleanup_free_ char *buf = NULL;
         size_t sz;
 
-        assert_se(memstream_init(&m));
-        assert_se(memstream_finalize(&m, &buf, &sz) >= 0);
+        ASSERT_NOT_NULL(memstream_init(&m));
+        ASSERT_OK(memstream_finalize(&m, &buf, &sz));
         ASSERT_STREQ(buf, "");
-        assert_se(sz == 0);
+        ASSERT_EQ(sz, 0);
 }
 
 TEST(memstream) {
@@ -26,7 +26,7 @@ TEST(memstream) {
         size_t sz;
         FILE *f;
 
-        assert_se(f = memstream_init(&m));
+        ASSERT_NOT_NULL(f = memstream_init(&m));
         fputs("hoge", f);
         fputs("おはよう！", f);
         fputs(UTF8("😀😀😀"), f);
@@ -36,24 +36,24 @@ TEST(memstream) {
 
         buf = mfree(buf);
 
-        assert_se(f = memstream_init(&m));
+        ASSERT_NOT_NULL(f = memstream_init(&m));
         fputs("second", f);
-        assert_se(memstream_finalize(&m, &buf, &sz) >= 0);
+        ASSERT_OK(memstream_finalize(&m, &buf, &sz));
         ASSERT_STREQ(buf, "second");
-        assert_se(sz == strlen("second"));
+        ASSERT_EQ(sz, strlen("second"));
 }
 
 TEST(memstream_dump) {
         _cleanup_(memstream_done) MemStream m = {};
         FILE *f;
 
-        assert_se(f = memstream_init(&m));
+        ASSERT_NOT_NULL(f = memstream_init(&m));
         fputs("first", f);
-        assert_se(memstream_dump(LOG_DEBUG, &m) >= 0);
+        ASSERT_OK(memstream_dump(LOG_DEBUG, &m));
 
-        assert_se(f = memstream_init(&m));
+        ASSERT_NOT_NULL(f = memstream_init(&m));
         fputs("second", f);
-        assert_se(memstream_dump(LOG_DEBUG, &m) >= 0);
+        ASSERT_OK(memstream_dump(LOG_DEBUG, &m));
 }
 
 DEFINE_TEST_MAIN(LOG_DEBUG);
