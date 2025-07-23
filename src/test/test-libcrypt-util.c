699c77d245e0ec31bb558e9736c8191255d16b52
Yu Watanabe <watanabe.yu+github@gmail.com>
2025-07-23 13:13:29 +0900

    meson: crypt.h must always exist
    We require at least crypt_r() exists, and it is provided since glibc-2.0
    (and dropped in glibc-2.39) or by libxcrypt, and the function is
    provided in crypt.h regardless it is provided by glibc or libxcrypt.
    Hence, we cannot fallback to unistd.h.
    
    This makes the condition about crypt.h more strict, and stop compilation
    earlier when crypt.h does not exist.

--- src/test/test-libcrypt-util.c
+++ src/test/test-libcrypt-util.c
@@ -1,10 +1,6 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 
-#if HAVE_CRYPT_H
-#  include <crypt.h>
-#else
-#  include <unistd.h>
-#endif
+#include <crypt.h>
 
 #include "libcrypt-util.h"
 #include "strv.h"
