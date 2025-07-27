56f4ae6a1045b8a71b6a8c9ab959ae030ad6354a
Yu Watanabe <watanabe.yu+github@gmail.com>
2025-07-27 21:42:12 +0900

    test: skip verification for racy test cases
    FORMAT_LIFETIME() internally calls now(), hence we cannot provide any
    reliable verifications for finite lifetime.

--- src/network/test-networkd-address.c
+++ src/network/test-networkd-address.c
@@ -7,8 +7,9 @@
 static void test_FORMAT_LIFETIME_one(usec_t lifetime, const char *expected) {
         const char *t = FORMAT_LIFETIME(lifetime);
 
-        log_debug(USEC_FMT " → \"%s\" (expected \"%s\")", lifetime, t, expected);
-        assert_se(streq(t, expected));
+        log_debug(USEC_FMT " → \"%s\" (expected \"%s\")", lifetime, t, strna(expected));
+        if (expected)
+                ASSERT_STREQ(t, expected);
 }
 
 TEST(FORMAT_LIFETIME) {
@@ -17,9 +18,11 @@ TEST(FORMAT_LIFETIME) {
         now_usec = now(CLOCK_BOOTTIME);
 
         test_FORMAT_LIFETIME_one(now_usec, "for 0");
-        test_FORMAT_LIFETIME_one(usec_add(now_usec, 2 * USEC_PER_SEC - 1), "for 1s");
-        test_FORMAT_LIFETIME_one(usec_add(now_usec, 3 * USEC_PER_WEEK + USEC_PER_SEC - 1), "for 3w");
         test_FORMAT_LIFETIME_one(USEC_INFINITY, "forever");
+
+        /* These two are necessarily racy, especially for slow test environment. */
+        test_FORMAT_LIFETIME_one(usec_add(now_usec, 2 * USEC_PER_SEC - 1), NULL);
+        test_FORMAT_LIFETIME_one(usec_add(now_usec, 3 * USEC_PER_WEEK + USEC_PER_SEC - 1), NULL);
 }
 
 DEFINE_TEST_MAIN(LOG_DEBUG);
