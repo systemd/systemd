3341b1b071b1ba0ec20bcf42da2b7f717acacf9d
ssoss <skyler.soss@gmail.com>
2025-07-30 10:42:45 -0500

    network: add new DHCPv6 message types and options
    Gives enum definitions for the DHCPv6 options from RFC9527 and RFC 9686
    and the new message types defined in RFC 9686.

--- src/libsystemd-network/dhcp6-protocol.h
+++ src/libsystemd-network/dhcp6-protocol.h
@@ -98,6 +98,8 @@ typedef enum DHCP6MessageType {
         DHCP6_MESSAGE_DISCONNECT                = 33, /* RFC 8156 */
         DHCP6_MESSAGE_STATE                     = 34, /* RFC 8156 */
         DHCP6_MESSAGE_CONTACT                   = 35, /* RFC 8156 */
+        DHCP6_MESSAGE_ADDR_REG_INFORM           = 36, /* RFC 9686 */
+        DHCP6_MESSAGE_ADDR_REG_REPLY            = 37, /* RFC 9686 */
         _DHCP6_MESSAGE_TYPE_MAX,
         _DHCP6_MESSAGE_TYPE_INVALID             = -EINVAL,
 } DHCP6MessageType;
