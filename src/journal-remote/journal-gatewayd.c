systemd/systemd

commit a7bfb9f76b96888d60b4f287f29dcbf758ba34c0

journal-gatewayd: fix handling of num_skip pointing beyond the last entry
When `num_skip` is supplied to the `Range` header, journal-gatewayd
always returns the very last record even though it should have been
skipped. This is because the `sd_journal_next_skip` always returns
non-zero value on the first call, leading to one iteration of the
`request_reader_entries` returning the last record.

To avoid this unexpected behavior, check that the number of lines we
have skipped by is not lower than the requested skip value. If it is,
then it means there are lines which should not be returned now -
decrement the n_skip counter then and return from the function, closing
the stream if follow flag is not set.

Fixes #37954

src/journal-remote/journal-gatewayd.c
@@ -182,9 +182,21 @@ static ssize_t request_reader_entries(
 
                 if (m->n_skip < 0)
                         r = sd_journal_previous_skip(m->journal, (uint64_t) -m->n_skip + 1);
-                else if (m->n_skip > 0)
+                else if (m->n_skip > 0) {
                         r = sd_journal_next_skip(m->journal, (uint64_t) m->n_skip + 1);
-                else
+                        if (r < 0) {
+                                log_error_errno(r, "Failed to skip journal entries: %m");
+                                return MHD_CONTENT_READER_END_WITH_ERROR;
+                        }
+                        /* We skipped beyond the end, make sure entries between the cursor and n_skip offset
+                         * from it are not returned. */
+                        if (r < m->n_skip + 1) {
+                                m->n_skip -= r;
+                                if (m->follow)
+                                        return 0;
+                                return MHD_CONTENT_READER_END_OF_STREAM;
+                        }
+                } else
                         r = sd_journal_next(m->journal);
 
                 if (r < 0) {

test/units/TEST-04-JOURNAL.journal-gatewayd.sh
@@ -82,6 +82,12 @@ timeout 5 curl -LSfs \
      --header "Range: entries=:-20:10" \
      http://localhost:19531/entries?follow >"$LOG_FILE"
 jq -se "length == 10" "$LOG_FILE"
+# Test positive skip beyond the last entry
+curl -LSfs \
+     --header "Accept: application/json" \
+     --header "Range: entries=$TEST_CURSOR:1:1" \
+     http://localhost:19531/entries?SYSLOG_IDENTIFIER="$TEST_TAG" >"$LOG_FILE"
+jq -se "length == 0" "$LOG_FILE"
 # Check if the specified cursor refers to an existing entry and return just that entry
 curl -LSfs \
      --header "Accept: application/json" \

commit 85fa4bb016c92252d5e47716a8d436d88f6c1c6e

journal-gatewayd: fix busy loop when following way beyond journal end
Fix regression introduced in a7bfb9f76b96888d60b4f287f29dcbf758ba34c0,
where busy loop can be started with a request for following logs with a
range header set with num_skip value pointing beyond the end of the
journal. In that case the reader callback returns 0 and is called
immediately again, usually causing an endless loop that is not recovered
even when new journal events are added.

The bug does not occur if num_skip is not set - in that case if no
journal entries matching the filters are added, the tight loop is
avoided by the sd_journal_wait().

To fix the issue, when no matching journal events are available, set a
flag and reuse the backoff mechanism using the sd_journal_wait().

Link: https://github.com/home-assistant/operating-system/issues/4190

src/journal-remote/journal-gatewayd.c
@@ -172,6 +172,7 @@ static ssize_t request_reader_entries(
 
         while (pos >= m->size) {
                 off_t sz;
+                bool wait_for_events = false;
 
                 /* End of this entry, so let's serialize the next
                  * one */
@@ -191,10 +192,12 @@ static ssize_t request_reader_entries(
                         /* We skipped beyond the end, make sure entries between the cursor and n_skip offset
                          * from it are not returned. */
                         if (r < m->n_skip + 1) {
-                                m->n_skip -= r;
                                 if (m->follow)
-                                        return 0;
-                                return MHD_CONTENT_READER_END_OF_STREAM;
+                                        wait_for_events = true;
+                                else
+                                        return MHD_CONTENT_READER_END_OF_STREAM;
+
+                                m->n_skip -= r;
                         }
                 } else
                         r = sd_journal_next(m->journal);
@@ -203,20 +206,22 @@ static ssize_t request_reader_entries(
                         log_error_errno(r, "Failed to advance journal pointer: %m");
                         return MHD_CONTENT_READER_END_WITH_ERROR;
                 } else if (r == 0) {
+                        if (m->follow)
+                                wait_for_events = true;
+                        else
+                                return MHD_CONTENT_READER_END_OF_STREAM;
+                }
 
-                        if (m->follow) {
-                                r = sd_journal_wait(m->journal, (uint64_t) JOURNAL_WAIT_TIMEOUT);
-                                if (r < 0) {
-                                        log_error_errno(r, "Couldn't wait for journal event: %m");
-                                        return MHD_CONTENT_READER_END_WITH_ERROR;
-                                }
-                                if (r == SD_JOURNAL_NOP)
-                                        break;
-
-                                continue;
+                if (wait_for_events) {
+                        r = sd_journal_wait(m->journal, (uint64_t) JOURNAL_WAIT_TIMEOUT);
+                        if (r < 0) {
+                                log_error_errno(r, "Couldn't wait for journal event: %m");
+                                return MHD_CONTENT_READER_END_WITH_ERROR;
                         }
+                        if (r == SD_JOURNAL_NOP)
+                                break;
 
-                        return MHD_CONTENT_READER_END_OF_STREAM;
+                        continue;
                 }
 
                 if (m->discrete) {