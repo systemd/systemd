systemd/systemd

commit 78738adf88ad288a6af37a1998adc749ac6b5f2d

network/bridge-vlan: allow to configure bridge vlan on stacked bridge master interface
Fixes #38157.

src/network/networkd-bridge-vlan.c
@@ -245,8 +245,9 @@ int bridge_vlan_set_message(Link *link, sd_netlink_message *m, bool is_set) {
         if (r < 0)
                 return r;
 
-        if (link->master_ifindex <= 0) {
-                /* master needs BRIDGE_FLAGS_SELF flag */
+        if (link->master_ifindex <= 0 || streq(link->kind, "bridge")) {
+                /* If the setting is requested in a .network file for a bridge master (or a physical master)
+                 * interface, then BRIDGE_FLAGS_SELF flag needs to be set. */
                 r = sd_netlink_message_append_u16(m, IFLA_BRIDGE_FLAGS, BRIDGE_FLAGS_SELF);
                 if (r < 0)
                         return r;

commit 570210eb281f33e4c013cfdabc142838691aaa9f

network/bridge-vlan: fix segfault
Fixes a bug introduced by 78738adf88ad288a6af37a1998adc749ac6b5f2d.
Fixes #38515.

src/network/networkd-bridge-vlan.c
@@ -245,7 +245,7 @@ int bridge_vlan_set_message(Link *link, sd_netlink_message *m, bool is_set) {
         if (r < 0)
                 return r;
 
-        if (link->master_ifindex <= 0 || streq(link->kind, "bridge")) {
+        if (link->master_ifindex <= 0 || streq_ptr(link->kind, "bridge")) {
                 /* If the setting is requested in a .network file for a bridge master (or a physical master)
                  * interface, then BRIDGE_FLAGS_SELF flag needs to be set. */
                 r = sd_netlink_message_append_u16(m, IFLA_BRIDGE_FLAGS, BRIDGE_FLAGS_SELF);