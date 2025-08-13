9fb90ffd4d42cfdd89755097b275633f8f681bad
Yu Watanabe <watanabe.yu+github@gmail.com>
2025-08-13 15:26:35 +0900

    import: add a debugging log of importing blob
    This should be helpful for debugging issue #38524.
    
    (cherry picked from commit 2633ed01caa93a7bf909fc2c6ee4af1ff6fb48d4)

--- src/import/import-compress.c
+++ src/import/import-compress.c
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 
 #include "import-compress.h"
+#include "log.h"
 #include "string-table.h"
 
 void import_compress_free(ImportCompress *c) {
@@ -79,6 +80,7 @@ int import_uncompress_detect(ImportCompress *c, const void *data, size_t size) {
 
         c->encoding = false;
 
+        log_debug("Detected compression type: %s", import_compress_type_to_string(c->type));
         return 1;
 }
 
