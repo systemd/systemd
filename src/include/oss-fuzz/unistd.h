aed6e7e2ecda7fe1ba15d916fe8bef06822beeda
Yu Watanabe <watanabe.yu+github@gmail.com>
2025-08-18 05:51:56 +0900

    Bump required minimum version of glibc to 2.34
    Major distributions already have glibc >= 2.34.
    Let's bump the required minimum version.
    
    Note, glibc-2.34 was released on 2021-08-01.
    
    Note, oss-fuzz still uses Ubuntu 20.04 which already EOL. To make
    fuzzers can be built by oss-fuzz, this moves several definitions
    to src/include/oss-fuzz/.

---
+++ src/include/oss-fuzz/unistd.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#pragma once
+
+#include_next <unistd.h>
+
+/* Defined since glibc-2.34.
+ * Supported since kernel v5.9 (9b4feb630e8e9801603f3cab3a36369e3c1cf88d). */
+#if !HAVE_CLOSE_RANGE
+int missing_close_range(unsigned first_fd, unsigned end_fd, unsigned flags);
+#  define close_range missing_close_range
+#endif
+
+/* Defined since glibc-2.34.
+ * Supported since kernel v3.19 (51f39a1f0cea1cacf8c787f652f26dfee9611874). */
+#if !HAVE_EXECVEAT
+int missing_execveat(int dirfd, const char *pathname, char * const argv[], char * const envp[], int flags);
+#  define execveat missing_execveat
+#endif
