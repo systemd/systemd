19f9596747ae736f7f087ad5ab73943d72b0b5b4
Felix Pehla <29adc1fd92@gmail.com>
2025-08-24 22:21:36 +0200

    sd-boot: allow setting the maximum log level

--- src/boot/efi-log.h
+++ src/boot/efi-log.h
@@ -34,6 +34,13 @@ typedef enum LogLevel {
         _LOG_INVALID = -1,
 } LogLevel;
 
+LogLevel log_level_from_string(const char *s) _pure_;
+const char* log_level_to_string(LogLevel l) _const_;
+
+LogLevel log_get_max_level(void) _pure_;
+int log_set_max_level(LogLevel level);
+int log_set_max_level_from_string(const char *e);
+
 _noreturn_ void freeze(void);
 void log_wait(void);
 _gnu_printf_(3, 4) EFI_STATUS log_internal(EFI_STATUS status, LogLevel log_level, const char *format, ...);
