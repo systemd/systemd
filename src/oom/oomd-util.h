92e844f33f90e6881a7ec6b650ca8ab5859f9713
Matteo Croce <teknoraver@meta.com>
2025-08-25 17:13:00 +0200

    oomd: implement a prekill varlink event
    When a cgroup is selected for termination, send varlink messages
    to hooks registered in `/run/systemd/oom/prekill/`.
    oomd waits up to `PrekillHookTimeoutSec=` seconds for response
    before proceeding with the kill.
    Option is enabled by a new option EnablePrekillHook=.

--- src/oom/oomd-util.h
+++ src/oom/oomd-util.h
@@ -45,6 +45,11 @@ struct OomdSystemContext {
         uint64_t swap_used;
 };
 
+struct PreKillContext {
+        usec_t timeout;
+        sd_event *event;
+};
+
 OomdCGroupContext *oomd_cgroup_context_free(OomdCGroupContext *ctx);
 DEFINE_TRIVIAL_CLEANUP_FUNC(OomdCGroupContext*, oomd_cgroup_context_free);
 
@@ -119,14 +124,14 @@ int oomd_sort_cgroup_contexts(Hashmap *h, oomd_compare_t compare_func, const cha
 int oomd_fetch_cgroup_oom_preference(OomdCGroupContext *ctx, const char *prefix);
 
 /* Returns a negative value on error, 0 if no processes were killed, or 1 if processes were killed. */
-int oomd_cgroup_kill(const char *path, bool recurse, bool dry_run);
+int oomd_cgroup_kill(const char *path, bool recurse);
 
 /* The following oomd_kill_by_* functions return 1 if processes were killed, or negative otherwise. */
 /* If `prefix` is supplied, only cgroups whose paths start with `prefix` are eligible candidates. Otherwise,
  * everything in `h` is a candidate.
  * Returns the killed cgroup in ret_selected. */
-int oomd_kill_by_pgscan_rate(Hashmap *h, const char *prefix, bool dry_run, char **ret_selected);
-int oomd_kill_by_swap_usage(Hashmap *h, uint64_t threshold_usage, bool dry_run, char **ret_selected);
+int oomd_kill_by_pgscan_rate(Hashmap *h, const char *prefix, bool dry_run, char **ret_selected, struct PreKillContext *pk_ctx);
+int oomd_kill_by_swap_usage(Hashmap *h, uint64_t threshold_usage, bool dry_run, char **ret_selected, struct PreKillContext *pk_ctx);
 
 int oomd_cgroup_context_acquire(const char *path, OomdCGroupContext **ret);
 int oomd_system_context_acquire(const char *proc_swaps_path, OomdSystemContext *ret);
