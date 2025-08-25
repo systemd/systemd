fc95f500bc01b8e7fe75c44273cbb52f9279ee2b
Matteo Croce <teknoraver@meta.com>
2025-08-25 17:13:00 +0200

    oomd: implement a prekill varlink event
    When a cgroup is selected for termination, send varlink messages
    to hooks registered in `/run/systemd/oom/prekill/`.
    oomd waits up to `PrekillHookTimeoutSec=` seconds for response
    before proceeding with the kill.
    Option is enabled by a new option EnablePrekillHook=.

---
+++ src/shared/varlink-io.systemd.oom.PreKill.c
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+
+#include "varlink-io.systemd.oom.PreKill.h"
+
+/* This is a new Varlink interface for pre-kill notifications from oomd.
+ * It will be available through /run/systemd/oom/prekill/<hooks> */
+
+static SD_VARLINK_DEFINE_METHOD(
+                Notify,
+                SD_VARLINK_DEFINE_INPUT(cgroup, SD_VARLINK_STRING, 0),
+
+SD_VARLINK_DEFINE_INTERFACE(
+                io_systemd_oom_PreKill,
+                "io.systemd.oom.PreKill",
+                &vl_method_Notify);
