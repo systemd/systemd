a6eba28582184ca3a022c576f367ab618c3a8d0a
Lennart Poettering <lennart@poettering.net>
2025-08-25 12:26:53 +0200

    discover-image: imply that hidden images are read-only
    Marking a whole directory tree OS image as read-only is difficult
    privilege-wise, because so far we rely on the FS_IMMUTABLE_FL which is
    not accessible to unpriv clients.
    
    One fundamental place where we currently rely on marking images
    read-only is for keeping pristine copies of the originally downloaded
    image around, which we place in "hidden" image directories. This is
    probably the most relevant usecase for the read-only flag. And moreover,
    the only usecase for the hidden images are these read-only pristine
    copies.
    
    Hence, let's make this work reasonably in the unpriv case, and simply
    imply the read-only flag for hidden images. This is strictly speaking a
    change in behaviour, but effectively it shouldn't be, because for nspawn
    containers that are executed we insist on names that are hostname
    compatible, and hidden names aren't (because they start with a dot).

--- src/portable/portabled-image-bus.c
+++ src/portable/portabled-image-bus.c
@@ -30,6 +30,20 @@
 
 static BUS_DEFINE_PROPERTY_GET_ENUM(property_get_type, image_type, ImageType);
 
+static int property_get_read_only(
+                sd_bus *bus,
+                const char *path,
+                const char *interface,
+                const char *property,
+                sd_bus_message *reply,
+                void *userdata,
+                sd_bus_error *error) {
+
+        Image *image = ASSERT_PTR(userdata);
+
+        return sd_bus_message_append(reply, "b", image_is_read_only(image));
+}
+
 int bus_image_common_get_os_release(
                 Manager *m,
                 sd_bus_message *message,
@@ -865,7 +879,7 @@ const sd_bus_vtable image_vtable[] = {
         SD_BUS_PROPERTY("Name", "s", NULL, offsetof(Image, name), 0),
         SD_BUS_PROPERTY("Path", "s", NULL, offsetof(Image, path), 0),
         SD_BUS_PROPERTY("Type", "s", property_get_type,  offsetof(Image, type), 0),
-        SD_BUS_PROPERTY("ReadOnly", "b", bus_property_get_bool, offsetof(Image, read_only), 0),
+        SD_BUS_PROPERTY("ReadOnly", "b", property_get_read_only, 0, 0),
         SD_BUS_PROPERTY("CreationTimestamp", "t", NULL, offsetof(Image, crtime), 0),
         SD_BUS_PROPERTY("ModificationTimestamp", "t", NULL, offsetof(Image, mtime), 0),
         SD_BUS_PROPERTY("Usage", "t", NULL, offsetof(Image, usage), 0),
