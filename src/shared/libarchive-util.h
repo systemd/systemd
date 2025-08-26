fe06b9e4afefdb975b7a47a914a8eb565b4291c3
Lennart Poettering <lennart@poettering.net>
2025-08-26 19:09:05 +0200

    libarchive: add limited compat with old libarchive lacking archive_entry_{gid,hardlink,uid}_is_set()

--- src/shared/libarchive-util.h
+++ src/shared/libarchive-util.h
@@ -16,9 +16,7 @@ extern DLSYM_PROTOTYPE(archive_entry_fflags);
 extern DLSYM_PROTOTYPE(archive_entry_filetype);
 extern DLSYM_PROTOTYPE(archive_entry_free);
 extern DLSYM_PROTOTYPE(archive_entry_gid);
-extern DLSYM_PROTOTYPE(archive_entry_gid_is_set);
 extern DLSYM_PROTOTYPE(archive_entry_hardlink);
-extern DLSYM_PROTOTYPE(archive_entry_hardlink_is_set);
 extern DLSYM_PROTOTYPE(archive_entry_mode);
 extern DLSYM_PROTOTYPE(archive_entry_mtime);
 extern DLSYM_PROTOTYPE(archive_entry_mtime_is_set);
@@ -43,7 +41,6 @@ extern DLSYM_PROTOTYPE(archive_entry_set_uid);
 extern DLSYM_PROTOTYPE(archive_entry_sparse_add_entry);
 extern DLSYM_PROTOTYPE(archive_entry_symlink);
 extern DLSYM_PROTOTYPE(archive_entry_uid);
-extern DLSYM_PROTOTYPE(archive_entry_uid_is_set);
 extern DLSYM_PROTOTYPE(archive_entry_xattr_add_entry);
 extern DLSYM_PROTOTYPE(archive_entry_xattr_next);
 extern DLSYM_PROTOTYPE(archive_entry_xattr_reset);
@@ -64,6 +61,23 @@ extern DLSYM_PROTOTYPE(archive_write_open_FILE);
 extern DLSYM_PROTOTYPE(archive_write_open_fd);
 extern DLSYM_PROTOTYPE(archive_write_set_format_filter_by_ext);
 extern DLSYM_PROTOTYPE(archive_write_set_format_pax);
+
+#if HAVE_LIBARCHIVE_IS_SET
+extern DLSYM_PROTOTYPE(archive_entry_gid_is_set);
+extern DLSYM_PROTOTYPE(archive_entry_hardlink_is_set);
+extern DLSYM_PROTOTYPE(archive_entry_uid_is_set);
+#else
+#include "user-util.h"
+static inline int sym_archive_entry_gid_is_set(struct archive_entry *e) {
+        return gid_is_valid(sym_archive_entry_gid(e));
+}
+static inline int sym_archive_entry_hardlink_is_set(struct archive_entry *e) {
+        return !!sym_archive_entry_hardlink(e);
+}
+static inline int sym_archive_entry_uid_is_set(struct archive_entry *e) {
+        return uid_is_valid(sym_archive_entry_uid(e));
+}
+#endif
 
 int dlopen_libarchive(void);
 
