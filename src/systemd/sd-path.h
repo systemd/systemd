41ffbb6de62e08f416e9876061ebb83b1be3e83d
Lennart Poettering <lennart@poettering.net>
2025-08-26 17:00:17 +0200

    nspawn: support .nspawn files in per-user mode

--- src/systemd/sd-path.h
+++ src/systemd/sd-path.h
@@ -21,7 +21,7 @@
 
 _SD_BEGIN_DECLARATIONS;
 
-enum {
+__extension__ enum {
         /* Temporary files */
         SD_PATH_TEMPORARY,
         SD_PATH_TEMPORARY_LARGE,
@@ -129,7 +129,8 @@ enum {
         SD_PATH_USER_CREDENTIAL_STORE_ENCRYPTED,
         SD_PATH_USER_SEARCH_CREDENTIAL_STORE_ENCRYPTED,
 
-        _SD_PATH_MAX
+        _SD_PATH_MAX,
+        _SD_PATH_INVALID = UINT64_MAX
 };
 
 int sd_path_lookup(uint64_t type, const char *suffix, char **ret);
