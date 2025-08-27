9b8f0dfee1e1d857a3f96cad49bc5ffca5611ee3
Marc Schoolderman <info@squell.net>
2025-08-27 22:22:28 +0200

    add sources from https://github.com/squell/nts.git

---
+++ src/timesync/nts_error.c
@@ -0,0 +1,25 @@
+#include <assert.h>
+#include "nts.h"
+
+#define ERROR(kind) case kind: return &#kind[4]
+
+const char *NTS_error_string(enum NTS_ErrorType error) {
+        switch (error) {
+                ERROR(NTS_SERVER_UNKNOWN_CRIT_RECORD);
+                ERROR(NTS_SERVER_BAD_REQUEST);
+                ERROR(NTS_SERVER_INTERNAL_ERROR);
+
+                ERROR(NTS_UNEXPECTED_WARNING);
+                ERROR(NTS_BAD_RESPONSE);
+                ERROR(NTS_INTERNAL_CLIENT_ERROR);
+                ERROR(NTS_NO_PROTOCOL);
+                ERROR(NTS_NO_AEAD);
+                ERROR(NTS_INSUFFICIENT_DATA);
+                ERROR(NTS_UNKNOWN_CRIT_RECORD);
+        case NTS_SUCCESS:
+                return "Success?";
+        }
+
+        /* this is unreachable code */
+        assert(!"Unknown error");
+}
