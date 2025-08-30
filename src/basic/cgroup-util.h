c741f31104121a0bf7a5d99061887148d2a1f8aa
Natalie Vock <natalie.vock@gmx.de>
2025-08-30 16:41:25 +0200

    cgroup-util: Add cg_get_all_keyed_attributes
    For cases where we need to retrieve all key/value pairs of a cgroup
    attribute file. Primary user is the dmem cgroup, to iterate over all
    device memory regions and apply limit scales to each individually.

--- src/basic/cgroup-util.h
+++ src/basic/cgroup-util.h
@@ -167,6 +167,7 @@ int cg_get_attribute_as_uint64(const char *path, const char *attribute, uint64_t
 int cg_get_attribute_as_bool(const char *path, const char *attribute);
 
 int cg_get_keyed_attribute(const char *path, const char *attribute, char * const *keys, char **values);
+int cg_get_all_keyed_attributes(const char *path, const char *attribute, char ***keyvals);
 
 int cg_get_owner(const char *path, uid_t *ret_uid);
 
