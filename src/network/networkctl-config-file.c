8cb00fd1aa9c653fe12d8b0617a1228b5604ce25
Lennart Poettering <lennart@poettering.net>
2025-09-05 14:21:53 +0200

    fd-util: introduce XAT_FDROOT fd shortcut for the root dir
    This takes inspiration from AT_FDCWD, but always references the root dir
    rather than the cwd. This allows various convenience uses, as we can now
    reference the root dir without actually allocating an fd for chaseat(),
    fd_get_path() and various others.

--- src/network/networkctl-config-file.c
+++ src/network/networkctl-config-file.c
@@ -106,7 +106,12 @@ static int get_config_files_by_name(
                 if (!dropin_dirname)
                         return -ENOMEM;
 
-                r = conf_files_list_dropins(ret_dropins, dropin_dirname, /* root = */ NULL, NETWORK_DIRS);
+                r = conf_files_list_dropins(
+                                ret_dropins,
+                                dropin_dirname,
+                                /* root= */ NULL,
+                                /* root_fd= */ -EBADF,
+                                NETWORK_DIRS);
                 if (r < 0)
                         return r;
         }
