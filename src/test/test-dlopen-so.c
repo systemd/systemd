b49f9733bf3f63e67372fadc15e84af616415063
Lennart Poettering <lennart@poettering.net>
2025-09-08 18:31:49 +0200

    libselinux: turn into dlopen() dep

--- src/test/test-dlopen-so.c
+++ src/test/test-dlopen-so.c
@@ -21,6 +21,7 @@
 #include "pkcs11-util.h"
 #include "qrcode-util.h"
 #include "seccomp-util.h"
+#include "selinux-util.h"
 #include "tests.h"
 #include "tpm2-util.h"
 
@@ -58,6 +59,10 @@ static int run(int argc, char **argv) {
         ASSERT_DLOPEN(dlopen_libacl, HAVE_ACL);
         ASSERT_DLOPEN(dlopen_libblkid, HAVE_BLKID);
         ASSERT_DLOPEN(dlopen_libseccomp, HAVE_SECCOMP);
+
+#if HAVE_SELINUX
+        assert_se(dlopen_libselinux() >= 0);
+#endif
 
         return 0;
 }
