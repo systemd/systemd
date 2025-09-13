a2ef8fb4e3a7e1a19a63b28bb0c56a1827cbecd7
Luca Boccassi <luca.boccassi@gmail.com>
2025-09-13 01:37:17 +0100

    machine: do not allow unprivileged users to register directories they don't own as machines
    Ensure root directories are stored in a directory the caller owns,
    so that they can't register and gain access to other users
    containers.
    
    Follow-up for adaff8eb35d9c471af81fddaa4403bc5843a256f

--- src/machine/machine-varlink.c
+++ src/machine/machine-varlink.c
@@ -1,5 +1,7 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 
+#include <sys/stat.h>
+
 #include "sd-json.h"
 #include "sd-varlink.h"
 
@@ -18,6 +20,7 @@
 #include "namespace-util.h"
 #include "operation.h"
 #include "os-util.h"
+#include "path-util.h"
 #include "pidref.h"
 #include "socket-util.h"
 #include "string-table.h"
@@ -191,13 +194,28 @@ int vl_method_register(sd_varlink *link, sd_json_variant *parameters, sd_varlink
         if (!isempty(machine->root_directory) && path_is_os_tree(machine->root_directory) <= 0)
                 return sd_varlink_error(link, SD_VARLINK_ERROR_INVALID_PARAMETER, NULL);
 
-        /* Ensure an unprivileged user cannot claim any process they don't control as their own machine */
+        /* Ensure an unprivileged user cannot claim any process or directory they don't control as their own machine */
         if (machine->uid != 0) {
                 r = process_is_owned_by_uid(&machine->leader, machine->uid);
                 if (r < 0)
                         return r;
                 if (r == 0)
                         return sd_varlink_error(link, SD_VARLINK_ERROR_PERMISSION_DENIED, NULL);
+
+
+                if (!isempty(machine->root_directory)) {
+                        _cleanup_free_ char *parent = NULL;
+
+                        r = path_extract_directory(machine->root_directory, &parent);
+                        if (r < 0)
+                                return r;
+
+                        struct stat st;
+                        if (stat(parent, &st) < 0)
+                                return -errno;
+                        if (st.st_uid != machine->uid)
+                                return sd_varlink_error(link, SD_VARLINK_ERROR_PERMISSION_DENIED, NULL);
+                }
         }
 
         r = machine_link(manager, machine);
