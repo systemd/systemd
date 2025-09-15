b11d157a7d4a25177c9b5827bbe302950f473795
Usama Arif <usamaarif642@gmail.com>
2025-09-15 13:33:28 +0100

    core: introduce MemoryTHP= unit file setting
    Transparent Hugepages (THP) is a Linux kernel feature that manages
    memory using larger pages (2MB on x86, compared to the default 4KB).
    The main goal is to improve memory management efficiency and system
    performance, especially for memory-intensive applications.
    However, it can cause drawbacks in some scenarios, such as memory
    regression and latency spikes. THP policy is governed for the entire
    system via /sys/kernel/mm/transparent_hugepage/enabled.
    However, it can be overridden for individual workloads via prctl(2)
    call.
    MemoryTHP= is used to disable THPs at exec-invoke to stop
    providing THPs for workloads where the drawbacks outweigh the advantages.
    When set to "disable", MemoryTHP= disables THPs completely for the
    process, irrespecitive of global THP controls.
    When set to "madvise", MemoryTHP= disables THPs for the process except
    when specifically madvised by the process with MADV_HUGEPAGE or MADV_COLLAPSE.

--- src/core/namespace.h
+++ src/core/namespace.h
@@ -91,6 +91,24 @@ typedef enum PrivatePIDs {
         _PRIVATE_PIDS_INVALID = -EINVAL,
 } PrivatePIDs;
 
+typedef enum MemoryTHP {
+        /*
+         * Inherit default from process that starts systemd, i.e. do not make
+         * any PR_SET_THP_DISABLE call.
+         */
+        MEMORY_THP_INHERIT,
+        MEMORY_THP_DISABLE, /* Disable THPs completely for the prcess */
+        MEMORY_THP_MADVISE, /* Disable THPs for the process except when madvised */
+        /*
+         * Use system default THP setting. this can be used when the process that
+         * starts systemd has already disabled THPs via PR_SET_THP_DISABLE, and we
+         * want to restore the system default THP setting at process invokation time.
+         */
+        MEMORY_THP_SYSTEM,
+        _MEMORY_THP_MAX,
+        _MEMORY_THP_INVALID = -EINVAL,
+} MemoryTHP;
+
 typedef struct BindMount {
         char *source;
         char *destination;
@@ -232,6 +250,8 @@ DECLARE_STRING_TABLE_LOOKUP(protect_proc, ProtectProc);
 DECLARE_STRING_TABLE_LOOKUP(proc_subset, ProcSubset);
 
 DECLARE_STRING_TABLE_LOOKUP(private_bpf, PrivateBPF);
+
+DECLARE_STRING_TABLE_LOOKUP(memory_thp, MemoryTHP);
 
 DECLARE_STRING_TABLE_LOOKUP(bpf_delegate_cmd, uint64_t);
 
