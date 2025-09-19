26e98fac0201079261b71f23e5ead54381265e94
Luca Boccassi <luca.boccassi@gmail.com>
2025-09-20 00:49:00 +0100

    polkit: check if user authenticated as admin
    The new polkit will return a new detail regarding a successful
    authentication: the actual result type, which we can use to
    see whether the user authenticated as admin. This can be used
    to grant additional privileges.

--- src/login/logind-dbus.c
+++ src/login/logind-dbus.c
@@ -1647,6 +1647,7 @@ static int method_set_user_linger(sd_bus_message *message, void *userdata, sd_bu
                         /* good_user= */ UID_INVALID,
                         interactive ? POLKIT_ALLOW_INTERACTIVE : 0,
                         &m->polkit_registry,
+                        /* ret_admin= */ NULL,
                         error);
         if (r < 0)
                 return r;
@@ -1823,6 +1824,7 @@ static int method_attach_device(sd_bus_message *message, void *userdata, sd_bus_
                         /* good_user= */ UID_INVALID,
                         interactive ? POLKIT_ALLOW_INTERACTIVE : 0,
                         &m->polkit_registry,
+                        /* ret_admin= */ NULL,
                         error);
         if (r < 0)
                 return r;
@@ -1853,6 +1855,7 @@ static int method_flush_devices(sd_bus_message *message, void *userdata, sd_bus_
                         /* good_user= */ UID_INVALID,
                         interactive ? POLKIT_ALLOW_INTERACTIVE : 0,
                         &m->polkit_registry,
+                        /* ret_admin= */ NULL,
                         error);
         if (r < 0)
                 return r;
@@ -2231,6 +2234,7 @@ static int verify_shutdown_creds(
                                 /* good_user= */ UID_INVALID,
                                 interactive ? POLKIT_ALLOW_INTERACTIVE : 0,
                                 &m->polkit_registry,
+                                /* ret_admin= */ NULL,
                                 error);
                 if (r < 0) {
                         /* If we get -EBUSY, it means a polkit decision was made, but not for
@@ -2272,6 +2276,7 @@ static int verify_shutdown_creds(
                                 /* good_user= */ UID_INVALID,
                                 polkit_flags,
                                 &m->polkit_registry,
+                                /* ret_admin= */ NULL,
                                 error);
                 if (r < 0)
                         return r;
@@ -2287,6 +2292,7 @@ static int verify_shutdown_creds(
                                 /* good_user= */ UID_INVALID,
                                 interactive ? POLKIT_ALLOW_INTERACTIVE : 0,
                                 &m->polkit_registry,
+                                /* ret_admin= */ NULL,
                                 error);
                 if (r < 0)
                         return r;
