9d9d144712b7ea9f2d78e8e942faaf9e784010e5
Marc Schoolderman <marc@tweedegolf.com>
2025-09-19 10:45:21 +0200

    make nts logic depend on systemd headers

--- src/timesync/test-nts.c
+++ src/timesync/test-nts.c
@@ -1,24 +1,7 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 
-#ifndef NTS_STANDALONE_TEST
-#    include "tests.h"
-#    include "timesyncd-conf.h"
-#else
-#    define _GNU_SOURCE 1
-#    include <assert.h>
-#    define HAVE_OPENSSL 1
-#    define assert_se assert
-#    define TEST(name) static void test_##name(void)
-#    define DEFINE_TEST_MAIN(_ignore) int main(void) { \
-        test_nts_encoding(); \
-        test_nts_decoding(); \
-        test_ntp_field_encoding(); \
-        test_ntp_field_decoding(); \
-        test_crypto(); \
-        test_keysize(); \
-        return 0; \
-     } int _placeholder
-#endif
+#include "tests.h"
+#include "timesyncd-conf.h"
 
 #include <string.h>
 #include <stdio.h>
