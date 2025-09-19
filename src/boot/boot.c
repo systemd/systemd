9a45ffbaeaa22c140bbb0de89a84772684f5268b
Lennart Poettering <lennart@poettering.net>
2025-09-19 15:32:29 +0200

    boot: let's make the one space we output early on invisible
    let's place the cursor at the beginning of the line before/after, so we
    know it's the first char we overwrite, and we return to the front again
    right after.

--- src/boot/boot.c
+++ src/boot/boot.c
@@ -494,7 +494,7 @@ static bool menu_run(
         ST->ConOut->EnableCursor(ST->ConOut, false);
 
         /* draw a single character to make ClearScreen work on some firmware */
-        ST->ConOut->OutputString(ST->ConOut, (char16_t *) u" ");
+        printf("\r \r");
 
         err = console_set_mode(config->console_mode_efivar != CONSOLE_MODE_KEEP ?
                                config->console_mode_efivar : config->console_mode);
