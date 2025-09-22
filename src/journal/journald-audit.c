systemd/systemd

commit f48cf2a96dfdc23fe30ba0f870125fe55cab64c7

journald: move all config entries from Manager to JournalConfig
Previously, only config entries controlled by multiple sources were located in
JournalConfig, and still other config entries were in Manager.
That's hard to maintain. Let's move all config entries to JournalConfig.

This also makes JournalConfig.forward_to_kmsg and friends tristate.
Otherwise, even if a higher precedence config source disables the
feature, it may be enabled by a lower precedence config.

src/journal/journald-audit.c
@@ -544,12 +544,12 @@ int manager_open_audit(Manager *m) {
         if (r < 0)
                 return log_error_errno(r, "Failed to add audit fd to event loop: %m");
 
-        if (m->set_audit >= 0) {
+        if (m->config.set_audit >= 0) {
                 /* We are listening now, try to enable audit if configured so */
-                r = enable_audit(m->audit_fd, m->set_audit);
+                r = enable_audit(m->audit_fd, m->config.set_audit);
                 if (r < 0)
                         log_warning_errno(r, "Failed to issue audit enable call: %m");
-                else if (m->set_audit > 0)
+                else if (m->config.set_audit > 0)
                         log_debug("Auditing in kernel turned on.");
                 else
                         log_debug("Auditing in kernel turned off.");

src/journal/journald-config.c
@@ -26,115 +26,121 @@
  * for a bit of additional metadata. */
 #define DEFAULT_LINE_MAX            (48*1024)
 
-#define JOURNAL_CONFIG_INIT                                                                     \
-        (JournalConfig) {                                                                       \
-                .forward_to_socket = (SocketAddress) { .sockaddr.sa.sa_family = AF_UNSPEC },    \
-                .storage = _STORAGE_INVALID,                                                    \
-                .max_level_store = -1,                                                          \
-                .max_level_syslog = -1,                                                         \
-                .max_level_kmsg = -1,                                                           \
-                .max_level_console = -1,                                                        \
-                .max_level_wall = -1,                                                           \
-                .max_level_socket = -1,                                                         \
-        }
+void journal_config_done(JournalConfig *c) {
+        assert(c);
 
-static void manager_set_defaults(Manager *m) {
-        assert(m);
-
-        m->compress.enabled = true;
-        m->compress.threshold_bytes = UINT64_MAX;
-
-        m->seal = true;
-
-        /* By default, only read from /dev/kmsg if are the main namespace */
-        m->read_kmsg = !m->namespace;
-
-        /* By default, kernel auditing is enabled by the main namespace instance, and not controlled by
-         * non-default namespace instances. */
-        m->set_audit = m->namespace ? -1 : true;
-
-        m->sync_interval_usec = DEFAULT_SYNC_INTERVAL_USEC;
-
-        m->ratelimit_interval = DEFAULT_RATE_LIMIT_INTERVAL;
-        m->ratelimit_burst = DEFAULT_RATE_LIMIT_BURST;
-
-        m->system_storage.name = "System Journal";
-        journal_reset_metrics(&m->system_storage.metrics);
-
-        m->runtime_storage.name = "Runtime Journal";
-        journal_reset_metrics(&m->runtime_storage.metrics);
-
-        m->max_file_usec = DEFAULT_MAX_FILE_USEC;
-
-        m->config.forward_to_wall = true;
-
-        m->config.max_level_store = LOG_DEBUG;
-        m->config.max_level_syslog = LOG_DEBUG;
-        m->config.max_level_kmsg = LOG_NOTICE;
-        m->config.max_level_console = LOG_INFO;
-        m->config.max_level_wall = LOG_EMERG;
-        m->config.max_level_socket = LOG_DEBUG;
-
-        m->line_max = DEFAULT_LINE_MAX;
+        free(c->tty_path);
 }
 
-static void manager_reset_configs(Manager *m) {
+static void journal_config_set_defaults(JournalConfig *c) {
+        assert(c);
+
+        journal_config_done(c);
+
+        *c = (JournalConfig) {
+                .storage = _STORAGE_INVALID,
+                .compress.enabled = -1,
+                .compress.threshold_bytes = UINT64_MAX,
+                .seal = -1,
+                .read_kmsg = -1,
+                .set_audit = -1,
+                .ratelimit_interval = DEFAULT_RATE_LIMIT_INTERVAL,
+                .ratelimit_burst = DEFAULT_RATE_LIMIT_BURST,
+                .forward_to_syslog = -1,
+                .forward_to_kmsg = -1,
+                .forward_to_console = -1,
+                .forward_to_wall = -1,
+                .max_level_store = -1,
+                .max_level_syslog = -1,
+                .max_level_kmsg = -1,
+                .max_level_console = -1,
+                .max_level_wall = -1,
+                .max_level_socket = -1,
+        };
+
+        journal_reset_metrics(&c->system_storage_metrics);
+        journal_reset_metrics(&c->runtime_storage_metrics);
+}
+
+static void manager_merge_journal_compress_options(Manager *m) {
         assert(m);
 
-        m->config_by_cmdline = JOURNAL_CONFIG_INIT;
-        m->config_by_conf = JOURNAL_CONFIG_INIT;
-        m->config_by_cred = JOURNAL_CONFIG_INIT;
+        if (m->config_by_cmdline.compress.enabled >= 0)
+                m->config.compress = m->config_by_cmdline.compress;
+        else if (m->config_by_conf.compress.enabled >= 0)
+                m->config.compress = m->config_by_conf.compress;
+        else if (m->config_by_cred.compress.enabled >= 0)
+                m->config.compress = m->config_by_cred.compress;
+        else
+                m->config.compress = (JournalCompressOptions) {
+                        .enabled = true,
+                        .threshold_bytes = UINT64_MAX,
+                };
 }
 
 static void manager_merge_forward_to_socket(Manager *m) {
         assert(m);
 
-        /* Conf file takes precedence over credentials. */
-        if (m->config_by_conf.forward_to_socket.sockaddr.sa.sa_family != AF_UNSPEC)
+        if (m->config_by_cmdline.forward_to_socket.sockaddr.sa.sa_family != AF_UNSPEC)
+                m->config.forward_to_socket = m->config_by_cmdline.forward_to_socket;
+        else if (m->config_by_conf.forward_to_socket.sockaddr.sa.sa_family != AF_UNSPEC)
                 m->config.forward_to_socket = m->config_by_conf.forward_to_socket;
         else if (m->config_by_cred.forward_to_socket.sockaddr.sa.sa_family != AF_UNSPEC)
                 m->config.forward_to_socket = m->config_by_cred.forward_to_socket;
         else
-                m->config.forward_to_socket = (SocketAddress) { .sockaddr.sa.sa_family = AF_UNSPEC };
+                m->config.forward_to_socket = (SocketAddress) {};
 }
 
-static void manager_merge_storage(Manager *m) {
-        assert(m);
+#define MERGE_NON_NEGATIVE(name, default_value)                         \
+        m->config.name =                                                \
+                m->config_by_cmdline.name >= 0 ? m->config_by_cmdline.name : \
+                m->config_by_conf.name >= 0 ? m->config_by_conf.name :  \
+                m->config_by_cred.name >= 0 ? m->config_by_cred.name :  \
+                default_value
 
-        /* Conf file takes precedence over credentials. */
-        if (m->config_by_conf.storage != _STORAGE_INVALID)
-                m->config.storage = m->config_by_conf.storage;
-        else if (m->config_by_cred.storage != _STORAGE_INVALID)
-                m->config.storage = m->config_by_cred.storage;
-        else
-                m->config.storage = m->namespace ? STORAGE_PERSISTENT : STORAGE_AUTO;
-}
-
-#define MERGE_BOOL(name, default_value)                                                 \
-    (m->config.name = (m->config_by_cmdline.name  ? m->config_by_cmdline.name :         \
-                       m->config_by_conf.name     ? m->config_by_conf.name     :        \
-                       m->config_by_cred.name     ? m->config_by_cred.name     :        \
-                       default_value))
-
-#define MERGE_NON_NEGATIVE(name, default_value)                                         \
-        (m->config.name = (m->config_by_cmdline.name >= 0 ? m->config_by_cmdline.name : \
-                           m->config_by_conf.name >= 0    ? m->config_by_conf.name :    \
-                           m->config_by_cred.name >= 0    ? m->config_by_cred.name :    \
-                           default_value))
+#define MERGE_NON_ZERO(name, default_value)                             \
+        m->config.name =                                                \
+                m->config_by_cmdline.name ?:                            \
+                m->config_by_conf.name ?:                               \
+                m->config_by_cred.name ?:                               \
+                default_value
 
 static void manager_merge_configs(Manager *m) {
         assert(m);
 
-        /*
-         * From highest to lowest priority: cmdline, conf, cred
-         */
-        manager_merge_storage(m);
+        /* From highest to lowest priority: cmdline, conf, cred */
+
+        journal_config_done(&m->config);
+
+        MERGE_NON_NEGATIVE(storage, STORAGE_AUTO);
+        manager_merge_journal_compress_options(m);
+        MERGE_NON_NEGATIVE(seal, true);
+        /* By default, /dev/kmsg is read only by the main namespace instance. */
+        MERGE_NON_NEGATIVE(read_kmsg, !m->namespace);
+        /* By default, kernel auditing is enabled by the main namespace instance, and not controlled by
+         * non-default namespace instances. */
+        MERGE_NON_NEGATIVE(set_audit, m->namespace ? -1 : true);
+        MERGE_NON_ZERO(sync_interval_usec, DEFAULT_SYNC_INTERVAL_USEC);
+
+        /* TODO: also merge them when comdline or credentials support to configure them. */
+        m->config.ratelimit_interval = m->config_by_conf.ratelimit_interval;
+        m->config.ratelimit_burst = m->config_by_conf.ratelimit_burst;
+        m->config.system_storage_metrics = m->config_by_conf.system_storage_metrics;
+        m->config.runtime_storage_metrics = m->config_by_conf.runtime_storage_metrics;
+
+        MERGE_NON_ZERO(max_retention_usec, 0);
+        MERGE_NON_ZERO(max_file_usec, DEFAULT_MAX_FILE_USEC);
+        MERGE_NON_NEGATIVE(forward_to_syslog, false);
+        MERGE_NON_NEGATIVE(forward_to_kmsg, false);
+        MERGE_NON_NEGATIVE(forward_to_console, false);
+        MERGE_NON_NEGATIVE(forward_to_wall, true);
         manager_merge_forward_to_socket(m);
 
-        MERGE_BOOL(forward_to_syslog, false);
-        MERGE_BOOL(forward_to_kmsg, false);
-        MERGE_BOOL(forward_to_console, false);
-        MERGE_BOOL(forward_to_wall, true);
+        if (strdup_to(&m->config.tty_path,
+                      m->config_by_cmdline.tty_path ?:
+                      m->config_by_conf.tty_path ?:
+                      m->config_by_cred.tty_path) < 0)
+                log_oom_debug();
 
         MERGE_NON_NEGATIVE(max_level_store, LOG_DEBUG);
         MERGE_NON_NEGATIVE(max_level_syslog, LOG_DEBUG);
@@ -142,17 +148,24 @@ static void manager_merge_configs(Manager *m) {
         MERGE_NON_NEGATIVE(max_level_console, LOG_INFO);
         MERGE_NON_NEGATIVE(max_level_wall, LOG_EMERG);
         MERGE_NON_NEGATIVE(max_level_socket, LOG_DEBUG);
+        MERGE_NON_NEGATIVE(split_mode, SPLIT_UID);
+        MERGE_NON_ZERO(line_max, DEFAULT_LINE_MAX);
 }
 
 static void manager_adjust_configs(Manager *m) {
         assert(m);
 
-        if (!!m->ratelimit_interval != !!m->ratelimit_burst) { /* One set to 0 and the other not? */
+        if ((m->config.ratelimit_interval == 0) != (m->config.ratelimit_burst == 0)) { /* One set to 0 and the other not? */
                 log_debug("Setting both rate limit interval and burst from %s/%u to 0/0",
-                          FORMAT_TIMESPAN(m->ratelimit_interval, USEC_PER_SEC),
-                          m->ratelimit_burst);
-                m->ratelimit_interval = m->ratelimit_burst = 0;
+                          FORMAT_TIMESPAN(m->config.ratelimit_interval, USEC_PER_SEC),
+                          m->config.ratelimit_burst);
+                m->config.ratelimit_interval = 0;
+                m->config.ratelimit_burst = 0;
         }
+
+        /* copy metrics to manager */
+        m->system_storage.metrics = m->config.system_storage_metrics;
+        m->runtime_storage.metrics = m->config.runtime_storage_metrics;
 }
 
 static int parse_proc_cmdline_item(const char *key, const char *value, void *data) {
@@ -280,7 +293,7 @@ static void manager_parse_config_file(Manager *m) {
                         config_item_perf_lookup,
                         journald_gperf_lookup,
                         CONFIG_PARSE_WARN,
-                        m);
+                        &m->config_by_conf);
 }
 
 static void manager_load_credentials(JournalConfig *c) {
@@ -317,8 +330,9 @@ void manager_load_config(Manager *m) {
 
         assert(m);
 
-        manager_set_defaults(m);
-        manager_reset_configs(m);
+        journal_config_set_defaults(&m->config_by_conf);
+        journal_config_set_defaults(&m->config_by_cred);
+        journal_config_set_defaults(&m->config_by_cmdline);
 
         manager_load_credentials(&m->config_by_cred);
         manager_parse_config_file(m);
@@ -331,18 +345,14 @@ void manager_load_config(Manager *m) {
         }
 
         manager_merge_configs(m);
-
         manager_adjust_configs(m);
 }
 
 static void manager_reload_config(Manager *m) {
         assert(m);
 
-        manager_set_defaults(m);
-
-        m->config_by_conf = JOURNAL_CONFIG_INIT;
+        journal_config_set_defaults(&m->config_by_conf);
         manager_parse_config_file(m);
-
         manager_merge_configs(m);
         manager_adjust_configs(m);
 }
@@ -449,7 +459,7 @@ int config_parse_compress(
         int r;
 
         if (isempty(rvalue)) {
-                compress->enabled = true;
+                compress->enabled = -1;
                 compress->threshold_bytes = UINT64_MAX;
                 return 0;
         }

src/journal/journald-config.h
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 #pragma once
 
+#include "journal-file.h"
 #include "journald-forward.h"
 #include "socket-util.h"
 
@@ -22,28 +23,66 @@ typedef enum SplitMode {
 } SplitMode;
 
 typedef struct JournalCompressOptions {
-        bool enabled;
+        int enabled;
         uint64_t threshold_bytes;
 } JournalCompressOptions;
 
 typedef struct JournalConfig {
+        /* Storage=, cred: journal.storage */
         Storage storage;
-
-        bool forward_to_kmsg;
-        bool forward_to_syslog;
-        bool forward_to_console;
-        bool forward_to_wall;
-
+        /* Compress= */
+        JournalCompressOptions compress;
+        /* Seal= */
+        int seal;
+        /* ReadKMsg= */
+        int read_kmsg;
+        /* Audit= */
+        int set_audit;
+        /* SyncIntervalSec= */
+        usec_t sync_interval_usec;
+        /* RateLimitIntervalSec= */
+        usec_t ratelimit_interval;
+        /* RateLimitBurst= */
+        unsigned ratelimit_burst;
+        /* SystemMaxUse=, SystemMaxFileSize=, SystemKeepFree=, SystemMaxFiles= */
+        JournalMetrics system_storage_metrics;
+        /* RuntimeMaxUse=, RuntimeMaxFileSize=, RuntimeKeepFree=, RuntimeMaxFiles= */
+        JournalMetrics runtime_storage_metrics;
+        /* MaxRetentionSec= */
+        usec_t max_retention_usec;
+        /* MaxFileSec= */
+        usec_t max_file_usec;
+        /* ForwardToSyslog=, proc: systemd.journald.forward_to_syslog */
+        int forward_to_syslog;
+        /* ForwardToKMsg=, proc: systemd.journald.forward_to_kmsg */
+        int forward_to_kmsg;
+        /* ForwardToConsole=, proc: systemd.journald.forward_to_console */
+        int forward_to_console;
+        /* ForwardToWall=, proc: systemd.journald.forward_to_wall */
+        int forward_to_wall;
+        /* ForwardToSocket=, cred: journal.forward_to_socket */
         SocketAddress forward_to_socket;
-
+        /* TTYPath= */
+        char *tty_path;
+        /* MaxLevelStore=, proc: systemd.journald.max_level_store */
         int max_level_store;
+        /* MaxLevelSyslog=, proc: systemd.journald.max_level_syslog */
         int max_level_syslog;
+        /* MaxLevelKMsg=, proc: systemd.journald.max_level_kmsg */
         int max_level_kmsg;
+        /* MaxLevelConsole=, proc: systemd.journald.max_level_console */
         int max_level_console;
+        /* MaxLevelWall=, systemd.journald.max_level_wall */
         int max_level_wall;
+        /* MaxLevelSocket=, systemd.journald.max_level_socket */
         int max_level_socket;
+        /* SplitMode= */
+        SplitMode split_mode;
+        /* LineMax= */
+        size_t line_max;
 } JournalConfig;
 
+void journal_config_done(JournalConfig *c);
 void manager_load_config(Manager *m);
 int manager_dispatch_reload_signal(sd_event_source *s, const struct signalfd_siginfo *si, void *userdata);
 

commit 78ff191455ec7b0002e52a57680fa7f108833696

journal: make JournalConfig.set_audit as enum
In systemd <= 257, each set_audit tristate value had special meaning,
- true: enable the kernel audit subsystem,
- false: disable the kernel audit subsystem,
- negative: keep the current kernel audit subsystem state.

And the default is true, rather than negative. So, users sometimes
explicitly pass an empty string to Audit= setting to keep the state.

But since f48cf2a96dfdc23fe30ba0f870125fe55cab64c7 (v258), the negative
value is mistakenly used as 'really unspecified' even if an empty string
is explicitly specified.

This makes negative values handled as unspecified as usual, and assign a new
positive value AUDIT_KEEP for when an empty string is explicitly specified.
Also, make the Audit= setting accept "keep" setting, and suggest to use "keep"
rather than an empty string.

Fixes a regression caused by f48cf2a96dfdc23fe30ba0f870125fe55cab64c7 (v258).
Fixes #39057.

man/journald.conf.xml
@@ -478,11 +478,14 @@
       <varlistentry>
         <term><varname>Audit=</varname></term>
 
-        <listitem><para>Takes a boolean value. If enabled <command>systemd-journald</command> will turn on
-        kernel auditing on start-up. If disabled it will turn it off. If unset it will neither enable nor
-        disable it, leaving the previous state unchanged.  This means if another tool turns on auditing even
-        if <command>systemd-journald</command> left it off, it will still collect the generated
-        messages. Defaults to on in the default journal namespace, and unset otherwise.</para>
+        <listitem><para>Takes a boolean value or special value <literal>keep</literal>. If enabled
+        <command>systemd-journald</command> will turn on kernel auditing on start-up. If disabled it will
+        turn it off. When <literal>keep</literal> it will neither enable nor disable it, leaving the previous
+        state unchanged. This means if another tool turns on auditing even if
+        <command>systemd-journald</command> left it off, it will still collect the generated messages.
+        Defaults to yes in the default journal namespace, and <literal>keep</literal> otherwise.</para>
+
+        <!-- Explicit assignment of an empty string is equivalent to 'keep', for backward compatibility. -->
 
         <para>Note that this option does not control whether <command>systemd-journald</command> collects
         generated audit records, it just controls whether it tells the kernel to generate them. If you need

src/journal/journald-audit.c
@@ -465,9 +465,13 @@ static int manager_set_kernel_audit(Manager *m) {
 
         assert(m);
         assert(m->audit_fd >= 0);
+        assert(m->config.set_audit >= 0);
 
-        if (m->config.set_audit < 0)
+        if (m->config.set_audit == AUDIT_KEEP)
                 return 0;
+
+        /* In the following, we can handle 'set_audit' as a boolean. */
+        assert(IN_SET(m->config.set_audit, AUDIT_DISABLE, AUDIT_ENABLE));
 
         struct {
                 union {
@@ -557,7 +561,7 @@ int manager_open_audit(Manager *m) {
         return 0;
 }
 
-void manager_reset_kernel_audit(Manager *m, int old_set_audit) {
+void manager_reset_kernel_audit(Manager *m, AuditSetMode old_set_audit) {
         assert(m);
 
         if (m->audit_fd < 0)

src/journal/journald-audit.h
@@ -10,4 +10,4 @@ void manager_process_audit_message(Manager *m, const void *buffer, size_t buffer
 void process_audit_string(Manager *m, int type, const char *data, size_t size);
 
 int manager_open_audit(Manager *m);
-void manager_reset_kernel_audit(Manager *m, int old_set_audit);
+void manager_reset_kernel_audit(Manager *m, AuditSetMode old_set_audit);

src/journal/journald-config.c
@@ -46,7 +46,7 @@ void journal_config_set_defaults(JournalConfig *c) {
                 .compress.threshold_bytes = UINT64_MAX,
                 .seal = -1,
                 .read_kmsg = -1,
-                .set_audit = -1,
+                .set_audit = _AUDIT_SET_MODE_INVALID,
                 .ratelimit_interval = DEFAULT_RATE_LIMIT_INTERVAL,
                 .ratelimit_burst = DEFAULT_RATE_LIMIT_BURST,
                 .forward_to_syslog = -1,
@@ -123,7 +123,7 @@ void manager_merge_configs(Manager *m) {
         MERGE_NON_NEGATIVE(read_kmsg, !m->namespace);
         /* By default, kernel auditing is enabled by the main namespace instance, and not controlled by
          * non-default namespace instances. */
-        MERGE_NON_NEGATIVE(set_audit, m->namespace ? -1 : true);
+        MERGE_NON_NEGATIVE(set_audit, m->namespace ? AUDIT_KEEP : AUDIT_ENABLE);
         MERGE_NON_ZERO(sync_interval_usec, DEFAULT_SYNC_INTERVAL_USEC);
 
         /* TODO: also merge them when comdline or credentials support to configure them. */
@@ -401,6 +401,16 @@ static const char* const split_mode_table[_SPLIT_MAX] = {
 
 DEFINE_STRING_TABLE_LOOKUP(split_mode, SplitMode);
 DEFINE_CONFIG_PARSE_ENUM(config_parse_split_mode, split_mode, SplitMode);
+
+static const char* const audit_set_mode_table[_AUDIT_SET_MODE_MAX] = {
+        [AUDIT_DISABLE] = "no",
+        [AUDIT_ENABLE]  = "yes",
+        [AUDIT_KEEP]    = "keep",
+};
+
+DEFINE_PRIVATE_STRING_TABLE_LOOKUP_FROM_STRING_WITH_BOOLEAN(audit_set_mode, AuditSetMode, AUDIT_ENABLE);
+/* Historically, an empty string has special meaning and equals to 'keep'. */
+DEFINE_CONFIG_PARSE_ENUM_WITH_DEFAULT(config_parse_audit_set_mode, audit_set_mode, AuditSetMode, AUDIT_KEEP);
 
 int config_parse_line_max(
                 const char *unit,

src/journal/journald-config.h
@@ -27,6 +27,14 @@ typedef struct JournalCompressOptions {
         uint64_t threshold_bytes;
 } JournalCompressOptions;
 
+typedef enum AuditSetMode {
+        AUDIT_DISABLE,
+        AUDIT_ENABLE,
+        AUDIT_KEEP,
+        _AUDIT_SET_MODE_MAX,
+        _AUDIT_SET_MODE_INVALID = -EINVAL,
+} AuditSetMode;
+
 typedef struct JournalConfig {
         /* Storage=, cred: journal.storage */
         Storage storage;
@@ -37,7 +45,7 @@ typedef struct JournalConfig {
         /* ReadKMsg= */
         int read_kmsg;
         /* Audit= */
-        int set_audit;
+        AuditSetMode set_audit;
         /* SyncIntervalSec= */
         usec_t sync_interval_usec;
         /* RateLimitIntervalSec= */
@@ -102,3 +110,4 @@ CONFIG_PARSER_PROTOTYPE(config_parse_line_max);
 CONFIG_PARSER_PROTOTYPE(config_parse_compress);
 CONFIG_PARSER_PROTOTYPE(config_parse_forward_to_socket);
 CONFIG_PARSER_PROTOTYPE(config_parse_split_mode);
+CONFIG_PARSER_PROTOTYPE(config_parse_audit_set_mode);

src/journal/journald-forward.h
@@ -6,6 +6,7 @@
 
 typedef enum Storage Storage;
 typedef enum SplitMode SplitMode;
+typedef enum AuditSetMode AuditSetMode;
 typedef struct JournalCompressOptions JournalCompressOptions;
 typedef struct JournalConfig JournalConfig;
 

src/journal/journald-gperf.gperf
@@ -23,7 +23,7 @@ Journal.Storage,              config_parse_storage,           0, offsetof(Journa
 Journal.Compress,             config_parse_compress,          0, offsetof(JournalConfig, compress)
 Journal.Seal,                 config_parse_tristate,          0, offsetof(JournalConfig, seal)
 Journal.ReadKMsg,             config_parse_tristate,          0, offsetof(JournalConfig, read_kmsg)
-Journal.Audit,                config_parse_tristate,          0, offsetof(JournalConfig, set_audit)
+Journal.Audit,                config_parse_audit_set_mode,    0, offsetof(JournalConfig, set_audit)
 Journal.SyncIntervalSec,      config_parse_sec,               0, offsetof(JournalConfig, sync_interval_usec)
 # The following is a legacy name for compatibility
 Journal.RateLimitInterval,    config_parse_sec,               0, offsetof(JournalConfig, ratelimit_interval)