d277e7680606ebdfaff2aebb070da21668542c34
Lennart Poettering <lennart@poettering.net>
2025-09-23 11:46:49 +0200

    acl: turn libacl dep into a dlopen() one
    I initially didn't think it would be worth doing this, but I changed my
    mind. People out there quite successfully build systemd without ACL
    support, and that suggests life without it is quite possible. Moreover
    we only use it as very specific places:
    
    1. in udev/logind for "uaccess" mgmt
    2. in tmpfiles to implement explicitly configured acl changes
    3. in journald/coredump/pstore to manage access to unpriv users
    4. in pid1 to manage access to credential files
    5. when shifting UIDs of container trees
    
    I specific container environments it should be entirely fine to live without all
    of these, hence let's pull this in on demand only.

--- src/udev/udev-builtin-uaccess.c
+++ src/udev/udev-builtin-uaccess.c
@@ -18,43 +18,47 @@ static int devnode_acl(int fd, uid_t uid) {
 
         assert(fd >= 0);
 
+        r = dlopen_libacl();
+        if (r < 0)
+                return r;
+
         _cleanup_(acl_freep) acl_t acl = NULL;
-        acl = acl_get_file(FORMAT_PROC_FD_PATH(fd), ACL_TYPE_ACCESS);
+        acl = sym_acl_get_file(FORMAT_PROC_FD_PATH(fd), ACL_TYPE_ACCESS);
         if (!acl)
                 return -errno;
 
         acl_entry_t entry;
-        for (r = acl_get_entry(acl, ACL_FIRST_ENTRY, &entry);
+        for (r = sym_acl_get_entry(acl, ACL_FIRST_ENTRY, &entry);
              r > 0;
-             r = acl_get_entry(acl, ACL_NEXT_ENTRY, &entry)) {
+             r = sym_acl_get_entry(acl, ACL_NEXT_ENTRY, &entry)) {
 
                 acl_tag_t tag;
-                if (acl_get_tag_type(entry, &tag) < 0)
+                if (sym_acl_get_tag_type(entry, &tag) < 0)
                         return -errno;
 
                 if (tag != ACL_USER)
                         continue;
 
                 if (uid > 0) {
-                        uid_t *u = acl_get_qualifier(entry);
+                        uid_t *u = sym_acl_get_qualifier(entry);
                         if (!u)
                                 return -errno;
 
                         if (*u == uid) {
                                 acl_permset_t permset;
-                                if (acl_get_permset(entry, &permset) < 0)
+                                if (sym_acl_get_permset(entry, &permset) < 0)
                                         return -errno;
 
-                                int rd = acl_get_perm(permset, ACL_READ);
+                                int rd = sym_acl_get_perm(permset, ACL_READ);
                                 if (rd < 0)
                                         return -errno;
 
-                                int wt = acl_get_perm(permset, ACL_WRITE);
+                                int wt = sym_acl_get_perm(permset, ACL_WRITE);
                                 if (wt < 0)
                                         return -errno;
 
                                 if (!rd || !wt) {
-                                        if (acl_add_perm(permset, ACL_READ|ACL_WRITE) < 0)
+                                        if (sym_acl_add_perm(permset, ACL_READ|ACL_WRITE) < 0)
                                                 return -errno;
 
                                         changed = true;
@@ -65,7 +69,7 @@ static int devnode_acl(int fd, uid_t uid) {
                         }
                 }
 
-                if (acl_delete_entry(acl, entry) < 0)
+                if (sym_acl_delete_entry(acl, entry) < 0)
                         return -errno;
 
                 changed = true;
@@ -74,20 +78,20 @@ static int devnode_acl(int fd, uid_t uid) {
                 return -errno;
 
         if (!found && uid > 0) {
-                if (acl_create_entry(&acl, &entry) < 0)
+                if (sym_acl_create_entry(&acl, &entry) < 0)
                         return -errno;
 
-                if (acl_set_tag_type(entry, ACL_USER) < 0)
+                if (sym_acl_set_tag_type(entry, ACL_USER) < 0)
                         return -errno;
 
-                if (acl_set_qualifier(entry, &uid) < 0)
+                if (sym_acl_set_qualifier(entry, &uid) < 0)
                         return -errno;
 
                 acl_permset_t permset;
-                if (acl_get_permset(entry, &permset) < 0)
+                if (sym_acl_get_permset(entry, &permset) < 0)
                         return -errno;
 
-                if (acl_add_perm(permset, ACL_READ|ACL_WRITE) < 0)
+                if (sym_acl_add_perm(permset, ACL_READ|ACL_WRITE) < 0)
                         return -errno;
 
                 changed = true;
@@ -96,10 +100,10 @@ static int devnode_acl(int fd, uid_t uid) {
         if (!changed)
                 return 0;
 
-        if (acl_calc_mask(&acl) < 0)
+        if (sym_acl_calc_mask(&acl) < 0)
                 return -errno;
 
-        if (acl_set_file(FORMAT_PROC_FD_PATH(fd), ACL_TYPE_ACCESS, acl) < 0)
+        if (sym_acl_set_file(FORMAT_PROC_FD_PATH(fd), ACL_TYPE_ACCESS, acl) < 0)
                 return -errno;
 
         return 0;
