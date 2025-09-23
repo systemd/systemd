818cdc033f43eae7801779f972775de4a1f717ed
Itxaka <itxaka@kairos.io>
2025-09-23 12:07:21 +0200

    Parse a new profile key in Type 1 boot entries
    This allows Type 1 entries to take advantage of multiprofile efi files
    by allowing the configuration of the profile associated with that entry.
    
    Signed-off-by: Itxaka <itxaka@kairos.io>

--- src/boot/boot.c
+++ src/boot/boot.c
@@ -1284,7 +1284,13 @@ static void boot_entry_add_type1(
                 } else if (streq8(key, "sort-key")) {
                         free(entry->sort_key);
                         entry->sort_key = xstr8_to_16(value);
-
+                } else if (streq8(key, "profile")) {
+                        uint64_t u;
+                        if (parse_number8(value, &u, NULL) && u <= UINT_MAX) {
+                                entry->profile = (unsigned)u;
+                        } else {
+                                log_error("Error parsing 'profile' entry option, ignoring: %s", value);
+                        }
                 } else if (streq8(key, "version")) {
                         free(entry->version);
                         entry->version = xstr8_to_16(value);
