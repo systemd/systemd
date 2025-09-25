53e00d0bdcbf31b71a74cab5f3e787fdabdc3d52
Matteo Croce <teknoraver@meta.com>
2025-09-25 21:17:26 +0200

    oomd: ruleset parsing
    Add to oomd the capability to define rulesets in /etc/systemd/oomd/rules.d/
    and then reference them with a new config option OOMRule=

--- src/oom/oomd-conf.h
+++ src/oom/oomd-conf.h
@@ -2,7 +2,15 @@
 #pragma once
 
 typedef struct Manager Manager;
+enum OomdAction;
+
+struct oom_ruleset;
+
+void oom_ruleset_free(struct oom_ruleset *ruleset);
 
 void manager_set_defaults(Manager *m);
 
 void manager_parse_config_file(Manager *m);
+
+const char* actions_to_string(enum OomdAction i);
+enum OomdAction actions_from_string(const char *s);
