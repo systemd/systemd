0dd9c67805e4f0639f9c754ba8c1c3f5142587ea
Usama Arif <usamaarif642@gmail.com>
2025-09-25 22:07:42 +0100

    Introduce mmode for disable THPs except when madvised
    When MemoryTHPDisable is set to 2, systemd disables THPs for the
    process except when specifically madvised by the process
    with MADV_HUGEPAGE or MADV_COLLAPSE.
    
    Signed-off-by: Usama Arif <usamaarif642@gmail.com>

--- src/core/exec-invoke.c
+++ src/core/exec-invoke.c
@@ -5498,6 +5498,15 @@ int exec_invoke(
                                         return log_error_errno(errno, "Failed to disable THP per process: %m");
                                 }
                         }
+                } else if (context->memory_thp_disable == 2) {
+                        if (prctl(PR_SET_THP_DISABLE, context->memory_thp_disable, PR_THP_DISABLE_EXCEPT_ADVISED, 0, 0) < 0) {
+                                if (errno == EINVAL)
+                                        log_debug_errno(errno, "Per process THP disable (except madvise) support not available, ignoring.");
+                                else {
+                                        *exit_status = EXIT_THP_DISABLE;
+                                        return log_error_errno(errno, "Failed to disable (except madvise) THP per process: %m");
+                                }
+                        }
                 } else {
                         *exit_status = EXIT_THP_DISABLE;
                         return log_error_errno(-EINVAL, "Invalid value for MemoryTHPDisable");
