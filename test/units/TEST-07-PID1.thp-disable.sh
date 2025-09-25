0a388e7fa96227107548c755ca9bc2249a813e73
Usama Arif <usamaarif642@gmail.com>
2025-09-25 22:12:42 +0100

    src/test: add unittest for disabling THPs except when madvised
    This checks if the prctl is set correctly when the property name
    is passed to systemd-run.
    
    Signed-off-by: Usama Arif <usamaarif642@gmail.com>

--- test/units/TEST-07-PID1.thp-disable.sh
+++ test/units/TEST-07-PID1.thp-disable.sh
@@ -9,3 +9,17 @@ systemd-run --wait \
 systemd-run --wait \
         -p MemoryTHP=disable \
         /usr/lib/systemd/tests/unit-tests/manual/test-thp-disable-completely
+
+# The following test will always return 77 if at compile time the kernel version
+# is less than 6.18. If it happens don't let the whole test fail
+set +e
+
+systemd-run --wait \
+        -p MemoryTHP=madvise \
+        /usr/lib/systemd/tests/unit-tests/manual/test-thp-disable-except-madvise
+
+if [ $? -eq 77 ]; then
+        exit 0
+fi
+
+set -e
