a228628012622bc39218b2b74d0deba5e034b540
Felix Pehla <29adc1fd92@gmail.com>
2025-09-27 21:58:21 +0200

    shared/bootspec: parse loader.conf options added in v258
    (cherry picked from commit aaa3e6880da71c2faefb619ea7a974dab6a8443d)

--- src/shared/bootspec.c
+++ src/shared/bootspec.c
@@ -514,7 +514,8 @@ int boot_loader_read_conf(BootConfig *config, FILE *file, const char *path) {
                         r = free_and_strdup(&config->default_pattern, p);
                 else if (STR_IN_SET(field, "timeout", "editor", "auto-entries", "auto-firmware",
                                     "auto-poweroff", "auto-reboot", "beep", "reboot-for-bitlocker",
-                                    "secure-boot-enroll", "console-mode"))
+                                    "reboot-on-error", "secure-boot-enroll", "secure-boot-enroll-action",
+                                    "console-mode"))
                         r = 0; /* we don't parse these in userspace, but they are OK */
                 else {
                         log_syntax(NULL, LOG_WARNING, path, line, 0, "Unknown line '%s', ignoring.", field);
