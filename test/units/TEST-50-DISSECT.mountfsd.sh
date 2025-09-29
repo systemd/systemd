6a1338efcd458a3ed426169ba07300087c580afd
Lennart Poettering <lennart@poettering.net>
2025-09-29 10:45:14 +0200

    test: make sure TEST-50-DISSECT.mountfsd.sh can run standalone

--- test/units/TEST-50-DISSECT.mountfsd.sh
+++ test/units/TEST-50-DISSECT.mountfsd.sh
@@ -61,7 +61,8 @@ if (SYSTEMD_LOG_TARGET=console varlinkctl call \
 fi
 
 # Install key in keychain
-cp /tmp/test-50-unpriv-cert.crt /run/verity.d
+mkdir -p /run/verity.d
+cp /tmp/test-50-unpriv-cert.crt /run/verity.d/
 
 # This should work now
 systemd-dissect --image-policy='root=signed:=absent+unused' --mtree /var/tmp/unpriv.raw >/dev/null
