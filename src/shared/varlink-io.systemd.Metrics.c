cf6d9d5144703b2f5bf3c18ac7eb2177532516aa
Yaping Li <202858510+YapingLi04@users.noreply.github.com>
2025-10-01 07:58:52 -0700

    [metrics] Introduce metrics API framework
    This commit introduces the shared code for the metrics API framework:
    
    - Metrics API definitions
    - Code to set up the varlink server
    - The describe method which shows all the metrics families
    - The list method which lists all the metrics
    - Type definitions related to MetricFamily
    - Common code to build json objects

---
+++ src/shared/varlink-io.systemd.Metrics.c
@@ -0,0 +1,52 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+
+#include "sd-varlink-idl.h"
+
+#include "varlink-io.systemd.Metrics.h"
+
+static SD_VARLINK_DEFINE_ENUM_TYPE(
+                MetricFamilyType,
+                SD_VARLINK_FIELD_COMMENT("A counter metric family type"),
+                SD_VARLINK_DEFINE_ENUM_VALUE(counter),
+                SD_VARLINK_FIELD_COMMENT("A gauge metric family type"),
+                SD_VARLINK_DEFINE_ENUM_VALUE(gauge),
+                SD_VARLINK_FIELD_COMMENT("A string metric family type"),
+                SD_VARLINK_DEFINE_ENUM_VALUE(string));
+
+static SD_VARLINK_DEFINE_ERROR(NoSuchMetric);
+
+static SD_VARLINK_DEFINE_METHOD_FULL(
+                List,
+                SD_VARLINK_SUPPORTS_MORE,
+                SD_VARLINK_FIELD_COMMENT("Metric name"),
+                SD_VARLINK_DEFINE_FIELD(name, SD_VARLINK_STRING, SD_VARLINK_NULLABLE),
+                /* metric value has various types depending on MetricFamilyType and actual data double/int/uint */
+                SD_VARLINK_FIELD_COMMENT("Metric value"),
+                SD_VARLINK_DEFINE_FIELD(value, SD_VARLINK_OBJECT, SD_VARLINK_NULLABLE),
+                SD_VARLINK_FIELD_COMMENT("Metric object name"),
+                SD_VARLINK_DEFINE_FIELD(object, SD_VARLINK_STRING, SD_VARLINK_NULLABLE),
+                SD_VARLINK_FIELD_COMMENT("Metric fields"),
+                SD_VARLINK_DEFINE_FIELD(fields, SD_VARLINK_OBJECT, SD_VARLINK_MAP|SD_VARLINK_NULLABLE));
+
+static SD_VARLINK_DEFINE_METHOD_FULL(
+                Describe,
+                SD_VARLINK_SUPPORTS_MORE,
+                SD_VARLINK_FIELD_COMMENT("Metric family name"),
+                SD_VARLINK_DEFINE_FIELD(name, SD_VARLINK_STRING, 0),
+                SD_VARLINK_FIELD_COMMENT("Metric family description"),
+                SD_VARLINK_DEFINE_FIELD(description, SD_VARLINK_STRING, 0),
+                SD_VARLINK_FIELD_COMMENT("Metric family type"),
+                SD_VARLINK_DEFINE_FIELD_BY_TYPE(type, MetricFamilyType, 0));
+
+SD_VARLINK_DEFINE_INTERFACE(
+               io_systemd_Metrics,
+               "io.systemd.Metrics",
+               SD_VARLINK_INTERFACE_COMMENT("Metrics APIs"),
+               SD_VARLINK_SYMBOL_COMMENT("An enum representing various metric family types"),
+               &vl_type_MetricFamilyType,
+               SD_VARLINK_SYMBOL_COMMENT("Method to get a list of metrics"),
+               &vl_method_List,
+               SD_VARLINK_SYMBOL_COMMENT("Method to get the metric families"),
+               &vl_method_Describe,
+               SD_VARLINK_SYMBOL_COMMENT("No such metric found"),
+               &vl_error_NoSuchMetric);
