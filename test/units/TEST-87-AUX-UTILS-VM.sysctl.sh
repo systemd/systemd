086890bc52979526a3a3685048c8de9ed4280cc6
Yu Watanabe <watanabe.yu+github@gmail.com>
2025-10-06 03:21:05 +0900

    test: add test cases for sysctl reading configuration from stdin or positional arguments

--- test/units/TEST-87-AUX-UTILS-VM.sysctl.sh
+++ test/units/TEST-87-AUX-UTILS-VM.sysctl.sh
@@ -16,6 +16,32 @@ echo "-foo.foo=42" >/tmp/foo.conf
 assert_rc 0 /usr/lib/systemd/systemd-sysctl /tmp/foo.conf
 assert_rc 0 /usr/lib/systemd/systemd-sysctl --strict /tmp/foo.conf
 
+assert_rc 0 /usr/lib/systemd/systemd-sysctl --inline "foo.bar=42"
+assert_rc 1 /usr/lib/systemd/systemd-sysctl --inline --strict "foo.bar=42"
+assert_rc 0 /usr/lib/systemd/systemd-sysctl --inline "-foo.bar=42"
+assert_rc 0 /usr/lib/systemd/systemd-sysctl --inline --strict "-foo.bar=42"
+
+assert_rc 0 (
+    /usr/lib/systemd/systemd-sysctl - <<EOF
+foo.bar=42
+EOF
+)
+assert_rc 1 (
+    /usr/lib/systemd/systemd-sysctl --strict - <<EOF
+foo.bar=42
+EOF
+)
+assert_rc 0 (
+    /usr/lib/systemd/systemd-sysctl - <<EOF
+-foo.bar=42
+EOF
+)
+assert_rc 0 (
+    /usr/lib/systemd/systemd-sysctl --strict - <<EOF
+-foo.bar=42
+EOF
+)
+
 ip link add hoge type dummy
 trap 'ip link del hoge' EXIT
 udevadm wait --timeout=30 /sys/class/net/hoge
@@ -31,6 +57,33 @@ echo 0 >/proc/sys/net/ipv4/conf/hoge/bootp_relay
 echo 0 >/proc/sys/net/ipv4/conf/hoge/disable_policy
 
 assert_rc 0 /usr/lib/systemd/systemd-sysctl --prefix=/net/ipv4/conf/hoge /tmp/foo.conf
+assert_eq "$(cat /proc/sys/net/ipv4/conf/hoge/drop_gratuitous_arp)" "1"
+assert_eq "$(cat /proc/sys/net/ipv4/conf/hoge/bootp_relay)" "1"
+assert_eq "$(cat /proc/sys/net/ipv4/conf/hoge/disable_policy)" "0"
+
+echo 0 >/proc/sys/net/ipv4/conf/hoge/drop_gratuitous_arp
+echo 0 >/proc/sys/net/ipv4/conf/hoge/bootp_relay
+echo 0 >/proc/sys/net/ipv4/conf/hoge/disable_policy
+
+assert_rc 0 /usr/lib/systemd/systemd-sysctl --prefix=/net/ipv4/conf/hoge --inline \
+          'net.ipv4.conf.*.drop_gratuitous_arp=1' \
+          'net.ipv4.*.*.bootp_relay=1' \
+          'net.ipv4.aaa.*.disable_policy=1'
+assert_eq "$(cat /proc/sys/net/ipv4/conf/hoge/drop_gratuitous_arp)" "1"
+assert_eq "$(cat /proc/sys/net/ipv4/conf/hoge/bootp_relay)" "1"
+assert_eq "$(cat /proc/sys/net/ipv4/conf/hoge/disable_policy)" "0"
+
+echo 0 >/proc/sys/net/ipv4/conf/hoge/drop_gratuitous_arp
+echo 0 >/proc/sys/net/ipv4/conf/hoge/bootp_relay
+echo 0 >/proc/sys/net/ipv4/conf/hoge/disable_policy
+
+assert_rc 0 (
+    /usr/lib/systemd/systemd-sysctl --prefix=/net/ipv4/conf/hoge - <<EOF
+net.ipv4.conf.*.drop_gratuitous_arp=1
+net.ipv4.*.*.bootp_relay=1
+net.ipv4.aaa.*.disable_policy=1
+EOF
+)
 assert_eq "$(cat /proc/sys/net/ipv4/conf/hoge/drop_gratuitous_arp)" "1"
 assert_eq "$(cat /proc/sys/net/ipv4/conf/hoge/bootp_relay)" "1"
 assert_eq "$(cat /proc/sys/net/ipv4/conf/hoge/disable_policy)" "0"
