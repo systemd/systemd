c9e121abad3ed6a4e87eb3e9dd33501afa467d0b
Yu Watanabe <watanabe.yu+github@gmail.com>
2025-10-08 09:53:01 +0900

    coredump: split out submit_coredump() to coredump-submit.[ch]
    Then this renames the function to coredump_submit().

---
+++ src/coredump/coredump-submit.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#pragma once
+
+#include "coredump-forward.h"
+
+int coredump_submit(
+                const Context *context,
+                struct iovec_wrapper *iovw,
+                int input_fd);
