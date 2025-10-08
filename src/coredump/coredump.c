760b4513cb8a212c8ddade056b4260a6b321aefe
Yu Watanabe <watanabe.yu+github@gmail.com>
2025-10-08 10:06:55 +0900

    coredump: drop unnecessary header inclusion

--- src/coredump/coredump.c
+++ src/coredump/coredump.c
@@ -1,67 +1,16 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 
-#include <elf.h>
-#include <stdio.h>
-#include <sys/mount.h>
-#include <sys/statvfs.h>
-#include <sys/xattr.h>
-#include <unistd.h>
-
-#include "sd-bus.h"
 #include "sd-daemon.h"
-#include "sd-journal.h"
-#include "sd-json.h"
-#include "sd-login.h"
-#include "sd-messages.h"
 
-#include "acl-util.h"
-#include "alloc-util.h"
-#include "bus-error.h"
-#include "capability-util.h"
-#include "cgroup-util.h"
-#include "compress.h"
-#include "conf-parser.h"
-#include "copy.h"
 #include "coredump-backtrace.h"
 #include "coredump-config.h"
 #include "coredump-context.h"
 #include "coredump-kernel-helper.h"
 #include "coredump-receive.h"
-#include "coredump-send.h"
-#include "coredump-submit.h"
 #include "coredump-util.h"
-#include "coredump-vacuum.h"
-#include "dirent-util.h"
-#include "elf-util.h"
-#include "errno-util.h"
-#include "escape.h"
-#include "fd-util.h"
-#include "fileio.h"
-#include "fs-util.h"
-#include "io-util.h"
-#include "iovec-util.h"
-#include "journal-importer.h"
-#include "journal-send.h"
-#include "json-util.h"
 #include "log.h"
 #include "main-func.h"
-#include "memory-util.h"
-#include "memstream-util.h"
-#include "mkdir-label.h"
-#include "namespace-util.h"
-#include "parse-util.h"
-#include "path-util.h"
-#include "pidref.h"
-#include "process-util.h"
-#include "signal-util.h"
-#include "socket-util.h"
-#include "special.h"
-#include "stat-util.h"
-#include "string-table.h"
 #include "string-util.h"
-#include "tmpfile-util.h"
-#include "uid-classification.h"
-#include "user-util.h"
 
 static int run(int argc, char *argv[]) {
         int r;
