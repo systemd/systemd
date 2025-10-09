b6192661252c4a3c2018abaeb6b826f172e03d3e
Frantisek Sumsal <frantisek@sumsal.cz>
2025-10-09 23:08:19 +0200

    test: build the crashing test binary outside of the test
    So we don't have to pull in gcc and other stuff into it.
    
    Also, make the test itself a bit more robust and debug-able.

---
+++ src/test/test-coredump-stacktrace.c
@@ -0,0 +1,27 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+
+/* This is a test program that intentionally segfaults so we can generate a
+ * predictable-ish stack trace in tests. */
+
+static void baz(void) {
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wzero-as-null-pointer-constant"
+        int *x = 0;
+#pragma GCC diagnostic pop
+
+        *x = 42;
+}
+
+static void bar(void) {
+        baz();
+}
+
+static void foo(void) {
+        bar();
+}
+
+int main(void) {
+        foo();
+
+        return 0;
+}
