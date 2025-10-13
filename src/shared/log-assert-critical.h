9c1db4cd8e0dfeb2d35f9720236b1bcc9844db23
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2025-10-13 20:44:05 +0200

    Do not use "critical assert_return" in libsystemd or libudev
    Previously, when compiled in developer mode, a call into libsystemd with
    invalid parameters would result in an abort. This means that it's effectively
    impossible to install such libsystemd in a normal system, since various
    third-party programs may now abort. A shared library should generally never
    abort or exit the calling program.
    
    In python-systemd, the test suite calls into libsystemd, to check if the proper
    return values are received and propagated through the Python wrappers.
    Obviously with libsystemd compiled from git, the test suite now fails
    in a nasty way.
    
    So rework the code to set assert_return_is_critical similarly to how we handle
    mempool enablement: the function that returns true is declared as a week
    symbol, and we "opt in" by linking a file that provides the function in
    libsystemd-shared. Effectively, libsystemd and libudev always have
    assert_return_is_critical==false, and our binaries and modules enable it
    conditionally.
    
    (cherry picked from commit 0bb0316f5eb2c8d30e91feac571404687c6a0dc2)

---
+++ src/shared/log-assert-critical.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#pragma once
+
+#include "forward.h"
+
+void log_set_assert_return_is_critical(bool b);
+void log_set_assert_return_is_critical_from_env(void);
