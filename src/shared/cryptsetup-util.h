2c262ce785f2b31f76735069cde2831c1a4474bb
Vitaly Kuznetsov <vkuznets@redhat.com>
2025-10-16 14:47:22 +0200

    dissect-image: introduce 'encryptedwithintegrity' policy
    LUKS2 supports built-in integrity checking which may come very handy to
    mitigate partial rollback attacks on the storage when only some specific
    parts are restored to some old encrypted state. Specific use-cases like
    Confidential VMs may want to mandate the usage of feature e.g. on the root
    volume. Introduce "encryptedwithintegrity" image policy to support that.
    
    Note, due to the current libcryptsetup limitations, checking whether the
    feature is enabled or not for the 'file' case (e.g. DDI image as a raw file)
    requires setting up a loop device. It is not yet possible to detect integrity
    for the non-privileged systemd-dissect usage.

--- src/shared/cryptsetup-util.h
+++ src/shared/cryptsetup-util.h
@@ -79,6 +79,7 @@ extern DLSYM_PROTOTYPE(crypt_set_data_offset);
 extern DLSYM_PROTOTYPE(crypt_header_restore);
 extern DLSYM_PROTOTYPE(crypt_volume_key_keyring);
 extern DLSYM_PROTOTYPE(crypt_wipe);
+extern DLSYM_PROTOTYPE(crypt_get_integrity_info);
 
 DEFINE_TRIVIAL_CLEANUP_FUNC_FULL(struct crypt_device *, crypt_free, NULL);
 DEFINE_TRIVIAL_CLEANUP_FUNC_FULL(struct crypt_device *, sym_crypt_free, NULL);
