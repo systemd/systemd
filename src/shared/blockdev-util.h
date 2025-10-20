39f71af804d52e3d1d639ce6493b0c7eb2980d59
Lennart Poettering <lennart@poettering.net>
2025-10-20 15:21:44 +0200

    udev: switch over to rereadpt() rather than raw BLKRRPART
    Fixes: #38672

--- src/shared/blockdev-util.h
+++ src/shared/blockdev-util.h
@@ -51,7 +51,6 @@ int block_device_remove_partition(int fd, const char *name, int nr);
 int block_device_resize_partition(int fd, int nr, uint64_t start, uint64_t size);
 int partition_enumerator_new(sd_device *dev, sd_device_enumerator **ret);
 int block_device_remove_all_partitions(sd_device *dev, int fd);
-int blockdev_reread_partition_table(sd_device *dev);
 
 int blockdev_get_sector_size(int fd, uint32_t *ret);
 int blockdev_get_device_size(int fd, uint64_t *ret);
