75d5b3f8fafa1caaf4780ea6096300ebde5a4eb3
Yaping Li <202858510+YapingLi04@users.noreply.github.com>
2025-10-20 18:25:10 -0700

    [metrics] Add basic system wide and per unit metrics
    This commit adds some basic metrics and integration tests.
    
    System wide metrics:
    - units_by_type_total: target/device/automount etc.
    - units_by_state_total: active/reloading/inactive etc.
    
    Two per unit metrics which shows the current state of a unit:
    - unit_active_state
    - unit_load_state
    
    A metric for service state:
    - nrestarts
    
    Here are some sample outputs:
    
    units_by_type_total:
    
    {
            "name" : "io.systemd.Manager.units_by_type_total",
            "value" : 52,
            "fields" : {
                    "type" : "target"
            }
    }
    {
            "name" : "io.systemd.Manager.units_by_type_total",
            "value" : 82,
            "fields" : {
                    "type" : "device"
            }
    }
    {
            "name" : "io.systemd.Manager.units_by_type_total",
            "value" : 2,
            "fields" : {
                    "type" : "automount"
            }
    }
    
    units_by_state_total:
    
    {
            "name" : "io.systemd.Manager.units_by_state_total",
            "value" : 216,
            "fields" : {
                    "state" : "active"
            }
    }
    {
            "name" : "io.systemd.Manager.units_by_state_total",
            "value" : 0,
            "fields" : {
                    "state" : "reloading"
            }
    }
    {
            "name" : "io.systemd.Manager.units_by_state_total",
            "value" : 120,
            "fields" : {
                    "state" : "inactive"
            }
    }
    
    unit_active_state:
    
    {
            "name" : "io.systemd.Manager.unit_active_state",
            "object" : "multi-user.target",
            "value" : "active"
    }
    {
            "name" : "io.systemd.Manager.unit_active_state",
            "object" : "systemd-sysusers.service",
            "value" : "inactive"
    }
    
    unit_load_state:
    
    {
            "name" : "io.systemd.Manager.unit_load_state",
            "object" : "multi-user.target",
            "value" : "loaded"
    }
    
    nrestarts:
    
    {
            "name" : "io.systemd.Manager.nrestarts",
            "object" : "user@0.service",
            "value" : 0
    }
    {
            "name" : "io.systemd.Manager.nrestarts",
            "object" : "user-runtime-dir@0.service",
            "value" : 0
    }

---
+++ src/core/varlink-metrics.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#pragma once
+
+#include "sd-varlink.h"
+#include "sd-json.h"
+
+#define METRIC_IO_SYSTEMD_MANAGER_PREFIX "io.systemd.Manager."
+
+int vl_method_list(sd_varlink *link, sd_json_variant *parameters, sd_varlink_method_flags_t flags, void *userdata);
+int vl_method_describe(sd_varlink *link, sd_json_variant *parameters, sd_varlink_method_flags_t flags, void *userdata);
