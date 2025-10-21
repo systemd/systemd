a864ff360b5d68c5c377914eea2a2f99e71aef30
Lennart Poettering <lennart@poettering.net>
2025-10-21 23:00:46 +0200

    nsresource: add constants for the only supported UID range size allocations
    As requested: https://github.com/systemd/systemd/pull/39322#discussion_r2447366728

--- src/shared/nsresource.h
+++ src/shared/nsresource.h
@@ -3,6 +3,10 @@
 
 #include "shared-forward.h"
 
+/* Helpful constants for the only numbers of UIDs that can currently be allocated */
+#define NSRESOURCE_UIDS_64K 0x10000U
+#define NSRESOURCE_UIDS_1 1U
+
 int nsresource_allocate_userns(const char *name, uint64_t size);
 int nsresource_register_userns(const char *name, int userns_fd);
 int nsresource_add_mount(int userns_fd, int mount_fd);
