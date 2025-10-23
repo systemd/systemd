f0ee87a7e334223cbe0cb28358862a21f6a28f58
Daan De Meyer <daan.j.demeyer@gmail.com>
2025-10-23 20:58:40 +0200

    rpm: Rewrite systemd-update-helper in C
    Let's rewrite the script in C, which gives us access to
    libsystemd-shared for implementing future features.
    
    We immediately make use of libsystemd-shared to add a new --stdin
    option, allowing the script to read units to operate on as unit file
    paths from stdin. This allows using the script with various hook/trigger
    features provided by package managers such as pacman and rpm.
    
    As a bonus, the amount of forks is reduced by not shelling out to
    systemctl multiple times which could matter for a script that's
    executed quite a few times during every dnf system upgrade.

--- src/shared/bus-wait-for-jobs.h
+++ src/shared/bus-wait-for-jobs.h
@@ -12,6 +12,7 @@ typedef enum WaitJobsFlags {
 
 BusWaitForJobs* bus_wait_for_jobs_free(BusWaitForJobs *d);
 DEFINE_TRIVIAL_CLEANUP_FUNC(BusWaitForJobs*, bus_wait_for_jobs_free);
+void bus_wait_for_jobs_free_many(BusWaitForJobs **array, size_t n);
 
 int bus_wait_for_jobs_new(sd_bus *bus, BusWaitForJobs **ret);
 int bus_wait_for_jobs_add(BusWaitForJobs *d, const char *path);
