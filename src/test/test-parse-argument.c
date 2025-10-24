4ad9934f5b37fc82aa6f133e5d35aab4f8928b58
Daniel Hast <hast.daniel@protonmail.com>
2025-10-24 18:47:59 -0400

    tree-wide: add basic validation of --background argument
    Check whether the argument of the `--background` option of
    `systemd-run`, `run0`, `systemd-nspawn`, `systemd-vmspawn`, and
    `systemd-pty-forward` is either empty or looks like an ANSI color code,
    and reject invalid values when parsing arguments.
    
    We consider a string to look like an ANSI color code if it consists of
    one or more sequences of ASCII digits separated by semicolons. This
    permits every valid ANSI color code, and should reject anything that
    results in garbled output.

--- src/test/test-parse-argument.c
+++ src/test/test-parse-argument.c
@@ -54,4 +54,37 @@ TEST(parse_signal_argument) {
         assert_se(signal == SIGABRT);
 }
 
+TEST(parse_background_argument) {
+        _cleanup_free_ char *arg_bg_good = NULL;
+
+        /* Should accept empty string */
+        assert_se(parse_background_argument("", &arg_bg_good) >= 0);
+        ASSERT_STREQ(arg_bg_good, "");
+
+        /* Should accept ANSI color codes in palette, 8-bit, or 24-bit format */
+        assert_se(parse_background_argument("42", &arg_bg_good) >= 0);
+        ASSERT_STREQ(arg_bg_good, "42");
+
+        assert_se(parse_background_argument("48;5;219", &arg_bg_good) >= 0);
+        ASSERT_STREQ(arg_bg_good, "48;5;219");
+
+        assert_se(parse_background_argument("48;2;3;141;59", &arg_bg_good) >= 0);
+        ASSERT_STREQ(arg_bg_good, "48;2;3;141;59");
+
+        _cleanup_free_ char *arg_bg_bad = NULL;
+
+        /* Should reject various invalid arguments */
+        assert_se(parse_background_argument("42;", &arg_bg_bad) < 0);
+        ASSERT_NULL(arg_bg_bad);
+
+        assert_se(parse_background_argument(";42", &arg_bg_bad) < 0);
+        ASSERT_NULL(arg_bg_bad);
+
+        assert_se(parse_background_argument("4;;2", &arg_bg_bad) < 0);
+        ASSERT_NULL(arg_bg_bad);
+
+        assert_se(parse_background_argument("4a2", &arg_bg_bad) < 0);
+        ASSERT_NULL(arg_bg_bad);
+}
+
 DEFINE_TEST_MAIN(LOG_INFO);
