7d7b1755e9f19c70c90bea849f5e51c7a2dd5f86
Yu Watanabe <watanabe.yu+github@gmail.com>
2025-10-25 11:50:58 +0900

    passwdqc: drop 'sym_' prefix from cleanup function
    This also drops unnecessary symbols in header.

--- src/shared/password-quality-util-passwdqc.h
+++ src/shared/password-quality-util-passwdqc.h
@@ -6,20 +6,8 @@
 #if HAVE_PASSWDQC
 #include <passwdqc.h>
 
-#include "dlfcn-util.h"
-
-extern DLSYM_PROTOTYPE(passwdqc_params_reset);
-extern DLSYM_PROTOTYPE(passwdqc_params_load);
-extern DLSYM_PROTOTYPE(passwdqc_params_parse);
-extern DLSYM_PROTOTYPE(passwdqc_params_free);
-extern DLSYM_PROTOTYPE(passwdqc_check);
-extern DLSYM_PROTOTYPE(passwdqc_random);
-
-DEFINE_TRIVIAL_CLEANUP_FUNC_FULL(passwdqc_params_t*, sym_passwdqc_params_free, NULL);
-
 int suggest_passwords(void);
 int check_password_quality(const char *password, const char *old, const char *username, char **ret_error);
-
 #endif
 
 int dlopen_passwdqc(void);
