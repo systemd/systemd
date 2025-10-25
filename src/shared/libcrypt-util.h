f46cb96956a5c8cb44388a702fe0c4ca9c0a16bc
Yu Watanabe <watanabe.yu+github@gmail.com>
2025-10-25 14:59:54 +0900

    libcrypt: allow to build systemd without libcrypt/libxcrypt
    libcrypt is only used by firstboot, homed, and sysusers, which can be
    disabled by meson option.
    Let's not require the library unconditionally.

--- src/shared/libcrypt-util.h
+++ src/shared/libcrypt-util.h
@@ -3,8 +3,17 @@
 
 #include "shared-forward.h"
 
+#if HAVE_LIBCRYPT
 int make_salt(char **ret);
 int hash_password(const char *password, char **ret);
 int test_password_one(const char *hashed_password, const char *password);
 int test_password_many(char **hashed_password, const char *password);
+
+#else
+
+static inline int hash_password(const char *password, char **ret) {
+        return -EOPNOTSUPP;
+}
+#endif
+
 bool looks_like_hashed_password(const char *s);
