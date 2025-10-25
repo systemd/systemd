7ba7f59c1dc47053331de9caf74e83a09bc9aca1
Yu Watanabe <watanabe.yu+github@gmail.com>
2025-10-25 13:41:33 +0900

    libcrypt-util: turn into dlopen() dependency
    Note, this drops logging only test case for crypt_preferred_method(),
    as that requires explicitly dlopen() the library. But, we should test
    that make_salt() and friends automatically dlopen() it.

--- src/shared/libcrypt-util.h
+++ src/shared/libcrypt-util.h
@@ -4,6 +4,7 @@
 #include "shared-forward.h"
 
 #if HAVE_LIBCRYPT
+int dlopen_libcrypt(void);
 int make_salt(char **ret);
 int hash_password(const char *password, char **ret);
 int test_password_one(const char *hashed_password, const char *password);
@@ -11,6 +12,9 @@ int test_password_many(char **hashed_password, const char *password);
 
 #else
 
+static inline int dlopen_libcrypt(void) {
+        return -EOPNOTSUPP;
+}
 static inline int hash_password(const char *password, char **ret) {
         return -EOPNOTSUPP;
 }
