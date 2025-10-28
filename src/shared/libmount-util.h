8792f8a6c130dd974e413b59acf3e5cbf40ba038
Daan De Meyer <daan.j.demeyer@gmail.com>
2025-10-28 22:54:14 +0100

    mount-util: Iterate mountinfo backwards when unmounting
    Submounts will always be located further in the mountinfo file, so
    when we're unmounting, iterating backwards is likely to be more
    efficient than iterating forwards. It'll also reduce the amount of
    EBUSY debug logging we'll get since we'll stop trying to unmount
    parent mounts with submounts which will always fail with EBUSY.

--- src/shared/libmount-util.h
+++ src/shared/libmount-util.h
@@ -47,6 +47,7 @@ DEFINE_TRIVIAL_CLEANUP_FUNC_FULL_RENAME(struct libmnt_iter*, sym_mnt_free_iter, 
 int libmount_parse_full(
                 const char *path,
                 FILE *source,
+                int direction,
                 struct libmnt_table **ret_table,
                 struct libmnt_iter **ret_iter);
 
@@ -55,14 +56,14 @@ static inline int libmount_parse_mountinfo(
                 struct libmnt_table **ret_table,
                 struct libmnt_iter **ret_iter) {
 
-        return libmount_parse_full("/proc/self/mountinfo", source, ret_table, ret_iter);
+        return libmount_parse_full("/proc/self/mountinfo", source, MNT_ITER_FORWARD, ret_table, ret_iter);
 }
 
 static inline int libmount_parse_with_utab(
                 struct libmnt_table **ret_table,
                 struct libmnt_iter **ret_iter) {
 
-        return libmount_parse_full(NULL, NULL, ret_table, ret_iter);
+        return libmount_parse_full(NULL, NULL, MNT_ITER_FORWARD, ret_table, ret_iter);
 }
 
 int libmount_parse_fstab(struct libmnt_table **ret_table, struct libmnt_iter **ret_iter);
