c4df285ed4af2390f30a77a2ae32496b42c8fa18
Daan De Meyer <daan.j.demeyer@gmail.com>
2025-11-01 22:18:19 +0100

    bus-wait-for-jobs: Introduce _full() variants
    The extra arguments are almost never required so let's hide them
    behind _full() variants.

--- src/shared/bus-wait-for-jobs.h
+++ src/shared/bus-wait-for-jobs.h
@@ -15,5 +15,11 @@ DEFINE_TRIVIAL_CLEANUP_FUNC(BusWaitForJobs*, bus_wait_for_jobs_free);
 
 int bus_wait_for_jobs_new(sd_bus *bus, BusWaitForJobs **ret);
 int bus_wait_for_jobs_add(BusWaitForJobs *d, const char *path);
-int bus_wait_for_jobs(BusWaitForJobs *d, WaitJobsFlags flags, const char* const* extra_args);
-int bus_wait_for_jobs_one(BusWaitForJobs *d, const char *path, WaitJobsFlags flags, const char* const* extra_args);
+int bus_wait_for_jobs_full(BusWaitForJobs *d, WaitJobsFlags flags, const char* const* extra_args);
+static inline int bus_wait_for_jobs(BusWaitForJobs *d, WaitJobsFlags flags) {
+        return bus_wait_for_jobs_full(d, flags, /* extra_args= */ NULL);
+}
+int bus_wait_for_jobs_one_full(BusWaitForJobs *d, const char *path, WaitJobsFlags flags, const char* const* extra_args);
+static inline int bus_wait_for_jobs_one(BusWaitForJobs *d, const char *path, WaitJobsFlags flags, const char* const* extra_args) {
+        return bus_wait_for_jobs_one_full(d, path, flags, /* extra_args= */ NULL);
+}
