11abacb6bb317955cf467e4a5f65280002bb997b
Yu Watanabe <watanabe.yu+github@gmail.com>
2025-11-05 05:44:00 +0900

    coredump: introduce varlink interface to register socket core pattern
    Since kernel v6.17, we can set a path to unix socket to core_pattern (with request mode).
    See also: https://lore.kernel.org/all/20250603-work-coredump-socket-protocol-v2-0-05a5f0c18ecc@kernel.org/
    
    This adds new mode to invoke systemd-coredump executable. When invoked with
    '--register' option, it accepts a varlink method to set new core pattern to
    /proc/sys/kernel/core_pattern. This is necessary as the core pattern cannot
    be registered from sandboxed environment, but we'd like to run the main
    service, which will be added in later commit, in a sandbox.

--- src/coredump/coredump.c
+++ src/coredump/coredump.c
@@ -5,6 +5,7 @@
 #include "coredump-backtrace.h"
 #include "coredump-kernel-helper.h"
 #include "coredump-receive.h"
+#include "coredump-register.h"
 #include "coredump-util.h"
 #include "log.h"
 #include "main-func.h"
@@ -17,6 +18,9 @@ static int run(int argc, char *argv[]) {
          * from the command, and unexpectedly passed file descriptors can be silently ignored. */
         if (streq_ptr(argv[1], "--backtrace"))
                 return coredump_backtrace(argc, argv);
+
+        if (streq_ptr(argv[1], "--register"))
+                return coredump_register(argc, argv);
 
         /* First, log to a safe place, since we don't know what crashed and it might be journald which we'd
          * rather not log to then. */
