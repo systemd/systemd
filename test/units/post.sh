9d485e02234c10dd16d89e8066348f31f2f17e61
Luca Boccassi <luca.boccassi@gmail.com>
2025-11-04 12:51:09 +0000

    test: stop piping post-test journalctl commands to /failed
    We can't see what the actual issues are when tests fail at that point, eg:
    
    https://github.com/systemd/systemd/actions/runs/19034752357/job/54356278052
    (cherry picked from commit 22311a12912509dd8f275b4f5d42383ee70327eb)

--- test/units/post.sh
+++ test/units/post.sh
@@ -3,11 +3,11 @@
 set -eux
 set -o pipefail
 
-(! journalctl -q -o short-monotonic --grep "didn't pass validation" | grep -v "test-varlink-idl" >>/failed)
+(! journalctl -q -o short-monotonic --grep "didn't pass validation" | grep -v "test-varlink-idl")
 
 # Here, the redundant '[ ]' in the pattern is required in order not to match the logged command itself.
-(! journalctl -q -o short-monotonic --grep 'Warning: cannot close sd-bus connection[ ].*after fork' >>/failed)
+(! journalctl -q -o short-monotonic --grep 'Warning: cannot close sd-bus connection[ ].*after fork')
 
 # Check if sd-executor doesn't complain about not being able to (de)serialize stuff
-(! journalctl -q -o short-monotonic --grep "[F]ailed to parse serialized line" >>/failed)
-(! journalctl -q -o short-monotonic --grep "[F]ailed to (de)?serialize \w+" >>/failed)
+(! journalctl -q -o short-monotonic --grep "[F]ailed to parse serialized line")
+(! journalctl -q -o short-monotonic --grep "[F]ailed to (de)?serialize \w+")
