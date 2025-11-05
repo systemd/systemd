b9a675432077134ccb7c24a7fa8ddabdeff59739
Oleksandr Andrushchenko <andr2000@gmail.com>
2025-11-05 10:10:06 +0200

    Fix linter

--- src/network/networkd-wwan.h
+++ src/network/networkd-wwan.h
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 #pragma once
 
+#include "in-addr-util.h"
 #include "networkd-forward.h"
 #include "networkd-util.h"
 #include "networkd-wwan-bus.h"
