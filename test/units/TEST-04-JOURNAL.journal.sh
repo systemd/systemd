4b6c05a43ea14d3921a39323e4ceb252384bb7bc
Frantisek Sumsal <frantisek@sumsal.cz>
2025-11-06 14:40:56 +0100

    test: sync journal after the test unit finishes
    In these two cases we need to sync the journal _after_ the unit
    finishes, because we try to match messages from systemd itself, not
    (only) from the unit, and the messages about units are dispatched
    asynchronously.
    
    That is, in the first case (silent-success.service) we want to make sure
    that LogLevelMax= filters out messages _about_ units (from systemd) as
    well, including messages like "Deactivated..."  and "Finished...", which
    are sent out only when/after the unit is stopped.
    
    In the second case we try to match messages with the "systemd" syslog
    tag, but these messages come from systemd (obviously) and are sent out
    asynchronously, which means they might not reach the journal before we
    call `journalctl --sync` from the test unit itself, like happened here:
    
    [ 1754.150391] TEST-04-JOURNAL.sh[13331]: + systemctl start verbose-success.service
    [ 1754.172256] bash[13692]: success
    [ 1754.221210] TEST-04-JOURNAL.sh[13694]: ++ journalctl -b -q -u verbose-success.service -t systemd
    [ 1754.221493] TEST-04-JOURNAL.sh[13331]: + [[ -n '' ]]
    [ 1754.175709] systemd[1]: Starting verbose-success.service - Verbose successful service...
    [ 1754.221697] TEST-04-JOURNAL.sh[122]: + echo 'Subtest /usr/lib/systemd/tests/testdata/units/TEST-04-JOURNAL.journal.sh failed'
    [ 1754.221697] TEST-04-JOURNAL.sh[122]: Subtest /usr/lib/systemd/tests/testdata/units/TEST-04-JOURNAL.journal.sh failed
    [ 1754.221697] TEST-04-JOURNAL.sh[122]: + return 1
    [ 1754.205408] systemd[1]: verbose-success.service: Deactivated successfully.
    [ 1754.205687] systemd[1]: Finished verbose-success.service - Verbose successful service.
    
    By syncing the journal after the unit is stopped we have much bigger
    chance that the systemd messages already reached the journal - the race
    is technically still there, but the chance we'd hit it should be pretty
    negligible.
    
    Resolves: #39555

--- test/units/TEST-04-JOURNAL.journal.sh
+++ test/units/TEST-04-JOURNAL.journal.sh
@@ -104,15 +104,17 @@ diff /tmp/expected /tmp/output
 
 # test that LogLevelMax can also suppress logging about services, not only by services
 systemctl start silent-success
+journalctl --sync
 [[ -z "$(journalctl -b -q -u silent-success.service)" ]]
 
 # Test syslog identifiers exclusion
 systemctl start verbose-success.service
+journalctl --sync
 [[ -n "$(journalctl -b -q -u verbose-success.service -t systemd)" ]]
-[[ -n "$(journalctl -b -q -u verbose-success.service -t bash)" ]]
+[[ -n "$(journalctl -b -q -u verbose-success.service -t echo)" ]]
 [[ -n "$(journalctl -b -q -u verbose-success.service -T systemd)" ]]
-[[ -n "$(journalctl -b -q -u verbose-success.service -T bash)" ]]
-[[ -z "$(journalctl -b -q -u verbose-success.service -T bash -T '(bash)' -T systemd -T '(systemd)')" ]]
+[[ -n "$(journalctl -b -q -u verbose-success.service -T echo)" ]]
+[[ -z "$(journalctl -b -q -u verbose-success.service -T echo -T '(echo)' -T systemd -T '(systemd)')" ]]
 
 # Exercise the matching machinery
 SYSTEMD_LOG_LEVEL=debug journalctl -b -n 1 /dev/null /dev/zero /dev/null /dev/null /dev/null
