c27da74841430ff0e1f2a61717513f99fb9816e1
Lennart Poettering <lennart@amutable.com>
2025-11-07 08:33:32 +0100

    tar-util: add support for extracting OCI compatible whiteouts, and turn them into overlayfs whiteouts

--- src/shared/tar-util.h
+++ src/shared/tar-util.h
@@ -4,6 +4,7 @@
 typedef enum TarFlags {
         TAR_SELINUX               = 1 << 0, /* Include SELinux xattr in tarball, or unpack it */
         TAR_SQUASH_UIDS_ABOVE_64K = 1 << 1, /* Squash UIDs/GIDs above 64K when packing/unpacking to the nobody user */
+        TAR_OCI_WHITEOUTS         = 1 << 2, /* Turn OCI/aufs whiteout inodes into overlayfs whiteouts */
 } TarFlags;
 
 int tar_x(int input_fd, int tree_fd, TarFlags flags);
