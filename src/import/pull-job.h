9bd86318391d7b51bf15ba8a763eedcd1097e235
Lennart Poettering <lennart@amutable.com>
2025-11-07 08:32:39 +0100

    pull-job: make sure pull_job_restart() can be used to fetch the same resource again, just with new headers
    Let's flush out all response state from the job, but let's keep the
    request data previously configured, in particular the headers set. This
    is useful to re-request a resource, just with a slightly modified or
    identical URL.

--- src/import/pull-job.h
+++ src/import/pull-job.h
@@ -104,4 +104,6 @@ int pull_job_add_request_header(PullJob *j, const char *hdr);
 int pull_job_set_accept(PullJob *j, char * const *l);
 int pull_job_set_bearer_token(PullJob *j, const char *token);
 
+int pull_job_restart(PullJob *j, const char *new_url);
+
 DEFINE_TRIVIAL_CLEANUP_FUNC(PullJob*, pull_job_unref);
