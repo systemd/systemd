dfda09adf6afec4d6b62cb4edb7c051ff0f94e25
Lennart Poettering <lennart@amutable.com>
2025-11-07 08:31:34 +0100

    pull-job: add helpers to detect requests for authentication, and accept bearer tokens

--- src/import/pull-job.h
+++ src/import/pull-job.h
@@ -87,6 +87,8 @@ typedef struct PullJob {
 
         bool sync;
         bool force_memory;
+
+        char *authentication_challenge;
 } PullJob;
 
 int pull_job_new(PullJob **ret, const char *url, CurlGlue *glue, void *userdata);
@@ -100,5 +102,6 @@ void pull_job_close_disk_fd(PullJob *j);
 
 int pull_job_add_request_header(PullJob *j, const char *hdr);
 int pull_job_set_accept(PullJob *j, char * const *l);
+int pull_job_set_bearer_token(PullJob *j, const char *token);
 
 DEFINE_TRIVIAL_CLEANUP_FUNC(PullJob*, pull_job_unref);
