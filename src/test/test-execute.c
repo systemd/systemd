71d04e48909bf920560b885a6fba50029003e9a5
Yu Watanabe <watanabe.yu+github@gmail.com>
2025-11-12 02:29:34 +0900

    tree-wide: drop redundant inclusion of linux/prctl.h
    sys/prctl.h anyway includes linux/prctl.h and actually these .c files
    includes sys/prctl.h. Hence, it is not necessary to explicitly include
    linux/prctl.h.
    
    (cherry picked from commit 9b55c4b85972df3d224a5a0852334e54324ca12c)

--- src/test/test-execute.c
+++ src/test/test-execute.c
@@ -1,7 +1,6 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 
 #include <fnmatch.h>
-#include <linux/prctl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/mount.h>
--- src/test/test-namespace.c
+++ src/test/test-namespace.c
@@ -1,7 +1,6 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 
 #include <fcntl.h>
-#include <linux/prctl.h>
 #include <sched.h>
 #include <stdlib.h>
 #include <sys/prctl.h>
