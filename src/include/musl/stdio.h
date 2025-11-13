90cc2949bcdd5898025e9f50635cb96c493687cd
Yu Watanabe <watanabe.yu+github@gmail.com>
2025-11-13 13:40:19 +0900

    musl: stdio: workarounds for bugs in fputs() and friends

--- src/include/musl/stdio.h
+++ src/include/musl/stdio.h
@@ -11,3 +11,21 @@
 int missing_renameat2(int __oldfd, const char *__old, int __newfd, const char *__new, unsigned __flags);
 #  define renameat2 missing_renameat2
 #endif
+
+/* When a stream is opened read-only under glibc, fputs() and friends fail with EBADF. However, they
+ * unexpectedly succeed under musl. The following _bugfix() functions first check if the passed stream is
+ * writable, and refuse to write with EBADF if not. */
+
+int putc_bugfix(int c, FILE *stream);
+int putc_unlocked_bugfix(int c, FILE *stream);
+int fputc_bugfix(int c, FILE *stream);
+int fputc_unlocked_bugfix(int c, FILE *stream);
+int fputs_bugfix(const char *s, FILE *stream);
+int fputs_unlocked_bugfix(const char *s, FILE *stream);
+
+#define putc           putc_bugfix
+#define putc_unlocked  putc_unlocked_bugfix
+#define fputc          fputc_bugfix
+#define fputc_unlocked fputc_unlocked_bugfix
+#define fputs          fputs_bugfix
+#define fputs_unlocked fputs_unlocked_bugfix
