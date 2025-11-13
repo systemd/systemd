b2b8fb810509442ceca33555d47a04ff527b3da7
Lennart Poettering <lennart@poettering.net>
2025-11-13 09:30:43 +0100

    boot: report missing GetActivePcrBanks() call in formware as UINT32_MAX PCR bank mask
    Fixes: #39150

--- src/shared/tpm2-util.c
+++ src/shared/tpm2-util.c
@@ -2705,7 +2705,9 @@ int tpm2_get_best_pcr_bank(
 
                 /* If variable is not set use guesswork below */
                 log_debug("Boot loader didn't set the LoaderTpm2ActivePcrBanks EFI variable, we have to guess the used PCR banks.");
-        } else {
+        } else if (efi_banks == UINT32_MAX)
+                log_debug("Boot loader set the LoaderTpm2ActivePcrBanks EFI variable to indicate that the GetActivePcrBanks() API is not available in the firmware. We have to guess the used PCR banks.");
+        else {
                 if (BIT_SET(efi_banks, TPM2_ALG_SHA256))
                         *ret = TPM2_ALG_SHA256;
                 else if (BIT_SET(efi_banks, TPM2_ALG_SHA1))
@@ -2812,7 +2814,9 @@ int tpm2_get_good_pcr_banks(
 
                 /* If the variable is not set we have to guess via the code below */
                 log_debug("Boot loader didn't set the LoaderTpm2ActivePcrBanks EFI variable, we have to guess the used PCR banks.");
-        } else {
+        } else if (efi_banks == UINT32_MAX)
+                log_debug("Boot loader set the LoaderTpm2ActivePcrBanks EFI variable to indicate that the GetActivePcrBanks() API is not available in the firmware. We have to guess the used PCR banks.");
+        else {
                 FOREACH_ARRAY(hash, tpm2_hash_algorithms, TPM2_N_HASH_ALGORITHMS) {
                         if (!BIT_SET(efi_banks, *hash))
                                 continue;
