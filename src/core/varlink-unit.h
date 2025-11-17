4fa7c7bea7c2bbf9c717bce8e2001e11c3e1bd26
Luca Boccassi <luca.boccassi@gmail.com>
2025-11-17 00:16:44 +0000

    manager: add EnqueueMarkedJobs varlink method
    Same as the D-Bus method. Returns array of job IDs.

--- src/core/varlink-unit.h
+++ src/core/varlink-unit.h
@@ -4,5 +4,8 @@
 #include "core-forward.h"
 
 #define VARLINK_ERROR_UNIT_NO_SUCH_UNIT "io.systemd.Unit.NoSuchUnit"
+#define VARLINK_ERROR_UNIT_ONLY_BY_DEPENDENCY "io.systemd.Unit.OnlyByDependency"
+#define VARLINK_ERROR_UNIT_BUS_SHUTTING_DOWN "io.systemd.Unit.BusShuttingDown"
 
 int vl_method_list_units(sd_varlink *link, sd_json_variant *parameters, sd_varlink_method_flags_t flags, void *userdata);
+int vl_method_enqueue_marked_jobs_units(sd_varlink *link, sd_json_variant *parameters, sd_varlink_method_flags_t flags, void *userdata);
