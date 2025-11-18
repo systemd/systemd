187626063fe27008abb8a04d18c3ce25f90bcdb4
Yu Watanabe <watanabe.yu+github@gmail.com>
2025-11-18 09:52:37 +0900

    libcrypt-util: drop unused hash_passwrod_full()
    It is only used by test cases. Not necessary to keep it.

--- src/shared/libcrypt-util.h
+++ src/shared/libcrypt-util.h
@@ -4,10 +4,7 @@
 #include "shared-forward.h"
 
 int make_salt(char **ret);
-int hash_password_full(const char *password, void **cd_data, int *cd_size, char **ret);
-static inline int hash_password(const char *password, char **ret) {
-        return hash_password_full(password, NULL, NULL, ret);
-}
+int hash_password(const char *password, char **ret);
 int test_password_one(const char *hashed_password, const char *password);
 int test_password_many(char **hashed_password, const char *password);
 bool looks_like_hashed_password(const char *s);
