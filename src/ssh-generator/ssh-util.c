cb43f94656c2af915822ac16a9819aeb09a2a81d
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2025-11-18 11:19:41 +0100

    ssh-generator: split out common helper function

---
+++ src/ssh-generator/ssh-util.c
@@ -0,0 +1,20 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+
+#include <sys/socket.h>
+
+#include "errno-util.h"
+#include "fd-util.h"
+#include "log.h"
+#include "ssh-util.h"
+
+int vsock_open_or_warn(int *ret) {
+        _cleanup_close_ int fd = RET_NERRNO(socket(AF_VSOCK, SOCK_STREAM|SOCK_CLOEXEC, 0));
+        if (ERRNO_IS_NOT_SUPPORTED(fd))
+                log_debug_errno(fd, "AF_VSOCK is not available, ignoring: %m");
+        else if (fd < 0)
+                return log_error_errno(fd, "Unable to test if AF_VSOCK is available: %m");
+
+        if (ret)
+                *ret = TAKE_FD(fd);
+        return fd >= 0;
+}
---
+++ src/ssh-generator/ssh-util.h
@@ -0,0 +1,3 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+
+int vsock_open_or_warn(int *ret);
