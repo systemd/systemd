b70c248de1e8ad0728acd2f7b22beb7748c8d5b8
Julian Sparber <julian@sparber.net>
2025-11-19 18:46:19 +0100

    repart-varlink: Only report DiskTooSmall when additonal space doesn't fit
    We need to report `InsufficientFreeSpace` in most cases except when the
    additonal free space needed doesn't fit the disk we need to report
    `DiskTooSmall`.
    
    Closes: https://github.com/systemd/systemd/issues/39788

--- src/repart/repart.c
+++ src/repart/repart.c
@@ -10367,23 +10367,16 @@ static int vl_method_run(
         if (r == -ENOSPC) {
                 uint64_t current_size, size;
 
-                r = determine_auto_size(context, LOG_DEBUG, &current_size, &size);
+                r = determine_auto_size(context, LOG_DEBUG, &size, &current_size);
                 if (r < 0)
                         return r;
 
-                /* Check if space issue is caused by the whole disk being too small */
-                if (size > context->total)
-                        return sd_varlink_errorbo(
-                                        link,
-                                        "io.systemd.Repart.DiskTooSmall",
-                                        SD_JSON_BUILD_PAIR_UNSIGNED("currentSizeBytes", current_size),
-                                        SD_JSON_BUILD_PAIR_UNSIGNED("minimalSizeBytes", size));
-
-                /* Or if the disk would fit, but theres's not enough unallocated space */
                 uint64_t need_free = LESS_BY(size, current_size);
+
                 return sd_varlink_errorbo(
                                 link,
-                                "io.systemd.Repart.InsufficientFreeSpace",
+                                need_free > context->total ? "io.systemd.Repart.DiskTooSmall" :
+                                                             "io.systemd.Repart.InsufficientFreeSpace",
                                 JSON_BUILD_PAIR_UNSIGNED_NOT_EQUAL("currentSizeBytes", current_size, UINT64_MAX),
                                 JSON_BUILD_PAIR_UNSIGNED_NOT_EQUAL("minimalSizeBytes", size, UINT64_MAX));
         }
