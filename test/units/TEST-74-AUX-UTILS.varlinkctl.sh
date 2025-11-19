8d50438ba56100d6d855442210d145194b84b37b
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2025-11-19 15:57:29 +0100

    test/TEST-74-AUX-UTILS: fix racy check
    We were getting a list of invocation IDs, picking one at random,
    and then querying the unit. This is obviously racy.
    
    TEST-74-AUX-UTILS.sh[2873]: + varlinkctl call /run/systemd/io.systemd.Manager io.systemd.Unit.List
                                  '{"invocationID": "2052c9a5-7983-4f72-9910-c49e38c91dab"}'
    TEST-74-AUX-UTILS.sh[3707]: Method call io.systemd.Unit.List() failed: io.systemd.Unit.NoSuchUnit
    
    The complicated varlink + jq callout is replaced by a simple systemctl call.
    I think that's better to avoid a complicated jq expression.
    
    Fixes https://github.com/systemd/systemd/issues/38647.

--- test/units/TEST-74-AUX-UTILS.varlinkctl.sh
+++ test/units/TEST-74-AUX-UTILS.varlinkctl.sh
@@ -207,7 +207,7 @@ varlinkctl call /run/systemd/io.systemd.Manager io.systemd.Unit.List '{"name": "
 (! varlinkctl call /run/systemd/io.systemd.Manager io.systemd.Unit.List '{"name": "multi-user.target", "pid": {"pid": 1}}')
 
 varlinkctl call /run/systemd/io.systemd.Manager io.systemd.Unit.List '{"cgroup": "/init.scope"}'
-invocation_id=$(varlinkctl call --collect /run/systemd/io.systemd.Manager io.systemd.Unit.List '{}' | jq -r '.[] | .runtime.InvocationID' | grep -v null | tail -n 1)
+invocation_id="$(systemctl show -P InvocationID systemd-journald.service)"
 varlinkctl call /run/systemd/io.systemd.Manager io.systemd.Unit.List "{\"invocationID\": \"$invocation_id\"}"
 
 # test io.systemd.Manager in user manager
