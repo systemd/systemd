22a80cf2cb938ba277699a23cf2469fe582f7ff0
Lennart Poettering <lennart@poettering.net>
2025-11-20 13:38:18 +0100

    execute: attempt to load three libraries we need before we turning of dlopen()

--- src/core/exec-invoke.c
+++ src/core/exec-invoke.c
@@ -48,6 +48,7 @@
 #include "io-util.h"
 #include "iovec-util.h"
 #include "journal-send.h"
+#include "libmount-util.h"
 #include "manager.h"
 #include "memfd-util.h"
 #include "mkdir-label.h"
@@ -5822,6 +5823,11 @@ int exec_invoke(
                         return log_error_errno(r, "Failed to mount bpffs in bpffs_prepare(): %m");
                 }
         }
+
+        /* Load a bunch of libraries we'll possibly need later, before we turn off dlopen() */
+        (void) dlopen_bpf();
+        (void) dlopen_libmount();
+        (void) dlopen_libseccomp();
 
         /* Let's now disable further dlopen()ing of libraries, since we are about to do namespace
          * shenanigans, and do not want to mix resources from host and namespace */
