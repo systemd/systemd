15f565df8fbef8002e887aa21f529135222d343f
Simran Singh <simsingh@redhat.com>
2025-11-24 20:30:19 +0530

    Add support for X-systemd.mount-dmclone flag

--- src/test/test-dlopen-so.c
+++ src/test/test-dlopen-so.c
@@ -25,6 +25,7 @@
 #include "selinux-util.h"
 #include "tests.h"
 #include "tpm2-util.h"
+#include "dm-util.h"
 
 #define ASSERT_DLOPEN(func, cond)                               \
         do {                                                    \
@@ -67,7 +68,7 @@ static int run(int argc, char **argv) {
         ASSERT_DLOPEN(dlopen_qrencode, HAVE_QRENCODE);
         ASSERT_DLOPEN(dlopen_tpm2, HAVE_TPM2);
         ASSERT_DLOPEN(dlopen_zstd, HAVE_ZSTD);
-
+        ASSERT_DLOPEN(dlopen_libdevmapper, HAVE_LIBDEVMAPPER);
         return 0;
 }
 
