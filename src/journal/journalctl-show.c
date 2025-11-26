143c5c1ad7d05d5d26ad9d85e580d4fe3cedc903
Daan De Meyer <daan.j.demeyer@gmail.com>
2025-11-26 11:56:11 +0100

    sd-journal: Fix return value coding style
    Let's always use ret, ret_data and ret_size for output arguments.
    
    size is better than length in this case because we're dealing with
    arbitrary, possibly binary, data and not strings.

--- src/journal/journalctl-show.c
+++ src/journal/journalctl-show.c
@@ -342,7 +342,7 @@ static int on_first_event(sd_event_source *s, void *userdata) {
                 return r;
 
         if (arg_follow && !c->has_cursor && !arg_since_set) {
-                r = sd_journal_get_cursor(c->journal, /* ret_cursor= */ NULL);
+                r = sd_journal_get_cursor(c->journal, /* ret= */ NULL);
                 if (r == -EADDRNOTAVAIL) {
                         /* If we shall operate in --follow mode, and we are unable to get a cursor after
                          * doing our first round of output, then this means there was no data to show
