79fc232e71bb838a413ba72c80a60ef54c3f1ec1
Simran Singh <simsingh@redhat.com>
2025-11-27 14:16:07 +0530

    Add support for x-systemd.mount-dmclone fstab option
    This commit adds support for the x-systemd.mount-dmclone mount option in
    fstab entries, enabling automatic setup of dm-clone devices during boot.
    
    Key changes:
    - Add new dmclone service and generator infrastructure
    - Implement dm-clone device management utilities in dm-util
    - Add fstab-generator support for x-systemd.mount-dmclone option
    - Add gpt-auto-generator support for dm-clone
    - Include complete libdevmapper function declarations
    - Modernize license headers to SPDX format
    - Fix clang-tidy C++ style comment warnings
    
    The implementation allows users to specify dm-clone configuration in
    /etc/fstab, which will be automatically processed during system boot
    to set up the appropriate device-mapper clone devices.

--- src/shared/dm-util.h
+++ src/shared/dm-util.h
@@ -1,4 +1,22 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 #pragma once
 
+#include "dlfcn-util.h"
+
 int dm_deferred_remove_cancel(const char *name);
+
+#if HAVE_LIBDEVMAPPER
+#include <libdevmapper.h> /* IWYU pragma: export */
+
+extern DLSYM_PROTOTYPE(dm_task_create);
+extern DLSYM_PROTOTYPE(dm_task_destroy);
+extern DLSYM_PROTOTYPE(dm_task_set_name);
+extern DLSYM_PROTOTYPE(dm_task_add_target);
+extern DLSYM_PROTOTYPE(dm_task_set_cookie);
+extern DLSYM_PROTOTYPE(dm_task_run);
+extern DLSYM_PROTOTYPE(dm_udev_wait);
+extern DLSYM_PROTOTYPE(dm_task_set_sector);
+extern DLSYM_PROTOTYPE(dm_task_set_message);
+
+int dlopen_libdevmapper(void);
+#endif
