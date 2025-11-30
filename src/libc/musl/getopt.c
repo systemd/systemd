1bff3416df42809913268ce02a57904a340268fd
Yu Watanabe <watanabe.yu+github@gmail.com>
2025-11-30 11:10:02 +0900

    musl: workaround issues in getopt() and getopt_long()
    musl's getopt_long() behaves something different in handling optional arguments.
    ```
    $ journalctl _PID=1 _COMM=systemd --since 19:19:01 -n all --follow
    Failed to add match 'all': Invalid argument
    ```
    This happens when an unmatching option appears before an option that takes an
    optional argument. So, the following works as expected:
    ```
    $ journalctl --since 19:19:01 -n all --follow _PID=1 _COMM=systemd
    ```
    This introduces getopt_long_reorder() that reorders the passed arguments to make
    getopt_long() provided by musl works as what we expect.
    
    Also, musl's getopt() always behaves POSIXLY_CORRECT mode, and stops parsing when
    a non-option string found. Let's always use getopt_long().

---
+++ src/libc/musl/getopt.c
@@ -0,0 +1,115 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+
+#include <errno.h>
+#include <getopt.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+static void move_to_end(int argc, char * const *argv, int target) {
+        char **av = (char**) argv;
+        char *saved = av[target];
+
+        for (int i = target + 1; i < argc; i++)
+                av[i - 1] = av[i];
+
+        av[argc - 1] = saved;
+}
+
+int getopt_long_reorder(
+                int argc,
+                char * const *argv,
+                const char *optstring,
+                const struct option *longopts,
+                int *longindex) {
+
+        /* If this is not the first call, arguments should be already reordered if necessary. */
+        if (optind != 0)
+                return (getopt_long)(argc, argv, optstring, longopts, longindex);
+
+        /* From getopt(3):
+         * ========
+         * If the first character of optstring is '+' or the  environment variable POSIXLY_CORRECT is set,
+         * then option processing stops as soon as a nonoption argument is encountered.
+         * ======== */
+        if ((optstring && optstring[0] == '+') || secure_getenv("POSIXLY_CORRECT"))
+                return (getopt_long)(argc, argv, optstring, longopts, longindex);
+
+        const char *saved_optstring = optstring;
+        int saved_argc = argc;
+
+        /* From getopt(3):
+         * ========
+         * If the first character of optstring is '-', then each nonoption argv-element is handled as if it
+         * were the argument of an option with character code 1.
+         * ========
+         * So, the prefix is unrelated to that if we should reorder arguments. Simply ignore it. */
+        if (optstring && optstring[0] == '-')
+                optstring++;
+
+        /* Do not reorder arguments after "--". */
+        for (int i = 1; i < argc; i++)
+                if (strcmp(argv[i], "--") == 0) {
+                        argc = i;
+                        break;
+                }
+
+        bool dont_move = false;
+        for (int i = 1, skipped = 0; i < argc - skipped; ) {
+                if (argv[i][0] != '-') {
+                        if (dont_move) {
+                                i++; /* The previous argument takes an optional argument. Do not reorder this. */
+                                dont_move = false;
+                        } else {
+                                move_to_end(argc, argv, i);
+                                skipped++;
+                        }
+                        continue;
+                }
+
+                dont_move = false;
+
+                if (argv[i][1] == '-')
+                        for (const struct option *o = longopts; o && o->name; o++) {
+                                size_t n = strlen(o->name);
+                                if (strncmp(argv[i] + 2, o->name, n) != 0)
+                                        continue;
+
+                                if (argv[i][2 + n] == '=')
+                                        break; /* Found a valid long option. */
+
+                                if (argv[i][2 + n] != '\0')
+                                        continue;
+
+                                if (o->has_arg == optional_argument)
+                                        dont_move = true; /* We may read it as an optional argument. Do not reorder. */
+                                else if (o->has_arg == required_argument)
+                                        i++; /* The next element is handled as the argument of this option. */
+
+                                break; /* Found a valid long option. */
+                        }
+                else if (optstring)
+                        for (const char *p = argv[i] + 1; *p != '0'; p++) {
+                                const char *o = strchr(optstring, *p);
+                                if (!o)
+                                        continue; /* Invalid option, ignoring. */
+
+                                if (o[1] != ':')
+                                        continue; /* The option does not take an argument, proceeding. */
+
+                                if (p[1] != '\0')
+                                        break; /* The remaining string is handled as the argument of this option. */
+
+                                if (o[2] == ':') /* The option takes an optional argument. */
+                                        dont_move = true; /* We may read it as an optional argument. Do not reorder. */
+                                else /* The option takes an optional argument. */
+                                        i++; /* The next element is handled as the argument of this option. */
+
+                                break; /* Found a valid short option. */
+                        }
+
+                i++; /* Found an option-like string. Keep the ordering regardless if it is a valid option or not. */
+        }
+
+        return (getopt_long)(saved_argc, argv, saved_optstring, longopts, longindex);
+}
