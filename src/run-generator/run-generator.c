0fc5b3616090ef0283d190bb7615e19aa1257263
Antonio Alvarez Feijoo <antonio.feijoo@suse.com>
2025-12-01 17:29:17 +0100

    run-generator: use generator_add_symlink()

--- src/run-generator/run-generator.c
+++ src/run-generator/run-generator.c
@@ -115,12 +115,11 @@ static int generate(void) {
                 return log_error_errno(r, "Failed to create unit file %s: %m", p);
 
         /* And now redirect default.target to our new target */
-        p = strjoina(arg_dest, "/" SPECIAL_DEFAULT_TARGET);
-        if (symlink("kernel-command-line.target", p) < 0)
-                return log_error_errno(errno, "Failed to link unit file kernel-command-line.target %s %s: %m",
-                                       glyph(GLYPH_ARROW_RIGHT), p);
-
-        return 0;
+        return generator_add_symlink(
+                        arg_dest,
+                        SPECIAL_DEFAULT_TARGET,
+                        /* dep_type= */ NULL,
+                        "kernel-command-line.target");
 }
 
 static int run(const char *dest, const char *dest_early, const char *dest_late) {
