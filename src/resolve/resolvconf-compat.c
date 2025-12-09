a65caaafe047f9b3d668f6ff418752c5a1ba1b20
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2025-12-09 12:10:06 +0100

    resolvectl: add third generated parser

--- src/resolve/resolvconf-compat.c
+++ src/resolve/resolvconf-compat.c
@@ -20,39 +20,7 @@ typedef enum LookupType  {
         LOOKUP_TYPE_EXCLUSIVE, /* -x */
 } LookupType;
 
-static int resolvconf_help(void) {
-        _cleanup_free_ char *link = NULL;
-        int r;
-
-        r = terminal_urlify_man("resolvectl", "1", &link);
-        if (r < 0)
-                return log_oom();
-
-        printf("%1$s -a INTERFACE < FILE\n"
-               "%1$s -d INTERFACE\n"
-               "\n"
-               "Register DNS server and domain configuration with systemd-resolved.\n\n"
-               "  -h --help     Show this help\n"
-               "     --version  Show package version\n"
-               "  -a            Register per-interface DNS server and domain data\n"
-               "  -d            Unregister per-interface DNS server and domain data\n"
-               "  -p            Do not use this interface as default route\n"
-               "  -f            Ignore if specified interface does not exist\n"
-               "  -x            Send DNS traffic preferably over this interface\n"
-               "\n"
-               "This is a compatibility alias for the resolvectl(1) tool, providing native\n"
-               "command line compatibility with the resolvconf(8) tool of various Linux\n"
-               "distributions and BSD systems. Some options supported by other implementations\n"
-               "are not supported and are ignored: -m, -u. Various options supported by other\n"
-               "implementations are not supported and will cause the invocation to fail:\n"
-               "-I, -i, -l, -R, -r, -v, -V, --enable-updates, --disable-updates,\n"
-               "--updates-are-enabled.\n"
-               "\nSee the %2$s for details.\n",
-               program_invocation_short_name,
-               link);
-
-        return 0;
-}
+LookupType arg_lookup_type = LOOKUP_TYPE_REGULAR;
 
 static int parse_nameserver(const char *string) {
         int r;
@@ -100,7 +68,37 @@ static int parse_search_domain(const char *string) {
         return 0;
 }
 
-static int parse_stdin(LookupType lookup_type) {
+#include "resolvconf-compat.args.inc"
+
+static int help_resolvconf(void) {
+        _cleanup_free_ char *link = NULL;
+        int r;
+
+        r = terminal_urlify_man("resolvectl", "1", &link);
+        if (r < 0)
+                return log_oom();
+
+        printf("%1$s -a INTERFACE <FILE\n"
+               "%1$s -d INTERFACE\n"
+               "\n"
+               "Register DNS server and domain configuration with systemd-resolved.\n\n"
+               OPTION_HELP_GENERATED_RESOLVCONF
+               "\n"
+               "This is a compatibility alias for the resolvectl(1) tool, providing native\n"
+               "command line compatibility with the resolvconf(8) tool of various Linux\n"
+               "distributions and BSD systems. Some options supported by other implementations\n"
+               "are not supported and are ignored: -m, -u. Various options supported by other\n"
+               "implementations are not supported and will cause the invocation to fail:\n"
+               "-I, -i, -l, -R, -r, -v, -V, --enable-updates, --disable-updates,\n"
+               "--updates-are-enabled.\n"
+               "\nSee the %2$s for details.\n",
+               program_invocation_short_name,
+               link);
+
+        return 0;
+}
+
+static int parse_stdin(void) {
         int r;
 
         for (unsigned n = 0;;) {
@@ -134,7 +132,7 @@ static int parse_stdin(LookupType lookup_type) {
                 log_syntax(NULL, LOG_DEBUG, "stdin", n, 0, "Ignoring resolv.conf line: %s", line);
         }
 
-        switch (lookup_type) {
+        switch (arg_lookup_type) {
         case LOOKUP_TYPE_REGULAR:
                 break;
 
@@ -170,126 +168,14 @@ static int parse_stdin(LookupType lookup_type) {
 }
 
 int resolvconf_parse_argv(int argc, char *argv[]) {
+        int r;
 
-        enum {
-                ARG_VERSION = 0x100,
-                ARG_ENABLE_UPDATES,
-                ARG_DISABLE_UPDATES,
-                ARG_UPDATES_ARE_ENABLED,
-        };
-
-        static const struct option options[] = {
-                { "help",                no_argument, NULL, 'h'                     },
-                { "version",             no_argument, NULL, ARG_VERSION             },
-
-                /* The following are specific to Debian's original resolvconf */
-                { "enable-updates",      no_argument, NULL, ARG_ENABLE_UPDATES      },
-                { "disable-updates",     no_argument, NULL, ARG_DISABLE_UPDATES     },
-                { "updates-are-enabled", no_argument, NULL, ARG_UPDATES_ARE_ENABLED },
-                {}
-        };
-
-        int c, r;
-
-        assert(argc >= 0);
-        assert(argv);
-
-        /* openresolv checks these environment variables */
-        LookupType lookup_type = LOOKUP_TYPE_REGULAR;
-
-        if (getenv("IF_EXCLUSIVE"))
-                lookup_type = LOOKUP_TYPE_EXCLUSIVE;
-        if (getenv("IF_PRIVATE"))
-                lookup_type = LOOKUP_TYPE_PRIVATE;
-
-        arg_mode = _MODE_INVALID;
-
-        while ((c = getopt_long(argc, argv, "hadxpfm:uIi:l:Rr:vV", options, NULL)) >= 0)
-                switch (c) {
-
-                case 'h':
-                        return resolvconf_help();
-
-                case ARG_VERSION:
-                        return version();
-
-                /* -a and -d is what everybody can agree on */
-                case 'a':
-                        arg_mode = MODE_SET_LINK;
-                        break;
-
-                case 'd':
-                        arg_mode = MODE_REVERT_LINK;
-                        break;
-
-                /* The exclusive/private/force stuff is an openresolv invention, we support in some skewed way */
-                case 'x':
-                        lookup_type = LOOKUP_TYPE_EXCLUSIVE;
-                        break;
-
-                case 'p':
-                        lookup_type = LOOKUP_TYPE_PRIVATE;
-                        break;
-
-                case 'f':
-                        arg_ifindex_permissive = true;
-                        break;
-
-                /* The metrics stuff is an openresolv invention we ignore (and don't really need) */
-                case 'm':
-                        log_debug("Switch -%c ignored.", c);
-                        break;
-
-                /* -u supposedly should "update all subscribers". We have no subscribers, hence let's make
-                    this a NOP, and exit immediately, cleanly. */
-                case 'u':
-                        log_info("Switch -%c ignored.", c);
-                        return 0;
-
-                /* The following options are openresolv inventions we don't support. */
-                case 'I':
-                case 'i':
-                case 'l':
-                case 'R':
-                case 'r':
-                case 'v':
-                case 'V':
-                        return log_error_errno(SYNTHETIC_ERRNO(EINVAL),
-                                               "Switch -%c not supported.", c);
-
-                /* The Debian resolvconf commands we don't support. */
-                case ARG_ENABLE_UPDATES:
-                        return log_error_errno(SYNTHETIC_ERRNO(EINVAL),
-                                               "Switch --enable-updates not supported.");
-                case ARG_DISABLE_UPDATES:
-                        return log_error_errno(SYNTHETIC_ERRNO(EINVAL),
-                                               "Switch --disable-updates not supported.");
-                case ARG_UPDATES_ARE_ENABLED:
-                        return log_error_errno(SYNTHETIC_ERRNO(EINVAL),
-                                               "Switch --updates-are-enabled not supported.");
-
-                case '?':
-                        return -EINVAL;
-
-                default:
-                        assert_not_reached();
-                }
-
-        if (arg_mode == _MODE_INVALID)
-                return log_error_errno(SYNTHETIC_ERRNO(EINVAL),
-                                       "Expected either -a or -d on the command line.");
-
-        if (optind+1 != argc)
-                return log_error_errno(SYNTHETIC_ERRNO(EINVAL),
-                                       "Expected interface name as argument.");
-
-        r = ifname_resolvconf_mangle(argv[optind]);
+        r = parse_argv_generated_resolvconf(argc, argv);
         if (r <= 0)
                 return r;
-        optind++;
 
         if (arg_mode == MODE_SET_LINK) {
-                r = parse_stdin(lookup_type);
+                r = parse_stdin();
                 if (r < 0)
                         return r;
         }
