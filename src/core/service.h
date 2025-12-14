6839e454db5ff7cbf6e0f82a73271e6d533a3eef
Mike Yuan <me@yhndnzj.com>
2025-12-14 18:20:36 +0100

    core/service: introduce RefreshOnReload= setting
    This allows controlling resources to be refreshed before performing
    reload, with one extra benefit that in the future we can permit
    "seemless reload"s, i.e. no active signaling is done to the main process
    after refreshing get updated. This could come in handy for programs
    that loads stuff on demand or watches changes via inotify.

--- src/core/service.h
+++ src/core/service.h
@@ -96,6 +96,14 @@ typedef enum ServiceRestartMode {
         _SERVICE_RESTART_MODE_INVALID = -EINVAL,
 } ServiceRestartMode;
 
+typedef enum ServiceRefreshOnReload {
+        SERVICE_RELOAD_EXTENSIONS  = 1 << 0,
+        _SERVICE_REFRESH_ON_RELOAD_ALL = (1 << 1) - 1,
+        _SERVICE_REFRESH_ON_RELOAD_INVALID = -EINVAL,
+} ServiceRefreshOnReload;
+
+#define SERVICE_REFRESH_ON_RELOAD_DEFAULT SERVICE_RELOAD_EXTENSIONS
+
 typedef struct ServiceFDStore {
         Service *service;
 
@@ -237,6 +245,9 @@ typedef struct Service {
         int reload_signal;
         usec_t reload_begin_usec;
 
+        bool refresh_on_reload_set;
+        ServiceRefreshOnReload refresh_on_reload_flags;
+
         OOMPolicy oom_policy;
 
         char *usb_function_descriptors;
@@ -287,6 +298,10 @@ DECLARE_STRING_TABLE_LOOKUP(notify_state, NotifyState);
 DECLARE_STRING_TABLE_LOOKUP(service_result, ServiceResult);
 
 DECLARE_STRING_TABLE_LOOKUP(service_timeout_failure_mode, ServiceTimeoutFailureMode);
+
+ServiceRefreshOnReload service_refresh_on_reload_flag_from_string(const char *s) _pure_;
+int service_refresh_on_reload_from_string_many(const char *s, ServiceRefreshOnReload *ret);
+int service_refresh_on_reload_to_strv(ServiceRefreshOnReload flags, char ***ret);
 
 DEFINE_CAST(SERVICE, Service);
 
