ec2c5f9e2de71de1ef9cae5b8ee63da878d8faa5
Luca Boccassi <luca.boccassi@gmail.com>
2025-12-17 23:11:13 +0000

    ci: switch semaphore-ci to new ci/v259-stable deb branch

--- .semaphore/semaphore-runner.sh
+++ .semaphore/semaphore-runner.sh
@@ -7,7 +7,7 @@ set -o pipefail
 DISTRO="${DISTRO:-debian}"
 RELEASE="${RELEASE:-bookworm}"
 SALSA_URL="${SALSA_URL:-https://salsa.debian.org/systemd-team/systemd.git}"
-BRANCH="${BRANCH:-debian/master}"
+BRANCH="${BRANCH:-ci/v259-stable}"
 ARCH="${ARCH:-amd64}"
 CONTAINER="${RELEASE}-${ARCH}"
 CACHE_DIR=/var/tmp
