systemd/systemd

commit ab932a622d57fd327ef95992c343fd4425324088

core: simplify unit_need_daemon_reload() a bit
And let's make it more accurate: if we have acquire the list of unit drop-ins,
then let's do a full comparison against the old list we already have, and if
things differ in any way, we know we have to reload.

This makes sure we detect changes to drop-in directories in more cases.

src/core/unit.c
@@ -2968,32 +2968,24 @@ static bool fragment_mtime_newer(const char *path, usec_t mtime) {
 bool unit_need_daemon_reload(Unit *u) {
         _cleanup_strv_free_ char **t = NULL;
         char **path;
-        unsigned loaded_cnt, current_cnt;
 
         assert(u);
 
-        if (fragment_mtime_newer(u->fragment_path, u->fragment_mtime) ||
-            fragment_mtime_newer(u->source_path, u->source_mtime))
+        if (fragment_mtime_newer(u->fragment_path, u->fragment_mtime))
+                return true;
+
+        if (fragment_mtime_newer(u->source_path, u->source_mtime))
                 return true;
 
         (void) unit_find_dropin_paths(u, &t);
-        loaded_cnt = strv_length(t);
-        current_cnt = strv_length(u->dropin_paths);
+        if (!strv_equal(u->dropin_paths, t))
+                return true;
 
-        if (loaded_cnt == current_cnt) {
-                if (loaded_cnt == 0)
-                        return false;
+        STRV_FOREACH(path, u->dropin_paths)
+                if (fragment_mtime_newer(*path, u->dropin_mtime))
+                        return true;
 
-                if (strv_overlap(u->dropin_paths, t)) {
-                        STRV_FOREACH(path, u->dropin_paths)
-                                if (fragment_mtime_newer(*path, u->dropin_mtime))
-                                        return true;
-
-                        return false;
-                }
-        }
-
-        return true;
+        return false;
 }
 
 void unit_reset_failed(Unit *u) {

commit d93de8e1d2ed2511f51fd97e1a200f9c401e9c12

core/unit: re-find dropin paths of a unit after writting settings
Fixes: ab932a622d57 ("core: simplify unit_need_daemon_reload() a bit")
Fixes #35710

Co-authored-by: Jian Wen <wenjianhn@gmail.com>

src/core/unit.c
@@ -4679,6 +4679,7 @@ char* unit_concat_strv(char **l, UnitWriteFlags flags) {
 
 int unit_write_setting(Unit *u, UnitWriteFlags flags, const char *name, const char *data) {
         _cleanup_free_ char *p = NULL, *q = NULL, *escaped = NULL;
+        _cleanup_strv_free_ char **dropins = NULL;
         const char *dir, *wrapped;
         int r;
 
@@ -4748,12 +4749,12 @@ int unit_write_setting(Unit *u, UnitWriteFlags flags, const char *name, const ch
         if (r < 0)
                 return r;
 
-        r = strv_push(&u->dropin_paths, q);
-        if (r < 0)
-                return r;
         q = NULL;
 
-        strv_uniq(u->dropin_paths);
+        r = unit_find_dropin_paths(u, /* use_unit_path_cache= */ false, &dropins);
+        if (r < 0)
+                return r;
+        strv_free_and_replace(u->dropin_paths, dropins);
 
         u->dropin_mtime = now(CLOCK_REALTIME);
 

test/units/TEST-15-DROPIN.sh
@@ -712,6 +712,17 @@ EOF
     clear_units test15-a.service
 }
 
+testcase_order_dropin_paths_set_property() {
+    # For issue #35710.
+    echo "Testing the order of dropin paths that are created by set-property"
+
+    create_service test15-a
+    systemctl set-property test15-a DevicePolicy=strict DeviceAllow="char-* m"
+    check_ok test15-a NeedDaemonReload no
+
+    clear_units test15-a.service
+}
+
 run_testcases
 
 touch /testok