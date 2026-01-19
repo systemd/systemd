8b1a85dfcd79214731a3202b8e8ded33c14508dc
gavinchen <zxc556620011@gmail.com>
2026-01-19 12:50:12 +0000

    web-util: insert port before path and support IPv6 in http_url_add_port
    Update http_url_add_port to comply with RFC 3986 by splitting the URL
    into scheme, host, and path components before port insertion. This
    ensures the port is placed correctly and adds support for IPv6 addresses.

--- src/shared/web-util.c
+++ src/shared/web-util.c
@@ -1,4 +1,5 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
+#include <stdio.h>
 
 #include "string-util.h"
 #include "strv.h"
@@ -34,32 +35,40 @@ bool http_url_is_valid(const char *url) {
         return ascii_is_valid(p);
 }
 
-char* http_url_add_port(const char *url , const char *port) {
+char* http_url_add_port(const char *url, const char *port) {
 
         assert(url);
         assert(port);
-        const char *host, *proto = "";
+        const char *url_without_scheme, *scheme = "";
+        _cleanup_free_ char *host = NULL, *path = NULL;
 
-        host = STARTSWITH_SET(url, "http://", "https://");
-        if (!host) {
-                host = url;
-                proto = "https://";
+        const char *strport = strjoina(":", port);
+        url_without_scheme = STARTSWITH_SET(url, "http://", "https://");
+        if (url_without_scheme) {
+                scheme = startswith(url, "https://") ? "https://" : "http://";
+                host = strdup(url_without_scheme);
+        } else {
+                /* Default to the https scheme if the URL does not specify one */
+                scheme = "https://";
+                host = strdup(url);
+        }
+        if (!host) return NULL;
+
+        delete_trailing_chars(host, "/");
+        char *t = strchr(host, '/');
+        if (t) {
+                path = strdup(t);
+                if (!path) return NULL;
+                *t = '\0';
+        }
+        if (strchr(host,']')){
+                if (strstr_ptr(host, "]:"))
+                        strport = "";
+        } else if (strchr(host, ':')){
+                strport = "";
         }
 
-        if (strchr(host, ':'))
-                return strjoin(proto, url, "/upload");
-        else {
-                char *t;
-                size_t x;
-
-                t = strdupa_safe(url);
-                x = strlen(t);
-                while (x > 0 && t[x - 1] == '/')
-                        t[x - 1] = '\0';
-
-                return strjoin(proto, t, ":", port, "/upload");
-        }
-        return NULL;
+        return strjoin(scheme, host, strport, strempty(path), "/upload");
 }
 
 bool file_url_is_valid(const char *url) {
