c2745186b7f8262916e56f018759214b9e8469bc
Chris Down <chris@chrisdown.name>
2026-01-22 18:13:44 +0800

    blockdev-util: Add helpers for recursive blockdev origin
    Originating block devices are resolved one layer at a time by
    block_device_get_originating(). That makes sense, but there are multiple
    places that want recursive iteration until the originating device is
    found, so let's add a helper to standardise how to go about this.

--- src/shared/blockdev-util.c
+++ src/shared/blockdev-util.c
@@ -148,6 +148,35 @@ int block_device_get_originating(sd_device *dev, sd_device **ret) {
         return 0;
 }
 
+int block_device_get_originating_recursive(sd_device *dev, sd_device **ret) {
+        _cleanup_(sd_device_unrefp) sd_device *current = NULL;
+        sd_device *origin;
+        bool advanced = false;
+        int r;
+
+        assert(dev);
+        assert(ret);
+
+        current = sd_device_ref(dev);
+
+        for (;;) {
+                r = block_device_get_originating(current, &origin);
+                if (r < 0)
+                        break;
+
+                sd_device_unref(current);
+                current = origin;
+                advanced = true;
+        }
+
+        *ret = TAKE_PTR(current);
+
+        if (r == -ENOENT && advanced)
+                return 0;
+
+        return r;
+}
+
 int block_device_new_from_fd(int fd, BlockDeviceLookupFlags flags, sd_device **ret) {
         _cleanup_(sd_device_unrefp) sd_device *dev = NULL;
         dev_t devnum;
@@ -305,6 +334,25 @@ int block_get_originating(dev_t dt, dev_t *ret) {
                 return r;
 
         return sd_device_get_devnum(origin, ret);
+}
+
+int block_get_originating_recursive(dev_t dt, dev_t *ret) {
+        _cleanup_(sd_device_unrefp) sd_device *dev = NULL, *origin = NULL;
+        int r, r2;
+
+        assert(ret);
+
+        r = sd_device_new_from_devnum(&dev, 'b', dt);
+        if (r < 0)
+                return r;
+
+        r = block_device_get_originating_recursive(dev, &origin);
+
+        r2 = sd_device_get_devnum(origin, ret);
+        if (r2 < 0)
+                return r2;
+
+        return r;
 }
 
 int get_block_device_harder_fd(int fd, dev_t *ret) {
