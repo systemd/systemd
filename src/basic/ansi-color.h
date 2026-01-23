293e6f1c2f8469701bfc908e10158fcf9ed5cc43
Peter Oliver <git@mavit.org.uk>
2026-01-23 11:46:47 +0000

    ansi-color: Be more explicit about the ColorMode implementation in enum comments

--- src/basic/ansi-color.h
+++ src/basic/ansi-color.h
@@ -9,9 +9,9 @@ typedef enum ColorMode {
         COLOR_16,         /* Only the base 16 colors. */
         COLOR_256,        /* Only 256 colors. */
         COLOR_24BIT,      /* For truecolor or 24bit color support, no restriction. */
-        COLOR_AUTO_16,    /* At most the base 16 colors. */
-        COLOR_AUTO_256,   /* At most 256 colors. */
-        COLOR_AUTO_24BIT, /* Most colors available (the default). */
+        COLOR_AUTO_16,    /* The "AUTO" modes are as the above, but subject to suitable settings for */
+        COLOR_AUTO_256,   /* the environment variables TERM, NO_COLOR, and COLORTERM. */
+        COLOR_AUTO_24BIT,
         _COLOR_MODE_MAX,
         _COLOR_MODE_INVALID = -EINVAL,
 } ColorMode;
