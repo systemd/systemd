e172ce3693d610eb4cd3169f80044b5c6268d1f1
Daan De Meyer <daan.j.demeyer@gmail.com>
2026-01-23 12:30:59 +0100

    nsresourced: Allow operations outside transient UID/GID ranges
    The purpose of the userns-restrict BPF-LSM program is to prevent the
    transient ranges leaking to disk, so let's allow operations outside the
    transient UID ranges, even if the mount is not allowlisted.
    
    This is preparation for the next commits where we'll add support for mapping
    the current user and the foreign UID range into the user namespaces provisioned
    by nsresourced. Operations creating files/directories as these UIDs/GIDs should
    not need the corresponding mount to be allowlisted with nsresourced.

--- src/nsresourced/bpf/userns-restrict/userns-restrict.bpf.c
+++ src/nsresourced/bpf/userns-restrict/userns-restrict.bpf.c
@@ -20,6 +20,9 @@
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
 
+#define CONTAINER_UID_MIN (CONTAINER_UID_BASE_MIN + 0U)
+#define CONTAINER_UID_MAX (CONTAINER_UID_BASE_MAX + 0xFFFFU)
+
 #ifndef bpf_core_cast
 /* bpf_rdonly_cast() was introduced in libbpf commit 688879f together with
  * the definition of a bpf_core_cast macro. So use that one to avoid
@@ -68,13 +71,37 @@ static inline struct mount *real_mount(struct vfsmount *mnt) {
         return container_of(mnt, struct mount, mnt);
 }
 
-static int validate_mount(struct vfsmount *v) {
+static inline bool uid_is_dynamic(uid_t uid) {
+        return DYNAMIC_UID_MIN <= uid && uid <= DYNAMIC_UID_MAX;
+}
+
+static inline bool uid_is_container(uid_t uid) {
+        return CONTAINER_UID_MIN <= uid && uid <= CONTAINER_UID_MAX;
+}
+
+static inline bool uid_is_transient(uid_t uid) {
+        return uid_is_dynamic(uid) || uid_is_container(uid);
+}
+
+static int validate_mount(struct vfsmount *v, uid_t uid, gid_t gid) {
         struct user_namespace *mount_userns, *task_userns, *p;
         unsigned task_userns_inode;
         struct task_struct *task;
         void *mnt_id_map;
         struct mount *m;
         int mnt_id;
+
+        /* uid/gid are the UID/GID in the initial user namespace, before any idmapped mounts or idmapping to
+         * the user namespace attached to the mount's filesystem have been applied. For transient UIDs/GIDs,
+         * we have to check that the mount is owned by the user namespace or allowlisted to make sure that
+         * the mount is an idmapped mount that maps the transient user range to something else on disk. For
+         * other UIDs/GIDs, there's no need to do these checks as we don't insist on idmapped mounts or such
+         * for UIDs/GIDs outside the transient ranges.  */
+        if (!uid_is_transient(uid) && !uid_is_transient((uid_t) gid))
+                return 0;
+
+        if (!uid_is_transient(uid) && !uid_is_transient((uid_t) gid))
+                return 0;
 
         /* Get user namespace from vfsmount */
         m = bpf_rdonly_cast(real_mount(v), bpf_core_type_id_kernel(struct mount));
@@ -118,6 +145,20 @@ static int validate_mount(struct vfsmount *v) {
 }
 
 static int validate_path(const struct path *path, int ret) {
+        struct task_struct *task;
+        struct vfsmount *v;
+
+        if (ret != 0) /* propagate earlier error */
+                return ret;
+
+        task = (struct task_struct*) bpf_get_current_task_btf();
+        v = path->mnt;
+
+        return validate_mount(v, task->cred->fsuid.val, task->cred->fsgid.val);
+}
+
+SEC("lsm/path_chown")
+int BPF_PROG(userns_restrict_path_chown, struct path *path, unsigned long long uid, unsigned long long gid, int ret) {
         struct inode *inode;
         struct vfsmount *v;
 
@@ -126,12 +167,7 @@ static int validate_path(const struct path *path, int ret) {
 
         v = path->mnt;
 
-        return validate_mount(v);
-}
-
-SEC("lsm/path_chown")
-int BPF_PROG(userns_restrict_path_chown, struct path *path, void* uid, void *gid, int ret) {
-        return validate_path(path, ret);
+        return validate_mount(v, (uid_t) uid, (gid_t) gid);
 }
 
 SEC("lsm/path_mkdir")
