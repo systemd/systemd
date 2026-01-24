6f58d55205cca95b65c02bcddcc4574bca497497
Daan De Meyer <daan.j.demeyer@gmail.com>
2026-01-24 01:03:35 +0100

    nsresourced: Add support for self mappings with delegations
    mkosi does all of its environment setup in an unprivileged user
    namespace with an identity mapping. When it invokes nspawn and nspawn
    tries to get a transient userns from nsresourced, this fails as no
    transient ranges are mapped into mkosi's unprivileged userns (as doing
    so would require privileges).
    
    To fix this problem, let's allow allocating unprivileged self user
    namespaces in nsresourced, similar to what the kernel allows, except that
    we also support delegations for these. This means that mkosi can get its
    unprivileged userns as before from nsresourced, but it can also request a
    delegated 64K range inside that userns as well, which nsresourced can then
    allocate to nspawn later when it asks for one.
    
    Similar to the kernel, we disallow setgroups for self mappings. However,
    instead of doing this via /proc/self/setgroups, which applies to the current
    user namespace and all its child user namespaces, we use the BPF LSM to deny
    setgroups instead, so that it can still be allowed for child user namespaces.
    We need this because as soon as a container launches in a child user namespace
    using one of the delegated mappings, it has to be able to do setgroups() to be
    able to function properly.
    
    To allow mapping the root user, we need to add the CAP_SETFCAP capability to
    nsresourced.

--- src/nsresourced/userns-registry.c
+++ src/nsresourced/userns-registry.c
@@ -20,6 +20,7 @@
 #include "stat-util.h"
 #include "string-util.h"
 #include "strv.h"
+#include "uid-classification.h"
 #include "user-util.h"
 #include "userns-registry.h"
 
@@ -284,8 +285,6 @@ static int userns_registry_load(int dir_fd, const char *fn, UserNamespaceInfo **
                 return r;
 
         if (userns_info->userns_inode == 0)
-                return -EBADMSG;
-        if (userns_info->start_uid == 0 || userns_info->start_gid == 0)
                 return -EBADMSG;
 
         if (userns_info->size == 0) {
@@ -611,7 +610,7 @@ int userns_registry_store(int dir_fd, UserNamespaceInfo *info) {
                 goto fail;
         }
 
-        if (uid_is_valid(info->start_uid)) {
+        if (uid_is_transient(info->start_uid)) {
                 if (asprintf(&link2_fn, "u" UID_FMT ".userns", info->start_uid) < 0) {
                         r = log_oom_debug();
                         goto fail;
@@ -624,7 +623,7 @@ int userns_registry_store(int dir_fd, UserNamespaceInfo *info) {
                 }
         }
 
-        if (gid_is_valid(info->start_gid)) {
+        if (gid_is_transient(info->start_gid)) {
                 if (asprintf(&link3_fn, "g" GID_FMT ".userns", info->start_gid) < 0) {
                         r = log_oom_debug();
                         goto fail;
@@ -795,7 +794,7 @@ int userns_registry_remove(int dir_fd, UserNamespaceInfo *info) {
         if (r < 0)
                 RET_GATHER(ret, log_debug_errno(r, "Failed to remove %s: %m", link1_fn));
 
-        if (uid_is_valid(info->start_uid)) {
+        if (uid_is_transient(info->start_uid)) {
                 _cleanup_free_ char *link2_fn = NULL;
 
                 if (asprintf(&link2_fn, "u" UID_FMT ".userns", info->start_uid) < 0)
@@ -806,7 +805,7 @@ int userns_registry_remove(int dir_fd, UserNamespaceInfo *info) {
                         RET_GATHER(ret, log_debug_errno(r, "Failed to remove %s: %m", link2_fn));
         }
 
-        if (uid_is_valid(info->start_gid)) {
+        if (gid_is_transient(info->start_gid)) {
                 _cleanup_free_ char *link3_fn = NULL;
 
                 if (asprintf(&link3_fn, "g" GID_FMT ".userns", info->start_gid) < 0)
