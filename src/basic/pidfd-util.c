3107ea29d5b9545c84ced4af12a704224b9c8b6f
Yu Watanabe <watanabe.yu+github@gmail.com>
2026-01-29 01:53:23 +0900

    pidfd-util: replace pidfd_get_inode_id_impl() with fd_get_inode_id()

--- src/basic/pidfd-util.c
+++ src/basic/pidfd-util.c
@@ -230,56 +230,6 @@ int pidfd_get_cgroupid(int fd, uint64_t *ret) {
         return 0;
 }
 
-int pidfd_get_inode_id_impl(int fd, uint64_t *ret) {
-        static thread_local bool file_handle_supported = true;
-        int r;
-
-        assert(fd >= 0);
-
-        if (file_handle_supported) {
-                union {
-                        struct file_handle file_handle;
-                        uint8_t space[MAX_HANDLE_SZ];
-                } fh = {
-                        .file_handle.handle_bytes = sizeof(uint64_t),
-                        .file_handle.handle_type = FILEID_KERNFS,
-                };
-                int mnt_id;
-
-                r = RET_NERRNO(name_to_handle_at(fd, "", &fh.file_handle, &mnt_id, AT_EMPTY_PATH));
-                if (r >= 0) {
-                        if (ret)
-                                /* Note, "struct file_handle" is 32bit aligned usually, but we need to read a 64bit value from it */
-                                *ret = unaligned_read_ne64(fh.file_handle.f_handle);
-                        return 0;
-                }
-                assert(r != -EOVERFLOW);
-                if (is_name_to_handle_at_fatal_error(r))
-                        return r;
-
-                file_handle_supported = false;
-        }
-
-#if SIZEOF_INO_T == 8
-        struct stat st;
-        if (fstat(fd, &st) < 0)
-                return -errno;
-
-        if (ret)
-                *ret = (uint64_t) st.st_ino;
-        return 0;
-
-#elif SIZEOF_INO_T == 4
-        /* On 32-bit systems (where sizeof(ino_t) == 4), the inode id returned by fstat() cannot be used to
-         * reliably identify the process, nor can we communicate the origin of the id with the clients.
-         * Hence let's just refuse to acquire pidfdid through fstat() here. All clients shall also insist on
-         * the 64-bit id from name_to_handle_at(). */
-        return -EOPNOTSUPP;
-#else
-#  error Unsupported ino_t size
-#endif
-}
-
 int pidfd_get_inode_id(int fd, uint64_t *ret) {
         int r;
 
@@ -291,7 +241,7 @@ int pidfd_get_inode_id(int fd, uint64_t *ret) {
         if (r == 0)
                 return -EOPNOTSUPP;
 
-        return pidfd_get_inode_id_impl(fd, ret);
+        return fd_get_inode_id(fd, ret);
 }
 
 int pidfd_get_inode_id_self_cached(uint64_t *ret) {
