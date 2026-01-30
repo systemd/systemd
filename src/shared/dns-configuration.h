8731d33b58c06c81537d36facdee274360165b04
Nick Rosbrook <enr0n@ubuntu.com>
2026-01-30 14:34:27 -0500

    dns-configuration: dispatch delegate name to DNSConfiguration
    This is one of several commits to dispatch all fields of the
    DNSConfiguration JSON.

--- src/shared/dns-configuration.h
+++ src/shared/dns-configuration.h
@@ -53,6 +53,7 @@ typedef struct DNSConfiguration {
         char *mdns_mode_str;
         char **negative_trust_anchors;
         char *resolv_conf_mode_str;
+        char *delegate;
 } DNSConfiguration;
 
 DNSConfiguration* dns_configuration_free(DNSConfiguration *c);
