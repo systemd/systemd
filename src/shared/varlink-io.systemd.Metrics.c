5f815d6fa1878a0f6c28039f5e2fb0df348dc405
Yu Watanabe <watanabe.yu+github@gmail.com>
2026-02-06 15:19:51 +0900

    metrics: fix typo
    Follow-up for fd73cd6c912a4d31f404d54700654d8398ea8f27.

--- src/shared/varlink-io.systemd.Metrics.c
+++ src/shared/varlink-io.systemd.Metrics.c
@@ -25,7 +25,7 @@ static SD_VARLINK_DEFINE_METHOD_FULL(
                 SD_VARLINK_DEFINE_OUTPUT(value, SD_VARLINK_ANY, 0),
                 SD_VARLINK_FIELD_COMMENT("Metric object name can be unit name, process name, etc, e.g. dev-hvc0.device"),
                 SD_VARLINK_DEFINE_OUTPUT(object, SD_VARLINK_STRING, SD_VARLINK_NULLABLE),
-                SD_VARLINK_FIELD_COMMENT("Metric fields are values to deferentiate between different metrics in the same metric family"),
+                SD_VARLINK_FIELD_COMMENT("Metric fields are values to differentiate between different metrics in the same metric family"),
                 SD_VARLINK_DEFINE_OUTPUT(fields, SD_VARLINK_OBJECT, SD_VARLINK_NULLABLE));
 
 static SD_VARLINK_DEFINE_METHOD_FULL(
