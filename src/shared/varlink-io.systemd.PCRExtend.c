1632070cb876a3142ac384aa4d3482ae42fddaa2
Lennart Poettering <lennart@amutable.com>
2026-02-11 13:11:38 +0100

    pcrextend-util: add helpers for measuring roothash/signature of Verity volumes
    This adds infrastructure for measuring Verity root hashes from
    userspace, along with he issuer/serial of the signatures used to unlock
    them.
    
    We measure the triplet of volume name, root hash and issuer/serial. if
    confext/sysext use different signing keys then this ensures the event
    log carry information about the type of image measures.

--- src/shared/varlink-io.systemd.PCRExtend.c
+++ src/shared/varlink-io.systemd.PCRExtend.c
@@ -12,7 +12,7 @@ static SD_VARLINK_DEFINE_ENUM_TYPE(
                 SD_VARLINK_DEFINE_ENUM_VALUE(keyslot),
                 SD_VARLINK_DEFINE_ENUM_VALUE(nvpcr_init),
                 SD_VARLINK_DEFINE_ENUM_VALUE(nvpcr_separator),
-                SD_VARLINK_DEFINE_ENUM_VALUE(verity));
+                SD_VARLINK_DEFINE_ENUM_VALUE(dm_verity));
 
 static SD_VARLINK_DEFINE_METHOD(
                 Extend,
