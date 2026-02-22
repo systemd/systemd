17e911ffc853c54f44edd11ead8b6edab72c1217
Yu Watanabe <watanabe.yu+github@gmail.com>
2026-02-23 01:01:45 +0900

    udev: drop home-grown udev-ctrl socket
    Nowadays, varlink is used to control systemd-udevd. Let's drop the
    legacy socket.
    
    Note, the existence of /run/udev/control socket is widely used in both
    our code and external projects. Also, the dependency to
    systemd-udevd-control.socket is widely used in many projects.
    Hence, we need to create a symlink to the socket file and .socket unit
    file.

--- src/udev/udevadm-util.h
+++ src/udev/udevadm-util.h
@@ -11,5 +11,5 @@ int find_device_with_action(const char *id, sd_device_action_t action, sd_device
 int parse_device_action(const char *str, sd_device_action_t *ret);
 int parse_resolve_name_timing(const char *str, ResolveNameTiming *ret);
 int parse_key_value_argument(const char *str, bool require_value, char **key, char **value);
-int udev_ping(usec_t timeout, bool ignore_connection_failure);
+int udev_ping(usec_t timeout);
 int search_rules_files(char * const *a, const char *root, ConfFile ***ret_files, size_t *ret_n_files);
