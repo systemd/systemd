f117e216309abe8d00cf7e068f71b03155dd0a2e
noxiouz <atiurin@proton.me>
2026-02-23 01:23:58 +0000

    network: add io.systemd.Network.Link interface, rename link-specific methods
    Move all link-specific Varlink methods from the io.systemd.Network
    interface into a new dedicated io.systemd.Network.Link interface defined
    
    Methods renamed:
      io.systemd.Network.DescribeLink    → io.systemd.Network.Link.Describe
      io.systemd.Network.RenewLink       → io.systemd.Network.Link.Renew
      io.systemd.Network.ForceRenewLink  → io.systemd.Network.Link.ForceRenew
      io.systemd.Network.ReconfigureLink → io.systemd.Network.Link.Reconfigure
      io.systemd.Network.LinkUp          → io.systemd.Network.Link.Up
      io.systemd.Network.LinkDown        → io.systemd.Network.Link.Down
    
    The io.systemd.Network.Link interface shares the type definitions from
    io.systemd.Network by exporting the relevant symbols

--- src/network/networkctl-misc.c
+++ src/network/networkctl-misc.c
@@ -64,7 +64,7 @@ int link_up_down(int argc, char *argv[], void *userdata) {
         ORDERED_SET_FOREACH(p, indexes)
                 RET_GATHER(ret, varlink_callbo_and_log(
                                            vl,
-                                           up ? "io.systemd.Network.LinkUp" : "io.systemd.Network.LinkDown",
+                                           up ? "io.systemd.Network.Link.Up" : "io.systemd.Network.Link.Down",
                                            /* reply= */ NULL,
                                            SD_JSON_BUILD_PAIR_INTEGER("InterfaceIndex", PTR_TO_INT(p)),
                                            SD_JSON_BUILD_PAIR_BOOLEAN("allowInteractiveAuthentication", arg_ask_password)));
@@ -120,7 +120,7 @@ int verb_reconfigure(int argc, char *argv[], void *userdata) {
         ORDERED_SET_FOREACH(p, indexes)
                 RET_GATHER(ret, varlink_callbo_and_log(
                                            vl,
-                                           "io.systemd.Network.ReconfigureLink",
+                                           "io.systemd.Network.Link.Reconfigure",
                                            /* reply= */ NULL,
                                            SD_JSON_BUILD_PAIR_INTEGER("InterfaceIndex", PTR_TO_INT(p)),
                                            SD_JSON_BUILD_PAIR_BOOLEAN("allowInteractiveAuthentication", arg_ask_password)));
@@ -147,7 +147,7 @@ int verb_force_renew(int argc, char *argv[], void *userdata) {
         ORDERED_SET_FOREACH(p, indexes)
                 RET_GATHER(ret, varlink_callbo_and_log(
                                            vl,
-                                           "io.systemd.Network.ForceRenewLink",
+                                           "io.systemd.Network.Link.ForceRenew",
                                            /* reply= */ NULL,
                                            SD_JSON_BUILD_PAIR_INTEGER("InterfaceIndex", PTR_TO_INT(p)),
                                            SD_JSON_BUILD_PAIR_BOOLEAN("allowInteractiveAuthentication", arg_ask_password)));
@@ -174,7 +174,7 @@ int verb_renew(int argc, char *argv[], void *userdata) {
         ORDERED_SET_FOREACH(p, indexes)
                 RET_GATHER(ret, varlink_callbo_and_log(
                                            vl,
-                                           "io.systemd.Network.RenewLink",
+                                           "io.systemd.Network.Link.Renew",
                                            /* reply= */ NULL,
                                            SD_JSON_BUILD_PAIR_INTEGER("InterfaceIndex", PTR_TO_INT(p)),
                                            SD_JSON_BUILD_PAIR_BOOLEAN("allowInteractiveAuthentication", arg_ask_password)));
