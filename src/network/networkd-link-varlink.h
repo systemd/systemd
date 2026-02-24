7879f055f8371b0b866536851ef37cc47c023777
Yu Watanabe <watanabe.yu+github@gmail.com>
2026-02-24 21:06:43 +0900

    network: rename dispatch_interface() and make it take flags
    No functional change. Just refactoring and preparation for later change.

--- src/network/networkd-link-varlink.h
+++ src/network/networkd-link-varlink.h
@@ -3,7 +3,12 @@
 
 #include "networkd-forward.h"
 
-int dispatch_interface(sd_varlink *vlink, sd_json_variant *parameters, Manager *manager, bool polkit, Link **ret);
+typedef enum DispatchLinkFlag {
+        DISPATCH_LINK_POLKIT    = 1 << 0,
+        DISPATCH_LINK_MANDATORY = 1 << 1,
+} DispatchLinkFlag;
+
+int dispatch_link(sd_varlink *vlink, sd_json_variant *parameters, Manager *manager, DispatchLinkFlag flags, Link **ret);
 
 int vl_method_link_up(sd_varlink *vlink, sd_json_variant *parameters, sd_varlink_method_flags_t flags, void *userdata);
 int vl_method_link_down(sd_varlink *vlink, sd_json_variant *parameters, sd_varlink_method_flags_t flags, void *userdata);
