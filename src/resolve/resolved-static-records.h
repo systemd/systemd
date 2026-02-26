ee5a9bca03e1498ba4b5b6005d1bb7e94fc86ec3
Lennart Poettering <lennart@amutable.com>
2026-02-26 15:54:14 +0100

    resolved: add ability to define additional local RRs via drop-ins
    This is an extension of the /etc/hosts concept, but can provide any kind
    of RRs (well, actually, we only parse A/AAAA/PTR for now, but the
    concept is open for more).
    
    Fixes: #17791

---
+++ src/resolve/resolved-static-records.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#pragma once
+
+#include "resolved-forward.h"
+
+void manager_static_records_loads(Manager *m);
+void manager_static_records_flush(Manager *m);
+int manager_static_records_lookup(Manager *m, DnsQuestion* q, DnsAnswer **answer);
