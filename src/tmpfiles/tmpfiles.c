4a80c77b8765f1c807093959bf83b27809f90a12
Mike Yuan <me@yhndnzj.com>
2026-02-27 17:04:41 +0100

    tmpfiles: fix doubled "/" in root + path logging
    (cherry picked from commit b1c6bcf94953d98614ddf2fb320b553e5c9d1bd0)

--- src/tmpfiles/tmpfiles.c
+++ src/tmpfiles/tmpfiles.c
@@ -2424,12 +2424,12 @@ static int create_symlink(Context *c, Item *i) {
                 if (r == -ENOENT) {
                         /* Silently skip over lines where the source file is missing. */
                         log_info("Symlink source path '%s/%s' does not exist, skipping line.",
-                                 empty_to_root(arg_root), skip_leading_slash(i->argument));
+                                 strempty(arg_root), skip_leading_slash(i->argument));
                         return 0;
                 }
                 if (r < 0)
                         return log_error_errno(r, "Failed to check if symlink source path '%s/%s' exists: %m",
-                                               empty_to_root(arg_root), skip_leading_slash(i->argument));
+                                               strempty(arg_root), skip_leading_slash(i->argument));
         }
 
         r = path_extract_filename(i->path, &bn);
