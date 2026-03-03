740297149101996eada5ece2114cedbbc66597de
Lennart Poettering <lennart@amutable.com>
2026-03-03 12:32:11 +0100

    udev: add tpm2_id builtin
    We need to be able to look up tpm2 metadata from hwdb, hence add a way
    to synthesize a whdb lookup key from with udev rules.

--- src/udev/udev-builtin.c
+++ src/udev/udev-builtin.c
@@ -29,6 +29,7 @@ static const UdevBuiltin *const builtins[_UDEV_BUILTIN_MAX] = {
         [UDEV_BUILTIN_NET_ID]        = &udev_builtin_net_id,
         [UDEV_BUILTIN_NET_LINK]      = &udev_builtin_net_setup_link,
         [UDEV_BUILTIN_PATH_ID]       = &udev_builtin_path_id,
+        [UDEV_BUILTIN_TPM2_ID]       = &udev_builtin_tpm2_id,
 #if HAVE_ACL
         [UDEV_BUILTIN_UACCESS]       = &udev_builtin_uaccess,
 #endif
