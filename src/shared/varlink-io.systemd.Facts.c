a7a346d8ac8be581755bd8fa5bfaf976dfdeac3e
Zbigniew Jędrzejewski-Szmek <zbyszek@amutable.com>
2026-03-05 11:49:11 +0100

    report: move facts generator out of PID1 into a separate varlink service
    The collection of facts is entirely unprivileged and has very little to
    do with PID1. PID1 is privileged and single-threaded and a point of
    contention, so we shouldn't put things in PID1 that don't need to be
    there. A separate service can be enabled/disabled/started/stopped at
    will, is easy to sandbox, etc. If it turns out to be necessary to
    collect some facts through PID1 in the future, we can always add a
    smaller facts endpoint to PID1 again.

--- src/shared/varlink-io.systemd.Facts.c
+++ src/shared/varlink-io.systemd.Facts.c
@@ -9,8 +9,9 @@ static SD_VARLINK_DEFINE_ERROR(NoSuchFact);
 static SD_VARLINK_DEFINE_METHOD_FULL(
                 List,
                 SD_VARLINK_REQUIRES_MORE,
-                SD_VARLINK_FIELD_COMMENT("Fact family name, e.g. io.systemd.Manager.Hostname"),
+                SD_VARLINK_FIELD_COMMENT("Fact family name, e.g. io.systemd.Basic.Hostname"),
                 SD_VARLINK_DEFINE_OUTPUT(name, SD_VARLINK_STRING, 0),
+                /* This is currently an unused placeholder. Add examples when we have them. */
                 SD_VARLINK_FIELD_COMMENT("Fact object name"),
                 SD_VARLINK_DEFINE_OUTPUT(object, SD_VARLINK_STRING, SD_VARLINK_NULLABLE),
                 SD_VARLINK_FIELD_COMMENT("Fact value"),
@@ -19,7 +20,7 @@ static SD_VARLINK_DEFINE_METHOD_FULL(
 static SD_VARLINK_DEFINE_METHOD_FULL(
                 Describe,
                 SD_VARLINK_REQUIRES_MORE,
-                SD_VARLINK_FIELD_COMMENT("Fact family name"),
+                SD_VARLINK_FIELD_COMMENT("Fact family name, e.g. io.systemd.Basic.Hostname"),
                 SD_VARLINK_DEFINE_OUTPUT(name, SD_VARLINK_STRING, 0),
                 SD_VARLINK_FIELD_COMMENT("Fact family description"),
                 SD_VARLINK_DEFINE_OUTPUT(description, SD_VARLINK_STRING, 0));
