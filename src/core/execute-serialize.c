f120953b6cc3b81f080b3d32a140837e5556a442
Mike Yuan <me@yhndnzj.com>
2026-03-07 05:53:14 +0100

    core/execute-serialize: fix error handling
    (cherry picked from commit cd13dc16c3651678a541289dc51c4a4e5131f405)

--- src/core/execute-serialize.c
+++ src/core/execute-serialize.c
@@ -3224,7 +3224,7 @@ static int exec_context_deserialize(ExecContext *c, FILE *f) {
 
                                         r = extract_first_word(&val, &options, NULL, EXTRACT_UNQUOTE);
                                         if (r < 0)
-                                                return -r;
+                                                return r;
 
                                         if (isempty(options) || streq(options, "rbind"))
                                                 rbind = true;
@@ -3283,7 +3283,7 @@ static int exec_context_deserialize(ExecContext *c, FILE *f) {
 
                                         r = extract_first_word(&val, &options, NULL, EXTRACT_UNQUOTE);
                                         if (r < 0)
-                                                return -r;
+                                                return r;
 
                                         if (isempty(options) || streq(options, "rbind"))
                                                 rbind = true;
