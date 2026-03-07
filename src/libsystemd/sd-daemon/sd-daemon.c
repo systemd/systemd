29c3938620f29be5915695d4f2ef2d5811c9b6b2
Mike Yuan <me@yhndnzj.com>
2026-03-07 05:37:30 +0100

    sd-daemon: escape notification msg using shell_escape()
    Follow-up for f9e0a62392fa62364bb7ac31a1cd4a8cc521cced
    
    xescape() would unconditionally emit hex escapes, rendering
    \n unreadable.

--- src/libsystemd/sd-daemon/sd-daemon.c
+++ src/libsystemd/sd-daemon/sd-daemon.c
@@ -635,7 +635,7 @@ static int pid_notify_with_fds_internal(
         }
 
         if (DEBUG_LOGGING) {
-                _cleanup_free_ char *escaped = xescape_full(state, "\"", /* console_width = */ SIZE_MAX, XESCAPE_8_BIT);
+                _cleanup_free_ char *escaped = shell_escape(state, "\"");
                 log_debug("Notify message sent to '%s': \"%s\"", e, escaped ?: state);
         }
 
