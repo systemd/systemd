107440667354a60cc1c87e70b8e1bc389ea6a96e
Lennart Poettering <lennart@amutable.com>
2026-03-10 08:09:52 +0100

    pcrextend: measure another separator at boot

--- src/shared/tpm2-util.c
+++ src/shared/tpm2-util.c
@@ -6674,6 +6674,7 @@ static const char* tpm2_userspace_event_type_table[_TPM2_USERSPACE_EVENT_TYPE_MA
         [TPM2_EVENT_NVPCR_INIT]      = "nvpcr-init",
         [TPM2_EVENT_NVPCR_SEPARATOR] = "nvpcr-separator",
         [TPM2_EVENT_DM_VERITY]       = "dm-verity",
+        [TPM2_EVENT_OS_SEPARATOR]    = "os-separator",
 };
 
 DEFINE_STRING_TABLE_LOOKUP(tpm2_userspace_event_type, Tpm2UserspaceEventType);
