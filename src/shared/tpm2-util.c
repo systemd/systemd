94bd7f8a162abf0eb4a8c6b52b09b5a3ae072f8b
Lennart Poettering <lennart@amutable.com>
2026-03-10 08:07:25 +0100

    pcrlock: deal with firmwares which understand TPM but where no TPM is available

--- src/shared/tpm2-util.c
+++ src/shared/tpm2-util.c
@@ -3003,6 +3003,8 @@ int tpm2_get_good_pcr_banks(
                 log_debug("Boot loader didn't set the LoaderTpm2ActivePcrBanks EFI variable, we have to guess the used PCR banks.");
         } else if (efi_banks == UINT32_MAX)
                 log_debug("Boot loader set the LoaderTpm2ActivePcrBanks EFI variable to indicate that the GetActivePcrBanks() API is not available in the firmware. We have to guess the used PCR banks.");
+        else if (efi_banks == 0)
+                log_debug("Boot loader set LoaderTpm2ActivePcrBanks EFI variable to zero to indicate firmware has TPM support is not available in the firmware. We'll have to guess the used PCR banks.");
         else {
                 FOREACH_ARRAY(hash, tpm2_hash_algorithms, TPM2_N_HASH_ALGORITHMS) {
                         if (!BIT_SET(efi_banks, *hash))
