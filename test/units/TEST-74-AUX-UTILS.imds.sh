a4c8fbf9175299f074022fa8582b0a6265272fd8
Lennart Poettering <lennart@amutable.com>
2026-03-12 13:46:16 +0100

    doesn't work

--- test/units/TEST-74-AUX-UTILS.imds.sh
+++ test/units/TEST-74-AUX-UTILS.imds.sh
@@ -21,62 +21,9 @@ at_exit() {
 
 trap at_exit EXIT
 
-cat >/tmp/fake-imds.py <<EOF
-#!/usr/bin/python3
 
-import os, socket
-from http.server import BaseHTTPRequestHandler, HTTPServer
-
-def sd_notify(state: str) -> bool:
-    notify_socket = os.environ.get("NOTIFY_SOCKET")
-    if not notify_socket:
-        return False
-    if notify_socket.startswith("@"):
-        notify_socket = "\0" + notify_socket[1:]
-    try:
-        with socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM) as sock:
-            sock.sendto(state.encode(), notify_socket)
-    except OSError:
-        return False
-
-    return True
-
-class Handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == "/userdata":
-            body = b"{\"systemd.credentials\":[{\"name\":\"acredtest\",\"text\":\"avalue\"}]}"
-            self.send_response(200)
-            self.send_header("Content-Type", "text/plain")
-            self.send_header("Content-Length", len(body))
-            self.end_headers()
-            self.wfile.write(body)
-        elif self.path == "/hostname":
-            body = b"piff"
-            self.send_response(200)
-            self.send_header("Content-Type", "text/plain")
-            self.send_header("Content-Length", len(body))
-            self.end_headers()
-            self.wfile.write(body)
-        else:
-            self.send_error(404)
-
-    def log_message(self, fmt, *args):
-        print(f"{self.address_string()} - {fmt % args}")
-
-PORT=8088
-
-server = HTTPServer(("", PORT), Handler)
-print(f"Serving on http://localhost:{PORT}/")
-try:
-        sd_notify("READY=1")
-        server.serve_forever()
-except KeyboardInterrupt:
-        print("\nStopped.")
-EOF
-
-chmod +x /tmp/fake-imds.py
-
-systemd-run -p Type=notify --unit=fake-imds /tmp/fake-imds.py
+find /usr/lib/systemd/tests/testdata/ | sort
+systemd-run -p Type=notify --unit=fake-imds /usr/lib/systemd/tests/testdata/TEST-74-AUX-UTILS.units/fake-imds.py
 systemctl status fake-imds
 
 # Add a fake network interface so that IMDS gets going
