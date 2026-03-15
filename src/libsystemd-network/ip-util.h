0b767a57e1dcb44f728ecdac287f8880a3d9adc1
Yu Watanabe <watanabe.yu+github@gmail.com>
2026-03-15 13:57:33 +0900

    ip-util: introduce udp_packet_build()
    Then make dhcp_packet_append_ip_headers() just a wrapper of the new
    function. Currently, the wrapper is inefficient, but will be removed in
    a later commit.

--- src/libsystemd-network/ip-util.h
+++ src/libsystemd-network/ip-util.h
@@ -1,6 +1,25 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 #pragma once
 
+#include <netinet/ip.h>
+#include <netinet/udp.h>
+
 #include "sd-forward.h"
 
+#include "sparse-endian.h"
+
+/* This is a maximal UDP payload size in a packet when its IP header does not contain options. When a packet
+ * contains some IP options, then of course the allowed UDP payload size in the packet becomes smaller. */
+#define UDP_PAYLOAD_MAX_SIZE (UINT16_MAX - sizeof(struct iphdr) - sizeof(struct udphdr))
+
 uint16_t ip_checksum(const void *buf, size_t len);
+
+int udp_packet_build(
+                be32_t source_addr,
+                uint16_t source_port,
+                be32_t destination_addr,
+                uint16_t destination_port,
+                int ip_service_type,
+                const struct iovec_wrapper *payload,
+                struct iphdr *ret_iphdr,
+                struct udphdr *ret_udphdr);
