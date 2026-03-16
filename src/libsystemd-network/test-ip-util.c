32433e897343ed29b0f8a893bcd3126fdf68674c
Yu Watanabe <watanabe.yu+github@gmail.com>
2026-03-17 00:11:25 +0900

    ip-util: introduce udp_packet_verify()
    This is mostly equivalent to dhcp_packet_verify_headers(), but
    - it optionally returns the UDP payload as iovec, and
    - supports IP header with options.

--- src/libsystemd-network/test-ip-util.c
+++ src/libsystemd-network/test-ip-util.c
@@ -1,6 +1,10 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 
+#include "alloc-util.h"
+#include "iovec-util.h"
+#include "iovec-wrapper.h"
 #include "ip-util.h"
+#include "random-util.h"
 #include "tests.h"
 
 TEST(ip_checksum) {
@@ -11,6 +15,32 @@ TEST(ip_checksum) {
         };
 
         ASSERT_EQ(ip_checksum(buf, 20), be16toh(0x78ae));
+}
+
+TEST(udp_packet_build_and_verify) {
+        size_t n = random_u64_range(100) + 100;
+
+        _cleanup_(iovec_done) struct iovec payload = {};
+        ASSERT_OK(random_bytes_allocate_iovec(n, &payload));
+
+        _cleanup_(iovw_done_free) struct iovec_wrapper iovw = {};
+        ASSERT_OK(udp_packet_build(
+                                  /* source_addr= */ 0xC0020001,
+                                  /* source_port= */ 42,
+                                  /* destination_addr= */ 0xC0020002,
+                                  /* destination_port= */ 43,
+                                  /* ip_service_type= */ 7,
+                                  &payload,
+                                  &iovw));
+
+        ASSERT_EQ(iovw.count, 3u); /* IP header, UDP header, and UDP payload. */
+
+        _cleanup_(iovec_done) struct iovec packet = {};
+        ASSERT_OK(iovw_concat(&iovw, &packet));
+
+        struct iovec iov;
+        ASSERT_OK(udp_packet_verify(packet.iov_base, packet.iov_len, /* port= */ 43, /* checksum= */ true, &iov));
+        ASSERT_EQ(iovec_memcmp(&iov, &iovw.iovec[2]), 0);
 }
 
 DEFINE_TEST_MAIN(LOG_DEBUG);
