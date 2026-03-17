0655414a04e3c7ce7b1c117638e121ed13a9878e
Yu Watanabe <watanabe.yu+github@gmail.com>
2026-03-18 02:04:08 +0900

    dhcp-protocol: introduce DHCPMessageHeader struct
    It is similar to DHCPMessage, but does not have the tail 'options'
    variable array. Then, we can use it in another struct.

--- src/libsystemd-network/dhcp-protocol.h
+++ src/libsystemd-network/dhcp-protocol.h
@@ -35,12 +35,19 @@
         uint8_t file[128];             \
         be32_t magic;
 
+struct DHCPMessageHeader {
+        DHCP_MESSAGE_HEADER_DEFINITION;
+} _packed_;
+
+typedef struct DHCPMessageHeader DHCPMessageHeader;
+
 struct DHCPMessage {
         DHCP_MESSAGE_HEADER_DEFINITION;
         uint8_t options[];
 } _packed_;
 
 typedef struct DHCPMessage DHCPMessage;
+assert_cc(sizeof(DHCPMessageHeader) == offsetof(DHCPMessage, options));
 
 struct DHCPPacket {
         struct iphdr ip;
