eb79a2384f0b6aac777b46bf3f9f7850b48fb6f2
Yu Watanabe <watanabe.yu+github@gmail.com>
2026-03-18 19:26:22 +0900

    sd-dhcp-client: move the object definition to the header
    Then, we can split the long sd-dhcp-client.c into small pieces later.

--- src/libsystemd-network/dhcp-client-internal.h
+++ src/libsystemd-network/dhcp-client-internal.h
@@ -2,9 +2,11 @@
 #pragma once
 
 #include "sd-dhcp-client.h"
-
 #include "sd-forward.h"
+
+#include "dhcp-client-id-internal.h"
 #include "network-common.h"
+#include "socket-util.h"
 
 typedef enum DHCPState {
         DHCP_STATE_STOPPED,
@@ -21,6 +23,68 @@ typedef enum DHCPState {
 } DHCPState;
 
 DECLARE_STRING_TABLE_LOOKUP_TO_STRING(dhcp_state, DHCPState);
+
+struct sd_dhcp_client {
+        unsigned n_ref;
+
+        DHCPState state;
+        sd_event *event;
+        int event_priority;
+        sd_event_source *timeout_resend;
+
+        int ifindex;
+        char *ifname;
+
+        sd_device *dev;
+
+        int fd;
+        uint16_t port;
+        uint16_t server_port;
+        union sockaddr_union link;
+        sd_event_source *receive_message;
+        bool request_broadcast;
+        Set *req_opts;
+        bool anonymize;
+        bool rapid_commit;
+        be32_t last_addr;
+        struct hw_addr_data hw_addr;
+        struct hw_addr_data bcast_addr;
+        uint16_t arp_type;
+        sd_dhcp_client_id client_id;
+        char *hostname;
+        char *vendor_class_identifier;
+        char *mudurl;
+        char **user_class;
+        uint32_t mtu;
+        usec_t fallback_lease_lifetime;
+        uint32_t xid;
+        usec_t start_time;
+        usec_t t1_time;
+        usec_t t2_time;
+        usec_t expire_time;
+        uint64_t discover_attempt;
+        uint64_t request_attempt;
+        uint64_t max_discover_attempts;
+        uint64_t max_request_attempts;
+        OrderedHashmap *extra_options;
+        OrderedHashmap *vendor_options;
+        sd_event_source *timeout_t1;
+        sd_event_source *timeout_t2;
+        sd_event_source *timeout_expire;
+        sd_event_source *timeout_ipv6_only_mode;
+        sd_dhcp_client_callback_t callback;
+        void *userdata;
+        sd_dhcp_client_callback_t state_callback;
+        void *state_userdata;
+        sd_dhcp_lease *lease;
+        usec_t start_delay;
+        int ip_service_type;
+        int socket_priority;
+        bool socket_priority_set;
+        bool ipv6_acquired;
+        bool bootp;
+        bool send_release;
+};
 
 typedef struct sd_dhcp_client sd_dhcp_client;
 
