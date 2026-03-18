f0a9e6bbeb7bcf248cd4cc77e72916fd0e729965
Yu Watanabe <watanabe.yu+github@gmail.com>
2026-03-19 01:01:50 +0900

    dhcp-client-send: introduce dhcp_client_send_message()
    This internally uses sd_dhcp_message object, and replaces functions
    for creating and sending DHCP messages.
    
    By using sd_dhcp_message internally, now we can correctly send long
    (> 255 bytes) option data that cannot be fit in a single DHCP option TLV.
    
    This also fixes the value in DHCP option 57 (Maximum Message Size).
    Previously the IP and UDP header size is subtracted from the interface
    MTU, but it should not.
    
    Except for the above, this should not change any effective behaviors.

--- src/libsystemd-network/dhcp-client-send.h
+++ src/libsystemd-network/dhcp-client-send.h
@@ -3,16 +3,4 @@
 
 #include "sd-forward.h"
 
-#include "dhcp-protocol.h"
-
-int dhcp_client_send_raw(
-                sd_dhcp_client *client,
-                bool expect_reply,
-                DHCPPacket *packet,
-                size_t optoffset);
-
-int dhcp_client_send_udp(
-                sd_dhcp_client *client,
-                bool expect_reply,
-                DHCPPacket *packet,
-                size_t optoffset);
+int dhcp_client_send_message(sd_dhcp_client *client, uint8_t type);
