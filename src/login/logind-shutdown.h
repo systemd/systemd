c1b928c810c8d231657393ad3499d6d2940059b3
Michael Vogt <michael@amutable.com>
2026-03-20 13:42:30 +0100

    logind: move verify_shutdown_creds() to logind-shutdown.c
    Move verify_shutdown_creds() and its helper have_multiple_sessions()
    from logind-dbus.c to logind-shutdown.c so that they can be reused
    by the varlink transport. No functional changes.
    
    Also prefix both with `manager_` now that they are public.

--- src/login/logind-shutdown.h
+++ src/login/logind-shutdown.h
@@ -5,4 +5,6 @@
 
 #define SHUTDOWN_SCHEDULE_FILE "/run/systemd/shutdown/scheduled"
 
+int manager_have_multiple_sessions(Manager *m, uid_t uid);
+int manager_verify_shutdown_creds(Manager *m, sd_bus_message *message, const HandleActionData *a, uint64_t flags, sd_bus_error *error);
 void manager_reset_scheduled_shutdown(Manager *m);
