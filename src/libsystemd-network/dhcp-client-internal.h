80932cf3a4da563d051ca90740ac05f33ad004cc
Yu Watanabe <watanabe.yu+github@gmail.com>
2026-03-22 17:00:33 +0900

    dhcp: use TLV object to manage extra and vendor options
    Note, previously we replaced the previous option with the same option code with
    new one. But, DHCP message can have multiple options with same option code.
    Hence, this make the conf parser not replace, but append new one.

--- src/libsystemd-network/dhcp-client-internal.h
+++ src/libsystemd-network/dhcp-client-internal.h
@@ -8,6 +8,7 @@
 #include "network-common.h"
 #include "sd-forward.h"
 #include "socket-util.h"
+#include "tlv-util.h"
 
 typedef enum DHCPState {
         DHCP_STATE_STOPPED,
@@ -65,8 +66,8 @@ struct sd_dhcp_client {
         uint64_t discover_attempt;
         uint64_t request_attempt;
         uint64_t max_discover_attempts;
-        OrderedHashmap *extra_options;
-        OrderedHashmap *vendor_options;
+        TLV *extra_options;
+        TLV *vendor_options;
         sd_event_source *timeout_t1;
         sd_event_source *timeout_t2;
         sd_event_source *timeout_expire;
@@ -89,6 +90,9 @@ int dhcp_client_set_state_callback(
                 sd_dhcp_client_callback_t cb,
                 void *userdata);
 int dhcp_client_get_state(sd_dhcp_client *client);
+
+int dhcp_client_set_extra_options(sd_dhcp_client *client, TLV *options);
+int dhcp_client_set_vendor_options(sd_dhcp_client *client, TLV *options);
 
 int client_receive_message_raw(
                 sd_event_source *s,
