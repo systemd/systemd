513170463cc8fb4b0bae6530a18d5778feec6c92
Yu Watanabe <watanabe.yu+github@gmail.com>
2026-03-22 16:19:06 +0900

    dhcp-message: introduce sd_dhcp_message object and several functions for the object

---
+++ src/libsystemd-network/dhcp-message.h
@@ -0,0 +1,75 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#pragma once
+
+#include "sd-dhcp-client-id.h"
+#include "sd-forward.h"
+
+#include "dhcp-protocol.h"
+
+typedef struct sd_dhcp_message sd_dhcp_message;
+
+struct sd_dhcp_message {
+        unsigned n_ref;
+
+        DHCPMessageHeader header;
+        Hashmap *options;
+};
+
+sd_dhcp_message* sd_dhcp_message_ref(sd_dhcp_message *p);
+sd_dhcp_message* sd_dhcp_message_unref(sd_dhcp_message *p);
+DEFINE_TRIVIAL_CLEANUP_FUNC(sd_dhcp_message*, sd_dhcp_message_unref);
+
+int dhcp_message_new(sd_dhcp_message **ret);
+
+int dhcp_message_init_header(
+                sd_dhcp_message *message,
+                uint8_t op,
+                uint32_t xid,
+                uint16_t arp_type,
+                const struct hw_addr_data *hw_addr);
+
+int dhcp_message_get_hw_addr(sd_dhcp_message *message, struct hw_addr_data *ret);
+
+void dhcp_message_remove_option(sd_dhcp_message *message, uint8_t code);
+
+int dhcp_message_append_option(sd_dhcp_message *message, uint8_t code, size_t length, const void *data);
+int dhcp_message_append_options(sd_dhcp_message *message, Hashmap *options);
+int dhcp_message_append_option_string(sd_dhcp_message *message, uint8_t code, const char *data);
+int dhcp_message_append_option_flag(sd_dhcp_message *message, uint8_t code);
+int dhcp_message_append_option_u8(sd_dhcp_message *message, uint8_t code, uint8_t data);
+int dhcp_message_append_option_u16(sd_dhcp_message *message, uint8_t code, uint16_t data);
+int dhcp_message_append_option_be32(sd_dhcp_message *message, uint8_t code, be32_t data);
+int dhcp_message_append_option_address(sd_dhcp_message *message, uint8_t code, const struct in_addr *addr);
+int dhcp_message_append_option_addresses(sd_dhcp_message *message, uint8_t code, size_t n_addr, const struct in_addr *addr);
+int dhcp_message_append_option_client_id(sd_dhcp_message *message, const sd_dhcp_client_id *id);
+int dhcp_message_append_option_parameter_request_list(sd_dhcp_message *message, Set *prl);
+int dhcp_message_append_option_hostname(sd_dhcp_message *message, uint8_t flags, bool is_client, const char *hostname);
+int dhcp_message_append_option_vendor_specific(sd_dhcp_message *message, Hashmap *options);
+int dhcp_message_append_option_user_class(sd_dhcp_message *message, const struct iovec_wrapper *user_class);
+
+int dhcp_message_get_option(sd_dhcp_message *message, uint8_t code, size_t length, void *ret);
+int dhcp_message_get_option_alloc(sd_dhcp_message *message, uint8_t code, size_t *ret_size, void **ret_data);
+int dhcp_message_get_option_alloc_iovec(sd_dhcp_message *message, uint8_t code, struct iovec *ret);
+int dhcp_message_get_option_string(sd_dhcp_message *message, uint8_t code, char **ret);
+int dhcp_message_get_option_flag(sd_dhcp_message *message, uint8_t code);
+int dhcp_message_get_option_u8(sd_dhcp_message *message, uint8_t code, uint8_t *ret);
+int dhcp_message_get_option_u16(sd_dhcp_message *message, uint8_t code, uint16_t *ret);
+int dhcp_message_get_option_be32(sd_dhcp_message *message, uint8_t code, be32_t *ret);
+int dhcp_message_get_option_address(sd_dhcp_message *message, uint8_t code, struct in_addr *ret);
+int dhcp_message_get_option_addresses(sd_dhcp_message *message, uint8_t code, size_t *ret_n_addr, struct in_addr **ret_addr);
+int dhcp_message_get_option_client_id(sd_dhcp_message *message, sd_dhcp_client_id *ret);
+int dhcp_message_get_option_parameter_request_list(sd_dhcp_message *message, Set **ret);
+int dhcp_message_get_option_dns_name(sd_dhcp_message *message, uint8_t code, char **ret);
+int dhcp_message_get_option_hostname(sd_dhcp_message *message, char **ret);
+int dhcp_message_get_option_vendor_specific(sd_dhcp_message *message, Hashmap **ret);
+int dhcp_message_get_option_user_class(sd_dhcp_message *message, struct iovec_wrapper *ret);
+
+int dhcp_message_parse(
+                const struct iovec *iov,
+                uint8_t op,
+                const uint32_t *xid,
+                uint16_t arp_type,
+                const struct hw_addr_data *hw_addr,
+                sd_dhcp_message **ret);
+
+int dhcp_message_build(const sd_dhcp_message *message, struct iovec_wrapper *ret);
