f912de93125bcf0b6c59770503424bcafc683e78
Frantisek Sumsal <frantisek@sumsal.cz>
2026-03-24 14:29:27 +0100

    homectl: apply all --member-of= groups from a comma-separated list
    Commit 0e1ede4b4b6d1ce6b5b6cda5f803e4f1b5aa4a03 introduced a bug where
    we'd always fetch the "original" (empty) list of groups when processing
    a comma-separated list of groups from the --member-of= option, so only
    the last group from the list would get applied. This bug was then later
    (in 316e9887f2a48bd1c4efa3e31b4bfbaeb22de3a3) refactored into a separate
    function.
    
    Follow-up for 0e1ede4b4b6d1ce6b5b6cda5f803e4f1b5aa4a03.
    Resolves: #41286

--- src/home/homectl.c
+++ src/home/homectl.c
@@ -3691,7 +3691,6 @@ static int parse_language_field(char ***languages, const char *arg) {
 }
 
 static int parse_group_field(
-                sd_json_variant *source_identity,
                 sd_json_variant **identity,
                 const char *field,
                 const char *arg) {
@@ -3717,7 +3716,7 @@ static int parse_group_field(
                         return log_error_errno(SYNTHETIC_ERRNO(EINVAL), "Invalid group name %s.", word);
 
                 _cleanup_(sd_json_variant_unrefp) sd_json_variant *mo =
-                        sd_json_variant_ref(sd_json_variant_by_key(source_identity, field));
+                        sd_json_variant_ref(sd_json_variant_by_key(*identity, field));
 
                 r = sd_json_variant_strv(mo, &list);
                 if (r < 0)
@@ -4383,7 +4382,7 @@ static int parse_argv(int argc, char *argv[]) {
                         break;
 
                 case ARG_ALIAS:
-                        r = parse_group_field(arg_identity_extra, &arg_identity_extra, "aliases", optarg);
+                        r = parse_group_field(&arg_identity_extra, "aliases", optarg);
                         if (r < 0)
                                 return r;
                         break;
@@ -4692,9 +4691,7 @@ static int parse_argv(int argc, char *argv[]) {
                 }
 
                 case 'G':
-                        r = parse_group_field(arg_identity_extra,
-                                              match_identity ?: &arg_identity_extra,
-                                              "memberOf", optarg);
+                        r = parse_group_field(match_identity ?: &arg_identity_extra, "memberOf", optarg);
                         if (r < 0)
                                 return r;
                         break;
