51302ed3b33e2312c69cea491a1dd2cb474f02a7
Michael Vogt <michael@amutable.com>
2026-03-28 09:46:33 +0100

    cleanup: address review feedback from laude
    Trivial ordering change that got highlighted by claude, i.e.
    move the assert checks after variable declaration.

--- src/import/qcow2-util.c
+++ src/import/qcow2-util.c
@@ -150,11 +150,11 @@ static int normalize_offset(
                 bool *compressed,
                 uint64_t *compressed_size) {
 
+        uint64_t q;
+
         assert(ret);
         POINTER_MAY_BE_NULL(compressed);
         POINTER_MAY_BE_NULL(compressed_size);
-
-        uint64_t q;
 
         q = be64toh(p);
 
