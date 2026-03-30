222467704506286ae33feee9073111bbc0e23811
Christian Brauner <brauner@kernel.org>
2026-03-30 14:08:40 +0200

    vmspawn: set up QMP-varlink bridge infrastructure
    After the QEMU fork, perform the blocking QMP handshake and create a
    varlink server exposing three interfaces:
    
    - io.systemd.MachineInstance (generic machine control)
    - io.systemd.VirtualMachineInstance (VM-specific, placeholder)
    - io.systemd.QemuMachineInstance (QEMU-specific)
    
    All MachineInstance method handlers are stubs returning NotSupported at
    this point. They are implemented in subsequent commits.
    
    The server listens on <runtime_dir>/io.systemd.MachineInstance with mode
    0666.
    
    Signed-off-by: Christian Brauner (Amutable) <brauner@kernel.org>

---
+++ src/vmspawn/vmspawn-qmp.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#pragma once
+
+#include "sd-event.h"
+
+#include "cleanup-util.h"
+
+typedef struct VmspawnQmpContext VmspawnQmpContext;
+
+int vmspawn_qmp_setup(VmspawnQmpContext **ret, int qmp_fd, sd_event *event, const char *runtime_dir, char **ret_varlink_address);
+VmspawnQmpContext *vmspawn_qmp_context_free(VmspawnQmpContext *ctx);
+
+DEFINE_TRIVIAL_CLEANUP_FUNC(VmspawnQmpContext *, vmspawn_qmp_context_free);
