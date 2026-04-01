0c7ca74584e343af87f8cbe84cb394118e42ac94
Zbigniew Jędrzejewski-Szmek <zbyszek@amutable.com>
2026-04-01 07:32:46 +0200

    report: add option to inject additional HTTP headers
    This is useful when debugging things. The option is named and implements
    the same logic as imdsd.

--- src/shared/web-util.h
+++ src/shared/web-util.h
@@ -6,4 +6,5 @@
 bool http_url_is_valid(const char *url) _pure_;
 bool file_url_is_valid(const char *url) _pure_;
 bool documentation_url_is_valid(const char *url) _pure_;
+bool http_header_valid(const char *header) _pure_;
 bool http_etag_is_valid(const char *etag) _pure_;
