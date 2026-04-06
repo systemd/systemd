6f468244b941f1953b2eab69e35af852a4b65b0c
Arnout Engelen <arnout@bzzt.net>
2026-04-06 10:00:37 +0200

    run: allow setting output format
    This is useful when moving from `--pty` or `--pipe` to using
    `--verbose`: you can use `--verbose-output=cat` to get similar output on
    stdout while still having all of the advantages of `--verbose` over the
    other options.

--- src/shared/fork-notify.h
+++ src/shared/fork-notify.h
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 #pragma once
 
+#include "output-mode.h"
 #include "shared-forward.h"
 
 int fork_notify(char * const *argv, PidRef *ret_pidref);
@@ -9,4 +10,4 @@ void fork_notify_terminate(PidRef *pidref);
 
 void fork_notify_terminate_many(sd_event_source **array, size_t n);
 
-int journal_fork(RuntimeScope scope, char * const *units, PidRef *ret_pidref);
+int journal_fork(RuntimeScope scope, char * const *units, OutputMode output, PidRef *ret_pidref);
