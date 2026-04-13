c30b6affa32c5a4458d130ee4300f4beaceb12ce
Zbigniew Jędrzejewski-Szmek <zbyszek@amutable.com>
2026-04-13 21:41:32 +0200

    basic/iovec-wrapper: add iovw_append and iovw_to_cstring
    Existing iovw_append is renamed to iovw_append_iovw.
    iovw_consume is made noninline.

--- src/basic/iovec-wrapper.h
+++ src/basic/iovec-wrapper.h
@@ -12,16 +12,8 @@ void iovw_done_free(struct iovec_wrapper *iovw);
 void iovw_done(struct iovec_wrapper *iovw);
 
 int iovw_put(struct iovec_wrapper *iovw, void *data, size_t len);
-static inline int iovw_consume(struct iovec_wrapper *iovw, void *data, size_t len) {
-        /* Move data into iovw or free on error */
-        int r;
-
-        r = iovw_put(iovw, data, len);
-        if (r < 0)
-                free(data);
-
-        return r;
-}
+int iovw_append(struct iovec_wrapper *iovw, const void *data, size_t len);
+int iovw_consume(struct iovec_wrapper *iovw, void *data, size_t len);
 
 static inline bool iovw_isempty(const struct iovec_wrapper *iovw) {
         return !iovw || iovw->count == 0;
@@ -40,4 +32,5 @@ int iovw_put_string_fieldf_full(struct iovec_wrapper *iovw, bool replace, const 
 int iovw_put_string_field_free(struct iovec_wrapper *iovw, const char *field, char *value);
 void iovw_rebase(struct iovec_wrapper *iovw, void *old, void *new);
 size_t iovw_size(const struct iovec_wrapper *iovw);
-int iovw_append(struct iovec_wrapper *target, const struct iovec_wrapper *source);
+int iovw_append_iovw(struct iovec_wrapper *target, const struct iovec_wrapper *source);
+char* iovw_to_cstring(const struct iovec_wrapper *iovw);
