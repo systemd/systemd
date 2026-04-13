a39e1b89b5f33f46142efd5bbfcf43a434b08ae4
Lennart Poettering <lennart@amutable.com>
2026-04-13 18:04:29 +0200

    find-esp: return pinned fd to ESP/XBOOTLDR
    The reworks the ESP/XBOOTLDR logic to pin the ESP/XBOOTLDR via an fd,
    and return that as optional return parameter.
    
    So far we only pinned the parent dir of the ESP/XBOOTLDR, which was
    useful when verifying that ESP/XBOOTLDR is actually a mount point by
    comparing mount ids. This however became obsolete with
    a98a6eb95cc980edab4b0f9c59e6573edc7ffe0c. Hence, let's clean this up,
    and pin the inode we really care about and return it.

--- src/bootctl/bootctl-cleanup.c
+++ src/bootctl/bootctl-cleanup.c
@@ -93,6 +93,7 @@ int verb_cleanup(int argc, char *argv[], uintptr_t _data, void *userdata) {
 
         r = acquire_esp(/* unprivileged_mode= */ false,
                         /* graceful= */ false,
+                        /* ret_fd= */ NULL,
                         /* ret_part= */ NULL,
                         /* ret_pstart= */ NULL,
                         /* ret_psize= */ NULL,
@@ -105,6 +106,7 @@ int verb_cleanup(int argc, char *argv[], uintptr_t _data, void *userdata) {
 
         r = acquire_xbootldr(
                         /* unprivileged_mode= */ false,
+                        /* ret_fd= */ NULL,
                         /* ret_uuid= */ NULL,
                         &xbootldr_devid);
         if (r == -EACCES)
