8d2a8c313c51e7f5b85884a474cbb0385d5af668
Zbigniew Jędrzejewski-Szmek <zbyszek@amutable.com>
2026-04-16 18:32:13 +0200

    oomctl: convert to the new option and verb parsers
    --help is the same, except for whitespace and common option
    descriptions.
    
    Co-developed-by: Claude Opus 4.6 <noreply@anthropic.com>

--- src/oom/oomctl.c
+++ src/oom/oomctl.c
@@ -1,7 +1,5 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 
-#include <getopt.h>
-
 #include "sd-bus.h"
 
 #include "alloc-util.h"
@@ -9,8 +7,10 @@
 #include "bus-error.h"
 #include "bus-locator.h"
 #include "bus-message-util.h"
+#include "format-table.h"
 #include "log.h"
 #include "main-func.h"
+#include "options.h"
 #include "pager.h"
 #include "pretty-print.h"
 #include "verbs.h"
@@ -19,6 +19,7 @@ static PagerFlags arg_pager_flags = 0;
 
 static int help(void) {
         _cleanup_free_ char *link = NULL;
+        _cleanup_(table_unrefp) Table *options = NULL, *verbs = NULL;
         int r;
 
         pager_open(arg_pager_flags);
@@ -27,29 +28,45 @@ static int help(void) {
         if (r < 0)
                 return log_oom();
 
-        printf("%1$s [OPTIONS...] COMMAND ...\n\n"
-               "%2$sManage or inspect the userspace OOM killer.%3$s\n"
-               "\n%4$sCommands:%5$s\n"
-               "  dump                        Output the current state of systemd-oomd\n"
-               "\n%4$sOptions:%5$s\n"
-               "  -h --help                   Show this help\n"
-               "     --version                Show package version\n"
-               "     --no-pager               Do not pipe output into a pager\n"
-               "\nSee the %6$s for details.\n",
+        r = verbs_get_help_table(&verbs);
+        if (r < 0)
+                return r;
+
+        r = option_parser_get_help_table(&options);
+        if (r < 0)
+                return r;
+
+        (void) table_sync_column_widths(0, verbs, options);
+
+        printf("%s [OPTIONS...] COMMAND ...\n\n"
+               "%sManage or inspect the userspace OOM killer.%s\n"
+               "\n%sCommands:%s\n",
                program_invocation_short_name,
                ansi_highlight(),
                ansi_normal(),
                ansi_underline(),
-               ansi_normal(),
-               link);
+               ansi_normal());
 
+        r = table_print_or_warn(verbs);
+        if (r < 0)
+                return r;
+
+        printf("\n%sOptions:%s\n",
+               ansi_underline(),
+               ansi_normal());
+
+        r = table_print_or_warn(options);
+        if (r < 0)
+                return r;
+
+        printf("\nSee the %s for details.\n", link);
         return 0;
 }
 
-static int verb_help(int argc, char *argv[], uintptr_t _data, void *userdata) {
-        return help();
-}
+VERB_COMMON_HELP_HIDDEN(help);
 
+VERB(verb_dump_state, "dump", NULL, VERB_ANY, 1, VERB_DEFAULT,
+     "Output the current state of systemd-oomd");
 static int verb_dump_state(int argc, char *argv[], uintptr_t _data, void *userdata) {
         _cleanup_(sd_bus_error_free) sd_bus_error error = SD_BUS_ERROR_NULL;
         _cleanup_(sd_bus_message_unrefp) sd_bus_message *reply = NULL;
@@ -69,64 +86,42 @@ static int verb_dump_state(int argc, char *argv[], uintptr_t _data, void *userda
         return bus_message_dump_fd(reply);
 }
 
-static int parse_argv(int argc, char *argv[]) {
-        enum {
-                ARG_VERSION = 0x100,
-                ARG_NO_PAGER,
-        };
-
-        static const struct option options[] = {
-                { "help",     no_argument, NULL, 'h'          },
-                { "version",  no_argument, NULL, ARG_VERSION  },
-                { "no-pager", no_argument, NULL, ARG_NO_PAGER },
-                {}
-        };
-
-        int c;
-
+static int parse_argv(int argc, char *argv[], char ***ret_args) {
         assert(argc >= 0);
         assert(argv);
 
-        while ((c = getopt_long(argc, argv, "h", options, NULL)) >= 0)
+        OptionParser state = { argc, argv };
+        const char *arg;
 
+        FOREACH_OPTION(&state, c, &arg, /* on_error= */ return c)
                 switch (c) {
 
-                        case 'h':
-                                return help();
+                OPTION_COMMON_HELP:
+                        return help();
 
-                        case ARG_VERSION:
-                                return version();
+                OPTION_COMMON_VERSION:
+                        return version();
 
-                        case ARG_NO_PAGER:
-                                arg_pager_flags |= PAGER_DISABLE;
-                                break;
-
-                        case '?':
-                                return -EINVAL;
-
-                        default:
-                                assert_not_reached();
+                OPTION_COMMON_NO_PAGER:
+                        arg_pager_flags |= PAGER_DISABLE;
+                        break;
                 }
 
+        *ret_args = option_parser_get_args(&state);
         return 1;
 }
 
 static int run(int argc, char* argv[]) {
-        static const Verb verbs[] = {
-                { "help", VERB_ANY, VERB_ANY, 0,            verb_help       },
-                { "dump", VERB_ANY, 1,        VERB_DEFAULT, verb_dump_state },
-                {}
-        };
-
         int r;
 
         log_setup();
 
-        r = parse_argv(argc, argv);
+        char **args = NULL;
+        r = parse_argv(argc, argv, &args);
         if (r <= 0)
                 return r;
 
-        return dispatch_verb(argc, argv, verbs, NULL);
+        return dispatch_verb_with_args(args, NULL);
 }
 
 DEFINE_MAIN_FUNCTION(run);
