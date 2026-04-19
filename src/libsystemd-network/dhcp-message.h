417a67bc19a87621f747d26869da2f18214a7b63
Yu Watanabe <watanabe.yu+github@gmail.com>
2026-04-19 15:47:45 +0900

    dhcp-message: introduce dhcp_message_{append,get}_option_parameter_request_list()
    These are for DHCP option 55 (parameter request list).

--- src/libsystemd-network/dhcp-message.h
+++ src/libsystemd-network/dhcp-message.h
@@ -40,6 +40,7 @@ int dhcp_message_append_option_address(sd_dhcp_message *message, uint8_t code, c
 int dhcp_message_append_option_addresses(sd_dhcp_message *message, uint8_t code, size_t n_addr, const struct in_addr *addr);
 int dhcp_message_append_option_string(sd_dhcp_message *message, uint8_t code, const char *data);
 int dhcp_message_append_option_client_id(sd_dhcp_message *message, const sd_dhcp_client_id *id);
+int dhcp_message_append_option_parameter_request_list(sd_dhcp_message *message, Set *prl);
 
 int dhcp_message_get_option(sd_dhcp_message *message, uint8_t code, size_t length, void *ret);
 int dhcp_message_get_option_alloc(sd_dhcp_message *message, uint8_t code, struct iovec *ret);
@@ -52,6 +53,7 @@ int dhcp_message_get_option_address(sd_dhcp_message *message, uint8_t code, stru
 int dhcp_message_get_option_addresses(sd_dhcp_message *message, uint8_t code, size_t *ret_n_addr, struct in_addr **ret_addr);
 int dhcp_message_get_option_string(sd_dhcp_message *message, uint8_t code, char **ret);
 int dhcp_message_get_option_client_id(sd_dhcp_message *message, sd_dhcp_client_id *ret);
+int dhcp_message_get_option_parameter_request_list(sd_dhcp_message *message, Set **ret);
 
 int dhcp_message_parse(
                 const struct iovec *iov,
