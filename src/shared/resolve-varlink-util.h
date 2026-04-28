49a230a43ab3133f8223d849c29cbe7689983039
Nick Rosbrook <enr0n@ubuntu.com>
2026-04-28 16:32:38 -0400

    resolvectl: use varlink instead of dbus for ResolveAddress

--- src/shared/resolve-varlink-util.h
+++ src/shared/resolve-varlink-util.h
@@ -23,3 +23,21 @@ ResolveHostnameReply* resolve_hostname_reply_free(ResolveHostnameReply *reply);
 DEFINE_TRIVIAL_CLEANUP_FUNC(ResolveHostnameReply*, resolve_hostname_reply_free);
 
 int dispatch_resolve_hostname_reply(const char *name, sd_json_variant *variant, sd_json_dispatch_flags_t flags, void *userdata);
+
+typedef struct ResolvedName {
+        int ifindex;
+        char *name;
+} ResolvedName;
+
+ResolvedName* resolved_name_free(ResolvedName *name);
+DEFINE_TRIVIAL_CLEANUP_FUNC(ResolvedName*, resolved_name_free);
+
+typedef struct ResolveAddressReply {
+        uint64_t flags;
+        OrderedSet *names;
+} ResolveAddressReply;
+
+ResolveAddressReply* resolve_address_reply_free(ResolveAddressReply *reply);
+DEFINE_TRIVIAL_CLEANUP_FUNC(ResolveAddressReply*, resolve_address_reply_free);
+
+int dispatch_resolve_address_reply(const char *name, sd_json_variant *variant, sd_json_dispatch_flags_t flags, void *userdata);
