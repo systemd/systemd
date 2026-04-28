77bb8dd1439c3587a15323e61b2accca3decd80b
Nick Rosbrook <enr0n@ubuntu.com>
2026-04-28 16:32:38 -0400

    resolvectl: use varlink instead of dbus for ResolveAddress

--- src/shared/resolve-varlink-util.c
+++ src/shared/resolve-varlink-util.c
@@ -120,3 +120,97 @@ int dispatch_resolve_hostname_reply(const char *name, sd_json_variant *variant, 
 
         return 0;
 }
+
+ResolvedName* resolved_name_free(ResolvedName *name) {
+        if (!name)
+                return NULL;
+
+        free(name->name);
+
+        return mfree(name);
+}
+
+DEFINE_PRIVATE_HASH_OPS_WITH_VALUE_DESTRUCTOR(
+        resolved_name_hash_ops,
+        void,
+        trivial_hash_func,
+        trivial_compare_func,
+        ResolvedName,
+        resolved_name_free);
+
+static int dispatch_resolved_name(const char *name, sd_json_variant *variant, sd_json_dispatch_flags_t flags, void *userdata) {
+        static const sd_json_dispatch_field resolved_name_dispatch_table[] = {
+                { "ifindex", _SD_JSON_VARIANT_TYPE_INVALID, json_dispatch_ifindex,   offsetof(ResolvedName, ifindex), SD_JSON_RELAX     },
+                { "name",    SD_JSON_VARIANT_STRING,        sd_json_dispatch_string, offsetof(ResolvedName, name),    SD_JSON_MANDATORY },
+                {},
+        };
+        ResolvedName **ret = ASSERT_PTR(userdata);
+        int r;
+
+        _cleanup_(resolved_name_freep) ResolvedName *resolved_name = new0(ResolvedName, 1);
+        if (!resolved_name)
+                return log_oom();
+
+        r = sd_json_dispatch(variant, resolved_name_dispatch_table, flags & ~SD_JSON_MANDATORY, resolved_name);
+        if (r < 0)
+                return r;
+
+        *ret = TAKE_PTR(resolved_name);
+
+        return 0;
+}
+
+static int dispatch_resolved_name_array(const char *name, sd_json_variant *variant, sd_json_dispatch_flags_t flags, void *userdata) {
+        OrderedSet **ret = ASSERT_PTR(userdata);
+        _cleanup_ordered_set_free_ OrderedSet *resolved_names = NULL;
+        sd_json_variant *v;
+        int r;
+
+        JSON_VARIANT_ARRAY_FOREACH(v, variant) {
+                _cleanup_(resolved_name_freep) ResolvedName *resolved_name = NULL;
+
+                r = dispatch_resolved_name(name, v, flags, &resolved_name);
+                if (r < 0)
+                        return json_log(v, flags, r, "JSON array element is not a valid ResolvedName.");
+
+                r = ordered_set_ensure_put(&resolved_names, &resolved_name_hash_ops, resolved_name);
+                if (r < 0)
+                        return r;
+                TAKE_PTR(resolved_name);
+        }
+
+        free_and_replace_full(*ret, resolved_names, ordered_set_free);
+
+        return 0;
+}
+
+ResolveAddressReply* resolve_address_reply_free(ResolveAddressReply *reply) {
+        if (!reply)
+                return NULL;
+
+        ordered_set_free(reply->names);
+
+        return mfree(reply);
+}
+
+int dispatch_resolve_address_reply(const char *name, sd_json_variant *variant, sd_json_dispatch_flags_t flags, void *userdata) {
+        static const sd_json_dispatch_field resolve_address_reply_dispatch_table[] = {
+                { "flags", _SD_JSON_VARIANT_TYPE_INVALID, sd_json_dispatch_uint64,      offsetof(ResolveAddressReply, flags), SD_JSON_MANDATORY },
+                { "names", SD_JSON_VARIANT_ARRAY,         dispatch_resolved_name_array, offsetof(ResolveAddressReply, names), SD_JSON_MANDATORY },
+                {},
+        };
+        ResolveAddressReply **ret = ASSERT_PTR(userdata);
+        int r;
+
+        _cleanup_(resolve_address_reply_freep) ResolveAddressReply *reply = new0(ResolveAddressReply, 1);
+        if (!reply)
+                return log_oom();
+
+        r = sd_json_dispatch(variant, resolve_address_reply_dispatch_table, flags, reply);
+        if (r < 0)
+                return r;
+
+        *ret = TAKE_PTR(reply);
+
+        return 0;
+}
