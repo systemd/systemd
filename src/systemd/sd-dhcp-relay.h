4295c893e7612a332286928b288dae54e2fbb31f
Yu Watanabe <watanabe.yu+github@gmail.com>
2026-04-28 13:27:43 +0900

    sd-dhcp-relay: introduce new DHCP relay agent
    Previously, sd-dhcp-server can be run as a DHCP relay agent.
    But, DHCP server and DHCP relay agent behave completely differently,
    hence there is almost no code that can be shared between the two modes.
    Let's split out the DHCP relay agent feature from sd-dhcp-server.
    
    The new DHCP relay agent supports:
    - multiple upstream and downstream interfaces,
    - gateway address (giaddr field in DHCP message header) is configurable,
    - supports more DHCP relay agent information sub-options,
    - each interface has their own socket fd, and each socket is bound to
      the interface, so that we can enable/disable each interface
      safely without affecting other interfaces, and we can filter out any
      unexpected packets from unmanaged interfaces,
    - enable BPF filter for AF_PACKET socket fd, like we already do in DHCP
      client, then the IO event source for the socket is triggered only when
      a DHCP message is received.
    
    networkd integration and test cases will be added later.

---
+++ src/systemd/sd-dhcp-relay.h
@@ -0,0 +1,73 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#ifndef foosddhcprelayhfoo
+#define foosddhcprelayhfoo
+
+/***
+  systemd is free software; you can redistribute it and/or modify it
+  under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation; either version 2.1 of the License, or
+  (at your option) any later version.
+
+  systemd is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with systemd; If not, see <https://www.gnu.org/licenses/>.
+***/
+
+#include <netinet/in.h>
+
+#include "_sd-common.h"
+
+_SD_BEGIN_DECLARATIONS;
+
+typedef struct sd_event sd_event;
+typedef struct sd_dhcp_relay sd_dhcp_relay;
+typedef struct sd_dhcp_relay_interface sd_dhcp_relay_interface;
+
+_SD_DECLARE_TRIVIAL_REF_UNREF_FUNC(sd_dhcp_relay);
+_SD_DECLARE_TRIVIAL_REF_UNREF_FUNC(sd_dhcp_relay_interface);
+
+int sd_dhcp_relay_new(sd_dhcp_relay **ret);
+
+int sd_dhcp_relay_attach_event(sd_dhcp_relay *relay, sd_event *event, int64_t priority);
+int sd_dhcp_relay_detach_event(sd_dhcp_relay *relay);
+sd_event *sd_dhcp_relay_get_event(sd_dhcp_relay *relay);
+
+int sd_dhcp_relay_set_server_address(sd_dhcp_relay *relay, const struct in_addr *address);
+int sd_dhcp_relay_get_server_address(sd_dhcp_relay *relay, struct in_addr *ret);
+int sd_dhcp_relay_set_server_port(sd_dhcp_relay *relay, uint16_t port);
+int sd_dhcp_relay_set_remote_id(sd_dhcp_relay *relay, const struct iovec *iov);
+int sd_dhcp_relay_set_server_identifier_override(sd_dhcp_relay *relay, int b);
+
+int sd_dhcp_relay_add_interface(sd_dhcp_relay *relay, int ifindex, int is_upstream, sd_dhcp_relay_interface **ret);
+
+int sd_dhcp_relay_interface_set_ifname(sd_dhcp_relay_interface *interface, const char *ifname);
+int sd_dhcp_relay_interface_get_ifname(sd_dhcp_relay_interface *interface, const char **ret);
+int sd_dhcp_relay_interface_set_address(sd_dhcp_relay_interface *interface, const struct in_addr *address);
+int sd_dhcp_relay_interface_get_address(sd_dhcp_relay_interface *interface, struct in_addr *ret);
+int sd_dhcp_relay_interface_set_port(sd_dhcp_relay_interface *interface, uint16_t port);
+int sd_dhcp_relay_interface_set_ip_service_type(sd_dhcp_relay_interface *interface, uint8_t type);
+int sd_dhcp_relay_interface_is_running(sd_dhcp_relay_interface *interface);
+int sd_dhcp_relay_interface_start(sd_dhcp_relay_interface *interface);
+int sd_dhcp_relay_interface_stop(sd_dhcp_relay_interface *interface);
+
+int sd_dhcp_relay_downstream_set_broadcast_address(
+                sd_dhcp_relay_interface *interface,
+                uint16_t arp_type,
+                size_t length,
+                const uint8_t *address);
+int sd_dhcp_relay_downstream_set_gateway_address(sd_dhcp_relay_interface *interface, const struct in_addr *address);
+int sd_dhcp_relay_downstream_set_circuit_id(sd_dhcp_relay_interface *interface, const struct iovec *iov);
+int sd_dhcp_relay_downstream_set_virtual_subnet_selection(sd_dhcp_relay_interface *interface, const struct iovec *iov);
+
+int sd_dhcp_relay_upstream_set_priority(sd_dhcp_relay_interface *interface, int64_t priority);
+
+_SD_DEFINE_POINTER_CLEANUP_FUNC(sd_dhcp_relay, sd_dhcp_relay_unref);
+_SD_DEFINE_POINTER_CLEANUP_FUNC(sd_dhcp_relay_interface, sd_dhcp_relay_interface_unref);
+
+_SD_END_DECLARATIONS;
+
+#endif
