d8cfeefdee6642edbce7ca7e6a40917dff8c7c53
Lennart Poettering <lennart@amutable.com>
2026-04-29 18:47:48 +0200

    repart: hide read-only block devices from candidates
    If they are read-only they are no candidates, since we cannot write to
    them.

--- src/repart/repart.c
+++ src/repart/repart.c
@@ -9896,7 +9896,7 @@ static int parse_argv(int argc, char *argv[]) {
 
                 OPTION_LONG("list-devices", NULL,
                             "List candidate block devices to operate on"):
-                        r = blockdev_list(BLOCKDEV_LIST_REQUIRE_PARTITION_SCANNING|BLOCKDEV_LIST_SHOW_SYMLINKS|BLOCKDEV_LIST_IGNORE_ZRAM, /* ret_devices= */ NULL, /* ret_n_devices= */ NULL);
+                        r = blockdev_list(BLOCKDEV_LIST_REQUIRE_PARTITION_SCANNING|BLOCKDEV_LIST_SHOW_SYMLINKS|BLOCKDEV_LIST_IGNORE_ZRAM|BLOCKDEV_LIST_IGNORE_READ_ONLY, /* ret_devices= */ NULL, /* ret_n_devices= */ NULL);
                         if (r < 0)
                                 return r;
 
