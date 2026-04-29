7671b43cb88532cce2aa9ad12f777922206d6a42
Frantisek Sumsal <frantisek@sumsal.cz>
2026-04-29 16:50:57 +0200

    sd-json: limit the number of env variables to something reasonable
    Let's start with 1024, as that should be plenty for all sane use cases.

--- src/libsystemd/sd-json/json-util.h
+++ src/libsystemd/sd-json/json-util.h
@@ -9,6 +9,8 @@
 #include "sd-forward.h"
 #include "string-util.h"        /* IWYU pragma: keep */
 
+#define ENVIRONMENT_ASSIGNMENTS_MAX 1024U
+
 #define JSON_VARIANT_REPLACE(v, q)        \
         do {                              \
                 typeof(v)* _v = &(v);     \
