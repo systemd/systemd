adbcfe540fb74695c9259e052c3cbac92d1ecc2e
Yu Watanabe <watanabe.yu+github@gmail.com>
2026-05-04 19:57:49 +0900

    sd-dhcp-server: refactoring for socket fd handling
    This makes
    - UDP socket fd is owned by IO event source,
    - open RAW socket fd just before sending packet, and close after sending,
    - set TOS and socket priority,
    - use AF_UNIX soxket pair in the unit test and fuzzer, so the unit test
      can now run by unprivileged user.

--- src/libsystemd-network/dhcp-server-internal.h
+++ src/libsystemd-network/dhcp-server-internal.h
@@ -29,9 +29,9 @@ typedef struct sd_dhcp_server {
 
         sd_event *event;
         int event_priority;
-        sd_event_source *receive_message;
-        int fd;
-        int fd_raw;
+        sd_event_source *io_event_source;
+        uint8_t ip_service_type;
+        int socket_fd; /* socket fd set externally, used by unit tests */
 
         int ifindex;
         char *ifname;
