bcb1794ad1404283ece534d5b27a8e53071ed4a8
Zbigniew Jędrzejewski-Szmek <zbyszek@amutable.com>
2026-05-05 13:21:30 +0200

    test-homectl-prompts: add manual test to exercise prompt functionality
    The prompt for groups is nice. The prompt for a shell could use some
    love. Looking at this is much easier if we can invoke the code outside
    in isolation.
    
    I wrote this when looking at https://github.com/systemd/systemd/pull/41947,
    where I wanted to see how the homectl prompt works with the changes.

---
+++ src/home/test-homectl-prompts.c
@@ -0,0 +1,101 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+
+#include "format-table.h"
+#include "help-util.h"
+#include "homectl-prompts.h"
+#include "main-func.h"
+#include "options.h"
+#include "strv.h"
+#include "tests.h"
+#include "verbs.h"
+
+static int help(void) {
+        _cleanup_(table_unrefp) Table *options = NULL, *verbs = NULL;
+        int r;
+
+        r = option_parser_get_help_table(&options);
+        if (r < 0)
+                return r;
+
+        r = verbs_get_help_table(&verbs);
+        if (r < 0)
+                return r;
+
+        (void) table_sync_column_widths(0, options, verbs);
+
+        help_cmdline("[OPTIONS...] USERNAME");
+        help_abstract("Exercise homectl prompt functions in isolation.");
+
+        help_section("Verbs");
+        r = table_print_or_warn(verbs);
+        if (r < 0)
+                return r;
+
+        help_section("Options");
+        return table_print_or_warn(options);
+}
+
+VERB(verb_groups, "groups", "[USER]", VERB_ANY, 2, 0, "Select groups");
+static int verb_groups(int argc, char *argv[], uintptr_t _data, void *userdata) {
+        assert(argv);
+
+        const char *username = argv[1] ?: "test";
+        int r;
+
+        _cleanup_strv_free_ char **t = NULL;
+
+        r = prompt_groups(username, &t);
+        if (r < 0)
+                return r;
+
+        log_info("groups: %s → %s", username, strnull(strv_join(t, ", ")));
+        return 0;
+}
+
+VERB(verb_shell, "shell", "[USER]", VERB_ANY, 2, 0, "Select shell");
+static int verb_shell(int argc, char *argv[], uintptr_t _data, void *userdata) {
+        assert(argv);
+
+        const char *username = argv[1] ?: "test";
+        int r;
+
+        _cleanup_free_ char *s = NULL;
+
+        r = prompt_shell(username, &s);
+        if (r < 0)
+                return r;
+
+        log_info("shell: %s → %s", username, strnull(s));
+        return 0;
+}
+
+static int parse_argv(int argc, char **argv, char ***remaining_args) {
+        assert(argc >= 0);
+        assert(argv);
+        assert(remaining_args);
+
+        OptionParser opts = { argc, argv };
+
+        FOREACH_OPTION_OR_RETURN(c, &opts)
+                switch (c) {
+
+                OPTION_COMMON_HELP:
+                        return help();
+                };
+
+        *remaining_args = option_parser_get_args(&opts);
+        return 1;
+}
+
+static int run(int argc, char **argv) {
+        int r;
+
+        char **args = NULL;
+        r = parse_argv(argc, argv, &args);
+        if (r <= 0)
+                return r;
+
+        return dispatch_verb_with_args(args, /* userdata= */ NULL);
+}
+
+DEFINE_MAIN_FUNCTION(run);
