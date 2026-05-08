0c693d5a77db906f4052f1a521df73ba1a8465ad
Zbigniew Jędrzejewski-Szmek <zbyszek@amutable.com>
2026-05-08 09:23:09 +0200

    report-basic: expose os-release fields as a metric
    Add io.systemd.Basic.OSRelease metric family that reports all the fields
    in os-release.
    
    $ cat /etc/os-release
    NAME="Fedora Linux"
    VERSION="45 (Cloud Edition Prerelease)"
    RELEASE_TYPE=development
    ID=fedora
    VERSION_ID=45
    VERSION_CODENAME=""
    PRETTY_NAME="Fedora Linux 45 (Cloud Edition Prerelease)"
    ANSI_COLOR="0;38;2;60;110;180"
    LOGO=fedora-logo-icon
    CPE_NAME="cpe:/o:fedoraproject:fedora:45"
    HOME_URL="https://fedoraproject.org/"
    DOCUMENTATION_URL="https://docs.fedoraproject.org/en-US/fedora/rawhide/"
    SUPPORT_URL="https://ask.fedoraproject.org/"
    BUG_REPORT_URL="https://bugzilla.redhat.com/"
    REDHAT_BUGZILLA_PRODUCT="Fedora"
    REDHAT_BUGZILLA_PRODUCT_VERSION=rawhide
    REDHAT_SUPPORT_PRODUCT="Fedora"
    REDHAT_SUPPORT_PRODUCT_VERSION=rawhide
    SUPPORT_END=2027-11-24
    VARIANT="Cloud Edition"
    VARIANT_ID=cloud
    
    $ varlinkctl call --more ./build/systemd-report-basic io.systemd.Metrics.List {}
    ...
    {
    	"name" : "io.systemd.Basic.OSRelease",
    	"value" : 0,
    	"fields" : {
    		"NAME" : "Fedora Linux",
    		"VERSION" : "44 (Workstation Edition)",
    		"RELEASE_TYPE" : "stable",
    		"ID" : "fedora",
    		"VERSION_ID" : "44",
    		"VERSION_CODENAME" : "",
    		"PRETTY_NAME" : "Fedora Linux 44 (Workstation Edition)",
    		"ANSI_COLOR" : "0;38;2;60;110;180",
    		"LOGO" : "fedora-logo-icon",
    		"CPE_NAME" : "cpe:/o:fedoraproject:fedora:44",
    		"DEFAULT_HOSTNAME" : "fedora",
    		"HOME_URL" : "https://fedoraproject.org/",
    		"DOCUMENTATION_URL" : "https://docs.fedoraproject.org/en-US/fedora/f44/",
    		"SUPPORT_URL" : "https://ask.fedoraproject.org/",
    		"BUG_REPORT_URL" : "https://bugzilla.redhat.com/",
    		"REDHAT_BUGZILLA_PRODUCT" : "Fedora",
    		"REDHAT_BUGZILLA_PRODUCT_VERSION" : "44",
    		"REDHAT_SUPPORT_PRODUCT" : "Fedora",
    		"REDHAT_SUPPORT_PRODUCT_VERSION" : "44",
    		"SUPPORT_END" : "2027-05-19",
    		"VARIANT" : "Workstation Edition",
    		"VARIANT_ID" : "workstation"
    	}
    }

--- src/report/report-basic.c
+++ src/report/report-basic.c
@@ -8,9 +8,14 @@
 
 #include "alloc-util.h"
 #include "architecture.h"
+#include "escape.h"
 #include "hostname-setup.h"
+#include "log.h"
 #include "metrics.h"
+#include "os-util.h"
 #include "report-basic.h"
+#include "string-util.h"
+#include "strv.h"
 #include "virt.h"
 
 static int architecture_generate(MetricFamilyContext *context, void *userdata) {
@@ -88,6 +93,41 @@ static int machine_id_generate(MetricFamilyContext *context, void *userdata) {
                         /* fields= */ NULL);
 }
 
+static int os_release_generate(MetricFamilyContext *context, void *userdata) {
+        _cleanup_strv_free_ char **pairs = NULL;
+        _cleanup_(sd_json_variant_unrefp) sd_json_variant *fields = NULL;
+        int r;
+
+        assert(context);
+        assert(context->link);
+
+        r = load_os_release_pairs(/* root= */ NULL, &pairs);
+        if (r < 0) {
+                log_warning_errno(r, "Failed to load os-release, ignoring: %m");
+                return 0;
+        }
+
+        STRV_FOREACH_PAIR(k, v, pairs) {
+                if (!string_is_safe(*k, STRING_ASCII | STRING_FILENAME)) {
+                        if (DEBUG_LOGGING) {
+                                _cleanup_free_ char *t = cescape(*k);
+                                log_debug("Invalid field in os-release, ignoring: %s", strnull(t));
+                        }
+                        continue;
+                }
+
+                r = sd_json_variant_set_field_string(&fields, *k, *v);
+                if (r < 0)
+                        return r;
+        }
+
+        return metric_build_send_unsigned(
+                        context,
+                        /* object= */ NULL,
+                        /* value= */ 0,       /* placeholder value */
+                        fields);              /* the actual data */
+}
+
 static int virtualization_generate(MetricFamilyContext *context, void *userdata) {
         Virtualization v;
 
@@ -135,6 +175,12 @@ static const MetricFamily metric_family_table[] = {
                 .description = "Machine ID",
                 .type = METRIC_FAMILY_TYPE_STRING,
                 .generate = machine_id_generate,
+        },
+        {
+                .name = METRIC_IO_SYSTEMD_BASIC_PREFIX "OSRelease",
+                .description = "Operating system identification (from os-release)",
+                .type = METRIC_FAMILY_TYPE_STRING,
+                .generate = os_release_generate,
         },
         {
                 .name = METRIC_IO_SYSTEMD_BASIC_PREFIX "Virtualization",
