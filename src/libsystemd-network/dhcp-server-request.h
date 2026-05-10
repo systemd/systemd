b4f7e30d0b9b7b2734edffc12ed14d0298fd9022
Yu Watanabe <watanabe.yu+github@gmail.com>
2026-05-11 01:49:56 +0900

    dhcp-server-request: rename dhcp_server_handle_message() -> dhcp_server_process_message()
    Then, make it take struct iovec.
    No functional change, just refactoring.

--- src/libsystemd-network/dhcp-server-request.h
+++ src/libsystemd-network/dhcp-server-request.h
@@ -5,7 +5,6 @@
 
 #include "dhcp-client-id-internal.h"
 #include "dhcp-message.h"
-#include "dhcp-protocol.h"
 #include "ether-addr-util.h"
 #include "sd-forward.h"
 #include "sparse-endian.h"
@@ -36,5 +35,5 @@ typedef struct DHCPRequest {
 
 int dhcp_request_get_lifetime_timestamp(DHCPRequest *req, clockid_t clock, usec_t *ret);
 
-int dhcp_server_handle_message(sd_dhcp_server *server, DHCPMessage *message, size_t length, struct msghdr *mh);
+int dhcp_server_process_message(sd_dhcp_server *server, const struct iovec *iov, struct msghdr *mh);
 int dhcp_server_setup_io_event_source(sd_dhcp_server *server);
