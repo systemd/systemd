22d4070b79b5719b8d1930011ed8183415cb38c4
Emanuele Rocca <emanuele.rocca@arm.com>
2026-05-12 19:02:44 +0200

    include: fully override sys/pidfd.h
    Instead of relying on the system's sys/pidfd.h, provide a full override.
    
    Signed-off-by: Emanuele Rocca <emanuele.rocca@arm.com>

--- src/include/override/sys/pidfd.h
+++ src/include/override/sys/pidfd.h
@@ -1,25 +1,25 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 #pragma once
 
-/* since glibc-2.36 */
-#if HAVE_PIDFD_OPEN
-#include_next <sys/pidfd.h>     /* IWYU pragma: export */
-#endif
-
 #include <linux/types.h>
 #include <signal.h>
 #include <sys/ioctl.h>
 
 /* Defined since glibc-2.36.
  * Supported since kernel v5.3 (7615d9e1780e26e0178c93c55b73309a5dc093d7). */
-#if !HAVE_PIDFD_OPEN
+#if HAVE_PIDFD_OPEN
+extern int pidfd_open (__pid_t __pid, unsigned int __flags) __THROW;
+#else
 int missing_pidfd_open(pid_t pid, unsigned flags);
 #  define pidfd_open missing_pidfd_open
 #endif
 
 /* Defined since glibc-2.36.
  * Supported since kernel v5.1 (3eb39f47934f9d5a3027fe00d906a45fe3a15fad). */
-#if !HAVE_PIDFD_SEND_SIGNAL
+#if HAVE_PIDFD_SEND_SIGNAL
+extern int pidfd_send_signal (int __pidfd, int __sig, siginfo_t *__info,
+                              unsigned int __flags) __THROW;
+#else
 int missing_pidfd_send_signal(int fd, int sig, siginfo_t *info, unsigned flags);
 #  define pidfd_send_signal missing_pidfd_send_signal
 #endif
