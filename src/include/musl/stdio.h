c15765abc65dc38247ccef8503a1978493852ede
Yu Watanabe <watanabe.yu+github@gmail.com>
2026-05-21 01:08:24 +0900

    musl: drop renameat2() wrapper
    musl provides renameat2() since v1.2.6:
    https://git.musl-libc.org/cgit/musl/commit/?id=05ce67fea99ca09cd4b6625cff7aec9cc222dd5a

--- src/include/musl/stdio.h
+++ src/include/musl/stdio.h
@@ -3,15 +3,6 @@
 
 #include_next <stdio.h>
 
-#ifndef RENAME_NOREPLACE
-#  define RENAME_NOREPLACE (1 << 0)
-#  define RENAME_EXCHANGE  (1 << 1)
-#  define RENAME_WHITEOUT  (1 << 2)
-#endif
-
-int renameat2_shim(int __oldfd, const char *__old, int __newfd, const char *__new, unsigned __flags);
-#define renameat2 renameat2_shim
-
 /* When a stream is opened read-only under glibc, fputs() and friends fail with EBADF. However, they
  * succeed under musl. We rely on the glibc behavior in the code base. The following _check_writable()
  * functions first check if the passed stream is writable, and refuse to write with EBADF if not. */
