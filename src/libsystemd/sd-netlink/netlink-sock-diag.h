5b7c5c0a3d92c4933188e1a92b83814aac2f4df7
Lennart Poettering <lennart@amutable.com>
2026-06-02 18:52:00 +0200

    sd-netlink: beef up sock-diag code a bit
    Let's make it useful to enumerate AF_UNIX sockets.

--- src/libsystemd/sd-netlink/netlink-sock-diag.h
+++ src/libsystemd/sd-netlink/netlink-sock-diag.h
@@ -1,9 +1,13 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 #pragma once
 
+#include <linux/unix_diag.h>
+
 #include "sd-netlink.h"
 
 /* TODO: to be exported later */
 
 int sd_sock_diag_socket_open(sd_netlink **ret);
 int sd_sock_diag_message_new_unix(sd_netlink *sdnl, sd_netlink_message **ret, ino_t inode, uint64_t cookie, uint32_t show);
+int sd_sock_diag_message_new_unix_dump(sd_netlink *sdnl, sd_netlink_message **ret, uint32_t states, uint32_t show);
+int sd_sock_diag_message_get_unix(sd_netlink_message *m, struct unix_diag_msg *ret);
