7ecc095cfd5c8e4f04dfe6866a49f43185d99f54
jmestwa-coder <jmestwa@gmail.com>
2026-06-09 10:36:58 +0530

    fix ellipsis buffer overflow in xescape_full and utf8 escapers

--- src/basic/utf8.c
+++ src/basic/utf8.c
@@ -200,7 +200,7 @@ char* utf8_escape_non_printable_full(const char *str, size_t console_width, bool
         if (console_width == 0)
                 return strdup("");
 
-        p = s = prev_s = malloc(strlen(str) * 4 + 1);
+        p = s = prev_s = malloc(strlen(str) * 4 + STRLEN("…") + 1);
         if (!p)
                 return NULL;
 
