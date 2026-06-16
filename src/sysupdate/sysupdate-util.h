9524c8785a4c95c4e29be08f81ff2a0806d7fe57
Lennart Poettering <lennart@amutable.com>
2026-06-16 18:14:17 +0200

    sysupdate: split out component validation/enumeration into sysupdate-util.[ch]
    Just some refactoring.
    
    The code is slightly updated, for example it now uses string_is_safe()

--- src/sysupdate/sysupdate-util.h
+++ src/sysupdate/sysupdate-util.h
@@ -1,8 +1,12 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
-
 #pragma once
+
+#include <stdbool.h>
 
 int reboot_now(void);
 
-#define SD_SYSUPDATE_OFFLINE  (UINT64_C(1) << 0)
+#define SD_SYSUPDATE_OFFLINE   (UINT64_C(1) << 0)
 #define SD_SYSUPDATE_FLAGS_ALL (SD_SYSUPDATE_OFFLINE)
+
+bool component_name_valid(const char *c);
+int get_component_list(const char *root, char ***ret);
