8a3b30391f8c2da9284d7259b3e47efe97db3cae
Paul Meyer <katexochen0@gmail.com>
2026-06-17 18:03:55 +0200

    pcrextend: refuse empty measurement over Varlink
    vl_method_extend() accepted an empty text/data value and measured an
    empty word, bypassing the empty-word refusal the CLI path already
    enforces. Measured words are joined with ":" in the record, so an empty
    word is ambiguous. Reject it.
    
    Signed-off-by: Paul Meyer <katexochen0@gmail.com>
    (cherry picked from commit e514dadd1c3f8244d8dd7a420a4f89c2dd26e4de)
    (cherry picked from commit 5032f20ba0e926cfcd3067ce4938fc77f8066386)
    (cherry picked from commit 473238d64764e12db4f08f401a9b26025b5fc90b)

--- src/pcrextend/pcrextend.c
+++ src/pcrextend/pcrextend.c
@@ -467,6 +467,9 @@ static int vl_method_extend(sd_varlink *link, sd_json_variant *parameters, sd_va
         else
                 return sd_varlink_error_invalid_parameter_name(link, "text");
 
+        if (!iovec_is_set(extend_iovec))
+                return sd_varlink_error_invalid_parameter_name(link, p.text ? "text" : "data");
+
         if (p.nvpcr) {
                 r = extend_nvpcr_now(p.nvpcr, extend_iovec->iov_base, extend_iovec->iov_len, _TPM2_USERSPACE_EVENT_TYPE_INVALID);
                 if (IN_SET(r, -ENOENT, -ENODEV))
