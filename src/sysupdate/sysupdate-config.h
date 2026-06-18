950f5193f1add628e2c9c204088e2b0c37dbe7b8
Lennart Poettering <lennart@amutable.com>
2026-06-18 17:58:09 +0200

    sysupdate: split out url config parser into new file
    These are located at two distinct places, and are very similar, let's
    put them in a common place.
    
    This is preparation for reusing them later.

---
+++ src/sysupdate/sysupdate-config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#pragma once
+
+#include "conf-parser-forward.h"
+#include "specifier.h"
+
+CONFIG_PARSER_PROTOTYPE(config_parse_url_specifiers);
+CONFIG_PARSER_PROTOTYPE(config_parse_url_specifiers_many);
+
+extern const Specifier specifier_table[];
