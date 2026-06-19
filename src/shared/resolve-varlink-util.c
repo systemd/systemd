f30bc2222d164e1571b39da31121f9519923bc0d
Nick Rosbrook <enr0n@ubuntu.com>
2026-06-19 15:01:01 -0400

    resolvectl: use varlink instead of dbus for ResolveRecord

--- src/shared/resolve-varlink-util.c
+++ src/shared/resolve-varlink-util.c
@@ -188,3 +188,74 @@ int dispatch_resolve_address_reply(const char *name, sd_json_variant *variant, s
         *ret = TAKE_STRUCT(reply);
         return 0;
 }
+
+static void resolved_record_done(ResolvedRecord *record) {
+        if (!record)
+                return;
+
+        iovec_done(&record->raw);
+}
+
+static int dispatch_resolved_record(const char *name, sd_json_variant *variant, sd_json_dispatch_flags_t flags, void *userdata) {
+        static const sd_json_dispatch_field resolved_record_dispatch_table[] = {
+                { "ifindex", _SD_JSON_VARIANT_TYPE_INVALID, json_dispatch_ifindex,        offsetof(ResolvedRecord, ifindex), SD_JSON_RELAX     },
+                { "raw",     _SD_JSON_VARIANT_TYPE_INVALID, json_dispatch_unbase64_iovec, offsetof(ResolvedRecord, raw),     SD_JSON_MANDATORY },
+                { "rr",      _SD_JSON_VARIANT_TYPE_INVALID, NULL,                         0,                                 0                 },
+                {},
+        };
+        ResolvedRecord *ret = ASSERT_PTR(userdata);
+        int r;
+
+        _cleanup_(resolved_record_done) ResolvedRecord record = {};
+        r = sd_json_dispatch(variant, resolved_record_dispatch_table, flags & ~SD_JSON_MANDATORY, &record);
+        if (r < 0)
+                return r;
+
+        *ret = TAKE_STRUCT(record);
+        return 0;
+}
+
+static int dispatch_resolved_record_array(const char *name, sd_json_variant *variant, sd_json_dispatch_flags_t flags, void *userdata) {
+        ResolveRecordReply *reply = ASSERT_PTR(userdata);
+        int r;
+
+        sd_json_variant *v;
+        JSON_VARIANT_ARRAY_FOREACH(v, variant) {
+                if (!GREEDY_REALLOC0(reply->records, reply->n_records + 1))
+                        return log_oom();
+
+                r = dispatch_resolved_record(name, v, flags, &reply->records[reply->n_records++]);
+                if (r < 0)
+                        return json_log(v, flags, r, "JSON array element is not a valid ResolvedRecord.");
+        }
+
+        return 0;
+}
+
+void resolve_record_reply_done(ResolveRecordReply *reply) {
+        if (!reply)
+                return;
+
+        FOREACH_ARRAY(record, reply->records, reply->n_records)
+                resolved_record_done(record);
+        reply->records = mfree(reply->records);
+        reply->n_records = 0;
+}
+
+int dispatch_resolve_record_reply(const char *name, sd_json_variant *variant, sd_json_dispatch_flags_t flags, void *userdata) {
+        static const sd_json_dispatch_field resolve_record_reply_dispatch_table[] = {
+                { "flags", _SD_JSON_VARIANT_TYPE_INVALID, sd_json_dispatch_uint64,        offsetof(ResolveRecordReply, flags), SD_JSON_MANDATORY },
+                { "rrs",   SD_JSON_VARIANT_ARRAY,         dispatch_resolved_record_array, 0,                                   SD_JSON_MANDATORY },
+                {},
+        };
+        ResolveRecordReply *ret = ASSERT_PTR(userdata);
+        int r;
+
+        _cleanup_(resolve_record_reply_done) ResolveRecordReply reply = {};
+        r = sd_json_dispatch(variant, resolve_record_reply_dispatch_table, flags, &reply);
+        if (r < 0)
+                return r;
+
+        *ret = TAKE_STRUCT(reply);
+        return 0;
+}
