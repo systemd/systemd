68ea90d12309085dfb7296f2e94c0920bab4033a
Nick Rosbrook <enr0n@ubuntu.com>
2026-06-19 15:01:00 -0400

    resolvectl: use varlink instead of dbus for ResolveHostname

---
+++ src/shared/resolve-varlink-util.c
@@ -0,0 +1,107 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+
+#include "sd-json.h"
+
+#include "dns-packet.h"
+#include "json-util.h"
+#include "resolve-varlink-util.h"
+
+void resolve_error_done(ResolveError *error) {
+        if (!error)
+                return;
+
+        error->ede_msg = mfree(error->ede_msg);
+        error->query_string = mfree(error->query_string);
+        error->result = mfree(error->result);
+}
+
+int dispatch_resolve_error(const char *name, sd_json_variant *variant, sd_json_dispatch_flags_t flags, void *userdata) {
+        static const sd_json_dispatch_field resolve_error_dispatch_table[] = {
+                { "rcode",                   _SD_JSON_VARIANT_TYPE_INVALID, sd_json_dispatch_int,    offsetof(ResolveError, rcode),        0 },
+                { "extendedDNSErrorCode",    _SD_JSON_VARIANT_TYPE_INVALID, sd_json_dispatch_int,    offsetof(ResolveError, ede_rcode),    0 },
+                { "extendedDNSErrorMessage", SD_JSON_VARIANT_STRING,        sd_json_dispatch_string, offsetof(ResolveError, ede_msg),      0 },
+                { "queryString",             SD_JSON_VARIANT_STRING,        sd_json_dispatch_string, offsetof(ResolveError, query_string), 0 },
+                { "result",                  SD_JSON_VARIANT_STRING,        sd_json_dispatch_string, offsetof(ResolveError, result),       0 },
+                {},
+        };
+        ResolveError *ret = ASSERT_PTR(userdata);
+        int r;
+
+        _cleanup_(resolve_error_done) ResolveError error = {
+                .rcode = _DNS_RCODE_INVALID,
+                .ede_rcode = _DNS_EDE_RCODE_INVALID,
+        };
+        r = sd_json_dispatch(variant, resolve_error_dispatch_table, flags | SD_JSON_ALLOW_EXTENSIONS, &error);
+        if (r < 0)
+                return r;
+
+        *ret = TAKE_STRUCT(error);
+        return 0;
+}
+
+static int dispatch_resolved_address(const char *name, sd_json_variant *variant, sd_json_dispatch_flags_t flags, void *userdata) {
+        static const sd_json_dispatch_field resolved_address_dispatch_table[] = {
+                { "ifindex", _SD_JSON_VARIANT_TYPE_INVALID, json_dispatch_ifindex,        offsetof(ResolvedAddress, ifindex), SD_JSON_RELAX     },
+                { "family",  _SD_JSON_VARIANT_TYPE_INVALID, json_dispatch_address_family, offsetof(ResolvedAddress, family),  SD_JSON_MANDATORY },
+                { "address", _SD_JSON_VARIANT_TYPE_INVALID, json_dispatch_in_addr_data,   offsetof(ResolvedAddress, in_addr), SD_JSON_MANDATORY },
+                {},
+        };
+        ResolvedAddress *ret = ASSERT_PTR(userdata);
+        int r;
+
+        ResolvedAddress address = {};
+        r = sd_json_dispatch(variant, resolved_address_dispatch_table, flags & ~SD_JSON_MANDATORY, &address);
+        if (r < 0)
+                return r;
+
+        if (address.family != address.in_addr.family)
+                return json_log(variant, flags, SYNTHETIC_ERRNO(EINVAL), "'%s' address and family are inconsistent.", strna(name));
+
+        *ret = TAKE_STRUCT(address);
+        return 0;
+}
+
+static int dispatch_resolved_address_array(const char *name, sd_json_variant *variant, sd_json_dispatch_flags_t flags, void *userdata) {
+        ResolveHostnameReply *reply = ASSERT_PTR(userdata);
+        int r;
+
+        sd_json_variant *v;
+        JSON_VARIANT_ARRAY_FOREACH(v, variant) {
+                if (!GREEDY_REALLOC0(reply->addresses, reply->n_addresses + 1))
+                        return json_log_oom(variant, flags);
+
+                r = dispatch_resolved_address(name, v, flags, &reply->addresses[reply->n_addresses++]);
+                if (r < 0)
+                        return r;
+        }
+
+        return 0;
+}
+
+void resolve_hostname_reply_done(ResolveHostnameReply *reply) {
+        if (!reply)
+                return;
+
+        reply->name = mfree(reply->name);
+        reply->addresses = mfree(reply->addresses);
+        reply->n_addresses = 0;
+}
+
+int dispatch_resolve_hostname_reply(const char *name, sd_json_variant *variant, sd_json_dispatch_flags_t flags, void *userdata) {
+        static const sd_json_dispatch_field resolve_hostname_reply_dispatch_table[] = {
+                { "name",      SD_JSON_VARIANT_STRING,        sd_json_dispatch_string,         offsetof(ResolveHostnameReply, name),  SD_JSON_MANDATORY },
+                { "flags",     _SD_JSON_VARIANT_TYPE_INVALID, sd_json_dispatch_uint64,         offsetof(ResolveHostnameReply, flags), SD_JSON_MANDATORY },
+                { "addresses", SD_JSON_VARIANT_ARRAY,         dispatch_resolved_address_array, 0,                                     SD_JSON_MANDATORY },
+                {},
+        };
+        ResolveHostnameReply *ret = ASSERT_PTR(userdata);
+        int r;
+
+        _cleanup_(resolve_hostname_reply_done) ResolveHostnameReply reply = {};
+        r = sd_json_dispatch(variant, resolve_hostname_reply_dispatch_table, flags | SD_JSON_ALLOW_EXTENSIONS, &reply);
+        if (r < 0)
+                return r;
+
+        *ret = TAKE_STRUCT(reply);
+        return 0;
+}
