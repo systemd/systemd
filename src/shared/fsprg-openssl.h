b91c9ed522a74cf20b2aa638fc52211dd6a0af6a
Yu Watanabe <watanabe.yu+github@gmail.com>
2026-06-20 03:29:47 +0900

    fsprg-openssl: rewrite fsprg with OpenSSL
    This introduce OpenSSL port of fsprg, which is implemented by using libgcrypt.

---
+++ src/shared/fsprg-openssl.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#pragma once
+
+#include "sd-forward.h"
+
+#define FSPRG_RECOMMENDED_SECPAR 1536
+#define FSPRG_RECOMMENDED_SEEDLEN (96/8)
+
+bool fsprg_secpar_is_valid(uint16_t secpar);
+size_t fsprg_state_size(uint16_t secpar);
+int fsprg_generate_state(
+                uint16_t secpar,
+                uint64_t epoch,
+                const struct iovec *seed,
+                struct iovec *state);
+int fsprg_evolve(struct iovec *state);
+int fsprg_get_epoch(const struct iovec *state, uint64_t *ret);
+int fsprg_get_key(const struct iovec *state, struct iovec *key);
