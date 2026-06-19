21e90e17a811986acd50c30261c99679ec3be9f6
Nick Rosbrook <enr0n@ubuntu.com>
2026-06-19 15:01:00 -0400

    resolvectl: use varlink instead of dbus for ResolveHostname

---
+++ src/shared/resolve-varlink-util.h
@@ -0,0 +1,35 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#pragma once
+
+#include <sys/io.h>
+
+#include "in-addr-util.h"
+#include "shared-forward.h"
+
+typedef struct ResolveError {
+        int rcode;
+        int ede_rcode;
+        char *ede_msg;
+        char *query_string;
+} ResolveError;
+
+void resolve_error_done(ResolveError *error);
+
+int dispatch_resolve_error(const char *name, sd_json_variant *variant, sd_json_dispatch_flags_t flags, void *userdata);
+
+typedef struct ResolvedAddress {
+        int ifindex;
+        int family;
+        union in_addr_union in_addr;
+} ResolvedAddress;
+
+typedef struct ResolveHostnameReply {
+        char *name;
+        uint64_t flags;
+        ResolvedAddress *addresses;
+        size_t n_addresses;
+} ResolveHostnameReply;
+
+void resolve_hostname_reply_done(ResolveHostnameReply *reply);
+
+int dispatch_resolve_hostname_reply(const char *name, sd_json_variant *variant, sd_json_dispatch_flags_t flags, void *userdata);
