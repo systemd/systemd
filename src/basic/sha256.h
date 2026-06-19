491e3808dbbd37669ea1c03a484e13fd0ef2d17c
Lennart Poettering <lennart@amutable.com>
2026-06-19 05:12:10 +0200

    sha256: add sha256_direct_hex() helper

--- src/basic/sha256.h
+++ src/basic/sha256.h
@@ -11,3 +11,5 @@ int sha256_fd(int fd, uint64_t max_size, uint8_t ret[static SHA256_DIGEST_SIZE])
 int parse_sha256(const char *s, uint8_t ret[static SHA256_DIGEST_SIZE]);
 
 bool sha256_is_valid(const char *s) _pure_;
+
+char *sha256_direct_hex(const void *buffer, size_t sz);
