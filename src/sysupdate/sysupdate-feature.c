5bcb17d0d3858c2819cf533d535aa5a955d687a7
Lennart Poettering <lennart@amutable.com>
2026-06-24 17:09:19 +0200

    sysupdate: add a "suggests" concept to features and components
    Let's make it possible to "suggest" that certain features or components
    are enabled under some conditions.
    
    For this, both features and components gain two things:
    
    1. A Suggested= field which takes a boolean. If true the
       feature/component will be suggested for installation, if false it
       will not.
    
    2. A set of SuggestedOnXYZ= settings are modelled after ConditionXYZ= in
       unit files (and implement a subset of them), will suggest some
       component/feature under specific conditions.
    
    The result of the condition is shown in the various output tools.

--- src/sysupdate/sysupdate-feature.c
+++ src/sysupdate/sysupdate-feature.c
@@ -1,6 +1,9 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 
+#include <unistd.h>
+
 #include "alloc-util.h"
+#include "condition.h"
 #include "conf-parser.h"
 #include "hash-funcs.h"
 #include "path-util.h"
@@ -20,6 +23,8 @@ static Feature *feature_free(Feature *f) {
         free(f->documentation);
         free(f->appstream);
 
+        condition_free_list(f->suggest_on);
+
         return mfree(f);
 }
 
@@ -32,6 +37,7 @@ Feature *feature_new(void) {
 
         *f = (Feature) {
                 .n_ref = 1,
+                .suggest = -1,
         };
 
         return f;
@@ -47,10 +53,22 @@ int feature_read_definition(Feature *f, const char *root, const char *path, cons
         assert(f);
 
         ConfigTableItem table[] = {
-                { "Feature", "Description",   config_parse_string,         0, &f->description },
-                { "Feature", "Documentation", config_parse_url_specifiers, 0, &f->documentation },
-                { "Feature", "AppStream",     config_parse_url_specifiers, 0, &f->appstream },
-                { "Feature", "Enabled",       config_parse_bool,           0, &f->enabled },
+                { "Feature", "Description",                config_parse_string,         0,                             &f->description   },
+                { "Feature", "Documentation",              config_parse_url_specifiers, 0,                             &f->documentation },
+                { "Feature", "AppStream",                  config_parse_url_specifiers, 0,                             &f->appstream     },
+                { "Feature", "Enabled",                    config_parse_bool,           0,                             &f->enabled       },
+                { "Feature", "Suggest",                    config_parse_tristate,       0,                             &f->suggest       },
+                { "Feature", "SuggestOnArchitecture",      config_parse_condition,      CONDITION_ARCHITECTURE,        &f->suggest_on    },
+                { "Feature", "SuggestOnFirmware",          config_parse_condition,      CONDITION_FIRMWARE,            &f->suggest_on    },
+                { "Feature", "SuggestOnVirtualization",    config_parse_condition,      CONDITION_VIRTUALIZATION,      &f->suggest_on    },
+                { "Feature", "SuggestOnHost",              config_parse_condition,      CONDITION_HOST,                &f->suggest_on    },
+                { "Feature", "SuggestOnFraction",          config_parse_condition,      CONDITION_FRACTION,            &f->suggest_on    },
+                { "Feature", "SuggestOnKernelCommandLine", config_parse_condition,      CONDITION_KERNEL_COMMAND_LINE, &f->suggest_on    },
+                { "Feature", "SuggestOnVersion",           config_parse_condition,      CONDITION_VERSION,             &f->suggest_on    },
+                { "Feature", "SuggestOnCredential",        config_parse_condition,      CONDITION_CREDENTIAL,          &f->suggest_on    },
+                { "Feature", "SuggestOnSecurity",          config_parse_condition,      CONDITION_SECURITY,            &f->suggest_on    },
+                { "Feature", "SuggestOnOSRelease",         config_parse_condition,      CONDITION_OS_RELEASE,          &f->suggest_on    },
+                { "Feature", "SuggestOnMachineTag",        config_parse_condition,      CONDITION_MACHINE_TAG,         &f->suggest_on    },
                 {}
         };
 
@@ -83,4 +101,16 @@ int feature_read_definition(Feature *f, const char *root, const char *path, cons
         f->id = TAKE_PTR(filename);
 
         return 0;
+}
+
+int feature_is_suggested(Feature *f) {
+        assert(f);
+
+        if (f->suggest >= 0)
+                return f->suggest;
+
+        if (!f->suggest_on) /* no condition → false */
+                return false;
+
+        return condition_test_list(f->suggest_on, environ, suggest_on_type_to_string, /* logger= */ NULL, /* userdata= */ NULL);
 }
