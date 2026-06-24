8d26091940b8d28b39cf5360f46752a046e57219
Lennart Poettering <lennart@amutable.com>
2026-06-24 17:09:19 +0200

    sysupdate: add a "suggests" concept to features and components
    Let's make it possible to "suggest" that certain features or components
    are enabled under some conditions.
    
    For this, both features and components gain two things:
    
    1. A Suggested= field which takes a boolean. If true the
       feature/component will be suggested for installation, if false it
       will not.
    
    2. A set of SuggestedOnXYZ= settings are modelled after ConditionXYZ= in
       unit files (and implement a subset of them), will suggest some
       component/feature under specific conditions.
    
    The result of the condition is shown in the various output tools.

--- src/sysupdate/sysupdate-config.c
+++ src/sysupdate/sysupdate-config.c
@@ -1,8 +1,10 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 
 #include "alloc-util.h"
+#include "condition.h"
 #include "log.h"
 #include "specifier.h"
+#include "string-table.h"
 #include "string-util.h"
 #include "strv.h"
 #include "sysupdate-config.h"
@@ -54,6 +56,22 @@ int config_parse_url_specifiers(
         return free_and_replace(*s, resolved);
 }
 
+static const char* const suggest_on_type_table[_CONDITION_TYPE_MAX] = {
+        [CONDITION_ARCHITECTURE]        = "SuggestOnArchitecture",
+        [CONDITION_FIRMWARE]            = "SuggestOnFirmware",
+        [CONDITION_VIRTUALIZATION]      = "SuggestOnVirtualization",
+        [CONDITION_HOST]                = "SuggestOnHost",
+        [CONDITION_FRACTION]            = "SuggestOnFraction",
+        [CONDITION_KERNEL_COMMAND_LINE] = "SuggestOnKernelCommandLine",
+        [CONDITION_VERSION]             = "SuggestOnVersion",
+        [CONDITION_CREDENTIAL]          = "SuggestOnCredential",
+        [CONDITION_SECURITY]            = "SuggestOnSecurity",
+        [CONDITION_OS_RELEASE]          = "SuggestOnOSRelease",
+        [CONDITION_MACHINE_TAG]         = "SuggestOnMachineTag",
+};
+
+DEFINE_STRING_TABLE_LOOKUP(suggest_on_type, ConditionType);
+
 int config_parse_url_specifiers_many(
                 const char *unit,
                 const char *filename,
@@ -94,5 +112,46 @@ int config_parse_url_specifiers_many(
         if (strv_consume(s, TAKE_PTR(resolved)) < 0)
                 return log_oom();
 
+        return 0;
+}
+
+int config_parse_condition(
+                const char *unit,
+                const char *filename,
+                unsigned line,
+                const char *section,
+                unsigned section_line,
+                const char *lvalue,
+                int ltype,
+                const char *rvalue,
+                void *data,
+                void *userdata) {
+
+        ConditionType cond = ltype;
+        Condition **list = data, *c;
+        bool negate;
+
+        assert(filename);
+        assert(lvalue);
+        assert(rvalue);
+        assert(data);
+
+        if (isempty(rvalue)) {
+                *list = condition_free_list_type(*list, cond);
+                return 0;
+        }
+
+        negate = rvalue[0] == '!';
+        if (negate)
+                rvalue++;
+
+        c = condition_new(cond, rvalue, /* triggering= */ false, negate);
+        if (!c)
+                return log_oom();
+
+        /* Drop previous assignment. */
+        *list = condition_free_list_type(*list, cond);
+
+        LIST_PREPEND(conditions, *list, c);
         return 0;
 }
