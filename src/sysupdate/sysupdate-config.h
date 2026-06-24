fc6f56cee6b001b2576bc333fd86a5f5b81d0b01
Lennart Poettering <lennart@amutable.com>
2026-06-24 17:09:19 +0200

    sysupdate: add a "suggests" concept to features and components
    Let's make it possible to "suggest" that certain features or components
    are enabled under some conditions.
    
    For this, both features and components gain two things:
    
    1. A Suggested= field which takes a boolean. If true the
       feature/component will be suggested for installation, if false it
       will not.
    
    2. A set of SuggestedOnXYZ= settings are modelled after ConditionXYZ= in
       unit files (and implement a subset of them), will suggest some
       component/feature under specific conditions.
    
    The result of the condition is shown in the various output tools.

--- src/sysupdate/sysupdate-config.h
+++ src/sysupdate/sysupdate-config.h
@@ -2,9 +2,15 @@
 #pragma once
 
 #include "conf-parser-forward.h"
+#include "shared-forward.h"
 #include "specifier.h"
+#include "string-table.h"
 
 CONFIG_PARSER_PROTOTYPE(config_parse_url_specifiers);
 CONFIG_PARSER_PROTOTYPE(config_parse_url_specifiers_many);
 
 extern const Specifier specifier_table[];
+
+CONFIG_PARSER_PROTOTYPE(config_parse_condition);
+
+DECLARE_STRING_TABLE_LOOKUP(suggest_on_type, ConditionType);
