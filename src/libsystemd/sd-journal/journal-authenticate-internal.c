0fb5f74517767cddd6ad8f348f562b81f6569890
Yu Watanabe <watanabe.yu+github@gmail.com>
2026-06-25 23:38:16 +0900

    sd-journal: drop libgcrypt dependency from libsystemd
    This introduce a vtable for journal tagging feature in sd-journal,
    and makes libgcrypt dependent features loaded by users (journald,
    journalctl, journal-remote, and unit tests) when necessary.

--- src/libsystemd/sd-journal/journal-authenticate-internal.c
+++ src/libsystemd/sd-journal/journal-authenticate-internal.c
@@ -1,35 +1,48 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 
-#include "journal-authenticate.h"
 #include "journal-authenticate-internal.h"
 #include "journal-file.h"
+
+static const JournalAuthOps *auth_ops = NULL;
+
+void journal_auth_set_ops(const JournalAuthOps *ops) {
+        auth_ops = ops;
+}
 
 void journal_file_auth_done(JournalFile *f) {
         assert(f);
 
-#if HAVE_GCRYPT
-        f->auth_context = journal_auth_free(f->auth_context);
-#endif
+        if (!auth_ops)
+                return;
+
+        assert(auth_ops->free);
+        f->auth_context = auth_ops->free(f->auth_context);
 }
 
 int journal_file_auth_load(JournalFile *f) {
         assert(f);
 
-#if HAVE_GCRYPT
-        return journal_auth_load(&f->auth_context);
-#else
-        return -EOPNOTSUPP;
-#endif
+        if (!auth_ops)
+                return -EOPNOTSUPP;
+
+        if (f->auth_context)
+                return -EBUSY;
+
+        assert(auth_ops->load);
+        return auth_ops->load(&f->auth_context);
 }
 
 int journal_file_auth_load_key(JournalFile *f, const char *key) {
         assert(f);
 
-#if HAVE_GCRYPT
-        return journal_auth_load_key(&f->auth_context, key);
-#else
-        return -EOPNOTSUPP;
-#endif
+        if (!auth_ops)
+                return -EOPNOTSUPP;
+
+        if (f->auth_context)
+                return -EBUSY;
+
+        assert(auth_ops->load_key);
+        return auth_ops->load_key(&f->auth_context, key);
 }
 
 int journal_file_auth_epoch_to_realtime_usec(JournalFile *f, uint64_t epoch, usec_t *ret_start, usec_t *ret_end) {
@@ -38,11 +51,9 @@ int journal_file_auth_epoch_to_realtime_usec(JournalFile *f, uint64_t epoch, use
         if (!JOURNAL_HEADER_SEALED(f->header))
                 return -EOPNOTSUPP;
 
-#if HAVE_GCRYPT
-        return journal_auth_epoch_to_realtime_usec(f->auth_context, epoch, ret_start, ret_end);
-#else
-        assert_not_reached();
-#endif
+        assert(auth_ops);
+        assert(auth_ops->epoch_to_realtime_usec);
+        return auth_ops->epoch_to_realtime_usec(f->auth_context, epoch, ret_start, ret_end);
 }
 
 int journal_file_auth_next_evolve_usec(JournalFile *f, usec_t *ret) {
@@ -51,11 +62,9 @@ int journal_file_auth_next_evolve_usec(JournalFile *f, usec_t *ret) {
         if (!JOURNAL_HEADER_SEALED(f->header))
                 return -EOPNOTSUPP;
 
-#if HAVE_GCRYPT
-        return journal_auth_next_evolve_usec(f->auth_context, ret);
-#else
-        assert_not_reached();
-#endif
+        assert(auth_ops);
+        assert(auth_ops->next_evolve_usec);
+        return auth_ops->next_evolve_usec(f->auth_context, ret);
 }
 
 int journal_file_auth_seek(JournalFile *f, uint64_t goal) {
@@ -64,11 +73,9 @@ int journal_file_auth_seek(JournalFile *f, uint64_t goal) {
         if (!JOURNAL_HEADER_SEALED(f->header))
                 return 0;
 
-#if HAVE_GCRYPT
-        return journal_auth_seek(f->auth_context, goal);
-#else
-        assert_not_reached();
-#endif
+        assert(auth_ops);
+        assert(auth_ops->seek);
+        return auth_ops->seek(f->auth_context, goal);
 }
 
 int journal_file_auth_start(JournalFile *f) {
@@ -77,11 +84,9 @@ int journal_file_auth_start(JournalFile *f) {
         if (!JOURNAL_HEADER_SEALED(f->header))
                 return 0;
 
-#if HAVE_GCRYPT
-        return journal_auth_start(f->auth_context);
-#else
-        assert_not_reached();
-#endif
+        assert(auth_ops);
+        assert(auth_ops->start);
+        return auth_ops->start(f->auth_context);
 }
 
 int journal_file_auth_end(JournalFile *f, uint8_t ret[static TAG_LENGTH]) {
@@ -90,11 +95,9 @@ int journal_file_auth_end(JournalFile *f, uint8_t ret[static TAG_LENGTH]) {
         if (!JOURNAL_HEADER_SEALED(f->header))
                 return -EOPNOTSUPP;
 
-#if HAVE_GCRYPT
-        return journal_auth_end(f->auth_context, ret);
-#else
-        assert_not_reached();
-#endif
+        assert(auth_ops);
+        assert(auth_ops->end);
+        return auth_ops->end(f->auth_context, ret);
 }
 
 int journal_file_auth_put_header(JournalFile *f) {
@@ -103,11 +106,9 @@ int journal_file_auth_put_header(JournalFile *f) {
         if (!JOURNAL_HEADER_SEALED(f->header))
                 return 0;
 
-#if HAVE_GCRYPT
-        return journal_auth_put_header(f->auth_context, f);
-#else
-        assert_not_reached();
-#endif
+        assert(auth_ops);
+        assert(auth_ops->put_header);
+        return auth_ops->put_header(f->auth_context, f);
 }
 
 int journal_file_auth_put_object(JournalFile *f, ObjectType type, Object *o, uint64_t p) {
@@ -116,11 +117,9 @@ int journal_file_auth_put_object(JournalFile *f, ObjectType type, Object *o, uin
         if (!JOURNAL_HEADER_SEALED(f->header))
                 return 0;
 
-#if HAVE_GCRYPT
-        return journal_auth_put_object(f->auth_context, f, type, o, p);
-#else
-        assert_not_reached();
-#endif
+        assert(auth_ops);
+        assert(auth_ops->put_object);
+        return auth_ops->put_object(f->auth_context, f, type, o, p);
 }
 
 int journal_file_auth_append_tag(JournalFile *f) {
@@ -132,11 +131,9 @@ int journal_file_auth_append_tag(JournalFile *f) {
         if (!journal_file_writable(f))
                 return 0;
 
-#if HAVE_GCRYPT
-        return journal_auth_append_tag(f->auth_context, f);
-#else
-        assert_not_reached();
-#endif
+        assert(auth_ops);
+        assert(auth_ops->append_tag);
+        return auth_ops->append_tag(f->auth_context, f);
 }
 
 int journal_file_auth_append_tag_first(JournalFile *f) {
@@ -148,11 +145,9 @@ int journal_file_auth_append_tag_first(JournalFile *f) {
         if (!journal_file_writable(f))
                 return 0;
 
-#if HAVE_GCRYPT
-        return journal_auth_append_tag_first(f->auth_context, f);
-#else
-        assert_not_reached();
-#endif
+        assert(auth_ops);
+        assert(auth_ops->append_tag_first);
+        return auth_ops->append_tag_first(f->auth_context, f);
 }
 
 int journal_file_auth_append_tag_maybe(JournalFile *f, usec_t realtime) {
@@ -164,9 +159,7 @@ int journal_file_auth_append_tag_maybe(JournalFile *f, usec_t realtime) {
         if (!journal_file_writable(f))
                 return 0;
 
-#if HAVE_GCRYPT
-        return journal_auth_append_tag_maybe(f->auth_context, f, realtime);
-#else
-        assert_not_reached();
-#endif
+        assert(auth_ops);
+        assert(auth_ops->append_tag_maybe);
+        return auth_ops->append_tag_maybe(f->auth_context, f, realtime);
 }
