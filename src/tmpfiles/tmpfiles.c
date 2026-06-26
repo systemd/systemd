3243fd71f644e68d8e2a887dd997abc262b12684
Simon de Vlieger <cmdr@supakeen.com>
2026-06-26 10:14:11 +0200

    many: move `mac_init` to after mount
    Move potential MAC initialization to *after* the image mount blocks so
    that `arg_root` is set before MAC labels are read. This is intended to
    not be a functional change. The next commit will use the now available
    `arg_root` to switch over to the alternate-root aware SELinux paths when
    set.
    
    Signed-off-by: Simon de Vlieger <cmdr@supakeen.com>

--- src/tmpfiles/tmpfiles.c
+++ src/tmpfiles/tmpfiles.c
@@ -4918,10 +4918,6 @@ static int run(int argc, char *argv[]) {
 
         umask(0022);
 
-        r = mac_init();
-        if (r < 0)
-                return r;
-
         if (arg_image) {
                 assert(!arg_root);
 
@@ -4945,6 +4941,10 @@ static int run(int argc, char *argv[]) {
                 if (!arg_root)
                         return log_oom();
         }
+
+        r = mac_init();
+        if (r < 0)
+                return r;
 
         c.items = ordered_hashmap_new(&item_array_hash_ops);
         c.globs = ordered_hashmap_new(&item_array_hash_ops);
