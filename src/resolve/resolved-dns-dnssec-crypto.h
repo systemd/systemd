13be55350746554a1b5a0f26c5c5c21e7e538087
Yu Watanabe <watanabe.yu+github@gmail.com>
2026-06-30 20:16:00 +0900

    resolve: make dnssec_ecdsa_verify_raw() take struct iovec
    This also
    - adds missing assertions,
    - moves variable declarations,
    - rename variables.
    
    No functional change, just refactoring.

--- src/resolve/resolved-dns-dnssec-crypto.h
+++ src/resolve/resolved-dns-dnssec-crypto.h
@@ -16,9 +16,9 @@ int dnssec_rsa_verify_raw(
 int dnssec_ecdsa_verify_raw(
                 const EVP_MD *hash_algorithm,
                 int curve,
-                const void *signature_r, size_t signature_r_size,
-                const void *signature_s, size_t signature_s_size,
-                const void *data, size_t data_size,
-                const void *key, size_t key_size);
+                const struct iovec *signature_r,
+                const struct iovec *signature_s,
+                const struct iovec *hash,
+                const struct iovec *key);
 
 #endif
