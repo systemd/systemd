8de5c5edac9e471b8d7d102cbbd7a1dba45095dd
Emanuele Rocca <emanuele.rocca@arm.com>
2026-07-06 14:13:31 +0200

    include: add glibc hwcaps introduced after v2.34
    Signed-off-by: Emanuele Rocca <emanuele.rocca@arm.com>

---
+++ src/include/glibc/elf.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#pragma once
+
+#include_next <elf.h> /* IWYU pragma: export */
+
+/* AT_HWCAP3/4 were added in glibc 2.39. */
+#ifndef AT_HWCAP3
+#define AT_HWCAP3 29
+#endif
+#ifndef AT_HWCAP4
+#define AT_HWCAP4 30
+#endif
