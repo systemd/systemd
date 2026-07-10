d298f9bc8e735ad14eef6c989d50c21a241bbe55
Lennart Poettering <lennart@amutable.com>
2026-07-10 14:34:59 +0200

    sysupdate: generalize feature name validity check
    Let's switch to string_is_safe(), and make this available to the rest of
    the sysupdate sources too.
    
    This both relaxes and tighten the rules slightly. i.e. control character
    and stuff are no longer allowed, but valid UTF-8 (as opposed to ASCII)
    now is.

--- src/sysupdate/sysupdate-util.h
+++ src/sysupdate/sysupdate-util.h
@@ -8,5 +8,7 @@ int reboot_now(void);
 #define SD_SYSUPDATE_OFFLINE   (UINT64_C(1) << 0)
 #define SD_SYSUPDATE_FLAGS_ALL (SD_SYSUPDATE_OFFLINE)
 
+bool feature_name_valid(const char *c);
+
 bool component_name_valid(const char *c);
 int get_component_list(const char *root, char ***ret);
