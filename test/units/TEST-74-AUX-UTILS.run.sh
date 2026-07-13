176e9621952f79b0010f1e75d77dea260649cf92
dongshengyuan <545258830@qq.com>
2026-07-13 14:16:55 +0800

    run: reject JSON output for trigger units
    Reproducer:
      unit=run-json-trigger-$(date +%s)
      sudo systemd-run --json=short --unit="$unit" \
          --on-active=30s \
          /bin/true
    
    Before, trigger mode accepted --json=short but printed only human-readable
    "Running timer as unit" and "Will run service" lines. Reject the option
    until trigger mode has structured output.
    
    Follow-up for fe5a6c47af675bc0020c545d86fb103492e1d77c
    
    (cherry picked from commit 3fa641d01fc559a4d1c73dcc72bdc7ebe9d08200)

--- test/units/TEST-74-AUX-UTILS.run.sh
+++ test/units/TEST-74-AUX-UTILS.run.sh
@@ -253,6 +253,7 @@ systemd-run --wait --machine=.host --pty true
 systemd-run --json=short true | jq . >/dev/null
 systemd-run --json=pretty true | jq . >/dev/null
 (! systemd-run --wait --pipe --json=short true)
+(! systemd-run --json=short --on-active=30s true)
 (! SHELL=/bin/false systemd-run --quiet --shell)
 
 (! systemd-run)
