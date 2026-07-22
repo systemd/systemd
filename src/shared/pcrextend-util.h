30729d74ca9dd702da2aa239226d1b31d8e96e11
Paul Meyer <katexochen0@gmail.com>
2026-07-22 08:28:53 +0200

    cryptsetup: measure via the pcrextend varlink service
    Measure the volume key and unlock keyslot through io.systemd.PCRExtend
    instead of driving the TPM directly via tpm2-util, matching how the
    verity and imds measurements already work.
    
    Bank selection and the TPM context now live entirely in
    systemd-pcrextend. As a result the tpm2-measure-bank= crypttab option
    can no longer be honored per volume and is now a deprecated no-op.
    
    Signed-off-by: Paul Meyer <katexochen0@gmail.com>

--- src/shared/pcrextend-util.h
+++ src/shared/pcrextend-util.h
@@ -13,3 +13,6 @@ int pcrextend_login_word(UserRecord *ur, char **ret);
 
 int pcrextend_verity_now(const char *name, const struct iovec *root_hash, const struct iovec *root_hash_sig);
 int pcrextend_imds_userdata_now(const struct iovec *data);
+
+int pcrextend_volume_key_now(unsigned pcr, const char *word, const struct iovec *volume_key);
+int pcrextend_keyslot_now(const char *nvpcr, const char *word);
