8d51ac25e6afddbdd7ed72073ce73eaeddf6be02
Chris Coulson <chris.coulson@amutable.com>
2026-07-24 23:43:04 +0100

    Add a new service for acquiring RFC 3161 timestamps over varlink
    This adds a new systemd-timestampd service which provides a varlink
    interface that can be used to acquire RFC 3161 timestamps from a
    Time-Stamp Authority (TSA).
    
    TImestamps are useful for things such as providing evidence of report
    freshness and attesting the creation time of TPM keys. It's a varlink
    interface because there are potentially multiple systemd services that
    may find this useful in the future (and there may be some users outside
    of systemd too).
    
    When requesting a timestamp, a client either supplies a pre-computed
    digest, or a file descriptor to a file so that its contents can be
    digested. A TimeStampReq is constructed, containing the digest as the
    message imprint, an optional nonce, and an option that specifies whether
    the response should include the TSA's certificate.
    
    The TSA responds with a TimeStampResp, containing a status code and the
    timestamp token if the request succeeded. The token is returned to the
    client as a PEM encoded PKCS7 SignedData structure. No other verification
    or interpretation of the token is performed - that is left to the relying
    party to do.
    
    The service can be configured with a default TSA. Alternatively, clients
    can specify a TSA to request a timestamp from.
    
    RFC 3161 documents several different transports. This implements both
    the HTTP and socket based transports.
    
    Example usage is:
    
    $ varlinkctl call /run/systemd/io.systemd.Timestamp io.systemd.Timestamp.Request \
      '{"digest":"9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08","hashAlgorithm":"SHA256","authority":"https://freetsa.org/tsr"}'
    
    https://www.rfc-editor.org/info/rfc3161/

---
+++ src/timestamp/timestampd.h
@@ -0,0 +1,35 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#pragma once
+
+#include "forward.h"
+
+/* The prefix shared by all our Varlink error IDs. */
+#define TIMESTAMP_ERROR(id) "io.systemd.Timestamp." id
+
+typedef struct Manager Manager;
+typedef struct TimestampRequest TimestampRequest;
+
+struct Manager {
+        sd_event *event;
+        sd_varlink_server *varlink_server;
+
+        /* Lazily allocated on first use of the respective transport. */
+        CurlGlue *curl_glue;
+        sd_resolve *resolve;
+
+        /* Configuration (from timestampd.conf) */
+        char *authority;                /* default Time-Stamp Authority URL */
+        usec_t connection_timeout_usec;
+        uint64_t data_size_max;         /* how much data we are willing to digest, UINT64_MAX for no limit */
+
+        /* All in-flight TimestampRequest objects. */
+        Set *requests;
+};
+
+int manager_new(Manager **ret);
+Manager* manager_free(Manager *m);
+DEFINE_TRIVIAL_CLEANUP_FUNC(Manager*, manager_free);
+
+int manager_parse_config(Manager *m);
+
+const struct ConfigPerfItem* timestampd_gperf_lookup(const char *str, GPERF_LEN_TYPE length);
