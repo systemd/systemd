5ee9bca9cfdaa6b2a9c90e0a48e53cf5b43da2f0
Nick Rosbrook <enr0n@ubuntu.com>
2026-07-27 12:03:03 -0400

    resolve: introduce link_set_dns_servers() helper
    This will be used again in the varlink implementation.

--- src/resolve/resolved-link.h
+++ src/resolve/resolved-link.h
@@ -117,3 +117,5 @@ void link_address_add_rrs(LinkAddress *a, bool force_remove);
 bool link_negative_trust_anchor_lookup(Link *l, const char *name);
 
 DEFINE_TRIVIAL_CLEANUP_FUNC(Link*, link_free);
+
+int link_set_dns_servers(Link *l, struct in_addr_full **dns, size_t n_dns, ResolveConfigSource source);
