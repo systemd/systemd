fbddc44509057e5fa8e0a67f6f1ee9708910b609
Nick Rosbrook <enr0n@ubuntu.com>
2026-07-31 14:33:00 -0400

    network: introduce io.systemd.Link.SetMulticastDNS
    This is part of the effort to move resolvectl completely to varlink. For
    link configuration methods, resolvectl makes calls to networkd if the
    given link is managed by networkd, so networkd and resolved need to
    implement the same methods.

--- src/network/networkd-manager-varlink.c
+++ src/network/networkd-manager-varlink.c
@@ -332,6 +332,7 @@ int manager_varlink_init(Manager *m, int fd) {
                         "io.systemd.Network.Link.SetDomains",         vl_method_link_set_domains,
                         "io.systemd.Network.Link.SetDNSDefaultRoute", vl_method_link_set_dns_default_route,
                         "io.systemd.Network.Link.SetLLMNR",           vl_method_link_set_llmnr,
+                        "io.systemd.Network.Link.SetMulticastDNS",    vl_method_link_set_mdns,
                         "io.systemd.service.Ping",                    varlink_method_ping,
                         "io.systemd.service.Reload",                  vl_method_reload,
                         "io.systemd.service.SetLogLevel",             varlink_method_set_log_level,
