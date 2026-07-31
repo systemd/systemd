77227cf3e457b2721df0dab8f8baf15a7ec16bf1
Nick Rosbrook <enr0n@ubuntu.com>
2026-07-31 14:06:17 -0400

    network: introduce io.systemd.Network.Link.SetLLMNR
    This is part of the effort to move resolvectl completely to varlink. For
    link configuration methods, resolvectl makes calls to networkd if the
    given link is managed by networkd, so networkd and resolved need to
    implement the same methods.

--- src/network/networkd-link-varlink.h
+++ src/network/networkd-link-varlink.h
@@ -21,3 +21,4 @@ int vl_method_link_reconfigure(sd_varlink *vlink, sd_json_variant *parameters, s
 int vl_method_link_set_dns(sd_varlink *vlink, sd_json_variant *parameters, sd_varlink_method_flags_t flags, void *userdata);
 int vl_method_link_set_domains(sd_varlink *vlink, sd_json_variant *parameters, sd_varlink_method_flags_t flags, void *userdata);
 int vl_method_link_set_dns_default_route(sd_varlink *vlink, sd_json_variant *parameters, sd_varlink_method_flags_t flags, void *userdata);
+int vl_method_link_set_llmnr(sd_varlink *vlink, sd_json_variant *parameters, sd_varlink_method_flags_t flags, void *userdata);
