a072da35d3097d29a6db41497d190b24c3a27fa8
ShiroKSH <kushidashiro@gmail.com>
2026-08-02 13:14:05 +0300

    test: take owned varlink ref after add_connection

--- src/libsystemd/sd-varlink/test-varlink.c
+++ src/libsystemd/sd-varlink/test-varlink.c
@@ -348,7 +348,7 @@ static int block_fd_handler(sd_event_source *s, int fd, uint32_t revents, void *
 
 TEST(add_connection_failure_cleanup) {
         _cleanup_(sd_varlink_server_unrefp) sd_varlink_server *s = NULL;
-        _cleanup_(sd_varlink_close_unrefp) sd_varlink *v = NULL;
+        _cleanup_(sd_varlink_unrefp) sd_varlink *v = NULL;
         _cleanup_close_pair_ int fds[2] = EBADF_PAIR;
         const struct ucred ucred = {
                 .pid = getpid(),
@@ -367,6 +367,8 @@ TEST(add_connection_failure_cleanup) {
         ASSERT_EQ(sd_varlink_server_current_connections(s), 0U);
 
         ASSERT_OK(sd_varlink_server_add_connection(s, TAKE_FD(fds[0]), &v));
+        v = sd_varlink_ref(v);
+        ASSERT_OK(sd_varlink_close(v));
 }
 
 TEST(chat) {
