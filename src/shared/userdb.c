6df7cc40e6749b8a4fb66267b611310f189b190f
dongshengyuan <545258830@qq.com>
2026-08-04 09:58:17 +0800

    userdb: apply UUID filters to group records
    group_record_match() handled the other filters, but ignored match->uuid,
    so userdbctl group --uuid=... could still return non-matching groups.
    
    Add the same UUID check used by user_record_match().
    
    Reproduced locally:
    build/userdbctl --uuid=aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa \
      --output=json group root
    
    Before result:
    The command succeeded and printed the root group record.
    
    Follow-up for: 466562c69b75cec197176f556b940a43bb8350f2
    
    (cherry picked from commit 0b7c68eb3aa797be17112bea6fb639455a40236b)
    (cherry picked from commit e2bb7a258aed70eb47efe3cc26a9bab65e9f34f2)
    (cherry picked from commit fc0c259b6326a47bb7befafc3359d2f515d1f57c)

--- src/shared/userdb.c
+++ src/shared/userdb.c
@@ -192,6 +192,7 @@ static int userdb_maybe_restart_query(
         static const char *const fields[] = {
                 "fuzzyNames",
                 "dispositionMask",
+                "uuid",
                 "uidMin",
                 "uidMax",
                 "gidMin",
