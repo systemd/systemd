252c01097e3d2c974ac73cc1fe48c1a2ee3da403
Yu Watanabe <watanabe.yu+github@gmail.com>
2026-08-06 04:45:13 +0900

    coredump: add --check-pidfd-features option
    This will be used for checking if the kernel coredump socket feature
    is supported.

--- src/coredump/coredump.c
+++ src/coredump/coredump.c
@@ -7,8 +7,10 @@
 #include "coredump-receive.h"
 #include "coredump-util.h"
 #include "dlopen-note.h"
+#include "errno-util.h"
 #include "log.h"
 #include "main-func.h"
+#include "pidfd-util.h"
 #include "string-util.h"
 
 static int run(int argc, char *argv[]) {
@@ -19,6 +21,29 @@ static int run(int argc, char *argv[]) {
         LIBDW_NOTE(suggested);
         LIBELF_NOTE(suggested);
         LIBSELINUX_NOTE(recommended);
+
+        if (streq_ptr(argv[1], "--check-pidfd-features")) {
+                /* This checks the following flags are supported by the running kernel:
+                 * PIDFD_INFO_COREDUMP        : 1d8db6fd698de1f73b1a7d72aea578fdd18d9a87 (v6.16),
+                 * PIDFD_INFO_COREDUMP_SIGNAL : 036375522be8425874e9e0f907c7127e315c7a52 (v6.19).
+                 *
+                 * These flags are required for using the kernel coredump socket feature. Note that the
+                 * request mode coredump socket pattern (@@ prefixed) is supported since kernel v6.17. Old
+                 * kernels do not refuse the new core patterns (moreover, any strings are accepted), hence we
+                 * need to check kernel version in some ways other than reading/writing core patterns. This
+                 * method can be also used for checking that. If the required flags are supported, we assume
+                 * the kernel is new enough, and the kernel coredump socket is also supported. */
+
+                r = pidfd_info_mask_is_supported(PIDFD_INFO_COREDUMP | PIDFD_INFO_COREDUMP_SIGNAL);
+                if (IN_SET(r, 0, -EOPNOTSUPP))
+                        return log_debug_errno(SYNTHETIC_ERRNO(EOPNOTSUPP),
+                                               "ioctl(PIDFD_GET_INFO) does not support PIDFD_INFO_COREDUMP and/or PIDFD_INFO_COREDUMP_SIGNAL.");
+                if (r < 0)
+                        return log_error_errno(r, "Failed to check if PIDFD_INFO_COREDUMP and PIDFD_INFO_COREDUMP_SIGNAL flags are supported by ioctl(PIDFD_GET_INFO): %m");
+
+                log_debug("The kernel supports both PIDFD_INFO_COREDUMP and PIDFD_INFO_COREDUMP_SIGNAL, assuming the kernel coredump socket is also supported.");
+                return 0;
+        }
 
         /* When running as backtrace mode, it is not necessary to use kmsg, not necessary to disable coredump
          * from the command, and unexpectedly passed file descriptors can be silently ignored. */
