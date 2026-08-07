9801e8589510f6bb8333d5556f45acac501d651f
Beniamino Galvani <b.galvani@gmail.com>
2026-08-07 16:48:27 +0200

    sd-dhcp6-client: implement DHCPv6 address registration (RFC 9686)
    Add support for RFC 9686 ("Registering Self-Generated IPv6 Addresses Using
    DHCPv6"), which allows clients to inform a DHCPv6 server about self-generated
    SLAAC and static IPv6 addresses. This feature can be used to track the
    addresses in use on the network for troubleshooting and forensic purposes.
    
    Clients register addresses by sending an ADDR-REG-INFORM message containing an
    "IA Address" option with the address being registered. The server replies with
    an ADDR-REG-REPLY message to confirm the request.
    
    The protocol includes a retransmission mechanism to handle potential message
    losses, and a refresh mechanism to ensure that the server always has an
    accurate view of the registered addresses and their lifetimes. At the moment,
    the coalescing of refresh messages described as optional in section 4.6.3 of
    the RFC is not implemented yet, and all refreshes are sent exactly when the
    timer expires.
    
    To avoid cluttering the DHCPv6 code, this functionality is implemented in a
    separate file, dhcp6-addr-reg.c. The protocol uses the address being registered
    as the source address for ADDR-REG-INFORM messages and as the destination
    address for ADDR-REG-REPLY messages. Since the main DHCPv6 socket is bound to
    the IPv6 link-local address, a dedicated socket is used.

--- src/libsystemd-network/sd-dhcp6-client.c
+++ src/libsystemd-network/sd-dhcp6-client.c
@@ -12,6 +12,7 @@
 #include "alloc-util.h"
 #include "device-util.h"
 #include "dhcp-duid-internal.h"
+#include "dhcp6-addr-reg.h"
 #include "dhcp6-client-internal.h"
 #include "dhcp6-internal.h"
 #include "dhcp6-lease-internal.h"
@@ -1464,6 +1465,8 @@ int sd_dhcp6_client_stop(sd_dhcp6_client *client) {
         client->receive_message = sd_event_source_unref(client->receive_message);
         client->fd = safe_close(client->fd);
 
+        dhcp6_client_addr_reg_flush(client);
+
         return 0;
 }
 
@@ -1588,6 +1591,8 @@ static sd_dhcp6_client *dhcp6_client_free(sd_dhcp6_client *client) {
 
         sd_dhcp6_lease_unref(client->lease);
 
+        dhcp6_client_addr_reg_flush(client);
+
         sd_event_source_disable_unref(client->receive_message);
         sd_event_source_disable_unref(client->timeout_resend);
         sd_event_source_disable_unref(client->timeout_expire);
@@ -1631,6 +1636,7 @@ int sd_dhcp6_client_new(sd_dhcp6_client **ret) {
                 .request_ia = DHCP6_REQUEST_IA_NA | DHCP6_REQUEST_IA_PD,
                 .fd = -EBADF,
                 .rapid_commit = true,
+                .addr_reg_fd = -EBADF,
         };
 
         *ret = TAKE_PTR(client);
