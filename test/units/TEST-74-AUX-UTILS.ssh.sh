70a0327c26f5024b8112baf1a67c4963113f37d8
Jan Macku <jamacku@redhat.com>
2026-08-07 14:50:28 +0200

    test/units: apply chmod 0700 to root/.ssh
    mkdir without -m treats 0700 as another directory name argument.
    
    Follow-up to https://github.com/systemd/systemd/commit/52d863defcd05d0b676ce743b9eaf1060131f9a1

--- test/units/TEST-74-AUX-UTILS.ssh.sh
+++ test/units/TEST-74-AUX-UTILS.ssh.sh
@@ -31,7 +31,8 @@ removesshid() {
 
 ssh-keygen -N '' -C '' -t rsa -f "$ROOTID"
 
-mkdir -p 0700 /root/.ssh
+mkdir -p /root/.ssh
+chmod 0700 /root/.ssh
 # Add a newline in case authorized_keys wasn't terminated correctly.
 echo >>/root/.ssh/authorized_keys
 cat "$ROOTID".pub >>/root/.ssh/authorized_keys
