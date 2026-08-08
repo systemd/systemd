bf60224783d3cf3593a8b9fd9d1a521695f44a30
Yu Watanabe <watanabe.yu+github@gmail.com>
2026-08-08 19:45:18 +0900

    sysctl: skip leading whitespaces and ignore arguments start with '#' when --inline
    (cherry picked from commit 1c223728cf404d7d5c1802e0d5b149b966a80e0e)

--- test/units/TEST-87-AUX-UTILS-VM.sysctl.sh
+++ test/units/TEST-87-AUX-UTILS-VM.sysctl.sh
@@ -17,7 +17,7 @@ assert_rc 0 /usr/lib/systemd/systemd-sysctl /tmp/foo.conf
 assert_rc 0 /usr/lib/systemd/systemd-sysctl --strict /tmp/foo.conf
 
 assert_rc 1 /usr/lib/systemd/systemd-sysctl --inline
-assert_rc 0 /usr/lib/systemd/systemd-sysctl --inline "foo.bar=42"
+assert_rc 0 /usr/lib/systemd/systemd-sysctl --inline "   foo.bar=42" "#comment" "   "
 assert_rc 1 /usr/lib/systemd/systemd-sysctl --inline --strict "foo.bar=42"
 assert_rc 0 /usr/lib/systemd/systemd-sysctl --inline -- "-foo.bar=42"
 assert_rc 0 /usr/lib/systemd/systemd-sysctl --inline --strict -- "-foo.bar=42"
