bf18150ee3482ee23a1febd1b277306b24542f90
Yu Watanabe <watanabe.yu+github@gmail.com>
2026-08-10 05:56:07 +0900

    test: support the new coredump interface

--- test/units/TEST-87-AUX-UTILS-VM.coredump.sh
+++ test/units/TEST-87-AUX-UTILS-VM.coredump.sh
@@ -26,7 +26,11 @@ journalctl --sync
 journalctl --rotate
 
 # Check that we're the ones to receive coredumps
-sysctl kernel.core_pattern | grep systemd-coredump
+if systemctl is-active systemd-coredumpd.service; then
+    sysctl kernel.core_pattern | grep -F '@@/run/systemd/coredump-kernel'
+else
+    sysctl kernel.core_pattern | grep -F 'systemd-coredump'
+fi
 
 # Prepare "fake" binaries for coredumps, so we can properly exercise
 # the matching stuff too
@@ -70,11 +74,17 @@ chmod +x "$MAKE_DUMP_SCRIPT"
 # with Storage=external as well
 mkdir -p /run/systemd/coredump.conf.d/
 printf '[Coredump]\nStorage=external' >/run/systemd/coredump.conf.d/99-external.conf
+if systemctl is-active systemd-coredumpd.service; then
+    systemctl reload systemd-coredumpd.service
+fi
 "$MAKE_DUMP_SCRIPT" "$CORE_TEST_BIN" "SIGTRAP"
 "$MAKE_DUMP_SCRIPT" "$CORE_TEST_BIN" "SIGABRT"
-rm -fv /run/systemd/coredump.conf.d/99-external.conf
 # Wait a bit for the coredumps to get processed
 timeout 30 bash -c "while [[ \$(coredumpctl list -q --no-legend $CORE_TEST_BIN | wc -l) -lt 4 ]]; do sleep 1; done"
+rm -fv /run/systemd/coredump.conf.d/99-external.conf
+if systemctl is-active systemd-coredumpd.service; then
+    systemctl reload systemd-coredumpd.service
+fi
 
 if cgroupfs_supports_user_xattrs; then
     # Make sure we can forward crashes back to containers
@@ -192,11 +202,17 @@ UNPRIV_CMD=(systemd-run --user --wait --pipe -M "testuser@.host" -E SYSTEMD_PAGE
 # with Storage=external as well
 mkdir -p /run/systemd/coredump.conf.d/
 printf '[Coredump]\nStorage=external' >/run/systemd/coredump.conf.d/99-external.conf
+if systemctl is-active systemd-coredumpd.service; then
+    systemctl reload systemd-coredumpd.service
+fi
 "${UNPRIV_CMD[@]}" "$MAKE_DUMP_SCRIPT" "$CORE_TEST_UNPRIV_BIN" "SIGTRAP"
 "${UNPRIV_CMD[@]}" "$MAKE_DUMP_SCRIPT" "$CORE_TEST_UNPRIV_BIN" "SIGABRT"
-rm -fv /run/systemd/coredump.conf.d/99-external.conf
 # Wait a bit for the coredumps to get processed
 timeout 30 bash -c "while [[ \$(coredumpctl list -q --no-legend $CORE_TEST_UNPRIV_BIN | wc -l) -lt 4 ]]; do sleep 1; done"
+rm -fv /run/systemd/coredump.conf.d/99-external.conf
+if systemctl is-active systemd-coredumpd.service; then
+    systemctl reload systemd-coredumpd.service
+fi
 
 # Sync and rotate journal again to make the coredump stored in an archived journal.
 journalctl --sync
@@ -317,6 +333,9 @@ EOF
 
     mkdir -p /run/systemd/coredump.conf.d/
     printf '[Coredump]\nEnterNamespace=no' >/run/systemd/coredump.conf.d/99-enter-namespace.conf
+    if systemctl is-active systemd-coredumpd.service; then
+        systemctl reload systemd-coredumpd.service
+    fi
 
     unshare --pid --fork --mount-proc --mount --uts --ipc --net "$MAKE_STACKTRACE_DUMP" "test-stacktrace-not-symbolized"
     timeout 30 bash -c "until coredumpctl list -q --no-legend /tmp/test-stacktrace-not-symbolized; do sleep .2; done"
@@ -327,6 +346,9 @@ EOF
     (! grep -E "#[0-9]+ .* baz " /tmp/not-symbolized.log)
 
     printf '[Coredump]\nEnterNamespace=yes' >/run/systemd/coredump.conf.d/99-enter-namespace.conf
+    if systemctl is-active systemd-coredumpd.service; then
+        systemctl reload systemd-coredumpd.service
+    fi
     unshare --pid --fork --mount-proc --mount --uts --ipc --net "$MAKE_STACKTRACE_DUMP" "test-stacktrace-symbolized"
     timeout 30 bash -c "until coredumpctl list -q --no-legend /tmp/test-stacktrace-symbolized; do sleep .2; done"
     coredumpctl info /tmp/test-stacktrace-symbolized | tee /tmp/symbolized.log
@@ -337,6 +359,9 @@ EOF
 
     test -d /usr/lib/debug/ && umount /usr/lib/debug/
     rm -f "$MAKE_STACKTRACE_DUMP" /run/systemd/coredump.conf.d/99-enter-namespace.conf /tmp/{not-,}symbolized.log
+    if systemctl is-active systemd-coredumpd.service; then
+        systemctl reload systemd-coredumpd.service
+    fi
 else
     echo "libdw doesn't not support setting sysroot, skipping EnterNamespace= test"
 fi
