7b30fe10f081c19c7fe24c6724dcdcfdd416b119
Yu Watanabe <watanabe.yu+github@gmail.com>
2026-08-10 05:25:32 +0900

    coredump: support kernel coredump socket protocol
    Since kernel v6.17, we can set a path to unix socket to core_pattern
    (with request mode). See also:
    https://lore.kernel.org/all/20250603-work-coredump-socket-protocol-v2-0-05a5f0c18ecc@kernel.org/
    
    This introduces a tiny service that prepares and listens on a UNIX
    socket to be passed to the kernel core pattern. When a process is
    crashed, the kernel connects to the socket, and sends information
    about the crashed process and passes coredump through the socket.
    The service accepts the connection, and forks a worker process that
    receives coredump from the kernel.

--- src/coredump/coredump-context.c
+++ src/coredump/coredump-context.c
@@ -1,5 +1,7 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 
+#include <sched.h>
+
 #include "sd-login.h"
 #include "sd-messages.h"
 
@@ -8,6 +10,7 @@
 #include "dirent-util.h"
 #include "fd-util.h"
 #include "fs-util.h"
+#include "hostname-setup.h"
 #include "hostname-util.h"
 #include "iovec-wrapper.h"
 #include "log.h"
@@ -16,7 +19,9 @@
 #include "parse-util.h"
 #include "pidfd-util.h"
 #include "process-util.h"
+#include "rlimit-util.h"
 #include "signal-util.h"
+#include "socket-util.h"
 #include "special.h"
 #include "string-table.h"
 #include "string-util.h"
@@ -645,6 +650,114 @@ int coredump_context_parse_from_argv(CoredumpContext *context, int argc, char **
                 if (r < 0)
                         return r;
         }
+
+        coredump_context_check_pidns(context);
+        return coredump_context_parse_from_procfs(context);
+}
+
+static int get_crashing_tid(pid_t pid, PidRef *ret) {
+        int r;
+
+        assert(pid > 0);
+        assert(ret);
+
+        /* Read /proc/[PID]/task/[TID]/stat then find a thread that has the PF_DUMPCORE flag. */
+
+        const char *p = procfs_file_alloca(pid, "task");
+        _cleanup_closedir_ DIR *d = opendir(p);
+        if (!d)
+                return errno == ENOENT ? -ESRCH : -errno;
+
+        FOREACH_DIRENT_ALL(de, d, return -errno) {
+                if (!in_charset(de->d_name, DIGITS))
+                        continue;
+
+                _cleanup_free_ char *q = path_join(p, de->d_name, "stat");
+                if (!q)
+                        return -ENOMEM;
+
+                unsigned long long flags;
+                r = procfs_get_stat_flags(q, &flags);
+                if (r == -ENOMEM)
+                        return r;
+                if (r < 0)
+                        continue;
+
+                if (!FLAGS_SET(flags, PF_DUMPCORE))
+                        continue;
+
+                return pidref_set_pidstr_full(ret, de->d_name, PIDFD_THREAD);
+        }
+
+        return -ESRCH;
+}
+
+int coredump_context_parse_from_peer(CoredumpContext *context) {
+        int r;
+
+        assert(context);
+        assert(context->input_fd >= 0);
+
+        r = getpeerpidref(context->input_fd, &context->pidref);
+        if (r < 0)
+                return log_error_errno(r, "Failed to get peer pidref: %m");
+
+        if (context->pidref.fd < 0)
+                return log_error_errno(SYNTHETIC_ERRNO(ENOMEDIUM), "We do not have pidfd of the crashed process.");
+
+        context->got_pidfd = true;
+
+        struct pidfd_info info = {
+                .mask = PIDFD_INFO_COREDUMP,
+        };
+
+        r = pidfd_get_info(context->pidref.fd, &info);
+        if (r < 0)
+                return log_error_errno(r, "ioctl(PIDFD_GET_INFO) failed: %m");
+
+        if (!FLAGS_SET(info.mask, PIDFD_INFO_COREDUMP | PIDFD_INFO_COREDUMP_SIGNAL))
+                return log_error_errno(SYNTHETIC_ERRNO(ENODATA),
+                                       "Failed to get coredump information by ioctl(PIDFD_GET_INFO).");
+
+        if (!FLAGS_SET(info.coredump_mask, PIDFD_COREDUMPED))
+                return log_error_errno(SYNTHETIC_ERRNO(EINVAL),
+                                       "The process ["PID_FMT"] did not crash.", context->pidref.pid);
+
+        if (FLAGS_SET(info.coredump_mask, PIDFD_COREDUMP_SKIP))
+                return log_error_errno(SYNTHETIC_ERRNO(ENODATA),
+                                       "Coredump generation for process ["PID_FMT"] is skipped.", context->pidref.pid);
+
+        context->uid = info.ruid;
+        context->gid = info.rgid;
+        context->dumpable = FLAGS_SET(info.coredump_mask, PIDFD_COREDUMP_USER) ? SUID_DUMP_USER : SUID_DUMP_SAFE;
+        context->signo = info.coredump_signal;
+
+        /* This is relatively new (since kernel v7.1), thus optional. */
+        if (FLAGS_SET(info.mask, PIDFD_INFO_COREDUMP_CODE)) {
+                context->code = (int) info.coredump_code;
+                context->got_code = true;
+        } else
+                context->got_code = false;
+
+        r = get_crashing_tid(context->pidref.pid, &context->tidref);
+        if (r < 0)
+                log_warning_errno(r, "Failed to get crashing TID of process ["PID_FMT"], ignoring: %m", context->pidref.pid);
+
+        struct rlimit rl;
+        r = pid_getrlimit(context->pidref.pid, RLIMIT_CORE, &rl);
+        if (r < 0)
+                log_warning_errno(r, "Failed to get coredump size limit of process ["PID_FMT"], ignoring: %m", context->pidref.pid);
+        else
+                context->rlimit = rl.rlim_cur;
+
+        _cleanup_free_ char *h = NULL;
+        r = pidref_gethostname_full(&context->pidref, GET_HOSTNAME_ALLOW_LOCALHOST | GET_HOSTNAME_FALLBACK_DEFAULT, &h);
+        if (r < 0)
+                log_warning_errno(r, "Failed to get hostname, ignoring: %m");
+        else if (!hostname_is_valid(h, /* flags= */ 0))
+                log_warning("Obtained an invalid hostname, ignoring: %s", h);
+        else
+                context->hostname = TAKE_PTR(h);
 
         coredump_context_check_pidns(context);
         return coredump_context_parse_from_procfs(context);
