4f3595b9e274924652a12f945979002bca537fd9
Simon de Vlieger <cmdr@supakeen.com>
2026-08-09 20:44:54 +0200

    test: sysext IMAGE_ID/IMAGE_VERSION
    The DDI test (TEST-50-DISSECT.DDI.sh) included IMAGE_ID=waldo and
    IMAGE_VERSION=7 in its extension-release files as extra metadata.
    These fields were originally unused, extension_release_validate()
    did not check them. Now it does so adjust the `os-release` to make the
    test case match.
    
    Also add new tests that cover the matching of IMAGE_ID/IMAGE_VERSION
    separately.

--- test/units/TEST-50-DISSECT.DDI.sh
+++ test/units/TEST-50-DISSECT.DDI.sh
@@ -17,6 +17,13 @@ fi
 openssl req -config "$OPENSSL_CONFIG" -subj="/CN=waldo" \
             -x509 -sha256 -nodes -days 365 -newkey rsa:4096 \
             -keyout /tmp/test-50-privkey.key -out /tmp/test-50-cert.crt
+
+# Add IMAGE_ID and IMAGE_VERSION to host os-release so extensions declaring
+# these fields pass validation.
+cp /usr/lib/os-release /usr/lib/os-release.bak
+trap "mv /usr/lib/os-release.bak /usr/lib/os-release" EXIT
+echo -e "IMAGE_ID=waldo\nIMAGE_VERSION=7" >>/usr/lib/os-release
+
 mkdir -p /tmp/test-50-confext/etc/extension-release.d/
 echo "foobar50" >/tmp/test-50-confext/etc/waldo
 {
