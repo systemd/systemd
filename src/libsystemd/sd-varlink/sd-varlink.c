c3409deb483532470af5e358e21718fffb835a63
ShiroKSH <kushidashiro@gmail.com>
2026-08-12 19:10:33 +0000

    sd-varlink: decrement connection counters on failure
    Previously, sd_varlink_server_add_connection_pair() did not decrement
    the connection counters when json_stream_attach_fds() failed.
    
    Set the ucred before calling json_stream_attach_fds() so that
    sd_varlink_close() also decrements the counters when it fails.

--- src/libsystemd/sd-varlink/sd-varlink.c
+++ src/libsystemd/sd-varlink/sd-varlink.c
@@ -3438,9 +3438,14 @@ _public_ int sd_varlink_server_add_connection_pair(
         v->server = sd_varlink_server_ref(server);
         sd_varlink_ref(v);
 
+        if (ucred_acquired)
+                json_stream_set_peer_ucred(&v->stream, &ucred);
+
         r = json_stream_attach_fds(&v->stream, input_fd, output_fd);
-        if (r < 0)
+        if (r < 0) {
+                sd_varlink_close(v);
                 return r;
+        }
 
         if (server->flags & SD_VARLINK_SERVER_INHERIT_USERDATA)
                 v->userdata = server->userdata;
@@ -3449,9 +3454,6 @@ _public_ int sd_varlink_server_add_connection_pair(
          * byte-bounded reads so we don't accidentally consume post-upgrade bytes. */
         if (FLAGS_SET(server->flags, SD_VARLINK_SERVER_UPGRADABLE))
                 json_stream_set_flags(&v->stream, JSON_STREAM_BOUNDED_READS, true);
-
-        if (ucred_acquired)
-                json_stream_set_peer_ucred(&v->stream, &ucred);
 
         _cleanup_free_ char *desc = NULL;
         if (asprintf(&desc, "%s-%i-%i", varlink_server_description(server), input_fd, output_fd) >= 0)
