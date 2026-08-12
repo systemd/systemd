0bd9ef9e6b1a149c0b818177b00ba05df468a373
Yu Watanabe <watanabe.yu+github@gmail.com>
2026-08-13 05:54:35 +0900

    sd-varlink: fix memleak on failure path
    (cherry picked from commit 3e96a99bab9d62faaa240a607390832964ca0c6e)
    (cherry picked from commit 9abc72a8e5c5671effd2306ae28592b0feb51985)

--- src/libsystemd/sd-varlink/sd-varlink.c
+++ src/libsystemd/sd-varlink/sd-varlink.c
@@ -286,7 +286,7 @@ _public_ int sd_varlink_connect_exec(sd_varlink **ret, const char *_command, cha
 
         pair[1] = safe_close(pair[1]);
 
-        sd_varlink *v;
+        _cleanup_(sd_varlink_unrefp) sd_varlink *v = NULL;
         r = varlink_new(&v);
         if (r < 0)
                 return log_debug_errno(r, "Failed to create varlink object: %m");
@@ -296,7 +296,7 @@ _public_ int sd_varlink_connect_exec(sd_varlink **ret, const char *_command, cha
         v->exec_pidref = TAKE_PIDREF(pidref);
         varlink_set_state(v, VARLINK_IDLE_CLIENT);
 
-        *ret = v;
+        *ret = TAKE_PTR(v);
         return 0;
 }
 
@@ -370,7 +370,7 @@ static int varlink_connect_ssh_unix(sd_varlink **ret, const char *where) {
 
         pair[1] = safe_close(pair[1]);
 
-        sd_varlink *v;
+        _cleanup_(sd_varlink_unrefp) sd_varlink *v = NULL;
         r = varlink_new(&v);
         if (r < 0)
                 return log_debug_errno(r, "Failed to create varlink object: %m");
@@ -380,7 +380,7 @@ static int varlink_connect_ssh_unix(sd_varlink **ret, const char *where) {
         v->exec_pidref = TAKE_PIDREF(pidref);
         varlink_set_state(v, VARLINK_IDLE_CLIENT);
 
-        *ret = v;
+        *ret = TAKE_PTR(v);
         return 0;
 }
 
@@ -462,7 +462,7 @@ static int varlink_connect_ssh_exec(sd_varlink **ret, const char *where) {
         if (r < 0)
                 return log_debug_errno(r, "Failed to make output pipe non-blocking: %m");
 
-        sd_varlink *v;
+        _cleanup_(sd_varlink_unrefp) sd_varlink *v = NULL;
         r = varlink_new(&v);
         if (r < 0)
                 return log_debug_errno(r, "Failed to create varlink object: %m");
@@ -473,7 +473,7 @@ static int varlink_connect_ssh_exec(sd_varlink **ret, const char *where) {
         v->exec_pidref = TAKE_PIDREF(pidref);
         varlink_set_state(v, VARLINK_IDLE_CLIENT);
 
-        *ret = v;
+        *ret = TAKE_PTR(v);
         return 0;
 }
 
