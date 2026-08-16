32eefc338d5cf18f68414b18b2ec7e5bdf32e712
Zbigniew Jędrzejewski-Szmek <zbyszek@amutable.com>
2026-08-16 14:36:42 +0200

    modules-load: convert to VERB_COMMAND and add --introspect-cli
    The custom help() is replaced by a COMMAND() description, and
    --introspect-cli is added. The abstract is changed to use the imperative
    form ("Load …" instead of "Loads …"), consistent with the other
    commands.

--- test/units/TEST-74-AUX-UTILS.introspect-cli.sh
+++ test/units/TEST-74-AUX-UTILS.introspect-cli.sh
@@ -37,6 +37,7 @@ INTROSPECTABLE=(
     systemd-journal-gatewayd
     systemd-journal-remote
     systemd-journal-upload
+    systemd-modules-load
     systemd-mute-console
     systemd-notify
     systemd-oomd
