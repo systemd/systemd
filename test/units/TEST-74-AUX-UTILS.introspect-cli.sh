80fd0f5c2ad289f69e978e074222c1d84e4d5e8c
Zbigniew Jędrzejewski-Szmek <zbyszek@amutable.com>
2026-08-16 16:52:16 +0200

    TEST-74-AUX-UTILS.introspect-cli: verify that --help works
    This is redundant for public programs, for which we test --help
    more extensively, but is also good as a basic check that e.g. the
    program name used to print help matches what is specified in COMMAND.

--- test/units/TEST-74-AUX-UTILS.introspect-cli.sh
+++ test/units/TEST-74-AUX-UTILS.introspect-cli.sh
@@ -7,6 +7,8 @@ set -o pipefail
 # Check the other location too.
 PATH=$PATH:/usr/lib/systemd
 
+export SYSTEMD_PAGER=cat
+
 # A smoke test for the introspection code
 INTROSPECTABLE=(
     homectl
@@ -44,6 +46,8 @@ INTROSPECTABLE=(
 
 for i in "${INTROSPECTABLE[@]}"; do
     command -v "$i" >/dev/null || continue
+
+    $i --help >/dev/null
 
     $i --introspect-cli | jq -e \
         '.mediaType == "application/vnd.io.systemd.cli-introspection-0"'
