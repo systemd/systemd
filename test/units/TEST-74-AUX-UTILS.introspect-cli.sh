9f402dcce52096e84ff0e8c5a0c33e7906e3e67f
Zbigniew Jędrzejewski-Szmek <zbyszek@amutable.com>
2026-08-16 14:40:46 +0200

    tpm2-clear: convert to VERB_COMMAND and add --introspect-cli
    The custom help() is replaced by a COMMAND() description, and
    --introspect-cli is added. The man page reference is fixed to
    systemd-tpm2-clear.service(8), the only name that page is known by.

--- test/units/TEST-74-AUX-UTILS.introspect-cli.sh
+++ test/units/TEST-74-AUX-UTILS.introspect-cli.sh
@@ -55,6 +55,7 @@ INTROSPECTABLE=(
     systemd-storage-fs
     systemd-storagetm
     systemd-sysinstall
+    systemd-tpm2-clear
     systemd-tty-ask-password-agent
     systemd-veritysetup
     timedatectl
