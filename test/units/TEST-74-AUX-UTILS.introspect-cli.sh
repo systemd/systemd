9b293be9c026a5c2d28ac75a73065dd84a5751bc
Zbigniew Jędrzejewski-Szmek <zbyszek@amutable.com>
2026-08-16 14:41:41 +0200

    validatefs: convert to VERB_COMMAND and add --introspect-cli
    The custom help() is replaced by a COMMAND() description, and
    --introspect-cli is added. The positional argument is spelled MOUNTPOINT
    in the synopsis, like the other metavars, instead of /path/to/mountpoint,
    and the man page synopsis, which said "[DEVICE]", is fixed to match: the
    argument is required and it is a mount point.

--- test/units/TEST-74-AUX-UTILS.introspect-cli.sh
+++ test/units/TEST-74-AUX-UTILS.introspect-cli.sh
@@ -57,6 +57,7 @@ INTROSPECTABLE=(
     systemd-tpm2-setup
     systemd-tty-ask-password-agent
     systemd-update-done
+    systemd-validatefs
     systemd-veritysetup
     timedatectl
     varlinkctl
