8f53e3ac89a94632133e6811dd237b3b6fb6398c
Zbigniew Jędrzejewski-Szmek <zbyszek@amutable.com>
2026-08-17 13:32:25 +0200

    bless-boot: convert to VERB_COMMAND and add --introspect-cli
    The custom help() is replaced by a COMMAND() description, and
    --introspect-cli is added.

--- test/units/TEST-74-AUX-UTILS.introspect-cli.sh
+++ test/units/TEST-74-AUX-UTILS.introspect-cli.sh
@@ -19,6 +19,7 @@ INTROSPECTABLE=(
     systemd-backlight
     systemd-battery-check
     systemd-binfmt
+    systemd-bless-boot
     systemd-boot-check-no-failures
     systemd-bsod
     systemd-cat
