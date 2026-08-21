d061848472e8966ae1987eaace54363af4575ad4
Nick Rosbrook <enr0n@ubuntu.com>
2026-08-21 15:58:10 -0400

    shared: add varlink_{get,set}_log_level helpers to verb-log-control.c
    These are basic client helpers for io.systemd.service.{Get,Set}LogLevel.

--- src/shared/verb-log-control.h
+++ src/shared/verb-log-control.h
@@ -4,3 +4,6 @@
 #include "forward.h"
 
 int verb_log_control_common(sd_bus *bus, const char *destination, const char *verb, const char *value);
+
+int varlink_get_log_level(const char *address, int *ret_level, char **ret_level_str);
+int varlink_set_log_level(const char *address, const char *value);
