23ae030826a94e8f90cc65dcf0f068904a15dd51
Yu Watanabe <watanabe.yu+github@gmail.com>
2026-08-21 10:11:54 +0900

    coredumpctl: allow to show coredump info in json format in dump and debug command
    Previously `coredumpctl dump/debug` silently ignored --json= option.
    Let's honor the option.

--- src/coredump/coredumpctl-debugger.c
+++ src/coredump/coredumpctl-debugger.c
@@ -61,7 +61,7 @@ int verb_run_debug(int argc, char *argv[], uintptr_t _data, void *userdata) {
                 return r;
 
         if (!arg_quiet) {
-                print_info(stdout, j, false);
+                print_entry(stdout, j, /* need_space= */ false, /* table= */ NULL);
                 fputs("\n", stdout);
         }
 
