1e7a69ff8706ed5c14bdee89bf2edd5341ec9b9d
Yu Watanabe <watanabe.yu+github@gmail.com>
2026-08-23 06:46:59 +0900

    compress: use decompressor_new() at several more places

--- src/basic/compress.h
+++ src/basic/compress.h
@@ -52,8 +52,8 @@ int compressor_new(Compressor **ret, Compression type);
 int compressor_start(Compressor *c, const void *data, size_t size, void **buffer, size_t *buffer_size, size_t *buffer_allocated);
 int compressor_finish(Compressor *c, void **buffer, size_t *buffer_size, size_t *buffer_allocated);
 
-int decompressor_detect(Decompressor **ret, const void *data, size_t size);
-int decompressor_force_off(Decompressor **ret);
+int decompressor_detect(Decompressor **decompressor, const void *data, size_t size);
+int decompressor_force_off(Decompressor **decompressor);
 int decompressor_push(Decompressor *c, const void *data, size_t size, DecompressorCallback callback, void *userdata);
 
 Compression compressor_type(const Compressor *c);
