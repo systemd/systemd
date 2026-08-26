7dc79b371bb47a91fd7d88e6d9687f2bace6b31c
Christian Brauner <brauner@kernel.org>
2026-08-26 18:02:22 +0200

    keyring-util: add keyring_find_by_name()
    Various kernel keyring such as .dm-verity, .fs-verity, and the .bpf
    keyring are not visible to request_key() and KEYCTL_SEARCH. So we can
    only learn their serial by parsing through /proc/keys.
    
    Note that it's possible to match more than one keyring. Userspace could
    create a ".dm-verity" keyring that would also show up in /proc/keys.
    Handle that case and report -ENOTUNIQ.
    
    Signed-off-by: Christian Brauner (Amutable) <brauner@kernel.org>

--- src/basic/keyring-util.h
+++ src/basic/keyring-util.h
@@ -10,3 +10,27 @@
 
 int keyring_read(key_serial_t serial, void **ret, size_t *ret_size);
 int keyring_describe(key_serial_t serial, char **ret);
+
+/* One line of /proc/keys, see proc_keys_show() in the kernel. */
+typedef struct ProcKeysEntry {
+        key_serial_t serial;
+        char flags[sizeof("IRDQUNi")]; /* '-' for unset */
+        uint32_t perm;
+        uid_t uid;
+        gid_t gid;
+        char *type;             /* truncated to nine characters by the kernel */
+        char *description;      /* type specific, may contain spaces */
+} ProcKeysEntry;
+
+void proc_keys_entry_done(ProcKeysEntry *e);
+int proc_keys_entry_parse(const char *line, ProcKeysEntry *ret);
+bool proc_keys_entry_is_keyring(const ProcKeysEntry *e, const char *name);
+
+int keyring_find_by_name(const char *name, uid_t owner, key_serial_t *ret);
+
+int keyring_describe_full(
+                key_serial_t serial,
+                char **ret_type,
+                uid_t *ret_uid,
+                uint32_t *ret_perm,
+                char **ret_description);
