d2e00702bc4484b861802653bfd7550738f5ddd8
Zbigniew Jędrzejewski-Szmek <zbyszek@amutable.com>
2026-08-26 10:14:51 +0200

    udevadm-control: use common verb help and add --introspect-cli
    The custom help() is replaced by the common verb help, which derives the
    synopsis, abstract, and option list from the verb declaration in
    udevadm.c and the "udevadm-control" option namespace. The synopsis
    is now "udevadm [OPTION…] control OPTION…".

--- src/udev/udevadm-control.c
+++ src/udev/udevadm-control.c
@@ -2,9 +2,9 @@
 
 #include <string.h>
 
+#include "sd-json.h"
+
 #include "creds-util.h"
-#include "format-table.h"
-#include "help-util.h"
 #include "log.h"
 #include "options.h"
 #include "parse-argument.h"
@@ -16,6 +16,7 @@
 #include "udev-varlink.h"
 #include "udevadm.h"
 #include "varlink-util.h"
+#include "verbs.h"
 #include "virt.h"
 
 static char **arg_env = NULL;
@@ -45,25 +46,6 @@ static bool arg_has_control_commands(void) {
                 arg_revert;
 }
 
-static int help(void) {
-        _cleanup_(table_unrefp) Table *options = NULL;
-        int r;
-
-        r = option_parser_get_help_table_ns("udevadm-control", &options);
-        if (r < 0)
-                return r;
-
-        help_cmdline("control OPTION");
-        help_abstract("Control the udev daemon.");
-        help_section("Options");
-        r = table_print_or_warn(options);
-        if (r < 0)
-                return r;
-
-        help_man_page_reference("udevadm", "8");
-        return 0;
-}
-
 static int parse_argv(int argc, char *argv[]) {
         int r;
 
@@ -78,7 +60,7 @@ static int parse_argv(int argc, char *argv[]) {
                 OPTION_NAMESPACE("udevadm-control"): {}
 
                 OPTION_COMMON_HELP:
-                        return help();
+                        return command_print_verb_help("control");
 
                 OPTION('V', "version", NULL, "Show package version"):
                         return print_version();
@@ -155,6 +137,9 @@ static int parse_argv(int argc, char *argv[]) {
                 OPTION_LONG("load-credentials", NULL, "Load udev rules from credentials"):
                         arg_load_credentials = true;
                         break;
+
+                OPTION_COMMON_INTROSPECT_CLI:
+                        return introspect_cli(SD_JSON_FORMAT_OFF);
                 }
 
         if (!arg_has_control_commands() && !arg_load_credentials)
