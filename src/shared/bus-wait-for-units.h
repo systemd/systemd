587bbf4a54dd8e04c35ead0036344f96376c338f
Lennart Poettering <lennart@amutable.com>
2026-08-31 17:36:58 +0200

    bus-wait-for-units: optionally pick up UnitResult upon unit completion
    We typically use bus-wait-for-units.c to wait for unit completion. Let's
    teach it to optionally also pick up the unit result, then.

--- src/shared/bus-wait-for-units.h
+++ src/shared/bus-wait-for-units.h
@@ -18,10 +18,13 @@ typedef enum BusWaitForUnitsFlags {
         BUS_WAIT_FOR_INACTIVE        = 1 << 1, /* Wait until the unit is back in inactive or dead state */
         BUS_WAIT_NO_JOB              = 1 << 2, /* Wait until there's no more job pending */
         BUS_WAIT_REFFED              = 1 << 3, /* The unit is already reffed with RefUnit() */
+        BUS_WAIT_COLLECT_RESULT      = 1 << 4, /* Query the unit's final state and resource usage once done, and pass it to the callback */
         _BUS_WAIT_FOR_TARGET         = BUS_WAIT_FOR_MAINTENANCE_END|BUS_WAIT_FOR_INACTIVE|BUS_WAIT_NO_JOB,
 } BusWaitForUnitsFlags;
 
-typedef void (*bus_wait_for_units_unit_callback_t)(BusWaitForUnits *d, const char *unit_path, bool good, void *userdata);
+/* Note: 'result' is non-NULL only if BUS_WAIT_COLLECT_RESULT was set for the unit. The callback may take
+ * ownership of its contents via TAKE_STRUCT(). */
+typedef void (*bus_wait_for_units_unit_callback_t)(BusWaitForUnits *d, const char *unit, bool good, UnitResult *result, void *userdata);
 
 int bus_wait_for_units_new(sd_bus *bus, BusWaitForUnits **ret);
 
