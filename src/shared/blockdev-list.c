7a9558bafa45f2037ee6a86693f9d7c23b9e2c58
Lennart Poettering <lennart@amutable.com>
2026-09-02 12:16:14 +0200

    blockdev-list: add blockdev_list_full() which allows passing in a pre-determined pair of root_devno/whole_root_devno

--- src/shared/blockdev-list.c
+++ src/shared/blockdev-list.c
@@ -355,7 +355,13 @@ skipped:
         return BLOCKDEV_LIST_MATCH_SKIPPED;
 }
 
-int blockdev_list(BlockDevListFlags flags, BlockDevice **ret_devices, size_t *ret_n_devices) {
+int blockdev_list_full(
+                BlockDevListFlags flags,
+                dev_t root_devno,
+                dev_t whole_root_devno,
+                BlockDevice **ret_devices,
+                size_t *ret_n_devices) {
+
         _cleanup_(sd_device_enumerator_unrefp) sd_device_enumerator *e = NULL;
         int r;
 
@@ -368,8 +374,8 @@ int blockdev_list(BlockDevListFlags flags, BlockDevice **ret_devices, size_t *re
         size_t n = 0;
         CLEANUP_ARRAY(l, n, block_device_array_free);
 
-        dev_t root_devno = 0, whole_root_devno = 0;
-        (void) blockdev_list_get_root_devnos(flags, &root_devno, &whole_root_devno);
+        if (root_devno == 0 && whole_root_devno == 0)
+                (void) blockdev_list_get_root_devnos(flags, &root_devno, &whole_root_devno);
 
         if (sd_device_enumerator_new(&e) < 0)
                 return log_oom();
