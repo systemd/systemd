6c975ff1659a3c0b24947a6115eadb48573c8dc4
Lennart Poettering <lennart@amutable.com>
2026-09-03 23:32:04 +0200

    sysupdate: add JSON constructors for component, feature and transfer definitions
    Add component_from_json(), feature_from_json() and transfer_from_json(),
    which fill in the respective objects from JSON objects shaped like the
    types of the io.systemd.SysUpdate.Provider interface, i.e. mirroring the
    settings of the configuration files one to one. The transfer constructor
    shares the validation and completion logic with the file parser via
    transfer_finalize(). In contrast to the configuration files no specifier
    expansion takes place.

--- src/sysupdate/sysupdate-config.h
+++ src/sysupdate/sysupdate-config.h
@@ -11,3 +11,6 @@ CONFIG_PARSER_PROTOTYPE(config_parse_protect_version);
 CONFIG_PARSER_PROTOTYPE(config_parse_version_bound);
 
 DECLARE_STRING_TABLE_LOOKUP_TO_STRING(suggest_on_type, ConditionType);
+
+/* JSON dispatchers for fields mirroring the settings above, used when definitions are acquired in JSON form */
+int json_dispatch_versions(const char *name, sd_json_variant *variant, sd_json_dispatch_flags_t flags, void *userdata);
