3dbe256f6b7f342830dfde05ba0d407c0093ae6c
Wenwen Fu <wenwfu@qti.qualcomm.com>
2026-09-06 13:39:36 +0800

    test: cover per-device terminal type for console TTY

--- test/units/TEST-07-PID1.sh
+++ test/units/TEST-07-PID1.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: LGPL-2.1-or-later
+# shellcheck disable=SC2016
 set -eux
 set -o pipefail
 
@@ -9,6 +10,21 @@ set -o pipefail
 # Issue: https://github.com/systemd/systemd/issues/2730
 # See TEST-07-PID1/test.sh for the first "half" of the test
 mountpoint /issue2730
+
+# Verify that a per-device terminal type is applied when /dev/console is backed by a serial TTY.
+assert_eq "$(cat /sys/class/tty/console/active)" ttyS0
+
+TERM_FILE=/run/TEST-07-PID1-console-term
+systemd-run --wait \
+        --unit=TEST-07-PID1-console-term.service \
+        -p TTYPath=/dev/console \
+        -p StandardInput=tty \
+        -p StandardOutput=null \
+        -p StandardError=null \
+        /bin/sh -c 'printf "%s" "$TERM" >/run/TEST-07-PID1-console-term'
+assert_eq "$(cat "$TERM_FILE")" linux
+
+rm -f "$TERM_FILE"
 
 run_subtests
 
