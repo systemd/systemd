127ce762c250b3b4d1113a0f9607b383c1538126
Zbigniew Jędrzejewski-Szmek <zbyszek@amutable.com>
2026-09-08 12:09:30 +0200

    test-af-list: finish conversion to ASSERT macros
    Follow-up for f9d273e67a52a44ffcde0749b2edd9dc8cfc214a.

--- src/test/test-af-list.c
+++ src/test/test-af-list.c
@@ -10,18 +10,17 @@ static const struct af_name* lookup_af(register const char *str, register GPERF_
 #include "af-to-name.inc"
 
 TEST(af_list) {
-        for (unsigned i = 0; i < ELEMENTSOF(af_names); i++) {
+        for (int i = 0; i < (int) ELEMENTSOF(af_names); i++)
                 if (af_names[i]) {
                         ASSERT_STREQ(af_to_name(i), af_names[i]);
-                        assert_se(af_from_name(af_names[i]) == (int) i);
+                        ASSERT_EQ(af_from_name(af_names[i]), i);
                 }
-        }
 
         ASSERT_NULL(af_to_name(af_max()));
         ASSERT_NULL(af_to_name(0));
         ASSERT_NULL(af_to_name(-1));
-        assert_se(af_from_name("huddlduddl") == -EINVAL);
-        assert_se(af_from_name("") == -EINVAL);
+        ASSERT_ERROR(af_from_name("huddlduddl"), EINVAL);
+        ASSERT_ERROR(af_from_name(""), EINVAL);
 }
 
 DEFINE_TEST_MAIN(LOG_INFO);
