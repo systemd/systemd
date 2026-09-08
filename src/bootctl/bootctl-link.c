06e2f46b85408b28803aaaa37468cb3ad141de39
Christian Brauner <brauner@kernel.org>
2026-09-08 18:17:10 +0200

    bootctl: open via the xopenat() family
    Use xopenat{_full}().
    
    Minor semantic changes:
    
    (1) O_CLOEXEC by default
    (2) O_CREAT open without O_EXCL refuses to create the file through a
        dangling symlink
    (3) Empty path reopens dir_fd instead of failing with ENOENT
    
    Signed-off-by: Christian Brauner (Amutable) <brauner@kernel.org>

--- src/bootctl/bootctl-link.c
+++ src/bootctl/bootctl-link.c
@@ -284,9 +284,9 @@ static int link_context_from_cmdline_common(LinkContext *b) {
                 return log_oom();
 
         if (arg_root) {
-                b->root_fd = open(arg_root, O_CLOEXEC|O_DIRECTORY|O_PATH);
+                b->root_fd = xopenat(AT_FDCWD, arg_root, O_DIRECTORY|O_PATH);
                 if (b->root_fd < 0)
-                        return log_error_errno(errno, "Failed to open root directory '%s': %m", arg_root);
+                        return log_error_errno(b->root_fd, "Failed to open root directory '%s': %m", arg_root);
 
                 if (strdup_to(&b->root, arg_root) < 0)
                         return log_oom();
@@ -1487,9 +1487,9 @@ static int vl_link_prepare(sd_varlink *link, LinkParameters *p) {
                                 p->context.root = mfree(p->context.root);
                 }
         } else if (p->context.root) {
-                p->context.root_fd = open(p->context.root, O_RDONLY|O_CLOEXEC|O_DIRECTORY);
+                p->context.root_fd = xopenat(AT_FDCWD, p->context.root, O_RDONLY|O_DIRECTORY);
                 if (p->context.root_fd < 0)
-                        return log_debug_errno(errno, "Failed to open '%s': %m", p->context.root);
+                        return log_debug_errno(p->context.root_fd, "Failed to open '%s': %m", p->context.root);
         } else
                 p->context.root_fd = XAT_FDROOT;
 
