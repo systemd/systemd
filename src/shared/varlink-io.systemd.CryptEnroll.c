1044b9c8b7a9c9156b15586cc078dc52ff9cf1d9
Julian Sparber <julian@sparber.net>
2026-09-08 11:58:41 +0200

    cryptenroll-varlink: Add method to generate a recovery-key
    We want users to use a recovery-key generated by systemd. Therefore
    expose a method that let's clients generate a key that can be given to
    the Enroll method. This way the key can be displayed before enrolling
    it.

--- src/shared/varlink-io.systemd.CryptEnroll.c
+++ src/shared/varlink-io.systemd.CryptEnroll.c
@@ -42,7 +42,7 @@ static SD_VARLINK_DEFINE_METHOD_FULL(
 
                 SD_VARLINK_FIELD_COMMENT("The passphrase to enroll, when mechanism is 'password'. Contains key material."),
                 SD_VARLINK_DEFINE_INPUT(password, SD_VARLINK_STRING, SD_VARLINK_NULLABLE),
-                SD_VARLINK_FIELD_COMMENT("A recovery key to enroll, when mechanism is 'recovery'. The key must be in the normalized form and have high entropy. Contains key material."),
+                SD_VARLINK_FIELD_COMMENT("A recovery key to enroll, when mechanism is 'recovery'. The key must be in the normalized form and have high entropy. Use MakeRecoveryKey() to generate the recovery key. Contains key material."),
                 SD_VARLINK_DEFINE_INPUT(recoveryKey, SD_VARLINK_STRING, SD_VARLINK_NULLABLE),
 
                 SD_VARLINK_FIELD_COMMENT("Path to the FIDO2 device to enroll, when mechanism is 'fido2'. Leave empty to automatically discover a suitable device."),
@@ -82,6 +82,11 @@ static SD_VARLINK_DEFINE_METHOD_FULL(
                 SD_VARLINK_FIELD_COMMENT("The type of the keyslot."),
                 SD_VARLINK_DEFINE_OUTPUT_BY_TYPE(type, EnrollMechanism, SD_VARLINK_NULLABLE));
 
+static SD_VARLINK_DEFINE_METHOD(
+                MakeRecoveryKey,
+                SD_VARLINK_FIELD_COMMENT("The generated recovery key."),
+                SD_VARLINK_DEFINE_OUTPUT(recoveryKey, SD_VARLINK_STRING, 0));
+
 static SD_VARLINK_DEFINE_ERROR(VolumeUnderForeignManagement);
 static SD_VARLINK_DEFINE_ERROR(PasswordRequired);
 static SD_VARLINK_DEFINE_ERROR(PasswordIncorrect);
@@ -98,6 +103,8 @@ SD_VARLINK_DEFINE_INTERFACE(
                 &vl_method_Enroll,
                 SD_VARLINK_SYMBOL_COMMENT("Enumerate the keyslots currently enrolled in a LUKS2 volume, one per reply. Must be called with the 'more' flag."),
                 &vl_method_ListSlots,
+                SD_VARLINK_SYMBOL_COMMENT("Generate a recovery key that can be enrolled using the 'recovery' mechanism in Enroll()."),
+                &vl_method_MakeRecoveryKey,
                 SD_VARLINK_SYMBOL_COMMENT("The volume is managed by another subsystem (e.g. systemd-homed) and may not be enrolled into directly."),
                 &vl_error_VolumeUnderForeignManagement,
                 SD_VARLINK_SYMBOL_COMMENT("A password is required to unlock the volume, but none was provided."),
