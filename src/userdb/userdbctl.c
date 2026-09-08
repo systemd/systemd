dab688041fea62e0b9a274ada933f5153efde7aa
Christian Brauner <brauner@kernel.org>
2026-09-08 18:17:10 +0200

    userdb: open via the xopenat() family
    Use xopenat{_full}().
    Minor semantic changes:
    
    (1) O_CLOEXEC by default
    (2) O_CREAT open without O_EXCL refuses to create the file through a
        dangling symlink
    (3) Empty path reopens dir_fd instead of failing with ENOENT
    
    Signed-off-by: Christian Brauner (Amutable) <brauner@kernel.org>

--- src/userdb/userdbctl.c
+++ src/userdb/userdbctl.c
@@ -1233,9 +1233,9 @@ static int write_membership(int dir_fd, const char *dir, const char *user, const
         if (!membership)
                 return log_oom();
 
-        _cleanup_close_ int fd = openat(dir_fd, membership, O_WRONLY|O_CREAT|O_CLOEXEC, 0644);
+        _cleanup_close_ int fd = xopenat_full(dir_fd, membership, O_WRONLY|O_CREAT, /* xopen_flags= */ 0, 0644);
         if (fd < 0)
-                return log_error_errno(errno, "Failed to create %s/%s: %m", dir, membership);
+                return log_error_errno(fd, "Failed to create %s/%s: %m", dir, membership);
 
         r = loop_write(fd, "{}\n", SIZE_MAX);
         if (r < 0)
