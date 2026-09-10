ea8ca687ea8fbb2a1f50ac63797b77ed9703bd93
Christian Brauner <brauner@kernel.org>
2026-09-10 15:09:47 +0200

    run-generator: comment bare literal arguments
    docs/CODING_STYLE.md asks for a /* name= */ comment whenever NULL or
    another value meaning "unset" is passed to a function. Add it wherever
    a bare NULL, 0, false or true is passed for a parameter with a telling
    name. Mechanical, generated by tools/check-argument-comments.py --fix.
    
    Signed-off-by: Christian Brauner (Amutable) <brauner@kernel.org>

--- src/run-generator/run-generator.c
+++ src/run-generator/run-generator.c
@@ -126,7 +126,7 @@ static int run(const char *dest, const char *dest_early, const char *dest_late) 
 
         assert_se(arg_dest = dest);
 
-        r = proc_cmdline_parse(parse, NULL, PROC_CMDLINE_RD_STRICT|PROC_CMDLINE_STRIP_RD_PREFIX);
+        r = proc_cmdline_parse(parse, /* userdata= */ NULL, PROC_CMDLINE_RD_STRICT|PROC_CMDLINE_STRIP_RD_PREFIX);
         if (r < 0)
                 log_warning_errno(r, "Failed to parse kernel command line, ignoring: %m");
 
