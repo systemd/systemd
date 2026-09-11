29d73f8d3e798ce95929d448a1aed9d249bb989c
Kai Wohlfahrt <kai@proximabio.com>
2026-09-11 10:11:25 +0200

    Revert "Fall back to checking sysext backing device"
    This reverts commit 164b6eebe712a5e3450b7a3dc8509d30898699a0.

--- src/shared/extension-util.c
+++ src/shared/extension-util.c
@@ -12,7 +12,6 @@
 #include "fd-util.h"
 #include "fileio.h"
 #include "log.h"
-#include "macro.h"
 #include "os-util.h"
 #include "path-util.h"
 #include "stat-util.h"
@@ -193,7 +192,6 @@ int extension_overlay_block(const char *p, ImageClass image_class, dev_t *ret) {
 
         assert(p);
         assert(ret);
-        assert(IN_SET(image_class, IMAGE_SYSEXT, IMAGE_CONFEXT));
 
         /* Tries to read the backing device information systemd-sysext puts in
          * the virtual file .systemd-sysext/.systemd-confext */
