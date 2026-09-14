59d11d04ab973ec33149e2d180bea16982155d6a
Chris Down <chris@chrisdown.name>
2026-09-14 12:39:22 +0800

    report: Expose per-unit pressure stall information
    We currently export systemwide metrics from /proc/pressure, but have
    nothing at unit granularity. Let's add it.

--- src/report/report-cgroup.c
+++ src/report/report-cgroup.c
@@ -13,6 +13,7 @@
 #include "metrics.h"
 #include "parse-util.h"
 #include "path-util.h"
+#include "psi-util.h"
 #include "report-cgroup.h"
 #include "string-util.h"
 #include "time-util.h"
@@ -250,6 +251,72 @@ static int memory_usage_send(
         return 0;
 }
 
+static int pressure_send(
+                const MetricFamily mf[static 2],
+                sd_varlink *link,
+                const char *path,
+                const char *unit) {
+
+        int r;
+
+        assert(mf && mf[0].name && mf[1].name);
+        assert(link);
+        assert(path);
+        assert(unit);
+
+        for (PressureResource resource = 0; resource < _PRESSURE_RESOURCE_MAX; resource++) {
+                const char *name = pressure_resource_to_string(resource);
+
+                _cleanup_free_ char *attribute = strjoin(name, ".pressure");
+                if (!attribute)
+                        return log_oom();
+
+                _cleanup_free_ char *p = NULL;
+                r = cg_get_path(path, attribute, &p);
+                if (r < 0)
+                        return log_oom();
+
+                PressureType type;
+                FOREACH_ARGUMENT(type, PRESSURE_TYPE_SOME, PRESSURE_TYPE_FULL) {
+                        ResourcePressure rp;
+
+                        r = read_resource_pressure(p, type, &rp);
+                        if (r < 0) {
+                                if (!IN_SET(r, -ENOENT, -ENODATA) && !ERRNO_IS_NEG_NOT_SUPPORTED(r))
+                                        log_debug_errno(r, "Failed to read %s, ignoring: %m", p);
+                                break; /* If "some" can't be read, "full" won't work either */
+                        }
+
+                        _cleanup_(sd_json_variant_unrefp) sd_json_variant *fields = NULL;
+                        r = sd_json_buildo(&fields,
+                                           SD_JSON_BUILD_PAIR_STRING("resource", name),
+                                           SD_JSON_BUILD_PAIR_STRING("type", pressure_type_to_string(type)));
+                        if (r < 0)
+                                return r;
+
+                        r = metric_build_send_double(
+                                        mf,
+                                        link,
+                                        unit,
+                                        (double) rp.avg10 / LOADAVG_FIXED_POINT_1_0,
+                                        fields);
+                        if (r < 0)
+                                return r;
+
+                        r = metric_build_send_double(
+                                        mf + 1,
+                                        link,
+                                        unit,
+                                        (double) rp.total / USEC_PER_SEC,
+                                        fields);
+                        if (r < 0)
+                                return r;
+                }
+        }
+
+        return 0;
+}
+
 static int tasks_current_send(
                 const MetricFamily *mf,
                 sd_varlink *link,
@@ -275,14 +342,18 @@ static int tasks_current_send(
 }
 
 static int walk_cgroups(
-                const MetricFamily mf[static 5],
+                const MetricFamily mf[static 7],
                 sd_varlink *link,
-                const char *path) {
+                const char *path,
+                bool pressure_supported) {
 
         int r;
 
-        assert(mf && mf[0].name && mf[1].name && mf[2].name && mf[3].name && mf[4].name);
-        assert(mf[0].generate && !mf[1].generate && !mf[2].generate && !mf[3].generate && !mf[4].generate);
+        assert(mf);
+        for (size_t i = 0; i < 7; i++) {
+                assert(mf[i].name);
+                assert(!mf[i].generate == (i > 0));
+        }
         assert(path);
 
         _cleanup_free_ char *unit = NULL;
@@ -300,7 +371,13 @@ static int walk_cgroups(
                 if (r < 0)
                         return r;
 
-                r = tasks_current_send(mf + 4, link, path, unit);
+                if (pressure_supported) {
+                        r = pressure_send(mf + 4, link, path, unit);
+                        if (r < 0)
+                                return r;
+                }
+
+                r = tasks_current_send(mf + 6, link, path, unit);
                 if (r < 0)
                         return r;
 
@@ -338,7 +415,7 @@ static int walk_cgroups(
 
                 path_simplify(child);
 
-                r = walk_cgroups(mf, link, child);
+                r = walk_cgroups(mf, link, child, pressure_supported);
                 if (r < 0)
                         return r;
         }
@@ -347,7 +424,7 @@ static int walk_cgroups(
 }
 
 static int cgroup_stats_send(
-                const MetricFamily mf[static 5],
+                const MetricFamily mf[static 7],
                 sd_varlink *link,
                 void *userdata) {
 
@@ -355,7 +432,7 @@ static int cgroup_stats_send(
         assert(link);
         assert(!userdata);
 
-        return walk_cgroups(mf, link, "");
+        return walk_cgroups(mf, link, "", is_pressure_supported() > 0);
 }
 
 static const MetricFamily cgroup_metric_family_table[] = {
@@ -380,6 +457,16 @@ static const MetricFamily cgroup_metric_family_table[] = {
                 METRIC_IO_SYSTEMD_CGROUP_PREFIX "MemoryUsage",
                 "Per unit metric: memory usage in bytes",
                 METRIC_FAMILY_TYPE_GAUGE,
+        },
+        {
+                METRIC_IO_SYSTEMD_CGROUP_PREFIX "PressureAvg10",
+                "Per unit metric: pressure stall percentage over the last 10s (resource=cpu|memory|io, type=some|full)",
+                METRIC_FAMILY_TYPE_GAUGE,
+        },
+        {
+                METRIC_IO_SYSTEMD_CGROUP_PREFIX "PressureStallSeconds",
+                "Per unit metric: total time stalled in seconds (resource=cpu|memory|io, type=some|full)",
+                METRIC_FAMILY_TYPE_COUNTER,
         },
         {
                 METRIC_IO_SYSTEMD_CGROUP_PREFIX "TasksCurrent",
