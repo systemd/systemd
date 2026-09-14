798cad2f88ccb0c07e6729a4b034725ba288bdfe
Chris Down <chris@chrisdown.name>
2026-09-14 16:30:06 +0800

    test-psi-util: Convert to new assertion macros

--- src/test/test-psi-util.c
+++ src/test/test-psi-util.c
@@ -14,62 +14,62 @@ TEST(read_mem_pressure) {
         _cleanup_close_ int fd = -EBADF;
         ResourcePressure rp;
 
-        assert_se((fd = mkostemp_safe(path)) >= 0);
+        ASSERT_OK(fd = mkostemp_safe(path));
 
-        assert_se(read_resource_pressure("/verylikelynonexistentpath", PRESSURE_TYPE_SOME, &rp) < 0);
-        assert_se(read_resource_pressure(path, PRESSURE_TYPE_SOME, &rp) < 0);
+        ASSERT_LT(read_resource_pressure("/verylikelynonexistentpath", PRESSURE_TYPE_SOME, &rp), 0);
+        ASSERT_LT(read_resource_pressure(path, PRESSURE_TYPE_SOME, &rp), 0);
 
-        assert_se(write_string_file(path, "herpdederp\n", WRITE_STRING_FILE_CREATE) == 0);
-        assert_se(read_resource_pressure(path, PRESSURE_TYPE_SOME, &rp) < 0);
+        ASSERT_OK(write_string_file(path, "herpdederp\n", WRITE_STRING_FILE_CREATE));
+        ASSERT_LT(read_resource_pressure(path, PRESSURE_TYPE_SOME, &rp), 0);
 
         /* Pressure file with some invalid values */
-        assert_se(write_string_file(path, "some avg10=0.22=55 avg60=0.17=8 avg300=1.11=00 total=58761459\n"
-                                          "full avg10=0.23=55 avg60=0.16=8 avg300=1.08=00 total=58464525", WRITE_STRING_FILE_CREATE) == 0);
-        assert_se(read_resource_pressure(path, PRESSURE_TYPE_SOME, &rp) < 0);
+        ASSERT_OK(write_string_file(path, "some avg10=0.22=55 avg60=0.17=8 avg300=1.11=00 total=58761459\n"
+                                         "full avg10=0.23=55 avg60=0.16=8 avg300=1.08=00 total=58464525", WRITE_STRING_FILE_CREATE));
+        ASSERT_LT(read_resource_pressure(path, PRESSURE_TYPE_SOME, &rp), 0);
 
         /* Same pressure valid values as below but with duplicate avg60 field */
-        assert_se(write_string_file(path, "some avg10=0.22 avg60=0.17 avg60=0.18 avg300=1.11 total=58761459\n"
-                                          "full avg10=0.23 avg60=0.16 avg300=1.08 total=58464525", WRITE_STRING_FILE_CREATE) == 0);
-        assert_se(read_resource_pressure(path, PRESSURE_TYPE_SOME, &rp) < 0);
+        ASSERT_OK(write_string_file(path, "some avg10=0.22 avg60=0.17 avg60=0.18 avg300=1.11 total=58761459\n"
+                                         "full avg10=0.23 avg60=0.16 avg300=1.08 total=58464525", WRITE_STRING_FILE_CREATE));
+        ASSERT_LT(read_resource_pressure(path, PRESSURE_TYPE_SOME, &rp), 0);
 
-        assert_se(write_string_file(path, "some avg10=0.22 avg60=0.17 avg300=1.11 total=58761459\n"
-                                          "full avg10=0.23 avg60=0.16 avg300=1.08 total=58464525", WRITE_STRING_FILE_CREATE) == 0);
-        assert_se(read_resource_pressure(path, PRESSURE_TYPE_SOME, &rp) == 0);
-        assert_se(LOADAVG_INT_SIDE(rp.avg10) == 0);
-        assert_se(LOADAVG_DECIMAL_SIDE(rp.avg10) == 22);
-        assert_se(LOADAVG_INT_SIDE(rp.avg60) == 0);
-        assert_se(LOADAVG_DECIMAL_SIDE(rp.avg60) == 17);
-        assert_se(LOADAVG_INT_SIDE(rp.avg300) == 1);
-        assert_se(LOADAVG_DECIMAL_SIDE(rp.avg300) == 11);
-        assert_se(rp.total == 58761459);
-        assert_se(read_resource_pressure(path, PRESSURE_TYPE_FULL, &rp) == 0);
-        assert_se(LOADAVG_INT_SIDE(rp.avg10) == 0);
-        assert_se(LOADAVG_DECIMAL_SIDE(rp.avg10) == 23);
-        assert_se(LOADAVG_INT_SIDE(rp.avg60) == 0);
-        assert_se(LOADAVG_DECIMAL_SIDE(rp.avg60) == 16);
-        assert_se(LOADAVG_INT_SIDE(rp.avg300) == 1);
-        assert_se(LOADAVG_DECIMAL_SIDE(rp.avg300) == 8);
-        assert_se(rp.total == 58464525);
+        ASSERT_OK(write_string_file(path, "some avg10=0.22 avg60=0.17 avg300=1.11 total=58761459\n"
+                                         "full avg10=0.23 avg60=0.16 avg300=1.08 total=58464525", WRITE_STRING_FILE_CREATE));
+        ASSERT_OK(read_resource_pressure(path, PRESSURE_TYPE_SOME, &rp));
+        ASSERT_EQ(LOADAVG_INT_SIDE(rp.avg10), 0U);
+        ASSERT_EQ(LOADAVG_DECIMAL_SIDE(rp.avg10), 22U);
+        ASSERT_EQ(LOADAVG_INT_SIDE(rp.avg60), 0U);
+        ASSERT_EQ(LOADAVG_DECIMAL_SIDE(rp.avg60), 17U);
+        ASSERT_EQ(LOADAVG_INT_SIDE(rp.avg300), 1U);
+        ASSERT_EQ(LOADAVG_DECIMAL_SIDE(rp.avg300), 11U);
+        ASSERT_EQ(rp.total, 58761459U);
+        ASSERT_OK(read_resource_pressure(path, PRESSURE_TYPE_FULL, &rp));
+        ASSERT_EQ(LOADAVG_INT_SIDE(rp.avg10), 0U);
+        ASSERT_EQ(LOADAVG_DECIMAL_SIDE(rp.avg10), 23U);
+        ASSERT_EQ(LOADAVG_INT_SIDE(rp.avg60), 0U);
+        ASSERT_EQ(LOADAVG_DECIMAL_SIDE(rp.avg60), 16U);
+        ASSERT_EQ(LOADAVG_INT_SIDE(rp.avg300), 1U);
+        ASSERT_EQ(LOADAVG_DECIMAL_SIDE(rp.avg300), 8U);
+        ASSERT_EQ(rp.total, 58464525U);
 
         /* Pressure file with extra unsupported fields */
-        assert_se(write_string_file(path, "some avg5=0.55 avg10=0.22 avg60=0.17 avg300=1.11 total=58761459\n"
-                                          "full avg10=0.23 avg60=0.16 avg300=1.08 avg600=2.00 total=58464525", WRITE_STRING_FILE_CREATE) == 0);
-        assert_se(read_resource_pressure(path, PRESSURE_TYPE_SOME, &rp) == 0);
-        assert_se(LOADAVG_INT_SIDE(rp.avg10) == 0);
-        assert_se(LOADAVG_DECIMAL_SIDE(rp.avg10) == 22);
-        assert_se(LOADAVG_INT_SIDE(rp.avg60) == 0);
-        assert_se(LOADAVG_DECIMAL_SIDE(rp.avg60) == 17);
-        assert_se(LOADAVG_INT_SIDE(rp.avg300) == 1);
-        assert_se(LOADAVG_DECIMAL_SIDE(rp.avg300) == 11);
-        assert_se(rp.total == 58761459);
-        assert_se(read_resource_pressure(path, PRESSURE_TYPE_FULL, &rp) == 0);
-        assert_se(LOADAVG_INT_SIDE(rp.avg10) == 0);
-        assert_se(LOADAVG_DECIMAL_SIDE(rp.avg10) == 23);
-        assert_se(LOADAVG_INT_SIDE(rp.avg60) == 0);
-        assert_se(LOADAVG_DECIMAL_SIDE(rp.avg60) == 16);
-        assert_se(LOADAVG_INT_SIDE(rp.avg300) == 1);
-        assert_se(LOADAVG_DECIMAL_SIDE(rp.avg300) == 8);
-        assert_se(rp.total == 58464525);
+        ASSERT_OK(write_string_file(path, "some avg5=0.55 avg10=0.22 avg60=0.17 avg300=1.11 total=58761459\n"
+                                         "full avg10=0.23 avg60=0.16 avg300=1.08 avg600=2.00 total=58464525", WRITE_STRING_FILE_CREATE));
+        ASSERT_OK(read_resource_pressure(path, PRESSURE_TYPE_SOME, &rp));
+        ASSERT_EQ(LOADAVG_INT_SIDE(rp.avg10), 0U);
+        ASSERT_EQ(LOADAVG_DECIMAL_SIDE(rp.avg10), 22U);
+        ASSERT_EQ(LOADAVG_INT_SIDE(rp.avg60), 0U);
+        ASSERT_EQ(LOADAVG_DECIMAL_SIDE(rp.avg60), 17U);
+        ASSERT_EQ(LOADAVG_INT_SIDE(rp.avg300), 1U);
+        ASSERT_EQ(LOADAVG_DECIMAL_SIDE(rp.avg300), 11U);
+        ASSERT_EQ(rp.total, 58761459U);
+        ASSERT_OK(read_resource_pressure(path, PRESSURE_TYPE_FULL, &rp));
+        ASSERT_EQ(LOADAVG_INT_SIDE(rp.avg10), 0U);
+        ASSERT_EQ(LOADAVG_DECIMAL_SIDE(rp.avg10), 23U);
+        ASSERT_EQ(LOADAVG_INT_SIDE(rp.avg60), 0U);
+        ASSERT_EQ(LOADAVG_DECIMAL_SIDE(rp.avg60), 16U);
+        ASSERT_EQ(LOADAVG_INT_SIDE(rp.avg300), 1U);
+        ASSERT_EQ(LOADAVG_DECIMAL_SIDE(rp.avg300), 8U);
+        ASSERT_EQ(rp.total, 58464525U);
 }
 
 DEFINE_TEST_MAIN(LOG_DEBUG);
