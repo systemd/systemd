e75dbf8e1c7bcb1fffc6929707785dca7ada0285
Alberto Planas <aplanas@suse.com>
2026-09-14 11:16:00 +0200

    tpm2-util: allow mixing a FIDO2 hmac-secret into the TPM2 authValue
    In preparation for binding a TPM2 enrollment to a FIDO2 token, teach the
    seal/unseal paths to derive the authValue of the sealed object from a
    FIDO2 hmac-secret in addition to (or instead of) a PIN.
    
    tpm2_auth_value_from_pin() becomes tpm2_auth_value_from_pin_and_fido2()
    and hashes the PIN first, then extends the digest with the FIDO2 secret,
    so that either one alone or both together can protect the object. It
    also absorbs the "nothing to do" case that the callers used to guard
    themselves, which lets tpm2_seal() and tpm2_calculate_seal_private()
    drop their conditionals.
    
    Accordingly tpm2_calculate_sealing_policy() and
    tpm2_build_sealing_policy() gain a use_fido2 argument, as a FIDO2 secret
    requires a PolicyAuthValue in the policy exactly like a PIN does.
    
    No behaviour change: all call sites pass no FIDO2 secret for now.
    
    Signed-off-by: Alberto Planas <aplanas@suse.com>

--- src/repart/repart.c
+++ src/repart/repart.c
@@ -5980,6 +5980,7 @@ static int partition_encrypt(Context *context, Partition *p, PartitionTarget *ta
                                 iovec_is_set(&pubkey) ? &public : NULL,
                                 iovec_is_set(&pubkey) ? arg_tpm2_public_key_policyref : NULL,
                                 /* use_pin= */ false,
+                                /* use_fido2= */ false,
                                 arg_tpm2_pcrlock && !iovec_is_set(&pubkey) ? &pcrlock_policy : NULL,
                                 policy_hash + 0);
                 if (r < 0)
@@ -5992,6 +5993,7 @@ static int partition_encrypt(Context *context, Partition *p, PartitionTarget *ta
                                         /* public= */ NULL,      /* Turn this one off for the 2nd shard */
                                         /* pubkey_policy_ref= */ NULL,
                                         /* use_pin= */ false,
+                                        /* use_fido2= */ false,
                                         &pcrlock_policy,         /* But turn this one on */
                                         policy_hash + 1);
                         if (r < 0)
@@ -6022,6 +6024,7 @@ static int partition_encrypt(Context *context, Partition *p, PartitionTarget *ta
                                         /* secret= */ NULL,
                                         policy_hash + 0,
                                         /* pin= */ NULL,
+                                        /* fido2_secret= */ NULL,
                                         &secret,
                                         blobs + 0,
                                         &srk);
@@ -6031,6 +6034,7 @@ static int partition_encrypt(Context *context, Partition *p, PartitionTarget *ta
                                       policy_hash,
                                       n_policy_hash,
                                       /* pin= */ NULL,
+                                      /* fido2_secret= */ NULL,
                                       &secret,
                                       &blobs,
                                       &n_blobs,
