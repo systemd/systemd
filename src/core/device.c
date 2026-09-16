3563b23b4ecb12f60409c0a6ccb689c97205fa71
Yu Watanabe <watanabe.yu+github@gmail.com>
2026-09-17 08:33:01 +0900

    core/device: restore sysfs path by the deserialized value
    Otherwise, device_update_found_by_sysfs() may not work for the device.
    This is especially important when the first uevent for the device after
    startup is move/remove.
    
    Preparation for the next commit.

--- src/core/device.c
+++ src/core/device.c
@@ -116,6 +116,37 @@ static int device_set_sysfs(Device *d, const char *sysfs) {
         return 0;
 }
 
+static int device_restore_sysfs(Device *d, DeviceState state) {
+        int r;
+
+        assert(d);
+
+        if (!d->deserialized_sysfs)
+                return 0;
+
+        _cleanup_free_ char *syspath = TAKE_PTR(d->deserialized_sysfs);
+
+        if (state == DEVICE_DEAD)
+                return 0; /* The device is already dead. */
+
+        if (d->sysfs)
+                return 0; /* The device is already enumerated. */
+
+        _cleanup_(sd_device_unrefp) sd_device *dev = NULL;
+        r = sd_device_new_from_syspath(&dev, syspath);
+        if (r < 0)
+                return log_unit_debug_errno(
+                                UNIT(d), r,
+                                "Failed to validate '%s', assuming the device has already been removed: %m",
+                                syspath);
+
+        r = device_set_sysfs(d, syspath);
+        if (r < 0) /* Do not propagate the failure here. */
+                log_unit_warning_errno(UNIT(d), r, "Failed to set sysfs path '%s', ignoring: %m", syspath);
+
+        return 0;
+}
+
 static void device_init(Unit *u) {
         Device *d = ASSERT_PTR(DEVICE(u));
 
@@ -306,19 +337,15 @@ static int device_coldplug(Unit *u) {
                 found &= ~DEVICE_FOUND_UDEV;
                 if (state == DEVICE_PLUGGED)
                         state = DEVICE_TENTATIVE;
+        }
 
-                /* Also check the validity of the device syspath. Without this check, if the device was
-                 * removed while switching root, it would never go to inactive state, as both Device.found
-                 * and Device.enumerated_found do not have the DEVICE_FOUND_UDEV flag, so device_catchup() in
-                 * device_update_found_one() does nothing in most cases. See issue #25106. Note that the
-                 * syspath field is only serialized when systemd is sufficiently new and the device has been
-                 * already processed by udevd. */
-                if (d->deserialized_sysfs) {
-                        _cleanup_(sd_device_unrefp) sd_device *dev = NULL;
-
-                        if (sd_device_new_from_syspath(&dev, d->deserialized_sysfs) < 0)
-                                state = DEVICE_DEAD;
-                }
+        /* Restore the syspath from the deserialized value so that device_update_found_by_sysfs() can find
+         * the device. This is especially important when the first uevent for the device after startup is
+         * 'move' or 'remove'. If the deserialized sysfs path cannot be validated, assume the device has
+         * already been removed, hence drop the DEVICE_FOUND_UDEV flag and enter the DEVICE_DEAD state. */
+        if (device_restore_sysfs(d, state) < 0) {
+                found &= ~DEVICE_FOUND_UDEV;
+                state = DEVICE_DEAD;
         }
 
         if (d->found == found && d->state == state)
