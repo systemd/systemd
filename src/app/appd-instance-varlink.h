c595de13f2c256854f0e39984d35d45095b35ed1
Adrian Vovk <adrianvovk@gmail.com>
2026-09-23 16:02:16 -0400

    appd: Add notification mechanism for app instances
    Allows us to notify other services of changes to app instances. We
    connect to sockets in a drop-in directory, rather than doing the normal
    Varlink subscription pattern, because we use this notification mechanism
    to synchronize.
    
    For instance: let's say xdg-desktop-portal requests to change the
    instance's available permissions, to grant it access to the system
    camera. If we queue up some notifications and then return to
    xdg-desktop-portal, then the app might immediately act on the assumption
    that it now has access to the system's camera. However, since we didn't
    synchronize to the app instance monitors, that means that pipewire (the
    service actually enforcing the permission) might not have found out
    about the change yet. The app would, thus, get an unexpected permission
    denied error.
    
    We close this race condition by waiting for the the monitoring services
    to finish applying the changes before we return from the
    SetPermissions() call

--- src/app/appd-instance-varlink.h
+++ src/app/appd-instance-varlink.h
@@ -5,3 +5,14 @@
 
 int manager_instance_varlink_init(Manager *m);
 void manager_instance_varlink_done(Manager *m);
+
+typedef enum AppInstanceEvent {
+        APP_INSTANCE_CHANGED,
+        APP_INSTANCE_DISAPPEARED,
+        _APP_INSTANCE_EVENT_MAX,
+        _APP_INSTANCE_EVENT_INVALID = -EINVAL,
+} AppInstanceEvent;
+
+typedef int (*app_instance_notify_finished_t)(void *userdata);
+
+int app_instance_notify(AppInstance *instance, AppInstanceEvent event, app_instance_notify_finished_t callback, void *userdata);
